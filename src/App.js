7291096fc36bdf13cc6c41cc514cf8564715cce0
0imbean0 <tjsqls2003@gmail.com>
2021-04-21 15:17:22 +0900

    chore: CRA & StoryBook & Prettier 초기 설정

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
