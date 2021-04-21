2eb2665052e893edf111a223bb6a01675b12caf9
yujo <bedro27@gmail.com>
2021-04-21 18:35:18 +0900

    chore: scripts 명령어와 eslint config 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['react-app', 'prettier'],
   rules: {
     'max-depth': ['error', 2],
+    'import/no-anonymous-default-export': off,
   },
   plugins: ['jsx-a11y'],
 };
