40e46a5fff8a0f182600d5059608b517fcd55cb7
Tanney <dhxodms89@gmail.com>
2021-04-21 18:20:01 +0900

    feat: app 레이아웃 구성

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Header from "../stories/Header";
+import "../style/index.css";
+
+const PAGE = {
+  HOME: {
+    ID: "HOME",
+    NAME: "보유카드",
+  },
+  CARD_ADDITION: {
+    ID: "CARD_ADDITION",
+    NAME: "카드추가",
+  },
+  SUCCESS_CARD_ADDITION: {
+    ID: "SUCCESS_CARD_ADDITION",
+    NAME: "",
+  },
+};
+
+function App() {
+  const [page, setPage] = useState(PAGE.HOME);
+  const pageHandler = {
+    [PAGE.HOME.ID]: null,
+    [PAGE.CARD_ADDITION.ID]: () => setPage(PAGE.HOME),
+    [PAGE.SUCCESS_CARD_ADDITION.ID]: null,
+  };
+  const mainComponent = {
+    [PAGE.HOME.ID]: (
+      <div>
+        home
+        <button onClick={() => setPage(PAGE.CARD_ADDITION)}>
+          go card addition
+        </button>
+      </div>
+    ),
+    [PAGE.CARD_ADDITION.ID]: <div>card</div>,
+    [PAGE.SUCCESS_CARD_ADDITION.ID]: "",
+  };
+
+  return (
+    <div className="app">
+      <Header title={page.NAME} onPageBack={pageHandler[page.ID]} />
+      <main>{mainComponent[page.ID]}</main>
+    </div>
+  );
+}
+
+export default App;
