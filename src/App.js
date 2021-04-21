19d1a1ee4d7456b9623b245e56fae229b5ae6c0c
Shim MunSeong <Puterism.K@gmail.com>
2021-04-21 12:04:45 +0900

    ui: 폰트색, 배경색 theme 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { ThemeProvider } from '@emotion/react';
 import GlobalStyle from './GlobalStyle';
+
+const theme = {
+  color: {
+    normal: '#383838',
+    label: '#525252',
+    card: {
+      companyName: '#383838',
+      number: '#525252',
+    },
+  },
+  bgColor: '#E5E5E5',
+};
 
 const App = () => (
   <>
     <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <></>
+    </ThemeProvider>
   </>
 );
 
