d2d4fca15517d00fac934d9e73fde11b2b4193b8
swon3210 <swon3210@gmail.com>
2021-04-21 18:53:04 +0900

    styles: GuideInput 컴포넌트 제작

--- src/components/Input/GuideInput/GuideInput.jsx
+++ src/components/Input/GuideInput/GuideInput.jsx
@@ -3,18 +3,23 @@ import classNames from "classnames/bind";
 import "../../../app.css";
 import styles from "./GuideInput.module.scss";
 import Input from "../Input";
+import cardCVC from "../../../assets/images/card-cvc.png";
+import questionMark from "../../../assets/svgs/question-mark.svg";
 
 const cx = classNames.bind(styles);
 
-const GuideInput = ({ labelText, width }) => {
+const GuideInput = ({ labelText, width, guideIcon = questionMark, guideImage = cardCVC }) => {
   return (
     <div className={cx("guide-input")}>
       <label className={cx("guide-input__label")} name={labelText}>
         {labelText}
       </label>
-      <div>
+      <div className={cx("guide-input__bottom")}>
         <Input width={width} />
-        <img></img>
+        <div className={cx("guide-input__guide-icon")}>
+          <img src={guideIcon} alt="안내 아이콘" />
+        </div>
+        <img className={cx("guide-input__guide-image")} src={guideImage} />
       </div>
     </div>
   );
