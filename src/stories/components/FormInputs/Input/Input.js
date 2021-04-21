44498955eb9fa0b04286d5a5fdd5bf9e6f3fa132
sunhpark42 <sunhpark42@gmail.com>
2021-04-21 15:17:37 +0900

    feat: Input Component 구현

--- src/stories/components/FormInputs/Input/Input.js
+++ src/stories/components/FormInputs/Input/Input.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import './input.css';
 
-export default function Input() {
-  return <></>;
+export default function Input({
+  width,
+  label,
+  type,
+  placeholder,
+  textAlign,
+  required,
+  maxLength,
+  letterCounter,
+}) {
+  return (
+    <label className="basic-input__label">
+      {(label || letterCounter) && (
+        <p>
+          {label && <span className="label-text">{label}</span>}
+          {letterCounter && (
+            <span className="letter-counter">{`${letterCounter.current}/${letterCounter.max}`}</span>
+          )}
+        </p>
+      )}
+      <input
+        className={['basic-input__input', `text-${textAlign}`].join(' ')}
+        type={type}
+        placeholder={placeholder}
+        required={required}
+        maxLength={maxLength}
+        style={{ width }}
+      />
+    </label>
+  );
 }
--- src/stories/components/FormInputs/Input/Input.stories.js
+++ src/stories/components/FormInputs/Input/Input.stories.js
@@ -10,4 +10,17 @@ const Template = (args) => <Input {...args} />;
 
 export const Default = Template.bind({});
 
-Default.args = {};
+Default.args = {
+  width: '318px',
+  label: '이름',
+  type: 'text',
+  placeholder: '이름을 입력하세요',
+  textAlign: 'center',
+  required: false,
+  currentInputLength: 0,
+  maxLength: 30,
+  letterCounter: {
+    current: 0,
+    max: 30,
+  },
+};
