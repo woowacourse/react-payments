d0b35704a2cf138c688f60a693dbbb257fe45ec2
swon3210 <swon3210@gmail.com>
2021-04-21 15:25:25 +0900

    styles: 카드 컴포넌트 제작

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,50 @@
+import classNames from "classnames/bind";
+import styles from "./Card.module.scss";
+
+const cx = classNames.bind(styles);
+
+// TODO : 배경색에 따라 글자 색이 바뀌도록 만들기
+const Card = ({ cardName, backgroundColor, cardNumberList, cardOwner, cardExpiration }) => {
+  const [firstCardNumbers, secondCardNumbers, thirdCardNumbers, fourthCardNumbers] = cardNumberList;
+
+  return (
+    <div className={cx("card")} style={{ backgroundColor }}>
+      <span className={cx("card__name")}>{cardName}</span>
+      <div className={cx("card__chip")}></div>
+      <div className={cx("card__number")}>
+        <input
+          type="text"
+          name="카드번호 입력"
+          className={cx("card__number-input", "card__number-input--visible")}
+          value={firstCardNumbers.join("")}
+        />
+        <input
+          type="text"
+          name="카드번호 입력"
+          className={cx("card__number-input", "card__number-input--visible")}
+          value={secondCardNumbers.join("")}
+        />
+        <input
+          type="password"
+          name="카드번호 입력"
+          className={cx("card__number-input", "card__number-input--invisible")}
+          value={thirdCardNumbers.join("")}
+          disabled
+        />
+        <input
+          type="password"
+          name="카드번호 입력"
+          className={cx("card__number-input", "card__number-input--invisible")}
+          value={fourthCardNumbers.join("")}
+          disabled
+        />
+      </div>
+      <div className={cx("card__bottom")}>
+        <span className={cx("card__owner")}>{cardOwner}</span>
+        <span className={cx("card__expiration")}>{cardExpiration}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
