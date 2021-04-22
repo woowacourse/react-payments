8a59b82f828293b777d45c51021295d98080e664
Tanney <dhxodms89@gmail.com>
2021-04-22 21:50:16 +0900

    feat: 카드 번호 포맷팅 구현

--- src/components/CardAddition.js
+++ src/components/CardAddition.js
@@ -1,10 +1,99 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Card from "../stories/Card";
 import { CARD, CARD_SIZE } from "../stories/constants/card";
 import Input from "../stories/Input";
 
+const splitCardNumbers = (value) => {
+  const splitNumbers = [];
+  let i;
+
+  for (i = 0; i < value.length / 4; i++) {
+    splitNumbers.push(value.slice(i * 4, (i + 1) * 4));
+  }
+  if (value[i * 4] !== undefined) {
+    splitNumbers.push(value.slice(i * 4, (i + 1) * 4));
+  }
+
+  return splitNumbers;
+};
+
+const formatCardNumbers = (numbers) => {
+  const hiddenNumbers = numbers
+    .slice(2)
+    .map((value) => "•".repeat(value.length));
+
+  return [...numbers.slice(0, 2), ...hiddenNumbers].join(" - ");
+};
+
+const unformatCardNumbers = (formattedValue) => {
+  return formattedValue.replace(/[\s-]/g, "");
+};
+
+const isNonNumberValue = (value) => {
+  return /[^0-9]/g.test(value);
+};
+
 function CardAddition(props) {
   const [cardType, setCardType] = useState(CARD.UNKNOWN);
+  const [cardNumbers, setCardNumbers] = useState([]);
+  const [selectionStart, setSelectionStart] = useState(0);
+
+  const cardNumbersInputRef = useRef();
+
+  useEffect(() => {
+    cardNumbersInputRef?.current?.setSelectionRange(
+      selectionStart,
+      selectionStart
+    );
+  }, [cardNumbers, selectionStart]);
+
+  const onCardNumbersChange = (event) => {
+    const { value, selectionStart } = event.target;
+    const joinedCardNumbers = cardNumbers.join(" - ");
+    const diff = value.length - joinedCardNumbers.length;
+    let updatedCardNumbers = joinedCardNumbers;
+
+    const mod = selectionStart % 7;
+    if (mod === 0 || mod === 6 || mod === 5) {
+      setSelectionStart(selectionStart + ((8 - mod) % 7));
+    } else {
+      setSelectionStart(selectionStart);
+    }
+
+    switch (true) {
+      case diff > 0:
+        if (isNonNumberValue(value[selectionStart - 1])) {
+          return;
+        }
+
+        updatedCardNumbers =
+          joinedCardNumbers.slice(0, selectionStart - 1) +
+          value[selectionStart - 1] +
+          joinedCardNumbers.slice(selectionStart - 1);
+        break;
+      case diff === 0:
+        if (isNonNumberValue(value[selectionStart - 1])) {
+          return;
+        }
+
+        updatedCardNumbers =
+          joinedCardNumbers.slice(0, selectionStart - 1) +
+          value[selectionStart - 1] +
+          joinedCardNumbers.slice(selectionStart);
+        break;
+      case diff < 0:
+        updatedCardNumbers =
+          joinedCardNumbers.slice(0, selectionStart) +
+          joinedCardNumbers.slice(selectionStart + 1);
+        break;
+      default:
+    }
+
+    const unformattedValue = unformatCardNumbers(updatedCardNumbers);
+    const splitNumbers = splitCardNumbers(unformattedValue);
+
+    setCardNumbers(splitNumbers);
+  };
 
   return (
     <>
@@ -12,12 +101,22 @@ function CardAddition(props) {
         <Card cardType={cardType} size={CARD_SIZE.MEDIUM} />
         <form className="card-addition__form">
           <div className="card-addition__number-input mt-standard">
-            <label for="card-number">카드 번호</label>
-            <Input id="card-number" type="text" isCenter={true} />
+            <label htmlFor="card-number">카드 번호</label>
+            <Input
+              id="card-number"
+              type="text"
+              isCenter={true}
+              value={formatCardNumbers(cardNumbers)}
+              onChange={onCardNumbersChange}
+              option={{
+                ref: cardNumbersInputRef,
+                maxlength: "25",
+              }}
+            />
           </div>
 
           <div className="card-addition__expiration-input mt-standard">
-            <label for="expiration-date">만료일</label>
+            <label htmlFor="expiration-date">만료일</label>
             <Input
               id="expiration-date"
               type="text"
@@ -27,7 +126,7 @@ function CardAddition(props) {
           </div>
 
           <div className="card-addition__username-input mt-standard">
-            <label for="username">카드 소유자 이름(선택)</label>
+            <label htmlFor="username">카드 소유자 이름(선택)</label>
             <span className="card-addition__username-indicator">0/30</span>
             <Input
               id="username"
@@ -38,7 +137,7 @@ function CardAddition(props) {
           </div>
 
           <div className="card-addition__cvc mt-standard">
-            <label for="cvc">보안 코드(CVC/CVV)</label>
+            <label htmlFor="cvc">보안 코드(CVC/CVV)</label>
             <div className="card-addition__cvc-inner">
               <Input id="cvc" type="number" isCenter={true} />
               <div className="card-addition__tool-tip-button">
