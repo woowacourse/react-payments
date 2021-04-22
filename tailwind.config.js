b6b0d440ac73338d1b735de341e9bfc74100f9d1
Kyle <mkitigy@gmail.com>
2021-04-22 16:33:31 +0900

    chore: tailwindcss 를 storybook과 동기화한다

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        25: "6.25rem",
+      },
+    },
   },
   variants: {
     extend: {},
