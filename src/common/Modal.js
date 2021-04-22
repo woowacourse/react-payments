439aeb51fa061f921a25401e08b87eb37638b48d
Min Kyung Kang <mnk918p@gmail.com>
2021-04-22 20:20:43 +0900

    feat: Modal Component 생성 및 storybook 생성

---
+++ src/common/Modal.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const ModalWrapper = styled.div`
+  display: flex;
+  position: relative;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.5);
+  transition: opacity 0.25s ease;
+  z-index: 2;
+`;
+
+const ModalInner = styled.div`
+  transition: top 0.25s ease;
+  width: 100%;
+  height: 227px;
+  margin: auto;
+  overflow: auto;
+  background: #ffffff;
+  border-radius: 10px 10px 0px 0px;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  padding: 2.5rem;
+
+  @media screen and (max-width: 768px) {
+    box-sizing: border-box;
+  }
+`;
+
+// const ModalClose = styled.div`
+//   margin: 20px;
+//   width: 20px;
+//   position: absolute;
+//   right: 10px;
+//   top: 10px;
+//   cursor: pointer;
+
+//   .close-x {
+//     stroke: gray;
+//     fill: transparent;
+//     stroke-linecap: round;
+//     stroke-width: 5;
+//   }
+// `;
+
+const Modal = ({ handleModalClose, children }) => {
+  return (
+    <ModalWrapper onClick={handleModalClose}>
+      <ModalInner>{children}</ModalInner>
+    </ModalWrapper>
+  );
+};
+
+Modal.propTypes = {
+  handleModalClose: PropTypes.func.isRequired,
+  children: PropTypes.object.isRequired,
+};
+
+export default Modal;
---
+++ src/common/Modal.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Modal from './Modal';
+
+export default {
+  component: Modal,
+  title: 'Modal',
+};
+
+const Template = (args) => {
+  const { children, ...restArgs } = args;
+  return <Modal {...restArgs}>{children}</Modal>;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  children: <div>모달내컴포넌트</div>,
+  handleModalClose: () => {
+    console.log('모달 닫기');
+  },
+};
