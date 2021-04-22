42c51b7462378d8b3f687f24f024d5685e4423c2
Min Kyung Kang <mnk918p@gmail.com>
2021-04-22 15:29:14 +0900

    feat: Button Component 생성

---
+++ src/common/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import { color, fontWeight, fontSize } from '../global.styles';
+
+const Button = styled.button`
+  color: ${color.mint};
+  font-weight: ${fontWeight.bold};
+  font-size: ${fontSize.normal};
+`;
+
+export default Button;
