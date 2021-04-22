8fdacbb713d5ea9ce9d2d2d6a2166a33c84bfcf8
zigsong <wldms5764@gmail.com>
2021-04-22 20:23:53 +0900

    feat: dummy card data 추가

---
+++ src/mockData.js
@@ -0,0 +1,28 @@
+import PALETTE from './styles/palette';
+
+const dummyCards = [
+  {
+    key: '1',
+    color: PALETTE.COLLIN_ORANGE,
+    width: '208px',
+    height: '130px',
+    bank: '포코 카드',
+    owner: 'POCO',
+    numbers: ['1111', '2222', '3333', '4444'],
+    expirationDate: '04/21',
+    alias: '엄카',
+  },
+  {
+    key: '2',
+    color: PALETTE.SUN_YELLOW,
+    width: '208px',
+    height: '130px',
+    bank: '로이드 카드',
+    owner: 'LOYD',
+    numbers: ['4444', '3333', '2222', '1111'],
+    expirationDate: '02/17',
+    alias: '법카',
+  },
+];
+
+export { dummyCards };
