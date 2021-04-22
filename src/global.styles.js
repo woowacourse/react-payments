cecced8899f7c51d36186935296947cbac38eef9
Min Kyung Kang <mnk918p@gmail.com>
2021-04-22 15:29:22 +0900

    style: global styles 생성

---
+++ src/global.styles.js
@@ -0,0 +1,25 @@
+const color = {
+  default: '#d2d2d2',
+  mint: '#04c09e',
+  red: '#E24141',
+  blue: '#547cE4',
+  green: '#73bc6D',
+  hotPink: '#DE59B9',
+  pink: '#E76E9A',
+  orange: '#F37D3B',
+  yellow: '#FBCD58',
+};
+
+const fontWeight = {
+  normal: '500',
+  bold: '700',
+};
+
+const fontSize = {
+  mini: '0.75rem',
+  small: '0.875rem',
+  normal: '1rem',
+  large: '1.125rem',
+};
+
+export { fontSize, fontWeight, color };
