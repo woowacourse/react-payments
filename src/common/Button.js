0a3692212eebfcea9faaf09504a3f43787ca1ded
Min Kyung Kang <mnk918p@gmail.com>
2021-04-22 15:42:03 +0900

    feat: Button component 구현 및 스토리북 생성

--- src/common/Button.js
+++ src/common/Button.js
@@ -1,11 +1,12 @@
-import React from 'react';
 import styled from 'styled-components';
 import { color, fontWeight, fontSize } from '../global.styles';
 
 const Button = styled.button`
   color: ${color.mint};
+  background-color: white;
   font-weight: ${fontWeight.bold};
-  font-size: ${fontSize.normal};
+  font-size: ${fontSize.small};
+  border: 0;
 `;
 
 export default Button;
