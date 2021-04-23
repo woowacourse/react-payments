b77bb4ac067990af35d483e8402b28d931b976ad
yujo <bedro27@gmail.com>
2021-04-23 16:06:01 +0900

    feat: BackButton Component 구현

---
+++ src/components/BackButton/BackButton.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BackButton from './BackButton';
+
+export default {
+  component: BackButton,
+  title: 'BackButton',
+};
+
+const Template = () => <BackButton />;
+
+export const Default = Template.bind({});
+Default.args = {};
