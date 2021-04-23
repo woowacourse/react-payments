b0cfe8f0b85eba60200f3f0b4deea835f49732f0
zigsong <wldms5764@gmail.com>
2021-04-23 21:09:53 +0900

    fix: global style 적용 범위 수정

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
     @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
     font-family: 'Roboto', sans-serif;
     box-sizing: border-box;
