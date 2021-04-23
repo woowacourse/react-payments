c85a9e9cff30e788c25ed6f8fb4561e0e26c47f1
0imbean0 <tjsqls2003@gmail.com>
2021-04-23 15:25:46 +0900

    chore: styled component 설정

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Button } from '../Button';
 
 export default {
-  title: 'Example/Button',
+  title: 'Component/Button',
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
--- src/components/Header/Header.stories.js
+++ src/components/Header/Header.stories.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Header } from '../Header';
 
 export default {
-  title: 'Example/Header',
+  title: 'Component/Header',
   component: Header,
 };
 
