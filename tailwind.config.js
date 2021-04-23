16b9f347947ebab71025744dca712cba471b4c1d
yujo <bedro27@gmail.com>
2021-04-23 16:05:26 +0900

    chore: eslint, tatilwind config 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,46 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
+  mode: 'jit',
   purge: ['./src/**/*.{js,jsx}', './public/index.html'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ['Roboto Mono', ...defaultTheme.fontFamily.mono],
+      },
+      width: {
+        '1.5/12': '12.5%',
+        208: '208px',
+        213: '213px',
+        293: '293px',
+      },
+      height: {
+        '2/12': '8.333333%',
+        130: '130px',
+        133: '133px',
+        183: '183px',
+      },
+      colors: {
+        gray: {
+          250: '#ECEBF1',
+          350: '#d3d3d3',
+        },
+        yellow: {
+          450: '#cbba64',
+        },
+        black: {
+          700: '#525252',
+          500: '#383838',
+        },
+        green: {
+          350: '#04C09E',
+        },
+      },
+      fontSize: {
+        xxs: '0.6rem',
+      },
+    },
   },
   variants: {
     extend: {},
