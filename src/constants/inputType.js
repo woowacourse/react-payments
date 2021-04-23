6dbeda7e51a4330de403ca41197703527d071db7
zigsong <wldms5764@gmail.com>
2021-04-23 21:09:14 +0900

    feat: register input 값 객체 상수 분리

---
+++ src/constants/inputType.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  CARD_NUMBERS: {
+    type: 'cardNumbers',
+    label: '카드 번호',
+    width: '318px',
+  },
+  EXPIRATION_DATE: {
+    type: 'expirationDate',
+    label: '만료일',
+    width: '137px',
+  },
+  OWNER_NAME: {
+    type: 'ownerName',
+    label: '카드 소유자 이름(선택)',
+    width: '318px',
+  },
+  SECURE_CODE: {
+    type: 'secureCode',
+    label: '보안코드(CVC/CVV)',
+    width: '84px',
+  },
+  PASSWORD: {
+    type: 'password',
+    label: '카드 비밀번호',
+    width: '43px',
+  },
+};
