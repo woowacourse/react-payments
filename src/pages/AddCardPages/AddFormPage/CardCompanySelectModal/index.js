78b32df7a59bc3350ae754710a4395f312346cdc
365kim <365listener@gmail.com>
2021-04-24 12:57:13 +0900

    feat:  CardCompanySelectModal 구현
    Co-authored-by: shinsehantan <shinsehantan@users.noreply.github.com>

--- src/pages/AddCardPages/AddFormPage/CardCompanySelectModal/index.js
+++ src/pages/AddCardPages/AddFormPage/CardCompanySelectModal/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import './style.css';
-import { Button, Label } from '../../../../components';
+import { Button, Label, Modal } from '../../../../components';
 
 const CARD_COMPANY_LIST = [
   { name: '포코 카드', color: '#e24141' },
@@ -9,7 +9,7 @@ const CARD_COMPANY_LIST = [
   { name: '공원카드', color: '#73BC6D' },
   { name: '브랜 카드', color: '#DE59B9' },
   { name: '로이드 카드', color: '#94DACD' },
-  { name: '도비 카비', color: '#E76E9A' },
+  { name: '도비 카드', color: '#E76E9A' },
   { name: '콜린 카드', color: '#F37D3B' },
   { name: '썬 카드', color: ' #FBCD58' },
 ];
@@ -22,5 +22,23 @@ const CardCompanyItem = ({ cardCompany }) => {
       <Button backgroundColor={color} onClick={() => {}} theme="card-company" />
       <Label>{name}</Label>
     </li>
+  );
+};
+
+export const CardCompanyList = () => {
+  return (
+    <ul className="CardCompanyList">
+      {CARD_COMPANY_LIST.map((cardCompany, index) => (
+        <CardCompanyItem key={index} cardCompany={cardCompany} />
+      ))}
+    </ul>
+  );
+};
+
+export const CardCompanySelectModal = ({ isOpen }) => {
+  return (
+    <Modal isOpen={isOpen}>
+      <CardCompanyList />
+    </Modal>
   );
 };
--- src/pages/AddCardPages/index.js
+++ src/pages/AddCardPages/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { CardCompanySelectModal } from './AddFormPage/CardCompanySelectModal';
 
 export const AddCardPages = () => {
-  return <></>;
+  return (
+    <>
+      <CardCompanySelectModal isOpen />
+    </>
+  );
 };
