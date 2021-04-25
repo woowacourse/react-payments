8b2ad8923e67e17741d0da7b3e7facc99dc49ab2
zigsong <wldms5764@gmail.com>
2021-04-25 19:45:26 +0900

    feat: 카드등록 페이지의 빈 카드 구현
    Co-authored-by: Lee Soo Yeon <2SOOY@users.noreply.github.com>

--- src/components/shared/Card/index.js
+++ src/components/shared/Card/index.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import * as Style from './style.js';
 import SecureEllipseImage from '../../../assets/secure-ellipse.svg';
 
-const Card = ({ backgroundColor, width, height, bank, owner, numbers, expirationDate }) => {
-  const [first, second] = numbers;
+const Card = ({ isFilled, backgroundColor, width, height, bank, owner, numbers, expirationDate }) => {
+  const [first, second] = numbers || [];
 
   const secureDigits = Array.from({ length: 4 }, (_, idx) => (
     <img key={idx} src={SecureEllipseImage} alt="secure-mark" />
@@ -12,28 +12,32 @@ const Card = ({ backgroundColor, width, height, bank, owner, numbers, expiration
 
   return (
     <Style.Root width={width} height={height} backgroundColor={backgroundColor}>
-      <Style.CardName>{bank || '포코카드'}</Style.CardName>
+      {/* TODO: 조건부 렌더링 리팩토링 */}
+      {isFilled && <Style.CardName>{bank}</Style.CardName>}
       <Style.Chip />
-      <Style.CardNumbersWrapper>
-        <Style.CardNumberGroup>{first}</Style.CardNumberGroup>
-        <Style.CardNumberGroup>{second}</Style.CardNumberGroup>
-        <Style.CardNumberGroup>{secureDigits}</Style.CardNumberGroup>
-        <Style.CardNumberGroup>{secureDigits}</Style.CardNumberGroup>
-      </Style.CardNumbersWrapper>
-      <Style.OwnerName>{owner}</Style.OwnerName>
-      <Style.ExpirationDate>{expirationDate}</Style.ExpirationDate>
+      {isFilled && (
+        <Style.CardNumbersWrapper>
+          <Style.CardNumberGroup>{first}</Style.CardNumberGroup>
+          <Style.CardNumberGroup>{second}</Style.CardNumberGroup>
+          <Style.CardNumberGroup>{secureDigits}</Style.CardNumberGroup>
+          <Style.CardNumberGroup>{secureDigits}</Style.CardNumberGroup>
+        </Style.CardNumbersWrapper>
+      )}
+      <Style.OwnerName>{isFilled ? owner : 'NAME'}</Style.OwnerName>
+      <Style.ExpirationDate>{isFilled ? expirationDate : 'MM/YY'}</Style.ExpirationDate>
     </Style.Root>
   );
 };
 
 Card.propTypes = {
+  isFilled: PropTypes.bool.isRequired,
   backgroundColor: PropTypes.string.isRequired,
   width: PropTypes.string.isRequired,
   height: PropTypes.string.isRequired,
   bank: PropTypes.string,
   owner: PropTypes.string,
-  numbers: PropTypes.arrayOf(PropTypes.string).isRequired,
-  expirationDate: PropTypes.string.isRequired,
+  numbers: PropTypes.arrayOf(PropTypes.string),
+  expirationDate: PropTypes.string,
 };
 
 export default Card;
