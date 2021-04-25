dadcb4f7b6718d03b88a658a675f69f1f8960e08
Shim MunSeong <Puterism.K@gmail.com>
2021-04-25 19:52:13 +0900

    fix: 불필요한 스타일 제거

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -15,9 +15,6 @@ const GlobalStyle = () => (
       #root {
         font-family: 'Noto Sans KR', sans-serif;
         height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
       }
     `}
   />
