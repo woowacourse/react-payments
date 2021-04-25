0b99ef9e33190d94f2e2338889bcf752b89c7789
sunhpark42 <sunhpark42@gmail.com>
2021-04-25 10:28:26 +0900

    feat: 월/연 날짜 형식 formatter 및 에러처리

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -1,5 +1,11 @@
-export const cardSerialNumberFormatter = (serialnumber) => {
-  if (serialnumber.length > 16) return;
+export const cardSerialNumberFormatter = (serialNumber) => {
+  if (serialNumber.length > 16) throw Error('올바르지 않은 카드 번호입니다.');
 
-  return serialnumber.match(/.{1,4}/g)?.join('-') || serialnumber;
+  return serialNumber.match(/.{1,4}/g)?.join('-') || serialNumber;
+};
+
+export const MMYYDateFormatter = (date) => {
+  if (date.length > 4) throw Error('올바르지 않은 날짜 형식입니다.');
+
+  return date.match(/.{1,2}/g)?.join('/') || date;
 };
