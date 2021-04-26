cae51cf8b8809b33777f21c126b7a6972ba3b569
Tanney <dhxodms89@gmail.com>
2021-04-26 17:01:17 +0900

    feat: 카드 추가 완료 페이지 레이아웃 구성

--- src/components/CompleteCardAddition.js
+++ src/components/CompleteCardAddition.js
@@ -1,8 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import Card from "../stories/Card";
+import Button from "../stories/Button";
+import { CARD_SIZE } from "../stories/constants/card";
+
 function CompleteCardAddition(props) {
-  return <div>음?{props?.card.cardType.name}</div>;
+  const {
+    cardType,
+    cardNumbers,
+    expirationDate,
+    username,
+    secureCode,
+    password,
+  } = props.card;
+
+  return (
+    <div className="complete-card-addition">
+      <h2>카드등록이 완료되었습니다.</h2>
+      {/* TODO: props 네이밍 통일 */}
+      <Card
+        cardType={cardType}
+        numbers={cardNumbers}
+        userName={username}
+        expirationDate={Object.values(expirationDate).join("/")}
+        size={CARD_SIZE.LARGE}
+      />
+      <form>
+        <input type="text" placeholder="카드 별명을 입력해주세요" />
+        <Button innerText="확인" />
+      </form>
+    </div>
+  );
 }
 
 CompleteCardAddition.propTypes = {
