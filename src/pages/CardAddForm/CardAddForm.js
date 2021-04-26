531b119685a65caeecb481519f02590c3976c03b
Shim MunSeong <Puterism.K@gmail.com>
2021-04-26 20:37:09 +0900

    fix: 카드 번호 입력 시, 카드사를 인식하지 못하는 문제 수정

--- src/pages/CardAddForm/CardAddForm.js
+++ src/pages/CardAddForm/CardAddForm.js
@@ -114,7 +114,7 @@ const CardAddForm = () => {
       const matchedCardCompany = Object.values(CARD).find((company) =>
         company.bins.some((binNumber) => {
           const isMatchedFirstInput = firstInput === binNumber.slice(0, 4);
-          const isMatchedSecondInput = secondInput === binNumber.slice(4, 6);
+          const isMatchedSecondInput = secondInput.slice(0, 2) === binNumber.slice(4, 6);
 
           return binNumber.length === 4
             ? isMatchedFirstInput
