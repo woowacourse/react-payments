53c7d7d0e3e7b347f304b717ec795b5938ce9b81
zigsong <wldms5764@gmail.com>
2021-04-26 16:59:53 +0900

    feat: 은행 임시 데이터 추가
    Co-authored-by: Lee Soo Yeon <2SOOY@users.noreply.github.com>

--- src/mockData.js
+++ src/mockData.js
@@ -25,4 +25,47 @@ const dummyCards = [
   },
 ];
 
-export { dummyCards };
+const dummyBanks = [
+  {
+    id: '1',
+    color: PALETTE.POCO_RED,
+    name: '포코 카드',
+  },
+  {
+    id: '2',
+    color: PALETTE.JUN_BLUE,
+    name: '준 카드',
+  },
+  {
+    id: '3',
+    color: PALETTE.PARK_GREEN,
+    name: '공원 카드',
+  },
+  {
+    id: '4',
+    color: PALETTE.BRAN_PINK,
+    name: '브랜 카드',
+  },
+  {
+    id: '5',
+    color: PALETTE.LOYD_MINT,
+    name: '로이드 카드',
+  },
+  {
+    id: '6',
+    color: PALETTE.DOBY_PINK,
+    name: '도비 카드',
+  },
+  {
+    id: '7',
+    color: PALETTE.COLLIN_ORANGE,
+    name: '콜린 카드',
+  },
+  {
+    id: '8',
+    color: PALETTE.SUN_YELLOW,
+    name: '썬 카드',
+  },
+];
+
+export { dummyCards, dummyBanks };
