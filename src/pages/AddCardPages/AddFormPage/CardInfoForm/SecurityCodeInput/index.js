b46ca3e8749ed47effc57bda1f188dd95eac79be
365kim <365listener@gmail.com>
2021-04-27 21:04:54 +0900

    feat: 물음표 버튼 호버 시 보안 코드 설명 이미지 표시 기능 구현
    Co-authored-by: shinsehantan <shinsehantan@users.noreply.github.com>

--- src/pages/AddCardPages/AddFormPage/CardInfoForm/SecurityCodeInput/index.js
+++ src/pages/AddCardPages/AddFormPage/CardInfoForm/SecurityCodeInput/index.js
@@ -1,14 +1,21 @@
-import { Input, Label } from '../../../../../components';
+import { Button, Input, Label } from '../../../../../components';
+import cvcImage from '../../../../../images/cvc.png';
 
 export const SecurityCodeInput = () => {
   return (
     <>
       <Label>보안 코드(CVC/CVV)</Label>
-      <Input
-        className="SecurityCodeInput__Field"
-        container="CardInfoForm__Input__Filler--filled SecurityCodeInput__Filler"
-        type="password"
-      />
+      <div className="SecurityCodeInput__Wrapper">
+        <Input
+          className="SecurityCodeInput__Field"
+          container="CardInfoForm__Input__Filler--filled SecurityCodeInput__Filler"
+          type="password"
+        />
+        <Button theme="question-mark" type="button">
+          ?
+        </Button>
+        <img className="SecurityCodeInput__GuideImage" width="80" src={cvcImage} alt="cvc" />
+      </div>
     </>
   );
 };
