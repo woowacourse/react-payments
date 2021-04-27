8bc9a43e6a0ed6fd7f9cfb0aef4d52a9c1f7d45b
zigsong <wldms5764@gmail.com>
2021-04-27 18:08:42 +0900

    feat: 카드소유자이름 인풋 유효성 검사
    Co-authored-by: Lee Soo Yeon <2SOOY@users.noreply.github.com>

--- src/components/units/OwnerNameInput/index.js
+++ src/components/units/OwnerNameInput/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import * as Style from './style';
 
 const OwnerNameInput = (props) => {
+  const isEnglish = (char) => !/[^A-Za-z\s]/g.test(char);
+  const isValidLength = (value) => value.length <= 30;
+
+  const handleChangeName = (event) => {
+    const currentValue = event.target.value;
+    if (!isEnglish(currentValue)) return;
+    if (!isValidLength(currentValue)) return;
+
+    setOwnerName(currentValue.toUpperCase());
+  };
   return (
     <>
       <Style.Input width="302px" placeholder={'카드에 표시된 이름과 동일하게 입력하세요.'} />
