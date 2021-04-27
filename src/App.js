ff8e54d3a1420e4a8d7edefd5caeee231e922b05
0imbean0 <tjsqls2003@gmail.com>
2021-04-27 21:08:15 +0900

    feat: Page component를 App에 적용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,65 @@
+import { useState } from 'react';
+import { Header } from './components/Page/Header';
+import { Page } from './components/Page';
+import { CardList } from './components/Page/Body/CardList';
+import { CardRegister } from './components/Page/Body/CardRegister';
+import { CardRegistered } from './components/Page/Body/CardRegistered';
+
+const ChildComponents = {
+  cardRegister: [<CardRegister />, <Header titleText={'카드 추가'} hasButton={true} />],
+  cardRegistered: [
+    <CardRegistered
+      card={{
+        company: '로이드',
+        numbers: { first: '1111', second: '2222', third: '3333', fourth: '4444' },
+        owner: 'SUN',
+        validDay: { firstDigit: '04', secondDigit: '21' },
+      }}
+    />,
+  ],
+  cardList: [
+    <CardList
+      cards={[
+        {
+          company: '포코',
+          numbers: { first: '1111', second: '2222', third: '3333', fourth: '4444' },
+          owner: 'SUN',
+          validDay: { month: '04', year: '21' },
+          nickName: '엄카',
+        },
+        {
+          company: '로이드',
+          numbers: { first: '1111', second: '2222', third: '3333', fourth: '4444' },
+          owner: 'SUN',
+          validDay: { month: '04', year: '21' },
+          nickName: '법카',
+        },
+        {
+          company: '로이드',
+          numbers: { first: '1111', second: '2222', third: '3333', fourth: '4444' },
+          owner: 'SUN',
+          validDay: { month: '04', year: '21' },
+          nickName: '법카',
+        },
+        {
+          company: '로이드',
+          numbers: { first: '1111', second: '2222', third: '3333', fourth: '4444' },
+          owner: 'SUN',
+          validDay: { month: '04', year: '21' },
+          nickName: '법카',
+        },
+      ]}
+    />,
+    <Header titleText={'보유카드'} hasButton={false} />,
+  ],
+};
+
 function App() {
+  const [currentPage, setCurrentPage] = useState('cardList');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Page>{ChildComponents[currentPage]}</Page>
     </div>
   );
 }
