2495f3ed37f557abe4bf02ae088a0cd12bde54bd
sunhpark42 <sunhpark42@gmail.com>
2021-04-27 18:35:04 +0900

    feat: 앱과 페이지에 대한 story 파일 생성
    - App, CardCompany, SecurityCodeGuide, AddCardComplete, AddCardForm

---
+++ src/pages/AddCardForm/stories.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import AddCardForm from '.';
+
+export default {
+  title: 'Page/AddCardForm',
+  component: AddCardForm,
+  argTypes: {
+    expirationDate: {
+      control: {
+        type: 'text',
+      },
+    },
+    cardCompany: {
+      control: {
+        type: 'select',
+        options: ['POCO', 'ROID', 'JUN', 'GONGWON'],
+      },
+    },
+    setExpirationDate: {
+      control: {
+        type: 'none',
+      },
+    },
+  },
+};
+
+const Template = (args) => <AddCardForm {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  serialNumber: '1234123412341234',
+  setSerialNumber: () => {},
+  cardCompany: 'POCO',
+  setCardCompany: () => {},
+  expirationDate: '0911',
+  setExpirationDate: () => {},
+  userName: 'DEV JANG',
+  setUserName: () => {},
+  securityCode: '123',
+  setSecurityCode: () => {},
+  password: { first: '0', second: '0' },
+  setPassword: () => {},
+  onSetModalContents: () => {},
+  setPage: () => {},
+};
