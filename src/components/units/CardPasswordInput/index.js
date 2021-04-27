401da94a4673f8358450eb3818d0d086586e7045
zigsong <wldms5764@gmail.com>
2021-04-27 18:10:59 +0900

    feat: 카드 비밀번호 인풋 유효성 검사
    Co-authored-by: Lee Soo Yeon <2SOOY@users.noreply.github.com>

--- src/components/units/CardPasswordInput/index.js
+++ src/components/units/CardPasswordInput/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import * as Style from './style';
 
 const CardPasswordInput = (props) => {
+  const handleChangeNumbers = (event) => {
+    const currentValue = event.target.value;
+    const index = Number(event.target.dataset.passwordIdx);
+
+    if (isNaN(currentValue)) return;
+
+    if (index === 1) {
+      moveFocusToNextFragment();
+    } else if (index === 2) {
+      if (currentValue.length > 1) return;
+    }
+
+    setCardPassword((prevPassword) => ({ ...prevPassword, [index]: currentValue }));
+  };
   return (
     <>
       <Style.Input type="password" width="12px" />
