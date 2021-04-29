ac2e3e34daa3f4447b9834c1c58d86b947675ed3
iborymagic <mlch8023@gmail.com>
2021-04-30 04:17:15 +0900

    fix: NickNameForm에서 storybook 기본값이 없던 버그 수정

--- src/components/InputForm/NickNameForm/NickNameForm.stories.js
+++ src/components/InputForm/NickNameForm/NickNameForm.stories.js
@@ -9,4 +9,10 @@ export default {
 const Template = (args) => <NickNameForm {...args} />;
 
 export const defaultForm = Template.bind({});
-defaultForm.args = {};
+defaultForm.args = {
+  nickName: {
+    value: '직박구리',
+    handleChange: () => {},
+  },
+  submitCardNickName: () => {},
+};
