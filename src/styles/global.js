7352cef33f80d55e333feac29e00bb98b182f525
zigsong <wldms5764@gmail.com>
2021-04-29 19:03:39 +0900

    fix: globalStyle 문법 수정

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,8 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  * {
-    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+  
+  body * {
     font-family: 'Roboto', sans-serif;
     box-sizing: border-box;
   }
