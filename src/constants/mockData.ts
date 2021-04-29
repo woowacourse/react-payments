8808a341f19814414651e3e93f015f4d31e6beaa
Sunny.K <sunnyk5780@gmail.com>
2021-04-29 19:06:23 +0900

    refactor: mockData 파일 분리

---
+++ src/constants/mockData.ts
@@ -0,0 +1,49 @@
+import { Card } from '../types';
+
+export const CARD_MOCK_DATA: Card[] = [
+  {
+    cardBrand: {
+      name: '포코카드',
+      color: '#547CE4',
+    },
+    ownerName: 'FANO',
+    cardNumber: '1234-1234-1234-1234',
+    expDate: {
+      month: '3',
+      year: '3',
+    },
+    password: '12',
+    CVC: '123',
+    nickname: '엄카',
+  },
+  {
+    cardBrand: {
+      name: '포코카드',
+      color: '#547CE4',
+    },
+    ownerName: 'FANO',
+    cardNumber: '1234-1234-1234-1234',
+    expDate: {
+      month: '3',
+      year: '3',
+    },
+    password: '12',
+    CVC: '123',
+    nickname: '엄카',
+  },
+  {
+    cardBrand: {
+      name: '포코카드',
+      color: '#547CE4',
+    },
+    ownerName: 'FANO',
+    cardNumber: '1234-1234-1234-1234',
+    expDate: {
+      month: '3',
+      year: '3',
+    },
+    password: '12',
+    CVC: '123',
+    nickname: '엄카',
+  },
+];
--- src/pages/CardListPage/index.tsx
+++ src/pages/CardListPage/index.tsx
@@ -1,63 +1,16 @@
 import CreditCard from '../../components/common/CreditCard';
 import CardList from '../../components/CardList';
 import Template from '../../components/common/Template';
+import { CARD_MOCK_DATA } from '../../constants/mockData';
 import { Card } from '../../types';
 
-const dummies: Card[] = [
-  {
-    cardBrand: {
-      name: '포코카드',
-      color: '#547CE4',
-    },
-    ownerName: 'FANO',
-    cardNumber: '1234-1234-1234-1234',
-    expDate: {
-      month: '3',
-      year: '3',
-    },
-    password: '12',
-    CVC: '123',
-    nickname: '엄카',
-  },
-  {
-    cardBrand: {
-      name: '포코카드',
-      color: '#547CE4',
-    },
-    ownerName: 'FANO',
-    cardNumber: '1234-1234-1234-1234',
-    expDate: {
-      month: '3',
-      year: '3',
-    },
-    password: '12',
-    CVC: '123',
-    nickname: '엄카',
-  },
-  {
-    cardBrand: {
-      name: '포코카드',
-      color: '#547CE4',
-    },
-    ownerName: 'FANO',
-    cardNumber: '1234-1234-1234-1234',
-    expDate: {
-      month: '3',
-      year: '3',
-    },
-    password: '12',
-    CVC: '123',
-    nickname: '엄카',
-  },
-];
-
-const title = '보유카드';
+const TITLE = '보유카드';
 
 const CardListPage = () => {
   return (
-    <Template title={title}>
+    <Template title={TITLE}>
       <CardList>
-        {dummies.map(card => (
+        {CARD_MOCK_DATA.map((card: Card) => (
           <li key={card.cardNumber}>
             <CreditCard
               cardBrand={card.cardBrand}
