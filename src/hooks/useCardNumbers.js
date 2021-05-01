c084d1d2aa7d1c7c933da0100e4698e57501a2c9
권세진 <0307kwon@gmail.com>
2021-05-01 21:05:35 +0900

    refactor: switch문을 if 문으로 변경하여 가독성을 높임

--- src/hooks/useCardNumbers.js
+++ src/hooks/useCardNumbers.js
@@ -11,68 +11,64 @@ const splitCardNumbers = (value) => {
   return value.replace(/\D/g, "").match(/.{1,4}/g) || [];
 };
 
+const correctSelectionRange = (selectionStart, inputRef) => {
+  const mod =
+    CARD_NUMBER.PARTIAL_LENGTH + FORMAT_CHAR.CARD_NUMBERS_SEPARATOR.length;
+  const remainder = selectionStart % mod;
+
+  const isSelectionValid = [
+    ...Array(FORMAT_CHAR.CARD_NUMBERS_SEPARATOR.length),
+  ].every((_, index) => remainder !== (mod - index) % mod);
+
+  const fixedSelectionStart = isSelectionValid
+    ? selectionStart
+    : selectionStart + ((mod + 1 - remainder) % mod);
+
+  inputRef?.current?.setSelectionRange(
+    fixedSelectionStart,
+    fixedSelectionStart
+  );
+};
+
 const useCardNumbers = (initialCardNumbers) => {
   const [cardNumbers, setCardNumbers] = useState(initialCardNumbers);
   const cardNumbersInputRef = useRef();
 
   const onCardNumbersChange = (event) => {
-    const { value, selectionStart } = event.target;
+    const { value: inputString, selectionStart } = event.target;
     const joinedCardNumbers = cardNumbers.join(
       FORMAT_CHAR.CARD_NUMBERS_SEPARATOR
     );
-    const diff = value.length - joinedCardNumbers.length;
+    const lengthDiffComparedToPrev =
+      inputString.length - joinedCardNumbers.length;
     let updatedCardNumbers = joinedCardNumbers;
 
-    const mod =
-      CARD_NUMBER.PARTIAL_LENGTH + FORMAT_CHAR.CARD_NUMBERS_SEPARATOR.length;
-    const remainder = selectionStart % mod;
+    if (lengthDiffComparedToPrev >= 0) {
+      const inputChar = inputString[selectionStart - 1];
 
-    const isSelectionValid = [
-      ...Array(FORMAT_CHAR.CARD_NUMBERS_SEPARATOR.length),
-    ].every((_, index) => remainder !== (mod - index) % mod);
+      if (!isNumberValue(inputChar)) {
+        return;
+      }
 
-    const fixedSelectionStart = isSelectionValid
-      ? selectionStart
-      : selectionStart + ((mod + 1 - remainder) % mod);
+      updatedCardNumbers =
+        joinedCardNumbers.slice(0, selectionStart - 1) +
+        inputChar +
+        joinedCardNumbers.slice(
+          lengthDiffComparedToPrev === 0 ? selectionStart : selectionStart - 1
+        );
+    }
 
-    cardNumbersInputRef?.current?.setSelectionRange(
-      fixedSelectionStart,
-      fixedSelectionStart
-    );
-
-    switch (true) {
-      case diff > 0:
-        if (!isNumberValue(value[selectionStart - 1])) {
-          return;
-        }
-
-        updatedCardNumbers =
-          joinedCardNumbers.slice(0, selectionStart - 1) +
-          value[selectionStart - 1] +
-          joinedCardNumbers.slice(selectionStart - 1);
-        break;
-      case diff === 0:
-        if (!isNumberValue(value[selectionStart - 1])) {
-          return;
-        }
-
-        updatedCardNumbers =
-          joinedCardNumbers.slice(0, selectionStart - 1) +
-          value[selectionStart - 1] +
-          joinedCardNumbers.slice(selectionStart);
-        break;
-      case diff < 0:
-        updatedCardNumbers =
-          joinedCardNumbers.slice(0, selectionStart) +
-          joinedCardNumbers.slice(selectionStart + 1);
-        break;
-      default:
+    if (lengthDiffComparedToPrev < 0) {
+      updatedCardNumbers =
+        joinedCardNumbers.slice(0, selectionStart) +
+        joinedCardNumbers.slice(selectionStart + 1);
     }
 
     const unformattedValue = unformatCardNumbers(updatedCardNumbers);
     const splitNumbers = splitCardNumbers(unformattedValue);
 
     setCardNumbers(splitNumbers);
+    correctSelectionRange(selectionStart, cardNumbersInputRef);
   };
 
   return [cardNumbers, cardNumbersInputRef, onCardNumbersChange];
