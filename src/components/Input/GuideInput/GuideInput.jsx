cb072f198719e7bd019382ddc20c06c6f2bde126
jum0 <wnsah052@naver.com>
2021-05-01 21:14:13 +0900

    feat: 카드 보안코드 입력 기능 구현

--- src/components/Input/GuideInput/GuideInput.jsx
+++ src/components/Input/GuideInput/GuideInput.jsx
@@ -4,23 +4,36 @@ import styles from "./GuideInput.module.scss";
 import Input from "../Input";
 import cardCVC from "../../../assets/images/card-cvc.png";
 import questionMark from "../../../assets/svgs/question-mark.svg";
+import { STATE_KEY } from "../../../constants";
 
 const cx = classNames.bind(styles);
 
-// TODO : 숫자 입력을 받되, 한글자마다 Dot 이 되도록 처리
+const GuideInput = ({ setAppState, labelText, guideIcon = questionMark, guideImage = cardCVC, className = "", inputWidth }) => {
+  const onCardCVCInputChange = (event) => {
+    const { value } = event.target;
 
-const GuideInput = ({ labelText, guideIcon = questionMark, guideImage = cardCVC, className = "", inputWidth }) => {
+    if (Number.isNaN(Number(value))) {
+      event.target.value = event.target.value.slice(0, -1);
+      return;
+    }
+
+    setAppState(state => ({
+      ...state,
+      [STATE_KEY.CARD_CVC]: value
+    }))
+  }
+
   return (
     <div className={`${cx("guide-input")} ${className}`}>
       <label className={cx("guide-input__label")} name={labelText}>
         {labelText}
       </label>
       <div className={cx("guide-input__bottom")}>
-        <Input inputWidth={inputWidth} />
+        <Input onChange={onCardCVCInputChange} maxLength="3" type="password" inputWidth={inputWidth} />
         <div className={cx("guide-input__guide-icon")}>
           <img src={guideIcon} alt="안내 아이콘" />
         </div>
-        <img className={cx("guide-input__guide-image")} src={guideImage} />
+        <img className={cx("guide-input__guide-image")} src={guideImage} alt="what is cvc" />
       </div>
     </div>
   );
