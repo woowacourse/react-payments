24bcf074af9e6355dcf36b6d948bba16310c8c4c
swon3210 <swon3210@gmail.com>
2021-05-04 00:32:16 +0900

    chore: index.scss 파일의 이름을 normalize.scss 로 명시화

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import "./index.scss";
+import "./normalize.scss";
 
 ReactDOM.render(
   <React.StrictMode>
