b4c1101b1d408fbe3a1c5411e2c6464fc9314044
365kim <365listener@gmail.com>
2021-05-03 17:44:39 +0900

    refactor: Form 컴포넌트 재사용성 개선

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames/bind';
-import styles from './style.css';
-
-const cx = classnames.bind(styles);
 
 export const Form = ({ className, children, ...props }) => {
-  const formClass = cx('Form', className);
-
   return (
-    <form className={formClass} {...props}>
+    <form className={className} {...props}>
       {children}
     </form>
   );
@@ -17,9 +11,5 @@ export const Form = ({ className, children, ...props }) => {
 
 Form.propTypes = {
   className: PropTypes.string,
-  children: PropTypes.any.isRequired,
-};
-
-Form.defaultProps = {
-  className: '',
+  children: PropTypes.node.isRequired,
 };
