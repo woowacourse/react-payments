db10546e3c42144be6aae9371ba295fa98718f94
Shim MunSeong <Puterism.K@gmail.com>
2021-05-03 17:37:38 +0900

    chore: 미사용하는 Context Provider 제거

--- src/App.js
+++ src/App.js
@@ -6,21 +6,18 @@ import CardList from './pages/CardList/CardList';
 import CardAddForm from './pages/CardAddForm/CardAddForm';
 import CardAddComplete from './pages/CardAddComplete/CardAddComplete';
 import ROUTE from './constants/route';
-import { CardListProvider } from './contexts/CardList';
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
 
-    <CardListProvider>
-      <Router>
-        <Switch>
-          <Route exact path={ROUTE.HOME} component={CardList} />
-          <Route exact path={ROUTE.ADD} component={CardAddForm} />
-          <Route exact path={ROUTE.COMPLETE} component={CardAddComplete} />
-        </Switch>
-      </Router>
-    </CardListProvider>
+    <Router>
+      <Switch>
+        <Route exact path={ROUTE.HOME} component={CardList} />
+        <Route exact path={ROUTE.ADD} component={CardAddForm} />
+        <Route exact path={ROUTE.COMPLETE} component={CardAddComplete} />
+      </Switch>
+    </Router>
   </ThemeProvider>
 );
 
