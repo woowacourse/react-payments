02783fd2c187ec839d4bf2019afb6de73ac5c319
sunhpark42 <sunhpark42@gmail.com>
2021-05-03 14:35:11 +0900

    feat: useForm 훅에 setInput 추가

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -3,14 +3,18 @@ import { useState, useCallback } from 'react';
 const useForm = (initialForm) => {
   const [form, setForm] = useState(initialForm);
 
-  const onChange = useCallback((event) => {
+  const onInputChange = useCallback((event) => {
     const { name, value } = event.target;
-    setForm({ ...form, [name]: value });
+    setForm((form) => ({ ...form, [name]: value }));
+  }, []);
+
+  const setInput = useCallback((name, value) => {
+    setForm((form) => ({ ...form, [name]: value }));
   }, []);
 
   const reset = useCallback(() => setForm(initialForm), [initialForm]);
 
-  return [form, onChange, reset];
+  return [form, onInputChange, setInput, reset];
 };
 
 export default useForm;
