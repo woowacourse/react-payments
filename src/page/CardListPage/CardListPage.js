34fe2fdb83d4beb8075d3046c6571971a6691335
yujo <bedro27@gmail.com>
2021-05-15 12:04:58 +0900

    feat: 저장된 카드의 목록을 보여주는 기능

--- src/page/CardListPage/CardListPage.js
+++ src/page/CardListPage/CardListPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Card from '../../components/Card/Card';
 import PropTypes from 'prop-types';
-import { PAGE } from '../../utils/constant';
+import { KEY, PAGE } from '../../utils/constant';
 
 const CardListPage = (props) => {
   const { setPageRouter } = props;
+
+  const cards = JSON.parse(localStorage.getItem(KEY.CARDS)) ?? [];
 
   const handlePageRouter = () => {
     setPageRouter(PAGE.ADD);
@@ -14,22 +16,18 @@ const CardListPage = (props) => {
     <div className="p-5">
       <h1 className="ml-1 text-xl">보유카드</h1>
       <ul className="flex flex-col items-center mt-10">
-        {/* <li className="mt-5">
-          <Card
-            size="small"
-            cardNumbers={{
-              0: '1234',
-              1: '1234',
-              2: '1234',
-              3: '1234',
-            }}
-            cardCompany={{
-              name: '로이드카드',
-            }}
-            expiration={'4/30'}
-          />
-          <h2 className="mt-2 text-center text-xl">asd</h2>
-        </li> */}
+        {cards.map((card, index) => (
+          <li key={index} className="mt-5">
+            <Card
+              size="small"
+              cardNumbers={card.cardNumbers}
+              cardCompany={card.cardCompany}
+              expiration={card.expiration.month + '/' + card.expiration.year}
+            />
+            <h2 className="mt-2 text-center">{card.cardName}</h2>
+          </li>
+        ))}
+
         <li className="mt-5">
           <div className="flex items-center justify-center w-208 h-130 bg-gray-350 rounded-xl">
             <button className="text-4xl" onClick={handlePageRouter}>
