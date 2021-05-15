b57f5c87b6e2246860b81409d25e0ad71038a5ff
365kim <365listener@gmail.com>
2021-05-15 16:10:24 +0900

    feat: 보유카드 목록 표시기능 구현

--- src/pages/ListPage/index.js
+++ src/pages/ListPage/index.js
@@ -1,18 +1,39 @@
 import { useHistory } from 'react-router-dom';
-import { Card, Text, Heading } from '../../components';
+import { Card, CreditCardPreview, Text, Heading } from '../../components';
+import { getFormattedCardInfo } from '../../cardInfoFormatter';
 import { ROUTE } from '../../constants';
 import './style.css';
 
-export const ListPage = () => {
+export const ListPage = (props) => {
+  const { cardList } = props;
   const history = useHistory();
   const handleAddButtonClick = () => history.push(ROUTE.ADD);
 
   return (
     <div className="ListPage">
       <Heading>보유카드</Heading>
+      <ul className="ListPage__UnorderedList">
+        {cardList.map((cardInfo, index) => {
+          const { company, nickname } = cardInfo;
+          const { formattedNumber, formattedExpirationDate, formattedOwnerName } = getFormattedCardInfo({ cardInfo });
+          return (
+            <li key={index}>
+              <CreditCardPreview
+                className="ListPage__UnorderedList__CreditCardPreview"
+                companyColor={company.color}
+                companyName={company.name}
+                cardNumber={formattedNumber}
+                ownerName={formattedOwnerName}
+                expirationDate={formattedExpirationDate}
+              />
+              <span className="ListPage__UnorderedList__Nickname">{nickname}</span>
+            </li>
+          );
+        })}
+      </ul>
       <button className="ListPage__AddButton" onClick={handleAddButtonClick}>
         <Card>
-          <Text className="ListPage__AddSign">+</Text>
+          <Text className="ListPage__AddButton__AddSign">+</Text>
         </Card>
       </button>
     </div>
