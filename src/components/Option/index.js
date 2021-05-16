f82415c881ee251ea8d0f4464b1775c424b4f921
sunhpark42 <sunhpark42@gmail.com>
2021-05-16 15:06:17 +0900

    feat: Option Color에 default 값 설정

--- src/components/Option/index.js
+++ src/components/Option/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Dot } from '../Icon/assets';
 import './style.css';
 
-export default function Option({ name, color, ...props }) {
+export default function Option({ name, color = '#e5e5e5', ...props }) {
   return (
     <button type="button" className="option" aria-label={`${name} 선택 옵션`} {...props}>
       <Dot className="option__button" size="36px" color={color} />
--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -5,7 +5,7 @@ import './style.css';
 export default function Select({ options, onSelect }) {
   return (
     <div className="select">
-      {Object.entries(options).map(([key, { NAME, COLOR }]) => (
+      {Object.entries(options)?.map(([key, { NAME, COLOR }]) => (
         <Option key={key} name={NAME} color={COLOR} onClick={() => onSelect(key)} />
       ))}
     </div>
