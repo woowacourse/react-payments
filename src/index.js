cc258bc585af2b740b3ead403f3ab4ce49a2889e
swon3210 <swon3210@gmail.com>
2021-05-25 01:31:19 +0900

    chore: 필요없는 webVitals 파일 삭제

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "./normalize.scss";
 
@@ -13,5 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-reportWebVitals();
