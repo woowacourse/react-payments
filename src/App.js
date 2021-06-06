10b89672b55028c77cbeb20832e8c367a0fe557e
iborymagic <mlch8023@gmail.com>
2021-06-06 20:46:36 +0900

    refactor: pages 객체 PageContextProvider로 이동

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
 import { useContext } from 'react';
 
-import { Header } from './components/Page/Header';
 import { Page } from './components/Page';
-import { CardList } from './components/Page/Body/CardList';
-import { CardRegister } from './components/Page/Body/CardRegister';
-import { CardRegistered } from './components/Page/Body/CardRegistered';
-
-import { PAGE_TITLE } from './utils/constants/messages';
 import { PageContext } from './contexts/PageContextProvider';
 
-import * as Styled from './style';
-
 function App() {
-  const { currentPage, setCurrentPage } = useContext(PageContext);
-
-  const pages = {
-    cardRegister: [
-      <Header>
-        <Styled.Button onClick={() => setCurrentPage('cardList')}>{'◀︎'}</Styled.Button>
-        <Styled.Title>{PAGE_TITLE.CARD_REGISTER}</Styled.Title>
-      </Header>,
-      <CardRegister />,
-    ],
-    cardRegistered: [<CardRegistered />],
-    cardList: [
-      <Header>
-        <Styled.Title>{PAGE_TITLE.CARD_LIST}</Styled.Title>
-      </Header>,
-      <CardList />,
-    ],
-  };
+  const { renderCurrentPage } = useContext(PageContext);
 
   return (
     <div className="App">
-      <Page>{pages[currentPage]}</Page>
+      <Page>{renderCurrentPage()}</Page>
     </div>
   );
 }
--- src/contexts/PageContextProvider.js
+++ src/contexts/PageContextProvider.js
@@ -1,12 +1,44 @@
 import { createContext, useState } from 'react';
 
+import { CardList } from '../components/Page/Body/CardList';
+import { CardRegister } from '../components/Page/Body/CardRegister';
+import { CardRegistered } from '../components/Page/Body/CardRegistered';
+import { Header } from '../components/Page/Header';
+
+import { PAGE_TITLE } from '../utils/constants/messages';
+
+import { Button, Title } from '../style';
+
 const PageContext = createContext();
 
 const PageContextProvider = ({ children }) => {
   const [currentPage, setCurrentPage] = useState('cardList');
 
+  const pages = {
+    cardRegister: [
+      <Header>
+        <Button onClick={() => setCurrentPage('cardList')}>{'◀︎'}</Button>
+        <Title>{PAGE_TITLE.CARD_REGISTER}</Title>
+      </Header>,
+      <CardRegister />,
+    ],
+    cardRegistered: [<CardRegistered />],
+    cardList: [
+      <Header>
+        <Title>{PAGE_TITLE.CARD_LIST}</Title>
+      </Header>,
+      <CardList />,
+    ],
+  };
+
+  const renderCurrentPage = () => {
+    return pages[currentPage];
+  };
+
   return (
-    <PageContext.Provider value={{ currentPage, setCurrentPage }}>{children}</PageContext.Provider>
+    <PageContext.Provider value={{ currentPage, setCurrentPage, renderCurrentPage }}>
+      {children}
+    </PageContext.Provider>
   );
 };
 
