dfda2800be76b672d158b4dcc0f578ae528db9a1
Min Kyung Kang <mnk918p@gmail.com>
2021-06-10 15:32:39 +0900

    chore: change base url for heroku

--- src/service/card.js
+++ src/service/card.js
@@ -3,7 +3,7 @@ import { fetchOptions } from './API';
 export const getCards = async () => {
   try {
     const response = await fetch(
-      'http://localhost:4000/cards'
+      'https://ddongule-payments-api.herokuapp.com/cards'
     ).then((response) => response.json());
 
     return response;
@@ -14,7 +14,10 @@ export const getCards = async () => {
 
 export const postNewCard = async (history, body) => {
   try {
-    await fetch('http://localhost:4000/cards', fetchOptions('POST', body));
+    await fetch(
+      'https://ddongule-payments-api.herokuapp.com/cards',
+      fetchOptions('POST', body)
+    );
 
     history.push('/lists');
   } catch (error) {
