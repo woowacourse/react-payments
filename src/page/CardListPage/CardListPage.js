29ffb793b16aaba1deb4d8a89313a46a8d2065b8
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-07-04 08:55:28 +0900

    refactor: 카드 목록 페이지 구현

---
+++ src/page/CardListPage/CardListPage.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Card from '../../components/Card/Card';
+import PropTypes from 'prop-types';
+import { PAGE } from '../../utils/constants';
+
+const CardListPage = (props) => {
+  const { setPageRouter } = props;
+
+  const cards = JSON.parse(localStorage.getItem('cards')) ?? [];
+
+  const handlePageRouter = () => {
+    setPageRouter(PAGE.ADD);
+  };
+
+  return (
+    <div className="p-5">
+      <h1 className="ml-1 text-xl">보유카드</h1>
+      <ul className="flex flex-col items-center mt-10">
+        {cards.map((card, index) => (
+          <li key={index} className="mt-5">
+            <Card
+              size="small"
+              cardNumbers={card.cardNumbers}
+              cardCompany={card.cardCompany}
+              expiration={`${card.expiration.month}/${card.expiration.year}`}
+            />
+            <h2 className="mt-2 text-center">{card.cardName}</h2>
+          </li>
+        ))}
+
+        <li className="mt-5">
+          <div className="flex items-center justify-center w-208 h-130 bg-gray-350 rounded-xl">
+            <button className="text-4xl" onClick={handlePageRouter}>
+              +
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default CardListPage;
+
+CardListPage.propTypes = {
+  setPageRouter: PropTypes.func.isRequired,
+};
