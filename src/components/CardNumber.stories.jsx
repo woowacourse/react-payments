4dfb9fec155a8f2e486c131c1f94e94c01f5ef0b
intae92 <kangit2013@gmail.com>
2022-04-26 20:23:31 +0900

    test: 스토리북 작성 및 스토리북 설치
    Co-authored-by: TaeYoon <uni613@naver.com>

---
+++ src/components/CardNumber.stories.jsx
@@ -0,0 +1,11 @@
+// CardNumber.stories.js|jsx
+
+import React from 'react';
+import CardNumber from './CardNumber';
+
+export default {
+  title: 'CardNumber',
+  component: CardNumber,
+};
+
+export const cardNumber = () => <CardNumber />;
