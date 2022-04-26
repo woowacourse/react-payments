b9815de9bbcfe2fb37466b81eca2ff5c21ac3356
Hwang Jung Min <hwangstar156@gmail.com>
2022-04-26 21:47:55 +0900

    feat: Dot 컴포넌트 스토리북 추가
    Co-authored-by: ronci <ronci@users.noreply.github.com>

---
+++ react-payments/src/component/Dot/dot.component.jsx
@@ -0,0 +1,11 @@
+import "./dot.css";
+
+const Dot = () => {
+  return (
+    <div className="dot-container">
+      <div className="dot" />
+    </div>
+  );
+};
+
+export default Dot;
---
+++ react-payments/src/stories/dot.stories.jsx
@@ -0,0 +1,8 @@
+import Dot from "../component/Dot/dot.component";
+
+export default {
+  title: "Dot",
+  component: Dot,
+};
+
+export const DefaultDot = (args) => <Dot {...args} />;
