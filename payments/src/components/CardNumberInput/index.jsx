119cde09f85f3ecc47453f7e742b15c32c3085b1
JUDONGHYEOK <judonghyeok@gmail.com>
2022-04-26 20:45:03 +0900

    refactor: onChange 함수 분리
    - autoFocus 내의 함수 분리
    - onChange useCallback 사용하여 성능개선
    Co-authored-by: KangYunHo1221 <ansghkdbsgh@gmail.com>

--- payments/src/components/CardNumberInput/index.jsx
+++ payments/src/components/CardNumberInput/index.jsx
@@ -1,42 +1,51 @@
 import "./index.scss";
 import InputContainer from "../elements/InputContainer";
 import { InputContents } from "../elements/InputContents";
-import { useRef } from "react";
+import { useCallback, useRef } from "react";
 
 const CardNumberInput = () => {
   const itemRef = useRef([]);
-  const autoFocus = (e) => {
-    if (e.target.value.length >= 4) {
+
+  const onChange = useCallback((e) => {
+    const limitInputLength = (e) => {
       e.target.value = e.target.value.substring(0, 4);
+    };
+
+    const autoFocus = (e) => {
       const currentIndex = itemRef.current.indexOf(e.target);
       itemRef.current[Math.min(currentIndex + 1, 3)].focus();
+    };
+
+    if (e.target.value.length >= 4) {
+      limitInputLength(e);
+      autoFocus(e);
     }
-  };
+  }, []);
 
   return (
     <div>
       <InputContainer labelName={"카드번호"} classList={[""]}>
         <InputContents
-          onChange={autoFocus}
+          onChange={onChange}
           type="number"
           ref={(el) => (itemRef.current[0] = el)}
         />
         -
         <InputContents
-          onChange={autoFocus}
+          onChange={onChange}
           ref={(el) => (itemRef.current[1] = el)}
           type="number"
         />
         -
         <InputContents
-          onChange={autoFocus}
+          onChange={onChange}
           ref={(el) => (itemRef.current[2] = el)}
           type="password"
           maxLength="4"
         />
         -
         <InputContents
-          onChange={autoFocus}
+          onChange={onChange}
           ref={(el) => (itemRef.current[3] = el)}
           type="password"
           maxLength="4"
