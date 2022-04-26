af16b0807d3664ab2404ee0d3ed8b991d89b972a
woose28 <dldntmdsla@naver.com>
2022-04-26 17:53:13 +0900

    style: app css 적용
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import './App.css';
+import CardAddPage from './pages/CardAddPage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <CardAddPage />
+    </div>
+  );
 }
 
 export default App;
