409a23b4e81518fd51078bfc73477bf47cd67956
intae92 <kangit2013@gmail.com>
2022-04-26 15:17:10 +0900

    chore: CRA 설치 및 설정
    Co-authored-by: TaeYoon <uni613@naver.com>

---
+++ . prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    printWidth: 100, // 화면 폭
+    tabWidth: 2, // 탭 간격, 스페이스 설정 시 2칸 띄어쓰기
+    singleQuote: true, // '' 작은 따옴표 사용
+    trailingComma: "all", // , 자동으로 붙이기
+    semi: false, // ; 사용 안 함
+    useTabs: false, // tab키 사용. false 추천 == space 대체
+    arrowParens: "always", // (x) => x, x => x로 전자처럼 괄호 유지
+    endOfLine: "auto",
+    bracketSpacing: "true", // 대괄호 {} 사이 공백
+    jsxBracketSameLine: "true", // JSX 요소 > 줄바꿈
+  };
+  
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="app"></div>;
+}
+
+export default App;
