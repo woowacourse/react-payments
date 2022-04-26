89d1b1370eca2bb1bc61e028c7ae6d9c8edfdfd8
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-04-26 21:39:08 +0900

    feat: FormInput 공통 컴포넌트 구현
    Co-authored-by: lokba <lokba@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import FormInput from './components/common/FormInput';
+import { v4 as uuid } from 'uuid';
+
+const cardNumberInputInfoList = [
+  { id: uuid(), type: 'text' },
+  { id: uuid(), type: 'text' },
+  { id: uuid(), type: 'password' },
+  { id: uuid(), type: 'password' },
+];
+
+const expiryDateInputInfoList = [
+  { id: uuid(), type: 'text', placeholder: 'MM' },
+  { id: uuid(), type: 'text', placeholder: 'YY' },
+];
+
+const cardOwnerNameInfoList = [
+  { id: uuid(), type: 'text', placeholder: '카드에 표시된 이름과 동일하게 입력하세요.' },
+];
+
+const privacyCodeInfoList = [{ id: uuid(), type: 'password', className: 'w-25' }];
+
+const cardPasswordInfoList = [
+  { id: uuid(), type: 'password', className: 'w-15' },
+  { id: uuid(), type: 'password', className: 'w-15' },
+  { id: uuid(), type: 'password', className: 'w-15' },
+  { id: uuid(), type: 'password', className: 'w-15' },
+];
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <FormInput inputInfoList={cardNumberInputInfoList} inputTitle="카드 번호" />
+      <FormInput inputInfoList={expiryDateInputInfoList} className="w-50" inputTitle="만료일" />
+      <FormInput inputInfoList={cardOwnerNameInfoList} inputTitle="카드 소유자 이름(선택)" />
+      <FormInput inputInfoList={privacyCodeInfoList} inputTitle="보안코드(CVC/CVV)" />
+      <FormInput inputInfoList={cardPasswordInfoList} inputTitle="카드 비밀번호" />
+    </>
+  );
 }
 
 export default App;
--- src/components/common/FormInput.jsx
+++ src/components/common/FormInput.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const FormInput = () => {
-  return <input />;
+const FormInput = ({ inputTitle, inputInfoList, className = '' }) => {
+  return (
+    <div className="input-container">
+      <label>{inputTitle}</label>
+      <div className={`input-box ${className}`}>
+        {inputInfoList.map(({ type, placeholder = '', id, className = '' }) => (
+          <input
+            key={id}
+            type={type}
+            className={`input-basic ${className}`}
+            placeholder={placeholder}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+FormInput.propTypes = {
+  inputTitle: PropTypes.string.isRequired,
+  inputInfoList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      placeholder: PropTypes.string,
+      className: PropTypes.string,
+    }),
+  ),
+  className: PropTypes.string,
 };
 
 export default FormInput;
