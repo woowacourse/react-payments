59a2d44e76935f8f923c8f89011613d52e6a0fd1
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-04-26 16:27:53 +0900

    chore: CRA 및 ESlint 프로젝트 기본 설정
    Co-authored-by: lokba <lokba@users.noreply.github.com>

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// 성능 측정하기를 원한다면, 로그함수를 인자로 넘기면 성능을 콘솔에 찍어줍니다.
+// 예시 : reportWebVitals(console.log)
+// 참조 : https://bit.ly/CRA-vitals
+reportWebVitals();
