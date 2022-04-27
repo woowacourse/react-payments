498abfa5d4c931fd14d86adbd886d0ecd3c07cd2
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-04-27 16:55:37 +0900

    feat: FormInput 비즈니스 상태 로직 작성
    Co-authored-by: lokba <lokba@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import FormInput from './components/common/FormInput';
+import { useState } from 'react';
 import { v4 as uuid } from 'uuid';
 
+import FormInput from './components/common/FormInput';
+
 const cardNumberInputInfoList = [
-  { id: uuid(), type: 'text', className: 'mr-n15' },
-  { id: uuid(), type: 'text', className: 'mr-n15' },
-  { id: uuid(), type: 'password', className: 'mr-n15' },
-  { id: uuid(), type: 'password' },
+  { id: uuid(), type: 'text', className: 'mr-n15', name: 'first' },
+  { id: uuid(), type: 'text', className: 'mr-n15', name: 'second' },
+  { id: uuid(), type: 'password', className: 'mr-n15', name: 'third' },
+  { id: uuid(), type: 'password', name: 'fourth' },
 ];
 
 const expiryDateInputInfoList = [
-  { id: uuid(), type: 'text', placeholder: 'MM', className: 'mr-n15' },
-  { id: uuid(), type: 'text', placeholder: 'YY' },
+  { id: uuid(), type: 'text', placeholder: 'MM', className: 'mr-n15', name: 'month' },
+  { id: uuid(), type: 'text', placeholder: 'YY', name: 'year' },
 ];
 
 const cardOwnerNameInfoList = [
@@ -20,20 +22,91 @@ const cardOwnerNameInfoList = [
 const privacyCodeInfoList = [{ id: uuid(), type: 'password', className: 'w-25' }];
 
 const cardPasswordInfoList = [
-  { id: uuid(), type: 'password', className: 'w-15' },
-  { id: uuid(), type: 'password', className: 'w-15' },
+  { id: uuid(), type: 'password', className: 'w-15', name: 'first' },
+  { id: uuid(), type: 'password', className: 'w-15', name: 'second' },
   { id: uuid(), type: 'password', className: 'w-15' },
   { id: uuid(), type: 'password', className: 'w-15' },
 ];
 
 function App() {
+  const [cardInfo, setCardInfo] = useState({
+    number: {
+      first: '',
+      second: '',
+      third: '',
+      fourth: '',
+    },
+    expiryDate: {
+      month: '',
+      year: '',
+    },
+    ownerName: '',
+    privacyCode: '',
+    password: {
+      first: '',
+      second: '',
+    },
+  });
+
+  const onChange = ({ target }, item) => {
+    const { name, value } = target;
+
+    setCardInfo((prevCardInfo) => {
+      if (typeof prevCardInfo[item] === 'object') {
+        return {
+          ...prevCardInfo,
+          [item]: {
+            ...prevCardInfo[item],
+            [name]: value,
+          },
+        };
+      }
+
+      return {
+        ...prevCardInfo,
+        [item]: value,
+      };
+    });
+  };
+
   return (
     <>
-      <FormInput inputInfoList={cardNumberInputInfoList} inputTitle="카드 번호" />
-      <FormInput inputInfoList={expiryDateInputInfoList} className="w-50" inputTitle="만료일" />
-      <FormInput inputInfoList={cardOwnerNameInfoList} inputTitle="카드 소유자 이름(선택)" />
-      <FormInput inputInfoList={privacyCodeInfoList} inputTitle="보안코드(CVC/CVV)" />
-      <FormInput inputInfoList={cardPasswordInfoList} inputTitle="카드 비밀번호" />
+      <FormInput
+        item="number"
+        inputTitle="카드 번호"
+        inputInfoList={cardNumberInputInfoList}
+        cardInfo={cardInfo}
+        onChange={onChange}
+      />
+      <FormInput
+        className="w-50"
+        item="expiryDate"
+        inputTitle="만료일"
+        inputInfoList={expiryDateInputInfoList}
+        cardInfo={cardInfo}
+        onChange={onChange}
+      />
+      <FormInput
+        item="ownerName"
+        inputTitle="카드 소유자 이름(선택)"
+        inputInfoList={cardOwnerNameInfoList}
+        cardInfo={cardInfo}
+        onChange={onChange}
+      />
+      <FormInput
+        item="privacyCode"
+        inputTitle="보안코드(CVC/CVV)"
+        inputInfoList={privacyCodeInfoList}
+        onChange={onChange}
+        cardInfo={cardInfo}
+      />
+      <FormInput
+        item="password"
+        inputTitle="카드 비밀번호"
+        inputInfoList={cardPasswordInfoList}
+        cardInfo={cardInfo}
+        onChange={onChange}
+      />
     </>
   );
 }
--- src/components/common/FormInput.jsx
+++ src/components/common/FormInput.jsx
@@ -7,6 +7,10 @@ const propTypes = {
    */
   className: PropTypes.string,
   /**
+   * category of FormInput
+   */
+  item: PropTypes.string.isRequired,
+  /**
    * name of FormInput
    */
   inputTitle: PropTypes.string.isRequired,
@@ -16,29 +20,49 @@ const propTypes = {
   inputInfoList: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
+      name: PropTypes.string,
       type: PropTypes.string.isRequired,
       placeholder: PropTypes.string,
       className: PropTypes.string,
     }),
   ).isRequired,
+  /**
+   * card information for input value
+   */
+  cardInfo: PropTypes.shape({
+    number: PropTypes.object.isRequired,
+    expiryDate: PropTypes.object.isRequired,
+    ownerName: PropTypes.string,
+    privacyCode: PropTypes.string.isRequired,
+    password: PropTypes.object.isRequired,
+  }),
+  /**
+   * handle change event of input tag
+   */
+  onChange: PropTypes.func,
 };
 
 const defaultProps = {
   className: '',
+  cardInfo: {},
+  onChange: undefined,
 };
 
 // component
-const FormInput = ({ className, inputTitle, inputInfoList }) => {
+const FormInput = ({ className, item, inputTitle, inputInfoList, cardInfo, onChange }) => {
   return (
     <div className="input-container">
       <label>{inputTitle}</label>
       <div className={`input-box ${className}`}>
-        {inputInfoList.map(({ id, type, className = '', placeholder = '' }) => (
+        {inputInfoList.map(({ id, name, type, className = '', placeholder = '' }) => (
           <input
             key={id}
+            name={name}
             type={type}
             className={`input-basic ${className}`}
             placeholder={placeholder}
+            value={typeof cardInfo[item] === 'object' ? cardInfo[item][name] : cardInfo[item]}
+            onChange={(e) => onChange(e, item)}
           />
         ))}
       </div>
