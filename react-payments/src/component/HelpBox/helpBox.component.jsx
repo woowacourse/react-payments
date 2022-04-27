c4297330cb1d29c95446e8c2de67d2d3f06ab568
Hwang Jung Min <hwangstar156@gmail.com>
2022-04-27 17:59:40 +0900

    feat: helpbox component mouse 이벤트 추가
     Co-authored-by: ronci <ronci@users.noreply.github.com>

--- react-payments/src/component/HelpBox/helpBox.component.jsx
+++ react-payments/src/component/HelpBox/helpBox.component.jsx
@@ -1,7 +1,22 @@
 import "./helpBox.css";
+import { useState } from "react";
 
 const HelpBox = () => {
-  return <div className="help-box-container">?</div>;
+  const [isShown, setIsShown] = useState(false);
+  return (
+    <div class="help-box-container">
+      <div
+        className="help-box"
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}
+      >
+        ?
+      </div>
+      {isShown && (
+        <div className="help-modal">카드 뒷면의 3자리 숫자를 입력해주세요.</div>
+      )}
+    </div>
+  );
 };
 
 export default HelpBox;
