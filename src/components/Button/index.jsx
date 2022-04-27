bfac85dd9660867dab8130da65a56ab600864077
sangminsong <ssmin940606@gmail.com>
2022-04-27 11:22:15 +0900

    fix: 타입 검사 안되는 버그 해결
    Co-authored-by: LAH1203 <LAH1203@users.noreply.github.com>

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -14,17 +14,17 @@ const getButtonSizeTemplate = size => {
 };
 
 export default function Button({
-  bgColor = 'white',
-  border = '0px',
-  color = 'black',
+  bgColor,
+  border,
+  color,
   content,
-  fontWeight = 'normal',
-  shape = 'rectangle',
-  size = 'medium',
+  fontWeight,
+  shape,
+  size,
   onClickFunc,
 }) {
   const buttonStyle = getButtonSizeTemplate(size);
-  const Button = styled.button`
+  const StyledButton = styled.button`
     background: ${bgColor};
     border: ${border};
     border-radius: ${shape === 'circle' && '50%'};
@@ -35,16 +35,25 @@ export default function Button({
     width: ${shape === 'circle' && buttonStyle.height};
   `;
 
-  return <Button onClick={onClickFunc}>{content}</Button>;
+  return <StyledButton onClick={onClickFunc}>{content}</StyledButton>;
 }
+
+Button.defaultProps = {
+  bgColor: 'white',
+  border: '0px',
+  color: 'black',
+  fontWeight: 'normal',
+  shape: 'rectangle',
+  size: 'medium',
+};
 
 Button.propTypes = {
   bgColor: PropTypes.string,
   border: PropTypes.string,
   color: PropTypes.string,
-  content: PropTypes.node,
+  content: PropTypes.node.isRequired,
   fontWeight: PropTypes.string,
   shape: PropTypes.string,
-  size: PropTypes.object,
-  onClickFunc: PropTypes.func,
+  size: PropTypes.string,
+  onClickFunc: PropTypes.func.isRequired,
 };
