70b12adfbc18fd80033e1328759b0bb940566f39
Jeongmin Woo <greenblues1190@gmail.com>
2022-04-27 21:55:16 +0900

    feat: App.jsx에 CardForm 추가
    Co-authored-by: jhy979 <jhy979@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import './App.css';
-import Field from './components/Field';
+import CardForm from './components/CardForm';
+import ToolTip from './components/ToolTip';
 
 const App = () => {
   return (
     <div className="App">
-      <Field>
-        <input name="first" value="wow" />
-        <p>hi</p>
-        <input name="second" />
-      </Field>
+      <CardForm />
     </div>
   );
 };
