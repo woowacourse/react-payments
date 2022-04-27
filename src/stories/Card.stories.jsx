00fa20a372db6a45b1677250c62efaf794bca3ef
Ahn <tn3748@naver.com>
2022-04-27 14:02:21 +0900

    feat: card 컴포넌트 stories 작성
    Co-authored-by: DomMorello <zuzudnf@gmail.com>

---
+++ src/stories/Card.stories.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from '../components/Card.jsx';
+import '../css/card.css';
+
+export default {
+  title: 'Card',
+  component: Card,
+};
+
+const Template = (args) => <Card {...args} />;
+
+export const Deactivated = Template.bind({});
+
+Deactivated.args = {
+  isEmptyCard: true,
+  cardNumber: '',
+  ownerName: 'NAME',
+  expireDate: 'MM/YY',
+};
+
+export const Activated = Template.bind({});
+
+Activated.args = {
+  isEmptyCard: false,
+  cardNumber: '1324 1234 oooo oooo',
+  ownerName: 'AHN',
+  expireDate: '04/23',
+};
