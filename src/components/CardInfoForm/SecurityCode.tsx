f7ab6f052d697f91758564d45bef151008fe9c13
gytks4 <gytks4@naver.com>
2022-04-27 17:48:21 +0900

    feat: CVC guide 이미지 보여주기
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>

--- src/components/CardInfoForm/SecurityCode.tsx
+++ src/components/CardInfoForm/SecurityCode.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
+import CVC from "../../assets/cvcImage.png";
 import Input from "../../elements/Input";
 
 export default function SecurityCode({ securityCode, onChange }) {
+  const [isCvcShown, setIsCvcShown] = useState(false);
+
   return (
     <div className="input-container">
       <span className="input-title">보안코드(CVC/CVV)</span>
@@ -13,6 +16,16 @@ export default function SecurityCode({ securityCode, onChange }) {
         onChange={onChange}
         maxLength={3}
       />
+      <div className="cvc-container">
+        <span
+          className="cvc-guide"
+          onMouseEnter={() => setIsCvcShown(true)}
+          onMouseLeave={() => setIsCvcShown(false)}
+        >
+          ?
+        </span>
+        {isCvcShown && <img className="cvc-image" src={CVC} alt="cvc" />}
+      </div>
     </div>
   );
 }
