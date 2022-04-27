1b753d923e7a42410e9902dd529786d4c98821bd
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-04-27 16:58:46 +0900

    test: FormInput - story 업데이트
    Co-authored-by: lokba <lokba@users.noreply.github.com>

--- src/stories/FormInput.stories.jsx
+++ src/stories/FormInput.stories.jsx
@@ -15,6 +15,7 @@ const Template = (args) => <FormInput {...args} />;
 
 export const CardNumberFormInput = Template.bind({});
 CardNumberFormInput.args = {
+  item: 'number',
   inputTitle: '카드 번호',
   inputInfoList: [
     { id: uuid(), type: 'text', className: 'mr-n15' },
@@ -26,6 +27,7 @@ CardNumberFormInput.args = {
 
 export const ExpiryDateFormInput = Template.bind({});
 ExpiryDateFormInput.args = {
+  item: 'expiryDate',
   className: 'w-50',
   inputTitle: '만료일',
   inputInfoList: [
@@ -36,6 +38,7 @@ ExpiryDateFormInput.args = {
 
 export const CardOwnerNameFormInput = Template.bind({});
 CardOwnerNameFormInput.args = {
+  item: 'ownerName',
   inputTitle: '카드 소유자 이름(선택)',
   inputInfoList: [
     { id: uuid(), type: 'text', placeholder: '카드에 표시된 이름과 동일하게 입력하세요.' },
@@ -44,12 +47,14 @@ CardOwnerNameFormInput.args = {
 
 export const PrivacyCodeFormInput = Template.bind({});
 PrivacyCodeFormInput.args = {
+  item: 'privacyCode',
   inputTitle: '보안코드(CVC/CVV)',
   inputInfoList: [{ id: uuid(), type: 'password', className: 'w-25' }],
 };
 
 export const CardPasswordFormInput = Template.bind({});
 CardPasswordFormInput.args = {
+  item: 'password',
   inputTitle: '카드 비밀번호',
   inputInfoList: [
     { id: uuid(), type: 'password', className: 'w-15' },
