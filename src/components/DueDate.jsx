0bb99a87ef6f9c333c09733ad1601de2d3532bd7
intae92 <kangit2013@gmail.com>
2022-04-27 14:45:59 +0900

    feat: DueDate component 구현
    Co-authored-by: TaeYoon <uni613@naver.com>

---
+++ src/components/DueDate.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useRef } from 'react';
+import { hyphenPrimaryColor } from '../style';
+import Calendar from './Calendar';
+import { InputContainer, InputWrapper, Span, Label } from './common';
+
+const getList = (length, n) => Array.from({ length }, (_, i) => `${i + n}`.slice(2));
+
+const MONTH = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+
+function DueDate() {
+  const now = useRef(new Date());
+  const months = useRef(MONTH);
+  const years = useRef(getList(10, now.current.getFullYear()));
+
+  const [month, setMonth] = useState();
+  const [year, setYear] = useState();
+
+  return (
+    <InputContainer>
+      <Label>만료일</Label>
+      <InputWrapper color={hyphenPrimaryColor}>
+        <Calendar items={months.current} setItem={setMonth} placeholder="MM" />
+        <Span>/</Span>
+        <Calendar items={years.current} setItem={setYear} placeholder="YY" />
+      </InputWrapper>
+    </InputContainer>
+  );
+}
+
+export default DueDate;
---
+++ src/components/DueDate.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import DueDate from './DueDate';
+
+export default {
+  title: 'DueDate',
+  component: DueDate,
+};
+
+export const dueDate = () => <DueDate />;
