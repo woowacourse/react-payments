008e1b33edc0752e1ed5e3cb64973309a3916498
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-04-27 15:09:57 +0900

    test: FormInput - story 작성
    Co-authored-by: lokba <lokba@users.noreply.github.com>

--- src/components/common/FormInput.jsx
+++ src/components/common/FormInput.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FormInput = ({ inputTitle, inputInfoList, className = '' }) => {
+const propTypes = {
+  /**
+   * className of FormInput
+   */
+  className: PropTypes.string,
+  /**
+   * name of FormInput
+   */
+  inputTitle: PropTypes.string.isRequired,
+  /**
+   * information of input tags in FormInput
+   */
+  inputInfoList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      placeholder: PropTypes.string,
+      className: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+const defaultProps = {
+  className: '',
+};
+
+// component
+const FormInput = ({ className, inputTitle, inputInfoList }) => {
   return (
     <div className="input-container">
       <label>{inputTitle}</label>
       <div className={`input-box ${className}`}>
-        {inputInfoList.map(({ type, placeholder = '', id, className = '' }) => (
+        {inputInfoList.map(({ id, type, className = '', placeholder = '' }) => (
           <input
             key={id}
             type={type}
@@ -19,17 +46,7 @@ const FormInput = ({ inputTitle, inputInfoList, className = '' }) => {
   );
 };
 
-FormInput.propTypes = {
-  inputTitle: PropTypes.string.isRequired,
-  inputInfoList: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      type: PropTypes.string.isRequired,
-      placeholder: PropTypes.string,
-      className: PropTypes.string,
-    }),
-  ),
-  className: PropTypes.string,
-};
+FormInput.defaultProps = defaultProps;
+FormInput.propTypes = propTypes;
 
 export default FormInput;
---
+++ src/stories/FormInput.stories.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { v4 as uuid } from 'uuid';
+
+import FormInput from '../components/common/FormInput';
+
+import '../css/input.css';
+import '../css/utils.css';
+
+export default {
+  title: 'Components/common/FormInput',
+  component: FormInput,
+};
+
+const Template = (args) => <FormInput {...args} />;
+
+export const CardNumberFormInput = Template.bind({});
+CardNumberFormInput.args = {
+  inputTitle: '카드 번호',
+  inputInfoList: [
+    { id: uuid(), type: 'text', className: 'mr-n15' },
+    { id: uuid(), type: 'text', className: 'mr-n15' },
+    { id: uuid(), type: 'password', className: 'mr-n15' },
+    { id: uuid(), type: 'password' },
+  ],
+};
+
+export const ExpiryDateFormInput = Template.bind({});
+ExpiryDateFormInput.args = {
+  className: 'w-50',
+  inputTitle: '만료일',
+  inputInfoList: [
+    { id: uuid(), type: 'text', placeholder: 'MM', className: 'mr-n15' },
+    { id: uuid(), type: 'text', placeholder: 'YY' },
+  ],
+};
+
+export const CardOwnerNameFormInput = Template.bind({});
+CardOwnerNameFormInput.args = {
+  inputTitle: '카드 소유자 이름(선택)',
+  inputInfoList: [
+    { id: uuid(), type: 'text', placeholder: '카드에 표시된 이름과 동일하게 입력하세요.' },
+  ],
+};
+
+export const PrivacyCodeFormInput = Template.bind({});
+PrivacyCodeFormInput.args = {
+  inputTitle: '보안코드(CVC/CVV)',
+  inputInfoList: [{ id: uuid(), type: 'password', className: 'w-25' }],
+};
+
+export const CardPasswordFormInput = Template.bind({});
+CardPasswordFormInput.args = {
+  inputTitle: '카드 비밀번호',
+  inputInfoList: [
+    { id: uuid(), type: 'password', className: 'w-15' },
+    { id: uuid(), type: 'password', className: 'w-15' },
+    { id: uuid(), type: 'password', className: 'w-15' },
+    { id: uuid(), type: 'password', className: 'w-15' },
+  ],
+};
