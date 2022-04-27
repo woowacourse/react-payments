46e16105fbdbfa958167bcb9113392db28a2f363
최광희 <polyckh@gmail.com>
2022-04-27 20:07:04 +0900

    feat: CVCInputForm 컴포넌트 구현
    Co-authored-by: YongRae_Kim <kyr9389@naver.com>

--- src/components/cardRegister/CVCInputForm.js
+++ src/components/cardRegister/CVCInputForm.js
@@ -4,12 +4,20 @@ import { InputBasic } from '../common/InputBasic';
 import { InputBox } from '../common/InputBox';
 import { InputContainer, InputTitle } from '../common/styled';
 
-export const CVCInputForm = () => {
+export const CVCInputForm = ({ CVC, handleCVCInput }) => {
+  const handleCVCChange = (e) => {
+    if (isNaN(e.nativeEvent.data) || e.target.value.length > 3) {
+      return;
+    }
+
+    handleCVCInput(e.target.value);
+  };
+
   return (
     <InputContainer>
       <InputTitle>보안카드(CVC/CVV)</InputTitle>
       <InputBox width="25%">
-        <InputBasic type="password" value="111" />
+        <InputBasic type="password" value={CVC} onChange={handleCVCChange} />
       </InputBox>
     </InputContainer>
   );
