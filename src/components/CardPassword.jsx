1c2ea8a6f680aeda20938c884a301ec507939e34
intae92 <kangit2013@gmail.com>
2022-04-27 19:38:38 +0900

    feat: CardPassword 컴포넌트 구현
    - 비밀번호 유효성 검증
    
    Co-authored-by: TaeYoon <uni613@naver.com>

---
+++ src/components/CardPassword.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Input, InputContainer, Label, InputWrapper } from './common';
+import InactiveContainer from './common/InactiveContainer';
+
+const MAX_PASSWORD_UNIT = 1;
+
+const validator = value => {
+  if (value.includes(' ') || Number.isNaN(Number(value))) {
+    throw new Error('숫자만 입력해주세요.');
+  }
+
+  if (value.includes('.')) {
+    throw new Error('소수점은 입력하실 수 없습니다.');
+  }
+
+  if (value.length > MAX_PASSWORD_UNIT) {
+    throw new Error(`한 칸당 최대 ${MAX_PASSWORD_UNIT}개의 숫자를 입력해야 합니다.`);
+  }
+};
+
+function CardPassword() {
+  const [pwd, setPwd] = useState({
+    pwdNoA: '',
+    pwdNoB: '',
+  });
+
+  const handleInputChange = ({ target: { name, value } }) => {
+    try {
+      validator(value);
+    } catch (err) {
+      console.log(err.message);
+      return;
+    }
+    setPwd(prevPwds => ({
+      ...prevPwds,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <InputContainer>
+      <Label>카드 비밀번호</Label>
+      <InputWrapper>
+        <Input type="password" maxLength={1} name="pwdNoA" onChange={handleInputChange} value={pwd.pwdNoA} />
+        <Input type="password" maxLength={1} name="pwdNoB" onChange={handleInputChange} value={pwd.pwdNoB} />
+        <InactiveContainer />
+        <InactiveContainer />
+      </InputWrapper>
+    </InputContainer>
+  );
+}
+
+export default CardPassword;
