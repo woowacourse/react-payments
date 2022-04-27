36cca21f9eea412b20b6c1288b1a11960b609a83
gytks4 <gytks4@naver.com>
2022-04-27 15:15:14 +0900

    feat: CardNumber 사이에 - 추가
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,10 +1,9 @@
 import React, { InputHTMLAttributes } from "react";
 
-interface InputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "maxLength"> {
-  maxLength?: number;
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   width?: "w-15" | "w-25" | "w-50" | "w-75" | "w-100";
 }
 
-export default function Input({ width, maxLength, ...props }: InputProps) {
+export default function Input({ width, ...props }: InputProps) {
   return <input className={`input-basic ${width}`} {...props} />;
 }
