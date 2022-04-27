fa57d926916258850298cc46fdfe6e871763956b
woose28 <dldntmdsla@naver.com>
2022-04-27 17:42:55 +0900

    feat: Input 컴포넌트 작성
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>

---
+++ src/components/Input/Input.stories.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Input from '.';
+import '../../index.css';
+
+export default {
+  title: 'Payment/Input',
+  component: Input,
+};
+
+const Template = args => <Input {...args} />;
+
+export const Default = Template.bind({});
+export const Month = Template.bind({});
+export const OwnerName = Template.bind({});
+export const SecurityNumber = Template.bind({});
+export const Password = Template.bind({});
+
+Default.args = {
+  type: 'number',
+  width: '318px',
+  placeholder: '카드에 표시된 이름과 동일하게 입력하세요.',
+};
+
+Month.args = {
+  type: 'text',
+  placeholder: 'MM / YY',
+  width: '137px',
+  isCenter: true,
+  maxLength: 5,
+};
+
+OwnerName.args = {
+  type: 'text',
+  width: '318px',
+  placeholder: '카드에 표시된 이름과 동일하게 입력하세요.',
+  isCenter: true,
+  maxLength: 30,
+};
+
+SecurityNumber.args = {
+  type: 'password',
+  width: '84px',
+  isCenter: true,
+  maxLength: 3,
+};
+
+Password.args = {
+  type: 'password',
+  width: '43px',
+  isCenter: true,
+  maxLength: 1,
+};
---
+++ src/components/Input/index.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+const StyledInput = styled.input`
+  width: ${props => props.width};
+  height: 45px;
+  background: #ecebf1;
+  border-radius: 7px;
+  padding: 12px;
+  font-weight: 500;
+  font-size: 17px;
+  line-height: 21px;
+  text-align: ${props => props.textAlign};
+  color: #737373;
+`;
+
+function Input({ width, type, maxLength, isCenter, placeholder, onChange }) {
+  return (
+    <StyledInput
+      width={width}
+      type={type}
+      maxLength={maxLength}
+      textAlign={isCenter ? 'center' : 'left'}
+      placeholder={placeholder}
+      onChange={onChange}
+    />
+  );
+}
+
+export default Input;
