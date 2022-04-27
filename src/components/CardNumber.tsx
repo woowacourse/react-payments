cb0131a6d8a33893d40b4990076cac94f417a4c6
gytks4 <gytks4@naver.com>
2022-04-27 15:24:36 +0900

    fix: key를 Fragment에 붙여주기
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>

--- src/components/CardNumber.tsx
+++ src/components/CardNumber.tsx
@@ -8,16 +8,15 @@ export default function CardNumber({ cardNumbers, onChange }) {
       <span className="input-title">카드 번호</span>
       <div className="input-box">
         {cardNumbers.map((cardNumber, index) => (
-          <>
+          <React.Fragment key={index}>
             <Input
-              key={index}
               type={index < 2 ? "text" : "password"}
               value={cardNumber}
               onChange={e => onChange(e, index)}
               maxLength={4}
             />
             {index !== 3 ? <span>-</span> : null}
-          </>
+          </React.Fragment>
         ))}
       </div>
     </div>
