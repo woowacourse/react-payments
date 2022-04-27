a573c41422bbc6ea1818c1acd60dc9656ce2c4e1
jswith <typingjs@gmail.com>
2022-04-27 15:49:42 +0900

    :lipstick: style: 카드 번호 스타일 추가
    Co-authored-by: rladpwl0512 <rladpwl0512@gmail.com>

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -12,7 +12,14 @@ function Card({ isEmpty, handleCardAdd }) {
             <div className="card-middle">
               <div className="small-card__chip"></div>
             </div>
+
             <div className="card-bottom">
+              <div className="card-text card-number-flex">
+                <div>0000</div>
+                <div>1111</div>
+                <div>2222</div>
+                <div>3333</div>
+              </div>
               <div className="card-bottom__info">
                 <span className="card-text">NAME</span>
                 <span className="card-text">MM / YY</span>
