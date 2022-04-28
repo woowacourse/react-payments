242a6b9c64bb98b75afbf53098f876d118dca5e6
intae92 <kangit2013@gmail.com>
2022-04-28 13:41:14 +0900

    feat: Calendar컴포넌트에  모달 컴포넌트 합성
    Co-authored-by: TaeYoon <uni613@naver.com>

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,30 +1,45 @@
 import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { Input } from './common';
+import Modal from './common/Modal';
 
-const CalendarContainer = styled.div`
-  position: relative;
-`;
+const CalendarContainer = styled.div``;
 
 const SelectBox = styled.div``;
 
 const SelectLayer = styled.div`
   display: ${props => (props.isShown ? 'block' : 'none')};
-  position: absolute;
+  width: 100%;
+  margin: 16px;
 `;
 
 const SelectListWrapper = styled.div``;
 
 const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   list-style: none;
   padding-left: 0;
 `;
 
-const Item = styled.li``;
+const Item = styled.li`
+  text-align: center;
+  margin: 8px 0;
+  padding: 8px;
+  width: 50%;
+  box-shadow: 0 4px 8px -8px #525252;
+  border-radius: 8px;
+  cursor: pointer;
 
-// const month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  &:hover {
+    background-color: #d2d2d2;
+  }
+`;
+
 // TODO: 네이밍
-function Calendar({ items, placeholder, setItem }) {
+function Calendar({ items, placeholder, setItem, dimensions }) {
   const inputRef = useRef(null);
   const [isShown, setIsShown] = useState();
   const [selectedDate, setSelectedDate] = useState();
@@ -45,17 +60,19 @@ function Calendar({ items, placeholder, setItem }) {
       <SelectBox onClick={handleClickBox}>
         <Input ref={inputRef} disabled placeholder={placeholder} type="text" />
       </SelectBox>
-      <SelectLayer isShown={isShown}>
-        <SelectListWrapper>
-          <List>
-            {items.map(m => (
-              <Item key={m} onClick={() => handleClickDate(m)}>
-                {m}
-              </Item>
-            ))}
-          </List>
-        </SelectListWrapper>
-      </SelectLayer>
+      <Modal isOpen={isShown} setIsOpen={setIsShown} dimensions={dimensions}>
+        <SelectLayer isShown={isShown}>
+          <SelectListWrapper>
+            <List>
+              {items.map(m => (
+                <Item key={m} onClick={() => handleClickDate(m)}>
+                  {m}
+                </Item>
+              ))}
+            </List>
+          </SelectListWrapper>
+        </SelectLayer>
+      </Modal>
     </CalendarContainer>
   );
 }
