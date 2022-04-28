d2ab5452093de4352b325e86a9820bbe16a69d84
Marco <bigdatajang@gmail.com>
2022-04-28 11:50:02 +0900

    feat: 카드 만료일 인풋 유효성 검사 추가 - 월 인풋은 1월부터 12월까지 중 하나여야 한다. - 현재 연월 기준으로 이후 연월만 입력되어야 한다.
    Co-authored-by: soobin <sanaandmomo@users.noreply.github.com>

--- src/validations/validator.js
+++ src/validations/validator.js
@@ -17,6 +17,21 @@ const validator = {
     if (0 < cardExpiration.join('').length && cardExpiration.join('').length < 4) {
       throw new Error('카드 만료일에 월 2자리(MM) 및 년 2자리(MM)를 모두 입력해주세요.');
     }
+
+    if (Number(cardExpiration[0]) < 1 || Number(cardExpiration[0]) > 12) {
+      throw new Error('카드 만료월은 1월부터 12월 이내의 수만 입력해주세요.');
+    }
+
+    const today = new Date();
+    const year = today.getFullYear() % 2000;
+    const month = today.getMonth() + 1;
+
+    if (
+      Number(cardExpiration[1]) < year ||
+      (Number(cardExpiration[1]) === year && Number(cardExpiration[0]) < month)
+    ) {
+      throw new Error('입력하신 만료일은 이미 경과되었습니다.');
+    }
   },
 };
 
