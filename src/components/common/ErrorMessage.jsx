fce833e3e83d9f734cfa1dcffaac7d789efb5999
intae92 <kangit2013@gmail.com>
2022-04-28 18:57:59 +0900

    style: 에러메시지 컴포넌트 스타일 변경
    Co-authored-by: TaeYoon <uni613@naver.com>

--- src/components/common/ErrorMessage.jsx
+++ src/components/common/ErrorMessage.jsx
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 
 const ErrorMessageCotainer = styled.span`
   width: 100%;
-  padding: 8px;
+  padding: 8px 8px 8px 0;
   color: #f00;
   font-size: 4px;
   font-weight: 300;
+  min-height: 25px;
 `;
 
 function ErrorMessage({ children }) {
