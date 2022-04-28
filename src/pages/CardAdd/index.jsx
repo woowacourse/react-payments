e599c94f6192f2aab32340bb66de3a911623089a
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-04-28 21:22:02 +0900

    feat: 카드 추가 페이지 추가

--- src/pages/CardAdd/index.jsx
+++ src/pages/CardAdd/index.jsx
@@ -1,5 +1,109 @@
+import useCardState from 'hooks/useCardState';
+
+import Header from 'components/@common/Header';
+import Button from 'components/@common/Button';
+
+import Card from 'components/Card';
+import CardNumberField from 'components/CardNumberField';
+import CardExpireDateField from 'components/CardExpireDateField';
+import CardUserNameField from 'components/CardUserNameField';
+import CardSecurityField from 'components/CardSecurityField';
+import CardPasswordField from 'components/CardPasswordField';
+
+import { ACTION_TYPE } from 'constants';
+import {
+  validateCardPassword,
+  validateExpireDate,
+  validateSecurityCode,
+  validateUserName,
+} from 'validators';
+
 function CardAdd() {
-  return <></>;
+  const [state, dispatch] = useCardState();
+  const { cardNumber, expireMonth, expireYear, userName, securityCode, cardPassword } = state;
+  const { isComplete } = state;
+
+  const onChangeTextField = ({ target }) => {
+    const textField = target.name;
+
+    const dispatchAction = {
+      cardNumber: () => ({
+        type: ACTION_TYPE.UPDATE_CARD_NUMBER,
+        contents: target.value,
+      }),
+      expireMonth: () => ({
+        type: ACTION_TYPE.UPDATE_EXPIRE_MONTH,
+        contents: target.value,
+      }),
+      expireYear: () => ({
+        type: ACTION_TYPE.UPDATE_EXPIRE_YEAR,
+        contents: target.value,
+      }),
+      userName: () => ({
+        type: ACTION_TYPE.UPDATE_USER_NAME,
+        contents: target.value,
+      }),
+      securityCode: () => ({
+        type: ACTION_TYPE.UPDATE_SECURITY_CODE,
+        contents: target.value,
+      }),
+      cardPassword: () => ({
+        type: ACTION_TYPE.UPDATE_CARD_PASSWORD,
+        contents: target.value,
+      }),
+    };
+
+    dispatch(dispatchAction[textField]());
+  };
+
+  const onClickConfirmButton = () => {
+    try {
+      validateCardPassword(cardPassword);
+      validateExpireDate(expireMonth, expireYear);
+      validateSecurityCode(securityCode);
+      // eslint-disable-next-line no-unused-expressions
+      userName && validateUserName(userName);
+
+      alert(`
+        리뷰어님 체크용 ✅
+        카드번호: ${cardNumber}
+        만료일: ${expireMonth} / ${expireYear}
+        소유자: ${userName}
+        보안 코드: ${securityCode}
+        비밀 번호: ${cardPassword}
+      `);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <>
+      <Header>카드 추가</Header>
+      <Card
+        companyName="티거 카드"
+        cardNumber={cardNumber}
+        userName={userName}
+        expireMonth={expireMonth}
+        expireYear={expireYear}
+      />
+      <CardNumberField cardNumber={cardNumber} onChange={onChangeTextField} />
+      <CardExpireDateField
+        expireMonth={expireMonth}
+        expireYear={expireYear}
+        onChange={onChangeTextField}
+      />
+      <CardUserNameField userName={userName} onChange={onChangeTextField} />
+      <CardSecurityField securityCode={securityCode} onChange={onChangeTextField} />
+      <CardPasswordField cardPassword={cardPassword} onChange={onChangeTextField} />
+
+      <div className="button-container right">
+        <Button isDisabled={!isComplete} onClick={onClickConfirmButton}>
+          다음
+        </Button>
+      </div>
+    </>
+  );
 }
 
 export default CardAdd;
--- src/pages/CardAdd/index.stories.jsx
+++ src/pages/CardAdd/index.stories.jsx
@@ -1,16 +1,15 @@
 import CardAdd from '.';
-import '../../index.css'; // 루트 디렉터리 세팅하기!
+import 'index.css';
 
 export default {
   title: 'Page/CardAdd',
   component: CardAdd,
   parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
   },
 };
 
 const Template = (args) => <CardAdd {...args} />;
 
-export const Page = Template.bind({});
-Page.args = {};
+export const CardAddPage = Template.bind({});
+CardAddPage.args = {};
