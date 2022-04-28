fc312cce895d038e1459f916bc08d7c379ba29bf
Hwang Jung Min <hwangstar156@gmail.com>
2022-04-28 17:36:31 +0900

    feat: 선택된 colorBox는 다른 스타일이 보일수 있도록 수정
            Co-authored-by: ronci <ronci@users.noreply.github.com>

--- react-payments/src/component/ColorBox/colorBox.component.jsx
+++ react-payments/src/component/ColorBox/colorBox.component.jsx
@@ -1,13 +1,26 @@
 import "./colorBox.css";
+import { cardInfos } from "../../constants";
 
-const ColorBox = ({ colorType, cardType, idx, onClickCardType }) => {
+const ColorBox = ({
+  colorType,
+  cardType,
+  idx,
+  onClickCardType,
+  currentCardType,
+}) => {
   return (
     <div className="color-box-container">
       <div
-        className={`${colorType} color-box`}
+        className={`${colorType} color-box `}
         onClick={() => onClickCardType(idx)}
       />
-      <div className="color-box-text">{cardType}</div>
+      <div
+        className={`color-box-text ${
+          currentCardType === cardInfos[idx].cardType ? "selected" : ""
+        }`}
+      >
+        {cardType}
+      </div>
     </div>
   );
 };
