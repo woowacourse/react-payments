77599c831412ca238aecaf915af9bc338a4d1dbb
JASUN LEE <liswktjs@gmail.com>
2022-04-28 13:48:24 +0900

    feat: ask mark에 마우스를 올렸을때 안내문구를 보여준다 Co-authored-by: moonheekim0118 <hellomooneekim@gmail.com>

--- src/components/AskMark/index.jsx
+++ src/components/AskMark/index.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const AskMark = () => {
-  return <AskMarkContainer>?</AskMarkContainer>;
+  return (
+    <Container>
+      <AskMarkContainer>?</AskMarkContainer>
+      <InformationContainer>
+        카드 뒷면 서명란에 인쇄되어 있는 19자리 중 마지막 3자리 숫자 입니다
+      </InformationContainer>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  display: flex;
+  width: fit-content;
+  gap: 10px;
+  text-align: center;
+  align-items: center;
+  margin-left: 20px;
+
+  &:hover > div {
+    display: block;
+  }
+
+  &:active > div {
+    display: block;
+  }
+`;
 
 const AskMarkContainer = styled.button`
   width: 27px;
@@ -22,6 +46,15 @@ const AskMarkContainer = styled.button`
     color: #04c09e;
     border-color: #04c09e;
   }
+`;
+
+const InformationContainer = styled.div`
+  display: none;
+  width: 180px;
+  height: fit-content;
+  font-size: 12px;
+  padding: 5px;
+  word-break: keep-all;
 `;
 
 export default AskMark;
