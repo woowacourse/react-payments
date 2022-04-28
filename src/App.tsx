b57e83f914725a9afe1277e4372b64a9804a1999
gytks4 <gytks4@naver.com>
2022-04-28 14:50:21 +0900

    refactor: useCardInfo 커스텀 훅 분리
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,123 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import Card from "./components/Card";
 import CardInfoForm from "./components/CardInfoForm";
 import Header from "./components/Header";
-import REGEXP from "./contants/regexp";
+import { useCardInfo } from "./hooks/useCardInfo";
 import { useCardInfoValidation } from "./hooks/useCardInfoValidation";
 import cardInfoValidator from "./lib/validation";
-import { CardInfo, CardNumbers, Password } from "./types";
-
-const initialCardInfo: CardInfo = {
-  cardNumbers: ["", "", "", ""],
-  expiredDate: { month: "", year: "" },
-  userName: "",
-  securityCode: "",
-  password: ["", ""],
-};
 
 function App() {
-  const [cardInfo, setCardInfo] = useState<CardInfo>(initialCardInfo);
+  const {
+    cardInfo,
+    resetCardInfo,
+    onChangeCardNumber,
+    onChangeExpiredDate,
+    onChangeUserName,
+    onBlurUserName,
+    onChangeSecurityCode,
+    onChangePassword,
+  } = useCardInfo();
 
   const cardInfoValidation = useCardInfoValidation(cardInfo, cardInfoValidator);
-
-  const resetCardInfo = () => {
-    setCardInfo(initialCardInfo);
-  };
-
-  // cardInfo의 종류는 name으로 구분하고 (cardNumbers, expiredDate..)
-  // cardNumbers의 index, expiredDate의 key, 이런 것들을 date-set
-
-  const onChangeCardNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      value,
-      dataset: { index },
-    } = e.target;
-
-    if (value === "" || REGEXP.NUMBER.test(value)) {
-      setCardInfo(prevCardInfo => {
-        const newCardNumbers: CardNumbers = [...prevCardInfo.cardNumbers];
-
-        newCardNumbers[index] = value;
-
-        return {
-          ...prevCardInfo,
-          cardNumbers: newCardNumbers,
-        };
-      });
-    }
-  };
-
-  const onChangeExpiredDate = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      value,
-      dataset: { key },
-    } = e.target;
-
-    if (value === "" || REGEXP.NUMBER.test(value)) {
-      setCardInfo(prevCardInfo => {
-        const newExpiredDate = {
-          ...prevCardInfo.expiredDate,
-        };
-
-        newExpiredDate[key] = value;
-
-        return {
-          ...prevCardInfo,
-          expiredDate: newExpiredDate,
-        };
-      });
-    }
-  };
-
-  const onChangeUserName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-
-    if (cardInfo.userName === "" && value === " ") {
-      return;
-    }
-
-    if (value === "" || REGEXP.ENGLISH.test(value)) {
-      const newUserName = value.replace("  ", " ").toUpperCase();
-
-      setCardInfo(prevCardInfo => ({ ...prevCardInfo, userName: newUserName }));
-    }
-  };
-
-  const onBlurUserName = (e: React.FocusEvent<HTMLInputElement, Element>) => {
-    setCardInfo(prevCardInfo => ({ ...prevCardInfo, userName: prevCardInfo.userName.trim() }));
-  };
-
-  const onChangeSecurityCode = (e: React.FocusEvent<HTMLInputElement, Element>) => {
-    const value = e.target.value;
-
-    if (value === "" || REGEXP.NUMBER.test(value)) {
-      setCardInfo(prevCardInfo => ({
-        ...prevCardInfo,
-        securityCode: value,
-      }));
-    }
-  };
-
-  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      value,
-      dataset: { index },
-    } = e.target;
-
-    if (value === "" || REGEXP.NUMBER.test(value)) {
-      setCardInfo(prevCardInfo => {
-        const newPassword: Password = [...prevCardInfo.password];
-
-        newPassword[index] = value;
-
-        return {
-          ...prevCardInfo,
-          password: newPassword,
-        };
-      });
-    }
-  };
 
   return (
     <div className="App">
---
+++ src/hooks/useCardInfo.ts
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+
+import REGEXP from "../contants/regexp";
+import { CardInfo, CardNumbers, Password } from "../types";
+
+const initialCardInfo: CardInfo = {
+  cardNumbers: ["", "", "", ""],
+  expiredDate: { month: "", year: "" },
+  userName: "",
+  securityCode: "",
+  password: ["", ""],
+};
+
+export const useCardInfo = () => {
+  const [cardInfo, setCardInfo] = useState<CardInfo>(initialCardInfo);
+  const resetCardInfo = () => {
+    setCardInfo(initialCardInfo);
+  };
+
+  const onChangeCardNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      value,
+      dataset: { index },
+    } = e.target;
+
+    if (value === "" || REGEXP.NUMBER.test(value)) {
+      setCardInfo(prevCardInfo => {
+        const newCardNumbers: CardNumbers = [...prevCardInfo.cardNumbers];
+
+        newCardNumbers[index] = value;
+
+        return {
+          ...prevCardInfo,
+          cardNumbers: newCardNumbers,
+        };
+      });
+    }
+  };
+
+  const onChangeExpiredDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      value,
+      dataset: { key },
+    } = e.target;
+
+    if (value === "" || REGEXP.NUMBER.test(value)) {
+      setCardInfo(prevCardInfo => {
+        const newExpiredDate = {
+          ...prevCardInfo.expiredDate,
+        };
+
+        newExpiredDate[key] = value;
+
+        return {
+          ...prevCardInfo,
+          expiredDate: newExpiredDate,
+        };
+      });
+    }
+  };
+
+  const onChangeUserName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    if (cardInfo.userName === "" && value === " ") {
+      return;
+    }
+
+    if (value === "" || REGEXP.ENGLISH.test(value)) {
+      const newUserName = value.replace("  ", " ").toUpperCase();
+
+      setCardInfo(prevCardInfo => ({ ...prevCardInfo, userName: newUserName }));
+    }
+  };
+
+  const onBlurUserName = (e: React.FocusEvent<HTMLInputElement, Element>) => {
+    setCardInfo(prevCardInfo => ({ ...prevCardInfo, userName: prevCardInfo.userName.trim() }));
+  };
+
+  const onChangeSecurityCode = (e: React.FocusEvent<HTMLInputElement, Element>) => {
+    const value = e.target.value;
+
+    if (value === "" || REGEXP.NUMBER.test(value)) {
+      setCardInfo(prevCardInfo => ({
+        ...prevCardInfo,
+        securityCode: value,
+      }));
+    }
+  };
+
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      value,
+      dataset: { index },
+    } = e.target;
+
+    if (value === "" || REGEXP.NUMBER.test(value)) {
+      setCardInfo(prevCardInfo => {
+        const newPassword: Password = [...prevCardInfo.password];
+
+        newPassword[index] = value;
+
+        return {
+          ...prevCardInfo,
+          password: newPassword,
+        };
+      });
+    }
+  };
+
+  return {
+    cardInfo,
+    resetCardInfo,
+    onChangeCardNumber,
+    onChangeExpiredDate,
+    onChangeUserName,
+    onBlurUserName,
+    onChangeSecurityCode,
+    onChangePassword,
+  };
+};
