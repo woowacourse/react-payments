c3a83464e7d7c41bbacab3c3186c412ec477d42b
Jeongmin Woo <greenblues1190@gmail.com>
2022-04-29 04:11:41 +0900

    refactor:  불필요한 import 제거
    Co-authored-by: jhy979 <jhy979@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import styled from 'styled-components';
 import BackwardButton from './components/BackwardButton';
 import CardForm from './components/CardForm';
-// import ToolTip from './components/ToolTip';
 
 const StyledApp = styled.div`
   padding: 16px 24px;
