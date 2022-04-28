5b4d6d7e431e79e03ef77643e68225e49f24b1cb
airman5573 <rpf5573@kakao.com>
2022-04-29 05:32:33 +0900

    fix(card-password): 다른 필드가 다 채워졌을때 비밀번호 필드에 포커싱을 준다

--- src/components/atoms/card-password/CardPasswordInputContainerList.tsx
+++ src/components/atoms/card-password/CardPasswordInputContainerList.tsx
@@ -3,16 +3,19 @@ import { useAppState } from "../../../hooks/hooks";
 import CardPasswordInputContainer from "./CardPasswordInputContainer";
 
 function CardPasswordInputContainerList() {
-  const { password } = useAppState();
+  const { cardNumber, expiredPeriod, name, cvc, password } = useAppState();
 
   const inputRef1 = useRef<HTMLInputElement>(null);
   const inputRef2 = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    // 첫번째 필드에 값이 비었다면
-    !password[0] && inputRef1.current?.focus();
-    // 첫번째 필드는 값이 있고 두번째 필드에 값이 비었다면
-    (password[0] && !password[1]) && inputRef2.current?.focus();
+    // 다른 모든 필드가 채워져 있다면
+    if (cardNumber.length === 16 && expiredPeriod.length === 4 && name.length > 0 && cvc.length === 3) {
+      // 첫번째 필드에 값이 비었다면
+      !password[0] && inputRef1.current?.focus();
+      // 첫번째 필드는 값이 있고 두번째 필드에 값이 비었다면
+      (password[0] && !password[1]) && inputRef2.current?.focus();
+    }
   }, [password]);
 
   return(
