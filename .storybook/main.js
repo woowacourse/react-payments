a098b9b70904ec931be6c5352024f8897611af32
airman5573 <rpf5573@kakao.com>
2022-04-29 05:31:23 +0900

    refactor(storybook): 불필요한 코드를 삭제한다

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,15 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    "../src/stories/*.story.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app"
   ],
-  "framework": "@storybook/react",
-  "core": {
+  framework: "@storybook/react",
+  core: {
     "builder": "@storybook/builder-webpack5"
-  }
+  },
 }
