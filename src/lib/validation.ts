81ffc15c6c5f186a6a47a382bd7fbaccb6987595
gytks4 <gytks4@naver.com>
2022-04-28 20:04:06 +0900

    feat: 만료일 유효성 검사
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -9,8 +9,29 @@ export type Validators = {
 const validateCardNumbers = (cardNumbers: CardNumbers) =>
   cardNumbers.every(cardNumber => cardNumber.length === 4);
 
-const validateExpiredDate = (expiredDate: ExpiredDate) =>
+const validateExpiredDateLength = (expiredDate: ExpiredDate) =>
   Object.keys(expiredDate).every(key => expiredDate[key].length === 2);
+
+const validateMonth = (expiredDate: ExpiredDate) =>
+  Number(expiredDate.month) >= 1 && Number(expiredDate.month) <= 12;
+
+const validateDate = (expiredDate: ExpiredDate) => {
+  const date = new Date();
+  const year = date.getFullYear() % 100;
+  const month = date.getMonth() + 1;
+
+  if (Number(expiredDate.year) < year) return false;
+  if (Number(expiredDate.year) > year + 5) return false;
+  if (Number(expiredDate.year) === year && Number(expiredDate.month) < month) return false;
+  if (Number(expiredDate.year) === year + 5 && Number(expiredDate.month) > month) return false;
+
+  return true;
+};
+
+const expiredDateValidators = [validateExpiredDateLength, validateMonth, validateDate];
+
+const validateExpiredDate = (expiredDate: ExpiredDate) =>
+  expiredDateValidators.every(validator => validator(expiredDate));
 
 const validateUserName = (userName: string) => userName.length > 0;
 
