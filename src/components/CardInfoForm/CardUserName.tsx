c25cdc6647f9ad8a133b2dc92a608f2a848d3f48
gytks4 <gytks4@naver.com>
2022-04-28 14:50:57 +0900

    feat: Input에 align props 추가

--- src/components/CardInfoForm/CardUserName.tsx
+++ src/components/CardInfoForm/CardUserName.tsx
@@ -18,8 +18,10 @@ export default function CardUserName({ cardUserName, onChange, onBlur }) {
         placeholder="카드에 표시된 이름과 동일하게 입력하세요."
         onChange={onChange}
         maxLength={MAX_USER_NAME_LENGTH}
+        style={{ padding: "0 10px" }}
         onBlur={onBlur}
         name="userName"
+        align="left"
       />
     </div>
   );
--- src/elements/Input.tsx
+++ src/elements/Input.tsx
@@ -8,16 +8,29 @@ const sizeTag = {
   full: "w-100",
 };
 
+const alignTag = {
+  right: "text-right",
+  left: "text-left",
+  center: "text-center",
+};
+
 interface InputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "size" | "maxLength"> {
   size?: "tiny" | "small" | "medium" | "large" | "full";
+  align?: "right" | "left" | "center";
   maxLength?: number;
 }
 
-export default function Input({ size, maxLength, onChange, ...props }: InputProps) {
+export default function Input({ size, maxLength, onChange, align, ...props }: InputProps) {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value.length > maxLength) return;
     onChange?.(e);
   };
 
-  return <input className={`input-basic ${sizeTag[size]}`} onChange={handleChange} {...props} />;
+  return (
+    <input
+      className={`input-basic ${sizeTag[size]} ${alignTag[align]}`}
+      onChange={handleChange}
+      {...props}
+    />
+  );
 }
