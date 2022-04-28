2e34e918907be3db9e068cef817fb6a75fc4e089
sangminsong <ssmin940606@gmail.com>
2022-04-28 17:36:46 +0900

    feat: App 컴포넌트 테스트
    Co-authored-by: LAH1203 <LAH1203@users.noreply.github.com>

---
+++ src/stories/app.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import App from '../App';
+
+export default {
+  title: 'Example/App',
+  component: App,
+};
+
+const Template = () => <App />;
+
+export const Default = Template.bind({});
