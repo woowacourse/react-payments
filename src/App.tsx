a2366af489dfb9d60571970fbb058a839506aa6e
airman5573 <rpf5573@kakao.com>
2022-04-28 23:31:09 +0900

    feat(ui): app에 컴포넌트들을 보여준다

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
+import React from "react";
+import { css } from "@emotion/react";
+import Card from "./components/atoms/Card";
+import CardNumberFieldset from "./components/molecules/CardNumberFieldset";
+import AppProvider from "./components/Provider";
+import CardOwnerNameFieldset from "./components/molecules/CardOwnerNameFieldset";
+import ExpiredPeriodFieldset from "./components/molecules/ExpiredPeriodFieldset";
+import CVCFieldset from "./components/molecules/CVCFieldset";
 import CardPasswordFieldset from "./components/molecules/CardPasswordFieldset";
+import Navigation from "./components/molecules/Navigation";
+import ConfirmButtonContainer from "./components/atoms/confirm-button/ConfirmButtonContainer";
+
+const style = css({
+  width: '375px',
+  margin: '0 auto',
+  padding: '22px 28px 16px 28px',
+  position: 'relative',
+});
+
+const cardWrapper = css({
+  display: 'flex',
+  justifyContent: 'center',
+})
 
 function App() {
   return (
-    <>
-      <CardPasswordFieldset />
-    </>
+    <div id="card-form" css={style}> 
+      <Navigation />
+      <AppProvider>
+        <div css={cardWrapper}>
+          <Card />
+        </div>
+        <CardNumberFieldset />
+        <ExpiredPeriodFieldset />
+        <CardOwnerNameFieldset />
+        <CVCFieldset />
+        <CardPasswordFieldset />
+        <ConfirmButtonContainer>다음</ConfirmButtonContainer>
+      </AppProvider>
+    </div>
   );
 }
 
