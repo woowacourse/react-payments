d1672a420e1db6dbd737c07b7096ba29d24d2fba
intae92 <kangit2013@gmail.com>
2022-04-28 19:39:06 +0900

    feat: app 컴포넌트에 하위 컴포넌트 합성 및 제어
    Co-authored-by: TaeYoon <uni613@naver.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,79 @@
+import React, { useState, useEffect, useRef, useLayoutEffect } from 'react';
+
 import CardNumber from './components/CardNumber';
+import DueDate from './components/DueDate';
+import CardOwner from './components/CardOwner';
+import CardSecurityCode from './components/CardSecurityCode';
+import CardPassword from './components/CardPassword';
+import CardShape from './components/CardShape';
+import Header from './components/common/Header';
+import TextButton from './components/common/TextButton';
+import Footer from './components/common/Footer';
 
 function App() {
+  const targetRef = useRef();
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  const [cardNumber, setCardNumber] = useState('');
+  const [cardOwnerName, setCardOwnerName] = useState('NAME');
+  const [cardDate, setCardDate] = useState('');
+
+  const [isCorrectCardDate, setIsCorrectCardDate] = useState(false);
+  const [isCorrectOwnerName, setIsCorrectOwnerName] = useState(false);
+  const [isCorrectSecurityCode, setIsCorrectSecurityCode] = useState(false);
+  const [isCorrectCardPwd, setIsCorrectCardPwd] = useState(false);
+
+  const [isAllCompleted, setIsAllCompleted] = useState(false);
+
+  const cardNumberCallback = numbers => setCardNumber(numbers);
+  const cardDateCallback = date => setCardDate(date);
+  const ownerNameCallback = ownerName => setCardOwnerName(ownerName);
+
+  const correctOwnerNameCallback = isCorrect => setIsCorrectOwnerName(isCorrect);
+  const correctSecurityCodeCallback = isCorrect => setIsCorrectSecurityCode(isCorrect);
+  const correctCardPwdCallback = isCorrect => setIsCorrectCardPwd(isCorrect);
+
+  useEffect(() => {
+    const isOkCardNumber = cardNumber !== '';
+
+    const allOk = [
+      isOkCardNumber,
+      isCorrectCardDate,
+      isCorrectOwnerName,
+      isCorrectSecurityCode,
+      isCorrectCardPwd,
+    ].every(ok => ok);
+
+    setIsAllCompleted(allOk);
+  }, [cardNumber, isCorrectCardDate, isCorrectOwnerName, isCorrectSecurityCode, isCorrectCardPwd]);
+
+  useLayoutEffect(() => {
+    if (targetRef.current) {
+      setDimensions({
+        width: targetRef.current.offsetWidth,
+        height: targetRef.current.offsetHeight,
+      });
+    }
+  }, []);
+
   return (
-    <div className="app">
-      <CardNumber />
+    <div className="app" ref={targetRef}>
+      <Header>{'카드추가'}</Header>
+      <CardShape cardNumber={cardNumber} cardOwnerName={cardOwnerName} cardDate={cardDate} />
+      <CardNumber cardNumberCallback={cardNumberCallback} />
+      <DueDate
+        dimensions={dimensions}
+        cardDateCallback={cardDateCallback}
+        setIsCorrectCardDate={setIsCorrectCardDate}
+      />
+      <CardOwner ownerNameCallback={ownerNameCallback} correctOwnerNameCallback={correctOwnerNameCallback} />
+      <CardSecurityCode correctSecurityCodeCallback={correctSecurityCodeCallback} />
+      <CardPassword correctCardPwdCallback={correctCardPwdCallback} />
+      <Footer>
+        <TextButton hexColor="#525252" isVisible={isAllCompleted}>
+          다음
+        </TextButton>
+      </Footer>
     </div>
   );
 }
