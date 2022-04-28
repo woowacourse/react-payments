353884645b9412ce0d169732841f3f087a8c57d4
최광희 <polyckh@gmail.com>
2022-04-28 18:23:29 +0900

    feat: Common 컴포넌트 스토리 작성 완료
    Co-authored-by: YongRae_Kim <kyr9389@naver.com>

--- src/stories/Card.stories.js
+++ src/stories/Card.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { Card } from '../components/common/Card';
 
 export default {
@@ -9,4 +10,17 @@ export default {
 const Template = (args) => <Card {...args} />;
 
 export const CardTemplate = Template.bind({});
-CardTemplate.args = {};
+CardTemplate.args = {
+  cardType: { name: '유세지', color: 'skyblue' },
+  cardNumbers: {
+    firstNumber: '5272',
+    secondNumber: '4163',
+    thirdNumber: '5809',
+    fourthNumber: '6092',
+  },
+  expireDate: {
+    month: '08',
+    year: '24',
+  },
+  ownerName: '유세지',
+};
---
+++ src/stories/Modal.stories.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { Modal } from '../components/common/Modal';
+import { CardSelectModal } from '../components/CardRegister/CardSelectModal';
+import { CVCHelperModal } from '../components/CardRegister/CVCHelperModal';
+
+const CARD_TYPES = [
+  { name: '포코', color: 'gold' },
+  { name: '준', color: '#04c09e' },
+  { name: '공원', color: 'green' },
+  { name: '후이', color: '#9198e5' },
+  { name: '유세지', color: '#AB46D2' },
+  { name: '마르코', color: '#E76E9A' },
+  { name: '아놀드', color: '#FF5F00' },
+  { name: '록바', color: '#FBCD58' },
+];
+
+export default {
+  title: 'Example/Modal',
+  component: Modal,
+};
+
+const Template = (args) => <Modal {...args} />;
+
+export const CardSelectModalTemplate = Template.bind({});
+CardSelectModalTemplate.args = {
+  children: <CardSelectModal cardTypes={CARD_TYPES} />,
+  visible: true,
+};
+
+export const CVCHelperModalTemplate = Template.bind({});
+CVCHelperModalTemplate.args = {
+  children: <CVCHelperModal />,
+  visible: true,
+};
