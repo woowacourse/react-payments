2e0461061501d894cbb74579d1bcde72b64203e7
airman5573 <rpf5573@kakao.com>
2022-04-28 23:28:13 +0900

    feat(component): navigation을 추가한다

---
+++ src/components/atoms/PageTitle.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PageTitle ({ children }: {children: React.ReactNode }) {
+
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default PageTitle;
