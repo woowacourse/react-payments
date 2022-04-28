c6d84c7c75a99fcef20f63d8f672193dd6b7a1cf
Daye Lee <508yeah@gmail.com>
2022-04-29 03:47:52 +0900

    refactor: 카드 상태 관리 Hook 사용 방식 개선

--- src/hooks/useCardState.js
+++ src/hooks/useCardState.js
@@ -13,29 +13,20 @@ const cardNumberFormatter = (cardNumber) => cardNumber.replace(/[^0-9]/g, '');
 
 function reducer(state, { type, contents }) {
   const newState = { ...state };
-  const updateState = {
-    cardNumber: () => {
+
+  switch (type) {
+    case 'cardNumber': {
       const { index, value } = contents;
       newState.cardNumber[index] = cardNumberFormatter(value);
-    },
-    expireMonth: () => {
-      newState.expireMonth = contents;
-    },
-    expireYear: () => {
-      newState.expireYear = contents;
-    },
-    userName: () => {
+      break;
+    }
+    case 'userName': {
       newState.userName = contents.toUpperCase();
-    },
-    securityCode: () => {
-      newState.securityCode = contents;
-    },
-    cardPassword: () => {
-      newState.cardPassword = contents;
-    },
-  };
-
-  updateState[type]();
+      break;
+    }
+    default:
+      newState[type] = contents;
+  }
 
   return newState;
 }
