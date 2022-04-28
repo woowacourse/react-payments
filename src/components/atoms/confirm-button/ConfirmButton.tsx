186bdf5b6dfe6424a91e2db05e9e96129754ce2c
airman5573 <rpf5573@kakao.com>
2022-04-28 23:25:22 +0900

    feat(component): confirm 버튼을 추가한다

---
+++ src/components/atoms/confirm-button/ConfirmButton.tsx
@@ -0,0 +1,29 @@
+import { css } from "@emotion/react";
+import React from "react";
+
+const style = css({
+  position: 'absolute',
+  right: '25px',
+  bottom: '16px',
+  backgroundColor: 'inherit',
+  border: 'none',
+  fontSize: '16px',
+  fontWeight: '500',
+  cursor: 'pointer',
+});
+
+type Props = {
+  children: React.ReactNode,
+  disabled?: boolean,
+  onClick: () => void
+}
+
+function ConfirmButton({ children, onClick, disabled }: Props) {
+  return (
+    <button css={style} onClick={onClick} {...(disabled ? { disabled: true } : {})}>
+      { children }
+    </button>
+  )
+}
+
+export default ConfirmButton;
