a198c5713158d3e1476c8cd88b908cb64213f9d1
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-04-29 02:36:11 +0900

    refactor: validator 함수 분리
    Co-authored-by: lokba <lokba@users.noreply.github.com>

--- src/page/cardAdd/validator.js
+++ src/page/cardAdd/validator.js
@@ -1,42 +1,40 @@
 import { isEnglish, isNumber } from '../../utils';
 import { INPUT_MAX_LENGTH, DATE_RANGE } from '../../constants';
 
+const validateYear = (value) => {
+  if (value.length === 1) {
+    return Number(value) >= 2;
+  }
+
+  if (value.length === INPUT_MAX_LENGTH.EXPIRY_DATE) {
+    return Number(value) >= DATE_RANGE.MIN_YEAR;
+  }
+
+  return true;
+};
+
+const validateMonth = (value) => {
+  if (value.length === 1) {
+    return Number(value) <= 1;
+  }
+
+  if (value.length === INPUT_MAX_LENGTH.EXPIRY_DATE) {
+    return Number(value) >= DATE_RANGE.MIN_MONTH && Number(value) <= DATE_RANGE.MAX_MONTH;
+  }
+
+  return true;
+};
+
 export const validator = {
   number(value) {
     return isNumber(Number(value));
   },
   expiryDate(value, name) {
-    if (name === 'year') {
-      if (!isNumber(Number(value))) {
-        return false;
-      }
-
-      if (value.length === 1) {
-        return Number(value) >= 2;
-      }
-
-      if (value.length === INPUT_MAX_LENGTH.EXPIRY_DATE) {
-        return Number(value) >= DATE_RANGE.MIN_YEAR;
-      }
-
-      return true;
+    if (!isNumber(Number(value))) {
+      return false;
     }
 
-    if (name === 'month') {
-      if (!isNumber(Number(value))) {
-        return false;
-      }
-
-      if (value.length === 1) {
-        return Number(value) <= 1;
-      }
-
-      if (value.length === INPUT_MAX_LENGTH.EXPIRY_DATE) {
-        return Number(value) >= DATE_RANGE.MIN_MONTH && Number(value) <= DATE_RANGE.MAX_MONTH;
-      }
-
-      return true;
-    }
+    return name === 'year' ? validateYear(value) : validateMonth(value);
   },
   ownerName(value) {
     return isEnglish(value);
@@ -50,27 +48,18 @@ export const validator = {
 };
 
 // 카드 폼의 각 정보들이 모두 입력되었는지 검사
-const isFullNumber = (number) => {
-  return Object.values(number).every((number) => number.length === INPUT_MAX_LENGTH.NUMBER);
-};
+const isFullNumber = (number) =>
+  Object.values(number).every((value) => value.length === INPUT_MAX_LENGTH.NUMBER);
 
-const isFullCompany = (company) => {
-  return company !== '';
-};
+const isFullCompany = (company) => company !== '';
 
-const isFullExpiryDate = (expiryDate) => {
-  return Object.values(expiryDate).every(
-    (number) => number.length === INPUT_MAX_LENGTH.EXPIRY_DATE,
-  );
-};
+const isFullExpiryDate = (expiryDate) =>
+  Object.values(expiryDate).every((value) => value.length === INPUT_MAX_LENGTH.EXPIRY_DATE);
 
-const isFullPrivacyCode = (privacyCode) => {
-  return privacyCode.length === INPUT_MAX_LENGTH.PRIVACY_CODE;
-};
+const isFullPrivacyCode = (privacyCode) => privacyCode.length === INPUT_MAX_LENGTH.PRIVACY_CODE;
 
-const isFullPassword = (password) => {
-  return Object.values(password).every((number) => number.length === INPUT_MAX_LENGTH.PASSWORD);
-};
+const isFullPassword = (password) =>
+  Object.values(password).every((value) => value.length === INPUT_MAX_LENGTH.PASSWORD);
 
 export const checkFullFilled = (cardInfo) => {
   const { number, company, expiryDate, privacyCode, password } = cardInfo;
