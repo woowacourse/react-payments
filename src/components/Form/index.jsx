c451552a3f0ce690b427efb4d205c8bd6300bd28
prefer2 <sunho6200@gmail.com>
2022-04-29 22:11:26 +0900

    feat: Form Component 추가

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+import InputBox from "../InputBox";
+import { ReactComponent as QuestionMark } from '../../assets/questionMark.svg';
+
+import { Label, FormWrapper, HelpTextWrapper, InputHelperWrapper } from "./style";
+
+function Form(props){
+  const { label, inputInfo, size, onChange } = props;
+
+  const [mouseHover, setMouseHover] = useState(false);
+
+  const handleMouseHover = () => {
+    setMouseHover((prev) => !prev);
+  }
+
+  return (
+    <FormWrapper>
+      <Label>
+        <label>{label}</label>
+        {props.countHelper && <div>{inputInfo[0].value?.length}/{props.countHelper}</div>}
+      </Label>
+      <InputHelperWrapper>
+        <InputBox inputInfo={inputInfo} size={size} onChange={onChange}/>
+        {props.questionHelper && <QuestionMark onMouseEnter={handleMouseHover} onMouseLeave={handleMouseHover}/>}
+        {mouseHover && <HelpTextWrapper>{props.helpText}</HelpTextWrapper>}
+      </InputHelperWrapper>
+    </FormWrapper>
+  )
+}
+
+export default Form;
---
+++ src/components/Form/style.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+const Label = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  & div {
+    font-size: 12px;
+    margin-bottom: 10px;
+    display: inline-block;
+    color: ${({ theme }) => theme.colors.DARK_GRAY};
+  }
+`;
+
+const HelpTextWrapper = styled.div`
+  font-size: 12px;
+`;
+
+const FormWrapper = styled.div`
+  margin-bottom: 15px;
+
+  & label {
+    font-size: 12px;
+    margin-bottom: 10px;
+    display: inline-block;
+    color: ${({ theme }) => theme.colors.DARK_GRAY};
+  }
+`;
+
+const InputHelperWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`;
+
+export { Label, HelpTextWrapper, FormWrapper, InputHelperWrapper };
