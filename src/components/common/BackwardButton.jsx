fa9ec38dc9419276b1c5e136a79d71b425ab863d
jhy979 <jhy979@gmail.com>
2022-05-01 20:50:07 +0900

    refactor: backward arrow svg 파일 분리

--- src/components/common/BackwardButton.jsx
+++ src/components/common/BackwardButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { ReactComponent as BackwardArrow } from '../../assets/backwardArrow.svg';
 
 const StyledButton = styled.button`
   background: none;
@@ -25,21 +26,7 @@ const StyledButton = styled.button`
 const BackwardButton = ({ children, ...rest }) => {
   return (
     <StyledButton type="button" {...rest}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth={1}
-        width="24"
-        height="24"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M15 19l-7-7 7-7"
-        />
-      </svg>
+      <BackwardArrow />
       <p>{children}</p>
     </StyledButton>
   );
