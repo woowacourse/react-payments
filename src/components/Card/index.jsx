cae042e72d535aa245066799b763fb297507fc4b
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-01 17:51:56 +0900

    refactor: 카드 컴포넌트 연산자 가독성 개선

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -16,7 +16,7 @@ const cardNumberFormatter = (cardNumber) => {
 function Card({ companyName, cardNumber, userName, expireMonth, expireYear, isComplete }) {
   return (
     <div className="card-box">
-      <div className={`small-card${(!isComplete && ' empty') || ''}`}>
+      <div className={`small-card${isComplete ? '' : ' empty'}`}>
         <div className="card-top">
           <span className="card-text">{companyName}</span>
         </div>
