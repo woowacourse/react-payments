f2efab2dc826b8627e4a36e1f18bfa5379c5b8f4
jswith <typingjs@gmail.com>
2022-05-02 23:39:19 +0900

    :recycle: refactor: 절대경로로 수정
    Co-authored-by: rladpwl0512 <rladpwl0512@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style/index.css';
-import App from './App';
+import 'style/index.css';
+import App from 'App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
