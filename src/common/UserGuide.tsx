67bbbd32f648594d5c59df65307d1e642b1a1ff5
cks3066 <cks3066@naver.com>
2022-05-02 15:36:26 +0900

    refactor: onMouse 이벤트 css hover 로 변경

--- src/common/UserGuide.tsx
+++ src/common/UserGuide.tsx
@@ -1,18 +1,10 @@
 import React, { useState } from "react";
 
 export default function UserGuide({ children }: { children: React.ReactNode }) {
-  const [isContentShown, setIsContentShown] = useState(false);
-
   return (
     <div className="user-guide-container">
-      <span
-        className="user-guide"
-        onMouseEnter={() => setIsContentShown(true)}
-        onMouseLeave={() => setIsContentShown(false)}
-      >
-        ?
-      </span>
-      {isContentShown && <div className="user-guide-content">{children}</div>}
+      <span className="user-guide">?</span>
+      {<div className="user-guide-content">{children}</div>}
     </div>
   );
 }
