be3379ed18c2317ad1cee5ac44f22155fda9a8c0
kimsanglok <evertree6031@gmail.com>
2022-05-02 21:12:03 +0900

    refactor: Modal 컴포넌트 - Fragment -> div로 래핑하기

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types';
 
 const Modal = ({ handleModal, children }) => {
   return (
-    <>
+    <div>
       <div className="modal-dimmed" onClick={handleModal}></div>
       <div className="modal">{children}</div>
-    </>
+    </div>
   );
 };
 
