1d1947780bc104ce30372656e591d15028715d03
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-03 01:36:48 +0900

    style: 버튼 컴포넌트 스타일 수정

--- src/components/@common/Button/styles.js
+++ src/components/@common/Button/styles.js
@@ -3,11 +3,13 @@ import styled from '@emotion/styled';
 import { BRAND_COLORS, COLORS, LAYOUT } from 'styles/theme';
 
 const Container = styled.button`
+  cursor: pointer;
   border: none;
   border-radius: ${LAYOUT.BORDER_RADIUS};
   padding: 0.714rem 1.429rem;
   background-color: ${BRAND_COLORS.accent};
   color: ${BRAND_COLORS.accentFont};
+  transition: background-color 0.3s ease;
 
   &:hover {
     filter: brightness(1.2);
