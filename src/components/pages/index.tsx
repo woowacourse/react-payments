902f7dd2a73106ac350c6d66b59daff6520ddf7f
gytks4 <gytks4@naver.com>
2022-05-03 21:33:11 +0900

    feat: 카드 추가 UI 구현

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -10,8 +10,15 @@ export default function Home() {
     <>
       <Header title="보유 카드" hasBackArrow={false} />
       {cards.map(cardInfo => (
-        <Card key={cardInfo.cardType.name + cardInfo.cardType.color} cardInfo={cardInfo} />
+        <div
+          className="mb-30 flex-column-center"
+          key={cardInfo.cardType.name + cardInfo.cardType.color}
+        >
+          <Card cardInfo={cardInfo} marginBottom="10px" />
+          <span className="card-nickname">{cardInfo.cardName}</span>
+        </div>
       ))}
+      <div className="small-card empty-card">+</div>
     </>
   );
 }
