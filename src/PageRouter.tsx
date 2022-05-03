ded936c3ec497dc66b1dc2b50aa2a3689f10c158
gytks4 <gytks4@naver.com>
2022-05-03 19:41:21 +0900

    feat: Home에서 Cards 렌더링

--- src/PageRouter.tsx
+++ src/PageRouter.tsx
@@ -1,6 +1,7 @@
 import Home from "components/pages";
 import Add from "components/pages/add";
 import Complete from "components/pages/complete";
+import { PATH } from "constant/path";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -8,9 +9,9 @@ export default function PageRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/add" element={<Add />} />
-        <Route path="/complete" element={<Complete />} />
+        <Route path={PATH.HOME} element={<Home />} />
+        <Route path={PATH.ADD} element={<Add />} />
+        <Route path={PATH.COMPLETE} element={<Complete />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import Card from "components/Card";
+import Header from "components/Header";
+import { CardsContext } from "contexts/CardsProvider";
+import React, { useContext } from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  const { cards } = useContext(CardsContext);
+
+  return (
+    <>
+      <Header title="보유 카드" hasBackArrow={false} />
+      {cards.map(cardInfo => (
+        <Card key={cardInfo.cardType.name + cardInfo.cardType.color} cardInfo={cardInfo} />
+      ))}
+    </>
+  );
 }
