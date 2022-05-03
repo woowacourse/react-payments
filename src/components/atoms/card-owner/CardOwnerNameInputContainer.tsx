d9a2980b29ad5597ddd9e466083e1285ba8ecd96
airman5573 <rpf5573@kakao.com>
2022-05-03 15:16:20 +0900

    refactor(logic): 이름 입력시 처리하는 로직을 단순화한다
    정규식을 사용하니 처리가 단순해졌다

--- src/components/atoms/card-owner/CardOwnerNameInputContainer.tsx
+++ src/components/atoms/card-owner/CardOwnerNameInputContainer.tsx
@@ -10,49 +10,44 @@ function CardOwnerNameInputContainer() {
   const { name } = useAppState();
   const dispatch = useAppDispatch();
 
-  const handleChage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const input = event.target;
+  const handleInsert = (input: HTMLInputElement) => {
     const { value, selectionStart } = input;
+    if (!selectionStart) return;
+    const insertedChar = value[selectionStart - 1];
 
-    if (selectionStart === null) return;
+    // value의 총 길이는 30을 넘으면 안 된다
+    if (value.length > MAX_NAME_LENGTH) return false;
+    // 영어와 스페이스바만 입력 가능하다
+    if (!isEnglish(insertedChar) && insertedChar !== ' ') return false;
 
-    // 입력하는 경우
-    if (name.length < value.length) {
-      // value의 총 길이는 30을 넘으면 안 된다
-      if (value.length > MAX_NAME_LENGTH) return;
+    const newVal = value.replace(/ {2,}/g, ' ').toUpperCase();
 
-      const insertedChar = value[selectionStart - 1];
-      // space인데
-      if (insertedChar === ' ') {
-        // 처음부터 space는 허용 안되지요
-        if (value.length === 1) return;
+    queueMicrotask(() => {
+      input.setSelectionRange(selectionStart, selectionStart);
+    });
+    dispatch(createAction(ActionType.INPUT_NAME, newVal));
+    return;
+  };
 
-        // 왼쪽에 스페이스바가 있으면 그것도 안되지요
-        if (value[selectionStart - 2] === ' ') return;
+  const handleDelete = (input: HTMLInputElement) => {
+    const { value, selectionStart } = input;
+    if (!selectionStart) return;
 
-        // 오른쪽에 스페이스바가 있으면 그것도 안되지요
-        if (value[selectionStart] === ' ') return;
-      }
-
-      // 영어하고 space만 입력 가능하다
-      if (!isEnglish(insertedChar) && insertedChar !== ' ') return;
-
-      // 대문자로 변환을 해준다
-      const upperName = value.toUpperCase();
-
-      queueMicrotask(() => {
-        input.setSelectionRange(selectionStart, selectionStart);
-      });
-      // name을 업데이트 한다
-      dispatch(createAction(ActionType.INPUT_NAME, upperName));
-      return;
-    }
-
-    // 지우는 경우
     queueMicrotask(() => {
       input.setSelectionRange(selectionStart, selectionStart);
     });
     dispatch(createAction(ActionType.INPUT_NAME, value.toUpperCase()));
+  };
+
+  const handleChage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const input = event.target;
+    const { value, selectionStart } = input;
+    if (selectionStart === null) return;
+
+    if (name.length < value.length) {
+      return handleInsert(input);
+    }
+    handleDelete(input);
   };
 
   return <CardOwnerNameInput onChange={handleChage} value={name} />;
