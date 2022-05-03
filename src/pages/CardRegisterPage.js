5bf2b7a9c4a5f1926e4661e39d7858a75fe29d4f
최광희 <polyckh@gmail.com>
2022-05-03 14:19:07 +0900

    refactor: CARD_TYPE 상수 파일로 이동

--- src/pages/CardRegisterPage.js
+++ src/pages/CardRegisterPage.js
@@ -19,17 +19,6 @@ import { ModalSelector } from '../components/common/ModalSelector';
 import { useModalSelector } from '../hooks/useModalSelector';
 import { MarginTB10 } from '../components/common/styled';
 
-const CARD_TYPES = [
-  { name: '포코', color: 'gold' },
-  { name: '준', color: '#04c09e' },
-  { name: '공원', color: 'green' },
-  { name: '후이', color: '#9198e5' },
-  { name: '유세지', color: '#AB46D2' },
-  { name: '마르코', color: '#E76E9A' },
-  { name: '아놀드', color: '#FF5F00' },
-  { name: '록바', color: '#FBCD58' },
-];
-
 export const CardRegisterPage = () => {
   const [cardInfo, dispatch] = useReducer(cardInfoReducer, initialCardInfo);
 
