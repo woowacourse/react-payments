004868f79a558dff433cac1a74519814da085c14
gytks4 <gytks4@naver.com>
2022-05-03 15:27:34 +0900

    feat: react-router-dom 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,10 @@
-import Card from "components/Card";
-import CardInfoForm from "components/CardInfoForm";
-import Header from "components/Header";
-import useCardInfoInput from "hooks/useCardInfoInput";
-import React, { useState } from "react";
-import { CardInfo } from "types/cardInfo";
+import PageRouter from "PageRouter";
+import React from "react";
 
-function App() {
-  const [cards, setCards] = useState<CardInfo[]>([]);
-  const {
-    cardInfo,
-    cardInfoValidation,
-    onChangeCardType,
-    resetCardInfo,
-    onChangeCardNumber,
-    onChangeExpirationDate,
-    onChangeUserName,
-    onBlurUserName,
-    onChangeSecurityCode,
-    onChangePassword,
-  } = useCardInfoInput();
-
-  const addCard = () => {
-    setCards(prevCards => [...prevCards, cardInfo]);
-  };
-
+export default function App() {
   return (
-    <div className="App">
-      <Header title="카드 추가" />
-      <Card cardInfo={cardInfo} onChangeCardType={onChangeCardType} />
-      <CardInfoForm
-        cardInfo={cardInfo}
-        onChangeCardNumber={onChangeCardNumber}
-        onChangeExpirationDate={onChangeExpirationDate}
-        onChangeUserName={onChangeUserName}
-        onBlurUserName={onBlurUserName}
-        onChangeSecurityCode={onChangeSecurityCode}
-        onChangePassword={onChangePassword}
-        resetCardInfo={resetCardInfo}
-        cardInfoValidation={cardInfoValidation}
-        addCard={addCard}
-      />
-    </div>
+    <>
+      <PageRouter />
+    </>
   );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import "styles/index.css";
 
+import App from "App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
