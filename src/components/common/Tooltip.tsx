b36b8e9437750e764c8b95b90acea7b7870f1f61
gytks4 <gytks4@naver.com>
2022-05-03 14:49:06 +0900

    refactor: tooltip을 css로 통제

--- src/components/common/Tooltip.tsx
+++ src/components/common/Tooltip.tsx
@@ -1,18 +1,10 @@
 import React, { useState } from "react";
 
 export default function Tooltip({ children }: { children: React.ReactNode }) {
-  const [isContentShown, setIsContentShown] = useState(false);
-
   return (
     <div className="tooltip-container">
-      <span
-        className="tooltip"
-        onMouseEnter={() => setIsContentShown(true)}
-        onMouseLeave={() => setIsContentShown(false)}
-      >
-        ?
-      </span>
-      {isContentShown && <div className="tooltip-content">{children}</div>}
+      <span className="tooltip">?</span>
+      <div className="tooltip-content">{children}</div>
     </div>
   );
 }
