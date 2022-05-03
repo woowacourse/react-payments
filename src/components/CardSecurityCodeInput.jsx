b18968aba92a906357616b0821088feedc777dc4
Soyi Jeon <soyi.dev@gmail.com>
2022-05-03 14:24:00 +0900

    refactor: DescriptionIcon 컴포넌트 분리

--- src/components/CardSecurityCodeInput.jsx
+++ src/components/CardSecurityCodeInput.jsx
@@ -1,53 +1,12 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React from 'react';
 
 import Input from './common/Input.jsx';
 import InputField from './common/InputField.jsx';
 
 import { CARD_INFO_RULES, CREATE_MASKED_CHARACTERS } from '../constants';
 
-import HelpIconImage from '../assets/images/questionMark.svg';
-
-const StyledIconContainer = styled.div`
-  position: relative;
-`;
-
-const StyledIcon = styled.img`
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const StyledDescription = styled.p`
-  ${props => !props.isOpen && `display: none;`}
-  position: absolute;
-  top: 5px;
-  right: -205px;
-
-  max-width: 200px;
-  width: max-content;
-
-  border: 1px solid #d2d2d2;
-  border-radius: 5px;
-  padding: 10px;
-  z-index: 1;
-  font-size: 12px;
-
-  background: #ffffff;
-  -webkit-box-shadow: 1px 2px 5px 0px rgba(0, 0, 0, 0.4);
-  box-shadow: 1px 2px 5px 0px rgba(0, 0, 0, 0.4);
-`;
-
-function HelpIcon({ description }) {
-  const [isDescriptionOpen, setIsDescriptionOpen] = useState(false);
-
-  return (
-    <StyledIconContainer>
-      <StyledIcon src={HelpIconImage} onClick={() => setIsDescriptionOpen(!isDescriptionOpen)} />
-      <StyledDescription isOpen={isDescriptionOpen}>{description}</StyledDescription>
-    </StyledIconContainer>
-  );
-}
+import QuestionMark from '../assets/images/questionMark.svg';
+import DescriptionIcon from './common/DescriptionIcon.jsx';
 
 const SECURITY_CODE_DESCRIPTION = 'CVV/CVC 번호는 카드 뒷 면에 있는 3자리 숫자이며 카드 보안을 위한 번호입니다.';
 
@@ -55,7 +14,7 @@ export default function CardSecurityCodeInput({ securityCode, onChange }) {
   return (
     <InputField
       labelText="보안 코드(CVC/CVV)"
-      OptionalComponent={<HelpIcon description={SECURITY_CODE_DESCRIPTION} />}
+      OptionalComponent={<DescriptionIcon iconImage={QuestionMark} description={SECURITY_CODE_DESCRIPTION} />}
       wrapperWidth="85px"
       isComplete={securityCode.length === CARD_INFO_RULES.SECURITY_CODE_LENGTH}>
       <Input
---
+++ src/components/common/DescriptionIcon.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const StyledIconContainer = styled.div`
+  position: relative;
+`;
+
+const StyledIcon = styled.img`
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const StyledDescription = styled.p`
+  ${props => !props.isOpen && `display: none;`}
+  position: absolute;
+  top: 5px;
+  right: -205px;
+
+  max-width: 200px;
+  width: max-content;
+
+  border: 1px solid #d2d2d2;
+  border-radius: 5px;
+  padding: 10px;
+  z-index: 1;
+  font-size: 12px;
+
+  background: #ffffff;
+  -webkit-box-shadow: 1px 2px 5px 0px rgba(0, 0, 0, 0.4);
+  box-shadow: 1px 2px 5px 0px rgba(0, 0, 0, 0.4);
+`;
+
+export default function DescriptionIcon({ iconImage, description }) {
+  const [isDescriptionOpen, setIsDescriptionOpen] = useState(false);
+
+  return (
+    <StyledIconContainer>
+      <StyledIcon src={iconImage} onClick={() => setIsDescriptionOpen(!isDescriptionOpen)} />
+      <StyledDescription isOpen={isDescriptionOpen}>{description}</StyledDescription>
+    </StyledIconContainer>
+  );
+}
