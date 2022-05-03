543e705192469fc86c50367715fb70105dde0d07
gytks4 <gytks4@naver.com>
2022-05-04 00:13:21 +0900

    feat: back arrow에 goBack navigate 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 interface HeaderProps {
   hasBackArrow?: boolean;
@@ -6,9 +7,15 @@ interface HeaderProps {
 }
 
 export default function Header({ hasBackArrow = true, title }: HeaderProps) {
+  const navigate = useNavigate();
+
   return (
     <header className="page-header">
-      {hasBackArrow && <span className="arrow"></span>}
+      {hasBackArrow && (
+        <div className="arrow-wrapper">
+          <span className="arrow" onClick={() => navigate(-1)}></span>
+        </div>
+      )}
       <h2 className="page-title">{title}</h2>
     </header>
   );
