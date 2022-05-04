eb1fb671b2b98f4407657339c8d2e56736fefdd2
JASUN LEE <liswktjs@gmail.com>
2022-05-04 15:59:17 +0900

    feat: 카드 추가 버튼 컴포넌트 작성

---
+++ src/components/Card/plusCard.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PlusCard from './PlusCard';
+
+export default {
+  title: 'PlusCard',
+  component: PlusCard,
+};
+
+const Template = () => <PlusCard />;
+
+export const Example = Template.bind({});
