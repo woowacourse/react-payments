f8708db871fc0b699fffb48ed42b1ce25073df35
(yunjin) <sion11290805@gmail.com>
2022-05-05 00:27:58 +0900

    refactor: app 스타일 -> AddCardPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,19 @@
 import React from 'react';
-import { css } from '@emotion/react';
-import CardNumberFieldset from './components/molecules/CardNumberFieldset';
-import AppProvider from './context/Provider';
-import CardOwnerNameFieldset from './components/molecules/CardOwnerNameFieldset';
-import ExpiredPeriodFieldset from './components/molecules/ExpiredPeriodFieldset';
-import CVCFieldset from './components/molecules/CVCFieldset';
-import CardPasswordFieldset from './components/molecules/CardPasswordFieldset';
-import Navigation from './components/molecules/Navigation';
-import ConfirmButtonContainer from './components/atoms/confirm-button/ConfirmButtonContainer';
-import CardContainer from './components/atoms/card/CardContainer';
-import CardForm from './components/molecules/CardForm';
-
-const style = css({
-  width: '375px',
-  margin: '0 auto',
-  padding: '22px 28px 16px 28px',
-  position: 'relative',
-  backgroundColor: '#ffffff',
-});
-
-const cardWrapper = css({
-  display: 'flex',
-  justifyContent: 'center',
-});
+import CardContainer from 'containers/card/CardContainer';
+import CardFormContainer from 'containers/card/CardFormContainer';
+import AppProvider from 'context/Provider';
+import Navigation from 'fields/Navigation';
+import AddCardPage from 'pages/AddCardPage';
 
 function App() {
   return (
-    <div id="card-form" css={style}>
-      <Navigation />
-      <AppProvider>
-        <div css={cardWrapper}>
-          <CardContainer />
-        </div>
-        <CardForm>
-          <CardNumberFieldset />
-          <ExpiredPeriodFieldset />
-          <CardOwnerNameFieldset />
-          <CVCFieldset />
-          <CardPasswordFieldset />
-          <ConfirmButtonContainer>다음</ConfirmButtonContainer>
-        </CardForm>
-      </AppProvider>
-    </div>
+    <AppProvider>
+      <AddCardPage>
+        <Navigation />
+        <CardContainer />
+        <CardFormContainer />
+      </AddCardPage>
+    </AppProvider>
   );
 }
 
