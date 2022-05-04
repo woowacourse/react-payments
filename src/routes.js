5a7b5acbf1076d5fee9ef26b2351477e2d197a6e
kimsanglok <evertree6031@gmail.com>
2022-05-04 20:53:33 +0900

    feat: 라우팅 path 변경

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,9 @@ import CardAddConfirmPage from 'page/cardAddConfirm';
 import CardListPage from 'page/cardList';
 
 const routes = [
-  { path: 'add', element: <CardAddPage /> },
-  { path: 'confirm', element: <CardAddConfirmPage /> },
-  { path: '', element: <CardListPage /> },
+  { path: '/add', element: <CardAddPage /> },
+  { path: '/confirm', element: <CardAddConfirmPage /> },
+  { path: '/', element: <CardListPage /> },
 ];
 
 export default routes;
