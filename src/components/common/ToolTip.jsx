ac1cbc443eff98416a836f88ae3799aa4610685c
Jeongmin Woo <greenblues1190@gmail.com>
2022-05-04 17:31:22 +0900

    refactor: ToolTip 호버 이벤트 css로 수정

--- src/components/common/ToolTip.jsx
+++ src/components/common/ToolTip.jsx
@@ -1,54 +1,48 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 const StyledToolTip = styled.div`
   position: relative;
+  border: 1px solid #bababa;
+  border-radius: 50%;
+  color: #969696;
+  width: 27px;
+  height: 27px;
 
-  .icon {
-    border: 1px solid #bababa;
-    border-radius: 50%;
-    color: #969696;
-    width: 27px;
-    height: 27px;
-
-    text-align: center;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 23px;
-    cursor: pointer;
-  }
+  text-align: center;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 23px;
+  cursor: pointer;
 
   .tip {
+    visibility: hidden;
     position: absolute;
     width: 300px;
     top: 50%;
     left: 50%;
     left: 32px;
 
+    text-align: justify;
+    font-size: 14px;
     background-color: white;
     border-radius: 4px;
     box-shadow: 0px 0px 5.3px rgba(0, 0, 0, 0.028),
       0px 0px 17.9px rgba(0, 0, 0, 0.042), 0px 0px 80px rgba(0, 0, 0, 0.07);
     padding: 4px;
+    z-index: 1;
+  }
+
+  &:hover > .tip {
+    visibility: visible;
   }
 `;
 
 const ToolTip = ({ children, tip }) => {
-  const [show, setShow] = useState(false);
   return (
     <StyledToolTip>
-      <div
-        className="icon"
-        onMouseEnter={() => {
-          setShow(true);
-        }}
-        onMouseLeave={() => {
-          setShow(false);
-        }}
-      >
-        {children}
-      </div>
-      {show && <p className="tip">{tip}</p>}
+      {children}
+      <p className="tip">{tip}</p>
     </StyledToolTip>
   );
 };
