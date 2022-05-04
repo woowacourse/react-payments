f730d6de92893e3a06a5141b13acfd19c8bdb63a
JASUN LEE <liswktjs@gmail.com>
2022-05-04 15:09:48 +0900

    feat: 카드 닉네임 인풋 컴포넌트 작성

---
+++ src/components/Input/CardNickNameInput.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CardNickNameInputWrapper = styled.input`
+  width: 250px;
+  border-style: none;
+  border-bottom: 1px solid #111;
+  text-align: center;
+
+  font-size: 18px;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+`;
+
+const CardNickNameInput = () => {
+  return <CardNickNameInputWrapper />;
+};
+
+export default CardNickNameInput;
---
+++ src/components/Input/cardNickNameInput.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CardNickNameInput from './CardNickNameInput';
+
+export default {
+  title: 'CardNickNameInput',
+  component: CardNickNameInput,
+};
+
+const Template = (args) => <CardNickNameInput />;
+
+export const Example = Template.bind({});
