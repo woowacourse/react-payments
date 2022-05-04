43d8eff503c5716a334c5c8198108304c719be43
JASUN LEE <liswktjs@gmail.com>
2022-05-04 15:35:28 +0900

    feat:  카드 추가 결과 화면 컴포넌트 작성

---
+++ src/components/Page/AddCardResult/AddCardResult.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components';
+import NextButton from '../../Button';
+import Card from '../../Card';
+import CardNickNameInput from '../../Input/CardNickNameInput';
+
+const Container = styled.div`
+  display: flex;
+  width: 375px;
+  height: 701px;
+  flex-direction: column;
+  position: relative;
+`;
+
+const TitleWrapper = styled.div`
+  margin-top: 130px;
+  font-size: 24px;
+  font-weight: 400;
+  text-align: center;
+`;
+
+const CardWrapper = styled.div`
+  margin-top: 77px;
+`;
+
+const CardNickNameInputWrapper = styled.div`
+  text-align: center;
+  margin-top: 33px;
+`;
+const NextButtonWrapper = styled.div`
+  position: absolute;
+  bottom: 20px;
+  right: 30px;
+`;
+
+const AddCardResultPage = () => {
+  return (
+    <Container>
+      <TitleWrapper>카드 등록이 완료되었습니다.</TitleWrapper>
+      <CardWrapper>
+        <Card
+          name={'샐리'}
+          expiredMonth={'12'}
+          expiredYear={'23'}
+          cardNumbers={['1111', '2222', '3333', '4444']}
+          cardInfo={{ color: 'red', name: '포코 카드' }}
+          size={'large'}
+        />
+      </CardWrapper>
+      <CardNickNameInputWrapper>
+        <CardNickNameInput />
+      </CardNickNameInputWrapper>
+      <NextButtonWrapper>
+        <NextButton name="confirmButton" type="button">
+          확인
+        </NextButton>
+      </NextButtonWrapper>
+    </Container>
+  );
+};
+
+export default AddCardResultPage;
---
+++ src/components/Page/AddCardResult/AddCardResult.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AddCardResultPage from './AddCardResult';
+
+export default {
+  title: 'AddCardResultPage',
+  component: AddCardResultPage,
+};
+
+const Template = () => <AddCardResultPage />;
+
+export const Exmaple = Template.bind({});
