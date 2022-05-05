94e85eb1261857c12788843e80033ee71703aad3
Soyi Jeon <soyi.dev@gmail.com>
2022-05-06 01:16:01 +0900

    fix: CardListPage에서 누락된 prop 추가

--- src/pages/CardListPage.jsx
+++ src/pages/CardListPage.jsx
@@ -70,7 +70,7 @@ function CardList() {
         {value =>
           value.cardInfoList.map((cardInfo, index) => (
             <CardListItem key={index}>
-              <CardItem size={'small'} {...cardInfo} />
+              <CardItem size={'small'} isComplete={true} {...cardInfo} />
               <CardNickName>{cardInfo.nickName || `나의 카드 ${index + 1}`}</CardNickName>
             </CardListItem>
           ))
