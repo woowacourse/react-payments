25b0bb00f51937c432b329d46da6a5d1d15893cd
kimsanglok <evertree6031@gmail.com>
2022-05-05 17:30:05 +0900

    feat: CardListPage - 초기 렌더링 직후, cardList 불러오기

--- src/page/cardList/index.jsx
+++ src/page/cardList/index.jsx
@@ -1,41 +1,26 @@
 import CardPreview from 'components/CardPreview';
 import Header from 'components/common/Header';
-import { CRYPTO_STRING } from 'constants';
-import React from 'react';
+import { getCardListAPI } from 'lib/api';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const mockCardInfo = {
-  company: '클린카드',
-  cardNumber: {
-    first: '1111',
-    second: '2222',
-    third: '3333',
-    fourth: '4444',
-  },
-  expiryDate: {
-    month: '12',
-    year: '23',
-  },
-  ownerName: 'lokba',
-  privacyCode: '123',
-  password: {
-    first: '1',
-    second: '1',
-    third: CRYPTO_STRING,
-    fourth: CRYPTO_STRING,
-  },
-  theme: 'red',
-};
-
 const CardListPage = () => {
+  const [cardList, setCardList] = useState([]);
+
+  useEffect(() => {
+    getCardListAPI().then((response) => setCardList(response));
+  }, []);
+
   return (
     <div>
       <Header title="보유 카드 목록" />
       <div className="card-list mt-10">
-        <div className="flex-column-center">
-          <CardPreview cardInfo={mockCardInfo} isVisibleButton="hide" theme="red" />
-          <span className="text-center">법인카드</span>
-        </div>
+        {cardList.map((card) => (
+          <div key={card.id} className="flex-column-center">
+            <CardPreview cardInfo={card} isVisibleButton="hide" theme="red" />
+            <span className="text-center">{card.alias}</span>
+          </div>
+        ))}
 
         {/* 카드 추가하는 페이지*/}
         <div className="card-item">
