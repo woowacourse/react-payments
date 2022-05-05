baaca50c7ce9a89159113599f26ee672881e3aca
woose28 <dldntmdsla@naver.com>
2022-05-06 06:48:23 +0900

    refactor: styled-components 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,6 @@
-import styled, { createGlobalStyle } from 'styled-components';
 import CardAddPage from './pages/CardAddPage/';
+import { GlobalStyle, ModalContainer } from './style';
 import useModal from './hooks/useModal';
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    border: none;
-    box-sizing: border-box;
-  }
-
-  body {
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    align-items: center;
-    justify-content: center;
-    background-color: #e5e5e5;
-  }
-  
-  input::-webkit-outer-spin-button,
-  input::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
-
-  #root {
-    position: relative;
-    background-color: #fff;
-    width: 375px;
-    min-width: 375px;
-    height: 700px;
-    position: relative;
-    border-radius: 15px;
-  }
-`;
-
-const ModalContainer = styled.div`
-  visibility: ${props => props.visibility};
-  position: absolute;
-  top: 0px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  width: 100%;
-  height: 100%;
-  border-radius: 15px;
-  background-color: rgba(0, 0, 0, 0.4);
-  overflow: hidden;
-`;
 
 function App() {
   const { isOpenModal, openModal, closeModal } = useModal();
---
+++ src/style.js
@@ -0,0 +1,49 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    border: none;
+    box-sizing: border-box;
+  }
+
+  body {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    align-items: center;
+    justify-content: center;
+    background-color: #e5e5e5;
+  }
+  
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  #root {
+    position: relative;
+    background-color: #fff;
+    width: 375px;
+    min-width: 375px;
+    height: 700px;
+    position: relative;
+    border-radius: 15px;
+  }
+`;
+
+export const ModalContainer = styled.div`
+  visibility: ${props => props.visibility};
+  position: absolute;
+  top: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  width: 100%;
+  height: 100%;
+  border-radius: 15px;
+  background-color: rgba(0, 0, 0, 0.4);
+  overflow: hidden;
+`;
