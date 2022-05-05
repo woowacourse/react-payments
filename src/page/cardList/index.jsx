59e380a4e6272d33752d8094fd37ec40ad9188f3
kimsanglok <evertree6031@gmail.com>
2022-05-05 20:23:10 +0900

    feat: 카드 삭제 기능 구현

--- src/page/cardList/index.jsx
+++ src/page/cardList/index.jsx
@@ -1,18 +1,27 @@
 import CardPreview from 'components/CardPreview';
 import Button from 'components/common/Button';
 import Header from 'components/common/Header';
-import { getCardListAPI } from 'lib/api';
+import { deleteCardAPI, getCardListAPI } from 'lib/api';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { ReactComponent as DeleteIcon } from 'assets/delete_icon.svg';
 
 const CardListPage = () => {
   const [cardList, setCardList] = useState([]);
+
+  const handleDeleteCard = (e, cardId) => {
+    e.preventDefault();
+
+    if (window.confirm('정말 삭제하시겠습니까?')) {
+      deleteCardAPI(cardId);
+      getCardListAPI().then((response) => setCardList(response));
+    }
+  };
 
   useEffect(() => {
     getCardListAPI().then((response) => setCardList(response));
   }, []);
 
-  //삭제 페이지 만들기
   // 리듀서로 관리하기
 
   return (
@@ -21,9 +30,16 @@ const CardListPage = () => {
       <div className="card-list mt-10">
         {cardList.map((card) => (
           <Link key={card.id} to={`/modify/${card.id}`}>
-            <div className="flex-column-center">
+            <div className="flex-column-center card-item-wrapper">
               <CardPreview cardInfo={card} isVisibleButton="hide" theme="red" />
               <span className="text-center">{card.alias}</span>
+              <Button
+                className="card-delete-button"
+                theme="red"
+                handleClick={(e) => handleDeleteCard(e, card.id)}
+              >
+                <DeleteIcon />
+              </Button>
             </div>
           </Link>
         ))}
