773889298625c0aca46e0d4a646605221960664d
2019015414 <2019015414@hanyang.ac.kr>
2022-05-06 02:12:34 +0900

    feat: HomePage의 scroll 구현

--- react-payments/src/component/common/PageContainer/PageContainer.component.jsx
+++ react-payments/src/component/common/PageContainer/PageContainer.component.jsx
@@ -8,6 +8,22 @@ const PageContainer = styled.div`
   border: 1px solid ${({ theme }) => theme.colors.pageDefault};
   padding: 20px 15px;
   gap: 5px;
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: #e4e4e4;
+    border-radius: 100px;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 100px;
+    border: 6px solid rgba(0, 0, 0, 0.18);
+    border-left: 0;
+    border-right: 0;
+    background-color: ${({ theme }) => theme.colors.scroll};
+  }
 `;
 
 export default PageContainer;
