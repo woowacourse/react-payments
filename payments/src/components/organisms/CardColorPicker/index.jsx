28fda630ecbc7a4d20bc5653a2d1574e4a86c8de
JUDONGHYEOK <judonghyeok@gmail.com>
2022-05-05 19:36:58 +0900

    refactor: useModal 내에서 Modal 컴포넌트 포함

--- payments/src/components/organisms/CardColorPicker/index.jsx
+++ payments/src/components/organisms/CardColorPicker/index.jsx
@@ -1,5 +1,4 @@
 import Color from "../../common/Color";
-import Modal from "../../common/Modal";
 import "./index.scss";
 
 const CARD_CATEGORY = [
@@ -23,21 +22,19 @@ const CardColorPicker = ({ closeModal, onChangeCardName }) => {
   };
 
   return (
-    <Modal closeModal={closeModal}>
-      <div className="palette">
-        {CARD_CATEGORY.map((card) => (
-          <Color
-            key={card.color}
-            name={card.name}
-            color={card.color}
-            onClick={() => {
-              pickColor(card.name, card.color);
-            }}
-            setVisible={closeModal}
-          />
-        ))}
-      </div>
-    </Modal>
+    <div className="palette">
+      {CARD_CATEGORY.map((card) => (
+        <Color
+          key={card.color}
+          name={card.name}
+          color={card.color}
+          onClick={() => {
+            pickColor(card.name, card.color);
+          }}
+          setVisible={closeModal}
+        />
+      ))}
+    </div>
   );
 };
 
--- payments/src/hooks/useModal.js
+++ payments/src/hooks/useModal.js
@@ -1,11 +1,21 @@
 import { useState } from "react";
+import Modal from "../components/common/Modal";
 import Portal from "../Portal";
 
 const useModal = (element) => {
   const [visible, setVisible] = useState(false);
 
+  const closeModal = () => {
+    setVisible(false);
+  };
   const ModalElement = () => {
-    return visible && <Portal>{element}</Portal>;
+    return (
+      visible && (
+        <Portal>
+          <Modal closeModal={closeModal}>{element}</Modal>
+        </Portal>
+      )
+    );
   };
 
   return [setVisible, ModalElement];
