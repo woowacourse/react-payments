cea2603b9f1606a5d2bcc62bbfc799390ff01817
kimsanglok <evertree6031@gmail.com>
2022-05-05 17:28:11 +0900

    feat: cardInfo, card CRUD API function 구현

---
+++ src/lib/api.js
@@ -0,0 +1,48 @@
+export const registerCardAPI = (cardInfo) => {
+  console.log(cardInfo);
+  try {
+    fetch('http://localhost:8000/cardList', {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(cardInfo),
+    });
+  } catch (e) {
+    throw Error('카드 등록 과정에서 오류가 발생했습니다.');
+  }
+};
+
+export const getCardListAPI = async () => {
+  try {
+    const response = await fetch('http://localhost:8000/cardList', {
+      method: 'GET',
+      headers: { 'Content-type': 'application/json' },
+    });
+
+    return response.json();
+  } catch (e) {
+    throw Error('카드 목록 불러오기 과정에서 오류가 발생했습니다.');
+  }
+};
+
+export const updateCard = (id, cardInfo) => {
+  try {
+    fetch(`http://localhost:8000//cardList/${id}`, {
+      method: 'PATCH',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(cardInfo),
+    });
+  } catch (e) {
+    throw Error('카드 수정 과정에서 오류가 발생했습니다.');
+  }
+};
+
+export const deleteCard = (id) => {
+  try {
+    fetch(`http://localhost:8000//cardList/${id}`, {
+      method: 'DELETE',
+      headers: { 'Content-type': 'application/json' },
+    });
+  } catch (e) {
+    throw Error('카드 삭제 과정에서 오류가 발생했습니다.');
+  }
+};
