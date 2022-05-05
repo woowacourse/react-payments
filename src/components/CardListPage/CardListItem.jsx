52a2f325c02457a339baf5795a6f69467493810c
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-05-05 22:24:30 +0900

    refactor: 카드리스트아이템 컴포넌트 폴더 이동

--- src/components/CardListPage/CardListItem.jsx
+++ src/components/CardListPage/CardListItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Card from './Card';
+import Card from '../common/Card';
 
 const ItemContainer = styled.li`
   display: flex;
