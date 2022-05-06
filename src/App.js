de152dacdcdf8f9eaff771e2eb5edb27ccfaba6f
최광희 <polyckh@gmail.com>
2022-05-06 20:35:44 +0900

    style: global style styled-components로 지정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import styled from 'styled-components';
+import GlobalStyle from './GlobalStyle';
+
 import { CardRegisterPage } from './pages/CardRegisterPage';
 
 function App() {
   return (
-    <div className="App">
-      <CardRegisterPage />
-    </div>
+    <>
+      <GlobalStyle />
+      <Style.App>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/react-payments" element={<CardRegisterPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Style.App>
+    </>
   );
 }
 
 export default App;
+
+const Style = {
+  App: styled.div`
+    width: 100%;
+    max-width: 450px;
+    height: 100%;
+    padding: 0 24px;
+    text-align: center;
+  `,
+};
