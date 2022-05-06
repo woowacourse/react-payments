01b803878039a3c55299d9279312470d32f843ad
(yunjin) <sion11290805@gmail.com>
2022-05-06 16:56:49 +0900

    feat: react-router-dom 추가
    패키지 설치 및 path 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import CardContainer from 'containers/card/CardContainer';
-import CardFormContainer from 'containers/card/CardFormContainer';
 import AppProvider from 'context/Provider';
-import Navigation from 'fields/Navigation';
 import AddCardPage from 'pages/AddCardPage';
+import { Route, Routes } from 'react-router-dom';
+import CardListPage from 'pages/CardListPage';
 
 function App() {
   return (
     <AppProvider>
-      <AddCardPage>
-        <Navigation />
-        <CardContainer />
-        <CardFormContainer />
-      </AddCardPage>
+      <Routes>
+        <Route path="/" element={<CardListPage />} />
+        <Route path="card/add" element={<AddCardPage />} />
+      </Routes>
     </AppProvider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'App';
 import 'scss/style.scss';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
