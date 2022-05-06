5010214cc51aa93982b259602e208586c44f2735
Hwang Jung Min <hwangstar156@gmail.com>
2022-05-06 15:04:11 +0900

    feat: HomePage 추가

--- react-payments/src/component/common/PageContainer/PageContainer.component.jsx
+++ react-payments/src/component/common/PageContainer/PageContainer.component.jsx
@@ -5,8 +5,26 @@ const PageContainer = styled.div`
   flex-direction: column;
   width: 350px;
   height: 850px;
-  border: 1px solid ${({ theme }) => theme.colors.pageDefault};
+  border: 1px solid ${({ theme }) => theme.colors.pageBorder};
   padding: 20px 15px;
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: #e4e4e4;
+    border-radius: 100px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 100px;
+    border: 6px solid rgba(0, 0, 0, 0.18);
+    border-left: 0;
+    border-right: 0;
+    background-color: ${({ theme }) => theme.colors.scroll};
+  }
 `;
 
 export default PageContainer;
--- react-payments/src/pages/HomePage/HomePage.pages.jsx
+++ react-payments/src/pages/HomePage/HomePage.pages.jsx
@@ -1,12 +1,30 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
-import { CardDataContext } from "../../provider/CardDataProvider";
+import styled from "styled-components";
+import CardAddLink from "../../component/CardAddLink/CardAddLink";
+import Header from "../../component/common/Header/Header.component";
+import PageContainer from "../../component/common/PageContainer/PageContainer.component";
+import PageTitle from "../../component/common/PageTitle/pageTitle.component";
+import SavedCardList from "../../component/SavedCardList/SavedCardList";
+
+const CardGroup = styled.div`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+`;
 
 const HomePage = () => {
-  const { state } = useContext(CardDataContext);
-  console.log(state);
-
-  return <Link to="/add">추가</Link>;
+  return (
+    <PageContainer>
+      <Header>
+        <PageTitle type="header">보유 카드</PageTitle>
+      </Header>
+      <CardGroup>
+        <SavedCardList />
+        <CardAddLink to="/add">+</CardAddLink>
+      </CardGroup>
+    </PageContainer>
+  );
 };
 
 export default HomePage;
