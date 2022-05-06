eceec706dde4beb9de7c0586d0d4ff56204add7e
cks3066 <cks3066@naver.com>
2022-05-06 14:36:57 +0900

    feat: portal을 이용하여 modal 구현

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,36 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { CardInfo } from "../../types";
+import Modal from "../Modal";
 
 export default function Card({ cardInfo }: { cardInfo: CardInfo }) {
   const { cardNumbers, expiredDate, userName } = cardInfo;
+  const [isModalShow, setIsModalShow] = useState(false);
+
+  const showModal = () => {
+    setIsModalShow(true);
+  };
+
+  const hideModal = () => {
+    setIsModalShow(false);
+  };
 
   return (
-    <div className="card-box">
-      <div className="empty-card">
-        <div className="card-top">
-          <span className="card-text"></span>
-          <span className="card-text">
-            <span className="card-expired-date">{expiredDate.month || "MM"}</span> /{" "}
-            <span className="card-expired-date">{expiredDate.year || "YY"}</span>
-          </span>
-        </div>
-        <div className="card-middle">
-          <div className="small-card__chip"></div>
-        </div>
-        <div className="card-bottom">
-          <div className="card-text card-numbers-text">
-            {cardNumbers.map((cardNumber, index) => {
-              return (
-                <span className="card-number" key={index}>
-                  {cardNumber}
-                </span>
-              );
-            })}
+    <>
+      <div className="card-box">
+        <div className="empty-card" onClick={showModal} aria-hidden="true">
+          <div className="card-top">
+            <span className="card-text"></span>
+            <span className="card-text">
+              <span className="card-expired-date">{expiredDate.month || "MM"}</span> /{" "}
+              <span className="card-expired-date">{expiredDate.year || "YY"}</span>
+            </span>
           </div>
-          <span className="card-text card-user-name">{userName || "NAME"}</span>
+          <div className="card-middle">
+            <div className="small-card__chip"></div>
+          </div>
+          <div className="card-bottom">
+            <div className="card-text card-numbers-text">
+              {cardNumbers.map((cardNumber, index) => {
+                return (
+                  <span className="card-number" key={index}>
+                    {cardNumber}
+                  </span>
+                );
+              })}
+            </div>
+            <span className="card-text card-user-name">{userName || "NAME"}</span>
+          </div>
         </div>
       </div>
-    </div>
+      {isModalShow && <Modal onClose={hideModal} />}
+    </>
   );
 }
---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import ModalPortal from "../../Portal";
+
+const cardList = [
+  "포코 카드",
+  "준 카드",
+  "공원 카드",
+  "브랜 카드",
+  "로이드 카드",
+  "도비 카드",
+  "콜린 카드",
+  "썬 카드",
+];
+
+export default function Modal({ onClose }) {
+  return (
+    <ModalPortal>
+      <div
+        className="modal-dimmed"
+        onClick={e => {
+          const eventTarget = e.target as HTMLDivElement;
+
+          if (eventTarget.className === "modal-dimmed") onClose();
+        }}
+        aria-hidden="true"
+      >
+        <div className="modal">
+          <div className="flex-center">
+            <div className="modal-item-container">
+              <div
+                className="modal-item-dot"
+                style={{ backgroundColor: "#E24141" }}
+                onClick={onClose}
+                aria-hidden="true"
+              ></div>
+              <span className="modal-item-name">포코 카드</span>
+            </div>
+            <div className="modal-item-container">
+              <div
+                className="modal-item-dot"
+                style={{ backgroundColor: "#547CE4" }}
+                onClick={onClose}
+                aria-hidden="true"
+              ></div>
+              <span className="modal-item-name">준 카드</span>
+            </div>
+            <div className="modal-item-container">
+              <div
+                className="modal-item-dot"
+                style={{ backgroundColor: "#73BC6D" }}
+                onClick={onClose}
+                aria-hidden="true"
+              ></div>
+              <span className="modal-item-name">공원 카드</span>
+            </div>
+            <div className="modal-item-container">
+              <div
+                className="modal-item-dot"
+                style={{ backgroundColor: "#DE59B9" }}
+                onClick={onClose}
+                aria-hidden="true"
+              ></div>
+              <span className="modal-item-name">브랜 카드</span>
+            </div>
+          </div>
+          <div className="flex-center">
+            <div className="modal-item-container">
+              <div
+                className="modal-item-dot"
+                style={{
+                  backgroundColor:
+                    "radial-gradient(50% 50% at 50% 50%, rgba(4, 192, 158, 0.31) 0%, rgba(4, 192, 158, 0.457344) 65.1%, #04C09E 100%)",
+                }}
+                onClick={onClose}
+                aria-hidden="true"
+              ></div>
+              <span className="modal-item-name">로이드 카드</span>
+            </div>
+            <div className="modal-item-container">
+              <div
+                className="modal-item-dot"
+                style={{ backgroundColor: "#E76E9A" }}
+                onClick={onClose}
+                aria-hidden="true"
+              ></div>
+              <span className="modal-item-name">도비 카드</span>
+            </div>
+            <div className="modal-item-container">
+              <div
+                className="modal-item-dot"
+                style={{ backgroundColor: "#F37D3B" }}
+                onClick={onClose}
+                aria-hidden="true"
+              ></div>
+              <span className="modal-item-name">콜린 카드</span>
+            </div>
+            <div className="modal-item-container">
+              <div
+                className="modal-item-dot"
+                style={{ backgroundColor: "#FBCD58" }}
+                onClick={onClose}
+                aria-hidden="true"
+              ></div>
+              <span className="modal-item-name">썬 카드</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </ModalPortal>
+  );
+}
