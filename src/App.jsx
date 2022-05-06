6b2bc66e7323ae336ad829d70e18861fcb150a5b
Ahn <tn3748@naver.com>
2022-05-07 02:51:00 +0900

    feat: router를 이용하여 카드목록 화면을 보여준다

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './css/App.css';
+import { Route, Routes } from 'react-router-dom';
 import AddCard from './components/addCard/AddCard';
+import CardList from './components/cardList/CardList';
 
 function App() {
   return (
     <div className="App">
-      <AddCard />
+      <Routes>
+        <Route exact path="/react-payments" element={<CardList />} />
+        <Route path="/react-payments/add" element={<AddCard />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/cardList/CardList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function CardList() {
+  return <div>list</div>;
+}
+
+CardList.propTypes = {};
+export default CardList;
