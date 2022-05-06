ea762a9eb8f4c328a6be59d809dda2792162e279
이아현 <lah1203@naver.com>
2022-05-06 15:14:14 +0900

    feat: 카드 소유자 이름에 영문만 입력될 수 있도록 구현
    만약 소문자가 입력될 경우 대문자로 바뀌도록 구현

--- src/hooks/useCardOwnerName.jsx
+++ src/hooks/useCardOwnerName.jsx
@@ -1,10 +1,14 @@
 import { useState, useCallback } from 'react';
 
+import { isValidOwnerName } from '../utils/regExp';
+
 export default function useCardOwnerName(initialValue) {
   const [cardOwnerName, setCardOwnerName] = useState(initialValue);
 
   const handler = useCallback(({ target: { value } }) => {
-    setCardOwnerName(value);
+    if (!isValidOwnerName(value)) return;
+
+    setCardOwnerName(value.toUpperCase());
   }, []);
 
   return [cardOwnerName, handler];
--- src/utils/regExp.js
+++ src/utils/regExp.js
@@ -1,7 +1,14 @@
 const splitCardNumbers = (numbers, separator) =>
   numbers.match(/[\d•]{1,4}/g)?.join(separator);
 const isValidDate = date => /^$|^[0-9]{0,2}[/]{0,1}[0-9]{0,2}$/.test(date);
+const isValidOwnerName = ownerName => /^$|[a-zA-Z]$/.test(ownerName);
 const isValidCVC = CVC => /^$|^[0-9]{0,3}$/.test(CVC);
 const isValidPassword = password => /^$|^[0-9]{0,1}$/.test(password);
 
-export { splitCardNumbers, isValidDate, isValidCVC, isValidPassword };
+export {
+  splitCardNumbers,
+  isValidOwnerName,
+  isValidDate,
+  isValidCVC,
+  isValidPassword,
+};
