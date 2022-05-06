93d61c1c6887b3333ec584f29a440423bc2b1962
Hwang Jung Min <hwangstar156@gmail.com>
2022-05-06 14:57:26 +0900

    feat: cardList를 보여주는 SavedCardList 추가

---
+++ react-payments/src/component/SavedCardList/SavedCardList.jsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import styled from "styled-components";
+import { CardDataContext } from "../../provider/CardDataProvider";
+import Card from "../common/Card/card.component";
+
+const CardNameText = styled.p`
+  font-weight: 700;
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.cardText};
+  text-align: center;
+  opacity: 90%;
+  margin-top: 5px;
+`;
+
+const SavedCardList = () => {
+  const { cardData } = useContext(CardDataContext);
+
+  return (
+    <>
+      {cardData.map((cardDatum, idx) => (
+        <div key={idx}>
+          <Card {...cardDatum} />
+          <CardNameText>{cardDatum.cardName}</CardNameText>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default SavedCardList;
