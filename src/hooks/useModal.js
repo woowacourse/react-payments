785b915343ce763a2039f1d7a1180c0ab4ae1326
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-07 02:14:11 +0900

    fix: 상위 컴포넌트 렌더링 발생 시 모달 오픈 애니메이션이 추가 발생하는 문제 해결

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import Modal from 'components/@common/Modal';
 
@@ -11,12 +11,21 @@ function useModal() {
   const handleDisappear = () => setVisible(MODAL_STATE.DISAPPEAR);
   const handleHidden = () => setVisible(MODAL_STATE.HIDDEN);
 
-  return {
-    Modal: ({ children }) => (
+  // 부모 컴포넌트에서 렌더링이 실행 될 때마다 모달 오픈 애니메이션이 계속 발동 됨 => useCallback으로 감싸니 해결
+  // 추정 원인 : 버그 수정 전에는 기존 앨리먼트 비교 없이, 계속해서 모달이 새롭게 그려지고 있었는 것으로 추정.
+  // 기존에는 커스텀훅에서 객체를 매번 새롭게 생성해서 반환했으니, 메모리 주소가 계속 바뀌어서...? 리액트가 변경되었다고 감지?
+  // 나중에 더 찾아보기
+  const render = useCallback(
+    ({ children }) => (
       <Modal className={modalState} handleClose={handleDisappear} handleHidden={handleHidden}>
         {children}
       </Modal>
     ),
+    [modalState],
+  );
+
+  return {
+    Modal: render,
     handleModalOpen: handleVisible,
     handleModalClose: handleDisappear,
   };
