de084aa739bea69888d291b7a092d509d77ad723
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-07 21:54:49 +0900

    feat: 페이지 타이틀 기능 추가, 페이지 이동 상태 처리

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import { usePageSetting } from 'contexts/PageContext';
-import { CardListContextProvider } from 'contexts/CardListContext';
-import { PAGE_LIST } from 'constants';
+import { usePageContext } from 'contexts/PageContext';
+import { CardDataContextProvider } from 'contexts/CardDataContext';
+import { APP_NAME, PAGE_LIST } from 'constants';
 
 import Layout from 'components/Layout';
 
 import CardUpdated from 'pages/CardUpdated';
 import CardEditor from 'pages/CardEditor';
+import CardList from 'pages/CardList';
+import { useEffect } from 'react';
 
 function App() {
-  const { pageLocation } = usePageSetting();
+  const { pageTitle, pageLocation } = usePageContext();
+
+  useEffect(() => {
+    document.querySelector('title').textContent = pageTitle
+      ? `${pageTitle} - ${APP_NAME}`
+      : APP_NAME;
+  }, [pageTitle]);
 
   return (
     <Layout>
       {(pageLocation === PAGE_LIST.CARD_EDITOR && <CardEditor />) ||
-        (pageLocation === PAGE_LIST.CARD_UPDATED && <CardUpdated />)}
+        (pageLocation === PAGE_LIST.CARD_UPDATED && <CardUpdated />) ||
+        (pageLocation === PAGE_LIST.CARD_LIST && <CardList />)}
     </Layout>
   );
 }
 
 function ContextWrap() {
   return (
-    <CardListContextProvider>
+    <CardDataContextProvider>
       <App />
-    </CardListContextProvider>
+    </CardDataContextProvider>
   );
 }
 
