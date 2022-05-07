105b9d92454df57a1c89568ddd421f414385d51f
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-08 01:57:23 +0900

    style: 헤더 스타일 변경

--- src/components/@common/Header/styles.js
+++ src/components/@common/Header/styles.js
@@ -4,23 +4,36 @@ import { css } from '@emotion/react';
 import { BRAND_COLORS, COLORS, LAYOUT } from 'styles/theme';
 import responsive from 'styles/utils/responsive';
 
+import Logo from 'assets/logo.png';
+
 const Container = styled.header`
   position: sticky;
-  width: 100%;
-  background-color: ${COLORS['white-80']};
-  backdrop-filter: blur(1rem);
-  border-bottom: 1px solid ${COLORS.alto};
-  border-radius: ${LAYOUT.BORDER_RADIUS * 2}px ${LAYOUT.BORDER_RADIUS * 2}px 0 0;
-  top: 0px;
-
   display: flex;
   align-items: center;
+
+  width: 100%;
+
+  background-image: url(${Logo});
+  background-repeat: no-repeat;
+  background-size: 7.5rem;
+  background-position: right 1.7rem center;
+
+  background-color: ${COLORS['white-50']};
+  backdrop-filter: blur(1rem);
+
+  border-bottom: 1px solid ${COLORS.alto};
+  border-radius: ${LAYOUT.BORDER_RADIUS * 2}px ${LAYOUT.BORDER_RADIUS * 2}px 0 0;
+
+  top: 0px;
   z-index: 100;
   padding: 1.5rem;
+
+  box-shadow: 0 0 2.25rem ${COLORS['black-15']};
 
   ${responsive.notMobile(
     css`
       position: relative;
+      box-shadow: unset;
     `,
   )}
 
@@ -40,8 +53,8 @@ const Container = styled.header`
   }
 
   & > .title {
-    font-weight: bold;
     font-size: 1.35rem;
+    font-weight: bold;
     line-height: 1.375rem;
     color: ${COLORS.dorado};
   }
