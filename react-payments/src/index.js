0551500aee8bcb6b05fdd1c7a70062556ecfe57d
2019015414 <2019015414@hanyang.ac.kr>
2022-05-07 17:57:56 +0900

    fix: url 수정

--- react-payments/src/index.js
+++ react-payments/src/index.js
@@ -11,7 +11,7 @@ import CardDataProvider from "./provider/CardDataProvider";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <ThemeProvider theme={theme}>
         <CardDataProvider>
           <FormDataProvider>
