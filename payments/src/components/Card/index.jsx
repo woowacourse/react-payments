dee83e9685f53223a85db059100ef510a7a20df9
YUNHO KANG <ansghkdbsgh@gmail.com>
2022-05-08 04:21:48 +0900

    feat: 카드 뒷면 확인 가능

--- payments/src/components/Card/index.jsx
+++ payments/src/components/Card/index.jsx
@@ -1,13 +1,28 @@
+import { useRef, useState } from 'react';
 import './index.scss';
 
-const Card = ({ state: { cardNumber, expiredDate, ownerName, cardName, color }, setVisible }) => {
+const Card = ({ state: { cardNumber, expiredDate, ownerName, cardName, color, secureCode }, setVisible }) => {
+  const cardFront = useRef();
+  const cardBack = useRef();
+
+  const mouserOver = () => {
+    cardFront.current.style.display = 'none';
+    cardBack.current.style.display = 'grid';
+  };
+
+  const mouseLeave = () => {
+    cardFront.current.style.display = 'grid';
+    cardBack.current.style.display = 'none';
+  };
+
   return (
-    <div
-      onClick={() => {
-        setVisible(true);
-      }}
-    >
-      <div className='card__container' style={{ backgroundColor: color }}>
+    <div className='card__container' onMouseOver={mouserOver} onMouseLeave={mouseLeave}>
+      <div
+        className='card-front__container'
+        style={{ display: 'grid', backgroundColor: color }}
+        onClick={() => setVisible(true)}
+        ref={cardFront}
+      >
         <p className='card__name'>{cardName}</p>
         <div className='rfid'></div>
         <div className='card__numbers'>
@@ -21,6 +36,20 @@ const Card = ({ state: { cardNumber, expiredDate, ownerName, cardName, color }, 
           <div className='expired__date'>
             {expiredDate[0] || 'MM'}/{expiredDate[1] || 'YY'}
           </div>
+        </div>
+      </div>
+
+      <div
+        className='card-back__container'
+        style={{ display: 'none', backgroundColor: color }}
+        onClick={() => setVisible(true)}
+        ref={cardBack}
+      >
+        <div className='magnetic'></div>
+        <p className='authorization'>Authorized signature</p>
+        <div className='rfid-back'></div>
+        <div className='signature-line'>
+          <div className='secure-code'>{secureCode}</div>
         </div>
       </div>
     </div>
