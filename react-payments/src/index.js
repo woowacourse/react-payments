03b5c6c2d7e994265feeef812b84e04b69ec7d32
Hwang Jung Min <hwangstar156@gmail.com>
2022-05-08 00:22:25 +0900

    feat: browser router에 basename 추가

--- react-payments/src/index.js
+++ react-payments/src/index.js
@@ -12,7 +12,7 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="react-payments">
     <ThemeProvider theme={theme}>
       <FormDataProvider>
         <CardDataProvider>
