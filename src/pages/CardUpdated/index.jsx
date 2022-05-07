6a9f896ef20ed369c96e477e2b798fa6d7bc65c5
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-07 21:55:57 +0900

    feat: 카드 추가 결과 컴포넌트 상태 연결

--- src/pages/CardUpdated/index.jsx
+++ src/pages/CardUpdated/index.jsx
@@ -1,24 +1,47 @@
+import { useEffect } from 'react';
+import { usePageContext } from 'contexts/PageContext';
+import { useCardDataContext } from 'contexts/CardDataContext';
+import { CARD_COMPANY, PAGE_LIST } from 'constants';
+
 import { Card } from 'components';
 import Button from 'components/@common/Button';
 
 import ResultMessage from './styles';
 
 function CardUpdated() {
+  const { setPageTitle, setPageLocation } = usePageContext();
+  useEffect(() => setPageTitle('카드 등록 완료'), []);
+
+  const { cardList } = useCardDataContext();
+  const { cardName, companyId, cardNumber, expireMonth, expireYear, userName } =
+    cardList[cardList.length - 1] || {};
+
   return (
     <>
       <div className="layout-side-container">
         <ResultMessage>
           <div className="emoji">😍</div>
           <h2 className="title">카드 등록 완료</h2>
-          <p className="description">콤피 카드가 추가되었어요</p>
+          <p className="description">{`${CARD_COMPANY[companyId].name}가 추가되었습니다`}</p>
         </ResultMessage>
       </div>
 
       <div className="layout-main-container fill">
-        <Card companyId="회사명" />
-        <h3 align="center">카드 이름</h3>
+        <Card
+          cardNumber={cardNumber}
+          companyId={companyId}
+          userName={userName}
+          expireMonth={expireMonth}
+          expireYear={expireYear}
+        />
+        <h3 align="center">{cardName}</h3>
 
-        <Button type="primary" size="large" width="100%">
+        <Button
+          type="primary"
+          size="large"
+          width="100%"
+          onClick={() => setPageLocation(PAGE_LIST.CARD_LIST)}
+        >
           목록으로 이동
         </Button>
       </div>
