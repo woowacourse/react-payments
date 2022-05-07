8740be143ad68d8053100b8e65c75d046aa57471
KIM YEJI <rladpwl0512@gmail.com>
2022-05-08 01:50:26 +0900

    :lipstick: style: 카드 리스트 페이지 스타일 적용

--- src/pages/CardListPage.jsx
+++ src/pages/CardListPage.jsx
@@ -1,30 +1,14 @@
 import Header from '../components/Header/Header';
-import Card from 'components/common/Card/Card';
 import PageTitle from 'components/common/PageTitle/PageTitle';
-import { useContext } from 'react';
-import { CardInfoContext } from 'App';
+import CardList from 'components/CardList/CardList';
 
 function CardListPage() {
-  const { state } = useContext(CardInfoContext);
-  const { card, cards } = state;
-
   return (
     <div className="app">
       <Header>
         <PageTitle title="보유카드" />
       </Header>
-      <div>
-        {cards.length !== 0
-          ? cards.map((card) => (
-              <>
-                <Card isEmpty={false} key={card.nickname} cardInfo={card} />
-                <div className="nickname-text">{card.nickname}</div>
-              </>
-            ))
-          : ''}
-
-        <Card isEmpty cardInfo={card} />
-      </div>
+      <CardList />
     </div>
   );
 }
