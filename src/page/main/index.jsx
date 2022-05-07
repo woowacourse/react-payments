ee8b2c998539187583780dd692301e5ec15a9351
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-05-07 16:21:29 +0900

    feat: add routing for card add page

--- src/page/main/index.jsx
+++ src/page/main/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as CardAddIcon } from 'assets/card_add_icon.svg';
 
 const MainPage = () => {
   return (
     <div className="main-container">
-      <div className="card-list"></div>
+      <div className="card-list">
+        <div className="card-items"></div>
+        <Link to="/react-payments/add">
+          <CardAddIcon className="card-add-icon" />
+        </Link>
+      </div>
     </div>
   );
 };
