0ca5f8129c65cb741d97fa87993d792b4b200188
JASUN LEE <liswktjs@gmail.com>
2022-05-07 14:30:42 +0900

    fix: 스토리북 에러 고침

--- src/components/Input/input.stories.jsx
+++ src/components/Input/input.stories.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Input from './index';
 import CardNumberInput from './CardNumberInput';
 import ExpiredDateInput from './ExpiredDateInput';
+import CardNickNameInput from './CardNickNameInput';
 
 export default {
   title: 'Input',
   component: Input,
 };
 
-const Template = args => <Input {...args} />;
-const CardNumberTemplate = args => <CardNumberInput {...args} />;
-const ExpiredDateTemplate = args => <ExpiredDateInput {...args} />;
+const Template = (args) => <Input {...args} />;
+const CardNumberTemplate = (args) => <CardNumberInput {...args} />;
+const ExpiredDateTemplate = (args) => <ExpiredDateInput {...args} />;
+const CardNickNameTemplate = (args) => <CardNickNameInput />;
 
 export const Password = Template.bind({});
 
@@ -28,5 +30,14 @@ UserName.args = {
 };
 
 export const CardNumber = CardNumberTemplate.bind({});
+CardNumber.args = {
+  cardNumbers: ['1111', '1111', '1111', '1111'],
+};
 
 export const ExpiredDate = ExpiredDateTemplate.bind({});
+ExpiredDate.args = {
+  expiredMonth: '07',
+  expiredYear: '24',
+};
+
+export const CardNickName = CardNickNameTemplate.bind({});
