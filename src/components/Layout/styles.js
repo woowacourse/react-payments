9fd3588185c2011e939e3331d977571644da2790
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-07 14:25:07 +0900

    style: 전역 레이아웃 모바일 사용성 개선

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -1,20 +1,54 @@
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 
-import { COLORS } from 'styles/theme';
+import { COLORS, LAYOUT } from 'styles/theme';
 import responsive from 'styles/utils/responsive';
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
   width: 100%;
-  min-height: 20rem;
-  padding: 1.5rem;
+  height: 100%;
   margin: 0 auto;
   background-color: ${COLORS.white};
-  border-radius: 10px;
+  border-radius: ${LAYOUT.BORDER_RADIUS * 2}px;
+
+  .layout-content {
+    padding: 1.5rem;
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    & > div {
+      padding: 1.5rem 0;
+      height: 100%;
+
+      &.fill {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+      }
+    }
+
+    & > .layout-side-container {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      ${responsive.notMobile(
+        css`
+          justify-content: start;
+        `,
+      )}
+    }
+  }
 
   ${responsive.notMobile(css`
-    max-width: unset;
     width: 700px;
+    height: unset;
     box-shadow: 0px 0.625rem 1.875rem 1.25rem ${COLORS['black-05']};
 
     .layout-content {
