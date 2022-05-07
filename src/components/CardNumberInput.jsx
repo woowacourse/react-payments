8b1916b96a3174ac4a088c1708a66ca99ca5fc5c
Soyi Jeon <soyi.dev@gmail.com>
2022-05-07 12:08:51 +0900

    refactor: CardNumberInput에 Fragment 추가
    Input와 '-'를 Fragment로 감싸도록 변경

--- src/components/CardNumberInput.jsx
+++ src/components/CardNumberInput.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import Input from './common/Input';
 import InputField from './common/InputField';
@@ -15,17 +15,17 @@ export default function CardNumberInput({ cardNumber, onChange }) {
         cardNumber.join('').length === CARD_INFO_RULES.NUMBER_UNIT_COUNT * CARD_INFO_RULES.NUMBER_UNIT_LENGTH
       }>
       {Array.from({ length: CARD_INFO_RULES.NUMBER_UNIT_COUNT }).map((_, index) => (
-        <Input
-          key={index}
-          type={index <= 1 ? 'number' : 'password'}
-          value={cardNumber[index]}
-          maxLength="4"
-          placeholder={index <= 1 ? '1 2 3 4' : CREATE_MASKED_CHARACTERS(4)}
-          onChange={e => onChange(e, index)}
-          isComplete={cardNumber[index].length === 4}
-          showPostFix={index < CARD_INFO_RULES.NUMBER_UNIT_COUNT - 1 && cardNumber[index].length === 4}
-          postfix={'-'}
-        />
+        <Fragment key={index}>
+          <Input
+            type={index <= 1 ? 'number' : 'password'}
+            value={cardNumber[index]}
+            maxLength="4"
+            placeholder={index <= 1 ? '1 2 3 4' : CREATE_MASKED_CHARACTERS(4)}
+            onChange={e => onChange(e, index)}
+            isComplete={cardNumber[index].length === 4}
+          />
+          {index < CARD_INFO_RULES.NUMBER_UNIT_COUNT - 1 && cardNumber[index].length === 4 && <p>-</p>}
+        </Fragment>
       ))}
     </InputField>
   );
