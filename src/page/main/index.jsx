9b384670c3c56d14475a2fcb4a4d93311ed3ec1f
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-05-07 19:05:08 +0900

    feat: show card list

--- src/page/main/index.jsx
+++ src/page/main/index.jsx
@@ -1,11 +1,30 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { CardDispatch } from 'App';
 import { ReactComponent as CardAddIcon } from 'assets/card_add_icon.svg';
+import CardPreview from 'components/CardPreview';
 
 const MainPage = () => {
+  const { state } = useContext(CardDispatch);
+
   return (
     <div className="main-container">
       <div className="card-list">
-        <div className="card-items"></div>
+        <div className="card-items">
+          {state.cards.map(({ id, theme, company, number, expiryDate, ownerName, alias }) => (
+            <div key={id}>
+              <CardPreview
+                theme={theme}
+                company={company}
+                number={number}
+                expiryDate={expiryDate}
+                ownerName={ownerName}
+                handleModal={() => {}}
+              />
+              <p className="alias">{alias}</p>
+            </div>
+          ))}
+        </div>
         <Link to="/react-payments/add">
           <CardAddIcon className="card-add-icon" />
         </Link>
