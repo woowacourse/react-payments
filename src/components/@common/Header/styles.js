157b816b423a4fe12b3b28c50895cd4ac8aa7804
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-07 14:24:14 +0900

    style: 헤더 화면 상단 고정 처리

--- src/components/@common/Header/styles.js
+++ src/components/@common/Header/styles.js
@@ -1,21 +1,39 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
-const Container = styled.h2`
+import { COLORS, LAYOUT } from 'styles/theme';
+import responsive from 'styles/utils/responsive';
+
+const Container = styled.header`
+  position: sticky;
+  width: 100%;
+  background-color: ${COLORS['white-80']};
+  border-bottom: 1px solid ${COLORS.alto};
+  border-radius: ${LAYOUT.BORDER_RADIUS * 2}px ${LAYOUT.BORDER_RADIUS * 2}px 0 0;
+  top: 0px;
+
   display: flex;
   align-items: center;
 
-  font-weight: 500;
-  font-size: 1.25rem;
+  font-weight: bold;
+  font-size: 1.35rem;
   line-height: 1.375rem;
-  color: #383838;
-  margin: 0 0 1.25rem;
+  color: ${COLORS.dorado};
+  padding: 1.5rem;
+  z-index: 100;
+
+  ${responsive.notMobile(
+    css`
+      position: relative;
+    `,
+  )}
 
   &::before {
     content: '<';
     font-size: 0.875rem;
     margin-right: 0.938rem;
     transform: scaleX(1.7);
-    color: #999;
+    color: ${COLORS.silverChalice};
   }
 `;
 
