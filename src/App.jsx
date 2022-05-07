d62a083729cf890220200620e4f30cdf93a1bf46
jswith <typingjs@gmail.com>
2022-05-07 14:00:28 +0900

    :recycle: refactor: 카드 정보 업데이트 방식을 useState -> useReducer로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,68 @@
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
+
 import CardListPage from 'pages/CardListPage';
 import CardAddPage from 'pages/CardAddPage';
+
 import { PAGES } from 'constants';
+
+const initialState = {
+  number1: '',
+  number2: '',
+  number3: '',
+  number4: '',
+  month: '',
+  year: '',
+  owner: '',
+  cvc: '',
+  password1: '',
+  password2: '',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_CARD_NUMBERS':
+      return {
+        ...state,
+        number1: action.cardNumbers[0],
+        number2: action.cardNumbers[1],
+        number3: action.cardNumbers[2],
+        number4: action.cardNumbers[3],
+      };
+    case 'SET_CRAD_EXPIRATION_DATE':
+      return {
+        ...state,
+        month: action.cardExpirationDate.month,
+        year: action.cardExpirationDate.year,
+      };
+    case 'SET_OWNER':
+      return {
+        ...state,
+        owner: action.cardOwner,
+      };
+    case 'SET_CVC':
+      return {
+        ...state,
+        cvc: action.cvc,
+      };
+    case 'SET_CARD_PASSWORDS':
+      return {
+        ...state,
+        password1: action.cardPasswords[0],
+        password2: action.cardPasswords[1],
+      };
+    default:
+      return state;
+  }
+};
 
 function App() {
   const [page, setPage] = useState(PAGES.LIST);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return page === PAGES.LIST ? (
     <CardListPage setPage={setPage} />
   ) : (
-    <CardAddPage setPage={setPage} />
+    <CardAddPage setPage={setPage} dispatch={dispatch} cardInfo={state} />
   );
 }
 
