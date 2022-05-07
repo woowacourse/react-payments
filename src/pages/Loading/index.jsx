c5233de9e0a06114de82b16d3590be64f3e4de11
moonheekim0118 <hellomooneekim@gmail.com>
2022-05-08 03:04:14 +0900

    style: NotFound, Loading 페이지 스타일링 수정

--- src/pages/Loading/index.jsx
+++ src/pages/Loading/index.jsx
@@ -1,7 +1,22 @@
 import { Layout } from '../index.styled';
+import styled from 'styled-components';
 
 const Loading = () => {
-  return <Layout>로딩중 입니다. 조금만 기다려주세요</Layout>;
+  return (
+    <Layout>
+      <Container>
+        <h3>로딩중 👀! 조금만 기다려주세요 💙</h3>
+      </Container>
+    </Layout>
+  );
 };
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default Loading;
