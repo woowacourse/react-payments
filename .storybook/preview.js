937cd23a9d4f0601db3ba5d222e09eafcaf188fb
kkojae <jaejeung3210@gmail.com>
2022-05-08 04:20:35 +0900

    feat: storybook global css, preview 수정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import GlobalStyle from "../src/globalStyles";
+import "./storybook-style.css";
+import { BrowserRouter } from "react-router-dom";
+import { CardInfoProvider } from "../src/contexts/CardInfoContext";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,3 +11,28 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <>
+      <BrowserRouter>
+        <CardInfoProvider>
+          <GlobalStyle />
+          <div
+            style={{
+              width: "375px",
+              minHeight: "700px",
+              margin: "50px auto",
+              padding: "20px 28px",
+              position: "relative",
+              backgroundColor: "#fff",
+              borderRadius: "4px",
+            }}
+          >
+            <Story />
+          </div>
+        </CardInfoProvider>
+      </BrowserRouter>
+    </>
+  ),
+];
