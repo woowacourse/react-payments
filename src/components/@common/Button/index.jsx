f957f5d2ef38c318d1f15beec19cba0b9cdc56fc
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-07 13:09:27 +0900

    feat: 버튼 컴포넌트 너비 및 클래스명 지정 기능 추가

--- src/components/@common/Button/index.jsx
+++ src/components/@common/Button/index.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 import Container from './styles';
 
-function Button({ className, type, action, size, isDisabled, onClick, children }) {
+function Button({ className, type, action, size, width, isDisabled, onClick, children }) {
   return (
     <Container
       className={`button ${className}`}
@@ -11,6 +11,7 @@ function Button({ className, type, action, size, isDisabled, onClick, children }
       size={size}
       onClick={onClick}
       disabled={isDisabled}
+      style={{ width }}
     >
       {children}
     </Container>
@@ -18,16 +19,20 @@ function Button({ className, type, action, size, isDisabled, onClick, children }
 }
 
 Button.defaultProps = {
+  className: '',
   type: 'default',
   action: 'button',
   size: 'medium',
+  width: 'unset',
   isDisabled: false,
 };
 
 Button.propTypes = {
+  className: PropTypes.string,
   type: PropTypes.string,
   action: PropTypes.string,
   size: PropTypes.string,
+  width: PropTypes.string,
   isDisabled: PropTypes.bool,
   onClick: PropTypes.func.isRequired,
 };
