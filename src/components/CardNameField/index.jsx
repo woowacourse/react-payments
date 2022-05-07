a82a4988a94abd528675b34b2d397a44eacc7f20
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-08 02:50:35 +0900

    refactor: 사용하지 않는 코드 제거

--- src/components/CardNameField/index.jsx
+++ src/components/CardNameField/index.jsx
@@ -1,24 +1,17 @@
 import PropTypes from 'prop-types';
-import useErrorMessage from 'hooks/useErrorMessage';
 
 import FieldSet from 'components/@common/FieldSet';
 import TextField from 'components/@common/TextField';
 
 function CardNameField({ cardName, onChange }) {
-  const { errorMessage, handleError } = useErrorMessage({
-    state: cardName,
-    validate: () => {},
-  });
-
   return (
-    <FieldSet title="카드 별칭" errorMessage={errorMessage}>
+    <FieldSet title="카드 별칭">
       <TextField
         name="cardName"
         value={cardName}
         placeholder="카드 별칭은 최대 30자까지 입력할 수 있습니다."
         maxLength={30}
         onChange={onChange}
-        onBlur={handleError}
       />
     </FieldSet>
   );
