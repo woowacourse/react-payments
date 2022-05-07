b857a8cfaa9cd4c85aa87cb2ebb1df37c03c4071
Soyi Jeon <soyi.dev@gmail.com>
2022-05-07 17:06:31 +0900

    test: InputField 스토리 수정

--- src/stories/InputField.stories.jsx
+++ src/stories/InputField.stories.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Input from '../components/common/Input';
 import InputField from '../components/common/InputField';
+import { ADD_CARD_FORM_ERROR_MESSAGE } from '../constants';
 
 export default {
   title: 'InputField',
@@ -19,6 +20,7 @@ export const Default = Template.bind({});
 Default.args = {
   labelText: '기본 input 라벨',
   children: <DefaultInput placeholder="placeholder" width="100px" />,
+  errorMessage: '에러 메세지 영역',
 };
 
 export const CardNumberInput = Template.bind({});
@@ -33,6 +35,7 @@ CardNumberInput.args = {
     '-',
     <DefaultInput type="password" placeholder="****" width="60px" maxLength={'4'} />,
   ],
+  errorMessage: ADD_CARD_FORM_ERROR_MESSAGE.CARD_NUMBER,
 };
 
 export const CardExpireDateInput = Template.bind({});
@@ -43,18 +46,21 @@ CardExpireDateInput.args = {
     '/',
     <DefaultInput placeholder="YY" width="50px" maxLength={'2'} />,
   ],
+  errorMessage: ADD_CARD_FORM_ERROR_MESSAGE.EXPIRE_DATE,
 };
 
 export const CardHolderNameInput = Template.bind({});
 CardHolderNameInput.args = {
   labelText: '카드 소유자 이름 (선택)',
   children: [<DefaultInput placeholder="카드에 표시된 이름과 동일하게 입력하세요." width="280px" maxLength={'30'} />],
+  errorMessage: ADD_CARD_FORM_ERROR_MESSAGE.HOLDER_NAME,
 };
 
 export const CardSecurityCodeInput = Template.bind({});
 CardSecurityCodeInput.args = {
   labelText: '보안 코드(CVC/CVV)',
   children: [<DefaultInput type="password" placeholder="***" width="60px" maxLength={'3'} />],
+  errorMessage: ADD_CARD_FORM_ERROR_MESSAGE.SECURITY_CODE,
 };
 
 export const CardPasswordInput = Template.bind({});
@@ -65,4 +71,5 @@ CardPasswordInput.args = {
     <DefaultInput type="password" placeholder="*" width="20px" maxLength={'1'} />,
     <DefaultInput type="password" placeholder="*" width="20px" maxLength={'1'} />,
   ],
+  errorMessage: ADD_CARD_FORM_ERROR_MESSAGE.PASSWORD,
 };
