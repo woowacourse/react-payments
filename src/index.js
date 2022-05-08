9c2b3daec520093d6cbb5bc210b0dfc8c0801cdc
sangminsong <ssmin940606@gmail.com>
2022-05-09 02:36:43 +0900

    fix: 배포 사이트 url 설정 버그 해결

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
