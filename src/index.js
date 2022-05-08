c979e2a90b63ce3f0a98395372c0cf6e099b646c
woose28 <dldntmdsla@naver.com>
2022-05-08 17:54:36 +0900

    chore: 배포 baes url 설정

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.REACT_APP_BASE_NAME}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
