333b28d8ee47b524263ea128ed2d3e15c7069bb1
Ahn <tn3748@naver.com>
2022-05-09 03:19:26 +0900

    feat: 카드회사 modal 스타일 추가

--- src/components/addCard/cardCompany/CardCompany.jsx
+++ src/components/addCard/cardCompany/CardCompany.jsx
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types';
 
 function CardCompany({ handleCompany, company, theme }) {
   return (
-    <div className="modal-item-container" onClick={() => handleCompany(company, theme)}>
-      <div className={`modal-item-dot bg-${theme}`} />
+    <div className="modal-item-container">
+      <div className={`modal-item-dot bg-${theme}`} onClick={() => handleCompany(company, theme)} />
       <span className="modal-item-name">{company}</span>
     </div>
   );
