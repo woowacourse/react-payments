52ab98bb0fe75fd0a1f061dedb6d7410f1ce1763
gytks4 <gytks4@naver.com>
2022-05-09 02:07:20 +0900

    refactor: validation을 onChange에 통합하여 리렌더링 최소화

--- src/hooks/useCardInfoInput.tsx
+++ src/hooks/useCardInfoInput.tsx
@@ -8,16 +8,17 @@ import {
   CardInfoValidation,
   CardName,
   CardNumbers,
+  ExpirationDate,
   Password,
 } from "types/cardInfo";
 
 type CardInfoAction =
   | { type: "CHANGE_CARD_TYPE"; payload: { name: CardName; color: CardColor } }
-  | { type: "CHANGE_CARD_NUMBER"; payload: { index: string; cardNumber: string } }
-  | { type: "CHANGE_EXPIRATION_DATE"; payload: { key: string; value: string } }
+  | { type: "CHANGE_CARD_NUMBER"; payload: { cardNumbers: CardNumbers } }
+  | { type: "CHANGE_EXPIRATION_DATE"; payload: { expirationDate: ExpirationDate } }
   | { type: "CHANGE_USER_NAME"; payload: { userName: string } }
   | { type: "CHANGE_SECURITY_CODE"; payload: { securityCode: string } }
-  | { type: "CHANGE_PASSWORD"; payload: { index: string; value: string } }
+  | { type: "CHANGE_PASSWORD"; payload: { password: Password } }
   | { type: "RESET_CARD_INFO"; payload: null }
   | { type: "BLUR_USER_NAME"; payload: null };
 
@@ -37,21 +38,13 @@ const reducer: CardInfoReducer = (state, action) => {
     }
 
     case "CHANGE_CARD_NUMBER": {
-      const { index, cardNumber } = payload;
-      const cardNumbers: CardNumbers = [...state.cardNumbers];
-
-      cardNumbers[index] = cardNumber;
+      const { cardNumbers } = payload;
 
       return { ...state, cardNumbers };
     }
 
     case "CHANGE_EXPIRATION_DATE": {
-      const { key, value } = payload;
-      const expirationDate = {
-        ...state.expirationDate,
-      };
-
-      expirationDate[key] = value;
+      const { expirationDate } = payload;
 
       return { ...state, expirationDate };
     }
@@ -69,10 +62,7 @@ const reducer: CardInfoReducer = (state, action) => {
     }
 
     case "CHANGE_PASSWORD": {
-      const { index, value } = payload;
-      const password: Password = [...state.password];
-
-      password[index] = value;
+      const { password } = payload;
 
       return { ...state, password };
     }
@@ -124,27 +114,49 @@ function useCardInfoInput() {
     cardInfoDispatch({ type: "CHANGE_CARD_TYPE", payload: { name, color } });
   };
 
-  const onChangeCardNumber: InputChangeFunction = useCallback(e => {
-    const {
-      value,
-      dataset: { index },
-    } = e.target;
+  const onChangeCardNumber: InputChangeFunction = useCallback(
+    e => {
+      const {
+        value,
+        dataset: { index },
+      } = e.target;
 
-    if (value === "" || REGEXP.NUMBER.test(value)) {
-      cardInfoDispatch({ type: "CHANGE_CARD_NUMBER", payload: { index, cardNumber: value } });
-    }
-  }, []);
+      if (value === "" || REGEXP.NUMBER.test(value)) {
+        const cardNumbers: CardNumbers = [...cardInfo.cardNumbers];
 
-  const onChangeExpirationDate: InputChangeFunction = useCallback(e => {
-    const {
-      value,
-      dataset: { key },
-    } = e.target;
+        cardNumbers[index] = value;
 
-    if (value === "" || REGEXP.NUMBER.test(value)) {
-      cardInfoDispatch({ type: "CHANGE_EXPIRATION_DATE", payload: { key, value } });
-    }
-  }, []);
+        cardInfoDispatch({ type: "CHANGE_CARD_NUMBER", payload: { cardNumbers } });
+        handleChangeValidation("cardNumbers", cardNumbers, cardInfoValidator["cardNumbers"]);
+      }
+    },
+    [cardInfo.cardNumbers]
+  );
+
+  const onChangeExpirationDate: InputChangeFunction = useCallback(
+    e => {
+      const {
+        value,
+        dataset: { key },
+      } = e.target;
+
+      if (value === "" || REGEXP.NUMBER.test(value)) {
+        const expirationDate = {
+          ...cardInfo.expirationDate,
+        };
+
+        expirationDate[key] = value;
+
+        cardInfoDispatch({ type: "CHANGE_EXPIRATION_DATE", payload: { expirationDate } });
+        handleChangeValidation(
+          "expirationDate",
+          expirationDate,
+          cardInfoValidator["expirationDate"]
+        );
+      }
+    },
+    [cardInfo.expirationDate]
+  );
 
   const onChangeUserName: InputChangeFunction = useCallback(e => {
     const value = e.target.value;
@@ -166,20 +178,30 @@ function useCardInfoInput() {
     const value = e.target.value;
 
     if (value === "" || REGEXP.NUMBER.test(value)) {
-      cardInfoDispatch({ type: "CHANGE_SECURITY_CODE", payload: { securityCode: value } });
+      const securityCode = value;
+
+      cardInfoDispatch({ type: "CHANGE_SECURITY_CODE", payload: { securityCode } });
+      handleChangeValidation("securityCode", securityCode, cardInfoValidator["securityCode"]);
     }
   }, []);
 
-  const onChangePassword: InputChangeFunction = useCallback(e => {
-    const {
-      value,
-      dataset: { index },
-    } = e.target;
+  const onChangePassword: InputChangeFunction = useCallback(
+    e => {
+      const {
+        value,
+        dataset: { index },
+      } = e.target;
 
-    if (value === "" || REGEXP.NUMBER.test(value)) {
-      cardInfoDispatch({ type: "CHANGE_PASSWORD", payload: { index, value } });
-    }
-  }, []);
+      if (value === "" || REGEXP.NUMBER.test(value)) {
+        const password: Password = [...cardInfo.password];
+
+        password[index] = value;
+        cardInfoDispatch({ type: "CHANGE_PASSWORD", payload: { password } });
+        handleChangeValidation("password", password, cardInfoValidator["password"]);
+      }
+    },
+    [cardInfo.password]
+  );
 
   // Validation
   const handleChangeValidation = <T,>(
@@ -201,30 +223,6 @@ function useCardInfoInput() {
       }));
     }
   };
-
-  useEffect(() => {
-    handleChangeValidation("cardNumbers", cardInfo.cardNumbers, cardInfoValidator["cardNumbers"]);
-  }, [cardInfo.cardNumbers]);
-
-  useEffect(() => {
-    handleChangeValidation(
-      "expirationDate",
-      cardInfo.expirationDate,
-      cardInfoValidator["expirationDate"]
-    );
-  }, [cardInfo.expirationDate]);
-
-  useEffect(() => {
-    handleChangeValidation(
-      "securityCode",
-      cardInfo.securityCode,
-      cardInfoValidator["securityCode"]
-    );
-  }, [cardInfo.securityCode]);
-
-  useEffect(() => {
-    handleChangeValidation("password", cardInfo.password, cardInfoValidator["password"]);
-  }, [cardInfo.password]);
 
   return {
     cardInfo,
