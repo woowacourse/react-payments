9d9a58861c2f6ef9bb0c637e78ab99b89999f7d2
woose28 <dldntmdsla@naver.com>
2022-05-08 16:38:47 +0900

    test: Input 컴포넌트 storybook 스토리 추가

---
+++ src/components/Input/input.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BlockInput, UnderlineInput } from '.';
+
+export default {
+  title: 'Payment/Input',
+};
+
+export const BlockedInputExample = args => <BlockInput {...args} />;
+
+export const UnderlineInputExample = args => <UnderlineInput {...args} />;
