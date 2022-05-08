a7cdfbf72d6db325fce02e21ef934bcfa157eae0
DomMorello <zuzudnf@gmail.com>
2022-05-08 22:07:11 +0900

    feat: 카드 추가하기 컴포넌트에서 뒤로가기 구현

--- src/addCard/components/AddCard.jsx
+++ src/addCard/components/AddCard.jsx
@@ -1,4 +1,5 @@
 import React, { useMemo, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AddCardForm from './AddCardForm';
 import Card from './Card';
 import { getCard } from '../../util';
@@ -7,10 +8,16 @@ import AddCardContext from '../../AddCardContext';
 function AddCard() {
   const [card, setCard] = useState(getCard());
 
+  const navigator = useNavigate();
+
   const updateCard = (name, value) => {
     setCard((prevCard) => {
       return { ...prevCard, [name]: value };
     });
+  };
+
+  const goBack = () => {
+    navigator(-1);
   };
 
   const contextValue = useMemo(() => ({ card, updateCard }), [card]);
@@ -18,7 +25,14 @@ function AddCard() {
   return (
     <AddCardContext.Provider value={contextValue}>
       <div className="header-wrapper">
-        <div className="back-button" />
+        <div
+          className="back-button"
+          onClick={goBack}
+          onKeyDown={() => {}}
+          role="button"
+          tabIndex={0}
+          aria-label="back button"
+        />
         <h2 className="page-title">카드 추가</h2>
       </div>
       <Card />
