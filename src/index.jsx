06df02495a9b8178d2a66b487a540ee4b09ac2b7
intae92 <kangit2013@gmail.com>
2022-05-08 20:12:42 +0900

    chore: react strictmode 활성화

--- src/index.jsx
+++ src/index.jsx
@@ -8,11 +8,11 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <CardProvider>
-      <App />
-    </CardProvider>
-  </BrowserRouter>,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <CardProvider>
+        <App />
+      </CardProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
