c12a6892cea15a4cbe1829886e3a7646de0503af
JUDONGHYEOK <judonghyeok@gmail.com>
2022-05-09 01:59:07 +0900

    feat: localStorage 저장 기능 추가

--- payments/src/hooks/useCardList.js
+++ payments/src/hooks/useCardList.js
@@ -3,8 +3,19 @@ import { useReducer } from "react";
 const controllCardList = (state, action) => {
   switch (action.type) {
     case "addCardList":
+      localStorage.setItem(
+        "CARD_LIST",
+        JSON.stringify([...state, action.payload])
+      );
       return [...state, action.payload];
     case "removeCard":
+      localStorage.setItem(
+        "CARD_LIST",
+        JSON.stringify([
+          ...state.slice(0, action.payload.targetIndex),
+          ...state.slice(action.payload.targetIndex + 1),
+        ])
+      );
       return [
         ...state.slice(0, action.payload.targetIndex),
         ...state.slice(action.payload.targetIndex + 1),
@@ -14,8 +25,10 @@ const controllCardList = (state, action) => {
   }
 };
 
+const initState = JSON.parse(localStorage.getItem("CARD_LIST"));
+
 const useCardList = () => {
-  const [cardList, updateCardList] = useReducer(controllCardList, []);
+  const [cardList, updateCardList] = useReducer(controllCardList, initState);
 
   return { cardList, updateCardList };
 };
