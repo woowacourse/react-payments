7245609ca6edf54cfdcd6632e39d3240c0002817
JUDONGHYEOK <judonghyeok@gmail.com>
2022-05-09 02:04:01 +0900

    refactor: context provider context파일로 이동
    - app.js의 크기를 줄이기 위해 provider로직 context폴더내 파일로 이동

--- payments/src/App.js
+++ payments/src/App.js
@@ -1,27 +1,22 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import useCard from "./hooks/useCard";
-import { CardContext } from "./context/CardProvider";
-import { CardListContext } from "./context/CardListProvider";
+import { CardProvider } from "./context/CardProvider";
+import { CardListProvider } from "./context/CardListProvider";
 import Home from "./pages/Home";
 import CardAddPage from "./pages/CardAddPage";
-import useCardList from "./hooks/useCardList";
 
 const App = () => {
-  const { cardList, updateCardList } = useCardList();
-  const { cardInfo, dispatch, validateCardInfo } = useCard();
-
   return (
-    <CardListContext.Provider value={{ cardList, updateCardList }}>
-      <CardContext.Provider value={{ cardInfo, dispatch, validateCardInfo }}>
+    <CardListProvider>
+      <CardProvider>
         <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/addcard" element={<CardAddPage />} />
           </Routes>
         </BrowserRouter>
-      </CardContext.Provider>
-    </CardListContext.Provider>
+      </CardProvider>
+    </CardListProvider>
   );
 };
 
--- payments/src/context/CardProvider.js
+++ payments/src/context/CardProvider.js
@@ -1,3 +1,14 @@
 import { createContext } from "react";
+import useCard from "../hooks/useCard";
 
 export const CardContext = createContext(null);
+
+export const CardProvider = ({ children }) => {
+  const { cardInfo, dispatch, validateCardInfo } = useCard();
+
+  return (
+    <CardContext.Provider value={{ cardInfo, dispatch, validateCardInfo }}>
+      {children}
+    </CardContext.Provider>
+  );
+};
