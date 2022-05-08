ac61dd7951833b010568432fb51c18b276096512
arnold <arnold@arnoldui-MacBookAir.local>
2022-05-08 21:12:52 +0900

    feat: 라우터 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import CardAddition from './pages/CardAddition';
 import { CardProvider } from './context/CardProvider';
@@ -8,7 +9,9 @@ const App = () => {
   return (
     <div className='App'>
       <CardProvider>
-        <CardAddition />
+        <Routes>
+          <Route path='/add' element={<CardAddition />} />
+        </Routes>
       </CardProvider>
     </div>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import GlobalStyle from './GlobalStyle.css';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
