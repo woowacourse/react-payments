3d1002529f8e18b445b28aa18ed283c1c278aab8
(yunjin) <sion11290805@gmail.com>
2022-05-08 10:32:46 +0900

    fix: 페이지 이동 로직 수정
    훅을 사용하면 상태가 남아있어 window객체 속성으로 변경

--- src/containers/button/EditButtonContainer.tsx
+++ src/containers/button/EditButtonContainer.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import Button from 'components/button/Button';
-import { useNavigate } from 'react-router-dom';
 
 function EditButtonContainer({ id }: { id: string }) {
-  const navigate = useNavigate();
-
   const handleEditButtonClick = (event: any) => {
     const cardId = event.target.id;
 
-    navigate(`/card/edit/${cardId}`);
+    window.location.pathname = `/card/edit/${cardId}`;
   };
 
   return <Button id={id} onClick={handleEditButtonClick} buttonType="edit" />;
