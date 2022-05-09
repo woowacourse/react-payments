72e7839a17c22ec9ee0b8e4c904842b49ef2b3b7
arnold <arnold@arnoldui-MacBookAir.local>
2022-05-09 11:16:49 +0900

    feat: 카드 추가 페이지에서 홈 링크 추가

--- src/system/PageTitle/PrevButton/index.jsx
+++ src/system/PageTitle/PrevButton/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import PrevbuttonStyled from './style';
 
 const PrevButton = ({ hasPrevButton }) => {
-  return hasPrevButton && <span>{'<'}</span>;
+  return hasPrevButton && (
+    <Link to='/'>
+      <PrevbuttonStyled>{'<'}</PrevbuttonStyled>
+    </Link>
+  );
 };
 
 export default PrevButton;
---
+++ src/system/PageTitle/PrevButton/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const PrevbuttonStyled = styled.span`
+  cursor: pointer;
+  
+  &:hover {
+    color: var(--card-chip-background-color);
+  }
+`;
+
+export default PrevbuttonStyled;
