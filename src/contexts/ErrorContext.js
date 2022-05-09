b92009b39de3f6b42e04c92c67476f67481f3d66
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-05-09 16:44:56 +0900

    chore: ErrorModal 컴포넌트 스토리북에 추가

--- src/contexts/ErrorContext.js
+++ src/contexts/ErrorContext.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const errorState = {
-  errorMessage: "",
+  errorMessage: "에러 메시지가 입력되지 않았습니다.",
 };
 
 export const ErrorContext = React.createContext(errorState);
--- src/stories/Modal.stories.js
+++ src/stories/Modal.stories.js
@@ -3,11 +3,22 @@ import React from "react";
 import { Modal } from "components/common/Modal";
 import { CardSelectModal } from "components/cardRegister/CardSelectModal";
 import { CVCHelperModal } from "components/cardRegister/CVCHelperModal";
+import { ErrorModal } from "components/common";
+import { errorState, ErrorContext } from "contexts";
 import { CARD_TYPES } from "constants/constants";
 
 export default {
   title: "Example/Modal",
   component: Modal,
+  decorators: [
+    (Story) => {
+      return (
+        <ErrorContext.Provider value={errorState}>
+          <Story />
+        </ErrorContext.Provider>
+      );
+    },
+  ],
 };
 
 const Template = (args) => <Modal {...args} />;
@@ -27,5 +38,11 @@ CardSelectModalTemplate.args = {
 export const CVCHelperModalTemplate = Template.bind({});
 CVCHelperModalTemplate.args = {
   children: <CVCHelperModal />,
+  visible: true,
+};
+
+export const ErrorModalTemplate = Template.bind({});
+ErrorModalTemplate.args = {
+  children: <ErrorModal />,
   visible: true,
 };
