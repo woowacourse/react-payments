775b829e791d23dfaeb792165031a199460f76da
Ahn <tn3748@naver.com>
2022-05-10 01:38:06 +0900

    feat: cardList stories 생성

---
+++ src/components/cardList/CardList.stories.jsx
@@ -0,0 +1,10 @@
+import CardList from './CardList';
+
+export default {
+  title: 'CardList',
+  component: CardList,
+};
+
+const Template = (args) => <CardList {...args} />;
+
+export const Example = Template.bind({});
