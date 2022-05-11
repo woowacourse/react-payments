15d7260af9078622d3c37a4b21f113b3ff146de6
kimsanglok <evertree6031@gmail.com>
2022-05-12 00:18:09 +0900

    feat: LoadingSpinner 컴포넌트 구현

---
+++ src/components/common/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingSpinner = () => {
+  return (
+    <div className="loading-spinner-wrapper">
+      <div className="loading-spinner" />
+    </div>
+  );
+};
+
+export default LoadingSpinner;
