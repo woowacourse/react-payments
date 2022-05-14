4d2b1cc33526c890d8b49c0b06e1ee1f5bb2bb87
Soyi Jeon <soyi.dev@gmail.com>
2022-05-14 18:48:07 +0900

    fix: Form 포커스 이벤트 KeyUp으로 변경

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -39,7 +39,7 @@ export default function Form({ children, onSubmit }) {
   }, []);
 
   return (
-    <StyledForm ref={formRef} onSubmit={onSubmit} onChange={handleNextInputFocus} onKeyDown={handlePrevInputFocus}>
+    <StyledForm ref={formRef} onSubmit={onSubmit} onKeyUp={handleNextInputFocus} onKeyDown={handlePrevInputFocus}>
       {children}
     </StyledForm>
   );
