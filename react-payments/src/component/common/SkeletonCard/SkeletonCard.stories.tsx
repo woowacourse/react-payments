12da5789d17521a3049ebb6bfc08ea8d5947770f
Hwang Jung Min <hwangstar156@gmail.com>
2022-05-23 02:13:17 +0900

    refactor: SkeletonCard jsx -> tsx 변경

--- react-payments/src/component/common/SkeletonCard/SkeletonCard.stories.tsx
+++ react-payments/src/component/common/SkeletonCard/SkeletonCard.stories.tsx
@@ -1,11 +1,12 @@
 import SkeletonCardBox from "component/common/SkeletonCard/SkeletonCard.component";
+import { Meta, Story } from "@storybook/react";
 
 export default {
   title: "Common/SkeletonCardBox",
   component: SkeletonCardBox,
-};
+} as Meta;
 
-const Template = (args) => <SkeletonCardBox {...args} />;
+const Template: Story = (args) => <SkeletonCardBox {...args} />;
 
 export const DefaultSkeletonCardBox = Template.bind({});
 DefaultSkeletonCardBox.args = {};
