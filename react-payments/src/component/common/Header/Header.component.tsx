d82278d77fdc534cda70a50d68b5451a81689897
Hwang Jung Min <hwangstar156@gmail.com>
2022-05-23 02:10:03 +0900

    refactor: Header jsx -> tsx로 변경

--- react-payments/src/component/common/Header/Header.component.tsx
+++ react-payments/src/component/common/Header/Header.component.tsx
@@ -1,7 +1,7 @@
-import { memo } from "react";
+import React, { memo } from "react";
 import Box from "styles/box";
 
-const Header = memo(({ children }) => {
+const Header = memo(({ children }: { children: React.ReactNode }) => {
   return (
     <Box display="flex" gap="25px" alignItems="center">
       {children}
