565386b5f5f68ce9c8536c03a6f3d79a0a5970ad
woose28 <dldntmdsla@naver.com>
2022-05-25 14:48:04 +0900

    refactor: 각 컴포넌트 디렉터리 내부 style.js 파일 제거
    - style 관련 코드를 각 컴포넌트 파일로 이동

--- src/components/Molecules/Card/index.jsx
+++ src/components/Molecules/Card/index.jsx
@@ -1,14 +1,117 @@
-import {
-  CardContainer,
-  CardCompanyName,
-  IC,
-  CardNumberContainer,
-  CardBottomContainer,
-  CardOwnerName,
-  sizeTable,
-} from './style';
+import styled from 'styled-components';
 import { CARD_SIZE, DEFAULT_TEXT } from 'constant';
 import { CARD_NUMBER_MARK, DATE_SEPARATOR } from 'constant/mark';
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: ${props => props.width};
+  height: ${props => props.height};
+  padding: ${props => props.padding};
+  background-color: ${props => props.color};
+  border-radius: 5px;
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.25);
+  cursor: ${props => (props.canClick ? 'pointer' : 'auto')};
+`;
+
+const CardCompanyName = styled.span`
+  font-weight: 500;
+  font-size: ${props => props.fontSize};
+  line-height: ${props => props.lineHeight};
+  color: #383838;
+`;
+
+const IC = styled.div`
+  width: ${props => props.width};
+  height: ${props => props.height};
+  margin-top: ${props => props.marginTop};
+  background: #cbba64;
+  border-radius: 4px;
+`;
+
+const CardNumberContainer = styled.p`
+  display: flex;
+  justify-content: center;
+  gap: 5px;
+  height: 10px;
+  margin-top: ${props => props.marginTop};
+  font-weight: 700;
+  font-size: ${props => props.fontSize};
+  line-height: ${props => props.lineHeight};
+  letter-spacing: 0.1em;
+  color: #525252;
+`;
+
+const CardBottomContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 4px;
+  margin-top: ${props => props.marginTop};
+  font-weight: 700;
+  font-size: ${props => props.fontSize};
+  line-height: ${props => props.lineHeight};
+  letter-spacing: 0.1em;
+  color: #525252;
+`;
+
+const CardOwnerName = styled.span`
+  word-break: break-all;
+`;
+
+const sizeTable = {
+  medium: {
+    cardContainerSize: {
+      width: '213px',
+      height: '133px',
+      padding: '14px 14px 0',
+    },
+    cardCompanyNameSize: {
+      fontSize: '10px',
+      lineHeight: '12px',
+    },
+    ICSize: {
+      width: '40px',
+      height: '26px',
+      marginTop: '16px',
+    },
+    cardNumberContainerSize: {
+      marginTop: '15px',
+      fontSize: '12px',
+      lineHeight: '13px',
+    },
+    cardBottomContainerSize: {
+      marginTop: '15px',
+      fontSize: '10px',
+      lineHeight: '12px',
+    },
+  },
+  large: {
+    cardContainerSize: {
+      width: '293px',
+      height: '183px',
+      padding: '19px 19px 0',
+    },
+    cardCompanyNameSize: {
+      fontSize: '14px',
+      lineHeight: '16px',
+    },
+    ICSize: {
+      width: '55px',
+      height: '36px',
+      marginTop: '30px',
+    },
+    cardNumberContainerSize: {
+      marginTop: '20px',
+      fontSize: '17px',
+      lineHeight: '18px',
+    },
+    cardBottomContainerSize: {
+      marginTop: '17px',
+      fontSize: '14px',
+      lineHeight: '16px',
+    },
+  },
+};
 
 function Card({
   cardCompany,
--- src/pages/CardListPage/index.jsx
+++ src/pages/CardListPage/index.jsx
@@ -1,16 +1,70 @@
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 import Card from 'components/Molecules/Card';
 import Head from 'components/Templates/Head';
-import {
-  Container,
-  HeadTitle,
-  CardListSection,
-  CardInfoWrapper,
-  CardAlias,
-  CardAddButton,
-} from './style';
 import { useCardListContext } from 'context/cardList';
 import PATH from 'constant/path';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 700px;
+  padding-bottom: 20px;
+`;
+
+const HeadTitle = styled.span`
+  padding-left: 12px;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  color: #383838;
+`;
+
+const CardListSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 26px;
+  height: 100%;
+  margin-top: 65px;
+  overflow-y: scroll;
+`;
+
+const CardInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 16px;
+  opacity: 0.9;
+`;
+
+const CardAlias = styled.span`
+  color: ${props => (props.isNoneAlias ? '#CACACA' : '#575757')};
+`;
+
+const CardAddButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 208px;
+  height: 123px;
+  background: #e5e5e5;
+  border-radius: 5px;
+  font-weight: 400;
+  font-size: 30px;
+  line-height: 35px;
+  text-align: center;
+  color: #575757;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.6;
+  }
+`;
 
 function CardListPage() {
   const navigate = useNavigate();
