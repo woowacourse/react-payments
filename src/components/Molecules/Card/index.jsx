20c381336073ad5bcadbb4bad72679dfc3d92103
woose28 <dldntmdsla@naver.com>
2022-05-25 16:37:29 +0900

    refactor: Card 컴포넌트 크기 조절 방법 수정

--- src/components/Molecules/Card/index.jsx
+++ src/components/Molecules/Card/index.jsx
@@ -2,12 +2,73 @@ import styled from 'styled-components';
 import { CARD_SIZE, DEFAULT_TEXT } from 'constant';
 import { CARD_NUMBER_MARK, DATE_SEPARATOR } from 'constant/mark';
 
+const sizeTable = {
+  cardContainer: {
+    [CARD_SIZE.MEDIUM]: {
+      width: '213px',
+      height: '133px',
+      padding: '14px 14px 0',
+    },
+    [CARD_SIZE.LARGE]: {
+      width: '293px',
+      height: '183px',
+      padding: '19px 19px 0',
+    },
+  },
+  cardCompanyName: {
+    [CARD_SIZE.MEDIUM]: {
+      fontSize: '10px',
+      lineHeight: '12px',
+    },
+    [CARD_SIZE.LARGE]: {
+      fontSize: '14px',
+      lineHeight: '16px',
+    },
+  },
+  IC: {
+    [CARD_SIZE.MEDIUM]: {
+      width: '40px',
+      height: '26px',
+      marginTop: '16px',
+    },
+    [CARD_SIZE.LARGE]: {
+      width: '55px',
+      height: '36px',
+      marginTop: '30px',
+    },
+  },
+  cardNumberContainer: {
+    [CARD_SIZE.MEDIUM]: {
+      marginTop: '15px',
+      fontSize: '12px',
+      lineHeight: '13px',
+    },
+    [CARD_SIZE.LARGE]: {
+      marginTop: '20px',
+      fontSize: '17px',
+      lineHeight: '18px',
+    },
+  },
+  cardBottomContainer: {
+    [CARD_SIZE.MEDIUM]: {
+      marginTop: '15px',
+      fontSize: '10px',
+      lineHeight: '12px',
+    },
+    [CARD_SIZE.LARGE]: {
+      marginTop: '17px',
+      fontSize: '14px',
+      lineHeight: '16px',
+    },
+  },
+};
+
 const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: ${props => props.width};
-  height: ${props => props.height};
-  padding: ${props => props.padding};
+  width: ${({ size }) => sizeTable.cardContainer[size].width};
+  height: ${({ size }) => sizeTable.cardContainer[size].height};
+  padding: ${({ size }) => sizeTable.cardContainer[size].padding};
   background-color: ${props => props.color};
   border-radius: 5px;
   box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.25);
@@ -16,15 +77,15 @@ const CardContainer = styled.div`
 
 const CardCompanyName = styled.span`
   font-weight: 500;
-  font-size: ${props => props.fontSize};
-  line-height: ${props => props.lineHeight};
+  font-size: ${({ size }) => sizeTable.cardCompanyName[size].fontSize};
+  line-height: ${({ size }) => sizeTable.cardCompanyName[size].lineHeight};
   color: #383838;
 `;
 
 const IC = styled.div`
-  width: ${props => props.width};
-  height: ${props => props.height};
-  margin-top: ${props => props.marginTop};
+  width: ${({ size }) => sizeTable.IC[size].width};
+  height: ${({ size }) => sizeTable.IC[size].height};
+  margin-top: ${({ size }) => sizeTable.IC[size].marginTop};
   background: #cbba64;
   border-radius: 4px;
 `;
@@ -34,10 +95,10 @@ const CardNumberContainer = styled.p`
   justify-content: center;
   gap: 5px;
   height: 10px;
-  margin-top: ${props => props.marginTop};
+  margin-top: ${({ size }) => sizeTable.cardNumberContainer[size].marginTop};
   font-weight: 700;
-  font-size: ${props => props.fontSize};
-  line-height: ${props => props.lineHeight};
+  font-size: ${({ size }) => sizeTable.cardNumberContainer[size].fontSize};
+  line-height: ${({ size }) => sizeTable.cardNumberContainer[size].lineHeight};
   letter-spacing: 0.1em;
   color: #525252;
 `;
@@ -46,10 +107,10 @@ const CardBottomContainer = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 0 4px;
-  margin-top: ${props => props.marginTop};
+  margin-top: ${({ size }) => sizeTable.cardBottomContainer[size].marginTop};
   font-weight: 700;
-  font-size: ${props => props.fontSize};
-  line-height: ${props => props.lineHeight};
+  font-size: ${({ size }) => sizeTable.cardBottomContainer[size].fontSize};
+  line-height: ${({ size }) => sizeTable.cardBottomContainer[size].lineHeight};
   letter-spacing: 0.1em;
   color: #525252;
 `;
@@ -57,61 +118,6 @@ const CardBottomContainer = styled.div`
 const CardOwnerName = styled.span`
   word-break: break-all;
 `;
-
-const sizeTable = {
-  medium: {
-    cardContainerSize: {
-      width: '213px',
-      height: '133px',
-      padding: '14px 14px 0',
-    },
-    cardCompanyNameSize: {
-      fontSize: '10px',
-      lineHeight: '12px',
-    },
-    ICSize: {
-      width: '40px',
-      height: '26px',
-      marginTop: '16px',
-    },
-    cardNumberContainerSize: {
-      marginTop: '15px',
-      fontSize: '12px',
-      lineHeight: '13px',
-    },
-    cardBottomContainerSize: {
-      marginTop: '15px',
-      fontSize: '10px',
-      lineHeight: '12px',
-    },
-  },
-  large: {
-    cardContainerSize: {
-      width: '293px',
-      height: '183px',
-      padding: '19px 19px 0',
-    },
-    cardCompanyNameSize: {
-      fontSize: '14px',
-      lineHeight: '16px',
-    },
-    ICSize: {
-      width: '55px',
-      height: '36px',
-      marginTop: '30px',
-    },
-    cardNumberContainerSize: {
-      marginTop: '20px',
-      fontSize: '17px',
-      lineHeight: '18px',
-    },
-    cardBottomContainerSize: {
-      marginTop: '17px',
-      fontSize: '14px',
-      lineHeight: '16px',
-    },
-  },
-};
 
 function Card({
   cardCompany,
@@ -122,14 +128,6 @@ function Card({
   size = CARD_SIZE.MEDIUM,
 }) {
   const { name, color } = cardCompany;
-  const {
-    cardContainerSize,
-    cardCompanyNameSize,
-    ICSize,
-    cardNumberContainerSize,
-    cardBottomContainerSize,
-  } = sizeTable[size];
-
   const displayExpiredDate = () => {
     return expiredDate.month || expiredDate.year
       ? `${expiredDate.month}${expiredDate.month.length === 2 ? DATE_SEPARATOR : ''}${
@@ -141,20 +139,20 @@ function Card({
   return (
     <CardContainer
       color={color}
+      size={size}
       onClick={handleClickCard}
       canClick={handleClickCard !== undefined}
-      {...cardContainerSize}
     >
-      <CardCompanyName {...cardCompanyNameSize}>{name}</CardCompanyName>
-      <IC {...ICSize} />
-      <CardNumberContainer {...cardNumberContainerSize}>
+      <CardCompanyName size={size}>{name}</CardCompanyName>
+      <IC size={size} />
+      <CardNumberContainer size={size}>
         {cardNumbers.map((cardNumber, index) => (
           <span key={cardNumber + index}>
             {index >= 2 ? CARD_NUMBER_MARK.repeat(cardNumber.length) : cardNumber}
           </span>
         ))}
       </CardNumberContainer>
-      <CardBottomContainer {...cardBottomContainerSize}>
+      <CardBottomContainer size={size}>
         <CardOwnerName>{ownerName || DEFAULT_TEXT.NAME}</CardOwnerName>
         <span>{displayExpiredDate()}</span>
       </CardBottomContainer>
