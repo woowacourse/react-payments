b2505aaea2c227692d6488029183d42db217498d
Yo Wook Kim <uk960214@gmail.com>
2022-05-25 20:17:19 +0900

    chore: 스토리북 오류 해결 및 설정 추가
    https://stackoverflow.com/a/65772747
    https://storybook.js.org/docs/react/configure/typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,32 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules.push(path.resolve(__dirname, "../src"));
+
+    return config;
+  },
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+      allowSyntheticDefaultImports: false,
+      esModuleInterop: false,
+    },
+  },
+};
