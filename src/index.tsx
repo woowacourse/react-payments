656adab975fbffd4080377d88ca50b75411274d3
Marco <bigdatajang@gmail.com>
2022-05-25 21:55:33 +0900

    refactor: typescript 설치 및 환경설정
    타입스크립트 적용 없이 몇 js 파일의 확장자만 ts로 바꿔봄

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from 'App';
-import * as S from 'globalStyles/index.js';
+import * as S from 'globalStyles/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
