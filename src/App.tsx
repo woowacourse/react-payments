83f645341cbfebd7f1644a1ea3af8e17025cd2e7
(yunjin) <sion11290805@gmail.com>
2022-05-25 16:17:01 +0900

    feat: 예외 url 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import AppProvider from 'context/Provider';
-import AddCardPage from 'pages/AddCardPage';
 import { Route, Routes } from 'react-router-dom';
+import { Global } from '@emotion/react';
+
+import AppProvider from 'context/Provider';
+
+import AddCardPage from 'pages/AddCardPage';
 import CardListPage from 'pages/CardListPage';
 import CardEditPage from 'pages/CardEditPage';
+import WrongPathPage from 'pages/WrongPathPage';
+
+import { initial } from 'styles/global';
 
 function App() {
   return (
     <AppProvider>
+      <Global styles={initial} />
       <Routes>
-        <Route path="/" element={<CardListPage />} />
+        <Route path="/card" element={<CardListPage />} />
         <Route path="card/add" element={<AddCardPage />} />
         <Route path="card/edit/*" element={<CardEditPage />} />
+        <Route path="*" element={<WrongPathPage />} />
       </Routes>
     </AppProvider>
   );
---
+++ src/pages/WrongPathPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import PageTitle from 'components/navigater/PageTitle';
+
+import { PageWrapper } from './stlye';
+
+function WrongPathPage() {
+  return (
+    <PageWrapper>
+      <PageTitle>잘못된 접근입니다.</PageTitle>
+      <Link to="/card">홈페이지로 이동하기</Link>
+    </PageWrapper>
+  );
+}
+
+export default WrongPathPage;
