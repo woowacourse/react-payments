31a8c25fd72b6aac5d1dd3840079bf33764650ea
(yunjin) <sion11290805@gmail.com>
2022-05-25 19:40:43 +0900

    rename: Margin 컴포넌트 제거

--- src/components/margin/index.tsx
+++
@@ -1,5 +0,0 @@
-import React, { ClassAttributes, HTMLAttributes } from 'react';
-
-export const MarginWrapper = (
-  props: ClassAttributes<HTMLDivElement> & HTMLAttributes<HTMLDivElement>,
-) => <div {...props}></div>;
