d3f644ba33816adf38d3e0ddaf8262d2c4f9c9c5
DomMorello <zuzudnf@gmail.com>
2022-05-26 13:03:51 +0900

    fix: CardList 스토리북 args 적절하게 수정

--- src/cardList/storybook/CardList.stories.jsx
+++ src/cardList/storybook/CardList.stories.jsx
@@ -21,9 +21,6 @@ export const SavedCardList = Template.bind({});
 
 SavedCardList.args = {
   cardList: [
-    {
-      card: Card('1324', '1234', '1234', '1234', '3', '23', 'dom', '123', '1', '2'),
-      nickName: 'dom',
-    },
+    { ...Card('1324', '1234', '1234', '1234', '3', '23', 'dom', '123', '1', '2'), nickName: 'dom' },
   ],
 };
