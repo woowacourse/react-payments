c3f55e91221e9ac9b9e474f9aa03e5e23ea3a09a
2019015414 <2019015414@hanyang.ac.kr>
2022-05-26 22:11:12 +0900

    refactor: constants ts로 변경

--- react-payments/src/constants/index.ts
+++ react-payments/src/constants/index.ts
@@ -31,12 +31,12 @@ export const cardInfos = [
     cardName: "썬 카드",
     cardType: "sunCard",
   },
-];
+] as const;
 
 export const defaultCardInfo = {
   cardName: "",
   cardType: "defaultCard",
-};
+} as const;
 
 export const MAX_LENGTH = {
   CARD_NUMBER: 4,
@@ -45,13 +45,13 @@ export const MAX_LENGTH = {
   SECURITY_CODE: 3,
   USER_NAME: 30,
   CARD_NAME: 15,
-};
+} as const;
 
-export const MIDDLE_CARD_NUMBER_LENGTH = 2;
+export const MIDDLE_CARD_NUMBER_LENGTH = 2 as const;
 
-export const MAX_MONTH = 12;
+export const MAX_MONTH = 12 as const;
 
-export const HELP_MESSAGE = "카드 뒷면의 3자리 숫자를 입력해주세요.";
+export const HELP_MESSAGE = "카드 뒷면의 3자리 숫자를 입력해주세요." as const;
 
 export const ERROR_MESSAGE = {
   CARD_NUMBER: "빈칸없이 입력을 완료해주세요.",
@@ -60,10 +60,10 @@ export const ERROR_MESSAGE = {
   CARD_PASSWORD: "비밀번호 앞 2자리를 입력해주세요.",
   CARD_NAME_LENGTH: `1 ~ ${MAX_LENGTH.CARD_NAME}자 이하의 이름을 입력해주세요.`,
   DUPLICATE_CARD_NAME: "이미 존재하는 카드이름입니다.",
-};
+} as const;
 
-export const SUCCESS_MESSAGE = "성공적으로 입력됐습니다.";
+export const SUCCESS_MESSAGE = "성공적으로 입력됐습니다." as const;
 
 export const REDUCER_TYPE = {
   CREATE: "CREATE",
-};
+} as const;
