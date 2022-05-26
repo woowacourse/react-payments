6bcdd89ff5430f09c91772dc98394a26a6bb1637
Marco <bigdatajang@gmail.com>
2022-05-26 21:52:49 +0900

    refactor: Container 컴포넌트 타입스트립트로 마이그레이션

--- src/common/Container/Container.tsx
+++ src/common/Container/Container.tsx
@@ -1,6 +1,11 @@
+import { ReactNode } from 'react';
 import styled from 'styled-components';
 
-export default function Container({ children }) {
+interface Props {
+  children: ReactNode;
+}
+
+export default function Container({ children }: Props) {
   return <Styled.Container>{children}</Styled.Container>;
 }
 
