703c607097ae512e0e1d28f2992386f7ab8d16f5
(yunjin) <sion11290805@gmail.com>
2022-05-27 15:45:20 +0900

    remove: helptip scss 삭제

--- src/fields/CVCFieldset.tsx
+++ src/fields/CVCFieldset.tsx
@@ -13,12 +13,6 @@ function CVCFieldset() {
       </FieldHead>
       <FieldBody style={{ marginBottom: '20px' }}>
         <CVCInputContainer />
-        <div className="helptip">
-          <div className="helptip-content">
-            CVC(Card Validation Code)는 Visa, MasterCard, Discover 카드의 뒷면과 아메리칸 익스프레스
-            카드의 앞면에 있는 3자리 또는 4자리 코드이다.
-          </div>
-        </div>
       </FieldBody>
     </FieldSet>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from 'App';
 import 'index.css';
-import 'scss/helptip.scss';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
