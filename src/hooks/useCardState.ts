93a93335dc6f24c1f3edf4daf90c8676fab70794
intae92 <kangit2013@gmail.com>
2022-05-28 21:22:37 +0900

    refactor: hooks 파일들 tsx로 마이그레이션

--- src/hooks/useCardState.ts
+++ src/hooks/useCardState.ts
@@ -1,8 +1,8 @@
 import { useContext } from 'react';
-import { UserState } from '../contexts';
+import { UserContext } from '../contexts';
 
 function useCardState() {
-  const state = useContext(UserState);
+  const { state } = useContext(UserContext);
   if (!state) throw new Error('Cannot find UserState');
   return state;
 }
