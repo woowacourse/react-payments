475ca23389f1f0b7cbe4066d64f4a0a1f98f15fb
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-05-29 02:37:01 +0900

    feat: add loading component

--- src/page/MainPage/index.jsx
+++ src/page/MainPage/index.jsx
@@ -7,18 +7,21 @@ import { CardPreview } from 'components';
 import styles from './index.module.css';
 import { ROUTE } from 'constants';
 import classNames from 'classnames/bind';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 import { ACTION } from 'actions/card';
+import Loading from 'components/@common/Loading';
 
 const cx = classNames.bind(styles);
 
 const MainPage = () => {
   const { state, dispatch } = useCardContext();
+  const [isLoading, setIsLoading] = useState(true);
 
   const getCards = useCallback(async () => {
     const response = await axios.get('/cards');
 
+    setIsLoading(false);
     dispatch({ type: ACTION.INITIALIZE, cards: response.data });
   }, [dispatch]);
 
@@ -28,25 +31,29 @@ const MainPage = () => {
 
   return (
     <div className={cx('container')}>
-      <div className={cx('card-list')}>
-        <>
-          {state.cards.map(({ id, theme, company, number, expiryDate, ownerName, alias }) => (
-            <div key={id}>
-              <CardPreview
-                theme={theme}
-                company={company}
-                number={number}
-                expiryDate={expiryDate}
-                ownerName={ownerName}
-              />
-              <p className={cx('alias')}>{alias ?? company}</p>
-            </div>
-          ))}
-        </>
-        <Link to={ROUTE.ADD}>
-          <CardAddIcon className={cx('add-icon')} />
-        </Link>
-      </div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className={cx('card-list')}>
+          <>
+            {state.cards.map(({ id, theme, company, number, expiryDate, ownerName, alias }) => (
+              <div key={id}>
+                <CardPreview
+                  theme={theme}
+                  company={company}
+                  number={number}
+                  expiryDate={expiryDate}
+                  ownerName={ownerName}
+                />
+                <p className={cx('alias')}>{alias ?? company}</p>
+              </div>
+            ))}
+          </>
+          <Link to={ROUTE.ADD}>
+            <CardAddIcon className={cx('add-icon')} />
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
