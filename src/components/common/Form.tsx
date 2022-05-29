88564bcbdf7dad5ea0c3e385110154e395f9e682
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-05-30 01:41:23 +0900

    refactor: Form 컴포넌트 타입스크립트로 마이그레이션

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-export const Form = ({ onSubmit, children }) => {
+interface FormComponent {
+  onSubmit: () => void;
+  children: React.ReactNode;
+}
+
+export const Form = ({ onSubmit, children }: FormComponent) => {
   return <FormStyle onSubmit={onSubmit}>{children}</FormStyle>;
 };
 
