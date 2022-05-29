761a5e86e7ccc4d0622dfaa5a2d34bae140830a5
woose28 <dldntmdsla@naver.com>
2022-05-30 08:55:53 +0900

    refactor: InputLabel 컴포넌트 ts 적용

--- src/components/Atoms/InputLabel/index.tsx
+++ src/components/Atoms/InputLabel/index.tsx
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
 
+interface InputLabelProps {
+  label: string;
+}
+
 const InputLabelText = styled.span`
   font-weight: 500;
   font-size: 12px;
   line-height: 14px;
   letter-spacing: -0.085em;
-  color: #525252;
+  color: ${({ theme }) => theme.PALETTE.GRAY_004};
 `;
 
-function InputLabel({ label }) {
+function InputLabel({ label }: InputLabelProps) {
   return <InputLabelText>{label}</InputLabelText>;
 }
 
