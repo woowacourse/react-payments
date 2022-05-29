217717ac25e7a49e8d4c64c986b3e0395bd393bc
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-29 20:41:01 +0900

    🏷️ : ToolTip 컴포넌트 타입스크립트 전환

--- src/components/@common/ToolTip/index.tsx
+++ src/components/@common/ToolTip/index.tsx
@@ -1,8 +1,14 @@
-import PropTypes from 'prop-types';
-
+import * as React from 'react';
 import Container from './styles';
 
-function ToolTip({ text, align, isDisabled, children }) {
+interface PropTypes {
+  text?: string;
+  isDisabled?: boolean;
+  align: 'top' | 'bottom' | 'left' | 'right';
+  children: React.ReactNode;
+}
+
+function ToolTip({ text, align, isDisabled, children }: PropTypes) {
   return (
     <Container align={align} text={text} isDisabled={isDisabled}>
       <div className="tool-tip-text">{text}</div>
@@ -14,12 +20,6 @@ function ToolTip({ text, align, isDisabled, children }) {
 ToolTip.defaultProps = {
   text: '팁 내용이 비어있습니다.',
   isDisabled: false,
-};
-
-ToolTip.propTypes = {
-  text: PropTypes.string,
-  isDisabled: PropTypes.bool,
-  align: PropTypes.string.isRequired,
 };
 
 export default ToolTip;
