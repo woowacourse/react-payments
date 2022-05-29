6f5140143c60a9273cb4c432a4669f9f70e37b08
prefer2 <sunho6200@gmail.com>
2022-05-29 15:43:33 +0900

    refactor: theme typescript로 마이그레이션

---
+++ src/theme/index.ts
@@ -0,0 +1,15 @@
+const colors = {
+  MINT: '#04C09E',
+  GRAY: '#D2D2D2',
+  LIGHT_GRAY: '#ecebf1',
+  DARK_GRAY: '#525252',
+  PINK: '#E36DB0',
+  LIGHT_PINK: '#fcdfef',
+  WHITE: '#FFFFFF',
+  YELLOW: '#cbba64',
+}
+const theme = {
+  colors,
+}
+
+export default theme
