e989334540fe16de8258b46b0f73b70f637cd4a5
airman5573 <rpf5573@kakao.com>
2022-05-29 20:25:07 +0900

    refactor(component): 가독성을 높인다
    3항 연산자 대신 early return을 사용한다

--- src/pages/card-register/components/card-form/card-password/CardPasswordInput.tsx
+++ src/pages/card-register/components/card-form/card-password/CardPasswordInput.tsx
@@ -12,21 +12,19 @@ function CardPasswordInput(props: Props) {
   const { position, disabled } = props;
   const { register } = useFormContext();
 
+  if (disabled) {
+    return <PasswordInput disabled placeholder="•" />;
+  }
+
   return (
-    <div>
-      {disabled ? (
-        <PasswordInput disabled placeholder="•" />
-      ) : (
-        <PositiveNumberInput
-          css={passwordInputStyle}
-          {...register(`password-${position}`, {
-            required: { value: true },
-            pattern: { value: '[0-9]', message: '비밀번호를 입력해 주세요' },
-            maxLength: { value: 1 },
-          })}
-        />
-      )}
-    </div>
+    <PositiveNumberInput
+      css={passwordInputStyle}
+      {...register(`password-${position}`, {
+        required: { value: true },
+        pattern: { value: '[0-9]', message: '비밀번호를 입력해 주세요' },
+        maxLength: { value: 1 },
+      })}
+    />
   );
 }
 CardPasswordInput.displayName = 'CardPasswordInput';
