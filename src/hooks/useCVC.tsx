ff6a8a4e0c6bb4ba035ae8e0a49c9412acfff103
sangminsong <ssmin940606@gmail.com>
2022-05-29 15:57:57 +0900

    refactor: type 적용
    - cvc 관련 컴포넌트 및 hook
    - 빈 카드 컴포넌트
    - svg import 하기 위해 custom type 설정

--- src/hooks/useCVC.tsx
+++ src/hooks/useCVC.tsx
@@ -1,11 +1,11 @@
-import { useCallback, useState } from 'react';
+import { ChangeEvent, FocusEvent, useCallback, useState } from 'react';
 
-import { ERROR_MESSAGE } from 'constants';
+import { ERROR_MESSAGE } from 'constants/index';
 
-export default function useCVC(initialValue) {
+export default function useCVC(initialValue: string) {
   const [CVC, setCVC] = useState(initialValue);
 
-  const handleCVC = useCallback(({ target }) => {
+  const handleCVC = useCallback(({ target }: ChangeEvent<HTMLInputElement>) => {
     // 정규식: 숫자로 구성된 문자열이 아니면
     if (!/^$|^[0-9]{0,3}$/.test(target.value)) return;
 
@@ -14,7 +14,7 @@ export default function useCVC(initialValue) {
     setCVC(target.value);
   }, []);
 
-  const showCVCValidation = ({ target }) => {
+  const showCVCValidation = ({ target }: FocusEvent<HTMLInputElement>) => {
     if (target.validity.tooShort) {
       target.setCustomValidity(ERROR_MESSAGE.INVALID_CVC);
       target.reportValidity();
