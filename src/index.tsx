874f4159da9edc4ea0412ecd888f68970d4ec646
Daye Lee <508yeah@gmail.com>
2022-05-29 20:39:22 +0900

    refactor: 기본 파일을 typescript로 마이그레이션

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import { CardContextProvider } from '@/contexts';
 import App from '@/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <CardContextProvider>
