eab5c01572be0a711287caa3d273d0b570c71ca1
woose28 <dldntmdsla@naver.com>
2022-05-30 04:37:21 +0900

    refactor: Button 컴포넌트 확장자 tsx로 변경

--- src/components/Atoms/Button/Button.stories.tsx
+++ src/components/Atoms/Button/Button.stories.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import Button from '.';
+import Button from './index';
+
+interface Args {
+  hidden: boolean;
+}
 
 export default {
   title: 'Atoms/Button',
   component: Button,
 };
 
-export const Default = args => {
+export const Default = (args: Args) => {
   return (
     <Button type="button" {...args}>
       다음
