04724d4230b5bda39aeed5f666d97a5dbfb55af5
woose28 <dldntmdsla@naver.com>
2022-05-30 10:34:48 +0900

    refactor: Button 컴포넌트 스토리북 argTypes 수정

--- src/components/Atoms/Button/Button.stories.tsx
+++ src/components/Atoms/Button/Button.stories.tsx
@@ -1,15 +1,37 @@
 import Button from './index';
 
-interface Args {
+interface ButtonArgs {
   hidden: boolean;
 }
 
 export default {
   title: 'Atoms/Button',
   component: Button,
+  argTypes: {
+    ref: {
+      table: {
+        disable: true,
+      },
+    },
+    theme: {
+      table: {
+        disable: true,
+      },
+    },
+    as: {
+      table: {
+        disable: true,
+      },
+    },
+    forwardedAs: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 };
 
-export const Default = (args: Args) => {
+export const Default = (args: ButtonArgs) => {
   return (
     <Button type="button" {...args}>
       다음
