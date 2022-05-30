94ef38dbfc64c750794360e42f003974ac081f00
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-05-30 22:12:46 +0900

    [3단계 - 페이먼츠] 해리(엄지혁) 미션 제출합니다. (#150)
    * chore: CRA 프로젝트 설정
    
    * chore: eslint, prettier 설정
    
    * docs: 기능 요구 사항 작성
    
    * design: 전체 css 코드 작성
    
    * feat: Input 컴포넌트 구현
    
    * feat: Card 컴포넌트 구현
    
    * feat: 카드번호를 입력받을 수 있다.
    
    * feat: inputForm 컴포넌트 개발
    
    Co-authored-by: juunzzi <juunzzi@users.noreply.github.com>
    
    * feat: 모든 input이 글자수에 맞게 입력되어야 제출되도록 구현
    
    * chore: 폴더 구조 변경
    
    Co-authored-by: juunzzi <juunzzi@users.noreply.github.com>
    
    * refactor: map 고차함수를 활용하여 리엑트 엘리먼트 렌더링
    
    * refactor: validation 함수 분리
    
    * refactor: 카드 소유자 이름 검증 로직 수정
    
    * refactor: input-box element 하위로 input들이 담기도록 수정
    
    * test: Card, Input 에 대한 storybook 테스트 진행
    
    Co-authored-by: juunzzi <juunzzi@users.noreply.github.com>
    
    * refactor: App에서 하던 변환역할을 Card에게 옮겨준다.
    
    * refactor: useState -> useReducer로 migration
    
    Co-authored-by: juunzzi <juunzzi@users.noreply.github.com>
    
    * design: input css 수정
    
    * feat: 카드에 카드번호가 *로 렌더링되도록 수정
    
    * design: 소유자 이름이 길어지면 두 줄로 카드에 렌더링
    
    * feat: 모든 input이 완전히 입력되었을때만 '다음'버튼이 렌더링되도록 수정
    
    * feat: CVC input 옆에 help content 렌더링
    
    * feat: 검증로직을 수정
    
    * feat: input이 모두 입력되면 다음 input이 focus되도록 구현
    
    * refactor: 가독성을 위해 선언문 위치 수정
    
    * chore: html lang, title 수정
    
    * refactor: objectToString join 수정
    
    * refactor: dispatch type 상수화
    
    * refactor: HelpContent 컴포넌트 추가
    
    * refactor: Ref 배열에서 객체로 변경
    
    * refactor: objectToString 함수명 변경
    
    * docs: STEP2 기능 구현 요구사항 작성
    
    * feat: 카드추가 페이지의 `다음`버튼을 눌러 카드추가완료 페이지로 이동할 수 있다.
    
    * feat: 카드추가완료 화면 구현, 카드 명칭 등록 구현
    
    * feat: 카드등록, 페이지 이동 구현
    
    * docs: STEP2 추가 적용할 사항 작성
    
    * refactor: 페이지별 컴포넌트, CardList 컴포넌트 재사용성 높임
    
    * feat: input관련 컴포넌트 범용성 향상, form관련 컴포넌트에 모든 역할 위임
    
    * feat: 이미 추가된 카드번호의 카드를 추가할 수 없다.
    
    * refactor: 시맨틱 태그, AddCardPage 컴포넌트 작성
    
    * chore: 폴더명 변경 page -> pages
    
    * refactor: `button`관련 컴포넌트를 `styled-components`로 변경
    
    * refactor: `helpContent` -> styled-components 로 변경
    
    * refactor: page context 활용
    
    * test: `AddCardPage`의 상호작용 테스트 작성
    
    * refactor: `input`의 수정을 담당하는 custom Hook 사용
    
    * chore: storybook 배포 준비
    
    * test: storybook 수정
    
    * test: 반영 안 된 테스트 수정
    
    * refactor: 잘못저장된 파일 삭제, 매직넘버 상수화
    
    * chore: 잘못된 import 수정
    
    * feat: cardInfo 타입스크립트로 변경
    
    Co-authored-by: juunzzi <juunzzi@users.noreply.github.com>

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,12 +1,6 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.PAGE =
-  exports.MAX_LENGTH =
-  exports.CARD_SIZE =
-  exports.DEFAULT_ROUTE_INFO =
-  exports.DEFAULT_CARD_INFO =
-    void 0;
-exports.DEFAULT_CARD_INFO = {
+import { cardInfo } from '../types/cardInfo';
+
+export const DEFAULT_CARD_INFO: cardInfo = {
   cardNumber: {
     firstColumn: '',
     secondColumn: '',
@@ -25,16 +19,19 @@ exports.DEFAULT_CARD_INFO = {
   },
   cardNickName: '',
 };
-exports.DEFAULT_ROUTE_INFO = {
+
+export const DEFAULT_ROUTE_INFO = {
   addCard: 'app',
   completeAddCard: 'app hide',
   cardList: 'app hide',
 };
-exports.CARD_SIZE = {
+
+export const CARD_SIZE = {
   SMALL: 'small',
   BIG: 'big',
 };
-exports.MAX_LENGTH = {
+
+export const MAX_LENGTH = {
   CARD_NUMBER: 4,
   CARD_EXPIRATION_DATE: 2,
   CARD_OWNER_NAME: 30,
@@ -42,7 +39,8 @@ exports.MAX_LENGTH = {
   CARD_PASSWORD: 1,
   CARD_NICK_NAME: 15,
 };
-exports.PAGE = {
+
+export const PAGE = {
   ADD_CARD: 'addCardPage',
   COMPLETE_ADD_CARD: 'completeAddCardPage',
   CARD_LIST: 'cardListPage',
