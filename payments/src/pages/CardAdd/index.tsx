c38c9e8a1ee420b16f92918688827e2cddcec069
Yunho <ansghkdbsgh@gmail.com>
2022-05-30 16:28:33 +0900

    refactor: pages to tsx

---
+++ payments/src/pages/CardAdd/index.tsx
@@ -0,0 +1,57 @@
+import './index.scss';
+
+import Card from '../../components/Card';
+import CardNumberInput from '../../components/CardNumberInput';
+import CardPasswordInput from '../../components/CardPasswordInput';
+import ExpiredDateInput from '../../components/ExpiredDateInput';
+import OwnerNameInput from '../../components/OwnerNameInput';
+import SecureCodeInput from '../../components/SecureCodeInput';
+import NextButton from '../../components/NextButton';
+import CardColorPicker from '../../components/CardColorPicker';
+import { useContext, useState } from 'react';
+import useNextButton from '../../hooks/useNextButton';
+import { UserContext } from '../../context/userContext';
+import { Link } from 'react-router-dom';
+
+const CardAdd = () => {
+  const { inputStates, updateInputStates } = useContext(UserContext);
+  const [visible, setVisible] = useState(false);
+  const { nextButtonClick } = useNextButton(inputStates, setVisible);
+
+  const { cardNumber, expiredDate, ownerName, secureCode, password } = inputStates;
+
+  const onClickSubmit = (e) => {
+    try {
+      nextButtonClick(e);
+    } catch (error) {
+      alert(error.message);
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <>
+      <div className='card-add'>
+        <header>
+          <Link to='/'>
+            <button>{'<'}</button>
+          </Link>
+          <p>카드 추가</p>
+        </header>
+        <form className='card-add__form'>
+          <Card state={inputStates} setVisible={setVisible} needBack={true} />
+          <CardNumberInput state={cardNumber} updateForm={updateInputStates} />
+          <ExpiredDateInput state={expiredDate} updateForm={updateInputStates} />
+          <OwnerNameInput state={ownerName} updateForm={updateInputStates} />
+          <SecureCodeInput state={secureCode} updateForm={updateInputStates} />
+          <CardPasswordInput state={password} updateForm={updateInputStates} />
+          <Link to={'/enterNickname'} onClick={onClickSubmit}>
+            <NextButton />
+          </Link>
+        </form>
+      </div>
+      <CardColorPicker visible={visible} setVisible={setVisible} updateForm={updateInputStates} />
+    </>
+  );
+};
+export default CardAdd;
