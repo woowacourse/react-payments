6dd38abbf268ab8d5c4e998f91e2df828fcedaad
Seungchan On <62434898+cks3066@users.noreply.github.com>
2022-06-01 21:19:17 +0900

    refactor: 코드 분할 적용 (#168)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AddCard from "./pages/AddCard";
-import CardList from "./pages/CardList";
-import ConfirmAddCard from "./pages/ConfirmAddCard";
-import ConfirmEditCard from "./pages/ConfirmEditCard";
-import EditCard from "./pages/EditCard";
+import Loader from "./common/Loading";
+
+const AddCard = lazy(() => import("./pages/AddCard"));
+const CardList = lazy(() => import("./pages/CardList"));
+const ConfirmAddCard = lazy(() => import("./pages/ConfirmAddCard"));
+const ConfirmEditCard = lazy(() => import("./pages/ConfirmEditCard"));
+const EditCard = lazy(() => import("./pages/EditCard"));
 
 function App() {
   return (
     <div className="App">
       <Router basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path="/" element={<CardList />} />
-          <Route path="/addCard" element={<AddCard />} />
-          <Route path="/editCard/:id" element={<EditCard />} />
-          <Route path="/confirmAddCard" element={<ConfirmAddCard />} />
-          <Route path="/confirmEditCard/:id" element={<ConfirmEditCard />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<CardList />} />
+            <Route path="/addCard" element={<AddCard />} />
+            <Route path="/editCard/:id" element={<EditCard />} />
+            <Route path="/confirmAddCard" element={<ConfirmAddCard />} />
+            <Route path="/confirmEditCard/:id" element={<ConfirmEditCard />} />
+          </Routes>
+        </Suspense>
       </Router>
     </div>
   );
 }
-
 export default App;
---
+++ src/common/Loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div className="spinner">
+      <div />
+    </div>
+  );
+}
+
+export default Loader;
