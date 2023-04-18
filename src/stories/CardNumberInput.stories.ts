92442815da00bd3c9188c93d63fbb289c49671b3
hae-on <solbi2004@naver.com>
2023-04-18 22:05:35 +0900

    test: input storybook test

---
+++ src/stories/CardNumberInput.stories.ts
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import CardNumberInput from '../components/inputs/CardNumberInput';
+
+const meta = {
+  title: 'Payment/CardNumberInput',
+  component: CardNumberInput,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof CardNumberInput>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
