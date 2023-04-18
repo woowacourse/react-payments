03f537f5e7b00283e1303d9ba8337f89b61ec428
hafnium1923 <hafnium1923@gmail.com>
2023-04-18 20:39:58 +0900

    feat: CardOwnerNameInput 컴포넌트 UI 구현
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

---
+++ src/component/CardOwnerNameInput/index.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import St from "./styled";
+
+function CardOwnerNameInput() {
+  return (
+    <>
+      <St.Title>
+        <div>카드 소유자 이름 (선택)</div>
+        <div>3/30</div>
+      </St.Title>
+      <St.InputSection>
+        <St.Input
+          type="text"
+          maxLength={30}
+          placeholder="카드에 표시된 이름과 동일하게 입력하세요."
+        ></St.Input>
+      </St.InputSection>
+    </>
+  );
+}
+
+export default CardOwnerNameInput;
---
+++ src/component/CardOwnerNameInput/styled.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+const St = {
+  InputSection: styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+
+    width: 100%;
+    height: 7vh;
+
+    border-radius: 7px;
+
+    background: #ecebf1;
+  `,
+
+  Input: styled.input`
+    width: 80%;
+
+    border: none;
+
+    text-align: center;
+    background: inherit;
+    font-size: 20px;
+  `,
+
+  Title: styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding-bottom: 10px;
+
+    align-items: center;
+
+    font: 500 14px/16px "Roboto";
+    letter-spacing: -0.085em;
+    color: #525252;
+  `,
+};
+
+export default St;
