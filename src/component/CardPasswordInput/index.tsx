edd4fb94011f104b9a4c4d3ffbf5f25e3d49a5ef
hafnium1923 <hafnium1923@gmail.com>
2023-04-18 21:40:03 +0900

    feat: CardpPassWordInput 컴포넌트 UI 구현
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

---
+++ src/component/CardPasswordInput/index.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import St from "./styled";
+
+function CardPasswordInput() {
+  return (
+    <>
+      <St.Title>카드 비밀번호</St.Title>
+      <St.Contents>
+        <St.InputSection>
+          <St.Input
+            type="password"
+            inputMode="numeric"
+            maxLength={1}
+          ></St.Input>
+        </St.InputSection>
+        <St.InputSection>
+          <St.Input
+            type="password"
+            inputMode="numeric"
+            maxLength={1}
+          ></St.Input>
+        </St.InputSection>
+        <St.LastDigits>•</St.LastDigits>
+        <St.LastDigits>•</St.LastDigits>
+      </St.Contents>
+    </>
+  );
+}
+
+export default CardPasswordInput;
---
+++ src/component/CardPasswordInput/styled.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+const St = {
+  Contents: styled.section`
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  `,
+  InputSection: styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+
+    width: 10%;
+    height: 7vh;
+
+    border-radius: 7px;
+
+    background: #ecebf1;
+  `,
+
+  Input: styled.input`
+    width: 60%;
+
+    border: none;
+
+    text-align: center;
+    background: inherit;
+    font-size: 30px;
+  `,
+
+  Title: styled.div`
+    padding-bottom: 10px;
+
+    font: 500 14px/16px "Roboto";
+    align-items: center;
+    letter-spacing: -0.085em;
+
+    color: #525252;
+  `,
+
+  LastDigits: styled.div`
+    text-align: center;
+    width: 8%;
+    font-size: 30px;
+  `,
+};
+
+export default St;
