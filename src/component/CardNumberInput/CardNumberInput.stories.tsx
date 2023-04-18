19acb06ff043cf4dde7ff7bbc96edbbd7320e41f
hafnium1923 <hafnium1923@gmail.com>
2023-04-18 18:50:45 +0900

    test: CardNumberInput 컴포넌트 story 생성
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

---
+++ src/component/CardNumberInput/CardNumberInput.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CardNumberInput from "./index";
+
+export default {
+  component: CardNumberInput,
+  title: "CardNumberInput",
+};
+
+const Template = () => <CardNumberInput />;
+
+export const Primary = Template.bind({});
