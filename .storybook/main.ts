11c2dd27dd927454e8ab83b97e4b45350cd5efa8
chsua <csj1919@naver.com>
2023-04-18 16:58:00 +0900

    chore: 스토리북 관련 패키지 및 eslint parser 설치
    Co-authored-by: Q Kim <qoli0703@gmail.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,14 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/react-webpack5",
-    options: {},
+    options: {}
   },
   docs: {
-    autodocs: "tag",
+    autodocs: "tag"
   },
-  staticDirs: ["../public"],
+  staticDirs: ["../public"]
 };
 export default config;
