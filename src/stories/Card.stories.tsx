fad465b78ae90e6c70d987170b80ca65169277dc
minjaeKim <smallkdb@gmail.com>
2023-04-18 21:59:55 +0900

    feat: Card 컴포넌트 스토리북 추가

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -11,5 +11,16 @@ const meta: Meta<typeof Card> = {
 export default meta;
 
 export const Default: Story = {
-  args: { cardNumber: ['1234', '1234', '1234', '1234'], expirationDate: { year: 23, month: 4 }, owner: 'SUN' },
+  args: {
+    cardNumber: ['1234', '1234', '1234', '1234'],
+    expirationDate: { year: 23, month: 4 },
+    owner: 'SUN',
+    isAddForm: true,
+  },
+};
+
+export const AddCard: Story = {
+  args: {
+    isAddForm: false,
+  },
 };
