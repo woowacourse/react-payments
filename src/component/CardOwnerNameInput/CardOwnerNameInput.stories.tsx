42e2cb2c6be310a0cac0dd89fb06222f6bb8cd2d
hafnium1923 <hafnium1923@gmail.com>
2023-04-18 20:40:05 +0900

    test: CardOwnerNameInput 컴포넌트 story 생성
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

---
+++ src/component/CardOwnerNameInput/CardOwnerNameInput.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CardOwnerNameInput from "./index";
+
+export default {
+  component: CardOwnerNameInput,
+  title: "CardOwnerNameInput",
+};
+
+const Template = () => <CardOwnerNameInput />;
+
+export const Primary = Template.bind({});
