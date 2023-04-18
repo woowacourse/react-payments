12727dd37449f0beea453c4377a132eb37209d06
chsua <csj1919@naver.com>
2023-04-18 20:33:35 +0900

    test: 만료일 입력 박스 컴포넌트 테스트 코드 작성

---
+++ src/stories/InputBoxExpirationDate.stories.tsx
@@ -0,0 +1,15 @@
+import { ChangeEvent } from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import InputBoxExpirationDate from "../component/CardInputPage/InputBoxExpirationDate/InputBoxExpirationDate";
+
+const meta: Meta = {
+  title: "InputBoxExpirationDate component",
+  component: InputBoxExpirationDate,
+};
+
+export default meta;
+
+export const InputTest = (args: any) => (
+  <InputBoxExpirationDate></InputBoxExpirationDate>
+);
