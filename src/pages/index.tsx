ebdc1adfdfff390d850934c86a889dc6bfe941e5
Suyoung <jsy8492@gmail.com>
2023-04-18 20:35:06 +0900

    feat: 앱 실행 시 보유중인 카드 목록을 localStorage에서 불러오는 기능 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
+import { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router';
 
 import HoldingCardsPage from './HoldingCardsPage';
 import CardRegisterPage from './CardRegisterPage';
 
 function App() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const cardsData = localStorage.getItem('cards');
+
+    if (cardsData === null) return;
+
+    setCards(JSON.parse(cardsData));
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<HoldingCardsPage />} />
