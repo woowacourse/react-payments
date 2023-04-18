06570d83320dfbaae885e9430890259641eefbb8
chsua <csj1919@naver.com>
2023-04-18 21:29:50 +0900

    test: 카드 비밀번호 입력 박스 컴포넌트 테스트 코드 작성

---
+++ src/stories/InputBoxPassword.stories.tsx
@@ -0,0 +1,13 @@
+import { ChangeEvent } from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import InputBoxPassword from "../component/CardInputPage/InputBoxPassword/InputBoxPassword";
+
+const meta: Meta = {
+  title: "InputBoxPassword component",
+  component: InputBoxPassword,
+};
+
+export default meta;
+
+export const InputTest = (args: any) => <InputBoxPassword></InputBoxPassword>;
