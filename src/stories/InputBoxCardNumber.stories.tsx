aeee3fa47d759bd1065399716a5362d21326368b
chsua <csj1919@naver.com>
2023-04-18 19:56:13 +0900

    test: 카드 번호 입력 박스 컴포넌트 테스트 코드 작성

---
+++ src/stories/InputBoxCardNumber.stories.tsx
@@ -0,0 +1,15 @@
+import { ChangeEvent } from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import InputBoxCardNumber from "../component/CardInputPage/InputBoxCardNumber/InputBoxCardNumber";
+
+const meta: Meta = {
+  title: "InputBoxCardNumber component",
+  component: InputBoxCardNumber,
+};
+
+export default meta;
+
+export const InputTest = (args: any) => (
+  <InputBoxCardNumber></InputBoxCardNumber>
+);
