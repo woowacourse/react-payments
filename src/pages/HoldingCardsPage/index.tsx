65d6ef03ba74b4c7c930e115eca1abbff4222c62
Suyoung <jsy8492@gmail.com>
2023-04-18 20:52:03 +0900

    feat: 보유중인 카드 목록 렌더링 기능 구현

--- src/pages/HoldingCardsPage/index.tsx
+++ src/pages/HoldingCardsPage/index.tsx
@@ -1,10 +1,15 @@
 import { useNavigate } from 'react-router-dom';
 
+import Card from '../../components/Card';
 import CardRegisterButton from '../../components/CardRegisterButton';
 
 import styles from './holdingCardsPage.module.css';
 
-const HoldingCardsPage = () => {
+interface Props {
+  cards: any[];
+}
+
+const HoldingCardsPage = ({ cards }: Props) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -17,10 +22,15 @@ const HoldingCardsPage = () => {
         <h1 className="text-title">보유 카드</h1>
       </header>
       <main>
-        <h2 className={`text-subtitle ${styles.subTitle}`}>
-          새로운 카드를 등록해주세요.
-        </h2>
+        {cards.length === 0 && (
+          <h2 className={`text-subtitle ${styles.subTitle}`}>
+            새로운 카드를 등록해주세요.
+          </h2>
+        )}
         <section className={styles.cardContainer}>
+          {cards.map((card, index) => (
+            <Card key={index} />
+          ))}
           <CardRegisterButton onClick={handleClick} />
         </section>
       </main>
