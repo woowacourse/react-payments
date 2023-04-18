b7dba0b954e4df088f49cfb23d0767672e76d9f0
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-18 20:25:55 +0900

    test: 만료일 입력 컴포넌트 스토리북 추가

---
+++ src/components/ExpirationDateInput/ExpirationDateInput.stories.tsx
@@ -0,0 +1,8 @@
+import ExpirationDateInput from "./ExpirationDateInput";
+
+export default {
+  title: "ExpirationDateInput",
+  component: ExpirationDateInput,
+};
+
+export const Default = () => <ExpirationDateInput></ExpirationDateInput>;
