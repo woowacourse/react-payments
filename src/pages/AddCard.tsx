60526400f3b51671f9ad75a6fb4bbd3112a26c9c
kangyeongmin <kym6050@hanyang.ac.kr>
2023-04-18 17:38:48 +0900

    feat: AddCard 페이지 초반 구현

---
+++ src/pages/AddCard.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/common/Header";
+import Page from "../components/common/Page";
+
+const AddCard = () => {
+  return (
+    <Page>
+      <Header title="카드 추가" isBack={true} />
+    </Page>
+  );
+};
+
+export default AddCard;
