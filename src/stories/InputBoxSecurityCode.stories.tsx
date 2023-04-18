3f78eba9ef68a8a130fb648069f2cf50090d8d14
chsua <csj1919@naver.com>
2023-04-18 21:28:22 +0900

    test: 보안코드 입력 박스 컴포넌트 테스트 코드 작성

---
+++ src/stories/InputBoxSecurityCode.stories.tsx
@@ -0,0 +1,15 @@
+import { ChangeEvent } from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import InputBoxSecurityCode from "../component/CardInputPage/InputBoxSecurityCode/InputBoxSecurityCode";
+
+const meta: Meta = {
+  title: "InputBoxSecurityCode component",
+  component: InputBoxSecurityCode,
+};
+
+export default meta;
+
+export const InputTest = (args: any) => (
+  <InputBoxSecurityCode></InputBoxSecurityCode>
+);
