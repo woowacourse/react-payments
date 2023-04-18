66af5de1c4dddc46b86cd669f4c40ed34348fb01
hafnium1923 <hafnium1923@gmail.com>
2023-04-18 18:51:57 +0900

    test: CardDateInput 컴포넌트 story 생성
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

---
+++ src/component/CardDateInput/CardDateInput.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CardDateInput from "./index";
+
+export default {
+  component: CardDateInput,
+  title: "CardDateInput",
+};
+
+const Template = () => <CardDateInput />;
+
+export const Primary = Template.bind({});
