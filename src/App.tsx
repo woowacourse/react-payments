a6c4a1d38487df8c8142c2d1b935b94d046f195d
hae-on <solbi2004@naver.com>
2023-04-18 16:48:59 +0900

    feat: 모바일 화면 컨테이너 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import CardListPage from './components/pages/CardListPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className='App'>
+    <AppContainer className='App'>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<CardListPage />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+  min-height: 100vh;
+  box-sizing: border-box;
+  background-color: white;
+
+  @media (min-width: 440px) {
+    width: 438px;
+  }
+  @media (max-width: 440px) {
+    width: 100vw;
+  }
+`;
 
 export default App;
---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+body {
+  background-color: #e2e2e8;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  margin: 0;
+
+  font-family: 'Apple SD Gothic Neo', 'Noto Sans KR'
+}
+`;
+
+export default GlobalStyle;
