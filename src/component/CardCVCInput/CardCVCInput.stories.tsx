0da1161bf6e614ef2de0dee920b14dd98999ae25
hafnium1923 <hafnium1923@gmail.com>
2023-04-18 21:05:02 +0900

    test: CardCVCInput 컴포넌트 story 생성
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

---
+++ src/component/CardCVCInput/CardCVCInput.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CardCVCInput from "./index";
+
+export default {
+  component: CardCVCInput,
+  title: "CardCVCInput",
+};
+
+const Template = () => <CardCVCInput />;
+
+export const Primary = Template.bind({});
