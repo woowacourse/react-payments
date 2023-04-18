1c2b825670b30e8cd6484993e92cdfd0f2481c63
turtle601 <turtle601@naver.com>
2023-04-18 22:04:54 +0900

    chore(Storybook): 글로벌 스타일 적용

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,18 @@
+import React from 'react';
 import type { Preview } from '@storybook/react';
+
+import GlobalStyle from '../src/styles/global';
+
+export const decorators = [
+  (Story) => {
+    return (
+      <>
+        <GlobalStyle />
+        <Story />
+      </>
+    );
+  },
+];
 
 const preview: Preview = {
   parameters: {
