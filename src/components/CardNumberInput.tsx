ac701671173d559535b7e53d9cd9ba64f4cfe0ef
Dain Lee <feb.dain@gmail.com>
2023-04-18 20:30:18 +0900

    feat: 인풋 값에 유효한 값만 입력할 수 있는 기능 구현
    Co-authored-by: gyeongza <gyeongza@naver.com>

--- src/components/CardNumberInput.tsx
+++ src/components/CardNumberInput.tsx
@@ -1,18 +1,23 @@
 import { ChangeEvent, useState } from "react";
+import { changeToValidValue } from "utils/inputValidator";
 
 const CardNumberInput = () => {
-  const [state, setState] = useState({
+  const [cardNumber, setCardNumber] = useState({
     number1: "",
     number2: "",
     number3: "",
     number4: "",
   });
 
-  const handleChangeState = ({ target }: ChangeEvent<HTMLInputElement>) => {
-    const value = target.value.replace(/[^\d]/g, "").slice(0, 4);
-
-    setState((prevState) => {
-      return { ...prevState, [target.name]: Number(value) };
+  const handleCardNumber = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    setCardNumber((prevState) => {
+      return {
+        ...prevState,
+        [target.name]: changeToValidValue(target.value, {
+          length: 4,
+          regex: /[^\d]/g,
+        }),
+      };
     });
   };
 
@@ -20,35 +25,37 @@ const CardNumberInput = () => {
     <>
       <label htmlFor="card-label">카드 번호</label>
       <input
-        type="number"
+        type="text"
         name="number1"
         id="card-label"
         aria-labelledby="card-label"
-        value={state.number1}
-        onChange={handleChangeState}
+        maxLength={4}
+        value={cardNumber.number1}
+        onChange={handleCardNumber}
       />
       <input
-        type="number"
+        type="text"
         name="number2"
+        maxLength={4}
         aria-labelledby="card-label"
-        value={state.number2}
-        onChange={handleChangeState}
+        value={cardNumber.number2}
+        onChange={handleCardNumber}
       />
       <input
         type="password"
         name="number3"
         maxLength={4}
         aria-labelledby="card-label"
-        value={state.number3}
-        onChange={handleChangeState}
+        value={cardNumber.number3}
+        onChange={handleCardNumber}
       />
       <input
         type="password"
         name="number4"
         maxLength={4}
         aria-labelledby="card-label"
-        value={state.number4}
-        onChange={handleChangeState}
+        value={cardNumber.number4}
+        onChange={handleCardNumber}
       />
     </>
   );
