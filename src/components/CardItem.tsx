496fb41607d88d1b954d2ab1fab6b86ccb8320f6
Dahye <06robin11@gmail.com>
2023-04-18 20:05:07 +0900

    style(cardItem): 카드 아이템 컴포넌트 마크업

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { CardType } from "../types/card";
 import Card from "./common/Card";
 
@@ -8,7 +9,48 @@ interface CardProps {
 export const CardItem = ({ card }: CardProps) => {
   return (
     <Card backgroundColor={card.color}>
-      <div></div>
+      <Container>
+        <IcChip />
+        <CardNumbers>{card.numbers}</CardNumbers>
+        <InfoWrapper>
+          <Name>{card.owner}</Name>
+          <ExpiryDate>{card.expiryDate}</ExpiryDate>
+        </InfoWrapper>
+      </Container>
     </Card>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 180px;
+  height: 95px;
+  color: white;
+
+  font-size: 10px;
+  font-weight: 600;
+  gap: 6px;
+`;
+
+const IcChip = styled.div`
+  width: 40px;
+  height: 26px;
+  border-radius: 4px;
+  background-color: #cbba64;
+  margin-top: 32px;
+`;
+
+const CardNumbers = styled.div`
+  text-align: center;
+`;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const Name = styled.div``;
+
+const ExpiryDate = styled.div``;
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,9 +1,24 @@
 import { CardList } from "../components/CardList";
 
+const mockData = [
+  {
+    numbers: "1234-1234-1243-1234",
+    owner: "DAHYE YUN",
+    expiryDate: "12/25",
+    color: "#525252",
+  },
+  {
+    numbers: "1111-2222-3333-4444",
+    owner: "ABC",
+    expiryDate: "11/11",
+    color: "#BA55D3",
+  },
+];
+
 export const Home = () => {
   return (
     <>
-      <CardList cards={[]} />
+      <CardList cards={mockData} />
     </>
   );
 };
