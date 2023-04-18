b1d13a2433f21c6a32ecb442249bfae34e04d3ec
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-04-18 15:53:37 +0900

    test: Input 검증(숫자만 입력) 테스트 추가

---
+++ __tests__/validator.test.js
@@ -0,0 +1,44 @@
+describe('', () => {
+  it('값은 숫자로만 이루어져 있다.', () => {
+    // given
+    const input = [1, 2, 3, 4, 5, 6];
+
+    // when
+    const results = inputs.map((input) => isNumber(input));
+
+    // then
+    expect(results.every((result) => result === true)).toBeTruthy();
+  });
+
+  // 만료일의 MM은 01 ~ 12 사이의 숫자로 이루어져 있다.
+
+  // 만료일의 YY는 올해 이상의 숫자로 이루어져 있다.
+
+  // 만료일의 YY가 올해일 때, MM은 이번 달 이상의 값을 갖고 있다.
+
+  // 이름은 영문과 공백으로 이루어져 있다.
+
+  // 이름은 30글자 이하다.
+});
+describe('', () => {
+  it('값은 숫자로만 이루어져 있다.', () => {
+    // given
+    const input = [1, 2, 3, 4, 5, 6];
+
+    // when
+    const results = inputs.map((input) => isNumber(input));
+
+    // then
+    expect(results.every((result) => result === true)).toBeTruthy();
+  });
+
+  // 만료일의 MM은 01 ~ 12 사이의 숫자로 이루어져 있다.
+
+  // 만료일의 YY는 올해 이상의 숫자로 이루어져 있다.
+
+  // 만료일의 YY가 올해일 때, MM은 이번 달 이상의 값을 갖고 있다.
+
+  // 이름은 영문과 공백으로 이루어져 있다.
+
+  // 이름은 30글자 이하다.
+});
