d8e5c3bab023cee4edb04cc723b28394409ec0e2
chsua <csj1919@naver.com>
2023-04-18 20:34:11 +0900

    test: 소유자 입력 박스 컴포넌트 테스트 코드 작성

---
+++ src/stories/InputBoxOwner.stories.tsx
@@ -0,0 +1,13 @@
+import { ChangeEvent } from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import InputBoxOwner from "../component/CardInputPage/InputBoxOwner/InputBoxOwner";
+
+const meta: Meta = {
+  title: "InputBoxOwner component",
+  component: InputBoxOwner,
+};
+
+export default meta;
+
+export const InputTest = (args: any) => <InputBoxOwner></InputBoxOwner>;
