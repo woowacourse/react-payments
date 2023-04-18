3161cfc6786c42067b860c5a050c44154bdda0c2
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-18 20:56:00 +0900

    test: 보안 코드 입력 컴포넌트 스토리북 추가

---
+++ src/components/SecurityCodeInput/SecurityCodeInput.stories.tsx
@@ -0,0 +1,8 @@
+import SecurityCodeInput from "./SecurityCodeInput";
+
+export default {
+  title: "SecurityCodeInput",
+  component: SecurityCodeInput,
+};
+
+export const Default = () => <SecurityCodeInput></SecurityCodeInput>;
