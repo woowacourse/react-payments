4f23d38117cc3db972137b4e9f088e01e2a4eaca
kangyeongmin <kym6050@hanyang.ac.kr>
2023-04-18 17:39:00 +0900

    feat: MyCard 페이지 초반 구현

---
+++ src/pages/MyCard.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/common/Header";
+import Page from "../components/common/Page";
+
+const MyCard = () => {
+  return (
+    <Page>
+      <Header title="보유카드" isBack={false} />
+    </Page>
+  );
+};
+
+export default MyCard;
