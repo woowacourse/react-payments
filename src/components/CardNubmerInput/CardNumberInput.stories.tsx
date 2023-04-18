2d629464737c3d4782b97a49e885c75e2da257b3
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-18 20:08:02 +0900

    test: 카드번호 입력 컴포넌트 스토리북 추가

---
+++ src/components/CardNubmerInput/CardNumberInput.stories.tsx
@@ -0,0 +1,8 @@
+import CardNumberInput from "./CardNumberInput";
+
+export default {
+  title: "CardNumberInput",
+  component: CardNumberInput,
+};
+
+export const Default = () => <CardNumberInput></CardNumberInput>;
