b37faf01d877482cd7cef5fd127e1a34c838401e
Dahye <06robin11@gmail.com>
2023-04-18 17:40:24 +0900

    style: 기본 css 설정 리셋

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./style/global";
-import { Home } from "./page/Home";
-import AddCard from "./page/AddCard";
-import { useState } from "react";
+import { GlobalStyle } from "./style/resetStyle";
 
 function App() {
-  const [isHome, setIsHome] = useState(true);
-
   return (
     <ThemeProvider theme={theme}>
-      {isHome ? <Home /> : <AddCard />}
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
