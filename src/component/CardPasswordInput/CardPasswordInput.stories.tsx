7c7b10f30ce01912c1fc9564737bdc882d3f0295
hafnium1923 <hafnium1923@gmail.com>
2023-04-18 21:40:38 +0900

    test: CardPassWordInput 컴포넌트 story 생성
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

---
+++ src/component/CardPasswordInput/CardPasswordInput.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CardPasswordInput from "./index";
+
+export default {
+  component: CardPasswordInput,
+  title: "CardPasswordInput",
+};
+
+const Template = () => <CardPasswordInput />;
+
+export const Primary = Template.bind({});
