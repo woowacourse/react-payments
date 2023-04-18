ff75ac20ef1a1ecfd90be715981701e31606006c
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-04-18 16:21:04 +0900

    chore: storybook setting
    Co-authored-by: 유강현 <yrkdgus@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import GlobalStyle from './GlobalStyle';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
 );
