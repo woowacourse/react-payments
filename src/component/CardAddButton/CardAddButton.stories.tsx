1453d707c471062cc31ed3ad6cfea915e6bc9a2b
hafnium1923 <hafnium1923@gmail.com>
2023-04-19 17:09:42 +0900

    test: CardAddButton 컴포넌트 story 생성
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

---
+++ src/component/CardAddButton/CardAddButton.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CardAddButton from "./index";
+
+export default {
+  component: CardAddButton,
+  title: "CardDetail/CardAddButton",
+};
+
+const Template = () => <CardAddButton />;
+
+export const Primary = Template.bind({});
