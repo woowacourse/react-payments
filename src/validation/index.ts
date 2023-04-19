02f5ce6568ac9ce7ab6893cf47d8eef4563687b9
NaveOWO <pianowell96@gmail.com>
2023-04-19 16:55:30 +0900

    feat : 유효성 검사 & 에러 던지는 메서드 생성

---
+++ src/validation/index.ts
@@ -0,0 +1,34 @@
+import { ERROR_MESSAGE } from "../constants/errors";
+
+const REGEX = Object.freeze({
+  NUMBER: /^[0-9]+$/,
+  STRING: /^[가-힣]{2,4}|[a-zA-Z]{2,10}\s[a-zA-Z]{2,10}$/,
+  MONTH: /^(1[0-2]|[1-9])$/,
+});
+export const validation = {
+  date(input: string) {
+    this.isNumber(input);
+    this.isCorrectMonth(input);
+  },
+
+  isNumber(input: string) {
+    if (!REGEX.NUMBER.test(input)) {
+      throw new Error(ERROR_MESSAGE.ONLY_NUMBER);
+    }
+  },
+
+  isString(input: string) {
+    if (!REGEX.STRING.test(input)) {
+      throw new Error(ERROR_MESSAGE.ONLY_STRING);
+    }
+  },
+
+  isCorrectMonth(input: string) {
+    const pureText = input[0] === "0" ? input.substring(1) : input;
+    if (!REGEX.MONTH.test(pureText)) {
+      throw new Error(ERROR_MESSAGE.INVALID_DATE);
+    }
+  },
+};
+
+export type TValidation = typeof validation;
