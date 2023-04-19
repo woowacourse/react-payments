1ae6242dedabd02cd0c45e59fc4fba0adf781cf1
hafnium1923 <hafnium1923@gmail.com>
2023-04-19 16:11:49 +0900

    refactor: CardDetail 관련 컴포넌트 CardDetailPage 하위로 이동
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

--- src/component/CardDetailPage/index.tsx
+++ src/component/CardDetailPage/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import St from "./styled";
-import CardDetailHeader from "../CardDetailHeader";
-import CardDetailView from "../CardDetailView";
-import CardDetailForm from "../CardDetailForm";
-
+import CardDetailHeader from "./CardDetailHeader";
+import CardDetailView from "./CardDetailView";
+import CardDetailForm from "./CardDetailForm";
 function CardDetailPage() {
   return (
     <St.Page>
