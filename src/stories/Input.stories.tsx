047e90db39604c4230e0aa8a17c8cfdab285dad0
Dahye <06robin11@gmail.com>
2023-04-19 16:53:43 +0900

    test(input):CVC입력 storybook test

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -27,9 +27,9 @@ CardNumberInput.args = {
 
     // 카드번호 저장 로직
 
-    const numbers = value.slice(0, 8) + "*".repeat(value.slice(8).length);
+    const numbers = value.slice(0, 8) + "●".repeat(value.slice(8).length);
 
-    e.target.value = (numbers.match(/\d{1,4}|\*{1,4}/g) ?? []).join(" - ");
+    e.target.value = (numbers.match(/\d{1,4}|●{1,4}/g) ?? []).join(" - ");
   },
 };
 
@@ -66,5 +66,22 @@ OwnerInput.args = {
     }
 
     e.target.value = e.target.value.toUpperCase();
+  },
+};
+
+export const CVCInput = Template.bind({});
+
+CVCInput.args = {
+  label: "cvc",
+  width: "84px",
+  placeholder: "",
+  textPosition: "center",
+  event: (e) => {
+    if (e.target.value.length > 3 || !/\d$/.test(e.target.value)) {
+      e.target.value = e.target.value.slice(0, -1);
+      return;
+    }
+
+    e.target.value = e.target.value.replaceAll(/\d/g, "●");
   },
 };
