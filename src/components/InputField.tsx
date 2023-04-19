2bbb4959851c1e2a50e2c8e330f402ea7e455032
JIWON <afds4567@gmail.com>
2023-04-19 15:44:47 +0900

    feat: InputField 컴포넌트 구현

---
+++ src/components/InputField.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export type InputTitle =
+  | "cardNumber"
+  | "expiracy"
+  | "owner"
+  | "cvc"
+  | "password";
+
+export interface InputContainerProps {
+  kind: InputTitle;
+  children: JSX.Element;
+}
+
+const INPUT_INFO = {
+  cardNumber: {
+    title: "카드 번호",
+    width: "318px",
+  },
+  expiracy: {
+    title: "만료일",
+    width: "137px",
+  },
+  owner: {
+    title: "카드 소유자 이름(선택)",
+    width: "318px",
+  },
+  cvc: {
+    title: "보안 코드(CVC/CVV)",
+    width: "84px",
+  },
+  password: {
+    title: "카드 비밀번호",
+    width: "174px",
+  },
+};
+
+const Wrapper = styled.div<{ width: string }>`
+  display: flex;
+  flex-direction: column;
+  max-width: 318px;
+  width: ${({ width }) => `${width}`};
+`;
+
+const Label = styled.label`
+  margin-bottom: 3px;
+  width: 318px;
+  font-size: 12px;
+  line-height: 14px;
+  letter-spacing: -0.085em;
+  color: #525252;
+`;
+
+export default function InputField({ kind, children }: InputContainerProps) {
+  return (
+    <Wrapper width={INPUT_INFO[kind].width}>
+      <Label htmlFor={kind}>{INPUT_INFO[kind].title}</Label>
+      {children}
+    </Wrapper>
+  );
+}
