15df42b8629b61517c28be66ca63d1a9c61a6a56
hafnium1923 <hafnium1923@gmail.com>
2023-04-19 15:28:28 +0900

    test: CardDetailHeader 컴포넌트 story 생성
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

---
+++ src/component/CardDetailHeader/CardDetailHeader.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CardDetailHeader from "./index";
+
+export default {
+  component: CardDetailHeader,
+  title: "CardDetailHeader",
+};
+
+const Template = () => <CardDetailHeader />;
+
+export const Primary = Template.bind({});
