072fe24949f6225fe2aff0d679f6cd8b84ff43bf
dladncks1217 <dlaxodud1217@gmail.com>
2023-04-19 19:24:26 +0900

    feat: FormCardAdd 컴포넌트 스타일 주입

--- src/components/FormCardAdd.tsx
+++ src/components/FormCardAdd.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import InputCardData from './InputCardData';
 import InputCardPassword from './InputCardPassword';
+import './FormCardAdd.css';
 
 const FormCardAdd = ({
   cardNumberData,
   cardExpireData,
   cardOwnerData,
   securityCodeData,
-  cardPaswordData,
+  cardPassword1Data,
+  cardPassword2Data,
 }: any) => {
   return (
-    <form>
+    <form className="add-card-form">
       <div>
         <span>카드 번호</span>
         <InputCardData
@@ -18,38 +20,38 @@ const FormCardAdd = ({
           onChange={cardNumberData.onChange}
           status={cardNumberData.status}
           className="example"
-          errorMessage="asdfasfas"
+          errorMessage="잘못된 값"
         />
       </div>
       <div>
         <span>만료일</span>
         <InputCardData
-          value={cardNumberData.value}
-          onChange={cardNumberData.onChange}
-          status={cardNumberData.status}
+          value={cardExpireData.value}
+          onChange={cardExpireData.onChange}
+          status={cardExpireData.status}
           className="example"
-          errorMessage="asdfasfas"
+          errorMessage="잘못된 값"
         />
       </div>
       <div>
         <span>카드 소유자 이름(선택)</span>
         <span>0/30</span>
         <InputCardData
-          value={cardNumberData.value}
-          onChange={cardNumberData.onChange}
-          status={cardNumberData.status}
+          value={cardOwnerData.value}
+          onChange={cardOwnerData.onChange}
+          status={cardOwnerData.status}
           className="example"
-          errorMessage="asdfasfas"
+          errorMessage="잘못된 값"
         />
       </div>
       <div>
         <span>보안코드(CVC/CVV)</span>
         <InputCardPassword
-          value={cardNumberData.value}
-          onChange={cardNumberData.onChange}
-          status={cardNumberData.status}
+          value={securityCodeData.value}
+          onChange={securityCodeData.onChange}
+          status={securityCodeData.status}
           className="example"
-          errorMessage="asdfasfas"
+          errorMessage="잘못된 값"
           maxDataLength={3}
         />
         {/* 물음표 버튼 */}
@@ -57,24 +59,26 @@ const FormCardAdd = ({
       <div>
         <span>카드 비밀번호</span>
         <InputCardPassword
-          value={cardNumberData.value}
-          onChange={cardNumberData.onChange}
-          status={cardNumberData.status}
+          value={cardPassword1Data.value}
+          onChange={cardPassword1Data.onChange}
+          status={cardPassword1Data.status}
           className="example"
-          errorMessage="asdfasfas"
+          errorMessage="잘못된 값"
           maxDataLength={1}
         />
         <InputCardPassword
-          value={cardNumberData.value}
-          onChange={cardNumberData.onChange}
-          status={cardNumberData.status}
+          value={cardPassword2Data.value}
+          onChange={cardPassword2Data.onChange}
+          status={cardPassword2Data.status}
           className="example"
-          errorMessage="asdfasfas"
+          errorMessage="잘못된 값"
           maxDataLength={1}
         />
         {/* ... 이미지 */}
       </div>
-      <button type="submit">다음</button>
+      <div className="add-card-submit">
+        <button type="submit">다음</button>
+      </div>
     </form>
   );
 };
