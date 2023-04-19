8718650c8cc5fe956ec0e4be3a2891d752aa6883
NaveOWO <pianowell96@gmail.com>
2023-04-19 19:20:25 +0900

    feat : 에러 메세지를 초기화 하는 기능 추가

--- src/hooks/useError.ts
+++ src/hooks/useError.ts
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
 export function useError() {
-  const [error, setError] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
 
   function handleError(input: any, validator: (input: string) => void) {
     try {
       validator(input);
+      setError(null);
     } catch (e) {
       console.error(e);
       const error = e as Error;
