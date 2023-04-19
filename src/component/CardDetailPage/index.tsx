bb1bce0b466e05ec068fa2a93688a5b4c134d90d
hafnium1923 <hafnium1923@gmail.com>
2023-04-19 15:47:06 +0900

    feat: CardDetailPage 컴포넌트 UI 구현
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

---
+++ src/component/CardDetailPage/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import St from "./styled";
+import CardDetailHeader from "../CardDetailHeader";
+import CardDetailView from "../CardDetailView";
+import CardDetailForm from "../CardDetailForm";
+
+function CardDetailPage() {
+  return (
+    <St.Page>
+      <CardDetailHeader />
+      <CardDetailView />
+      <CardDetailForm />
+    </St.Page>
+  );
+}
+
+export default CardDetailPage;
