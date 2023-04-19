d8c13f49894095810fb0aed0c7bc9ab1793f4b1a
hafnium1923 <hafnium1923@gmail.com>
2023-04-19 17:26:16 +0900

    feat: CardList 컴포넌트 UI 구현
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

---
+++ src/component/CardListPage/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import St from "./styled";
+import CardListHeader from "../CardListHeader";
+import CardList from "../CardList";
+
+function CardListPage() {
+  return (
+    <St.Page>
+      <CardListHeader />
+      <CardList />
+    </St.Page>
+  );
+}
+
+export default CardListPage;
---
+++ src/component/CardListPage/styled.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const St = {
+  Page: styled.section`
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+
+    width: 100vw;
+    height: 100vh;
+
+    padding: 28px;
+  `,
+};
+
+export default St;
