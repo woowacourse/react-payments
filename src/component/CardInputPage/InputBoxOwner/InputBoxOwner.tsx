e6aeb2fad6a0d8259d5e57f8dbde16c06805bc3c
chsua <csj1919@naver.com>
2023-04-19 16:00:34 +0900

    feat: 카드 소유자 입력칸 component css 적용

--- src/component/CardInputPage/InputBoxOwner/InputBoxOwner.tsx
+++ src/component/CardInputPage/InputBoxOwner/InputBoxOwner.tsx
@@ -1,21 +1,28 @@
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 import Input from "../../common/Input";
 
+import "./inputBoxOwner.css";
+
 export default function InputBoxOwner() {
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(true);
   const [nameLength, setNameLength] = useState(0);
 
+  const onChangeCallback = (event: ChangeEvent<HTMLInputElement>) => {
+    setNameLength(event.target.value.trim().length);
+  };
+
   return (
-    <div>
+    <div className="input-box-card-owner">
       <p>카드 소유자 이름(선택)</p>
       <p>{nameLength}/30</p>
       <Input
+        className="input-card-owner"
         type="text"
-        onChange={() => {}}
+        onChange={onChangeCallback}
         placeholder="카드에 표시된 이름과 동일하게 입력하세요."
         inputMode="text"
       ></Input>
-      <p>error message</p>
+      <p className={error ? "visible" : ""}>error message</p>
     </div>
   );
 }
