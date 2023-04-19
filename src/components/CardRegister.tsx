e0148dbb86c3d9b56eb3a1f373d796c91f491c21
turtle601 <turtle601@naver.com>
2023-04-19 11:06:37 +0900

    feat(CardRegister): 카드 등록 컴포넌트 UI 구현

---
+++ src/components/CardRegister.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+const CardRegister = () => {
+  return (
+    <CardRegisterWrapper>
+      <CardRegisterMessage>새로운 카드를 등록해주세요.</CardRegisterMessage>
+      <CardRegisterButton>+</CardRegisterButton>
+    </CardRegisterWrapper>
+  );
+};
+
+const CardRegisterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #575757;
+`;
+
+const CardRegisterMessage = styled.span`
+  margin-bottom: 8px;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 16px;
+  opacity: 0.9;
+`;
+
+const CardRegisterButton = styled.button`
+  font-size: 30px;
+  line-height: 35px;
+
+  width: 208px;
+  height: 124px;
+
+  color: #575757;
+  background-color: #e5e5e5;
+  border: none;
+  border-radius: 5px;
+`;
+
+export default CardRegister;
