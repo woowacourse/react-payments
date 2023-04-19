4e604dde9cbd60617b6ad82e41ffd03a3eacc445
solo5star <solo_5@naver.com>
2023-04-19 01:33:40 +0000

    feat: App 컴포넌트를 만들다
    Co-authored-by: yeopto <gunyeop530@gmail.com>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,22 @@
-function App() {
-  return <div className="App"></div>;
-}
+import styled from 'styled-components';
+import { Outlet } from 'react-router';
+import { ResetStyle } from '../styles/ResetStyle';
 
-export default App;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+
+  width: 100%;
+  min-height: 100px;
+  height: 100vh;
+`;
+
+export const App = () => {
+  return (
+    <Container>
+      <ResetStyle />
+
+      <Outlet />
+    </Container>
+  );
+};
