41e6b1454c18685a734755030bbbfd92809a74ed
jeongwusi <jeongwusi@gmail.com>
2023-04-19 19:17:16 +0900

    ✨ feat: CardNumberInput 컴포넌트 구현
    Co-authored-by: 요술토끼 <curious.wzrabbit@gmail.com>

--- src/components/CardNumberInput/CardNumberInput.tsx
+++ src/components/CardNumberInput/CardNumberInput.tsx
@@ -1,5 +1,25 @@
+import { useState } from 'react';
+import CardInfoInput from '../CardInfoInput/CardInfoInput';
+import Input from '../Input/Input';
+
 const CardNumberInput = () => {
-  return <input className="" />;
+  const [cardNumber, setCardNumber] = useState('');
+
+  const addHyphensInCardNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (/[^0-9-]/.test(e.target.value)) return alert('숫자만 입력이 가능합니다!');
+
+    const cardNumber = e.target.value;
+    const hyphenRemovedCardNumber = cardNumber.replaceAll('-', '');
+    const cardNumberWithHyphens = (hyphenRemovedCardNumber.match(/.{1,4}/g) || []).join('-');
+
+    setCardNumber(cardNumberWithHyphens);
+  };
+
+  return (
+    <CardInfoInput title="카드 번호">
+      <Input width="100%" onChange={addHyphensInCardNumber} maxLength={19} value={cardNumber} />
+    </CardInfoInput>
+  );
 };
 
 export default CardNumberInput;
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,12 +2,21 @@ import styles from './Input.module.css';
 
 type InputProps = {
   width: string;
+  value: string;
+  maxLength?: number;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const Input = ({ width }: InputProps) => {
+const Input = ({ width, value, maxLength, onChange }: InputProps) => {
   return (
     <>
-      <input className={styles.input} style={{ width: width }} />
+      <input
+        className={styles.input}
+        style={{ width: width }}
+        value={value}
+        maxLength={maxLength ? maxLength : 9999999}
+        onChange={onChange}
+      />
     </>
   );
 };
