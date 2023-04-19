b9fa2cf626894e5d334adc1d2c9204158f97da81
JIWON <afds4567@gmail.com>
2023-04-19 21:04:03 +0900

    feat: InformationButton 컴포넌트 구현

---
+++ src/components/InformationButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.button`
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #bababa;
+  border-radius: 50%;
+  color: #969696;
+  background-color: transparent;
+  width: 27px;
+  height: 27px;
+  font-size: 20px;
+  font-weight: 500;
+`;
+export default function InformationButton() {
+  return <Wrapper>﹖</Wrapper>;
+}
---
+++ src/stories/InformationButton.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import InformationButton from "../components/InformationButton";
+
+const meta: Meta<typeof InformationButton> = {
+  component: InformationButton,
+  title: "Button",
+};
+
+export default meta;
+type Story = StoryObj<typeof InformationButton>;
+
+export const Information: Story = {
+  args: {},
+};
