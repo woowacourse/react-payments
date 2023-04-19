288d68d1ebdbaf545eca9df870f660f8cd1ec429
jeongwusi <jeongwusi@gmail.com>
2023-04-19 22:43:40 +0900

    ✨ feat: AddCardForm 컴포넌트 props 속성 추가
    Co-authored-by: 요술토끼 <curious.wzrabbit@gmail.com>

--- src/components/AddCardForm/AddCardForm.tsx
+++ src/components/AddCardForm/AddCardForm.tsx
@@ -1,26 +1,25 @@
 import styles from './AddCardForm.module.css';
-import Input from '../Input/Input';
-import CardInfoInput from '../CardInfoInput/CardInfoInput';
 import NextButton from '../NextButton/NextButton';
 import CardNumberInput from '../CardNumberInput/CardNumberInput';
+import ExpirationDateInput from '../ExpirationDateInput/ExpirationDateInput';
+import CardOwnerName from '../CardOwnerName/CardOwnerName';
+import CardSecurityCodeInput from '../CardSecurityCodeInput/CardSecurityCodeInput';
+import CardPasswordInput from '../CardPasswordInput/CardPasswordInput';
 
-const AddCardForm = () => {
+type AddCardFormProps = {
+  updateCardNumber: (cardNumber: string) => void;
+  updateExpirationDate: (expirationDate: string) => void;
+  updateCardOwnerName: (cardOwnerName: string) => void;
+};
+
+const AddCardForm = ({ updateCardNumber, updateExpirationDate, updateCardOwnerName }: AddCardFormProps) => {
   return (
     <form className={styles.container}>
-      <CardNumberInput />
-      <CardInfoInput title="만료일">
-        <Input width="40%" value="" onChange={() => {}} />
-      </CardInfoInput>
-      <CardInfoInput title="카드 소유자 이름 (선택)" numberOfLetter={[0, 30]}>
-        <Input width="100%" value="" onChange={() => {}} />
-      </CardInfoInput>
-      <CardInfoInput title="보안 코드(CVC/CVV)">
-        <Input width="30%" value="" onChange={() => {}} />
-      </CardInfoInput>
-      <CardInfoInput title="카드 비밀번호">
-        <Input width="15%" value="" onChange={() => {}} />
-        <Input width="15%" value="" onChange={() => {}} />
-      </CardInfoInput>
+      <CardNumberInput updateCardNumber={updateCardNumber} />
+      <ExpirationDateInput updateExpirationDate={updateExpirationDate} />
+      <CardOwnerName updateCardOwnerName={updateCardOwnerName} />
+      <CardSecurityCodeInput />
+      <CardPasswordInput />
       <div className={styles.button}>
         <NextButton />
       </div>
