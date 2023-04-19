bedd8d2b4d70066800f4e78613ecfecdab0a6b6c
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-04-19 10:51:42 +0900

    feat: 입력값 검증 함수 구현

--- src/validator.ts
+++ src/validator.ts
@@ -1,3 +1,47 @@
-export const isNumber = (input: number) => {
-  return typeof input === 'number';
+export const validateNumeric = (value: string) => {
+  const isContainNumbers = /[0-9]/.test(value);
+
+  if (!isContainNumbers) throw new Error('숫자만 입력하라 이거야');
+};
+
+export const validateMonth = (value: number) => {
+  const isValidRange = value >= 1 && value <= 12;
+
+  if (!isValidRange) throw new Error('MM에는 01에서 12까지의 값만 입력해주세요');
+};
+
+export const validateExpirationDate = (expirationYear: number, expirationMonth: number) => {
+  const currentYear = new Date().getFullYear() - 2000;
+  const currentMonth = new Date().getMonth() + 1;
+
+  const isValidDate =
+    expirationYear > currentYear ||
+    (expirationYear === currentYear && expirationMonth >= currentMonth);
+
+  if (!isValidDate) throw new Error('만료일이 지난 카드는 등록할 수 없습니다');
+};
+
+export const validateValidUserName = (userName: string) => {
+  const isEmpty = !userName.length;
+  const isValidFormat = /^[A-Za-z](?:\s?[A-Za-z])*$/.test(userName);
+  const isValidLength = userName.length <= 30;
+
+  if (isEmpty) return;
+
+  if (isValidFormat && isValidLength) return;
+
+  throw new Error('영문으로 이루어진 이름을 입력해주세요.');
+};
+
+export const validateCardExpirationValue = (month: string, year: string) => {
+  try {
+    validateNumeric(month);
+    validateNumeric(year);
+
+    // Number
+    validateMonth(Number(month));
+    validateExpirationDate(Number(year), Number(month));
+  } catch (error) {
+    console.error(error);
+  }
 };
