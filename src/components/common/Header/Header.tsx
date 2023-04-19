4a1979392df931304751c5586e219f8ebe995333
ashleysyheo <ashleysyheo@gmail.com>
2023-04-19 17:21:04 +0900

    refactor: Header 컴포넌트 수정

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -5,18 +5,17 @@ import leftIcon from '../../../assets/left-icon.svg';
 
 interface HeaderProps extends ComponentPropsWithoutRef<'header'> {
   content: string;
+  path?: string;
   isOverlayPage?: boolean;
 }
 
-function Header({ content, isOverlayPage = false, ...attributes }: HeaderProps) {
+function Header({ content, path = '/', isOverlayPage = false, ...attributes }: HeaderProps) {
   return (
     <header className={styles.header} {...attributes}>
-      {isOverlayPage && (
-        <Link to={'/'}>
-          <img src={leftIcon} alt="left back icon" />
-        </Link>
-      )}
-      <h3>{content}</h3>
+      <Link to={path}>
+        {isOverlayPage && <img src={leftIcon} alt="left back icon" />}
+        <h3>{content}</h3>
+      </Link>
     </header>
   );
 }
