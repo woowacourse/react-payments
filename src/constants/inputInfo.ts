0e7d47da1eaca9219608f5eda9e93f36842a9493
NaveOWO <pianowell96@gmail.com>
2023-04-19 11:58:20 +0900

    feat : label & placeholder 상수 선언

---
+++ src/constants/inputInfo.ts
@@ -0,0 +1,14 @@
+export const LABEL = {
+  NUMBER: "카드 번호",
+  DATE: "만료일",
+  NAME: "카드 소유자 이름(선택)",
+  CODE: "보안 코드(CVC/CVV)",
+  PASSWORD: "카드 비밀번호",
+} as const;
+
+export const PLACEHOLDER = Object.freeze({
+  DATE: ["MM", "YY"],
+  NAME: ["카드에 표시된 이름과 동일하게 입력하세요."],
+});
+
+export type LabelOption = keyof typeof LABEL;
