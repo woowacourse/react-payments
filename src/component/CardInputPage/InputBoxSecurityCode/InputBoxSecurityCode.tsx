f22cb87ce1d47d0158924de3640c9cfeba2b06f1
chsua <csj1919@naver.com>
2023-04-19 16:02:07 +0900

    feat: 카드 보안코드 입력칸 component css 적용

--- src/component/CardInputPage/InputBoxSecurityCode/InputBoxSecurityCode.tsx
+++ src/component/CardInputPage/InputBoxSecurityCode/InputBoxSecurityCode.tsx
@@ -1,24 +1,29 @@
 import { useState, ChangeEvent } from "react";
 import Input from "../../common/Input";
 
+import "./inputBoxSecurityCode.css";
+
 export default function InputBoxSecurityCode() {
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(true);
 
   const onChangeCallback = (event: ChangeEvent<HTMLInputElement>) => {
     // setNameLength(event.target.value.trim().length);
   };
 
   return (
-    <div>
+    <div className="input-box-security-code">
       <p>보안 코드(CVC/CVV)</p>
       <Input
+        className="input-security-code"
         type="number"
         onChange={onChangeCallback}
         placeholder="X X X"
         inputMode="numeric"
       ></Input>
-      <button type="button">?</button>
-      <p>error message</p>
+      <button className="button-security-code" type="button">
+        ?
+      </button>
+      <p className={error ? "visible" : ""}>error message</p>
     </div>
   );
 }
