6227b97e30c1b7f82e8fa17ec54d6f08b25ab5ac
hafnium1923 <hafnium1923@gmail.com>
2023-04-19 11:58:10 +0900

    test: CardDetailForm 컴포넌트 story 생성
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

---
+++ src/component/CardDetailForm/CardDetailForm.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CardDetailForm from "./index";
+
+export default {
+  component: CardDetailForm,
+  title: "CardDetailForm",
+};
+
+const Template = () => <CardDetailForm />;
+
+export const Primary = Template.bind({});
