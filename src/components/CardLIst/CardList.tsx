0385ec49e56293b387fdc031956ce32ee6dcf052
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-19 20:24:37 +0900

    refactor: CardList 컴포넌트 map함수에 key추가

--- src/components/CardLIst/CardList.tsx
+++ src/components/CardLIst/CardList.tsx
@@ -1,5 +1,7 @@
+import styled from "styled-components";
 import { Card } from "../../types";
 import CardPreview from "../CardPreview/CardPreview";
+import AddCardButton from "../AddCardButton/AddCardButton";
 
 type CardListProps = {
   cards: Card[];
@@ -7,13 +9,25 @@ type CardListProps = {
 
 const CardList = ({ cards }: CardListProps) => {
   return (
-    <ul>
+    <Ul>
       {cards.map((card) => {
         const { cardNumber, ownerName, expirationDate } = card;
-        return <CardPreview card={{ cardNumber, ownerName, expirationDate }} />;
+        const { firstGroup, secondGroup, thirdGroup, fourthGroup } = cardNumber;
+        const key = `${firstGroup}${secondGroup}${thirdGroup}${fourthGroup}`;
+
+        return <CardPreview key={key} card={{ cardNumber, ownerName, expirationDate }} />;
       })}
-    </ul>
+
+      <AddCardButton />
+    </Ul>
   );
 };
+
+const Ul = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 46px;
+`;
 
 export default CardList;
