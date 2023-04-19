3e497df272791da87c86e88a04fea9b2bb889e73
ashleysyheo <ashleysyheo@gmail.com>
2023-04-20 00:34:49 +0900

    refactor: Header 컴포넌트 수정

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -12,10 +12,8 @@ interface HeaderProps extends ComponentPropsWithoutRef<'header'> {
 function Header({ content, path = '/', isOverlayPage = false, ...attributes }: HeaderProps) {
   return (
     <header className={styles.header} {...attributes}>
-      <Link to={path}>
-        {isOverlayPage && <img src={leftIcon} alt="left back icon" />}
-        <h3>{content}</h3>
-      </Link>
+      <Link to={path}>{isOverlayPage && <img src={leftIcon} alt="left back icon" />}</Link>
+      <h3 className="center-hoz-item">{content}</h3>
     </header>
   );
 }
