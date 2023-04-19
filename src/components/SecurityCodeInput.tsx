60bf51130c59c22c8979b07514a1106c7b938b04
yogjin <youngjin988@gmail.com>
2023-04-19 11:16:59 +0900

    refactor: `SecurityCode` 컴포넌트 추가
    Co-authored-by: regularPark <srk99279@naver.com>

---
+++ src/components/SecurityCodeInput.tsx
@@ -0,0 +1,21 @@
+import { ChangeEventHandler, useState } from 'react';
+import Input from './Input/Input';
+
+function SecurityCodeInput() {
+  const [securityCode, setSecurityCode] = useState('');
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { value } = e.target;
+    const isNumber = !isNaN(Number(value));
+
+    if (!isNumber) {
+      setSecurityCode(value.slice(0, -1));
+      return;
+    }
+    setSecurityCode(value);
+  };
+
+  return <Input value={securityCode} type="password" maxLength={3} onChange={handleChange} />;
+}
+
+export default SecurityCodeInput;
