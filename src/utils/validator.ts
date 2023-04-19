0eb00c4974cf8188989fe185f85049e908310f13
ashleysyheo <ashleysyheo@gmail.com>
2023-04-19 17:29:28 +0900

    feat: input 값 validator 유틸 함수 생성

---
+++ src/utils/validator.ts
@@ -0,0 +1,38 @@
+const validateOwnerName = (input: string) => {
+  return /^\s*[A-Za-z\s]*\s*$/.test(input);
+};
+
+const validateSecurityNumber = (input: string) => {
+  return input.length >= 3 && input.length <= 4;
+};
+
+const validatePassword = (input: string) => {
+  return input.length === 1;
+};
+
+const validateExpirationDate = (input: string) => {
+  const [month, year] = input.split('/');
+
+  if (!year) return false;
+
+  if (month.length !== 2 || year.length !== 2) return false;
+
+  const date = new Date();
+  const currentMonth = (date.getMonth() + 1).toString().padStart(2, '0');
+  const currentYear = date.getFullYear() % 100;
+
+  if (month < currentMonth || Number(month) > 12) return false;
+
+  if (Number(year) < currentYear || Number(year) > Number(currentYear) + 5) return false;
+
+  return true;
+};
+
+const validator = {
+  expirationDate: validateExpirationDate,
+  ownerName: validateOwnerName,
+  securityCode: validateSecurityNumber,
+  password: validatePassword,
+};
+
+export default validator;
