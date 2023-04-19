b57b3dd701c1f780baac2ea5207cb539fdaaaf5b
solo5star <solo_5@naver.com>
2023-04-19 04:54:48 +0000

    feat: Text 컴포넌트에서 weight를 props로 받도록 구현하다
    Co-authored-by: yeopto <gunyeop530@gmail.com>

--- src/components/common/Text.tsx
+++ src/components/common/Text.tsx
@@ -3,24 +3,36 @@ import styled from 'styled-components';
 
 type TextProps = PropsWithChildren<{
   size?: 'small' | 'medium' | 'large';
+  weight?: 'normal' | 'bold';
 }>;
 
 type StyledTextProps = {
   $size: NonNullable<TextProps['size']>;
+  $weight: NonNullable<TextProps['weight']>;
 };
 
-const fontSizes: Record<NonNullable<TextProps['size']>, string> = {
+const fontSizes: Record<StyledTextProps['$size'], string> = {
   small: '12px',
   medium: '14px',
   large: '16px',
 };
 
-const StyledText = styled.div<StyledTextProps>`
+const fontWeights: Record<StyledTextProps['$weight'], number> = {
+  normal: 500,
+  bold: 700,
+};
+
+export const StyledText = styled.div<StyledTextProps>`
   font-size: ${(props) => fontSizes[props.$size]};
+  font-weight: ${(props) => fontWeights[props.$weight]};
 `;
 
 export const Text = (props: TextProps) => {
-  const { size = 'medium', children } = props;
+  const { size = 'medium', weight = 'normal', children } = props;
 
-  return <StyledText $size={size}>{children}</StyledText>;
+  return (
+    <StyledText $size={size} $weight={weight}>
+      {children}
+    </StyledText>
+  );
 };
