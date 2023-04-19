57275f33adf542cf52eb63fd1734e8abbad0ec2b
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-19 12:03:58 +0900

    chore: storybook 설정 파일 수정
    react-router-dom 관련 설정 추가

---
+++ .storybook/preview.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { MemoryRouter } from "react-router";
+import type { Preview } from "@storybook/react";
+
+const customViewports = {
+  Default: {
+    name: "Default",
+    styles: {
+      width: "375px",
+      height: "700px",
+    },
+  },
+};
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    viewport: {
+      viewports: { ...customViewports },
+      defaultViewport: "Defalut",
+    },
+  },
+};
+
+export default preview;
+
+export const decorators = [
+  (Story) => (
+    <MemoryRouter initialEntries={["/"]}>
+      <Story />
+    </MemoryRouter>
+  ),
+];
