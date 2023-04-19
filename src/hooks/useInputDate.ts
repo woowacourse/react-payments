63f4e9e8b394732c2d6a8d246ac9f0a47177aa0b
NaveOWO <pianowell96@gmail.com>
2023-04-19 19:19:57 +0900

    feat : 월 & 년도를 입력하는 로직 구현

---
+++ src/hooks/useInputDate.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import { useError } from "./useError";
+
+export function useInputDate() {
+  const [month, setMonth] = useState<string>("");
+  const [year, setYear] = useState<string>("");
+
+  useEffect(() => {
+    console.log(month);
+  }, [month]);
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    e.target.value = value.padStart(2, "0");
+    if (name === "month") setMonth(value.padStart(2, "0"));
+    else setYear(value.padStart(2, "0"));
+  }
+
+  return { month, year, handleChange };
+}
