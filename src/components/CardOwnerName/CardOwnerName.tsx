9457c9b0753b33c8a11dbd08e0eefa9cbcfc2d31
jeongwusi <jeongwusi@gmail.com>
2023-04-19 22:38:41 +0900

    ✨ feat: CardOwnerName 컴포넌트 구현
    Co-authored-by: 요술토끼 <curious.wzrabbit@gmail.com>

---
+++ src/components/CardOwnerName/CardOwnerName.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import CardInfoInput from '../CardInfoInput/CardInfoInput';
+import Input from '../Input/Input';
+
+type CardOwnerNameProps = {
+  updateCardOwnerName: (cardOwnerName: string) => void;
+};
+
+const CardOwnerName = ({ updateCardOwnerName }: CardOwnerNameProps) => {
+  const [cardOwnerName, setCardOwnerName] = useState('');
+
+  const checkCardOwnerNameLength = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const currentCardOwnerName = e.target.value;
+
+    setCardOwnerName(currentCardOwnerName);
+    updateCardOwnerName(currentCardOwnerName);
+  };
+
+  return (
+    <CardInfoInput title="카드 소유자 이름 (선택)" numberOfLetter={[cardOwnerName.length, 30]}>
+      <Input width="100%" onChange={checkCardOwnerNameLength} maxLength={30} value={cardOwnerName} />
+    </CardInfoInput>
+  );
+};
+
+export default CardOwnerName;
