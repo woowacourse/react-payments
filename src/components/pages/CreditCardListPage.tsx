b8da280a136d321e4e2d50cb31545c99d67401da
solo5star <solo_5@naver.com>
2023-04-19 04:56:26 +0000

    feat: CreditCardListPage에 NewCreditCardButton을 추가하다
    Co-authored-by: yeopto <gunyeop530@gmail.com>

--- src/components/pages/CreditCardListPage.tsx
+++ src/components/pages/CreditCardListPage.tsx
@@ -1,9 +1,20 @@
+import styled from 'styled-components';
+import { NewCreditCardButton } from '../NewCreditCardButton';
 import { Page } from '../common/Page';
+
+const StyledContent = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export const CreditCardListPage = () => {
   return (
     <Page>
       <Page.Header>보유카드</Page.Header>
+      <StyledContent>
+        <NewCreditCardButton helperText />
+      </StyledContent>
     </Page>
   );
 };
