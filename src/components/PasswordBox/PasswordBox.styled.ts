28746a5a947b411d8faddfdb0e185b5dcab77e14
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-04-19 21:46:25 +0900

    feat: PasswordBox 컴포넌트 구현

---
+++ src/components/PasswordBox/PasswordBox.styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const PasswordBox = styled.div``;
+
+export const ErrorMessage = styled.p`
+  color: red;
+`;
---
+++ src/components/PasswordBox/PasswordBox.tsx
@@ -0,0 +1,59 @@
+import { ChangeEvent, useState } from 'react';
+import Input from '../Input/Input';
+import * as styled from './PasswordBox.styled';
+import { isNumeric } from '../../validator';
+
+export interface PasswordsState {
+  first: string;
+  second: string;
+  [key: string]: string;
+}
+
+const PasswordBox = () => {
+  const [passwords, setPasswords] = useState<PasswordsState>({
+    first: '',
+    second: '',
+  });
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const onChange = ({ target: { name, value } }: ChangeEvent<HTMLInputElement>) => {
+    if (!isNumeric(value)) {
+      return setErrorMessage('숫자만 입력하세요');
+    }
+
+    if (errorMessage) setErrorMessage('');
+
+    if (value.length > 1) return;
+
+    setPasswords({
+      ...passwords,
+      [name]: value,
+    });
+  };
+
+  return (
+    <styled.PasswordBox>
+      <label>
+        <span>카드 비밀번호</span>
+        <div>
+          {Object.keys(passwords).map((key) => {
+            return (
+              <Input
+                key={key}
+                name={key}
+                value={passwords[key]}
+                onChange={onChange}
+                width="small"
+                type="text"
+                maxLength={1}
+              />
+            );
+          })}
+        </div>
+      </label>
+      <styled.ErrorMessage>{errorMessage}</styled.ErrorMessage>
+    </styled.PasswordBox>
+  );
+};
+
+export default PasswordBox;
