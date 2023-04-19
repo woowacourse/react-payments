fd9b5fdd54260d4369ed2a856e6a159dfd28e73b
ashleysyheo <ashleysyheo@gmail.com>
2023-04-19 17:25:53 +0900

    refactor: App 컴포넌트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import CardListPage from './pages/CardListPage';
 import CardAddPage from './pages/CardAddPage';
+import { useState } from 'react';
+import { Card } from './types';
 
 function App() {
+  const [cardList, setCardList] = useState<Card[]>([]);
+
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<CardListPage />} />
-        <Route path="/add-card" element={<CardAddPage />} />
+        <Route path="/" element={<CardListPage cardList={cardList} />} />
+        <Route path="/add-card" element={<CardAddPage addCard={setCardList} />} />
       </Routes>
     </div>
   );
