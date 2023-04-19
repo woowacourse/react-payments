20b59a6d84d2af2e229a679f9b9fdcb34df1e0a3
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-19 13:43:54 +0900

    feat: 카드 추가 버튼 컴포넌트 ui 구현

---
+++ src/components/AddCardButton/AddCardButton.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const AddCardButton = () => {
+  return <Button>✚</Button>;
+};
+
+const Button = styled.button`
+  height: 123px;
+  width: 208px;
+
+  border: none;
+
+  background-color: #e5e5e5;
+
+  border-radius: 5px;
+
+  cursor: pointer;
+`;
+export default AddCardButton;
