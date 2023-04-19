6e9ca5fbbbda59357176cd0f1cc1537f93e4f1d3
jeongwusi <jeongwusi@gmail.com>
2023-04-19 22:35:45 +0900

    ✨ feat: CardPreview 컴포넌트 구현
    Co-authored-by: 요술토끼 <curious.wzrabbit@gmail.com>

--- src/components/CardPreview/CardPreview.tsx
+++ src/components/CardPreview/CardPreview.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import styles from './CardPreview.module.css';
 
-const CardPreview = () => {
+type CardPreviewProps = {
+  cardNumber: string;
+  cardOwnerName: string;
+  cardExpirationDate: string;
+};
+
+const CardPreview = ({ cardNumber, cardOwnerName, cardExpirationDate }: CardPreviewProps) => {
   return (
     <div className={styles.container}>
       <div className={styles.card}>
         <div className={styles.chip} />
-        <span className={styles.cardNumber}>1111 2222 3333 4444</span>
+        <span className={styles.cardNumber}>{cardNumber}</span>
         <div className={styles.wrap}>
-          <span className={styles.word}>NAME</span>
-          <span className={styles.word}>MM/YY</span>
+          <span className={styles.word}>{cardOwnerName}</span>
+          <span className={styles.word}>{cardExpirationDate}</span>
         </div>
       </div>
     </div>
