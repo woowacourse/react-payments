405bb1074b97d1aa4f681f51f5de72c15b4087bd
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-19 14:35:24 +0900

    feat: 카드 목록 페이지 컴포넌트 ui구현

---
+++ src/pages/CardListPage/CardListPage.tsx
@@ -0,0 +1,37 @@
+import AddCardButton from "../../components/AddCardButton/AddCardButton";
+import AppBar from "../../components/AppBar/AppBar";
+import CardList from "../../components/CardLIst/CardList";
+import { Container } from "../../components/common";
+
+const CardListPage = () => {
+  return (
+    <Container>
+      <AppBar title="보유카드" />
+      <CardList
+        cards={[
+          {
+            number: {
+              firstGroup: "1234",
+              secondGroup: "1234",
+              thirdGroup: "1234",
+              fourthGroup: "1234",
+            },
+
+            expirationDate: {
+              month: "12",
+              year: "12",
+            },
+
+            ownerName: "aaa",
+            securityCode: "111",
+            password: "12",
+          },
+        ]}
+      />
+      <span>새로운 카드를 등록해주세요.</span>
+      <AddCardButton />
+    </Container>
+  );
+};
+
+export default CardListPage;
