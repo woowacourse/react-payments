ac3dd4400e4ebaf23169431ae7a0c08c03c16f6f
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-04-19 21:05:31 +0900

    feat: 페이지 템플릿 구현

---
+++ src/components/Template/PageTemplate.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../Header';
+
+interface PageTemplateProps {
+  children: React.ReactNode;
+  title: string;
+  onClickBack?: () => void;
+}
+
+const PageTemplate = ({ children, title, onClickBack }: PageTemplateProps) => {
+  return (
+    <PageWrapper>
+      <Header title={title} onClickBack={onClickBack}></Header>
+      {children}
+    </PageWrapper>
+  );
+};
+
+export default PageTemplate;
+
+const PageWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  min-width: 375px;
+  width: 375px;
+
+  padding: 0 28px;
+`;
