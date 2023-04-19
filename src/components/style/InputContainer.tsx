90e82018931197a7f95d8e831fd568d6ae52bc3b
yogjin <youngjin988@gmail.com>
2023-04-19 20:28:32 +0900

    style: `InputContainer` 공통 styled component 추가
    Co-authored-by: regularPark <srk99279@naver.com>

---
+++ src/components/style/InputContainer.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: #ecebf1;
+  border-radius: 7px;
+  padding: 10px;
+  width: 80%;
+
+  Input {
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0);
+    border: none;
+  }
+`;
