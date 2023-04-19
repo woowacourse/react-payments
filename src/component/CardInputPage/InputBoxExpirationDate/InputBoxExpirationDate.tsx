21c89e785d11737189e60bc079708cc9034a91fa
chsua <csj1919@naver.com>
2023-04-19 15:59:27 +0900

    feat: 카드 만료일 입력칸 component css 적용

--- src/component/CardInputPage/InputBoxExpirationDate/InputBoxExpirationDate.tsx
+++ src/component/CardInputPage/InputBoxExpirationDate/InputBoxExpirationDate.tsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 import Input from "../../common/Input";
 
+import "./inputBoxExpirationDate.css";
+
 export default function InputBoxExpirationDate() {
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(true);
 
   return (
-    <div>
+    <div className="input-box-expiration-date">
       <p>만료일</p>
       <Input
+        className="input-expiration-date"
         type="number"
         onChange={() => {}}
         placeholder="MM / YY"
         inputMode="numeric"
       ></Input>
-      <p>error message</p>
+      <p className={error ? "visible" : ""}>error message</p>
     </div>
   );
 }
