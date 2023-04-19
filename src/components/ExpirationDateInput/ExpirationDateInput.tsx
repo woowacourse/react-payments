ab01c5e509cbaec4037fab53aed4ab1cb062e30e
jeongwusi <jeongwusi@gmail.com>
2023-04-19 22:40:17 +0900

    ✨ feat: ExpirationDateInput 컴포넌트 구현
    Co-authored-by: 요술토끼 <curious.wzrabbit@gmail.com>

---
+++ src/components/ExpirationDateInput/ExpirationDateInput.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import CardInfoInput from '../CardInfoInput/CardInfoInput';
+import Input from '../Input/Input';
+
+type ExpirationDateInputProps = {
+  updateExpirationDate: (expirationDate: string) => void;
+};
+
+const ExpirationDateInput = ({ updateExpirationDate }: ExpirationDateInputProps) => {
+  const [expirationDate, setExpirationDate] = useState('');
+
+  const addSlashInExpirationDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (/[^0-9/]/.test(e.target.value)) return alert('숫자만 입력이 가능합니다!');
+
+    const expirationDate = e.target.value;
+    const slashRemovedExpirationDate = expirationDate.replaceAll('/', '');
+    const expirationDateWithSlash = (slashRemovedExpirationDate.match(/.{1,2}/g) || []).join('/');
+
+    setExpirationDate(expirationDateWithSlash);
+    updateExpirationDate(expirationDateWithSlash);
+  };
+
+  return (
+    <CardInfoInput title="만료일">
+      <Input width="40%" onChange={addSlashInExpirationDate} value={expirationDate} maxLength={5} />
+    </CardInfoInput>
+  );
+};
+
+export default ExpirationDateInput;
