4070e81efa1640b3fb6f38b9762b208beaaa93d4
hafnium1923 <hafnium1923@gmail.com>
2023-04-19 15:00:38 +0900

    feat: CardDetailView 컴포넌트 UI 구현
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

---
+++ src/component/CardDetailView/CardDetailView.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CardDetailView from "./index";
+
+export default {
+  component: CardDetailView,
+  title: "CardDetailView",
+};
+
+const Template = () => <CardDetailView />;
+
+export const Primary = Template.bind({});
