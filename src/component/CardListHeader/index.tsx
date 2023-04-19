2ecbc4fc219b9a63343c87b955acd2141ae395ba
hafnium1923 <hafnium1923@gmail.com>
2023-04-19 16:42:17 +0900

    feat: CardListHeader 컴포넌트 UI 구현
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

---
+++ src/component/CardListHeader/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import St from "./styled";
+
+function CardListHeader() {
+  return <St.Title>보유 카드</St.Title>;
+}
+
+export default CardListHeader;
---
+++ src/component/CardListHeader/styled.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const St = {
+  Header: styled.header`
+    align-items: center;
+    display: flex;
+    gap: 18px;
+  `,
+  Title: styled.h1`
+    align-items: center;
+    font: 400 17px "Roboto";
+    letter-spacing: -2px;
+    color: #525252;
+  `,
+};
+
+export default St;
