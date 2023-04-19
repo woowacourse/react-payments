8873601ae4f31f97e96b7dbede021ff8ef4a5097
kangyeongmin <kym6050@hanyang.ac.kr>
2023-04-19 18:08:09 +0900

    refactor: Header Component props 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,15 +1,20 @@
 import styled from "styled-components";
 import { ArrowLeft } from "../../assets";
+import { Link } from "react-router-dom";
 
 interface HeaderType {
   title: string;
-  isBack?: boolean;
+  isBack: boolean;
 }
 
 const Header = (props: HeaderType) => {
   return (
     <HeaderWrapper>
-      {props.isBack && <img src={ArrowLeft} alt={"뒤로가기"} />}
+      {props.isBack && (
+        <Link to="/">
+          <img src={ArrowLeft} alt={"뒤로가기"} />
+        </Link>
+      )}
       <h3>{props.title}</h3>
     </HeaderWrapper>
   );
@@ -21,14 +26,19 @@ const HeaderWrapper = styled.div`
   width: 100%;
   height: 64px;
 
-  background-color: pink;
+  padding-left: 40px;
 
   position: fixed;
   top: 0;
 
   & > h3 {
+    margin-left: 10px;
     font-size: 16px;
     color: #383838;
+  }
+
+  & > a {
+    font-size: 0;
   }
 `;
 
