0d63b701850eedecd3dce2a96bd5139e9ce4d709
chsua <csj1919@naver.com>
2023-04-19 16:01:48 +0900

    feat: 카드 비밀번호 입력칸 component css 적용

--- src/component/CardInputPage/InputBoxPassword/CardPassword.tsx
+++ src/component/CardInputPage/InputBoxPassword/CardPassword.tsx
@@ -1,26 +1,39 @@
 import { useState, ChangeEvent } from "react";
 import Input from "../../common/Input";
 
+import "./cardPassword.css";
+
 interface Props {}
 
 export default function CardPassword(props: Props) {
   return (
-    <>
+    <div className="input-box-card-password">
       <Input
+        className="input-password"
         type="password"
         onChange={() => {}}
         placeholder="X"
         inputMode="numeric"
       ></Input>
       <Input
+        className="input-password"
         type="password"
         onChange={() => {}}
         placeholder="X"
         inputMode="numeric"
       ></Input>
-
-      <input disabled type="password" defaultValue={0} />
-      <input disabled type="password" defaultValue={0} />
-    </>
+      <input
+        className="input-password"
+        disabled
+        type="password"
+        defaultValue={0}
+      />
+      <input
+        className="input-password"
+        disabled
+        type="password"
+        defaultValue={0}
+      />
+    </div>
   );
 }
