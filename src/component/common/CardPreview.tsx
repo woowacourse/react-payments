8c551aafddddfc04bf3fe046e20e62e995cda9fd
chsua <csj1919@naver.com>
2023-04-19 18:54:33 +0900

    feat: 카드 미리보기 컴포넌트 css 적용
    Co-authored-by: Q Kim <qoli0703@gmail.com>

--- src/component/common/CardPreview.tsx
+++ src/component/common/CardPreview.tsx
@@ -1,3 +1,5 @@
+import styles from "./CardPreview.module.css";
+
 interface CreditCard {
   name?: string;
   date?: string;
@@ -15,11 +17,11 @@ export default function CardPreview(props: Props) {
   const { card } = props;
 
   return (
-    <div>
-      <div></div>
-      <span>{card.number ?? ""}</span>
-      <span>{card.name ?? "NAME"}</span>
-      <span>{card.date ?? "MM/YY"}</span>
+    <div className={styles.container}>
+      <div className={styles.chip}></div>
+      <p className={styles.number}>{card.number ?? "1111 1111 1111 1111"}</p>
+      <span className={styles.name}>{card.name ?? "NAME"}</span>
+      <span className={styles.expireDate}>{card.date ?? "MM/YY"}</span>
     </div>
   );
 }
