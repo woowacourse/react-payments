dbebdea832010cd14015d4b9b5d58a5d47b92502
semnil5202 <semnil5202@gmail.com>
2023-04-19 17:26:11 +0900

    feat: CardOwnerName Storybook 추가
    Co-authored-by: Taeeun Kim <taeeun.dev@gmail.com>

---
+++ src/components/CardOwnerName/CardOwnerName.stories.tsx
@@ -0,0 +1,13 @@
+import { StoryObj, Meta } from '@storybook/react';
+import CardOwnerName from './CardOwnerName';
+
+const meta = {
+  component: CardOwnerName,
+  title: 'Section/CardOwnerName',
+} satisfies Meta<typeof CardOwnerName>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const CardOwnerNameStory: Story = {};
