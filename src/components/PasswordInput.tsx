b077410c1bcac5b38782cbd7ca5f119c78358c37
yogjin <youngjin988@gmail.com>
2023-04-19 11:17:26 +0900

    feat: `PasswordInput` 컴포넌트 추가
    Co-authored-by: regularPark <srk99279@naver.com>

---
+++ src/components/PasswordInput.tsx
@@ -0,0 +1,55 @@
+import { ChangeEventHandler, useRef, useState } from 'react';
+import Input from './Input/Input';
+
+function PasswordInput() {
+  const firstDigitRef = useRef<HTMLInputElement>(null);
+  const secondDigitRef = useRef<HTMLInputElement>(null);
+
+  const [firstDigit, setFirstDigit] = useState('');
+  const [secondDigit, setSecondDigit] = useState('');
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (e.target === firstDigitRef.current) {
+      const { value } = e.target;
+      const isNumber = !isNaN(Number(value));
+
+      if (!isNumber) {
+        setFirstDigit(value.slice(0, -1));
+        return;
+      }
+      setFirstDigit(value);
+    }
+
+    if (e.target === secondDigitRef.current) {
+      const { value } = e.target;
+      const isNumber = !isNaN(Number(value));
+
+      if (!isNumber) {
+        setSecondDigit(value.slice(0, -1));
+        return;
+      }
+      setSecondDigit(value);
+    }
+  };
+
+  return (
+    <>
+      <Input
+        value={firstDigit}
+        type="password"
+        maxLength={1}
+        ref={firstDigitRef}
+        onChange={handleChange}
+      />
+      <Input
+        value={secondDigit}
+        type="password"
+        maxLength={1}
+        ref={secondDigitRef}
+        onChange={handleChange}
+      />
+    </>
+  );
+}
+
+export default PasswordInput;
