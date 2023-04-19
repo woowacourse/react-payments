5eb4562976a06d0fb3326eafb98e81e086b4c94e
chsua <csj1919@naver.com>
2023-04-19 16:04:39 +0900

    feat: 기본 app css 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import CardInputForm from "./component/CardInputPage/CardInputForm/CardInputForm";
+
 import "./App.css";
+import "./style/reset.css";
+import "./style/palette.css";
 
 function App() {
-  return <div className="App">ddd</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
