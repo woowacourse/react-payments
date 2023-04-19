2c0098842611ecb52b5849529fd42a085e03cda8
ashleysyheo <ashleysyheo@gmail.com>
2023-04-19 17:27:02 +0900

    refactor: CardListPage 수정

--- src/pages/CardListPage.tsx
+++ src/pages/CardListPage.tsx
@@ -1,10 +1,18 @@
 import { Link } from 'react-router-dom';
 import Header from '../components/common/Header/Header';
 import Button from '../components/common/Button/Button';
-function CardListPage() {
+import CardList from '../components/CardList/CardList';
+import { Card } from '../types';
+
+interface CardListPageProps {
+  cardList: Card[];
+}
+
+function CardListPage({ cardList }: CardListPageProps) {
   return (
     <>
       <Header content="나의 카드" />
+      <CardList cardList={cardList} />
       <Link to={'/add-card'}>
         <Button className="add-button">카드 추가하기</Button>
       </Link>
