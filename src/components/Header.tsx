64ff2473504b8241bf63665ea6324fc4f087d3ba
turtle601 <turtle601@naver.com>
2023-04-19 11:13:25 +0900

    fix: 보유카드 헤더일 때 뒤로가기 버튼 생기는 현상 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Header = ({ title }: { title: string }) => {
   return (
     <StyledHeader>
-      <StyledLeftArrowButton />
+      <StyledLeftArrowButton title={title} />
       <StyledHeaderTitle>{title}</StyledHeaderTitle>
     </StyledHeader>
   );
