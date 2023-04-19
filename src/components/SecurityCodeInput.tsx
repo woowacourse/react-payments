bdaa34120764d9ea60c8f831ddc238993356de38
kyw0716 <kyw0716@naver.com>
2023-04-19 16:39:12 +0900

    feat: SecurityCodeInput 컴포넌트 구현
    Co-authored-by: HyeryongChoi <chex1004@gmail.com>

--- src/components/SecurityCodeInput.tsx
+++ src/components/SecurityCodeInput.tsx
@@ -1,3 +1,76 @@
-export const SecurityCodeInput = () => {
-  return <input />;
+import { useState, useRef } from 'react';
+import { InputWrapper } from './InputWrapper';
+import { Input } from './Input';
+import styled from 'styled-components';
+
+export function SecurityCodeInput() {
+  const [securityCode, setSecurityCode] = useState('');
+
+  const securityCodeInputRef = useRef<HTMLInputElement>(null);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!isNumeric(e.target.value)) {
+      e.target.value = '';
+
+      alert('유효한 보안 코드가 아닙니다.');
+    }
+
+    setSecurityCode(e.target.value.toUpperCase());
+  };
+
+  const validateSecurityCode = () => {
+    if (!isValidSecurityCode(securityCode) && securityCode !== '') {
+      alert('유효한 보안 코드가 아닙니다.');
+
+      setSecurityCode('');
+      securityCodeInputRef.current?.focus();
+    }
+  };
+
+  return (
+    <>
+      <Style.Label>
+        <Style.Title>보안 코드(CVC/CVV)</Style.Title>
+      </Style.Label>
+      <InputWrapper width={84}>
+        <Input
+          ref={securityCodeInputRef}
+          value={securityCode}
+          width={'84'}
+          minLength={3}
+          maxLength={3}
+          placeholder="•••"
+          onChange={handleInputChange}
+          onBlur={validateSecurityCode}
+          type="password"
+          required
+        />
+      </InputWrapper>
+    </>
+  );
+}
+
+const isNumeric = (input: string) => {
+  return /^[0-9]*$/.test(input);
+};
+
+const isValidSecurityCode = (input: string) => {
+  return input.length === 3;
+};
+
+const Style = {
+  Label: styled.div`
+    width: 318px;
+
+    display: flex;
+    justify-content: space-between;
+
+    font-size: 12px;
+  `,
+  NameLength: styled.span`
+    color: #2f2f2f;
+  `,
+  Title: styled.span`
+    color: #2f2f2f;
+  `,
 };
