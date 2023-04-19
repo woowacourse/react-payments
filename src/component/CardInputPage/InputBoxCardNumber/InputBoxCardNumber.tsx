0dcca8afa54bfe9dded05da1b014be3951ce45a6
chsua <csj1919@naver.com>
2023-04-19 15:59:15 +0900

    feat: 카드 번호 입력칸 component css 적용

--- src/component/CardInputPage/InputBoxCardNumber/InputBoxCardNumber.tsx
+++ src/component/CardInputPage/InputBoxCardNumber/InputBoxCardNumber.tsx
@@ -1,18 +1,20 @@
 import { ChangeEvent, useState } from "react";
 import CardNumber from "./CardNumber";
 
+import "./inputBoxCardNumber.css";
+
 interface Props {
   //   setCardNumber?: React.Dispatch<React.SetStateAction<number[]>>;
 }
 
 export default function InputBoxCardNumber() {
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(true);
 
   return (
-    <div>
+    <div className="input-box-card-number">
       <p>카드번호</p>
-      <CardNumber />
-      <p>에러메세지</p>
+      <CardNumber setError={setError} />
+      <p className={error ? "visible" : ""}>에러</p>
     </div>
   );
 }
