dded5d4792262eeeb80a3debf13c8699f7cab8b9
hafnium1923 <hafnium1923@gmail.com>
2023-04-19 17:22:41 +0900

    test: CardList 컴포넌트 story 생성
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

---
+++ src/component/CardList/CardList.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CardList from "./index";
+
+export default {
+  component: CardList,
+  title: "CardList",
+};
+
+const Template = () => <CardList />;
+
+export const Page = Template.bind({});
