84bcc632229e564dec180c01dca240f0acdbb661
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-19 14:39:10 +0900

    test: 카드 목록 페이지 컴포넌트 스토리 추가

---
+++ src/pages/CardListPage/CardListpage.stories.tsx
@@ -0,0 +1,17 @@
+import { Layout } from "../../App";
+import GlobalStyle from "../../styled/GlobalStyle";
+import CardListPage from "./CardListPage";
+
+export default {
+  title: "CardListPage",
+  component: CardListPage,
+};
+
+export const Default = () => (
+  <>
+    <GlobalStyle />
+    <Layout>
+      <CardListPage />
+    </Layout>
+  </>
+);
