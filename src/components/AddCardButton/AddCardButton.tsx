1ad98e6439c9ddedc61ae760cca46ed0509f8b64
jeongwusi <jeongwusi@gmail.com>
2023-04-20 15:48:51 +0900

    ✨ feat: AddCardButton 메시지 공개 여부 기능 추가
    Co-authored-by: 요술토끼 <curious.wzrabbit@gmail.com>

--- src/components/AddCardButton/AddCardButton.tsx
+++ src/components/AddCardButton/AddCardButton.tsx
@@ -3,10 +3,14 @@ import styles from './AddCardButton.module.css';
 import { GrAdd } from 'react-icons/gr';
 import { Link } from 'react-router-dom';
 
-const AddCardButton = () => {
+type AddCardButtonProps = {
+  showMessage: boolean;
+};
+
+const AddCardButton = ({ showMessage }: AddCardButtonProps) => {
   return (
     <div className={styles.container}>
-      <p className={styles.title}>새로운 카드를 등록해주세요.</p>
+      {showMessage && <p className={styles.title}>새로운 카드를 등록해주세요.</p>}
       <Link to="/card-registration">
         <button className={styles.card}>
           <GrAdd />
