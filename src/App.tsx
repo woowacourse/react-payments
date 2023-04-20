12ba1d4c423102fa4c1ca3048f2b0b61111f9f75
hafnium1923 <hafnium1923@gmail.com>
2023-04-20 14:33:27 +0900

    feat: 페이지 이동 기능 구현
    Co-Authored-By: KangSan Lee <xylitol79@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CardDetailPage from "./component/CardDetailPage";
+import CardListPage from "./component/CardListPage";
+
+type CreditCard = {
+  cardNumberOrigin: string;
+  cardNumberHidden: string;
+  cardDate: string;
+  cardOwnerName: string;
+  cardCVC: string;
+  cardPassword: [string, string];
+};
 
 function App() {
+  const [creditCardList, setCreditCardList] = useState<CreditCard[]>([]);
+
+  const addCreditCard = (card: CreditCard) => {
+    setCreditCardList([...creditCardList, card]);
+
+    console.log(creditCardList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<CardListPage creditCardList={creditCardList} />}
+          />
+          <Route
+            path="/addCardForm"
+            element={<CardDetailPage addCreditCard={addCreditCard} />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
