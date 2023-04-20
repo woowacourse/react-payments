d48ce5e0e1adb79d752ad89ab04631eec208a600
chsua <csj1919@naver.com>
2023-04-20 13:01:37 +0900

    feat: 카드 만료일 입력 완료여부상태 관리 구현

--- src/component/CardInputPage/InputBoxExpirationDate/InputBoxExpirationDate.tsx
+++ src/component/CardInputPage/InputBoxExpirationDate/InputBoxExpirationDate.tsx
@@ -1,11 +1,22 @@
-import { useState, ChangeEvent } from "react";
+import { useState, useEffect, ChangeEvent } from "react";
 import Input from "../../common/Input";
 
 import "./inputBoxExpirationDate.css";
 import { validateExpirationDate } from "../../../validation/ExpirationDate";
+interface Props {
+  setIsComplete: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-export default function InputBoxExpirationDate() {
-  const [error, setError] = useState(true);
+const INPUT_STATUS = {
+  ERROR: 0,
+  NOT_COMPLETE: 1,
+  COMPLETE: 2,
+};
+
+export default function InputBoxExpirationDate(props: Props) {
+  const { setIsComplete } = props;
+
+  const [inputStatus, setInputStatus] = useState(INPUT_STATUS.NOT_COMPLETE);
 
   const changeExpirationDate = (e: ChangeEvent<HTMLInputElement>) => {
     const originDate = e.target.value.split("/").join("").slice(0, 4);
@@ -17,8 +28,18 @@ export default function InputBoxExpirationDate() {
     e.target.value = formattedDate;
     if (e.target.value.length > 5) e.target.value = e.target.value.slice(0, 5);
 
-    setError(!validateExpirationDate(e.target.value));
+    if (validateExpirationDate(e.target.value)) {
+      e.target.value.length === 5
+        ? setInputStatus(INPUT_STATUS.COMPLETE)
+        : setInputStatus(INPUT_STATUS.NOT_COMPLETE);
+    } else {
+      setInputStatus(INPUT_STATUS.ERROR);
+    }
   };
+
+  useEffect(() => {
+    setIsComplete(inputStatus === INPUT_STATUS.COMPLETE ? true : false);
+  }, [inputStatus]);
 
   return (
     <div className="input-box-expiration-date">
@@ -30,7 +51,9 @@ export default function InputBoxExpirationDate() {
         placeholder="MM / YY"
         inputMode="numeric"
       ></Input>
-      <p className={error ? "visible" : ""}>error message</p>
+      <p className={inputStatus === INPUT_STATUS.ERROR ? "visible" : ""}>
+        error message
+      </p>
     </div>
   );
 }
