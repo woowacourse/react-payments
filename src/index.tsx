8de71f4d788f70d2faee3849e5824301dafe5d0a
cruelladevil <dev.timetravel@gmail.com>
2023-04-20 17:07:44 +0900

    chore: 배포를 위한 basename 추가

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
