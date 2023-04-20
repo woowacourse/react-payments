dbf4fbff6c39b8eb221fa0be18730412c1a067b8
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-04-20 14:02:59 +0900

    refactor: input state 구조 객체로 변경

--- src/components/Page/CardRegisterPage.tsx
+++ src/components/Page/CardRegisterPage.tsx
@@ -1,25 +1,28 @@
 import { useState } from 'react';
 import Card from '../Common/Card';
-import CardNumberInput from '../Input/CardNumberInput';
-import ExpireDateInput from '../Input/ExpireDateInput';
-import OwnerNameInput from '../Input/OwnerNameInput';
-import SecurityCodeInput from '../Input/SecurityCodeInput';
-import CardPasswordInput from '../Input/CardPasswordInput';
+import CardNumberInput from '../Box/InputSection/CardNumberInput';
+import ExpireDateInput from '../Box/InputSection/ExpireDateInput';
+import OwnerNameInput from '../Box/InputSection/OwnerNameInput';
+import SecurityCodeInput from '../Box/InputSection/SecurityCodeInput';
+import CardPasswordInput from '../Box/InputSection/CardPasswordInput';
 import styled from 'styled-components';
-import { CardType } from '../../types';
+import { CardType, PageProps } from '../../types';
 import PageTemplate from '../Template/PageTemplate';
 
-interface CardRegisterPageProps {
-  navigate: (page: string) => void;
+interface CardFormState extends Omit<CardType, 'id' | 'cardPassword'> {
+  cardPassword1: string;
+  cardPassword2: string;
 }
 
-const CardRegisterPage = ({ navigate }: CardRegisterPageProps) => {
-  const [cardNumber, setCardNumber] = useState<string[]>(Array(4).fill(''));
-  const [expireDate, setExpireDate] = useState<string[]>(Array(2).fill(''));
-  const [ownerName, setOwnerName] = useState('');
-  const [securityCode, setSecurityCode] = useState('');
-  const [cardPassword1, setCardPassword1] = useState('');
-  const [cardPassword2, setCardPassword2] = useState('');
+const CardRegisterPage = ({ navigate }: PageProps) => {
+  const [card, setCard] = useState<CardFormState>({
+    cardNumber: ['', '', '', ''],
+    expireDate: ['', ''],
+    ownerName: '',
+    securityCode: '',
+    cardPassword1: '',
+    cardPassword2: '',
+  });
 
   const submitNewCard = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -33,10 +36,7 @@ const CardRegisterPage = ({ navigate }: CardRegisterPageProps) => {
       cardPassword: cardPassword1 + cardPassword2,
     };
 
-    console.log(newCard);
-
     const cardList: CardType[] = JSON.parse(localStorage.getItem('cardList') || '[]');
-    console.log(cardList);
     cardList.push(newCard);
     localStorage.setItem('cardList', JSON.stringify(cardList));
     navigate('list');
@@ -46,34 +46,28 @@ const CardRegisterPage = ({ navigate }: CardRegisterPageProps) => {
     navigate('list');
   };
 
+  const onChange = (key: keyof CardFormState) => (value: string | string[]) => {
+    setCard((prev) => ({ ...prev, [key]: value }));
+  };
+
+  const { cardNumber, expireDate, ownerName, securityCode, cardPassword1, cardPassword2 } = card;
+
   return (
     <PageTemplate title={'카드 추가'} onClickBack={onClickBack}>
       <Card cardNumber={cardNumber} ownerName={ownerName} expireDate={expireDate} />
       <InputForm onSubmit={submitNewCard}>
-        <CardNumberInput
-          inputValues={cardNumber}
-          setInputValues={(val) => setCardNumber(val as string[])}
-        />
-        <ExpireDateInput
-          inputValues={expireDate}
-          setInputValues={(val) => setExpireDate(val as string[])}
-        />
-        <OwnerNameInput
-          inputValues={ownerName}
-          setInputValues={(val) => setOwnerName(val as string)}
-        />
-        <SecurityCodeInput
-          inputValues={securityCode}
-          setInputValues={(val) => setSecurityCode(val as string)}
-        />
+        <CardNumberInput inputValues={cardNumber} setInputValues={onChange('cardNumber')} />
+        <ExpireDateInput inputValues={expireDate} setInputValues={onChange('expireDate')} />
+        <OwnerNameInput inputValues={ownerName} setInputValues={onChange('ownerName')} />
+        <SecurityCodeInput inputValues={securityCode} setInputValues={onChange('securityCode')} />
         <CardPasswordInput
           cardPassword1Props={{
             inputValues: cardPassword1,
-            setInputValues: (val) => setCardPassword1(val as string),
+            setInputValues: onChange('cardPassword1'),
           }}
           cardPassword2Props={{
             inputValues: cardPassword2,
-            setInputValues: (val) => setCardPassword2(val as string),
+            setInputValues: onChange('cardPassword2'),
           }}
         />
         <ButtonWrapper>
