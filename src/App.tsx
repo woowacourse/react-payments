3f3dcb7adb3bf15299c458320f7fd5b51586993c
woo-jk <201702042@o.cnu.ac.kr>
2023-04-20 16:58:21 +0900

    build: 배포용 빌드

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import styled from 'styled-components';
-import CardListPage from './components/pages/CardListPage';
-import CardRegistrationPage from './components/pages/CardRegistrationPage';
-import { CardItemInfo } from './types/Card';
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import CardListPage from "./components/pages/CardListPage";
+import CardRegistrationPage from "./components/pages/CardRegistrationPage";
+import { CardItemInfo } from "./types/Card";
 
 function App() {
   const [cardList, setCardList] = useState<CardItemInfo[]>([]);
@@ -14,14 +14,11 @@ function App() {
   };
 
   return (
-    <AppContainer className='App'>
+    <AppContainer className="App">
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
-          <Route path='/' element={<CardListPage cardList={cardList} />} />
-          <Route
-            path='/register'
-            element={<CardRegistrationPage addCardItem={addCardItem} />}
-          />
+          <Route path="/" element={<CardListPage cardList={cardList} />} />
+          <Route path="/register" element={<CardRegistrationPage addCardItem={addCardItem} />} />
         </Routes>
       </BrowserRouter>
     </AppContainer>
