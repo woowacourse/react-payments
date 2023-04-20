a3f3c8cd5799db0c1e944e687b0a815a7ed4adf4
chsua <csj1919@naver.com>
2023-04-20 15:11:03 +0900

    feat: 보유 중인 카드 목록 컴포넌트 구현

---
+++ src/component/CardListPage/CardList/CardList.tsx
@@ -0,0 +1,31 @@
+import CardPreview from "../../common/CardPreview";
+import AddCardButton from "../AddCardButton/AddCardButton";
+import "./cardList.css";
+
+interface CreditCard {
+  name?: string;
+  date?: string;
+  bank?: string;
+  number?: number[];
+  securityCode?: number;
+  password?: number;
+}
+
+interface Props {
+  cardList: CreditCard[];
+}
+
+export default function CardList(props: Props) {
+  const { cardList } = props;
+  return (
+    <div className="card-list">
+      {cardList.map((card) => CardPreview({ card }))}
+      {cardList.length === 0 && (
+        <p className="add-card-button-explanation">
+          새로운 카드를 등록해주세요.
+        </p>
+      )}
+      <AddCardButton />
+    </div>
+  );
+}
