268b528e138eaf2ee63772519453a870624140e7
ashleysyheo <ashleysyheo@gmail.com>
2023-04-20 16:55:05 +0900

    chore: BrowserRouter 배포 설정 추가

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
