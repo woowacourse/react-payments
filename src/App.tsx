608b00f4f7bec479c14c80ac232e5c1505baadf1
chsua <csj1919@naver.com>
2023-04-20 15:15:04 +0900

    feat: App에 라우터를 이용한 페이지 이동 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import CardInputForm from "./component/CardInputPage/CardInputForm/CardInputForm";
-
 import "./App.css";
 import "./style/reset.css";
 import "./style/palette.css";
 
+import CardInputPage from "./component/CardInputPage/CardInputPage";
+import CardListPage from "./component/CardListPage/CardListPage";
+
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CardListPage />} />
+          <Route path="/CardListPage" element={<CardListPage />} />
+          <Route path="/CardInputPage" element={<CardInputPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
