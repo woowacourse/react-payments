57967cfa3bdecd03e1b45c12b73bc41db8e88dc6
hae-on <solbi2004@naver.com>
2023-04-20 10:47:26 +0900

    feat: 카드 비밀번호 input 예외사항 구현
    Co-authored-by: Jungkyun Woo <woo-jk@users.noreply.github.com>

--- src/components/inputs/PasswordInput.tsx
+++ src/components/inputs/PasswordInput.tsx
@@ -3,16 +3,64 @@ import Input from '../common/Input';
 import InputBox from '../common/InputBox';
 import InputGroup from '../common/InputGroup';
 import { DotIcon } from '../../assets/icons';
+import { useState, useRef } from 'react';
 
 const PasswordInput = () => {
+  const [inputs, setInputs] = useState(['', '']);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const refs = [useRef<HTMLInputElement>(null), useRef<HTMLInputElement>(null)];
+
+  const handleChangeInput =
+    (inputIndex: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      const inputValue = event.target.value;
+
+      if (isNotInputNumber(inputValue)) {
+        setErrorMessage('숫자만 입력해주세요');
+        return;
+      }
+      if (isOverLength(inputValue)) return;
+
+      const newInputs = [...inputs];
+      newInputs[inputIndex] = inputValue;
+
+      setInputs(newInputs);
+
+      if (isNextInputFocusable(inputValue, inputIndex)) {
+        refs[inputIndex + 1].current?.focus();
+      }
+    };
+
+  const isNotInputNumber = (inputValue: string) => {
+    return Number.isNaN(Number(inputValue));
+  };
+
+  const isOverLength = (inputValue: string) => {
+    return inputValue.length > 1;
+  };
+
+  const isNextInputFocusable = (inputValue: string, inputIndex: number) => {
+    return inputIndex === 0 && inputValue.length > 0;
+  };
+
   return (
-    <InputGroup labelValue='카드 비밀번호'>
+    <InputGroup labelValue='카드 비밀번호' errorMessage={errorMessage}>
       <BoxContainer>
-        <InputBox width='43px'>
-          <Input type='password'></Input>
+        <InputBox width='43px' isError={!!errorMessage}>
+          <Input
+            type='password'
+            ref={refs[0]}
+            value={inputs[0]}
+            onChange={handleChangeInput(0)}
+          ></Input>
         </InputBox>
-        <InputBox width='43px'>
-          <Input type='password'></Input>
+        <InputBox width='43px' isError={!!errorMessage}>
+          <Input
+            type='password'
+            ref={refs[1]}
+            value={inputs[1]}
+            onChange={handleChangeInput(1)}
+          ></Input>
         </InputBox>
         <DotIcon />
         <DotIcon />
