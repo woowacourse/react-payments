57aa3bc318e8db5076808a73a9b7d60a40429147
JIWON <afds4567@gmail.com>
2023-04-20 15:38:53 +0900

    feat: useLocalStorage hook 구현

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export const useLocalStorage = <T,>(
+  initialValue: T,
+  key: string
+): [T, (data: T) => void] => {
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    const savedData = JSON.parse(localStorage.getItem(key) || "[]");
+    if (savedData && savedData.length > 0) setValue(savedData);
+  }, []);
+
+  const setState = (data: T) => {
+    setValue(data);
+    localStorage.setItem(key, JSON.stringify(data));
+  };
+
+  return [value, setState];
+};
