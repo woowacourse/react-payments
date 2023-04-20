fb0525a29376cd25a0db5b8e42d16c3f4b928233
n0eyes <doll4586633@naver.com>
2023-04-20 17:12:13 +0900

    feat: 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CardRegister from './pages/CardRegister/CardRegister';
 import MyCardList from './pages/MyCardList/MyCardList';
+
+import Layout from './components/@common/Layout/Layout';
 
 function App() {
   return (
     <>
-      <MyCardList />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index path="/" element={<MyCardList />} />
+            <Route path="/registerCard" element={<CardRegister />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
