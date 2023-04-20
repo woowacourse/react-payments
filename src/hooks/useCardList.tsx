c5acf13548a83319f6770eed2a2b9284beb38fe0
hozzijeong <ghwls0905@naver.com>
2023-04-20 15:22:21 +0900

    feat: CardList 저장 및 조회 hook 구현
    Co-authored-by: ukkodeveloper <ukkodeveloper@gmail.com>

---
+++ src/hooks/useCardList.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { CardInfoType } from "src/components/RegisterForm/Main";
+
+interface Props {
+  key: string;
+  initialValue?: Array<CardInfoType>;
+}
+
+function useCardList({ key, initialValue }: Props) {
+  const saved = localStorage.getItem(key);
+  const savedList = (saved ? JSON.parse(saved) : []) as Array<CardInfoType>;
+
+  const [cardList, setCardList] = useState<Array<CardInfoType>>([
+    ...(initialValue ?? []),
+    ...savedList,
+  ]);
+
+  const saveCard = (cardInfo: CardInfoType) => {
+    const savedData = JSON.stringify([...cardList, cardInfo]);
+    localStorage.setItem(key, savedData);
+
+    setCardList((prev) => [...prev, cardInfo]);
+  };
+
+  return { cardList, saveCard };
+}
+
+export default useCardList;
