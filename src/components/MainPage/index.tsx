26fe60f6147662afea5a3d063fedcbfbebf35338
Dain Lee <feb.dain@gmail.com>
2023-04-20 20:48:37 +0900

    design: 메인 페이지 CSS 수정

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -21,20 +21,35 @@ const MainPage = () => {
   });
 
   return (
-    <div>
+    <S.Wrapper>
       <Header navigator={false} title="보유카드" />
 
-      {cardList.map((card, index) => (
-        <CardPreview key={index} cardInfo={card} />
-      ))}
+      <S.Main>
+        {cardList.map((card, index) => (
+          <CardPreview key={index} cardInfo={card} />
+        ))}
 
-      {!cardList.length && <S.Message>새로운 카드를 등록해 주세요.</S.Message>}
-      <S.Button onClick={goToRegister}>+</S.Button>
-    </div>
+        {!cardList.length && (
+          <S.Message>새로운 카드를 등록해 주세요.</S.Message>
+        )}
+
+        <S.Button onClick={goToRegister}>+</S.Button>
+      </S.Main>
+    </S.Wrapper>
   );
 };
 
 const S = {
+  Wrapper: styled.div`
+    width: 88vw;
+  `,
+
+  Main: styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `,
+
   Message: styled.p`
     margin: 34px 0 12px;
   `,
