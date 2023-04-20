10fedc52484c1ad05230e30a80e8f0a875813cb7
Q Kim <qoli0703@gmail.com>
2023-04-20 17:21:49 +0900

    chore: 빌드 후 자동 배포를 위한 패키지 설치

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<CardListPage cardList={cardList} />} />
           <Route
