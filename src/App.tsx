314070b5bcb60e76219bac2d64ebc8cd5c1755ea
chsua <csj1919@naver.com>
2023-04-20 16:29:13 +0900

    feat: 등록된 카드 목록 state 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,42 @@ import "./App.css";
 import "./style/reset.css";
 import "./style/palette.css";
 
+import { useState } from "react";
+
 import CardInputPage from "./component/CardInputPage/CardInputPage";
 import CardListPage from "./component/CardListPage/CardListPage";
 
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+interface CreditCard {
+  name?: string;
+  date?: string;
+  bank?: string;
+  number?: number[];
+  securityCode?: number;
+  password?: number;
+}
+
 function App() {
+  const [cardList, setCardList] = useState<CreditCard[]>([]);
+
+  const addNewCard = (card: CreditCard) => {
+    setCardList([...cardList, card]);
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<CardListPage />} />
-          <Route path="/CardListPage" element={<CardListPage />} />
-          <Route path="/CardInputPage" element={<CardInputPage />} />
+          <Route path="/" element={<CardListPage cardList={cardList} />} />
+          <Route
+            path="/CardListPage"
+            element={<CardListPage cardList={cardList} />}
+          />
+          <Route
+            path="/CardInputPage"
+            element={<CardInputPage addNewCard={addNewCard} />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
