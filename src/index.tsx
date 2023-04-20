2cc09647995f81d3515d9f61ea8cb4aab2030f1c
n0eyes <doll4586633@naver.com>
2023-04-20 17:12:25 +0900

    feat: 전역 테마 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyles from './styles/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <>
-      <GlobalStyles />
+    <GlobalStyles />
+    <ThemeProvider theme={theme}>
       <App />
-    </>
+    </ThemeProvider>
   </React.StrictMode>
 );
