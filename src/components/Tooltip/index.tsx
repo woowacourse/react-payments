dbc87921a7e898d8f6dc04362e790c32a5f811b8
Suyoung <jsy8492@gmail.com>
2023-04-20 11:51:34 +0900

    feat: Tooltip 컴포넌트 작성

---
+++ src/components/Tooltip/index.tsx
@@ -0,0 +1,51 @@
+import {
+  MouseEventHandler,
+  ReactNode,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+
+import styles from './tooltip.module.css';
+
+interface Props {
+  children: ReactNode;
+}
+
+const Tooltip = ({ children }: Props) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [isShow, setIsShow] = useState(false);
+
+  const handleClick: MouseEventHandler<HTMLDivElement> = () => {
+    setIsShow((prev) => !prev);
+  };
+
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      setIsShow(false);
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.body.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
+
+  return (
+    <div className={styles.container} onClick={handleClick} ref={ref}>
+      {children}
+      {isShow && (
+        <div className={styles.message}>
+          <span>
+            카드 뒷면의 서명란에 인쇄된 숫자 끝 3자리가 CVC 번호입니다.
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Tooltip;
