c246c7fb0458d1aedad543fe58fd9ffc7d29893b
jeongwusi <jeongwusi@gmail.com>
2023-04-20 15:57:07 +0900

    ✨ feat: Home.tsx props 추가
    Co-authored-by: 요술토끼 <curious.wzrabbit@gmail.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
 import AddCardButton from '../components/AddCardButton/AddCardButton';
+import CardList from '../components/CardList/CardList';
 import EmptyHeader from '../components/EmptyHeader/EmptyHeader';
+import { CardInfo } from '../types';
 
-const Home = () => {
+type HomeProps = {
+  cardInfo: CardInfo[];
+};
+
+const Home = ({ cardInfo }: HomeProps) => {
   return (
     <>
       <EmptyHeader />
-      <AddCardButton />
+      <CardList cardInfo={cardInfo} />
+      <AddCardButton showMessage={cardInfo.length === 0} />
     </>
   );
 };
