c6a4d27eb5a4eb561ff694489d4f05afa4d15cd1
n0eyes <doll4586633@naver.com>
2023-04-20 17:06:44 +0900

    feat: CardNameInput 컴포넌트 추가

---
+++ src/components/pages/CardRegister/CardNameInput/CardNameInput.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useCardRegisterContext } from '../../../../context/CardRegisterContext';
+import { useCardName } from '../../../../hooks/card/card';
+import { CardRegisterInfo } from '../../../../types/card.type';
+import Flex from '../../../@common/Flex/Flex';
+import Input from '../../../@common/Input/Input';
+import * as Styled from './CardNameInput.styles';
+
+export default function CardNameInput() {
+  const {
+    cardRegisterInfo: { holderName },
+    handleCardInfo,
+  } = useCardRegisterContext();
+  const { defaultConditions } = useCardName();
+
+  const onChangeValue = (value: CardRegisterInfo['holderName']) => {
+    handleCardInfo('holderName', value);
+  };
+
+  return (
+    <Flex dir="column">
+      <Input>
+        <Flex justify="space-between">
+          <Input.Label asChild>
+            <Styled.Label>카드소유자이름(선택)</Styled.Label>
+          </Input.Label>
+          <Input.Limit limit={30}>
+            {({ value, limit }) => (
+              <span>
+                {value.length}/{limit}
+              </span>
+            )}
+          </Input.Limit>
+        </Flex>
+        <Styled.InputBackground>
+          <Input.Field
+            name="name"
+            id="name"
+            value={holderName}
+            onChange={({ target: { value } }) => {
+              onChangeValue(value);
+            }}
+            {...defaultConditions}>
+            <Styled.Input />
+          </Input.Field>
+        </Styled.InputBackground>
+      </Input>
+    </Flex>
+  );
+}
