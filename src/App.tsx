bec7aa61e7b4b6979991623c3837696d247d1c71
Dahye <06robin11@gmail.com>
2023-04-20 16:58:44 +0900

    chore: build

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home cards={cards} />} />
           <Route path="/addCard" element={<AddCard setCards={setCards} />} />
