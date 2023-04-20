3e6f63b9c5f1f438274fe2f3f2e388af463cbfb7
n0eyes <doll4586633@naver.com>
2023-04-20 17:00:26 +0900

    feat: theme 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  light: {
+    border: '#ccc',
+    color: '#333',
+    backgroundColor: '#f8f8f8',
+  },
+  dark: {
+    border: '#333',
+    color: '#fff',
+    backgroundColor: '#111',
+  },
+  blue: {
+    border: 'blue',
+    color: 'white',
+    backgroundColor: 'blue',
+  },
+  pale: {
+    border: '#ccc',
+    color: '#333',
+    backgroundColor: '#ECEBF1',
+  },
+};
