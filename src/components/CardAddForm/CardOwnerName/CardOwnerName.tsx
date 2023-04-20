49a482644280568198b8f428a77a82303bedd42f
ashleysyheo <ashleysyheo@gmail.com>
2023-04-20 13:30:54 +0900

    refactor: CardOwnerName 컴포넌트 수정

--- src/components/CardAddForm/CardOwnerName/CardOwnerName.tsx
+++ src/components/CardAddForm/CardOwnerName/CardOwnerName.tsx
@@ -1,42 +1,23 @@
-import Input from '../../common/Input/Input';
-import InputContainer from '../../common/InputContainer/InputContainer';
-import { useInputContainer } from '../../../hooks/useInputContainer';
-import { CardInputValidation } from '../../../types';
-import { formatEnglishName } from '../../../utils/formatter';
 import { ChangeEvent } from 'react';
+import InputContainer from '../../common/InputContainer/InputContainer';
+import Input from '../../common/Input/Input';
 
 interface CardOwnerNameProps {
-  validator: (input: string) => boolean;
-  onValidation: (key: keyof CardInputValidation, value: boolean) => void;
-  onChange: (key: keyof CardInputValidation, value: string) => void;
+  onChange: ({ target: { value, dataset } }: ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
 }
 
-function CardOwnerName({ validator, onValidation, onChange }: CardOwnerNameProps) {
-  const { inputValue, handleInputChange, isError, handleInputBlur } = useInputContainer({
-    formatter: formatEnglishName,
-    validator,
-    onValidation,
-  });
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    handleInputChange(event);
-    onChange('ownerName', event.target.value);
-  };
-
+function CardOwnerName({ onChange, value = '' }: CardOwnerNameProps) {
   return (
-    <InputContainer
-      label="카드 소유자 이름"
-      id="ownerName"
-      supportingText="카드에 표시된 이름과 동일하게 입력해주세요"
-    >
+    <InputContainer label="카드 소유자 이름" id="ownerName" characterCounter={[value.length, 20]}>
       <Input
-        type="text"
         id="ownerName"
-        value={inputValue}
-        maxLength={30}
-        isError={isError}
-        onChange={handleChange}
-        onBlur={handleInputBlur}
+        data-name="ownerName"
+        value={value}
+        placeholder="카드에 표시된 이름과 동일하게 입력해주세요"
+        maxLength={20}
+        onChange={onChange}
+        autoComplete="cc-csc"
       />
     </InputContainer>
   );
