ed4c78a223c40c9204e1e96557b5890df34a81cb
Dahye <06robin11@gmail.com>
2023-04-20 16:03:24 +0900

    feat(cvc):도움말 버튼 생성

--- src/components/cardForm/CVCInput.tsx
+++ src/components/cardForm/CVCInput.tsx
@@ -1,7 +1,7 @@
 import { Container } from "../common/Container";
-
 import { Input } from "../common/Input";
 import { InputLabel } from "../common/InputLabel";
+import styled from "styled-components";
 
 const CVCInfo = {
   label: "cvc",
@@ -22,12 +22,45 @@ export const CVCInput = () => {
   return (
     <Container>
       <InputLabel text="보안 코드 (CVC/CVV)" name="CVC" />
-      <Input
-        {...CVCInfo}
-        handleInput={handleInput}
-        handleChange={(e) => {}}
-        error={{ isValid: true, errorMessage: "" }}
-      />
+      <Wrapper>
+        <Input
+          {...CVCInfo}
+          handleInput={handleInput}
+          handleChange={(e) => {}}
+          error={{ isValid: true, errorMessage: "" }}
+        />
+        <HelpIcon
+          onClick={() => {
+            console.log("CVC설명");
+          }}
+        >
+          ?
+        </HelpIcon>
+      </Wrapper>
     </Container>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  gap: 10px;
+`;
+
+const HelpIcon = styled.div`
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+
+  width: 27px;
+  height: 27px;
+  border: 1px solid darkgray;
+  border-radius: 50%;
+
+  font-size: 15px;
+  font-weight: 700;
+  color: darkgray;
+`;
