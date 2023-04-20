b17b67bc2e3ce10e54bedfb68ca7c8e8c5fa92eb
hozzijeong <ghwls0905@naver.com>
2023-04-20 15:24:04 +0900

    feat: Card Info 작성 폼 컴포넌트 구현
    Co-authored-by: ukkodeveloper <ukkodeveloper@gmail.com>

--- src/components/registerForm/cardRegisterForm/index.tsx
+++ src/components/registerForm/cardRegisterForm/index.tsx
@@ -1,22 +1,61 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { CardNumber } from "../CardNumber";
 import ExpireDate from "../ExpireDate";
 import OwnerNameInput from "../OwnerNameInput";
 import SecurityCode from "../SecurityCode";
 import CardPassword from "../CardPassword";
 import styled from "styled-components";
+import { InputValuesContext } from "../Main";
+import useCardList from "src/hooks/useCardList";
+import { useNavigate } from "react-router-dom";
+
+const objectValueToString = (obj: { [key: string]: string }) => {
+  return Object.values(obj).reduce((acc, cur) => acc + cur, "");
+};
 
 function CardRegisterForm() {
+  const navigation = useNavigate();
+  const [cardInput] = useContext(InputValuesContext);
+  const [nextShow, setNextShow] = useState(false);
+  const { saveCard } = useCardList({ key: "card-list" });
+
+  useEffect(() => {
+    const { cardNumbers, expireDate, securityCode, password, ownerName } =
+      cardInput;
+    try {
+      const exceptOwnerName =
+        objectValueToString(cardNumbers).length !== 16 ||
+        expireDate.length !== 5 ||
+        securityCode.length !== 3 ||
+        objectValueToString(password).length !== 2;
+
+      if ((ownerName.length > 0 && ownerName.length < 3) || exceptOwnerName) {
+        throw new Error();
+      }
+      setNextShow(true);
+    } catch {
+      setNextShow(false);
+    }
+  }, [cardInput]);
+
+  const cardInputSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    saveCard(cardInput);
+    navigation("/card-list");
+  };
+
   return (
-    <Form>
+    <Form onSubmit={cardInputSubmit}>
       <CardNumber />
       <ExpireDate />
       <OwnerNameInput />
       <SecurityCode />
       <CardPassword />
-      <div style={{ display: "flex", justifyContent: "end" }}>
-        <NextButton>다음</NextButton>
-      </div>
+      {nextShow && (
+        <div style={{ display: "flex", justifyContent: "end" }}>
+          <NextButton>다음</NextButton>
+        </div>
+      )}
     </Form>
   );
 }
