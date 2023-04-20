b13a58fe94898910cff679960dc4e4fb98a846f7
gyeongza <gyeongza@naver.com>
2023-04-20 18:02:48 +0900

    chore: 배포 환경 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import MainPage from "components/MainPage";
-import CardRegisterForm from "components/RegisterPage/CardRegisterForm";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from 'components/MainPage';
+import CardRegisterForm from 'components/RegisterPage/CardRegisterForm';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/register" element={<CardRegisterForm />} />
