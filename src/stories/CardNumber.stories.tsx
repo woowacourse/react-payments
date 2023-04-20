2f8a18e514e3f1b218bec1a806bed629a6df4651
chsua <csj1919@naver.com>
2023-04-20 13:01:26 +0900

    test: 카드 번호 입력 완료여부상태 관리 테스트

--- src/stories/CardNumber.stories.tsx
+++ src/stories/CardNumber.stories.tsx
@@ -11,5 +11,5 @@ const meta: Meta = {
 export default meta;
 
 export const InputTest = (args: any) => (
-  <CardNumber setError={() => {}}></CardNumber>
+  <CardNumber setError={() => {}} setIsComplete={() => {}}></CardNumber>
 );
