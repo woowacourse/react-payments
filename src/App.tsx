6dcf327589e7d5a965ee71a57b960e787adf759a
JIWON <afds4567@gmail.com>
2023-04-20 15:41:06 +0900

    feat: 새로운 카드 등록 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,63 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import AddCardPage from "./components/AddCardPage";
 import Homepage from "./components/Homepage";
+import { LOCAL_STORAGE_CARD_KEY } from "./constant";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 import "./styles/index.css";
+import { CardInfo } from "./types";
 export default function App() {
   const [pageIndex, setPageIndex] = useState(0);
-  const [cardList, setCardList] = useState([]);
+  const [cardList, setCardList] = useLocalStorage<CardInfo[]>(
+    [],
+    LOCAL_STORAGE_CARD_KEY
+  );
 
-  const onSubmit = () => {
-    setCardList([]);
+  const onCardInfoSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const {
+      firstCardNumber,
+      secondCardNumber,
+      thirdCardNumber,
+      fourthCardNumber,
+      month,
+      year,
+      owner,
+      cvc,
+      firstPassword,
+      secondPassword,
+    } = event.currentTarget;
+
+    const newCard: CardInfo = {
+      cardNumber: {
+        fisrt: firstCardNumber.value,
+        second: secondCardNumber.value,
+        third: thirdCardNumber.value,
+        fourth: fourthCardNumber.value,
+      },
+      expiracy: {
+        month: month.value,
+        year: year.value,
+      },
+      owner: owner.value,
+      cvc: cvc.value,
+      password: {
+        first: firstPassword.value,
+        second: secondPassword.value,
+      },
+    };
+
+    const updatedCardList = [...cardList, newCard];
+    setCardList(updatedCardList);
     setPageIndex(0);
   };
 
   return (
     <div className="app">
-      {pageIndex === 0 && <Homepage onAddCardClick={() => setPageIndex(1)} />}
-      {pageIndex === 1 && <AddCardPage />}
+      {pageIndex === 0 && (
+        <Homepage cardList={cardList} onClick={() => setPageIndex(1)} />
+      )}
+      {pageIndex === 1 && <AddCardPage onSubmit={onCardInfoSubmit} />}
     </div>
   );
 }
