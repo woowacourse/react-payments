3cf7b0c43e74e80d836c740ea1fa5fc40181deca
solo5star <solo_5@naver.com>
2023-04-20 07:32:28 +0000

    feat: localStorage 저장되도록 구현하다
    Co-authored-by: yeopto <gunyeop530@gmail.com>

--- src/context/PaymentsContext.tsx
+++ src/context/PaymentsContext.tsx
@@ -16,11 +16,19 @@ export const PaymentsContext = createContext<PaymentsContextValue>({
 
 export const PaymentsProvider = (props: PropsWithChildren) => {
   const { children } = props;
-  const [creditCards, setCreditCards] = useState<CreditCard[]>([]);
+  const [creditCards, setCreditCards] = useState<CreditCard[]>(
+    JSON.parse(localStorage.getItem('creditCards') ?? '[]'),
+  );
+
+  const internalSetCreditCards: Dispatch<CreditCard[]> = (nextCreditCards) => {
+    localStorage.setItem('creditCards', JSON.stringify(nextCreditCards));
+
+    setCreditCards(nextCreditCards);
+  };
 
   return (
     // eslint-disable-next-line react/jsx-no-constructed-context-values
-    <PaymentsContext.Provider value={{ creditCards, setCreditCards }}>
+    <PaymentsContext.Provider value={{ creditCards, setCreditCards: internalSetCreditCards }}>
       {children}
     </PaymentsContext.Provider>
   );
