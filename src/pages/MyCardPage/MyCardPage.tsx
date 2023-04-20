cf553191314073febf4cbf4a0b99030d71b4bcdf
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-04-20 13:31:51 +0900

    feat: MyCardPage에서 CardRegisterPage로 넘어가는 로직 구현

--- src/pages/MyCardPage/MyCardPage.tsx
+++ src/pages/MyCardPage/MyCardPage.tsx
@@ -1,11 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import * as styled from './MyCardPage.styled';
 
 const MyCardPage = () => {
+  const navigation = useNavigate();
+
+  const handleClick = () => {
+    navigation('/register');
+  };
+
   return (
     <styled.MyCardPage>
       <styled.CardRegisterButtonContainer>
         <p>새로운 카드를 등록해 주세요.</p>
-        <styled.Card color="black" shadow="0">
+        <styled.Card color="black" shadow="0" onClick={handleClick}>
           <styled.ButtonIcon>+</styled.ButtonIcon>
         </styled.Card>
       </styled.CardRegisterButtonContainer>
