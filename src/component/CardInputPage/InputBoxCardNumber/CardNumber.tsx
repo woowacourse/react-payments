507677821e0335c30562d1ff928913253df86dfb
chsua <csj1919@naver.com>
2023-04-20 13:01:02 +0900

    feat: 카드 번호 입력 완료여부상태 관리 구현

--- src/component/CardInputPage/InputBoxCardNumber/CardNumber.tsx
+++ src/component/CardInputPage/InputBoxCardNumber/CardNumber.tsx
@@ -7,22 +7,45 @@ import "./cardNumber.css";
 interface Props {
   setCardNumber?: React.Dispatch<React.SetStateAction<number[]>>;
   setError: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsComplete: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function CardNumber(props: Props) {
-  const [error1, setError1] = useState(false);
-  const [error2, setError2] = useState(false);
-  const [error3, setError3] = useState(false);
-  const [error4, setError4] = useState(false);
+const INPUT_STATUS = {
+  ERROR: 0,
+  NOT_COMPLETE: 1,
+  COMPLETE: 2,
+};
 
-  const { setError } = props;
+export default function CardNumber(props: Props) {
+  const [inputStatus1, setInputStatus1] = useState(INPUT_STATUS.NOT_COMPLETE);
+  const [inputStatus2, setInputStatus2] = useState(INPUT_STATUS.NOT_COMPLETE);
+  const [inputStatus3, setInputStatus3] = useState(INPUT_STATUS.NOT_COMPLETE);
+  const [inputStatus4, setInputStatus4] = useState(INPUT_STATUS.NOT_COMPLETE);
+
+  const { setError, setIsComplete } = props;
 
   useEffect(() => {
-    setError(error1 || error2 || error3 || error4);
-  }, [error1, error2, error3, error4]);
+    const hasError = [
+      inputStatus1,
+      inputStatus2,
+      inputStatus3,
+      inputStatus4,
+    ].includes(INPUT_STATUS.ERROR);
+
+    setError(hasError);
+
+    const isComplete = [
+      inputStatus1,
+      inputStatus2,
+      inputStatus3,
+      inputStatus4,
+    ].every((status) => status === INPUT_STATUS.COMPLETE);
+
+    setIsComplete(isComplete);
+  }, [inputStatus1, inputStatus2, inputStatus3, inputStatus4]);
 
   const onChangeCardNumber =
-    (setError: React.Dispatch<React.SetStateAction<boolean>>) =>
+    (setInputStatus: React.Dispatch<React.SetStateAction<number>>) =>
     (e: ChangeEvent<HTMLInputElement>) => {
       const value = e.target.value;
 
@@ -30,8 +53,15 @@ export default function CardNumber(props: Props) {
         e.target.value = value.slice(0, 4);
       }
 
-      if (validateCardNumber(e.target.value)) setError(false);
-      else setError(true);
+      if (validateCardNumber(e.target.value)) {
+        setInputStatus(
+          e.target.value.length === 4
+            ? INPUT_STATUS.COMPLETE
+            : INPUT_STATUS.NOT_COMPLETE
+        );
+      } else {
+        setInputStatus(INPUT_STATUS.ERROR);
+      }
     };
 
   return (
@@ -40,29 +70,29 @@ export default function CardNumber(props: Props) {
         className="first input-card-number"
         type="text"
         inputMode="numeric"
-        onChange={onChangeCardNumber(setError1)}
-        placeholder="X X X X"
+        onChange={onChangeCardNumber(setInputStatus1)}
+        placeholder="XXXX"
       />
       <Input
         className=" input-card-number"
         type="password"
         inputMode="numeric"
-        onChange={onChangeCardNumber(setError2)}
-        placeholder="X X X X"
+        onChange={onChangeCardNumber(setInputStatus2)}
+        placeholder="XXXX"
       />
       <Input
         className=" input-card-number"
         type="password"
         inputMode="numeric"
-        onChange={onChangeCardNumber(setError3)}
-        placeholder="X X X X"
+        onChange={onChangeCardNumber(setInputStatus3)}
+        placeholder="XXXX"
       />
       <Input
         className="last input-card-number"
         type="text"
         inputMode="numeric"
-        onChange={onChangeCardNumber(setError4)}
-        placeholder="X X X X"
+        onChange={onChangeCardNumber(setInputStatus4)}
+        placeholder="XXXX"
       />
     </>
   );
--- src/component/CardInputPage/InputBoxCardNumber/InputBoxCardNumber.tsx
+++ src/component/CardInputPage/InputBoxCardNumber/InputBoxCardNumber.tsx
@@ -1,19 +1,21 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import CardNumber from "./CardNumber";
 
 import "./inputBoxCardNumber.css";
 
 interface Props {
-  //   setCardNumber?: React.Dispatch<React.SetStateAction<number[]>>;
+  //   setCardNumber?: React.Dispatch<React.SetStateAction<number[]>>
+  setIsComplete: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function InputBoxCardNumber() {
-  const [error, setError] = useState(true);
+export default function InputBoxCardNumber(props: Props) {
+  const { setIsComplete } = props;
 
+  const [error, setError] = useState(true);
   return (
     <div className="input-box-card-number">
       <p>카드번호</p>
-      <CardNumber setError={setError} />
+      <CardNumber setError={setError} setIsComplete={setIsComplete} />
       <p className={error ? "visible" : ""}>에러</p>
     </div>
   );
