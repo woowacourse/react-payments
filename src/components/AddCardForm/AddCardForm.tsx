63c9242695a5298e9979aaf6964507236958a3fd
jeongwusi <jeongwusi@gmail.com>
2023-04-20 15:50:28 +0900

    ✨ feat: AddCardForm 컴포넌트 데이터 처리 기능 추가
    Co-authored-by: 요술토끼 <curious.wzrabbit@gmail.com>

--- src/components/AddCardForm/AddCardForm.tsx
+++ src/components/AddCardForm/AddCardForm.tsx
@@ -5,16 +5,55 @@ import ExpirationDateInput from '../ExpirationDateInput/ExpirationDateInput';
 import CardOwnerName from '../CardOwnerName/CardOwnerName';
 import CardSecurityCodeInput from '../CardSecurityCodeInput/CardSecurityCodeInput';
 import CardPasswordInput from '../CardPasswordInput/CardPasswordInput';
+import { useNavigate } from 'react-router-dom';
+import type { CardInfo } from '../../types';
 
 type AddCardFormProps = {
   updateCardNumber: (cardNumber: string) => void;
   updateExpirationDate: (expirationDate: string) => void;
   updateCardOwnerName: (cardOwnerName: string) => void;
+  registerNewCard: (cardInfo: CardInfo) => void;
 };
 
-const AddCardForm = ({ updateCardNumber, updateExpirationDate, updateCardOwnerName }: AddCardFormProps) => {
+const AddCardForm = ({
+  updateCardNumber,
+  updateExpirationDate,
+  updateCardOwnerName,
+  registerNewCard,
+}: AddCardFormProps) => {
+  const navigate = useNavigate();
+
+  const handleCardInfo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const cardNumberInput = e.currentTarget.elements.namedItem('cardNumber');
+    const expirationDateInput = e.currentTarget.elements.namedItem('expirationDate');
+    const cardOwnerNameInput = e.currentTarget.elements.namedItem('cardOwnerName');
+
+    if (
+      !(
+        cardNumberInput instanceof HTMLInputElement &&
+        expirationDateInput instanceof HTMLInputElement &&
+        cardOwnerNameInput instanceof HTMLInputElement
+      )
+    ) {
+      alert('폼 전송에 실패했습니다.');
+      return false;
+    }
+
+    const cardInfo: CardInfo = {
+      cardNumber: cardNumberInput.value,
+      cardExpirationDate: expirationDateInput.value,
+      cardOwnerName: cardOwnerNameInput.value,
+    };
+
+    registerNewCard(cardInfo);
+
+    navigate('/');
+  };
+
   return (
-    <form className={styles.container}>
+    <form onSubmit={handleCardInfo} className={styles.container}>
       <CardNumberInput updateCardNumber={updateCardNumber} />
       <ExpirationDateInput updateExpirationDate={updateExpirationDate} />
       <CardOwnerName updateCardOwnerName={updateCardOwnerName} />
