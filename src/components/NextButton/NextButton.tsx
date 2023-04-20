b39919d4158c0baa16b955e6abc378d6a6db5cb3
jeongwusi <jeongwusi@gmail.com>
2023-04-20 15:55:23 +0900

    ♻️ refactor: 다음 버튼 라우터 삭제
    Co-authored-by: 요술토끼 <curious.wzrabbit@gmail.com>

--- src/components/NextButton/NextButton.tsx
+++ src/components/NextButton/NextButton.tsx
@@ -1,12 +1,7 @@
-import { Link } from 'react-router-dom';
 import styles from './NextButton.module.css';
 
 const NextButton = () => {
-  return (
-    <Link to="/">
-      <button className={styles.button}>다음</button>
-    </Link>
-  );
+  return <button className={styles.button}>다음</button>;
 };
 
 export default NextButton;
