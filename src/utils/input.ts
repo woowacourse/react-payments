86552e963048ee8e6195d354e8f90171c38b2b14
n0eyes <doll4586633@naver.com>
2023-04-20 17:09:59 +0900

    feat: Keydown validation 로직 분리

---
+++ src/utils/input.ts
@@ -0,0 +1,24 @@
+import { KeyboardEvent } from 'react';
+
+export const isValidateKey = (
+  event: KeyboardEvent<HTMLInputElement>,
+  pattern: string
+): boolean => {
+  const validKeys = [
+    'Backspace',
+    'Delete',
+    'ArrowLeft',
+    'ArrowRight',
+    'Tab',
+    'Escape',
+    'Enter',
+    'Home',
+    'End',
+  ];
+
+  if (validKeys.includes(event.key) || new RegExp(pattern).test(event.key)) {
+    return true;
+  }
+
+  return false;
+};
