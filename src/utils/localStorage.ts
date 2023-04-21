4e75f2654a1b7883d39800764d730c0d09ad0ba8
n0eyes <doll4586633@naver.com>
2023-04-21 22:22:49 +0900

    refactor: getCardList defaultValue 추가

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -10,6 +10,6 @@ export const setLocalStorageItem = (key: string, data: unknown) => {
   localStorage.setItem(key, JSON.stringify(data));
 };
 
-export const getCardList = () => getLocalStorageItem<CardRegisterInfo[]>("CardList");
+export const getCardList = () => getLocalStorageItem<CardRegisterInfo[]>("CardList", []);
 
 export const setCardList = (data: unknown) => setLocalStorageItem("CardList", data);
