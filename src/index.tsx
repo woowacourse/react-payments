fbed803fa18984752353e17ed96c3a961f707631
gyeongza <gyeongza@naver.com>
2023-04-22 18:21:16 +0900

    refactor: 재사용되지 않는 함수 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GlobalStyle } from './styles/GlobalStyle';
-import { $ } from 'utils/selector';
 import './styles/reset.css';
 
-const root = ReactDOM.createRoot($('#root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <GlobalStyle />
