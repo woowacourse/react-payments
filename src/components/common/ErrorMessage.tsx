b1c79a47fcb9b84a773ca3c0fc1e7b33fe8ab7b8
inyeong-kang <tomatorkd@naver.com>
2023-04-22 11:17:26 +0900

    feat: Label, ErrorMessage 컴포넌트 구현

---
+++ src/components/common/ErrorMessage.tsx
@@ -0,0 +1,14 @@
+import { HTMLAttributes } from 'react';
+
+import styled from 'styled-components';
+
+type ErrorMessageProps = HTMLAttributes<HTMLParagraphElement>;
+
+export function ErrorMessage({ children, ...props }: ErrorMessageProps) {
+  return <_ErrorMessage {...props}>{children}</_ErrorMessage>;
+}
+
+const _ErrorMessage = styled.p`
+  height: 0.4rem;
+  color: red;
+`;
