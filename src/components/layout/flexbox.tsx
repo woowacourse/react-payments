03112031e98039a4901b879a1c6935fd09edbedf
turtle601 <turtle601@naver.com>
2023-04-22 12:52:43 +0900

    feat(layout): flexbox 관련 css 작성

---
+++ src/components/layout/flexbox.tsx
@@ -0,0 +1,11 @@
+import { css } from 'styled-components';
+
+export const VStack = css`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SBetweenStack = css`
+  display: flex;
+  justify-content: space-between;
+`;
