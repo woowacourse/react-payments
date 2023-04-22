0dd2eb96070b24c628553fcd70164fe2aeef91b2
gyeongza <gyeongza@naver.com>
2023-04-22 17:27:17 +0900

    style: GlobalStyle과 reset.css 분리

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { GlobalStyle } from "GlobalStyle";
-import { $ } from "utils/selector";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { GlobalStyle } from './styles/GlobalStyle';
+import { $ } from 'utils/selector';
+import './styles/reset.css';
 
-const root = ReactDOM.createRoot($("#root"));
+const root = ReactDOM.createRoot($('#root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
