3831f90a16938c550976bc537713c555e92ee8b1
turtle601 <turtle601@naver.com>
2023-04-22 17:31:56 +0900

    feat(CardCVC): CardCVC 컴포넌트 구현 및 storybook 코드 작성

---
+++ src/components/molecules/CardCVC/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import CardInput from '../../atomics/CardInput';
+import CardMessage from '../../atomics/Message';
+import ErrorMessage from '../../atomics/ErrorMessage';
+
+import { VStack } from '../../layout/flexbox';
+
+/* component */
+const CardCVC: React.FC = () => {
+  return (
+    <StyledCardCVCWrapper>
+      <StyledCardLabel>
+        <CardMessage type="label">보안 코드(CVC/CVV)</CardMessage>
+        <CardInput type="password" width="75px" minLength={3} maxLength={3} center={true} />
+      </StyledCardLabel>
+      <ErrorMessage isError={true}>3자리 숫자를 입력하세요!!</ErrorMessage>
+    </StyledCardCVCWrapper>
+  );
+};
+
+/* styles */
+const StyledCardLabel = styled.label`
+  ${VStack}
+
+  span + input {
+    margin-top: 8px;
+  }
+`;
+
+const StyledCardCVCWrapper = styled.article`
+  ${VStack}
+
+  label + span {
+    margin-top: 8px;
+  }
+
+  margin-bottom: 8px;
+`;
+
+export default CardCVC;
