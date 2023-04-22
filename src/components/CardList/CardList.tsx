abbb48d9eb363f3e1c7ae4682e27c7a14cfcb4aa
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-22 18:40:50 +0900

    chore: 디렉터리 대, 소문자 에러 수정

---
+++ src/components/CardList/CardList.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { Card } from "../../types";
+import CardPreview from "../CardPreview/CardPreview";
+import AddCardButton from "../AddCardButton/AddCardButton";
+
+type CardListProps = {
+  cards: Card[];
+};
+
+const CardList = ({ cards }: CardListProps) => {
+  return (
+    <Ul>
+      {cards.map((card) => {
+        const { cardNumber, ownerName, expirationDate } = card;
+        const { firstGroup, secondGroup, thirdGroup, fourthGroup } = cardNumber;
+        const key = `${firstGroup}${secondGroup}${thirdGroup}${fourthGroup}`;
+
+        return <CardPreview key={key} card={{ cardNumber, ownerName, expirationDate }} />;
+      })}
+
+      <AddCardButton />
+    </Ul>
+  );
+};
+
+const Ul = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  gap: 46px;
+`;
+
+export default CardList;
