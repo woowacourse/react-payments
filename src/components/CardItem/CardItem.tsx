a94db0a31ef2b383ac4b42607336997222f090f6
ashleysyheo <ashleysyheo@gmail.com>
2023-04-23 01:05:50 +0900

    refactor: CardItem 컴포넌트 수정

--- src/components/CardItem/CardItem.tsx
+++ src/components/CardItem/CardItem.tsx
@@ -1,19 +1,27 @@
+import styles from './style.module.css';
 import { ComponentPropsWithoutRef } from 'react';
 import { Card } from '../../types';
-import styles from './style.module.css';
+import { CARD_NUMBER_UNIT_MAX_LENGTH, REGEX, SECURITY_TEXT_ICON } from '../../constants';
 
 interface CardItemProps extends ComponentPropsWithoutRef<'div'> {
   information: Card;
 }
 
 function CardItem({ information, className }: CardItemProps) {
+  const cardNumber: string[] = information.cardNumber.match(REGEX.FOUR_CHAR_SEQUENCE) ?? [];
+  const displayedCardNumber = cardNumber.concat(
+    Array(CARD_NUMBER_UNIT_MAX_LENGTH - cardNumber.length).fill('')
+  );
+
   return (
     <div className={`${styles.cardItemContainer} ${className}`}>
       <div className={styles.cardChip}></div>
       <div className={styles.cardNumber}>
-        {/* {information.cardNumber.map((number, index) => (
-          <span key={index}>{index < 2 ? number : '•'.repeat(number.length)}</span>
-        ))} */}
+        {displayedCardNumber.map((number, index, cardNumber) => (
+          <span key={index}>
+            {index < cardNumber.length / 2 ? number : SECURITY_TEXT_ICON.repeat(number.length)}
+          </span>
+        ))}
       </div>
       <div className={styles.cardMetaInfo}>
         <span className={styles.cardOwnerName}>{information.ownerName || 'NAME'}</span>
