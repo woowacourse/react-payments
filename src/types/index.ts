29dabfe59b48e2655afe2aa981c43236e1edda04
bassyu <yrkdgus@gmail.com>
2023-04-22 17:52:49 +0900

    refactor(CardType): cardPassword 타입을 리스트로 변경

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,10 @@
-export interface InputStateProps {
-  inputValues: string | string[];
-  setInputValues: (val: string | string[]) => void;
-}
-
 export interface CardType {
   id: number;
   cardNumber: string[];
   expireDate: string[];
   ownerName: string;
   securityCode: string;
-  cardPassword: string;
+  cardPassword: string[];
 }
 
 export enum Page {
