dc0f7f42e731bff705ed3782e7ac75cd1799c2b6
chex <chex1004@gmail.com>
2023-04-23 03:34:44 +0900

    fix: 배포 페이지에서 NotFound페이지 안 나오는 문제 해결

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/register' element={<Register />} />
-        <Route path='*' element={<NotFound />}></Route>
+        <Route path='/*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
