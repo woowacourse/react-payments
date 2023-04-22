f62de99bec5dde7811f89420357d59299a7ed825
ukkodeveloper <ukkodeveloper@gmail.com>
2023-04-22 13:56:45 +0900

    rename: type 이름 card에서 creditCardInfo 로 변경

---
+++ src/@types/creditCardInfo.ts
@@ -0,0 +1,10 @@
+interface CreditCardInfo {
+  cardNumber: string;
+  expirationDate: [string, string];
+  ownerName: string;
+  securityCode: string;
+  password: string;
+  bank: string;
+}
+
+export default CreditCardInfo;
