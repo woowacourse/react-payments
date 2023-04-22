446f1c9fefae741af4753c13ca93f3e2d7e430fa
ashleysyheo <ashleysyheo@gmail.com>
2023-04-23 00:02:40 +0900

    refactor: 카드 번호 인풋 필드에서 중간에 값 변경할 수 있도록 수정

--- src/components/CardAddForm/CardNumber/CardNumber.tsx
+++ src/components/CardAddForm/CardNumber/CardNumber.tsx
@@ -1,8 +1,12 @@
-import { ChangeEvent, FocusEvent } from 'react';
+import { ChangeEvent, FocusEvent, useRef } from 'react';
 import { CardInputValidation } from '../../../types';
-import { CARD_NUMBER_INPUT_MAX_LENGTH } from '../../../constants';
+import {
+  CARD_NUMBER_INPUT_MAX_LENGTH,
+  CARD_NUMBER_INPUT_UNIT_MAX_LENGTH,
+} from '../../../constants';
 import InputContainer from '../../common/InputContainer/InputContainer';
 import Input from '../../common/Input/Input';
+import { useInputCursorPosition } from '../../../hooks/useInputCursorPosition';
 import { useError } from '../../../hooks/useError';
 import validator from '../../../utils/validator';
 
@@ -13,6 +17,8 @@ interface CardNumberProps {
 }
 
 function CardNumber({ changeInputValidation, onInputChange, value }: CardNumberProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const setCursor = useInputCursorPosition(inputRef);
   const [isError, handleError] = useError<CardInputValidation>({
     validator: validator.cardNumber,
     changeInputValidation,
@@ -20,6 +26,21 @@ function CardNumber({ changeInputValidation, onInputChange, value }: CardNumberP
 
   const onBlur = (event: FocusEvent<HTMLInputElement>) => {
     handleError(event.target.name, event.target.value);
+  };
+
+  const onInputCursorPositionChange = ({ target, nativeEvent }: ChangeEvent<HTMLInputElement>) => {
+    if (!(nativeEvent instanceof InputEvent) || !target.selectionStart) return;
+
+    const key = nativeEvent.data;
+    const cursor =
+      target.selectionStart +
+      (target.selectionStart % CARD_NUMBER_INPUT_UNIT_MAX_LENGTH === 0 && key ? 1 : 0);
+    setCursor(cursor);
+  };
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onInputCursorPositionChange(event);
+    onInputChange(event);
   };
 
   return (
@@ -31,13 +52,14 @@ function CardNumber({ changeInputValidation, onInputChange, value }: CardNumberP
       required
     >
       <Input
+        ref={inputRef}
         id="cardNumber"
         name="cardNumber"
         value={value}
         maxLength={CARD_NUMBER_INPUT_MAX_LENGTH}
         autoComplete="cc-csc"
         isError={isError}
-        onChange={onInputChange}
+        onChange={onChange}
         onBlur={onBlur}
       />
     </InputContainer>
