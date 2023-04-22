4924f7fc31833b87c6cdb340272872cdf88a6d74
bassyu <yrkdgus@gmail.com>
2023-04-22 14:23:39 +0900

    chore: reportWebVitals 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GlobalStyle from './GlobalStyle';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -11,5 +10,3 @@ root.render(
     <App />
   </React.StrictMode>,
 );
-
-reportWebVitals();
