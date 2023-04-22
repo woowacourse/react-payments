48afafb56b511088d026ea57834cc5dff1577686
chsua <csj1919@naver.com>
2023-04-22 21:17:55 +0900

    feat: 카드 정보 입력 화면에서 카드목록으로 뒤로가기 기능 구현

--- src/component/CardInputPage/CardInputPage.tsx
+++ src/component/CardInputPage/CardInputPage.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import { CreditCard } from "../../type";
 import CardInputForm from "./CardInputForm/CardInputForm";
 
@@ -13,21 +15,23 @@ export default function CardInputPage(props: Props) {
   return (
     <section className="card-Input-section">
       <div className="card-Input-section-header">
-        <button className="back-page-button" type="button">
-          <svg
-            width="10"
-            height="17"
-            viewBox="0 0 10 17"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M8.30425 1L1.36475 8.78658L9.15133 15.7261"
-              stroke="#525252"
-              stroke-width="1.5"
-            />
-          </svg>
-        </button>
+        <Link to="/">
+          <button className="back-page-button" type="button">
+            <svg
+              width="10"
+              height="17"
+              viewBox="0 0 10 17"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8.30425 1L1.36475 8.78658L9.15133 15.7261"
+                stroke="#525252"
+                stroke-width="1.5"
+              />
+            </svg>
+          </button>
+        </Link>
         <span className="page-explanation">카드 추가</span>
       </div>
       <CardInputForm addNewCard={addNewCard} />
