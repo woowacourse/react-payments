f20075ef0d1e2f441453d63e44aff55b2c4f297e
wzrabbit <curious.wzrabbit@gmail.com>
2023-04-23 20:41:46 +0900

    ♻️ refactor: Input 불필요한 삼항 연산자/Fragment 제거,

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -11,17 +11,15 @@ type InputProps = {
 
 const Input = forwardRef<HTMLInputElement, InputProps>(({ width, value, name, maxLength, onChange }, ref) => {
   return (
-    <>
-      <input
-        className={styles.input}
-        name={name}
-        style={{ width: width }}
-        value={value}
-        maxLength={maxLength ? maxLength : 9999999}
-        onChange={onChange}
-        ref={ref}
-      />
-    </>
+    <input
+      className={styles.input}
+      name={name}
+      style={{ width: width }}
+      value={value}
+      maxLength={maxLength}
+      onChange={onChange}
+      ref={ref}
+    />
   );
 });
 
