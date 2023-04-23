9405756135051aed3086c1c49c4cc8cde88582ee
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-04-23 13:18:12 +0900

    feat: 글로벌 폰트 Roboto 지정

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -2,24 +2,29 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle` 
-  ${reset}
 
-  body{
-    padding:28px 24px;
+  ${reset}
+	* {
+			box-sizing: border-box;
+	}
+
+	body {
+		padding: 28px 24px;
+		font-family: "Roboto", sans-serif;
   }
-  a{
+
+  a {
       text-decoration: none;
       color: inherit;
   }
-  *{
-      box-sizing: border-box;
-  }
+
   input, textarea { 
     -moz-user-select: auto;
     -webkit-user-select: auto;
     -ms-user-select: auto;
     user-select: auto;
   }
+
   input:focus {
     outline: none;
   }
