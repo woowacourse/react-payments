f15e4d49f109a0cadaa05e24cbf3ec811ba7c46f
unknown <tkdrb15@gmail.com>
2023-04-23 21:07:57 +0900

    feat: 카드 번호 붙여넣기, 자르기를 금지하는 기능 추가

--- src/components/cardForm/CardNumberInput.tsx
+++ src/components/cardForm/CardNumberInput.tsx
@@ -26,10 +26,19 @@ export const CardNumberInput = ({
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const text = e.target.value;
 
+    // 문자열 중간 부분을 수정했을 때
+    if (postText !== text.slice(0, -1) && postText.slice(0, -1) !== text) {
+      e.target.value = postText;
+      return;
+    }
+
+    const hideNumbers = (numbers: string): string => {
+      const hiddenNumbers = numbers.slice(0, 8) + "●".repeat(numbers.slice(8).length);
+      return (hiddenNumbers.match(/\d{1,4}|●{1,4}/g) ?? []).join(" - ");
+    };
+
     //문자가 추가됐을 때
-    if (
-      postText.replaceAll(" - ", "").length < text.replaceAll(" - ", "").length
-    ) {
+    if (postText.replaceAll(" - ", "").length < text.replaceAll(" - ", "").length) {
       const numbers = text.replaceAll(" - ", "");
 
       if (numbers.length > 16 || !/\d/g.test(text.slice(-1))) {
@@ -40,25 +49,20 @@ export const CardNumberInput = ({
       const addNumbers = cardNumbers + text.slice(-1);
       setCardNumbers(addNumbers);
 
-      setPostText(e.target.value);
-      const nextText =
-        addNumbers.slice(0, 8) + "●".repeat(addNumbers.slice(8).length);
-      e.target.value = (nextText.match(/\d{1,4}|●{1,4}/g) ?? []).join(" - ");
+      setPostText(hideNumbers(addNumbers));
+      e.target.value = hideNumbers(addNumbers);
+
+      if (addNumbers.length === 16) setIsCompleted(true);
+
+      return;
     }
+
     //문자가 제거 됐을 때
-    else {
-      const minusNumbers = cardNumbers.slice(0, -1);
+    const minusNumbers = postText.slice(-1) === "●" ? cardNumbers.slice(0, 8) : cardNumbers.slice(0, -1);
       setCardNumbers(minusNumbers);
 
-      setPostText(e.target.value);
-      const nextText =
-        minusNumbers.slice(0, 8) + "●".repeat(minusNumbers.slice(8).length);
-      e.target.value = (nextText.match(/\d{1,4}|●{1,4}/g) ?? []).join(" - ");
-    }
-  };
-
-  const handleOutFocusEvent = (e: any) => {
-    const value = e.target.value.replaceAll(" - ", "");
+    setPostText(hideNumbers(minusNumbers));
+    e.target.value = hideNumbers(minusNumbers);
 
     setIsCompleted(false);
 
