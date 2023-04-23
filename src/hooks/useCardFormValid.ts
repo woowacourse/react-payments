6df24de69ea932a876bd88cc981658a5e564314d
regularPark <srk99279@naver.com>
2023-04-23 20:37:27 +0900

    refactor: month 유효성 검사 추가

--- src/hooks/useCardFormValid.ts
+++ src/hooks/useCardFormValid.ts
@@ -1,6 +1,16 @@
 import { Card } from 'components/common/Card/types';
 
-export function useCardFormValid(card: Card) {
+export function useCardFormValid(
+  card: Card,
+): [
+  isValid: boolean,
+  errorMessages: {
+    numbers: string;
+    expirationDate: string;
+    securityCode: string;
+    password: string;
+  },
+] {
   const { numbers, expirationDate, securityCode, password } = card;
 
   const isNumberLengthValid = (value: string, length: number) => {
@@ -10,7 +20,10 @@ export function useCardFormValid(card: Card) {
   const isCardNumberValid = numbers.every((number) => isNumberLengthValid(number, 4));
 
   const isExpirationDateValid =
-    isNumberLengthValid(expirationDate.month, 2) && isNumberLengthValid(expirationDate.year, 2);
+    isNumberLengthValid(expirationDate.month, 2) &&
+    isNumberLengthValid(expirationDate.year, 2) &&
+    Number(expirationDate.month) > 0 &&
+    Number(expirationDate.month) <= 12;
 
   const isSecurityCodeValid = isNumberLengthValid(securityCode, 3);
 
@@ -19,9 +32,9 @@ export function useCardFormValid(card: Card) {
 
   const errorMessages = {
     numbers: isCardNumberValid ? '' : '카드번호는 16자리로 입력해주세요.',
-    expirationDate: isExpirationDateValid ? '' : '만료일을 입력해주세요.',
-    securityCode: isSecurityCodeValid ? '' : '보안 코드는 3자리로 입력해주세요.',
-    password: isPasswordValid ? '' : '카드 비밀번호 앞 2자리를 전부 입력해주세요.',
+    expirationDate: isExpirationDateValid ? '' : '유효한 만료일을 입력해주세요.',
+    securityCode: isSecurityCodeValid ? '' : '보안 코드 3자리를 입력해주세요.',
+    password: isPasswordValid ? '' : '카드 비밀번호 앞 2자리를 입력해주세요.',
   };
 
   return [
