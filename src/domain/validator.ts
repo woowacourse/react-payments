becec5cc76d7d2d73be6371b6fae164115aca368
Jungwoo <evencoding@gmail.com>
2023-04-24 00:10:32 +0900

    feat: 카드 정보 Form에 대한 유효성 검증 구현

--- src/domain/validator.ts
+++ src/domain/validator.ts
@@ -1,45 +1,97 @@
-export const isNumeric = (value: string) => {
-  return /^[0-9]*$/.test(value);
+import { Validator } from '../types/validator';
+
+const ERROR_MESSAGE = Object.freeze({
+  CARD_NUMBERS: '카드 번호는 0000-0000-0000-0000의 형태로 입력 해 주세요.',
+  SECURITY_NUMBERS: '보안 코드 3자리를 입력 해 주세요.',
+  PASSWORD: '비밀번호 앞 두 자리를 입력 해 주세요.',
+  EXPIRATION: '만료일은 00 / 00의 형태로 입력 해 주세요.',
+  EXPIRATION_MONTH: '월은 01~12까지의 값만 입력할 수 있습니다.',
+  EXPIRATION_YEAR: '지나간 연도를 입력할 수 없습니다.',
+  EXPIRATION_DATE: '지나간 날짜를 입력할 수 없습니다.',
+});
+
+const validateCardNumbers = (value: string) => {
+  if (value.length < 4) {
+    return ERROR_MESSAGE.CARD_NUMBERS;
+  }
+
+  return '';
 };
 
-export const validateMonth = (value: number) => {
-  const isValidRange = value >= 1 && value <= 12;
+let [month, year] = ['', ''];
 
-  if (!isValidRange) throw new Error('MM에는 01에서 12까지의 값만 입력해주세요');
+const validateExpirationDate = () => {
+  const curYear = String(new Date().getFullYear()).substring(2);
+  const curMonth = new Date().getMonth() + 1;
+
+  if (year === curYear && Number(month) < curMonth) {
+    return ERROR_MESSAGE.EXPIRATION_DATE;
+  }
+  return '';
 };
 
-export const validateExpirationDate = (expirationYear: number, expirationMonth: number) => {
-  const currentYear = new Date().getFullYear() - 2000;
-  const currentMonth = new Date().getMonth() + 1;
+const validateExpirationMonth = (value: string) => {
+  if (value.length < 2) {
+    return ERROR_MESSAGE.EXPIRATION;
+  }
 
-  const isValidDate =
-    expirationYear > currentYear ||
-    (expirationYear === currentYear && expirationMonth >= currentMonth);
+  if (Number(value) < 0 || Number(value) > 12) {
+    return ERROR_MESSAGE.EXPIRATION_MONTH;
+  }
 
-  if (!isValidDate) throw new Error('만료일이 지난 카드는 등록할 수 없습니다');
+  month = value;
+  if (month && year) return validateExpirationDate();
+
+  return '';
 };
 
-export const validateValidUserName = (userName: string) => {
-  const isEmpty = !userName.length;
-  const isValidFormat = /^([A-Za-z](?:\s?[A-Za-z])*)*$/.test(userName);
-  const isValidLength = userName.length <= 30;
+const validateExpirationYear = (value: string) => {
+  if (value.length < 2) {
+    return ERROR_MESSAGE.EXPIRATION;
+  }
 
-  if (isEmpty) return;
+  const curYear = String(new Date().getFullYear()).substring(2);
+  if (Number(value) < Number(curYear)) {
+    return ERROR_MESSAGE.EXPIRATION_YEAR;
+  }
 
-  if (isValidFormat && isValidLength) return;
+  year = value;
+  if (month && year) return validateExpirationDate();
 
-  throw new Error('영문으로 이루어진 이름을 입력해주세요.');
+  return '';
 };
 
-// export const validateCardExpirationValue = (month: string, year: string) => {
-//   try {
-//     validateNumeric(month);
-//     validateNumeric(year);
+const validateOwnerName = (value: string) => {
+  return '';
+};
 
-//     // Number
-//     validateMonth(Number(month));
-//     validateExpirationDate(Number(year), Number(month));
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
+const validateSecurityNumbers = (value: string) => {
+  if (value.length < 3) {
+    return ERROR_MESSAGE.SECURITY_NUMBERS;
+  }
+
+  return '';
+};
+
+const validatePassword = (value: string) => {
+  if (value.length < 1) {
+    return ERROR_MESSAGE.PASSWORD;
+  }
+
+  return '';
+};
+
+const validator: Validator = {
+  firstCardNumbers: validateCardNumbers,
+  secondCardNumbers: validateCardNumbers,
+  thirdCardNumbers: validateCardNumbers,
+  fourthCardNumbers: validateCardNumbers,
+  expirationMonth: validateExpirationMonth,
+  expirationYear: validateExpirationYear,
+  ownerName: validateOwnerName,
+  securityNumbers: validateSecurityNumbers,
+  firstPassword: validatePassword,
+  secondPassword: validatePassword,
+};
+
+export default validator;
