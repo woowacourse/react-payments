449af249516d7bbab2084ae9dfd3677111a451d4
Suyoung <jsy8492@gmail.com>
2023-04-23 18:25:20 +0900

    feat: 카드사 로고 svg 아이콘 추가

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,8 @@
+export { ReactComponent as BCCardLogo } from './bc.svg';
+export { ReactComponent as ShinhanCardLogo } from './shinhan.svg';
+export { ReactComponent as KakaobankCardLogo } from './kakaobank.svg';
+export { ReactComponent as HyundaiCardLogo } from './hyundai.svg';
+export { ReactComponent as WooriCardLogo } from './woori.svg';
+export { ReactComponent as LotteCardLogo } from './lotte.svg';
+export { ReactComponent as HanaCardLogo } from './hana.svg';
+export { ReactComponent as KBCardLogo } from './kb.svg';
