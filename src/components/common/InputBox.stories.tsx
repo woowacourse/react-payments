2d40f761b8651a7069be1a9514fce7eaf9b3b51c
bassyu <yrkdgus@gmail.com>
2023-04-23 16:42:58 +0900

    test(InputBox): Storybook에 리팩터링 내용 적용

--- src/components/common/InputBox.stories.tsx
+++ src/components/common/InputBox.stories.tsx
@@ -1,4 +1,7 @@
-import InputBox, { InputType } from './InputBox';
+import InputBox from './InputBox';
+import Input from './Input';
+import useFocusRef from '../../utils/useFocusRef';
+import useList from '../../utils/useList';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
@@ -6,46 +9,62 @@ export default {
   component: InputBox,
 };
 
-export function CardNumberInput() {
-  const inputs: InputType[] = [
-    { textType: 'number', maxLength: 4, placeholder: '1234' },
-    { textType: 'number', maxLength: 4, placeholder: '5678' },
-    { textType: 'number', maxLength: 4, placeholder: '∙∙∙∙' },
-    { textType: 'number', maxLength: 4, placeholder: '∙∙∙∙' },
-  ];
+export function AutoFocusNumber() {
+  const [list, setList] = useList(['', '', '', '', '', '']);
+  const [insert, focus] = useFocusRef();
   return (
-    <InputBox
-      inputs={inputs}
-      align="center"
-      separator="-"
-      isFullWidth
-      inputValues=""
-      setInputValues={() => {}}
-    />
+    <InputBox align="center" separator="/">
+      {Array(6)
+        .fill(null)
+        .map((_, index) => (
+          <Input
+            textType="number"
+            value={list[index]}
+            setValue={setList(index)}
+            length={4}
+            insert={insert(index)}
+            focus={focus(index)}
+            placeholder="0000"
+          />
+        ))}
+    </InputBox>
   );
 }
 
-export function ExpireDateInput() {
-  const inputs: InputType[] = [
-    { textType: 'number', maxLength: 2, placeholder: 'MM' },
-    { textType: 'number', maxLength: 2, placeholder: 'YY' },
-  ];
-  return <InputBox inputs={inputs} align="center" separator="/" inputValues="" setInputValues={() => {}} />;
+export function PhoneNumber() {
+  return (
+    <InputBox align="center" separator="-" isFullWidth>
+      <Input textType="number" value="010" setValue={() => {}} length={3} />
+      <Input textType="number" value="1029" setValue={() => {}} length={4} />
+      <Input textType="number" value="3847" setValue={() => {}} length={4} />
+    </InputBox>
+  );
 }
 
-export function OwnerNameInput() {
-  const inputs: InputType[] = [
-    { textType: 'text', maxLength: 30, placeholder: '카드에 표시된 이름과 동일하게 입력하세요.' },
-  ];
-  return <InputBox inputs={inputs} align="left" isFullWidth inputValues="" setInputValues={() => {}} />;
+export function Name() {
+  return (
+    <InputBox align="center">
+      <Input textType="number" value="GANG HYEON" setValue={() => {}} length={20} textAlign="left" />
+      <Input textType="number" value="YU" setValue={() => {}} length={10} textAlign="left" />
+    </InputBox>
+  );
 }
 
-export function SecurityCodeInput() {
-  const inputs: InputType[] = [{ textType: 'number', maxLength: 3 }];
-  return <InputBox inputs={inputs} align="center" inputValues="" setInputValues={() => {}} />;
+export function Password() {
+  return (
+    <InputBox align="center">
+      <Input textType="number" value="1234" setValue={() => {}} length={4} textSecurity />
+    </InputBox>
+  );
 }
 
-export function CardPasswordInput() {
-  const inputs: InputType[] = [{ textType: 'number', maxLength: 1 }];
-  return <InputBox inputs={inputs} align="center" inputValues="" setInputValues={() => {}} />;
+export function SplitPassword() {
+  return (
+    <InputBox align="center">
+      <Input textType="number" value="1" setValue={() => {}} length={1} textSecurity />
+      <Input textType="number" value="2" setValue={() => {}} length={1} textSecurity />
+      <Input textType="number" value="3" setValue={() => {}} length={1} textSecurity />
+      <Input textType="number" value="4" setValue={() => {}} length={1} textSecurity />
+    </InputBox>
+  );
 }
