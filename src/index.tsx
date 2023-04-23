62945b686272c45a1cebfdcffd352d1bbf349319
2yunseong <dbsdltjd3701@naver.com>
2023-04-23 21:02:23 +0900

    refactor: Router 역할 컴포넌트 분리

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import AddCardPage from './pages/AddCardPage';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import CardListPage from './pages/CardListPage';
-
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <CardListPage />,
-  },
-  {
-    path: '/add',
-    element: <AddCardPage />,
-  },
-]);
+import AppRouter from './router/AppRouter';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<RouterProvider router={router} />);
+root.render(<AppRouter />);
