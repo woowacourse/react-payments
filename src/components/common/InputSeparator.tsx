1fb9c0fc69a30f27e3a0159b55b6c909a7dd9f9e
woo-jk <201702042@o.cnu.ac.kr>
2023-04-24 03:46:43 +0900

    fix: InputSeparator isActive 속성 제거

--- src/components/common/InputSeparator.tsx
+++ src/components/common/InputSeparator.tsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
-const InputSeparator = styled.span<{ isActive?: boolean }>`
-  visibility: ${({ isActive }) => (isActive ? "visible" : "hidden")};
+const InputSeparator = styled.span`
   display: flex;
   justify-content: center;
   align-items: center;
