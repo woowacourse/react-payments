c396303a322377ce4d6120f1ca85ff1f753d1a88
bassyu <yrkdgus@gmail.com>
2023-04-23 16:49:27 +0900

    test(InputSectionTemplate): Storybook 작성

--- src/components/template/InputSectionTemplate.stories.tsx
+++ src/components/template/InputSectionTemplate.stories.tsx
@@ -1,85 +1,55 @@
 import InputBox from '../common/InputBox';
 import InputSectionTemplate from './InputSectionTemplate';
 
+import useList from '../../utils/useList';
+import useFocusRef from '../../utils/useFocusRef';
+import Input from '../common/Input';
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   title: 'InputSectionTemplate',
   component: InputSectionTemplate,
 };
 
-export const InputNumberType = () => {
-  return (
-    <InputSectionTemplate label="숫자">
-      <InputBox
-        inputs={[{ textType: 'number', maxLength: 10 }]}
-        inputValues={'1234'}
-        setInputValues={() => {}}
-        align="center"
-      />
-    </InputSectionTemplate>
-  );
-};
+export function PhoneNumberInputSection() {
+  const [phoneNumber, setPhoneNumber] = useList(['010', '', '']);
+  const [insert, focus] = useFocusRef();
 
-export const InputNameType = () => {
-  const inputValue = '강상원';
   return (
-    <InputSectionTemplate label="이름" isCountLength maxLength={10} inputValues={inputValue}>
-      <InputBox
-        inputs={[{ textType: 'text', maxLength: 10 }]}
-        inputValues={inputValue}
-        setInputValues={() => {}}
-        align="left"
-        isFullWidth
-      />
+    <InputSectionTemplate
+      label="전화번호를 입력해주세요"
+      countLength={phoneNumber.join('').length}
+      maxLength={11}
+    >
+      <InputBox align="center" separator="-" isFullWidth>
+        <Input
+          textType="number"
+          value={phoneNumber[0]}
+          setValue={setPhoneNumber(0)}
+          length={3}
+          insert={insert(0)}
+          focus={focus(0)}
+          placeholder="010"
+        />
+        <Input
+          textType="number"
+          value={phoneNumber[1]}
+          setValue={setPhoneNumber(1)}
+          length={4}
+          insert={insert(1)}
+          focus={focus(1)}
+          placeholder="1234"
+        />
+        <Input
+          textType="number"
+          value={phoneNumber[2]}
+          setValue={setPhoneNumber(2)}
+          length={4}
+          insert={insert(2)}
+          focus={focus(2)}
+          placeholder="5678"
+        />
+      </InputBox>
     </InputSectionTemplate>
   );
-};
-
-export const InputPasswordType = () => {
-  return (
-    <InputSectionTemplate label="비밀번호">
-      <InputBox
-        inputs={[{ textType: 'number', maxLength: 1, textSecurity: true }]}
-        inputValues={'1'}
-        setInputValues={() => {}}
-        align="center"
-      />
-      <InputBox
-        inputs={[{ textType: 'number', maxLength: 1, textSecurity: true }]}
-        inputValues={'1'}
-        setInputValues={() => {}}
-        align="center"
-      />
-      <InputBox
-        inputs={[{ textType: 'number', maxLength: 1, textSecurity: true }]}
-        inputValues={'1'}
-        setInputValues={() => {}}
-        align="center"
-      />
-      <InputBox
-        inputs={[{ textType: 'number', maxLength: 1, textSecurity: true }]}
-        inputValues={'1'}
-        setInputValues={() => {}}
-        align="center"
-      />
-    </InputSectionTemplate>
-  );
-};
-
-export const InputPhoneNumberType = () => {
-  return (
-    <InputSectionTemplate label="전화번호">
-      <InputBox
-        inputs={[
-          { textType: 'number', maxLength: 3 },
-          { textType: 'number', maxLength: 4 },
-          { textType: 'number', maxLength: 4 },
-        ]}
-        inputValues={['010', '1234', '5678']}
-        setInputValues={() => {}}
-        align="center"
-        separator="-"
-      />
-    </InputSectionTemplate>
-  );
-};
+}
