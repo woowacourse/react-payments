610d4ee4994bbcf6112c7b7f17535722c17fd6e4
Suyoung <jsy8492@gmail.com>
2023-04-23 18:27:33 +0900

    feat: Modal 열고 닫을 시 슬라이드 애니메이션 구현

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -1,5 +1,7 @@
-import { ReactNode, useEffect } from 'react';
+import { ReactNode, useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
+
+import useModal from './useModal';
 
 import styles from './modal.module.css';
 
@@ -7,10 +9,44 @@ interface Props {
   children: ReactNode;
 }
 
+const ANIMATION_DURATION = 300;
+
 const Modal = ({ children }: Props) => {
+  const { isModalOpen, closeModal } = useModal();
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [isVisible, setIsVisible] = useState(isModalOpen);
+  const timeoutId = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const handleModalClose = () => {
+    setIsVisible(false);
+
+    setTimeout(() => {
+      closeModal();
+    }, ANIMATION_DURATION);
+  };
+
+  useEffect(() => {
+    setIsVisible(isModalOpen);
+
+    if (isVisible && !isModalOpen) {
+      setIsAnimating(true);
+
+      timeoutId.current = setTimeout(() => {
+        setIsAnimating(false);
+      }, ANIMATION_DURATION);
+    }
+
+    return () => {
+      setIsVisible(false);
+
+      if (!timeoutId.current) return;
+      clearTimeout(timeoutId.current);
+    };
+  }, [isModalOpen]);
+
   const handleEscapeKeydown = (event: KeyboardEvent) => {
     if (event.key === 'Escape') {
-      closeModal();
+      handleModalClose();
     }
   };
 
@@ -21,9 +57,13 @@ const Modal = ({ children }: Props) => {
       document.body.removeEventListener('keydown', handleEscapeKeydown);
   }, []);
 
+  if (isModalOpen === false && isVisible === false && isAnimating === false) {
+    return null;
+  }
+
   return createPortal(
     <div role="dialog" aria-modal>
-      <div className={styles.backdrop} onClick={closeModal} />
+      <div className={styles.backdrop} onClick={handleModalClose} />
       <div className={styles.container}>{children}</div>
     </div>,
     document.body,
