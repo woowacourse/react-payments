4699bf0d00d0448ceb41d41a2b094c77eaee217e
hafnium1923 <hafnium1923@gmail.com>
2023-04-23 21:35:52 +0900

    feat: 카드 중간번호 지우는 기능 구현

--- src/hooks/useCardNumber.tsx
+++ src/hooks/useCardNumber.tsx
@@ -5,35 +5,43 @@ function useCardNumber() {
   const [cardNumberHidden, setCardNumberHidden] = useState("");
 
   const changeCardNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const cardNumber = e.target.value.replace(/[^\d•]/g, "").slice(0, 16); // 16자리 이상은 자르기
+    const cardNumber = e.target.value.replace(/[^\d•]/g, "").slice(0, 16);
+    const realNumber = cardNumber.replace(/[^\d]/g, "");
 
-    //오리진 저장
-    if (cardNumberOrigin.length > 8) {
-      // 별 표시 출력과 관련된 로직
-      if (cardNumberOrigin.length < cardNumber.length) {
-        // 추가시
-        setCardNumberOrigin(cardNumberOrigin + cardNumber.slice(-1));
-      }
+    const hiddenNumber = changeHidden(realNumber, cardNumber.length);
+    setCardNumberHidden(hiddenNumber);
 
-      if (cardNumberOrigin.length > cardNumber.length) {
-        // 제거시
-        setCardNumberOrigin(cardNumberOrigin.slice(0, -1));
-      }
+    if (cardNumberOrigin.length < cardNumber.length) {
+      setCardNumberOrigin(cardNumberOrigin + cardNumber.slice(-1));
     }
 
-    if (cardNumberOrigin.length <= 8) {
-      setCardNumberOrigin(cardNumber);
+    if (cardNumberOrigin.length > cardNumber.length) {
+      setCardNumberOrigin(deleteNumber(realNumber));
     }
+  };
 
-    //히든 저장
+  const deleteNumber = (realNumber: string) => {
+    if (cardNumberOrigin.length <= 8) return realNumber;
+
+    if (realNumber !== cardNumberOrigin.slice(0, 8))
+      return realNumber + cardNumberOrigin.slice(8, 16);
+
+    return cardNumberOrigin.slice(0, -1);
+  };
+
+  const changeHidden = (realNumber: string, length: number) => {
+    const frontNumber =
+      realNumber !== cardNumberOrigin.slice(0, 8)
+        ? realNumber + cardNumberOrigin.slice(8, 16)
+        : realNumber;
+
     const hiddenNumber =
-      cardNumber.length > 8
-        ? cardNumber.slice(0, 8) + "•".repeat(cardNumber.length - 8)
-        : cardNumber;
-    const showNumber = hiddenNumber.match(/.{1,4}/g);
-    const resultNumber = showNumber ? showNumber.join("-") : "";
+      length > 8
+        ? frontNumber.slice(0, 8) + "•".repeat(length - 8)
+        : frontNumber;
 
-    setCardNumberHidden(resultNumber);
+    const showNumber = hiddenNumber.match(/.{1,4}/g);
+    return showNumber ? showNumber.join("-") : "";
   };
 
   return {
