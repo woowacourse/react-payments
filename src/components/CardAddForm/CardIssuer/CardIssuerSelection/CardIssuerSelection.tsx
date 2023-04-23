a125bdb25f3cc770287da47671b088c08915856c
ashleysyheo <ashleysyheo@gmail.com>
2023-04-23 17:24:46 +0900

    refactor: CardIssuerSelection 컴포넌트 수정

--- src/components/CardAddForm/CardIssuer/CardIssuerSelection/CardIssuerSelection.tsx
+++ src/components/CardAddForm/CardIssuer/CardIssuerSelection/CardIssuerSelection.tsx
@@ -1,18 +1,27 @@
 import styles from './style.module.css';
 import { CARD_ISSUERS } from '../../../../constants';
 import CardIssuerOption from './CardIssuerOption/CardIssuerOption';
+import Button from '../../../common/Button/Button';
+import CloseIcon from '../../../../assets/x-icon.svg';
 
 interface CardIssuerSelectionProps {
   onOptionClick: CallableFunction;
+  close: () => void;
 }
 
-function CardIssuerSelection({ onOptionClick }: CardIssuerSelectionProps) {
+function CardIssuerSelection({ onOptionClick, close }: CardIssuerSelectionProps) {
   return (
-    <div className={styles.container}>
-      {CARD_ISSUERS.map((cardIssuer) => (
-        <CardIssuerOption issuer={cardIssuer} onClick={onOptionClick} />
-      ))}
-    </div>
+    <>
+      <div className={styles.header}>
+        <h3 className={styles.title}>카드사 선택</h3>
+        <Button variant="textButton" size="small" icon={CloseIcon} onClick={close} />
+      </div>
+      <div className={styles.container}>
+        {CARD_ISSUERS.map((cardIssuer, index) => (
+          <CardIssuerOption key={index} issuer={cardIssuer} onClick={onOptionClick} />
+        ))}
+      </div>
+    </>
   );
 }
 
