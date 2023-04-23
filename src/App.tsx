e6929861037effb01ec23eb6b6d6ffa926bc2812
sh981013s <sh981013s@gmail.com>
2023-04-24 00:39:35 +0900

    refactor: Layout 에서 provider 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import CardRegister from './pages/CardRegister/CardRegister';
-import MyCardList from './pages/MyCardList/MyCardList';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CardRegister from "./pages/CardRegister/CardRegister";
+import MyCardList from "./pages/MyCardList/MyCardList";
+import CardRegisterProvider from "./context/CardRegisterContext";
 
-import Layout from './components/@common/Layout/Layout';
+import Layout from "./components/@common/Layout/Layout";
 
 function App() {
   return (
@@ -11,8 +12,23 @@ function App() {
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index path="/" element={<MyCardList />} />
-            <Route path="/registerCard" element={<CardRegister />} />
+            <Route
+              index
+              path="/"
+              element={
+                <CardRegisterProvider>
+                  <MyCardList />
+                </CardRegisterProvider>
+              }
+            />
+            <Route
+              path="/registerCard"
+              element={
+                <CardRegisterProvider>
+                  <CardRegister />
+                </CardRegisterProvider>
+              }
+            />
           </Route>
         </Routes>
       </BrowserRouter>
