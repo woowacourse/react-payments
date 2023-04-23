82167b97b16d7d53484beede734f54da9daaf2ec
ashleysyheo <ashleysyheo@gmail.com>
2023-04-23 20:31:31 +0900

    refactor: useCardValidation 커스텀 훅 수정

--- src/hooks/useCardValidation.ts
+++ src/hooks/useCardValidation.ts
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { CardInputValidation } from '../types';
+import validator, { validateMultipleInputField } from '../utils/validator';
+import { isKeyOfObj } from '../utils/typeUtils';
 
 const useCardValidator = () => {
   const [cardInputValidation, setCardInputValidation] = useState<CardInputValidation>({
@@ -11,13 +13,21 @@ const useCardValidator = () => {
     password: false,
   });
 
-  const handleValidationChange = (key: keyof CardInputValidation, value: boolean) => {
+  const handleValidationChange = (key: string, value: string | string[]) => {
+    if (!isKeyOfObj(validator, key)) return;
+
+    const isValid = !validateMultipleInputField(key)
+      ? validator[key](value as string)
+      : validator[key](value as string[]);
+
     setCardInputValidation((cardInputValidation) => {
       return {
         ...cardInputValidation,
-        [key]: value,
+        [key]: isValid,
       };
     });
+
+    return isValid;
   };
 
   return [cardInputValidation, handleValidationChange] as const;
