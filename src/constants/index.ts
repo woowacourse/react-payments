b006a030f07ece42bee322cd8f5eb779d944263a
Patrick <kuyt3122@naver.com>
2023-04-23 12:17:01 +0900

    refactor: 상수 선언에 readonly 적용

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,20 +1,20 @@
-const INPUT_MAX_LENGTH = Object.freeze({
+const INPUT_MAX_LENGTH: Readonly<{ [key: string]: number }> = {
   CARD_NUMBER: 25,
   EXPIRED_DATE: 7,
   OWNER_NAME: 30,
   CVC: 3,
   PASSWORD: 1,
-});
+};
 
-const SEPERATED_CARD_NUMBER_LENGTH = Object.freeze({
+const SEPERATED_CARD_NUMBER_LENGTH: Readonly<{ [key: string]: number }> = {
   FIRST: 4,
   SECOND: 11,
   THIRD: 18,
-});
+};
 
-const PASSWORD_DIGIT_INDEX = Object.freeze({
+const PASSWORD_DIGIT_INDEX: Readonly<{ [key: string]: number }> = {
   FIRST: 0,
   SECOND: 1,
-});
+};
 
 export { INPUT_MAX_LENGTH, SEPERATED_CARD_NUMBER_LENGTH, PASSWORD_DIGIT_INDEX };
