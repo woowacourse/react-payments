6bd04a8db0fda97ddff9260531271c42f26fce98
Suyoung <jsy8492@gmail.com>
2023-04-23 18:18:15 +0900

    feat: Modal 컴포넌트를 전역에서 사용할 수 있도록 ModalContext 구현

---
+++ src/components/common/Modal/ModalContext.tsx
@@ -0,0 +1,29 @@
+import { PropsWithChildren, createContext, useState } from 'react';
+
+import { noop } from '../../../utils/noop';
+
+interface ModalContextProps {
+  isModalOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+}
+
+export const ModalContext = createContext<ModalContextProps>({
+  isModalOpen: false,
+  openModal: noop,
+  closeModal: noop,
+});
+
+export const ModalProvider = ({ children }: PropsWithChildren) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <ModalContext.Provider value={{ isModalOpen, openModal, closeModal }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
