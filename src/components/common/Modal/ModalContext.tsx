ebb3b4e3fe8ed8275df88d25126ab57e92a17bf8
Suyoung <jsy8492@gmail.com>
2023-04-23 21:18:35 +0900

    refactor: useModal 커스텀 훅을 ModalContext 모듈 내부로 이동

--- src/components/common/Modal/ModalContext.tsx
+++ src/components/common/Modal/ModalContext.tsx
@@ -27,3 +27,13 @@ export const ModalProvider = ({ children }: PropsWithChildren) => {
     </ModalContext.Provider>
   );
 };
+
+export const useModal = () => {
+  const context = useContext(ModalContext);
+
+  if (context === undefined) {
+    throw new Error('useModal must be used within a ModalProvider');
+  }
+
+  return context;
+};
