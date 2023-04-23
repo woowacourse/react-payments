04e0c2758b32768b15d24f31306cfdffaf92d065
Suyoung <jsy8492@gmail.com>
2023-04-23 18:19:04 +0900

    feat: Modal의 상태를 관리하는 커스텀 훅 작성

---
+++ src/components/common/Modal/useModal.ts
@@ -0,0 +1,15 @@
+import { useContext } from 'react';
+
+import { ModalContext } from './ModalContext';
+
+const useModal = () => {
+  const { isModalOpen, openModal, closeModal } = useContext(ModalContext);
+
+  return {
+    isModalOpen,
+    openModal,
+    closeModal,
+  };
+};
+
+export default useModal;
