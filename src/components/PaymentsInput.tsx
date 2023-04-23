66c27783bd518e261204eccee7817f8f26a794e0
minjaeKim <smallkdb@gmail.com>
2023-04-24 01:53:48 +0900

    feat: 카드 등록에 쓰이는 input컴포넌트인 PaymentsInput 구현

--- src/components/PaymentsInput.tsx
+++ src/components/PaymentsInput.tsx
@@ -1,99 +1,73 @@
-import styled from 'styled-components';
-import type { ChangeEvent } from 'react';
+import styled, { css } from 'styled-components';
+import NamedInputListBox from './NamedInputListBox';
+import type { NamedInputListBoxProps } from './NamedInputListBox';
 
-const LabelTitle = styled.div`
-  font-size: 12px;
-  font-weight: 500;
-  line-height: 14px;
-  color: #525252;
-`;
-
-const InputContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  max-width: 318px;
-  height: 48.75px;
-  margin-top: 8.67px;
-  background: #ecebf1;
-  border-radius: 7px;
-  input[type='password'] {
-    text-align: center;
-  }
-`;
-
-const InputItem = styled.input<{ maxLength: number }>`
-  width: ${({ maxLength }) => maxLength * 12}px;
-  font-size: 18px;
-  background: none;
-  border: none;
-  outline: none;
-  letter-spacing: 0.1em;
-  ::placeholder {
-    font-size: 14px;
-    letter-spacing: 0.0625rem;
-  }
-`;
-
-const InputDivider = styled.div`
-  font-size: 18px;
-`;
-
-interface PaymentsInputProps {
-  title?: string;
-  inputInformationList: InputInformation[];
-  inputDivideLetter?: string | '';
-  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
-}
-
-interface InputInformation {
-  type: string;
-  isRequired: boolean;
-  pattern: [number, number];
-  name: string;
-  placeholder?: string;
-}
-
-export type { PaymentsInputProps };
-
-function PaymentsInput({ title, inputInformationList, inputDivideLetter, onChange }: PaymentsInputProps) {
-  const inputAmount = inputInformationList.length;
-
+function PaymentsInput({ inputListInformation, name, id }: NamedInputListBoxProps) {
   return (
-    <div>
-      {title && <LabelTitle>{title}</LabelTitle>}
-      <InputContainer>
-        {Array.from({ length: inputAmount - 1 }, (_, i) => (
-          <>
-            <InputItem
-              name={inputInformationList[i].name}
-              key={i}
-              type={inputInformationList[i].type}
-              pattern={`.{${inputInformationList[i].pattern[0]},${inputInformationList[i].pattern[1]}}`}
-              maxLength={inputInformationList[i].pattern[1]}
-              onChange={onChange}
-              data-index={i}
-              required={inputInformationList[i].isRequired}
-              placeholder={inputInformationList[i].placeholder}
-            />
-            {inputDivideLetter && <InputDivider>{inputDivideLetter}</InputDivider>}
-          </>
-        ))}
-        <InputItem
-          name={inputInformationList[inputAmount - 1].name}
-          type={inputInformationList[inputAmount - 1].type}
-          pattern={`.{${inputInformationList[inputAmount - 1].pattern[0]},${
-            inputInformationList[inputAmount - 1].pattern[1]
-          }}`}
-          maxLength={inputInformationList[inputAmount - 1].pattern[1]}
-          onChange={onChange}
-          data-index={inputAmount - 1}
-          required={inputInformationList[inputAmount - 1].isRequired}
-          placeholder={inputInformationList[inputAmount - 1].placeholder}
-        />
-      </InputContainer>
-    </div>
+    <StyledPaymentsInput isBlankBridge={inputListInformation.bridgeLetter === ''}>
+      <NamedInputListBox inputListInformation={inputListInformation} name={name} id={id} />
+    </StyledPaymentsInput>
   );
 }
+
+const StyledPaymentsInput = styled.div<{ isBlankBridge: boolean }>`
+  display: flex;
+  flex-direction: column;
+  max-width: 318px;
+
+  & > label {
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 14px;
+    align-items: center;
+    letter-spacing: -0.085em;
+    color: #525252;
+    margin-bottom: 8px;
+  }
+
+  & > div > div:first-child {
+    display: flex;
+    ${({ isBlankBridge }) =>
+      isBlankBridge
+        ? css`
+            & > input {
+              background: #ecebf1;
+              min-width: 46px;
+              border-radius: 7px;
+              margin-right: 8px;
+              height: 46px;
+              font-size: 18px;
+              letter-spacing: 0.0625rem;
+              ::placeholder {
+                letter-spacing: -0.085em;
+              }
+            }
+          `
+        : css`
+            background: #ecebf1;
+            padding: 0 12px;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            border-radius: 7px;
+            height: 46px;
+            & > input {
+              font-size: 18px;
+              letter-spacing: 0.0625rem;
+              ::placeholder {
+                letter-spacing: -0.085em;
+              }
+            }
+          `};
+  }
+
+  & > div > *:nth-child(n + 1) {
+    display: flex;
+    min-width: 46px;
+    height: 46px;
+    align-items: center;
+    justify-content: center;
+  }
+`;
 
 export default PaymentsInput;
--- src/stories/PaymentsInput.stories.tsx
+++ src/stories/PaymentsInput.stories.tsx
@@ -1,4 +1,6 @@
+import { ReactComponent as BlackDot } from '../assets/black-dot.svg';
 import PaymentsInput from '../components/PaymentsInput';
+import QuestionToolTip from '../components/QuestionToolTip';
 import type { Meta, StoryObj } from '@storybook/react';
 
 type Story = StoryObj<typeof PaymentsInput>;
@@ -12,49 +14,72 @@ export default meta;
 
 export const CardNumber: Story = {
   args: {
-    title: '카드 번호',
-    inputInformationList: [
-      { type: 'text', isRequired: true, pattern: [4, 4], name: 'cardNumber' },
-      { type: 'text', isRequired: true, pattern: [4, 4], name: 'cardNumber' },
-      { type: 'password', isRequired: true, pattern: [4, 4], name: 'cardNumber' },
-      { type: 'password', isRequired: true, pattern: [4, 4], name: 'cardNumber' },
-    ],
-    inputDivideLetter: '-',
+    name: '카드 번호',
+    inputListInformation: {
+      inputInformation: [
+        { type: 'string', maxLength: 4, textAlign: 'center', placeholder: '1234' },
+        { type: 'string', maxLength: 4, textAlign: 'center', placeholder: '1234' },
+        { type: 'password', maxLength: 4, textAlign: 'center', placeholder: '1234' },
+        { type: 'password', maxLength: 4, textAlign: 'center', placeholder: '1234' },
+      ],
+      bridgeLetter: '-',
+    },
   },
 };
 
 export const ExpirationDate: Story = {
   args: {
-    title: '만료일',
-    inputInformationList: [
-      { type: 'text', isRequired: true, pattern: [2, 2], name: 'expirationDate' },
-      { type: 'text', isRequired: true, pattern: [2, 2], name: 'expirationDate' },
-    ],
-    inputDivideLetter: '/',
+    name: '만료일',
+    inputListInformation: {
+      inputInformation: [
+        { type: 'string', maxLength: 2, textAlign: 'center', placeholder: 'MM' },
+        { type: 'string', maxLength: 2, textAlign: 'center', placeholder: 'YY' },
+      ],
+      bridgeLetter: '/',
+    },
   },
 };
 
-export const CardOwner: Story = {
+export const Owner: Story = {
   args: {
-    title: '카드 소유자 이름(선택)',
-    inputInformationList: [{ type: 'text', isRequired: false, pattern: [0, 30], name: 'owner' }],
+    name: '카드 소유자 이름 (선택)',
+    inputListInformation: {
+      inputInformation: [{ type: 'string', maxLength: 30, placeholder: '카드에 표시된 이름과 동일하게 입력하세요.' }],
+    },
   },
 };
 
 export const SecurityCode: Story = {
   args: {
-    title: '보언 코드(CVC/CVV)',
-    inputInformationList: [{ type: 'password', isRequired: true, pattern: [3, 3], name: 'securityCode' }],
+    name: '보안 코드(CVC/CVV)',
+    inputListInformation: {
+      inputInformation: [{ type: 'password', textAlign: 'center', maxLength: 3 }],
+      children: (
+        <QuestionToolTip questionMessage="CVV/CVC 번호는 카드 뒷 면에 있는 3자리 숫자이며 카드 보안을 위한 번호입니다." />
+      ),
+    },
   },
 };
 
 export const CardPassword: Story = {
   args: {
-    title: '카드 비밀번호',
-    inputInformationList: [
-      { type: 'password', isRequired: true, pattern: [1, 1], name: 'cardPassword' },
-      { type: 'password', isRequired: true, pattern: [1, 1], name: 'cardPassword' },
-    ],
-    inputDivideLetter: '',
+    name: '카드 비밀번호',
+    inputListInformation: {
+      inputInformation: [
+        { type: 'password', textAlign: 'center', maxLength: 1 },
+        { type: 'password', textAlign: 'center', maxLength: 1 },
+      ],
+      bridgeLetter: '',
+      children: (
+        <>
+          <div>
+            <BlackDot />
+          </div>
+          <div>
+            <BlackDot />
+          </div>
+        </>
+      ),
+    },
   },
 };
