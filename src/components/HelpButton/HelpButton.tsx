4c0b683c90951ecb35238508a6b0188e8a7f3418
wzrabbit <curious.wzrabbit@gmail.com>
2023-04-23 21:56:31 +0900

    ✨ feat: HelpButton 컴포넌트 메시지를 alert 대신 풍선 메시지로 표현하도록 변경

--- src/components/HelpButton/HelpButton.tsx
+++ src/components/HelpButton/HelpButton.tsx
@@ -1,20 +1,21 @@
+import React from 'react';
 import { BsQuestionCircle } from 'react-icons/bs';
 import styles from './HelpButton.module.css';
 
 type HelpButtonProps = {
   message: string;
+  balloonWidth?: string;
 };
 
-const HelpButton = ({ message }: HelpButtonProps) => {
-  const showMessage = () => {
-    alert(message);
-  };
-
+const HelpButton = ({ message, balloonWidth = '200px' }: HelpButtonProps) => {
   return (
     <div className={styles.container}>
-      <BsQuestionCircle className={styles.button} onClick={showMessage} />
+      <BsQuestionCircle className={styles.button} />
+      <div className={styles.balloon} style={{ width: balloonWidth }}>
+        {message}
+      </div>
     </div>
   );
 };
 
-export default HelpButton;
+export default React.memo(HelpButton);
