af24eb3ec2d751043e28c1836ed71b540c89c823
jeongwusi <jeongwusi@gmail.com>
2023-04-24 20:32:06 +0900

    ✨ feat: 카드 로고 이미지 추가

---
+++ src/images/images.ts
@@ -0,0 +1,21 @@
+import bcCard from './bc.png';
+import hanaCard from './hana.png';
+import hyundaiCard from './hyundai.png';
+import lotteCard from './lotte.png';
+import wooriCard from './woori.png';
+import shinhanCard from './shinhan.png';
+import visaCard from './visa.png';
+import citiCard from './citi.png';
+
+const cards = {
+  bcCard: bcCard,
+  hanaCard: hanaCard,
+  hyundaiCard: hyundaiCard,
+  lotteCard: lotteCard,
+  wooriCard: wooriCard,
+  shinhanCard: shinhanCard,
+  visaCard: visaCard,
+  citiCard: citiCard,
+};
+
+export default cards;
