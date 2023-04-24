0638ee90387f9c5de5914f84c1a3a34a905dfbbf
kangyeongmin <kym6050@hanyang.ac.kr>
2023-04-24 20:53:03 +0900

    chore: 배포를 위한 임의 수정

--- src/stories/Card.stories.ts
+++ src/stories/Card.stories.ts
@@ -9,20 +9,20 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const RedCard: Story = {
-  args: {
-    color: "red",
-    cardNumber: "1111-2222-3333-4444",
-    ownerName: "LIGHT",
-    expiredDate: "12 / 24",
-  },
-};
+// export const RedCard: Story = {
+//   args: {
+//     color: "red",
+//     cardNumber: "1111-2222-3333-4444",
+//     ownerName: "LIGHT",
+//     expiredDate: "12 / 24",
+//   },
+// };
 
-export const PinkCard: Story = {
-  args: {
-    color: "green",
-    cardNumber: "9999-9999-9999-9999",
-    ownerName: "PATRICK",
-    expiredDate: "08 / 25",
-  },
-};
+// export const PinkCard: Story = {
+//   args: {
+//     color: "green",
+//     cardNumber: "9999-9999-9999-9999",
+//     ownerName: "PATRICK",
+//     expiredDate: "08 / 25",
+//   },
+// };
--- src/stories/CardInput.stories.ts
+++ src/stories/CardInput.stories.ts
@@ -6,66 +6,61 @@ const meta = {
   component: CardInput,
 };
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+// export default meta;
+// type Story = StoryObj<typeof meta>;
 
-export const CardNumber: Story = {
-  args: {
-    id: "cardNumber",
-    placeholder: "카드 번호를 입력해 주세요.",
-    value: "",
-    isSecured: false,
-    isAutoFocus: true,
-    isRequired: true,
-    maxLength: 25,
-  },
-};
+// export const CardNumber: Story = {
+//   args: {
+//     id: "cardNumber",
+//     placeholder: "카드 번호를 입력해 주세요.",
+//     value: "",
+//     isSecured: false,
+//     isAutoFocus: true,
+//     isRequired: true,
+//   },
+// };
 
-export const ExpiredDate: Story = {
-  args: {
-    id: "expiredDate",
-    placeholder: "MM / YY",
-    width: "137px",
-    value: "",
-    isSecured: false,
-    isAutoFocus: false,
-    isRequired: true,
-    maxLength: 7,
-  },
-};
+// export const ExpiredDate: Story = {
+//   args: {
+//     id: "expiredDate",
+//     placeholder: "MM / YY",
+//     width: "137px",
+//     value: "",
+//     isSecured: false,
+//     isAutoFocus: false,
+//     isRequired: true,
+//   },
+// };
 
-export const OwnerName: Story = {
-  args: {
-    id: "ownerName",
-    placeholder: "카드에 표시된 이름과 동일하게 입력하세요.",
-    value: "",
-    isSecured: false,
-    isAutoFocus: false,
-    isRequired: true,
-    maxLength: 7,
-  },
-};
+// export const OwnerName: Story = {
+//   args: {
+//     id: "ownerName",
+//     placeholder: "카드에 표시된 이름과 동일하게 입력하세요.",
+//     value: "",
+//     isSecured: false,
+//     isAutoFocus: false,
+//     isRequired: true,
+//   },
+// };
 
-export const Cvc: Story = {
-  args: {
-    id: "cvc",
-    width: "84px",
-    value: "",
-    isSecured: true,
-    isAutoFocus: false,
-    isRequired: true,
-    maxLength: 3,
-  },
-};
+// export const Cvc: Story = {
+//   args: {
+//     id: "cvc",
+//     width: "84px",
+//     value: "",
+//     isSecured: true,
+//     isAutoFocus: false,
+//     isRequired: true,
+//   },
+// };
 
-export const Password: Story = {
-  args: {
-    id: "password",
-    width: "42px",
-    value: "",
-    isSecured: true,
-    isAutoFocus: false,
-    isRequired: true,
-    maxLength: 1,
-  },
-};
+// export const Password: Story = {
+//   args: {
+//     id: "password",
+//     width: "42px",
+//     value: "",
+//     isSecured: true,
+//     isAutoFocus: false,
+//     isRequired: true,
+//   },
+// };
