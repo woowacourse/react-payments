9019c780c3de40ec00ba6ab1dac1eeffed07d43a
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-04-24 22:00:10 +0900

    fix: 만료일 유효성 검사 복구

--- src/pages/CreditCardRegister/inputs/CreditCardExpiryInput.tsx
+++ src/pages/CreditCardRegister/inputs/CreditCardExpiryInput.tsx
@@ -12,29 +12,39 @@ type Props = {
 
 function CreditCardExpiryInput({ name, creditCard, setCreditCard }: Props) {
   const handleChangeCreditCardExpiry = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newCardExpiry = event.target.value
+    const newCardExpiry = event.target.value;
+    const cleanedExpiry = newCardExpiry
       .replaceAll('/', '')
       .replace(/\D/g, '');
-    if (newCardExpiry.length > 4) return;
-    setCreditCard({ ...creditCard, [name]: newCardExpiry });
+    if (cleanedExpiry.length > 4) return;
+    setCreditCard({ ...creditCard, [name]: cleanedExpiry });
+    // }
   };
 
-  const foo = (expiry: string) => {
+  const markedExpiry = ((expiry: string) => {
     const newCardExpiryArray = expiry.split('');
-    console.log(newCardExpiryArray);
     if (newCardExpiryArray.length > 2) {
       newCardExpiryArray.splice(2, 0, '/');
     }
     return newCardExpiryArray.join('');
-  };
+  })(creditCard.expiry);
 
-  // const isError = creditCard.expiry.length > 0 && creditCard.expiry.length < 5;
+  const isError = ((expiry: string) => {
+    const [month, year] = [parseInt(expiry.slice(0, 2), 10), parseInt(expiry.slice(2), 10)];
+    if (month < 1 || month > 12 || year <= 22) {
+      return true;
+    }
+    if (expiry.length > 0 && expiry.length < 4) {
+      return true;
+    }
+    return false;
+  })(creditCard.expiry);
 
   return (
     <S.Box>
       <S.CreditCardRegisterLabel>만료일</S.CreditCardRegisterLabel>
-      <Input placeholder="MM/YY" type="string" value={foo(creditCard.expiry)} width="40%" textAlign="center" onChange={handleChangeCreditCardExpiry} />
-      {/* {isError && <S.ErrorMessage>만료일은 MM/YY 형식이어야 합니다.</S.ErrorMessage>} */}
+      <Input placeholder="MM/YY" type="string" value={markedExpiry} width="40%" textAlign="center" onChange={handleChangeCreditCardExpiry} />
+      {isError && <S.ErrorMessage>만료일은 유효한 MM/YY 형식이어야 합니다.</S.ErrorMessage>}
     </S.Box>
   );
 }
