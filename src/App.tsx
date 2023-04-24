8096f98d7ce34121e3d28e800df7f2f029db40ce
yogjin <youngjin988@gmail.com>
2023-04-24 19:49:26 +0900

    refactor: styled component globalStyle을 이용하여 전역 스타일 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from 'pages/Home';
 import RegisterCard from 'pages/RegisterCard';
+import { GlobalStyle } from 'GlobalStyle';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div className="App">
+    <div className="App">
+      <GlobalStyle />
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<RegisterCard />} />
         </Routes>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </div>
   );
 }
 
