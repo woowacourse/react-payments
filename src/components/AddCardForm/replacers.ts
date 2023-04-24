2f04be6bdffa1f59bd59f9866f608c88bbda7fd2
wzrabbit <curious.wzrabbit@gmail.com>
2023-04-24 17:55:20 +0900

    ♻️ refactor(replacers): 정규표현식 상수화

--- src/components/AddCardForm/replacers.ts
+++ src/components/AddCardForm/replacers.ts
@@ -1,13 +1,16 @@
+const FOUR_CHARACTER_CHUNKER = /.{1,4}/g;
+const TWO_CHARACTER_CHUNKER = /.{1,2}/g;
+
 const addHyphensInCardNumber = (cardNumber: string) => {
   const hyphenRemovedCardNumber = cardNumber.replaceAll('-', '');
-  const cardNumberWithHyphens = (hyphenRemovedCardNumber.match(/.{1,4}/g) || []).join('-');
+  const cardNumberWithHyphens = (hyphenRemovedCardNumber.match(FOUR_CHARACTER_CHUNKER) || []).join('-');
 
   return cardNumberWithHyphens;
 };
 
 const addSlashInExpirationDate = (expirationDate: string) => {
   const slashRemovedExpirationDate = expirationDate.replaceAll('/', '');
-  const expirationDateWithSlash = (slashRemovedExpirationDate.match(/.{1,2}/g) || []).join('/');
+  const expirationDateWithSlash = (slashRemovedExpirationDate.match(TWO_CHARACTER_CHUNKER) || []).join('/');
 
   return expirationDateWithSlash;
 };
