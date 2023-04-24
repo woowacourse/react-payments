93fbae9ff53d89c54fb9e73bff36db141ac3b74e
Dain Lee <feb.dain@gmail.com>
2023-04-24 16:29:52 +0900

    refactor: GlobalStyle에서 reset css 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import MainPage from "pages/MainPage";
 import CardRegisterForm from "pages/RegisterPage/CardRegisterForm";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./css/reset.css";
 
 const App = () => {
   return (
