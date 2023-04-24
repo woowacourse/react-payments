35f78f18247e4469d87e8b71cc143237de217079
Suyoung <jsy8492@gmail.com>
2023-04-24 22:34:35 +0900

    feat: 카드 별칭 입력 페이지 컴포넌트 작성
    - 카드 별칭 업데이트 기능 구현
    - 도메인 로직을 useCardNicknameInputPage 커스텀 훅으로 분리

---
+++ src/pages/CardNicknameInputPage/useCardNicknameInputPage.ts
@@ -0,0 +1,38 @@
+import { ChangeEventHandler, FormEventHandler, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { useCardsContext } from '../../domain/context/CardsContext';
+
+const useCardNicknameInputPage = () => {
+  const navigate = useNavigate();
+  const { lastRegisteredCard, modifyCardNickname } = useCardsContext();
+  const [nickname, setNickname] = useState('');
+
+  const handleNicknameSubmit: FormEventHandler<HTMLDivElement> = (event) => {
+    event.preventDefault();
+
+    if (lastRegisteredCard === null) return;
+
+    if (nickname === '') {
+      navigate('/');
+    }
+
+    modifyCardNickname(lastRegisteredCard, nickname);
+    navigate('/');
+  };
+
+  const handleNicknameChange: ChangeEventHandler<HTMLInputElement> = (
+    event,
+  ) => {
+    setNickname(event.target.value);
+  };
+
+  return {
+    lastRegisteredCard,
+    nickname,
+    handleNicknameSubmit,
+    handleNicknameChange,
+  };
+};
+
+export default useCardNicknameInputPage;
