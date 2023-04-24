3d783a69b4d88fc5fccebddb5a58717a5e175c17
minjaeKim <smallkdb@gmail.com>
2023-04-24 17:58:52 +0900

    refacotor: cardForm 간소화

--- src/components/CardRegistrationForm/hooks/useCardForm.ts
+++ src/components/CardRegistrationForm/hooks/useCardForm.ts
@@ -1,38 +1,21 @@
 import { useState } from 'react';
 import type { CardInformation } from '../../Common/Card/types';
 
-function useCardForm(
-  initialState: CardInformation = {
-    cardNumber: ['', '', '', ''],
-    expirationDate: ['', ''],
-    owner: '',
-    securityCode: '',
-    password: ['', ''],
-  },
-) {
-  const [card, setCard] = useState<CardInformation>(initialState);
+function useCardForm() {
+  const [cardNumber, setCardNumber] = useState<string[]>(['', '', '', '']);
+  const [expirationDate, setExpirationDate] = useState<string[]>(['MM', 'YY']);
+  const [owner, setOwner] = useState<string[]>(['NAME']);
+  const [securityCode, setSecurityCode] = useState<string[]>(['']);
+  const [password, setPassword] = useState<string[]>(['', '']);
 
-  const setCardNumber = (cardNumber: string[]) => {
-    setCard(prev => ({ ...prev, cardNumber } as CardInformation));
+  return {
+    card: { cardNumber, expirationDate, owner, securityCode, password } as unknown as CardInformation,
+    setCardNumber,
+    setExpirationDate,
+    setOwner,
+    setSecurityCode,
+    setPassword,
   };
-
-  const setExpirationDate = (expirationDate: string[]) => {
-    setCard(prev => ({ ...prev, expirationDate } as CardInformation));
-  };
-
-  const setOwner = (owner: string[]) => {
-    setCard(prev => ({ ...prev, owner: owner[0] }));
-  };
-
-  const setSecurityCode = (securityCode: string[]) => {
-    setCard(prev => ({ ...prev, securityCode: securityCode[0] }));
-  };
-
-  const setPassword = (password: string[]) => {
-    setCard(prev => ({ ...prev, password } as CardInformation));
-  };
-
-  return { card, setCardNumber, setExpirationDate, setOwner, setSecurityCode, setPassword };
 }
 
 export default useCardForm;
