8cdd4a09972f6409d79185e58449c265eb09c1ea
wzrabbit <curious.wzrabbit@gmail.com>
2023-04-24 21:51:32 +0900

    ✨ feat: 타입 추가
    - ValidatorResponseType, FormInputValueType

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,11 @@
-export type CardInfo = {
+type CardInfo = {
   cardNumber: string;
   cardExpirationDate: string;
   cardOwnerName: string;
 };
+
+type ValidatorResponseType = { result: boolean; errorMessage: string };
+
+type FormInputValueType = { isValid: boolean; value: string };
+
+export type { CardInfo, ValidatorResponseType, FormInputValueType };
