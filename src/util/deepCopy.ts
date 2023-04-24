2dbbf3a8821e59827e93ef9f5e0d73c5661f8a44
chsua <csj1919@naver.com>
2023-04-24 21:31:16 +0900

    feat: 객체 복사하는 유틸 생성

---
+++ src/util/deepCopy.ts
@@ -0,0 +1,5 @@
+export const deepCopyObject = (origin: object, key: string, value: any) => {
+  const updateResult = JSON.parse(JSON.stringify(origin));
+  updateResult[key] = value;
+  return updateResult;
+};
