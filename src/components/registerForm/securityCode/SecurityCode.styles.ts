67868d71baec0c07d8642fb9816b6d00a9f03a93
hozzijeong <ghwls0905@naver.com>
2023-04-24 15:20:51 +0900

    feat: CVC 카드 더 보기 추가

--- src/components/registerForm/securityCode/SecurityCode.styles.ts
+++ src/components/registerForm/securityCode/SecurityCode.styles.ts
@@ -13,4 +13,37 @@ export const Styled = {
     text-align: center;
     font-size: 28px;
   `,
+
+  SecurityInfoContainer: styled.div`
+    display: flex;
+    gap: 11px;
+
+    position: relative;
+  `,
+
+  CVVInfoContainer: styled.div`
+    width: 60%;
+    height: 155px;
+
+    display: flex;
+    align-items: center;
+    -webkit-box-align: center;
+
+    position: absolute;
+    right: 0;
+    top: -50px;
+
+    background: rgba(0, 0, 0, 0.55);
+    border-radius: 7px;
+
+    p {
+      color: white;
+      font-size: 12px;
+      margin-left: 10px;
+    }
+
+    img {
+      height: 80px;
+    }
+  `,
 };
--- src/components/registerForm/securityCode/index.tsx
+++ src/components/registerForm/securityCode/index.tsx
@@ -2,15 +2,19 @@ import React, { useState, useContext } from "react";
 import ErrorSpan from "src/components/@common/ErrorSpan";
 import FormLabel from "src/components/@common/FormLabel";
 import Input from "src/components/@common/Input";
+import QuestionMark from "src/assets/sequrity_question.svg";
+import SecurityIcon from "src/assets/security-icon.svg";
 import { ONLY_NUMBER_REGEXP } from "src/utils/regexp";
 import { cardInfoContext } from "src/context/CardInfoContext";
-import { Styled } from "./SecurityCode.styles";
+import { Styled as S } from "./SecurityCode.styles";
 import { NUMBERS } from "src/utils/constant";
 import { lengthMatchValidation } from "src/utils/validation";
+import useTimeoutShow from "src/hooks/useTimeoutShow";
 
 function SecurityCode() {
   const [cardInput, setCardInput] = useContext(cardInfoContext);
   const [error, setError] = useState(false);
+  const { isShow, showForIntervals } = useTimeoutShow(3000);
 
   const codeChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     const value = event.currentTarget.value;
@@ -28,18 +32,35 @@ function SecurityCode() {
   };
 
   return (
-    <Styled.SecurityCodeContainer>
+    <S.SecurityCodeContainer>
       <FormLabel>{"보안 코드(CVC/CVV)"}</FormLabel>
-      <Input
-        value={cardInput.securityCode}
-        onChange={codeChange}
-        maxLength={NUMBERS.MAX_SECURITY}
-        type="password"
-        placeholder="•••"
-        customInputStyle={Styled.SecurityInput}
-      />
+      <S.SecurityInfoContainer>
+        <Input
+          value={cardInput.securityCode}
+          onChange={codeChange}
+          maxLength={NUMBERS.MAX_SECURITY}
+          type="password"
+          placeholder="•••"
+          customInputStyle={S.SecurityInput}
+        />
+        <img
+          src={QuestionMark}
+          alt="cvc-question-information"
+          onClick={showForIntervals}
+        />
+        {isShow && (
+          <S.CVVInfoContainer>
+            <p>
+              CVV/CVC 번호는 카드 뒷 면에 있는 3자리 숫자이며 카드 보안을 위한
+              번호입니다. 이 번호는 보안 확인 목적으로 사용되며, 온라인 결제에
+              주로 사용됩니다.
+            </p>
+            <img src={SecurityIcon} alt="cvc-information" />
+          </S.CVVInfoContainer>
+        )}
+      </S.SecurityInfoContainer>
       {error && <ErrorSpan>보안 코드는 3자리 입니다.</ErrorSpan>}
-    </Styled.SecurityCodeContainer>
+    </S.SecurityCodeContainer>
   );
 }
 
