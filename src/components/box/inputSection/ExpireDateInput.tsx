4aafa8f92ed6c7dc17e41193219086eb4bbb6324
bassyu <yrkdgus@gmail.com>
2023-04-24 15:10:35 +0900

    feat(ExpireDateInput): 유효 기간 입력 검증 기능 구현

--- src/components/box/inputSection/ExpireDateInput.tsx
+++ src/components/box/inputSection/ExpireDateInput.tsx
@@ -12,8 +12,18 @@ interface Props {
 }
 
 const ExpireDateInput = ({ expireDate, setExpireDateIndex, insert, focus }: Props) => {
+  const validExpireDate = (expireDate: CardType['expireDate']) => {
+    const [month, year] = expireDate.map(Number);
+    const currentYear = new Date().getFullYear() - 2000;
+
+    console.log(year, currentYear);
+    if (!(month >= 1 && month <= 12)) return '1월에서 12월 사이로 입력 해주세요';
+    if (!(year >= currentYear && year <= currentYear + 5)) return '유효기간은 현재부터 최대 5년입니다.';
+    return '';
+  };
+
   return (
-    <InputSectionTemplate label="만료일">
+    <InputSectionTemplate label="만료일" message={validExpireDate(expireDate)}>
       <InputBox separator="/">
         {expireDate.map((value, index) => (
           <Input
