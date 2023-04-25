11918df2c7b0378697f1f8fa649745aee7455d2d
n0eyes <doll4586633@naver.com>
2023-04-26 01:13:25 +0900

    refactor: REGEX value 타입 변경
    - RegExp > string

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,6 +1,6 @@
 const REGEX = {
-  NUMBER: /^[0-9]+$/,
-  KOREAN: /^[ㄱ-ㅎ가-힣a-zA-Z]+$/,
+  NUMBER: '^[0-9]+',
+  KOREAN: '^[ㄱ-ㅎ가-힣a-zA-Z]+$',
 } as const;
 
-export const isNumber = (value: string) => REGEX.NUMBER.test(value);
+export const isNumber = (value: string) => new RegExp(REGEX.NUMBER).test(value);
