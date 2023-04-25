d5f797522b919a54faad35536e3118dc5cbe661d
chsua <csj1919@naver.com>
2023-04-25 16:39:31 +0900

    test: 카드 비밀번호 컴포넌트 로직 수정에 따른 테스트 코드 수정

--- src/stories/InputBoxPassword.stories.tsx
+++ src/stories/InputBoxPassword.stories.tsx
@@ -11,5 +11,5 @@ const meta: Meta = {
 export default meta;
 
 export const InputTest = (args: any) => (
-  <InputBoxPassword setIsComplete={() => {}}></InputBoxPassword>
+  <InputBoxPassword changePasswordStatus={() => {}}></InputBoxPassword>
 );
