9c399526ea3f554e97e46829c345bfce4dc7e5a6
NaveOWO <pianowell96@gmail.com>
2023-04-26 01:58:30 +0900

    feat : 스타일링 된 child 여부에 따른 랜더링 분기처리

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,15 +1,46 @@
-import { InputHTMLAttributes } from "react";
+import {
+  Children,
+  cloneElement,
+  InputHTMLAttributes,
+  isValidElement,
+  PropsWithChildren,
+  useContext,
+} from "react";
 import styled from "styled-components";
+import { InputContext } from "../../contexts/inputContext";
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleError?: () => void;
+  inputRef: any;
+  name: string;
+  asChild?: boolean;
 }
 
-export function Input(props: InputProps) {
-  const { handleChange, handleError, ...restProps } = props;
+export function Input(props: PropsWithChildren<InputProps>) {
+  const { name, inputRef, asChild = false, children, ...restProps } = props;
+  const inputState = useContext(InputContext);
+  const { handleChange } = inputState;
+  const childrenArray = Children.toArray(children);
+  const child = childrenArray[0];
 
-  return (
-    <InputUnit onChange={handleChange} onBlur={handleError} {...restProps} />
+  return asChild ? (
+    isValidElement<{ name: string; inputRef: any }>(child) ? (
+      cloneElement(child, {
+        name,
+        inputRef,
+        onChange: handleChange,
+        ...restProps,
+      })
+    ) : (
+      <></>
+    )
+  ) : (
+    <InputUnit
+      onChange={handleChange}
+      name={name}
+      ref={(elem) => {
+        inputRef.current[name] = elem;
+      }}
+      {...restProps}
+    />
   );
 }
 
@@ -24,4 +55,5 @@ const InputUnit = styled.input`
   border-radius: 0.7rem;
 
   text-align: center;
+  outline: none;
 `;
