c7f25d38a07874f68787605d1c104d34f94c41d9
NaveOWO <pianowell96@gmail.com>
2023-04-26 01:56:08 +0900

    etc : 사용하지 않는 파일 삭제

--- src/type/validator.ts
+++
@@ -1,7 +0,0 @@
-export interface Validator {
-  validCardNumber: string | null;
-  validDate: string | null;
-  validName: string | null;
-  validCode: string | null;
-  validPassword: string | null;
-}
