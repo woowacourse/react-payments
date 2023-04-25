771ab79ee1a6d8bb24760dcf4d67b963b284d320
ashleysyheo <ashleysyheo@gmail.com>
2023-04-25 20:49:38 +0900

    refactor: Header 컴포넌트 아이콘 img 태그에서 Button 컴포넌트로 변경

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,6 +1,7 @@
 import styles from './style.module.css';
 import { ComponentPropsWithoutRef } from 'react';
 import { Link } from 'react-router-dom';
+import Button from '../Button/Button';
 import leftIcon from '../../../assets/left-icon.svg';
 
 interface HeaderProps extends ComponentPropsWithoutRef<'header'> {
@@ -12,7 +13,9 @@ interface HeaderProps extends ComponentPropsWithoutRef<'header'> {
 function Header({ content, path = '/', isOverlayPage = false, ...attributes }: HeaderProps) {
   return (
     <header className={styles.header} {...attributes}>
-      <Link to={path}>{isOverlayPage && <img src={leftIcon} alt="left back icon" />}</Link>
+      <Link to={path}>
+        {isOverlayPage && <Button variant="textButton" icon={leftIcon}></Button>}
+      </Link>
       <h3 className="center-hoz-item">{content}</h3>
     </header>
   );
