0f8f531c3dba66f334d117a967bee1811fe34dfe
NaveOWO <pianowell96@gmail.com>
2023-04-26 02:43:41 +0900

    feat : 타입 추가

--- src/type/input.ts
+++ src/type/input.ts
@@ -1,3 +1,10 @@
 import { LABEL } from "../constants/inputInfo";
 
 export type LabelOption = keyof typeof LABEL;
+
+export interface CardNumber {
+  first: string;
+  second: string;
+  third: string;
+  fourth: string;
+}
