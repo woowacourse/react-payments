4fc5fafb43d5cf647d0efc2c7b36e6cb603d3c76
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-25 20:59:07 +0900

    feat: Card type에 cardCompony 프로퍼티 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,10 +7,13 @@ export type CardExpirationDate = Record<CardExpirationDateKey, string>;
 export type CardPasswordKey = "first" | "second";
 export type CardPassword = Record<CardPasswordKey, string>;
 
+export type CardCompany = "BC" | "SHINHAN" | "HANA" | "HYUNDAI" | "KAKAO" | "KB" | "LOTTE" | "WOORI";
+
 export type Card = {
   cardNumber: CardNumber;
   expirationDate: CardExpirationDate;
   ownerName: string;
   securityCode: string;
   password: CardPassword;
+  cardCompany: CardCompany;
 };
