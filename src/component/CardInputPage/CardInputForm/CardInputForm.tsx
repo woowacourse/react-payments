74669786f91d1719c40c31894291ce5f39af0bf2
chsua <csj1919@naver.com>
2023-04-25 22:19:43 +0900

    refactor: useRef로 form에 직접 이벤트 부여 -> onSubmit으로 수정

--- src/component/CardInputPage/CardInputForm/CardInputForm.tsx
+++ src/component/CardInputPage/CardInputForm/CardInputForm.tsx
@@ -6,12 +6,11 @@ import InputBoxOwner from "../InputBoxOwner/InputBoxOwner";
 import InputBoxPassword from "../InputBoxPassword/InputBoxPassword";
 import InputBoxSecurityCode from "../InputBoxSecurityCode/InputBoxSecurityCode";
 
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import "./cardInputForm.css";
 import { CreditCard, InputStatus } from "../../../type";
-import { deepCopyObject } from "../../../util/deepCopy";
 import { EachUserInputState } from "../../../type";
 
 interface Props {
@@ -44,20 +43,15 @@ const initialInputStatus = {
   password: initialArrayInputState,
 };
 
-//하나의 inputState에는 {isComplete:true, userInput: value}
 export default function CardInputForm(props: Props) {
   const { addNewCard } = props;
 
   const [isFormFilled, setIsFormFilled] = useState(false);
-
-  const nowCardInfo = initialCard;
-
-  const navigate = useNavigate();
-  const formElement = useRef<HTMLFormElement>(null);
-
-  //total user Input status
   const [inputStatus, setInputStatus] =
     useState<InputStatus>(initialInputStatus);
+
+  const [nowCardInfo, setNowCardInfo] = useState<CreditCard>(initialCard);
+  const navigate = useNavigate();
 
   const changeInputStatus = (inputName: keyof InputStatus) => {
     return (key: keyof EachUserInputState, value: any, index?: number) => {
@@ -66,55 +60,30 @@ export default function CardInputForm(props: Props) {
 
         if (key === "isComplete") {
           updateResult[inputName][key] = value;
-          console.log(updateResult);
           return updateResult;
         }
+
+        const originNowCard = JSON.parse(JSON.stringify(nowCardInfo));
 
         if (
           (inputName === "cardNumber" || inputName === "password") &&
           index !== undefined
         ) {
           updateResult[inputName][key][index] = value;
-          nowCardInfo[inputName][index] = value;
+          originNowCard[inputName][index] = value;
+          setNowCardInfo(originNowCard);
           return updateResult;
         }
 
         updateResult[inputName][key] = value;
-        nowCardInfo[inputName] = value;
+        originNowCard[inputName] = value;
+        setNowCardInfo(originNowCard);
+
         return updateResult;
       });
     };
   };
 
-  function submitCardInfo(e: SubmitEvent) {
-    e.preventDefault();
-
-    if (!formElement.current) return;
-
-    const formData = new FormData(formElement.current);
-
-    const card: CreditCard = {
-      owner: formData.get("card-owner")?.toString() as string,
-      expirationDate: formData.get("expiration-date")?.toString() as string,
-      cardNumber: [
-        formData.get("card-number-1"),
-        formData.get("card-number-2"),
-        formData.get("card-number-3"),
-        formData.get("card-number-4"),
-      ].map(Number),
-      securityCode: formData.get("security-code")?.toString() as string,
-      password: [
-        formData.get("card-password-1"),
-        formData.get("card-password-2"),
-      ].map(Number),
-    };
-
-    addNewCard(card);
-    // resetNowCardInfo();
-    navigate("/CardListPage");
-  }
-
-  //유저 입력값이 달라지면 complete를 확인해서 formFilled 여부 처리
   useEffect(() => {
     const { cardNumber, expirationDate, securityCode, password } = inputStatus;
     if (
@@ -127,20 +96,15 @@ export default function CardInputForm(props: Props) {
     else setIsFormFilled(false);
   }, [inputStatus]);
 
-  //formFilled 여부에 따라 제출 이벤트 생성/삭제
-  useEffect(() => {
-    // if (!isFormFilled) {
-    //   formElement.current?.removeEventListener("submit", submitCardInfo);
-    // }
-
-    // formElement.current?.addEventListener("submit", submitCardInfo);
-
-    if (isFormFilled) console.log(nowCardInfo);
-    console.log(inputStatus);
-  }, [isFormFilled]);
+  const submitCardInfo = (e: React.FormEvent<HTMLFormElement>) => {
+    if (isFormFilled) {
+      addNewCard(nowCardInfo);
+      navigate("/CardListPage");
+    }
+  };
 
   return (
-    <form ref={formElement} className="form">
+    <form onSubmit={(e) => submitCardInfo(e)} className="form">
       <CardPreview card={nowCardInfo} />
       <InputBoxCardNumber
         changeCardNumberStatus={changeInputStatus("cardNumber")}
@@ -157,62 +121,3 @@ export default function CardInputForm(props: Props) {
     </form>
   );
 }
-
-// const changeNowCardInfo = (
-//   key: keyof CreditCard,
-//   value: any,
-//   index?: number
-// ) => {
-//   if (key === "number" && index !== undefined) {
-//     const updateCardInfo = JSON.parse(JSON.stringify(nowCardInfo));
-//     updateCardInfo.number[index] = value;
-
-//     cardNumberStatus && setNowCardInfo(updateCardInfo);
-//     return;
-//   }
-
-//   if (key === "expirationDate") {
-//     setNowCardInfo({ ...nowCardInfo, [key]: value });
-//     return;
-//   }
-//   if (key === "owner") setNowCardInfo({ ...nowCardInfo, [key]: value });
-// };
-
-// const [cardNumberStatus, setCardNumberStatus] = useState<EachUserInputState>(
-//   initialEachUserInputState
-// );
-// const [expirationDateStatus, setExpirationDateStatus] =
-//   useState<EachUserInputState>(initialEachUserInputState);
-// const [ownerStatus, setOwnerStatus] = useState(initialEachUserInputState);
-// const [securityCodeStatus, setSecurityCodeStatus] = useState(
-//   initialEachUserInputState
-// );
-// const [passwordStatus, setPasswordStatus] = useState(
-//   initialEachUserInputState
-// );
-
-// function changeCardNumberStatus(key: "isComplete" | "userInput", value: any) {
-//   setCardNumberStatus(deepCopyObject(cardNumberStatus, key, value));
-// }
-
-// function changeCardExpirationDateStatus(
-//   key: "isComplete" | "userInput",
-//   value: any
-// ) {
-//   setExpirationDateStatus(deepCopyObject(expirationDateStatus, key, value));
-// }
-
-// function changeOwnerStatus(key: "isComplete" | "userInput", value: any) {
-//   setOwnerStatus(deepCopyObject(ownerStatus, key, value));
-// }
-
-// function changeSecurityCodeStatus(
-//   key: "isComplete" | "userInput",
-//   value: any
-// ) {
-//   setSecurityCodeStatus(deepCopyObject(ownerStatus, key, value));
-// }
-
-// function changePasswordStatus(key: "isComplete" | "userInput", value: any) {
-//   setPasswordStatus(deepCopyObject(passwordStatus, key, value));
-// }
