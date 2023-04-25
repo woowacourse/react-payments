7f353a61eeafeea7d55747c520d9871474d4de9b
ashleysyheo <ashleysyheo@gmail.com>
2023-04-25 21:33:01 +0900

    refactor: 카드사별 카드 스타일 CardItem css 파일에 정의

--- src/components/CardItem/CardItem.tsx
+++ src/components/CardItem/CardItem.tsx
@@ -2,24 +2,24 @@ import styles from './style.module.css';
 import { ComponentPropsWithoutRef, memo } from 'react';
 import { CardDisplayInformation } from '../../types';
 import { CARD_NUMBER_UNIT_MAX_LENGTH, REGEX, SECURITY_TEXT_ICON } from '../../constants';
-import { CARD_BG_COLOR } from '../../constants/cardColors';
+import { ISSUER_CLASS_NAME } from '../../constants/issuerClassName';
 
 interface CardItemProps extends ComponentPropsWithoutRef<'div'> {
   information: CardDisplayInformation;
 }
 
-function CardItem({ information, className }: CardItemProps) {
+function CardItem({ information, className = '' }: CardItemProps) {
   const cardNumber: string[] = information.cardNumber.match(REGEX.FOUR_CHAR_SEQUENCE) ?? [];
   const displayedCardNumber = cardNumber.concat(
     Array(CARD_NUMBER_UNIT_MAX_LENGTH - cardNumber.length).fill('')
   );
-  const style = {
-    backgroundColor: information.issuer && CARD_BG_COLOR[information.issuer],
-    color: information.issuer === '카카오뱅크' ? '#0e0e0e' : 'white',
-  };
 
   return (
-    <div className={`${styles.cardItemContainer} ${className}`} style={style}>
+    <div
+      className={`${styles.cardItemContainer} ${
+        information.issuer && styles[ISSUER_CLASS_NAME[information.issuer]]
+      } ${className}`}
+    >
       <p className={styles.cardIssuer}>{information.issuer}</p>
       <div className={styles.cardChip}></div>
       <div className={styles.cardNumber}>
