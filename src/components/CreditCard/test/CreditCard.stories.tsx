3b8c58139c6b8557a4686460b2f6e873816f24dc
nlom0218 <nlom0218@naver.com>
2023-04-25 19:52:46 +0900

    test: 카드 회사별 CreditCard 컴포넌트 스토리 작성

--- src/components/CreditCard/test/CreditCard.stories.tsx
+++ src/components/CreditCard/test/CreditCard.stories.tsx
@@ -41,3 +41,91 @@ invaildCreditCard.args = {
     expiry: '03/45',
   },
 };
+
+export const BCCreditCardCompany = Template.bind({});
+BCCreditCardCompany.args = {
+  fullFilled: true,
+  company: 'bc',
+  creditCard: {
+    number: '1234123412341234',
+    expiry: '03/45',
+    owner: 'NOAH',
+  },
+};
+
+export const KakaoCreditCardCompany = Template.bind({});
+KakaoCreditCardCompany.args = {
+  fullFilled: true,
+  company: 'kakao',
+  creditCard: {
+    number: '1234123412341234',
+    expiry: '03/45',
+    owner: 'NOAH',
+  },
+};
+
+export const ShinhanCreditCardCompany = Template.bind({});
+ShinhanCreditCardCompany.args = {
+  fullFilled: true,
+  company: 'shinhan',
+  creditCard: {
+    number: '1234123412341234',
+    expiry: '03/45',
+    owner: 'NOAH',
+  },
+};
+
+export const HanaCreditCardCompany = Template.bind({});
+HanaCreditCardCompany.args = {
+  fullFilled: true,
+  company: 'hana',
+  creditCard: {
+    number: '1234123412341234',
+    expiry: '03/45',
+    owner: 'NOAH',
+  },
+};
+
+export const HyundaiCreditCardCompany = Template.bind({});
+HyundaiCreditCardCompany.args = {
+  fullFilled: true,
+  company: 'hyundai',
+  creditCard: {
+    number: '1234123412341234',
+    expiry: '03/45',
+    owner: 'NOAH',
+  },
+};
+
+export const KBCreditCardCompany = Template.bind({});
+KBCreditCardCompany.args = {
+  fullFilled: true,
+  company: 'kb',
+  creditCard: {
+    number: '1234123412341234',
+    expiry: '03/45',
+    owner: 'NOAH',
+  },
+};
+
+export const LotteCreditCardCompany = Template.bind({});
+LotteCreditCardCompany.args = {
+  fullFilled: true,
+  company: 'lotte',
+  creditCard: {
+    number: '1234123412341234',
+    expiry: '03/45',
+    owner: 'NOAH',
+  },
+};
+
+export const WooriCreditCardCompany = Template.bind({});
+WooriCreditCardCompany.args = {
+  fullFilled: true,
+  company: 'woori',
+  creditCard: {
+    number: '1234123412341234',
+    expiry: '03/45',
+    owner: 'NOAH',
+  },
+};
