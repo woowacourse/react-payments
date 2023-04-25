904df4c6bfd9d68b1e5a3d119f5d8202792b5e58
2yunseong <dbsdltjd3701@naver.com>
2023-04-25 23:33:00 +0900

    feat: CardOwner 스토리북 구현

---
+++ src/stories/AddCardOwnerInput.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import AddCardOwnerInput from '../components/AddCardOwnerInput';
+import { stringToUpperCase } from '../utils/util';
+import { cardOwnerCondition } from '../pages/cardInputCondition';
+import useInput from '../hooks/useInput';
+import { APP_WIDTH } from './constants';
+
+export default {
+  title: 'AddCardOwnerInput',
+  component: AddCardOwnerInput,
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          width: APP_WIDTH,
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta<typeof AddCardOwnerInput>;
+
+type Story = StoryObj<typeof AddCardOwnerInput>;
+
+const AddHooks = () => {
+  const cardOwner = useInput('', cardOwnerCondition, stringToUpperCase);
+
+  return <AddCardOwnerInput cardOwner={cardOwner} />;
+};
+
+export const Primary: Story = {
+  render: () => <AddHooks />,
+};
