cd7333cbda03b2f4564b17ddbf2e76e9f88687dc
ukkodeveloper <ukkodeveloper@gmail.com>
2023-04-25 19:09:16 +0900

    feat: Register Page에서 모달 사용

--- src/components/pages/RegisterPage.tsx
+++ src/components/pages/RegisterPage.tsx
@@ -3,35 +3,60 @@ import { CreditCardContext } from '../../contexts/CreditCardContext';
 import CardRegisterForm from '../registerForm/cardRegisterForm/CardRegisterForm';
 import { useState } from 'react';
 import CreditCardInfo from '../../@types/creditCardInfo';
+import BottomModal from '../@common/BottomModal';
+import BankSelectModal from '../BankSelectModal/BankSelectModal';
+import { Banks, COLOR_BY_BANK, KOR_BANK_NAME_BY_BANK } from '../../@types/banks';
 
 function RegisterPage() {
-  const [creditCardEntered, setCreditCardEntered] = useState<CreditCardInfo>({
+  const [creditCardInfo, setCreditCardInfo] = useState<CreditCardInfo>({
     cardNumber: ['', '', '', ''],
     expirationDate: ['', ''],
     ownerName: '',
     securityCode: '',
     password: ['', ''],
-    bank: '현대카드',
+    bank: 'hyundai' as Banks,
   });
+
+  const [showModal, setShowModal] = useState(true);
 
   const setCreditCard = <T extends keyof CreditCardInfo>(
     target: T,
     newValue: CreditCardInfo[T]
   ) => {
-    setCreditCardEntered((prev) => ({
+    setCreditCardInfo((prev) => ({
       ...prev,
       [target]: newValue,
     }));
   };
 
+  const bankName = KOR_BANK_NAME_BY_BANK[creditCardInfo.bank];
+  const cardColor = COLOR_BY_BANK[creditCardInfo.bank];
+
   return (
-    <CreditCardContext.Provider value={[creditCardEntered, setCreditCard]}>
+    <CreditCardContext.Provider value={[creditCardInfo, setCreditCard]}>
       <Card
-        cardNumber={creditCardEntered.cardNumber}
-        ownerName={creditCardEntered.ownerName}
-        expirationDate={creditCardEntered.expirationDate}
+        cardNumber={creditCardInfo.cardNumber}
+        ownerName={creditCardInfo.ownerName}
+        expirationDate={creditCardInfo.expirationDate}
+        onClick={() => {
+          setShowModal(true);
+        }}
+        bankName={bankName}
+        cardColor={cardColor}
       />
       <CardRegisterForm />
+      <BottomModal
+        open={showModal}
+        onClose={() => {
+          setShowModal(false);
+        }}
+      >
+        <BankSelectModal
+          onClose={() => {
+            setShowModal(false);
+          }}
+        ></BankSelectModal>
+      </BottomModal>
     </CreditCardContext.Provider>
   );
 }
