f3338c56a5c9a716045df5c882824e86c0c66c3f
guridaek <xylitol79@gmail.com>
2023-04-25 16:37:39 +0900

    feat: 카드 유효기간 유효성 검사 추가

--- src/hooks/useCardDate.ts
+++ src/hooks/useCardDate.ts
@@ -4,12 +4,28 @@ function useCardDate() {
   const [cardDate, setCardDate] = useState("");
 
   const changeCardDate = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const dateString = e.target.value.replace(/[^\d]/g, "").slice(0, 4);
+    const dateString = e.currentTarget.value.replace(/[^\d]/g, "").slice(0, 4);
 
     const expirationDate = dateString.match(/.{1,2}/g);
+
+    e.currentTarget.setCustomValidity(
+      expirationDate && isValid(expirationDate[0], expirationDate[1])
+        ? ""
+        : "올바른 유효 기간을 입력해야 합니다."
+    );
+
     const resultDate = expirationDate ? expirationDate.join("/") : "";
 
     setCardDate(resultDate);
+
+    console.log(resultDate);
+  };
+
+  const isValid = (MM: string, YY: string) => {
+    if (!MM || +MM < 1 || +MM > 12) return false;
+    if (!YY || +YY < 23) return false;
+
+    return true;
   };
 
   return { cardDate, changeCardDate };
