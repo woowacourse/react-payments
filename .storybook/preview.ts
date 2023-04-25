690870e869dcecb32965161ef1154cfe47517b5f
Dain Lee <feb.dain@gmail.com>
2023-04-25 21:27:16 +0900

    test: CardNumberInput storybook 작성

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,12 @@
-/** @type { import('@storybook/react').Preview } */
+import { withThemeFromJSXProvider } from "@storybook/addon-styling";
+import { GlobalStyle } from "../src/GlobalStyle";
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    GlobalStyles: GlobalStyle,
+  }),
+];
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import MainPage from "pages/MainPage";
 import CardRegisterForm from "pages/RegisterPage/CardRegisterForm";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./css/reset.css";
 
 const App = () => {
   return (
