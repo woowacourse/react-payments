ca2a22338e71672b413bf9166c6bfb4ec86e9886
2yunseong <dbsdltjd3701@naver.com>
2023-04-25 23:41:27 +0900

    feat: AddCardSecurityCodeInput 스토리북 구현

---
+++ src/stories/AddCardSecurityCodeInput.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import AddCardSecurityCodeInput from '../components/AddCardSecurityCodeInput';
+import { handleNumberInput } from '../utils/util';
+import { securityCodeCondition } from '../pages/cardInputCondition';
+import useInput from '../hooks/useInput';
+import { APP_WIDTH } from './constants';
+
+export default {
+  title: 'AddCardSecurityCodeInput',
+  component: AddCardSecurityCodeInput,
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          width: APP_WIDTH,
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta<typeof AddCardSecurityCodeInput>;
+
+type Story = StoryObj<typeof AddCardSecurityCodeInput>;
+
+const AddHooks = () => {
+  const securityCode = useInput('', securityCodeCondition, handleNumberInput);
+
+  return <AddCardSecurityCodeInput securityCode={securityCode} />;
+};
+
+export const Primary: Story = {
+  render: () => <AddHooks />,
+};
