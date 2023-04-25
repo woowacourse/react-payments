fb38fe4559287b1d21d587c9005a17cdcfac6947
NaveOWO <pianowell96@gmail.com>
2023-04-26 01:59:09 +0900

    feat : input 전체 상태에 대한 provider를 반환하는 함수로 변경

--- src/components/common/InputBox.tsx
+++ src/components/common/InputBox.tsx
@@ -1,56 +1,13 @@
 import React from "react";
-import styled from "styled-components";
-import { LABEL } from "../../constants/inputInfo";
-import { LabelOption } from "../../type/input";
+import { InputProvider } from "../../contexts/inputContext";
 
-export interface InputBoxProps extends React.HTMLAttributes<HTMLDivElement> {
-  type: LabelOption;
-  render?: () => JSX.Element;
+export interface InputBoxProps {
   children: React.ReactNode;
-  error?: string | null;
+  inputState: any;
 }
 
 export function InputBox(props: InputBoxProps) {
-  const { type, render, children, error, ...restProps } = props;
-  return (
-    <InputContainer>
-      {render && render()}
-      <Label>{LABEL[type]}</Label>
-      <InputWrapper {...restProps}>{children}</InputWrapper>
-      {error && <ErrorMessage>{error}</ErrorMessage>}
-    </InputContainer>
-  );
+  const { children, inputState } = props;
+
+  return <InputProvider inputState={inputState}>{children}</InputProvider>;
 }
-
-const InputContainer = styled.div`
-  position: relative;
-  width: 100%;
-
-  display: flex;
-  flex-direction: column;
-`;
-
-const Label = styled.label`
-  ${({ theme }) => theme.fonts.label};
-  color: ${({ theme }) => theme.colors.gray400};
-  margin-bottom: 0.3rem;
-`;
-
-const InputWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 4.5rem;
-
-  ${({ theme }) => theme.fonts.body}
-
-  background: ${({ theme }) => theme.colors.gray200};
-  border-radius: 7px;
-`;
-
-const ErrorMessage = styled.strong`
-  margin-top: 1rem;
-
-  color: ${({ theme }) => theme.colors.error};
-`;
