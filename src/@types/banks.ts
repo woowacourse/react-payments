43883ca6615e5625406be0f10b1a59b136f6c53f
ukkodeveloper <ukkodeveloper@gmail.com>
2023-04-25 19:06:02 +0900

    feat: 카드사 관련 이미지, 색상 등 상수화 및 타입 구현

---
+++ src/@types/banks.ts
@@ -0,0 +1,43 @@
+import woori from '../assets/woori.png';
+import bc from '../assets/bc.png';
+import shinhan from '../assets/shinhan.png';
+import kakao from '../assets/kakao.png';
+import kb from '../assets/kb.png';
+import lotte from '../assets/lotte.png';
+import hana from '../assets/hana.png';
+import hyundai from '../assets/hyundai.png';
+
+export const KOR_BANK_NAME_BY_BANK = {
+  bc: 'BC카드',
+  shinhan: '신한카드',
+  kakao: '카카오뱅크',
+  hyundai: '현대카드',
+  woori: '우리카드',
+  lotte: '롯데카드',
+  hana: '하나카드',
+  kb: '국민카드',
+} as const;
+
+export const ICON_BY_BANK = {
+  bc,
+  shinhan,
+  kakao,
+  hyundai,
+  woori,
+  lotte,
+  hana,
+  kb,
+} as const;
+
+export const COLOR_BY_BANK = {
+  bc: '#F04651',
+  shinhan: '#025CFF',
+  kakao: '#FFE600',
+  hyundai: '#000000',
+  woori: '#6BCAF2',
+  lotte: '#F25A5C',
+  hana: '#009490',
+  kb: '#6A6056',
+} as const;
+
+export type Banks = keyof typeof KOR_BANK_NAME_BY_BANK;
