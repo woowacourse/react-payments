edae4a965a86a19c17a294c02387ed7982cf593c
minjaeKim <smallkdb@gmail.com>
2023-04-25 17:08:33 +0900

    feat: 앱에 적용되는 기본 css 스타일 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
