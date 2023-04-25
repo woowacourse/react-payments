b4546ee3be2e65d6df8c3c2f226ad9e3c3bd4a37
nlom0218 <nlom0218@naver.com>
2023-04-25 14:07:09 +0900

    chore: paths alias 설정

--- craco.config.js
+++ craco.config.js
@@ -1,15 +1,14 @@
-const path = require('path');
+const { CracoAliasPlugin } = require('react-app-alias');
 
 module.exports = {
-  webpack: {
-    alias: {
-      '@Components': path.resolve(__dirname, 'src/components'),
-      '@Constant': path.resolve(__dirname, 'src/constant'),
-      '@Hooks': path.resolve(__dirname, 'src/hooks'),
-      '@Pages': path.resolve(__dirname, 'src/pages'),
-      '@Utils': path.resolve(__dirname, 'src/utils'),
-      '@Types': path.resolve(__dirname, 'src/types'),
-      '@Domains': path.resolve(__dirname, 'src/domains'),
+  plugins: [
+    {
+      plugin: CracoAliasPlugin,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './src',
+        tsConfigPath: './tsconfig.paths.json',
+      },
     },
-  },
+  ],
 };
