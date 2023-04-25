0c11041275558ff2c566480289c41c96a4b61f64
nlom0218 <nlom0218@naver.com>
2023-04-25 16:14:26 +0900

    test: CarCompany 회사별 스토리 작성

--- src/components/CardCompany/test/CardCompany.stories.tsx
+++ src/components/CardCompany/test/CardCompany.stories.tsx
@@ -15,3 +15,38 @@ export const BCCardCompany = Template.bind({});
 BCCardCompany.args = {
   company: 'BC',
 };
+
+export const KakaoCardCompany = Template.bind({});
+KakaoCardCompany.args = {
+  company: 'KAKAO',
+};
+
+export const ShinhanCardCompany = Template.bind({});
+ShinhanCardCompany.args = {
+  company: 'SHINHAN',
+};
+
+export const HanaCardCompany = Template.bind({});
+HanaCardCompany.args = {
+  company: 'HANA',
+};
+
+export const HyundaiCardCompany = Template.bind({});
+HyundaiCardCompany.args = {
+  company: 'HYUNDAI',
+};
+
+export const KBCardCompany = Template.bind({});
+KBCardCompany.args = {
+  company: 'KB',
+};
+
+export const LotteCardCompany = Template.bind({});
+LotteCardCompany.args = {
+  company: 'LOTTE',
+};
+
+export const WooriCardCompany = Template.bind({});
+WooriCardCompany.args = {
+  company: 'WOORI',
+};
