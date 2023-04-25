99f24b1830b393c9e9187f730aeafc62aad0d5f4
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-25 16:21:21 +0900

    test: CardNumberInput 컴포넌트 스토리 버전 개선 및 상태 추가
    CSF format3 버전으로 통일

--- src/components/CardNumberInput/CardNumberInput.stories.tsx
+++ src/components/CardNumberInput/CardNumberInput.stories.tsx
@@ -1,17 +1,40 @@
+import { useState } from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
 import GlobalStyle from "../../styles/GlobalStyle";
 import CardNumberInput from "./CardNumberInput";
+import { CardNumber, CardNumberGroups } from "../../types";
 
-export default {
+const meta: Meta<typeof CardNumberInput> = {
   title: "CardNumberInput",
   component: CardNumberInput,
 };
 
-export const Default = () => (
-  <>
-    <GlobalStyle />
-    <CardNumberInput
-      cardNumber={{ firstGroup: "", secondGroup: "", thirdGroup: "", fourthGroup: "" }}
-      onChange={() => {}}
-    ></CardNumberInput>
-  </>
-);
+export default meta;
+type Story = StoryObj<typeof CardNumberInput>;
+
+const CardNumberInputwithHooks = () => {
+  const [cardNumber, setCardNumber] = useState<CardNumber>({
+    firstGroup: "",
+    secondGroup: "",
+    thirdGroup: "",
+    fourthGroup: "",
+  });
+
+  const handleCardNumber = (value: string, targetGroup: CardNumberGroups) => {
+    setCardNumber({ ...cardNumber, [targetGroup]: value });
+  };
+
+  return <CardNumberInput cardNumber={cardNumber} onChange={handleCardNumber} />;
+};
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <>
+        <GlobalStyle />
+        <CardNumberInputwithHooks />
+      </>
+    );
+  },
+};
