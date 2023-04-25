e5089833f7b8b765f3a4299b76616276ca5b6b7a
ashleysyheo <ashleysyheo@gmail.com>
2023-04-25 19:52:10 +0900

    feat: useCardList 커스텀 훅 생성

---
+++ src/hooks/useCardList.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { getLocalStorage, saveToLocalStorage } from '../utils/localStorage';
+import { Card } from '../types';
+
+const useCard = () => {
+  const [cardList, setCardList] = useState<Card[]>(getLocalStorage() ?? []);
+
+  const newCardId = (cardList.at(-1)?.id ?? 0) + 1;
+  const cardListLength = cardList.length;
+
+  useEffect(() => {
+    saveToLocalStorage(cardList);
+  }, [cardList]);
+
+  const addCard = (newCard: Card) => {
+    setCardList((cardList) => [...cardList, newCard]);
+  };
+
+  const updateCardName = (id: number, cardName: string) => {
+    setCardList((cardList) => {
+      return cardList.map((card) => {
+        if (card.id === id) card.cardName = cardName;
+
+        return card;
+      });
+    });
+  };
+
+  return { cardList, newCardId, cardListLength, addCard, updateCardName };
+};
+
+export { useCard };
