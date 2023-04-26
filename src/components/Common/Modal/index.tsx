4933713926966ed8c7c6d2dfabc4a3b72ecf2f2e
minjaeKim <smallkdb@gmail.com>
2023-04-26 17:02:04 +0900

    refactor: Modal 컴포넌트 스타일 통일

--- src/components/Common/Modal/index.tsx
+++ src/components/Common/Modal/index.tsx
@@ -1,25 +1,6 @@
 import { createPortal } from 'react-dom';
 import styled from 'styled-components';
 
-const Style = {
-  BackDrop: styled.div`
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background: rgba(0, 0, 0, 0.35);
-  `,
-  Wrapper: styled.div`
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    padding: 16px;
-    border-radius: 8px 8px 0 0;
-    background: white;
-  `,
-};
-
 interface ModalProps {
   children: React.ReactNode;
   closeModal: () => void;
@@ -28,11 +9,29 @@ interface ModalProps {
 function Modal({ children, closeModal }: ModalProps) {
   return createPortal(
     <>
-      <Style.BackDrop onClick={closeModal} />
-      <Style.Wrapper>{children}</Style.Wrapper>
+      <StyledBackDrop onClick={closeModal} />
+      <StyledWrapper>{children}</StyledWrapper>
     </>,
     document.body,
   );
 }
+
+const StyledBackDrop = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.35);
+`;
+
+const StyledWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding: 16px;
+  border-radius: 8px 8px 0 0;
+  background: white;
+`;
 
 export default Modal;
