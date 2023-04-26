daf59f72edab1aad6f8c8c69b073092e417d0184
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-26 14:09:28 +0900

    refactor: 카드사 type 개선
    카드사 상수로 부터 type을 추출하도록 하여 카드사 추가 시 type이 자동 추가되도록 개선하였음.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import CARD_COMPANIES from "../constants/cardCompanies";
+
 export type CardNumberGroups = "firstGroup" | "secondGroup" | "thirdGroup" | "fourthGroup";
 export type CardNumber = Record<CardNumberGroups, string>;
 
@@ -7,7 +9,7 @@ export type CardExpirationDate = Record<CardExpirationDateKey, string>;
 export type CardPasswordKey = "first" | "second";
 export type CardPassword = Record<CardPasswordKey, string>;
 
-export type CardCompany = "BC" | "SHINHAN" | "HANA" | "HYUNDAI" | "KAKAO" | "KB" | "LOTTE" | "WOORI";
+export type CardCompany = keyof typeof CARD_COMPANIES;
 
 export type Card = {
   cardNumber: CardNumber;
@@ -15,5 +17,5 @@ export type Card = {
   ownerName: string;
   securityCode: string;
   password: CardPassword;
-  cardCompany: CardCompany;
+  cardCompany?: CardCompany;
 };
