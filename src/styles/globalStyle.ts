b4b3a14eb5a3468094684fc66cdb5e7991ad47b6
NaveOWO <pianowell96@gmail.com>
2023-04-26 21:21:53 +0900

    style : 뷸필요한 코드 제거

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -22,17 +22,4 @@ export const GlobalStyle = createGlobalStyle`
     font-size: 62.5%;
   }
   
-  /* Colors *****************************************/
-  :root {
-    --grey-100: #ECEBF1;
-    --grey-200: #E5E5E5;
-    --grey-300: #525252;
-    --black: #333333;
-    --yellow: #CBBA64;
-
-    --text-title: 600 20px/24px san-serif;
-    --text-subtitle: 600 18px/28px san-serif;
-    --text-body: 400 16px/24px san-serif;
-    --text-caption: 500 12px/14px Roboto ;
-  }
 `;
