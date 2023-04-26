c6ed02ee21c3c9359f129e5a6b65eb798142d525
Q Kim <qoli0703@gmail.com>
2023-04-26 15:07:15 +0900

    test: 카드 미리보기 카드사 선택 기능 추가

--- src/stories/CardPreview.stories.tsx
+++ src/stories/CardPreview.stories.tsx
@@ -1,43 +1,56 @@
 import type { Meta, StoryObj } from "@storybook/react";
-
 import CardPreview from "../component/common/CardPreview";
-import { CARD_COMPANIES_ENGLISH } from "../type/CardCompany";
+import { CARD_COMPANIES_ENGLISH, cardCompanyEnglishToKorean } from "../type/CardCompany";
+import { getDefaultCreditCard } from "../type/CreditCard";
 
 type Story = StoryObj<typeof CardPreview>;
 
 const meta: Meta = {
-  title: "CardPreview",
+  title: "Card Preview",
   component: CardPreview,
   argTypes: {
-    // card: {
-    // },
-    
     company: {
-      options: CARD_COMPANIES_ENGLISH,
+      name: '카드사',
+      options: Object.fromEntries(
+        CARD_COMPANIES_ENGLISH.map((enName) => [cardCompanyEnglishToKorean(enName), enName])
+      ),
       control: { type: 'inline-radio' },
+    },
+    card: {
+      table: { disable: true },
     },
   },
 };
 
 export default meta;
 
-export const CreditCard: Story = {
-  args: { 
-    card: { number: ['1234', '1234', '1234', '1234'], name: '', date: '12/25' , securityCode: 0, password: 0},
+export const EmptyCard: Story = {
+  render: (args) => {
+    const newArg = {
+      card: {
+        ...getDefaultCreditCard(),
+        ...args,
+      },
+    };
+    return (
+      <CardPreview {...newArg}/>
+    );
   },
 };
 
-// export const emptyCard = (args: any) => <CardPreview {...args}></CardPreview>;
-// emptyCard.args = {
-//   card: {},
-// };
-
-// export const filledCard = (args: any) => <CardPreview {...args}></CardPreview>;
-// filledCard.args = {
-//   card: {
-//     company
-//     name: "qwer",
-//     number: [1234, 5678, 9012, 3456],
-//     date: "12/25",
-//   },
-// };
+export const FilledCard: Story = {
+  render: (args) => {
+    const newArg = {
+      card: {
+        ...getDefaultCreditCard(),
+        ...args,
+        number: ['1234', '5678', '9012', '3456'],
+        name: 'CHAMSAE',
+        date: '02/25',
+      },
+    };
+    return (
+      <CardPreview {...newArg}/>
+    );
+  },
+};
