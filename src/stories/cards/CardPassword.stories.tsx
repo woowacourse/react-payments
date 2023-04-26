8c8f8298571e8b670adba9d7b0258788fbdcdac0
ashleysyheo <ashleysyheo@gmail.com>
2023-04-26 10:40:36 +0900

    refactor: CardPassword 컴포넌트 스토리 수정

--- src/stories/cards/CardPassword.stories.tsx
+++ src/stories/cards/CardPassword.stories.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState } from 'react';
 import type { Meta } from '@storybook/react';
 import CardPassword from '../../components/CardAddForm/CardPassword/CardPassword';
 
@@ -9,10 +10,19 @@ const meta = {
 
 export default meta;
 
-export const Default = () => (
-  <CardPassword
-    onInputChange={() => {}}
-    changeInputValidation={() => {}}
-    values={['', '']}
-  ></CardPassword>
-);
+export const Default = () => {
+  const [securityCode, setSecurityCode] = useState(['', '']);
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSecurityCode((securityCode) => {
+      const newSecurityCode = [...securityCode];
+      newSecurityCode[Number(event.target.dataset.index)] = event.target.value;
+
+      return newSecurityCode;
+    });
+  };
+
+  return (
+    <CardPassword onInputChange={onChange} values={securityCode} isValid={false}></CardPassword>
+  );
+};
