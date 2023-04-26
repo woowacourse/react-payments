88e389cc74d891932b3df65bcb61ce469857e0e4
hae-on <solbi2004@naver.com>
2023-04-26 16:54:41 +0900

    feat: ESC 키 눌렀을 때 bottom sheet 종료 기능 구현

--- src/hooks/useBottomSheet.ts
+++ src/hooks/useBottomSheet.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useBottomSheet = () => {
   const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
@@ -10,6 +10,17 @@ export const useBottomSheet = () => {
   const handleBottomSheetClose = () => {
     setIsBottomSheetOpen(false);
   };
+
+  useEffect(() => {
+    const handleEscapeKey = (event: KeyboardEvent) => {
+      if (event.code === 'Escape') {
+        setIsBottomSheetOpen(false);
+      }
+    };
+    document.addEventListener('keydown', handleEscapeKey);
+
+    return () => document.removeEventListener('keydown', handleEscapeKey);
+  }, []);
 
   return { isBottomSheetOpen, handleBottomSheetOpen, handleBottomSheetClose };
 };
