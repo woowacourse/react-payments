bd5a4f7be46d76b2e98b1a37d55be0cc4cccf526
turtle601 <turtle601@naver.com>
2023-04-26 10:55:12 +0900

    feat(CardExpiration): CardExpiration 에러 처리 및 기능 구현

--- src/components/molecules/CardExpiration/index.tsx
+++ src/components/molecules/CardExpiration/index.tsx
@@ -1,27 +1,78 @@
 import React from 'react';
 import styled from 'styled-components';
+import Input from '../../atomics/Input';
 
-import CardInput from '../../atomics/CardInput';
-import ErrorMessage from '../../atomics/ErrorMessage';
 import Message from '../../atomics/Message';
+
 import { VStack } from '../../layout/flexbox';
+
+import { changeCardExpirationDate } from '../../../store/action';
+import { useExpirationError } from '../../../hooks/useExpirationError';
+import { useCardFocusRefs, useCardPaymentDispatch } from '../../../hooks/useContextHooks';
 
 /* component */
 const CardExpiration: React.FC = () => {
+  const inputRefs = useCardFocusRefs();
+  const [{ isDataError, isError1, isError2 }, { handleError1, handleError2 }] =
+    useExpirationError();
+
+  const dispatch = useCardPaymentDispatch();
+
+  const handleChange = (id: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(changeCardExpirationDate(id, e.target.value));
+
+    if (id === 0) {
+      handleError1(e);
+      return;
+    }
+
+    handleError2(e);
+  };
+
   return (
     <StyledCardExpirationWrapper>
       <StyledCardLabel>
-        <Message type="label">만료일</Message>
-        <CardInput
-          type="text"
-          placeholder="MM / YY"
-          width="150px"
-          minLength={7}
-          maxLength={7}
-          center={true}
-        />
+        <Message fontWeight={500} fontSize="12px" color="#525252" lineHeight="14px">
+          만료일
+        </Message>
+        <StyledCardInputWrapper>
+          <Input
+            id={4}
+            type="text"
+            variant="underline"
+            placeholder="MM"
+            pattern="0[1-9]|1[012]"
+            width="75px"
+            minLength={2}
+            maxLength={2}
+            center={true}
+            required={true}
+            isValid={!isError1}
+            ref={(el: HTMLInputElement) => (inputRefs.current[4] = el)}
+            onChange={handleChange(0)}
+          />
+          <Input
+            id={5}
+            type="text"
+            variant="underline"
+            placeholder="YY"
+            pattern="^2[3-9]{1}$|^[3-9]{1}[0-9]{1}$"
+            width="75px"
+            minLength={2}
+            maxLength={2}
+            center={true}
+            required={true}
+            isValid={!isError2}
+            ref={(el: HTMLInputElement) => (inputRefs.current[5] = el)}
+            onChange={handleChange(1)}
+          />
+        </StyledCardInputWrapper>
       </StyledCardLabel>
-      <ErrorMessage isError={true}>만료가 되지 않은 카드 만료일만 입력이 가능합니다!!</ErrorMessage>
+      {(isDataError || isError1 || isError2) && (
+        <Message fontWeight={500} fontSize="12px" color="red" lineHeight="14px">
+          만료가 되지 않은 카드 만료일만 입력이 가능합니다!!
+        </Message>
+      )}
     </StyledCardExpirationWrapper>
   );
 };
@@ -30,7 +81,7 @@ const CardExpiration: React.FC = () => {
 const StyledCardLabel = styled.label`
   ${VStack}
 
-  span + input {
+  span + div {
     margin-top: 8px;
   }
 `;
@@ -43,6 +94,12 @@ const StyledCardExpirationWrapper = styled.article`
   }
 
   margin-bottom: 8px;
+`;
+
+const StyledCardInputWrapper = styled.div`
+  input + input {
+    margin-left: 12px;
+  }
 `;
 
 export default CardExpiration;
