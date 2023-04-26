f560883e588803166e0f388e47fd7b89374290b9
bassyu <yrkdgus@gmail.com>
2023-04-26 14:03:43 +0900

    feat(Modal): 카드사 선택 모달 구현

---
+++ src/components/template/Modal.tsx
@@ -0,0 +1,125 @@
+import type { CardType } from '../../types';
+import styled from 'styled-components';
+
+interface Props {
+  closeModal: () => void;
+  setCardCompany: (cardCompany: CardType['cardCompany']) => void;
+}
+
+const Modal = ({ closeModal, setCardCompany }: Props) => {
+  const onClickCardCompanyItem = (cardCompany: CardType['cardCompany']) => () => {
+    setCardCompany(cardCompany);
+    closeModal();
+  };
+
+  return (
+    <>
+      <ModalBackdrop />
+      <ModalWrapper>
+        {CARD_COMPANY_MATRIX.map((row) => (
+          <CardCompanyRow>
+            {row.map(({ iconSrc, cardCompany }) => (
+              <CardCompanyItem onClick={onClickCardCompanyItem(cardCompany)}>
+                <img src={iconSrc} alt={cardCompany} />
+                <p>{cardCompany}</p>
+              </CardCompanyItem>
+            ))}
+          </CardCompanyRow>
+        ))}
+      </ModalWrapper>
+    </>
+  );
+};
+
+export default Modal;
+
+const ModalBackdrop = styled.div`
+  position: absolute;
+  top: 0;
+
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.6);
+`;
+
+const ModalWrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+
+  width: 100%;
+  height: auto;
+  border-radius: 6px 6px 0px 0px;
+  padding: 34px 48px;
+
+  background: white;
+`;
+
+const CardCompanyRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  & + & {
+    margin-top: 26px;
+  }
+`;
+
+const CardCompanyItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 54px;
+  height: 70px;
+
+  cursor: pointer;
+
+  img {
+    width: 38px;
+  }
+
+  p {
+    font-size: 12px;
+    font-weight: 500;
+    color: #525252;
+  }
+`;
+
+const CARD_COMPANY_MATRIX = [
+  [
+    {
+      iconSrc: './icons/BC카드.svg',
+      cardCompany: 'BC카드',
+    },
+    {
+      iconSrc: './icons/신한카드.svg',
+      cardCompany: '신한카드',
+    },
+    {
+      iconSrc: './icons/카카오뱅크.svg',
+      cardCompany: '카카오뱅크',
+    },
+    {
+      iconSrc: './icons/현대카드.svg',
+      cardCompany: '현대카드',
+    },
+  ],
+  [
+    {
+      iconSrc: './icons/우리카드.svg',
+      cardCompany: '우리카드',
+    },
+    {
+      iconSrc: './icons/롯데카드.svg',
+      cardCompany: '롯데카드',
+    },
+    {
+      iconSrc: './icons/하나카드.svg',
+      cardCompany: '하나카드',
+    },
+    {
+      iconSrc: './icons/국민카드.svg',
+      cardCompany: '국민카드',
+    },
+  ],
+];
