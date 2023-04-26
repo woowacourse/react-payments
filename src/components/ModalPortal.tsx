523ce3b8158f01d6a22e39f22a0047f80c78747b
Patrick <kuyt3122@naver.com>
2023-04-27 00:21:13 +0900

    feat: 모달창 구현

---
+++ src/components/ModalPortal.tsx
@@ -0,0 +1,42 @@
+import React, { ReactChild, useEffect, useRef } from 'react';
+import ReactDom from 'react-dom';
+
+interface Props {
+  children: ReactChild;
+  closeEvent: () => void;
+}
+
+const ModalPortal=(props:Props)=> {
+  const $modalRoot = document.getElementById('modal-root') as HTMLElement;
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  const dialogKeyDownListener=(event: React.KeyboardEvent<HTMLDialogElement>)=>{
+    if (event.key === 'Escape') {
+      props.closeEvent();
+    }
+  }
+
+  const dialogBackdropListener=(event: React.MouseEvent<HTMLDialogElement>)=>{
+    if (event.target === event.currentTarget) {
+      props.closeEvent();
+    }
+  }
+
+  useEffect(() => {
+    modalRef.current?.showModal();
+    
+    return () => {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        modalRef.current?.close();
+    };
+  }, []);
+
+    return ReactDom.createPortal(
+      <dialog ref={modalRef} onKeyDown={dialogKeyDownListener} onClick={dialogBackdropListener}>
+        {props.children}
+      </dialog>,
+      $modalRoot
+    );
+}
+
+export default ModalPortal;
