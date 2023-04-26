59bc2bda72501b4b22c117dd05f142fdc39cf2a3
chsua <csj1919@naver.com>
2023-04-26 14:10:38 +0900

    refactor: 사용하지 않는 파일 삭제

--- src/util/deepCopy.ts
+++
@@ -1,6 +0,0 @@
-export const deepCopyObject = (origin: object, key: string, value: any) => {
-  const updateResult = JSON.parse(JSON.stringify(origin));
-
-  updateResult[key] = value;
-  return updateResult;
-};
