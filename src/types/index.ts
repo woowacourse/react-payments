d857e0291dc5c4249e00b6862fb8d04a0ef378a1
bassyu <yrkdgus@gmail.com>
2023-04-26 14:06:41 +0900

    feat(types): CardType에 카드사와 카드이름 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,16 @@
 export interface CardType {
   id: number;
+  cardCompany: string;
   cardNumber: string[];
   expireDate: string[];
   ownerName: string;
   securityCode: string;
   cardPassword: string[];
+  cardName: string;
 }
 
 export enum Page {
   list = 'list',
   register = 'register',
+  name = 'name',
 }
