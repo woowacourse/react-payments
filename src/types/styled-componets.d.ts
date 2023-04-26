6c3990b20e973ea1be854966feb1df8da1319677
sh981013s <sh981013s@gmail.com>
2023-04-26 11:01:29 +0900

    feeat: 카드별 대표 색상 theme 에 추가 및 type 에 추가

--- src/types/styled-componets.d.ts
+++ src/types/styled-componets.d.ts
@@ -1,10 +1,15 @@
-import 'styled-components';
+import "styled-components";
+import { BankColors } from "../styles/theme";
 
-declare module 'styled-components' {
+declare module "styled-components" {
   interface Theme {
     border: string;
     color: string;
     backgroundColor: string;
+  }
+
+  interface BankColors {
+    [key: string]: string;
   }
 
   export interface DefaultTheme {
@@ -12,5 +17,6 @@ declare module 'styled-components' {
     dark: Theme;
     blue: Theme;
     pale: Theme;
+    banks: BankColors;
   }
 }
