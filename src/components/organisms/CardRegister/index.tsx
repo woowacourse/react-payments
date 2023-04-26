2c95b555f93b7832ed98a517ba3dd5efc5a29d77
turtle601 <turtle601@naver.com>
2023-04-26 10:58:10 +0900

    feat(CardRegister): CardRegister 컴포넌트 기능 구현

--- src/components/organisms/CardRegister/index.tsx
+++ src/components/organisms/CardRegister/index.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import styled from 'styled-components';
 import Button from '../../atomics/Button';
 import Message from '../../atomics/Message';
 
 /* component */
 const CardRegister: React.FC = () => {
+  const navigate = useNavigate();
+
+  const goAddCardPage = () => {
+    navigate('/addCard');
+  };
+
   return (
     <StyledCardRegisterWrapper>
       <Message fontSize="14px" lineHeight="16px" fontWeight={700} color="#575757" opacity={0.9}>
         새로운 카드를 등록해주세요
       </Message>
-      <Button type="button" kind="register">
-        +
+      <Button
+        type="button"
+        width="212px"
+        height="124px"
+        bgColor="#e5e5e5"
+        borderRadius="5px"
+        onClick={goAddCardPage}
+      >
+        <Message fontSize="30px" color="#575757">
+          +
+        </Message>
       </Button>
     </StyledCardRegisterWrapper>
   );
