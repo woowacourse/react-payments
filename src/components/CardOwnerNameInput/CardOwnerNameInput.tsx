7a5ee7d6debdca5665bc3d7ee578fc846bff4855
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-26 19:35:24 +0900

    refactor: 이름 입력 최대 길이 상수화 및 placeholder 개선
    영문입력 의미 부여

--- src/components/CardOwnerNameInput/CardOwnerNameInput.tsx
+++ src/components/CardOwnerNameInput/CardOwnerNameInput.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import { InputContainer, Input, Label } from "../common";
 import { isAlphabetic } from "../../validator/Validator";
 
+const MAX_NAME_LENGTH = 30;
+
 type CardOwnerNameInputProp = {
   ownerName: string;
   onChange: (name: string) => void;
@@ -20,16 +22,18 @@ const CardOwnerNameInput = ({ ownerName, onChange }: CardOwnerNameInputProp) => 
     <Label>
       <Div>
         <span>카드 소유자 이름</span>
-        <span>{ownerName.length} / 30</span>
+        <span>
+          {ownerName.length} / {MAX_NAME_LENGTH}
+        </span>
       </Div>
       <InputContainer width="318px">
         <Input
           value={ownerName}
           width="100%"
           textAlign="left"
-          placeholder="카드에 표시된 이름과 동일하게 입력하세요."
+          placeholder="카드에 표시된 영문 이름을 입력하세요."
           type="text"
-          maxLength={30}
+          maxLength={MAX_NAME_LENGTH}
           onChange={handleOwnerName}
         />
       </InputContainer>
