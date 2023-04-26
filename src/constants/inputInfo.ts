aa9f82e83c320dee631fba98c1736007b79c7496
NaveOWO <pianowell96@gmail.com>
2023-04-26 15:12:00 +0900

    feat : 상수 추가

--- src/constants/inputInfo.ts
+++ src/constants/inputInfo.ts
@@ -1,20 +1,40 @@
+import { ErrorMessage, TextLength } from "../type/input";
+
 export const LABEL = {
   NUMBER: "카드 번호",
   DATE: "만료일",
   NAME: "카드 소유자 이름(선택)",
   CODE: "보안 코드(CVC/CVV)",
   PASSWORD: "카드 비밀번호",
-} as const;
+};
 
-export const PLACEHOLDER = Object.freeze({
+export const PLACEHOLDER = {
   MONTH: "MM",
   YEAR: "YY",
   NAME: "카드에 표시된 이름과 동일하게 입력하세요.",
-});
+};
 
-export const MAX_LENGTH = Object.freeze({
-  NAME: 30,
-  DATE: 2,
+export const TEXT_LENGTH: TextLength = {
+  FIRST: 4,
+  SECOND: 4,
+  THIRD: 4,
+  FOURTH: 4,
+  MONTH: 1,
+  YAER: 2,
   CODE: 3,
-  PASSWORD: 1,
-});
+  FIRSTPASSWORD: 1,
+  SECONDPASSWORD: 1,
+  NAME: 0,
+};
+
+export const ERROR_MESSAGE: ErrorMessage = {
+  FIRST: "4개의 숫자를 입력해주세요",
+  SECOND: "4개의 숫자를 입력해주세요",
+  THIRD: "4개의 숫자를 입력해주세요",
+  FOURTH: "4개의 숫자를 입력해주세요",
+  MONTH: "올바른 날짜를 입력해주세요",
+  YAER: "올바른 날짜를 입력해주세요",
+  CODE: "3개의 숫자를 입력해주세요",
+  FIRSTPASSWORD: "올바른 비밀번호를 입력해주세요",
+  SECONDPASSWORD: "올바른 비밀번호를 입력해주세요",
+};
