34b01155a07d8bd4bb39ff5fd4476a09ed83e5ef
ukkodeveloper <ukkodeveloper@gmail.com>
2023-04-26 15:11:55 +0900

    chore: storybook actions add-on 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,41 +1,41 @@
 // .storybook/main.ts
 
 // Replace your-framework with the framework you are using (e.g., react-webpack5, vue3-webpack5)
-import type { StorybookConfig } from "@storybook/react-webpack5";
+import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-interactions',
+    '@storybook/addon-actions',
   ],
   typescript: {
     check: false,
     checkOptions: {},
-    reactDocgen: "react-docgen-typescript",
+    reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) =>
-        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
     },
   },
   framework: {
-    name: "@storybook/react-webpack5",
+    name: '@storybook/react-webpack5',
     options: {},
   },
   core: {
     disableTelemetry: true,
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
   },
   refs: {
-    "design-system": {
-      title: "Storybook Design System",
-      url: "https://5ccbc373887ca40020446347-yldsqjoxzb.chromatic.com",
+    'design-system': {
+      title: 'Storybook Design System',
+      url: 'https://5ccbc373887ca40020446347-yldsqjoxzb.chromatic.com',
     },
   },
 };
