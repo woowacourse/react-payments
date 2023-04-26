7d82eeaecd4a648f47af1da9150b7b6884df0744
sh981013s <sh981013s@gmail.com>
2023-04-26 11:22:39 +0900

    refactor: overflow 속성 제거

--- src/pages/MyCardList/MyCardList.styles.tsx
+++ src/pages/MyCardList/MyCardList.styles.tsx
@@ -1,9 +1,8 @@
-import styled from 'styled-components';
-import Flex from '../../components/@common/Flex/Flex';
+import styled from "styled-components";
+import Flex from "../../components/@common/Flex/Flex";
 
 export const Root = styled(Flex)`
   height: 100%;
-  overflow: scroll;
 
   padding: 50px 0;
 `;
