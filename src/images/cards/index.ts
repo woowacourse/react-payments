996b03feec98becba0777b137f643e2a75273e07
wzrabbit <curious.wzrabbit@gmail.com>
2023-04-26 14:51:57 +0900

    📦 chore: 카드 이미지 업로드

---
+++ src/images/cards/index.ts
@@ -0,0 +1,19 @@
+export { default as bcCard } from './background_bc_card.png';
+export { default as shinhanCard } from './background_shinhan_card.png';
+export { default as hanaCard } from './background_hana_card.png';
+export { default as hyundaiCard } from './background_hyundai_card.png';
+export { default as kakaoBankCard } from './background_kakao_bank_card.png';
+export { default as kbCard } from './background_kb_card.png';
+export { default as lotteCard } from './background_lotte_card.png';
+export { default as wooriCard } from './background_woori_card.png';
+
+export { default as bcIcon } from './icon_bc_card.png';
+export { default as shinhanIcon } from './icon_shinhan_card.png';
+export { default as hanaIcon } from './icon_hana_card.png';
+export { default as hyundaiIcon } from './icon_hyundai_card.png';
+export { default as kakaoBankIcon } from './icon_kakao_bank_card.png';
+export { default as kbIcon } from './icon_kb_card.png';
+export { default as lotteIcon } from './icon_lotte_card.png';
+export { default as wooriIcon } from './icon_woori_card.png';
+
+export { default as chip } from './chip.png';
