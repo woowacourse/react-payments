0204391df5c04bca9b8226af37c02f36b3536487
ashleysyheo <ashleysyheo@gmail.com>
2023-04-26 10:28:01 +0900

    refactor: CardList 컴포넌트 스토리 수정

--- src/stories/cards/CardList.stories.tsx
+++ src/stories/cards/CardList.stories.tsx
@@ -15,22 +15,28 @@ export const List = () => (
   <CardList
     cardList={[
       {
-        cardNumber: '12341234123412341234',
+        id: 1,
+        cardName: '카드 1',
+        issuer: '우리카드',
+        cardNumber: '1234123412341234',
         expirationDate: {
           month: '12',
           year: '23',
         },
-        ownerName: 'WOOWACOURSE',
+        ownerName: 'WOOWA',
         securityCode: '123',
         password: ['1', '1'],
       },
       {
-        cardNumber: '12341234123412341234',
+        id: 2,
+        issuer: '현대카드',
+        cardName: 'WOOWA의 현대카드',
+        cardNumber: '1234123412341234',
         expirationDate: {
           month: '12',
           year: '23',
         },
-        ownerName: 'WOOWACOURSE',
+        ownerName: 'WOOWA',
         securityCode: '123',
         password: ['1', '1'],
       },
