b30d95bbe35df0fe7f1e2577397f409fd04b14b3
NaveOWO <pianowell96@gmail.com>
2023-04-27 02:56:49 +0900

    feat : 상수 추가

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PATH } from "./constants/path";
 import { AddCard } from "./pages/addCard";
 import { CardList } from "./pages/cardList";
 import { ErrorPage } from "./pages/error";
@@ -7,8 +8,8 @@ export function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CardList />}></Route>
-        <Route path="/add-card" element={<AddCard />}></Route>
+        <Route path={PATH.CARD_LIST} element={<CardList />}></Route>
+        <Route path={PATH.ADD_CARD} element={<AddCard />}></Route>
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
