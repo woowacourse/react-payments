09c9f3264d1f6e9a87a8ac46671f1bc9692215c9
turtle601 <turtle601@naver.com>
2023-04-26 10:56:00 +0900

    feat(CardNumbers): CardNumbers 에러 처리 및 기능 구현

--- src/components/molecules/CardNumbers/index.tsx
+++ src/components/molecules/CardNumbers/index.tsx
@@ -1,27 +1,64 @@
 import React from 'react';
 import styled from 'styled-components';
+import {
+  useCardFocusRefs,
+  useCardPaymentDispatch,
+  useMoveFocus,
+} from '../../../hooks/useContextHooks';
+import { useInputError } from '../../../hooks/useInputError';
+import { changeNumbersAction } from '../../../store/action';
 
-import CardInput from '../../atomics/CardInput';
-import ErrorMessage from '../../atomics/ErrorMessage';
+import Input from '../../atomics/Input';
 import Message from '../../atomics/Message';
 
-import { SBetweenStack, VStack } from '../../layout/flexbox';
+import { VStack } from '../../layout/flexbox';
 
 /* component */
 const CardNumbers: React.FC = () => {
+  const inputRefs = useCardFocusRefs();
+  const moveFocus = useMoveFocus();
+  const errorList = [useInputError(), useInputError(), useInputError(), useInputError()];
+  const dispatch = useCardPaymentDispatch();
+
+  const handleChange =
+    (id: number, handleError: (e: React.ChangeEvent<HTMLInputElement>) => void) =>
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      handleError(e);
+      dispatch(changeNumbersAction(id, e.target.value));
+      moveFocus(e);
+    };
+
   return (
     <StyledCardNumbersWrapper>
       <StyledCardLabel>
-        <Message type="label">카드 번호</Message>
+        <Message fontWeight={500} fontSize="12px" color="#525252" lineHeight="14px">
+          카드 번호
+        </Message>
         <StyledCardInputWrapper>
-          {Array(4)
-            .fill(0)
-            .map(() => (
-              <CardInput type="text" width="75px" minLength={4} maxLength={4} center={true} />
-            ))}
+          {errorList.map(([error, handleError], idx) => (
+            <Input
+              id={idx}
+              key={idx}
+              type={idx < 2 ? 'text' : 'password'}
+              variant="underline"
+              pattern="[0-9]{4}"
+              width="75px"
+              minLength={4}
+              maxLength={4}
+              center={true}
+              required={true}
+              isValid={!error}
+              ref={(el: HTMLInputElement) => (inputRefs.current[idx] = el)}
+              onChange={handleChange(idx, handleError)}
+            />
+          ))}
         </StyledCardInputWrapper>
       </StyledCardLabel>
-      <ErrorMessage isError={true}>총 16자리 카드 번호를 입력해주세요!!</ErrorMessage>
+      {(errorList[0][0] || errorList[1][0] || errorList[2][0] || errorList[3][0]) && (
+        <Message fontWeight={500} fontSize="12px" color="red" lineHeight="14px">
+          총 16자리 카드 번호를 입력해주세요!!
+        </Message>
+      )}
     </StyledCardNumbersWrapper>
   );
 };
@@ -30,19 +67,12 @@ const CardNumbers: React.FC = () => {
 const StyledCardLabel = styled.label`
   ${VStack}
 
-  span + p {
+  span + div {
     margin-top: 8px;
   }
 `;
 
-const StyledCardInputWrapper = styled.p`
-  ${SBetweenStack}
-
-  width: 350px;
-  height: 45px;
-
-  background-color: #ecebf1;
-
+const StyledCardInputWrapper = styled.div`
   input + input {
     margin-left: 12px;
   }
