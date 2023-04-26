443ecf9dcde8f16374796300803abce9fb3e7e0f
turtle601 <turtle601@naver.com>
2023-04-26 10:45:27 +0900

    refactor(utils): 사용하지 않는 utils 파일 제거

--- src/utils/localStorage.ts
+++
@@ -1,8 +0,0 @@
-export const getLocalStorage = (key: string) => {
-  const item = localStorage.getItem(key);
-  return item ? JSON.parse(item) : [];
-};
-
-export const setLocalStorage = <T extends Object>(key: string, value: T) => {
-  localStorage.setItem(key, JSON.stringify(value));
-};
