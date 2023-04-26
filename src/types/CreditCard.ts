d3c71269cca93c0ac9876b6ac4297efe6a75a602
yeopto <gunyeop530@gmail.com>
2023-04-26 16:00:32 +0900

    feat: CreditCard 속성 타입을 추가해주다

--- src/types/CreditCard.ts
+++ src/types/CreditCard.ts
@@ -1,8 +1,11 @@
+import type { CardCompanyButtonProps } from '../components/CardCompanyButton';
+
 export type Month = string;
 
 export type Year = string;
 
 export type CreditCard = {
+  cardCompany?: CardCompanyButtonProps['cardCompany'];
   name: string;
   cardNumbers: string;
   expirationDate: [Month, Year];
