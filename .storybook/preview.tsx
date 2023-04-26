cdc43c611f4b2f10a23beba695d0fc1c703e85f4
turtle601 <turtle601@naver.com>
2023-04-26 10:59:40 +0900

    refactor(storybook): Context API 관련 Provider Storybook에 적용

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -4,12 +4,22 @@ import type { Preview } from '@storybook/react';
 import GlobalStyle from '../src/styles/global';
 import { BrowserRouter } from 'react-router-dom';
 
+import PaymentsProvider from '../src/components/context/PaymentsProvider';
+import FocusRefProvider from '../src/components/context/FocusRefsProvider';
+import CardListProvider from '../src/components/context/CardListProvider';
+
 export const decorators = [
   (Story) => {
     return (
       <BrowserRouter>
-        <GlobalStyle />
-        <Story />
+        <CardListProvider>
+          <PaymentsProvider>
+            <FocusRefProvider>
+              <GlobalStyle />
+              <Story />
+            </FocusRefProvider>
+          </PaymentsProvider>
+        </CardListProvider>
       </BrowserRouter>
     );
   },
