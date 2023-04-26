d90539cd80c8d74345e337cfe710e45978371a4e
nlom0218 <nlom0218@naver.com>
2023-04-26 14:02:03 +0900

    refactor: 모달 스타일 수정

--- src/components/Modal/style.ts
+++ src/components/Modal/style.ts
@@ -2,15 +2,18 @@ import styled, { keyframes } from 'styled-components';
 
 export const ModalLayout = styled.div`
   position: fixed;
+  top: 0;
+  border: 0;
+  left: 0;
+  right: 0;
   width: 375px;
   min-height: 100vh;
   margin: 0 auto;
-  background-color: rgba(0, 0, 0, 0.1);
+  background-color: rgba(0, 0, 0, 0.3);
 `;
 
 export const Backdrop = styled.div`
   height: 100vh;
-  filter: blur(1px);
 `;
 
 const ModalAppear = keyframes`
