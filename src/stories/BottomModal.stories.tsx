1d73fa36e8509dd01013a74a57ed1ebbe56847c5
Q Kim <qoli0703@gmail.com>
2023-04-26 18:08:21 +0900

    test: 모달 스토리북 추가

---
+++ src/stories/BottomModal.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import BottomModal from "../component/common/Modal/BottomModal";
+import { useState } from "react";
+
+type Story = StoryObj<typeof BottomModal>;
+
+const meta: Meta = {
+  title: "Bottom Modal",
+  component: BottomModal,
+  argTypes: {
+    setIsOpen: {
+      action: 'Closing modal',
+      description: 'This is for setting isOpen state to false.'
+    },
+    children: {
+      table: { disable: true },
+    },
+  },
+};
+
+export default meta;
+
+export const Modal: Story = {
+  args: {
+    isOpen: true,
+    children: <p>모달 내용은 여기에 표시됩니다!!</p>
+  }
+};
