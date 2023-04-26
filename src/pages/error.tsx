caf773de676d1a441152b9a28599867e67606fae
NaveOWO <pianowell96@gmail.com>
2023-04-27 02:40:52 +0900

    feat : error page 생성

---
+++ src/pages/error.tsx
@@ -0,0 +1,3 @@
+export function ErrorPage() {
+  return <div>잘못된 경로입니다.</div>;
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AddCard } from "./pages/addCard";
 import { CardList } from "./pages/cardList";
+import { ErrorPage } from "./pages/error";
 
 export function Router() {
   return (
@@ -8,6 +9,7 @@ export function Router() {
       <Routes>
         <Route path="/" element={<CardList />}></Route>
         <Route path="/add-card" element={<AddCard />}></Route>
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
