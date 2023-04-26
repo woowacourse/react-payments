b564edeffb1ede0ad814532ea7e5981892ff93d7
chsua <csj1919@naver.com>
2023-04-26 13:23:51 +0900

    feat: 보안코드 컴포넌트에 "?"버튼을 누르면 안내문구 보여주기/감추기

--- src/CONSTANT.ts
+++ src/CONSTANT.ts
@@ -3,7 +3,7 @@ export const CARD_ERROR_MESSAGE = {
   INPUT_CARD_EXPIRATION_DATE:
     "현재 월 기준 5년 이내의 만료일만 입력 가능합니다",
   INPUT_CARD_OWNER: "영문 이름을 입력해주세요",
-  INPUT_CARD_SECURITY: "카드 뒤에 표시된 3자리 숫자를 입력해주세요",
+  INPUT_CARD_SECURITY: "3자리 숫자를 입력해주세요",
   INPUT_CARD_PASSWORD: "카드 비밀번호 앞 두 자리 숫자를 입력해주세요",
 };
 
@@ -11,4 +11,8 @@ export const INPUT_STATUS = {
   ERROR: 0,
   NOT_COMPLETE: 1,
   COMPLETE: 2,
+};
+
+export const GUIDE_MESSAGE = {
+  SECURITY_CODE: "cvc/cvv번호는 카드 뒷면에 기재되어 있는 숫자 3자리입니다",
 };
--- src/component/CardInputPage/InputBoxSecurityCode/InputBoxSecurityCode.tsx
+++ src/component/CardInputPage/InputBoxSecurityCode/InputBoxSecurityCode.tsx
@@ -2,7 +2,7 @@ import { useState, ChangeEvent } from "react";
 import Input from "../../common/Input";
 
 import "./inputBoxSecurityCode.css";
-import { CARD_ERROR_MESSAGE } from "../../../CONSTANT";
+import { CARD_ERROR_MESSAGE, GUIDE_MESSAGE } from "../../../CONSTANT";
 import { makeAppropriateSecurityCode } from "../../../trans";
 
 interface InputBoxSecurityProps {
@@ -17,6 +17,7 @@ export default function InputBoxSecurityCode(props: InputBoxSecurityProps) {
 
   const [haveError, setHaveError] = useState(false);
   const [securityCode, setSecurityCode] = useState("");
+  const [guideClick, isGuideClick] = useState(false);
 
   const changeSecurityCode = (e: ChangeEvent<HTMLInputElement>) => {
     const userSecurityCode = e.target.value.slice(0, 3);
@@ -35,6 +36,10 @@ export default function InputBoxSecurityCode(props: InputBoxSecurityProps) {
     setSecurityCode(appropriateSecurityCode);
   };
 
+  const changeGuideClick = () => {
+    isGuideClick(!guideClick);
+  };
+
   return (
     <div className="input-box-security-code">
       <p>보안 코드(CVC/CVV)</p>
@@ -46,9 +51,16 @@ export default function InputBoxSecurityCode(props: InputBoxSecurityProps) {
         inputMode="numeric"
         value={securityCode}
       />
-      <button className="button-security-code" type="button">
+      <button
+        className="button-security-code"
+        type="button"
+        onClick={changeGuideClick}
+      >
         ?
       </button>
+      {guideClick && (
+        <p className="guide-security-code">{GUIDE_MESSAGE.SECURITY_CODE}</p>
+      )}
       <p className="error-message">
         {haveError && CARD_ERROR_MESSAGE.INPUT_CARD_SECURITY}
       </p>
