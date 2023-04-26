c824ebb2611d941370904be7bc910e2c686e993f
Jungwoo <evencoding@gmail.com>
2023-04-26 11:15:03 +0900

    refactor: localStorage 관련 로직을 hook이 아닌 utils로 변경

--- src/utils/handleLocalStorage.ts
+++ src/utils/handleLocalStorage.ts
@@ -1,11 +1,11 @@
-const useLocalStorage = <T>(key: string) => {
+const handleLocalStorage = <T>(key: string) => {
   const localData = JSON.parse(localStorage.getItem(key) || '[]');
 
   const setLocalData = (data: T[]) => {
     localStorage.setItem(key, JSON.stringify(data));
   };
 
-  return { localData, setLocalData };
+  return [localData, setLocalData];
 };
 
-export default useLocalStorage;
+export default handleLocalStorage;
