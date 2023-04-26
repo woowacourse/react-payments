e70d2e82fcefd6c97ba72afd994d731aad20df0f
wzrabbit <curious.wzrabbit@gmail.com>
2023-04-26 20:30:25 +0900

    ✨ feat(validators): 글자수 검사 기능 추가
    - 카드 소유자 이름 검증에 사용

--- src/components/AddCardForm/validators.ts
+++ src/components/AddCardForm/validators.ts
@@ -5,6 +5,7 @@ const YEAR_MONTH_FORMAT = /^\d{2}\/\d{2}$/;
 const STRICT_YEAR_MONTH_FORMAT = /^(?:1[0-2]|0[1-9])\/\d{2}$/;
 const CVC_FORMAT = /^\d{3}$/;
 const PASSWORD_FORMAT = /^\d{2}$/;
+const LIMIT_LENGTH = 30;
 
 const checkValidCardNumber = (cardNumber: string): ValidatorResponseType => {
   if (!CARD_NUMBER_FORMAT.test(cardNumber)) {
@@ -26,6 +27,14 @@ const checkValidYearMonth = (yearMonth: string): ValidatorResponseType => {
   return { result: true, errorMessage: '' };
 };
 
+const checkOwnerNameLength = (cardOwnerName: string): ValidatorResponseType => {
+  if (cardOwnerName.length > LIMIT_LENGTH) {
+    return { result: false, errorMessage: '이름의 글자 수는 30글자 이하로 입력해 주세요!' };
+  }
+
+  return { result: true, errorMessage: '' };
+};
+
 const checkValidCVC = (cardNumber: string): ValidatorResponseType => {
   if (!CVC_FORMAT.test(cardNumber)) {
     return { result: false, errorMessage: '3자리의 숫자로만 입력해 주세요!' };
@@ -42,4 +51,4 @@ const checkValidPassword = (password: string): ValidatorResponseType => {
   return { result: true, errorMessage: '' };
 };
 
-export { checkValidCardNumber, checkValidYearMonth, checkValidCVC, checkValidPassword };
+export { checkValidCardNumber, checkValidYearMonth, checkOwnerNameLength, checkValidCVC, checkValidPassword };
