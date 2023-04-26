5840ed7a66b87e59be9057f5380d5eaf99ee6ca3
2yunseong <dbsdltjd3701@naver.com>
2023-04-26 18:28:24 +0900

    refactor: 범용적으로 쓰이는 변수 상수 파일로 분리

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,4 +1,3 @@
-export const MONTH_DATA = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
 const NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const ALPHABET = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
