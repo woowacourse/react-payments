428543258e8f7c3de33e32a3ac9334e241b184f3
turtle601 <turtle601@naver.com>
2023-04-26 10:54:51 +0900

    feat(CardCVC): CardCVC 에러 처리 및 기능 구현

--- src/components/molecules/CardCVC/index.tsx
+++ src/components/molecules/CardCVC/index.tsx
@@ -1,23 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import CardInput from '../../atomics/CardInput';
-import CardMessage from '../../atomics/Message';
-import ErrorMessage from '../../atomics/ErrorMessage';
+import Input from '../../atomics/Input';
+import Message from '../../atomics/Message';
 
+import { useInputError } from '../../../hooks/useInputError';
 import { VStack } from '../../layout/flexbox';
+
+import { changeCardCVCAction } from '../../../store/action';
+import { useCardFocusRefs, useCardPaymentDispatch } from '../../../hooks/useContextHooks';
 
 /* component */
 const CardCVC: React.FC = () => {
+  const inputRefs = useCardFocusRefs();
+  const [isError, handleError] = useInputError();
+  const dispatch = useCardPaymentDispatch();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    handleError(e);
+    dispatch(changeCardCVCAction(0, e.target.value));
+  };
+
   return (
     <StyledCardCVCWrapper>
       <StyledCardLabel>
-        <CardMessage fontWeight={500} fontSize="12px" color="#525252" lineHeight="14px">
+        <Message fontWeight={500} fontSize="12px" color="#525252" lineHeight="14px">
           보안 코드(CVC/CVV)
-        </CardMessage>
-        <CardInput type="password" width="75px" minLength={3} maxLength={3} center={true} />
+        </Message>
+        <Input
+          id={6}
+          variant="underline"
+          type="password"
+          pattern="[0-9]{3}"
+          isValid={!isError}
+          width="75px"
+          minLength={3}
+          maxLength={3}
+          center={true}
+          required={true}
+          ref={(el: HTMLInputElement) => (inputRefs.current[7] = el)}
+          onChange={handleChange}
+        />
       </StyledCardLabel>
-      <ErrorMessage isError={true}>3자리 숫자를 입력하세요!!</ErrorMessage>
+      {isError && (
+        <Message fontWeight={500} fontSize="12px" color="red" lineHeight="14px">
+          3자리 숫자를 입력하세요!!
+        </Message>
+      )}
     </StyledCardCVCWrapper>
   );
 };
