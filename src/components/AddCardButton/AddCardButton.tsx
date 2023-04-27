1d4d34709fc4845ac6ddbb17e29cb5e36efefba3
wzrabbit <curious.wzrabbit@gmail.com>
2023-04-27 22:08:21 +0900

    ✨ feat(addcardbutton): 클릭 시 카드 Context 정보 초기화 진행 기능 구현

--- src/components/AddCardButton/AddCardButton.tsx
+++ src/components/AddCardButton/AddCardButton.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import styles from './AddCardButton.module.css';
 import { GrAdd } from 'react-icons/gr';
-import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { CardInfoContext } from '../../CardInfoProvider';
 
 type AddCardButtonProps = {
   showMessage: boolean;
 };
 
 const AddCardButton = ({ showMessage }: AddCardButtonProps) => {
+  const navigate = useNavigate();
+  const { reset } = useContext(CardInfoContext);
+
+  const moveToRegistrationPage = () => {
+    reset();
+    navigate('/card-registration');
+  };
+
   return (
     <div className={styles.container}>
       {showMessage && <p className={styles.title}>새로운 카드를 등록해주세요.</p>}
-      <Link to="/card-registration">
-        <button className={styles.card}>
-          <GrAdd />
-        </button>
-      </Link>
+      <button className={styles.card} onClick={moveToRegistrationPage}>
+        <GrAdd />
+      </button>
     </div>
   );
 };
