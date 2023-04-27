f7de67a30c6252e2d0b171d18bc758d9d7fea56e
dladncks1217 <dlaxodud1217@gmail.com>
2023-04-27 11:33:19 +0900

    feat: 카드 타입 선택 모달 기능 추가

--- src/components/CardSelectModal.tsx
+++ src/components/CardSelectModal.tsx
@@ -1,48 +1,29 @@
 import React from 'react';
 
-import { BANK_ICONS } from '../utils/constants';
+import { BANK_LIST } from '../utils/constants';
+import BankIcon from './BankIcon';
 import './CardSelectModal.css';
 
-const Modal = () => {
+type modalProps = {
+  determineCardType: React.Dispatch<React.SetStateAction<string>>;
+  closeModal: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const CardSelectModal = ({ determineCardType, closeModal }: modalProps) => {
   return (
     <>
       <div className="card-select-modal-backdrop"></div>
       <div className="card-select-modal">
-        <div className="card-icon">
-          <img src={BANK_ICONS['BC']} alt="BC" className="bank-icon" />
-          <div>BC카드</div>
-        </div>
-        <div className="card-icon">
-          <img src={BANK_ICONS['shinhan']} alt="shinhan" className="bank-icon" />
-          <div>신한카드</div>
-        </div>
-        <div className="card-icon">
-          <img src={BANK_ICONS['kakaobank']} alt="kakaobank" className="bank-icon" />
-          <div>카카오뱅크</div>
-        </div>
-        <div className="card-icon">
-          <img src={BANK_ICONS['hyundai']} alt="hyundai" className="bank-icon" />
-          <div>현대카드</div>
-        </div>
-        <div className="card-icon">
-          <img src={BANK_ICONS['woori']} alt="woori" className="bank-icon" />
-          <div>우리카드</div>
-        </div>
-        <div className="card-icon">
-          <img src={BANK_ICONS['lotte']} alt="lotte" className="bank-icon" />
-          <div>롯데카드</div>
-        </div>
-        <div className="card-icon">
-          <img src={BANK_ICONS['hana']} alt="hana" className="bank-icon" />
-          <div>하나카드</div>
-        </div>
-        <div className="card-icon">
-          <img src={BANK_ICONS['kookmin']} alt="kookmin" className="bank-icon" />
-          <div>국민카드</div>
-        </div>
+        {BANK_LIST.map((name) => (
+          <BankIcon
+            bankName={name}
+            determineCardType={determineCardType}
+            selectCardType={closeModal}
+          />
+        ))}
       </div>
     </>
   );
 };
 
-export default Modal;
+export default CardSelectModal;
