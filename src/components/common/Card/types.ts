f2b8089f586ac2cb138b4fdde06982df7e896929
yogjin <youngjin988@gmail.com>
2023-04-27 10:40:07 +0900

    feat: `BankCodeList`와 `CardName` 선언, Card 타입에 `bankCode` 추가

--- src/components/common/Card/types.ts
+++ src/components/common/Card/types.ts
@@ -1,3 +1,27 @@
+export const BankCodeList = {
+  BCCard: '361',
+  ShinHanCard: '366',
+  KakaoBank: '090',
+  HyunDaiCard: '367',
+  WooriCard: '041',
+  LotteCard: '368',
+  HanaCard: '374',
+  KookMinCard: '381',
+} as const;
+
+export const CardName = {
+  [BankCodeList.BCCard]: '비씨카드',
+  [BankCodeList.ShinHanCard]: '신한카드',
+  [BankCodeList.KakaoBank]: '카카오뱅크',
+  [BankCodeList.HyunDaiCard]: '현대카드',
+  [BankCodeList.WooriCard]: '우리카드',
+  [BankCodeList.LotteCard]: '롯데카드',
+  [BankCodeList.HanaCard]: '하나카드',
+  [BankCodeList.KookMinCard]: '국민카드',
+} as const;
+
+export type BankCode = (typeof BankCodeList)[keyof typeof BankCodeList];
+
 export type ExpirationDate = {
   year: string;
   month: string;
@@ -14,4 +38,5 @@ export type Card = {
   name: string;
   securityCode: string;
   password: Password;
+  bankCode: BankCode;
 };
