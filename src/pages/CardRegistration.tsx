aa862eb11df9cfab255d76efb90d77886735389f
wzrabbit <curious.wzrabbit@gmail.com>
2023-04-28 01:22:52 +0900

    ♻️ refactor: Context API 사용 이후 불필요한 drilling 구조 제거

--- src/pages/CardRegistration.tsx
+++ src/pages/CardRegistration.tsx
@@ -1,72 +1,22 @@
-import { useState } from 'react';
+import { useContext } from 'react';
 import AddCardForm from '../components/AddCardForm/AddCardForm';
 import CardPreview from '../components/common/CardPreview/CardPreview';
-import type { CardInfo } from '../types';
 import Header from '../components/common/Header/Header';
-import type { FormInputValueType } from '../types';
-import useBooleanLocker from '../hooks/useBooleanLocker';
+import { cardImages } from './cardImages';
+import { CardInfoContext } from '../CardInfoProvider';
 
-type CardRegistrationProps = {
-  registerNewCard: (cardInfo: CardInfo) => void;
-};
-
-const CardRegistration = ({ registerNewCard }: CardRegistrationProps) => {
-  const [cardNumber, setCardNumber] = useState<string>('');
-  const [cardOwnerName, setCardOwnerName] = useState<string>('');
-  const [cardExpirationDate, setCardExpirationDate] = useState<string>('');
-  const { isUnlocked, setBooleanValueByIndex } = useBooleanLocker(5);
-
-  const updateCardNumber = useCallback(
-    (newCardNumber: FormInputValueType) => {
-      setCardNumber(() => newCardNumber.value);
-      setBooleanValueByIndex(0, newCardNumber.isValid);
-    },
-    [setBooleanValueByIndex]
-  );
-
-  const updateCardOwnerName = useCallback(
-    (newCardOwnerName: FormInputValueType) => {
-      setCardOwnerName(() => newCardOwnerName.value);
-      setBooleanValueByIndex(1, newCardOwnerName.isValid);
-    },
-    [setBooleanValueByIndex]
-  );
-
-  const updateCardExpirationDate = useCallback(
-    (newCardExpirationDate: FormInputValueType) => {
-      setCardExpirationDate(() => newCardExpirationDate.value);
-      setBooleanValueByIndex(2, newCardExpirationDate.isValid);
-    },
-    [setBooleanValueByIndex]
-  );
-
-  const updateCardSecurityCode = useCallback(
-    (newCardSecurityCode: FormInputValueType) => {
-      setBooleanValueByIndex(3, newCardSecurityCode.isValid);
-    },
-    [setBooleanValueByIndex]
-  );
-
-  const updateCardPassword = useCallback(
-    (newCardPassword: FormInputValueType) => {
-      setBooleanValueByIndex(4, newCardPassword.isValid);
-    },
-    [setBooleanValueByIndex]
-  );
-
+const CardRegistration = () => {
+  const { cardIssuer, cardNumber, cardOwnerName, cardExpirationDate } = useContext(CardInfoContext);
   return (
     <>
       <Header title="카드 추가" hasBackButton={true} />
-      <CardPreview cardNumber={cardNumber} cardOwnerName={cardOwnerName} cardExpirationDate={cardExpirationDate} />
-      <AddCardForm
-        updateCardNumber={updateCardNumber}
-        updateExpirationDate={updateCardExpirationDate}
-        updateCardOwnerName={updateCardOwnerName}
-        updateCardSecurityCode={updateCardSecurityCode}
-        updateCardPassword={updateCardPassword}
-        registerNewCard={registerNewCard}
-        isUnlocked={isUnlocked}
+      <CardPreview
+        cardNumber={cardNumber.value}
+        cardOwnerName={cardOwnerName.value}
+        cardExpirationDate={cardExpirationDate.value}
+        image={cardImages[cardIssuer]}
       />
+      <AddCardForm />
     </>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import AddCardButton from '../components/AddCardButton/AddCardButton';
 import CardList from '../components/CardList/CardList';
 import Header from '../components/common/Header/Header';
 import { CardInfo } from '../types';
@@ -12,7 +11,6 @@ const Home = ({ cardInfo }: HomeProps) => {
     <>
       <Header title="보유카드" />
       <CardList cardInfo={cardInfo} />
-      <AddCardButton showMessage={cardInfo.length === 0} />
     </>
   );
 };
