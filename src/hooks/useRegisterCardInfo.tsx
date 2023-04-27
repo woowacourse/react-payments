e37307005c4a629ac844de57410fa22935ec6878
hozzijeong <ghwls0905@naver.com>
2023-04-27 15:51:18 +0900

    refactor: 카드정보 등록 관련 기능 hook 분리

---
+++ src/hooks/useRegisterCardInfo.tsx
@@ -0,0 +1,25 @@
+import { useContext, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+import { CardInfoContext } from "src/context/CardInfoContext";
+import { PATHS } from "src/utils/constant";
+import { isValidateFormValues } from "src/utils/validation";
+
+function useRegisterCardInfo() {
+  const navigation = useNavigate();
+
+  const [cardInfo] = useContext(CardInfoContext);
+
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    navigation(PATHS.cardNickName, { state: cardInfo });
+  };
+
+  const isValidateInfo = useMemo(
+    () => isValidateFormValues(cardInfo),
+    [cardInfo],
+  );
+
+  return { cardInfo, onSubmit, isValidateInfo };
+}
+
+export default useRegisterCardInfo;
