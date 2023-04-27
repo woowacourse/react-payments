6c6eebe5080f6e345395293a4a754ff795c1c1b3
woo-jk <201702042@o.cnu.ac.kr>
2023-04-27 16:54:25 +0900

    refactor: Card 타입에 카드사, 별칭 추가

--- src/types/Card.ts
+++ src/types/Card.ts
@@ -1,8 +1,21 @@
+export type Company =
+  | "BC카드"
+  | "신한카드"
+  | "카카오뱅크"
+  | "현대카드"
+  | "우리카드"
+  | "롯데카드"
+  | "하나카드"
+  | "국민카드"
+  | "";
+
 export interface CardPublicInfo {
   id: number;
   cardNumber: string[];
   expirationDate: string[];
   name: string;
+  company: Company;
+  nickName?: string;
 }
 
 export interface CardPrivateInfo extends CardPublicInfo {
