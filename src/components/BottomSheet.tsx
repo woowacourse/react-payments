ff3cf5766b3b7db1e3e0b2a193b4f24095df11c9
2yunseong <dbsdltjd3701@naver.com>
2023-04-27 13:22:04 +0900

    feat: BottomSheet 열기/닫기 기능 전달

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -1,11 +1,22 @@
 import { PropsWithChildren } from 'react';
+import './BottomSheet.css';
 
 type BottomSheetProps = {
   isOpen: boolean;
-  onChangeOpen: () => void;
+  onToggleOpen: () => void;
 };
-const BottomSheet = ({ children }: PropsWithChildren<BottomSheetProps>) => {
-  return <div className="bottom-sheet">{children}</div>;
+
+const BottomSheet = ({ children, isOpen, onToggleOpen }: PropsWithChildren<BottomSheetProps>) => {
+  return (
+    <>
+      {isOpen && (
+        <div className="bottom-sheet">
+          <div className="back-drop" onClick={onToggleOpen}></div>
+          <div className="bottom-sheet-body">{children}</div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default BottomSheet;
