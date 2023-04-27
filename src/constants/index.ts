c5385500c3409f884643784ae9fb0f6ca9a7b853
Jungwoo <evencoding@gmail.com>
2023-04-27 17:07:54 +0900

    feat: 카드 회사 정보를 담은 constants 생성

---
+++ src/constants/index.ts
@@ -0,0 +1,54 @@
+import { bc, hana, hyundai, kakao, kookmin, lotte, sinhan, woori } from '../images';
+
+const CARD_COMPANIES = {
+  bc: {
+    name: 'BC카드',
+    logo: bc,
+    bgColor: '#DE5356',
+    color: '#FFFFFF',
+  },
+  sinhan: {
+    name: '신한카드',
+    logo: sinhan,
+    bgColor: '#1B45F5',
+    color: '#FFFFFF',
+  },
+  kakao: {
+    name: '카카오뱅크',
+    logo: kakao,
+    bgColor: '#FBE74D',
+    color: '#333333',
+  },
+  hyundai: {
+    name: '현대카드',
+    logo: hyundai,
+    bgColor: '#000000',
+    color: '#FFFFFF',
+  },
+  woori: {
+    name: '우리카드',
+    logo: woori,
+    bgColor: '#3579C2',
+    color: '#FFFFFF',
+  },
+  lotte: {
+    name: '롯데카드',
+    logo: lotte,
+    bgColor: '#DA3832',
+    color: '#FFFFFF',
+  },
+  hana: {
+    name: '하나카드',
+    logo: hana,
+    bgColor: '#41928F',
+    color: '#FFFFFF',
+  },
+  kookmin: {
+    name: '국민카드',
+    logo: kookmin,
+    bgColor: '#6D655C',
+    color: '#F7CF47',
+  },
+};
+
+export { CARD_COMPANIES };
