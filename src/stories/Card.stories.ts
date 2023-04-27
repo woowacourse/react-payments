aa10f784dcd8cdac3d59c0e526874e6d5876d01d
dladncks1217 <dlaxodud1217@gmail.com>
2023-04-27 15:06:03 +0900

    refactor: 모달 css 스타일 수정

--- src/stories/Card.stories.ts
+++ src/stories/Card.stories.ts
@@ -12,6 +12,34 @@ type Story = StoryObj<typeof Card>;
 
 export const Primary: Story = {
   args: {
+    cardType: '현대카드',
+    cardNumber: {
+      first: '1234',
+      second: '1234',
+      third: '1234',
+      fourth: '1234',
+    },
+    cardOwner: 'LEE',
+    expired: '01/01',
+  },
+};
+
+export const Primary2: Story = {
+  args: {
+    cardType: 'BC카드',
+    cardNumber: {
+      first: '1234',
+      second: '1234',
+      third: '1234',
+      fourth: '1234',
+    },
+    cardOwner: 'LEE',
+    expired: '01/01',
+  },
+};
+export const Primary3: Story = {
+  args: {
+    cardType: '신한카드',
     cardNumber: {
       first: '1234',
       second: '1234',
