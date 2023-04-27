f901071cccf87b9c5844e48fbd66309058a72877
wzrabbit <curious.wzrabbit@gmail.com>
2023-04-27 16:03:33 +0900

    ✨ feat: 카드사 타입 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,4 +8,14 @@ type ValidatorResponseType = { result: boolean; errorMessage: string };
 
 type FormInputValueType = { isValid: boolean; value: string };
 
-export type { CardInfo, ValidatorResponseType, FormInputValueType };
+type CardIssuerType =
+  | 'BC카드'
+  | '신한카드'
+  | '카카오뱅크'
+  | '현대카드'
+  | '우리카드'
+  | '롯데카드'
+  | '하나카드'
+  | '국민카드';
+
+export type { CardInfo, ValidatorResponseType, FormInputValueType, CardIssuerType };
