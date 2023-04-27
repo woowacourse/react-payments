980e284d70e0afb2020cef87b3d993a8ae84c353
dladncks1217 <dlaxodud1217@gmail.com>
2023-04-27 16:16:02 +0900

    refactor: 배포 시 404페이지 이미지 수정

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import notFoundImage from '../asset/404.png';
 import { LOCATION } from '../utils/constants';
+import './NotFoundPage.css';
 
 const NotFoundPage = () => {
   const navigate = useNavigate();
@@ -10,14 +12,15 @@ const NotFoundPage = () => {
     navigate(LOCATION.CARD_LIST_PAGE);
   };
   return (
-    <>
+    <div className="not-found-main">
+      <img className="not-found-image" alt="404" src={notFoundImage} />
+      <br />
       <div>찾을 수 없는 페이지입니다.</div>
       <div>잘못된 경로를 이용하셨어요 :{')'}</div>
-
-      <button type="button" onClick={backToHome}>
+      <button className="home-button" type="button" onClick={backToHome}>
         홈으로 이동
       </button>
-    </>
+    </div>
   );
 };
 
