293b92e25afb0f3268afaf8592a1ba34e10836b7
yogjin <youngjin988@gmail.com>
2023-04-27 12:09:25 +0900

    style: 전체 스타일 font-family 수정, box-sizing 적용, reset 적용

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -1,10 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+  ${reset}
+
+  * {
+    font-family: 'Roboto' !important;
+    box-sizing: border-box;
+  }
+
   body {
   margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
-    'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
