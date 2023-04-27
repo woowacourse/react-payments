76f3286245b4a9c565bb00e54050a0e2665dbab9
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-04-27 20:04:49 +0900

    refactor: 카드 등록 헬퍼 텍스트 분리

--- src/pages/CardListPage/CardListPage.tsx
+++ src/pages/CardListPage/CardListPage.tsx
@@ -2,6 +2,7 @@ import AppBar from "../../components/AppBar/AppBar";
 import CardList from "../../components/CardList/CardList";
 import { Container } from "../../components/common";
 import { Card } from "../../types";
+import styled from "styled-components";
 
 type CardListPageProps = {
   cards: Card[];
@@ -11,10 +12,15 @@ const CardListPage = ({ cards }: CardListPageProps) => {
   return (
     <Container>
       <AppBar title="보유카드" />
-      {!cards.length && <span>새로운 카드를 등록해주세요.</span>}
+
+      {!cards.length && <HelperText>새로운 카드 등록하기👆</HelperText>}
       <CardList cards={cards} />
     </Container>
   );
 };
+
+const HelperText = styled.span`
+  font-size: 12px;
+`;
 
 export default CardListPage;
