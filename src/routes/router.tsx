b60848cadb6e3779f5184315e3470ddacc422430
ukkodeveloper <ukkodeveloper@gmail.com>
2023-04-27 13:55:33 +0900

    feat: router 페이지 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
-import CardList from '../components/pages/CardList';
+import CardListPage from '../components/pages/CardListPage';
 import RegisterPage from '../components/pages/RegisterPage';
 import App from '../App';
+import SuccessPage from '../components/pages/SuccessPage';
 
 const router = createBrowserRouter(
   [
@@ -11,11 +12,15 @@ const router = createBrowserRouter(
       children: [
         {
           path: '',
-          element: <CardList />,
+          element: <CardListPage />,
         },
         {
           path: 'card-register',
           element: <RegisterPage />,
+        },
+        {
+          path: 'register-success',
+          element: <SuccessPage />,
         },
       ],
     },
