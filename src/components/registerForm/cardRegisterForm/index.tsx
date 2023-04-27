004037e1bb70013b960893725104597bf001ec4f
hozzijeong <ghwls0905@naver.com>
2023-04-27 16:00:04 +0900

    refactor: 카드 폼 제출 커스텀 훅 적용

--- src/components/registerForm/cardRegisterForm/index.tsx
+++ src/components/registerForm/cardRegisterForm/index.tsx
@@ -1,33 +1,22 @@
-import React, { useContext, useMemo } from "react";
+import React, { useMemo } from "react";
 import CardNumber from "src/components/registerForm/CardNumber";
 import ExpireDate from "src/components/registerForm/ExpireDate";
 import OwnerNameInput from "src/components/registerForm/OwnerNameInput";
 import SecurityCode from "src/components/registerForm/SecurityCode";
 import CardPassword from "src/components/registerForm/CardPassword";
-import { CardInfoContext } from "src/context/CardInfoContext";
-import { useNavigate } from "react-router-dom";
 import { Styled as S } from "./CardRegisterForm.styles";
 import Card from "src/components/@common/Card";
 import Modal from "src/components/@common/Modal";
 import CardCompany from "../CardCompany";
 import useModal from "src/hooks/useModal";
-import { isValidateFormValues } from "src/utils/validation";
-import { PATHS } from "src/utils/constant";
+import useRegisterCardInfo from "src/hooks/useRegisterCardInfo";
 
 function CardRegisterForm() {
-  const navigation = useNavigate();
-  const [cardInfo] = useContext(CardInfoContext);
+  const { cardInfo, onSubmit, isValidateInfo } = useRegisterCardInfo();
 
   const { cardNumbers, expireDate, cardName, ownerName } = cardInfo;
 
   const { openModal, closeModal, modalRef, isModalOpen } = useModal();
-
-  const cardInputSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
-    event.preventDefault();
-    navigation(PATHS.cardNickName, { state: cardInfo });
-  };
-
-  const isValidateValues = isValidateFormValues(cardInfo);
 
   const cardPreview = useMemo(
     () => (
@@ -45,13 +34,13 @@ function CardRegisterForm() {
   return (
     <>
       {cardPreview}
-      <S.Form onSubmit={cardInputSubmit}>
+      <S.Form onSubmit={onSubmit}>
         <CardNumber />
         <ExpireDate />
         <OwnerNameInput />
         <SecurityCode />
         <CardPassword />
-        {isValidateValues && (
+        {isValidateInfo && (
           <S.ButtonContainer>
             <S.NextButton>다음</S.NextButton>
           </S.ButtonContainer>
