3851945b153110f5a1a9d911ea2164ae98993f67
yogjin <youngjin988@gmail.com>
2023-04-27 10:35:37 +0900

    test: `CreditCard` 스토리 추가
    카드의 은행을 식별할 수 있는 `bankCode`와 카드 색깔을 정할 ThemeProvider 이용

---
+++ src/components/common/Card/CreditCard.stories.tsx
@@ -0,0 +1,84 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { CreditCard } from './CreditCard';
+import { BankCodeList, Card } from './types';
+import { theme } from 'components/style/theme';
+import { ThemeProvider } from 'styled-components';
+
+const meta = {
+  title: 'CreditCard',
+  component: CreditCard,
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+} satisfies Meta<typeof CreditCard>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const defaultCard: Card = {
+  numbers: ['8721', '3469', '2837', '4612'],
+  expirationDate: {
+    month: '23',
+    year: '48',
+  },
+  name: 'PARK',
+  securityCode: '123',
+  password: {
+    first: '1',
+    second: '2',
+  },
+  bankCode: BankCodeList.BCCard,
+};
+
+export const BCCard: Story = {
+  args: {
+    card: { ...defaultCard, bankCode: BankCodeList.BCCard },
+  },
+};
+
+export const ShinHanCard: Story = {
+  args: {
+    card: { ...defaultCard, bankCode: BankCodeList.ShinHanCard },
+  },
+};
+
+export const KakaoBank: Story = {
+  args: {
+    card: { ...defaultCard, bankCode: BankCodeList.KakaoBank },
+  },
+};
+
+export const HyunDaiCard: Story = {
+  args: {
+    card: { ...defaultCard, bankCode: BankCodeList.HyunDaiCard },
+  },
+};
+
+export const WooriCard: Story = {
+  args: {
+    card: { ...defaultCard, bankCode: BankCodeList.WooriCard },
+  },
+};
+
+export const LotteCard: Story = {
+  args: {
+    card: { ...defaultCard, bankCode: BankCodeList.LotteCard },
+  },
+};
+
+export const HanaCard: Story = {
+  args: {
+    card: { ...defaultCard, bankCode: BankCodeList.HanaCard },
+  },
+};
+
+export const KookMinCard: Story = {
+  args: {
+    card: { ...defaultCard, bankCode: BankCodeList.KookMinCard },
+  },
+};
