d9f22cb257ba018cd6c417651825a4c403ff7c3d
hozzijeong <ghwls0905@naver.com>
2023-04-27 14:32:33 +0900

    test: 카드사 modal 스토리북 구현

--- src/components/registerForm/cardCompany/CardCompany.stories.tsx
+++ src/components/registerForm/cardCompany/CardCompany.stories.tsx
@@ -1,14 +1,40 @@
 import { Meta } from "@storybook/react";
 import React from "react";
 import CardCompany from ".";
+import { ModalProvider } from "src/context/ModalContext";
+import useModal from "src/hooks/useModal";
+import Modal from "src/components/@common/Modal";
 
 const cardNumber = {
   component: CardCompany,
   title: "Card Company Input",
+  decorators: [
+    (Story) => {
+      return (
+        <ModalProvider>
+          <Story />
+        </ModalProvider>
+      );
+    },
+  ],
 } satisfies Meta<typeof CardCompany>;
 
 export default cardNumber;
 
 export const Example = () => {
-  return <CardCompany closeEvent={() => {}} />;
+  const { isModalOpen, openModal, closeModal, modalRef } = useModal();
+
+  return (
+    <div>
+      <div>
+        <button onClick={openModal}>모달 열기</button>
+        {isModalOpen && (
+          <Modal dialogRef={modalRef} closeEvent={closeModal}>
+            <CardCompany closeEvent={closeModal} />
+          </Modal>
+        )}
+      </div>
+      <div id="modal-root"></div>
+    </div>
+  );
 };
