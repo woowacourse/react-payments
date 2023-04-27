34c2e2c848880d68dc82a6b4c42e3328ff48001d
inyeong-kang <tomatorkd@naver.com>
2023-04-27 12:07:03 +0900

    chore: 패키지 설치

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  settings: {
+    'import/parsers': { '@typescript-eslint/parser': ['.ts'] },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:import/recommended',
+  ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { GlobalStyle } from './styles/global-style';
