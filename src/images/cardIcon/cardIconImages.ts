9406d6646fe9efd2d6515b57ec854a1ebc57e9fd
jeongwusi <jeongwusi@gmail.com>
2023-04-27 16:57:26 +0900

    ✨ feat: 카드회사 아이콘 추가

---
+++ src/images/cardIcon/cardIconImages.ts
@@ -0,0 +1,21 @@
+import bcCardIcon from './icon_bc_card.png';
+import hanaCardIcon from './icon_hana_card.png';
+import hyundaiCardIcon from './icon_hyundai_card.png';
+import lotteCardIcon from './icon_lotte_card.png';
+import wooriCardIcon from './icon_woori_card.png';
+import shinhanCardIcon from './icon_shinhan_card.png';
+import kakaoCardIcon from './icon_kakao_bank_card.png';
+import kbCardIcon from './icon_kb_card.png';
+
+const cardIcons = {
+  bcCardIcon: bcCardIcon,
+  hanaCardIcon: hanaCardIcon,
+  hyundaiCardIcon: hyundaiCardIcon,
+  lotteCardIcon: lotteCardIcon,
+  wooriCardIcon: wooriCardIcon,
+  shinhanCardIcon: shinhanCardIcon,
+  kakaoCardIcon: kakaoCardIcon,
+  kbCardIcon: kbCardIcon,
+};
+
+export default cardIcons;
