be1df13b1e8ca849896420825f9ce2010e86ca5c
wzrabbit <curious.wzrabbit@gmail.com>
2023-04-27 15:56:16 +0900

    ✨ feat: CardSelectButton 컴포넌트 구현
    - 카드사 변경 버튼

--- src/components/CardSelectButtonPack/CardSelectButton/CardSelectButton.tsx
+++ src/components/CardSelectButtonPack/CardSelectButton/CardSelectButton.tsx
@@ -1,17 +1,30 @@
+import React from 'react';
+import { useContext, useCallback } from 'react';
+import { ModalContext } from '../../../ModalProvider';
+import { CardInfoContext } from '../../../CardInfoProvider';
 import styles from './CardSelectButton.module.css';
+import { CardIssuerType } from '../../../types';
 
 type CardSelectButtonProps = {
-  title: string;
+  title: CardIssuerType;
   src: string;
 };
 
 const CardSelectButton = ({ title, src }: CardSelectButtonProps) => {
+  const { closeModal } = useContext(ModalContext);
+  const { setCardIssuer } = useContext(CardInfoContext);
+
+  const onClick = useCallback(() => {
+    setCardIssuer(title);
+    closeModal();
+  }, [title, closeModal, setCardIssuer]);
+
   return (
-    <button className={styles.button}>
+    <button className={styles.button} onClick={onClick}>
       <img className={styles.logo} src={src} alt={title} />
       <span className={styles.title}>{title}</span>
     </button>
   );
 };
 
-export default CardSelectButton;
+export default React.memo(CardSelectButton);
