b37e2528639e27efcad31c77474b3ff4c4752ab7
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-04-28 02:07:12 +0900

    refactor: CardAliasPage 스타일 수정

--- src/components/Pages/CardAliasPage/CardAliasPage.styled.ts
+++ src/components/Pages/CardAliasPage/CardAliasPage.styled.ts
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 import { CardInfoSubmitButton } from '../../CardInfoSubmitButtonContainer/CardInfoSubmitButtonContainer.styled';
+import { COLOR } from '../../../constants/cardInfo';
 
 export const CardAliasPage = styled.section`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
 
   width: 100%;
@@ -13,9 +13,31 @@ export const CardAliasPage = styled.section`
 `;
 
 export const CardAliasMessage = styled.h2`
-  font-size: xx-large;
+  margin: 40% 0 8% 0;
+
+  font-size: x-large;
+  font-weight: 400;
 `;
 
-export const CardAliasInputBox = styled.div``;
+export const CardAliasInputBox = styled.div`
+  margin: 24% 0 0 0;
+
+  input {
+    width: 240px;
+    height: 40px;
+
+    font-size: large;
+    font-weight: 400;
+    color: ${COLOR.BLACK};
+    opacity: 1;
+    border-bottom: solid 2px;
+    border-radius: 0;
+    background-color: transparent;
+
+    &:focus {
+      outline: none;
+    }
+  }
+`;
 
 export const CardAliasSubmitButton = styled(CardInfoSubmitButton)``;
