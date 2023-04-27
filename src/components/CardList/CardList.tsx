b411fd4d536e2f289801603291d127847ef5ec8f
wzrabbit <curious.wzrabbit@gmail.com>
2023-04-28 01:23:23 +0900

    ✨ feat: CardName 프로퍼티 추가

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -15,6 +15,7 @@ const CardList = ({ cardInfo }: CardListProps) => {
         {cardInfo.map(card => (
           <li className={styles.card} key={crypto.randomUUID()}>
             <CardPreview
+              cardName={card.cardName}
               cardNumber={card.cardNumber}
               cardOwnerName={card.cardOwnerName}
               cardExpirationDate={card.cardExpirationDate}
