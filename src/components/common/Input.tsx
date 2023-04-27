0908d4ca14fb0b0830f52ec6b49771057ac562e1
hae-on <solbi2004@naver.com>
2023-04-27 11:00:47 +0900

    style: completion page 스타일링

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
-const Input = styled.input<{ textAlign?: string }>`
-  width: 100%;
+const Input = styled.input<{
+  textAlign?: string;
+  borderBottom?: string;
+  width?: string;
+}>`
+  width: ${({ width }) => width || '100%'};
   height: 100%;
 
   border: none;
@@ -11,6 +15,7 @@ const Input = styled.input<{ textAlign?: string }>`
   font-size: 18px;
 
   outline: none;
+  border-bottom: ${({ borderBottom }) => borderBottom || 'none'};
   text-align: ${({ textAlign }) => textAlign || 'center'};
 `;
 
--- src/components/pages/CardAdditionCompletionPage.tsx
+++ src/components/pages/CardAdditionCompletionPage.tsx
@@ -1,7 +1,9 @@
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 import CardItem from '../common/CardItem';
 import Title from '../common/Title';
 import { cardLocalStorage } from '../domain/CardLocalStorage';
+import Input from '../common/Input';
 
 interface test {
   newCardId: number;
@@ -20,12 +22,44 @@ const CardAdditionCompletionPage = ({ newCardId }: test) => {
   };
 
   return (
-    <>
+    <CompletionContainer>
       <Title title='카드등록이 완료되었습니다.' fontSize={24} />
-      <CardItem card={card} />
-      <button onClick={handleComplete}>확인</button>
-    </>
+      <CardItemContainer>
+        <CardItem card={card} />
+      </CardItemContainer>
+      <Input borderBottom='1px solid var(--black-color)' width={'60%'} />
+      <CheckBtn onClick={handleComplete}>확인</CheckBtn>
+    </CompletionContainer>
   );
 };
+
+const CompletionContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 130px;
+`;
+
+const CardItemContainer = styled.div`
+  margin: 36px 0 124px 0;
+`;
+
+const CheckBtn = styled.button`
+  position: absolute;
+  bottom: 0;
+  margin-bottom: 1rem;
+
+  padding: 10px 20px;
+
+  background-color: transparent;
+  border: none;
+
+  font-weight: 700;
+  font-size: 14px;
+
+  align-self: flex-end;
+  cursor: pointer;
+`;
 
 export default CardAdditionCompletionPage;
