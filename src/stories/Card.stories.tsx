24180190c84aef018a815d32a49fce8241625654
Leejin-Yang <ejin1996@gmail.com>
2023-04-27 21:07:18 +0900

    test: 카드 컴포넌트 스토리 작성

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -11,21 +11,84 @@ const meta: Meta<typeof Card> = {
 export default meta;
 type Story = StoryObj<typeof Card>;
 
+const DEFAULT_ARGS = {
+  cardNumber1: '0000',
+  cardNumber2: '0000',
+  expiredMonth: '12',
+  expiredYear: '24',
+};
+
 export const Default: Story = {
-  args: {
-    cardNumber1: '0000',
-    cardNumber2: '0000',
-    expiredMonth: '12',
-    expiredYear: '24',
-    owner: 'WOOWA',
-  },
+  args: DEFAULT_ARGS,
 };
 
 export const NoOwnerName: Story = {
   args: {
-    cardNumber1: '0000',
-    cardNumber2: '0000',
-    expiredMonth: '12',
-    expiredYear: '24',
+    ...DEFAULT_ARGS,
+    owner: 'WOOWA',
+  },
+};
+
+export const BC: Story = {
+  args: {
+    ...DEFAULT_ARGS,
+    company: 'BC카드',
+    owner: 'WOOWA',
+  },
+};
+
+export const Shinhan: Story = {
+  args: {
+    ...DEFAULT_ARGS,
+    company: '신한카드',
+    owner: 'WOOWA',
+  },
+};
+
+export const Kakaobank: Story = {
+  args: {
+    ...DEFAULT_ARGS,
+    company: '카카오뱅크',
+    owner: 'WOOWA',
+  },
+};
+
+export const Hyundai: Story = {
+  args: {
+    ...DEFAULT_ARGS,
+    company: '현대카드',
+    owner: 'WOOWA',
+  },
+};
+
+export const Woori: Story = {
+  args: {
+    ...DEFAULT_ARGS,
+    company: '우리카드',
+    owner: 'WOOWA',
+  },
+};
+
+export const Lotte: Story = {
+  args: {
+    ...DEFAULT_ARGS,
+    company: '롯데카드',
+    owner: 'WOOWA',
+  },
+};
+
+export const Hana: Story = {
+  args: {
+    ...DEFAULT_ARGS,
+    company: '하나카드',
+    owner: 'WOOWA',
+  },
+};
+
+export const KB: Story = {
+  args: {
+    ...DEFAULT_ARGS,
+    company: '국민카드',
+    owner: 'WOOWA',
   },
 };
