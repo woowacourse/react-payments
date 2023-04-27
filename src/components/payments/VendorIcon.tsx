e5090b38421939ac545545ee79c426d0a746e9e7
solo5star <solo_5@naver.com>
2023-04-27 19:46:20 +0900

    feat: 카드사 아이콘 컴포넌트 VendorIcon 추가

---
+++ src/components/payments/VendorIcon.tsx
@@ -0,0 +1,35 @@
+import type { ComponentProps } from 'react';
+import BC카드 from '../../assets/payments/vendors/BC카드.png';
+import 국민카드 from '../../assets/payments/vendors/국민카드.png';
+import 롯데카드 from '../../assets/payments/vendors/롯데카드.png';
+import 신한카드 from '../../assets/payments/vendors/신한카드.png';
+import 우리카드 from '../../assets/payments/vendors/우리카드.png';
+import 카카오뱅크 from '../../assets/payments/vendors/카카오뱅크.png';
+import 하나카드 from '../../assets/payments/vendors/하나카드.png';
+import 현대카드 from '../../assets/payments/vendors/현대카드.png';
+import type { CreditCardVendorName } from '../../domain/CreditCardVendor';
+import { Icon } from '../common/Icon';
+
+const CreditCardVendorIcons: Record<CreditCardVendorName, string> = {
+  BC카드,
+  신한카드,
+  카카오뱅크,
+  현대카드,
+  우리카드,
+  롯데카드,
+  하나카드,
+  국민카드,
+};
+
+type VendorIconProps = {
+  vendor: CreditCardVendorName;
+} & Omit<ComponentProps<typeof Icon>, 'src' | 'alt' | 'size'> &
+  Partial<Pick<ComponentProps<typeof Icon>, 'size'>>;
+
+export const VendorIcon = (props: VendorIconProps) => {
+  const { vendor, size, ...iconProps } = props;
+
+  return (
+    <Icon src={CreditCardVendorIcons[vendor]} alt={vendor} size={size ?? 3.6} {...iconProps} />
+  );
+};
