c73af7d87a1b2ff7df0eb93cf05c4ec9774995b2
jeongwusi <jeongwusi@gmail.com>
2023-04-28 09:35:57 +0900

    ✨ feat: 카드 등록 확인과 카드 별명을 넣을 수 있는 페이지 생성

---
+++ src/pages/CardRegistrationConfirmation/CardRegistrationConfirmation.tsx
@@ -0,0 +1,59 @@
+import { useContext } from 'react';
+import CardPreview from '../../components/CardPreview/CardPreview';
+import { CardInfoContext } from '../../context/CardInfoContext';
+import styles from './CardRegistrationConfirmation.module.css';
+import CardNicknameInput from '../../components/CardNickname/CardNicknameInput';
+import Button from '../../components/Button/Button';
+import { useNavigate } from 'react-router-dom';
+import { CardInfo } from '../../types';
+
+type CardRegistrationConfirmationProps = {
+  registerNewCard: (cardInfo: CardInfo) => void;
+};
+
+const CardRegistrationConfirmation = ({ registerNewCard }: CardRegistrationConfirmationProps) => {
+  const navigate = useNavigate();
+  const { cardNumber, cardOwnerName, expirationDate, selectedCard, cardNickName } = useContext(CardInfoContext);
+
+  const handleCardInfo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const cardInfo: CardInfo = {
+      cardNumber: cardNumber,
+      expirationDate: expirationDate,
+      cardOwnerName: cardOwnerName,
+      selectedCard: selectedCard,
+      cardNickName: cardNickName,
+    };
+
+    registerNewCard(cardInfo);
+
+    navigate('/');
+  };
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.box}>
+        <h2 className={styles.registrationLetter}>카드등록이 완료되었습니다.</h2>
+        <CardPreview
+          cardNumber={cardNumber}
+          cardOwnerName={cardOwnerName}
+          expirationDate={expirationDate}
+          selectedCard={selectedCard}
+        />
+        <form onSubmit={handleCardInfo}>
+          <div>
+            <CardNicknameInput />
+          </div>
+          <div>
+            <Button type="submit" className={styles.confirmButton}>
+              확인
+            </Button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default CardRegistrationConfirmation;
