62279ea1a22a30cd76fecca392c6fa6d70512a72
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-04-28 12:31:50 +0900

    fix: 잘못된 파일 이름 수정

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from 'router';
-import CardFormProvider from 'CardProvider';
+import CardFormProvider from 'CardFormProvider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
