b16dccac7e108edea1ca10dd48912b694e1780f1
ashleysyheo <ashleysyheo@gmail.com>
2023-04-29 00:56:47 +0900

    refactor: useModal 수정

--- src/hooks/common/useModal.ts
+++ src/hooks/common/useModal.ts
@@ -2,17 +2,24 @@ import { useCallback, useState } from 'react';
 
 const useModal = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   const open = useCallback(() => {
     setIsOpen(true);
+    setIsVisible(true);
   }, []);
 
   const close = useCallback(() => {
-    setIsOpen(false);
+    setIsVisible(false);
+
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 600);
   }, []);
 
   return {
     isModalOpen: isOpen,
+    isVisible,
     openModal: open,
     closeModal: close,
   };
