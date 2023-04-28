3842b4b5945f6a8027b2a168e7e36023445909b4
woo-jk <201702042@o.cnu.ac.kr>
2023-04-28 17:43:46 +0900

    feat: Title 컴포넌트 size 옵션 추가

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+
+type TitleSize = "large" | "medium";
 
 interface TitleProps {
   title: string;
+  size?: TitleSize;
 }
 
-const Title = ({ title }: TitleProps) => {
-  return <TitleContainer>{title}</TitleContainer>;
+const Title = ({ title, size = "medium" }: TitleProps) => {
+  return <TitleContainer size={size}>{title}</TitleContainer>;
 };
 
-const TitleContainer = styled.h3`
+const TitleContainer = styled.h3<{ size: TitleSize }>`
   font-weight: 700;
-  font-size: 18px;
+  font-size: ${({ size }) => (size === "large" ? "26px" : "18px")};
 
   color: #575757;
 
