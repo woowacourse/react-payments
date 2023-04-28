0b0f1ca1a9dc35d8bfefde6d8a271e222d60f4a9
solo5star <solo_5@naver.com>
2023-04-28 20:25:48 +0900

    feat: Button 컴포넌트에 disabled 속성 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,17 +1,30 @@
 import type { ComponentPropsWithoutRef } from 'react';
 import styled from 'styled-components';
 
-const StyledButton = styled.button`
+type StyledButtonProps = {
+  disabled?: boolean;
+};
+
+const StyledButton = styled.button<StyledButtonProps>`
   padding: 10px;
 
   font-size: ${(props) => props.theme.fontSize.medium};
   font-weight: ${(props) => props.theme.fontWeight.bold};
+
+  opacity: ${(props) => (props.disabled ? 0.5 : 1)};
+  cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
 `;
 
-type ButtonProps = ComponentPropsWithoutRef<'button'>;
+type ButtonProps = ComponentPropsWithoutRef<'button'> & {
+  disabled?: boolean;
+};
 
 export const Button = (props: ButtonProps) => {
-  const { children, ...buttonProps } = props;
+  const { disabled, children, ...buttonProps } = props;
 
-  return <StyledButton {...buttonProps}>{children}</StyledButton>;
+  return (
+    <StyledButton {...buttonProps} disabled={disabled}>
+      {children}
+    </StyledButton>
+  );
 };
