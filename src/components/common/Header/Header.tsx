bbbe7a4979e3da6e257a72c971c18083f2ceaa00
ashleysyheo <ashleysyheo@gmail.com>
2023-04-28 13:50:33 +0900

    refactor: Header 컴포넌트에서 path props를 받는 대신 navigate로 뒤로가기 구현

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,21 +1,22 @@
 import styles from './style.module.css';
 import { ComponentPropsWithoutRef } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Button from '../Button/Button';
 import leftIcon from '../../../assets/left-icon.svg';
 
 interface HeaderProps extends ComponentPropsWithoutRef<'header'> {
   content: string;
-  path?: string;
   isOverlayPage?: boolean;
 }
 
-function Header({ content, path = '/', isOverlayPage = false, ...attributes }: HeaderProps) {
+function Header({ content, isOverlayPage = false, ...attributes }: HeaderProps) {
+  const navigate = useNavigate();
+
   return (
     <header className={styles.header} {...attributes}>
-      <Link to={path}>
-        {isOverlayPage && <Button variant="textButton" icon={leftIcon}></Button>}
-      </Link>
+      {isOverlayPage && (
+        <Button variant="textButton" icon={leftIcon} onClick={() => navigate(-1)}></Button>
+      )}
       <h3 className="center-hoz-item">{content}</h3>
     </header>
   );
