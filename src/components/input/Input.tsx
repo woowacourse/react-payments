fe484e3aa3d267d849b1cee1158f021a0d0e2853
chex <chex1004@gmail.com>
2023-04-28 16:46:24 +0900

    fix: Input 입력 완료 시 자동으로 다음 포커스로 이동하지 않는 문제 해결

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,13 +1,19 @@
+import { InputHTMLAttributes, forwardRef } from 'react';
 import styled from 'styled-components';
 
-interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   designType?: 'basic' | 'underline';
   backgroundColor?: string;
 }
 
-export function Input({ designType = 'basic', backgroundColor, ...props }: Props) {
-  return <Style.Input className={designType} backgroundColor={backgroundColor} {...props} />;
-}
+export const Input = forwardRef<HTMLInputElement, Props>(function (
+  { designType = 'basic', backgroundColor, ...props },
+  ref,
+) {
+  return (
+    <Style.Input ref={ref} className={designType} backgroundColor={backgroundColor} {...props} />
+  );
+});
 
 const Style = {
   Input: styled.input<Props>`
@@ -40,27 +46,3 @@ const Style = {
     }
   `,
 };
-
-// export const Input = styled.input`
-//   display: flex;
-//   align-items: center;
-
-//   width: ${(props) => `${props.width}px`};
-//   height: 45px;
-
-//   padding: 0;
-//   border: 0;
-//   border-radius: 7px;
-//   background-color: #ecebf1;
-
-//   font-size: 15px;
-//   text-align: center;
-
-//   :focus {
-//     outline: none;
-//   }
-
-//   ::placeholder {
-//     color: #c6c6c6;
-//   }
-// `;
