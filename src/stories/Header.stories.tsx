20ea4634fc4432a975f81e977d723909e96c863f
wzrabbit <curious.wzrabbit@gmail.com>
2023-04-28 22:06:29 +0900

    ✅ test : Header 스토리 타입 오류를 개선하기 위해, decorator를 사용

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -1,34 +1,33 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Header from '../components/common/Header/Header';
-import { MemoryRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
+
+type Story = StoryObj<typeof Header>;
 
 const meta = {
   title: 'Header',
   component: Header,
-  tags: ['autodocs'],
+  decorators: [
+    Story => (
+      <HashRouter>
+        <Story />
+      </HashRouter>
+    ),
+  ],
 } satisfies Meta<typeof Header>;
 
-type HeaderProps = {
-  title: string;
-  hasBackButton?: boolean;
+export const Standard: Story = {
+  args: {
+    title: '뒤로가기 버튼이 있는 헤더?',
+    hasBackButton: true,
+  },
 };
 
-const Template = ({ title, hasBackButton }: HeaderProps) => (
-  <MemoryRouter>
-    <Header title={title} hasBackButton={hasBackButton} />
-  </MemoryRouter>
-);
-
-export const Standard = Template.bind({});
-Standard.args = {
-  title: '뒤로가기 버튼이 있는 헤더',
-  hasBackButton: true,
-};
-
-export const NoBackButton = Template.bind({});
-NoBackButton.args = {
-  title: '뒤로가기 버튼이 없는 헤더',
-  hasBackButton: false,
+export const NoBackButton: Story = {
+  args: {
+    title: '뒤로가기 버튼이 없는 헤더',
+    hasBackButton: false,
+  },
 };
 
 export default meta;
