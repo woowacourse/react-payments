c89b16ab49c00fc389f4f16616a86d62282f3cd1
solo5star <solo_5@naver.com>
2023-04-28 21:37:09 +0900

    refactor: router 분리 및 index 코드 App 으로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import { Outlet } from 'react-router-dom';
-import styled from 'styled-components';
+import type { PropsWithChildren } from 'react';
+import { useMemo } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import styled, { ThemeProvider } from 'styled-components';
 import { PaymentsProvider } from './context/PaymentsContext';
+
+import { useMediaQuery } from './hooks/useMediaQuery';
+import { router } from './router';
+import { GlobalStyle } from './styles/GlobalStyle';
+import { ResetStyle } from './styles/ResetStyle';
+import { dark, light } from './styles/theme';
 
 const Container = styled.div`
   display: flex;
@@ -11,12 +19,29 @@ const Container = styled.div`
   height: 100vh;
 `;
 
+const PaymentsThemeProvider = (props: PropsWithChildren) => {
+  const { children } = props;
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = useMemo(() => (prefersDarkMode ? dark : light), [prefersDarkMode]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <ResetStyle />
+      <GlobalStyle />
+
+      {children}
+    </ThemeProvider>
+  );
+};
+
 export const App = () => {
   return (
     <PaymentsProvider>
-      <Container>
-        <Outlet />
-      </Container>
+      <PaymentsThemeProvider>
+        <Container>
+          <RouterProvider router={router} />
+        </Container>
+      </PaymentsThemeProvider>
     </PaymentsProvider>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,65 +1,10 @@
-import type { PropsWithChildren } from 'react';
-import React, { useMemo } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
 import { App } from './App';
-import { useMediaQuery } from './hooks/useMediaQuery';
-import { CreditCardListPage } from './pages/CreditCardListPage';
-import { ErrorPage } from './pages/ErrorPage';
-import { NewCreditCardDisplayNamePage } from './pages/NewCreditCardDisplayNamePage';
-import { NewCreditCardPage } from './pages/NewCreditCardPage';
-import { GlobalStyle } from './styles/GlobalStyle';
-import { ResetStyle } from './styles/ResetStyle';
-import { dark, light } from './styles/theme';
-
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <App />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: '',
-          element: <CreditCardListPage />,
-        },
-        {
-          path: 'register',
-          element: <NewCreditCardPage />,
-        },
-        {
-          path: 'complete',
-          element: <NewCreditCardDisplayNamePage />,
-        },
-      ],
-    },
-  ],
-  {
-    basename: process.env.PUBLIC_URL,
-  },
-);
-
-const PaymentsThemeProvider = (props: PropsWithChildren) => {
-  const { children } = props;
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const theme = useMemo(() => (prefersDarkMode ? dark : light), [prefersDarkMode]);
-
-  return (
-    <ThemeProvider theme={theme}>
-      <ResetStyle />
-      <GlobalStyle />
-
-      {children}
-    </ThemeProvider>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <PaymentsThemeProvider>
-      <RouterProvider router={router} />
-    </PaymentsThemeProvider>
+    <App />
   </React.StrictMode>,
 );
