841cea9c017d0ed3b22e7bb364b537646f73fe51
unknown <tkdrb15@gmail.com>
2023-04-29 02:45:27 +0900

    refactor: 라우터 컴포넌트를 파일로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import { Home } from "./page/Home";
-import { AddCard } from "./page/AddCard";
-import { SetAlias } from "./page/SetAlias";
-
 import { GlobalStyle } from "./style/resetStyle";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-
 import { useCards } from "./hook/useCards";
-import { CardsContext } from "./contexts/CardsContext";
+import { Routers } from "./page/Routers";
 
 const App = () => {
   const { cards, addNewCard, setAlias } = useCards();
@@ -14,28 +8,7 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <CardsContext.Provider value={{ cards, addNewCard }}>
-                <Home />
-              </CardsContext.Provider>
-            }
-          />
-          <Route
-            path="/addCard"
-            element={
-              <CardsContext.Provider value={{ cards, addNewCard }}>
-                {" "}
-                <AddCard />
-              </CardsContext.Provider>
-            }
-          />
-          <Route path="/setAlias" element={<SetAlias setAlias={setAlias} />} />
-        </Routes>
-      </BrowserRouter>
+      <Routers />
     </>
   );
 };
---
+++ src/page/Routers.tsx
@@ -0,0 +1,37 @@
+import { Home } from "./Home";
+import { AddCard } from "./AddCard";
+import { SetAlias } from "./SetAlias";
+
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import { CardsContext } from "../contexts/CardsContext";
+import { useCards } from "../hook/useCards";
+
+export const Routers = () => {
+  const { cards, addNewCard, setAlias } = useCards();
+
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <CardsContext.Provider value={{ cards, addNewCard }}>
+              <Home />
+            </CardsContext.Provider>
+          }
+        />
+        <Route
+          path="/addCard"
+          element={
+            <CardsContext.Provider value={{ cards, addNewCard }}>
+              {" "}
+              <AddCard />
+            </CardsContext.Provider>
+          }
+        />
+        <Route path="/setAlias" element={<SetAlias setAlias={setAlias} />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
