efa7d09c3c6ac9021d19b7ac7241ac8753fba0cb
hae-on <solbi2004@naver.com>
2023-04-28 14:24:21 +0900

    test: cardNumberInput 입력 테스트

--- src/stories/CardNumberInput.stories.tsx
+++ src/stories/CardNumberInput.stories.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Story } from '@storybook/react';
 import CardNumberInput from '../components/cardForm/CardNumberInput';
 import type { CardItemInfo, InputProps } from '../types/Card';
@@ -9,14 +10,19 @@ export default {
 
 type CardNumberInputProps = InputProps<CardItemInfo['cardNumber']>;
 
-const Template: Story<CardNumberInputProps> = (args) => (
-  <CardNumberInput {...args} />
-);
+const Template: Story<CardNumberInputProps> = (args) => {
+  const [value, setValue] = useState(args.value);
+
+  const handleChange = (newValue: string[]) => {
+    setValue(newValue);
+  };
+
+  return <CardNumberInput {...args} value={value} setValue={handleChange} />;
+};
 
 export const Default = Template.bind({});
 Default.args = {
   value: ['', '', '', ''],
-  setValue: () => {},
   errorMessage: '',
   setErrorMessage: () => {},
 };
@@ -24,7 +30,6 @@ Default.args = {
 export const Error = Template.bind({});
 Error.args = {
   value: ['rm', '', '', ''],
-  setValue: () => {},
   errorMessage: '카드 번호는 숫자로만 입력해주세요.',
   setErrorMessage: () => {},
 };
@@ -32,7 +37,6 @@ Error.args = {
 export const FinishInput = Template.bind({});
 FinishInput.args = {
   value: ['1234', '2234', '3455', '3456'],
-  setValue: () => {},
   errorMessage: '',
   setErrorMessage: () => {},
 };
