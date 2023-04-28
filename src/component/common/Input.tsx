8b21135abf14a10e0550efe52b62e8bb75c9107d
Q Kim <qoli0703@gmail.com>
2023-04-28 15:23:54 +0900

    refactor: input props 제대로 내려주게 수정

--- src/component/common/Input.tsx
+++ src/component/common/Input.tsx
@@ -9,32 +9,34 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export default function Input(props: Props) {
-  const { valueChangeSubscribers, parsers, defaultValue, className } = props;
+  const { valueChangeSubscribers, parsers, defaultValue, className, ...inputProps } = props;
 
   const [value, setValue] = useState(defaultValue ?? "");
 
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value;
 
-    const validatedValue = parsers?.reduce(
-      (prev, validator) => validator(prev), newValue
+    const parsedValue = parsers?.reduce(
+      (prev, validator) => validator(prev),
+      newValue,
     );
 
-    setValue(validatedValue ?? newValue);
+    setValue(parsedValue ?? newValue);
   };
 
   useEffect(() => {
     valueChangeSubscribers?.forEach((subscriberFunction) => {
       subscriberFunction(value);
     });
-  }, [value, valueChangeSubscribers]);
+  }, [value]);
 
   return (
     <input
-      {...props}
+      {...inputProps}
       value={value}
       onChange={onChangeHandler}
       className={`${styles.input} ${className}`}
+      defaultValue={defaultValue}
     />
   );
 }
