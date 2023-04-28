1dca85dcb96f6562b9514463f75a5d9c8b0e570b
semnil5202 <semnil5202@gmail.com>
2023-04-28 17:57:00 +0900

    style: 카드 별칭 입력 input 너비 조정

--- src/components/@common/CardAliasInput.styles.ts
+++ src/components/@common/CardAliasInput.styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Input = styled.input`
   margin-top: 120px;
-  width: 300px;
+  width: 320px;
   border-bottom: 1px solid #737373;
   font-size: 20px;
   text-align: center;
