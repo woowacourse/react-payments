65d0fb1eb291b92b386886ba8e8e86c0acb9dd68
unknown <tkdrb15@gmail.com>
2023-04-29 01:18:22 +0900

    feat: CardItem 스토리북 추가

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -8,17 +8,7 @@ export default {
 
 const Template: StoryFn<typeof Card> = (args: { $backgroundColor?: string }): React.ReactElement => <Card {...args} />;
 
-export const Purple = Template.bind({});
-Purple.args = {
-  $backgroundColor: "#BA55D3",
-};
-
 export const Gray = Template.bind({});
 Gray.args = {
   $backgroundColor: "#E5E5E5",
-};
-
-export const Green = Template.bind({});
-Green.args = {
-  $backgroundColor: "green",
 };
---
+++ src/stories/CardItem.stories.tsx
@@ -0,0 +1,23 @@
+import { StoryFn } from "@storybook/react";
+import { CardItem } from "../components/cardList/CardItem";
+import { CardType } from "../types/card";
+
+export default {
+  title: "CardItem",
+  component: CardItem,
+};
+
+const Template: StoryFn<typeof CardItem> = (args: { card: CardType }): React.ReactElement => <CardItem {...args} />;
+
+export const cardItem = Template.bind({});
+cardItem.args = {
+  card: {
+    CVC: 312,
+    alias: "내 카드",
+    brand: "신한카드",
+    expiryDate: "12 / 35",
+    numbers: "1233123123122231",
+    owner: "ME",
+    password: [2, 2],
+  },
+};
