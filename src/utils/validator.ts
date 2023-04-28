d2bbcf70cc072fd5bc976a2e41d41d693c955c12
ashleysyheo <ashleysyheo@gmail.com>
2023-04-29 00:04:48 +0900

    refactor: validator 유틸 함수 수정

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,21 +1,17 @@
-import { MultipleInputFieldCardInformation } from '../types';
-import {
-  CARD_NUMBER_INPUT_MAX_LENGTH,
-  DATE_DIVIDER,
-  MAX_VALID_EXPIRATION_YEAR,
-  REGEX,
-} from '../constants';
+import type { ExpirationDate, MultipleInputFieldCardInformation, Validator } from '../types';
+import { CARD_NUMBER_MAX_LENGTH, MAX_VALID_EXPIRATION_YEAR, REGEX } from '../constants';
 
 const validateNonEmptyInput = (input: string) => {
   return input !== '';
 };
 
 const validateCardNumber = (input: string) => {
-  return input.length === CARD_NUMBER_INPUT_MAX_LENGTH;
+  return input.length === CARD_NUMBER_MAX_LENGTH;
 };
 
-const validateExpirationDate = (input: string) => {
-  const [month, year] = input.split(DATE_DIVIDER).map(Number);
+const validateExpirationDate = (input: ExpirationDate) => {
+  const month = Number(input.month);
+  const year = Number(input.year);
 
   if (month < 1 || month > 12) return false;
   if (!year || !REGEX.TWO_DIGIT.test(String(year))) return false;
@@ -41,7 +37,7 @@ const validatePassword = (passwordInputs: string[]) => {
   return passwordInputs.every((password) => password.length === 1);
 };
 
-const validator = {
+const validator: Validator = {
   issuer: validateNonEmptyInput,
   cardNumber: validateCardNumber,
   expirationDate: validateExpirationDate,
