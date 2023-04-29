dc4fc354701ed019417d63f16a64ceecfef3f344
Jungwoo <evencoding@gmail.com>
2023-04-30 00:21:16 +0900

    refactor: 폴더 내 파일 이름 구체화

--- src/components/InputBox/index.ts
+++
@@ -1,8 +0,0 @@
-import Input from './Input';
-import CardNumbers from './CardNumbers';
-import ExpirationDate from './ExpirationDate';
-import OwnerName from './OwnerName';
-import Password from './Password';
-import SecurityNumbers from './SecurityNumbers';
-
-export { Input, CardNumbers, ExpirationDate, OwnerName, Password, SecurityNumbers };
--- src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export const isNumeric = (value: string) => {
-  return /^[0-9]*$/.test(value);
-};
