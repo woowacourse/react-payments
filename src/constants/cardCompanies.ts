62e85e2b23ce24d59400b5f2c3b4a75d3673aa87
Dain Lee <feb.dain@gmail.com>
2023-04-30 00:56:30 +0900

    design: 카카오뱅크 카드 색깔 변경

--- src/constants/cardCompanies.ts
+++ src/constants/cardCompanies.ts
@@ -14,7 +14,7 @@ export const CARD_COMPANIES: Readonly<{ [key: string]: string }> = {
 export const CARD_COLORS: Readonly<{ [key: string]: string }> = {
   BC카드: "#F04652",
   신한카드: "#0046FF",
-  카카오뱅크: "#FFE600",
+  카카오뱅크: " #FFEF38",
   현대카드: "#333",
   우리카드: "#027BC8",
   롯데카드: "#ED1C23",
