f9bfbd6981bf3fce3d8d51ec721090837d410072
solo5star <solo_5@naver.com>
2023-04-29 19:24:17 +0900

    feat: NewCreditCardButton에 rounded props 추가

--- src/components/payments/NewCreditCardButton.tsx
+++ src/components/payments/NewCreditCardButton.tsx
@@ -6,25 +6,41 @@ const ButtonContainer = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 8px;
-`;
 
-const Button = styled.button`
   width: 200px;
   height: 120px;
+`;
+
+type ButtonProps = {
+  $rounded?: boolean;
+};
+
+const Button = styled.button<ButtonProps>`
+  // NOTE: RoundedButton, Button 두 개로 분리할 시 서로 다른 Element로 인식되어
+  // DOM 노드의 삭제와 생성이 일어나 transition이 동작하지 않음, 따라서
+  // 하나의 파일에서 rounded에 대한 스타일 처리를 하도록 설정
+  width: ${(props) => (props.$rounded ? '80px' : '200px')};
+  height: ${(props) => (props.$rounded ? '80px' : '120px')};
+
+  margin: ${(props) => (props.$rounded ? '20px' : '0')} 0;
 
   font-size: 20px;
   font-weight: 900;
-  border-radius: 5px;
+  border-radius: ${(props) => (props.$rounded ? '48px' : '5px')};
   background: ${(props) => props.theme.color.grey3};
+
+  transition-property: width height border-radius margin;
+  transition-duration: 0.1s;
 `;
 
 type NewCreditCardButtonProps = {
+  rounded?: boolean;
   helperText?: boolean;
   onClick?: () => void;
 };
 
 export const NewCreditCardButton = (props: NewCreditCardButtonProps) => {
-  const { helperText, onClick } = props;
+  const { rounded, helperText, onClick } = props;
 
   return (
     <ButtonContainer>
@@ -33,7 +49,9 @@ export const NewCreditCardButton = (props: NewCreditCardButtonProps) => {
           새로운 카드를 등록해주세요.
         </Text>
       )}
-      <Button onClick={onClick}>+</Button>
+      <Button $rounded={rounded} onClick={onClick}>
+        +
+      </Button>
     </ButtonContainer>
   );
 };
