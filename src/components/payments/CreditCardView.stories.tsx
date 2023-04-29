87089e4faf676e241599faa9f5af95d527db3d44
solo5star <solo_5@naver.com>
2023-04-29 19:45:33 +0900

    test: CreditCardView backface 토글 버튼 추가

--- src/components/payments/CreditCardView.stories.tsx
+++ src/components/payments/CreditCardView.stories.tsx
@@ -1,12 +1,20 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
 import styled from 'styled-components';
-import { CREDIT_CARD_VENDORS } from '../../domain/CreditCardVendor';
-
 import { CREDIT_CARD_VENDOR_BRAND_COLORS } from '../../domain/CreditCardBrandColors';
+import { CREDIT_CARD_VENDORS } from '../../domain/CreditCardVendor';
+import { Button } from '../common/Button';
 import { CreditCardView } from './CreditCardView';
 import { VendorIcon } from './VendorIcon';
 
 const Container = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`;
+
+const CreditCardGrid = styled.div`
   display: inline-grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 16px;
@@ -56,21 +64,28 @@ export const WithBackface: Story = {
 };
 
 export const WithVendor = () => {
+  const [showBackface, setShowBackface] = useState(false);
+  const handleClickFlipButton = () => {
+    setShowBackface(!showBackface);
+  };
   return (
     <Container>
-      {' '}
-      {CREDIT_CARD_VENDORS.map((vendor) => (
-        <CreditCardView
-          key={vendor}
-          owner="NOAH"
-          vendor={vendor}
-          cardNumbers="1290-9072-3345-2081"
-          expirationDate={['6', '25']}
-          cvc="127"
-          color={CREDIT_CARD_VENDOR_BRAND_COLORS[vendor]}
-          icon={<VendorIcon vendor={vendor} />}
-        />
-      ))}
+      <CreditCardGrid>
+        {CREDIT_CARD_VENDORS.map((vendor) => (
+          <CreditCardView
+            key={vendor}
+            owner="NOAH"
+            vendor={vendor}
+            cardNumbers="1290-9072-3345-2081"
+            expirationDate={['6', '25']}
+            cvc="127"
+            color={CREDIT_CARD_VENDOR_BRAND_COLORS[vendor]}
+            icon={<VendorIcon vendor={vendor} />}
+            showBackface={showBackface}
+          />
+        ))}
+      </CreditCardGrid>
+      <Button onClick={handleClickFlipButton}>뒤집기</Button>
     </Container>
   );
 };
