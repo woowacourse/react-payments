cf7ba73abc7816adbebe923eb381cfc8dc28809b
jeongwusi <jeongwusi@gmail.com>
2023-04-29 20:03:35 +0900

    ♻️ refactor: CardInfoContextType 분리

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,22 @@ export type CardInfo = {
   selectedCard: string;
   cardNickName: string;
 };
+
+export type CardInfoContextType = {
+  cardNumber: string;
+  setCardNumber: (cardNumber: string) => void;
+  expirationDate: string;
+  setExpirationDate: (expirationDate: string) => void;
+  cardOwnerName: string;
+  setCardOwnerName: (cardOwnerName: string) => void;
+  securityCode: string;
+  setSecurityCode: (securityCode: string) => void;
+  firstDigit: string;
+  setFirstDigit: (firstDigit: string) => void;
+  secondDigit: string;
+  setSecondDigit: (secondDigit: string) => void;
+  selectedCard: string;
+  setSelectedCard: (selectedCard: string) => void;
+  cardNickName: string;
+  setCardNickName: (cardNickName: string) => void;
+};
