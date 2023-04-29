98f3572b0648588a2cf77d31f8790d41c83ce4c8
2yunseong <dbsdltjd3701@naver.com>
2023-04-29 18:32:17 +0900

    test: useInput 카드 만료일(년도) 테스트

--- src/test/hooks/useInput.test.ts
+++ src/test/hooks/useInput.test.ts
@@ -25,3 +25,25 @@ describe('카드 번호 테스트', () => {
     expect(result.current.status).toBe('INVALID');
   });
 });
+
+describe('만료일(년도) 테스트', () => {
+  it('카드 번호 Hook의 초기 상태는 INIT를 가진다.', () => {
+    const { result } = renderHook(() => useInput(isValidExpiredYearFormat));
+    expect(result.current.status).toBe('INIT');
+  });
+  it('유효한 카드 번호 입력이 들어오면, 카드번호의 상태가 유효한 상태로 변경된다.', () => {
+    const { result } = renderHook(() => useInput(isValidExpiredYearFormat));
+    act(() => {
+      result.current.onChange(mockEventTarget('23') as React.ChangeEvent<HTMLInputElement>);
+    });
+    expect(result.current.status).toBe('VALID');
+    expect(result.current.value).toBe('23');
+  });
+  it('유효하지 않은 카드 번호 입력이 들어오면, 카드번호의 상태가 유효하지 않는 상태로 변경된다.', () => {
+    const { result } = renderHook(() => useInput(isValidExpiredYearFormat));
+    act(() => {
+      result.current.onChange(mockEventTarget('a8') as React.ChangeEvent<HTMLInputElement>);
+    });
+    expect(result.current.status).toBe('INVALID');
+  });
+});
