f0d13a3ea485c92cad8fee30355f239def1d858d
yeopto <gunyeop530@gmail.com>
2023-04-30 03:14:35 +0900

    refactor: contextProvider를 위해 Content 컴포넌트를 만들다

---
+++ src/components/CreditCardsListPageContent.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { useCreditCards } from '../hooks/useCreditCards';
+import { CreditCardView } from './CreditCardView';
+import { NewCreditCardButton } from './NewCreditCardButton';
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  align-items: center;
+  flex: 1;
+  padding-bottom: 25px;
+`;
+
+export const CreditCardsListPageContent = () => {
+  const { creditCards } = useCreditCards();
+
+  return (
+    <Container>
+      {creditCards.map((creditCard) => (
+        <CreditCardView
+          key={creditCard.name}
+          name={creditCard.name}
+          cardNumbers={creditCard.cardNumbers}
+          expirationDate={creditCard.expirationDate}
+          cardCompany={creditCard.cardCompany}
+          nickName={creditCard.nickName}
+        />
+      ))}
+
+      <NewCreditCardButton helperText={creditCards.length === 0} />
+    </Container>
+  );
+};
