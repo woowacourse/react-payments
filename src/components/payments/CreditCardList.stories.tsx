11c36091a9b6d9a0ad595f7ecfce520784a7f092
solo5star <solo_5@naver.com>
2023-04-29 19:21:22 +0900

    test: NewCreditCardButton이 함께 있는 스토리 추가

--- src/components/payments/CreditCardList.stories.tsx
+++ src/components/payments/CreditCardList.stories.tsx
@@ -1,7 +1,27 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { useRef, useState } from 'react';
+import styled from 'styled-components';
 import type { CreditCard } from '../../domain/CreditCard';
 import creditCards from '../../fixtures/creditCards.json';
+import { useIntersectionObserver } from '../../hooks/useIntersectionObserver';
 import { CreditCardList } from './CreditCardList';
+import { NewCreditCardButton } from './NewCreditCardButton';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  gap: 24px;
+`;
+
+const CreditCardListBottom = styled.div`
+  padding-bottom: 16px;
+
+  position: sticky;
+  bottom: -1px;
+  z-index: 2;
+`;
 
 const meta = {
   title: 'payments/CreditCardList',
@@ -21,5 +41,28 @@ export const Default: Story = {
 export const WithManyItems: Story = {
   args: {
     creditCards: creditCards as CreditCard[],
+  },
+};
+
+export const WithNewCreditCardButton: Story = {
+  args: {
+    creditCards: creditCards as CreditCard[],
+  },
+  render: (args) => {
+    const bottomRef = useRef<HTMLDivElement>(null);
+    const [isBottomSticky, setIsBottomSticky] = useState(false);
+
+    useIntersectionObserver(bottomRef, (isIntersecting: boolean) => {
+      setIsBottomSticky(!isIntersecting);
+    });
+
+    return (
+      <Container>
+        <CreditCardList {...args} />
+        <CreditCardListBottom ref={bottomRef}>
+          <NewCreditCardButton rounded={isBottomSticky} />
+        </CreditCardListBottom>
+      </Container>
+    );
   },
 };
