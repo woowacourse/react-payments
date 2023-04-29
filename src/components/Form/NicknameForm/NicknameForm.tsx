7b039b31948ad069571117d0868879fb1e5a5be0
Jungwoo <evencoding@gmail.com>
2023-04-30 01:06:55 +0900

    feat: form이 렌더링되면 input에 focus되도록 구현

--- src/components/Form/NicknameForm/NicknameForm.tsx
+++ src/components/Form/NicknameForm/NicknameForm.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 
+import { useEffect, useRef } from 'react';
+
 import * as styled from './NicknameForm.styled';
 
 import { useCardInfoActions, useCardInfoValue } from '../../../context/CardInfoContext';
@@ -10,6 +12,8 @@ const NicknameForm = () => {
   const navigate = useNavigate();
   const [cardInfo, { setCardInfo, initCardInfo }] = [useCardInfoValue(), useCardInfoActions()];
   const { setCardList } = useCardListActions();
+
+  const nicknameInputRef = useRef<HTMLInputElement>(null);
 
   const { onChange, onSubmit } = useForm({
     submitAction: () => {
@@ -23,9 +27,18 @@ const NicknameForm = () => {
     errorOptions: {},
   });
 
+  useEffect(() => {
+    nicknameInputRef.current?.focus();
+  }, []);
+
   return (
     <styled.NicknameForm onSubmit={onSubmit}>
-      <styled.NicknameInput name="nickname" onChange={onChange} />
+      <styled.NicknameInput
+        ref={nicknameInputRef}
+        name="nickname"
+        onChange={onChange}
+        placeholder="카드 별칭 (선택)"
+      />
       <styled.NicknameSubmitButton>완료</styled.NicknameSubmitButton>
     </styled.NicknameForm>
   );
