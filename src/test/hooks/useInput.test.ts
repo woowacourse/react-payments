6a66921e2e0312cfd8bddc95857436b953364916
2yunseong <dbsdltjd3701@naver.com>
2023-04-29 19:53:21 +0900

    test: useInput 카드 만료일(월) 테스트

--- src/test/hooks/useInput.test.ts
+++ src/test/hooks/useInput.test.ts
@@ -1,6 +1,10 @@
 import { renderHook } from '@testing-library/react';
 import useInput from '../../hooks/useInput';
-import { isValidCardNumber, isValidExpiredYearFormat } from '../../pages/AddCard/domain/dispatcher';
+import {
+  isValidCardNumber,
+  isValidExpiredMonthFormat,
+  isValidExpiredYearFormat,
+} from '../../pages/AddCard/domain/dispatcher';
 import { act } from 'react-dom/test-utils';
 import { mockEventTarget } from '../utils/util';
 
@@ -27,11 +31,11 @@ describe('카드 번호 테스트', () => {
 });
 
 describe('만료일(년도) 테스트', () => {
-  it('카드 번호 Hook의 초기 상태는 INIT를 가진다.', () => {
+  it('만료일(년도) Hook의 초기 상태는 INIT를 가진다.', () => {
     const { result } = renderHook(() => useInput(isValidExpiredYearFormat));
     expect(result.current.status).toBe('INIT');
   });
-  it('유효한 카드 번호 입력이 들어오면, 카드번호의 상태가 유효한 상태로 변경된다.', () => {
+  it('유효한 만료일(년도) 입력이 들어오면, 카드번호의 상태가 유효한 상태로 변경된다.', () => {
     const { result } = renderHook(() => useInput(isValidExpiredYearFormat));
     act(() => {
       result.current.onChange(mockEventTarget('23') as React.ChangeEvent<HTMLInputElement>);
@@ -39,10 +43,32 @@ describe('만료일(년도) 테스트', () => {
     expect(result.current.status).toBe('VALID');
     expect(result.current.value).toBe('23');
   });
-  it('유효하지 않은 카드 번호 입력이 들어오면, 카드번호의 상태가 유효하지 않는 상태로 변경된다.', () => {
+  it('유효하지 않은 만료일(년도) 입력이 들어오면, 카드번호의 상태가 유효하지 않는 상태로 변경된다.', () => {
     const { result } = renderHook(() => useInput(isValidExpiredYearFormat));
     act(() => {
       result.current.onChange(mockEventTarget('a8') as React.ChangeEvent<HTMLInputElement>);
+    });
+    expect(result.current.status).toBe('INVALID');
+  });
+});
+
+describe('만료일(월) 테스트', () => {
+  it('만료일(월) Hook의 초기 상태는 INIT를 가진다.', () => {
+    const { result } = renderHook(() => useInput(isValidExpiredMonthFormat));
+    expect(result.current.status).toBe('INIT');
+  });
+  it('유효한 만료일(월) 입력이 들어오면, 카드번호의 상태가 유효한 상태로 변경된다.', () => {
+    const { result } = renderHook(() => useInput(isValidExpiredMonthFormat));
+    act(() => {
+      result.current.onChange(mockEventTarget('12') as React.ChangeEvent<HTMLInputElement>);
+    });
+    expect(result.current.status).toBe('VALID');
+    expect(result.current.value).toBe('12');
+  });
+  it('유효하지 않은 만료일(월) 입력이 들어오면, 카드번호의 상태가 유효하지 않는 상태로 변경된다.', () => {
+    const { result } = renderHook(() => useInput(isValidExpiredMonthFormat));
+    act(() => {
+      result.current.onChange(mockEventTarget('13') as React.ChangeEvent<HTMLInputElement>);
     });
     expect(result.current.status).toBe('INVALID');
   });
