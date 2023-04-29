171d3b773f262a68e8e1ac2a1174a53834eaaff6
JIWON <afds4567@gmail.com>
2023-04-30 01:54:23 +0900

    fix: 카드 선택 안 한 경우에도 등록되는 에러 해결

--- src/hooks/useFormValidation.tsx
+++ src/hooks/useFormValidation.tsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 import { UseInputProps } from "./useInput";
+import { useCardState } from "../context/CardContext";
 
 export function useFormValidation(
   ref: React.RefObject<HTMLFormElement>,
   fields: UseInputProps["value"][]
 ) {
   const [isFormFilled, setIsFormFilled] = useState(true);
-
+  const { title } = useCardState();
   useEffect(() => {
     if (!ref.current) return;
-    setIsFormFilled(!ref.current.checkValidity());
+    setIsFormFilled(!ref.current.checkValidity() || title === "");
   }, [ref, fields]);
-
   return {
     isFormFilled,
   };
