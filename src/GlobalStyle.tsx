c6b515b2c40de5080b053a1ed0fa58dc16e4708d
cruelladevil <dev.timetravel@gmail.com>
2023-04-29 23:22:28 +0900

    style: GlobalStyle 구현

---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,46 @@
+import { PropsWithChildren } from 'react';
+import { createGlobalStyle } from 'styled-components';
+import ResetStyle from './ResetStyle';
+
+const GlobalStyle = createGlobalStyle<PropsWithChildren>`
+  ${ResetStyle}
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    background-color: #F5F5F5;
+  }
+
+  #root {
+    width: 375px;
+    height: 700px;
+    margin: 0 auto;
+    background-color: white;
+  }
+
+  @media (max-width: 500px) {
+    #root {
+      width: 100vw;
+      height: 100vh;
+    }
+  }
+
+  button {
+    background: none;
+    border: none;
+    padding: 0;
+    font: inherit;
+    cursor: pointer;
+    outline: inherit;
+  }
+
+  input {
+    border: none;
+    background: transparent;
+  }
+`;
+
+export default GlobalStyle;
