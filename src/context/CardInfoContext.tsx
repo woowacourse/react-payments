2c1545090817936670ad6c6cf215a3978c77eda5
jeongwusi <jeongwusi@gmail.com>
2023-04-29 20:10:28 +0900

    ♻️ refactor: CardInfoContextType 분리

--- src/context/CardInfoContext.tsx
+++ src/context/CardInfoContext.tsx
@@ -1,30 +1,12 @@
 import { createContext, PropsWithChildren, useContext, useState } from 'react';
-
-type CardInfoContextType = {
-  cardNumber: string;
-  setCardNumber: (cardNumber: string) => void;
-  expirationDate: string;
-  setExpirationDate: (expirationDate: string) => void;
-  cardOwnerName: string;
-  setCardOwnerName: (cardOwnerName: string) => void;
-  securityCode: string;
-  setSecurityCode: (securityCode: string) => void;
-  firstDigit: string;
-  setFirstDigit: (firstDigit: string) => void;
-  secondDigit: string;
-  setSecondDigit: (secondDigit: string) => void;
-  selectedCard: string;
-  setSelectedCard: (selectedCard: string) => void;
-  cardNickName: string;
-  setCardNickName: (cardNickName: string) => void;
-};
+import { CardInfoContextType } from '../types';
 
 export const CardInfoContext = createContext<CardInfoContextType | undefined>(undefined);
 
 export const useCardInfoContext = () => {
   const context = useContext(CardInfoContext);
   if (!context) {
-    throw new Error('useCardInfoContext는 CardInfoProvider안에서 사용해야 합니다.');
+    throw new Error('useCardInfoContext는 CardInfoProvider 안에서 사용해야 합니다.');
   }
   return context;
 };
