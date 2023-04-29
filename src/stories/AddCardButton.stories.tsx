7f0c4de017975a639aa9a352a7a616a61980cd24
hae-on <solbi2004@naver.com>
2023-04-29 17:17:29 +0900

    test: addCardButton 렌더링 테스트

---
+++ src/stories/AddCardButton.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, Story } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { BrowserRouter } from 'react-router-dom';
+import AddCardButton from '../components/CardListPageComponents/AddCardButton';
+
+const meta = {
+  title: 'Payment/AddCardButton',
+  component: AddCardButton,
+  tags: ['autodocs'],
+  argTypes: {},
+} as Meta<typeof AddCardButton>;
+
+interface AddCardButtonProps {
+  onOpen: () => void;
+}
+
+const Template: Story<AddCardButtonProps> = (args) => (
+  <BrowserRouter>
+    <AddCardButton {...args} />
+  </BrowserRouter>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  onOpen: action('onOpen'),
+};
+
+export default meta;
