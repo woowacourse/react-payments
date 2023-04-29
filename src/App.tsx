d1d305ac14e8823d3ee8364c99075f0e935098a5
cruelladevil <dev.timetravel@gmail.com>
2023-04-29 16:13:46 +0900

    chore: 개발 환경 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
-import { CardListProvider } from './CardListContext';
-import CardRegistration from './pages/CardRegistration/CardRegistration';
-import Home from './pages/Home';
-
 function App() {
-  return (
-    <CardListProvider>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/registration" element={<CardRegistration />} />
-      </Routes>
-    </CardListProvider>
-  );
+  return <div>App</div>;
 }
 
 export default App;
