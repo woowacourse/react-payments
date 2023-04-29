302e1c49f7841607a3fa293003c46f788ab46f4f
Leejin-Yang <ejin1996@gmail.com>
2023-04-29 22:57:11 +0900

    feat: Input 컴포넌트에 underlined prop 추가

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -4,13 +4,14 @@ import styles from './input.module.css';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   align?: 'left' | 'center' | 'right';
+  underlined?: true;
 }
 
 const Input = forwardRef<HTMLInputElement, Props>(
-  ({ align = 'left', ...props }, ref) => {
+  ({ align = 'left', underlined, ...props }, ref) => {
     return (
       <input
-        className={`${styles.input} ${styles[align]}`}
+        className={`${styles.input} ${styles[align]} ${underlined ? styles.underlined : ''}`}
         {...props}
         ref={ref}
       />
