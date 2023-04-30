773a193e2b8734a4021911fe18147beeed53f5bc
Jungwoo <evencoding@gmail.com>
2023-04-30 14:40:32 +0900

    story: Card 컴포넌트에 대한 스토리 구현

--- src/stories/components/Card.stories.ts
+++ src/stories/components/Card.stories.ts
@@ -1,30 +1,81 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
-import Card from '../../components/Card';
+import { Card } from '../../components';
 
 const meta: Meta<typeof Card> = {
   component: Card,
   title: 'Components/Card',
+  tags: ['autodocs'],
 };
 
 export default meta;
-type Story = StoryObj<typeof Card>;
+type Story = StoryObj<typeof meta>;
+
+const getCardInfo = (bank: string = '') => ({
+  id: '1',
+  firstCardNumbers: '1234',
+  secondCardNumbers: '1234',
+  thirdCardNumbers: '1234',
+  fourthCardNumbers: '1234',
+  expirationMonth: '12',
+  expirationYear: '23',
+  ownerName: 'Woody',
+  securityNumbers: '123',
+  firstPassword: '1',
+  secondPassword: '2',
+  bank,
+  nickname: bank.toUpperCase(),
+});
 
 export const Default: Story = {
   args: {
-    bgColor: '#333333',
-    cardInfo: {
-      id: 'o3qiOI#$#',
-      firstCardNumbers: '1234',
-      secondCardNumbers: '1234',
-      thirdCardNumbers: '1234',
-      fourthCardNumbers: '1234',
-      expirationMonth: '03',
-      expirationYear: '28',
-      ownerName: 'Woody',
-      securityNumbers: '234',
-      firstPassword: '1',
-      secondPassword: '2',
-    },
+    cardInfo: getCardInfo(),
+  },
+};
+
+export const Bc: Story = {
+  args: {
+    cardInfo: getCardInfo('bc'),
+  },
+};
+
+export const Sinhan: Story = {
+  args: {
+    cardInfo: getCardInfo('sinhan'),
+  },
+};
+
+export const Kakao: Story = {
+  args: {
+    cardInfo: getCardInfo('kakao'),
+  },
+};
+
+export const Hyundai: Story = {
+  args: {
+    cardInfo: getCardInfo('hyundai'),
+  },
+};
+
+export const Woori: Story = {
+  args: {
+    cardInfo: getCardInfo('woori'),
+  },
+};
+
+export const Lotte: Story = {
+  args: {
+    cardInfo: getCardInfo('lotte'),
+  },
+};
+
+export const Hana: Story = {
+  args: {
+    cardInfo: getCardInfo('hana'),
+  },
+};
+
+export const Kookmin: Story = {
+  args: {
+    cardInfo: getCardInfo('kookmin'),
   },
 };
