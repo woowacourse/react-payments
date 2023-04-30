16b407866b66199d9bd1c389ce7a57f247ea07ed
turtle601 <turtle601@naver.com>
2023-04-30 20:49:05 +0900

    style(color): 카드 색깔 속성 추가

---
+++ src/components/stylesheet/theme.ts
@@ -0,0 +1,10 @@
+export const color: Record<string, string> = {
+  BC: 'rgb(240, 70, 81)',
+  HANA: 'rgb(0, 148, 144)',
+  HYUNDAI: 'rgb(0, 0, 0)',
+  KAKAO: 'rgb(255, 231, 0)',
+  KB: 'rgb(107, 97, 87)',
+  LOTTE: 'rgb(237, 28, 36)',
+  SHINHAN: 'rgb(0, 70, 255)',
+  WORRI: 'rgb(0, 123, 200)',
+};
