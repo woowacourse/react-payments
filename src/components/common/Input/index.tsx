ee71cb40f80aa735f90d370be498770deb4938ef
Leejin-Yang <ejin1996@gmail.com>
2023-04-30 17:39:44 +0900

    refactor: Input 컴포넌트 props 타입 수정

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -3,12 +3,12 @@ import { InputHTMLAttributes, forwardRef } from 'react';
 import styles from './input.module.css';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  align?: 'left' | 'center' | 'right';
-  underlined?: true;
+  align?: 'left' | 'center';
+  underlined?: boolean;
 }
 
 const Input = forwardRef<HTMLInputElement, Props>(
-  ({ align = 'left', underlined, ...props }, ref) => {
+  ({ align = 'left', underlined = false, ...props }, ref) => {
     return (
       <input
         className={`${styles.input} ${styles[align]} ${underlined ? styles.underlined : ''}`}
