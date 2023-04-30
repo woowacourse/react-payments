b1cbf4d895f6383178e690816ed0dde932348822
turtle601 <turtle601@naver.com>
2023-04-30 20:52:38 +0900

    feat(NameRegisterCard): NameRegisterCard 컴포넌트 구현

--- src/components/templates/NameRegisterCard/index.tsx
+++ src/components/templates/NameRegisterCard/index.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { Card } from '../../../store/type';
+import { useCardListDispatch, useCardListState } from '../../../hooks/useContextHooks';
+import Button from '../../atomics/Button';
 import Input from '../../atomics/Input';
 import Message from '../../atomics/Message';
 import { VStack } from '../../layout/flexbox';
 import CardItem from '../../molecules/CardItem';
 
-interface NameRegisterCardProps {
-  card: Card;
-}
+const NameRegisterCard: React.FC = () => {
+  const [nickName, setNickName] = useState<string>('');
 
-const NameRegisterCard: React.FC<NameRegisterCardProps> = ({ card }) => {
+  const cardList = useCardListState();
+  const cardListDispatcher = useCardListDispatch();
+
+  const navigate = useNavigate();
+
+  const card = cardList[cardList.length - 1];
+
+  const handleRegisterCard = () => {
+    card.nickName = nickName;
+
+    cardListDispatcher([...cardList.slice(0, cardList.length - 1), card]);
+    navigate('/');
+  };
+
   return (
     <NameRegisterCardWrapper>
       <Message fontSize="24px" lineHeight="28px" color="#383838">
@@ -24,10 +38,29 @@ const NameRegisterCard: React.FC<NameRegisterCardProps> = ({ card }) => {
         width="244px"
         center={true}
         placeholder="엄마카드"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setNickName(e.target.value);
+        }}
       />
+      <ButtonWrapper>
+        <Button type="button" width="50px" height="30px" onClick={handleRegisterCard}>
+          <Message fontSize="14px" lineHeight="16px" fontWeight={700} color="#000">
+            확인
+          </Message>
+        </Button>
+      </ButtonWrapper>
     </NameRegisterCardWrapper>
   );
 };
+
+const ButtonWrapper = styled.div`
+  margin-top: 24px;
+
+  width: 90%;
+
+  display: flex;
+  justify-content: flex-end;
+`;
 
 const NameRegisterCardWrapper = styled.div`
   ${VStack};
@@ -36,7 +69,7 @@ const NameRegisterCardWrapper = styled.div`
   align-items: center;
 
   span + div {
-    margin-top: 36px;
+    margin-top: 20px;
   }
 
   div + input {
