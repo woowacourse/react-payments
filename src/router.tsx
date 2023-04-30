6d3c1ec6923e46a6d7ed38cb73127a3d6216f26f
NaveOWO <pianowell96@gmail.com>
2023-04-30 18:47:49 +0900

    feat : nickname설정페이지 라우터 생성

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PATH } from "./constants/path";
+import { AddCardNicknamePage } from "./pages/addCardNicknamePage";
 import { AddCardPage } from "./pages/addCardPage";
 import { CardListPage } from "./pages/cardListPage";
 import { ErrorPage } from "./pages/errorPage";
@@ -9,7 +10,8 @@ export function Router() {
     <BrowserRouter>
       <Routes>
         <Route path={PATH.CARD_LIST} element={<CardListPage />}></Route>
-        <Route path={PATH.ADD_CARD} element={<AddCardPage />}></Route>
+        <Route path={`${PATH.ADD_CARD}*`} element={<AddCardPage />}></Route>
+        <Route path="/add-nickname" element={<AddCardNicknamePage />}></Route>
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
