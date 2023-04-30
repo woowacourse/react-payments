6d940ad495b53d7492e92d6ad170fd54e5e53aff
JIWON <afds4567@gmail.com>
2023-04-30 18:06:54 +0900

    feat: 카드 별칭 추가 페이지 구현

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,31 +1,43 @@
 import { InputHTMLAttributes } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   textAlign?: string;
   isNumber?: boolean;
+  shape?: string;
   showError?: (e: React.FocusEvent<HTMLInputElement>) => void;
 }
 
-const StyledInput = styled.input<{ textAlign?: string }>`
-  border: none;
-  background-color: #ecebf1;
+const StyledInput = styled.input<{ shape?: string; textAlign?: string }>`
   font-size: 18px;
   font-weight: 500;
   width: 100%;
-  text-align: ${(props) => props.textAlign};
+  text-align: ${(props) => props.textAlign || "left"};
+  border: none;
+  outline: none;
+  background-color: #ecebf1;
+
+  ${(props) =>
+    props.shape === "underline" &&
+    css`
+      border-bottom: 1px solid #d1d1d1;
+      background-color: transparent;
+    `}
 `;
 
 export default function Input({
   textAlign = "baseline",
   isNumber,
+  shape,
   showError,
   ...rest
 }: InputProps) {
+  console.log(shape);
   return (
     <StyledInput
       textAlign={textAlign}
       inputMode={isNumber ? "numeric" : "text"}
+      shape={shape}
       onBlur={showError}
       {...rest}
     />
