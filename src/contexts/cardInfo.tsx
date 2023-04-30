5808e2b2f7eb6ea13e676ba0eb678d59b7b908a1
NaveOWO <pianowell96@gmail.com>
2023-04-30 14:56:40 +0900

    feat : 카드사 선택하는 context api 추가

--- src/contexts/cardInfo.tsx
+++ src/contexts/cardInfo.tsx
@@ -1,7 +1,10 @@
+import { number } from "prop-types";
 import React, { createContext, useRef } from "react";
+import { useState } from "react";
 import { useInputDate } from "../hooks/useInputDate";
 import { useInputName } from "../hooks/useInputName";
 import { useInputNumber } from "../hooks/useInputNumber";
+import { useSelect } from "../hooks/useSelect";
 import { CardNumberIndex } from "../type/input";
 import { Ref } from "../type/ref";
 
@@ -40,18 +43,29 @@ export const RefContext = createContext<React.MutableRefObject<Ref>>({
   current: {},
 });
 
+export const BankContext = createContext<{
+  selectedItem: number;
+  selectItem: (e: React.MouseEvent<HTMLElement>) => void;
+}>({
+  selectedItem: -1,
+  selectItem: (e: React.MouseEvent<HTMLElement>) => {},
+});
+
 export function CardInfoProvider({ children }: { children: React.ReactNode }) {
   const numberInfo = useInputNumber();
   const dateInfo = useInputDate();
   const nameInfo = useInputName();
   const inputRef = useRef<React.MutableRefObject<Ref>>({ current: {} });
+  const bankInfo = useSelect();
 
   return (
     <RefContext.Provider value={inputRef.current}>
       <NumberContext.Provider value={numberInfo}>
         <DateContext.Provider value={dateInfo}>
           <NameContext.Provider value={nameInfo}>
-            {children}
+            <BankContext.Provider value={bankInfo}>
+              {children}
+            </BankContext.Provider>
           </NameContext.Provider>
         </DateContext.Provider>
       </NumberContext.Provider>
