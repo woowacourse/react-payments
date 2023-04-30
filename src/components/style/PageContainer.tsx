22bf45cef471c0eb590867c4a7406ab5ad2be1b0
yogjin <youngjin988@gmail.com>
2023-04-30 12:50:51 +0900

    style: page max-width를 350px로 수정

--- src/components/style/PageContainer.tsx
+++ src/components/style/PageContainer.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const PageContainer = styled.section`
   width: 100vw;
-  max-width: 600px;
+  max-width: 350px;
   height: 100vh;
   margin: 0 auto;
   padding: 10px;
