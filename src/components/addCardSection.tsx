075b160c0338cd052642f87808113dc87af538df
NaveOWO <pianowell96@gmail.com>
2023-04-30 14:55:29 +0900

    feat : 모달 컴포넌트 추가

--- src/components/addCardSection.tsx
+++ src/components/addCardSection.tsx
@@ -1,12 +1,24 @@
 import { useContext } from "react";
-import { DateContext, NameContext, NumberContext } from "../contexts/cardInfo";
+import {
+  BankContext,
+  DateContext,
+  NameContext,
+  NumberContext,
+} from "../contexts/cardInfo";
+import { bank } from "../core/bank";
 import { AddCardForm } from "./addCardForm";
+import { BankMenu } from "./bankMenu";
 import { Card } from "./common/card";
+import { ModalBackDrop } from "./common/modal/modalBackDrop";
+import { ModalBox } from "./common/modal/modalBox";
+import { ModalContent } from "./common/modal/modalContent";
+import { ModalTrigger } from "./common/modal/modalTrigger";
 
 export function AddCardSection() {
   const { cardNumber } = useContext(NumberContext);
   const { month, year } = useContext(DateContext);
   const { userName } = useContext(NameContext);
+  const { selectedItem, selectItem } = useContext(BankContext);
 
   return (
     <>
@@ -15,8 +27,20 @@ export function AddCardSection() {
         month={month}
         year={year}
         userName={userName}
+        cardColor={{
+          bgColor: bank[selectedItem]?.color,
+          fontColor: bank[selectedItem]?.font,
+        }}
+        bank={bank[selectedItem]?.logoName}
       />
       <AddCardForm />
+      <ModalBox>
+        <ModalTrigger />
+        <ModalBackDrop />
+        <ModalContent>
+          <BankMenu selectItem={selectItem} />
+        </ModalContent>
+      </ModalBox>
     </>
   );
 }
