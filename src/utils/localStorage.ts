8a0f7a9887b7e1f10605f364f555cdef969bcab4
AhnGeunTaek <dksrmsxor@gmail.com>
2023-05-01 14:19:13 +0900

    refactor: 로컬스토리지 로직 변경과 로컬스토리지 hook 삭제

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,8 +1,10 @@
-function getLocalStorage(key: string) {
+import { LOCAL_STORAGE_KEY } from "../constants";
+
+function getLocalStorage(key: string = LOCAL_STORAGE_KEY) {
   return JSON.parse(localStorage.getItem(key) as string);
 }
 
-function saveToLocalStorage<T>(key: string, data: T[]) {
+function saveToLocalStorage<T>(data: T[], key: string = LOCAL_STORAGE_KEY) {
   localStorage.setItem(key, JSON.stringify(data));
 }
 
