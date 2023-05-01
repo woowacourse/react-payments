8729c3437ca74fe7a23e1c77e4ff6803cbaa9e8c
AhnGeunTaek <dksrmsxor@gmail.com>
2023-05-01 16:50:34 +0900

    feat: CardItem 컴포넌트 생성

--- src/components/CardItem/CardItem.tsx
+++ src/components/CardItem/CardItem.tsx
@@ -1,28 +1,53 @@
-import { ComponentPropsWithoutRef } from 'react';
-import { Card } from '../../types';
-import styles from './style.module.css';
+import { memo } from "react";
+import type { ComponentPropsWithoutRef } from "react";
+import type { CardDisplayInformation } from "../../types";
+import { REGEX, SECURITY_TEXT_ICON } from "../../constants";
+import { ISSUER_CLASS_NAME } from "../../constants/issuerClassName";
+import styles from "./style.module.css";
+import { CARD_NUMBER_UNIT_MAX_LENGTH } from "../../constants/input";
 
-interface CardItemProps extends ComponentPropsWithoutRef<'div'> {
-  information: Card;
-}
+interface CardItemProps
+  extends ComponentPropsWithoutRef<"div">,
+    CardDisplayInformation {}
 
-function CardItem({ information, className }: CardItemProps) {
+const CardItem = ({
+  issuer,
+  cardNumber,
+  expirationDate,
+  ownerName,
+  className = "",
+}: CardItemProps) => {
+  const cardNumberArray: string[] =
+    cardNumber.match(REGEX.FOUR_CHAR_SEQUENCE) ?? [];
+  const displayedCardNumber = cardNumberArray.concat(
+    Array(CARD_NUMBER_UNIT_MAX_LENGTH - cardNumberArray.length).fill("")
+  );
+
   return (
-    <div className={`${styles.cardItemContainer} ${className}`}>
-      <div className={styles.cardChip}></div>
+    <div
+      className={`${styles.cardItemContainer} ${
+        issuer && styles[ISSUER_CLASS_NAME[issuer]]
+      } ${className}`}
+    >
+      <p className={styles.cardIssuer}>{issuer}</p>
+      <div className={styles.cardChip} />
       <div className={styles.cardNumber}>
-        {information.cardNumber.map((number, index) => (
-          <span key={index}>{index < 2 ? number : '•'.repeat(number.length)}</span>
+        {displayedCardNumber.map((number, index, array) => (
+          <span key={index}>
+            {index < array.length / 2
+              ? number
+              : SECURITY_TEXT_ICON.repeat(number.length)}
+          </span>
         ))}
       </div>
       <div className={styles.cardMetaInfo}>
-        <span className={styles.cardOwnerName}>{information.ownerName || 'NAME'}</span>
+        <span className={styles.cardOwnerName}>{ownerName || "NAME"}</span>
         <span className={styles.cardExpirationDate}>
-          {information.expirationDate.month || 'MM'} / {information.expirationDate.year || 'YY'}
+          {expirationDate.month || "MM"} / {expirationDate.year || "YY"}
         </span>
       </div>
     </div>
   );
-}
+};
 
-export default CardItem;
+export default memo(CardItem);
