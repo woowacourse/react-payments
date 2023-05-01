aa1ee8f868bb73e23c653fde9594ec52733afc47
nangkyeonglim <nangkyeonglim@gmail.com>
2023-05-01 13:14:27 +0900

    refactor: useAddCardForm custom hook 구현

---
+++ src/hooks/useAddCardForm.ts
@@ -0,0 +1,44 @@
+import { isAlphabet, isNumber, validateMonth, validateYear } from '../utils/validateInput';
+import { SetState } from '../@types';
+
+const useAddCardForm = () => {
+  const onChangeCardDateState =
+    (type: string) =>
+    (setState: SetState, key: string) =>
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      switch (key) {
+        case 'month':
+          validateMonth(e.target);
+          break;
+        case 'year':
+          validateYear(e.target);
+          break;
+        default:
+      }
+      onChangeState('number')(setState, key)(e);
+    };
+
+  const onChangeState =
+    (type: string) =>
+    (setState: SetState, key?: string) =>
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      switch (type) {
+        case 'number':
+        case 'password':
+          if (!isNumber(e.target.value)) return;
+          break;
+        case 'text':
+          if (!isAlphabet(e.target.value)) return;
+          break;
+      }
+      if (key) setState(e.target.value, key);
+      else setState(e.target.value);
+    };
+
+  return {
+    onChangeCardDateState,
+    onChangeState,
+  };
+};
+
+export default useAddCardForm;
