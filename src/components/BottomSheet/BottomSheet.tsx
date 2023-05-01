b3fd2d93210d029ed9c9f95ed0490ab3b9562ddd
cruelladevil <dev.timetravel@gmail.com>
2023-05-01 13:49:34 +0900

    style: BottomSheet 애니메이션 적용

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -20,6 +20,18 @@ const Contents = styled.div`
   background-color: #fff;
   padding: 20px;
   border-radius: 7px 7px 0px 0px;
+
+  transform: translateY(100%);
+  animation: openModalAnimation 0.5s cubic-bezier(0.165, 0.84, 0.44, 1) 0.2s forwards;
+
+  @keyframes openModalAnimation {
+    0% {
+      transform: translateY(100%);
+    }
+    100% {
+      transform: translateY(0%);
+    }
+  }
 `;
 
 const Styled = {
