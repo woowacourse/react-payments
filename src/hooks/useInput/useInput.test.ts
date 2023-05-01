0d866e194b17c219f17b354baf576081912aae48
nlom0218 <nlom0218@naver.com>
2023-05-01 11:50:06 +0900

    test: useInput hook 테스트코드 작성

--- src/hooks/useInput/useInput.test.ts
+++ src/hooks/useInput/useInput.test.ts
@@ -39,17 +39,43 @@ describe('useInput hook 테스트', () => {
   });
 
   it('value 업데이트가 잘 동작하는 확인', () => {
-    const {
-      result: {
-        current: [value, updateValue, errorMessage],
-      },
-    } = renderHook(() => useInput<string>('', numberValidationFns));
+    const { result } = renderHook(() => useInput<string>('', numberValidationFns));
+    const [, updateValue] = result.current;
 
     act(() => {
       updateValue('3');
     });
 
-    // 어떤 이유로 act 함수가 실행되고 있지 않은지 파악하기
+    const [value] = result.current;
+
     expect(value).toBe('3');
+  });
+
+  it('잘못된 value인 경우 value가 업데이트 안되는지 확인', () => {
+    const {
+      result: { current },
+    } = renderHook(() => useInput<string>('', numberValidationFns));
+    const [, updateValue] = current;
+
+    act(() => {
+      updateValue('a');
+    });
+
+    const [value] = current;
+
+    expect(value).toBe('');
+  });
+
+  it('잘못된 value인 경우 오류메시지가 있는 확인', () => {
+    const { result } = renderHook(() => useInput<string>('', numberValidationFns));
+    const [, updateValue] = result.current;
+
+    act(() => {
+      updateValue('a');
+    });
+
+    const [, , errorMessage] = result.current;
+
+    expect(errorMessage).not.toBeFalsy();
   });
 });
