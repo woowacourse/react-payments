1ef26249948028457b512bab1080a2852426af73
AhnGeunTaek <dksrmsxor@gmail.com>
2023-05-01 16:28:02 +0900

    feat: CardExpirationDate 컴포넌트 생성

--- src/components/CardAddForm/CardExpirationDate/CardExpirationDate.tsx
+++ src/components/CardAddForm/CardExpirationDate/CardExpirationDate.tsx
@@ -1,60 +1,70 @@
-import { CardInputValidation, ExpirationDateFormat } from "../../../types";
-import InputContainer from "../../common/InputContainer/InputContainer";
+import { memo } from "react";
+import type { ChangeEvent, FocusEvent } from "react";
+import type { CardFormData, CardFormValidation } from "../../../types";
 import Input from "../../common/Input/Input";
-import { useError } from "../../../hooks/useError";
-import validator from "../../../utils/validator";
-import { formatDisplayedExpirationDate } from "../../../utils/formatter";
-import { ChangeEvent } from "react";
+import InputContainer from "../../common/InputContainer/InputContainer";
+import Label from "../../common/Label/Label";
+import {
+  EXPIRATION_DATE_INPUT_MAX_LENGTH,
+  PATTERN,
+} from "../../../constants/input";
+import {
+  formatDisplayedExpirationDate,
+  formatExpirationDate,
+} from "../../../utils/formatter";
 
 interface CardExpirationDateProps {
-  handleValidationChange: (
-    key: keyof CardInputValidation,
-    value: boolean
+  isError: boolean;
+  updateInputValue: <K extends keyof CardFormData>(
+    key: K,
+    value: CardFormData[K]
   ) => void;
-  onChange: ({ target: { value } }: ChangeEvent<HTMLInputElement>) => void;
-  value: ExpirationDateFormat;
+  updateInputError: <K extends keyof CardFormValidation>(
+    key: K,
+    value: CardFormData[K]
+  ) => void;
 }
 
-function CardExpirationDate({
-  onChange,
-  handleValidationChange,
-  value,
-}: CardExpirationDateProps) {
-  const [isError, onErrorBlur] = useError({
-    validator: validator.expirationDate,
-    handleValidationChange,
-  });
+const CardExpirationDate = ({
+  isError,
+  updateInputValue,
+  updateInputError: updateCardInputError,
+}: CardExpirationDateProps) => {
+  const onChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    target.value = formatDisplayedExpirationDate(target.value);
+    updateInputValue("expirationDate", formatExpirationDate(target.value));
+  };
 
-  const expirationDate = formatDisplayedExpirationDate(
-    `${value.month}${value.year}`
-  );
+  const onBlur = ({ target }: FocusEvent<HTMLInputElement>) => {
+    updateCardInputError("expirationDate", formatExpirationDate(target.value));
+  };
 
   return (
     <InputContainer
-      label="만료일"
-      id="expirationDate"
+      supportingText={{
+        error: "카드에 표시된 유효한 만료일을 (MM/YY) 순서로 입력해주세요",
+      }}
       isError={isError}
-      supportingText={
-        isError
-          ? "카드에 표시된 만료일을 (MM/YY) 순서로 동일하게 입력해주세요"
-          : undefined
-      }
-      required
     >
+      <Label htmlFor="expirationDate" required>
+        만료일
+      </Label>
       <Input
-        type="text"
         id="expirationDate"
-        data-name="expirationDate"
-        value={expirationDate}
-        placeholder="연/년도(MM/YY) 순서로 4자리 숫자를 입력해주세요"
+        name="expirationDate"
+        placeholder="월/년도(MM/YY) 순서로 4자리 숫자를 입력해주세요"
+        minLength={EXPIRATION_DATE_INPUT_MAX_LENGTH}
+        maxLength={EXPIRATION_DATE_INPUT_MAX_LENGTH}
+        autoComplete="cc-exp"
+        inputMode="numeric"
+        pattern={PATTERN.EXPIRATION_DATE}
+        required
         isError={isError}
-        maxLength={5}
-        autoComplete="cc-csc"
         onChange={onChange}
-        onBlur={onErrorBlur}
+        onBlur={onBlur}
       />
     </InputContainer>
   );
-}
+};
 
-export default CardExpirationDate;
+export default memo(CardExpirationDate);
