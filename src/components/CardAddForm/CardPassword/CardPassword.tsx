c187824b825db94ee41f11b77304faadf663e7d2
AhnGeunTaek <dksrmsxor@gmail.com>
2023-05-01 16:44:55 +0900

    feat: CardPassword 컴포넌트 생성

--- src/components/CardAddForm/CardPassword/CardPassword.tsx
+++ src/components/CardAddForm/CardPassword/CardPassword.tsx
@@ -1,54 +1,102 @@
-import styles from './style.module.css';
-import { ChangeEvent } from 'react';
-import InputContainer from '../../common/InputContainer/InputContainer';
-import Input from '../../common/Input/Input';
-import { CardInputValidation, PasswordFormat } from '../../../types';
-import { useError } from '../../../hooks/useError';
-import validator from '../../../utils/validator';
+import { memo, useRef } from "react";
+import type { ChangeEvent, FocusEvent } from "react";
+import type { CardFormData, CardFormValidation } from "../../../types";
+import Input from "../../common/Input/Input";
+import InputContainer from "../../common/InputContainer/InputContainer";
+import Label from "../../common/Label/Label";
+import { SECURITY_TEXT_ICON } from "../../../constants";
+import { PASSWORD_UNIT_MAX_LENGTH, PATTERN } from "../../../constants/input";
+import { formatNumber } from "../../../utils/formatter";
+import styles from "./style.module.css";
 
 interface CardPasswordProps {
-  handleValidationChange: (key: keyof CardInputValidation, value: boolean) => void;
-  onChange: ({ target: { value } }: ChangeEvent<HTMLInputElement>) => void;
-  values: PasswordFormat;
+  isError: boolean;
+  updateInputValue: <K extends keyof CardFormData>(
+    key: K,
+    value: CardFormData[K]
+  ) => void;
+  updateInputError: <K extends keyof CardFormValidation>(
+    key: K,
+    value: CardFormData[K]
+  ) => void;
 }
 
-function CardPassword({ handleValidationChange, onChange, values }: CardPasswordProps) {
-  const [isError, onErrorBlur] = useError({
-    validator: validator.password,
-    handleValidationChange,
-    inputs: values,
-  });
+const CardPassword = ({
+  isError,
+  updateInputValue,
+  updateInputError,
+}: CardPasswordProps) => {
+  const firstInputRef = useRef<HTMLInputElement>(null);
+  const lastInputRef = useRef<HTMLInputElement>(null);
+
+  const onChange = () => {
+    if (!firstInputRef.current || !lastInputRef.current) return;
+
+    firstInputRef.current.value = formatNumber(firstInputRef.current.value);
+    lastInputRef.current.value = formatNumber(lastInputRef.current.value);
+    updateInputValue("password", [
+      firstInputRef.current.value,
+      lastInputRef.current.value,
+    ]);
+  };
+
+  const onFirstInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange();
+
+    if (
+      event.target.value.length === PASSWORD_UNIT_MAX_LENGTH &&
+      lastInputRef.current
+    ) {
+      lastInputRef.current.focus();
+    }
+  };
+
+  const onBlur = (event: FocusEvent<HTMLElement>) => {
+    if (event.currentTarget.contains(event.relatedTarget)) return;
+    if (!firstInputRef.current || !lastInputRef.current) return;
+
+    updateInputError("password", [
+      firstInputRef.current.value,
+      lastInputRef.current.value,
+    ]);
+  };
 
   return (
     <InputContainer
-      label="비밀 번호"
-      id="password"
-      supportingText={isError ? '카드 비밀번호 앞 2자리 숫자를 입력해주세요' : undefined}
+      supportingText={{
+        error: "카드 비밀번호 앞 2자리 숫자를 입력해주세요",
+      }}
       isError={isError}
-      required
     >
-      <div className={styles.container} onBlur={onErrorBlur}>
-        {values.map((password, index) => (
+      <Label htmlFor="password" id="password-label" required>
+        비밀번호
+      </Label>
+      <div className={styles.container} onBlur={onBlur}>
+        {Array.from({ length: 2 }, (_, index) => (
           <Input
+            ref={index === 0 ? firstInputRef : lastInputRef}
             key={index}
             type="password"
-            id={index === 0 ? 'password' : `password${index}`}
-            value={password}
-            maxLength={1}
-            isError={isError}
-            autoComplete="off"
+            id={index === 0 ? "password" : `password${index}`}
+            name="password"
             data-index={index}
-            data-name="password"
-            onChange={onChange}
+            maxLength={PASSWORD_UNIT_MAX_LENGTH}
+            autoComplete="off"
+            inputMode="numeric"
+            pattern={PATTERN.PASSWORD}
+            required
+            aria-labelledby={index === 0 ? undefined : "password-label"}
+            isError={isError}
+            onChange={index === 0 ? onFirstInputChange : onChange}
           />
         ))}
         <div className={styles.passwordPlaceholder}>
-          <div className="passwordItem">•</div>
-          <div className="passwordItem">•</div>
+          <p className="passwordItem">{SECURITY_TEXT_ICON}</p>
+          <p className="passwordItem">{SECURITY_TEXT_ICON}</p>
         </div>
       </div>
     </InputContainer>
   );
-}
+};
 
-export default CardPassword;
+export default memo(CardPassword);
