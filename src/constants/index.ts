17fb0c632f5f3a0a79f7b375cae1460fbaa46af3
AhnGeunTaek <dksrmsxor@gmail.com>
2023-05-01 14:41:32 +0900

    refactor: 정규표현식 상수화 및 formatChecker 객체 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,11 @@
-const LOCAL_STORAGE_KEY = 'cardList';
+const LOCAL_STORAGE_KEY = "cardList";
 
-export { LOCAL_STORAGE_KEY };
+const SECURITY_TEXT_ICON = "•";
+
+const REGEX = {
+  ONLY_NUMBER: /^\d*$/,
+  ENGLISH_AND_WHITESPACE: /^[a-zA-Z ]*$/,
+  DATE_FORMAT: /^(?:\d{0,2}\/?\d{0,2})?$/,
+};
+
+export { LOCAL_STORAGE_KEY, SECURITY_TEXT_ICON, REGEX };
