8034da9d5d0a8b6ce0fd5ef58efebd2847d7def4
AhnGeunTaek <dksrmsxor@gmail.com>
2023-05-01 15:41:42 +0900

    refactor: 카드 번호 입력 필드 1개로 변경

--- src/components/CardAddForm/CardNumber/CardNumber.tsx
+++ src/components/CardAddForm/CardNumber/CardNumber.tsx
@@ -1,22 +1,30 @@
-import styles from './style.module.css';
-import { ChangeEvent } from 'react';
-import { CardInputValidation, CardNumberFormat } from '../../../types';
-import InputContainer from '../../common/InputContainer/InputContainer';
-import Input from '../../common/Input/Input';
-import { useError } from '../../../hooks/useError';
-import validator from '../../../utils/validator';
+import { ChangeEvent } from "react";
+import { CardInputValidation } from "../../../types";
+import { CARD_NUMBER_INPUT_MAX_LENGTH } from "../../../constants";
+import InputContainer from "../../common/InputContainer/InputContainer";
+import Input from "../../common/Input/Input";
+import { useError } from "../../../hooks/useError";
+import validator from "../../../utils/validator";
 
 interface CardNumberProps {
-  handleValidationChange: (key: keyof CardInputValidation, value: boolean) => void;
-  onChange: ({ target: { value, dataset } }: ChangeEvent<HTMLInputElement>) => void;
-  values: CardNumberFormat;
+  handleValidationChange: (
+    key: keyof CardInputValidation,
+    value: boolean
+  ) => void;
+  onChange: ({
+    target: { value, dataset },
+  }: ChangeEvent<HTMLInputElement>) => void;
+  value: string;
 }
 
-function CardNumber({ handleValidationChange, onChange, values }: CardNumberProps) {
+function CardNumber({
+  handleValidationChange,
+  onChange,
+  value,
+}: CardNumberProps) {
   const [isError, onErrorBlur] = useError({
     validator: validator.cardNumber,
     handleValidationChange,
-    inputs: values,
   });
 
   return (
@@ -24,29 +32,23 @@ function CardNumber({ handleValidationChange, onChange, values }: CardNumberProp
       label="카드 번호"
       id="cardNumber"
       isError={isError}
-      supportingText={isError ? '카드에 표시된 16자리 숫자와 동일하게 입력해주세요' : undefined}
+      supportingText={
+        isError
+          ? "카드에 표시된 16자리 숫자와 동일하게 입력해주세요"
+          : undefined
+      }
       required
     >
-      <div
-        className={`${styles.container} ${isError ? styles.error : ''}`}
-        data-name="cardNumber"
+      <Input
+        id="cardNumber"
+        name="cardNumber"
+        value={value}
+        maxLength={CARD_NUMBER_INPUT_MAX_LENGTH}
+        autoComplete="cc-csc"
+        isError={isError}
+        onChange={onChange}
         onBlur={onErrorBlur}
-        tabIndex={0}
-      >
-        {values.map((number, index) => (
-          <Input
-            key={index}
-            type={index < 2 ? 'text' : 'password'}
-            id={index === 0 ? 'cardNumber' : `cardNumber${index}`}
-            value={number}
-            maxLength={4}
-            autoComplete="cc-csc"
-            data-index={index}
-            data-name="cardNumber"
-            onChange={onChange}
-          />
-        ))}
-      </div>
+      />
     </InputContainer>
   );
 }
