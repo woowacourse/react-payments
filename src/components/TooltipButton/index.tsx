f10f709544835e84e3e02bc66ee976ab156eb71d
Suyoung <jsy8492@gmail.com>
2023-05-01 17:02:08 +0900

    refactor: TooltipButton 컴포넌트의 aria-label을 역할이 잘 드러나도록 수정

--- src/components/TooltipButton/index.tsx
+++ src/components/TooltipButton/index.tsx
@@ -2,7 +2,11 @@ import styles from './tooltipButton.module.css';
 
 const TooltipButton = () => {
   return (
-    <button type="button" className={styles.button} aria-label="CVC 툴팁 버튼">
+    <button
+      type="button"
+      className={styles.button}
+      aria-label="보안 코드(CVC/CVV)에 대한 상세 설명 툴팁"
+    >
       <span className={styles.mark}>?</span>
     </button>
   );
