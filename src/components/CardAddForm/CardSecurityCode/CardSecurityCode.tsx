e6f7e7685e5b6011a087b0bb9fcd703d01490148
AhnGeunTaek <dksrmsxor@gmail.com>
2023-05-01 16:46:44 +0900

    feat: CardSecurityCode 컴포넌트 생성

--- src/components/CardAddForm/CardSecurityCode/CardSecurityCode.tsx
+++ src/components/CardAddForm/CardSecurityCode/CardSecurityCode.tsx
@@ -1,48 +1,70 @@
-import { ChangeEvent } from 'react';
-import { CardInputValidation } from '../../../types';
-import Input from '../../common/Input/Input';
-import InputContainer from '../../common/InputContainer/InputContainer';
-import { useError } from '../../../hooks/useError';
-import validator from '../../../utils/validator';
+import { memo } from "react";
+import type { ChangeEvent, FocusEvent } from "react";
+import type { CardFormData, CardFormValidation } from "../../../types";
+import Input from "../../common/Input/Input";
+import InputContainer from "../../common/InputContainer/InputContainer";
+import Label from "../../common/Label/Label";
+import {
+  PATTERN,
+  SECURITY_CODE_MAX_LENGTH,
+  SECURITY_CODE_MIN_LENGTH,
+} from "../../../constants/input";
+import { formatNumber } from "../../../utils/formatter";
 
 interface CardSecurityCodeProps {
-  handleValidationChange: (key: keyof CardInputValidation, value: boolean) => void;
-  onChange: ({ target: { value } }: ChangeEvent<HTMLInputElement>) => void;
-  value: string;
+  isError: boolean;
+  updateInputValue: <K extends keyof CardFormData>(
+    key: K,
+    value: CardFormData[K]
+  ) => void;
+  updateInputError: <K extends keyof CardFormValidation>(
+    key: K,
+    value: CardFormData[K]
+  ) => void;
 }
 
-function CardSecurityCode({ handleValidationChange, onChange, value }: CardSecurityCodeProps) {
-  const [isError, onErrorBlur] = useError({
-    validator: validator.securityCode,
-    handleValidationChange,
-  });
+const CardSecurityCode = ({
+  isError,
+  updateInputValue,
+  updateInputError,
+}: CardSecurityCodeProps) => {
+  const onChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    target.value = formatNumber(target.value);
+    updateInputValue("securityCode", target.value);
+  };
+
+  const onBlur = (event: FocusEvent<HTMLInputElement>) => {
+    updateInputError("securityCode", event.target.value);
+  };
 
   return (
     <InputContainer
-      label="보안 코드 (CVC/CVV)"
-      id="securityCode"
-      supportingText={
-        isError
-          ? '카드에 표시된 CVC/CVV 번호와 동일하게 입력해주세요'
-          : '카드 뒷면의 CVC 번호 3자리 숫자를 입력해주세요'
-      }
+      supportingText={{
+        default:
+          "카드 뒷면의 CVC/CVV 번호 3자리 또는 4자리 숫자를 입력해주세요",
+        error: "카드에 표시된 CVC/CVV 번호와 동일하게 입력해주세요",
+      }}
       isError={isError}
-      required
     >
+      <Label htmlFor="securityCode" required>
+        보안 코드 (CVC/CVV)
+      </Label>
       <Input
         type="password"
         id="securityCode"
-        data-name="securityCode"
-        value={value}
-        minLength={3}
-        maxLength={4}
+        name="securityCode"
+        minLength={SECURITY_CODE_MIN_LENGTH}
+        maxLength={SECURITY_CODE_MAX_LENGTH}
+        autoComplete="cc-csc"
+        inputMode="numeric"
+        pattern={PATTERN.SECURITY_CODE}
+        required
         isError={isError}
-        autoComplete="off"
         onChange={onChange}
-        onBlur={onErrorBlur}
+        onBlur={onBlur}
       />
     </InputContainer>
   );
-}
+};
 
-export default CardSecurityCode;
+export default memo(CardSecurityCode);
