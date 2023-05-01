1c43502fe6d0d78569d9685813713948654e4472
n0eyes <doll4586633@naver.com>
2023-05-01 14:34:41 +0900

    css: Layout width > max-width로 변경

--- src/components/@common/Layout/Layout.tsx
+++ src/components/@common/Layout/Layout.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import Header from "../Header/Header";
-import * as Styled from "./Layout.styles";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../Header/Header';
+import * as Styled from './Layout.styles';
 
 export default function Layout() {
   return (
