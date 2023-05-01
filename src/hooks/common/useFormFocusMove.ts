715e7751dbed807de713b822f2ee339daa72ea29
ashleysyheo <ashleysyheo@gmail.com>
2023-05-01 18:35:18 +0900

    refactor: useFormFocus 수정

--- src/hooks/common/useFormFocusMove.ts
+++ src/hooks/common/useFormFocusMove.ts
@@ -1,12 +1,25 @@
-import type { MutableRefObject } from 'react';
+import { useCallback } from 'react';
+import type { ChangeEvent } from 'react';
 
-const useFormFocus = (inputRefs: MutableRefObject<(HTMLButtonElement | HTMLInputElement)[]>) => {
-  const moveFocus = (index: number, value: string, maxLength?: number) => {
-    if (maxLength && value.length !== maxLength) return;
-    if (value === '') return;
+const useFormFocus = () => {
+  const moveFocus = useCallback((event: ChangeEvent<HTMLFormElement>) => {
+    const inputElements = Array.from(event.currentTarget.elements).filter(
+      (element): element is HTMLButtonElement | HTMLInputElement =>
+        element.tagName === 'BUTTON' || element.tagName === 'INPUT'
+    );
 
-    inputRefs.current[index].focus();
-  };
+    const { value, maxLength } = event.target;
+
+    const focusedElement = inputElements.find((element) => element === document.activeElement);
+
+    if (focusedElement instanceof HTMLButtonElement && value === '') return;
+    if (focusedElement instanceof HTMLInputElement && value.length !== maxLength) return;
+
+    const nextElement = inputElements.find(
+      (_, index) => inputElements[index - 1] === focusedElement
+    );
+    nextElement?.focus();
+  }, []);
 
   return { moveFocus };
 };
