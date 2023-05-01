5de7009f16b98bd2d836d1c06491f7e61b7f36d7
AhnGeunTaek <dksrmsxor@gmail.com>
2023-05-01 16:43:46 +0900

    feat: CardOwnerName 컴포넌트 생성

--- src/components/CardAddForm/CardOwnerName/CardOwnerName.tsx
+++ src/components/CardAddForm/CardOwnerName/CardOwnerName.tsx
@@ -1,22 +1,26 @@
-import { ChangeEvent } from 'react';
-import InputContainer from '../../common/InputContainer/InputContainer';
-import Input from '../../common/Input/Input';
+import { ChangeEvent } from "react";
+import InputContainer from "../../common/InputContainer/InputContainer";
+import Input from "../../common/Input/Input";
 
 interface CardOwnerNameProps {
-  onChange: ({ target: { value, dataset } }: ChangeEvent<HTMLInputElement>) => void;
+  onInputChange: (event: ChangeEvent<HTMLInputElement>) => void;
   value?: string;
 }
 
-function CardOwnerName({ onChange, value = '' }: CardOwnerNameProps) {
+function CardOwnerName({ onInputChange, value = "" }: CardOwnerNameProps) {
   return (
-    <InputContainer label="카드 소유자 이름" id="ownerName" characterCounter={[value.length, 20]}>
+    <InputContainer
+      label="카드 소유자 이름"
+      id="ownerName"
+      characterCounter={[value.length, 20]}
+    >
       <Input
         id="ownerName"
         data-name="ownerName"
         value={value}
         placeholder="카드에 표시된 이름과 동일하게 입력해주세요"
         maxLength={20}
-        onChange={onChange}
+        onChange={onInputChange}
         autoComplete="cc-csc"
       />
     </InputContainer>
