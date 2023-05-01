160cafaff443b4b1d747659c95b83d45c7af3a34
ashleysyheo <ashleysyheo@gmail.com>
2023-05-01 18:59:01 +0900

    refactor: 카드 번호 인풋 패턴 오류 수정

--- src/constants/input.ts
+++ src/constants/input.ts
@@ -17,7 +17,7 @@ const PASSWORD_UNIT_MAX_LENGTH = 1;
 const CARD_NAME_INPUT_MAX_LENGTH = 20;
 
 const PATTERN = {
-  CARD_NUMBER: '^d{4} d{4} •{4} •{4}$',
+  CARD_NUMBER: '^\\d{4} \\d{4} •{4} •{4}$',
   EXPIRATION_DATE: '^(0[1-9]|1[0-2])/(2[3-8]|[4-9][0-9])$',
   SECURITY_CODE: '^\\d{3,4}$',
   PASSWORD: '^\\d$',
