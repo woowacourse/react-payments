f6c17647b513e82dd863511ba176fb82fdad1493
n0eyes <doll4586633@naver.com>
2023-05-01 15:20:54 +0900

    feat: Routes 경로 상수화

---
+++ src/constants/routes.ts
@@ -0,0 +1,12 @@
+export const ROUTES = {
+  MY_CARD_LIST: '/',
+  REGISTER_CARD: '/registerCard',
+  ALIAS: '/alias',
+  PREV: -1,
+};
+
+export const HEADER = {
+  '/': '보유카드',
+  '/registerCard': '카드 추가',
+  '/alias': '',
+};
