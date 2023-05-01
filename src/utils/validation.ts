7a819efd0421708c46d0ea0cc47b08dec7fac2df
Suyoung <jsy8492@gmail.com>
2023-05-01 11:56:07 +0900

    refactor: 숫자가 아님을 검증하는 로직을 정규식을 사용하도록 수정

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,4 +1,3 @@
-export const isNotNumber = (value: string) =>
-  Number.isNaN(Number(value)) || value.includes(' ') || value.includes('.');
+export const isNotNumber = (value: string) => !/^[0-9]+$/.test(value);
 
 export const isNotAlphabet = (value: string) => !/^[a-zA-Z\s]*$/.test(value);
