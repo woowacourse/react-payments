4712c879b5e9285d1827813e3ad967b81615eeae
AhnGeunTaek <dksrmsxor@gmail.com>
2023-05-01 14:19:13 +0900

    refactor: 로컬스토리지 로직 변경과 로컬스토리지 hook 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import { useEffect, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { Card } from './types';
-import CardListPage from './pages/CardListPage';
-import CardAddPage from './pages/CardAddPage';
-import { useLocalStorage } from './hooks/useLocalStorage';
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Card } from "./types";
+import CardListPage from "./pages/CardListPage";
+import CardAddPage from "./pages/CardAddPage";
+import { getLocalStorage, saveToLocalStorage } from "./utils/localStorage";
 
 function App() {
-  const { data, setDataBeforeUnload } = useLocalStorage<Card>([]);
-  const [cardList, setCardList] = useState<Card[]>(data);
+  const [cardList, setCardList] = useState<Card[]>(getLocalStorage() ?? []);
 
   useEffect(() => {
-    setDataBeforeUnload(cardList);
-  }, [cardList, setDataBeforeUnload]);
+    saveToLocalStorage(cardList);
+  }, [cardList]);
 
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<CardListPage cardList={cardList} />} />
-        <Route path="/add-card" element={<CardAddPage addCard={setCardList} />} />
+        <Route
+          path="/add-card"
+          element={<CardAddPage addCard={setCardList} />}
+        />
       </Routes>
     </div>
   );
