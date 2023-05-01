70f232ee3ca2c7a37a25e685700f2ca9ac5d158d
TaeeunKim <xodms0309@naver.com>
2023-05-01 20:29:31 +0900

    fix: 카드 리스트에 key값 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import Layout from '../components/Layout/Layout';
 import { CardContext } from '../context/CardProvider';
 import { CardType } from '../types/Card';
 
-const Wrapper = styled.div`
+const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -20,11 +20,18 @@ const EmptyCardText = styled.p`
   margin-bottom: 8px;
 `;
 
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 24px;
+`;
+
 const Home = () => {
   const { cards } = useContext(CardContext);
   return (
     <Layout>
-      <Wrapper>
+      <CardContainer>
         {cards.length === 0 && (
           <EmptyCardText>새로운 카드를 등록해주세요.</EmptyCardText>
         )}
@@ -37,20 +44,19 @@ const Home = () => {
             cardCompany,
             nickname,
           }: CardType) => (
-            <>
+            <CardWrapper key={id}>
               <Card
-                key={id}
                 cardNumbers={cardNumbers}
                 expiredDate={expiredDate}
                 cardOwnerName={cardOwnerName}
                 cardCompany={cardCompany}
               />
               <p>{nickname}</p>
-            </>
+            </CardWrapper>
           )
         )}
         <AddCardButton></AddCardButton>
-      </Wrapper>
+      </CardContainer>
     </Layout>
   );
 };
