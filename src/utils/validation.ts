3cf3c48d1adcf4983a4c139ed76b774ba6bfc18d
n0eyes <doll4586633@naver.com>
2023-05-01 14:36:30 +0900

    feat: validation 추가

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,5 +1,5 @@
 export const REGEX = {
-  NUMBER: '^[0-9]+',
+  NUMBER: '^[0-9]+$',
   KOREAN: '^[ㄱ-ㅎ가-힣a-zA-Z]+$',
   ENGLISH: '^[a-zA-Z]+$',
   THREE_DIGIT: '^[0-9]{3}$',
@@ -12,4 +12,10 @@ export const isNumber = (value: string) => new RegExp(REGEX.NUMBER).test(value);
 
 export const isEnglish = (value: string) => new RegExp(REGEX.ENGLISH).test(value);
 
+export const isKorean = (value: string) => new RegExp(REGEX.KOREAN).test(value);
+
 export const isPatternMatch = (value: string, pattern: string) => new RegExp(pattern).test(value);
+
+export const isShorterThan = (maxLength: number) => (value: string) => value.length <= maxLength;
+
+export const isLongerThan = (maxLength: number) => (value: string) => value.length > maxLength;
