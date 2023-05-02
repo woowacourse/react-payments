13c42451dc5b3e1642e2e51d115216392721d379
semnil5202 <semnil5202@gmail.com>
2023-05-02 16:37:54 +0900

    refactor: 카드 만료 일자 월, 연도 입력 검증 수정

--- src/hooks/useExpiredDates.ts
+++ src/hooks/useExpiredDates.ts
@@ -1,11 +1,20 @@
 import { useState } from 'react';
 import { REG_EXP } from '../constants/regexp';
 
+const nowYear = new Date().getFullYear() % 2000;
+
 const useExpiredDates = () => {
   const [expiredDates, setExpiredDates] = useState<Array<string>>(['', '']);
 
   const isSetExpiredDates = (order: number, value: string) => {
+    const monthValue = order === 0 && value.length === 2;
+    const yearValue = order === 1 && value.length === 2;
+
     if (REG_EXP.cardNumberLimit.test(value)) return false;
+
+    if (monthValue && !REG_EXP.cardMonthLimit.test(value)) return false;
+
+    if (yearValue && Number(value) < nowYear) return false;
 
     setExpiredDates({ ...expiredDates, [order]: value });
     return true;
