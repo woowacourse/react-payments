593cdd68a041b0d55982cf56c57c8371282b8568
inyeong-kang <tomatorkd@naver.com>
2023-05-02 18:47:34 +0900

    test: 모바일 기준 viewport 설정

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,18 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    viewports: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewport: 'iphone6',
     },
   },
 };
