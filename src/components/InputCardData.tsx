b1aa2f20ff82935de90748b05a28c7bd49340634
dladncks1217 <dlaxodud1217@gmail.com>
2023-05-02 19:51:33 +0900

    refactor: reducer type 지정 관련 리팩토링

--- src/components/InputCardData.tsx
+++ src/components/InputCardData.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { InputProps } from '../type';
 import './InputCardData.css';
 
--- src/reducer/cardReducer.ts
+++ src/reducer/cardReducer.ts
@@ -1,9 +1,10 @@
 import { CARDLIST_FAILURE, CARDLIST_SUCCESS } from '../actions/cardDataAction';
-import { CardType } from '../type';
+import { CardType, actionName } from '../type';
 
 export const initialState: CardType[] = [];
+type Action = { type: actionName; card: CardType[]; errorMessage: string };
 
-const cardReducer = (state: CardType[], action: any) => {
+const cardReducer = (state: CardType[], action: Action) => {
   switch (action.type) {
     case CARDLIST_SUCCESS:
       return [...state, ...action.card];
