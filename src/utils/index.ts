d0e01110645d2f6214b679d04c896f35bc9b5177
kangyeongmin <kym6050@hanyang.ac.kr>
2023-05-02 15:06:32 +0900

    refactor: export 코드 단축

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,3 @@
-export { getLocalStorage, setLocalStorage } from "../utils/localStorage";
-export {
-  getReplacedCardNumber,
-  getSeperatedCardNumber,
-  getSeperatedExpiredDate,
-  getSubCardNumber,
-  getSubExpiredDate,
-  getEmptyCard,
-} from "../utils/card";
-
-export {
-  validateExpiredDate,
-  validateNumber,
-  validateOwnerName,
-} from "../utils/validation";
+export * from "../utils/localStorage";
+export * from "../utils/card";
+export * from "../utils/validation";
