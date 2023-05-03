3282deac2336ddb591678a3685c2bc38ad34c4bd
regularPark <srk99279@naver.com>
2023-05-03 20:22:56 +0900

    refactor:  `ModalProvider` 제거 및 모달 open시 스크롤 차단

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,21 @@ import RegisterCard from 'pages/RegisterCard';
 import NotFound from 'pages/NotFound';
 import RegisterCardName from 'pages/RegisterCardName';
 import { CardInfoProvider } from 'context/CardInfoContext';
-import { ModalProvider } from 'context/ModalContext';
 
 function App() {
   return (
     <>
-      <ModalProvider>
-        <CardInfoProvider>
-          <GlobalStyle />
-          <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/register" element={<RegisterCard />} />
-              <Route path="/register-name" element={<RegisterCardName />} />
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </CardInfoProvider>
-      </ModalProvider>
+      <CardInfoProvider>
+        <GlobalStyle />
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<RegisterCard />} />
+            <Route path="/register-name" element={<RegisterCardName />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CardInfoProvider>
     </>
   );
 }
