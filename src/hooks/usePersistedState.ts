aaed028a0a124929293fa9e95404b40b2c0e3226
solo5star <solo_5@naver.com>
2023-05-04 01:49:07 +0900

    feat: usePersistedState에서 영속성 계층 추상화

--- src/hooks/usePersistedState.ts
+++ src/hooks/usePersistedState.ts
@@ -1,14 +1,28 @@
 import { useEffect, useMemo, useState } from 'react';
 
-export const usePersistedState = <S = undefined>(key: string, initialState: S) => {
-  const hydratedState = useMemo(() => localStorage.getItem(key), [key]);
+type Persistor = {
+  save: (key: string, value: string) => void;
+  load: (key: string) => string | null;
+};
+
+const localStoragePersistor: Persistor = {
+  save: (key, value) => localStorage.setItem(key, value),
+  load: (key) => localStorage.getItem(key),
+};
+
+export const usePersistedState = <S = undefined>(
+  key: string,
+  initialState: S,
+  persistor: Persistor = localStoragePersistor,
+) => {
+  const hydratedState = useMemo(() => persistor.load(key), [persistor, key]);
   const dehydratedState = hydratedState === null ? initialState : JSON.parse(hydratedState);
 
   const [internalState, internalSetState] = useState<S>(dehydratedState);
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(internalState));
-  }, [key, internalState]);
+    persistor.save(key, JSON.stringify(internalState));
+  }, [key, internalState, persistor]);
 
   return [internalState, internalSetState] as const;
 };
