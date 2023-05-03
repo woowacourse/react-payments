e68d08de48d6269d0c19b6e2167490e1288f5fb4
unknown <tkdrb15@gmail.com>
2023-05-03 15:22:52 +0900

    refactor: 카드 colormap객체 상수 파일로 이동

--- src/components/cardList/CardItem.tsx
+++ src/components/cardList/CardItem.tsx
@@ -11,30 +11,6 @@ interface CardProps {
   handleClick?: (e: React.MouseEvent<HTMLElement> | React.TouchEvent<HTMLElement>) => void;
 }
 
-const backgroundColorMap: Record<BrandType | typeof DEFAULT_BRAND, string> = {
-  현대카드: "#000000",
-  BC카드: "#fc6d62",
-  신한카드: "#4a7cff",
-  카카오뱅크: "#ffed45",
-  하나카드: "#1db8b3",
-  우리카드: "#1994e0",
-  KB카드: "#8c8176",
-  롯데카드: "#f25769",
-  [DEFAULT_BRAND]: "#000000",
-} as const;
-
-const fontColorMap: Record<BrandType | typeof DEFAULT_BRAND, string> = {
-  현대카드: "#FFFFFF",
-  BC카드: "#FFFFFF",
-  신한카드: "#FFFFFF",
-  카카오뱅크: "#786b5f",
-  하나카드: "#FFFFFF",
-  우리카드: "#FFFFFF",
-  KB카드: "#FFFFFF",
-  롯데카드: "#FFFFFF",
-  [DEFAULT_BRAND]: "#FFFFFF",
-} as const;
-
 const CardItem = ({ card, handleClick }: CardProps) => {
   const { numbers, expiryDate, owner = DEFAULT_NAME, brand = DEFAULT_BRAND } = card;
 
