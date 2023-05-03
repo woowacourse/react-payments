03ddc67126238a79751a6ef2d7b4f9578e058389
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-04 00:14:32 +0900

    fix: 만료일 연도 유효성 검사 regex 수정

--- src/utils/regexp.ts
+++ src/utils/regexp.ts
@@ -1,6 +1,6 @@
 export const VALID_CARD_NUMBER_REGEX = /^\d{16}$/;
 export const VALID_EXPIRATION_MONTH_REGEX = /^0[1-9]|1[0-2]$/;
-export const VALID_EXPIRATION_YEAR_REGEX = /^(2[3-9]|[3-9][0-9])$/;
+export const VALID_EXPIRATION_YEAR_REGEX = /^(\d{2})$/;
 export const VALID_SECURITY_CODE_REGEX = /^\d{3}$/;
 export const VALID_PASSWORD_REGEX = /^\d{2}$/;
 
