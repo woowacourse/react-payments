97e140b5e65e867a980f404cf3a34b6c27839ca9
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-05-03 15:30:12 +0900

    refactor: CompanyLogoItem 타입 선언 위치 수정

--- src/components/CompanyLogoItem/CompanyLogoItem.tsx
+++ src/components/CompanyLogoItem/CompanyLogoItem.tsx
@@ -2,13 +2,15 @@ import { ReactNode } from 'react';
 
 import * as styled from './CompanyLogoItem.styled';
 
-interface CompanyLogoItemProps {
+const CompanyLogoItem = ({
+  SvgLogo,
+  name,
+  onClick,
+}: {
   SvgLogo: ReactNode;
   name: string;
   onClick: () => void;
-}
-
-const CompanyLogoItem = ({ SvgLogo, name, onClick }: CompanyLogoItemProps) => {
+}) => {
   return (
     <styled.CompanyLogoItem onClick={onClick}>
       {SvgLogo}
