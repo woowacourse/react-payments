191d8fdadd933c702aaf2c5c28503575a2561fec
2yunseong <dbsdltjd3701@naver.com>
2023-05-03 13:17:49 +0900

    feat: createPortal 사용

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -2,16 +2,20 @@ import { PropsWithChildren } from 'react';
 
 import './BottomSheet.css';
 import { BottomSheetProps } from '../type';
+import { createPortal } from 'react-dom';
 
 const BottomSheet = ({ children, isOpen, onToggleOpen }: PropsWithChildren<BottomSheetProps>) => {
   return (
     <>
-      {isOpen && (
-        <div className="bottom-sheet">
-          <div className="back-drop" onClick={onToggleOpen}></div>
-          <div className="bottom-sheet-body">{children}</div>
-        </div>
-      )}
+      {isOpen &&
+        createPortal(
+          <div className="bottom-sheet">
+            <div className="back-drop" onClick={onToggleOpen}></div>
+            <div className="bottom-sheet-body">{children}</div>
+          </div>,
+          document.body
+        )}
+      )
     </>
   );
 };
