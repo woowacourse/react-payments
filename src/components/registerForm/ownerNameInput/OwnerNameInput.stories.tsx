36f77970eb2b5fe24f5946803c6a167e6470ab02
hozzijeong <ghwls0905@naver.com>
2023-05-03 20:44:31 +0900

    test: ownername 인터렉션 구현

--- src/components/registerForm/ownerNameInput/OwnerNameInput.stories.tsx
+++ src/components/registerForm/ownerNameInput/OwnerNameInput.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from "@storybook/react";
 import React from "react";
 import OwnerNameInput from ".";
 import { CardInfoProvider } from "src/context/CardInfoContext";
+import { userEvent, within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
 
 const ownerNameInput = {
   component: OwnerNameInput,
@@ -23,6 +25,46 @@ export default ownerNameInput;
 
 type Story = StoryObj<typeof ownerNameInput>;
 
-export const Example = {
+export const Default = {
   args: {},
 } satisfies Story;
+
+export const Success: Story = {
+  render: () => {
+    return <OwnerNameInput />;
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const ownerNameInput = canvas.queryByPlaceholderText(
+      "영어와 띄어쓰기만 입력 가능합니다.",
+    ) as HTMLInputElement;
+
+    userEvent.clear(ownerNameInput);
+
+    await userEvent.type(ownerNameInput, "clean", { delay: 200 });
+    expect(ownerNameInput).toHaveValue("CLEAN");
+  },
+};
+
+export const Fail: Story = {
+  render: () => {
+    return <OwnerNameInput />;
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const ownerNameInput = canvas.queryByPlaceholderText(
+      "영어와 띄어쓰기만 입력 가능합니다.",
+    ) as HTMLInputElement;
+
+    userEvent.clear(ownerNameInput);
+
+    await userEvent.type(ownerNameInput, "CL", { delay: 200 });
+    expect(
+      canvas.queryByText("카드 소유자 이름은 3글자 이상 30글자 이하입니다."),
+    ).toBeInTheDocument();
+  },
+};
