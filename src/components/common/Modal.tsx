ecd7fc34efca8e389a61774035b8bfb544e0d8b2
Dahye <06robin11@gmail.com>
2023-05-03 14:25:53 +0900

    refactor(modal):모달 컴포넌트 분리

---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,40 @@
+import { PropsWithChildren } from "react";
+import styled from "styled-components";
+
+interface ModalProps {
+  closeModal?: () => void;
+}
+
+export const Modal = ({
+  children,
+  closeModal,
+}: PropsWithChildren<ModalProps>) => {
+  return (
+    <>
+      <BackDrop onClick={closeModal} />
+      <ModalContainer>{children}</ModalContainer>
+    </>
+  );
+};
+
+const BackDrop = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.35);
+`;
+
+const ModalContainer = styled.div`
+  position: fixed;
+  bottom: 0;
+  height: 33%;
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+
+  border-radius: 5px 5px 0 0;
+  background-color: white;
+`;
--- src/components/companySelectModal/index.tsx
+++ src/components/companySelectModal/index.tsx
@@ -3,6 +3,7 @@ import { CompanyIcon } from "./CompanyIcon";
 import { CARD_COMPANIES } from "../../constant/cardCompany";
 import { CardType } from "../../types/card";
 import { Company } from "../../types/company";
+import { Modal } from "../common/Modal";
 
 interface CompanySelectModalProps {
   setCardInfo: React.Dispatch<React.SetStateAction<CardType>>;
@@ -39,28 +40,6 @@ export const CompanySelectModal = ({
       </ModalContainer>
     </>
   );
-};
-
-const BackDrop = styled.div`
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  background: rgba(0, 0, 0, 0.35);
-`;
-
-const ModalContainer = styled.div`
-  position: fixed;
-  bottom: 0;
-  height: 33%;
-  width: 100%;
-
-  display: flex;
-  align-items: center;
-
-  border-radius: 5px 5px 0 0;
-  background-color: white;
 `;
 
 const IconWrapper = styled.div`
