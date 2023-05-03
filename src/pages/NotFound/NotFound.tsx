2d74f6bcea49a6d91e63fca353f4c88d0c96c5e7
wzrabbit <curious.wzrabbit@gmail.com>
2023-05-03 17:09:38 +0900

    ✨ feat: NotFound 페이지 사용자 친화적으로 개선 / 레이아웃 개편

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,16 @@
-import notFound from '../../images/notFoundCat.png';
+import styles from './NotFound.module.css';
+import thinkingFace from '../../images/thinking_face.png';
+import BackButton from '../../components/common/BackButton/BackButton';
 
 const NotFound = () => {
-  return <img src={notFound} alt="notFound" />;
+  return (
+    <div className={styles.container}>
+      <img className={styles['thinking-face']} src={thinkingFace} alt="페이지를 찾을 수 없습니다" />
+      <h2>죄송합니다. 원하시는 페이지를 찾지 못했어요.</h2>
+      <p>주소가 올바른지 다시 한 번 확인해 주시겠어요? 아니면, 메인 페이지로 돌아가실 수도 있어요.</p>
+      <BackButton href="/">메인 페이지로</BackButton>
+    </div>
+  );
 };
 
 export default NotFound;
