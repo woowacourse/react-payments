d6e0af580d3d5d07071333cd1ea2a803acb22806
ashleysyheo <ashleysyheo@gmail.com>
2023-05-04 09:34:50 +0900

    refactor: 만료일 인풋 패턴 수정

--- src/constants/input.ts
+++ src/constants/input.ts
@@ -18,7 +18,7 @@ const CARD_NAME_INPUT_MAX_LENGTH = 20;
 
 const PATTERN = {
   CARD_NUMBER: '^\\d{4} \\d{4} •{4} •{4}$',
-  EXPIRATION_DATE: '^(0[1-9]|1[0-2])/(2[3-8]|[4-9][0-9])$',
+  EXPIRATION_DATE: '^(0[1-9]|1[0-2])\\/(2[1-9]|3[1-9])$',
   SECURITY_CODE: '^\\d{3,4}$',
   PASSWORD: '^\\d$',
 };
