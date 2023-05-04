48ea90aa5c52559fdfc65487f9b5aebe12410bb6
ashleysyheo <ashleysyheo@gmail.com>
2023-05-04 14:51:01 +0900

    refactor: Modal 스토리 수정

--- src/stories/commmon/Modal.stories.tsx
+++ src/stories/commmon/Modal.stories.tsx
@@ -18,11 +18,7 @@ export default meta;
 type Story = StoryObj<typeof Modal>;
 
 export const Default: Story = {
-  args: {
-    children: <div>Modal Content</div>,
-  },
-
-  render: ({ children }) => {
+  render: () => {
     const { isModalOpen, openModal } = useModalContext();
 
     return (
@@ -30,7 +26,31 @@ export const Default: Story = {
         <Button variant="default" onClick={openModal}>
           Open
         </Button>
-        {isModalOpen && <Modal>{children}</Modal>}
+        {isModalOpen && (
+          <Modal>
+            <div
+              style={{
+                position: 'relative',
+                left: '50%',
+                maxWidth: '500px',
+                transform: 'translateX(-50%)',
+              }}
+            >
+              <h2>Anna Karenina</h2>
+              <br />
+              <p>
+                Happy families are all alike; every unhappy family is unhappy in its own way.
+                <br />
+                Everything was in confusion in the Oblonskys house. The wife had discovered that the
+                husband was carrying on an intrigue with a French girl, who had been a governess in
+                their family, and she had announced to her husband that she could not go on living
+                in the same house with him. This position of affairs had now lasted three days, and
+                not only the husband and wife themselves, but all the members of their family and
+                household, were painfully conscious of it.
+              </p>
+            </div>
+          </Modal>
+        )}
       </>
     );
   },
