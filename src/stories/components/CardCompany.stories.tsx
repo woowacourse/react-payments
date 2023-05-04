d52080146a45b253da202c5e459b0ee59e504635
Dain Lee <feb.dain@gmail.com>
2023-05-04 17:01:48 +0900

    test: 카드사 description 추가 및 각 카드사 story 추가

--- src/stories/components/CardCompany.stories.tsx
+++ src/stories/components/CardCompany.stories.tsx
@@ -6,6 +6,12 @@ const meta = {
   component: CardCompanyComponent,
   title: "Components/CardCompany",
   tags: ["autodocs"],
+  argTypes: {
+    cardCompanyName: {
+      description:
+        "카드사를 선택하면 해당 카드사의 로고와 이름을 볼 수 있습니다.",
+    },
+  },
 } satisfies Meta<typeof CardCompanyComponent>;
 
 export default meta;
@@ -24,5 +30,53 @@ export const CardCompany: Story = {
         type: "select",
       },
     },
+  },
+};
+
+export const BcCard: Story = {
+  args: {
+    cardCompanyName: "BC카드",
+  },
+};
+
+export const ShinhanCard: Story = {
+  args: {
+    cardCompanyName: "신한카드",
+  },
+};
+
+export const KakaoCard: Story = {
+  args: {
+    cardCompanyName: "카카오뱅크",
+  },
+};
+
+export const HyundaiCard: Story = {
+  args: {
+    cardCompanyName: "현대카드",
+  },
+};
+
+export const WooriCard: Story = {
+  args: {
+    cardCompanyName: "우리카드",
+  },
+};
+
+export const LotteCard: Story = {
+  args: {
+    cardCompanyName: "롯데카드",
+  },
+};
+
+export const HanaCard: Story = {
+  args: {
+    cardCompanyName: "하나카드",
+  },
+};
+
+export const KbCard: Story = {
+  args: {
+    cardCompanyName: "국민카드",
   },
 };
