3edb697cfe4bdee4b255f61ab0c28e22a6e9e2ac
chsua <csj1919@naver.com>
2023-05-04 15:34:59 +0900

    refactor: 카드 추가 버튼에서 link와 button가 중첩된 부분 수정

--- src/component/CardListPage/AddCardButton/AddCardButton.tsx
+++ src/component/CardListPage/AddCardButton/AddCardButton.tsx
@@ -4,22 +4,20 @@ import "./addCardButton.css";
 
 export default function AddCardButton() {
   return (
-    <Link to="/CardInputPage">
-      <button className="add-card-button">
-        <svg
-          className="add-card-button-icon"
-          width="16"
-          height="17"
-          viewBox="0 0 16 17"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M9.30372 7.18842H15.251V9.75189H9.30372V16.4902H6.57911V9.75189H0.631842V7.18842H6.57911V0.96283H9.30372V7.18842Z"
-            fill="#575757"
-          />
-        </svg>
-      </button>
+    <Link to="/CardInputPage" className="add-card-button">
+      <svg
+        className="add-card-button-icon"
+        width="16"
+        height="17"
+        viewBox="0 0 16 17"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M9.30372 7.18842H15.251V9.75189H9.30372V16.4902H6.57911V9.75189H0.631842V7.18842H6.57911V0.96283H9.30372V7.18842Z"
+          fill="#575757"
+        />
+      </svg>
     </Link>
   );
 }
