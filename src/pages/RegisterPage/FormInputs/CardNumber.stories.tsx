c573fefc4b29e817619cb261110e6f098d0a9b73
Dain Lee <feb.dain@gmail.com>
2023-05-04 12:21:29 +0900

    test: CardNumber 컴포넌트에 provider(context api) 추가

--- src/pages/RegisterPage/FormInputs/CardNumber.stories.tsx
+++ src/pages/RegisterPage/FormInputs/CardNumber.stories.tsx
@@ -1,9 +1,21 @@
 import { Meta, StoryObj } from "@storybook/react";
 import CardNumberInput from "../FormInputs/CardNumberInput";
+import CardInfoProvider from "components/provider/CardInfoProvider";
+import ModalStateProvider from "components/provider/ModalStateProvider";
+
 
 const meta = {
   component: CardNumberInput,
   title: "Input/CardNumbers",
+  decorators: [
+    (Story) => (
+      <CardInfoProvider>
+        <ModalStateProvider>
+          <Story />
+        </ModalStateProvider>
+      </CardInfoProvider>
+    ),
+  ],
 } satisfies Meta<typeof CardNumberInput>;
 
 export default meta;
@@ -11,12 +23,5 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const CardNumbers: Story = {
-  args: {
-    cardNumber: {
-      number1: "1111",
-      number2: "2222",
-      number3: "3333",
-      number4: "4444",
-    },
-  },
+  args: {},
 };
