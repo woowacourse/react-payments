809c1b48538f27d6fd7ee662d7b94591803cef79
Leejin-Yang <ejin1996@gmail.com>
2023-05-05 23:34:08 +0900

    feat: 체크 아이콘 구현

---
+++ src/components/common/CheckIcon/index.tsx
@@ -0,0 +1,7 @@
+import styles from './checkIcon.module.css';
+
+const CheckIcon = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default CheckIcon;
--- src/pages/CardRegisterPage/index.tsx
+++ src/pages/CardRegisterPage/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import useCardFormValue from '../../hooks/useCardFormValue';
@@ -6,6 +6,8 @@ import useTimeout from '../../hooks/useTimeout';
 import type { CardData } from '../../types/card';
 
 import styles from './cardRegisterPage.module.css';
+import Spinner from '../../components/common/Spinner';
+import CheckIcon from '../../components/common/CheckIcon';
 
 interface Props {
   registerCard: (card: CardData) => void;
@@ -13,8 +15,13 @@ interface Props {
 
 const CardRegisterPage = ({ registerCard }: Props) => {
   const { company, number, owner, expiredDate } = useCardFormValue();
+  const [isFinish, setIsFinish] = useState(false);
 
   const navigate = useNavigate();
+
+  const finishWork = () => {
+    setIsFinish(true);
+  };
 
   const navigateHome = () => {
     navigate('/');
@@ -31,6 +38,7 @@ const CardRegisterPage = ({ registerCard }: Props) => {
     [company, expiredDate, number.first, number.second, owner],
   );
 
+  const [finishTimer] = useTimeout(finishWork, 2000);
   const [navigateTimer] = useTimeout(navigateHome, 1000);
 
   useEffect(() => {
@@ -38,13 +46,23 @@ const CardRegisterPage = ({ registerCard }: Props) => {
   }, [cardData, registerCard]);
 
   useEffect(() => {
-    navigateTimer();
-  }, [navigateTimer]);
+    finishTimer();
+  }, [finishTimer]);
+
+  useEffect(() => {
+    if (isFinish) {
+      navigateTimer();
+    }
+  }, [isFinish, navigateTimer]);
 
   return (
     <main className={styles.container}>
-      <h1 className={styles.title}>카드를 등록 중입니다.</h1>
-      <section className={styles.formContainer}></section>
+      <h1 className={styles.title}>
+        {isFinish ? '카드 등록을 완료했습니다!' : '카드를 등록 중입니다.'}
+      </h1>
+      <div className={styles.spinnerContainer}>
+        {isFinish ? <CheckIcon /> : <Spinner />}
+      </div>
     </main>
   );
 };
