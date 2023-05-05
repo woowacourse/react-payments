735cb10d3eeb253ca10398fc35a26457b114bdd6
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-05 23:00:53 +0900

    refactor: styled components 모듈화

--- src/components/CreditCardNicknameInputForm/CreditCardNicknameInputForm.tsx
+++ src/components/CreditCardNicknameInputForm/CreditCardNicknameInputForm.tsx
@@ -1,29 +1,10 @@
 import CreditCard from 'components/CreditCard/CreditCard';
-import styled from 'styled-components';
 import { ChangeEvent, useState } from 'react';
 import useCreditCardForm from 'hooks/useCreditCardForm';
 import ControlButton from 'components/ControlButton';
 import { useCreditCardList } from 'hooks/useCreditCardList';
 import LoadingSpinner from 'components/LoadingSpinner';
 import * as S from './style';
-
-const NicknameInput = styled.input`
-  border-width: 0px 0px 1px 0px;
-
-  width: 100%;
-
-  margin-top: 100px;
-  padding: 10px;
-
-  font-family: 'Roboto';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 18px;
-  line-height: 21px;
-  display: flex;
-  align-items: center;
-  text-align: center;
-`;
 
 function CreditCardNicknameInputForm() {
   const { creditCardForm } = useCreditCardForm();
@@ -53,19 +34,20 @@ function CreditCardNicknameInputForm() {
           />
         </S.CreditCardPreview>
 
-        <NicknameInput
+        <S.NicknameInput
           value={nickname}
           onChange={handleNicknameInput}
           placeholder="카드 별명을 입력하세요"
         />
       </S.CreditCardNicknameInputFormLayout>
-      {isLoading ? (
-        <LoadingSpinner />
-      ) : (
-        <ControlButton onClick={submitNicknameInput}>
-          {nickname.length > 0 ? '확인' : '건너뛰기'}
-        </ControlButton>
-      )}
+      {isLoading
+        ? (
+          <LoadingSpinner />
+        ) : (
+          <ControlButton onClick={submitNicknameInput}>
+            {nickname.length > 0 ? '확인' : '건너뛰기'}
+          </ControlButton>
+        )}
     </>
   );
 }
