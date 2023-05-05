f68fa2d52636ac34164ea7ea6fea30eeaa828139
bassyu <yrkdgus@gmail.com>
2023-05-05 13:47:26 +0900

    refactor(CardCompanyModal): Modal을 PortalDrawer로 대체

--- src/components/box/CardCompanyModal.tsx
+++ src/components/box/CardCompanyModal.tsx
@@ -1,5 +1,6 @@
 import type { CardType } from '../../types';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
+import PortalDrawer from 'react-portal-drawer';
 
 import { useCardForm } from '../../context/CardFormContext';
 import { CARD_COMPANY_OPTION_MATRIX } from '../../constants';
@@ -17,58 +18,22 @@ const CardCompanyModal = ({ closeModal }: Props) => {
   };
 
   return (
-    <>
-      <ModalBackdrop onClick={cardCompany ? closeModal : undefined} />
-      <ModalWrapper>
-        {CARD_COMPANY_OPTION_MATRIX.map((row) => (
-          <CardCompanyRow>
-            {row.map(({ iconSrc, cardCompany }) => (
-              <CardCompanyItem onClick={onClickCardCompanyItem(cardCompany)}>
-                <img src={iconSrc} alt={cardCompany} />
-                <p>{cardCompany}</p>
-              </CardCompanyItem>
-            ))}
-          </CardCompanyRow>
-        ))}
-      </ModalWrapper>
-    </>
+    <PortalDrawer selectors="#root" requestClose={cardCompany ? closeModal : undefined}>
+      {CARD_COMPANY_OPTION_MATRIX.map((row) => (
+        <CardCompanyRow>
+          {row.map(({ iconSrc, cardCompany }) => (
+            <CardCompanyItem onClick={onClickCardCompanyItem(cardCompany)}>
+              <img src={iconSrc} alt={cardCompany} />
+              <p>{cardCompany}</p>
+            </CardCompanyItem>
+          ))}
+        </CardCompanyRow>
+      ))}
+    </PortalDrawer>
   );
 };
 
 export default CardCompanyModal;
-
-const darkening = keyframes`
-  from { background-color: rgba(0, 0, 0, 0.0); }
-  to { background-color: rgba(0, 0, 0, 0.6); }
-`;
-
-const drawer = keyframes`
-  from { bottom: -100px }
-  to { bottom: 0; }
-`;
-
-const ModalBackdrop = styled.div`
-  animation: ${darkening} 0.3s;
-  position: absolute;
-  top: 0;
-
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.6);
-`;
-
-const ModalWrapper = styled.div`
-  animation: ${drawer} 0.3s;
-  position: absolute;
-  bottom: 0;
-
-  width: 100%;
-  height: auto;
-  border-radius: 6px 6px 0px 0px;
-  padding: 34px 48px;
-
-  background: white;
-`;
 
 const CardCompanyRow = styled.div`
   display: flex;
