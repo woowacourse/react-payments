1df90b39bc9a60a63bac73e28c539d8216a9e2f4
regularPark <srk99279@naver.com>
2023-05-05 10:24:48 +0900

    refactor: 타입 가드 내부의 불필요한 return 통합

--- src/util/guard.ts
+++ src/util/guard.ts
@@ -2,6 +2,5 @@ import { COMPANY_NAME } from 'components/common/Card/types';
 import { COMPANY_LIST } from '../constants';
 
 export const isCompanyName = (company: string): company is COMPANY_NAME => {
-  if (COMPANY_LIST.includes(company as COMPANY_NAME)) return true;
-  return false;
+  return Object.values(COMPANY_LIST).includes(company as COMPANY_NAME);
 };
