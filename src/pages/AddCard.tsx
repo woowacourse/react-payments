2c18ee5a0ce5060dbcf73ed6989b211eb6231079
kangyeongmin <kym6050@hanyang.ac.kr>
2023-05-05 14:41:07 +0900

    feat: BottomSheet 라이브러리로 분리

--- src/pages/AddCard.tsx
+++ src/pages/AddCard.tsx
@@ -1,21 +1,16 @@
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { ModalDispatchContext } from "../context";
+import { ModalDispatchContext, ModalStateContext } from "../context";
 import { ROUTER_PATH } from "../router/path";
 import { getLocalStorage, setLocalStorage } from "../utils";
-import {
-  Page,
-  Header,
-  Card,
-  CardInputForm,
-  BottomSheet,
-  CardCompany,
-} from "../components";
+import { Page, Header, Card, CardInputForm, CardCompany } from "../components";
 import { useCard } from "../hooks";
+import BottomSheet from "woowa-light-modal";
 
 const AddCard = () => {
   const navigate = useNavigate();
   const [card, setNewCard] = useCard();
+  const { isModalOpen } = useContext(ModalStateContext);
   const { toggleModal } = useContext(ModalDispatchContext);
 
   useEffect(() => {
@@ -34,14 +29,14 @@ const AddCard = () => {
 
   return (
     <Page>
-      <Header title="카드 추가" isBack />
+      <Header title="카드 추가" path={ROUTER_PATH.MyCard} />
       <Card {...card} />
       <CardInputForm
         card={card}
         setNewCard={setNewCard}
         onSubmit={handleFormSubmited}
       />
-      <BottomSheet>
+      <BottomSheet isOpen={isModalOpen} onClose={toggleModal}>
         <CardCompany onChange={handleCardCompanyChanged} />
       </BottomSheet>
     </Page>
