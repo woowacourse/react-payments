322ca3d5762797094e4f313c0d83e8a526dfb545
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-05 22:15:03 +0900

    feat(Button): common 컴포넌트 구현

---
+++ src/components/@common/Button/Button.tsx
@@ -0,0 +1,12 @@
+import StyledButton from "./StyledButton";
+import type { StyledButtonProps } from "./StyledButton";
+
+interface ButtonProps extends StyledButtonProps, React.HTMLAttributes<HTMLButtonElement> {
+  onClick: () => void;
+}
+
+const Button = ({ onClick, ...$styledProps }: ButtonProps) => {
+  return <StyledButton onClick={onClick} {...$styledProps}></StyledButton>;
+};
+
+export default Button;
