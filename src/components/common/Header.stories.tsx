97bb25458d333b7968cbc9a0c8ecbdc2d67936eb
bassyu <yrkdgus@gmail.com>
2023-05-05 14:40:24 +0900

    feat(stories): 스토리북에 Controls 추가

--- src/components/common/Header.stories.tsx
+++ src/components/common/Header.stories.tsx
@@ -1,15 +1,25 @@
 import Header from './Header';
 
+import { Meta, StoryFn } from '@storybook/react';
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   title: 'Header',
   component: Header,
+} satisfies Meta<React.ComponentProps<typeof Header>>;
+
+const Template: StoryFn<React.ComponentProps<typeof Header>> = (props) => <Header {...props} />;
+
+export const Controls = Template.bind({});
+Controls.args = {
+  title: '헤더 타이틀',
+  onClickBack: () => {},
 };
 
-export function InListPage() {
+export const InListPage = () => {
   return <Header title="보유카드" />;
-}
+};
 
-export function InRegisterPage() {
+export const InRegisterPage = () => {
   return <Header title="카드 추가" onClickBack={() => {}} />;
-}
+};
