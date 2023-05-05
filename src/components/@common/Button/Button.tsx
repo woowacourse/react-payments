6f4f101cbc9337fb2d52d013033bffde8710f20e
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-05 22:30:55 +0900

    refactor(Button): props optional 처리

--- src/components/@common/Button/Button.tsx
+++ src/components/@common/Button/Button.tsx
@@ -2,7 +2,7 @@ import StyledButton from "./StyledButton";
 import type { StyledButtonProps } from "./StyledButton";
 
 interface ButtonProps extends StyledButtonProps, React.HTMLAttributes<HTMLButtonElement> {
-  onClick: () => void;
+  onClick?: () => void;
 }
 
 const Button = ({ onClick, ...$styledProps }: ButtonProps) => {
