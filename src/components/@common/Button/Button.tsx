0b9beac494d91841080a73f0d5fef1dbb745b62b
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-06 11:19:44 +0900

    refactor(Button): onClick props 파라미터 추가

--- src/components/@common/Button/Button.tsx
+++ src/components/@common/Button/Button.tsx
@@ -2,7 +2,7 @@ import StyledButton from "./StyledButton";
 import type { StyledButtonProps } from "./StyledButton";
 
 interface ButtonProps extends StyledButtonProps, React.ComponentPropsWithoutRef<"button"> {
-  onClick?: () => void;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const Button = ({ onClick, ...restProps }: ButtonProps) => {
