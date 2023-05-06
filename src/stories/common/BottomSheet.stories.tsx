5cd27ab358ebb274d2e629e9d85bb3fadd2f2fb2
regularPark <srk99279@naver.com>
2023-05-06 22:31:55 +0900

    test: form test 추가

--- src/stories/common/BottomSheet.stories.tsx
+++ src/stories/common/BottomSheet.stories.tsx
@@ -9,6 +9,6 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const CardCompanyModalStory: Story = {
+export const BottomSheetStory: Story = {
   args: {},
 };
--- src/stories/form/AddCardForm.stories.tsx
+++ src/stories/form/AddCardForm.stories.tsx
@@ -71,6 +71,64 @@ export const SuccessStory: Story = {
   },
 };
 
+export const FailedWithInvalidNumberStory: Story = {
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <>
+          <GlobalStyle />
+          <PageContainer>
+            <CardFormContainer>
+              <Story onSubmit={() => {}} />
+            </CardFormContainer>
+          </PageContainer>
+        </>
+      );
+    },
+  ],
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const KBCardButton = document.querySelector('span[id="국민카드"]')!;
+
+    await userEvent.click(KBCardButton);
+    expect(canvas.getAllByText('국민카드')).toBeTruthy();
+
+    const submitButton = document.querySelector('button[id="card-submit-btn"]');
+    expect(submitButton).toBeDisabled();
+
+    const cardNumber0 = document.querySelector('input[id="card-number0"]')!;
+    const cardNumber1 = document.querySelector('input[id="card-number1"]')!;
+
+    await userEvent.click(cardNumber0);
+
+    await userEvent.type(cardNumber0, '527212341234123', { delay: 100 });
+    expect(cardNumber0).toHaveValue('5272');
+    expect(cardNumber1).toHaveValue('1234');
+
+    const dateInput = document.querySelector('input[id="month"]')!;
+
+    await userEvent.type(dateInput, '1223', { delay: 100 });
+    expect(dateInput).toHaveValue('12');
+
+    const ownerNameInput = document.querySelector('input[id="owner-name"]')!;
+
+    await userEvent.type(ownerNameInput, 'regular park123', { delay: 100 });
+    expect(ownerNameInput).toHaveValue('REGULAR PARK');
+
+    const securityCodeInput = document.querySelector('input[id="security-code"]')!;
+    await userEvent.type(securityCodeInput, '123', { delay: 100 });
+
+    const passwordInput = document.querySelector('input[id="first-password"]')!;
+    await userEvent.type(passwordInput, '12', { delay: 100 });
+
+    expect(submitButton).toBeDisabled();
+  },
+};
+
 export const FailedWithInvalidYearStory: Story = {
   args: {},
   decorators: [
@@ -124,6 +182,122 @@ export const FailedWithInvalidYearStory: Story = {
 
     const passwordInput = document.querySelector('input[id="first-password"]')!;
     await userEvent.type(passwordInput, '12', { delay: 100 });
+
+    expect(submitButton).toBeDisabled();
+  },
+};
+
+export const FailedWithInvalidSecurityCodeStory: Story = {
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <>
+          <GlobalStyle />
+          <PageContainer>
+            <CardFormContainer>
+              <Story onSubmit={() => {}} />
+            </CardFormContainer>
+          </PageContainer>
+        </>
+      );
+    },
+  ],
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const KBCardButton = document.querySelector('span[id="국민카드"]')!;
+
+    await userEvent.click(KBCardButton);
+    expect(canvas.getAllByText('국민카드')).toBeTruthy();
+
+    const submitButton = document.querySelector('button[id="card-submit-btn"]');
+    expect(submitButton).toBeDisabled();
+
+    const cardNumber0 = document.querySelector('input[id="card-number0"]')!;
+    const cardNumber1 = document.querySelector('input[id="card-number1"]')!;
+
+    await userEvent.click(cardNumber0);
+
+    await userEvent.type(cardNumber0, '5272123412341234', { delay: 100 });
+    expect(cardNumber0).toHaveValue('5272');
+    expect(cardNumber1).toHaveValue('1234');
+
+    const dateInput = document.querySelector('input[id="month"]')!;
+
+    await userEvent.type(dateInput, '1223', { delay: 100 });
+    expect(dateInput).toHaveValue('12');
+
+    const ownerNameInput = document.querySelector('input[id="owner-name"]')!;
+
+    await userEvent.type(ownerNameInput, 'regular park123', { delay: 100 });
+    expect(ownerNameInput).toHaveValue('REGULAR PARK');
+
+    const securityCodeInput = document.querySelector('input[id="security-code"]')!;
+    await userEvent.type(securityCodeInput, '45', { delay: 100 });
+
+    const passwordInput = document.querySelector('input[id="first-password"]')!;
+    await userEvent.type(passwordInput, '12', { delay: 100 });
+
+    expect(submitButton).toBeDisabled();
+  },
+};
+
+export const FailedWithInvalidPasswordStory: Story = {
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <>
+          <GlobalStyle />
+          <PageContainer>
+            <CardFormContainer>
+              <Story onSubmit={() => {}} />
+            </CardFormContainer>
+          </PageContainer>
+        </>
+      );
+    },
+  ],
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const KBCardButton = document.querySelector('span[id="국민카드"]')!;
+
+    await userEvent.click(KBCardButton);
+    expect(canvas.getAllByText('국민카드')).toBeTruthy();
+
+    const submitButton = document.querySelector('button[id="card-submit-btn"]');
+    expect(submitButton).toBeDisabled();
+
+    const cardNumber0 = document.querySelector('input[id="card-number0"]')!;
+    const cardNumber1 = document.querySelector('input[id="card-number1"]')!;
+
+    await userEvent.click(cardNumber0);
+
+    await userEvent.type(cardNumber0, '5272123412341234', { delay: 100 });
+    expect(cardNumber0).toHaveValue('5272');
+    expect(cardNumber1).toHaveValue('1234');
+
+    const dateInput = document.querySelector('input[id="month"]')!;
+
+    await userEvent.type(dateInput, '1223', { delay: 100 });
+    expect(dateInput).toHaveValue('12');
+
+    const ownerNameInput = document.querySelector('input[id="owner-name"]')!;
+
+    await userEvent.type(ownerNameInput, 'regular park123', { delay: 100 });
+    expect(ownerNameInput).toHaveValue('REGULAR PARK');
+
+    const securityCodeInput = document.querySelector('input[id="security-code"]')!;
+    await userEvent.type(securityCodeInput, '456', { delay: 100 });
+
+    const passwordInput = document.querySelector('input[id="first-password"]')!;
+    await userEvent.type(passwordInput, '1e', { delay: 100 });
 
     expect(submitButton).toBeDisabled();
   },
