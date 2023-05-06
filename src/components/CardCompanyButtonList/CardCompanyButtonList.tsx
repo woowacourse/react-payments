acc7bc5c3365886d99893b0665c881df35c149da
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-06 11:56:13 +0900

    refactor: 불필요한 div 제거 및 styled 컴포넌트 명 변경

--- src/components/CardCompanyButtonList/CardCompanyButtonList.tsx
+++ src/components/CardCompanyButtonList/CardCompanyButtonList.tsx
@@ -11,24 +11,21 @@ const cardCompanies = Object.keys(CARD_COMPANIES) as CardCompany[];
 
 const CardCompanyButtonList = ({ handleCardCompany }: CardCompanyButtonListProps) => {
   return (
-    <CardCompanyButtonContainer>
+    <>
       <Instruction>카드사 선택</Instruction>
       <CardCompanyButtonWrapper>
         {cardCompanies.map((company) => (
           <CardCompanyButton key={company} cardCompany={company} onClick={() => handleCardCompany(company)} />
         ))}
       </CardCompanyButtonWrapper>
-    </CardCompanyButtonContainer>
+    </>
   );
 };
 
 const Instruction = styled.h3`
   font-size: 16px;
-
   margin-bottom: 20px;
 `;
-
-const CardCompanyButtonContainer = styled.div``;
 
 const CardCompanyButtonWrapper = styled.div`
   display: grid;
