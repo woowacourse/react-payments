8991691de7b3c2dafcbe5cb6deb1d59406a57066
Dain Lee <feb.dain@gmail.com>
2023-05-07 04:47:29 +0900

    test: LastPage Interaction Test 분리

---
+++ src/stories/pages/lastPage/LastPageInteractions.stories.tsx
@@ -0,0 +1,82 @@
+import { userEvent, waitFor, within } from "@storybook/testing-library";
+import { Meta, StoryObj } from "@storybook/react";
+import { BrowserRouter } from "react-router-dom";
+import { CardNicknameSetting } from "./LastPage.stories";
+import LastPage from "pages/LastPage";
+import CardInfoProvider from "components/provider/CardInfoProvider";
+
+const meta = {
+  title: "Pages/LastPage",
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <CardInfoProvider>
+          <div id="root">
+            <Story />
+          </div>
+        </CardInfoProvider>
+      </BrowserRouter>
+    ),
+  ],
+
+  args: {
+    cardCompany: "우리카드",
+    number1: "1232",
+    number2: "1312",
+    number3: "3123",
+    number4: "2131",
+    name: "YUMMY",
+    month: "12",
+    year: "31",
+  },
+} satisfies Meta<typeof LastPage>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const SuccessInteraction: Story = {
+  render: () => {
+    return CardNicknameSetting(meta.args);
+  },
+
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    const cardNicknameInput =
+      canvas.getByPlaceholderText("카드 별칭을 입력해 주세요.");
+
+    await waitFor(() => cardNicknameInput);
+
+    await step("Typing Card Nickname", async () => {
+      await userEvent.type(cardNicknameInput, "🥰야미 카드🥰", { delay: 200 });
+    });
+
+    await step("Button Click", () => {
+      userEvent.click(canvas.getByRole("button"));
+    });
+  },
+};
+
+export const ErrorInteraction: Story = {
+  render: () => {
+    return CardNicknameSetting(meta.args);
+  },
+
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    const cardNicknameInput =
+      canvas.getByPlaceholderText("카드 별칭을 입력해 주세요.");
+
+    await waitFor(() => cardNicknameInput);
+
+    await step("Not Typing Card Nickname", async () => {
+      await userEvent.type(cardNicknameInput, "          ", { delay: 200 });
+    });
+
+    await step("Button Click", () => {
+      userEvent.click(canvas.getByRole("button"));
+    });
+  },
+};
