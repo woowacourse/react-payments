9c8510e1a52effe7e22c325e7df561477879225c
chex <chex1004@gmail.com>
2023-05-06 19:58:37 +0900

    chore: 스토리북 버전 업데이트

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,6 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
     '@storybook/addon-interactions',
-    '@storybook/addon-styling',
   ],
   framework: {
     name: '@storybook/react-webpack5',
