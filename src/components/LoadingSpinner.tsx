19fc1b75c2d22817e5ba7cc461b4d283f929a5d8
2yunseong <dbsdltjd3701@naver.com>
2023-05-06 16:49:07 +0900

    feat: css 스타일링 수정

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -9,14 +9,14 @@ type LoadingSpinnerProps = {
 const LoadingSpinner = ({ cardCompany }: LoadingSpinnerProps) => {
   return (
     <div className="loading-spinner-container">
-      <div className={`card ${CARD_COMPANY_ENG[cardCompany]}`}>
+      <div className={`spinner-card ${CARD_COMPANY_ENG[cardCompany]}`}>
         <div className="spinner">
           <svg className="spin-circle" viewBox="0 0 50 50">
             <circle cx="25" cy="25" r="20" />
           </svg>
         </div>
       </div>
-      <div>카드를 등록중입니다...</div>
+      <div className="loading-spinner-message">카드를 등록중입니다...</div>
     </div>
   );
 };
