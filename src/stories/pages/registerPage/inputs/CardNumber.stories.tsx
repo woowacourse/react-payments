51007fd1ab82088bec139c8beb22a7706ed1c0c2
Dain Lee <feb.dain@gmail.com>
2023-05-06 15:37:33 +0900

    test: CardNumber storybook에서 autofocus 기능되도록 추가

--- src/stories/pages/registerPage/inputs/CardNumber.stories.tsx
+++ src/stories/pages/registerPage/inputs/CardNumber.stories.tsx
@@ -2,6 +2,51 @@ import { Meta, StoryObj } from "@storybook/react";
 import CardNumberInput from "pages/RegisterPage/FormInputs/CardNumberInput";
 import CardInfoProvider from "components/provider/CardInfoProvider";
 import ModalStateProvider from "components/provider/ModalStateProvider";
+import { VALID_INPUT } from "constants/limit";
+import { DIRECTION } from "constants/inputDirection";
+import { ChangeEvent, KeyboardEvent } from "react";
+const { ONLY_NUMBER } = VALID_INPUT;
+const { NEXT, PREV } = DIRECTION;
+
+const handleFocusNext = ({ target }: ChangeEvent<HTMLFormElement>) => {
+  if (shouldPreventFocusMovement(target)) return;
+
+  focusFormInput(target.form, target, NEXT);
+};
+
+const shouldPreventFocusMovement = (target: EventTarget & HTMLFormElement) => {
+  const { value, maxLength } = target;
+
+  const validValue = value.replace(ONLY_NUMBER, "");
+
+  return validValue.length !== maxLength;
+};
+
+const focusFormInput = (
+  form: HTMLFormElement,
+  currentInput: EventTarget,
+  direction: number
+) => {
+  const formControlList = [...form];
+
+  if (!(currentInput instanceof HTMLInputElement)) return;
+
+  const currentInputIndex = formControlList.indexOf(currentInput);
+  const nextInput = formControlList[currentInputIndex + direction];
+
+  if (!nextInput || !(nextInput instanceof HTMLInputElement)) return;
+
+  nextInput.focus();
+};
+
+const handleKeyDown = (event: KeyboardEvent<HTMLFormElement>) => {
+  const { key, target, currentTarget: form } = event;
+
+  if (!(target instanceof HTMLInputElement)) return;
+  if (key !== "Backspace" || target.value !== "") return;
+
+  focusFormInput(form, target, PREV);
+};
 
 const meta = {
   component: CardNumberInput,
@@ -10,7 +55,9 @@ const meta = {
     (Story) => (
       <CardInfoProvider>
         <ModalStateProvider>
-          <Story />
+          <form onChange={handleFocusNext} onKeyDown={handleKeyDown}>
+            <Story />
+          </form>
         </ModalStateProvider>
       </CardInfoProvider>
     ),
