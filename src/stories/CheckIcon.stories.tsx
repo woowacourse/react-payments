735a17deb4b0d0bd6bb36caac461712080ed853f
Leejin-Yang <ejin1996@gmail.com>
2023-05-07 17:43:21 +0900

    test: 스피너, 체크아이콘 스토리 작성

---
+++ src/stories/CheckIcon.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import CheckIcon from '../components/common/CheckIcon';
+
+const meta: Meta<typeof CheckIcon> = {
+  title: 'components/common/CheckIcon',
+  component: CheckIcon,
+  decorators: [
+    (Story) => (
+      <div style={{ width: '420px', margin: '0 auto' }}>
+        <div style={{ position: 'relative', width: '180px', height: '180px' }}>
+          <Story />
+        </div>
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof CheckIcon>;
+
+export const Default: Story = {};
