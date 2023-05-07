30dc907bb43b27f2b10d765f490a17a6c4c12bc5
Q Kim <qoli0703@gmail.com>
2023-05-07 17:24:50 +0900

    test: 카드사 선택 모달 사용예시 추가

--- src/stories/CardCompanySelectModal.stories.tsx
+++ src/stories/CardCompanySelectModal.stories.tsx
@@ -1,24 +1,20 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import CardCompanySelectModal from "../component/CardInputPage/CardCompanySelectModal/CardCompanySelectModal";
 import { CreditCardProvider } from "../context/CreditCardContext";
+import { useState } from "react";
 
 type Story = StoryObj<typeof CardCompanySelectModal>;
 
 const meta: Meta = {
   title: "Card Company Select Modal",
   component: CardCompanySelectModal,
+
   argTypes: {
-    setOpen: {
-      action: 'Closing modal',
-      description: 'This is for setting isOpen state to false.'
-    },
     children: {
       table: { disable: true },
     },
-    companyClickHandler: {
-      action: 'Company clicked',
-    }
   },
+
   decorators: [
     (Story) => (
       <CreditCardProvider>
@@ -30,8 +26,46 @@ const meta: Meta = {
 
 export default meta;
 
-export const Modal: Story = {
+export const Default: Story = {
   args: {
     open: true,
-  }
+  },
+
+  argTypes: {
+    setOpen: {
+      action: 'Closing modal',
+      description: 'This is for setting isOpen state to false.'
+    },
+    companyClickHandler: {
+      action: 'Company clicked',
+    }
+  },
 };
+
+const ExampleComponent = () => {
+  const [open, setOpen] = useState(false);
+  const [company, setCompany] = useState('없음');
+
+  return (
+    <>
+      <h1>현재 선택한 카드사: {company}</h1>
+      <button 
+        type="button"
+        onClick={() => setOpen(true)}
+        style={{ border: "1px solid black", marginTop: "10px" }}
+      >
+        <h2>카드사 고르기</h2>
+      </button>
+      <CardCompanySelectModal 
+        open={open}
+        setOpen={setOpen}
+        companyClickHandler={(value) => {
+          setCompany(value);
+          setOpen(false);
+        }}
+      />
+    </>
+  );
+}
+
+export const Example: Story = { render: ExampleComponent };
