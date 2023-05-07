9606e9d979f5c1b2a9dbcdf4802ff2d68bc7fcfc
yogjin <youngjin988@gmail.com>
2023-05-07 23:06:17 +0900

    feat: 카드 등록 시 스피너 적용
    실제 api 요청을 흉내내기위해 setTimeout 설정

--- src/pages/RegisterCardNickName.tsx
+++ src/pages/RegisterCardNickName.tsx
@@ -1,11 +1,14 @@
 import { Card } from 'components/common/Card/types';
+import { RegisterCardLoadingBar } from 'components/common/LodaingBar/RegisterCardLoadingBar';
 import { AddCardNickNameForm } from 'components/Form/AddCardNickNameForm';
 import { PageContainer } from 'components/style/PageContainer';
 import { useUserCards } from 'contexts/UserCardProvider';
+import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const RegisterCardNickName = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const {
     actions: { addCard },
   } = useUserCards();
@@ -17,13 +20,19 @@ export const RegisterCardNickName = () => {
   };
 
   const handleCardNickNameSubmit = (card: Card) => {
-    addCard(card);
-    goHome();
+    setIsSubmitting((prev) => !prev);
+    addCard(card)
+      .then(() => setIsSubmitting((prev) => !prev))
+      .then(goHome);
   };
 
   return (
     <Container>
-      <AddCardNickNameForm card={location.state.card} onSubmit={handleCardNickNameSubmit} />
+      {!isSubmitting ? (
+        <AddCardNickNameForm card={location.state.card} onSubmit={handleCardNickNameSubmit} />
+      ) : (
+        <RegisterCardLoadingBar />
+      )}
     </Container>
   );
 };
