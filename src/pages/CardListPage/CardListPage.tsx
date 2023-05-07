05fffdcd55d64162f13a8d2dda26afee04a8337d
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-05-07 09:43:32 +0900

    fix: 카드리스트 문구 오류 수정

--- src/pages/CardListPage/CardListPage.tsx
+++ src/pages/CardListPage/CardListPage.tsx
@@ -10,7 +10,7 @@ const CardListPage = () => {
   return (
     <Container>
       <AppBar title="보유카드" />
-      {cards.length ? <span>새로운 카드를 등록해주세요.</span> : null}
+      {cards.length ? null : <span>새로운 카드를 등록해주세요.</span>}
       <CardList cards={cards} />
     </Container>
   );
