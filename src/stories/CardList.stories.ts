28c5181edc42a35c25f032d4b0c84fed1bfbb179
jeongwusi <jeongwusi@gmail.com>
2023-05-08 19:47:17 +0900

    ♻️ refactor: args 옵션을 이용한 중복된 props 간결화

--- src/stories/CardList.stories.ts
+++ src/stories/CardList.stories.ts
@@ -5,6 +5,17 @@ const meta = {
   component: CardList,
   title: 'CardList',
   tags: ['autodocs'],
+  args: {
+    cardInfo: [
+      {
+        cardNumber: '1111-2222-3333-4444',
+        expirationDate: '11/28',
+        cardOwnerName: '김고니',
+        selectedCard: '',
+        cardNickName: '',
+      },
+    ],
+  },
 } satisfies Meta<typeof CardList>;
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -13,58 +24,42 @@ export const Primary: Story = {
   args: {
     cardInfo: [
       {
-        cardNumber: '1111-2222-3333-4444',
-        expirationDate: '11/28',
-        cardOwnerName: '김고니',
+        ...meta.args.cardInfo[0],
         selectedCard: '비씨카드',
         cardNickName: '고니의 비씨카드',
       },
       {
-        cardNumber: '1111-2222-3333-4444',
-        expirationDate: '11/28',
-        cardOwnerName: '김고니',
+        ...meta.args.cardInfo[0],
         selectedCard: '하나카드',
         cardNickName: '고니의 하나카드',
       },
       {
-        cardNumber: '1111-2222-3333-4444',
-        expirationDate: '11/28',
-        cardOwnerName: '김고니',
+        ...meta.args.cardInfo[0],
         selectedCard: '현대카드',
         cardNickName: '고니의 현대카드',
       },
       {
-        cardNumber: '1111-2222-3333-4444',
-        expirationDate: '11/28',
-        cardOwnerName: '김고니',
+        ...meta.args.cardInfo[0],
         selectedCard: '롯데카드',
         cardNickName: '고니의 롯데카드',
       },
       {
-        cardNumber: '1111-2222-3333-4444',
-        expirationDate: '11/28',
-        cardOwnerName: '김고니',
+        ...meta.args.cardInfo[0],
         selectedCard: '우리카드',
         cardNickName: '고니의 우리카드',
       },
       {
-        cardNumber: '1111-2222-3333-4444',
-        expirationDate: '11/28',
-        cardOwnerName: '김고니',
+        ...meta.args.cardInfo[0],
         selectedCard: '신한카드',
         cardNickName: '고니의 신한카드',
       },
       {
-        cardNumber: '1111-2222-3333-4444',
-        expirationDate: '11/28',
-        cardOwnerName: '김고니',
+        ...meta.args.cardInfo[0],
         selectedCard: '카카오뱅크',
         cardNickName: '고니의 카카오뱅크 카드',
       },
       {
-        cardNumber: '1111-2222-3333-4444',
-        expirationDate: '11/28',
-        cardOwnerName: '김고니',
+        ...meta.args.cardInfo[0],
         selectedCard: '국민카드',
         cardNickName: '고니의 국민카드',
       },
