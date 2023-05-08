5777801f37a0c44a99c057e08799425a9d73305b
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-05-08 16:41:42 +0900

    test : storybook contextApi 추가

--- src/components/addCard/cardNumber/cardNumber.stories.tsx
+++ src/components/addCard/cardNumber/cardNumber.stories.tsx
@@ -1,9 +1,17 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { CardInfoProvider } from "../../../contexts/cardInfo";
 import { CardNumber } from "./cardNumber";
 
 const meta: Meta<typeof CardNumber> = {
   component: CardNumber,
   title: "CardNumber",
+  decorators: [
+    (Story) => (
+      <CardInfoProvider>
+        <Story />
+      </CardInfoProvider>
+    ),
+  ],
 };
 
 export default meta;
--- src/components/addCard/expiredDate/expiredDate.stories.tsx
+++ src/components/addCard/expiredDate/expiredDate.stories.tsx
@@ -1,9 +1,17 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { CardInfoProvider } from "../../../contexts/cardInfo";
 import { ExpiredDate } from "./expiredDate";
 
 const meta: Meta<typeof ExpiredDate> = {
   component: ExpiredDate,
   title: "ExpiredDate",
+  decorators: [
+    (Story) => (
+      <CardInfoProvider>
+        <Story />
+      </CardInfoProvider>
+    ),
+  ],
 };
 
 export default meta;
