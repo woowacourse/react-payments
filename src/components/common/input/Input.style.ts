52e80bcef7a26809551b80519600be7a22b42c72
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-08 15:11:11 +0900

    feat: Input style component 추가

--- src/components/common/input/Input.style.ts
+++ src/components/common/input/Input.style.ts
@@ -1,22 +1,59 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export interface StyledProps {
+export interface InputProps {
+  $primary?: boolean;
+  textAlign?: 'center' | 'left' | 'right';
+  inputSize?: 'small' | 'medium' | 'large';
   width?: string;
-  textAlign?: string;
+  hasError?: boolean;
 }
-export const InputStyle = styled.input<StyledProps>`
-  width: ${(props) => props.width ?? '100%'};
+
+const getSizeStyles = (size: InputProps['inputSize']) => {
+  switch (size) {
+    case 'small':
+      return `
+        font-size: var(--font-size-small));
+        padding: 0 1.2rem;
+      `;
+    case 'medium':
+      return `
+        font-size: var(--font-size-medium));
+        padding: 0 1.4rem;
+      `;
+    case 'large':
+      return `
+        font-size: var(--font-size-large));
+        padding: 0 1.6rem;
+      `;
+    default:
+      return `
+      font-size: var(--font-size-small));
+      padding: 0 0.5rem;
+    `;
+  }
+};
+
+export const Input = styled.input<InputProps>`
   height: 45px;
-  letter-spacing: 1.5px;
-  background-color: #ecebf1;
+  letter-spacing: 1.1px;
+  font-weight: 600;
+  background-color: var(--gray-light);
   border-radius: 7px;
-  border: none;
-  font-size: 18px;
-  padding: 0 10px;
-  box-sizing: border-box;
-  text-align: ${(props) => props.textAlign ?? 'center'};
+  border: ${(props) => (props.hasError ? '2px solid var(--error-color)' : 'none')};
+  text-align: ${({ textAlign }) => textAlign ?? 'center'};
+  width: ${({ width }) => width ?? '100%'};
+
+  ${({ inputSize: size }) => getSizeStyles(size)}
+
+  ${({ $primary }) =>
+    $primary &&
+    css`
+      box-shadow: 1px 1px 1px var(--primary-color);
+    `}
 
   &:focus {
-    outline: none;
+    outline: 2px solid var(--primary-color);
   }
 `;
+
+export default Input;
