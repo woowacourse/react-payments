466907d5e9fa0abe38175cddb23689c39c783c26
turtle601 <turtle601@naver.com>
2023-05-08 12:46:50 +0900

    feat(constant): Router, url, Input Count 상수화

---
+++ src/utils/constant/index.ts
@@ -0,0 +1,9 @@
+export const INPUT_COUNT = 9 as const;
+
+export const ROUTER = {
+  HOME: '/',
+  addCard: '/addCard',
+  addCardAlias: '/addCardAlias',
+};
+
+export const URL = 'https://json-server-4140.onrender.com/payment';
