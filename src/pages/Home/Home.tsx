0b36d4a58376240cf00477055aff6fa70b8cc396
jeongwusi <jeongwusi@gmail.com>
2023-05-08 15:52:34 +0900

    ♻️ refactor: 홈화면에서 뒤로 가기 기능 불가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,12 +3,32 @@ import CardList from '../../components/CardList/CardList';
 import EmptyHeader from '../../components/EmptyHeader/EmptyHeader';
 import styles from './Home.module.css';
 import { CardInfo } from '../../types';
+import { useEffect } from 'react';
+import { history } from '../../routes/history';
+import { useNavigate } from 'react-router-dom';
 
 type HomeProps = {
   cardInfo: CardInfo[];
 };
 
 const Home = ({ cardInfo }: HomeProps) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const listenBackButton = () => {
+      alert('결제 페이지에서 뒤로 가기는 어렵습니다.');
+      navigate('/');
+    };
+
+    const ignoreHistoryEvent = history.listen(({ action }) => {
+      if (action === 'POP') {
+        listenBackButton();
+      }
+    });
+
+    return ignoreHistoryEvent;
+  }, [navigate]);
+
   return (
     <div className={styles.container}>
       <EmptyHeader />
