2b80de6f0748b1be264c7a72b1c182e073987552
jeongwusi <jeongwusi@gmail.com>
2023-05-08 22:30:08 +0900

    ♻️ refactor: 라우터 컴포넌트로 분리

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,34 +1,13 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import NotFound from '../pages/NotFound/NotFound';
-import Home from '../pages/Home/Home';
-import CardRegistration from '../pages/CardRegistration/CardRegistration';
 import styles from './App.module.css';
-import CardRegistrationConfirmation from '../pages/CardRegistrationConfirmation/CardRegistrationConfirmation';
+import Router from '../routes/Router';
 import useCardInfo from '../hook/useCardInfo';
-import Root from '../pages/Root/Root';
 
 const App = () => {
   const { cardInfo, registerNewCard } = useCardInfo();
 
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Root />,
-      errorElement: <NotFound />,
-      children: [
-        { index: true, element: <Home cardInfo={cardInfo} /> },
-        { path: '/card-registration', element: <CardRegistration /> },
-        {
-          path: '/card-registration-confirmation',
-          element: <CardRegistrationConfirmation registerNewCard={registerNewCard} />,
-        },
-      ],
-    },
-  ]);
-
   return (
     <div className={styles.container}>
-      <RouterProvider router={router} />
+      <Router cardInfo={cardInfo} registerNewCard={registerNewCard} />
     </div>
   );
 };
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import { CardProvider } from '../../context/CardContext';
+import { CardProvider } from '../context/CardContext';
 
 const Root = () => {
   return (
