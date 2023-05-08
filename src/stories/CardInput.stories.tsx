6d102d9be9bf0dddc75c444630b42698505f682e
semnil5202 <semnil5202@gmail.com>
2023-05-09 03:17:43 +0900

    refactor: Storybook CardInput 각각의 섹션 별 스토리 추가

--- src/stories/CardInput.stories.tsx
+++ src/stories/CardInput.stories.tsx
@@ -10,11 +10,47 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const CardInputStory: Story = {
+export const CardNumber: Story = {
   args: {
     type: 'text',
     value: '',
     placeholder: '0000',
+    inputMode: 'numeric',
+  },
+};
+
+export const CardExpiredDate: Story = {
+  args: {
+    type: 'text',
+    value: '',
+    placeholder: 'MM',
+    inputMode: 'numeric',
+  },
+};
+
+export const CardOwnerName: Story = {
+  args: {
+    type: 'text',
+    value: '',
+    placeholder: 'NAME',
+    inputMode: 'text',
+  },
+};
+
+export const CardSecurityCode: Story = {
+  args: {
+    type: 'password',
+    value: '',
+    placeholder: '•••',
+    inputMode: 'numeric',
+  },
+};
+
+export const CardPassword: Story = {
+  args: {
+    type: 'password',
+    value: '',
+    placeholder: '•',
     inputMode: 'numeric',
   },
 };
