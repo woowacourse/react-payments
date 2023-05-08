0c176b8b51b4c3ad4130cbeafbb701f2a7b62976
semnil5202 <semnil5202@gmail.com>
2023-05-09 02:54:50 +0900

    refactor: Storybook SubmitButton 스토리 변경
    Enable 상황과 Disable 상황을 구분하여 사용한다.

--- src/stories/SubmitButton.stories.ts
+++ src/stories/SubmitButton.stories.ts
@@ -10,10 +10,18 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const SubmitButtonStory: Story = {
+export const EnableToSubmitStory: Story = {
   args: {
     textContent: '확인',
     color: true,
     cursor: true,
+  },
+};
+
+export const DisableToSubmitStory: Story = {
+  args: {
+    textContent: '확인',
+    color: false,
+    cursor: false,
   },
 };
