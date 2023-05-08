f6ca953efb067e33cdec95dad2cea5a441fec4f4
turtle601 <turtle601@naver.com>
2023-05-08 12:47:16 +0900

    refactor(utils): 사용하지 않는 파일 제거

--- src/utils/checkIsCustomProps.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const checkIsCustomProps = (children: React.ReactNode, isCustom: boolean | undefined) => {
-  if (React.isValidElement(children) && !isCustom) throw new Error('Check isCustom props');
-};
--- src/utils/getContentLayoutStyle.ts
+++
@@ -1,9 +0,0 @@
-export const getContentLayoutStyle = (
-  isCenter: boolean | undefined,
-  isBottom: boolean | undefined,
-) => {
-  if (isCenter && isBottom) throw Error('isBottom 속성과 isCenter 속성을 동시에 둘 수 없습니다.');
-  if (isCenter) return 'center';
-  if (isBottom) return 'bottom';
-  return 'default';
-};
