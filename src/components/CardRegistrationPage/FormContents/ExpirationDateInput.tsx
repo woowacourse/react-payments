5a09e0ca6e7d014a1eedef10e7e4eedbb180b199
woo-jk <201702042@o.cnu.ac.kr>
2023-05-08 15:07:35 +0900

    feat: 만료일 입력 컴포넌트 onBlur시 에러메시지 보여주도록 변경

--- src/components/CardRegistrationPage/FormContents/ExpirationDateInput.tsx
+++ src/components/CardRegistrationPage/FormContents/ExpirationDateInput.tsx
@@ -8,7 +8,7 @@ import { useCardItemAction, useCardItemValue, useErrorMessageValue } from "../..
 
 const ExpirationDateInput = () => {
   const { expirationDate } = useCardItemValue();
-  const { onChangeExpirationDate } = useCardItemAction();
+  const { onChangeExpirationDate, onBlurExpirationDate } = useCardItemAction();
   const { expirationDateErrorMessage } = useErrorMessageValue();
 
   const { registRef, isNextInputFocusable, focusNextInput } = useInputFocus(INPUT_MAX_LENGTH.EXPIRATION_DATE);
@@ -27,6 +27,18 @@ const ExpirationDateInput = () => {
     onChangeExpirationDate.onChangeYear(inputValue);
   };
 
+  const handleBlurMonth = (event: React.FocusEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+
+    onBlurExpirationDate.onBlurMonth(inputValue);
+  };
+
+  const handleBlurYear = (event: React.FocusEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+
+    onBlurExpirationDate.onBlurYear(inputValue);
+  };
+
   return (
     <InputGroup labelValue="만료일" errorMessage={expirationDateErrorMessage}>
       <InputBox width="137px" isError={!!expirationDateErrorMessage}>
@@ -35,6 +47,7 @@ const ExpirationDateInput = () => {
           ref={(element) => registRef(0, element)}
           value={expirationDate[0]}
           onChange={handleChangeMonth}
+          onBlur={handleBlurMonth}
         />
         <InputSeparator color="#737373">/</InputSeparator>
         <Input
@@ -42,6 +55,7 @@ const ExpirationDateInput = () => {
           ref={(element) => registRef(1, element)}
           value={expirationDate[1]}
           onChange={handleChangeYear}
+          onBlur={handleBlurYear}
         />
       </InputBox>
     </InputGroup>
