912882470ee57e1d76dc3570dec86c6d35820c59
Leejin-Yang <ejin1996@gmail.com>
2023-05-10 16:20:53 +0900

    refactor: Button 스토리 prop 수정

--- src/stories/components/common/Button.stories.tsx
+++ src/stories/components/common/Button.stories.tsx
@@ -6,28 +6,25 @@ const meta: Meta<typeof Button> = {
   title: 'components/common/Button',
   component: Button,
   tags: ['autodocs'],
+  args: {
+    children: 'Button',
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-export const WhiteDefault: Story = {
-  args: {
-    children: '<',
-  },
-};
+export const WhiteDefault: Story = {};
 
 export const GrayDefault: Story = {
   args: {
     color: 'gray',
-    children: '<',
   },
 };
 
 export const WhiteCard: Story = {
   args: {
     size: 'card',
-    children: '<',
   },
 };
 
@@ -35,14 +32,12 @@ export const GrayCard: Story = {
   args: {
     size: 'card',
     color: 'gray',
-    children: '<',
   },
 };
 
 export const ButtonWithPadding: Story = {
   args: {
     color: 'gray',
-    children: '<',
     padding: true,
   },
 };
