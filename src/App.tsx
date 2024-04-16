2baa8fee204a280db54613056c42166316f2a3c6
anttiey <hanuuny@gmail.com>
2024-04-16 16:12:36 +0900

    feat: CardPreviewBox 컴포넌트 생성 및 전체 구조 설계
    Co-authored-by: Seongjin Hong <hongseongjin.to@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,30 @@ import styled from 'styled-components';
 import CardNumberInput from './components/CardNumberInput';
 import CardExpirationInput from './components/CardExpirationInput';
 import CardOwnerInput from './components/CardOwnerInput';
+import CardPreviewBox from './components/CardPreviewBox';
 import './styles/reset.css';
+
+const AppContainer = styled.div`
+  padding: 77px 30px;
+`;
 
 const CardForm = styled.div`
   display: flex;
   flex-direction: column;
   row-gap: 16px;
+  margin-top: 45px;
 `;
 
 function App() {
   return (
-    <CardForm>
-      <CardNumberInput />
-      <CardExpirationInput />
-      <CardOwnerInput />
-    </CardForm>
+    <AppContainer>
+      <CardPreviewBox />
+      <CardForm>
+        <CardNumberInput />
+        <CardExpirationInput />
+        <CardOwnerInput />
+      </CardForm>
+    </AppContainer>
   );
 }
 
---
+++ src/assets/index.ts
@@ -0,0 +1,2 @@
+export { default as MasterCard } from './images/Mastercard.png';
+export { default as Visa } from './images/Visa.png';
