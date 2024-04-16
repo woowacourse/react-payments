e385b1630dad0d1c02569fb410e209e7f91d9e4d
jinsil <jinsil378900@gmail.com>
2024-04-16 22:20:29 +0900

    feat: 3, 4번째 카드 번호들 마스킹
    Co-authored-by: Yuseon Kim(썬데이) <useon@users.noreply.github.com>

--- src/components/cardPreview/CardPreview.tsx
+++ src/components/cardPreview/CardPreview.tsx
@@ -9,6 +9,10 @@ const CardPreview = ({ cardInfo }: Props) => {
     <>
       <div>
         {cardInfo.cardNumbers.map((cardNumber, index) => {
+          const cardNumberLength = cardNumber.toString().length;
+          if (index > 1) {
+            return <span key={index}>{cardNumber !== 0 && "●".repeat(cardNumberLength)}</span>;
+          }
           return <span key={index}>{cardNumber !== 0 && cardNumber}</span>;
         })}
       </div>
