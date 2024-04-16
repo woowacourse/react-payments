e0abbc2135b87d010373259684588964226c43f4
anttiey <hanuuny@gmail.com>
2024-04-16 15:00:35 +0900

    feat: reset.css 추가
    Co-authored-by: Seongjin Hong <hongseongjin.to@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import "./App.css";
+import CardNumberInput from './components/CardNumberInput';
+
+import './styles/reset.css';
 
 function App() {
   return (
     <>
-      <h1>React Payments</h1>
+      <CardNumberInput />
     </>
   );
 }
