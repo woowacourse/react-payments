1d0f7af5ff2dc4a452aeadb5da46ff4ee8f7bd64
Soyeon Choe <soy2302ten@gmail.com>
2024-04-16 17:12:10 +0900

    chore: eslint, prettier, package 기본 설정
    Co-Authored-By: TaehunLee <85233397+todari@users.noreply.github.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,65 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:storybook/recommended",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
-  rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-  },
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:storybook/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'airbnb-base',
+		'airbnb-typescript/base',
+		'prettier',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-refresh'],
+	rules: {
+		'max-depth': ['error', 2],
+		'max-params': ['error', 2],
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+		'import/order': [
+			'error',
+			{
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
+			},
+		],
+		'import/no-unresolved': 'off',
+		'no-param-reassign': 'off',
+		'import/no-extraneous-dependencies': 'off',
+		'no-unused-vars': 'warn',
+		'prettier/prettier': ['error', { endOfLine: 'auto' }],
+		'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'] }],
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				js: 'never',
+				jsx: 'never',
+				ts: 'never',
+				tsx: 'never',
+				'd.ts': 'never',
+				'': 'never',
+			},
+		],
+	},
+	setting: {
+		'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+		'import/parser': {
+			'@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+		},
+		'import/resolver': {
+			node: {
+				extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+				moduleDirectory: ['node_modules', '@types'],
+			},
+			typescript: {},
+		},
+	},
 };
