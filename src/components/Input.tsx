ab244e622a20e1e300732177f46a3e107eab0efc
anttiey <hanuuny@gmail.com>
2024-04-16 18:09:27 +0900

    feat: 카드 소유자 이름 유효성 검사 추가
    Co-authored-by: Seongjin Hong <hongseongjin.to@gmail.com>

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,17 +1,21 @@
 import { InputHTMLAttributes } from 'react';
 import styled from 'styled-components';
 
-const StyledInput = styled.input`
+const StyledInput = styled.input<{ $isValid: boolean }>`
   width: 100%;
   height: 32px;
   padding: 8px;
   border-radius: 2px;
-  border: 1px solid #acacac;
+  border: 1px solid ${(props) => (props.$isValid ? '#acacac' : '#ff3d3d')};
   font-size: 11px;
 `;
 
-function Input({ type, value, placeholder, onChange }: InputHTMLAttributes<HTMLInputElement>) {
-  return <StyledInput type={type} placeholder={placeholder} value={value} onChange={onChange}></StyledInput>;
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  isValid: boolean;
+}
+
+function Input({ type, value, placeholder, onChange, isValid }: InputProps) {
+  return <StyledInput $isValid={isValid} type={type} placeholder={placeholder} value={value} onChange={onChange} />;
 }
 
 export default Input;
