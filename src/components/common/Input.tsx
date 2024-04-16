c86cd981bde3889bc674ca285ce0cdf3cf63b997
rbgksqkr <rbgks1937@naver.com>
2024-04-16 22:22:55 +0900

    refactor(Input): handleChange를 props로 받도록 수정 및 스타일링 추가
    Co-authored-by: Parkhanyoung <phanyoung@naver.com>

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,24 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
 interface InputProps {
+  id: string;
   value: string;
-  setValue: (value: string) => void;
-  validate: (value: string) => boolean;
-  handleError: () => void;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  isError: boolean;
+  placeholder: string;
   width?: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Input = ({ value, setValue, validate, handleError, width = '100%' }: InputProps) => {
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    if (!validate(value)) {
-      handleError();
-      return;
-    }
-
-    setValue(value);
-  };
-
-  return <input value={value} onChange={handleChange} />;
+const Input = ({ id, value, handleChange, isError, placeholder, width = '100%' }: InputProps) => {
+  return (
+    <StyledInput
+      id={id}
+      value={value}
+      onChange={handleChange}
+      placeholder={placeholder}
+      width={width}
+      isError={isError}
+    />
+  );
 };
+
+interface StyledInputProps {
+  width: string;
+  isError: boolean;
+}
+
+const StyledInput = styled.input<StyledInputProps>`
+  width: ${props => props.width};
+  padding: 12px 10px;
+
+  border: 1.2px solid ${props => (props.isError ? '#ff3d3d' : '#acacac')};
+  border-radius: 5px;
+  font-size: 16px;
+
+  &::placeholder {
+    color: #acacac;
+  }
+  &:focus {
+    outline: none;
+  }
+`;
 
 export default Input;
