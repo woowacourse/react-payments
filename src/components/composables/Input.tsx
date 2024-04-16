cded571783ad3c473c8038a6fc92407757cbb2dd
Largopie <106071687+Largopie@users.noreply.github.com>
2024-04-16 19:11:58 +0900

    feat: 공용 인풋 컴포넌트 생성
    Co-authored-by: brgndyy <brgndyy@gmail.com>

---
+++ src/components/composables/Input.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+type InputProps = {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type: 'text' | 'number' | 'email' | 'password';
+  placeholder: string;
+  id: string;
+};
+
+const StyledInput = styled.input`
+  border: 1px solid rgba(172, 172, 172, 1);
+  padding: 8px;
+  font-size: 0.6875rem;
+  border-radius: 2px;
+  height: 32px;
+`;
+
+export default function Input({ value, onChange, type, placeholder, id }: InputProps) {
+  return (
+    <StyledInput value={value} onChange={onChange} type={type} placeholder={placeholder} id={id} />
+  );
+}
