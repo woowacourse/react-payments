0240a6800b05d6257189ed7f74367a014a2cc7f4
anttiey <hanuuny@gmail.com>
2024-04-16 16:12:36 +0900

    feat: CardPreviewBox 컴포넌트 생성 및 전체 구조 설계
    Co-authored-by: Seongjin Hong <hongseongjin.to@gmail.com>

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,2 @@
-export { default as Dot } from './images/Dot.svg';
 export { default as MasterCard } from './images/Mastercard.png';
 export { default as Visa } from './images/Visa.png';
---
+++ src/components/CardPreviewBox.tsx
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+import { Visa, MasterCard } from '../assets';
+
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+const Card = styled.div`
+  width: 212px;
+  height: 132px;
+  border-radius: 4px;
+  padding: 8px 12px;
+  background: #333333;
+  box-shadow: 3px 3px 5px 0px #00000040;
+`;
+
+const CardHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Chip = styled.div`
+  width: 36px;
+  height: 22px;
+  border-radius: 4px;
+  background: #ddcd78;
+  border: 0.5px solid #ddcd781a;
+`;
+
+const Logo = styled.div`
+  width: 36px;
+  height: 22px;
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 8px;
+  padding: 14px 5px;
+`;
+
+const Info = styled.p`
+  color: white;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  letter-spacing: 0.16em;
+  text-align: left;
+`;
+
+function CardPreviewBox() {
+  return (
+    <CardContainer>
+      <Card>
+        <CardHeader>
+          <Chip></Chip>
+          <Logo>
+            <img src={Visa} />
+          </Logo>
+        </CardHeader>
+        <InfoContainer>
+          <Info>1111 2222 3333 4444</Info>
+          <Info>04/21</Info>
+          <Info>LIM DONGJUN</Info>
+        </InfoContainer>
+      </Card>
+    </CardContainer>
+  );
+}
+
+export default CardPreviewBox;
