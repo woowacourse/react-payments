bbbeccbd88b3e2d25b60c57f03250e5f518c20a8
GimGayeon <kky1373@naver.com>
2024-04-16 20:40:25 +0900

    feat(InputGroup): input 개별 입력값 상태 관리 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { useState } from 'react';
 import './App.css';
 import InputGroup from './components/InputGroup';
-import { CARD_NUMBER, CARD_OWNER, CARD_PERIOD } from './constants/inputInfomation';
+import { CARD_NUMBER, CARD_OWNER, CARD_PERIOD } from './constants/inputInformation';
 
 function App() {
-  const [cardNumber, setCardNumber] = useState<string>('');
-  const [cardPeriod, setCardPeriod] = useState<string>('');
-  const [cardOwner, setCardOwner] = useState<string>('');
+  const [cardNumber, setCardNumber] = useState<string[]>(['']);
+  const [cardPeriod, setCardPeriod] = useState<string[]>(['']);
+  const [cardOwner, setCardOwner] = useState<string[]>(['']);
 
   return (
     <>
-      <InputGroup setState={(e) => setCardNumber(e)} inputType={CARD_NUMBER} />
+      <InputGroup setState={setCardNumber} inputType={CARD_NUMBER} />
       {cardNumber}
-      <InputGroup setState={(e) => setCardPeriod(e)} inputType={CARD_PERIOD} />
+      <InputGroup setState={setCardPeriod} inputType={CARD_PERIOD} />
       {cardPeriod}
-      <InputGroup setState={(e) => setCardOwner(e)} inputType={CARD_OWNER} />
+      <InputGroup setState={setCardOwner} inputType={CARD_OWNER} />
       {cardOwner}
     </>
   );
--- src/components/InputGroup.tsx
+++ src/components/InputGroup.tsx
@@ -1,21 +1,29 @@
 import { css } from '@emotion/react';
 import InputTitle from './InputTitle';
 import Input from './Input';
-import { CARD_NUMBER, CARD_OWNER, CARD_PERIOD } from '../constants/inputInfomation';
+import { CARD_NUMBER, CARD_OWNER, CARD_PERIOD } from '../constants/inputInformation';
 
 interface InputGroupType {
-  setState: (s: string) => void;
+  setState: React.Dispatch<React.SetStateAction<string[]>>;
   inputType: typeof CARD_NUMBER | typeof CARD_PERIOD | typeof CARD_OWNER;
 }
 
 function InputGroup({ setState, inputType }: InputGroupType) {
   const { title, subtitle, label, placeholders } = inputType;
+  const concatStringToArray = (value: string, index: number) => {
+    setState((prop: string[]) => {
+      const copy: string[] = [...prop];
+      copy[index] = value;
+      return copy;
+    });
+  };
+
   return (
     <>
       <InputTitle title={title} subtitle={subtitle} />
       <label htmlFor="">{label}</label>
-      {placeholders.map((placeholder: string) => {
-        return <Input placeholder={placeholder} setState={(e) => setState(e)} />;
+      {placeholders.map((placeholder: string, index: number) => {
+        return <Input key={index} placeholder={placeholder} setState={(e) => concatStringToArray(e, index)} />;
       })}
       {/* <ErrorMessage></ErrorMessage> */}
     </>
