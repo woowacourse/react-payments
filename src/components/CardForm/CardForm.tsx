04ff53436022e82bf626590f91818edb4fd4b7a8
skiende74 <skiende74@gmail.com>
2024-04-16 16:16:11 +0900

    feat(CardForm): CardForm 컴포넌트 생성
    Co-authored-by: ooherin <555ooherin@naver.com>

---
+++ src/components/CardForm/CardForm.tsx
@@ -0,0 +1,18 @@
+import useInput from "../../hooks/useInput";
+
+const cardNumbers: string[] = ["", "", "", ""];
+
+const CardForm = () => {
+  // const {
+  //   cardNumbers: values,
+  //   setCardNumbers: setValues,
+  //   onChangeCardNumbers: onChange,
+  //   errorMsg,
+  // } = useInput({ cardNumbers });
+
+  return cardNumbers.map((numbers, index) => {
+    <input />;
+  });
+};
+
+export default CardForm;
