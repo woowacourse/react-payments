7e5ee96b4b6b34eff2e0e0e2069d594743040e1c
jayming66 <mjw8941@gmail.com>
2024-04-16 16:14:08 +0900

    feat: CardNumbers 컴포넌트
    Co-Authored-By: cys4585 <77481524+cys4585@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import CardNumbers from "./components/CardNumbers";
+import Input from "./components/Input";
 
 function App() {
   return (
     <>
       <h1>React Payments</h1>
+      <CardNumbers></CardNumbers>
     </>
   );
 }
---
+++ src/components/CardNumbers.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Input from "./Input";
+
+export default function CardNumbers() {
+  return (
+    <>
+      <Input placeholder="1234"></Input>
+      <Input placeholder="1234"></Input>
+      <Input placeholder="1234"></Input>
+      <Input placeholder="1234"></Input>
+    </>
+  );
+}
