53e8af04b8cd8870c54ec60d3c54659c99a4ff34
GimGayeon <kky1373@naver.com>
2024-04-16 17:44:28 +0900

    feat(inputInformation): input 상수 추가
    Co-authored-by: Minji <chosim-dvlpr@users.noreply.github.com>

---
+++ src/constants/inputInfomation.ts
@@ -0,0 +1,19 @@
+export const CARD_NUMBER = {
+  title: '결제할 카드 번호를 입력해 주세요',
+  subtitle: '본인 명의의 카드만 결제 가능합니다.',
+  label: '카드 번호',
+  placeholder: ['1234', '1234', '1234', '1234'],
+} as const;
+
+export const CARD_PERIOD = {
+  title: '카드 유효기간을 입력해 주세요',
+  subtitle: '월/년도(MMYY)를 순서대로 입력해 주세요.',
+  label: '유효기간',
+  placeholder: ['MM', 'YY'],
+} as const;
+
+export const CARD_OWNER = {
+  title: '카드 소유자 이름을 입력해 주세요',
+  label: '소유자 이름',
+  placeholder: ['WOOTECO'],
+} as const;
