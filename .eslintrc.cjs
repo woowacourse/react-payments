565fdd07988f26a40a32e0ab94c685cba597a379
rbgksqkr <rbgks1937@naver.com>
2024-04-16 17:59:38 +0900

    chore(eslint): eslint에 prettier 플러그인 설정
    Co-authored-by: Parkhanyoung <phanyoung@naver.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    "plugin:prettier/recommended",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
