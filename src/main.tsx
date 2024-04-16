52cd651a09c5b9ed2f65ba61d7e625fcfca428e7
김진호 <rlawlsgh1227@gmail.com>
2024-04-16 15:48:55 +0900

    design: 전역 스타일 설정
    Co-authored-by: simorimi <hori5020@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import theme from './styles/theme.ts';
+import GlobalStyle from './styles/GlobalStyle.ts';
+import { ThemeProvider } from 'styled-components';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
