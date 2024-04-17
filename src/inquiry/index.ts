89ca4c43700c3c6abe1590c7fff2a7a042fe3fb6
rbgksqkr <rbgks1937@naver.com>
2024-04-17 19:26:14 +0900

    chore(inquireCardHolderName): 불필요한 입력값 길이 검증 로직 제거
    Co-authored-by: Parkhanyoung <phanyoung@naver.com>
    
    Co-authored-by: Parkhanyoung <phanyoung@naver.com>

--- src/inquiry/index.ts
+++ src/inquiry/index.ts
@@ -1,5 +1,6 @@
 import inquireCardholderName from './inquireCardholderName';
 import inquireExpiryMonth from './inquireExpiryMonth';
 import inquireExpiryYear from './inquireExpiryYear';
+import inquireCardNumber from './inquireCardNumber';
 
-export { inquireCardholderName, inquireExpiryMonth, inquireExpiryYear };
+export { inquireCardholderName, inquireExpiryMonth, inquireExpiryYear, inquireCardNumber };
