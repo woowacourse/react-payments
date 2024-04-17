8c73e29c3626663dd0799d80738c1d5d1251b3f2
greetings1012 <ahnqwerty@gmail.com>
2024-04-17 22:22:07 +0900

    feat: 카드 소유자 이름 에러 핸들링 구현

--- src/components/CardOwnerNameInputField.tsx
+++ src/components/CardOwnerNameInputField.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import Input from "./Input";
 
 const OWNER_NAME_PLACEHOLDER = "JOHN DOE";
@@ -10,10 +10,46 @@ export default function CardOwnerNameInputField({
   ownerName: string;
   setOwnerName: Dispatch<SetStateAction<string>>;
 }) {
-  const handleChange = (e) => {
-    const updateOwnerName = e.target.value;
-    setOwnerName(updateOwnerName.toUpperCase());
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const normalizeSpaces = (str: string): string => {
+    return str.replace(/\s+/g, " ").trimStart();
   };
+
+  const filterEnglish = (str: string) => {
+    return str
+      .split("")
+      .filter((char: string) => {
+        if (
+          char === " " ||
+          (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90)
+        )
+          return true;
+        return false;
+      })
+      .join("");
+  };
+
+  const handleChange = (e) => {
+    const onlyEngStr = filterEnglish(e.target.value.toUpperCase());
+
+    if (onlyEngStr.length < e.target.value.length) {
+      setErrorMessage("영어만 입력 가능합니다.");
+      setOwnerName(onlyEngStr);
+      return;
+    }
+
+    const normalizedOwnerName = normalizeSpaces(onlyEngStr);
+    if (onlyEngStr.length > normalizedOwnerName.length) {
+      setErrorMessage("공백을 연속으로 입력할 수 없습니다.");
+      return;
+    }
+
+    setErrorMessage("");
+
+    setOwnerName(normalizedOwnerName);
+  };
+
   return (
     <>
       <div>소유자 이름</div>
@@ -23,9 +59,10 @@ export default function CardOwnerNameInputField({
           placeholder={OWNER_NAME_PLACEHOLDER}
           maxLength={26} // 비자 21자, 마스터카드 26자!
           style={{ textTransform: "uppercase" }}
+          value={ownerName}
         />
       </div>
-      <div>에러메세지</div>
+      {errorMessage !== "" && <div>{errorMessage}</div>}
     </>
   );
 }
