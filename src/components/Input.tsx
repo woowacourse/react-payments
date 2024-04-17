9900629d992ffc19561983a922c26c004457036d
greetings1012 <ahnqwerty@gmail.com>
2024-04-17 22:21:35 +0900

    feat: 에러 발생 시 테두리 색상 빨간색으로 바뀌는 기능 구현

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,16 @@
+import { CSSProperties } from "react";
+
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   // checkError?: React.ChangeEventHandler<HTMLInputElement>;
+  isError?: boolean;
 }
 
 export default function Input(props: InputProps) {
   const { onChange, ...rest } = props;
 
-  return <input {...rest} onChange={onChange} />;
+  const inputStyle: CSSProperties = {
+    border: `4px solid ${props.isError ? "red" : "grey"}`,
+  };
+
+  return <input {...rest} onChange={onChange} style={inputStyle} />;
 }
