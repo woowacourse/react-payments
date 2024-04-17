4b7c3136ff2731b522a5398963ae35da45a93fb0
greetings1012 <ahnqwerty@gmail.com>
2024-04-17 22:19:12 +0900

    feat: date 형식 및 image 링크 상수로 선언

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,12 @@
+export const IMAGE_URL = {
+  mastercard: "/src/assets/image/Mastercard.png",
+  visa: "/src/assets/image/Visa.png",
+};
+
 export const CARD_NUMBER_UNIT_LENGTH = 4;
 export const CARD_NUMBER_UNIT_PLACEHOLDER = "1234";
-export const DATE_PLACEHOLDER = ["MM", "YY"];
+export const DATE_PLACEHOLDER = {
+  MONTH: "MM",
+  YEAR: "YY",
+};
 export const OWNER_NAME_PLACEHOLDER = "JOHN DOE";
