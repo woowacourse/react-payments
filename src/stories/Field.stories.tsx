b62247837cac615f60c34602b06c4c12cd70f42b
Kim Da Eun <midekuna@gmail.com>
2024-04-17 16:54:24 +0900

    test: 카드 만료일, 소유자 이름 스토리 추가
    Co-Authored-By: novice0840 <111696934+novice0840@users.noreply.github.com>

--- src/stories/Field.stories.tsx
+++ src/stories/Field.stories.tsx
@@ -11,6 +11,7 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const CARD_NUMBERS_FIELDS = ['first', 'second', 'third', 'fourth'];
 export const CardNumbers: Story = {
   args: {
     title: '결제할 카드 번호를 입력해 주세요',
@@ -19,15 +20,64 @@ export const CardNumbers: Story = {
     errMsg: '',
     children: (
       <>
-        {Array.from({ length: 4 }).map(() => (
+        {CARD_NUMBERS_FIELDS.map((name, index) => (
           <Input
-            name="name"
+            key={index}
+            name={name}
             placeholder="1234"
             value=""
             isError={false}
             handleChange={() => 1}
             handleOnBlur={() => 1}
             maxLength={4}
+          ></Input>
+        ))}
+      </>
+    ),
+  },
+};
+
+const EXPIRATION_DATE_FIELDS = ['month', 'year'];
+export const ExpirationDate: Story = {
+  args: {
+    title: '카드 유효기간을 입력해 주세요',
+    description: '월/년도(MMYY)를 순서대로 입력해 주세요',
+    labelText: '유효기간',
+    errMsg: '',
+    children: (
+      <>
+        {EXPIRATION_DATE_FIELDS.map((name, index) => (
+          <Input
+            key={index}
+            name={name}
+            placeholder={name === 'month' ? 'MM' : 'YY'}
+            value=""
+            isError={false}
+            handleChange={() => 1}
+            handleOnBlur={() => 1}
+            maxLength={4}
+          ></Input>
+        ))}
+      </>
+    ),
+  },
+};
+
+export const OwnerName: Story = {
+  args: {
+    title: '카드 소유자 이름을 입력해 주세요',
+    labelText: '소유자 이름',
+    errMsg: '',
+    children: (
+      <>
+        {Array.from({ length: 1 }).map(() => (
+          <Input
+            name="ownerName"
+            placeholder="JOHN DOE"
+            value=""
+            isError={false}
+            handleChange={() => 1}
+            handleOnBlur={() => 1}
           ></Input>
         ))}
       </>
