bc50a6a90b26f80196a82d19c1e56ff389c7e8bb
00kang <kdb070970@gmail.com>
2024-04-17 17:40:31 +0900

    feat : 재사용 가능한 InputBox 컴포넌트 구현

---
+++ src/components/input/common/InputBox.tsx
@@ -0,0 +1,39 @@
+interface InputBoxProps {
+  inputValue: string;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  size: "small" | "medium" | "large";
+  placeholder: string;
+  id: string;
+  name: string;
+}
+
+const InputBox = ({
+  inputValue,
+  handleChange,
+  size,
+  placeholder,
+  id,
+  name,
+}: InputBoxProps) => {
+  const sizeWidthMap = {
+    small: 20,
+    medium: 45,
+    large: 100,
+  };
+
+  const widthPercentage = sizeWidthMap[size];
+
+  return (
+    <input
+      type="text"
+      value={inputValue}
+      onChange={handleChange}
+      style={{ width: `${widthPercentage}%` }}
+      placeholder={placeholder}
+      id={id}
+      name={name}
+    />
+  );
+};
+
+export default InputBox;
