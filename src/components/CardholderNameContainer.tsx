c0152c40bc20745a85e1bfa64ebf15a2161c8ac2
rbgksqkr <rbgks1937@naver.com>
2024-04-17 15:28:42 +0900

    feat(CardholderNameContainer): onBlur 핸들러로updateErrorMessage 등록
    Co-authored-by: Parkhanyoung <phanyoung@naver.com>

--- src/components/CardholderNameContainer.tsx
+++ src/components/CardholderNameContainer.tsx
@@ -4,20 +4,28 @@ import Input from './common/Input';
 interface CardholderNameContainerProps {
   cardholderName: string;
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  updateErrorMessage: () => void;
   errorMessage: string;
 }
 
-const CardholderNameContainer = ({ cardholderName, handleChange, errorMessage }: CardholderNameContainerProps) => {
+const CardholderNameContainer = ({
+  cardholderName,
+  handleChange,
+  updateErrorMessage,
+  errorMessage,
+}: CardholderNameContainerProps) => {
   return (
     <>
       <h1>카드 소유자 이름을 입력해주세요</h1>
       <label htmlFor="cardholder-name-input">소유자 이름</label>
       <Input
         id="cardholder-name-input"
-        value={cardholderName}
-        handleChange={handleChange}
         isError={!!errorMessage}
+        value={cardholderName}
+        onChange={handleChange}
+        onBlur={updateErrorMessage}
         placeholder="카드 소유자 이름을 입력해주세요"
+        width="100%"
       />
       <ErrorText>{errorMessage}</ErrorText>
     </>
