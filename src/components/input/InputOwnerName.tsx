031e79a70cc31376911121ff22abd2434d4841a9
00kang <kdb070970@gmail.com>
2024-04-17 17:43:38 +0900

    feat : 카드 소유자 이름 입력 기능 구현

--- src/components/input/InputOwnerName.tsx
+++ src/components/input/InputOwnerName.tsx
@@ -1,17 +1,15 @@
-import styled from "@emotion/styled";
-import CARD_FORM_MESSAGE from "../../constants/cardFormMessage";
-import CARD_INPUTBOX_NAME from "../../constants/cardInputBoxName";
 import InputBox from "./common/InputBox";
+import CARD_FORM_MESSAGE from "../../constants/cardFormMessage";
+import styled from "@emotion/styled";
 
 interface InputOwnerNameProps {
   inputValue: string;
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  inputError: boolean;
 }
 
-const InputOwnerName = ({ inputValue, handleChange, inputError }: InputOwnerNameProps) => {
+const InputOwnerName = ({ inputValue, handleChange }: InputOwnerNameProps) => {
   return (
-    <InputContainer>
+    <>
       <InputLabel htmlFor="ownerName">{CARD_FORM_MESSAGE.cardOwner}</InputLabel>
       <InputBox
         inputValue={inputValue}
@@ -19,19 +17,13 @@ const InputOwnerName = ({ inputValue, handleChange, inputError }: InputOwnerName
         size="large"
         placeholder="JOHN DOE"
         id="ownerName"
-        name={CARD_INPUTBOX_NAME.owner.name}
-        isError={inputError}
+        name="name"
       />
-    </InputContainer>
+    </>
   );
 };
 
 export default InputOwnerName;
-
-const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
 
 const InputLabel = styled.label`
   font-family: Noto Sans;
@@ -40,5 +32,4 @@ const InputLabel = styled.label`
   line-height: 15px;
   text-align: left;
   color: rgba(10, 13, 19, 1);
-  margin-bottom: 8px;
 `;
