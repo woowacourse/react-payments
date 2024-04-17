8b88e3ffd78385bdba21a4b2b971b6a9dc86d709
Hyelim Choi <HyelimChoi01@gmail.com>
2024-04-17 16:43:18 +0900

    chore: reset.css 임포트 위치 변경
    Co-Authored-By: jaeml06 <107801932+jaeml06@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import "./reset.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './reset.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
