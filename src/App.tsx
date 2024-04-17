8c8ca0e09b35029dcb5b2226765683094a61cc93
rbgksqkr <rbgks1937@naver.com>
2024-04-17 19:30:11 +0900

    feat(App): CardNumberContainer 컴포넌트 등록
    Co-authored-by: Parkhanyoung <phanyoung@naver.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import './App.css';
 import useInput from './hooks/useInput';
+import useInputs from './hooks/useInputs';
 import CardholderNameContainer from './components/CardholderNameContainer';
 import CardExpiryDateContainer from './components/CardExpiryDateContainer';
-import { inquireCardholderName, inquireExpiryMonth, inquireExpiryYear } from './inquiry';
+import { inquireCardNumber, inquireCardholderName, inquireExpiryMonth, inquireExpiryYear } from './inquiry';
+import CardNumberContainer from './components/CardNumbersContainer';
 
 const App = () => {
+  const {
+    value: cardNumbers,
+    generateChangeHandler: generateCardNumbersChangeHandler,
+    generateErrorMessageUpdater: generateCardNumberErrorMessageUpdater,
+    errorMessage: cardNumbersErrorMessage,
+  } = useInputs(
+    {
+      first: '',
+      second: '',
+      third: '',
+      fourth: '',
+    },
+    inquireCardNumber,
+  );
+
   const {
     value: cardholderName,
     handleChange: handleChangeCardholderName,
@@ -28,17 +45,23 @@ const App = () => {
 
   return (
     <>
-      <CardholderNameContainer
-        cardholderName={cardholderName}
-        handleChange={handleChangeCardholderName}
-        updateErrorMessage={updateCardholderNameErrorMessage}
-        errorMessage={cardholderNameErrorMessage}
+      <CardNumberContainer
+        cardNumbers={cardNumbers}
+        generateChangeHandler={generateCardNumbersChangeHandler}
+        generateErrorMessageUpdater={generateCardNumberErrorMessageUpdater}
+        errorMessage={cardNumbersErrorMessage}
       />
       <CardExpiryDateContainer
         expiryDate={{ month: expiryMonth, year: expiryYear }}
         changeHandler={{ month: handleChangeExpiryMonth, year: handleChangeExpiryYear }}
         errorMessageUpdater={{ month: updateExpiryMonthErrorMessage, year: updateExpiryYearErrorMessage }}
         errorMessage={{ month: expiryMonthErrorMessage, year: expiryYearErrorMessage }}
+      />
+      <CardholderNameContainer
+        cardholderName={cardholderName}
+        handleChange={handleChangeCardholderName}
+        updateErrorMessage={updateCardholderNameErrorMessage}
+        errorMessage={cardholderNameErrorMessage}
       />
     </>
   );
