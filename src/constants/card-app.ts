b0f12fac0cbdd7749154f5f92fecc9a2ad04fba7
hwinkr <dnddl8280@naver.com>
2024-04-17 22:43:02 +0900

    chore(constants): 카드 앱에서 사용하는 상수 설정

---
+++ src/constants/card-app.ts
@@ -0,0 +1,15 @@
+export const INPUT_QUERY = {
+  cardNumberInput: "결제할 카드 번호를 입력해 주세요.",
+  expirationDate: "카드 유효기간을 입력해 주세요.",
+  cardOwner: "카드 소유자 이름을 입력해 주세요.",
+};
+
+export const CAPTION = {
+  cardNumberInput: "본인 명의의 카드만 결제 가능합니다.",
+  expirationDate: "월/년도(MMYY)를 순서대로 입력해 주세요.",
+};
+
+export const VALIDATION_MESSAGES = {
+  onlyNumbersAllowed: "숫자만 입력 가능합니다.",
+  onlyEnglishAllowed: "영문만 입력 가능합니다.",
+};
