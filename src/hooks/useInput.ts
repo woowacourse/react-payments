af864605b5ad8dd7e0f5e32acb9528803575c6a1
00kang <kdb070970@gmail.com>
2024-04-17 17:39:09 +0900

    feat : input을 입력받는 커스텀 훅 구현

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,29 +1,18 @@
 import { useState } from "react";
-import CARD_INPUTBOX_NAME from "../constants/cardInputBoxName";
-import Validator from "../utils/Validator";
 
 const useInput = <T extends object>(initialValue: T) => {
   const [inputValue, setInputValue] = useState<T>(initialValue);
-  const [isInputError, setIsInputError] = useState<boolean>(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value, name } = e.target;
 
-    const { isError, isValid } = Validator.inputCreditCardInfo(value, name);
-    if (isError) return setIsInputError(true);
-    if (!isValid) return;
-
-    const validValue = name === CARD_INPUTBOX_NAME.owner.name ? value.toUpperCase() : value;
-
     setInputValue((prevState) => ({
       ...prevState,
-      [name]: validValue,
+      [name]: value,
     }));
-
-    setIsInputError(false);
   };
 
-  return [inputValue, handleChange, isInputError] as const;
+  return [inputValue, handleChange] as const;
 };
 
 export default useInput;
