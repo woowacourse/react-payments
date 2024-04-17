7fe9c2a79f20cb35d5d9de3c26772efd2b84b4f6
anttiey <hanuuny@gmail.com>
2024-04-17 16:29:43 +0900

    test: Input, InputField 스토리북 테스트 추가
    Co-authored-by: Seongjin Hong <hongseongjin.to@gmail.com>

---
+++ src/stories/InputField.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import InputField from '../components/InputField';
+import Input from '../components/Input';
+
+const meta = {
+  title: 'InputField',
+  component: InputField,
+} satisfies Meta<typeof InputField>;
+
+export default meta;
+type Story = StoryObj<typeof InputField>;
+
+export const Default: Story = {
+  args: {
+    label: '예시',
+    errorMessage: '',
+    length: 1,
+    children: <Input isValid={true} type="text" placeholder="sample" />,
+  },
+};
+
+export const Error: Story = {
+  args: {
+    label: '예시',
+    errorMessage: '잘못된 입력입니다.',
+    length: 1,
+    children: <Input isValid={false} type="text" placeholder="sample" />,
+  },
+};
