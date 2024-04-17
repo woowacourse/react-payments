5dc66c719824c54d7da2858df7d493feb12d0df8
anttiey <hanuuny@gmail.com>
2024-04-17 15:15:56 +0900

    refactor: TitleContainer 컴포넌트 분리
    Co-authored-by: Seongjin Hong <hongseongjin.to@gmail.com>

---
+++ src/styles/TitleContainer.style.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 3px;
+  margin-bottom: 16px;
+`;
+
+export const Title = styled.p`
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 26px;
+`;
+
+export const SubTitle = styled.p`
+  color: #8b95a1;
+  font-size: 10px;
+  font-weight: 400;
+  line-height: 13px;
+`;
--- src/styles/common.style.tsx
+++ src/styles/common.style.tsx
@@ -1,25 +1,5 @@
 import styled from 'styled-components';
 
-export const TitleContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 3px;
-  margin-bottom: 16px;
-`;
-
-export const Title = styled.p`
-  font-size: 18px;
-  font-weight: 700;
-  line-height: 26px;
-`;
-
-export const SubTitle = styled.p`
-  color: #8b95a1;
-  font-size: 10px;
-  font-weight: 400;
-  line-height: 13px;
-`;
-
 export const InputContainer = styled.div<{ $length?: number }>`
   display: grid;
   grid-template-columns: ${(props) => `repeat(${props.$length}, 1fr)`};
