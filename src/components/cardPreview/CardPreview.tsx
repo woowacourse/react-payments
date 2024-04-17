47521f24d13a3a590b3da5b9aed9ab495771b36c
jinsil <jinsil378900@gmail.com>
2024-04-17 21:51:04 +0900

    feat: 카드 번호에 따른 카드 브랜드 로고 표시

--- src/components/cardPreview/CardPreview.tsx
+++ src/components/cardPreview/CardPreview.tsx
@@ -1,26 +1,63 @@
+import { useEffect, useState } from "react";
 import { ICardInfo } from "../../types/type";
+import * as Styled from "./CardPreview.styles";
+import MasterCardImage from "../../assets/Mastercard.png";
+import VisaImage from "../../assets/Visa.png";
+import CardNumbers from "../cardNumbers/CardNumbers";
+import CardExpiration from "../cardExpiration/CardExpiration";
 
 interface Props {
   cardInfo: ICardInfo;
 }
 
 const CardPreview = ({ cardInfo }: Props) => {
+  const [cardBrand, setCardBrand] = useState("");
+
+  useEffect(() => {
+    const firstNumber = Math.floor(cardInfo.cardNumbers[0] / 100);
+
+    if (firstNumber >= 40 && firstNumber <= 49) {
+      setCardBrand("MasterCard");
+    } else if (firstNumber >= 51 && firstNumber <= 55) {
+      setCardBrand("Visa");
+    } else {
+      setCardBrand("");
+    }
+  }, [cardInfo.cardNumbers]);
+
+  const getCardBrandImage = () => {
+    switch (cardBrand) {
+      case "MasterCard":
+        return MasterCardImage;
+      case "Visa":
+        return VisaImage;
+      default:
+        return "";
+    }
+  };
+
   return (
-    <>
-      <div>
-        {cardInfo.cardNumbers.map((cardNumber, index) => {
-          const cardNumberLength = cardNumber.toString().length;
-          if (index > 1) {
-            return <span key={index}>{cardNumber !== 0 && "●".repeat(cardNumberLength)}</span>;
-          }
-          return <span key={index}>{cardNumber !== 0 && cardNumber}</span>;
-        })}
-        <div>
-          {cardInfo.cardExpiration["month"] !== 0 && cardInfo.cardExpiration["month"] + "/"}
-          {cardInfo.cardExpiration["year"] !== 0 && cardInfo.cardExpiration["year"]}
-        </div>
-      </div>
-    </>
+    <Styled.CardPreviewContainer>
+      <Styled.ChipSection>
+        <Styled.ICChip></Styled.ICChip>
+        {cardBrand && (
+          <Styled.CardBrand>
+            <img
+              src={getCardBrandImage()}
+              alt={cardBrand}
+            />
+          </Styled.CardBrand>
+        )}
+      </Styled.ChipSection>
+      <Styled.CardInfoSection>
+        <CardNumbers cardNumbers={cardInfo.cardNumbers} />
+        <CardExpiration
+          month={cardInfo.cardExpiration["month"]}
+          year={cardInfo.cardExpiration["year"]}
+        />
+        <div>{cardInfo.userName && cardInfo.userName}</div>
+      </Styled.CardInfoSection>
+    </Styled.CardPreviewContainer>
   );
 };
 
