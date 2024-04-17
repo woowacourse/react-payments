c394cc9f4502fa3a8ee59691d4bd7c33a43fddba
GimGayeon <kky1373@naver.com>
2024-04-17 17:54:29 +0900

    feat(isValidateInput): 유효성 검증 로직 구현
    Co-authored-by: Minji <chosim-dvlpr@users.noreply.github.com>

--- src/validations/isValidateInput.tsx
+++ src/validations/isValidateInput.tsx
@@ -1,30 +1,74 @@
-import { SectionType } from '../types/cardType';
-
 interface isValidateInputTableType {
-  number: () => boolean;
-  period: () => boolean;
-  owner: () => boolean;
+  number: () => void;
+  month: () => void;
+  year: () => void;
+  owner: () => void;
 }
 
-const isValidateInput = (value: string, section: SectionType): boolean => {
+const isValidateInput = (value: string, section: 'number' | 'month' | 'year' | 'owner'): boolean => {
   const isValidateInputTable: isValidateInputTableType = {
-    number: () => A(),
-    period: () => B(),
-    owner: () => C(),
+    number: () => isCardNumberValidated(value),
+    month: () => isCardMonthValidated(value),
+    year: () => isCardYearValidated(value),
+    owner: () => isCardOwnerValidated(value),
   };
 
-  const a = isValidateInputTable[section];
-  return a();
+  const validateFunction = isValidateInputTable[section];
+  validateFunction();
 };
 
-function A() {
+function isCardNumberValidated(value: string) {
+  const valueToNumber = Number(value);
+  isNumber(valueToNumber);
+  isNumberCount(value, 4);
+}
+
+function isCardMonthValidated(value: string) {
+  const valueToNumber = Number(value);
+  isNumber(valueToNumber);
+  isInRange(valueToNumber, 1, 12);
+}
+
+function isCardYearValidated(value: string) {
+  const valueToNumber = Number(value);
+  const now = new Date();
+  const year = now.getFullYear();
+  const lastTwoDigits = year % 100;
+  isNumber(valueToNumber);
+  isInRange(valueToNumber, lastTwoDigits, lastTwoDigits + 10);
+}
+
+function isCardOwnerValidated(value: string) {
+  isUpperCase(value);
+  isInRange(value.length, 1, 30);
+}
+
+function isNumber(value: number) {
+  if (isNaN(value)) {
+    throw new Error('숫자를 입력해주세요.');
+  }
   return true;
 }
 
-function B() {
+function isInRange(value: number, min: number, max: number) {
+  if (value < min || value > max) {
+    throw new Error(`${min} 이상 ${max} 이하로 작성해주세요.`);
+  }
   return true;
 }
-function C() {
+
+function isUpperCase(value: string) {
+  if (value !== value.toUpperCase()) {
+    throw new Error('이름은 영대문자로 입력해주세요.');
+  }
+  return true;
+}
+
+function isNumberCount(value: string, count: number) {
+  const valueLength = value.length;
+  if (valueLength !== count) {
+    throw new Error(`${count}글자를 입력해주세요.`);
+  }
   return true;
 }
 
