1e9afc56f1f8b3436c3acee71370359c02f8cacf
00kang <kdb070970@gmail.com>
2024-04-17 20:12:07 +0900

    style : InputBox 컴포넌트 스타일 구현

--- src/components/input/common/InputBox.tsx
+++ src/components/input/common/InputBox.tsx
@@ -1,10 +1,12 @@
+import styled from "@emotion/styled";
+
 interface InputBoxProps {
   inputValue: string;
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   size: "small" | "medium" | "large";
   placeholder: string;
-  id: string;
-  name: string;
+  id?: string;
+  name?: string;
 }
 
 const InputBox = ({
@@ -12,28 +14,42 @@ const InputBox = ({
   handleChange,
   size,
   placeholder,
-  id,
-  name,
+  ...props
 }: InputBoxProps) => {
   const sizeWidthMap = {
-    small: 20,
-    medium: 45,
+    small: 23,
+    medium: 48,
     large: 100,
   };
 
   const widthPercentage = sizeWidthMap[size];
 
   return (
-    <input
-      type="text"
+    <Input
       value={inputValue}
       onChange={handleChange}
       style={{ width: `${widthPercentage}%` }}
       placeholder={placeholder}
-      id={id}
-      name={name}
+      {...props}
     />
   );
 };
 
 export default InputBox;
+
+const Input = styled.input`
+  border: 1.01px solid rgba(172, 172, 172, 1);
+  height: 32px;
+  padding: 8px;
+  border-radius: 2px;
+  box-sizing: border-box;
+  font-family: Inter;
+  font-size: 11px;
+  font-weight: 400;
+  line-height: 14.88px;
+  text-align: left;
+
+  ::placeholder {
+    color: rgba(172, 172, 172, 1);
+  }
+`;
