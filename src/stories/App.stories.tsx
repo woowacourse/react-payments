e96affdc3235d20d80435a2860898009d76f2752
anttiey <hanuuny@gmail.com>
2024-04-17 17:04:39 +0900

    test: 테스트 docs 파일 추가
    Co-authored-by: Seongjin Hong <hongseongjin.to@gmail.com>

---
+++ src/stories/App.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import App from '../App';
+
+const meta = {
+  title: 'App',
+  component: App,
+  tags: ['autodocs'],
+} satisfies Meta<typeof App>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
---
+++ src/stories/CardOwnerInput.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import CardOwnerInput from '../components/CardOwnerInput';
+
+const meta = {
+  title: 'CardOwnerInput',
+  component: CardOwnerInput,
+  tags: ['autodocs'],
+} satisfies Meta<typeof CardOwnerInput>;
+
+export default meta;
+
+type Story = StoryObj<typeof CardOwnerInput>;
+
+export const Default: Story = {
+  args: {
+    setOwner: () => {},
+  },
+};
