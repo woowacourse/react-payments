17fabd60151b21175f99f1b1a8e1316fcb0a7ccb
Largopie <106071687+Largopie@users.noreply.github.com>
2024-04-18 14:17:35 +0900

    feat(validate): 유효성 검사 추가
    - 숫자 유효성 검증
    - 비자 카드 유효성 검증
    - 마스터 카드 유효성 검증
    - 영어 유효성 검증
    
    Co-authored-by: brgndyy <brgndyy@gmail.com>

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -10,6 +10,22 @@ const validate = {
   }) => {
     return compareNumber >= min && compareNumber <= max;
   },
+
+  isValidDigit: (value: string) => {
+    return /^\d+$/.test(value);
+  },
+
+  isVisa: (value: string) => {
+    return /\b4\d{15}\b/.test(value);
+  },
+
+  isMasterCard: (value: string) => {
+    return /\b5[1-5]\d{14}\b/.test(value);
+  },
+
+  isEnglish: (value: string) => {
+    return /^[a-zA-Z ]*$/.test(value);
+  },
 };
 
 export default validate;
