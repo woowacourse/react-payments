826b46c6f1955a80e1258fb98ec589c298529287
lurgi <wahoo9040@naver.com>
2024-04-18 09:14:51 +0900

    feat: creaditcard컴포넌트 수정
    Co-authored-by: river <0jenn0@users.noreply.github.com>

--- src/stories/CreditCard.stories.tsx
+++ src/stories/CreditCard.stories.tsx
@@ -18,11 +18,30 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   name: "신용 카드 UI 기본값",
-  args: { cardInfo: {} },
+  args: {
+    cardInfo: {
+      cardNumbers: {
+        firstNumbers: [],
+        secondNumbers: [],
+        thirdNumbers: [],
+        fourthNumbers: [],
+      },
+      cardValidityPeriod: {
+        month: undefined,
+        year: undefined,
+      },
+      ownerName: "",
+    },
+  },
 };
 
 const dummyCardInfo: CardInfo = {
-  cardNumbers: [5, 1, 2, 3, 4, 5, 7, 8, 9, 9, 9, 1, 0, 1, 2, 3],
+  cardNumbers: {
+    firstNumbers: [5, 1, 2, 3],
+    secondNumbers: [4, 5, 7, 8],
+    thirdNumbers: [9, 9, 9, 1],
+    fourthNumbers: [0, 1, 2, 3],
+  },
   cardValidityPeriod: { month: 9, year: 25 },
   ownerName: "River",
 };
@@ -30,34 +49,75 @@ const dummyCardInfo: CardInfo = {
 export const MasterCard: Story = {
   name: "51~56 숫자로 시작하는 숫자라면 마스터 카드 이미지가 나와야 한다",
   args: {
-    cardInfo: { ...dummyCardInfo, cardNumbers: [5, 1, 2, 3, 4, 5, 7, 8, 9, 9, 9, 1, 0, 1, 2, 3] },
+    cardInfo: {
+      ...dummyCardInfo,
+      cardNumbers: {
+        firstNumbers: [5, 4, 2, 3],
+        secondNumbers: [4, 5, 7, 8],
+        thirdNumbers: [9, 1, 0, 1],
+        fourthNumbers: [1, 2, 3, 4],
+      },
+    },
   },
 };
 
 export const CardNumbersUnder16: Story = {
   name: "카드번호가 10자리인 경우",
   args: {
-    cardInfo: { ...dummyCardInfo, cardNumbers: [5, 1, 2, 3, 4, 5, 7, 8, 9, 9, 9, 1, 0, 1] },
+    cardInfo: {
+      ...dummyCardInfo,
+      cardNumbers: {
+        firstNumbers: [5, 4, 2, 3],
+        secondNumbers: [4, 5, 7, 8],
+        thirdNumbers: [9, 1, 0, 1],
+        fourthNumbers: [1, 2],
+      },
+    },
   },
 };
 
 export const VisaCard: Story = {
   name: "4 숫자로 시작하는 숫자라면 비자 카드 이미지가 나와야 한다",
   args: {
-    cardInfo: { ...dummyCardInfo, cardNumbers: [4, 1, 2, 3, 4, 5, 7, 8, 9, 9, 9, 1, 0, 1, 2, 3] },
+    cardInfo: {
+      ...dummyCardInfo,
+      cardNumbers: {
+        firstNumbers: [4, 4, 2, 3],
+        secondNumbers: [4, 5, 7, 8],
+        thirdNumbers: [9, 1, 0, 1],
+        fourthNumbers: [1, 2, 3, 4],
+      },
+    },
   },
 };
 
 export const NoImageCard: Story = {
   name: "비자, 마스터카드가 아니면 이미지가 나오지 않는다.",
   args: {
-    cardInfo: { ...dummyCardInfo, cardNumbers: [3, 1, 2, 3, 4, 5, 7, 8, 9, 9, 9, 1, 0, 1, 2, 3] },
+    cardInfo: {
+      ...dummyCardInfo,
+      cardNumbers: {
+        firstNumbers: [3, 4, 2, 3],
+        secondNumbers: [4, 5, 7, 8],
+        thirdNumbers: [9, 1, 0, 1],
+        fourthNumbers: [1, 2, 3, 4],
+      },
+    },
   },
 };
 
 export const NoNumbersButPeriod: Story = {
   name: "기한만 제공될 경우 ui가 잘 나와야 한다.",
   args: {
-    cardInfo: { cardValidityPeriod: { month: 9 } },
+    cardInfo: {
+      cardNumbers: {
+        firstNumbers: [],
+        secondNumbers: [],
+        thirdNumbers: [],
+        fourthNumbers: [],
+      },
+      cardValidityPeriod: { month: 9 },
+      ownerName: "",
+    },
   },
 };
