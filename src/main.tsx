f155f615a6926467f64f5ed4efb2463dd88004dc
손진영 <sm099@naver.com>
2024-04-18 09:16:01 +0900

    design: 공통 CSS main.tsx 내 추가
    Co-authored-by: vi-wolhwa <cerezo00@naver.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
+import '@styles/reset.css';
+import '@styles/index.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
