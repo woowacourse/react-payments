91624a45f24d06a00999de9c33afbfd121f9178a
Kim Da Eun <midekuna@gmail.com>
2024-04-18 15:39:03 +0900

    chore: storybook Preview 설정에서 Date matcher 제거
    Co-Authored-By: novice0840 <111696934+novice0840@users.noreply.github.com>

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,10 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
       },
     },
   },
