c50182471d91547b1478423a436fe2a80815aa98
ImxYJL <allensain14@gmail.com>
2024-04-18 14:16:03 +0900

    feat: CardLogo 구현 - 카드 번호 형식에 따라 해당하는 카드 브랜드 로고를 렌더링하는 컴포넌트
    Co-authored-by: soosoo22 <soosoo22@users.noreply.github.com>

---
+++ src/components/Card/CardLogo.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+import Visa from "../../assets/Visa.png";
+import MasterCard from "../../assets/Mastercard.png";
+
+import { CardInfo } from "../PaymentApp";
+
+const CardLogoWrapper = styled.img`
+  width: 36px;
+  height: 22px;
+  gap: 0px;
+  opacity: 0px;
+`;
+
+const CardLogo = ({ cardNumbers }: { cardNumbers: CardInfo[] }) => {
+  let cardType = "";
+
+  if (!cardNumbers || cardNumbers.length === 0) {
+    return;
+  }
+ 
+  const firstCardNumberObject = cardNumbers
+    .slice()
+    .reverse()
+    .find((item) => item.index === "0");
+  if (!firstCardNumberObject) {
+    cardType = "";
+  } else {
+    const firstCardNumber = firstCardNumberObject.currentValue;
+
+    if (firstCardNumber.startsWith("4")) {
+      cardType = Visa;
+    }
+
+    if (firstCardNumber.startsWith("5")) {
+      const firstTwoDigits = firstCardNumber.slice(0, 2);
+      if (Number(firstTwoDigits) >= 51 && Number(firstTwoDigits) <= 55) {
+        cardType = MasterCard;
+      }
+    }
+  }
+
+  if (cardType === Visa || cardType === MasterCard)
+    return <CardLogoWrapper src={cardType} />;
+};
+
+export default CardLogo;
