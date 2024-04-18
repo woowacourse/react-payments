e3c4a62b36f703bcfbd5a64191dbb71b3b411062
Hyelim Choi <HyelimChoi01@gmail.com>
2024-04-18 17:00:28 +0900

    refactor: 변경된 컴포넌트 반영

--- src/stories/Input.stories.ts
+++ src/stories/Input.stories.ts
@@ -2,7 +2,7 @@
 
 import type { Meta, StoryObj } from '@storybook/react';
 import Input from '../components/Input';
-import InputType from '../constants/inputType';
+import INPUT_TYPE_CATEGORIES from '../constants/inputType';
 import { action } from '@storybook/addon-actions';
 
 const meta = {
@@ -15,29 +15,16 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 const mockIndex = 0;
-const mockInfo = InputType.CARD_NUMBER;
-const mockCardInfo = {
-  cardNumber1: '5123',
-  cardNumber2: '1234',
-  cardNumber3: '1212',
-  cardNumber4: '2323',
-  month: '1',
-  year: '12',
-  userName: 'HAILEY CHOI',
-};
+const mockInfo = INPUT_TYPE_CATEGORIES.CARD_NUMBER;
 const mockHandleInput = action('handleInput');
-const mockErrorMessages = {
-  0: '',
-};
 const mockSetErrorMessages = action('setErrorMessages');
 
 export const Default: Story = {
   args: {
     info: mockInfo.inputInfo[mockIndex],
     index: mockIndex,
-    cardInfo: mockCardInfo,
     handleInput: mockHandleInput,
-    errorMessages: mockErrorMessages,
-    setErrorMessages: mockSetErrorMessages,
+    isError: false,
+    handleErrorMessage: mockSetErrorMessages,
   },
 };
