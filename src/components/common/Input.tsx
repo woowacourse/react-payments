be31813c24bf46bfea878928112f3432c037f3b7
hanyoung <phanyoung@naver.com>
2024-04-18 16:59:19 +0900

    fix(Input): isError prop을 transient prop으로 수정

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -6,18 +6,18 @@ interface InputProps {
 }
 
 const Input = ({ isError = false, ...rest }: InputProps) => {
-  return <StyledInput isError={isError} {...rest} />;
+  return <StyledInput $isError={isError} {...rest} />;
 };
 
 interface StyledInputProps {
-  isError: boolean;
+  $isError: boolean;
 }
 
 const StyledInput = styled.input<StyledInputProps>`
   width: ${props => props.width};
   padding: 10px 7px;
 
-  border: 1.2px solid ${props => (props.isError ? '#ff3d3d' : '#acacac')};
+  border: 1.2px solid ${props => (props.$isError ? '#ff3d3d' : '#acacac')};
   border-radius: 5px;
   font-size: 15px;
 
