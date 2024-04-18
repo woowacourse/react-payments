482a849f00a22b9c97cf700f1ab494a440558c67
greetings1012 <ahnqwerty@gmail.com>
2024-04-18 14:57:05 +0900

    design: CardOwnerNameInputField 디자인 적용

--- src/components/CardOwnerNameInputField.tsx
+++ src/components/CardOwnerNameInputField.tsx
@@ -1,8 +1,8 @@
 import { Dispatch, SetStateAction, useState } from "react";
-import Input from "./Input";
+import Input from "./common/Input/Input";
+import styles from "../App.module.css";
 
 const OWNER_NAME_PLACEHOLDER = "JOHN DOE";
-
 export default function CardOwnerNameInputField({
   ownerName,
   setOwnerName,
@@ -10,7 +10,7 @@ export default function CardOwnerNameInputField({
   ownerName: string;
   setOwnerName: Dispatch<SetStateAction<string>>;
 }) {
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
   const normalizeSpaces = (str: string): string => {
     return str.replace(/\s+/g, " ").trimStart();
@@ -29,9 +29,16 @@ export default function CardOwnerNameInputField({
       })
       .join("");
   };
-
   const handleChange = (e) => {
     const onlyEngStr = filterEnglish(e.target.value.toUpperCase());
+    const normalizedOwnerName = normalizeSpaces(onlyEngStr);
+    if (
+      onlyEngStr.length > normalizedOwnerName.length &&
+      normalizedOwnerName.length !== 0
+    ) {
+      setErrorMessage("공백을 연속으로 입력할 수 없습니다.");
+      return;
+    }
 
     if (onlyEngStr.length < e.target.value.length) {
       setErrorMessage("영어만 입력 가능합니다.");
@@ -39,30 +46,36 @@ export default function CardOwnerNameInputField({
       return;
     }
 
-    const normalizedOwnerName = normalizeSpaces(onlyEngStr);
-    if (onlyEngStr.length > normalizedOwnerName.length) {
-      setErrorMessage("공백을 연속으로 입력할 수 없습니다.");
+    setErrorMessage("");
+    setOwnerName(normalizedOwnerName);
+  };
+
+  const handleBlur = (e) => {
+    console.log("blur");
+    if (e.target.value.length === 0) {
+      setErrorMessage("이름을 입력해주세요.");
       return;
     }
 
     setErrorMessage("");
-
-    setOwnerName(normalizedOwnerName);
   };
 
   return (
     <>
-      <div>소유자 이름</div>
-      <div>
+      <div className={styles.label}>소유자 이름</div>
+      <div className={styles.horizon__gap__container}>
         <Input
           onChange={handleChange}
+          isError={errorMessage.length !== 0}
           placeholder={OWNER_NAME_PLACEHOLDER}
           maxLength={26} // 비자 21자, 마스터카드 26자!
-          style={{ textTransform: "uppercase" }}
           value={ownerName}
+          onBlur={handleBlur}
         />
       </div>
-      {errorMessage !== "" && <div>{errorMessage}</div>}
+      {errorMessage !== "" && (
+        <div className={styles.error_message}>{errorMessage}</div>
+      )}
     </>
   );
 }
