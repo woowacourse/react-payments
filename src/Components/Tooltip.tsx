626b2e884a0cedf430ea9eec79006e43bfd18262
lurgi <wahoo9040@naver.com>
2024-04-18 10:57:56 +0900

    feat: 에러 tooltip 스타일 적용
    Co-authored-by: river <jenn0.6n@gmail.com>

--- src/Components/Tooltip.tsx
+++ src/Components/Tooltip.tsx
@@ -1,11 +1,19 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import React from "react";
 
 interface Props {
   children: React.ReactNode;
 }
 
+const tooltipStyle = css({
+  color: "#D1180B",
+  marginTop: "10px",
+  fontSize: "0.8rem",
+});
+
 const Tooltip = ({ children }: Props) => {
-  return <div>{children}</div>;
+  return <div css={tooltipStyle}>{children}</div>;
 };
 
 export default Tooltip;
