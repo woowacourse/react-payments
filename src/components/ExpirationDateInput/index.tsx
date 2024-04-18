cd528044736251a1dcea517c14878cf8732afb05
hwinkr <dnddl8280@naver.com>
2024-04-18 13:11:56 +0900

    refactor(ExpirationDateInput): 년, 월 입력 유효성 검증 구분 및 레이아웃 스타일 적용
    Co-authored-by: chysis <chysiss@naver.com>

--- src/components/ExpirationDateInput/index.tsx
+++ src/components/ExpirationDateInput/index.tsx
@@ -4,28 +4,29 @@ import styled from "styled-components";
 import Input from "../common/Input";
 
 import cardInputValidator from "../../validators/cardInputValidator";
-
-type ExpirationDate = "YY" | "MM" | "DD";
+import { VALIDATION_MESSAGES } from "../../constants/card-app";
 
 interface ExpirationDateInputProps {
-  dates: ExpirationDate[];
+  expirationDate: string[];
   errorCaption: (errorText: string) => JSX.Element;
   handleExpirationDateChange: (index: number, value: string) => void;
 }
 
 const ExpirationDateInput = ({
-  dates,
+  expirationDate,
   errorCaption,
   handleExpirationDateChange,
 }: ExpirationDateInputProps) => {
   const [inputErrors, setInputErrors] = useState<boolean[]>(
-    Array.from<boolean>({ length: dates.length }).fill(false)
+    Array.from<boolean>({ length: expirationDate.length }).fill(false)
   );
 
   const monthRef = useRef<HTMLInputElement>(null);
   const yearRef = useRef<HTMLInputElement>(null);
 
   const handleInputChange = (index: number, value: string) => {
+    const isNumericInput = /^(\d*)$/.test(value);
+
     if (!monthRef.current || !yearRef.current) return;
 
     const isValidDate = cardInputValidator.validateExpiration([
@@ -35,9 +36,20 @@ const ExpirationDateInput = ({
 
     setInputErrors((prevErrors) => {
       const newPrevErrors = [...prevErrors];
-      newPrevErrors[index] = !isValidDate;
+      newPrevErrors[index] = !isValidDate || !isNumericInput;
+
       return newPrevErrors;
     });
+
+    if (!isNumericInput) return;
+
+    if (isValidDate) {
+      const newErrors = Array.from<boolean>({
+        length: expirationDate.length,
+      }).fill(false);
+
+      setInputErrors(newErrors);
+    }
 
     handleExpirationDateChange(index, value);
   };
@@ -45,17 +57,17 @@ const ExpirationDateInput = ({
   const hasErrorInput = () => inputErrors.some((error) => error);
 
   return (
-    <>
+    <InputField>
       <Label htmlFor="expiration-date">유효기간</Label>
       <InputContainer>
         <Input
           ref={monthRef}
-          id="car"
-          key={`input-0`}
-          size="medium"
+          id="expiration-date"
           type="text"
-          placeholder={dates[0]}
+          placeholder="MM"
+          value={expirationDate[0]}
           maxLength={2}
+          size="medium"
           isError={inputErrors[0]}
           onChange={(e) => {
             handleInputChange(0, e.target.value);
@@ -63,25 +75,39 @@ const ExpirationDateInput = ({
         />
         <Input
           ref={yearRef}
-          key={`input-1`}
-          size="medium"
           type="text"
-          placeholder={dates[1]}
+          placeholder="YY"
+          value={expirationDate[1]}
           maxLength={2}
+          size="medium"
           isError={inputErrors[1]}
           onChange={(e) => {
             handleInputChange(1, e.target.value);
           }}
         />
       </InputContainer>
-      {hasErrorInput() && errorCaption("유효한 날짜만 입력 가능합니다.")}
-    </>
+      {hasErrorInput()
+        ? errorCaption(VALIDATION_MESSAGES.invalidDate)
+        : errorCaption("")}
+    </InputField>
   );
 };
 
 export default ExpirationDateInput;
 
-const InputContainer = styled.section`
+const InputField = styled.section`
+  height: 77px;
+  width: 315px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  margin-top: 16px;
+  margin-bottom: 16px;
+`;
+
+const InputContainer = styled.div`
   display: flex;
   justify-content: space-between;
 `;
