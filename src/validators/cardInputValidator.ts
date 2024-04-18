296fadf8fff6e5bf5560cd7b5dfaf5446e4d1212
hwinkr <dnddl8280@naver.com>
2024-04-18 13:13:56 +0900

    refactor(cardInputValidator): 년, 월 입력 유효성 검증 함수 구분
    Co-authored-by: chysis <chysiss@naver.com>

--- src/validators/cardInputValidator.ts
+++ src/validators/cardInputValidator.ts
@@ -1,12 +1,26 @@
 const cardInputValidator = {
   validateMonth(month: string) {
+    if (month.length === 0) return true;
+
     if (month.length !== 2) return false;
 
     if (month[0] !== "0" && month[0] !== "1") return false;
 
-    if (+month < 1 || +month > 12) return false;
+    if (parseInt(month, 10) < 1 || parseInt(month, 10) > 12) return false;
 
     return true;
+  },
+
+  validateYear(year: string) {
+    if (year.length === 0) return true;
+
+    if (year.length !== 2) return false;
+
+    const date = new Date();
+
+    const currentYear = date.getFullYear() % 100;
+
+    return parseInt(year, 10) >= currentYear;
   },
 
   validateFutureDate(month: number, year: number) {
@@ -20,15 +34,17 @@ const cardInputValidator = {
   validateExpiration(expiration: string[]) {
     const [month, year] = expiration;
 
-    if (year === "") {
-      return this.validateMonth(month) ? true : false;
-    }
+    if (!this.validateMonth(month)) return false;
+
+    if (!this.validateYear(year)) return false;
 
     const [numericMonth, numericYear] = expiration.map((str) =>
       parseInt(str, 10)
     );
 
-    return this.validateFutureDate(numericMonth, numericYear) ? true : false;
+    if (!this.validateFutureDate(numericMonth, numericYear)) return false;
+
+    return true;
   },
 };
 
