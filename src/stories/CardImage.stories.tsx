1c9fb32bf819c9d6f0d7dafc2662cb8323ab74a3
bmj0053 <bmj05378@gmail.com>
2024-04-18 11:37:28 +0900

    test: CardImage 컴포넌트 스토리북 추가
    Co-authored-by: 해시 <dle234@users.noreply.github.com>

--- src/stories/CardImage.stories.tsx
+++ src/stories/CardImage.stories.tsx
@@ -4,10 +4,31 @@ import CardImage from '../components/CardImage';
 const meta = {
   title: 'CardImage',
   component: CardImage,
+  argTypes: {},
 } satisfies Meta<typeof CardImage>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    cardNumber: ['4234', '1234', '1234', '1234'],
+    cardPeriod: ['01', '25'],
+    cardOwner: ['WOOTECO'],
+  },
+};
+export const Default2: Story = {
+  args: {
+    cardNumber: ['5134', '1234', '1234', '1234'],
+    cardPeriod: ['01', '25'],
+    cardOwner: ['WOOTECO'],
+  },
+};
+export const Default3: Story = {
+  args: {
+    cardNumber: ['1134', '1234', '1234', '1234'],
+    cardPeriod: ['01', '25'],
+    cardOwner: ['WOOTECO'],
+  },
+};
