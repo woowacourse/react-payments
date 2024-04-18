2412b01db7af70070eae00c69dabe24e9906ec97
손진영 <sm099@naver.com>
2024-04-18 09:08:45 +0900

    chore: 초기 path alias 설정
    1. tsconfig.json 내 baseUrl 및 paths 설정
    
    2. vite.config.ts 내 alias 설정 추가
    
    Co-authored-by: vi-wolhwa <cerezo00@naver.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@src', replacement: resolve(__dirname, 'src') },
+      { find: '@components', replacement: resolve(__dirname, 'src/components') },
+      { find: '@styles', replacement: resolve(__dirname, 'src/styles') },
+      { find: '@assets', replacement: resolve(__dirname, 'src/assets') },
+      { find: '@constants', replacement: resolve(__dirname, 'src/constants') },
+      { find: '@types', replacement: resolve(__dirname, 'src/types') },
+      { find: '@utils', replacement: resolve(__dirname, 'src/utils') },
+      { find: '@hooks', replacement: resolve(__dirname, 'src/hooks') },
+    ],
+  },
+});
