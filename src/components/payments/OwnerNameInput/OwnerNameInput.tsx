26171df1f8769e6bbe39fcaa58204ab2b0d89287
손진영 <sm099@naver.com>
2024-04-18 09:24:14 +0900

    design(OwnerNameInput): 소유자 이름을 담당하는 Input 컴포넌트 추가
    Co-authored-by: vi-wolhwa <cerezo00@naver.com>

---
+++ src/components/payments/OwnerNameInput/OwnerNameInput.tsx
@@ -0,0 +1,21 @@
+import Input, { InputProps } from '@components/common/Input/Input';
+
+type OwnerNameInputProps = Omit<InputProps, 'placeholder' | 'type' | 'maxLength' | 'onChange'> & {
+  onAddOwnerName: React.ChangeEventHandler<HTMLInputElement>;
+  placeholder: string;
+};
+
+const OwnerNameInput: React.FC<OwnerNameInputProps> = ({ isError = false, value, onAddOwnerName }) => {
+  return (
+    <Input
+      maxLength={50}
+      type="text"
+      placeholder="JOHN DOE"
+      value={value}
+      isError={isError}
+      onChange={onAddOwnerName}
+    />
+  );
+};
+
+export default OwnerNameInput;
