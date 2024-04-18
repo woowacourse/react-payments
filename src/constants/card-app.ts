b0503971c14df24c4da17d98c980f003eb1c4a14
hwinkr <dnddl8280@naver.com>
2024-04-18 15:19:25 +0900

    chore(constants): 매직 넘버, 인풋 라벨 상수화
    Co-authored-by: chysis <chysiss@naver.com>

--- src/constants/card-app.ts
+++ src/constants/card-app.ts
@@ -4,6 +4,12 @@ export const INPUT_QUERY = {
   cardOwner: "카드 소유자 이름을 입력해 주세요.",
 };
 
+export const INPUT_LABEL = {
+  cardNumberInput: "카드 번호",
+  expirationDate: "유효기간",
+  cardOwnerNameInput: "소유자 이름",
+};
+
 export const CAPTION = {
   cardNumberInput: "본인 명의의 카드만 결제 가능합니다.",
   expirationDate: "월/년도(MMYY)를 순서대로 입력해 주세요.",
@@ -14,4 +20,16 @@ export const VALIDATION_MESSAGES = {
   onlyEnglishAllowed: "영문만 입력 가능합니다.",
   invalidDate: "유효하지 않은 MM/YY 형식입니다.",
   invalidOwnerName: "사용자 이름은 0 ~ 15자 사이의 영문이어야 합니다.",
+};
+
+export const CARD_NUMBER = {
+  visaStartNumber: 4,
+  minMasterNumber: 51,
+  maxMasterNumber: 55,
+};
+
+export const INPUT_RULES = {
+  maxCardNumberPartLength: 4,
+  maxExpirationDateLength: 2,
+  maxCardOwnerNameLength: 15,
 };
