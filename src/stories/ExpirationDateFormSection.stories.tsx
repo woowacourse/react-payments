e2078b713658ecc66a3a9dba8c8ecee9704e4e77
이태훈 <rhymint@gmail.com>
2024-04-18 14:35:35 +0900

    feat: formSection storybook 추가
    Co-Authored-by: soi-ha <soy2302ten@gmail.com>

---
+++ src/stories/ExpirationDateFormSection.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ExpirationDateFormSection from '../components/ExpirationDateFormSection';
+
+const meta = {
+  title: 'ExpirationDateFormSection',
+  component: ExpirationDateFormSection,
+} satisfies Meta<typeof ExpirationDateFormSection>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: { changeExpiration: () => { } }
+};
---
+++ src/stories/NameFormSection.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import NameFormSection from '../components/NameFormSection';
+
+const meta = {
+  title: 'NameFormSection',
+  component: NameFormSection,
+} satisfies Meta<typeof NameFormSection>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: { changeName: () => { } }
+};
