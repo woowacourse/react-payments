aeecf0e5a8964012ac1ae6a21b27d9e45a503aeb
Soyeon Choe <soy2302ten@gmail.com>
2024-04-18 16:23:35 +0900

    refactor: 사용하지 않는 코드 제거
    Co-Authored-By: TaehunLee <85233397+todari@users.noreply.github.com>

--- src/components/CardNumbersFormSection.tsx
+++ src/components/CardNumbersFormSection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import PaymentsFormTitle from './common/PaymentsFormTitle';
 import PaymentsInputField from './common/PaymentsInputField';
