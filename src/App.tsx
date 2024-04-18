b9d78b0feaccb95cea21c96edb5390760e5f000d
greetings1012 <ahnqwerty@gmail.com>
2024-04-18 14:53:56 +0900

    design: App 컴포넌트 css 디자인

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import "./App.css";
-import Form from "./components/Form";
+import styles from "./App.module.css";
+import Form from "./components/common/Form/Form";
 import { useState } from "react";
-import ShelfHeader from "./components/ShelfHeader";
+import ShelfHeader from "./components/ShelfHeader/ShelfHeader";
 import { CARD_NUMBER_UNIT_LENGTH } from "./constants";
 import CardNumbersInputField from "./components/CardNumbersInputField";
 import CardExpirationDateInputField from "./components/CardExpirationDateInputField";
 import CardOwnerNameInputField from "./components/CardOwnerNameInputField";
-import Card from "./components/Card";
+import Card from "./components/Card/Card";
 
 function App() {
   const [cardNumbers, setCardNumbers] = useState<string[]>(
@@ -20,37 +20,39 @@ function App() {
 
   const [ownerName, setOwnerName] = useState<string>("");
   return (
-    <>
-      <Card cardNumbers={cardNumbers} date={date} ownerName={ownerName} />
-      <Form>
-        <div className="shelf-section">
-          <ShelfHeader
-            title="결제할 카드 번호를 입력해 주세요"
-            description="본인 명의의 카드만 결제 가능합니다."
-          />
-          <CardNumbersInputField
-            cardNumbers={cardNumbers}
-            setCardNumbers={setCardNumbers}
-          />
-        </div>
+    <main className={styles.background}>
+      <section className={styles.layout}>
+        <Card cardNumbers={cardNumbers} date={date} ownerName={ownerName} />
+        <Form>
+          <div className={styles.shelf__section}>
+            <ShelfHeader
+              title="결제할 카드 번호를 입력해 주세요"
+              description="본인 명의의 카드만 결제 가능합니다."
+            />
+            <CardNumbersInputField
+              cardNumbers={cardNumbers}
+              setCardNumbers={setCardNumbers}
+            />
+          </div>
 
-        <div className="shelf-section">
-          <ShelfHeader
-            title="카드 유효기간을 입력해 주세요"
-            description="월/년도(MMYY)를 순서대로 입력해 주세요."
-          />
-          <CardExpirationDateInputField date={date} setDate={setDate} />
-        </div>
+          <div className={styles.shelf__section}>
+            <ShelfHeader
+              title="카드 유효기간을 입력해 주세요"
+              description="월/년도(MMYY)를 순서대로 입력해 주세요."
+            />
+            <CardExpirationDateInputField date={date} setDate={setDate} />
+          </div>
 
-        <div className="shelf-section">
-          <ShelfHeader title="카드 소유자 이름을 입력해주세요" />
-          <CardOwnerNameInputField
-            ownerName={ownerName}
-            setOwnerName={setOwnerName}
-          />
-        </div>
-      </Form>
-    </>
+          <div className={styles.shelf__section}>
+            <ShelfHeader title="카드 소유자 이름을 입력해주세요" />
+            <CardOwnerNameInputField
+              ownerName={ownerName}
+              setOwnerName={setOwnerName}
+            />
+          </div>
+        </Form>
+      </section>
+    </main>
   );
 }
 
