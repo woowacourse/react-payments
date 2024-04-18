16380c006f99f5d2829e1ffab6f20ab28afe91c3
rbgksqkr <rbgks1937@naver.com>
2024-04-18 13:30:15 +0900

    design(Input): 스타일링 수정
    Co-authored-by: Parkhanyoung <phanyoung@naver.com>

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -15,15 +15,16 @@ interface StyledInputProps {
 
 const StyledInput = styled.input<StyledInputProps>`
   width: ${props => props.width};
-  padding: 12px 10px;
+  padding: 10px 7px;
 
   border: 1.2px solid ${props => (props.isError ? '#ff3d3d' : '#acacac')};
   border-radius: 5px;
-  font-size: 16px;
+  font-size: 15px;
 
   &::placeholder {
     color: #acacac;
   }
+
   &:focus {
     outline: none;
   }
