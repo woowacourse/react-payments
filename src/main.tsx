864cf0bdf8382fb2fbd895beb9b5bb83dd0adb3f
rbgksqkr <rbgks1937@naver.com>
2024-04-18 13:28:37 +0900

    design(GlobalStyles): reset css 적용
    Co-authored-by: Parkhanyoung <phanyoung@naver.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { StrictMode } from 'react';
+import GlobalStyles from './styles/GlobalStyles.tsx';
+import { GlobalLayout } from './styles/common.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GlobalStyles />
+    <GlobalLayout>
+      <App />
+    </GlobalLayout>
   </StrictMode>,
 );
--- src/styles/common.ts
+++ src/styles/common.ts
@@ -1,6 +1,19 @@
 import styled from 'styled-components';
 
+const SCREEN_WIDTH = 480;
+
+export const ErrorWrapper = styled.div`
+  height: 44px;
+`;
+
 export const ErrorText = styled.p`
   color: red;
-  font-size: 14px;
+  font-size: 12px;
+  margin-top: 8px;
+`;
+
+export const GlobalLayout = styled.div`
+  width: ${SCREEN_WIDTH}px;
+  height: 100vh;
+  margin: 0 auto;
 `;
