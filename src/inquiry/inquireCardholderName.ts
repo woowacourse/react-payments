2aff5901334acc65d4e8fc29bfa9b5854136a65b
rbgksqkr <rbgks1937@naver.com>
2024-04-18 13:37:18 +0900

    feat(inquireCardholderName): 공백 관련 검증 로직 추가
    Co-authored-by: Parkhanyoung <phanyoung@naver.com>

--- src/inquiry/inquireCardholderName.ts
+++ src/inquiry/inquireCardholderName.ts
@@ -1,8 +1,14 @@
 const inquireCardholderName = (cardholderName: string) => {
-  const isEnglish = /^[a-zA-Z]*$/.test(cardholderName);
+  const isEnglish = /^[a-zA-Z ]+$/.test(cardholderName);
   if (!isEnglish) {
-    return '카드 소유자 이름은 영어로 입력해주세요';
+    return '카드 소유자 이름을 영어로만 입력해주세요';
   }
+
+  const containSideBlankOrMultipleBlanks = !/^(?:\S(?:\s\S*)?)?\S$/.test(cardholderName);
+  if (containSideBlankOrMultipleBlanks) {
+    return '양 끝에 공백이 포함되면 안 되며, 사이 공백의 길이는 최대 1입니다.';
+  }
+
   return '';
 };
 
