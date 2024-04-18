24e38db065281b0bea1b8ab85faea945d208debf
이태훈 <rhymint@gmail.com>
2024-04-18 13:57:34 +0900

    feat: inputField 의 width를 고정값으로 설정해 줄 수 있는 기능 추가
    Co-Authored-by: soi-ha <soy2302ten@gmail.com>

--- src/components/common/PaymentsInputField.tsx
+++ src/components/common/PaymentsInputField.tsx
@@ -2,27 +2,29 @@ import React, { useEffect, useState } from 'react';
 
 import styled from 'styled-components';
 
-const Input = styled.input<{ hasError?: boolean }>`
+const Input = styled.input<{ hasError?: boolean, width?: number }>`
   font-family: 'Inter', sans-serif;
-  width: 100%;
+  width: ${(props) => props.width ? `${props.width}px` : '100%'};
   padding: 8px;
   border-radius: 2px;
   border: ${(props) =>
-    props.hasError ? 'solid 1px #ff3d3d' : 'solid 1px #acacac'};
+    props.hasError ? 'solid 1px #ff3d3d' : 'solid 1px #acacac'
+  };
 
   &:focus {
-    outline: none;
-    border: ${(props) =>
-    props.hasError ? 'solid 1px #ff3d3d' : 'solid 1px #000000'};
-  }
+  outline: none;
+  border: ${(props) =>
+    props.hasError ? 'solid 1px #ff3d3d' : 'solid 1px #000000'
+  };
+}
 
   &::placeholder {
-    color: #acacac;
-  }
+  color: #acacac;
+}
 `;
 
 const PaymentsInputField = ({ ...props }: PaymentsInputFieldProps) => {
-  const { placeholder, maxLength, value, hasError, handleValueChange, handleOnBlur, handleOnFocus, className } = props
+  const { placeholder, maxLength, value, hasError, handleValueChange, handleOnBlur, handleOnFocus, className, width } = props
 
   return (
     <Input
@@ -34,6 +36,7 @@ const PaymentsInputField = ({ ...props }: PaymentsInputFieldProps) => {
       hasError={hasError}
       value={value}
       onChange={handleValueChange}
+      width={width}
     />
   );
 };
