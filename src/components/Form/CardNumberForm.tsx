3e8dff39ab95fcadf2b5aaca257d84d6c7ddac0a
ImxYJL <allensain14@gmail.com>
2024-04-18 13:53:59 +0900

    feat: CardNumberForm 구현 - 카드 번호를 입력할 수 있는 폼 엘리먼트
     Co-authored-by: soosoo22 <soosoo22@users.noreply.github.com>

---
+++ src/components/Form/CardNumberForm.tsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+
+import Input from "./Input";
+import FormElement from "../common/FormElement";
+
+import { CardInfo } from "../PaymentApp";
+
+export interface CardNumberFormProps {
+  labelContent: string;
+  inputCount: number;
+  type: string;
+  placeholders: string[];
+  setCardNumbers?: React.Dispatch<React.SetStateAction<CardInfo[]>>;
+  setExpirationDate?: React.Dispatch<React.SetStateAction<CardInfo[]>>;
+  setUserName?: React.Dispatch<React.SetStateAction<CardInfo[]>>;
+}
+
+const CardNumberForm = ({
+  labelContent,
+  inputCount,
+  type,
+  placeholders,
+  setCardNumbers,
+}: CardNumberFormProps) => {
+  const [isAllInputValid, setAllInputValid] = useState(true);
+  //const [isInputValid, setIsInputValid] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [inputValidities, setInputValidities] = useState({});
+
+  // NOTE: 각 입력 필드의 유효성 검사 결과를 업데이트하는 함수
+  const updateInputValidity = (index, isValid) => {
+    setInputValidities((prevValidities) => ({
+      ...prevValidities,
+      [index]: isValid,
+    }));
+  };
+
+  // NOTE: 모든 입력 필드가 유효한지 검사하는 로직
+  useEffect(() => {
+    const allValid = Object.values(inputValidities).every((isValid) => isValid);
+    setAllInputValid(allValid);
+    setErrorMessage(allValid ? "" : "4자리의 숫자를 입력해주세요.");
+  }, [inputValidities]);
+
+  const inputs = Array.from({ length: inputCount }, (_, index) => (
+    <Input
+      key={index}
+      index={index.toString()}
+      type={type}
+      placeholder={placeholders[index]}
+      maxLength={4}
+      setErrorMessage={setErrorMessage}
+      setData={setCardNumbers}
+      setAllInputValid={(isValid) =>
+        updateInputValidity(index.toString(), isValid)
+      }
+      validationRule={(value) => /^[0-9]{4}$/.test(value)}
+    />
+  ));
+
+  return (
+    <FormElement
+      labelContent={labelContent}
+      inputs={inputs}
+      errorMessage={errorMessage}
+    />
+  );
+};
+
+export default CardNumberForm;
