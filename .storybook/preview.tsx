f8368dc390efa3b9a5ddd75230586b0879b2debe
rbgksqkr <rbgks1937@naver.com>
2024-04-18 15:28:00 +0900

    test(CardPreview.stories): CardPreview story 생성
    Co-authored-by: Parkhanyoung <phanyoung@naver.com>

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,11 +1,21 @@
 import type { Preview } from "@storybook/react";
+import React from "react";
+import GlobalStyles from '../src/styles/GlobalStyles';
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  ),
+];
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
       },
     },
   },
