07e02d0b3eb08f5b1e88e90657d40d681b571226
00kang <kdb070970@gmail.com>
2024-04-18 11:28:51 +0900

    feat : 입력 값의 유효성 검사 진행
    - credit card 번호 확인 유효성 검사
    - credit card 유효기간 유효성 검사
    - credit card 소유자 이름 유효성 검사
    
    Co-Authored-By: Lee sang Yeop <mma7710@naver.com>

---
+++ src/utils/Validator.ts
@@ -0,0 +1,89 @@
+const ValidatorCondition = {
+  checkMaxDigit(value: string, digit: number) {
+    return value.length > digit;
+  },
+
+  checkIsDigit(value: string) {
+    return /^\d* ?\d*$/.test(value);
+  },
+
+  checkIsEnglish(value: string) {
+    return /^[a-zA-Z\s]*$/.test(value);
+  },
+
+  checkIsBelowNumber(value: string, limit: number) {
+    return parseInt(value) <= limit || !value.length;
+  },
+
+  checkIsNotDoubleZero(value: string) {
+    return value !== "00";
+  },
+};
+
+const Validator = {
+  checkCreditCardNumber(value: string) {
+    const maxDigit = 4;
+
+    if (ValidatorCondition.checkMaxDigit(value, maxDigit))
+      return { isError: false, isValid: false };
+
+    if (!ValidatorCondition.checkIsDigit(value)) {
+      return { isError: true, isValid: false };
+    }
+
+    return { isError: false, isValid: true };
+  },
+
+  checkCreditExpirationPeriod(value: string, name: string) {
+    const maxDigit = 2;
+    const limitValue = 12;
+
+    if (ValidatorCondition.checkMaxDigit(value, maxDigit))
+      return { isError: false, isValid: false };
+
+    if (!ValidatorCondition.checkIsDigit(value))
+      return { isError: true, isValid: false };
+
+    const isValidMonth =
+      name === "month"
+        ? ValidatorCondition.checkIsBelowNumber(value, limitValue) &&
+          ValidatorCondition.checkIsNotDoubleZero(value)
+        : true;
+
+    if (!isValidMonth) return { isError: true, isValid: false };
+
+    return { isError: false, isValid: true };
+  },
+
+  checkCreditCardOwner(value: string) {
+    if (ValidatorCondition.checkIsEnglish(value))
+      return { isError: false, isValid: true };
+
+    return { isError: true, isValid: false };
+  },
+
+  inputCreditCardInfo(
+    value: string,
+    name: string
+  ): { isError: boolean; isValid: boolean } {
+    switch (name) {
+      case "firstValue":
+      case "secondValue":
+      case "thirdValue":
+      case "fourthValue":
+        return this.checkCreditCardNumber(value);
+
+      case "month":
+      case "year":
+        return this.checkCreditExpirationPeriod(value, name);
+
+      case "name":
+        return this.checkCreditCardOwner(value);
+
+      default:
+        return { isError: false, isValid: false };
+    }
+  },
+};
+
+export default Validator;
