482505265e52cebb6d43d3a9807cf00ce376d1ec
손진영 <sm099@naver.com>
2024-04-18 14:48:48 +0900

    refactor(Input): onInput 제거
    type을 text로 바꿈에 따라 제거
    
    Co-authored-by: vi-wolhwa <cerezo00@naver.com>

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -2,31 +2,24 @@ import { InputType } from './Input.type';
 
 import styles from './Input.module.css';
 
-const handleEnforceMaxLength = (event: React.ChangeEvent<HTMLInputElement>) => {
-  const { maxLength, value } = event.target;
-  if (value.length > maxLength) {
-    event.target.value = value.slice(0, maxLength);
-  }
-};
-
 export interface InputProps {
+  id?: string;
   type?: InputType;
   value?: string | number;
   isError?: boolean;
   maxLength?: number;
   placeholder: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
-  id?: string;
 }
 
 const Input: React.FC<InputProps> = ({
+  id,
   type = 'text',
   maxLength = 4,
   isError = false,
   placeholder,
   value,
   onChange,
-  id,
 }) => {
   const errorInputClass = `${isError ? styles.errorInput : ''}`;
 
@@ -39,7 +32,6 @@ const Input: React.FC<InputProps> = ({
       placeholder={placeholder}
       value={value}
       onChange={onChange}
-      onInput={handleEnforceMaxLength}
     />
   );
 };
