94a2960956791a381ea8afe6b4c100c167a60a21
hain-tain <hain7ce@gmail.com>
2024-04-18 14:38:45 +0900

    feat(FormItem): input과 label, errorMessage를 가지는 FormItem 구현

---
+++ src/components/FormItem.tsx
@@ -0,0 +1,72 @@
+/** @jsxImportSource @emotion/react */
+import { ReactNode } from "react";
+import { css } from "@emotion/react";
+
+const errorColor = "#ff3d3d";
+
+const styledTextError = css`
+  color: ${errorColor};
+  font-size: 9.5px;
+  line-height: 13.76px;
+  font-weight: 400;
+  margin-top: 5px;
+  height: 10px;
+`;
+
+const styledLabel = css`
+  display: flex;
+  flex-direction: column;
+  font-size: 12px;
+  font-weight: 500;
+  margin-top: 10px;
+  gap: 5px;
+`;
+
+const styledInputContainer = css`
+  display: flex;
+  width: 100%;
+  gap: 5px;
+
+  input {
+    width: 0px;
+    flex: 1;
+    border: 1px solid #acacac;
+    border-radius: 2px;
+    padding: 8px;
+    outline: none;
+  }
+
+  input::placeholder {
+    color: #acacac;
+  }
+
+  input:invalid {
+    border: 1px solid ${errorColor};
+  }
+
+  input:focus {
+    border: 1px solid black;
+  }
+`;
+
+interface FormItemProps {
+  labelText: string;
+  errorMessage?: string;
+  children: ReactNode;
+}
+
+export default function FormItem({
+  labelText,
+  errorMessage = "",
+  children,
+}: FormItemProps) {
+  return (
+    <div>
+      <label css={styledLabel}>
+        {labelText}
+        <div css={styledInputContainer}> {children}</div>
+      </label>
+      <p css={styledTextError}>{errorMessage}</p>
+    </div>
+  );
+}
