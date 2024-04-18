fdb9caede962960b205c9297dacf7b6f582de1bc
Hyelim Choi <HyelimChoi01@gmail.com>
2024-04-18 11:04:23 +0900

    test: 스토리북 디폴트, 비자, 마스터 카드 옵션
    Co-Authored-By: jaeml06 <107801932+jaeml06@users.noreply.github.com>

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -1,27 +1,70 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import CardView from '../components/Card';
 
+const mockDefaultCardInfo = {
+  cardNumber1: '',
+  cardNumber2: '',
+  cardNumber3: '',
+  cardNumber4: '',
+  month: '',
+  year: '',
+  userName: '',
+};
+
+const mockVisaCardInfo = {
+  cardNumber1: '4123',
+  cardNumber2: '1234',
+  cardNumber3: '1234',
+  cardNumber4: '1234',
+  month: '12',
+  year: '29',
+  userName: 'SDF SDF',
+};
+
+const mockMasterCardInfo = {
+  cardNumber1: '5111',
+  cardNumber2: '1234',
+  cardNumber3: '1234',
+  cardNumber4: '1234',
+  month: '1',
+  year: '24',
+  userName: 'CRON LEE',
+};
+
 const meta = {
   title: 'CardView',
   component: CardView,
+  tags: ['autodocs'],
+  argTypes: {
+    cardInfo: {
+      options: ['Default', 'ViSA', 'MASTER_CARD'],
+      mapping: {
+        Default: mockDefaultCardInfo,
+        VISA: mockVisaCardInfo,
+        MASTER_CARD: mockMasterCardInfo,
+      },
+    },
+  },
 } satisfies Meta<typeof CardView>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const mockCardInfo = {
-  cardNumber1: '5123',
-  cardNumber2: '1234',
-  cardNumber3: '1212',
-  cardNumber4: '2323',
-  month: '1',
-  year: '12',
-  userName: 'HAILEY CHOI',
-};
-
 export const Default: Story = {
   args: {
-    cardInfo: mockCardInfo,
+    cardInfo: mockDefaultCardInfo,
+  },
+};
+
+export const Visa: Story = {
+  args: {
+    cardInfo: mockVisaCardInfo,
+  },
+};
+
+export const Master: Story = {
+  args: {
+    cardInfo: mockMasterCardInfo,
   },
 };
