8a582c55bea2e20ffe6d5fea12385fcf343b51a0
손진영 <sm099@naver.com>
2024-04-18 14:56:55 +0900

    fix(useCreditCardLogo): 50이 아닌 51이 시작 숫자가 되도록 변경
    Co-authored-by: vi-wolhwa <cerezo00@naver.com>

--- src/hooks/creditCard/useCreditCardLogo.ts
+++ src/hooks/creditCard/useCreditCardLogo.ts
@@ -1,11 +1,8 @@
 import { MasterCard, VisaCard } from '@assets/images';
 
 const useCreditCardLogo = ([firstCardNumbers]: string[]): string | null => {
-  const prefix = firstCardNumbers.substring(0, 2);
-  const prefixNumber = parseInt(prefix);
-
   const isVisaCardNumber = firstCardNumbers.startsWith('4');
-  const isMasterCardNumber = prefixNumber >= 50 && prefixNumber <= 55;
+  const isMasterCardNumber = /^5[1-5]/.test(firstCardNumbers);
 
   if (isVisaCardNumber) return VisaCard;
 
