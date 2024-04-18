0200c6a54db0019b9d544eef2c74e142ccc8210a
jinsil <jinsil378900@gmail.com>
2024-04-18 11:05:05 +0900

    design: 카드 인풋 섹션 디자인 구현

--- src/components/newCardInputSection/NewCardInputSection.tsx
+++ src/components/newCardInputSection/NewCardInputSection.tsx
@@ -1,22 +1,29 @@
 import React, { ReactNode } from "react";
+import * as Styled from "./NewCardInputSection.styles";
 
 interface Props {
+  label: string;
   mainText: string;
   subText?: string;
-  errorMessage: string;
+  errorMessage: string[];
   children: ReactNode;
 }
 
-const NewCardInputSection = ({ mainText, subText, errorMessage, children }: Props) => {
+const NewCardInputSection = ({ label, mainText, subText, errorMessage, children }: Props) => {
   return (
-    <>
-      <div>
-        <h1>{mainText}</h1>
-        {subText && <p>{subText}</p>}
-      </div>
-      <form>{children}</form>
-      {errorMessage && <span>{errorMessage}</span>}
-    </>
+    <Styled.InputSection>
+      <header>
+        <Styled.MainText>{mainText}</Styled.MainText>
+        <Styled.Subtext>{subText}</Styled.Subtext>
+      </header>
+      <article>
+        <Styled.Form>
+          <Styled.Label>{label}</Styled.Label>
+          <Styled.InputWrapper>{children}</Styled.InputWrapper>
+        </Styled.Form>
+        <Styled.ErrorText>{errorMessage && errorMessage.find((item) => item !== "")}</Styled.ErrorText>
+      </article>
+    </Styled.InputSection>
   );
 };
 
