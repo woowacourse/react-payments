0734e899b979413997f2a0744f6f0ecf74c075b2
greetings1012 <ahnqwerty@gmail.com>
2024-04-18 15:27:55 +0900

    refactor: Card 스토리북 데이터 수정

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -14,20 +14,19 @@ type CardProps = {
   ownerName: string;
 };
 
-export const Default: StoryObj<CardProps> = (args) => <Card {...args} />;
-
-Default.args = {
-  cardNumbers: ["1234", "5678", "9012", "3456"],
-  date: { month: "01", year: "25" },
-  ownerName: "John Doe",
+export const DefaultCard: StoryObj<CardProps> = (args) => (
+  <Card {...args} cardNumbers={["1111", "1111", "1111", "1111"]} />
+);
+DefaultCard.args = {
+  ownerName: "PARAN",
+  date: { month: "02", year: "40" },
 };
 
-// 이 부분은 스토리북에서 각각의 카드 로고를 테스트하기 위한 예시입니다.
 export const VisaCard: StoryObj<CardProps> = (args) => (
   <Card {...args} cardNumbers={["4111", "1111", "1111", "1111"]} />
 );
 VisaCard.args = {
-  ownerName: "John Doe",
+  ownerName: "CHICO",
   date: { month: "01", year: "25" },
 };
 
@@ -35,6 +34,6 @@ export const MasterCard: StoryObj<CardProps> = (args) => (
   <Card {...args} cardNumbers={["5210", "1111", "1111", "1111"]} />
 );
 MasterCard.args = {
-  ownerName: "John Doe",
-  date: { month: "01", year: "25" },
+  ownerName: "PAKXE",
+  date: { month: "06", year: "24" },
 };
