c1d88e4b6792041fc81a4e5519d1792cfac272ba
greetings1012 <ahnqwerty@gmail.com>
2024-04-18 14:54:51 +0900

    feat: Input 구현, 디자인 적용

---
+++ src/components/common/Input/Input.tsx
@@ -0,0 +1,26 @@
+import { CSSProperties, useState } from "react";
+import styles from "./Input.module.css";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  // checkError?: React.ChangeEventHandler<HTMLInputElement>;
+  isError?: boolean;
+  onBlur?: (e) => void;
+}
+
+export default function Input(props: InputProps) {
+  const { onChange, ...rest } = props;
+  const [isFocus, setIsFocus] = useState(false);
+
+  return (
+    <input
+      className={`${styles.customInput} ${isFocus ? styles.isFocus : ""} ${props.isError ? styles.isError : ""} `}
+      {...rest}
+      onFocus={() => setIsFocus(true)}
+      onBlur={(e) => {
+        setIsFocus(false);
+        props?.onBlur(e); // 넘겨준 블러 함수 사용
+      }}
+      onChange={onChange}
+    />
+  );
+}
