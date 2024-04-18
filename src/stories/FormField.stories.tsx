25aab2c3477006cdc3ea50f802c64056380de042
lurgi <wahoo9040@naver.com>
2024-04-18 10:58:10 +0900

    feat: 스토리 수정
    Co-authored-by: river <jenn0.6n@gmail.com>

--- src/stories/FormField.stories.tsx
+++ src/stories/FormField.stories.tsx
@@ -6,10 +6,7 @@ const meta = {
   component: FormField,
   argTypes: {
     formFieldInfo: { control: "object", description: "FormFieldInfo 객체" },
-    // title: { control: "object", description: "타이틀" },
-    // description: { control: "object", description: "설명" },
-    // label: { control: "object", description: "라벨" },
-    // inputPlaceholderList: { control: "object", description: "입력 힌트" },
+    formErrors: { control: "object", description: "FormError 객체" },
   },
 } satisfies Meta<typeof FormField>;
 
@@ -20,13 +17,31 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   name: "신용 카드 UI 기본값",
   args: {
-    id: 1,
     formFieldInfo: {
       key: "cardNumbers",
       title: "결제할 카드 번호를 입력해 주세요",
       label: "카드 번호",
-      inputPlaceholderList: Array(4).fill("1234"),
-      initialValue: [],
+      inputInfoList: [
+        {
+          name: "first",
+          placeholder: "1234",
+          onInputChange: () => {},
+        },
+      ],
+    },
+    formErrors: {
+      cardNumbers: {
+        errorMessage: "에러",
+        isError: true,
+      },
+      cardValidityPeriod: {
+        errorMessage: "",
+        isError: false,
+      },
+      ownerName: {
+        errorMessage: "에러",
+        isError: true,
+      },
     },
   },
 };
@@ -34,14 +49,48 @@ export const Default: Story = {
 export const SmallInputForm: Story = {
   name: "preset이 small인 경우",
   args: {
-    id: 1,
     formFieldInfo: {
       key: "cardNumbers",
       title: "결제할 카드 번호를 입력해 주세요",
       description: "본인 명의의 카드만 결제 가능합니다.",
       label: "카드 번호",
-      inputPlaceholderList: Array(4).fill("1234"),
-      initialValue: [],
+      sizePreset: "small",
+      inputInfoList: [
+        {
+          name: "1",
+          placeholder: "1234",
+          onInputChange: () => {},
+        },
+        {
+          name: "2",
+          placeholder: "1234",
+          onInputChange: () => {},
+        },
+        {
+          name: "3",
+          placeholder: "1234",
+          onInputChange: () => {},
+        },
+        {
+          name: "4",
+          placeholder: "1234",
+          onInputChange: () => {},
+        },
+      ],
+    },
+    formErrors: {
+      cardNumbers: {
+        errorMessage: "",
+        isError: false,
+      },
+      cardValidityPeriod: {
+        errorMessage: "",
+        isError: false,
+      },
+      ownerName: {
+        errorMessage: "에러",
+        isError: true,
+      },
     },
   },
 };
@@ -49,14 +98,38 @@ export const SmallInputForm: Story = {
 export const MediumInputForm: Story = {
   name: "preset이 medium인 경우",
   args: {
-    id: 2,
     formFieldInfo: {
       key: "cardValidityPeriod",
       title: "카드 유효기간을 입력해 주세요",
       description: "월/년도(MM/YY)를 순서대로 입력해 주세요.",
       label: "유효기간",
-      inputPlaceholderList: ["MM", "YY"],
-      initialValue: { month: undefined, year: undefined },
+      sizePreset: "medium",
+      inputInfoList: [
+        {
+          name: "1",
+          placeholder: "MM",
+          onInputChange: () => {},
+        },
+        {
+          name: "2",
+          placeholder: "YY",
+          onInputChange: () => {},
+        },
+      ],
+    },
+    formErrors: {
+      cardNumbers: {
+        errorMessage: "",
+        isError: false,
+      },
+      cardValidityPeriod: {
+        errorMessage: "",
+        isError: false,
+      },
+      ownerName: {
+        errorMessage: "에러",
+        isError: true,
+      },
     },
   },
 };
@@ -64,13 +137,32 @@ export const MediumInputForm: Story = {
 export const LargeInputForm: Story = {
   name: "preset이 large인 경우",
   args: {
-    id: 3,
     formFieldInfo: {
       key: "ownerName",
       title: "카드 소유자 이름을 입력해 주세요",
       label: "소유자 이름",
-      inputPlaceholderList: ["PARK JEONG-WOO"],
-      initialValue: undefined,
+      sizePreset: "large",
+      inputInfoList: [
+        {
+          name: "name",
+          placeholder: "LURGI PARK",
+          onInputChange: () => {},
+        },
+      ],
+    },
+    formErrors: {
+      cardNumbers: {
+        errorMessage: "",
+        isError: false,
+      },
+      cardValidityPeriod: {
+        errorMessage: "",
+        isError: false,
+      },
+      ownerName: {
+        errorMessage: "에러",
+        isError: true,
+      },
     },
   },
 };
