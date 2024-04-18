3db375b582fcdbda228e278aeb7d8e4ec2aacc1a
GimGayeon <kky1373@naver.com>
2024-04-18 13:59:44 +0900

    feat(cardType): 카드 브랜드 상수화 및 타입 설정

---
+++ src/constants/cardInformation.ts
@@ -0,0 +1,7 @@
+export const CARD_INFORMATION = {
+  visa: '4',
+  masterCard: {
+    min: 51,
+    max: 55,
+  },
+};
--- src/types/cardType.ts
+++ src/types/cardType.ts
@@ -2,3 +2,13 @@ import { CARD_NUMBER, CARD_OWNER, CARD_PERIOD } from '../constants/inputInformat
 
 export type CardDetailType = typeof CARD_NUMBER | typeof CARD_PERIOD | typeof CARD_OWNER;
 export type SectionType = typeof CARD_NUMBER.type | typeof CARD_PERIOD.type | typeof CARD_OWNER.type;
+
+export const cardBrand = {
+  visa: 'visa',
+  masterCard: 'masterCard',
+  noneImage: 'noneImage',
+} as const;
+
+type Union<T> = T[keyof T];
+
+export type CardBrandType = Union<typeof cardBrand>;
