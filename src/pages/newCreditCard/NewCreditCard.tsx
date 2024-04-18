25f9b5d501eb4544e837608cbc979f45ff42ae72
jinsil <jinsil378900@gmail.com>
2024-04-18 11:11:36 +0900

    refactor: 유효성 검증 로직 분리

--- src/pages/newCreditCard/NewCreditCard.tsx
+++ src/pages/newCreditCard/NewCreditCard.tsx
@@ -3,120 +3,82 @@ import Input from "../../components/input/Input";
 import CardPreview from "../../components/cardPreview/CardPreview";
 import { ICardInfo } from "../../types/type";
 import NewCardInputSection from "../../components/newCardInputSection/NewCardInputSection";
+import { NewCreditCardContainer } from "./NewCreditCard.styles";
+import { validateCardExpiration, validateCardNumber, validateUserName } from "../../validators/newCardInputValidator";
 
 const NewCreditCard = () => {
   const [cardInfo, setCardInfo] = useState<ICardInfo>({
     cardNumbers: [0, 0, 0, 0],
-    cardExpiration: { month: 0, year: 0 },
+    cardExpiration: [0, 0],
     userName: "",
   });
   const [errorMessage, setErrorMessage] = useState({
-    cardNumbers: "",
-    cardExpiration: "",
-    userName: "",
+    cardNumbers: ["", "", "", ""],
+    cardExpiration: ["", ""],
+    userName: [""],
   });
 
   const handleCardNumbersChange = (value: string, index: number) => {
-    if (value !== "" && Number.isNaN(value)) {
-      setErrorMessage({
-        ...errorMessage,
-        cardNumbers: "숫자만 입력 가능합니다.",
-      });
-      return;
-    }
-
-    if (value !== "" && value.length !== 4) {
-      setErrorMessage({
-        ...errorMessage,
-        cardNumbers: "숫자 4자리를 입력해주세요.",
-      });
-      return;
-    }
+    const errorMessageCopy = [...errorMessage.cardNumbers];
+    errorMessageCopy[index] = validateCardNumber(value);
 
     setErrorMessage({
       ...errorMessage,
-      cardNumbers: "",
+      cardNumbers: errorMessageCopy,
     });
 
-    const newCardNumbers = [...cardInfo.cardNumbers];
-    newCardNumbers[index] = Number(value);
-    setCardInfo({
-      ...cardInfo,
-      cardNumbers: newCardNumbers,
-    });
+    if (errorMessageCopy[index] === "") {
+      const newCardNumbers = [...cardInfo.cardNumbers];
+      newCardNumbers[index] = Number(value);
+      setCardInfo({
+        ...cardInfo,
+        cardNumbers: newCardNumbers,
+      });
+    }
   };
 
-  const handleCardExpirationChange = (value: string, type: string) => {
-    if (value !== "" && Number.isNaN(value)) {
-      setErrorMessage({
-        ...errorMessage,
-        cardExpiration: "숫자만 입력 가능합니다.",
-      });
-      return;
-    }
-
-    if (value !== "" && value.length !== 2) {
-      setErrorMessage({
-        ...errorMessage,
-        cardExpiration: "숫자 2개를 정확히 입력해주세요.",
-      });
-      return;
-    }
-
-    if (value !== "" && type === "month" && !(Number(value) >= 1 && Number(value) <= 12)) {
-      setErrorMessage({
-        ...errorMessage,
-        cardExpiration: "월은 1이상 12이하여야 합니다.",
-      });
-      return;
-    }
+  const handleCardExpirationChange = (value: string, index: number) => {
+    const errorMessageCopy = [...errorMessage.cardExpiration];
+    errorMessageCopy[index] = validateCardExpiration(value, index);
 
     setErrorMessage({
       ...errorMessage,
-      cardExpiration: "",
+      cardExpiration: errorMessageCopy,
     });
 
-    setCardInfo({
-      ...cardInfo,
-      cardExpiration: {
-        ...cardInfo.cardExpiration,
-        [type]: Number(value),
-      },
-    });
+    if (errorMessageCopy[index] === "") {
+      const newCardExpiration = [...cardInfo.cardExpiration];
+      newCardExpiration[index] = Number(value);
+      setCardInfo({
+        ...cardInfo,
+        cardExpiration: newCardExpiration,
+      });
+    }
   };
 
   const handleUserNameChange = (value: string) => {
-    setCardInfo({
-      ...cardInfo,
-      userName: value,
-    });
-
-    if (value !== "" && !/^[a-zA-Z]+$/.test(value)) {
-      setErrorMessage({
-        ...errorMessage,
-        userName: "영어만 입력 가능합니다.",
-      });
-      return;
-    }
+    const errorMessageCopy = validateUserName(value);
 
     setErrorMessage({
       ...errorMessage,
-      userName: "",
+      userName: [errorMessageCopy],
     });
 
-    setCardInfo({
-      ...cardInfo,
-      userName: value.toUpperCase(),
-    });
+    if (errorMessageCopy === "") {
+      setCardInfo({
+        ...cardInfo,
+        userName: value.toUpperCase(),
+      });
+    }
   };
 
   return (
-    <>
+    <NewCreditCardContainer>
       {/* 프리뷰 */}
       <CardPreview cardInfo={cardInfo}></CardPreview>
-
       {/* 입력 - 카드 번호 */}
       <NewCardInputSection
+        label="카드 번호"
         mainText="결제할 카드 번호를 입력해 주세요"
         subText="본인 명의의 카드만 결제 가능합니다."
         errorMessage={errorMessage.cardNumbers}
@@ -126,41 +88,43 @@ const NewCreditCard = () => {
             key={index}
             maxLength={4}
             placeholder="1234"
+            isError={!!errorMessage.cardNumbers[index]}
             onChange={(e) => handleCardNumbersChange(e.target.value, index)}
           ></Input>
         ))}
       </NewCardInputSection>
-
       {/* 입력 - 유효 기간 */}
       <NewCardInputSection
+        label="유효기간"
         mainText="카드 유효기간을 입력해 주세요"
         subText="월/년도(MMYY)를 순서대로 입력해 주세요."
         errorMessage={errorMessage.cardExpiration}
       >
-        <Input
-          maxLength={2}
-          placeholder="MM"
-          onChange={(e) => handleCardExpirationChange(e.target.value, "month")}
-        ></Input>
-        <Input
-          maxLength={2}
-          placeholder="YY"
-          onChange={(e) => handleCardExpirationChange(e.target.value, "year")}
-        ></Input>
+        {cardInfo.cardExpiration.map((cardNumber, index) => (
+          <Input
+            key={index}
+            maxLength={2}
+            placeholder={index === 0 ? "MM" : "YY"}
+            isError={!!errorMessage.cardExpiration[index]}
+            onChange={(e) => handleCardExpirationChange(e.target.value, index)}
+          ></Input>
+        ))}
       </NewCardInputSection>
-
       {/* 입력 - 소유자 이름 */}
       <NewCardInputSection
+        label="소유자 이름"
         mainText="카드 소유자 이름을 입력해 주세요"
+        subText=""
         errorMessage={errorMessage.userName}
       >
         <Input
           value={cardInfo.userName}
           placeholder="JOHN DOE"
+          isError={!!errorMessage.userName[0]}
           onChange={(e) => handleUserNameChange(e.target.value)}
         ></Input>
       </NewCardInputSection>
-    </>
+    </NewCreditCardContainer>
   );
 };
 
