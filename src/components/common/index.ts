61cb8f2145ac4ff83f241d1ea399658703e9d01e
손진영 <sm099@naver.com>
2024-04-18 09:38:39 +0900

    chore: 관련 폴더들에서 export 하는 index.ts 파일 추가
    Co-authored-by: vi-wolhwa <cerezo00@naver.com>

---
+++ src/components/common/index.ts
@@ -0,0 +1,2 @@
+export { default as TextField } from './TextField/TextField';
+export { default as Spacer } from './Spacer/Spacer';
---
+++ src/components/payments/index.ts
@@ -0,0 +1,4 @@
+export { default as CardNumberTextField } from './CardNumberTextField/CardNumberTextField';
+export { default as ExpirationDateTextField } from './ExpirationDateTextField/ExpirationDateTextField';
+export { default as OwnerNameTextField } from './OwnerNameTextField/OwnerNameTextField';
+export { default as PreviewCreditCard } from './PreviewCreditCard/PreviewCreditCard';
