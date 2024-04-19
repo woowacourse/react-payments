a010a2282c428da8519a943a007dcf4b4255b476
Seongjin Hong <hongseongjin.to@gmail.com>
2024-04-19 14:59:16 +0900

    refactor: 배열 형태의 state들에 대한 타입 범위를 좁혀서 재정의

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,8 +7,10 @@ import CardPreviewBox from '../CardPreview/CardPreview';
 import './reset.css';
 import * as S from './App.style';
 
+export type cardNumbersType = [string, string, string, string];
+
 function App() {
-  const [cardNumber, setCardNumber] = useState(['', '', '', '']);
+  const [cardNumber, setCardNumber] = useState<cardNumbersType>(['', '', '', '']);
   const [month, setMonth] = useState('');
   const [year, setYear] = useState('');
   const [owner, setOwner] = useState('');
