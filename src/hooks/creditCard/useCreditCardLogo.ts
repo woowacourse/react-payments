6dd957b8f90b263dab696638074c5521735ed27e
손진영 <sm099@naver.com>
2024-04-19 16:23:43 +0900

    refactor(useCreditCardLogo): 정규표현식 제거
    정규표현식를 제거 후 의도를 드러내는 방향으로 변경

--- src/hooks/creditCard/useCreditCardLogo.ts
+++ src/hooks/creditCard/useCreditCardLogo.ts
@@ -3,8 +3,10 @@ import { MasterCard, VisaCard } from '@assets/images';
 const useCreditCardLogo = ([firstCardNumbers]: string[]): string | null => {
   /* [VisaCard 구분 방법] - 4로 시작한다. */
   const isVisaCardNumber = firstCardNumbers.startsWith('4');
-  /* [MasterCard 구분 방법] - 51 ~ 55로 시작한다. */
-  const isMasterCardNumber = /^5[1-5]/.test(firstCardNumbers);
+
+  /* [MasterCard 구분 방법] - 카드 번호의 최초 2자리가 51 ~ 55로 시작한다. */
+  const cardNumberPrefix = parseInt(firstCardNumbers.substring(0, 2));
+  const isMasterCardNumber = cardNumberPrefix >= 51 && cardNumberPrefix <= 55;
 
   if (isVisaCardNumber) return VisaCard;
 
