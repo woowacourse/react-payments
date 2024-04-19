f60738d6963b21775438e6b1ebe9fd1758b1fc16
brgndy <brgndy96@gmail.com>
2024-04-19 14:30:45 +0900

    feat: useCardImageBrand hook 선언

---
+++ src/hooks/useCardBrandImage.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import validate from '../utils/validate';
+import MasterCardImage from '../assets/images/mastercard.png';
+import VisaCardImage from '../assets/images/visa.png';
+import { InitialCardNumberState } from '../App';
+
+const useCardBrandImage = (cardNumberStates: InitialCardNumberState[]) => {
+  const [cardImageSrc, setCardImageSrc] = useState('');
+
+  useEffect(() => {
+    setCardImageSrc('');
+
+    const cardNumberString = cardNumberStates.map((cardNumber) => cardNumber.value).join('');
+
+    if (validate.isVisa(cardNumberString)) {
+      setCardImageSrc(VisaCardImage);
+    }
+
+    if (validate.isMasterCard(cardNumberString)) {
+      setCardImageSrc(MasterCardImage);
+    }
+  }, [cardNumberStates]);
+
+  return { cardImageSrc };
+};
+
+export default useCardBrandImage;
