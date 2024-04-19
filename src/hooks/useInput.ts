420baadcd11a92e002eba106dc6651af78242fb3
brgndy <brgndy96@gmail.com>
2024-04-19 20:28:54 +0900

    refactor: useInput 커스텀훅 수정

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,14 +1,24 @@
 import { useState } from 'react';
 
-const useInput = () => {
-  const [inputState, setInputState] = useState('');
+const useInput = (validators: { fn: (value: string) => boolean }[]) => {
+  const [inputValue, setInputValue] = useState('');
   const [error, setError] = useState(false);
 
-  const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputState(e.target.value.toUpperCase());
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setError(false);
+    const inputValue = e.target.value.toUpperCase();
+
+    for (let validator of validators) {
+      if (!validator.fn(inputValue)) {
+        setError(true);
+        break;
+      }
+    }
+
+    setInputValue(e.target.value.toUpperCase());
   };
 
-  return { inputState, inputChangeHandler, error, setError };
+  return { inputValue, onChange, error };
 };
 
 export default useInput;
