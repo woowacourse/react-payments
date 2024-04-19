7256f7d2e60a85574b6d596e6dd6d29eef1036a8
손진영 <sm099@naver.com>
2024-04-19 16:18:49 +0900

    style(useCreditCardLogo): 비자 카드 및 마스터 카드 구분 관련 주석 추가

--- src/hooks/creditCard/useCreditCardLogo.ts
+++ src/hooks/creditCard/useCreditCardLogo.ts
@@ -1,7 +1,9 @@
 import { MasterCard, VisaCard } from '@assets/images';
 
 const useCreditCardLogo = ([firstCardNumbers]: string[]): string | null => {
+  /* [VisaCard 구분 방법] - 4로 시작한다. */
   const isVisaCardNumber = firstCardNumbers.startsWith('4');
+  /* [MasterCard 구분 방법] - 51 ~ 55로 시작한다. */
   const isMasterCardNumber = /^5[1-5]/.test(firstCardNumbers);
 
   if (isVisaCardNumber) return VisaCard;
