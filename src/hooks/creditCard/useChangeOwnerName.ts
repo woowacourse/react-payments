e0cb8743ef92bbd6804c3458cc84855a73e875da
손진영 <sm099@naver.com>
2024-04-19 21:04:35 +0900

    refactor(useChangeOwnerName): 정규표현식 개선
    {0,}을 *로 변경

--- src/hooks/creditCard/useChangeOwnerName.ts
+++ src/hooks/creditCard/useChangeOwnerName.ts
@@ -5,7 +5,7 @@ const useChangeOwnerName = () => {
   const [ownerNameError, setOwnerNameError] = useState({ isError: false, errorMessage: '' });
 
   const handleOwnerNameChange = (value: string) => {
-    const isValidName = /^[A-Za-z]{0,}$/.test(value);
+    const isValidName = /^[A-Za-z]*$/.test(value);
 
     const newOwnerNameError = isValidName
       ? { isError: false, errorMessage: '' }
