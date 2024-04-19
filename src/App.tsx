fd008eeb6192fa7536fa60386293d8062a3ae707
anttiey <hanuuny@gmail.com>
2024-04-19 15:26:11 +0900

    refactor: Global Style 적용

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import CardExpirationInput from './components/CardForm/CardExpirationInput/CardE
 import CardOwnerInput from './components/CardForm/CardOwnerInput/CardOwnerInput';
 import CardPreview from './components/CardForm/CardPreview/CardPreview';
 
-import './styles/reset.css';
+import GlobalStyles from './styles/Global.style';
 import * as S from './styles/App.style';
 
 function App() {
@@ -15,16 +15,19 @@ function App() {
   const [owner, setOwner] = useState<string>('');
 
   return (
-    <S.AppLayout>
-      <S.CardPreviewBox>
-        <CardPreview cardNumber={cardNumber} month={month} year={year} owner={owner} />
-      </S.CardPreviewBox>
-      <S.CardForm>
-        <CardNumberInput setCardNumber={setCardNumber} />
-        <CardExpirationInput setMonth={setMonth} setYear={setYear} />
-        <CardOwnerInput setOwner={setOwner} />
-      </S.CardForm>
-    </S.AppLayout>
+    <>
+      <GlobalStyles></GlobalStyles>
+      <S.AppLayout>
+        <S.CardPreviewBox>
+          <CardPreview cardNumber={cardNumber} month={month} year={year} owner={owner} />
+        </S.CardPreviewBox>
+        <S.CardForm>
+          <CardNumberInput setCardNumber={setCardNumber} />
+          <CardExpirationInput setMonth={setMonth} setYear={setYear} />
+          <CardOwnerInput setOwner={setOwner} />
+        </S.CardForm>
+      </S.AppLayout>
+    </>
   );
 }
 
