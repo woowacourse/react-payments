349f6c52a61576b729efe5687ea873e74378ec7d
ss0526100 <ss0526100@gmail.com>
2024-04-20 21:27:41 +0900

    feat:useValidateInput 구현

---
+++ src/hooks/useValidateInput.tsx
@@ -0,0 +1,53 @@
+import useInput, { useInputOption } from './useInput';
+
+import { useState } from 'react';
+
+interface validatorProps {
+  checkIsValid: (input: string) => boolean;
+  shouldReflect: boolean;
+  errorMessage: string;
+}
+
+interface validatedInputProps extends useInputOption {
+  validatorPropsArray: validatorProps[];
+}
+
+export default function useValidateInput({
+  validatorPropsArray,
+  decorateValue,
+  maxLength,
+}: validatedInputProps) {
+  const { input, onChange: inputOnChange } = useInput({
+    decorateValue,
+    maxLength,
+  });
+  const [errorMessage, setErrorMessage] = useState('');
+
+  // reflect 안시키는 에러를 우선 처리해주기 위해 정렬
+  const arrangedValidatorPropsArray = [...validatorPropsArray].sort(a =>
+    !a.shouldReflect ? -1 : 1
+  );
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    if (maxLength && maxLength < newValue.length) return;
+    const { willChange, errorMessage } = arrangedValidatorPropsArray.reduce(
+      (obj, props) => {
+        // 만약 이전에 reflect가 안되는 에러가 발생했다면(!obj.willChange !== newValue), 더 이상 validate를 진행하지 않음
+        if (!obj.willChange) return obj;
+
+        const isValid = props.checkIsValid(newValue);
+        if (isValid) return obj;
+
+        if (!props.shouldReflect) obj.willChange = false;
+        obj.errorMessage = props.errorMessage;
+        return obj;
+      },
+      { willChange: true, errorMessage: '' }
+    );
+    if (willChange) inputOnChange(event);
+    setErrorMessage(errorMessage);
+  };
+
+  return { input, onChange, errorMessage };
+}
