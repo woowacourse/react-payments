d9bc2bfea7996033678219cd572071ba6536e1c2
hanyoung <phanyoung@naver.com>
2024-04-20 19:52:27 +0900

    refactor(Input): 타입 안정성을 위해 InputProps가InputHTMLAttributes를 상속받도록 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,20 +2,16 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "plugin:prettier/recommended",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:storybook/recommended",
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    "@typescript-eslint/no-explicit-any": "warn"
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
 };
