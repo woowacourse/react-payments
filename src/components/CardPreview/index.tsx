689dbc6a494e9a57dc8d067508d44c345e5e40fd
badahertz52 <badahertz52@gmail.com>
2024-04-21 00:59:05 +0900

    refactor: 카드 2,3번째 dot 표기 방식 변경
    - js에서 dot을 넣는 방식에서 css로 dot을 표기하는 방법을 변경

--- src/components/CardPreview/index.tsx
+++ src/components/CardPreview/index.tsx
@@ -1,7 +1,7 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 
 import CardChip from '../../assets/images/cardChip.png';
-import { CARD_COLOR, CARD_MARK, CARD_NUMBERS } from '../../constants';
+import { CARD_COLOR, CARD_MARK } from '../../constants';
 import { CardInfo } from '../../modules/useCardInfoReducer';
 
 import styles from './style.module.css';
@@ -11,44 +11,17 @@ interface CardPreviewProps {
 }
 
 const SLASH = '/';
-const DOT = '·';
 const COMMA = ',';
 
 function CardPreview(props: CardPreviewProps) {
   const { cardInfo } = props;
   const { mark, number, period, userName, color } = cardInfo;
 
-  const [cardNumbers, setCardNumbers] = useState('');
-
   const markInfo = useMemo(() => {
     if (!mark) return;
 
     return CARD_MARK[mark];
   }, [mark]);
-
-  const numberList = useMemo(
-    () =>
-      number
-        ?.split(COMMA)
-        .map((item, index) => {
-          if (item && index > 1) {
-            return DOT.repeat(CARD_NUMBERS.length);
-          }
-          return item;
-        })
-        .join(' '),
-    [number],
-  );
-
-  const changeNumberToMasking = useCallback(() => {
-    if (!numberList) return;
-
-    setCardNumbers(numberList);
-  }, [numberList]);
-
-  useEffect(() => {
-    changeNumberToMasking();
-  }, [number, changeNumberToMasking]);
 
   return (
     <div className={styles.cardPreview}>
@@ -62,7 +35,13 @@ function CardPreview(props: CardPreviewProps) {
             <img src={markInfo?.src} alt={markInfo?.alt} />
           </section>
           <section className={styles.info}>
-            <div className="card-number">{cardNumbers}</div>
+            <div className={styles.cardNumber}>
+              {number
+                ?.split(COMMA)
+                .map((item, index) => (
+                  <span key={`card-number-${index + 1}`}>{item}</span>
+                ))}
+            </div>
             <div className="period">
               {period.month}
               {period.month && period.year ? SLASH : ''}
