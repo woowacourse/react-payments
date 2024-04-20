5e40221c0edb3ef7de7df13cf7427f188ef02b03
ss0526100 <ss0526100@gmail.com>
2024-04-20 21:38:29 +0900

    feat(FormItem): emotion 객체형으로 변경

--- src/components/FormItem.tsx
+++ src/components/FormItem.tsx
@@ -1,53 +1,27 @@
 /** @jsxImportSource @emotion/react */
-import { ReactNode } from "react";
-import { css } from "@emotion/react";
 
-const errorColor = "#ff3d3d";
+import { ReactNode } from 'react';
+import { css } from '@emotion/react';
 
-const styledTextError = css`
-  color: ${errorColor};
-  font-size: 9.5px;
-  line-height: 13.76px;
-  font-weight: 400;
-  margin-top: 5px;
-  height: 10px;
-`;
+const errorColor = '#ff3d3d';
 
-const styledLabel = css`
-  display: flex;
-  flex-direction: column;
-  font-size: 12px;
-  font-weight: 500;
-  margin-top: 10px;
-  gap: 5px;
-`;
+const styledTextError = {
+  color: errorColor,
+  fontSize: '9.5px',
+  lineHeight: '13.76px',
+  fontWeight: 400,
+  marginTop: '5px',
+  height: '10px',
+};
 
-const styledInputContainer = css`
-  display: flex;
-  width: 100%;
-  gap: 5px;
-
-  input {
-    width: 0px;
-    flex: 1;
-    border: 1px solid #acacac;
-    border-radius: 2px;
-    padding: 8px;
-    outline: none;
-  }
-
-  input::placeholder {
-    color: #acacac;
-  }
-
-  input:invalid {
-    border: 1px solid ${errorColor};
-  }
-
-  input:focus {
-    border: 1px solid black;
-  }
-`;
+const wrappedStyledLabel = css({
+  display: 'flex',
+  flexDirection: 'column',
+  fontSize: '12px',
+  fontWeight: 500,
+  marginTop: '10px',
+  gap: '5px',
+});
 
 interface FormItemProps {
   labelText: string;
@@ -57,16 +31,16 @@ interface FormItemProps {
 
 export default function FormItem({
   labelText,
-  errorMessage = "",
+  errorMessage = '',
   children,
 }: FormItemProps) {
   return (
     <div>
-      <label css={styledLabel}>
+      <label css={wrappedStyledLabel}>
         {labelText}
-        <div css={styledInputContainer}> {children}</div>
+        <div> {children}</div>
       </label>
-      <p css={styledTextError}>{errorMessage}</p>
+      <p css={css(styledTextError)}>{errorMessage}</p>
     </div>
   );
 }
