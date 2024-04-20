fb9efdadbe1da8640c450c723c65cc3437c35af2
Soyeon Choe <soy2302ten@gmail.com>
2024-04-20 14:56:15 +0900

    refactor: type 이름 변경

--- src/components/CardBrand.tsx
+++ src/components/CardBrand.tsx
@@ -9,16 +9,16 @@ const Image = styled.img`
   width: 36px;
   height: 28px;
 `;
-type CardBrand = 'Visa' | 'MasterCard';
+type Props = 'Visa' | 'MasterCard';
 
 const CardBrand = ({ ...props }) => {
   const { firstCardNumbers } = props;
-  const BRAND_TABLE: Record<CardBrand, string> = {
+  const BRAND_TABLE: Record<Props, string> = {
     Visa: Visa,
     MasterCard: MasterCard,
   };
 
-  const [brand, setBrand] = useState<CardBrand>('Visa');
+  const [brand, setBrand] = useState<Props>('Visa');
   const [isBrand, setIsBrand] = useState(false);
 
   const changeBrand = () => {
