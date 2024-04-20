817607d4039017b36bf4fb12876245b8b874f472
ss0526100 <ss0526100@gmail.com>
2024-04-20 21:29:38 +0900

    style(message): prettier 적용

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -1,28 +1,30 @@
 export const PAYMENTS_MESSAGE = {
-  expiredDateTitle: "카드 유효기간을 입력해주세요",
-  expiredDateDescription: "월/년도(MMYY)를 순서대로 입력해 주세요.",
+  expiredDateTitle: '카드 유효기간을 입력해주세요',
+  expiredDateDescription: '월/년도(MMYY)를 순서대로 입력해 주세요.',
 
-  cardNumbersTitle: "결제할 카드 번호를 입력해 주세요",
-  cardNumberDescription: "본인 명의의 카드만 결제 가능합니다.",
+  cardNumbersTitle: '결제할 카드 번호를 입력해 주세요',
+  cardNumberDescription: '본인 명의의 카드만 결제 가능합니다.',
 
-  cardHolderTitle: "카드 소유자 이름을 입력해 주세요",
+  cardHolderTitle: '카드 소유자 이름을 입력해 주세요',
 };
 
 export const PAYMENTS_INPUT_MESSAGE = {
-  cardNumberLabel: "카드 번호",
-  cardNumberPlaceHolder: "1234",
+  cardNumberLabel: '카드 번호',
+  cardNumberPlaceHolder: '1234',
 
-  expiredDateLabel: "유효기간",
-  expiredDatePlaceHolder: ["MM", "YY"],
+  expiredDateLabel: '유효기간',
+  expiredDatePlaceHolder: ['MM', 'YY'],
+  expiredDateMonthPlaceHolder: 'MM',
+  expiredDateYearPlaceHolder: 'YY',
 
-  cardHolderLabel: "소유자 이름",
-  cardHolderPlaceHolder: "SEUNGHA CHA",
+  cardHolderLabel: '소유자 이름',
+  cardHolderPlaceHolder: 'SEUNGHA CHA',
 };
 
 export const ERROR_MESSAGE = {
-  notDigit: "올바른 숫자(0~9)를 입력해주세요",
-  underLengthTail: "자리로 입력해주세요",
-  notEnglish: "영어로 입력해주세요",
-  wrongMonth: "올바른 월(01~12)를 입력해주세요",
-  wrongYear: "올바른 년도(00~99)를 입력해주세요",
+  notDigit: '올바른 숫자(0~9)를 입력해주세요',
+  invalidLengthTail: '자리를 입력해주세요',
+  notEnglishOrSpace: '영어로 입력해주세요',
+  wrongMonth: '올바른 월(01~12)를 입력해주세요',
+  wrongYear: '올바른 년도(00~99)를 입력해주세요',
 };
