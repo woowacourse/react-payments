caad428b185605193c5c78d4aab10a9ae8e3e0f8
greetings1012 <ahnqwerty@gmail.com>
2024-04-20 16:21:45 +0900

    refactor: isFocus를 css 내장 문법으로 교체

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import styles from "./Input.module.css";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -9,15 +8,12 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 
 export default function Input(props: InputProps) {
   const { onChange, ...rest } = props;
-  const [isFocus, setIsFocus] = useState(false);
 
   return (
     <input
-      className={`${styles.customInput} ${isFocus ? styles.isFocus : ""} ${props.isError ? styles.isError : ""} `}
+      className={`${styles.customInput} ${props.isError ? styles.isError : ""} `}
       {...rest}
-      onFocus={() => setIsFocus(true)}
       onBlur={(e) => {
-        setIsFocus(false);
         if (props.onBlur) {
           props.onBlur(e);
         } // 넘겨준 블러 함수 사용
