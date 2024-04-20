c8be2e97bdb284e450dcc2ff0021d828b0c1f1da
vi-wolhwa <cerezo00@naver.com>
2024-04-20 15:44:01 +0900

    refactor: 클린코딩 (useChangeCard)

--- src/hooks/creditCard/useChangeCardNumber.ts
+++ src/hooks/creditCard/useChangeCardNumber.ts
@@ -6,39 +6,21 @@ const validCardPrefixes = Object.values(CARD.PREFIXES).flat();
 
 const useChangeCardNumber = () => {
   const [cardNumbers, setCardNumbers] = useState(['', '', '', '']);
-  const [cardNumberError, setCardNumberError] = useState({
-    isError: false,
-    errorMessage: '',
-  });
+  const [cardNumberError, setCardNumberError] = useState({ isError: false, errorMessage: '' });
 
   const handleCardNumberChange = (index: number, value: string) => {
     if (index === 0 && !isValidCardTypeInput(value, validCardPrefixes)) {
-      setCardNumberError({
-        isError: true,
-        errorMessage: ERROR_MESSAGE.invalidCardType,
-      });
-
+      setCardNumberError({ isError: true, errorMessage: ERROR_MESSAGE.invalidCardType });
       return;
     }
 
     if (!isValidCardNumbersInput(value)) {
-      setCardNumberError({
-        isError: true,
-        errorMessage: ERROR_MESSAGE.invalidCardNumberInput,
-      });
-
+      setCardNumberError({ isError: true, errorMessage: ERROR_MESSAGE.invalidCardNumberInput });
       return;
     }
 
-    const newParts = [...cardNumbers];
-
-    newParts[index] = value;
-
-    setCardNumbers(newParts);
-    setCardNumberError({
-      isError: false,
-      errorMessage: '',
-    });
+    setCardNumbers(cardNumbers.map((num, i) => (i === index ? value : num)));
+    setCardNumberError({ isError: false, errorMessage: '' });
   };
 
   return { cardNumbers, cardNumberError, handleCardNumberChange };
