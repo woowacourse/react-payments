e05d868752abb535a950715314089c75c89aa75c
cys4585 <dev.youngsu@gmail.com>
2024-04-20 22:44:18 +0900

    refactor(CardExpirationDate): validate 로직을 컴포넌트 외부로 분리 및 함수 이름 개선 - 구현 로직을 컴포넌트가 알 필요 없음 - 컴포넌트 리랜더링 시 함수가 계속 선언되지 않도록 하기 위함

--- src/components/CardExpirationDate.tsx
+++ src/components/CardExpirationDate.tsx
@@ -24,6 +24,45 @@ const InputContainer = styled.div`
   gap: 10px;
 `;
 
+const isNumericString = (inputValue: string) => {
+  const regex = /^(\d+)?$/;
+  return regex.test(inputValue);
+};
+
+const validateExpirationMonthOnChange = (inputValue: string) => {
+  if (!isNumericString(inputValue)) {
+    throw new Error("월은 숫자만 입력할 수 있어요");
+  }
+};
+
+const validateExpirationYearOnChange = (inputValue: string) => {
+  if (!isNumericString(inputValue)) {
+    throw new Error("년도는 숫자만 입력할 수 있어요");
+  }
+};
+
+const validateExpirationMonthOnBlur = (expirationMonth: string) => {
+  if (expirationMonth.length !== 2) {
+    throw new Error("월을 'MM' 형식으로 입력해 주세요");
+  }
+
+  if (Number(expirationMonth) < 1 || Number(expirationMonth) > 12) {
+    throw new Error("월을 01 ~ 12 범위로 입력해 주세요");
+  }
+};
+
+const validateExpirationYearOnBlur = (expirationYear: string) => {
+  if (expirationYear.length !== 2) {
+    throw new Error("년도를 'YY' 형식으로 입력해 주세요");
+  }
+
+  if (Number(expirationYear) < 24) {
+    throw new Error("유효한 년도를 입력해 주세요");
+  }
+};
+
+// ---
+
 interface CardExpirationDateProps {
   cardExpiration: CardInformation["cardExpiration"];
   onChange: (inputValue: string, inputId: string) => void;
@@ -35,62 +74,58 @@ export default function CardExpirationDate({
 }: CardExpirationDateProps) {
   const [errorMessage, setErrorMessage] = useState("");
 
-  const numberValidator = (numbers: string) => {
-    if (numbers.length !== 0 && !Number.isInteger(Number(numbers))) {
-      setErrorMessage("숫자만 입력해주세요");
+  const onExpirationMonthChange = (inputValue: string) => {
+    try {
+      validateExpirationMonthOnChange(inputValue);
+      setErrorMessage("");
+      onChange(inputValue, "month");
       return false;
-    }
-    setErrorMessage("");
-    return true;
-  };
-
-  const lengthValidator = (numbers: string) => {
-    if (numbers.length !== 2) {
-      setErrorMessage("두 자리 수를 입력해주세요");
-      return false;
-    }
-    setErrorMessage("");
-    return true;
-  };
-
-  const monthValidator = (numbers: string) => {
-    if (Number(numbers) < 1 || Number(numbers) > 12) {
-      setErrorMessage("1~12월 중 하나를 입력해주세요");
-      return false;
-    }
-    setErrorMessage("");
-    return true;
-  };
-
-  const yearValidator = (numbers: string) => {
-    if (Number(numbers) < 24) {
-      setErrorMessage("유효한 년도를 입력해주세요");
-      return false;
-    }
-    setErrorMessage("");
-    return true;
-  };
-
-  const isValidInput = (value: string, fieldName: string) => {
-    if (!numberValidator(value)) {
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+      }
       return true;
     }
-    onChange(value, fieldName);
-    return false;
   };
 
-  const isValidMonth = (value: string) => {
-    if (!lengthValidator(value) || !monthValidator(value)) {
+  const onExpirationYearChange = (inputValue: string) => {
+    try {
+      validateExpirationYearOnChange(inputValue);
+      setErrorMessage("");
+      onChange(inputValue, "year");
+      return false;
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+      }
       return true;
     }
-    return false;
   };
 
-  const isValidYear = (value: string) => {
-    if (!lengthValidator(value) || !yearValidator(value)) {
+  const onExpirationMonthBlur = (inputValue: string) => {
+    try {
+      validateExpirationMonthOnBlur(inputValue);
+      setErrorMessage("");
+      return false;
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+      }
       return true;
     }
-    return false;
+  };
+
+  const onExpirationYearBlur = (inputValue: string) => {
+    try {
+      validateExpirationYearOnBlur(inputValue);
+      setErrorMessage("");
+      return false;
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+      }
+      return true;
+    }
   };
 
   return (
@@ -105,16 +140,16 @@ export default function CardExpirationDate({
           <Input
             maxLength={2}
             placeholder="MM"
-            onChange={(value) => isValidInput(value, "month")}
+            onChange={onExpirationMonthChange}
             value={cardExpiration.month}
-            onBlur={isValidMonth}
+            onBlur={onExpirationMonthBlur}
           />
           <Input
             maxLength={2}
             placeholder="YY"
-            onChange={(value) => isValidInput(value, "year")}
+            onChange={onExpirationYearChange}
             value={cardExpiration.year}
-            onBlur={isValidYear}
+            onBlur={onExpirationYearBlur}
           />
         </InputContainer>
         <ErrorMessage message={errorMessage}></ErrorMessage>
