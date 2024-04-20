d06d759556f052fa1507c570abc45a441284403e
Soyeon Choe <soy2302ten@gmail.com>
2024-04-20 17:07:02 +0900

    refactor: 매직넘버 상수 사용 및 BRAND_TABLE 위치 이동

--- src/components/CardBrand.tsx
+++ src/components/CardBrand.tsx
@@ -12,14 +12,15 @@ const Image = styled.img`
 `;
 type Props = 'Visa' | 'MasterCard';
 
+const BRAND_TABLE: Record<Props, string> = {
+  Visa: Visa,
+  MasterCard: MasterCard,
+};
+
 const CardBrand = ({ ...props }) => {
   const { firstCardNumbers } = props;
-  const BRAND_TABLE: Record<Props, string> = {
-    Visa: Visa,
-    MasterCard: MasterCard,
-  };
 
-  const [brand, setBrand] = useState<Props>('Visa');
+  const [brand, setBrand] = useState<Props>(OPTION.cardBrand.visa);
   const [isBrand, setIsBrand] = useState(false);
 
   const changeBrand = () => {
