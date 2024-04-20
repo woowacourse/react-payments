21e99caeeb80017b5fde5b178fd569e4d1e1b854
ss0526100 <ss0526100@gmail.com>
2024-04-20 21:36:29 +0900

    feat(CardForm): emotion 객체형으로 변경

--- src/components/CardForm.tsx
+++ src/components/CardForm.tsx
@@ -1,23 +1,38 @@
 /** @jsxImportSource @emotion/react */
 
-import CardExpiredDate from "./CardExpiredDate";
-import CardHolder from "./CardHolder";
-import CardNumbers from "./CardNumbers";
-import { css } from "@emotion/react";
+import { Dispatch, SetStateAction } from 'react';
 
-const styledForm = css`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-`;
+import CardExpiredDate from './CardExpiredDate';
+import CardHolder from './CardHolder';
+import CardNumbers from './CardNumbers';
+import { css } from '@emotion/react';
 
-export default function CardForm() {
+const styledForm = {
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '16px',
+};
+
+interface props {
+  setCardNumbers: React.Dispatch<
+    React.SetStateAction<[string, string, string, string]>
+  >;
+  setCardExpiredDate: Dispatch<SetStateAction<[string, string]>>;
+  setCardHolder: Dispatch<SetStateAction<string>>;
+}
+export default function CardForm({
+  setCardNumbers,
+  setCardExpiredDate,
+  setCardHolder,
+}: props) {
   return (
-    <form css={styledForm}>
-      <CardNumbers />
-      <CardExpiredDate />
-      <CardHolder />
+    // eslint-disable-next-line
+    // @ts-ignore
+    <form css={css(styledForm)}>
+      <CardNumbers setCardNumbers={setCardNumbers} />
+      <CardExpiredDate setCardExpiredDate={setCardExpiredDate} />
+      <CardHolder setCardHolder={setCardHolder} />
     </form>
   );
 }
