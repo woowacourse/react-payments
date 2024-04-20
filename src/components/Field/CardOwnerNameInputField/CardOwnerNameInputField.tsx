ba30b214b224dc0e57bc226591e4ed836b29c360
pakxe <pigkill40@naver.com>
2024-04-21 03:43:16 +0900

    refactor: 핸들러 함수 리펙터링과 상수 분리

--- src/components/Field/CardOwnerNameInputField/CardOwnerNameInputField.tsx
+++ src/components/Field/CardOwnerNameInputField/CardOwnerNameInputField.tsx
@@ -1,63 +1,63 @@
-import { Dispatch, SetStateAction, useState } from "react";
-import Input from "../../common/Input/Input";
-import styles from "../../../App.module.css";
+import { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
+import Input from '../../common/Input/Input';
+import styles from '../../../App.module.css';
+import normalizeSpaces from '../../../utils/normalizeSpaces';
+import filterEnglish from '../../../utils/filterEnglish';
+import { ERROR_MESSAGES } from '../../../constants/errorMessages';
+import { MAX_NAME_LENGTH, OWNER_NAME_PLACEHOLDER } from '../../../constants/input';
 
-const OWNER_NAME_PLACEHOLDER = "JOHN DOE";
-export default function CardOwnerNameInputField({
-  ownerName,
-  setOwnerName,
-}: {
+type CardOwnerNameInputField = {
   ownerName: string;
   setOwnerName: Dispatch<SetStateAction<string>>;
-}) {
-  const [errorMessage, setErrorMessage] = useState<string>("");
+};
 
-  const normalizeSpaces = (str: string): string => {
-    return str.replace(/\s+/g, " ").trimStart();
-  };
+export default function CardOwnerNameInputField({ ownerName, setOwnerName }: CardOwnerNameInputField) {
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
-  const filterEnglish = (str: string) => {
-    return str
-      .split("")
-      .filter((char: string) => {
-        if (
-          char === " " ||
-          (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90)
-        )
-          return true;
-        return false;
-      })
-      .join("");
-  };
-  const handleChange = (e: any) => {
-    const onlyEngStr = filterEnglish(e.target.value.toUpperCase());
-    const normalizedOwnerName = normalizeSpaces(onlyEngStr);
-    if (
-      onlyEngStr.length > normalizedOwnerName.length &&
-      normalizedOwnerName.length !== 0
-    ) {
-      setErrorMessage("공백을 연속으로 입력할 수 없습니다.");
-      return;
+  const checkOwnerNameError = ({
+    name,
+    engName,
+    normalizedOwnerName,
+  }: {
+    name: string;
+    engName: string;
+    normalizedOwnerName: string;
+  }) => {
+    const isExcessiveWhiteSpace = engName.length > normalizedOwnerName.length;
+
+    if (isExcessiveWhiteSpace && normalizedOwnerName.length !== 0) {
+      setErrorMessage(ERROR_MESSAGES.EXCESSIVE_WHITE_SPACE);
+      return true;
     }
 
-    if (onlyEngStr.length < e.target.value.length) {
-      setErrorMessage("영어만 입력 가능합니다.");
-      setOwnerName(onlyEngStr);
-      return;
+    if (engName.length < name.length) {
+      setErrorMessage(ERROR_MESSAGES.NOT_ENG);
+      setOwnerName(engName);
+      return true;
     }
 
-    setErrorMessage("");
+    return false;
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const upperCaseName = e.target.value.toUpperCase();
+    const engName = filterEnglish(upperCaseName);
+    const normalizedOwnerName = normalizeSpaces(engName);
+
+    const isError = checkOwnerNameError({ name: upperCaseName, engName, normalizedOwnerName });
+    if (isError) return;
+
+    setErrorMessage('');
     setOwnerName(normalizedOwnerName);
   };
 
-  const handleBlur = (e: any) => {
-    console.log("blur");
+  const handleMissingName = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.value.length === 0) {
-      setErrorMessage("이름을 입력해주세요.");
+      setErrorMessage(ERROR_MESSAGES.MISSING_NAME);
       return;
     }
 
-    setErrorMessage("");
+    setErrorMessage('');
   };
 
   return (
@@ -68,14 +68,12 @@ export default function CardOwnerNameInputField({
           onChange={handleChange}
           isError={errorMessage.length !== 0}
           placeholder={OWNER_NAME_PLACEHOLDER}
-          maxLength={26} // 비자 21자, 마스터카드 26자!
+          maxLength={MAX_NAME_LENGTH} // 비자 21자, 마스터카드 26자!
           value={ownerName}
-          onBlur={handleBlur}
+          onBlur={handleMissingName}
         />
       </div>
-      {errorMessage !== "" && (
-        <div className={styles.error_message}>{errorMessage}</div>
-      )}
+      {errorMessage !== '' && <div className={styles.error_message}>{errorMessage}</div>}
     </>
   );
 }
