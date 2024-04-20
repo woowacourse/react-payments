bd0f582dd3de54b182010bcc1408b70ce88950cd
vi-wolhwa <cerezo00@naver.com>
2024-04-20 14:02:07 +0900

    refactor: 에러메시지 상수화

---
+++ src/constants/errorMessage.ts
@@ -0,0 +1,6 @@
+const ERROR_MESSAGE = {
+  invalidMonthInput: '월은 01에서 12 사이의 숫자만 입력할 수 있습니다.',
+  invalidYearInput: '연도는 숫자만 입력할 수 있습니다.',
+};
+
+export default ERROR_MESSAGE;
---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export { default as ERROR_MESSAGE } from './errorMessage';
