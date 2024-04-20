e4de8225392ebe956e6b124d8d31437f8de44b52
ss0526100 <ss0526100@gmail.com>
2024-04-20 21:39:55 +0900

    feat(App): useContext를 state로 변경 및 emotion 객체로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
 /** @jsxImportSource @emotion/react */
-import "./App.css";
 
-import CardForm from "./components/CardForm";
-import CardPreview from "./components/CardPreview";
-import { createContext } from "react";
-import { css } from "@emotion/react";
-import useCardInfo from "./hooks/useCardInfo";
+import './App.css';
 
-interface ContextProps {
-  cardInfo: {
-    cardNumbers: [string, string, string, string];
-    cardIssuer: "" | "Visa" | "MasterCard";
-    cardExpiredDate: [string, string];
-    cardHolder: string;
-  };
-  setCardNumbers?: React.Dispatch<
-    React.SetStateAction<[string, string, string, string]>
-  >;
-  setCardIssuer?: React.Dispatch<
-    React.SetStateAction<"" | "Visa" | "MasterCard">
-  >;
-  setCardExpiredDate?: React.Dispatch<React.SetStateAction<[string, string]>>;
-  setCardHolder?: React.Dispatch<React.SetStateAction<string>>;
-}
-
-export const CardInfoContext = createContext<ContextProps>({
-  cardInfo: {
-    cardNumbers: ["", "", "", ""],
-    cardIssuer: "",
-    cardExpiredDate: ["", ""],
-    cardHolder: "",
-  },
-});
+import CardForm from './components/CardForm';
+import CardPreview from './components/CardPreview';
+import { css } from '@emotion/react';
+import useCardInfo from './hooks/useCardInfo';
 
 const styledCardInfoContainer = css`
   width: 315px;
@@ -42,33 +16,24 @@ const styledCardInfoContainer = css`
   gap: 30px;
 
   margin: auto;
-  margin-top: 50px;
+  margin-top: 30px;
+  padding: 100px;
+  background-color: white;
 `;
 
 function App() {
-  const {
-    cardInfo,
-    setCardNumbers,
-    setCardIssuer,
-    setCardExpiredDate,
-    setCardHolder,
-  } = useCardInfo();
+  const { cardInfo, setCardNumbers, setCardExpiredDate, setCardHolder } =
+    useCardInfo();
 
   return (
-    <div css={styledCardInfoContainer}>
-      <CardInfoContext.Provider
-        value={{
-          cardInfo,
-          setCardNumbers,
-          setCardIssuer,
-          setCardExpiredDate,
-          setCardHolder,
-        }}
-      >
-        <CardPreview />
-        <CardForm />
-      </CardInfoContext.Provider>
-    </div>
+    <section css={styledCardInfoContainer}>
+      <CardPreview cardInfo={cardInfo} />
+      <CardForm
+        setCardNumbers={setCardNumbers}
+        setCardExpiredDate={setCardExpiredDate}
+        setCardHolder={setCardHolder}
+      />
+    </section>
   );
 }
 
