33e78dab80257327f05a77ec1ab08c8a75359eda
useon <yoosk4986@naver.com>
2024-04-20 20:04:10 +0900

    refactor: 불필요한 파일삭제

--- storybook-static/assets/preview-Ak-KnB7R.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-K4EAMTCU-idUgvBMy.js","./iframe-s8QE7y_-.js","./index-CsdIBAqE.js","./react-18-Cu1enTKq.js","./index-Dkj0J1ds.js","./_getPrototype-B2S_C0PQ.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as a}from"./iframe-s8QE7y_-.js";import"../sb-preview/runtime.js";const{global:s}=__STORYBOOK_MODULE_GLOBAL__;var _=Object.entries(s.TAGS_OPTIONS??{}).reduce((e,r)=>{let[t,o]=r;return o.excludeFromDocsStories&&(e[t]=!0),e},{}),n={docs:{renderer:async()=>{let{DocsRenderer:e}=await a(()=>import("./DocsRenderer-K4EAMTCU-idUgvBMy.js").then(r=>r.D),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new e},stories:{filter:e=>{var r;return(e.tags||[]).filter(t=>_[t]).length===0&&!((r=e.parameters.docs)!=null&&r.disable)}}}};export{n as parameters};
