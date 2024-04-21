a5dcde85503a4bb0dd9dac301226b87b17a01956
vi-wolhwa <cerezo00@naver.com>
2024-04-21 16:39:39 +0900

    remove: 카드 타입 유효성검사 제거

--- src/constants/errorMessage.ts
+++ src/constants/errorMessage.ts
@@ -1,9 +1,4 @@
-import CARD from './card';
-
-const validCardTypes = Object.keys(CARD.TYPE);
-
 const ERROR_MESSAGE = {
-  invalidCardType: `${validCardTypes.join(', ')} 카드만 사용할 수 있습니다.`,
   invalidCardNumberInput: '카드번호는 숫자만 입력할 수 있습니다.',
   invalidMonthInput: '월은 01에서 12 사이의 숫자만 입력할 수 있습니다.',
   invalidYearInput: '연도는 숫자만 입력할 수 있습니다.',
--- src/hooks/creditCard/useChangeCardNumber.ts
+++ src/hooks/creditCard/useChangeCardNumber.ts
@@ -1,19 +1,12 @@
 import { useState } from 'react';
-import { isValidCardNumbersInput, isValidCardTypeInput } from '@utils/validator';
-import { CARD, ERROR_MESSAGE } from '@constants/index';
-
-const validCardPrefixes = Object.values(CARD.PREFIXES).flat();
+import { isValidCardNumbersInput } from '@utils/validator';
+import { ERROR_MESSAGE } from '@constants/index';
 
 const useChangeCardNumber = () => {
   const [cardNumbers, setCardNumbers] = useState(['', '', '', '']);
   const [cardNumberError, setCardNumberError] = useState({ isError: false, errorMessage: '' });
 
   const handleCardNumberChange = (index: number, value: string) => {
-    if (index === 0 && !isValidCardTypeInput(value, validCardPrefixes)) {
-      setCardNumberError({ isError: true, errorMessage: ERROR_MESSAGE.invalidCardType });
-      return;
-    }
-
     if (!isValidCardNumbersInput(value)) {
       setCardNumberError({ isError: true, errorMessage: ERROR_MESSAGE.invalidCardNumberInput });
       return;
