39b34e549315b7c1f2d41d7f9b142c53aaf158e2
Lee sang Yeop <mma7710@naver.com>
2024-04-21 20:07:55 +0900

    refactor(CreditCardInfo): useMemo 를 이용하여 최적화 - 배열로 만든 컴포넌트의 key 값을 index 가 아닌 값을 이용하도록 변경

--- src/components/creditCard/CreditCardInfo.tsx
+++ src/components/creditCard/CreditCardInfo.tsx
@@ -1,23 +1,32 @@
 import styled from "@emotion/styled";
 import { CreditCardProps, CreditCardNumber } from "../../@types/CreditCard";
 import replaceToMaskingNumber from "../../utils/replaceToMaskingNumber";
+import { useMemo } from "react";
 
 const maskLastEightDigits = (creditCardNumber: CreditCardNumber) => {
+  const digitKeys = ["firstNumber", "secondNumber", "thirdNumber", "fourthNumber"];
   const lastEightIndex = [2, 3];
 
   return creditCardNumber.map((number, idx) =>
     lastEightIndex.includes(idx) ? (
-      <CreditCardInfoSymbol key={idx}>{replaceToMaskingNumber(number)}</CreditCardInfoSymbol>
+      <CreditCardInfoSymbol key={digitKeys[idx]}>
+        {replaceToMaskingNumber(number)}
+      </CreditCardInfoSymbol>
     ) : (
-      <CreditCardInfoNumber key={idx}>{number}</CreditCardInfoNumber>
+      <CreditCardInfoNumber key={digitKeys[idx]}>{number}</CreditCardInfoNumber>
     )
   );
 };
 
 const CreditCardInfo = ({ creditCardNumber, expirationPeriod, ownerName }: CreditCardProps) => {
+  const maskedCreditCardNumber = useMemo(
+    () => maskLastEightDigits(creditCardNumber),
+    [creditCardNumber]
+  );
+
   return (
     <CreditCardInfoContainer>
-      <CreditCardInfoWrapper>{maskLastEightDigits(creditCardNumber)}</CreditCardInfoWrapper>
+      <CreditCardInfoWrapper>{maskedCreditCardNumber}</CreditCardInfoWrapper>
       <CreditCardExpirationPeriod>{expirationPeriod}</CreditCardExpirationPeriod>
       <CreditCardOwnerInfo>{ownerName}</CreditCardOwnerInfo>
     </CreditCardInfoContainer>
