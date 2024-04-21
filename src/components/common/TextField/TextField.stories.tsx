cae9faa4d41e48eac3a7db40e90885d74fc1c564
vi-wolhwa <cerezo00@naver.com>
2024-04-21 17:22:04 +0900

    test: TextField 메인컴포넌트 제거에 따른 스토리 변경

--- src/components/common/TextField/TextField.stories.tsx
+++ src/components/common/TextField/TextField.stories.tsx
@@ -1,11 +1,9 @@
 import TextField from '@components/common/TextField/TextField';
 import CardNumberInput from '@components/payments/CardNumberInput/CardNumberInput';
 import type { Meta, StoryObj } from '@storybook/react';
-import { generateArgTypes } from '@utils/generateArgTypes';
 
 const meta = {
   title: 'Base/TextField',
-  component: TextField,
   parameters: {
     layout: 'centered',
     docs: {
@@ -14,17 +12,20 @@ const meta = {
       },
     },
   },
-  argTypes: {
-    children: {
-      ...generateArgTypes({ control: 'object' }),
-      defaultValue: {},
-      description: 'TextField를 구성하는 요소들',
-      table: {
-        type: { summary: 'ReactNode' },
-      },
-    },
-  },
   tags: ['autodocs'],
+  render: () => (
+    <>
+      <TextField.Title title="결제할 카드 번호를 입력해 주세요" />
+      <TextField.SubTitle subTitle="본인 명의의 카드만 결제 가능합니다." />
+      <TextField.Label labelText="카드 번호"></TextField.Label>
+      <TextField.Content>
+        <CardNumberInput isError={false} value={''} onAddCardNumber={() => {}} />
+        <CardNumberInput isError={false} value={''} onAddCardNumber={() => {}} />
+        <CardNumberInput isError={false} value={''} onAddCardNumber={() => {}} />
+        <CardNumberInput isError={false} value={''} onAddCardNumber={() => {}} />
+      </TextField.Content>
+    </>
+  ),
 } satisfies Meta<typeof TextField>;
 
 export default meta;
@@ -33,21 +34,5 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   parameters: {
     docs: { description: { story: 'Title, SubTitle, Label, Content가 결합된 컴포넌트' } },
-  },
-
-  args: {
-    children: (
-      <>
-        <TextField.Title title="결제할 카드 번호를 입력해 주세요" />
-        <TextField.SubTitle subTitle="본인 명의의 카드만 결제 가능합니다." />
-        <TextField.Label labelText="카드 번호"></TextField.Label>
-        <TextField.Content>
-          <CardNumberInput isError={false} value={''} onAddCardNumber={() => {}} />
-          <CardNumberInput isError={false} value={''} onAddCardNumber={() => {}} />
-          <CardNumberInput isError={false} value={''} onAddCardNumber={() => {}} />
-          <CardNumberInput isError={false} value={''} onAddCardNumber={() => {}} />
-        </TextField.Content>
-      </>
-    ),
   },
 };
