44f40532a96902e6b4939ac0c4403f3a5924c402
chysis <chysiss@naver.com>
2024-04-21 22:59:08 +0900

    chore: 사용하지 않는 파일 제거

--- src/assets/image.ts
+++
@@ -1,3 +0,0 @@
-// export { default as visaCard } from "../../public/assets/visaCard.png";
-// export { default as masterCard } from "../../public/assets/masterCard.png";
-// export { default as ellipse } from "../../public/assets/ellipse.png";
