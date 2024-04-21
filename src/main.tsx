b46a9cfd5e5e1b5ac1c146ccfebca5eab9f88d14
useon <yoosk4986@naver.com>
2024-04-21 17:58:53 +0900

    refactor: reset.css를 styled component의 reset 메소드로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './styles/reset.styles.css';
+import GlobalStyle from './styles/GlobalStyle.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
 );
