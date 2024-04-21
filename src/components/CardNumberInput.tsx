c5fad22bd644ddb0bbdb8a8b21484308f6104119
jaeml06 <jaeml0630@gmail.com>
2024-04-21 13:30:49 +0900

    refactor: 불필요한 코드 주석, 삭제

--- src/components/CardNumberInput.tsx
+++ src/components/CardNumberInput.tsx
@@ -1,22 +1,27 @@
-import styled from "styled-components";
-import Input from "./Input";
+// import styled from "styled-components";
+// import Input from "./Input";
 
-const InputBox = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  height: 2rem;
-  align-items: center;
-  gap: 0.5rem;
-  margin-bottom: 8px;
-`;
-export default function CardNumberInput({handleInput} : {handleInput : () => void}) {
-  return (
-    <InputBox>
-      <Input type='number' maxLength={4} placeholder="1234" onChange={handleInput}></Input>
-      <Input type='number' maxLength={4} placeholder="1234" onChange={handleInput}></Input>
-      <Input type='number' maxLength={4} placeholder="1234" onChange={handleInput}></Input>
-      <Input type='number' maxLength={4} placeholder="1234" onChange={handleInput}></Input>
-    </InputBox>
-  )
-}
+// const InputBox = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   width: 100%;
+//   height: 2rem;
+//   align-items: center;
+//   gap: 0.5rem;
+//   margin-bottom: 8px;
+// `;
+// export default function CardNumberInput({handleInput} : {handleInput : () => void}) {
+//   return (
+//     <InputBox>
+//     {Array.from({ length: 4 }, (_, index) => (
+//       <Input
+//         key={index}
+//         type="number"
+//         maxLength={4}
+//         placeholder="1234"
+//         onChange={handleInput}
+//       />
+//     ))}
+//   </InputBox>
+//   )
+// }
