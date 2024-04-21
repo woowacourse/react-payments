c3e4838937e694a9653d78ed408d306c0851270d
ss0526100 <ss0526100@gmail.com>
2024-04-21 22:11:49 +0900

    delete(matchCardIssuer): 안쓰는 코드 삭제

--- src/domain/matchCardIssuer.ts
+++ src/domain/matchCardIssuer.ts
@@ -1,15 +1,13 @@
-import DEFAULT_BLANK_IMAGE from "../Images/blank.png";
-import MASTERCARD_IMAGE from "../Images/Mastercard.png";
-import VISA_IMAGE from "../Images/Visa.png";
+import { CardIssuer } from '../hooks/useCardInfo';
 
 interface CardListElement {
-  issuer: "" | "Visa" | "MasterCard";
+  issuer: CardIssuer;
   startingNumbers: string[];
 }
 
 const cardList: CardListElement[] = [
-  { issuer: "Visa", startingNumbers: ["4"] },
-  { issuer: "MasterCard", startingNumbers: ["51", "52", "53", "54", "55"] },
+  { issuer: 'Visa', startingNumbers: ['4'] },
+  { issuer: 'MasterCard', startingNumbers: ['51', '52', '53', '54', '55'] },
 ];
 
 export const matchCardIssuer = (cardNumber: string) => {
@@ -19,19 +17,9 @@ export const matchCardIssuer = (cardNumber: string) => {
   const hasStartingNumber = (startingNumbers: string[]) =>
     startingNumbers.some(matchedStartingNumber);
 
-  const matchedCardInfo = cardList.find((object) =>
+  const matchedCardInfo = cardList.find(object =>
     hasStartingNumber(object.startingNumbers)
   );
 
-  return matchedCardInfo?.issuer ?? "";
-};
-
-const issuerImg: { [key: string]: string } = {
-  ["Visa"]: VISA_IMAGE,
-  ["MasterCard"]: MASTERCARD_IMAGE,
-};
-
-export const matchCardIssuerImgSrc = (issuer: string) => {
-  const result = issuerImg[issuer];
-  return result ?? DEFAULT_BLANK_IMAGE;
+  return matchedCardInfo?.issuer ?? '';
 };
