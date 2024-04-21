4cee486f12e2a96e422f95d62db0afb406295abf
pakxe <pigkill40@naver.com>
2024-04-22 01:30:55 +0900

    storybook: 불필요한 arg 제거

--- src/components/common/Input/Input.stories.tsx
+++ src/components/common/Input/Input.stories.tsx
@@ -1,8 +1,8 @@
-import { Meta, StoryObj } from "@storybook/react";
-import Input from "./Input";
+import { Meta, StoryObj } from '@storybook/react';
+import Input from './Input';
 
 const meta: Meta = {
-  title: "Input",
+  title: 'Input',
   component: Input,
 };
 
@@ -13,20 +13,15 @@ type InputProps = {
   isError: boolean;
 };
 
-export const DefaultInput: StoryObj<InputProps> = (args: any) => (
-  <Input {...args} />
-);
+export const DefaultInput: StoryObj<InputProps> = (args: any) => <Input {...args} />;
 
 DefaultInput.args = {
-  value: "안녕하세요",
-  isError: false,
+  value: '안녕하세요',
 };
 
-export const ErrorInput: StoryObj<InputProps> = (args: any) => (
-  <Input {...args} />
-);
+export const ErrorInput: StoryObj<InputProps> = (args: any) => <Input {...args} />;
 
 ErrorInput.args = {
-  value: "이건 에러",
+  value: '이건 에러',
   isError: true,
 };
