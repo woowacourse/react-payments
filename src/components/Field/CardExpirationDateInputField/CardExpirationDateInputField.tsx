a4f9e6714258bab1ea93cfd2abe7e0be85b4a5ba
pakxe <pigkill40@naver.com>
2024-04-22 00:19:00 +0900

    refactor: 날짜의 유효성 검사를 다른 파일로 분리

--- src/components/Field/CardExpirationDateInputField/CardExpirationDateInputField.tsx
+++ src/components/Field/CardExpirationDateInputField/CardExpirationDateInputField.tsx
@@ -1,7 +1,8 @@
-import { Dispatch, SetStateAction, useState } from "react";
-import { DATE_PLACEHOLDER } from "../../../constants";
-import Input from "../../common/Input/Input";
-import styles from "../../../App.module.css";
+import { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
+import { DATE_PLACEHOLDER } from '../../../constants/input';
+import Input from '../../common/Input/Input';
+import styles from '../../../App.module.css';
+import { checkExpired, checkValidMonth, checkValidYear } from '../../../utils/checkDate';
 
 export default function CardExpirationDateInputField({
   date,
@@ -10,71 +11,28 @@ export default function CardExpirationDateInputField({
   date: Record<string, string>;
   setDate: Dispatch<SetStateAction<Record<string, string>>>;
 }) {
-  const [errorMessages, setErrorMessages] = useState<Array<[number, string]>>(
-    []
-  );
+  const [errorMessages, setErrorMessages] = useState(new Array(Object.keys(date).length).fill(''));
 
   // 여긴 최신 오류 순서가 아닌 0부터 시작함..
-  const checkValidDate = ({
-    month = date.month,
-    year = date.year,
-  }: {
-    month?: string;
-    year?: string;
-  }) => {
-    const updatedErrorMessages: any[] = new Array(Object.keys(date).length)
-      .fill(0)
-      .map(() => []);
-
+  const checkValidDate = ({ month = date.month, year = date.year }: { month?: string; year?: string }) => {
     const isExpiredDate = checkExpired(month, year);
 
-    if (year.length > 0 && year.length < 2)
-      updatedErrorMessages[1] = [1, "유효하지 않은 날짜입니다."];
-    if (isExpiredDate === "INVALID_MONTH")
-      updatedErrorMessages[0] = [0, "이미 만료된 카드입니다."];
-    if (isExpiredDate === "INVALID_YEAR")
-      updatedErrorMessages[1] = [1, "이미 만료된 카드입니다."];
+    const monthErrorMessage = checkValidMonth(month, isExpiredDate);
+    const yearErrorMessage = checkValidYear(year, isExpiredDate);
 
-    if (isNaN(Number(month)))
-      updatedErrorMessages[0] = [0, "숫자만 입력해주세요."];
-    if (Number(month) <= 0 || Number(month) > 12)
-      updatedErrorMessages[0] = [0, "유효하지 않은 날짜입니다."];
-
-    if (isNaN(Number(year)))
-      updatedErrorMessages[1] = [1, "숫자만 입력해주세요."];
-
-    setErrorMessages(updatedErrorMessages.filter((data) => data.length !== 0));
+    setErrorMessages([monthErrorMessage, yearErrorMessage]);
   };
 
-  const checkExpired = (month: string, year: string) => {
-    if (year.length < 2 || month.length < 2) return false;
-    const today = new Date();
-    const todayMonth = today.getMonth() + 1;
-    const todayYear = today.getFullYear() - 2000;
+  const handleMonth = (e: ChangeEvent<HTMLInputElement>) => {
+    const month = Number(e.target.value);
 
-    if (Number(year) === todayYear && Number(month) < todayMonth) {
-      return "INVALID_MONTH";
-    }
-
-    if (Number(year) < todayYear) {
-      return "INVALID_YEAR";
-    }
-
-    return false;
-  };
-
-  const handleMonth = (e: any) => {
-    const input = Number(e.target.value);
-
-    if (input <= 1 || e.target.value.length >= 2)
-      setDate({ ...date, month: e.target.value });
-    else if (input >= 2 && input <= 9)
-      setDate({ ...date, month: e.target.value.padStart(2, "0") });
+    if (month <= 1 || e.target.value.length >= 2) setDate({ ...date, month: e.target.value });
+    else if (month >= 2 && month <= 9) setDate({ ...date, month: e.target.value.padStart(2, '0') });
 
     checkValidDate({ month: e.target.value });
   };
 
-  const handleYear = (e: any) => {
+  const handleYear = (e: ChangeEvent<HTMLInputElement>) => {
     setDate({ ...date, year: e.target.value });
     checkValidDate({ year: e.target.value });
   };
@@ -88,20 +46,18 @@ export default function CardExpirationDateInputField({
           placeholder={DATE_PLACEHOLDER.MONTH}
           maxLength={2}
           value={date.month}
-          isError={errorMessages.filter(([i]) => i === 0).length !== 0}
+          isError={errorMessages[0] !== ''}
         />
         <Input
           onChange={handleYear}
           placeholder={DATE_PLACEHOLDER.YEAR}
           maxLength={2}
           value={date.year}
-          isError={errorMessages.filter(([i]) => i === 1).length !== 0}
+          isError={errorMessages[1] !== ''}
         />
       </div>
-      {errorMessages.length !== 0 && (
-        <div className={styles.error_message}>
-          {errorMessages[errorMessages.length - 1][1]}
-        </div>
+      {errorMessages.some((errorMessage) => errorMessage !== '') && (
+        <div className={styles.error_message}>{errorMessages.find((errorMessage) => errorMessage !== '')}</div>
       )}
     </>
   );
