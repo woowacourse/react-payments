b3093aa71088cd7b0b616b08b5185129a306b136
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-04-21 13:04:21 +0900

    feat: 커스텀 훅 useCardUserNameInput 구현
    - useCardUserNameInput 커스텀 훅을 생성하여 사용자 이름 입력 상태 관리 로직을 구현
    
    - 입력된 사용자 이름에 대해 알파벳과 공백만 허용하는 정규식 검증을 추가
    
    - 이름 입력이 유효하지 않을 경우, 상태 업데이트를 중단하고 오류 상태를 설정
    
    - 입력된 이름이 유효한 경우, 대문자로 변환하여 상태에 저장

---
+++ src/hooks/useCardUserNameInput.ts
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+function useCardUserNameInput() {
+  const [userName, setUserName] = useState('');
+  const [nameError, setNameError] = useState(false);
+
+  const handleNameChange = (value: string) => {
+    const regex = /^[a-zA-Z\s]*$/;
+    const isValidName = regex.test(value);
+
+    setNameError(!isValidName);
+
+    if (!isValidName) return;
+
+    setUserName(value.toUpperCase());
+  };
+
+  return { userName, nameError, handleNameChange };
+}
+
+export default useCardUserNameInput;
