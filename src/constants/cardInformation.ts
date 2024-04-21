fe77c555f6d49eb54e83e84daec5c0cd9d3ae69a
jayming66 <mjw8941@gmail.com>
2024-04-21 22:46:06 +0900

    feat: 카드 관련 정보, error 메시지 매직넘버 처리

---
+++ src/constants/cardInformation.ts
@@ -0,0 +1,10 @@
+export const CARD_INFO = {
+  NUMBER_LENGTH: 4,
+  DATE_LENGTH: 2,
+  NAME_LENGTH: 15,
+  MONTH_START: 1,
+  MONTH_END: 12,
+  VALID_YEAR: 24,
+};
+
+export const OWNER_NAME_REG = /^[A-Z\s]*$/; // 영문자 대문자 또는 공백만 허용
--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,3 +1,6 @@
+import { CARD_INFO, OWNER_NAME_REG } from "../constants/cardInformation";
+import { ERROR } from "../constants/message";
+
 type validateReturnValue = { isError: boolean; message: string };
 type validator = (value: string) => validateReturnValue;
 
@@ -21,52 +24,55 @@ export const executeValidators = (
 
 export const isInvalidNumber = (value: string) => {
   if (value.length !== 0 && !Number.isInteger(Number(value))) {
-    return { isError: true, message: "숫자만 입력해주세요" };
+    return { isError: true, message: ERROR.NOT_NUMBER };
   }
 
   return { isError: false, message: "" };
 };
 
 export const isInvalidCardNumberLength = (value: string) => {
-  if (value.length !== 4) {
-    return { isError: true, message: "네 자리 수를 입력해주세요" };
+  if (value.length !== CARD_INFO.NUMBER_LENGTH) {
+    return { isError: true, message: ERROR.CARD_NUMBER_LENGTH };
   }
   return { isError: false, message: "" };
 };
 
 export const isInvalidDateLength = (value: string) => {
-  if (value.length !== 2) {
-    return { isError: true, message: "두 자리 수를 입력해주세요" };
+  if (value.length !== CARD_INFO.DATE_LENGTH) {
+    return { isError: true, message: ERROR.CARD_DATE_LENGTH };
   }
   return { isError: false, message: "" };
 };
 
 export const isInvalidMonth = (value: string) => {
-  if (Number(value) < 1 || Number(value) > 12) {
-    return { isError: true, message: "1~12월 중 하나를 입력해주세요" };
+  if (
+    Number(value) < CARD_INFO.MONTH_START ||
+    Number(value) > CARD_INFO.MONTH_END
+  ) {
+    return { isError: true, message: ERROR.NOT_MONTH };
   }
   return { isError: false, message: "" };
 };
 
 export const isInvalidYear = (value: string) => {
-  if (Number(value) < 24) {
-    return { isError: true, message: "유효한 년도를 입력해주세요" };
+  if (Number(value) < CARD_INFO.VALID_YEAR) {
+    return { isError: true, message: ERROR.NOT_YEAR };
   }
   return { isError: false, message: "" };
 };
 
 export const isInvalidOwnerName = (upperName: string) => {
-  const pattern: RegExp = /^[A-Z\s]*$/; // 영문자 대문자 또는 공백만 허용
+  const pattern: RegExp = OWNER_NAME_REG;
 
   if (upperName.length !== 0 && !pattern.test(upperName)) {
-    return { isError: true, message: "영문자만 입력해주세요" };
+    return { isError: true, message: ERROR.NOT_ALPHABET };
   }
   return { isError: false, message: "" };
 };
 
 export const isInvalidNameLength = (value: string) => {
-  if (value.length < 1 && value.length > 15) {
-    return { isError: true, message: "1자 ~ 15자의 이름을 입력해주세요" };
+  if (value.length < 1 && value.length > CARD_INFO.NAME_LENGTH) {
+    return { isError: true, message: ERROR.CARD_NAME_LENGTH };
   }
   return { isError: false, message: "" };
 };
