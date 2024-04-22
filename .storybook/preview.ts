afa5e081be3c0effe135a15678b005aa853b3474
Seongjin Hong <hongseongjin.to@gmail.com>
2024-04-22 16:57:26 +0900

    chore: Storybook preview 설정에 누락된 공용 css 추가

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,6 @@
 import type { Preview } from '@storybook/react';
 import '../src/styles/reset.css';
+import '../src/styles/common.css';
 
 import { INITIAL_VIEWPORTS, MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
 
