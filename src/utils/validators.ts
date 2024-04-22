4c28f1de08542ec7357e489a8871a0ed9c4392f2
novice0840 <magnolia0840@gmail.com>
2024-04-22 16:14:36 +0900

    fix: Ownername regex 수정

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -8,7 +8,8 @@ export const hasFourDigit = (value: string) => {
 
 export const hasTwoDigit = (value: string) => value.length === 2;
 
-export const isValidMonth = (value: string) => 1 <= Number(value) && Number(value) <= 12;
+export const isValidMonth = (value: string) =>
+  1 <= Number(value) && Number(value) <= 12;
 
 export const isValidDate = ({ year, month }: ExpirationDate) => {
   if (year === "") return true;
@@ -20,6 +21,6 @@ export const isValidDate = ({ year, month }: ExpirationDate) => {
 };
 
 export const isEnglishCharacter = (value: string) => {
-  const regex = /^[a-zA-Z\s]+$/;
+  const regex = /^(?:[a-zA-Z\s]+)?$/;
   return regex.test(value);
 };
