f918f4a8a41fa1548f613646872bfc61680e3769
ss0526100 <ss0526100@gmail.com>
2024-04-22 16:44:35 +0900

    feat: body background css 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,38 @@
-/** @jsxImportSource @emotion/react */
-
 import './App.css';
 
 import CardForm from './components/CardForm';
 import CardPreview from './components/CardPreview';
-import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import useCardInfo from './hooks/useCardInfo';
-
-const styledCardInfoContainer = css`
-  width: 315px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 30px;
-
-  margin: auto;
-  margin-top: 30px;
-  padding: 100px;
-  background-color: white;
-`;
 
 function App() {
   const { cardInfo, setCardNumbers, setCardExpiredDate, setCardHolder } =
     useCardInfo();
 
   return (
-    <section css={styledCardInfoContainer}>
+    <Payments>
       <CardPreview cardInfo={cardInfo} />
       <CardForm
         setCardNumbers={setCardNumbers}
         setCardExpiredDate={setCardExpiredDate}
         setCardHolder={setCardHolder}
       />
-    </section>
+    </Payments>
   );
 }
 
 export default App;
+
+const Payments = styled.section({
+  width: '315px',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '30px',
+
+  margin: 'auto',
+  marginTop: '30px',
+  padding: '100px',
+  backgroundColor: 'white',
+});
