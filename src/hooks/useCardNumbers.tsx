ea0ccacafbb3cf3cf887f072a6cb02f46b35d98b
hain-tain <hain7ce@gmail.com>
2024-04-22 18:20:02 +0900

    feat: useCardNumbers hook 생성

---
+++ src/hooks/useCardNumbers.tsx
@@ -0,0 +1,35 @@
+import useInput, { UseInputHookValue } from "./useInput";
+import { validateLength, validateOnlyDigit } from "../domain/validateInput";
+
+import { useState } from "react";
+
+export default function useCardNumbers() {
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const useInputProps = {
+    validator: (string: string) => {
+      validateOnlyDigit(string);
+      validateLength(string, 4);
+
+      setErrorMessage("");
+    },
+    errorHandler: (error: unknown) => {
+      if (!(error instanceof Error)) return;
+      setErrorMessage(error.message);
+    },
+  };
+
+  const firstInput = useInput(useInputProps);
+  const secondInput = useInput(useInputProps);
+  const thirdInput = useInput(useInputProps);
+  const fourthInput = useInput(useInputProps);
+
+  const cardNumberInputs: [
+    UseInputHookValue,
+    UseInputHookValue,
+    UseInputHookValue,
+    UseInputHookValue,
+  ] = [firstInput, secondInput, thirdInput, fourthInput];
+
+  return { cardNumberInputs, errorMessage };
+}
