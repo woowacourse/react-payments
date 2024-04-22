e5eb3f1216a240028a71846f8ce7fca7039bdc61
Seongjin Hong <hongseongjin.to@gmail.com>
2024-04-22 16:59:01 +0900

    refactor: 각 입력 컴포넌트의 UI 이외 로직들을 custom hook으로 분리

--- src/types/cardNumbers.ts
+++ src/types/cardNumbers.ts
@@ -1,1 +1,6 @@
 export type cardNumbersType = [string, string, string, string];
+export type cardNumbersValidStatesType = [boolean, boolean, boolean, boolean];
+export type cardNumbersValidType = {
+  validStates: cardNumbersValidStatesType;
+  errorMessage: string;
+};
