54e9eea667189781bce3860353a54a823a804d7f
Seongjin Hong <hongseongjin.to@gmail.com>
2024-04-22 16:56:14 +0900

    refactor: 앱 내 공용 검증 로직을 유틸 메서드로 분리

---
+++ src/utils/Validation.ts
@@ -0,0 +1,19 @@
+const Validation = {
+  isNumeric(value: string) {
+    return !Number.isNaN(Number(value));
+  },
+
+  isNumberInRange({ min, max, number }: { min: number; max: number; number: number }) {
+    return number >= min && number <= max;
+  },
+
+  hasLength(value: string, length: number) {
+    return value.length === length;
+  },
+
+  isEnglishWithSpace(value: string) {
+    return /^[a-zA-Z][a-zA-Z ]*$/.test(value);
+  },
+};
+
+export default Validation;
