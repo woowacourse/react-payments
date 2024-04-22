720423dffc7f409d3ffd7f82c76a69cb1cb7a7ce
novice0840 <magnolia0840@gmail.com>
2024-04-22 16:43:12 +0900

    fix: vite base 옵션 변경

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/react-payments/",
   resolve: {
     alias: [
       { find: "@", replacement: resolve(__dirname, "src") },
