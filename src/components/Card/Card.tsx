9b8560987e55ca76c0e8fa1da8d41c5422c9f3b5
pakxe <pigkill40@naver.com>
2024-04-23 01:04:16 +0900

    refactor: 함수에 두었던 ui를 위한 로직을 서브 컴포넌트로 분리

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,46 +6,49 @@ import { getCardType } from '../../utils/getCardType';
 import { Date } from '../../types/date';
 
 const Card = ({ cardNumbers, date, ownerName }: { cardNumbers: string[]; date: Date; ownerName: string }) => {
-  const displayCardLogo = () => {
-    const cardStyle = styles.logo;
-    const cardType = getCardType(cardNumbers[0]);
-
-    if (cardType === 'VISA') return <img src={Visa} className={cardStyle} />;
-    if (cardType === 'MASTER') return <img src={MasterCard} className={cardStyle} />;
-  };
-
-  const displayExpirationDate = () => {
-    const dateStringList = Object.values(date);
-
-    // 모두 빈 칸이라면 /를 띄우지 않도록
-    if (dateStringList.every((date) => date === '')) return;
-
-    return <span>{dateStringList.join('/')}</span>;
-  };
-
   return (
     <div className={styles.card}>
       <div className={styles.ic_chip}></div>
       <div className={styles.chip__logo__wrapper}>
         <div className={styles.chip}></div>
-        {displayCardLogo()}
+        <CardLogo cardNumbers={cardNumbers} />
       </div>
       <div className={styles.card__info__container}>
-        <div className={styles.card__number__container}>
-          {cardNumbers.map((cardNumber, i) => (
-            <span className={styles.number}>
-              {i >= cardNumbers.length / 2
-                ? new Array(cardNumber.toString().length)
-                    .fill(0)
-                    .map(() => <img src={hideNumber} width='4' height='4' />)
-                : cardNumber}
-            </span>
-          ))}
-          <div />
-        </div>
-        <div>{displayExpirationDate()}</div>
+        <CardNumbers cardNumbers={cardNumbers} />
+        <ExpirationDate date={date} />
         <span>{ownerName}</span>
       </div>
+    </div>
+  );
+};
+
+const CardLogo = ({ cardNumbers }: { cardNumbers: string[] }) => {
+  const cardStyle = styles.logo;
+  const cardType = getCardType(cardNumbers[0]);
+
+  if (cardType === 'VISA') return <img src={Visa} className={cardStyle} />;
+  if (cardType === 'MASTER') return <img src={MasterCard} className={cardStyle} />;
+};
+
+const ExpirationDate = ({ date }: { date: Date }) => {
+  const dateStringList = Object.values(date);
+
+  // 모두 빈 칸이라면 /를 띄우지 않도록
+  if (dateStringList.every((date) => date === '')) return;
+
+  return <span>{dateStringList.join('/')}</span>;
+};
+
+const CardNumbers = ({ cardNumbers }: { cardNumbers: string[] }) => {
+  return (
+    <div className={styles.card__number__container}>
+      {cardNumbers.map((cardNumber, i) => (
+        <span className={styles.number}>
+          {i >= cardNumbers.length / 2
+            ? new Array(cardNumber.toString().length).fill(0).map(() => <img src={hideNumber} width='4' height='4' />)
+            : cardNumber}
+        </span>
+      ))}
     </div>
   );
 };
