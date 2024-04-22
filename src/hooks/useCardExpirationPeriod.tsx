e2bc5e29d218e6decd566368cf57adb200cd57e6
hain-tain <hain7ce@gmail.com>
2024-04-22 18:23:38 +0900

    feat: useCardExpirationPeriod hook 생성

---
+++ src/hooks/useCardExpirationPeriod.tsx
@@ -0,0 +1,46 @@
+import useInput, { UseInputHookValue } from "./useInput";
+import {
+  validateLength,
+  validateMonth,
+  validateYear,
+} from "../domain/validateInput";
+
+import { useState } from "react";
+
+export default function useCardExpirationPeriod() {
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const printErrorMessage = (error: unknown) => {
+    if (!(error instanceof Error)) return;
+    setErrorMessage(error.message);
+  };
+
+  const monthInputProps = {
+    validator: (string: string) => {
+      validateMonth(string);
+      validateLength(string, 2);
+
+      setErrorMessage("");
+    },
+    errorHandler: printErrorMessage,
+  };
+
+  const yearInputProps = {
+    validator: (string: string) => {
+      validateYear(string);
+      validateLength(string, 2);
+
+      setErrorMessage("");
+    },
+    errorHandler: printErrorMessage,
+  };
+
+  const monthInput = useInput(monthInputProps);
+  const yearInput = useInput(yearInputProps);
+  const expirationPeriodInputs: [UseInputHookValue, UseInputHookValue] = [
+    monthInput,
+    yearInput,
+  ];
+
+  return { expirationPeriodInputs, errorMessage };
+}
