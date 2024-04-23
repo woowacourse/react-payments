047605f147c7b92a05b17f03277636f6f29e918b
김진호 <rlawlsgh1227@gmail.com>
2024-04-23 21:05:31 +0900

    chore: svg declare와 화살표 이미지 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+declare module '*.svg' {
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
