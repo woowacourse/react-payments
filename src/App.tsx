e8c6a90c1c7421f9ab847ed133c8319a38bbfa2f
Largopie <106071687+Largopie@users.noreply.github.com>
2024-04-23 22:18:48 +0900

    refactor(useCardNumber): 이미지 state관리 useCardNumber 측에서 하도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment } from 'react';
 import GlobalStyles from './GlobalStyles';
 import * as S from './App.style';
 
@@ -6,8 +6,6 @@ import InputSection from './components/InputSection';
 import Input from './components/composables/Input';
 import ScreenReaderOnlyLabel from './components/composables/ScreenReaderOnlyLabel';
 import CreditCard from './components/CreditCard';
-import MasterCardImage from './assets/images/mastercard.png';
-import VisaCardImage from './assets/images/visa.png';
 
 import useCardNumber from './hooks/useCardNumber';
 import useInput, { ValidationType } from './hooks/useInput';
@@ -28,10 +26,8 @@ const INITIAL_CARD_NUMBER_STATE: CardNumberState = {
 };
 
 function App() {
-  const { cardNumbers, cardNumbersChangeHandler, cardNumbersFocusOutHandler } = useCardNumber(
-    Array.from({ length: 4 }, () => INITIAL_CARD_NUMBER_STATE),
-  );
-  const [cardImageSrc, setCardImageSrc] = useState('');
+  const { cardImageSrc, cardNumbers, cardNumbersChangeHandler, cardNumbersFocusOutHandler } =
+    useCardNumber(Array.from({ length: 4 }, () => INITIAL_CARD_NUMBER_STATE));
 
   const twoDigitValidation = {
     isError: (state: string) => state !== '' && !validate.isSatisfiedLength(2, state.length),
@@ -92,20 +88,6 @@ function App() {
     error: nameError,
   } = useInput(nameOnChangeValidations);
 
-  useEffect(() => {
-    setCardImageSrc('');
-
-    const cardNumberString = cardNumbers.map((cardNumber) => cardNumber.value).join('');
-
-    if (validate.isVisa(cardNumberString)) {
-      setCardImageSrc(VisaCardImage);
-    }
-
-    if (validate.isMasterCard(cardNumberString)) {
-      setCardImageSrc(MasterCardImage);
-    }
-  }, [cardNumbers]);
-
   return (
     <>
       <GlobalStyles />
@@ -117,98 +99,98 @@ function App() {
           name={name}
           cardImageSrc={cardImageSrc}
         />
-        <S.CardInfoContainer>
-          <S.Wrapper>
-            <InputSection
-              title={CARD_NUMBER.title}
-              description={CARD_NUMBER.description}
-              inputTitle={CARD_NUMBER.inputTitle}
-            >
-              {cardNumbers.map((cardNumber, index) => {
-                const uniqueId = 'cardNumbers' + index;
-                return (
-                  <Fragment key={uniqueId}>
-                    <ScreenReaderOnlyLabel
-                      htmlFor={uniqueId}
-                      description={`카드 번호 ${index + 1}번째 입력 섹션`}
-                    />
-                    <Input
-                      id={uniqueId}
-                      placeholder="1234"
-                      type="text"
-                      maxLength={4}
-                      value={cardNumber.value}
-                      onChange={(e) => cardNumbersChangeHandler(e, index)}
-                      onBlur={(e) => cardNumbersFocusOutHandler(e, index)}
-                      isError={cardNumber.isError}
-                    />
-                  </Fragment>
-                );
-              })}
-            </InputSection>
-            <S.ErrorWrapper>
-              <S.ErrorMessage>
-                {cardNumbers.find(({ isError }) => isError)?.errorMessage}
-              </S.ErrorMessage>
-            </S.ErrorWrapper>
-          </S.Wrapper>
+      <S.CardInfoContainer>
+        <S.Wrapper>
+          <InputSection
+            title={CARD_NUMBER.title}
+            description={CARD_NUMBER.description}
+            inputTitle={CARD_NUMBER.inputTitle}
+          >
+            {cardNumbers.map((cardNumber, index) => {
+              const uniqueId = 'cardNumbers' + index;
+              return (
+                <Fragment key={uniqueId}>
+                  <ScreenReaderOnlyLabel
+                    htmlFor={uniqueId}
+                    description={`카드 번호 ${index + 1}번째 입력 섹션`}
+                  />
+                  <Input
+                    id={uniqueId}
+                    placeholder="1234"
+                    type="text"
+                    maxLength={4}
+                    value={cardNumber.value}
+                    onChange={(e) => cardNumbersChangeHandler(e, index)}
+                    onBlur={(e) => cardNumbersFocusOutHandler(e, index)}
+                    isError={cardNumber.isError}
+                  />
+                </Fragment>
+              );
+            })}
+          </InputSection>
+          <S.ErrorWrapper>
+            <S.ErrorMessage>
+              {cardNumbers.find(({ isError }) => isError)?.errorMessage}
+            </S.ErrorMessage>
+          </S.ErrorWrapper>
+        </S.Wrapper>
 
-          <S.Wrapper>
-            <InputSection
-              title={EXPIRATION_PERIOD.title}
-              description={EXPIRATION_PERIOD.description}
-              inputTitle={EXPIRATION_PERIOD.inputTitle}
-            >
-              <ScreenReaderOnlyLabel htmlFor={'month'} description={'월 입력'} />
-              <Input
-                id={'month'}
-                placeholder={'MM'}
-                type="text"
-                value={month}
-                maxLength={2}
-                onChange={monthChangeHandler}
-                isError={monthError.state}
-                onBlur={monthFocusOutHandler}
-              />
-              <ScreenReaderOnlyLabel htmlFor={'year'} description={'년도 입력'} />
-              <Input
-                id={'year'}
-                placeholder={'YY'}
-                type="text"
-                maxLength={2}
-                value={year}
-                onChange={yearChangeHandler}
-                isError={yearError.state}
-              />
-            </InputSection>
-            <S.ErrorWrapper>
-              <S.ErrorMessage>
-                {monthError.state && yearError.state && monthError.message}
-                {!monthError.state && yearError.state && yearError.message}
-                {monthError.state && !yearError.state && monthError.message}
-              </S.ErrorMessage>
-            </S.ErrorWrapper>
-          </S.Wrapper>
+        <S.Wrapper>
+          <InputSection
+            title={EXPIRATION_PERIOD.title}
+            description={EXPIRATION_PERIOD.description}
+            inputTitle={EXPIRATION_PERIOD.inputTitle}
+          >
+            <ScreenReaderOnlyLabel htmlFor={'month'} description={'월 입력'} />
+            <Input
+              id={'month'}
+              placeholder={'MM'}
+              type="text"
+              value={month}
+              maxLength={2}
+              onChange={monthChangeHandler}
+              isError={monthError.state}
+              onBlur={monthFocusOutHandler}
+            />
+            <ScreenReaderOnlyLabel htmlFor={'year'} description={'년도 입력'} />
+            <Input
+              id={'year'}
+              placeholder={'YY'}
+              type="text"
+              maxLength={2}
+              value={year}
+              onChange={yearChangeHandler}
+              isError={yearError.state}
+            />
+          </InputSection>
+          <S.ErrorWrapper>
+            <S.ErrorMessage>
+              {monthError.state && yearError.state && monthError.message}
+              {!monthError.state && yearError.state && yearError.message}
+              {monthError.state && !yearError.state && monthError.message}
+            </S.ErrorMessage>
+          </S.ErrorWrapper>
+        </S.Wrapper>
 
-          <S.Wrapper>
-            <InputSection title={OWNER_NAME.title} inputTitle={OWNER_NAME.inputTitle}>
-              <ScreenReaderOnlyLabel htmlFor={'name'} description={'이름 입력'} />
-              <Input
-                id="name"
-                maxLength={30}
-                onChange={nameChangeHandler}
-                isError={nameError.state}
-                placeholder="JOHN DOE"
-                type="text"
-                value={name.toUpperCase()}
-              />
-            </InputSection>
-            <S.ErrorWrapper>
-              <S.ErrorMessage>{nameError.state && OWNER_NAME.errorMessage}</S.ErrorMessage>
-            </S.ErrorWrapper>
-          </S.Wrapper>
-        </S.CardInfoContainer>
-      </S.Container>
+        <S.Wrapper>
+          <InputSection title={OWNER_NAME.title} inputTitle={OWNER_NAME.inputTitle}>
+            <ScreenReaderOnlyLabel htmlFor={'name'} description={'이름 입력'} />
+            <Input
+              id="name"
+              maxLength={30}
+              onChange={nameChangeHandler}
+              isError={nameError.state}
+              placeholder="JOHN DOE"
+              type="text"
+              value={name.toUpperCase()}
+            />
+          </InputSection>
+          <S.ErrorWrapper>
+            <S.ErrorMessage>{nameError.state && OWNER_NAME.errorMessage}</S.ErrorMessage>
+          </S.ErrorWrapper>
+        </S.Wrapper>
+      </S.CardInfoContainer>
+    </S.Container>
     </>
   );
 }
--- src/hooks/useCardNumber.ts
+++ src/hooks/useCardNumber.ts
@@ -1,10 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import validate from '../utils/validate';
 import { CardNumberState } from '../App';
 import { CARD_NUMBER } from '../constants/cardSection';
 
+import MasterCardImage from '../assets/images/mastercard.png';
+import VisaCardImage from '../assets/images/visa.png';
+
+const VALID_CARD_NUMBERS_LENGTH = 16;
+
 const useCardNumber = (initialStates: CardNumberState[]) => {
   const [cardNumbers, setCardNumbers] = useState<CardNumberState[]>(initialStates);
+  const [cardImageSrc, setCardImageSrc] = useState('');
 
   const cardNumbersChangeHandler = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
     if (e.target.value !== '' && !validate.isValidDigit(e.target.value)) {
@@ -19,7 +25,7 @@ const useCardNumber = (initialStates: CardNumberState[]) => {
       return;
     }
 
-    return setCardNumbers(
+    setCardNumbers(
       cardNumbers.map((cardNumber, i) => {
         if (i === index) {
           return {
@@ -46,7 +52,25 @@ const useCardNumber = (initialStates: CardNumberState[]) => {
     }
   };
 
-  return { cardNumbers, cardNumbersChangeHandler, cardNumbersFocusOutHandler };
+  useEffect(() => {
+    const cardNumberString = cardNumbers.map(({ value }) => value).join('');
+
+    if (cardImageSrc && VALID_CARD_NUMBERS_LENGTH !== cardNumberString.length) {
+      setCardImageSrc('');
+    }
+
+    if (VALID_CARD_NUMBERS_LENGTH === cardNumberString.length) {
+      if (validate.isVisa(cardNumberString)) {
+        setCardImageSrc(VisaCardImage);
+      }
+
+      if (validate.isMasterCard(cardNumberString)) {
+        setCardImageSrc(MasterCardImage);
+      }
+    }
+  }, [cardNumbers]);
+
+  return { cardImageSrc, cardNumbers, cardNumbersChangeHandler, cardNumbersFocusOutHandler };
 };
 
 export default useCardNumber;
