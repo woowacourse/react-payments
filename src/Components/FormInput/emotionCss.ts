da48bc925d8ad07d361556f9c1ae2dd3f8ad4fb3
lurgi <wahoo9040@naver.com>
2024-04-24 08:37:00 +0900

    refactor: 색상 상수 분리

--- src/Components/FormInput/emotionCss.ts
+++ src/Components/FormInput/emotionCss.ts
@@ -6,16 +6,19 @@ const SIZE: Record<NonNullable<SizePresetType>, "17%" | "43%" | "100%"> = {
   large: "100%",
 };
 
+const HIGHLIGHT_COLOR = `#FF3D3D`;
+const NORMAL_COLOR = `#ACACAC`;
+
 export const inputStyle = (sizePreset: SizePresetType, isError: boolean = false) =>
   css({
     height: "25px",
     borderRadius: "2px",
-    border: "solid 1px #ACACAC",
+    border: `solid 1px ${NORMAL_COLOR}`,
     padding: "8px",
     width: SIZE[sizePreset],
     marginTop: "10px",
     textTransform: "uppercase",
     "&:focus-visible": css({
-      outlineColor: isError ? "#FF3D3D" : "#ACACAC",
+      outlineColor: isError ? HIGHLIGHT_COLOR : NORMAL_COLOR,
     }),
   });
