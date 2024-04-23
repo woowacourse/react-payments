779f0c5d34a4855895daa9b9ab7566fd9768d7d7
Soyeon Choe <soy2302ten@gmail.com>
2024-04-23 23:56:35 +0900

    test: PasswordFormSection 스토리북 추가

---
+++ src/stories/PasswordFormSection.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PasswordFormSection from '../components/PasswordFormSection';
+
+const meta = {
+  title: 'PasswordFormSection',
+  component: PasswordFormSection,
+} satisfies Meta<typeof PasswordFormSection>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
