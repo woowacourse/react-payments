b3df33479c3fd6fe31126b9e8a9738452aa74810
lurgi <wahoo9040@naver.com>
2024-04-24 15:31:58 +0900

    test: FormInput 스토리 작성

---
+++ src/Components/FormInput/stories/CardWithContext.tsx
@@ -0,0 +1,68 @@
+import FormInput from "..";
+import {
+  CardNumbersContext,
+  CardOwnerInfoContext,
+  CardValidityPeriodContext,
+} from "../../../routes/Payments/CardInfoContextProvider";
+import { useState } from "react";
+import {
+  CardNumberErrorContext,
+  CardOwnerInfoErrorContext,
+  CardValidityPeriodErrorContext,
+} from "../../Form/ErrorContextProvider";
+
+interface Props {
+  children: React.ReactNode;
+  cardNumbersValue: CardNumbers;
+  cardPeriodValue: CardValidityPeriod;
+  cardOwnerValue: CardOwnerInfo;
+  cardNumbersError: CardNumbersError;
+  cardPeriodError: CardValidityPeriodError;
+  cardOwnerError: CardOwnerInfoError;
+}
+
+const CardWithContextCompound: React.FC<Props> = ({
+  children,
+  cardNumbersValue,
+  cardPeriodValue,
+  cardOwnerValue,
+  cardNumbersError,
+  cardPeriodError,
+  cardOwnerError,
+}) => {
+  return (
+    <CardNumbersContext.Provider value={useState(cardNumbersValue)}>
+      <CardValidityPeriodContext.Provider value={useState(cardPeriodValue)}>
+        <CardOwnerInfoContext.Provider value={useState(cardOwnerValue)}>
+          <CardNumberErrorContext.Provider value={useState(cardNumbersError)}>
+            <CardValidityPeriodErrorContext.Provider value={useState(cardPeriodError)}>
+              <CardOwnerInfoErrorContext.Provider value={useState(cardOwnerError)}>
+                {children}
+              </CardOwnerInfoErrorContext.Provider>
+            </CardValidityPeriodErrorContext.Provider>
+          </CardNumberErrorContext.Provider>
+        </CardOwnerInfoContext.Provider>
+      </CardValidityPeriodContext.Provider>
+    </CardNumbersContext.Provider>
+  );
+};
+
+const CardWithContext: Record<"CardNumberInput" | "CardPeriodInput" | "CardOwnerInput", React.FC<Props>> = {
+  CardNumberInput: (props) => (
+    <CardWithContextCompound {...props}>
+      <FormInput.CardNumberInput />
+    </CardWithContextCompound>
+  ),
+  CardPeriodInput: (props) => (
+    <CardWithContextCompound {...props}>
+      <FormInput.CardPeriodInput />
+    </CardWithContextCompound>
+  ),
+  CardOwnerInput: (props) => (
+    <CardWithContextCompound {...props}>
+      <FormInput.CardOwnerInput />
+    </CardWithContextCompound>
+  ),
+};
+
+export default CardWithContext;
---
+++ src/Components/FormInput/stories/FormPeriodInput.stories.tsx
@@ -0,0 +1,61 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import CardWithContext from "./CardWithContext";
+
+const component = CardWithContext.CardPeriodInput;
+
+const meta: Meta<typeof component> = {
+  component,
+  argTypes: {
+    cardNumbersValue: {
+      description: "카드 숫자에 대한 정보",
+    },
+    cardPeriodValue: {
+      description: "카드 유호기간에 대한 정보",
+    },
+    cardOwnerValue: {
+      description: "카드 사용자에 대한 정보",
+    },
+    cardNumbersError: {
+      description: "카드 에러에 대한 정보",
+    },
+    cardPeriodError: {
+      description: "카드 유효기간에 대한 정보",
+    },
+    cardOwnerError: {
+      description: "카드 소유자에 대한 정보",
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof component>;
+
+const errorDefault: ErrorType = {
+  isError: false,
+  errorMessage: "ERROR",
+};
+
+const defaultArgs = {
+  cardNumbersValue: { firstNumbers: "0000", secondNumbers: "1234", thirdNumbers: "1234", fourthNumbers: "1234" },
+  cardPeriodValue: {
+    month: 12,
+    year: 12,
+  },
+  cardOwnerValue: { name: "PARK JEONG WOO" },
+  cardNumbersError: {
+    firstNumbers: errorDefault,
+    secondNumbers: errorDefault,
+    thirdNumbers: errorDefault,
+    fourthNumbers: errorDefault,
+  },
+  cardPeriodError: {
+    month: errorDefault,
+    year: errorDefault,
+  },
+  cardOwnerError: { name: errorDefault },
+};
+
+export const Default: Story = {
+  name: "기본적인 FormPeriodInput",
+  args: defaultArgs,
+};
