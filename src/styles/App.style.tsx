710038e951584c920725c813d90fc2461a89d56a
김진호 <rlawlsgh1227@gmail.com>
2024-04-24 14:56:44 +0900

    design: app height 제한 해제

--- src/styles/App.style.tsx
+++ src/styles/App.style.tsx
@@ -6,11 +6,11 @@ export const Container = styled.div`
   align-items: center;
   gap: 4.5rem;
   width: 37.6rem;
-  height: 70rem;
   margin: 5rem auto;
   padding: 7.7rem 3.2rem 2rem 3.2rem;
 
   background-color: ${(props) => props.theme.color.white};
   border: 0.1rem solid ${(props) => props.theme.color.gray};
   border-radius: 0.4rem;
+  overflow-y: scroll;
 `;
