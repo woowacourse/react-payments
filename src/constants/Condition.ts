922e5bc297ee011d0b96eec7fca1e4ddb0230205
simhorim <hori5020@gmail.com>
2024-04-24 18:01:45 +0900

    feat(Condition): card 상수 추가

--- src/constants/Condition.ts
+++ src/constants/Condition.ts
@@ -11,6 +11,17 @@ const REG_EXP = {
   userName: /^[A-Z]+\s[A-Z]+$/,
 } as const;
 
+const CARD = {
+  bc: 0,
+  sinhan: 1,
+  kakaobank: 2,
+  hyndai: 3,
+  woori: 4,
+  lotte: 5,
+  hana: 6,
+  kb: 7,
+};
+
 const CONDITION = {
   MAX_LENGTH,
   REG_EXP,
@@ -19,6 +30,7 @@ const CONDITION = {
   showVisa: (first: number | undefined) => String(first)[0] === '4',
   showMasterCard: (first: number | undefined) =>
     String(first)[0] === '5' && ['1', '2', '3', '4', '5'].includes(String(first)[1]),
+  CARD,
 } as const;
 
 export default CONDITION;
