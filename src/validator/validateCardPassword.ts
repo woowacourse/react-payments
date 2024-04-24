3948ebfedcd1e155dd6167797f83b5eff7a91143
rbgksqkr <rbgks1937@naver.com>
2024-04-25 01:24:04 +0900

    feat(validateCardPassword): 카드 비밀번호 유효성 검증 구현

---
+++ src/validator/validateCardPassword.ts
@@ -0,0 +1,15 @@
+import { ErrorDetail } from '../components/types/error';
+import CONDITION from '../constants/condition';
+
+const validateCardPassword = (password: string): ErrorDetail => {
+  const isValidLength =
+    password.length === CONDITION.TEXT_LENGTH_MIN || password.length === CONDITION.PASSWORD_LENGTH_MAX;
+
+  if (!isValidLength) {
+    return { isError: true, errorMessage: '비밀번호는 2자리로 입력해주세요' };
+  }
+
+  return { isError: false, errorMessage: '' };
+};
+
+export default validateCardPassword;
