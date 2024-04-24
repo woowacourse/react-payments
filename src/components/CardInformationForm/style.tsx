d9f48731ceed3f90e2b5f73efa19ba6d5fc8ae47
simhorim <hori5020@gmail.com>
2024-04-24 17:01:04 +0900

    rename(CardInformationForm): prefix 수정

--- src/components/CardInformationForm/style.tsx
+++ src/components/CardInformationForm/style.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
-export const CardInformationForm = styled.form`
+export const StyledCardInformationForm = styled.form`
   display: flex;
   flex-direction: column;
   gap: 1.6rem;
+
   width: 100%;
 `;
