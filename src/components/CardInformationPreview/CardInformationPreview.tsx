2a2dfb5d83dd5eb2bc6ad64b9335e2d27aaaf9b1
simhorim <hori5020@gmail.com>
2024-04-24 18:02:13 +0900

    feat(CardInformationPreview): 카드색상 변경 기능 구현

--- src/components/CardInformationPreview/CardInformationPreview.tsx
+++ src/components/CardInformationPreview/CardInformationPreview.tsx
@@ -7,17 +7,36 @@ import CONDITION from '../../constants/Condition';
 import { CardNumberStateType } from '../../hooks/useCardNumber';
 import { ExpirationDateStateType } from '../../hooks/useExpirationDate';
 import { UserNameStateType } from '../../hooks/useUserName';
-
+import { SelectedCardStateType } from '../../hooks/useSelectedCardState';
+import MESSAGE from '../../constants/Message';
 interface CardInformationPreviewProps {
   cardNumberState: CardNumberStateType;
   expirationDateState: ExpirationDateStateType;
   userNameState: UserNameStateType;
+  selectedCardState: SelectedCardStateType;
 }
+
+const { OPTION } = MESSAGE;
+
+const selectedCardColor = (selectedCard: string) => {
+  return (
+    (selectedCard === OPTION.cardSelect[CONDITION.CARD.bc] && theme.color.magenta) ||
+    (selectedCard === OPTION.cardSelect[CONDITION.CARD.sinhan] && theme.color.blue) ||
+    (selectedCard === OPTION.cardSelect[CONDITION.CARD.kakaobank] && theme.color.yellow) ||
+    (selectedCard === OPTION.cardSelect[CONDITION.CARD.hyndai] && theme.color.black) ||
+    (selectedCard === OPTION.cardSelect[CONDITION.CARD.woori] && theme.color.brightBlue) ||
+    (selectedCard === OPTION.cardSelect[CONDITION.CARD.lotte] && theme.color.orange) ||
+    (selectedCard === OPTION.cardSelect[CONDITION.CARD.hana] && theme.color.teal) ||
+    (selectedCard === OPTION.cardSelect[CONDITION.CARD.kb] && theme.color.brownishGray) ||
+    theme.color.darkGray
+  );
+};
 
 const CardInformationPreview = ({
   cardNumberState,
   expirationDateState,
   userNameState,
+  selectedCardState,
 }: CardInformationPreviewProps) => {
   const { firstState, secondState, thirdState, fourthState } = cardNumberState;
   const { monthState, yearState } = expirationDateState;
@@ -25,7 +44,7 @@ const CardInformationPreview = ({
   const { isVisa, isMasterCard } = cardNumberState.showImageCondition;
 
   return (
-    <Styled.CardInformationPreview>
+    <Styled.CardInformationPreview $selectedCardColor={selectedCardColor(selectedCardState.value)}>
       <Styled.ImgContainer>
         <Styled.CardImg src={magnetic} alt="magnetic" />
         {isVisa && <Styled.CardImg src={visa} alt="visa" />}
