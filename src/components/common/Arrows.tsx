66afb781c3d4eab6d3965645b6f413f896184641
chysis <chysiss@naver.com>
2024-04-24 16:58:09 +0900

    feat: 드롭다운 common 컴포넌트 구현

---
+++ src/components/common/Arrows.tsx
@@ -0,0 +1,27 @@
+export const UpperArrow = () => {
+  return (
+    <svg width='10' height='6' viewBox='0 0 10 6' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M9.18007 4.54985L5.36014 0.729919L1.54021 4.54985'
+        stroke='#ACACAC'
+        strokeWidth='1.32867'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
+export const LowerArrow = () => {
+  return (
+    <svg width='10' height='6' viewBox='0 0 10 6' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M1.04016 0.729874L4.86009 4.5498L8.68002 0.729874'
+        stroke='black'
+        strokeWidth='1.32867'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
