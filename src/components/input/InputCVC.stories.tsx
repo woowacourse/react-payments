57e8c8fe4615d7ec8274435f0933e3b02ce98dd5
Lee sang Yeop <mma7710@naver.com>
2024-04-24 22:44:23 +0900

    feat(InputCVC): cvc 번호 입력하는 컴포넌트 추가

---
+++ src/components/input/InputCVC.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import InputCVC from "./InputCVC";
+
+const meta = {
+  title: "Payment/InputCVC",
+  component: InputCVC,
+
+  parameters: {
+    layout: "centered",
+    docs: {
+      description: {
+        component: "신용카드 CVC 입력 컴포넌트",
+      },
+    },
+  },
+
+  argTypes: {
+    inputValue: {
+      description: "CVC 번호",
+    },
+    handleChange: {
+      description: "CVC 번호 핸들러",
+    },
+    inputError: {
+      description: "에러 발생 여부",
+    },
+  },
+} satisfies Meta<typeof InputCVC>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: "신용카드 CVC 번호 입력",
+      },
+    },
+  },
+
+  args: {
+    inputValue: "",
+    handleChange: () => {},
+    inputError: false,
+  },
+};
---
+++ src/components/input/InputCVC.tsx
@@ -0,0 +1,44 @@
+import InputBox from "./common/InputBox";
+import CARD_FORM_MESSAGE from "../../constants/cardFormMessage";
+import styled from "@emotion/styled";
+import CARD_INPUTBOX_NAME from "../../constants/cardInputBoxName";
+
+interface InputCvcProps {
+  inputValue: string;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  inputError: boolean;
+}
+
+const InputCvc = ({ inputValue, handleChange, inputError }: InputCvcProps) => {
+  return (
+    <InputContainer>
+      <InputLabel htmlFor="cvcNumber">{CARD_FORM_MESSAGE.cvc}</InputLabel>
+      <InputBox
+        inputValue={inputValue}
+        handleChange={handleChange}
+        size="large"
+        placeholder="cvc 3자리"
+        id="cvcNumber"
+        name={CARD_INPUTBOX_NAME.info.cvc}
+        isError={inputError}
+      />
+    </InputContainer>
+  );
+};
+
+export default InputCvc;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const InputLabel = styled.label`
+  font-family: Noto Sans;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 15px;
+  text-align: left;
+  color: rgba(10, 13, 19, 1);
+  margin-bottom: 8px;
+`;
