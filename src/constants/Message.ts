583ac6cecc00221e52daa7176e8fe09b9eecfcef
simhorim <hori5020@gmail.com>
2024-04-24 17:02:29 +0900

    feat(Message): cardSelect 상수 추가

--- src/constants/Message.ts
+++ src/constants/Message.ts
@@ -1,12 +1,14 @@
 const TITLE = {
-  cardNumber: '결제할 카드 번호를 입력해 주세요',
-  expirationDate: '카드 유효기간을 입력해 주세요',
+  cardNumber: '결제할 카드 번호를 입력해주세요',
+  cardSelect: '카드사를 선택해주세요',
+  expirationDate: '카드 유효기간을 입력해주세요',
   userName: '카드 소유자 이름을 입력해 주세요',
 } as const;
 
 const CAPTION = {
   cardNumber: '본인 명의의 카드만 결제 가능합니다.',
-  expirationDate: '월/년도(MMYY)를 순서대로 입력해 주세요.',
+  cardSelect: '현재 국내 카드사만 가능합니다.',
+  expirationDate: '월/년도(MMYY)를 순서대로 입력해주세요.',
 } as const;
 
 const LABEL = {
@@ -29,12 +31,26 @@ const PLACEHOLDER = {
   userName: 'LAST_NAME FIRST_NAME',
 } as const;
 
+const OPTION = {
+  cardSelect: [
+    'BC카드',
+    '신한카드',
+    '카카오뱅크',
+    '현대카드',
+    '우리카드',
+    '롯데카드',
+    '하나카드',
+    '국민카드',
+  ] as string[],
+} as const;
+
 const MESSAGE = {
   TITLE,
   CAPTION,
   LABEL,
   ERROR,
   PLACEHOLDER,
+  OPTION,
 } as const;
 
 export default MESSAGE;
