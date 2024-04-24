26c30e02cd3963a0a78e76f9802850f963389ec4
lurgi <wahoo9040@naver.com>
2024-04-24 18:50:03 +0900

    chore: state추가

--- src/Components/Form/ErrorContextProvider.tsx
+++ src/Components/Form/ErrorContextProvider.tsx
@@ -21,6 +21,18 @@ const initCardOwnerInfo: CardOwnerInfoError = {
   name: initError(),
 };
 
+const initCardIssuerError: CardIssuerError = {
+  name: initError(),
+};
+
+const initCardCVCError: CardCVCError = {
+  value: initError(),
+};
+
+const initCardPasswordError: CardPasswordError = {
+  value: initError(),
+};
+
 export const CardNumberErrorContext = createContext<
   [CardNumbersError, Dispatch<SetStateAction<CardNumbersError>>] | null
 >(null);
@@ -30,17 +42,33 @@ export const CardValidityPeriodErrorContext = createContext<
 export const CardOwnerInfoErrorContext = createContext<
   [CardOwnerInfoError, Dispatch<SetStateAction<CardOwnerInfoError>>] | null
 >(null);
+export const CardIssuerErrorContext = createContext<
+  [CardIssuerError, Dispatch<SetStateAction<CardIssuerError>>] | null
+>(null);
+export const CardCVCErrorContext = createContext<[CardCVCError, Dispatch<SetStateAction<CardCVCError>>] | null>(null);
+export const CardPasswordErrorContext = createContext<
+  [CardPasswordError, Dispatch<SetStateAction<CardPasswordError>>] | null
+>(null);
 
 const ErrorContextProvider = ({ children }: { children: React.ReactNode }) => {
   const cardNumberErrorState = useState(initCardNumbersError);
   const cardValidityPeriodErrorState = useState(initCardValidityPeriod);
   const cardOwnerInfoErrorState = useState(initCardOwnerInfo);
+  const cardIssuerErrorContextState = useState(initCardIssuerError);
+  const cardCVCErrorContextState = useState(initCardCVCError);
+  const cardPasswordErrorState = useState(initCardPasswordError);
 
   return (
     <CardNumberErrorContext.Provider value={cardNumberErrorState}>
       <CardValidityPeriodErrorContext.Provider value={cardValidityPeriodErrorState}>
         <CardOwnerInfoErrorContext.Provider value={cardOwnerInfoErrorState}>
-          {children}
+          <CardIssuerErrorContext.Provider value={cardIssuerErrorContextState}>
+            <CardCVCErrorContext.Provider value={cardCVCErrorContextState}>
+              <CardPasswordErrorContext.Provider value={cardPasswordErrorState}>
+                {children}
+              </CardPasswordErrorContext.Provider>
+            </CardCVCErrorContext.Provider>
+          </CardIssuerErrorContext.Provider>
         </CardOwnerInfoErrorContext.Provider>
       </CardValidityPeriodErrorContext.Provider>
     </CardNumberErrorContext.Provider>
