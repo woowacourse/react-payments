7759ef553acc76e39e2590903889bbabf60e5338
lurgi <wahoo9040@naver.com>
2024-04-24 15:34:20 +0900

    refactor: 불필요한 타입 삭제

--- src/types.d.ts
+++ src/types.d.ts
@@ -35,33 +35,4 @@ interface CardOwnerInfoError {
   name?: ErrorType;
 }
 
-type CardInfoInputKey = keyof CardNumbers | keyof CardValidityPeriod | keyof CardOwnerInfo;
-
-interface CardInfo {
-  cardNumbers: CardNumbers;
-  cardValidityPeriod: CardValidityPeriod;
-  cardOwnerInfo: CardOwnerInfo;
-}
-
-type InitCardInfoType = {
-  key: keyof CardInfo;
-  initValue: ValueOf<CardInfo>;
-};
-
-interface ErrorState {
-  cardNumbers: {
-    firstNumbers: ErrorType;
-    secondNumbers: ErrorType;
-    thirdNumbers: ErrorType;
-    fourthNumbers: ErrorType;
-  };
-  cardValidityPeriod: {
-    month: ErrorType;
-    year: ErrorType;
-  };
-  cardOwnerInfo: {
-    name: ErrorType;
-  };
-}
-
 type SizePresetType = "small" | "medium" | "large";
