e3d1643da63bd5c2b38329b4e55683aacbe1e116
jayming66 <mjw8941@gmail.com>
2024-04-24 19:37:56 +0900

    refactor: Input 컴포넌트를 useInput 커스텀훅에 따라 변경

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -4,8 +4,6 @@ import * as S from "./style";
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   isError: boolean;
   ariaLabel?: string;
-  onChangeInput?: (inputValue: string) => void;
-  onBlurInput?: (inputValue: string) => void;
 }
 
 export default function Input({
@@ -16,8 +14,8 @@ export default function Input({
   value,
   ariaLabel,
   isError,
-  onChangeInput,
-  onBlurInput,
+  onChange,
+  onBlur,
 }: Props) {
   return (
     <S.Input
@@ -28,12 +26,8 @@ export default function Input({
       value={value}
       aria-label={ariaLabel}
       isError={isError}
-      onChange={(e) => {
-        if (onChangeInput) onChangeInput(e.target.value);
-      }}
-      onBlur={(e) => {
-        if (onBlurInput) onBlurInput(e.target.value);
-      }}
-    ></S.Input>
+      onChange={onChange}
+      onBlur={onBlur}
+    />
   );
 }
