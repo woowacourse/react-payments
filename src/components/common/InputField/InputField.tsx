2cff79fcac2edf9c5437e1ec606d06f7dd571a58
Seongjin Hong <hongseongjin.to@gmail.com>
2024-04-24 17:14:35 +0900

    refactor: InputField에 넘기는 label과 length prop을 선택요소로 변경

--- src/components/common/InputField/InputField.tsx
+++ src/components/common/InputField/InputField.tsx
@@ -1,16 +1,16 @@
 import * as S from './InputField.style';
 
 interface InputFieldProps {
-  label: string;
+  label?: string;
   errorMessage: string;
-  length: number;
+  length?: number;
   children: React.ReactNode;
 }
 
 export default function InputField({ label, errorMessage, length, children }: InputFieldProps) {
   return (
     <S.InputField>
-      <S.InputLabel>{label}</S.InputLabel>
+      {label && <S.InputLabel>{label}</S.InputLabel>}
       <S.InputContainer $length={length}>{children}</S.InputContainer>
       <S.ErrorMessage>{errorMessage}</S.ErrorMessage>
     </S.InputField>
