e912acca93f37672a538e7d1d8d851ea547a5da7
pakxe <pigkill40@naver.com>
2024-04-26 00:02:03 +0900

    feat: 드롭다운의 값을 위한 훅 구현

---
+++ src/hooks/useDropdown.ts
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { ERROR_MESSAGES } from '../constants/errorMessages';
+
+// 커스텀 드롭다운을 위한 훅
+const useDropdown = <T>() => {
+  const [selected, setSelected] = useState<T | null>(null);
+  const [errorMessage, setErrorMessages] = useState<string>('');
+
+  // 드롭다운을 선택하지 않으면 오류가 난다.
+  const handleSelect = (selected: T) => {
+    if (selected === null) {
+      setErrorMessages(ERROR_MESSAGES.NO_SELECT);
+      return;
+    }
+
+    setErrorMessages('');
+    setSelected(selected);
+  };
+
+  return {
+    selected,
+    handleSelect,
+    errorMessage,
+  };
+};
+
+export default useDropdown;
