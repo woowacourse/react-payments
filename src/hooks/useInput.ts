dd7952477fe9a82fe6d05871c618c0cf98bb5ba3
brgndy <brgndy96@gmail.com>
2024-04-25 15:05:27 +0900

    refactor: useInput 훅 수정

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,25 +1,105 @@
-import { useState } from 'react';
+import { useState, useRef, RefObject, useEffect } from 'react';
+import validate from '../utils/validate';
 
-const useInput = (validators: { fn: (value: string) => boolean }[]) => {
+type ValidateFn = {
+  fn: (value: string) => boolean;
+};
+
+type UseInputProps = {
+  validators: ValidateFn[];
+  nextRef?: RefObject<HTMLInputElement>;
+  nextStepHandler?: () => void;
+  isValidCurrentStep: boolean;
+  maxLength: number;
+};
+
+const useInput = ({
+  validators,
+  nextRef,
+  nextStepHandler,
+  maxLength,
+  isValidCurrentStep,
+}: UseInputProps) => {
   const [inputValue, setInputValue] = useState('');
-  const [error, setError] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isCompleted, setIsCompleted] = useState(false);
+  const ref = useRef<HTMLInputElement>(null);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setError(false);
     const inputValue = e.target.value.toUpperCase();
+    setIsError(false);
+
+    if (!inputValue.length) {
+      setInputValue('');
+      setIsCompleted(false);
+      return;
+    }
 
     for (let validator of validators) {
       if (!validator.fn(inputValue)) {
-        setError(true);
+        setIsError(true);
         setInputValue('');
         return;
       }
     }
 
-    setInputValue(e.target.value.toUpperCase());
+    setInputValue(inputValue);
+
+    if (inputValue.length === maxLength) {
+      setIsCompleted(true);
+      if (nextStepHandler && isValidCurrentStep) {
+        nextStepHandler();
+      }
+    } else {
+      setIsCompleted(false);
+    }
   };
 
-  return { inputValue, onChange, error };
+  const onEnter = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      if (
+        !isCompleted &&
+        nextStepHandler &&
+        !validate.isEmptyValue(inputValue) &&
+        isValidCurrentStep
+      ) {
+        nextStepHandler();
+      }
+
+      setIsCompleted(true);
+    }
+  };
+
+  const onBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+
+    setIsError(false);
+    if (!inputValue) {
+      setIsError(true);
+      return;
+    }
+    if (
+      !isCompleted &&
+      nextStepHandler &&
+      isValidCurrentStep &&
+      !validate.isEmptyValue(inputValue)
+    ) {
+      nextStepHandler();
+    }
+    setIsCompleted(true);
+  };
+
+  useEffect(() => {
+    if (isCompleted && ref && nextStepHandler && isValidCurrentStep) {
+      nextStepHandler();
+      ref.current?.blur();
+    }
+    if (isCompleted && nextRef) {
+      nextRef.current?.focus();
+    }
+  }, [isCompleted]);
+
+  return { inputValue, onChange, isError, onEnter, ref, onBlur };
 };
 
 export default useInput;
