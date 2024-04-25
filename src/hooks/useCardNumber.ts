b43c38c99720de7984152a73357f7808136e34f1
brgndy <brgndy96@gmail.com>
2024-04-25 15:05:17 +0900

    refactor: useCardNumber 훅 수정

--- src/hooks/useCardNumber.ts
+++ src/hooks/useCardNumber.ts
@@ -1,24 +1,36 @@
-import { useState } from 'react';
+import { useRef, useState, RefObject, useEffect } from 'react';
 import validate from '../utils/validate';
+import { CARD_NUMBER } from '../constants/cardSection';
+import { InitialCardNumberState } from 'types';
 
-export type InitialCardNumberState = {
-  value: string;
-  isError: boolean;
+type UseCardNumberHookProps = {
+  initialCardNumberStates: InitialCardNumberState[];
+  nextStepHandler: () => void;
+  ref: RefObject<HTMLSelectElement>;
+  isValidCurrentStep: boolean;
 };
 
-const MAX_CARD_NUMBER_LENGTH = 16;
-
-const useCardNumber = (initialStates: InitialCardNumberState[]) => {
-  const [cardNumbers, setCardNumbers] = useState<InitialCardNumberState[]>(initialStates);
+const useCardNumber = ({
+  initialCardNumberStates,
+  nextStepHandler,
+  ref,
+  isValidCurrentStep,
+}: UseCardNumberHookProps) => {
+  const [cardNumbers, setCardNumbers] = useState<InitialCardNumberState[]>(initialCardNumberStates);
   const [cardBrand, setCardBrand] = useState<'none' | 'Visa' | 'MasterCard'>('none');
+  const [isCompleted, setIsCompleted] = useState(false);
+  const refs = [
+    useRef<HTMLInputElement>(null),
+    useRef<HTMLInputElement>(null),
+    useRef<HTMLInputElement>(null),
+    useRef<HTMLInputElement>(null),
+  ];
 
   const handleCardBrandImage = (totalCardNumbers: string) => {
     if (validate.isVisa(totalCardNumbers)) {
       setCardBrand('Visa');
       return;
-    }
-
-    if (validate.isMasterCard(totalCardNumbers)) {
+    } else if (validate.isMasterCard(totalCardNumbers)) {
       setCardBrand('MasterCard');
       return;
     }
@@ -28,7 +40,9 @@ const useCardNumber = (initialStates: InitialCardNumberState[]) => {
 
   const cardNumbersChangeHandler = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
     const newValue = e.target.value;
-    const isValid = newValue === '' || validate.isValidDigit(newValue);
+
+    const isValid =
+      newValue === '' || validate.isValidDigit(newValue) || validate.isEmptyValue(newValue);
 
     const newCardNumbers = cardNumbers.map((cardNumber, i) => {
       if (i === index) {
@@ -37,18 +51,41 @@ const useCardNumber = (initialStates: InitialCardNumberState[]) => {
           isError: !isValid,
         };
       }
+
       return cardNumber;
     });
 
     const totalCardNumbers = newCardNumbers.map((card) => card.value).join('');
-    if (totalCardNumbers.length === MAX_CARD_NUMBER_LENGTH) {
+
+    if (totalCardNumbers.length === CARD_NUMBER.TOTAL_MAX_LENGTH) {
       handleCardBrandImage(totalCardNumbers);
     }
 
     setCardNumbers(newCardNumbers);
+
+    if (isCompleted && totalCardNumbers.length < CARD_NUMBER.TOTAL_MAX_LENGTH) {
+      setIsCompleted(false);
+    }
+
+    if (isValid && newValue.length === CARD_NUMBER.INDIVIDUAL_MAX_LENGTH && index < 3) {
+      refs[index + 1].current?.focus();
+    }
+
+    if (totalCardNumbers.length === CARD_NUMBER.TOTAL_MAX_LENGTH) {
+      if (isValidCurrentStep) {
+        nextStepHandler();
+      }
+      setIsCompleted(true);
+    }
   };
 
-  return { cardNumbers, cardNumbersChangeHandler, cardBrand };
+  useEffect(() => {
+    if (isCompleted && ref.current) {
+      ref.current.focus();
+    }
+  }, [isCompleted]);
+
+  return { cardNumbers, cardNumbersChangeHandler, cardBrand, refs };
 };
 
 export default useCardNumber;
