01a1b6fd1b1dec93419ff4213d71bd4247e98254
손진영 <sm099@naver.com>
2024-04-25 22:44:18 +0900

    fix(Router): baseUrl 추가
    프로덕션에서 올바르지 않은 url로 동작하는 문제 수정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -4,9 +4,11 @@ import { CardRegisterCompletePage, CardRegisterPage, NotFoundPage } from '@pages
 
 import { ROUTE_ENDPOINT_MAP } from '@routes/constant';
 
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/react-payments/' : '/';
+
 const Router = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={BASE_URL}>
       <Routes>
         <Route path={ROUTE_ENDPOINT_MAP.root} element={<CardRegisterPage />} />
         <Route path={ROUTE_ENDPOINT_MAP.cardRegisterComplete} element={<CardRegisterCompletePage />} />
