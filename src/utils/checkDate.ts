a6bb83ee8543730d4736957f739cda9945b4fa61
pakxe <pigkill40@naver.com>
2024-04-25 23:58:20 +0900

    feat: 유효성 검사 로직 분리

--- src/utils/checkDate.ts
+++ src/utils/checkDate.ts
@@ -20,15 +20,24 @@ export const checkExpired = (month: string, year: string): EXPIRED_TYPE => {
 };
 
 export const checkValidMonth = (month: string, isExpiredDate: EXPIRED_TYPE) => {
-  if (Number(month) <= 0 || Number(month) > 12) return ERROR_MESSAGES.INVALID_DATE;
   if (isNaN(Number(month))) return ERROR_MESSAGES.NOT_NUMBER;
+  if (Number(month) <= 0 || Number(month) > 12) return ERROR_MESSAGES.INVALID_DATE;
   if (isExpiredDate === 'INVALID_MONTH') return ERROR_MESSAGES.EXPIRED_DATE;
   return '';
 };
 
 export const checkValidYear = (year: string, isExpiredDate: EXPIRED_TYPE) => {
+  if (isNaN(Number(year))) return ERROR_MESSAGES.NOT_NUMBER;
   if (year.length > 0 && year.length < 2) return ERROR_MESSAGES.INVALID_DATE;
   if (isExpiredDate === 'INVALID_YEAR') return ERROR_MESSAGES.EXPIRED_DATE;
-  if (isNaN(Number(year))) return ERROR_MESSAGES.NOT_NUMBER;
   return '';
+};
+
+export const isValidDate = (month: string, year: string) => {
+  const isExpiredDate = checkExpired(month, year);
+
+  const monthErrorMessage = checkValidMonth(month, isExpiredDate);
+  const yearErrorMessage = checkValidYear(year, isExpiredDate);
+
+  return !isExpiredDate && monthErrorMessage === '' && yearErrorMessage === '' && month.length >= 2 && year.length >= 2;
 };
