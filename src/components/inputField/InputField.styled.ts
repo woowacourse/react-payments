9430dcc357d8da76f401431ad8b12036805e2c80
rbgksqkr <rbgks1937@naver.com>
2024-04-26 01:02:02 +0900

    refactor: InputField 스타일 코드 분리

---
+++ src/components/inputField/InputField.styled.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 10px;
+`;
+
+export const Title = styled.div`
+  font-size: 24px;
+  font-weight: 700;
+  margin: 0;
+`;
+
+export const Subtitle = styled.div`
+  font-size: 13px;
+  font-weight: 300;
+  margin: 10px 0 12px 0;
+  color: #8b95a1;
+`;
+
+export const Label = styled.label`
+  font-size: 15.5px;
+  font-weight: 400;
+  display: block;
+  margin-top: 24px;
+  color: #0a0d13;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  width: 440px;
+  justify-content: space-between;
+  margin-top: 8px;
+`;
