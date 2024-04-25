f1b3d9cc1cc37595b65107feea3ad6af599bfcd1
hain-tain <hain7ce@gmail.com>
2024-04-25 18:04:30 +0900

    feat: SelectBox 생성

---
+++ src/stories/SelectionBox.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import SelectBox from "../components/common/SelectBox";
+import { CARD_ISSUER } from "../constants/cardIssuer";
+import { useState } from "react";
+import { fn } from "@storybook/test";
+
+const meta = {
+  title: "SelectBox",
+  component: SelectBox,
+  decorators: [
+    (Story, context) => {
+      const [selected, setSelected] = useState("카드사를 선택해주세요");
+
+      return <Story args={{ ...context.args, selected, setSelected }} />;
+    },
+  ],
+  args: {
+    setSelected: fn(),
+  },
+} satisfies Meta<typeof SelectBox>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    options: CARD_ISSUER,
+    selected: "카드사를 선택해주세요",
+  },
+};
