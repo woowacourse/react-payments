475856925bb4ca71e0c5c5db37c95a4d20afe8a6
simhorim <hori5020@gmail.com>
2024-04-25 17:12:22 +0900

    feat(useInput): 입력이 유효할 때 다음 필드로 넘어가는 동적 입력 구현

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const useInput = <T>(defaultValue: T, condition: RegExp) => {
   const [value, setValue] = useState<T>(defaultValue);
@@ -7,6 +7,11 @@ const useInput = <T>(defaultValue: T, condition: RegExp) => {
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setError(!condition.test(event.currentTarget.value));
     setValue(event.currentTarget.value as T);
+
+    const isValueValid = event.currentTarget.value && condition.test(event.currentTarget.value);
+    if (isValueValid && event.target.nextElementSibling) {
+      (event.target.nextElementSibling as HTMLElement).focus();
+    }
   };
 
   return [value, onChange, error] as const;
