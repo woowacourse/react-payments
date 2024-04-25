740e5dbb1f584850a6dbe57a4a505d05a78422ef
rbgksqkr <rbgks1937@naver.com>
2024-04-25 11:03:03 +0900

    chore(validateCardholderName): 중복 검증 로직 제거

--- src/validator/validateCardholderName.ts
+++ src/validator/validateCardholderName.ts
@@ -11,12 +11,7 @@ const validateEnglish = (cardholderName: string): ErrorDetail => {
 };
 
 const validateCardholderName = (cardholderName: string): ErrorDetail => {
-  const isEnglish = /^[a-zA-Z ]+$/.test(cardholderName);
   const containSideBlankOrMultipleBlanks = !/^(?!\s)(?!.*\s\s)(?!.*\s$)/.test(cardholderName);
-
-  if (!isEnglish) {
-    return { isError: true, errorMessage: '카드 소유자 이름을 영어로만 입력해주세요' };
-  }
 
   if (containSideBlankOrMultipleBlanks) {
     return { isError: true, errorMessage: '양 끝에 공백이 포함되면 안 되며, 사이 공백의 길이는 최대 1입니다.' };
