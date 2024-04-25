68b2d37716a313fccb7ef1e2f962475c7de37af7
rbgksqkr <rbgks1937@naver.com>
2024-04-25 19:34:58 +0900

    refactor: 커스텀 훅 cardInfo 추상화 레벨 맞추기

--- src/hooks/useCardInfo.ts
+++ src/hooks/useCardInfo.ts
@@ -1,31 +1,19 @@
-import useInputs from './useInputs';
 import useExpiryMonth from './useExpiryMonth';
 import useExpiryYear from './useExpiryYear';
 import useCardholderName from './useCardholderName';
-import validateCardNumber from '../validator/validateCardNumber';
 import useSelect from './useSelect';
 import { CARD_TYPE } from '../types/card';
-import useInput from './useInput';
-import validateNumber from '../validator/validateNumber';
-import validateCardPassword from '../validator/validateCardPassword';
 import useCardCVC from './useCardCVC';
-
-const INITIAL_CARD_NUMBER = { first: '', second: '', third: '', fourth: '' };
+import useCardNumber from './useCardNumber';
+import useCardPassword from './useCardPassword';
 
 const useCardInfo = () => {
-  const cardNumberInfo = useInputs(INITIAL_CARD_NUMBER, {
-    onChange: validateNumber,
-    onBlur: validateCardNumber,
-  });
+  const cardNumberInfo = useCardNumber();
   const expiryMonth = useExpiryMonth();
   const expiryYear = useExpiryYear();
   const cardholderNameInfo = useCardholderName();
-
   const cardCompanyInfo = useSelect<CARD_TYPE>('');
-  const cardPasswordInfo = useInput('', {
-    onChange: validateNumber,
-    onBlur: validateCardPassword,
-  });
+  const cardPasswordInfo = useCardPassword();
   const cardCVCInfo = useCardCVC();
 
   return {
