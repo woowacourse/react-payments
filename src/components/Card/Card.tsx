976f543b0e9ba8816339a6e6cfd6e38e6f3195ea
greetings1012 <ahnqwerty@gmail.com>
2024-04-25 20:06:36 +0900

    feat: 카드사 선택에 따라 카드 색상이 바뀌도록 구현

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,7 +2,6 @@ import MASTERCARD_IMAGE from "../../assets/image/Mastercard.png";
 import VISA_IMAGE from "../../assets/image/Visa.png";
 import CARD_NUMBER_BLIND_IMAGE from "../../assets/image/CardNumberBlind.svg";
 import checkCardType from "../../utils/checkCardType";
-
 import styles from "./Card.module.css";
 
 const Card = ({
@@ -17,16 +16,38 @@ const Card = ({
   ownerName: string;
 }) => {
   const displayCardLogo = () => {
-    const cardStyle = styles.logo;
-
     if (checkCardType(cardNumbers[0]) === "Mastercard")
-      return <img src={MASTERCARD_IMAGE} className={cardStyle} />;
+      return <img src={MASTERCARD_IMAGE} className={styles.logo} />;
     if (checkCardType(cardNumbers[0]) === "Visa")
-      return <img src={VISA_IMAGE} className={cardStyle} />;
+      return <img src={VISA_IMAGE} className={styles.logo} />;
   };
 
+  const getCardCompanyClass = (companyName: string) => {
+    switch (companyName) {
+      case "BC카드":
+        return styles.BC;
+      case "신한카드":
+        return styles.shinhan;
+      case "카카오뱅크":
+        return styles.kakao;
+      case "현대카드":
+        return styles.hyundai;
+      case "우리카드":
+        return styles.woori;
+      case "롯데카드":
+        return styles.lotte;
+      case "하나카드":
+        return styles.hana;
+      case "국민카드":
+        return styles.kookmin;
+      default:
+        return "";
+    }
+  };
+
+  const cardCompanyClass = getCardCompanyClass(cardCompanyName);
   return (
-    <div className={styles.card}>
+    <div className={`${styles.card} ${cardCompanyClass}`}>
       <div className={styles.ic_chip}></div>
       <div className={styles.chip__logo__wrapper}>
         <div className={styles.chip}></div>
