e2d46143ebb0139e8c8977c350e5aee0358ce5e3
ImxYJL <allensain14@gmail.com>
2024-04-25 17:04:08 +0900

    feat: 모든 필드가 유효한 경우에만 폼을 제출하는 확인 버튼을 렌더링하는 기능 구현

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import InputDescription from "./InputDescription";
 import ExpirationDateForm from "./ExpirationDateForm";
@@ -45,6 +45,8 @@ const Form = ({
   setPassword: React.Dispatch<React.SetStateAction<Map<string, string>>>;
   setIsFrontCardPreview: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const [isAllFormValid, setIsAllFormValid] = useState(false);
+
   const [isAllCardNumberValid, setIsAllCardNumberValid] = useState(false);
   const [isCardNumberFormFilledOnce, setIsCardNumberFormFilledOnce] =
     useState(false);
@@ -67,104 +69,125 @@ const Form = ({
   const [isPasswordValid, setIsPasswordValid] = useState(false);
   const [isPasswordFilledOnce, setIsPasswordFilledOnce] = useState(false);
 
+  useEffect(() => {
+    const allValid =
+      isAllCardNumberValid &&
+      isCardCompanySelected &&
+      isAllExpirationDateValid &&
+      isUserNameValid &&
+      isCVCNumberValid &&
+      isPasswordValid;
+    setIsAllFormValid(allValid);
+  }, [
+    isAllCardNumberValid,
+    isCardCompanySelected,
+    isAllExpirationDateValid,
+    isUserNameValid,
+    isCVCNumberValid,
+    isPasswordValid,
+  ]);
+
   return (
-    <FormWrapper>
-      <div style={{ height: "170px" }}>
-        <InputDescription
-          title="결제할 카드 번호를 입력해 주세요"
-          description="본인 명의의 카드만 결제 가능합니다."
-        ></InputDescription>
-        <CardNumberForm
-          labelContent="카드 번호"
-          inputCount={4}
-          type="text"
-          placeholders={["1234", "1234", "1234", "1234"]}
-          setCardNumbers={setCardNumbers}
-          setAllFormsValid={setIsAllCardNumberValid}
-          setIsFormFilledOnce={setIsCardNumberFormFilledOnce}
-        ></CardNumberForm>
-      </div>
-
-      {isCardNumberFormFilledOnce && (
-        <div style={{ height: "137px" }}>
-          <InputDescription
-            title="카드사를 선택해 주세요"
-            description="현재 국내 카드사만 가능합니다."
-          ></InputDescription>
-          <CardCompanyForm
-            setCardCompany={setCardCompany}
-            setAllFormsValid={setIsCardCompanySelected}
-            setIsFormFilledOnce={setIsCardCompanySelectedOnce}
-          ></CardCompanyForm>
-        </div>
-      )}
-
-      {isCardCompanySelectedOnce && (
+    <>
+      <FormWrapper>
         <div style={{ height: "170px" }}>
           <InputDescription
-            title="카드 유효기간을 입력해 주세요"
-            description="월/년도(MMYY)를 순서대로 입력해 주세요."
+            title="결제할 카드 번호를 입력해 주세요"
+            description="본인 명의의 카드만 결제 가능합니다."
           ></InputDescription>
-          <ExpirationDateForm
-            labelContent="유효기간"
-            inputCount={2}
+          <CardNumberForm
+            labelContent="카드 번호"
+            inputCount={4}
             type="text"
-            placeholders={["MM", "YY"]}
-            setExpirationDate={setExpirationDate}
-            setAllFormsValid={setIsAllExpirationDateValid}
-            setIsFormFilledOnce={setIsExpirationDateFilledOnce}
-          ></ExpirationDateForm>
+            placeholders={["1234", "1234", "1234", "1234"]}
+            setCardNumbers={setCardNumbers}
+            setAllFormsValid={setIsAllCardNumberValid}
+            setIsFormFilledOnce={setIsCardNumberFormFilledOnce}
+          ></CardNumberForm>
         </div>
-      )}
 
-      {isExpirationDateFilledOnce && (
-        <div style={{ height: "170px" }}>
-          <InputDescription title="카드 소유자 이름을 입력해 주세요"></InputDescription>
-          <UserNameForm
-            labelContent="소유자 이름"
-            inputCount={1}
-            type="text"
-            placeholders={["JOHN DOE"]}
-            setUserName={setUserName}
-            setAllFormsValid={setIsUserNameValid}
-            setIsFormFilledOnce={setIsUserNameFilledOnce}
-          ></UserNameForm>
-        </div>
-      )}
+        {isCardNumberFormFilledOnce && (
+          <div style={{ height: "137px" }}>
+            <InputDescription
+              title="카드사를 선택해 주세요"
+              description="현재 국내 카드사만 가능합니다."
+            ></InputDescription>
+            <CardCompanyForm
+              setCardCompany={setCardCompany}
+              setAllFormsValid={setIsCardCompanySelected}
+              setIsFormFilledOnce={setIsCardCompanySelectedOnce}
+            ></CardCompanyForm>
+          </div>
+        )}
 
-      {isUserNameFilledOnce && (
-        <div style={{ height: "170px" }}>
-          <InputDescription title="CVC 번호를 입력해 주세요"></InputDescription>
-          <CVCNumberForm
-            labelContent="CVC"
-            inputCount={1}
-            type="text"
-            placeholders={["123"]}
-            setCVCNumber={setCVCNumber}
-            setAllFormsValid={setIsCVCNumberValid}
-            setIsFormFilledOnce={setIsCVCNumberFilledOnce}
-            setIsFrontCardPreview={setIsFrontCardPreview}
-          ></CVCNumberForm>
-        </div>
-      )}
+        {isCardCompanySelectedOnce && (
+          <div style={{ height: "170px" }}>
+            <InputDescription
+              title="카드 유효기간을 입력해 주세요"
+              description="월/년도(MMYY)를 순서대로 입력해 주세요."
+            ></InputDescription>
+            <ExpirationDateForm
+              labelContent="유효기간"
+              inputCount={2}
+              type="text"
+              placeholders={["MM", "YY"]}
+              setExpirationDate={setExpirationDate}
+              setAllFormsValid={setIsAllExpirationDateValid}
+              setIsFormFilledOnce={setIsExpirationDateFilledOnce}
+            ></ExpirationDateForm>
+          </div>
+        )}
 
-      {isCVCNumberFilledOnce && (
-        <div style={{ height: "170px" }}>
-          <InputDescription
-            title="비밀번호를 입력해 주세요"
-            description="앞의 2자리를 입력해주세요"
-          ></InputDescription>
-          <PasswordForm
-            labelContent="비밀번호 앞 2자리"
-            inputCount={1}
-            type="password"
-            setPassword={setPassword}
-            setAllFormsValid={setIsPasswordValid}
-            setIsFormFilledOnce={setIsPasswordFilledOnce}
-          ></PasswordForm>
-        </div>
-      )}
-    </FormWrapper>
+        {isExpirationDateFilledOnce && (
+          <div style={{ height: "170px" }}>
+            <InputDescription title="카드 소유자 이름을 입력해 주세요"></InputDescription>
+            <UserNameForm
+              labelContent="소유자 이름"
+              inputCount={1}
+              type="text"
+              placeholders={["JOHN DOE"]}
+              setUserName={setUserName}
+              setAllFormsValid={setIsUserNameValid}
+              setIsFormFilledOnce={setIsUserNameFilledOnce}
+            ></UserNameForm>
+          </div>
+        )}
+
+        {isUserNameFilledOnce && (
+          <div style={{ height: "170px" }}>
+            <InputDescription title="CVC 번호를 입력해 주세요"></InputDescription>
+            <CVCNumberForm
+              labelContent="CVC"
+              inputCount={1}
+              type="text"
+              placeholders={["123"]}
+              setCVCNumber={setCVCNumber}
+              setAllFormsValid={setIsCVCNumberValid}
+              setIsFormFilledOnce={setIsCVCNumberFilledOnce}
+              setIsFrontCardPreview={setIsFrontCardPreview}
+            ></CVCNumberForm>
+          </div>
+        )}
+
+        {isCVCNumberFilledOnce && (
+          <div style={{ height: "170px" }}>
+            <InputDescription
+              title="비밀번호를 입력해 주세요"
+              description="앞의 2자리를 입력해주세요"
+            ></InputDescription>
+            <PasswordForm
+              labelContent="비밀번호 앞 2자리"
+              inputCount={1}
+              type="password"
+              setPassword={setPassword}
+              setAllFormsValid={setIsPasswordValid}
+              setIsFormFilledOnce={setIsPasswordFilledOnce}
+            ></PasswordForm>
+          </div>
+        )}
+      </FormWrapper>
+      {isAllFormValid && <SubmitButton>확인</SubmitButton>}
+    </>
   );
 };
 
