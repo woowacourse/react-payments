7a7eace784618be892ffc26aa28e8f3578b2c078
greetings1012 <ahnqwerty@gmail.com>
2024-04-25 20:07:00 +0900

    style: 카드 추가 완료 페이지 css 작성

--- src/pages/CardAddedPage/CardAddedPage.tsx
+++ src/pages/CardAddedPage/CardAddedPage.tsx
@@ -1,13 +1,19 @@
 import styles from "./CardAddedPage.module.css";
-import GoMainButton from "../../components/\bButton/GoMainButton";
+import GoMainButton from "../../components/Button/GoMainButton";
 import { useNavigate, useLocation } from "react-router-dom";
+import CheckImage from "../../assets/image/CheckImage.svg";
 
 export default function CardAddedPage() {
   const location = useLocation();
   const navigate = useNavigate();
   return (
-    <div>
-      <div>{location.state.firstFourCardNumber}</div>
+    <div className={styles.wrapper}>
+      <img src={CheckImage}></img>
+      <div className={styles.description}>
+        {location.state.firstFourCardNumber}로 시작하는
+        <br />
+        {location.state.cardCompanyName}가 등록되었어요.
+      </div>
       <GoMainButton onClick={() => navigate("/")}></GoMainButton>
     </div>
   );
