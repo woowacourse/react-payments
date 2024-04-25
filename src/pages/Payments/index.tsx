3acf5f801e3a22e335ce8ab7cf6354c50086a30a
Lee sang Yeop <mma7710@naver.com>
2024-04-25 17:12:44 +0900

    feat(Payments): 각 입력이 완료되면 다음 컴포넌트가 동적으로 렌더링되도록 추가

--- src/pages/Payments/index.tsx
+++ src/pages/Payments/index.tsx
@@ -3,36 +3,138 @@ import CreditCard from "../../components/creditCard";
 import CreditCardForm from "../../components/creditCardForm";
 import useInput from "../../hooks/useInput";
 import CARD_FORM_MESSAGE from "../../constants/cardFormMessage";
-import { CardNumberValue, ExpirationPeriodValue } from "../../@types/CreditCard";
+import {
+  AuthenticationValue,
+  CardNumberValue,
+  ExpirationPeriodValue,
+  InfoValue,
+  OwnerValue,
+} from "../../@types/CreditCard";
 import SIGN from "../../constants/sign";
 import InputCreditCardNumber from "../../components/input/InputCreditCardNumber";
 import InputExpirationPeriod from "../../components/input/InputExpirationPeriod";
 import InputOwnerName from "../../components/input/InputOwnerName";
+import SelectBox from "../../components/select/common/SelectBox";
+import CARDTYPE, { CardType } from "../../constants/cardType";
+import useSelect from "../../hooks/useSelect";
+import InputCVC from "../../components/input/InputCVC";
+import InputPassword from "../../components/input/InputPassword";
+import useCurrentIndex from "../../hooks/useCurrentIndex";
 
-interface Owner {
-  name: string;
+interface CreditCardForms {
+  title: string;
+  description: string;
+  inputError: boolean;
+  childComponent: JSX.Element;
 }
 
 const Payments = () => {
-  const [cardNumber, setCardNumber, cardNumberError] = useInput<CardNumberValue>({
-    firstValue: SIGN.empty,
-    secondValue: SIGN.empty,
-    thirdValue: SIGN.empty,
-    fourthValue: SIGN.empty,
-  });
+  const [cardNumber, setCardNumber, cardNumberError, cardNumberIsComplete] =
+    useInput<CardNumberValue>({
+      firstValue: SIGN.empty,
+      secondValue: SIGN.empty,
+      thirdValue: SIGN.empty,
+      fourthValue: SIGN.empty,
+    });
 
-  const [expirationPeriod, setExpirationPeriod, expirationPeriodError] =
+  const { isDropdown, handleDropdown, selected, handleSelected } = useSelect<CardType>();
+
+  const [expirationPeriod, setExpirationPeriod, expirationPeriodError, expirationPeriodIsComplete] =
     useInput<ExpirationPeriodValue>({
       month: SIGN.empty,
       year: SIGN.empty,
     });
 
-  const [owner, setOwner, ownerError] = useInput<Owner>({ name: SIGN.empty });
+  const [owner, setOwner, ownerError, ownerIsComplete] = useInput<OwnerValue>({ name: SIGN.empty });
+  const [info, setInfo, infoError, infoIsComplete] = useInput<InfoValue>({ cvc: SIGN.empty });
+  const [authentication, setAuthentication, authenticationError, authenticationComplete] =
+    useInput<AuthenticationValue>({
+      password: SIGN.empty,
+    });
+
+  const currentIndex = useCurrentIndex(
+    cardNumberIsComplete,
+    !!selected,
+    expirationPeriodIsComplete,
+    ownerIsComplete,
+    infoIsComplete,
+    authenticationComplete
+  );
 
   const formatExpirationPeriod = () =>
     expirationPeriod.year.length
       ? expirationPeriod.month + SIGN.slash + expirationPeriod.year
       : expirationPeriod.month;
+
+  const creditCardForms: CreditCardForms[] = [
+    {
+      title: CARD_FORM_MESSAGE.inputCardNumber,
+      description: CARD_FORM_MESSAGE.cardNumberDescription,
+      inputError: cardNumberError,
+      childComponent: (
+        <InputCreditCardNumber
+          inputValue={cardNumber}
+          handleChange={setCardNumber}
+          inputError={cardNumberError}
+        />
+      ),
+    },
+    {
+      title: CARD_FORM_MESSAGE.inputCardType,
+      description: CARD_FORM_MESSAGE.domasticCardOnly,
+      inputError: false,
+      childComponent: (
+        <SelectBox
+          isDropdown={isDropdown}
+          handleDropdown={handleDropdown}
+          selected={selected}
+          handleSelected={handleSelected}
+          optionsContents={CARDTYPE}
+          placeholder={CARD_FORM_MESSAGE.inputCardTypePlaceholder}
+        />
+      ),
+    },
+    {
+      title: CARD_FORM_MESSAGE.inputCardExpirationDate,
+      description: CARD_FORM_MESSAGE.cardExpirationDateDescription,
+      inputError: expirationPeriodError,
+      childComponent: (
+        <InputExpirationPeriod
+          inputValue={expirationPeriod}
+          handleChange={setExpirationPeriod}
+          inputError={expirationPeriodError}
+        />
+      ),
+    },
+    {
+      title: CARD_FORM_MESSAGE.inputCardOwner,
+      description: "",
+      inputError: ownerError,
+      childComponent: (
+        <InputOwnerName inputValue={owner.name} handleChange={setOwner} inputError={ownerError} />
+      ),
+    },
+    {
+      title: CARD_FORM_MESSAGE.inputCvc,
+      description: "",
+      inputError: infoError,
+      childComponent: (
+        <InputCVC inputValue={info.cvc} handleChange={setInfo} inputError={infoError} />
+      ),
+    },
+    {
+      title: CARD_FORM_MESSAGE.inputCardPassword,
+      description: CARD_FORM_MESSAGE.cardPasswordDescription,
+      inputError: authenticationError,
+      childComponent: (
+        <InputPassword
+          inputValue={authentication.password}
+          handleChange={setAuthentication}
+          inputError={authenticationError}
+        />
+      ),
+    },
+  ];
 
   return (
     <PaymentsContainer>
@@ -46,34 +148,21 @@ const Payments = () => {
         expirationPeriod={formatExpirationPeriod()}
         ownerName={owner.name}
       />
+
       <InputFormContainer>
-        <CreditCardForm
-          title={CARD_FORM_MESSAGE.inputCardNumber}
-          description={CARD_FORM_MESSAGE.cardNumberDescription}
-          inputError={cardNumberError}
-        >
-          <InputCreditCardNumber
-            inputValue={cardNumber}
-            handleChange={setCardNumber}
-            inputError={cardNumberError}
-          />
-        </CreditCardForm>
+        {creditCardForms.map((formData, idx) => {
+          if (idx > currentIndex) return;
 
-        <CreditCardForm
-          title={CARD_FORM_MESSAGE.inputCardExpirationDate}
-          description={CARD_FORM_MESSAGE.cardExpirationDateDescription}
-          inputError={expirationPeriodError}
-        >
-          <InputExpirationPeriod
-            inputValue={expirationPeriod}
-            handleChange={setExpirationPeriod}
-            inputError={expirationPeriodError}
-          />
-        </CreditCardForm>
-
-        <CreditCardForm title={CARD_FORM_MESSAGE.inputCardOwner} inputError={ownerError}>
-          <InputOwnerName inputValue={owner.name} handleChange={setOwner} inputError={ownerError} />
-        </CreditCardForm>
+          return (
+            <CreditCardForm
+              title={formData.title}
+              description={formData.description}
+              inputError={formData.inputError}
+            >
+              {formData.childComponent}
+            </CreditCardForm>
+          );
+        })}
       </InputFormContainer>
     </PaymentsContainer>
   );
@@ -90,8 +179,9 @@ const PaymentsContainer = styled.div`
 `;
 
 const InputFormContainer = styled.div`
+  display: flex;
+  flex-direction: column-reverse;
   margin-top: 45px;
   display: flex;
-  flex-direction: column;
   gap: 16px;
 `;
