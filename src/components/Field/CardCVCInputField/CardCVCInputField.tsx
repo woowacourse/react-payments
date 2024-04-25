b36d2bf162cfc10ce8d69b4a2afed418a8975077
greetings1012 <ahnqwerty@gmail.com>
2024-04-25 20:04:46 +0900

    feat: CardCVCInputField 구현

---
+++ src/components/Field/CardCVCInputField/CardCVCInputField.tsx
@@ -0,0 +1,68 @@
+import {
+  ChangeEvent,
+  Dispatch,
+  SetStateAction,
+  useState,
+  FocusEvent,
+  useEffect,
+} from "react";
+import Input from "../../common/Input/Input";
+import styles from "../../../pages/CardInputPage/CardInputPage.module.css";
+
+const CVC_PLACEHOLDER = "123";
+export default function CardCVCInputField({
+  cardCVC,
+  setCardCVC,
+  isCompletedSections,
+  setIsCompletedSections,
+}: {
+  cardCVC: string;
+  setCardCVC: Dispatch<SetStateAction<string>>;
+  isCompletedSections: boolean[];
+  setIsCompletedSections: Dispatch<SetStateAction<boolean[]>>;
+}) {
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (isNaN(Number(e.target.value))) {
+      setErrorMessage("숫자만 입력해주세요.");
+      return;
+    }
+    setErrorMessage("");
+    setCardCVC(e.target.value);
+  };
+
+  const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
+    if (e.target.value.length < 3) {
+      setErrorMessage("CVC를 정확히 입력해주세요.");
+      return;
+    }
+
+    setErrorMessage("");
+  };
+
+  useEffect(() => {
+    const updatedIsCompletedSections = [...isCompletedSections];
+    updatedIsCompletedSections[4] = cardCVC.length == 3;
+    setIsCompletedSections(updatedIsCompletedSections);
+  }, [cardCVC, setIsCompletedSections]);
+
+  return (
+    <>
+      <div className={styles.label}>CVC</div>
+      <div className={styles.horizon__gap__container}>
+        <Input
+          onChange={handleChange}
+          isError={errorMessage.length !== 0}
+          placeholder={CVC_PLACEHOLDER}
+          maxLength={3}
+          value={cardCVC}
+          onBlur={handleBlur}
+        />
+      </div>
+      <div className={styles.error_message}>
+        {errorMessage !== "" && errorMessage}
+      </div>
+    </>
+  );
+}
