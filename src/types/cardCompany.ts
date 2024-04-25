888cca78214681625e046b692362329e00fa981e
pakxe <pigkill40@naver.com>
2024-04-25 23:59:05 +0900

    feat: 카드사를 위한 타입 정의

---
+++ src/types/cardCompany.ts
@@ -0,0 +1,12 @@
+export type CardCompany = 'BC' | 'SHINHAN' | 'KAKAO' | 'HYUNDAI' | 'WOORI' | 'LOTTE' | 'HANA' | 'KOOKMIN';
+
+export const COMPANY_LIST = {
+  BC: 'BC카드',
+  SHINHAN: '신한카드',
+  KAKAO: '카카오뱅크',
+  HYUNDAI: '현대카드',
+  WOORI: '우리카드',
+  LOTTE: '롯데카드',
+  HANA: '하나카드',
+  KOOKMIN: '국민카드',
+};
