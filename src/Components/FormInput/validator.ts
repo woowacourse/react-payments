751ef1453fd2e9c05c4dbb0470e7fd956002c314
lurgi <wahoo9040@naver.com>
2024-04-25 10:49:18 +0900

    refactor: 상수 컨벤션 변경

--- src/Components/FormInput/validator.ts
+++ src/Components/FormInput/validator.ts
@@ -7,40 +7,40 @@ export interface validatorReturn<T> {
 export type ValidatorType<T> = (input: string, name?: "month" | "year") => validatorReturn<T>;
 
 const ERROR_MESSAGES = {
-  onlyNumber: "숫자만 입력해주세요.",
-  notValidMonth: "유효한 월이 아닙니다",
-  under15Length: "최대 15자로 입력해주세요.",
-  onlyEnglish: "영어만 입력해주세요.",
+  ONLY_NUMBER: "숫자만 입력해주세요.",
+  NOT_VALID_MONTH: "유효한 월이 아닙니다",
+  ONLY_UNDER_15_LENGTH: "최대 15자로 입력해주세요.",
+  ONLY_ENGLISH: "영어만 입력해주세요.",
 };
 
 export function cardNumbersValidator(input: string): validatorReturn<string> {
   const number = Number(input);
 
   if (Number.isNaN(number)) {
-    return { isValid: false, message: ERROR_MESSAGES.onlyNumber };
+    return { isValid: false, message: ERROR_MESSAGES.ONLY_NUMBER };
   }
   return { isValid: true, value: input };
 }
 
 export function cardPeriodValidator(input: string, category?: "month" | "year"): validatorReturn<string> {
   const number = Number(input);
-  const monthValid = { min: 0, max: 12 };
+  const VALID_MONTH = { MIN: 0, MAX: 12 };
 
   if (Number.isNaN(number)) {
-    return { isValid: false, message: ERROR_MESSAGES.onlyNumber };
+    return { isValid: false, message: ERROR_MESSAGES.ONLY_NUMBER };
   }
-  if (category === "month" && (number < monthValid.min || number > monthValid.max)) {
-    return { isValid: false, message: ERROR_MESSAGES.notValidMonth };
+  if (category === "month" && (number < VALID_MONTH.MIN || number > VALID_MONTH.MAX)) {
+    return { isValid: false, message: ERROR_MESSAGES.NOT_VALID_MONTH };
   }
   return { isValid: true, value: input };
 }
 
 export function cardOwnerValidator(input: string): validatorReturn<string> {
   if (input.length > 15) {
-    return { isValid: false, message: ERROR_MESSAGES.under15Length };
+    return { isValid: false, message: ERROR_MESSAGES.ONLY_UNDER_15_LENGTH };
   }
   if (!/[a-zA-Z ]/.test(input) && input) {
-    return { isValid: false, message: ERROR_MESSAGES.onlyEnglish };
+    return { isValid: false, message: ERROR_MESSAGES.ONLY_ENGLISH };
   }
   return { isValid: true, value: input };
 }
