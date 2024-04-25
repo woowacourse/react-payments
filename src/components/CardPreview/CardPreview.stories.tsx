37994ae8b6d15261ce5520b48fa1ae5396e88570
pakxe <pigkill40@naver.com>
2024-04-25 23:56:01 +0900

    rename: Card -> CardPreview로 이름 수정

--- src/components/CardPreview/CardPreview.stories.tsx
+++ src/components/CardPreview/CardPreview.stories.tsx
@@ -1,40 +1,40 @@
 import { Meta, StoryObj } from '@storybook/react';
-import Card from './Card';
+import CardPreview from './CardPreview';
 import { Date } from '../../types/date';
 
 const meta: Meta = {
-  title: 'Card',
-  component: Card,
+  title: 'CardPreview',
+  component: CardPreview,
 };
 
 export default meta;
 
 type CardProps = {
   cardNumbers: string[];
-  date: Date;
+  expirationDate: Date;
   ownerName: string;
 };
 
 export const DefaultCard: StoryObj<CardProps> = (args: any) => (
-  <Card {...args} cardNumbers={['1111', '1111', '1111', '1111']} />
+  <CardPreview {...args} cardNumbers={['1111', '1111', '1111', '1111']} />
 );
 DefaultCard.args = {
   ownerName: 'PARAN',
-  date: { month: '02', year: '40' },
+  expirationDate: { month: '02', year: '40' },
 };
 
 export const VisaCard: StoryObj<CardProps> = (args: any) => (
-  <Card {...args} cardNumbers={['4111', '1111', '1111', '1111']} />
+  <CardPreview {...args} cardNumbers={['4111', '1111', '1111', '1111']} />
 );
 VisaCard.args = {
   ownerName: 'CHICO',
-  date: { month: '01', year: '25' },
+  expirationDate: { month: '01', year: '25' },
 };
 
 export const MasterCard: StoryObj<CardProps> = (args: any) => (
-  <Card {...args} cardNumbers={['5210', '1111', '1111', '1111']} />
+  <CardPreview {...args} cardNumbers={['5210', '1111', '1111', '1111']} />
 );
 MasterCard.args = {
   ownerName: 'PAKXE',
-  date: { month: '06', year: '24' },
+  expirationDate: { month: '06', year: '24' },
 };
