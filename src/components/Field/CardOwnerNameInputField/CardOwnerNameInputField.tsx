c80e85257b8dbd3e6cc7f1eabc2777ae692dc6ff
pakxe <pigkill40@naver.com>
2024-04-26 00:05:12 +0900

    feat: 이름에 대한 유효성 검사 로직 분리

--- src/components/Field/CardOwnerNameInputField/CardOwnerNameInputField.tsx
+++ src/components/Field/CardOwnerNameInputField/CardOwnerNameInputField.tsx
@@ -1,64 +1,27 @@
-import { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
+import { ChangeEvent } from 'react';
 import Input from '../../common/Input/Input';
 import styles from '../../../App.module.css';
-import normalizeSpaces from '../../../utils/normalizeSpaces';
-import filterEnglish from '../../../utils/filterEnglish';
-import { ERROR_MESSAGES } from '../../../constants/errorMessages';
 import { MAX_NAME_LENGTH, OWNER_NAME_PLACEHOLDER } from '../../../constants/input';
 
 type CardOwnerNameInputField = {
   ownerName: string;
-  setOwnerName: Dispatch<SetStateAction<string>>;
+  handleOwnerName: (e: ChangeEvent<HTMLInputElement>) => void;
+  errorMessage: string;
 };
 
-export default function CardOwnerNameInputField({ ownerName, setOwnerName }: CardOwnerNameInputField) {
-  const [errorMessage, setErrorMessage] = useState<string>('');
-
-  const getErrorMessage = (name: string, engName: string, normalizedOwnerName: string) => {
-    const isExcessiveWhiteSpace = engName.length > normalizedOwnerName.length;
-
-    if (isExcessiveWhiteSpace && normalizedOwnerName.length !== 0) {
-      return ERROR_MESSAGES.EXCESSIVE_WHITE_SPACE;
-    }
-
-    if (engName.length < name.length) {
-      return ERROR_MESSAGES.NOT_ENG;
-    }
-
-    return '';
-  };
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const upperCaseName = e.target.value.toUpperCase();
-    const engName = filterEnglish(upperCaseName);
-    const normalizedOwnerName = normalizeSpaces(engName);
-
-    const errorMessage = getErrorMessage(upperCaseName, engName, normalizedOwnerName);
-
-    setErrorMessage(errorMessage);
-    setOwnerName(normalizedOwnerName);
-  };
-
-  const handleMissingName = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value.length === 0) {
-      setErrorMessage(ERROR_MESSAGES.MISSING_NAME);
-      return;
-    }
-
-    setErrorMessage('');
-  };
-
+export default function CardOwnerNameInputField({ ownerName, handleOwnerName, errorMessage }: CardOwnerNameInputField) {
   return (
     <>
       <div className={styles.label}>소유자 이름</div>
       <div className={styles.horizon__gap__container}>
         <Input
-          onChange={handleChange}
+          autoFocus
+          onChange={handleOwnerName}
           isError={errorMessage.length !== 0}
           placeholder={OWNER_NAME_PLACEHOLDER}
-          maxLength={MAX_NAME_LENGTH} // 비자 21자, 마스터카드 26자!
+          maxLength={MAX_NAME_LENGTH}
           value={ownerName}
-          onBlur={handleMissingName}
+          onBlur={handleOwnerName}
         />
       </div>
       {errorMessage !== '' && <div className={styles.error_message}>{errorMessage}</div>}
