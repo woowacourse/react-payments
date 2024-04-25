f4a20ec4f1d92c9f9274bf87e4b0a6a36c4d7f5e
greetings1012 <ahnqwerty@gmail.com>
2024-04-25 21:30:59 +0900

    style: 카카오뱅크는 완료 화면에서 '카드' 문자가 추가되어 등장하도록 수정

--- src/pages/CardAddedPage/CardAddedPage.tsx
+++ src/pages/CardAddedPage/CardAddedPage.tsx
@@ -6,13 +6,17 @@ import CheckImage from "../../assets/image/CheckImage.svg";
 export default function CardAddedPage() {
   const location = useLocation();
   const navigate = useNavigate();
+  const COMPANY_NAME = location.state.cardCompanyName;
+  const CARD_NAME =
+    COMPANY_NAME === "카카오뱅크" ? `${COMPANY_NAME} 카드` : COMPANY_NAME;
+
   return (
     <div className={styles.wrapper}>
       <img src={CheckImage}></img>
       <div className={styles.description}>
         {location.state.firstFourCardNumber}로 시작하는
         <br />
-        {location.state.cardCompanyName}가 등록되었어요.
+        {CARD_NAME}가 등록되었어요.
       </div>
       <GoMainButton onClick={() => navigate("/")}></GoMainButton>
     </div>
