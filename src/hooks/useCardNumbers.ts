4ea716e262ced1b29db85e9acb11e4ca0fee17c2
pakxe <pigkill40@naver.com>
2024-04-26 00:02:16 +0900

    feat: 카드 번호를 입력받는 훅 구현

---
+++ src/hooks/useCardNumbers.ts
@@ -0,0 +1,73 @@
+import { SyntheticEvent, useEffect, useState } from 'react';
+import { CARD_NUMBERS_UNIT_LENGTH, CARD_NUMBER_UNIT_LENGTH } from '../constants/input';
+import useErrorMessages from './useErrorMessages';
+import { ERROR_MESSAGES } from '../constants/errorMessages';
+
+const useCardNumbers = () => {
+  const [cardNumbers, setCardNumbers] = useState<string[]>(new Array(CARD_NUMBERS_UNIT_LENGTH).fill(''));
+  const { errorMessages, setErrorMessages } = useErrorMessages<string>(cardNumbers.length, '');
+  const [isValid, setIsValid] = useState(false);
+
+  // 값과, 에러가 났는지, 업데이트, (유효성 검사)
+
+  const updateCardNumbers = (index: number, cardNumber: string) => {
+    const updatedCardNumbers = [...cardNumbers];
+    updatedCardNumbers[index] = cardNumber;
+    setCardNumbers(updatedCardNumbers);
+  };
+
+  const isNotNumberInput = (numberUnit: string, eventType: string) => {
+    return eventType === 'change' && isNaN(Number(numberUnit)) && numberUnit.length !== 0;
+  };
+
+  const isFilledUnit = (numberUnit: string, eventType: string) => {
+    return eventType === 'blur' && numberUnit.length < CARD_NUMBER_UNIT_LENGTH;
+  };
+
+  const isFilledAllUnit = (cardNumbers: string[]) => {
+    return cardNumbers.every((cardNumberUnit) => cardNumberUnit.length === CARD_NUMBER_UNIT_LENGTH);
+  };
+
+  const getErrorMessage = (numberUnit: string, eventType: string) => {
+    if (isNotNumberInput(numberUnit, eventType)) {
+      return ERROR_MESSAGES.NOT_NUMBER;
+    }
+    if (isFilledUnit(numberUnit, eventType)) {
+      return ERROR_MESSAGES.CARD_NUMBER_UNIT_LENGTH;
+    }
+
+    return '';
+  };
+
+  const handleCardNumbersChange = (index: number) => (e: SyntheticEvent<HTMLInputElement>) => {
+    const { value } = e.target as HTMLInputElement;
+
+    const errorMessage = getErrorMessage(value, e.type);
+    setErrorMessages({ errorMessage: errorMessage, index });
+
+    if (errorMessage !== '') return;
+
+    updateCardNumbers(index, value);
+  };
+
+  useEffect(() => {
+    if (errorMessages.some((msg) => msg !== '')) return;
+
+    setIsValid(isFilledAllUnit(cardNumbers));
+  }, [cardNumbers, errorMessages]);
+
+  return {
+    value: cardNumbers,
+    handleCardNumbersChange,
+    errorMessages,
+    isValid,
+  };
+};
+
+export default useCardNumbers;
+
+/**
+ * isValid: errorMessage만으로 처리하기엔 입력 안된 값들과 혼동될 여지 존재
+ * value
+ * errorMessage
+ */
