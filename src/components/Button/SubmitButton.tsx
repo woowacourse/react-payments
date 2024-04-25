2de15cd2522bf4445c3d3166671c4d469c7f07fb
greetings1012 <ahnqwerty@gmail.com>
2024-04-25 20:05:59 +0900

    fix: 폴더 이름에 잘못된 공백이 들어있던 오류 수정

--- src/components/Button/SubmitButton.tsx
+++ src/components/Button/SubmitButton.tsx
@@ -8,7 +8,7 @@ export default function SubmitButton({
   isCompletedSections: boolean[];
   onClick: () => void;
 }) {
-  const allSectionsCompleted = isCompletedSections[0];
+  const allSectionsCompleted = isCompletedSections.every((form) => form);
   return allSectionsCompleted ? (
     <Button onClick={onClick} classNames={[styles.primary, styles.submit]}>
       확인
