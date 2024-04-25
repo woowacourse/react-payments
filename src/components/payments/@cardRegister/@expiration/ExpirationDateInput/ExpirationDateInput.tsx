a7dce2ca94f71fb9e91ab9c9c0fa0124de6b54db
손진영 <sm099@naver.com>
2024-04-25 17:35:34 +0900

    refactor(ExpirationDateInput): 불필요한 prop rest parameter로 간소화

--- src/components/payments/@cardRegister/@expiration/ExpirationDateInput/ExpirationDateInput.tsx
+++ src/components/payments/@cardRegister/@expiration/ExpirationDateInput/ExpirationDateInput.tsx
@@ -3,29 +3,11 @@ import Input, { InputProps } from '@components/common/Input/Input';
 type ExpirationDateInputProps = Omit<InputProps, 'type' | 'maxLength' | 'onChange'> & {
   onAddExpirationDate: React.ChangeEventHandler<HTMLInputElement>;
   placeholder: 'MM' | 'YY';
-  refCallback ?: (element : HTMLInputElement) => void
+  refCallback?: (element: HTMLInputElement) => void;
 };
 
-const ExpirationDateInput: React.FC<ExpirationDateInputProps> = ({
-  isError = false,
-  value,
-  placeholder,
-  onAddExpirationDate,
-  id,
-  refCallback
-}) => {
-  return (
-    <Input
-    ref={refCallback}
-      id={id}
-      maxLength={2}
-      type="text"
-      placeholder={placeholder}
-      value={value}
-      isError={isError}
-      onChange={onAddExpirationDate}
-    />
-  );
+const ExpirationDateInput: React.FC<ExpirationDateInputProps> = ({ onAddExpirationDate, refCallback, ...rest }) => {
+  return <Input maxLength={2} type="text" ref={refCallback} onChange={onAddExpirationDate} {...rest} />;
 };
 
 export default ExpirationDateInput;
