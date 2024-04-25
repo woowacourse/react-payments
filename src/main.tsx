ad3ba538941c4cba57b15d49532f5ee21a7a70da
rbgksqkr <rbgks1937@naver.com>
2024-04-26 01:06:55 +0900

    refactor: CardRegisterCompletedPage 스타일 코드 분리

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles.tsx';
 import { GlobalLayout } from './styles/common.ts';
-import CardRegisterCompletedPage from './pages/CardRegisterCompletedPage.tsx';
-import CardRegisterPage from './pages/CardRegisterPage.tsx';
+import CardRegisterCompletedPage from './pages/cardRegisterCompletedPage/CardRegisterCompletedPage.tsx';
+import CardRegisterPage from './pages/cardRegisterPage/CardRegisterPage.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
