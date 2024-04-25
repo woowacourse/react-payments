e103233adff66cf9e9c07dc401e6b035e28d7054
rbgksqkr <rbgks1937@naver.com>
2024-04-25 15:58:06 +0900

    fix(CardPreview): 이미지 경로 수정

--- src/components/cardPreview/CardPreview.tsx
+++ src/components/cardPreview/CardPreview.tsx
@@ -1,5 +1,5 @@
-import MasterCard from '../../src/assets/images/mastercard.png';
-import VisaCard from '../../src/assets/images/visa.png';
+import MasterCard from '../../../src/assets/images/mastercard.png';
+import VisaCard from '../../../src/assets/images/visa.png';
 import CONDITION from '../../constants/condition';
 import { CARD_TYPE, CardNumberKey, ExpiryDate } from '../../types/card';
 import * as S from './CardPreview.styled';
