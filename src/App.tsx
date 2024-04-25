50b334bfa1e8afd5718a8344017267a99c536215
simhorim <hori5020@gmail.com>
2024-04-25 12:23:52 +0900

    refactor(App): styleComponent prefix 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
 import CardInformationForm from './components/CardInformationForm/CardInformationForm';
 import CardInformationPreview from './components/CardInformationPreview/CardInformationPreview';
+import useCVCState from './hooks/useCVC';
 import useCardNumber from './hooks/useCardNumber';
 import useExpirationDate from './hooks/useExpirationDate';
 import useSelectedCardState from './hooks/useSelectedCardState';
 import useUserName from './hooks/useUserName';
-import * as StyledApp from './styles/App.style';
+import { StyledContainer } from './styles/App.style';
 
 function App() {
   const { cardNumberState } = useCardNumber([]);
   const { expirationDateState } = useExpirationDate([]);
   const { userNameState } = useUserName('');
   const { selectedCardState } = useSelectedCardState('');
+  const { cvcState } = useCVCState(undefined);
 
   const cardInformationProps = {
     cardNumberState,
     expirationDateState,
     userNameState,
     selectedCardState,
+    cvcState,
   };
 
   return (
-    <StyledApp.Container>
+    <StyledContainer>
       <CardInformationPreview {...cardInformationProps} />
       <CardInformationForm {...cardInformationProps} />
-    </StyledApp.Container>
+    </StyledContainer>
   );
 }
 
