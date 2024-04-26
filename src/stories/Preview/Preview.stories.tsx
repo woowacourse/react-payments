2294d5d82681fec938c1f920bbf6e154b4a5927d
simhorim <hori5020@gmail.com>
2024-04-26 22:02:25 +0900

    test(preview): 스토리북 구현을 위한 리팩토링

--- src/stories/Preview/Preview.stories.tsx
+++ src/stories/Preview/Preview.stories.tsx
@@ -12,72 +12,476 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    cardNumberState: {},
-    expirationDateState: {},
-    userNameState: '',
-    showImageCondition: {
-      isVisa: false,
-      isMasterCard: false,
+    cardNumberState: {
+      firstState: { value: undefined, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: undefined, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: false,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: '', setValue: (event) => {}, error: false },
+    selectedCardState: { value: '', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: false,
+      setIsFocus: () => {},
     },
   },
 };
 
-export const Fulfilled: Story = {
+export const Valid: Story = {
   args: {
-    cardNumberState: { first: 5123, second: 1234, third: 1234, fourth: 1234 },
-    expirationDateState: { month: 12, year: 24 },
-    userNameState: 'SIMO COOKIE',
-    showImageCondition: {
-      isVisa: false,
-      isMasterCard: true,
+    cardNumberState: {
+      firstState: { value: 5123, setValue: (event) => {}, error: false },
+      secondState: { value: 1234, setValue: (event) => {}, error: false },
+      thirdState: { value: 1234, setValue: (event) => {}, error: false },
+      fourthState: { value: 1234, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: true,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: 12, setValue: (event) => {}, error: false },
+      yearState: { value: 24, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: 'SIMO COOKIE', setValue: (event) => {}, error: false },
+    selectedCardState: { value: '', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: false,
+      setIsFocus: () => {},
     },
   },
 };
 
 export const Visa: Story = {
   args: {
-    cardNumberState: { first: 4123 },
-    expirationDateState: {},
-    userNameState: '',
-    showImageCondition: {
-      isVisa: true,
-      isMasterCard: false,
+    cardNumberState: {
+      firstState: { value: 4123, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: undefined, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: true,
+        isMasterCard: false,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: undefined, setValue: (event) => {}, error: false },
+    selectedCardState: { value: '', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: false,
+      setIsFocus: () => {},
     },
   },
 };
 
 export const MasterCard: Story = {
   args: {
-    cardNumberState: { first: 5123 },
-    expirationDateState: {},
-    userNameState: '',
-    showImageCondition: {
-      isVisa: false,
-      isMasterCard: true,
+    cardNumberState: {
+      firstState: { value: 5123, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: undefined, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: true,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: undefined, setValue: (event) => {}, error: false },
+    selectedCardState: { value: '', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: false,
+      setIsFocus: () => {},
     },
   },
 };
 
 export const NormalCard: Story = {
   args: {
-    cardNumberState: { first: 1111 },
-    expirationDateState: {},
-    userNameState: '',
-    showImageCondition: {
-      isVisa: false,
-      isMasterCard: false,
+    cardNumberState: {
+      firstState: { value: 1111, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: undefined, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: false,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: undefined, setValue: (event) => {}, error: false },
+    selectedCardState: { value: '', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: false,
+      setIsFocus: () => {},
     },
   },
 };
 
 export const HiddenCardNumber: Story = {
   args: {
-    cardNumberState: { third: 5123 },
-    expirationDateState: {},
-    userNameState: '',
-    showImageCondition: {
-      isVisa: false,
-      isMasterCard: false,
+    cardNumberState: {
+      firstState: { value: undefined, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: 5123, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: false,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: undefined, setValue: (event) => {}, error: false },
+    selectedCardState: { value: '', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: false,
+      setIsFocus: () => {},
+    },
+  },
+};
+
+export const SelectedCardDefault: Story = {
+  args: {
+    cardNumberState: {
+      firstState: { value: undefined, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: undefined, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: false,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: undefined, setValue: (event) => {}, error: false },
+    selectedCardState: { value: '', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: false,
+      setIsFocus: () => {},
+    },
+  },
+};
+
+export const SelectedBC: Story = {
+  args: {
+    cardNumberState: {
+      firstState: { value: undefined, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: undefined, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: false,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: undefined, setValue: (event) => {}, error: false },
+    selectedCardState: { value: 'BC카드', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: false,
+      setIsFocus: () => {},
+    },
+  },
+};
+
+export const SelectedSinhan: Story = {
+  args: {
+    cardNumberState: {
+      firstState: { value: undefined, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: undefined, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: false,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: undefined, setValue: (event) => {}, error: false },
+    selectedCardState: { value: '신한카드', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: false,
+      setIsFocus: () => {},
+    },
+  },
+};
+
+export const SelectedKakao: Story = {
+  args: {
+    cardNumberState: {
+      firstState: { value: undefined, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: undefined, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: false,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: undefined, setValue: (event) => {}, error: false },
+    selectedCardState: { value: '카카오뱅크', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: false,
+      setIsFocus: () => {},
+    },
+  },
+};
+
+export const SelectedHyndai: Story = {
+  args: {
+    cardNumberState: {
+      firstState: { value: undefined, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: undefined, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: false,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: undefined, setValue: (event) => {}, error: false },
+    selectedCardState: { value: '현대카드', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: false,
+      setIsFocus: () => {},
+    },
+  },
+};
+
+export const SelectedWoori: Story = {
+  args: {
+    cardNumberState: {
+      firstState: { value: undefined, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: undefined, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: false,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: undefined, setValue: (event) => {}, error: false },
+    selectedCardState: { value: '우리카드', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: false,
+      setIsFocus: () => {},
+    },
+  },
+};
+
+export const SelectedLotte: Story = {
+  args: {
+    cardNumberState: {
+      firstState: { value: undefined, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: undefined, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: false,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: undefined, setValue: (event) => {}, error: false },
+    selectedCardState: { value: '롯데카드', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: false,
+      setIsFocus: () => {},
+    },
+  },
+};
+
+export const SelectedHana: Story = {
+  args: {
+    cardNumberState: {
+      firstState: { value: undefined, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: undefined, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: false,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: undefined, setValue: (event) => {}, error: false },
+    selectedCardState: { value: '하나카드', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: false,
+      setIsFocus: () => {},
+    },
+  },
+};
+
+export const SelectedKB: Story = {
+  args: {
+    cardNumberState: {
+      firstState: { value: undefined, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: undefined, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: false,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: undefined, setValue: (event) => {}, error: false },
+    selectedCardState: { value: '국민카드', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: false,
+      setIsFocus: () => {},
+    },
+  },
+};
+
+export const CVCDefault: Story = {
+  args: {
+    cardNumberState: {
+      firstState: { value: undefined, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: 5123, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: false,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: undefined, setValue: (event) => {}, error: false },
+    selectedCardState: { value: '', setValue: (event) => {} },
+    cvcState: {
+      value: undefined,
+      setValue: (event) => {},
+      error: false,
+      isFocus: true,
+      setIsFocus: () => {},
+    },
+  },
+};
+
+export const CVCFulfilled: Story = {
+  args: {
+    cardNumberState: {
+      firstState: { value: undefined, setValue: (event) => {}, error: false },
+      secondState: { value: undefined, setValue: (event) => {}, error: false },
+      thirdState: { value: 5123, setValue: (event) => {}, error: false },
+      fourthState: { value: undefined, setValue: (event) => {}, error: false },
+      showImageCondition: {
+        isVisa: false,
+        isMasterCard: false,
+      },
+    },
+    expirationDateState: {
+      monthState: { value: undefined, setValue: (event) => {}, error: false },
+      yearState: { value: undefined, setValue: (event) => {}, error: false },
+    },
+    userNameState: { value: undefined, setValue: (event) => {}, error: false },
+    selectedCardState: { value: '', setValue: (event) => {} },
+    cvcState: {
+      value: 123,
+      setValue: (event) => {},
+      error: false,
+      isFocus: true,
+      setIsFocus: () => {},
     },
   },
 };
