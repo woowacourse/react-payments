3883217b9fc9b40e1a0fe97a252e18047241ec6c
simhorim <hori5020@gmail.com>
2024-04-26 19:15:35 +0900

    feat(main): BrowserRouter 로 구조 수정

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,19 @@ import App from './App.tsx';
 import theme from './styles/theme.ts';
 import GlobalStyle from './styles/GlobalStyle.ts';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Registration from './components/Registration/Registration.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/react-payments" element={<App />} />
+          <Route path="/registration" element={<Registration />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
 );
