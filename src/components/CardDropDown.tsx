adf40a72a150cd1dcfde9934fea8d0e30c6932c2
jaeml06 <jaeml0630@gmail.com>
2024-04-26 21:38:54 +0900

    style: DropdownHeader '⌃' : '⌄' 이미지로 교체

--- src/components/CardDropDown.tsx
+++ src/components/CardDropDown.tsx
@@ -1,10 +1,12 @@
 import { Dispatch, SetStateAction, useState } from 'react';
 import { CARD_BRAND } from '../constants/cardBrand';
 import { CardBrand } from '../types/card';
-import { ShowComponents } from '../types/showCompents';
+import { ShowComponents } from '../types/showComponents';
 import FieldTitle from './FieldTitle';
 import InputField from './InputField';
 import styled from 'styled-components';
+import arrow_bottom from '../assets/image/arrow-bottom.svg';
+import arrow_top from '../assets/image/arrow-top.svg';
 
 const DropdownContainer = styled.div`
   display: flex;
@@ -12,9 +14,9 @@ const DropdownContainer = styled.div`
   width: 100%;
 `;
 
-const DropdownHeader = styled.div`
+const DropdownHeader = styled.div<{ $clicked: boolean }>`
   padding: 10px;
-  border: 1px solid #ddd;
+  border: 1px solid ${(props) => (props.$clicked ? '#000' : '#ddd')};
   border-radius: 5px;
   width: 100%;
   display: flex;
@@ -38,7 +40,6 @@ const DropdownList = styled.ul`
   padding: 0;
   margin: 10px 0;
   list-style-type: none;
-  
 `;
 
 const ListItem = styled.li`
@@ -49,16 +50,15 @@ const ListItem = styled.li`
   }
 `;
 interface Props {
-  cardBrand: CardBrand;
+  cardBrand?: CardBrand;
   handleInput: Dispatch<SetStateAction<CardBrand>>;
   handleShowComponent: Dispatch<SetStateAction<ShowComponents>>;
 }
 export default function CardDropDown({
-  cardBrand,
   handleInput,
   handleShowComponent,
 }: Props) {
-  const [view, setView] = useState(false);
+  const [view, setView] = useState<boolean>(false);
   const [selected, setSelected] = useState('카드사를 선택해주세요');
 
   const handleSelectBrand = (brandName: string) => {
@@ -81,32 +81,39 @@ export default function CardDropDown({
 
   return (
     <>
-    <FieldTitle
-      title='카드사를 선택해 주세요'
-      subtitle='현재 국내 카드사만 가능합니다.'
-    />
-    <InputField label='카드사' count={4} errorMessages={''}>
-    <DropdownContainer>
-      <DropdownHeader onClick={() => setView(!view)}>
-        <DropdownHeaderTitle>{selected}</DropdownHeaderTitle>
-        <span>{view ? '⌃' : '⌄'}</span>
-      </DropdownHeader>
-      {view && (
-        <DropdownList>
-          {Object.entries(CARD_BRAND).map(([brandName]) => {
-            return (
-              <ListItem
-                key={brandName}
-                onClick={() => handleSelectBrand(brandName)}
-              >
-                {brandName}
-              </ListItem>
-            );
-          })}
-        </DropdownList>
-      )}
-    </DropdownContainer>
-    </InputField>
-  </>
+      <FieldTitle
+        title='카드사를 선택해 주세요'
+        subtitle='현재 국내 카드사만 가능합니다.'
+      />
+      <InputField
+        label='카드사'
+        count={4}
+        errorMessages={''}
+      >
+        <DropdownContainer>
+          <DropdownHeader
+            $clicked={view}
+            onClick={() => setView(!view)}
+          >
+            <DropdownHeaderTitle>{selected}</DropdownHeaderTitle>
+            <img src={view ? arrow_bottom : arrow_top} />
+          </DropdownHeader>
+          {view && (
+            <DropdownList>
+              {Object.entries(CARD_BRAND).map(([brandName]) => {
+                return (
+                  <ListItem
+                    key={brandName}
+                    onClick={() => handleSelectBrand(brandName)}
+                  >
+                    {brandName}
+                  </ListItem>
+                );
+              })}
+            </DropdownList>
+          )}
+        </DropdownContainer>
+      </InputField>
+    </>
   );
 }
