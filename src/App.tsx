93fa45197de1460bdd01573b700d1d3deaeca50a
chysis <chysiss@naver.com>
2024-04-27 03:53:48 +0900

    chore: router path 설정

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const router = createBrowserRouter(
       element: <ErrorPage />,
     },
   ],
-  { basename: '/react-payments/dist' }
+  { basename: '/react-payments/dist/' }
 );
 
 function App() {
