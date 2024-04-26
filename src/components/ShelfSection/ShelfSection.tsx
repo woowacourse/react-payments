5b0eec089f54e1e4525e23a05f637846ab79ef34
pakxe <pigkill40@naver.com>
2024-04-26 15:36:36 +0900

    feat: 타입 작성

--- src/components/ShelfSection/ShelfSection.tsx
+++ src/components/ShelfSection/ShelfSection.tsx
@@ -1,6 +1,7 @@
+import { ReactNode } from 'react';
 import styles from './ShelfSection.module.css';
 
-const ShelfSection = ({ children }) => {
+const ShelfSection = ({ children }: { children: ReactNode }) => {
   return <div className={styles.shelf__section}>{children}</div>;
 };
 
