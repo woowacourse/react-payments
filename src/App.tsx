eefaa33b38466abe2e788825e27dcc3c10a9dd5b
Largopie <106071687+Largopie@users.noreply.github.com>
2024-04-27 01:09:20 +0900

    feat(GeneralLayout): 기본 스타일 컴포넌트 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import styled from 'styled-components';
 import { RouterProvider } from 'react-router-dom';
-import GlobalStyles from './GlobalStyles';
 import { router } from './router';
 
-export const Container = styled.div`
-  width: 382px;
-  height: 700px;
-  background-color: #fff;
-
-  border-radius: 8px;
-  border: 3px solid #000;
-`;
-
 function App() {
-  return (
-    <Container>
-      <GlobalStyles />
-      <RouterProvider router={router} />
-    </Container>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 import CardRegister from './pages/CardRegister/CardRegister';
 import RegisterConfirm from './pages/RegisterConfirm/RegisterConfirm';
+import GeneralLayout from './components/GeneralLayout';
 
 export const router = createBrowserRouter([
   {
     path: '/',
+    element: <GeneralLayout />,
     children: [
       {
         path: '',
