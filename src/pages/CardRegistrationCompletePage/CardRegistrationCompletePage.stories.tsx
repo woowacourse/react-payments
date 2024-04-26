ec2122e5d031e1abb7e3e5d600608888ad95f5b5
anttiey <hanuuny@gmail.com>
2024-04-26 14:54:33 +0900

    test: 페이지 컴포넌트 스토리북 추가

---
+++ src/pages/CardRegistrationCompletePage/CardRegistrationCompletePage.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { MemoryRouter } from 'react-router-dom';
+import CardRegistrationCompletePage from './CardRegistrationCompletePage';
+
+const meta = {
+  title: 'CardRegistrationCompletePage',
+  component: CardRegistrationCompletePage,
+  decorators: [
+    (Story) => (
+      <MemoryRouter initialEntries={['/complete?number=1234&company=BC카드']}>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+} satisfies Meta<typeof CardRegistrationCompletePage>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
