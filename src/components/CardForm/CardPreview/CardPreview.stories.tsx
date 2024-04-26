b8f9bbfe0e82951a58b9fe1c648c86afc91d7c8e
anttiey <hanuuny@gmail.com>
2024-04-26 15:02:59 +0900

    test: CardPreview 컴포넌트 스토리북 업데이트

--- src/components/CardForm/CardPreview/CardPreview.stories.tsx
+++ src/components/CardForm/CardPreview/CardPreview.stories.tsx
@@ -5,6 +5,9 @@ import CardPreview from './CardPreview';
 const meta = {
   title: 'CardPreview',
   component: CardPreview,
+  parameters: {
+    controls: { exclude: 'isCVCInput' },
+  },
   argTypes: {
     cardNumber: {
       options: {
@@ -39,6 +42,17 @@ const meta = {
       },
       control: { type: 'select' },
     },
+    cvc: {
+      options: {
+        Default: '123',
+        'No input': '',
+      },
+      control: { type: 'select' },
+    },
+    company: {
+      options: ['BC카드', '신한카드', '카카오뱅크', '현대카드', '우리카드', '롯데카드', '하나카드', '국민카드'],
+      control: { type: 'select' },
+    },
   },
 } satisfies Meta<typeof CardPreview>;
 
@@ -52,6 +66,7 @@ export const Default: Story = {
     month: '12',
     year: '24',
     owner: 'PARSELY KIM',
+    cvc: '123',
   },
 };
 
