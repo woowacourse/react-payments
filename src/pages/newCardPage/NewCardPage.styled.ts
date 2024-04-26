3e14adb3420ff1600339c7e2941a7e10d16370cd
useon <yoosk4986@naver.com>
2024-04-26 20:17:35 +0900

    design: 스타일 수정

--- src/pages/newCardPage/NewCardPage.styled.ts
+++ src/pages/newCardPage/NewCardPage.styled.ts
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
 export const NewCardContainer = styled.div`
+  width: 376px;
   display: flex;
   flex-direction: column;
+  gap: 45px;
+  margin: 77px auto 0 auto;
+`;
+
+export const NewCardInputSectionWrapper = styled.div`
+  display: flex;
+  flex-direction: column-reverse;
   gap: 16px;
-  width: 376px;
-  margin: auto;
 `;
--- src/pages/newCardPage/NewCardPage.tsx
+++ src/pages/newCardPage/NewCardPage.tsx
@@ -3,7 +3,6 @@ import Input from '../../components/common/input/Input';
 import Select from '../../components/common/select/Select';
 import { ICardInfo, IErrorMessage } from '../../types/type';
 import NewCardInputSection from '../../components/newCardInputSection/NewCardInputSection';
-import { NewCardContainer } from './NewCardPage.styled';
 import {
   validateCVC,
   validateCardCompany,
@@ -16,6 +15,7 @@ import { CARD_FORM_INPUTS } from '../../constants/setting';
 import CardFrontPreview from '../../components/cardPreview/CardFrontPreview';
 import CardBackPreview from '../../components/cardPreview/cardBackPreview';
 import Button from '../../components/common/button/Button';
+import * as Styled from './NewCardPage.styled';
 
 const NewCardPage = () => {
   const [cardInfo, setCardInfo] = useState<ICardInfo>({
@@ -264,113 +264,124 @@ const NewCardPage = () => {
     return isCardInfoValid && isErrorMessageValid;
   };
 
+  const navigateToRegistrationCompletePage = () => {
+    console.log('d');
+  };
+
   return (
-    <NewCardContainer>
+    <Styled.NewCardContainer>
       {preview === 'front' && <CardFrontPreview cardInfo={cardInfo} />}
       {preview === 'back' && <CardBackPreview cvc={cardInfo.cvc} />}
-      <NewCardInputSection
-        label={CARD_FORM_INPUTS.CARD_NUMBERS.LABEL}
-        mainText={CARD_FORM_INPUTS.CARD_NUMBERS.MAIN_TEXT}
-        subText={CARD_FORM_INPUTS.CARD_NUMBERS.SUB_TEXT}
-        errorMessage={errorMessage.cardNumbers}
-      >
-        {cardInfo.cardNumbers.map((_, index) => (
-          <Input
-            key={index}
-            maxLength={CARD_FORM_INPUTS.CARD_NUMBERS.MAX_LENGTH}
-            placeholder={CARD_FORM_INPUTS.CARD_NUMBERS.PLACEHOLDER}
-            isError={!!errorMessage.cardNumbers[index]}
-            onChange={(event) => handleCardNumbersChange(event, index)}
-          ></Input>
-        ))}
-      </NewCardInputSection>
-      {creationStage >= 2 && (
+      <Styled.NewCardInputSectionWrapper>
         <NewCardInputSection
-          label={CARD_FORM_INPUTS.CARD_COMPANY.LABEL}
-          mainText={CARD_FORM_INPUTS.CARD_COMPANY.MAIN_TEXT}
-          subText={CARD_FORM_INPUTS.CARD_COMPANY.SUB_TEXT}
+          label={CARD_FORM_INPUTS.CARD_NUMBERS.LABEL}
+          mainText={CARD_FORM_INPUTS.CARD_NUMBERS.MAIN_TEXT}
+          subText={CARD_FORM_INPUTS.CARD_NUMBERS.SUB_TEXT}
+          errorMessage={errorMessage.cardNumbers}
         >
-          <Select
-            options={CARD_FORM_INPUTS.CARD_COMPANY.OPTIONS}
-            onChange={(e) => handleCardCompanyChange(e.target.value)}
-            value={cardInfo.cardCompany}
-          ></Select>
-        </NewCardInputSection>
-      )}
-      {creationStage >= 3 && (
-        <NewCardInputSection
-          label={CARD_FORM_INPUTS.CARD_EXPIRATION.LABEL}
-          mainText={CARD_FORM_INPUTS.CARD_EXPIRATION.MAIN_TEXT}
-          subText={CARD_FORM_INPUTS.CARD_EXPIRATION.SUB_TEXT}
-          errorMessage={errorMessage.cardExpiration}
-        >
-          {cardInfo.cardExpiration.map((_, index) => (
+          {cardInfo.cardNumbers.map((_, index) => (
             <Input
-              autoFocus={index === 0}
               key={index}
-              maxLength={CARD_FORM_INPUTS.CARD_EXPIRATION.MAX_LENGTH}
-              placeholder={
-                index === 0
-                  ? CARD_FORM_INPUTS.CARD_EXPIRATION.PLACEHOLDER.MONTH
-                  : CARD_FORM_INPUTS.CARD_EXPIRATION.PLACEHOLDER.YEAR
-              }
-              isError={!!errorMessage.cardExpiration[index]}
-              onChange={(event) => handleCardExpirationChange(event, index)}
+              maxLength={CARD_FORM_INPUTS.CARD_NUMBERS.MAX_LENGTH}
+              placeholder={CARD_FORM_INPUTS.CARD_NUMBERS.PLACEHOLDER}
+              isError={!!errorMessage.cardNumbers[index]}
+              onChange={(event) => handleCardNumbersChange(event, index)}
             ></Input>
           ))}
         </NewCardInputSection>
+        {creationStage >= 2 && (
+          <NewCardInputSection
+            label={CARD_FORM_INPUTS.CARD_COMPANY.LABEL}
+            mainText={CARD_FORM_INPUTS.CARD_COMPANY.MAIN_TEXT}
+            subText={CARD_FORM_INPUTS.CARD_COMPANY.SUB_TEXT}
+          >
+            <Select
+              options={CARD_FORM_INPUTS.CARD_COMPANY.OPTIONS}
+              onChange={(e) => handleCardCompanyChange(e.target.value)}
+              value={cardInfo.cardCompany}
+            ></Select>
+          </NewCardInputSection>
+        )}
+        {creationStage >= 3 && (
+          <NewCardInputSection
+            label={CARD_FORM_INPUTS.CARD_EXPIRATION.LABEL}
+            mainText={CARD_FORM_INPUTS.CARD_EXPIRATION.MAIN_TEXT}
+            subText={CARD_FORM_INPUTS.CARD_EXPIRATION.SUB_TEXT}
+            errorMessage={errorMessage.cardExpiration}
+          >
+            {cardInfo.cardExpiration.map((_, index) => (
+              <Input
+                autoFocus={index === 0}
+                key={index}
+                maxLength={CARD_FORM_INPUTS.CARD_EXPIRATION.MAX_LENGTH}
+                placeholder={
+                  index === 0
+                    ? CARD_FORM_INPUTS.CARD_EXPIRATION.PLACEHOLDER.MONTH
+                    : CARD_FORM_INPUTS.CARD_EXPIRATION.PLACEHOLDER.YEAR
+                }
+                isError={!!errorMessage.cardExpiration[index]}
+                onChange={(event) => handleCardExpirationChange(event, index)}
+              ></Input>
+            ))}
+          </NewCardInputSection>
+        )}
+        {creationStage >= 4 && (
+          <NewCardInputSection
+            label={CARD_FORM_INPUTS.USER_NAME.LABEL}
+            mainText={CARD_FORM_INPUTS.USER_NAME.MAIN_TEXT}
+            subText={CARD_FORM_INPUTS.USER_NAME.SUB_TEXT}
+            errorMessage={errorMessage.userName}
+          >
+            <Input
+              value={cardInfo.userName}
+              autoFocus
+              maxLength={CARD_FORM_INPUTS.USER_NAME.MAX_LENGTH}
+              placeholder={CARD_FORM_INPUTS.USER_NAME.PLACEHOLDER}
+              isError={!!errorMessage.userName[0]}
+              onChange={(e) => handleUserNameChange(e.target.value)}
+            ></Input>
+          </NewCardInputSection>
+        )}
+        {creationStage >= 5 && (
+          <NewCardInputSection
+            label={CARD_FORM_INPUTS.CVC.LABEL}
+            mainText={CARD_FORM_INPUTS.CVC.MAIN_TEXT}
+            subText={CARD_FORM_INPUTS.CVC.SUB_TEXT}
+            errorMessage={errorMessage.cvc}
+          >
+            <Input
+              maxLength={CARD_FORM_INPUTS.CVC.MAX_LENGTH}
+              placeholder={CARD_FORM_INPUTS.CVC.PLACEHOLDER}
+              isError={!!errorMessage.cvc[0]}
+              onChange={(e) => handleCVCChange(e.target.value)}
+            ></Input>
+          </NewCardInputSection>
+        )}
+        {creationStage >= 6 && (
+          <NewCardInputSection
+            label={CARD_FORM_INPUTS.PASSWORD.LABEL}
+            mainText={CARD_FORM_INPUTS.PASSWORD.MAIN_TEXT}
+            subText={CARD_FORM_INPUTS.PASSWORD.SUB_TEXT}
+            errorMessage={errorMessage.password}
+          >
+            <Input
+              type='password'
+              autoFocus
+              maxLength={CARD_FORM_INPUTS.PASSWORD.MAX_LENGTH}
+              placeholder={CARD_FORM_INPUTS.PASSWORD.PLACEHOLDER}
+              isError={!!errorMessage.password[0]}
+              onChange={(e) => handlePasswordChange(e.target.value)}
+            ></Input>
+          </NewCardInputSection>
+        )}
+      </Styled.NewCardInputSectionWrapper>
+      {isAllValidInput() && (
+        <Button
+          onClick={navigateToRegistrationCompletePage}
+          text='확인'
+        ></Button>
       )}
-      {creationStage >= 4 && (
-        <NewCardInputSection
-          label={CARD_FORM_INPUTS.USER_NAME.LABEL}
-          mainText={CARD_FORM_INPUTS.USER_NAME.MAIN_TEXT}
-          subText={CARD_FORM_INPUTS.USER_NAME.SUB_TEXT}
-          errorMessage={errorMessage.userName}
-        >
-          <Input
-            value={cardInfo.userName}
-            autoFocus
-            maxLength={CARD_FORM_INPUTS.USER_NAME.MAX_LENGTH}
-            placeholder={CARD_FORM_INPUTS.USER_NAME.PLACEHOLDER}
-            isError={!!errorMessage.userName[0]}
-            onChange={(e) => handleUserNameChange(e.target.value)}
-          ></Input>
-        </NewCardInputSection>
-      )}
-      {creationStage >= 5 && (
-        <NewCardInputSection
-          label={CARD_FORM_INPUTS.CVC.LABEL}
-          mainText={CARD_FORM_INPUTS.CVC.MAIN_TEXT}
-          subText={CARD_FORM_INPUTS.CVC.SUB_TEXT}
-          errorMessage={errorMessage.cvc}
-        >
-          <Input
-            maxLength={CARD_FORM_INPUTS.CVC.MAX_LENGTH}
-            placeholder={CARD_FORM_INPUTS.CVC.PLACEHOLDER}
-            isError={!!errorMessage.cvc[0]}
-            onChange={(e) => handleCVCChange(e.target.value)}
-          ></Input>
-        </NewCardInputSection>
-      )}
-      {creationStage >= 6 && (
-        <NewCardInputSection
-          label={CARD_FORM_INPUTS.PASSWORD.LABEL}
-          mainText={CARD_FORM_INPUTS.PASSWORD.MAIN_TEXT}
-          subText={CARD_FORM_INPUTS.PASSWORD.SUB_TEXT}
-          errorMessage={errorMessage.password}
-        >
-          <Input
-            type='password'
-            autoFocus
-            maxLength={CARD_FORM_INPUTS.PASSWORD.MAX_LENGTH}
-            placeholder={CARD_FORM_INPUTS.PASSWORD.PLACEHOLDER}
-            isError={!!errorMessage.password[0]}
-            onChange={(e) => handlePasswordChange(e.target.value)}
-          ></Input>
-        </NewCardInputSection>
-      )}
-      {isAllValidInput() && <Button text='확인'></Button>}
-    </NewCardContainer>
+    </Styled.NewCardContainer>
   );
 };
 
