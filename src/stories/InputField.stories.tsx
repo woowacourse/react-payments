fa365eb990c522456818d7882e0d20c0a8a7faac
rbgksqkr <rbgks1937@naver.com>
2024-04-26 18:58:42 +0900

    test(InputField): 수정된 레이아웃 추가

--- src/stories/InputField.stories.tsx
+++ src/stories/InputField.stories.tsx
@@ -97,3 +97,44 @@ export const 카드소유자_입력_레이아웃: Story = {
     ),
   },
 };
+
+export const CVC_입력_레이아웃: Story = {
+  args: {
+    title: 'CVC 번호를 입력해 주세요',
+    labelText: 'cvc',
+    labelFor: 'cvc-input',
+    children: (
+      <Input
+        id="cvc-input"
+        isError={false}
+        value=""
+        onChange={() => ''}
+        onBlur={() => ''}
+        placeholder="123"
+        width="100%"
+        maxLength={4}
+      />
+    ),
+  },
+};
+
+export const 카드_비밀번호_입력_레이아웃: Story = {
+  args: {
+    title: '비밀번호를 입력해 주세요',
+    subtitle: '앞의 2자리를 입력해주세요',
+    labelText: '비밀번호 앞 2자리',
+    labelFor: 'password-input',
+    children: (
+      <Input
+        id="password-input"
+        isError={false}
+        value=""
+        onChange={() => ''}
+        onBlur={() => ''}
+        placeholder="••"
+        width="100%"
+        maxLength={100}
+      />
+    ),
+  },
+};
