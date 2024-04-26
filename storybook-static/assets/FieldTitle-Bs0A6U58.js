d30abf07961d54ded98f805e815bc8d7cc4705cf
Hyelim Choi <HyelimChoi01@gmail.com>
2024-04-26 21:14:30 +0900

    buil: 웹 배포

---
+++ storybook-static/assets/FieldTitle-Bs0A6U58.js
@@ -0,0 +1,9 @@
+import{u as t,j as e}from"./styled-components.browser.esm-BJwir-wm.js";function s({title:i,subtitle:r}){return e.jsxs(e.Fragment,{children:[e.jsx(o,{children:i}),e.jsx(n,{children:r})]})}const o=t.h1`
+  color: #000;
+  font-size: 18px;
+  margin-bottom: 4px;
+`,n=t.p`
+  color: #8b95a1;
+  font-size: 9.5px;
+  margin-bottom: 16px;
+`;s.__docgenInfo={description:"",methods:[],displayName:"FieldTitle",props:{title:{required:!0,tsType:{name:"string"},description:""},subtitle:{required:!1,tsType:{name:"string"},description:""}}};export{s as F};
---
+++ storybook-static/assets/container.style-BJ-iVavo.js
@@ -0,0 +1,16 @@
+import{u as e}from"./styled-components.browser.esm-BJwir-wm.js";const t=e.div`
+  display: flex;
+  flex-direction: column;
+  height: 110px;
+`,o=e.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 245px;
+  height: 160px;
+  border-radius: 4px;
+  color: white;
+  font-size: 14px;
+  font-weight: 500;
+  box-shadow: 3px 3px 5px 0px rgba(0, 0, 0, 0.25);
+`;export{o as C,t as F};
