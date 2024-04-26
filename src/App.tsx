4102a4eb68f3dd0a47fbb0e6824cecce85652caa
jaeml06 <jaeml0630@gmail.com>
2024-04-26 21:52:44 +0900

    feat: 리액트 라우터 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import EnrollCard from './page/EnrollCard';
 import './reset.css';
+import CardRegistrationConfirmation from './page/CardRegistrationConfirmation';
 
 function App() {
   return (
     <>
-      <EnrollCard />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <EnrollCard/>} />
+          <Route path="/CardRegistrationConfirmation" element={<CardRegistrationConfirmation />}>
+          </Route>
+        </Routes>
+    </BrowserRouter>
     </>
   );
 }
