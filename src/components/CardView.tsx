5d4effd9d6e985ab8b075c31fb74d6343b9db586
Hyelim Choi <HyelimChoi01@gmail.com>
2024-04-26 21:15:32 +0900

    style: 카드 애니메이션 추가

--- src/components/CardView.tsx
+++ src/components/CardView.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Card } from '../types/card';
 import CardFrontView from './CardFrontView';
 import CardBackView from './CardBackView';
+import styled from 'styled-components';
 
 interface Props {
   cardInfo: Card;
@@ -16,9 +17,28 @@ export default function CardView({ cardInfo }: Props) {
   }, [cardInfo.cvc]);
 
   return (
-    <>
-      {!showBack && <CardFrontView cardInfo={cardInfo} />}
-      {showBack && <CardBackView cvc={cardInfo.cvc} />}
-    </>
+    <CardsContainer showBack={showBack}>
+      <Front> {!showBack && <CardFrontView cardInfo={cardInfo} />}</Front>
+      <Back> {showBack && <CardBackView cvc={cardInfo.cvc} />}</Back>
+    </CardsContainer>
   );
 }
+
+const CardsContainer = styled.div<{ showBack: boolean }>`
+  display: inline-grid;
+  display: inline-block;
+  color: white;
+  transition: transform 0.3s;
+  transform-style: preserve-3d;
+  cursor: pointer;
+  transform: ${(props) =>
+    props.showBack ? 'rotateY(180deg)' : 'rotateY(0deg)'};
+`;
+
+const Front = styled.div`
+  backface-visibility: hidden;
+`;
+const Back = styled.div`
+  backface-visibility: hidden;
+  transform: rotateY(180deg);
+`;
