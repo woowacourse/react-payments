dcecdd9b08280cf4feab6fda47f2be8b1c27a12c
pakxe <pigkill40@naver.com>
2024-04-26 15:55:09 +0900

    feat: 잘못된 접근일 경우 안내하도록 구현

--- src/pages/CompleteAddNewCardPage/CompleteAddNewCardPage.tsx
+++ src/pages/CompleteAddNewCardPage/CompleteAddNewCardPage.tsx
@@ -4,37 +4,57 @@ import Button from '../../components/common/Button/Button';
 import { COMPANY_LIST, CardCompany } from '../../types/cardCompany';
 import styles from './CompleteAddNewCardPage.module.css';
 import { isCardCompany } from '../../utils/isCardCompany';
+import { useEffect, useState } from 'react';
+import { ERROR_MESSAGES } from '../../constants/errorMessages';
 
 type CompleteAddNewCardPage = {
   firstCardNumberUnit: string;
   cardCompany: CardCompany;
 };
 
+const initialPageData = {
+  firstCardNumberUnit: '',
+  cardCompany: '',
+};
+
 const CompleteAddNewCardPage = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const [data, setData] = useState<Record<string, string> | null>(initialPageData);
 
-  const firstCardNumberUnit = location.state.firstCardNumberUnit;
-  const cardCompany = location.state.cardCompany;
+  useEffect(() => {
+    if (location.state === null) {
+      setData(null);
+      return;
+    }
+
+    const firstCardNumberUnit = location.state.firstCardNumberUnit;
+    const cardCompany = validatedCardCompany(location.state.cardCompany);
+    setData({ firstCardNumberUnit, cardCompany });
+  }, []);
 
   const handleConfirm = () => {
     navigate('/add-new-card');
   };
 
-  const validatedCardCompany = () => {
+  const validatedCardCompany = (cardCompany: string) => {
     return isCardCompany(cardCompany) ? COMPANY_LIST[cardCompany] : '존재하지 않는 카드';
   };
 
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <img src={checkCircle} height='76' />
-        <h2>
-          {firstCardNumberUnit}로 시작하는
-          <br /> {validatedCardCompany()}가 등록되었어요.
-        </h2>
-        <Button name='확인' size='l' buttonType='confirm' onClick={handleConfirm} />
-      </div>
+      {data === null ? (
+        <div>{ERROR_MESSAGES.INVALID_ACCESS}</div>
+      ) : (
+        <div className={styles.wrapper}>
+          <img src={checkCircle} height='76' />
+          <h2>
+            {data.firstCardNumberUnit}로 시작하는
+            <br /> {data.cardCompany}가 등록되었어요.
+          </h2>
+          <Button name='확인' size='l' buttonType='confirm' onClick={handleConfirm} />
+        </div>
+      )}
     </div>
   );
 };
