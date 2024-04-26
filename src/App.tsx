65bc58fceefa04cfef47351bace7dd513a122df0
Lee sang Yeop <mma7710@naver.com>
2024-04-27 02:50:03 +0900

    feat(Router): pathname 별 렌더링 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import CardRegistration from "./pages/cardRegistration";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import "./App.css";
+import CardRegistration from "./pages/payments/CardRegistration";
+import SuccessRegistration from "./pages/payments/SuccessRegistration";
 
 function App() {
-  return <CardRegistration />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CardRegistration />} />
+        <Route path="/register/success" element={<SuccessRegistration />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
