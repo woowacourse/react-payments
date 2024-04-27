57be60d65e85a9fc2bd383e1949727f6c5dde05c
hwinkr <dnddl8280@naver.com>
2024-04-27 11:22:17 +0900

    feat(useCardAddFormField): 각 카드 폼 입력이 유효한지 판단하고, 폼 필드의 현재 상태를 참조할 수 있도록 하는 커스텀 훅 구현

---
+++ src/hooks/useCardAddFormField.ts
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import FormContext from "../context/card-add-form";
+
+import { CardAddFormState, CardFormField, FormAction } from "../types/card";
+
+const useCardAddFormField = <T extends CardFormField>(
+  field: T
+): {
+  formFieldState: CardAddFormState[T];
+  isValidFormField: boolean;
+  handleFormFieldFocus: () => void;
+  dispatch: React.Dispatch<FormAction>;
+} => {
+  const context = useContext(FormContext);
+
+  if (!context) {
+    throw new Error("useFormContext must be used within a FormProvider");
+  }
+
+  const { formState, dispatch } = context;
+
+  const isValidFormField = Object.values(formState[field].errorState).every(
+    (isValid) => !isValid
+  );
+
+  const handleFormFieldFocus = () => {
+    if (formState[field].displayed) return;
+
+    dispatch({
+      type: "SET_DISPLAY",
+      field,
+    });
+  };
+
+  return {
+    formFieldState: formState[field],
+    isValidFormField,
+    handleFormFieldFocus,
+    dispatch,
+  };
+};
+
+export default useCardAddFormField;
