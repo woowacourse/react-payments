a1b8f682722c52e76d9f0c53016600b6fb781e8b
jayming66 <mjw8941@gmail.com>
2024-04-28 00:29:13 +0900

    refactor: font-family 중복 제거
    font가 렌더링 중간에 바뀌어 글자가 움직이는 현상

--- src/components/atoms/text.ts
+++ src/components/atoms/text.ts
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const TitleText = styled.h1`
-  font-family: NotoSansKR, Regular;
   font-size: 25px;
   font-weight: 700;
   line-height: 36px;
@@ -9,7 +8,6 @@ export const TitleText = styled.h1`
 `;
 
 export const SubTitleText = styled.h2`
-  font-family: NotoSansKR, Regular;
   font-size: 18px;
   font-weight: 700;
   line-height: 26.06px;
@@ -17,7 +15,6 @@ export const SubTitleText = styled.h2`
 `;
 
 export const CaptionText = styled.span`
-  font-family: NotoSansKR, Regular;
   font-size: 9.5px;
   font-weight: 400;
   line-height: 13.76px;
@@ -26,7 +23,6 @@ export const CaptionText = styled.span`
 `;
 
 export const LabelText = styled.label`
-  font-family: NotoSansKR, Regular;
   font-size: 12px;
   font-weight: 500;
   line-height: 15px;
