94bcaa2563c9c0ac1b426eee0f530aab78195578
김진호 <rlawlsgh1227@gmail.com>
2024-04-27 14:49:47 +0900

    fix: 프리뷰 스토리북 조정

--- src/stories/Preview/Preview.stories.tsx
+++ src/stories/Preview/Preview.stories.tsx
@@ -1,5 +1,27 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { useState } from 'react';
 import CardInformationPreview from '../../components/CardInformationPreview/CardInformationPreview';
+
+const 기본_props = {
+  cardNumberState: {
+    first: '',
+    second: '',
+    third: '',
+    fourth: '',
+  },
+  expirationDateState: {
+    month: '',
+    year: '',
+  },
+  userNameState: '',
+  showImageCondition: {
+    visaShowCondition: false,
+    masterCardShowCondition: false,
+  },
+  cardBrandState: null,
+  cvcNumberState: '',
+};
 
 const meta = {
   title: 'CardInformationPreview',
@@ -11,18 +33,64 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const 기본: Story = {
+  decorators: [
+    (Story) => {
+      const [isFront, setIsFront] = useState<boolean>(false);
+      return (
+        <Story
+          args={{ ...기본_props, isFocusCVCPreview: isFront, setIsFocusCVCPreview: setIsFront }}
+        />
+      );
+    },
+  ],
   args: {
-    cardNumberState: { first: '', second: '', third: '', fourth: '' },
-    expirationDateState: { month: '', year: '' },
+    cardNumberState: {
+      first: '',
+      second: '',
+      third: '',
+      fourth: '',
+    },
+    expirationDateState: {
+      month: '',
+      year: '',
+    },
     userNameState: '',
     showImageCondition: {
       visaShowCondition: false,
       masterCardShowCondition: false,
     },
+    cardBrandState: null,
+    cvcNumberState: '',
+    isFocusCVCPreview: false,
+    setIsFocusCVCPreview: fn(),
   },
 };
 
+const 전체_props = {
+  cardNumberState: { first: '5123', second: '1234', third: '1234', fourth: '1234' },
+  expirationDateState: { month: '12', year: '24' },
+  userNameState: 'SIMO COOKIE',
+  showImageCondition: {
+    visaShowCondition: false,
+    masterCardShowCondition: true,
+  },
+  cardBrandState: '롯데카드',
+  cvcNumberState: '123',
+  isFocusCVCPreview: false,
+  setIsFocusCVCPreview: fn(),
+};
+
 export const 전체: Story = {
+  decorators: [
+    (Story) => {
+      const [isFront, setIsFront] = useState<boolean>(false);
+      return (
+        <Story
+          args={{ ...전체_props, isFocusCVCPreview: isFront, setIsFocusCVCPreview: setIsFront }}
+        />
+      );
+    },
+  ],
   args: {
     cardNumberState: { first: '5123', second: '1234', third: '1234', fourth: '1234' },
     expirationDateState: { month: '12', year: '24' },
@@ -31,22 +99,82 @@ export const 전체: Story = {
       visaShowCondition: false,
       masterCardShowCondition: true,
     },
+    cardBrandState: null,
+    cvcNumberState: '',
+    isFocusCVCPreview: false,
+    setIsFocusCVCPreview: fn(),
   },
+};
+
+const 비자카드_props = {
+  cardNumberState: { first: '4123', second: '', third: '', fourth: '' },
+  expirationDateState: { month: '', year: '' },
+  userNameState: '',
+  showImageCondition: {
+    visaShowCondition: true,
+    masterCardShowCondition: false,
+  },
+  cardBrandState: '롯데카드',
+  cvcNumberState: '123',
+  isFocusCVCPreview: false,
+  setIsFocusCVCPreview: fn(),
 };
 
 export const 비자카드: Story = {
-  args: {
-    cardNumberState: { first: '4123', second: '', third: '', fourth: '' },
-    expirationDateState: { month: '', year: '' },
-    userNameState: '',
-    showImageCondition: {
-      visaShowCondition: true,
-      masterCardShowCondition: false,
+  decorators: [
+    (Story) => {
+      const [isFront, setIsFront] = useState<boolean>(false);
+      return (
+        <Story
+          args={{ ...비자카드_props, isFocusCVCPreview: isFront, setIsFocusCVCPreview: setIsFront }}
+        />
+      );
     },
+  ],
+  args: {
+    cardNumberState: { first: '5123', second: '1234', third: '1234', fourth: '1234' },
+    expirationDateState: { month: '12', year: '24' },
+    userNameState: 'SIMO COOKIE',
+    showImageCondition: {
+      visaShowCondition: false,
+      masterCardShowCondition: true,
+    },
+    cardBrandState: null,
+    cvcNumberState: '',
+    isFocusCVCPreview: false,
+    setIsFocusCVCPreview: fn(),
   },
 };
 
+const 마스터카드_props = {
+  cardNumberState: { first: '5123', second: '', third: '', fourth: '' },
+  expirationDateState: { month: '', year: '' },
+  userNameState: '',
+  showImageCondition: {
+    visaShowCondition: false,
+    masterCardShowCondition: true,
+  },
+  cardBrandState: '국민카드',
+  cvcNumberState: '123',
+  isFocusCVCPreview: false,
+  setIsFocusCVCPreview: fn(),
+};
+
 export const 마스터카드: Story = {
+  decorators: [
+    (Story) => {
+      const [isFront, setIsFront] = useState<boolean>(false);
+      return (
+        <Story
+          args={{
+            ...마스터카드_props,
+            isFocusCVCPreview: isFront,
+            setIsFocusCVCPreview: setIsFront,
+          }}
+        />
+      );
+    },
+  ],
   args: {
     cardNumberState: { first: '5123', second: '', third: '', fourth: '' },
     expirationDateState: { month: '', year: '' },
@@ -55,10 +183,42 @@ export const 마스터카드: Story = {
       visaShowCondition: false,
       masterCardShowCondition: true,
     },
+    cardBrandState: '국민카드',
+    cvcNumberState: '123',
+    isFocusCVCPreview: false,
+    setIsFocusCVCPreview: fn(),
   },
 };
 
+const 일반카드_props = {
+  cardNumberState: { first: '1111', second: '', third: '', fourth: '' },
+  expirationDateState: { month: '', year: '' },
+  userNameState: '',
+  showImageCondition: {
+    visaShowCondition: false,
+    masterCardShowCondition: false,
+  },
+  cardBrandState: '국민카드',
+  cvcNumberState: '123',
+  isFocusCVCPreview: false,
+  setIsFocusCVCPreview: fn(),
+};
+
 export const 일반카드: Story = {
+  decorators: [
+    (Story) => {
+      const [isFront, setIsFront] = useState<boolean>(false);
+      return (
+        <Story
+          args={{
+            ...일반카드_props,
+            isFocusCVCPreview: isFront,
+            setIsFocusCVCPreview: setIsFront,
+          }}
+        />
+      );
+    },
+  ],
   args: {
     cardNumberState: { first: '1111', second: '', third: '', fourth: '' },
     expirationDateState: { month: '', year: '' },
@@ -67,17 +227,53 @@ export const 일반카드: Story = {
       visaShowCondition: false,
       masterCardShowCondition: false,
     },
+    cardBrandState: '국민카드',
+    cvcNumberState: '123',
+    isFocusCVCPreview: false,
+    setIsFocusCVCPreview: fn(),
   },
 };
 
+const 카드번호_가리기_props = {
+  cardNumberState: { first: '', second: '', third: '1244', fourth: '' },
+  expirationDateState: { month: '', year: '' },
+  userNameState: '',
+  showImageCondition: {
+    visaShowCondition: false,
+    masterCardShowCondition: false,
+  },
+  cardBrandState: '국민카드',
+  cvcNumberState: '123',
+  isFocusCVCPreview: false,
+  setIsFocusCVCPreview: fn(),
+};
+
 export const 카드번호_가리기: Story = {
+  decorators: [
+    (Story) => {
+      const [isFront, setIsFront] = useState<boolean>(false);
+      return (
+        <Story
+          args={{
+            ...카드번호_가리기_props,
+            isFocusCVCPreview: isFront,
+            setIsFocusCVCPreview: setIsFront,
+          }}
+        />
+      );
+    },
+  ],
   args: {
-    cardNumberState: { first: '', second: '', third: '1231', fourth: '' },
+    cardNumberState: { first: '', second: '', third: '1234', fourth: '' },
     expirationDateState: { month: '', year: '' },
     userNameState: '',
     showImageCondition: {
       visaShowCondition: false,
       masterCardShowCondition: false,
     },
+    cardBrandState: '국민카드',
+    cvcNumberState: '123',
+    isFocusCVCPreview: false,
+    setIsFocusCVCPreview: fn(),
   },
 };
