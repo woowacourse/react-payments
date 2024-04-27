03496b90dc13bb08ad973e6cc2a36e97fc4e32f4
useon <yoosk4986@naver.com>
2024-04-27 21:49:04 +0900

    fix: button 공통컴포넌트 onClick이 되지 않는 문제 해결

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -1,11 +1,11 @@
 import * as Styled from './Button.styled';
 
-export interface ButtonProps extends React.ComponentPropsWithRef<'input'> {
+export interface ButtonProps extends React.ComponentPropsWithRef<'button'> {
   text?: string;
 }
 
-const Button = ({ text }: ButtonProps) => {
-  return <Styled.Button>{text}</Styled.Button>;
+const Button = ({ text, ...restProps }: ButtonProps) => {
+  return <Styled.Button {...restProps}>{text}</Styled.Button>;
 };
 
 export default Button;
