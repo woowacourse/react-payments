5dce9284fc2352efdcb299f5610a9a47a3936f78
useon <yoosk4986@naver.com>
2024-04-27 22:01:16 +0900

    feat: 카드 등록 완료 페이지 생성 및 카드 등록 완료 페이지에서 확인 버튼을 누르면 카드 등록 페이지로 이동하는 기능

---
+++ src/pages/cardRegistrationCompletePage/CardRegistrationCompletePage.tsx
@@ -0,0 +1,42 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import CardRegistrationCompleteImage from '../../assets/CardRegistrationComplete.png';
+import Button from '../../components/common/button/Button';
+import * as Styled from './CardRegistrationCompletePage.styled';
+
+const CardRegistrationCompletePage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const cardFirstFourDigits = location.state.cardFirstFourDigits;
+  const cardCompany = location.state.cardCompany;
+
+  const navigateToNewCardPage = () => {
+    navigate('/new-card');
+  };
+
+  return (
+    <Styled.RegistrationCompleteContainer>
+      <div>
+        <Styled.CardRegistrationCompleteImage
+          src={CardRegistrationCompleteImage}
+          alt=''
+        />
+      </div>
+      <Styled.CardInfoWrapper>
+        <Styled.CardInfoText>
+          {cardFirstFourDigits}로 시작하는
+        </Styled.CardInfoText>
+        <Styled.CardInfoText>{cardCompany}가 등록되었어요.</Styled.CardInfoText>
+      </Styled.CardInfoWrapper>
+      <Styled.ButtonWrapper>
+        <Button
+          onClick={navigateToNewCardPage}
+          text='확인'
+          style={{ borderRadius: 8 }}
+        ></Button>
+      </Styled.ButtonWrapper>
+    </Styled.RegistrationCompleteContainer>
+  );
+};
+
+export default CardRegistrationCompletePage;
