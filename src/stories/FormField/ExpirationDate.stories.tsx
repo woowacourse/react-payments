07a84ebc540cc2c464b9f4cf13ea63b0f9ab3f0f
김진호 <rlawlsgh1227@gmail.com>
2024-04-27 13:38:16 +0900

    refactor: 스토리북 기존 필드 컴포넌트로 교체

--- src/stories/FormField/ExpirationDate.stories.tsx
+++ src/stories/FormField/ExpirationDate.stories.tsx
@@ -1,17 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import InputField from '../../components/InputField/InputField';
-import Input from '../../components/Input/Input';
-import FormField from '../../components/FormField/FormField';
-import MESSAGE from '../../constants/Message';
-import CONDITION from '../../constants/Condition';
-
-const { TITLE, CAPTION, LABEL, PLACEHOLDER, ERROR } = MESSAGE;
-const { MAX_LENGTH } = CONDITION;
+import ExpirationDate from '../../components/FormField/ExpirationDate';
+import { fn } from '@storybook/test';
 
 const meta = {
   title: 'FormField_유효기간',
-  component: FormField,
-} satisfies Meta<typeof FormField>;
+  component: ExpirationDate,
+} satisfies Meta<typeof ExpirationDate>;
 
 export default meta;
 
@@ -19,110 +13,54 @@ type Story = StoryObj<typeof meta>;
 
 export const 기본: Story = {
   args: {
-    title: TITLE.expirationDate,
-    caption: CAPTION.expirationDate,
-    children: (
-      <InputField label={LABEL.expirationDate} error="">
-        <>
-          <Input placeholder={PLACEHOLDER.month} value="" maxLength={MAX_LENGTH.expirationDate} />
-          <Input placeholder={PLACEHOLDER.year} value="" maxLength={MAX_LENGTH.expirationDate} />
-        </>
-      </InputField>
-    ),
+    expirationDateState: { month: '', year: '' },
+    setExpirationDateState: { setMonth: fn(), setYear: fn() },
+    expirationDateErrorState: { isMonthError: false, isYearError: false },
+    showNextFieldOnLastElementBlur: fn(),
   },
 };
 
 export const 정상입력: Story = {
   args: {
-    title: TITLE.expirationDate,
-    caption: CAPTION.expirationDate,
-    children: (
-      <InputField label={LABEL.expirationDate} error="">
-        <>
-          <Input placeholder={PLACEHOLDER.month} value="04" maxLength={MAX_LENGTH.expirationDate} />
-          <Input placeholder={PLACEHOLDER.year} value="24" maxLength={MAX_LENGTH.expirationDate} />
-        </>
-      </InputField>
-    ),
+    expirationDateState: { month: '12', year: '24' },
+    setExpirationDateState: { setMonth: fn(), setYear: fn() },
+    expirationDateErrorState: { isMonthError: false, isYearError: false },
+    showNextFieldOnLastElementBlur: fn(),
   },
 };
 
 export const 월_입력의_형식이_아닌_경우: Story = {
   args: {
-    title: TITLE.expirationDate,
-    caption: CAPTION.expirationDate,
-    children: (
-      <InputField label={LABEL.expirationDate} error={ERROR.month}>
-        <>
-          <Input
-            placeholder={PLACEHOLDER.month}
-            value="13"
-            maxLength={MAX_LENGTH.expirationDate}
-            aria-aria-invalid
-          />
-          <Input placeholder={PLACEHOLDER.year} value="24" maxLength={MAX_LENGTH.expirationDate} />
-        </>
-      </InputField>
-    ),
+    expirationDateState: { month: '쿠', year: '24' },
+    setExpirationDateState: { setMonth: fn(), setYear: fn() },
+    expirationDateErrorState: { isMonthError: true, isYearError: false },
+    showNextFieldOnLastElementBlur: fn(),
   },
 };
 
 export const 월_입력_길이가_맞지_않은_경우: Story = {
   args: {
-    title: TITLE.expirationDate,
-    caption: CAPTION.expirationDate,
-    children: (
-      <InputField label={LABEL.expirationDate} error={ERROR.month}>
-        <>
-          <Input
-            placeholder={PLACEHOLDER.month}
-            value="1"
-            maxLength={MAX_LENGTH.expirationDate}
-            aria-aria-invalid
-          />
-          <Input placeholder={PLACEHOLDER.year} value="24" maxLength={MAX_LENGTH.expirationDate} />
-        </>
-      </InputField>
-    ),
+    expirationDateState: { month: '3', year: '24' },
+    setExpirationDateState: { setMonth: fn(), setYear: fn() },
+    expirationDateErrorState: { isMonthError: true, isYearError: false },
+    showNextFieldOnLastElementBlur: fn(),
   },
 };
 
 export const 년_입력의_형식이_아닌_경우: Story = {
   args: {
-    title: TITLE.expirationDate,
-    caption: CAPTION.expirationDate,
-    children: (
-      <InputField label={LABEL.expirationDate} error={ERROR.year}>
-        <>
-          <Input placeholder={PLACEHOLDER.month} value="04" maxLength={MAX_LENGTH.expirationDate} />
-          <Input
-            placeholder={PLACEHOLDER.year}
-            value="2d"
-            maxLength={MAX_LENGTH.expirationDate}
-            aria-invalid
-          />
-        </>
-      </InputField>
-    ),
+    expirationDateState: { month: '12', year: '쿠' },
+    setExpirationDateState: { setMonth: fn(), setYear: fn() },
+    expirationDateErrorState: { isMonthError: false, isYearError: true },
+    showNextFieldOnLastElementBlur: fn(),
   },
 };
 
 export const 년_입력의_길이가_맞지_않은_경우: Story = {
   args: {
-    title: TITLE.expirationDate,
-    caption: CAPTION.expirationDate,
-    children: (
-      <InputField label={LABEL.expirationDate} error={ERROR.year}>
-        <>
-          <Input placeholder={PLACEHOLDER.month} value="04" maxLength={MAX_LENGTH.expirationDate} />
-          <Input
-            placeholder={PLACEHOLDER.year}
-            value="1"
-            maxLength={MAX_LENGTH.expirationDate}
-            aria-invalid
-          />
-        </>
-      </InputField>
-    ),
+    expirationDateState: { month: '12', year: '234' },
+    setExpirationDateState: { setMonth: fn(), setYear: fn() },
+    expirationDateErrorState: { isMonthError: false, isYearError: true },
+    showNextFieldOnLastElementBlur: fn(),
   },
 };
