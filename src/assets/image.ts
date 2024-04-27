f7af23088839b804e17f3ba81af781a25f9a952d
hwinkr <dnddl8280@naver.com>
2024-04-27 10:40:20 +0900

    choree: 이미지 파일 추가
    - 카드 제출 완료할 경우 체크 이미지
    - 404 페이지에서 사용할 카드 이미지

--- src/assets/image.ts
+++ src/assets/image.ts
@@ -1,3 +1,5 @@
-export { default as visaCard } from "./visaCard.png";
-export { default as masterCard } from "./masterCard.png";
+export { default as checkImage } from "./check.png";
 export { default as ellipse } from "./ellipse.png";
+export { default as errorCard } from "./error-card.png";
+export { default as masterCard } from "./master-card.png";
+export { default as visaCard } from "./visa-card.png";
