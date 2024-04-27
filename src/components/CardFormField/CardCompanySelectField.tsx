dafcd87adbb876bb1aaada36c730b431f30993cc
hwinkr <dnddl8280@naver.com>
2024-04-27 11:04:53 +0900

    feat(CardCompanySelectField): 카드사 선택 컴포넌트 구현
    - test: 카드사가 변경될 때마다 카드 프리뷰 컴포넌트 배경색이 변경되는지 테스트

---
+++ src/components/CardFormField/CardCompanySelectField.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import useCardAddFormField from "../../hooks/useCardAddFormField";
+
+import FormField from "../FormField";
+import Select from "../Select";
+
+import {
+  CARD_COMPANIES,
+  CARD_FORM_ATTRIBUTES,
+  CardCompany,
+  VALIDATION_MESSAGES,
+} from "../../constants/card-app";
+
+import { isArrayElement } from "../../types/type-guard";
+
+const CardCompanySelectField = () => {
+  const { isValidFormField, handleFormFieldFocus, dispatch } =
+    useCardAddFormField("cardCompany");
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.target;
+
+    if (!isArrayElement<CardCompany>(CARD_COMPANIES, value)) return;
+
+    dispatch({
+      type: "SET_CARD_COMPANY",
+      value: value,
+    });
+  };
+
+  const { query, caption } = CARD_FORM_ATTRIBUTES["cardCompany"];
+
+  return (
+    <FormField>
+      <FormField.InputTitle text={query} />
+      <FormField.InfoText textType="caption" text={caption} />
+      <FormField.InputContent>
+        <Select
+          autoFocus
+          onFocus={handleFormFieldFocus}
+          options={CARD_COMPANIES}
+          placeholder="카드사를 선택해 주세요"
+          onSelect={handleSelectChange}
+        />
+      </FormField.InputContent>
+      <FormField.InfoText
+        textType="error"
+        text={isValidFormField ? "" : VALIDATION_MESSAGES.invalidOwnerName}
+      />
+    </FormField>
+  );
+};
+
+export default CardCompanySelectField;
