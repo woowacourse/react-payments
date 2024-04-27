ee28ce82c1ff4856ec95a1b90b4a98e08ad1d50a
jinsil <jinsil378900@gmail.com>
2024-04-27 16:03:52 +0900

    feat: 카드 인풋 섹션 스토리북 작성

--- src/components/newCardPage/newCardInputSection/NewCardInputSection.stories.tsx
+++ src/components/newCardPage/newCardInputSection/NewCardInputSection.stories.tsx
@@ -1,4 +1,5 @@
 import Input from "../../default/input/Input";
+import Dropdown from "../../default/dropdown/Dropdown";
 import { Meta, StoryObj } from "@storybook/react";
 import NewCardInputSection, { NewCardInputSectionProps } from "./NewCardInputSection";
 
@@ -31,27 +32,60 @@ export const Default: Story = {
   },
 };
 
-export const CardNumbers: Story = {
+export const Password: Story = {
   args: {
+    mainText: "비밀번호를 입력해 주세요",
+    subText: "앞의 2자리를 입력해주세요",
     label: "카드 번호",
-    mainText: "결제할 카드 번호를 입력해 주세요",
-    subText: "본인 명의의 카드만 결제 가능합니다.",
-    errorMessage: ["Error message 1", "Error message 2", "Error message 3", "Error message 4"],
-    children: Array.from({ length: 4 }).map((_, index) => (
+    errorMessage: ["Error message 1"],
+    children: [
       <Input
-        key={index}
-        maxLength={4}
-        placeholder="1234"
-      />
-    )),
+        key="password"
+        type="password"
+        maxLength={2}
+        placeholder=""
+      />,
+    ],
+  },
+};
+
+export const CVC: Story = {
+  args: {
+    mainText: "CVC 번호를 입력해 주세요",
+    subText: "",
+    label: "CVC",
+    errorMessage: ["Error message 1"],
+    children: [
+      <Input
+        key="cvc"
+        maxLength={3}
+        placeholder="숫자 3자리"
+      />,
+    ],
+  },
+};
+
+export const UserName: Story = {
+  args: {
+    mainText: "카드 소유자 이름을 입력해 주세요",
+    subText: "",
+    label: "소유자 이름",
+    errorMessage: ["Error message 1"],
+    children: [
+      <Input
+        key="userName"
+        maxLength={2}
+        placeholder="John Doe"
+      />,
+    ],
   },
 };
 
 export const CardExpiration: Story = {
   args: {
-    label: "유효 기간",
     mainText: "카드 유효기간을 입력해 주세요",
     subText: "월/년도(MMYY)를 순서대로 입력해 주세요.",
+    label: "유효 기간",
     errorMessage: ["Error message 1", "Error message 2"],
     children: [
       <Input
@@ -68,18 +102,33 @@ export const CardExpiration: Story = {
   },
 };
 
-export const UserName: Story = {
+export const CardCompany: Story = {
   args: {
-    label: "소유자 이름",
-    mainText: "카드 소유자 이름을 입력해 주세요",
-    subText: "",
-    errorMessage: ["Error message 1"],
+    mainText: "카드사를 선택해 주세요",
+    subText: "현재 국내 카드사만 가능합니다.",
+    label: "",
+    errorMessage: [""],
     children: [
-      <Input
-        key="month"
-        maxLength={2}
-        placeholder="John Doe"
+      <Dropdown
+        selectList={["BC카드", "신한카드", "카카오뱅크", "현대카드", "우리카드", "롯데카드", "하나카드", "국민카드"]}
+        onChange={(selectedValue) => console.log(`카드사: ${selectedValue}`)}
       />,
     ],
+  },
+};
+
+export const CardNumbers: Story = {
+  args: {
+    mainText: "결제할 카드 번호를 입력해 주세요",
+    subText: "본인 명의의 카드만 결제 가능합니다.",
+    label: "카드 번호",
+    errorMessage: ["Error message 1", "Error message 2", "Error message 3", "Error message 4"],
+    children: Array.from({ length: 4 }).map((_, index) => (
+      <Input
+        key={index}
+        maxLength={4}
+        placeholder="1234"
+      />
+    )),
   },
 };
