f2bb6ce28fe013c7ff4e95f3190f4ec7eab9179e
hanyoung <phanyoung@naver.com>
2024-04-27 11:59:13 +0900

    chore(routePath): 기본 경로를 /으로 수정

--- src/pages/constants/routePath.ts
+++ src/pages/constants/routePath.ts
@@ -1,7 +1,7 @@
 const BASE_URL = process.env.NODE_ENV === 'production' ? '/dist/index.html' : '';
 
 const ROUTE_PATH = {
-  cardRegister: BASE_URL + '',
+  cardRegister: BASE_URL + '/',
   cardRegisterComplete: BASE_URL + '/card-register-complete',
 };
 
