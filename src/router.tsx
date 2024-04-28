3bc1a049c973d2031dc942ed0c7931d9e3191823
useon <yoosk4986@naver.com>
2024-04-28 18:47:25 +0900

    feat: 설정되지 않은 url 입력시 잘못된 접근인 것을 안내하고 홈으로 이동 버튼을 누르면 홈으로 이동하는 기능

--- src/router.tsx
+++ src/router.tsx
@@ -2,12 +2,14 @@ import { createBrowserRouter } from 'react-router-dom';
 import NewCardPage from './pages/newCard/NewCardPage';
 import CardRegistrationCompletePage from './pages/cardRegistrationComplete/CardRegistrationCompletePage';
 import App from './App';
+import NotFoundPage from './pages/notFound/NotFoundPage';
 
 const router = createBrowserRouter(
   [
     {
       path: '/',
       element: <App />,
+      errorElement: <NotFoundPage />,
       children: [
         {
           path: 'new-card',
