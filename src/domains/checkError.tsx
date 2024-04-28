55337012909d91a5a04ed3c9a9d43174ec29797e
Soyeon Choe <soy2302ten@gmail.com>
2024-04-29 00:13:16 +0900

    refactor: checkError 코드 간결하게 수정

--- src/domains/checkError.tsx
+++ src/domains/checkError.tsx
@@ -1,19 +1,9 @@
 export default function checkError(inputState: InputStates) {
-  let hasAnyError = false;
-  let hasAnyNotFilled = false;
-  Object.keys(inputState).map((key) => {
+  for (const key in inputState) {
     const state = inputState[key];
-    if (state.hasError) {
-      hasAnyError = true;
+    if (state.hasError || !state.isFilled) {
+      return false;
     }
-    if (!state.isFilled) {
-      hasAnyNotFilled = true;
-    }
-  });
-
-  if (hasAnyError || hasAnyNotFilled) {
-    return false;
   }
-
   return true;
 }
