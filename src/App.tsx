071d0fe184f69c88d54342779c4016aa10ec4769
00kang <kdb070970@gmail.com>
2024-04-28 20:42:41 +0900

    feat : react-router를 활용한 페이지 관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import Payments from "./pages/Payments";
-
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Payments from "./pages/Payments";
+import PaymentsSuccess from "./pages/PaymentSuccess";
 
 function App() {
-  return <Payments />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Payments />} />
+        <Route path="/success" element={<PaymentsSuccess cardNumber="5511" cardName="BC카드" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
