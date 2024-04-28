ac87a2d0981e9048f1f3489b989e616e4628abe1
hain-tain <hain7ce@gmail.com>
2024-04-28 19:04:14 +0900

    feat(CardHolder): 카드 소유자 input autoFocus 설정 및 onEnterHandler 설정 (엔터 눌러야 다음으로 넘어가도록)

--- src/components/CardHolder.tsx
+++ src/components/CardHolder.tsx
@@ -9,9 +9,21 @@ export interface CardHolderProps {
 }
 
 export default function CardHolder({ holderInput }: CardHolderProps) {
+  const onEnterHandler = (e: React.KeyboardEvent) => {
+    if (!holderInput.value) {
+      return;
+    }
+    if (e.key === "Enter" && holderInput.setIsComplete) {
+      holderInput.setIsComplete(true);
+    }
+  };
+
   return (
     <section>
-      <SectionTitle title={PAYMENTS_MESSAGE.cardHolderTitle} />
+      <SectionTitle
+        title={PAYMENTS_MESSAGE.cardHolderTitle}
+        description={"영문 이름을 입력한 뒤 Enter를 눌러주세요"}
+      />
       <FormItem
         labelText={PAYMENTS_INPUT_MESSAGE.cardHolderLabel}
         errorMessage={holderInput.errorMessage}
@@ -22,6 +34,8 @@ export default function CardHolder({ holderInput }: CardHolderProps) {
             placeholder={PAYMENTS_INPUT_MESSAGE.cardHolderPlaceHolder}
             maxLength={30}
             onChange={holderInput.onChangeHandler}
+            onFocus={holderInput.onFocusHandler}
+            onKeyDown={onEnterHandler}
             value={holderInput.value}
             autoFocus
           />
