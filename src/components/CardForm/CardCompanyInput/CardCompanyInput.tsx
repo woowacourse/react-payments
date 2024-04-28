16e49b32497a361c2d752a9aac78fb873f93e53b
anttiey <hanuuny@gmail.com>
2024-04-28 15:11:23 +0900

    refactor(SelectBox): SelectBox 공통 컴포넌트로 분리

--- src/components/CardForm/CardCompanyInput/CardCompanyInput.tsx
+++ src/components/CardForm/CardCompanyInput/CardCompanyInput.tsx
@@ -1,52 +1,23 @@
-import React, { useState } from 'react';
-
 import TitleContainer from '../../common/TitleContainer/TitleContainer';
+import SelectBox from '../../common/SelectBox/SelectBox';
 
 import type { InputType } from '../../../hooks/useInput';
 import { CARD_COMPANY } from '../../../constants/Condition';
-import { UpIcon, DownIcon } from '../../../assets';
-
-import * as S from './CardCompanyInput.style';
 
 interface CardCompanyInputProps {
   company: InputType;
 }
 
 const CardCompanyInput = ({ company }: CardCompanyInputProps) => {
-  const [isOptionOpen, setIsOptionOpen] = useState(false);
-
-  const toggleOptionList = () => setIsOptionOpen(!isOptionOpen);
-
-  const handleCompanyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    company.handleValue(e.target.value);
-    setIsOptionOpen(false);
-  };
-
   return (
     <div>
       <TitleContainer title="카드사를 선택해 주세요." subTitle="현재 국내 카드사만 가능합니다." />
-      <S.CardCompanyInputContainer>
-        <S.SelectedCardCompanyBox $isDefault={!company.value} onClick={toggleOptionList}>
-          <p>{company.value || '카드사를 선택해 주세요'}</p>
-          <img src={isOptionOpen ? UpIcon : DownIcon} alt={isOptionOpen ? '옵션 닫기' : '옵션 열기'} />
-        </S.SelectedCardCompanyBox>
-        {isOptionOpen && (
-          <S.CardCompanyOptionList>
-            {Object.keys(CARD_COMPANY).map((option) => (
-              <S.CardCompanyOption key={option}>
-                <input
-                  type="radio"
-                  id={option}
-                  value={option}
-                  checked={option === company.value}
-                  onChange={handleCompanyChange}
-                ></input>
-                <S.CardCompanyOptionLabel htmlFor={option}>{option}</S.CardCompanyOptionLabel>
-              </S.CardCompanyOption>
-            ))}
-          </S.CardCompanyOptionList>
-        )}
-      </S.CardCompanyInputContainer>
+      <SelectBox
+        options={Object.keys(CARD_COMPANY)}
+        selectedOption={company.value}
+        placeholder="카드사를 선택해 주세요"
+        updateOption={(option: string) => company.handleValue(option)}
+      />
     </div>
   );
 };
