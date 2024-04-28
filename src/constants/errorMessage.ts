013b9a0e2b319c203cb6cf4779f41daf29d66b60
vi-wolhwa <cerezo00@naver.com>
2024-04-28 20:50:39 +0900

    fix: 에러메시지 오타 수정

--- src/constants/errorMessage.ts
+++ src/constants/errorMessage.ts
@@ -3,8 +3,8 @@ const ERROR_MESSAGE = {
   invalidMonthInput: '월은 01에서 12 사이의 숫자만 입력할 수 있습니다.',
   invalidYearInput: '연도는 숫자만 입력할 수 있습니다.',
   invalidOwnerNameInput: '이름은 영문만 입력할 수 있습니다.',
-  invalidCVCNumberInput: 'CVC번호은 숫자만 입력할 수 있습니다.',
-  invalidPasswordPrefixInput: '비밀번호은 숫자만 입력할 수 있습니다.',
+  invalidCVCNumberInput: 'CVC번호는 숫자만 입력할 수 있습니다.',
+  invalidPasswordPrefixInput: '비밀번호는 숫자만 입력할 수 있습니다.',
 };
 
 export default ERROR_MESSAGE;
