963d75c63247f704a8defc934ba9dd9832e1fe92
00kang <kdb070970@gmail.com>
2024-04-28 20:38:46 +0900

    feat : 버튼 컴포넌트 구현

---
+++ src/components/Button.tsx
@@ -0,0 +1,36 @@
+import styled from "@emotion/styled";
+
+interface ButtonProps {
+  content: string;
+  onClick: () => void;
+  width?: string;
+}
+
+const Button = ({ content, onClick, width }: ButtonProps) => {
+  return (
+    <ButtonContainer onClick={onClick} width={width}>
+      {content}
+    </ButtonContainer>
+  );
+};
+
+export default Button;
+
+const ButtonContainer = styled.button<{ width?: string }>`
+  width: ${(props) => props.width || "100%"};
+  height: 52px;
+  bottom: 0px;
+  padding: 20px;
+  background: rgba(51, 51, 51, 1);
+  border: none;
+  border-radius: 5px;
+
+  text-align: center;
+  font-family: Noto Sans KR;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 12px;
+  color: rgba(243, 243, 243, 1);
+
+  cursor: pointer;
+`;
