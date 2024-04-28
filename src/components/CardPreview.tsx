f77050cbe10815cbb97dc69e4a639cbb6ca26b0c
hain-tain <hain7ce@gmail.com>
2024-04-28 19:28:54 +0900

    feat: CardPreview 뒷면 추가

--- src/components/CardPreview.tsx
+++ src/components/CardPreview.tsx
@@ -1,9 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import IC_CHIP from "../Images/Ic_chip.png";
+import { cardColorMatcher } from "../constants/cardIssuer";
 import { css } from "@emotion/react";
-import { matchCardTypeImgSrc } from "../domain/matchCardIssuer";
+import { matchCardTypeImgSrc } from "../domain/matchCardType";
 
-const styledCardPreview = css`
+const styledCardPreview = (color: string) => css`
   width: 212px;
   height: 132px;
   top: 77px;
@@ -14,7 +15,7 @@ const styledCardPreview = css`
   justify-content: center;
   gap: 10px;
   border-radius: 4px;
-  background-color: #333333;
+  background-color: ${color};
   box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.25);
 `;
 
@@ -46,11 +47,28 @@ const styledCardNumber = css`
   width:30px;
 `;
 
+const styledCvcNumberLine = {
+  width: "212px",
+  height: "24px",
+  margin: "0px 0px -68px -17px",
+  padding: "1px 17px",
+  color: "white",
+  backgroundColor: "#CBBA64",
+
+  display: "flex",
+  justifyContent: "end",
+  alignItems: "center",
+};
+
 interface CardPreviewProps {
   cardType: "" | "Visa" | "MasterCard";
   cardNumbers: [string, string, string, string];
   cardExpirationPeriod: [string, string];
   cardHolder: string;
+  cardIssuer: string;
+  cardCvcNumber: string;
+  isCardFrontView: boolean;
+  setIsCardFrontView: React.Dispatch<boolean>;
 }
 
 export default function CardPreview({
@@ -58,26 +76,42 @@ export default function CardPreview({
   cardNumbers,
   cardExpirationPeriod,
   cardHolder,
+  cardIssuer,
+  cardCvcNumber,
+  isCardFrontView,
+  setIsCardFrontView,
 }: CardPreviewProps) {
   return (
-    <section css={styledCardPreview}>
-      <div css={styledCardHeader}>
-        <img src={IC_CHIP} alt="" />
-        <img src={matchCardTypeImgSrc(cardType)} alt={cardType} />
-      </div>
-      <div css={styledCardNumberContainer}>
-        {cardNumbers.map((number, idx) => (
-          <span key={idx} css={styledCardNumber}>
-            {idx < 2 ? number : "*".repeat(number.length)}
-          </span>
-        ))}
-      </div>
-      <div css={styledCardText}>
-        {cardExpirationPeriod.join("") !== ""
-          ? cardExpirationPeriod.join("/")
-          : ""}
-      </div>
-      <div css={styledCardText}>{cardHolder}</div>
-    </section>
+    <div
+      onClick={() => {
+        setIsCardFrontView(!isCardFrontView);
+      }}
+    >
+      {isCardFrontView ? (
+        <section css={styledCardPreview(cardColorMatcher(cardIssuer))}>
+          <div css={styledCardHeader}>
+            <img src={IC_CHIP} alt="" />
+            <img src={matchCardTypeImgSrc(cardType)} alt={cardType} />
+          </div>
+          <div css={styledCardNumberContainer}>
+            {cardNumbers.map((number, idx) => (
+              <span key={idx} css={styledCardNumber}>
+                {idx < 2 ? number : "*".repeat(number.length)}
+              </span>
+            ))}
+          </div>
+          <div css={styledCardText}>
+            {cardExpirationPeriod.join("") !== ""
+              ? cardExpirationPeriod.join("/")
+              : ""}
+          </div>
+          <div css={styledCardText}>{cardHolder}</div>
+        </section>
+      ) : (
+        <section css={styledCardPreview("#D5D5D5")}>
+          <div css={styledCvcNumberLine}>{cardCvcNumber}</div>
+        </section>
+      )}
+    </div>
   );
 }
