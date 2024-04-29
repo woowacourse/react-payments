27ff5f67b8cb3cd349ecb5edffaeb50525e846a3
00kang <kdb070970@gmail.com>
2024-04-29 16:18:41 +0900

    feat : 유효성 검사 로직 수정
    - 숫자인지 체크하는 checkIsDigit 메서드

--- src/utils/Validator.ts
+++ src/utils/Validator.ts
@@ -7,11 +7,11 @@ const ValidatorCondition = {
   },
 
   checkIsDigit(value: string) {
-    return /^\d* ?\d*$/.test(value);
+    return /^\d+$/.test(value);
   },
 
   checkIsEnglish(value: string) {
-    return /^[a-zA-Z\s]*$/.test(value);
+    return /^[a-zA-Z\s]+$/.test(value);
   },
 
   checkIsBelowNumber(value: string, limit: number) {
@@ -21,30 +21,42 @@ const ValidatorCondition = {
   checkIsNotDoubleZero(value: string) {
     return value !== SIGN.doubleZero;
   },
+
+  checkIsFourDigit(value: string, digit: number) {
+    return value.length === digit;
+  },
 };
 
 const Validator = {
   checkCreditCardNumber(value: string) {
     const maxDigit = 4;
 
-    if (ValidatorCondition.checkMaxDigit(value, maxDigit))
-      return { isError: false, isValid: false };
-
     if (!ValidatorCondition.checkIsDigit(value)) {
       return { isError: true, isValid: false };
     }
+    if (ValidatorCondition.checkMaxDigit(value, maxDigit))
+      return { isError: false, isValid: false };
 
-    return { isError: false, isValid: true };
+    return { isError: false, isValid: value.length === maxDigit };
+  },
+
+  checkCreditCardCompany(value: string) {
+    if (value !== "") return { isError: false, isValid: true };
+
+    return { isError: true, isValid: false };
   },
 
   checkCreditCardExpirationPeriod(value: string, name: string) {
     const maxDigit = 2;
     const limitValue = 12;
 
-    if (ValidatorCondition.checkMaxDigit(value, maxDigit))
-      return { isError: false, isValid: false };
+    if (!ValidatorCondition.checkIsDigit(value)) {
+      return { isError: true, isValid: false };
+    }
 
-    if (!ValidatorCondition.checkIsDigit(value)) return { isError: true, isValid: false };
+    if (ValidatorCondition.checkMaxDigit(value, maxDigit)) {
+      return { isError: false, isValid: false };
+    }
 
     const isValidMonth =
       name === CARD_INPUTBOX_NAME.expirationPeriod.month
@@ -52,12 +64,16 @@ const Validator = {
           ValidatorCondition.checkIsNotDoubleZero(value)
         : true;
 
-    if (!isValidMonth) return { isError: true, isValid: false };
+    if (!isValidMonth) {
+      return { isError: true, isValid: false };
+    }
 
     return { isError: false, isValid: true };
   },
 
   checkCreditCardOwner(value: string) {
+    if (value.length === 0) return { isError: true, isValid: false };
+
     if (ValidatorCondition.checkIsEnglish(value)) return { isError: false, isValid: true };
 
     return { isError: true, isValid: false };
