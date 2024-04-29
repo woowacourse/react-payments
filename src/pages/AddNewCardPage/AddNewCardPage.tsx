2db624aa7841afe62660a4149046244d129dac06
pakxe <pigkill40@naver.com>
2024-04-30 01:01:37 +0900

    feat: useAddNewCardForm 삭제

--- src/pages/AddNewCardPage/AddNewCardPage.tsx
+++ src/pages/AddNewCardPage/AddNewCardPage.tsx
@@ -9,17 +9,55 @@ import CardPasswordInputField from '../../components/Field/CardPasswordInputFiel
 import ShelfHeader from '../../components/ShelfHeader/ShelfHeader';
 import ShelfSection from '../../components/ShelfSection/ShelfSection';
 import BottomButton from '../../components/common/BottomButton/BottomButton';
-import useAddNewCardForm from '../../hooks/useAddNewCardForm';
 import Form from '../../components/common/Form/Form';
+import useCardNumbers from '../../hooks/useCardNumbers';
+import useExpirationDate from '../../hooks/useExpirationDate';
+import useOwnerName from '../../hooks/useOwnerName';
+import useNumberInput from '../../hooks/useNumberInput';
+import useDropdown from '../../hooks/useDropdown';
+import { CardCompany } from '../../types/cardCompany';
+import useShowCase from '../../hooks/useShowCase';
 
 const AddNewCardPage = () => {
-  const { values, errorMessage, handlers, showCase, completeList } = useAddNewCardForm();
+  const cardNumbers = useCardNumbers();
+  const expirationDate = useExpirationDate();
+  const ownerName = useOwnerName();
+  const {
+    value: CVCNumbers,
+    setValue: setCVCNumbers,
+    errorMessage: CVCNumbersErrorMessage,
+    isValid: isValidCVCNumbers,
+  } = useNumberInput(3);
+  const {
+    value: password,
+    setValue: setPassword,
+    errorMessage: passwordErrorMessage,
+    isValid: isValidPassword,
+  } = useNumberInput(2);
+  const {
+    selected: cardCompany,
+    handleSelect: handleSelectCardCompany,
+    errorMessage: cardCompanyErrorMessage,
+  } = useDropdown<CardCompany>();
+
+  const isValidList = {
+    cardNumbers: cardNumbers.isValid,
+    cardCompany: cardCompany !== null,
+    expirationDate: expirationDate.isValid,
+    ownerName: ownerName.isValid,
+    CVCNumbers: isValidCVCNumbers,
+    password: isValidPassword,
+  };
+
+  const showOrder = ['cardNumbers', 'cardCompany', 'expirationDate', 'ownerName', 'CVCNumbers', 'password'];
+
+  const { showCase } = useShowCase(showOrder, isValidList);
   const navigate = useNavigate();
   const handleSubmit = () => {
     navigate(`/complete-add-new-card`, {
       state: {
-        firstCardNumberUnit: values.cardNumbers[0],
-        cardCompany: values.cardCompany,
+        firstCardNumberUnit: cardNumbers.value[0],
+        cardCompany: cardCompany,
       },
     });
   };
@@ -27,19 +65,19 @@ const AddNewCardPage = () => {
   return (
     <Form>
       <CardPreview
-        cardNumbers={values.cardNumbers}
-        expirationDate={values.expirationDate}
-        ownerName={values.ownerName}
-        cardCompany={values.cardCompany}
-        CVCNumbers={values.CVCNumbers}
+        cardNumbers={cardNumbers.value}
+        expirationDate={expirationDate.value}
+        ownerName={ownerName.value}
+        cardCompany={cardCompany}
+        CVCNumbers={CVCNumbers}
       />
       {showCase.password && (
         <ShelfSection>
           <ShelfHeader title='비밀번호를 입력해 주세요' description='앞의 2자리를 입력해주세요' />
           <CardPasswordInputField
-            password={values.password}
-            handlePassword={handlers.password}
-            errorMessage={errorMessage.password}
+            password={password}
+            handlePassword={setPassword}
+            errorMessage={passwordErrorMessage}
           />
         </ShelfSection>
       )}
@@ -47,9 +85,9 @@ const AddNewCardPage = () => {
         <ShelfSection>
           <ShelfHeader title='CVC 번호를 입력해 주세요' />
           <CardCVCNumberInputField
-            CVCNumber={values.CVCNumbers}
-            handleCVCNumber={handlers.CVCNumbers}
-            errorMessage={errorMessage.CVCNumbers}
+            CVCNumber={CVCNumbers}
+            handleCVCNumber={setCVCNumbers}
+            errorMessage={CVCNumbersErrorMessage}
           />
         </ShelfSection>
       )}
@@ -57,9 +95,9 @@ const AddNewCardPage = () => {
         <ShelfSection>
           <ShelfHeader title='카드 소유자 이름을 입력해 주세요' description='본인 명의의 카드만 결제 가능합니다' />
           <CardOwnerNameInputField
-            ownerName={values.ownerName}
-            handleOwnerName={handlers.ownerName}
-            errorMessage={errorMessage.ownerName}
+            ownerName={ownerName.value}
+            handleOwnerName={ownerName.handleOwnerNameChange}
+            errorMessage={ownerName.errorMessage}
           />
         </ShelfSection>
       )}
@@ -67,9 +105,9 @@ const AddNewCardPage = () => {
         <ShelfSection>
           <ShelfHeader title='카드 유효기간을 입력해 주세요' description='월/년도(MMYY)를 순서대로 입력해 주세요' />
           <CardExpirationDateInputField
-            expirationDate={values.expirationDate}
-            errorMessages={errorMessage.expirationDate}
-            handleExpirationDateChange={handlers.expirationDate}
+            expirationDate={expirationDate.value}
+            errorMessages={expirationDate.errorMessages}
+            handleExpirationDateChange={expirationDate.handleExpirationDateChange}
           />
         </ShelfSection>
       )}
@@ -77,9 +115,9 @@ const AddNewCardPage = () => {
         <ShelfSection>
           <ShelfHeader title='카드사를 선택해 주세요' description='현재 국내 카드사만 가능합니다' />
           <CardCompanySelectField
-            cardCompany={values.cardCompany}
-            handleSelectCardCompany={handlers.cardCompany}
-            errorMessage={errorMessage.cardCompany}
+            cardCompany={cardCompany}
+            handleSelectCardCompany={handleSelectCardCompany}
+            errorMessage={cardCompanyErrorMessage}
           />
         </ShelfSection>
       )}
@@ -87,14 +125,14 @@ const AddNewCardPage = () => {
         <ShelfSection>
           <ShelfHeader title='결제할 카드 번호를 입력해 주세요' description='본인 명의의 카드만 결제 가능합니다' />
           <CardNumbersInputField
-            cardNumbers={values.cardNumbers}
-            handleCardNumbers={handlers.cardNumbers}
-            errorMessages={errorMessage.cardNumbers}
+            cardNumbers={cardNumbers.value}
+            handleCardNumbers={cardNumbers.handleCardNumbersChange}
+            errorMessages={cardNumbers.errorMessages}
           />
         </ShelfSection>
       )}
       <>
-        {Object.values(completeList).every((isValid) => isValid === true) && (
+        {Object.values(isValidList).every((value) => value === true) && (
           <BottomButton name='확인' onClick={handleSubmit} />
         )}
       </>
