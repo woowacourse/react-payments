d9b1865b0455947a045b09efaaea1df5824f673d
00kang <kdb070970@gmail.com>
2024-04-29 11:56:28 +0900

    feat : router path 상수화

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Payments from "./pages/Payments";
+import { routePath } from "./constants/routePath";
 import PaymentsSuccess from "./pages/PaymentSuccess";
+import Payments from "./pages/Payments";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Payments />} />
-        <Route path="/success" element={<PaymentsSuccess cardNumber="5511" cardName="BC카드" />} />
+        <Route path={routePath.home} element={<Payments />} />
+        <Route
+          path={routePath.success}
+          element={<PaymentsSuccess cardNumber="5511" cardName="BC카드" />}
+        />
       </Routes>
     </BrowserRouter>
   );
