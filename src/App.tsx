e0fe72696c5be367516ff60b9f1c597903cb44e1
river <jenn0.6n@gmail.com>
2024-04-29 16:05:40 +0900

    Deploy: 배포

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,19 @@
 import { mainStyle } from "./App.styles";
 import CardEnrollmentCompleteView from "./components/CardEnrollmentCompleteView/CardEnrollmentCompleteView";
 import CardForm from "./components/CardForm/CardForm";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <main css={mainStyle}>
-        <Routes>
-          <Route path="/" element={<CardForm />} />
-          <Route
-            path="/enrollmentCompleted"
-            element={<CardEnrollmentCompleteView />}
-          />
-        </Routes>
-      </main>
-    </Router>
+    <main css={mainStyle}>
+      <Routes>
+        <Route path="/" element={<CardForm />} />
+        <Route
+          path="/enrollmentCompleted"
+          element={<CardEnrollmentCompleteView />}
+        />
+      </Routes>
+    </main>
   );
 }
 
