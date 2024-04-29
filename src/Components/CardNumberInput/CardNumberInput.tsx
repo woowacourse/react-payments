17be98af613510881a4f060d853fec931c0dc7c8
river <jenn0.6n@gmail.com>
2024-04-29 09:20:53 +0900

    Chore: 주석 삭제

--- src/Components/CardNumberInput/CardNumberInput.tsx
+++ src/Components/CardNumberInput/CardNumberInput.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import Input from "../common/Input/Input";
 import { CardNumberInputWrapper, Dd, Tooltip } from "./CardNumberInput.styles";
-import useInput from "../../hooks/useInput";
 
 interface CardNumberInputProps {
   value: string;
@@ -79,71 +78,3 @@ const CardNumberInput: React.FC<CardNumberInputProps> = ({
 };
 
 export default CardNumberInput;
-
-// ********************************************* 리팩토링
-// const CardNumberInput: React.FC<CardNumberInputProps> = ({
-//   value,
-//   onChange,
-//   setCompleted,
-// }) => {
-//   const inputRefs = useRef<Array<HTMLInputElement | null>>([]);
-
-//   const validator = (value: string) => {
-//     if (!/^\d*$/.test(value)) {
-//       return "카드 번호는 숫자만 입력 가능합니다.";
-//     }
-
-//     if (value.length !== 4) {
-//       return "카드 번호는 4자리여야 합니다.";
-//     }
-
-//     return "";
-//   };
-
-//   const inputs = Array.from({ length: 4 }, (_, index) => {
-//     const { value, isValid, errorMessage, handleChange } = useInput({
-//       validator: (value: string) => validator(value) === "",
-//       onValidate: (isValid: boolean) => setCompleted(isValid),
-//     });
-
-//     return {
-//       value,
-//       isValid,
-//       errorMessage,
-//       handleChange: (inputValue: string) => {
-//         handleChange(inputValue);
-//         onChange(inputValue);
-
-//         if (inputValue.length === 4 && index < inputRefs.current.length - 1) {
-//           inputRefs.current[index + 1]?.focus();
-//         }
-//       },
-//     };
-//   });
-
-//   return (
-//     <CardNumberInputWrapper>
-//       <Dd>
-//         {inputs.map((input, index) => (
-//           <div key={index}>
-//             <Input
-//               ref={(el) => (inputRefs.current[index] = el)}
-//               value={input.value}
-//               onChange={input.handleChange}
-//               onValidate={() => {}}
-//               maxLength={4}
-//               size="small"
-//             />
-//           </div>
-//         ))}
-//       </Dd>
-
-//       <Tooltip>
-//         {inputs.some((input) => !input.isValid) &&
-//           inputs.find((input) => !input.isValid)?.errorMessage}
-//       </Tooltip>
-//     </CardNumberInputWrapper>
-//   );
-// };
-
-// export default CardNumberInput;
