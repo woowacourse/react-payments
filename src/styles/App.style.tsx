72b9b03923aad48dc3e0186102201b688d4aa9fe
김진호 <rlawlsgh1227@gmail.com>
2024-04-29 22:17:26 +0900

    design: 상하단 여백 제거

--- src/styles/App.style.tsx
+++ src/styles/App.style.tsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  position: relative;
   width: 37.6rem;
   min-height: 70rem;
-  margin: 5rem auto;
 
   background-color: ${(props) => props.theme.color.white};
   border: 0.1rem solid ${(props) => props.theme.color.gray};
