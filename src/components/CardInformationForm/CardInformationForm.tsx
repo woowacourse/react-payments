947a5f4b08eb8514e32ceebe2d69a66c0b93b66f
simhorim <hori5020@gmail.com>
2024-04-29 17:55:29 +0900

    feat(useCardInformationAppearedCondition): 커스텀 훅 구현을 통한 form에 있는 로직 분리

--- src/components/CardInformationForm/CardInformationForm.tsx
+++ src/components/CardInformationForm/CardInformationForm.tsx
@@ -5,9 +5,7 @@ import { UserNameStateType } from '../../hooks/useUserName';
 import { SelectedCardStateType } from '../../hooks/useSelectedCardState';
 import { CVCStateType } from '../../hooks/useCVC';
 import { PasswordStateType } from '../../hooks/usePassword';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import CONDITION from '../../constants/Condition';
 import ConfirmationButton from '../ConfirmationButton/ConfirmationButton';
 import CardNumber from '../FormField/CardNumber/CardNumber';
 import ExpirationDate from '../FormField/ExpirationDate/ExpirationDate';
@@ -15,8 +13,7 @@ import UserName from '../FormField/UserName/UserName';
 import CVC from '../FormField/CVC/CVC';
 import Password from '../FormField/Password/Password';
 import SelectedCard from '../FormField/SelectedCard/SelectedCard';
-
-const { CARD_INFORMATION_APPEARED } = CONDITION;
+import useCardInformationAppearedCondition from '../../hooks/useCardInformationAppearedCondition';
 
 interface CardInformationFormProps {
   cardNumberState: CardNumberStateType;
@@ -27,45 +24,36 @@ interface CardInformationFormProps {
   passwordState: PasswordStateType;
 }
 
+type AppearedConditionType =
+  | '카드번호'
+  | '카드선택'
+  | '유효기간'
+  | '사용자이름'
+  | 'cvc'
+  | '비밀번호';
+
 const CardInformationForm = ({
   cardNumberState,
-  expirationDateState,
   selectedCardState,
+  expirationDateState,
   userNameState,
   cvcState,
   passwordState,
 }: CardInformationFormProps) => {
   const navigate = useNavigate();
 
-  const [appearedCondition, setAppearedCondition] = useState<number>(
-    CARD_INFORMATION_APPEARED.cardNumber,
-  );
-
-  const isCardSelectedAppearedCondition =
-    appearedCondition >= CARD_INFORMATION_APPEARED.selectedCard;
-  const isExpirationDateAppearedCondition =
-    appearedCondition >= CARD_INFORMATION_APPEARED.expirationDate;
-  const isUserNameAppearedCondition = appearedCondition >= CARD_INFORMATION_APPEARED.userName;
-  const isCVCAppearedCondition = appearedCondition >= CARD_INFORMATION_APPEARED.cvc;
-  const isPasswordAppearedCondition = appearedCondition >= CARD_INFORMATION_APPEARED.password;
-  const isSubmitButtonAppearedCondition =
-    cardNumberState.isValid &&
-    selectedCardState.value &&
-    expirationDateState.isValid &&
-    userNameState.isValid &&
-    cvcState.isValid &&
-    passwordState.isValid;
-
-  if (cardNumberState.isValid && !isCardSelectedAppearedCondition)
-    setAppearedCondition(CARD_INFORMATION_APPEARED.selectedCard);
-  if (selectedCardState.value && !isExpirationDateAppearedCondition)
-    setAppearedCondition(CARD_INFORMATION_APPEARED.expirationDate);
-  if (expirationDateState.isValid && !isUserNameAppearedCondition)
-    setAppearedCondition(CARD_INFORMATION_APPEARED.userName);
-  if (userNameState.isValid && !isCVCAppearedCondition)
-    setAppearedCondition(CARD_INFORMATION_APPEARED.cvc);
-  if (cvcState.isValid && !isPasswordAppearedCondition)
-    setAppearedCondition(CARD_INFORMATION_APPEARED.password);
+  const cardInformationValid = {
+    isCardNumberValid: cardNumberState.isValid,
+    isCardSelected: selectedCardState.value,
+    isExpirationDateValid: expirationDateState.isValid,
+    isUserNameValid: userNameState.isValid,
+    isCVCValid: cvcState.isValid,
+    isPasswordValid: passwordState.isValid,
+  };
+  const { cardInformationAppeardConditionState } = useCardInformationAppearedCondition({
+    defaultValue: '카드번호',
+    cardInformationValid,
+  });
 
   const handleConfirmationButtonOnClick = () => {
     navigate('/registration', {
@@ -78,16 +66,22 @@ const CardInformationForm = ({
 
   return (
     <Styled.CardInformationForm>
-      {isSubmitButtonAppearedCondition && (
+      {cardInformationAppeardConditionState.isSubmitButtonAppearedCondition && (
         <ConfirmationButton isSubmit={true} onClick={handleConfirmationButtonOnClick} />
       )}
-      {isPasswordAppearedCondition && <Password passwordState={passwordState} />}
-      {isCVCAppearedCondition && <CVC cvcState={cvcState} />}
-      {isUserNameAppearedCondition && <UserName userNameState={userNameState} />}
-      {isExpirationDateAppearedCondition && (
+      {cardInformationAppeardConditionState.isPasswordAppearedCondition && (
+        <Password passwordState={passwordState} />
+      )}
+      {cardInformationAppeardConditionState.isCVCAppearedCondition && <CVC cvcState={cvcState} />}
+      {cardInformationAppeardConditionState.isUserNameAppearedCondition && (
+        <UserName userNameState={userNameState} />
+      )}
+      {cardInformationAppeardConditionState.isExpirationDateAppearedCondition && (
         <ExpirationDate expirationDateState={expirationDateState} />
       )}
-      {isCardSelectedAppearedCondition && <SelectedCard selectedCardState={selectedCardState} />}
+      {cardInformationAppeardConditionState.isCardSelectedAppearedCondition && (
+        <SelectedCard selectedCardState={selectedCardState} />
+      )}
       <CardNumber cardNumberState={cardNumberState} />
     </Styled.CardInformationForm>
   );
