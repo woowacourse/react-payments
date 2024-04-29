8257fb9775aa83ff9c5f365ec11094c060c9edc7
00kang <kdb070970@gmail.com>
2024-04-29 09:34:31 +0900

    feat : 버튼 스타일 속성 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -4,11 +4,14 @@ interface ButtonProps {
   content: string;
   onClick: () => void;
   width?: string;
+  disabled?: boolean;
+  position?: string;
+  bottom?: string;
 }
 
-const Button = ({ content, onClick, width }: ButtonProps) => {
+const Button = ({ content, onClick, width, disabled }: ButtonProps) => {
   return (
-    <ButtonContainer onClick={onClick} width={width}>
+    <ButtonContainer onClick={onClick} width={width} disabled={disabled}>
       {content}
     </ButtonContainer>
   );
@@ -16,10 +19,15 @@ const Button = ({ content, onClick, width }: ButtonProps) => {
 
 export default Button;
 
-const ButtonContainer = styled.button<{ width?: string }>`
+const ButtonContainer = styled.button<{
+  width?: string;
+  position?: string;
+  bottom?: string;
+}>`
   width: ${(props) => props.width || "100%"};
   height: 52px;
-  bottom: 0px;
+  position: ${(props) => props.position || "static"};
+  bottom: ${(props) => props.bottom || "auto"};
   padding: 20px;
   background: rgba(51, 51, 51, 1);
   border: none;
