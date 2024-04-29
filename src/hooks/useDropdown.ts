49cae50e919be6e90eb6768ae5daf887738f09bb
pakxe <pigkill40@naver.com>
2024-04-29 17:34:44 +0900

    chore: 불필요한 주석 제거

--- src/hooks/useDropdown.ts
+++ src/hooks/useDropdown.ts
@@ -1,12 +1,10 @@
 import { useState } from 'react';
 import { ERROR_MESSAGES } from '../constants/errorMessages';
 
-// 커스텀 드롭다운을 위한 훅
 const useDropdown = <T>() => {
   const [selected, setSelected] = useState<T | null>(null);
   const [errorMessage, setErrorMessages] = useState<string>('');
 
-  // 드롭다운을 선택하지 않으면 오류가 난다.
   const handleSelect = (selected: T | null) => {
     if (selected === null) {
       setErrorMessages(ERROR_MESSAGES.NO_SELECT);
