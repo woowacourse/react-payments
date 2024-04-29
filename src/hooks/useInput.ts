c61a514a325746cbaf12728fa9bfc02eebe66af2
김진호 <rlawlsgh1227@gmail.com>
2024-04-29 20:39:47 +0900

    refactor: sanitize 오타 수정

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -4,26 +4,26 @@ const useInput = <T>(
   defaultValue: T,
   regExp: { valid: RegExp; invalid: RegExp },
   condition: (value: string) => boolean,
-  santizeValue: (value: string) => string = (value) => value,
+  sanitizeValue: (value: string) => string = (value) => value,
 ) => {
   const [value, setValue] = useState<T>(defaultValue);
   const [isError, setIsError] = useState<boolean>(false);
 
   const preventInvalidInput = (
     event: React.ChangeEvent<HTMLInputElement>,
-    santizeValue: (value: string) => string,
+    sanitizeValue: (value: string) => string,
   ) => {
     const newValue = event.currentTarget.value;
 
     if (regExp.valid.test(newValue)) {
-      setValue(santizeValue(newValue) as T);
+      setValue(sanitizeValue(newValue) as T);
     } else {
       setValue(newValue.replace(regExp.invalid, '') as T);
     }
   };
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    preventInvalidInput(event, () => santizeValue(event.currentTarget.value));
+    preventInvalidInput(event, () => sanitizeValue(event.currentTarget.value));
     const isError =
       !regExp.valid.test(event.currentTarget.value) || !condition(event.currentTarget.value);
 
