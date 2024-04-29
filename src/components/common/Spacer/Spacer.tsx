c09c7d6c0d990ef9224b1d86f742dc4ae38a10b6
vi-wolhwa <cerezo00@naver.com>
2024-04-29 11:27:26 +0900

    remove: 미사용 파일 삭제

--- src/components/common/Spacer/Spacer.tsx
+++
@@ -1,9 +0,0 @@
-interface SpacerProps {
-  space: number;
-}
-
-const Spacer: React.FC<SpacerProps> = ({ space }) => {
-  return <div style={{ width: '1px', marginBottom: `${space}px` }} />;
-};
-
-export default Spacer;
