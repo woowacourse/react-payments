676edc052c77bbc2bd583b4f85b0fb70a22f5fb9
hain-tain <hain7ce@gmail.com>
2024-04-29 19:42:06 +0900

    refactor: cardColorMatcher 함수CardPreview 파일로 이동

--- src/components/CardPreview.tsx
+++ src/components/CardPreview.tsx
@@ -1,64 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import IC_CHIP from "../Images/Ic_chip.png";
-import { cardColorMatcher } from "../constants/cardIssuer";
 import { css } from "@emotion/react";
 import { matchCardTypeImgSrc } from "../domain/matchCardType";
-
-const styledCardPreview = (color: string) => css`
-  width: 212px;
-  height: 132px;
-  top: 77px;
-  left: 82px;
-  padding: 10px 17px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 10px;
-  border-radius: 4px;
-  background-color: ${color};
-  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.25);
-`;
-
-const styledCardHeader = css`
-  height: 22px;
-  display: flex;
-  justify-content: space-between;
-
-  img {
-    width: 32px;
-  }
-`;
-
-const styledCardText = css`
-  color: #ffffff;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 20px;
-  height: 20px;
-`;
-
-const styledCardNumberContainer = css`
-  display: flex;
-  gap: 10px;
-`;
-
-const styledCardNumber = css`
-  ${styledCardText.styles}
-  width:30px;
-`;
-
-const styledCvcNumberLine = {
-  width: "212px",
-  height: "24px",
-  margin: "0px 0px -68px -17px",
-  padding: "1px 17px",
-  color: "white",
-  backgroundColor: "#CBBA64",
-
-  display: "flex",
-  justifyContent: "end",
-  alignItems: "center",
-};
 
 interface CardPreviewProps {
   cardType: "" | "Visa" | "MasterCard";
@@ -115,3 +58,82 @@ export default function CardPreview({
     </div>
   );
 }
+
+function cardColorMatcher(issuer: string) {
+  switch (issuer) {
+    case "BC카드":
+      return "#F04651";
+    case "신한카드":
+      return "#0046FF";
+    case "카카오뱅크":
+      return "#FFE600";
+    case "현대카드":
+      return "#000000";
+    case "우리카드":
+      return "#007BC8";
+    case "롯데카드":
+      return "#ED1C24";
+    case "하나카드":
+      return "#009490";
+    case "국민카드":
+      return "#6A6056";
+    default:
+      return "#333333";
+  }
+}
+
+const styledCardPreview = (color: string) => css`
+  width: 212px;
+  height: 132px;
+  top: 77px;
+  left: 82px;
+  padding: 10px 17px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 10px;
+  border-radius: 4px;
+  background-color: ${color};
+  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.25);
+`;
+
+const styledCardHeader = css`
+  height: 22px;
+  display: flex;
+  justify-content: space-between;
+
+  img {
+    width: 32px;
+  }
+`;
+
+const styledCardText = css`
+  color: #ffffff;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+  height: 20px;
+`;
+
+const styledCardNumberContainer = css`
+  display: flex;
+  gap: 10px;
+`;
+
+const styledCardNumber = css`
+  ${styledCardText.styles}
+  width:30px;
+`;
+
+const styledCvcNumberLine = {
+  width: "212px",
+  height: "24px",
+  margin: "0px 0px -68px -17px",
+  padding: "1px 17px",
+  color: "white",
+  backgroundColor: "#CBBA64",
+
+  display: "flex",
+  justifyContent: "end",
+  alignItems: "center",
+};
--- src/constants/cardIssuer.ts
+++ src/constants/cardIssuer.ts
@@ -8,26 +8,3 @@ export const CARD_ISSUER = [
   "하나카드",
   "국민카드",
 ];
-
-export const cardColorMatcher = (issuer: string) => {
-  switch (issuer) {
-    case "BC카드":
-      return "#F04651";
-    case "신한카드":
-      return "#0046FF";
-    case "카카오뱅크":
-      return "#FFE600";
-    case "현대카드":
-      return "#000000";
-    case "우리카드":
-      return "#007BC8";
-    case "롯데카드":
-      return "#ED1C24";
-    case "하나카드":
-      return "#009490";
-    case "국민카드":
-      return "#6A6056";
-    default:
-      return "#333333";
-  }
-};
