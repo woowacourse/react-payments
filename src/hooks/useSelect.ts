8c4e79d384ba1b5400abbcd66af1b1ee582a613b
김진호 <rlawlsgh1227@gmail.com>
2024-04-30 13:56:34 +0900

    refactor: select onChange 함수를 제공하는 것으로 변경

--- src/hooks/useSelect.ts
+++ src/hooks/useSelect.ts
@@ -2,13 +2,18 @@ import { useState } from 'react';
 
 const useSelect = (defaultValue: string | null) => {
   const [value, setValue] = useState<string | null>(defaultValue);
+
+  const onChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setValue(event.target.value);
+  };
+
   const clear = () => {
     setValue(null);
   };
 
   return {
     value,
-    onChange: setValue,
+    onChange,
     clear,
   };
 };
