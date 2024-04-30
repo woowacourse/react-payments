c8d1efcf5a8cb4d68c1fa7f212bd4e22ddb46238
hwinkr <dnddl8280@naver.com>
2024-04-30 14:11:22 +0900

    chore: 유효성 검증에 의미있는 상수 설정

--- src/validators/cardAddFormValidator.ts
+++ src/validators/cardAddFormValidator.ts
@@ -1,52 +1,28 @@
 import cardInputValidator from "./cardInputValidator";
 
 import {
-  CVCNumberKeys,
-  CardCompanyKeys,
   CardNumberKeys,
   CardOwnerKeys,
-  CardPasswordKeys,
   ExpirationDateKeys,
 } from "../types/card";
+import { CardCompany, INPUT_RULES } from "../constants/card-app";
 
-type CardNumbers = {
-  [key in CardNumberKeys]: string;
-};
-
-type CardCompany = {
-  [key in CardCompanyKeys]: string;
-};
-
-type ExpirationDate = {
-  [key in ExpirationDateKeys]: string;
-};
-
-type CardOwner = {
-  [key in CardOwnerKeys]: string;
-};
-
-type CVCNumber = {
-  [key in CVCNumberKeys]: string;
-};
-
-type CardPassword = {
-  [key in CardPasswordKeys]: string;
-};
-
-export const validateCardNumbersInputCompleted = (cardNumbers: CardNumbers) => {
-  return Object.values(cardNumbers).every(
-    (cardNumber) => cardNumber.length === 4
+export const validateCardNumbersInputCompleted = (
+  cardNumbers: Record<CardNumberKeys, string>
+) => {
+  return Object.values(cardNumbers).every((cardNumber) =>
+    cardInputValidator.validateCardNumberLength(cardNumber)
   );
 };
 
-export const validateCardCompanySelectCompleted = ({
-  cardCompany,
-}: CardCompany) => {
+export const validateCardCompanySelectCompleted = (
+  cardCompany: CardCompany
+) => {
   return cardCompany !== "기본카드";
 };
 
 export const validateExpirationDateInputCompleted = (
-  expirationDate: ExpirationDate
+  expirationDate: Record<ExpirationDateKeys, string>
 ) => {
   const { mm, yy } = expirationDate;
 
@@ -65,16 +41,16 @@ export const validateExpirationDateInputCompleted = (
   return true;
 };
 
-export const validateCardOwnerInputCompleted = ({ ownerName }: CardOwner) => {
-  return ownerName.length >= 1;
+export const validateCardOwnerInputCompleted = ({
+  ownerName,
+}: Record<CardOwnerKeys, string>) => {
+  return ownerName.length >= INPUT_RULES.minCardOwnerNameLength;
 };
 
-export const validateCVCNumberInputCompleted = ({ cvcNumber }: CVCNumber) => {
-  return cvcNumber.length === 3;
+export const validateCVCNumberInputCompleted = (cvcNumber: string) => {
+  return cvcNumber.length === INPUT_RULES.validCVCNumberLength;
 };
 
-export const validateCardPasswordInputCompleted = ({
-  cardPassword,
-}: CardPassword) => {
-  return cardPassword.length === 2;
+export const validateCardPasswordInputCompleted = (cardPassword: string) => {
+  return cardPassword.length === INPUT_RULES.validCardPasswordNumberLength;
 };
--- src/validators/cardInputValidator.ts
+++ src/validators/cardInputValidator.ts
@@ -1,6 +1,20 @@
+import { INPUT_RULES } from "../constants/card-app";
+
 const cardInputValidator = {
+  validateNumericInput(value: string) {
+    return /^(\d*)$/.test(value);
+  },
+
+  validateAlphabetInput(value: string) {
+    return /^[a-zA-Z ]*$/.test(value);
+  },
+
   validateNumberInRange(number: number, min: number, max: number) {
     return number >= min && number <= max;
+  },
+
+  validateCardNumberLength(value: string) {
+    return value.length === INPUT_RULES.maxCardNumberPartLength;
   },
 
   validatePastYear(year: string) {
@@ -10,13 +24,13 @@ const cardInputValidator = {
   },
 
   validateMonth(month: string) {
-    if (month.length !== 2) return false;
+    if (month.length !== INPUT_RULES.validExpirationLength) return false;
 
     return this.validateNumberInRange(parseInt(month, 10), 1, 12);
   },
 
   validateYear(year: string) {
-    if (year.length !== 2) return false;
+    if (year.length !== INPUT_RULES.validExpirationLength) return false;
 
     return this.validatePastYear(year);
   },
@@ -36,7 +50,11 @@ const cardInputValidator = {
 
     if (name === "yy" && !this.validateYear(yy)) return false;
 
-    if (mm.length !== 2 || yy.length !== 2) return true;
+    if (
+      mm.length !== INPUT_RULES.validExpirationLength ||
+      yy.length !== INPUT_RULES.validExpirationLength
+    )
+      return true;
 
     return this.validateFutureDate(parseInt(mm), parseInt(yy)) ? true : false;
   },
