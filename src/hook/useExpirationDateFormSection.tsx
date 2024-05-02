56193e2a62e8914f27042817354b0311d67fb4cd
이태훈 <rhymint@gmail.com>
2024-05-02 21:33:32 +0900

    refactor: useExpirationDateFormSection 구조 변경

--- src/hook/useExpirationDateFormSection.tsx
+++ src/hook/useExpirationDateFormSection.tsx
@@ -1,92 +1,97 @@
-import { useEffect, useState } from "react";
-import OPTION from "../constants/option";
-import REGEX from "../constants/regex";
-import ERROR_MESSAGE from "../constants/errorMessage";
+import React from "react";
 import useMultiFormSection from "./useMultiFormSection";
+import useFocusNext from "./useFocusNext";
+import REGEX from "../constants/regex";
+import OPTION from "../constants/option";
 
-interface UseExpirationFormSectionProps {
-  cardInfo: CardInfo
-  dispatchCardInfo: React.Dispatch<CardInfoAction>
+interface UseExpirationDateFormSection {
   refs: React.MutableRefObject<HTMLInputElement[]>
+  values: string[];
+  updateValues: (values: string[]) => void;
+  updateComplete: () => void;
+  maxLength?: number
 }
 
-const nowDate = new Date();
-const year = nowDate.getFullYear().toString().slice(2, 4);
-const month = (nowDate.getMonth() + 1).toString().padStart(2, '0');
-const now = year + month;
+const useExpirationDateFormSection = ({ refs, values, updateValues, updateComplete, maxLength = OPTION.expirationDateMaxLength }: UseExpirationDateFormSection) => {
+  const { focusNext } = useFocusNext(refs);
 
-const useExpirationDateFormSection = (props: UseExpirationFormSectionProps) => {
-  const { cardInfo, dispatchCardInfo, refs } = props;
-  const [hasErrors, setHasErrors] = useState(new Array(refs.current.length).fill(false))
-  const [error, setError] = useState('');
+  const validateOnChange = (index: number, newValue: string) => {
+    if (!/^\d*$/.test(newValue)) {
+      return {
+        isValid: false,
+        errorMessage: '유효기간은 숫자만 입력이 가능해요.',
+      };
+    }
+    if (index === 0 && !/^$|^(0[1-9]|1[0-2]|0|1)$/.test(newValue))
+      return {
+        isValid: false,
+        errorMessage: '유효기간 월은 01~12 사이만 입력이 가능해요.',
+      };
+    if (newValue.length === maxLength) focusNext();
+    return { isValid: true, errorMessage: '' };
+  };
 
-  const validateCardExpiration = (values: string[]) => {
-    if (values.every(value => value.length === 0)) return
-    const newHasErrors = [...hasErrors];
-    let anyError = false;
+  const validateOnBlur = (index: number) => {
+    const nowDate = new Date();
+    const year = nowDate.getFullYear().toString().slice(2, 4);
+    const month = (nowDate.getMonth() + 1).toString().padStart(2, '0');
+    const now = Number(year + month);
+    const expireDate = Number(values[1] + values[0]);
 
+    if (values.join('').length !== values.length * maxLength) {
+      return {
+        isValid: false,
+        errorMessage: `유효기간은 MM / YY 형식의 4자리로 입력해 주세요.`,
+      };
+    }
+    if (now - expireDate > 0) {
+      return {
+        isValid: false,
+        errorMessage: `만료된 카드입니다.`,
+      };
+    }
+    return { isValid: true, errorMessage: '' };
+  };
+
+  const validateOnBlurAll = () => {
+    const result: number[] = []
     values.forEach((value, index) => {
-      if (value.length !== OPTION.expirationDateMaxLength) {
-        newHasErrors[index] = true;
-        anyError = true;
-      } else {
-        newHasErrors[index] = false;
+      if (value.length !== maxLength) {
+        result.push(index)
       }
-    });
-
-    setHasErrors(newHasErrors);
-
-    if (anyError) {
-      setError(ERROR_MESSAGE.expirationFormat);
-    } else {
-      validateExpired();
+    })
+    if (result.length !== 0) {
+      return {
+        indexList: result,
+        isValid: false,
+        errorMessage: `카드번호는 ${maxLength * values.length}글자로 입력해 주세요.`,
+      };
     }
-  };
-
-  useEffect(() => {
-    formatMonth()
-  }, [document.activeElement])
-
-  const formatMonth = () => {
-    if (cardInfo.expiration.value[0].length === 2)
-      if (REGEX.oneToNine.test(cardInfo.expiration.value[0])) {
-        dispatchCardInfo({ type: 'SET_CARD_EXPIRATION_VALUE', value: ['0' + cardInfo.expiration.value[0], cardInfo.expiration.value[1]] })
-      } else if (REGEX.zero.test(cardInfo.expiration.value[0])) {
-        dispatchCardInfo({ type: 'SET_CARD_EXPIRATION_VALUE', value: [OPTION.minMonth, cardInfo.expiration.value[1]] })
-      } else if (
-        !REGEX.month.test(cardInfo.expiration.value[0])
-      ) {
-        dispatchCardInfo({ type: 'SET_CARD_EXPIRATION_VALUE', value: [OPTION.maxMonth, cardInfo.expiration.value[1]] })
-      }
-  };
-
-  const { handleChange } = useMultiFormSection({
-    values: cardInfo.expiration.value,
-    refs: refs,
-    regex: REGEX.numbers,
-    errorMessage: ERROR_MESSAGE.onlyNumber,
-    maxLength: OPTION.expirationDateMaxLength,
-    dispatchCardInfo: (values: string[]) => dispatchCardInfo({ type: 'SET_CARD_EXPIRATION_VALUE', value: values }),
-    setError: setError,
-    hasErrors: hasErrors,
-    setHasErrors: setHasErrors,
-    validate: validateCardExpiration,
-  });
-
-  const validateExpired = () => {
-    const expireDate = +(cardInfo.expiration.value[1] + cardInfo.expiration.value[0]);
-    const nowDate = +now;
-
-    if (nowDate - expireDate > 0) {
-      setHasErrors([true, true])
-      setError(ERROR_MESSAGE.expiredCard)
-    } else {
-      setError('')
-      dispatchCardInfo({ type: 'SET_CARD_EXPIRATION_COMPLETED', value: true })
-    }
+    updateComplete();
+    return { indexList: result, isValid: true, errorMessage: '' };
   }
 
-  return { error, hasErrors, handleChange } as const
-}
+  const {
+    hasErrors,
+    errorMessage,
+    onChangeHandler,
+    onFocusHandler,
+    onBlurHandler,
+  } = useMultiFormSection({
+    refs,
+    values,
+    updateValues,
+    validateOnChange,
+    validateOnBlur,
+    validateOnBlurAll,
+  });
 
+  return {
+    hasErrors,
+    errorMessage,
+    onChangeHandler,
+    onBlurHandler,
+    onFocusHandler,
+  };
+};
 export default useExpirationDateFormSection;
