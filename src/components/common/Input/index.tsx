dca4288f5b0e9da143a0b6c7c7bff3cc49ab36bd
keemsebeen <sebin0580@gmail.com>
2025-04-15 16:56:43 +0900

    feat: Input 컴포넌트 구현
    Co-Authored-By: ohgus <123801385+ohgus@users.noreply.github.com>

---
+++ src/components/common/Input/index.tsx
@@ -0,0 +1,44 @@
+import { ComponentProps } from 'react';
+import { StyledInputContainer } from './Input.styled';
+
+// TODO : onChange 타입 정의
+export type Props = {
+  /**
+   * 입력 상태 타입을 나타냅니다.
+   */
+  value: string | null;
+  /**
+   * 입력 전 보여줄 임시 문구입니다.
+   */
+  placeholder: string;
+  /**
+   * 입력 전 보여줄 임시 문구입니다.
+   */
+  isValid: boolean;
+} & ComponentProps<'input'>;
+
+export const Input = ({
+  value,
+  type = 'text',
+  onChange,
+  disabled = true,
+  minLength = 0,
+  maxLength = 4,
+  placeholder,
+  isValid = false,
+  ...props
+}: Props) => {
+  return (
+    <StyledInputContainer
+      value={value}
+      onChange={onChange}
+      type={type}
+      disabled={disabled}
+      minLength={minLength}
+      maxLength={maxLength}
+      placeholder={placeholder}
+      isValid={isValid}
+      {...props}
+    />
+  );
+};
