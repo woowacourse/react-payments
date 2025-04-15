a52a10c1c6db508e8ef9b8a96bed9c4d39c5cceb
keemsebeen <sebin0580@gmail.com>
2025-04-15 15:08:36 +0900

    chore: eslint, prettierrc 설정
    Co-Authored-By: ohgus <123801385+ohgus@users.noreply.github.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,18 +2,48 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:storybook/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/jsx-tag-spacing': 1,
+    'no-restricted-exports': ['error', { restrictDefaultExports: { direct: true } }],
+    'import/no-unresolved': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'sibling'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@/**',
+            group: 'internal',
+          },
+          {
+            pattern: './*',
+            group: 'sibling',
+          },
+        ],
+        distinctGroup: false,
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
     ],
   },
 };
