1e8d694424d3093dde599f540670f1e5a3b7ff3a
ohgus <anakin1108@naver.com>
2025-04-15 18:12:29 +0900

    feat: Text 컴포넌트 stories 정의
    Co-Authored-By: keemsebeen <sebin0580@gmail.com>

---
+++ src/stories/Text.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Text } from '../components/common/Text';
+
+const meta = {
+  title: 'common/Text',
+  component: Text,
+} satisfies Meta<typeof Text>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    variant: 'Title',
+    color: 'black',
+    children: '텍스트',
+  },
+  argTypes: {
+    variant: {
+      control: { type: 'radio' },
+      options: ['Title', 'Body', 'Caption'],
+    },
+    color: {
+      control: { type: 'color' },
+    },
+  },
+  render: (args) => {
+    return <Text {...args} />;
+  },
+};
