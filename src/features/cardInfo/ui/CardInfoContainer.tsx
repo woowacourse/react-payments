3928adec24ee6ab989dea2d3f557ab40a218406e
eunwoo-levi <eunwoo1341@gmail.com>
2025-04-15 16:38:49 +0900

    feat: CardInfoContainer 컴포넌트 구현
    Co-authored-by: Claire <eunsoa0310@gmail.com>

---
+++ src/features/cardInfo/ui/CardInfoContainer.tsx
@@ -0,0 +1,11 @@
+import './CardInfoContainer.css';
+import CardInfoSection from './CardInfoSection';
+
+export default function CardInfoContainer() {
+  return (
+    <div className='card-info-container'>
+      <CardInfoSection />
+      <CardInfoSection />
+    </div>
+  );
+}
