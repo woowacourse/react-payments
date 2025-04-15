475a3096608f2a2af27372c46375959b16d57dfa
jin123457 <jinsol9909@gmail.com>
2025-04-15 17:40:34 +0900

    feat: input, label 컴포넌트 구현
    Co-authored-by: Kuesung Park <gueit214@naver.com>

---
+++ src/components/Input.tsx
@@ -0,0 +1,23 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
+interface InputProps {
+  placeholder: string
+}
+
+export default function Input({ placeholder }: InputProps) {
+  return (
+    <input
+      type="number"
+      placeholder={placeholder}
+      css={css`
+        border: 1px solid #acacac;
+        border-radius: 4px;
+        padding: 8px;
+        &::placeholder {
+          color: #acacac;
+        }
+      `}
+    />
+  )
+}
