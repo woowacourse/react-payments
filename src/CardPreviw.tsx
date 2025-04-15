ed33a647b7dcc04bf8cf84ea633a9251f6d8c259
mun-kyeong <horse6953@naver.com>
2025-04-15 15:44:36 +0900

    refactor : style 컴포넌트 위치 변경

--- src/CardPreviw.tsx
+++ src/CardPreviw.tsx
@@ -1,25 +1,25 @@
 import styled from 'styled-components';
 
+const Card = styled.div`
+  position: relative;
+  width: 212px;
+  height: 132px;
+  background: #333333;
+  margin: 40px 0px;
+  border-radius: 4px;
+  box-shadow: 3px 3px 5px 0px #00000040;
+`;
+
+const CardChip = styled.div`
+  position: absolute;
+  width: 36px;
+  height: 22px;
+  inset: 8px 0 0 12px;
+  border-radius: 4px;
+  background: #ddcd78;
+`;
+
 function CardPreviw() {
-  const Card = styled.div`
-    position: relative;
-    width: 212px;
-    height: 132px;
-    background: #333333;
-    margin: 40px 0px;
-    border-radius: 4px;
-    box-shadow: 3px 3px 5px 0px #00000040;
-  `;
-
-  const CardChip = styled.div`
-    position: absolute;
-    width: 36px;
-    height: 22px;
-    inset: 8px 0 0 12px;
-    border-radius: 4px;
-    background: #ddcd78;
-  `;
-
   return (
     <Card>
       <CardChip />
