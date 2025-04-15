a11862bdddfb05d27a58465bb780d5bab84e6806
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-04-15 15:01:19 +0900

    style: styled-compoents 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import styled from 'styled-components';
 import "./App.css";
+
+const Test = styled.div`
+  background-color: #000000
+`;
 
 function App() {
   return (
-    <>
+    <Test>
       <h1>React Payments</h1>
-    </>
+    </Test>
   );
 }
 
