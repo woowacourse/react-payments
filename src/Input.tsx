072a55830868e8514be765d604c6351a63c3615a
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-04-15 18:29:58 +0900

    feat : InputField 스타일 적용

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,11 +1,20 @@
 import { ChangeEvent } from 'react';
 import styled from 'styled-components';
 
-const StyledInput = styled.input`
+const StyledInput = styled.input<{ $isError: boolean }>`
   width: 100%;
   height: 32px;
+  border: 1px solid;
   border-radius: 2px;
   padding: 8px;
+  box-sizing: border-box;
+
+  border-color: ${(props) => (props.$isError ? 'red' : '#acacac')};
+
+  &:focus {
+    outline: none;
+    border-color: ${(props) => (props.$isError ? 'red' : 'black')};
+  }
 `;
 
 function Input({
@@ -23,6 +32,7 @@ function Input({
     <StyledInput
       placeholder={placeholder}
       value={value}
+      $isError={isError}
       onChange={onChange}
     ></StyledInput>
   );
--- src/InputField.tsx
+++ src/InputField.tsx
@@ -1,9 +1,32 @@
 import { ChangeEvent, useState } from 'react';
 import Input from './Input';
+import styled from 'styled-components';
 
 type inputType = '카드 번호' | '유효기간' | 'CVC';
 
-function InputField({ type }: { type: inputType }) {
+const InputFieldContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const Label = styled.p`
+  font-weight: 500;
+  font-size: 12px;
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const ErrorMessage = styled.p`
+  font-weight: 400;
+  font-size: 9.5px;
+  background-color: #ff3d3d;
+`;
+
+function InputField({ label, type }: { label: string; type: inputType }) {
   const [value, setValue] = useState('');
 
   const onChange = (e: ChangeEvent) => {
@@ -12,12 +35,18 @@ function InputField({ type }: { type: inputType }) {
   };
 
   return (
-    <Input
-      placeholder="1234"
-      isError={false}
-      value={value}
-      onChange={onChange}
-    ></Input>
+    <InputFieldContainer>
+      <Label>{label}</Label>
+      <InputWrapper>
+        <Input
+          placeholder="1234"
+          isError={false}
+          value={value}
+          onChange={onChange}
+        ></Input>
+      </InputWrapper>
+      <ErrorMessage>sdf</ErrorMessage>
+    </InputFieldContainer>
   );
 }
 
