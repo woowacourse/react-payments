339f4c50615d36c0b0e50da74335bd6ba0e0a976
keemsebeen <sebin0580@gmail.com>
2025-04-15 18:49:52 +0900

    refactor: Text 컴포넌트 font weight 설정
    Co-Authored-By: ohgus <123801385+ohgus@users.noreply.github.com>

--- src/components/common/Text/Text.styled.tsx
+++ src/components/common/Text/Text.styled.tsx
@@ -1,23 +1,31 @@
-import { css } from '@emotion/css';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+
 import { Props } from '.';
 
 const variants = {
   Title: css`
     font-size: 24px;
-    font-weight: bold;
+    font-weight: 700;
+    white-space: pre-wrap;
+    line-height: 140%;
   `,
   Body: css`
     font-size: 20px;
-    font-weight: medium;
+    font-weight: 500;
+    white-space: pre-wrap;
+    line-height: 140%;
   `,
   Caption: css`
     font-size: 16px;
-    font-weight: normal;
+    font-weight: 400;
+    white-space: pre-wrap;
+    line-height: 140%;
   `,
 };
 
 export const StyledTextContainer = styled.p<Props>`
+  margin: 0;
   color: ${({ color }) => color};
   ${({ variant }) => variants[variant]}
 `;
