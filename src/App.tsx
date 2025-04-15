06a85032ba2f953c678c04d3e4f743d7e3d2cfeb
H0ngJu <hongju4385@gmail.com>
2025-04-15 15:56:59 +0900

    feat: 카드 레이아웃 구현
    Co-authored-by: AHHYUNJU <AHHYUNJU@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import "./App.css";
 import CardNumber from "./CardNumber";
 import CardExpirationDate from "./CardExpirationDate";
 import CardCvcNumber from "./CardCvcNumber";
+import CardLayout from "./components/Card/CardLayout";
 
 function App() {
   return (
     <>
+      <CardLayout />
       <CardNumber />
       <CardExpirationDate />
       <CardCvcNumber />
---
+++ src/components/Card/CardLayout.tsx
@@ -0,0 +1,11 @@
+import "./CardLayout.css";
+import CardLogo from "./CardLogo";
+
+export default function CardLayout() {
+  return (
+    <div className="card-layout">
+      <div className="card-chip"></div>
+      <CardLogo />
+    </div>
+  );
+}
