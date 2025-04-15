d72808057b1f7d76c534c35683f4829b8ca37071
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-04-15 16:13:15 +0900

    style : Noto sans font 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import GlobalStyle from '../styles/global.ts';
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
---
+++ styles/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+import NotoSansKR from '../fonts/NotoSansKR-VariableFont_wght.ttf';
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+        font-family: 'NotoSansKR';
+        src: local('NotoSansKR');
+        font-style: normal;
+        src: url(${NotoSansKR}) format('truetype');
+  }
+  `;
+
+export default GlobalStyle;
