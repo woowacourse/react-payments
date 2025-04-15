a4a322e86d6a51febcba9b1698757f38a30c02f5
sanghee01 <sang6299@naver.com>
2025-04-15 17:17:31 +0900

    style: 공통 css 추가

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,15 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+import '../src/styles/index.css';
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+        date: /Date$/i
+      }
+    }
+  }
 };
 
 export default preview;
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import "./App.css";
-
+import './App.css';
+import './index.css';
 function App() {
   return (
     <>
