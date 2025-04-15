257d38aade0b47570d397b436d8bd857c57575a2
wo-o29 <woogur29@gmail.com>
2025-04-15 15:01:49 +0900

    ​​feat: 공통 인풋 컴포넌트 추가
    Co-authored-by: spoyodevelop <73924592+spoyodevelop@users.noreply.github.com>

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,18 @@
+import { ComponentProps } from "react";
+import styles from "./Input.module.css";
+
+interface InputProps extends ComponentProps<"input"> {
+  isError?: boolean;
+}
+
+function Input({ isError, ...props }: InputProps) {
+  return (
+    <input
+      type="text"
+      {...props}
+      className={`${styles.input} ${isError && styles.error}`}
+    />
+  );
+}
+
+export default Input;
