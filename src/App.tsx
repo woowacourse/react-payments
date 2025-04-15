74a8650120b1dd7c4bab0bbe4a646e6b0eff3a8d
keemsebeen <sebin0580@gmail.com>
2025-04-15 22:13:50 +0900

    feat: App.tsx에 Card 컴포넌트 추가
    Co-Authored-By: ohgus <123801385+ohgus@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import "./App.css";
+import { css } from '@emotion/css';
+import './App.css';
+import { AppLayout } from './components/common/AppLayout';
+import { Card } from './components/features/Card';
+import { useState } from 'react';
 
 function App() {
+  const [card, setCard] = useState({
+    cardType: 'VISA',
+    cardNumbers: [0, 0, 0, 0],
+    expireDate: '',
+  });
   return (
-    <>
-      <h1>React Payments</h1>
-    </>
+    <AppLayout>
+      <div
+        className={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding-top: 80px;
+        `}
+      >
+        <Card type={'VISA'} cardNumbers={[1234, 1234, 1234, 1234]} expireDate={'04/12'} />
+      </div>
+    </AppLayout>
   );
 }
 
