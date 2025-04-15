2ab29b0a68ec4ea7b40e9a5810dc818f053a26b8
eunwoo-levi <eunwoo1341@gmail.com>
2025-04-15 15:27:34 +0900

    feat: Preview 컴포넌트 UI 구현
    Co-authored-by: Claire <eunsoa0310@gmail.com>

---
+++ src/features/preview/ui/Preview.tsx
@@ -0,0 +1,15 @@
+import './Preview.css';
+
+export default function Preview() {
+  return (
+    <div className='card-background'>
+      <div className='payment-card-container'>
+        <div className='payment-card payment-sim'></div>
+        <div className='payment-card payment-method'>
+          <img src='./Mastercard.svg' alt='Mastercard' className='payment-card' />
+          {/* <img src='./Visa.svg' alt='Visa' className='payment-logo' /> */}
+        </div>
+      </div>
+    </div>
+  );
+}
