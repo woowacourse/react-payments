1cc690cd6f7522147655f1e781fae68e342bb694
yeji0214 <1225yj50@naver.com>
2025-04-15 16:51:45 +0900

    feat: CVC 유효성 검사 구현

--- src/components/CVCInput/CVCInput.tsx
+++ src/components/CVCInput/CVCInput.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import InputContainer from "../InputContainer/InputContainer";
+import { validateCVC } from "../../domain/validate";
 
 const CVCInput = () => {
-    return (
-        <InputContainer title="CVC 번호를 입력해 주세요">
-            <label>CVC</label>
-            <input name="cvc" placeholder="123"/>
-            <p>helper text</p>
-        </InputContainer>
-    );
-}
+  const [CVC, setCVC] = useState("");
+  const [helperText, setHelperText] = useState("");
+
+  const handleCVC = (e: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      setCVC(e.target.value);
+      validateCVC(e.target.value, 3);
+      setHelperText("");
+    } catch (error: unknown) {
+      if (error instanceof Error) setHelperText(error.message);
+    }
+  };
+  return (
+    <InputContainer title="CVC 번호를 입력해 주세요">
+      <label>CVC</label>
+      <input name="cvc" placeholder="123" value={CVC} onChange={handleCVC} />
+      <p>{helperText}</p>
+    </InputContainer>
+  );
+};
 
 export default CVCInput;
