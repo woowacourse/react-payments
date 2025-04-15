b04af68ceae6a35b34959abb241345eb45760170
mun-kyeong <horse6953@naver.com>
2025-04-15 15:48:13 +0900

    feat : InputSection props 정의

---
+++ src/InputSection.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react';
+
+function InputSection(title: string, description: string, children: ReactNode) {
+  return <div>{children}</div>;
+}
+
+export default InputSection;
