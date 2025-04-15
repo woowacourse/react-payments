b13e2789d7708388724dad454f7709ac36f60539
wo-o29 <woogur29@gmail.com>
2025-04-15 15:26:40 +0900

    ​feat: 공통 Label 컴포넌트 추가
    Co-authored-by: spoyodevelop <73924592+spoyodevelop@users.noreply.github.com>

---
+++ src/components/Label/Label.tsx
@@ -0,0 +1,19 @@
+import { ComponentProps, PropsWithChildren } from "react";
+import styles from "./Label.module.css";
+
+interface LabelProps extends PropsWithChildren, ComponentProps<"label"> {
+  isHidden?: boolean;
+}
+
+function Label({ isHidden, children, ...props }: LabelProps) {
+  return (
+    <label
+      {...props}
+      className={`${styles.label} ${isHidden ? styles.hidden : ""}`}
+    >
+      {children}
+    </label>
+  );
+}
+
+export default Label;
