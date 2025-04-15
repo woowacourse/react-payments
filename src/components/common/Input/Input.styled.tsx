957913aebd0196ae4349b74a20a0a964b25a33bd
keemsebeen <sebin0580@gmail.com>
2025-04-15 16:57:12 +0900

    feat: Input styled 구현
    Co-Authored-By: ohgus <123801385+ohgus@users.noreply.github.com>

---
+++ src/components/common/Input/Input.styled.tsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+import { Props } from '.';
+
+export const StyledInputContainer = styled.input<Pick<Props, 'isValid'>>`
+  box-sizing: border-box;
+  width: 100%;
+  padding: 8px;
+  height: 40px;
+  color: black;
+  background-color: white;
+  font-size: 14px;
+  border: solid 1px;
+  border-radius: 4px;
+  outline: none;
+  ${({ isValid }) => 'border-color : ' + (isValid ? '#ACACAC' : '#FF3D3D')};
+
+  &:focus {
+    border-color: black;
+  }
+
+  &:disabled {
+    cursor: default;
+  }
+`;
