ab65cb5e7bcfd0f11c61018843dc88e9c5a8cc2a
Jeongeun Lee <leeju1009@gmail.com>
2025-04-15 22:10:30 +0900

    feat: 카드 유효기간 입력 컴포넌트 작성
    Co-authored-by: dev-dino22 <dev-dino22@users.noreply.github.com>

---
+++ src/components/paymentInputPage/cardInputForm/cardInput/CardExpirationDateInput.tsx
@@ -0,0 +1,42 @@
+import Input from '../../../common/inputForm/input/Input';
+import InputForm from '../../../common/inputForm/InputForm';
+
+function CardExpirationDateInput() {
+  function onChangeHandler(
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) {
+    const expirationDate = e.target.value;
+    if (index === 0) {
+      //month관련 예외 처리
+    } else {
+      //year관련 예외 처리
+    }
+  }
+
+  const inputs = Array.from({ length: 2 }).map((_, index) => {
+    return (
+      <Input
+        type='tel'
+        name='cardExpirationDate'
+        placeholder={index === 0 ? 'MM' : 'YY'}
+        onChange={(e) => onChangeHandler(e, index)}
+        maxLength={4}
+      />
+    );
+  });
+
+  return (
+    <>
+      <InputForm
+        title='카드 유효기간을 입력해 주세요.'
+        description='월/년도(MMYY)를 순서대로 입력해 주세요.'
+        label='유효기간'
+      >
+        {inputs}
+      </InputForm>
+    </>
+  );
+}
+
+export default CardExpirationDateInput;
