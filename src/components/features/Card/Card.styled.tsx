356f8965d4a88e82376c0307dbcdc7755a9a2053
ohgus <anakin1108@naver.com>
2025-04-15 20:55:55 +0900

    feat: Card component style 정의
    Co-Authored-By: keemsebeen <sebin0580@gmail.com>

---
+++ src/components/features/Card/Card.styled.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+export const StyledCardContainer = styled.div`
+  position: relative;
+  background-color: #333;
+  border-radius: 12px;
+  width: 350px;
+  height: 200px;
+`;
+
+export const StyledICCheapContainer = styled.div`
+  position: absolute;
+  top: 8px;
+  left: 12px;
+  width: 60px;
+  height: 40px;
+  background-color: #ddcd78;
+  border-radius: 6px;
+`;
+
+export const StyledCardTypeIcon = styled.img`
+  position: absolute;
+  top: 8px;
+  right: 12px;
+  width: 60px;
+  height: 40px;
+`;
