481c76213a78ee7800848ead15d2c887fa66effb
mun-kyeong <horse6953@naver.com>
2025-04-15 15:41:35 +0900

    feat : cardPreview 컴포넌트 제작

---
+++ src/CardPreviw.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+function CardPreviw() {
+  const Card = styled.div`
+    position: relative;
+    width: 212px;
+    height: 132px;
+    background: #333333;
+    margin: 40px 0px;
+    border-radius: 4px;
+    box-shadow: 3px 3px 5px 0px #00000040;
+  `;
+
+  const CardChip = styled.div`
+    position: absolute;
+    width: 36px;
+    height: 22px;
+    inset: 8px 0 0 12px;
+    border-radius: 4px;
+    background: #ddcd78;
+  `;
+
+  return (
+    <Card>
+      <CardChip />
+    </Card>
+  );
+}
+export default CardPreviw;
