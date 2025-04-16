ba616a4479b42308904673a1ac3f073c9ad67358
Ko Daeun <drechsu@gmail.com>
2025-04-16 15:15:46 +0900

    feat: 카드 미리보기 htmL 뼈대 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import CardNumbersInputSection from './components/InputSection/CardNumbersInputS
 import CardExpirationDateInputSection from './components/InputSection/CardExpirationDateInputSection';
 import CardCVCNumberInputSection from './components/InputSection/CardCVCNumberInputSection';
 import styles from './css/cardForm.module.css';
+import CardDisplay from './components/CardDisplay';
 
 function App() {
   return (
     <>
       <h1>React Payments</h1>
-      <div className={styles.cardForm}>
-        <CardNumbersInputSection />
-        <CardExpirationDateInputSection />
-        <CardCVCNumberInputSection />
+      <div className={styles.main}>
+        <CardDisplay />
+        <div className={styles.cardForm}>
+          <CardNumbersInputSection />
+          <CardExpirationDateInputSection />
+          <CardCVCNumberInputSection />
+        </div>
       </div>
     </>
   );
---
+++ src/components/CardDisplay.tsx
@@ -0,0 +1,11 @@
+import styles from './cardDisplay.module.css';
+
+const CardDisplay = () => {
+  return (
+    <div className={styles.basic}>
+      <div className={styles.chip}></div>
+    </div>
+  );
+};
+
+export default CardDisplay;
