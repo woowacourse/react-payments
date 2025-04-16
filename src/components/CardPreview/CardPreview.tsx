66f5bf1f28de273202ab565f7af37abcc6c5e7af
yeji0214 <1225yj50@naver.com>
2025-04-16 14:31:03 +0900

    feat: 카드 프리뷰에 카드 번호 및 유효 기간 실시간 반영

--- src/components/CardPreview/CardPreview.tsx
+++ src/components/CardPreview/CardPreview.tsx
@@ -1,27 +1,38 @@
-import styles from './CardPreview.module.css'
+import styles from "./CardPreview.module.css";
 
 type CardPreviewProps = {
   cardNumbers: string[];
   month: string;
   year: string;
-}
+};
 
-const CardPreview = ({cardNumbers, month, year}: CardPreviewProps) => {
-      return (
-      <div>
-        <div className={styles.preview} >
-          <img src="./magnetic.png" alt="magnetic" className={styles.magnetic}/>
-          <img src="./Visa.png" alt="visa" className={styles.visa}/>
-          <div>
-            <p>{cardNumbers}</p>
-            <div>
-              <p>{month}</p>
-              <p>{year}</p>
-            </div>
-          </div>
+const CardPreview = ({ cardNumbers, month, year }: CardPreviewProps) => {
+  return (
+    <div className={styles.preview}>
+      <img src="./magnetic.png" alt="magnetic" className={styles.magnetic} />
+      <img src="./Visa.png" alt="visa" className={styles.visa} />
+      <div className={styles.cardInfo}>
+        <div className={styles.cardNumberContainer}>
+          {cardNumbers.map((number, index) => (
+            <span key={index}>
+              {index === 2 || index === 3
+                ? number
+                  ? "•".repeat(number.length).padEnd(4, " ")
+                  : "    "
+                : number
+                ? number.padEnd(4, " ")
+                : "    "}
+            </span>
+          ))}
+        </div>
+        <div className={styles.date}>
+          <p>
+            {month}{month.length === 2 && '/'}{year}
+          </p>
         </div>
       </div>
-      );
+    </div>
+  );
 };
 
 export default CardPreview;
