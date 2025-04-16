c516a68fe186cc92db14d0614145b45b2eba0f96
dlsxjzld <sinjuk1@naver.com>
2025-04-16 13:43:12 +0900

    feat: 공통 Input 컴포넌트 추가 및 스타일링
    Co-authored-by: Yugyeong Kim <kimyou1102@users.noreply.github.com>

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  isError: boolean;
+  type?: string;
+  placeholder?: string;
+  value: string;
+  name: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+  onFocus?: (e: React.FocusEvent<HTMLInputElement>) => void;
+  setValue: () => void;
+}
+
+function Input(props: InputProps) {
+  return <InputWrapper {...props} />;
+}
+
+export default Input;
+
+// isError를 통해 border 색상 붉은색과 회색
+const InputWrapper = styled.input<InputProps>`
+  border: 1px solid ${(props) => (props.isError ? 'red' : 'gray')};
+  border-radius: 2px;
+  padding: 8px;
+  font-size: 11px;
+`;
