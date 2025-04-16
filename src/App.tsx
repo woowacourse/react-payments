eed693e5d37ac504610592f5c1cd99cb38f27e2d
장정안 <wjddks96@naver.com>
2025-04-16 13:03:16 +0900

    feat: 각 컴포넌트 들 App에서 호출
    Co-authored-by: Luna <ShinjungOh@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import { Global, ThemeProvider } from '@emotion/react';
-import './App.css';
+import Card from './component/Card/Card';
+import { appLayout, mainLayout } from './App.style';
 import GlobalStyle from './styles/globalStyle';
 import theme from './styles/theme';
-import Card from './component/Card/Card';
+import CardNumberInput from './component/CardNumberInput/CardNumberInput';
+import CardPeriodInput from './component/CardPeriod/CardPeriodInput';
+import CvcInput from './component/CvcInput/CvcInput';
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
-        <Card />
+        <div css={appLayout}>
+          <Card />
+          <main css={mainLayout}>
+            <CardNumberInput />
+            <CardPeriodInput />
+            <CvcInput />
+          </main>
+        </div>
       </ThemeProvider>
     </>
   );
