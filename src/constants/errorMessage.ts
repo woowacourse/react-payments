483a0246d065c93559d9d07a0d64ee035cc25ea9
hanheel <hanhee630630@gmail.com>
2025-04-16 19:37:09 +0900

    refactor : 불필요한 errorMessage 삭제

--- src/constants/errorMessage.ts
+++ src/constants/errorMessage.ts
@@ -1,7 +1,7 @@
 const ERROR_MESSAGE = {
   NOT_A_NUMBER: "숫자만 입력 가능합니다",
   INVALID_EXPIRATION_MONTH: "유효기간 월의 범위가 올바르지 않습니다 (1~12)",
-  INVALID_EXPIRATION_YEAR: "유효기간 연도의 범위가 올바르지 않습니다 (1~31)",
+  INVALID_EXPIRATION_YEAR: "유효기간이 지난 카드입니다",
 };
 
 export default ERROR_MESSAGE;
