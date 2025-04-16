837ef1f69ad438f6ca3fcd9ce162c0138ac8cebf
H0ngJu <hongju4385@gmail.com>
2025-04-16 13:57:12 +0900

    feat: 카드 번호 커스텀 훅 구현
    Co-authored-by: AHHYUNJU <AHHYUNJU@users.noreply.github.com>

---
+++ src/hooks/useCardNumbers.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function useCardNumbers() {
+  const [cardNumbers, setCardNumbers] = useState(["", "", "", ""]);
+  const [cardNumbersError, setError] = useState(["", "", "", ""]);
+
+  const cardNumbersValidate = (value: string, index: number) => {
+    const newCardNumbers = [...cardNumbers];
+    newCardNumbers[index] = value;
+    setCardNumbers(newCardNumbers);
+
+    const isValidLength = value.length <= 4;
+    const isNumber = /^[0-9]*$/.test(value);
+
+    if (!isValidLength) {
+      const newError = [...cardNumbersError];
+      newError[index] = "카드 번호는 4자리 숫자여야 합니다.";
+      setError(newError);
+      return;
+    }
+
+    if (!isNumber) {
+      const newError = [...cardNumbersError];
+      newError[index] = "숫자만 입력 가능합니다.";
+      setError(newError);
+      return;
+    }
+
+    const newError = [...cardNumbersError];
+    newError[index] = "";
+    setError(newError);
+  };
+
+  return { cardNumbers, cardNumbersError, cardNumbersValidate };
+}
