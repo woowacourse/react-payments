c965e114ac3d432b2a5b2bbfcba0ffc8349fa12b
장정안 <wjddks96@naver.com>
2025-04-16 13:58:18 +0900

    feat: 카드 번호 상태 추가
    Co-authored-by: Luna <ShinjungOh@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
-import { Global, ThemeProvider } from '@emotion/react';
-import Card from './component/Card/Card';
-import { appLayout, mainLayout } from './App.style';
 import GlobalStyle from './styles/globalStyle';
 import theme from './styles/theme';
+import Card from './component/Card/Card';
 import CardNumberInput from './component/CardNumberInput/CardNumberInput';
 import CardPeriodInput from './component/CardPeriod/CardPeriodInput';
 import CvcInput from './component/CvcInput/CvcInput';
+import { appLayout, mainLayout } from './App.style';
+import { ChangeEvent, useState } from 'react';
+import { Global, ThemeProvider } from '@emotion/react';
 
 function App() {
+  const [cardNumber, setCardNumber] = useState({
+    first: '',
+    second: '',
+    third: '',
+    forth: '',
+  });
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setCardNumber({
+      ...cardNumber,
+      [e.target.name]: value,
+    });
+    console.log('cardNumber', cardNumber);
+  };
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
         <div css={appLayout}>
-          <Card />
+          <Card cardNumber={cardNumber} />
           <main css={mainLayout}>
-            <CardNumberInput />
+            <CardNumberInput
+              cardNumber={cardNumber}
+              onChange={handleInputChange}
+            />
             <CardPeriodInput />
             <CvcInput />
           </main>
