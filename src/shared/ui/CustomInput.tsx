3e8f7814ddc78aebc3e124216f6abda7dde6052e
eunwoo-levi <eunwoo1341@gmail.com>
2025-04-16 13:35:42 +0900

    style: CustomInput CSS 구현
    Co-authored-by: Claire <eunsoa0310@gmail.com>

--- src/shared/ui/CustomInput.tsx
+++ src/shared/ui/CustomInput.tsx
@@ -1,4 +1,5 @@
 import { CustomInputProps } from '../type/types';
+import './customInput.css';
 
 export default function CustomInput({ type, placeholder, name, onChange }: CustomInputProps) {
   return <input className='custom-input' onChange={onChange} type={type} placeholder={placeholder} name={name} />;
