a1ff89eda7a8f8c6c6d84cb23b38ef02b8b1dfc7
장정안 <wjddks96@naver.com>
2025-04-16 13:02:40 +0900

    feat: theme 내 font 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,25 +5,45 @@ const theme = {
     gray: '#8B95A1',
     cardBlack: '#333333',
     gold: '#DDCD78',
+    text: '#0A0D13',
   },
   font: {
     card: {
       number: {
         fontFamily: 'Inter',
-        fontSize: '14px',
+        fontSize: '1.4rem',
         fontStyle: 'normal',
         fontWeight: '500',
-        lineHeight: '20px',
-        letterSpacing: '2.24px',
+        lineHeight: '2rem',
+        letterSpacing: '0.24rem',
+      },
+    },
+    cardNumberInput: {
+      title: {
+        fontFamily: 'Noto Sans KR',
+        fontSize: '1.8rem',
+        fontStyle: 'normal',
+        fontWeight: '700',
+        lineHeight: 'normal',
+      },
+      caption: {
+        fontFamily: 'Noto Sans KR',
+        fontSize: '0.95rem',
+        fontStyle: 'normal',
+        fontWeight: '400',
+        lineHeight: 'normal',
+      },
+    },
+    input: {
+      label: {
+        fontFamily: 'Noto Sans',
+        fontSize: '1.2rem',
+        fontStyle: 'normal',
+        fontWeight: '500',
+        lineHeight: '1.5rem',
       },
     },
   },
 };
-
-export type ColorType = typeof theme.color;
-
-export interface ThemeType {
-  color: ColorType;
-}
 
 export default theme;
