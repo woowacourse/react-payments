7ae2974661d0577d80916eb65588fb01215664f7
H0ngJu <hongju4385@gmail.com>
2025-04-16 13:58:20 +0900

    feat: CVC 입력 에러 발생시 보더 및 에러 메시지 추가
    Co-authored-by: AHHYUNJU <AHHYUNJU@users.noreply.github.com>

--- src/CardCvcNumber/CardCvcNumber.tsx
+++ src/CardCvcNumber/CardCvcNumber.tsx
@@ -2,17 +2,31 @@ import InputSubtitle from "../components/InputSubtitle/InputSubtitle";
 import InputTitle from "../components/InputTitle/InputTitle";
 import styles from "./CardCvcNumber.module.css";
 import InputNumber from "../components/Input/InputNumber";
+import InputErrorMessage from "../components/Input/InputErrorMessage";
 
 interface CardCvcNumberProps {
   handleChange: (value: string) => void;
+  cvcNumbers: string;
+  errorMessage: string;
 }
 
-export default function CardCvcNumber({ handleChange }: CardCvcNumberProps) {
+export default function CardCvcNumber({
+  handleChange,
+  cvcNumbers,
+  errorMessage,
+}: CardCvcNumberProps) {
   return (
     <section className={styles["card-cvc"]}>
       <InputTitle inputValue={"CVC 번호를"} />
       <InputSubtitle inputValue={"CVC"} />
-      <InputNumber onChange={handleChange} placeholder="123" maxLength={3} />
+      <InputNumber
+        onChange={handleChange}
+        placeholder="123"
+        maxLength={3}
+        value={cvcNumbers}
+        errorMessage={errorMessage}
+      />
+      <InputErrorMessage message={errorMessage} />
     </section>
   );
 }
---
+++ src/hooks/useCvcNumber.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function useCvcNumber() {
+  const [cvcNumbers, setCardCvcNumbers] = useState("");
+  const [cvcNumbersError, setError] = useState("");
+
+  const cvcNumbersValidate = (value: string) => {
+    setCardCvcNumbers(value);
+    const isValidLength = value.length <= 3;
+    const isNumber = /^[0-9]*$/.test(value);
+    if (!isValidLength) {
+      setError("CVC 번호는 3자리 숫자여야 합니다.");
+      return;
+    }
+    if (!isNumber) {
+      setError("숫자만 입력 가능합니다.");
+      return;
+    }
+    setError("");
+  };
+
+  return { cvcNumbers, cvcNumbersError, cvcNumbersValidate };
+}
