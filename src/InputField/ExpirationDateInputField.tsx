858a4460163a965c55ca87baa87e6eba0c329141
mun-kyeong <horse6953@naver.com>
2025-04-16 14:44:34 +0900

    feat : type=text로 변환 및 inputMode 추가

--- src/InputField/ExpirationDateInputField.tsx
+++ src/InputField/ExpirationDateInputField.tsx
@@ -10,27 +10,37 @@ function ExpirationDateInputField() {
 
   const onChange = (e: ChangeEvent) => {
     const { value, name } = e.target as HTMLInputElement;
-    if (value.length <= 2) {
-      if (name === 'ExpirationDatePart1' && Number(value) > 12) return;
-      setInputValue({ ...inputValue, [name]: value });
+    const numericValue = value.replace(/[^0-9]/g, '');
+
+    if (numericValue.length <= 2) {
+      if (name === 'ExpirationDatePart1' && Number(numericValue) > 12) return;
+      setInputValue({ ...inputValue, [name]: numericValue });
     }
+  };
+
+  const onBlur = (e: ChangeEvent) => {
+    const { value, name } = e.target as HTMLInputElement;
+    if (value.length === 1)
+      setInputValue({ ...inputValue, [name]: `0${value}` });
   };
 
   return (
     <BaseInputField label="유효기간">
       <Input
-        type="number"
+        inputMode="numeric"
         placeholder="MM"
         value={inputValue.ExpirationDatePart1}
         onChange={onChange}
         name="ExpirationDatePart1"
+        onBlur={onBlur}
       />
       <Input
-        type="number"
+        inputMode="numeric"
         placeholder="YY"
         value={inputValue.ExpirationDatePart2}
         onChange={onChange}
         name="ExpirationDatePart2"
+        onBlur={onBlur}
       />
     </BaseInputField>
   );
