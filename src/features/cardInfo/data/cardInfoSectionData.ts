5788a8c283ddd5627d84ce0b68f4d00eb6225c70
eunwoo-levi <eunwoo1341@gmail.com>
2025-04-16 15:33:50 +0900

    chore: cardInfoSectionData 데이터 수정
    Co-authored-by: Claire <eunsoa0310@gmail.com>

--- src/features/cardInfo/data/cardInfoSectionData.ts
+++ src/features/cardInfo/data/cardInfoSectionData.ts
@@ -4,10 +4,10 @@ export const cardInfoSectionData = [
     description: '본인 명의의 카드만 결제 가능합니다.',
     subTitle: '카드 번호',
     inputArr: [
-      { type: 'number', placeholder: '1234', name: '', onChange: () => {} },
-      { type: 'number', placeholder: '1234', name: '', onChange: () => {} },
-      { type: 'number', placeholder: '1234', name: '', onChange: () => {} },
-      { type: 'number', placeholder: '1234', name: '', onChange: () => {} },
+      { type: 'number', placeholder: '1234', name: 'cardNumber-0' },
+      { type: 'number', placeholder: '1234', name: 'cardNumber-1' },
+      { type: 'number', placeholder: '1234', name: 'cardNumber-2' },
+      { type: 'number', placeholder: '1234', name: 'cardNumber-3' },
     ],
   },
   {
@@ -15,14 +15,14 @@ export const cardInfoSectionData = [
     description: '월/년도(MMYY)를 순서대로 입력해 주세요.',
     subTitle: '유효기간',
     inputArr: [
-      { type: 'number', placeholder: 'MM', name: '', onChange: () => {} },
-      { type: 'number', placeholder: 'YY', name: '', onChange: () => {} },
+      { type: 'number', placeholder: 'MM', name: 'cardExpirationDate-month' },
+      { type: 'number', placeholder: 'YY', name: 'cardExpirationDate-year' },
     ],
   },
   {
     title: 'CVC 번호를 입력해 주세요',
     description: '',
     subTitle: 'CVC',
-    inputArr: [{ type: 'number', placeholder: '123', name: '', onChange: () => {} }],
+    inputArr: [{ type: 'number', placeholder: '123', name: 'cardCVC' }],
   },
 ];
