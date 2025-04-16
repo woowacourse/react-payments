ae7d92d11e9fb824df4d193da502e870ac2201e3
eunoia-jaxson <eunoia@handong.ac.kr>
2025-04-16 17:59:26 +0900

    refactor: CVC 번호 컴포넌트 리팩터링

---
+++ src/components/CVCNumbers/CVCNumbersView.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import InputLabels from '../common/InputLabels';
+import InputTexts from '../common/InputTexts';
+
+export interface CVCNumbersViewProps {
+  cvcNumbers: string[];
+  errorMessage: string;
+  error: boolean;
+  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const CVCNumbersView: React.FC<CVCNumbersViewProps> = ({
+  cvcNumbers,
+  errorMessage,
+  error,
+  handleInputChange,
+}) => {
+  return (
+    <Container data-testid="cvcnumbers-component">
+      <InputLabels title="CVC 번호를 입력해 주세요" />
+      <InputTexts
+        label="CVC"
+        placeholder={['123']}
+        state={cvcNumbers}
+        eventHandler={handleInputChange}
+        errors={[error]}
+      />
+      <ErrorMessage>{errorMessage}</ErrorMessage>
+    </Container>
+  );
+};
+
+export default CVCNumbersView;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 100%;
+`;
+
+const ErrorMessage = styled.div`
+  font-weight: 400;
+  font-size: 9.5px;
+  line-height: 100%;
+  vertical-align: middle;
+  color: red;
+  height: 9.5px;
+`;
--- src/components/CVCNumbers/index.tsx
+++ src/components/CVCNumbers/index.tsx
@@ -1,9 +1,7 @@
-import { useEffect, useRef, useState } from 'react';
-import InputLabels from '../common/InputLabels';
-import InputTexts from '../common/InputTexts';
-import styled from '@emotion/styled';
+import React, { useState } from 'react';
+import CVCNumbersView from './CVCNumbersView';
 
-interface CVCNumbersProps {
+export interface CVCNumbersProps {
   cvcNumbers: string[];
   setCvcNumbers: React.Dispatch<React.SetStateAction<string[]>>;
 }
@@ -12,77 +10,33 @@ const CVCNumbers: React.FC<CVCNumbersProps> = ({
   cvcNumbers,
   setCvcNumbers,
 }) => {
-  const errorMessageRef = useRef<HTMLDivElement>(null);
-  const [isError, setIsError] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (errorMessageRef?.current && !isError) {
-      errorMessageRef.current.innerText = '';
-    }
-  }, [isError]);
-
-  const enterCorrectCVC = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    newState: string[]
-  ) => {
-    newState[0] = e.target.value;
-    setIsError(false);
-    e.target.style.borderColor = '#ccc';
-  };
-
-  const enterWrongCVC = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    message: string
-  ) => {
-    e.target.style.borderColor = 'red';
-    setIsError(true);
-    if (errorMessageRef?.current) {
-      errorMessageRef.current.innerText = message;
-    }
-  };
+  const [errorMessage, setErrorMessage] = useState('');
+  const [error, setError] = useState(false);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCvcNumbers((prev) => {
       const newState = [...prev];
-      if (/^[0-9]*$/.test(e.target.value) && e.target.value.length <= 3) {
-        enterCorrectCVC(e, newState);
+      const value = e.target.value;
+      if (/^[0-9]*$/.test(value) && value.length <= 3) {
+        newState[0] = value;
+        setErrorMessage('');
+        setError(false);
       } else {
-        enterWrongCVC(e, '숫자만 가능합니다.');
+        setErrorMessage('숫자만 입력 가능합니다.');
+        setError(true);
       }
       return newState;
     });
   };
 
   return (
-    <CVCNumbersContainer
-    data-testid="cvcnumbers-component">
-      <InputLabels title="CVC 번호를 입력해 주세요" />
-      <InputTexts
-        label="CVC"
-        placeholder={['123']}
-        state={cvcNumbers}
-        eventHandler={handleInputChange}
-      />
-      <ErrorMessage ref={errorMessageRef}></ErrorMessage>
-    </CVCNumbersContainer>
+    <CVCNumbersView
+      cvcNumbers={cvcNumbers}
+      errorMessage={errorMessage}
+      error={error}
+      handleInputChange={handleInputChange}
+    />
   );
 };
+
 export default CVCNumbers;
-
-const CVCNumbersContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  width: 100%;
-`;
-
-const ErrorMessage = styled.div`
-  font-weight: 400;
-  font-size: 9.5px;
-  line-height: 100%;
-  letter-spacing: 0%;
-  vertical-align: middle;
-  color: red;
-  height: 9.5px;
-`;
