2aea9ffe7b93fe43018b0e05ce72e620ddf05b6d
Oliy <eunsoa0310@gmail.com>
2025-04-17 00:45:58 +0900

    feat: useCardInfo 로직 분리
    Co-authored-by: EunWoo <eunwoo1341@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,14 @@
 import './App.css';
 import Preview from './features/preview/ui/Preview';
 import CardInfoContainer from './features/cardInfo/ui/CardInfoContainer';
-import { useState } from 'react';
+import useCardInfo from './features/cardInfo/hooks/useCardInfo';
 
 function App() {
-  const [cardInfo, setCardInfo] = useState({
-    cardNumber: ['', '', '', ''],
-    cardExpirationDate: { month: '', year: '' },
-    cardCVC: '',
-  });
-
-  const handleCardInfoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-
-    if (name.includes('cardNumber')) {
-      const index = Number(name[name.length - 1]);
-      const updatedCardNumber = [...cardInfo.cardNumber];
-      updatedCardNumber[index] = value;
-
-      setCardInfo({
-        ...cardInfo,
-        cardNumber: updatedCardNumber,
-      });
-    } else if (name.includes('cardExpirationDate')) {
-      const key = name.split('-')[1];
-      const updatedCardExpirationDate = cardInfo.cardExpirationDate;
-      if (key === 'month') {
-        updatedCardExpirationDate.month = value;
-      } else if (key === 'year') {
-        updatedCardExpirationDate.year = value;
-      }
-      setCardInfo({
-        ...cardInfo,
-        cardExpirationDate: updatedCardExpirationDate,
-      });
-    } else {
-      setCardInfo({
-        ...cardInfo,
-        [name]: value,
-      });
-    }
-  };
+  const { cardInfo, handleCardInfoChange } = useCardInfo();
 
   return (
-    <div className='app-container'>
-      <main className='card-container'>
+    <div className="app-container">
+      <main className="card-container">
         <Preview cardInfo={cardInfo} />
         <CardInfoContainer onChange={handleCardInfoChange} />
       </main>
