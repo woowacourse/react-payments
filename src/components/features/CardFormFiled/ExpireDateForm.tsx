e954208cf8ac2418cac9b0b8eacc5b64b8fffed2
ohgus <anakin1108@naver.com>
2025-04-16 16:44:54 +0900

    feat: 유효기간 입력 영역 구현
    Co-Authored-By: keemsebeen <sebin0580@gmail.com>

--- src/components/features/CardFormFiled/ExpireDateForm.tsx
+++ src/components/features/CardFormFiled/ExpireDateForm.tsx
@@ -1,20 +1,35 @@
 import { CardInputLayout } from '../CardInputLayout';
 import { Flex } from '@/components/common/Flex';
 import { Input } from '@/components/common/Input';
-import { useCardInput } from '@/hooks/useCardInput';
+import { CardInputType } from '@/hooks/useCardInput';
+import { CardFormFiledProps } from './CardFormFiled.types';
 
-export const ExpireDateForm = () => {
+type Props = {
+  expireDate: CardInputType[];
+} & CardFormFiledProps;
+
+export const ExpireDateForm = ({ expireDate, onChange, onBlur }: Props) => {
+  const ExpireDatePlaceholder = ['MM', 'YY'];
+
   return (
     <CardInputLayout
       headerText="카드 유효기간을 입력해 주세요."
       description="월/년도(MMYY)를 순서대로 입력해 주세요."
       label="유효기간"
     >
-      {/* <Flex>
-        {cardNumeber.map((number, index) => {
-          <Input isValid={false} />;
-        })}
-      </Flex> */}
+      <Flex gap="8px" width="100%">
+        {expireDate.map((date, index) => (
+          <Input
+            key={`expire-${index}`}
+            value={date.value}
+            maxLength={2}
+            onChange={(e) => onChange(index, e)}
+            onBlur={(e) => onBlur(index, e)}
+            isValid={date.isValid}
+            placeholder={ExpireDatePlaceholder[index]}
+          />
+        ))}
+      </Flex>
     </CardInputLayout>
   );
 };
