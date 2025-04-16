21b8bd4d2c1b3843980cef41c74ea958bb6f819f
ohgus <anakin1108@naver.com>
2025-04-16 21:52:48 +0900

    feat: 월, 년도에 대한 예외처리 추가
    Co-Authored-By: keemsebeen <sebin0580@gmail.com>

--- src/hooks/useCardInput.ts
+++ src/hooks/useCardInput.ts
@@ -1,11 +1,19 @@
+import { CardFormFiledType } from '@/components/features/CardFormFiled/CardFormFiled.types';
 import { useState } from 'react';
+
+const ExpireDateIndex = {
+  MONTH: 0,
+  YEAR: 1,
+};
 
 export type CardInputType = {
   value: string;
   isValid: boolean;
 };
 
-export const useCardInput = (arrLength: number, valueLength: number) => {
+const currentYear = new Date().getFullYear().toString().slice(-2);
+
+export const useCardInput = (type: CardFormFiledType, arrLength: number, valueLength: number) => {
   const [value, setValue] = useState<CardInputType[]>(
     Array.from({ length: arrLength }, () => ({ value: '', isValid: true }))
   );
@@ -19,7 +27,7 @@ export const useCardInput = (arrLength: number, valueLength: number) => {
     });
   };
 
-  const validateInput = (value: string) => {
+  const validateInput = (value: string, index: number) => {
     if (value.length < valueLength) {
       setErrorMessage(`${valueLength}자리의 숫자를 입력하셔야 합니다.`);
       return false;
@@ -30,12 +38,24 @@ export const useCardInput = (arrLength: number, valueLength: number) => {
       return false;
     }
 
+    if (type === 'expireDate') {
+      if (index === ExpireDateIndex.MONTH && (parseInt(value) < 1 || parseInt(value) > 12)) {
+        setErrorMessage('월을 잘못 입력했습니다.');
+        return false;
+      }
+
+      if (index === ExpireDateIndex.YEAR && parseInt(value) < parseInt(currentYear)) {
+        setErrorMessage('만료 년도는 현재 년도보다 낮을 수 없습니다.');
+        return false;
+      }
+    }
+
     return true;
   };
 
   const handleBlur = (index: number, e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
-    const isValidate = validateInput(inputValue);
+    const isValidate = validateInput(inputValue, index);
 
     setValue((prev) => {
       const newArr = [...prev];
