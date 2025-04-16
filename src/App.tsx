cc4b7dc1a38ec3db74ea61c1d51d6a21dd08395d
mun-kyeong <horse6953@naver.com>
2025-04-16 11:41:20 +0900

    feat : CardNumber InputFiled 컴포넌트 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import CardPreview from './CardPreview';
-import InputField from './InputField';
+
 import InputSection from './InputSection';
+import BaseInputField from './BaseInputField';
+import CardNumberInputField from './InputField/CardNumberInputField';
 
 const PaymentsLayout = styled.div`
   height: 100vh;
@@ -31,7 +33,7 @@ function App() {
           title="결제할 카드 번호를 입력해 주세요"
           caption="본인 명의의 카드만 결제 가능합니다."
         >
-          <InputField type="카드 번호" />
+          <CardNumberInputField />
         </InputSection>
       </PaymentsContainer>
     </PaymentsLayout>
---
+++ src/InputField/CardNumberInputField.tsx
@@ -0,0 +1,48 @@
+import { ChangeEvent, useState } from 'react';
+import BaseInputField from '../BaseInputField';
+import Input from '../Input';
+
+function CardNumberInputField() {
+  const [inputValue, setInputValue] = useState({
+    CardNumberPart1: '',
+    CardNumberPart2: '',
+    CardNumberPart3: '',
+    CardNumberPart4: '',
+  });
+
+  const onChange = (e: ChangeEvent) => {
+    const { value, name } = e.target as HTMLInputElement;
+    setInputValue({ ...inputValue, [name]: value });
+  };
+
+  return (
+    <BaseInputField label="카드 번호">
+      <Input
+        placeholder="1234"
+        value={inputValue.CardNumberPart1}
+        onChange={onChange}
+        name="CardNumberPart1"
+      />
+      <Input
+        placeholder="1234"
+        value={inputValue.CardNumberPart2}
+        onChange={onChange}
+        name="CardNumberPart2"
+      />
+      <Input
+        placeholder="1234"
+        value={inputValue.CardNumberPart3}
+        onChange={onChange}
+        name="CardNumberPart3"
+      />
+      <Input
+        placeholder="1234"
+        value={inputValue.CardNumberPart4}
+        onChange={onChange}
+        name="CardNumberPart4"
+      />
+    </BaseInputField>
+  );
+}
+
+export default CardNumberInputField;
