7aafb181fa8d8f42a74f4f93984d383795a579d2
dlsxjzld <sinjuk1@naver.com>
2025-04-16 15:32:53 +0900

    feat: 카드 번호 입력을 위한 CardNumber 컴포넌트 추가

---
+++ src/components/cardInfo/CardNumber.tsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+
+interface CardNumberProps {
+  title: string;
+  description: string;
+  errorMessage: string;
+  children: React.ReactElement;
+}
+
+function CardNumber({
+  title,
+  description,
+  errorMessage,
+  children,
+}: CardNumberProps) {
+  return (
+    <Wrapper>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+      {children}
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </Wrapper>
+  );
+}
+
+export default CardNumber;
+
+const Wrapper = styled.div``;
+
+const Title = styled.p`
+  font-weight: bold;
+  font-size: 18px;
+  margin-bottom: 4px;
+`;
+
+const Description = styled.p`
+  font-size: 10px;
+  color: #8b95a1;
+`;
+
+const ErrorMessage = styled.p`
+  font-size: 10px;
+  color: red;
+  margin-top: 4px;
+`;
