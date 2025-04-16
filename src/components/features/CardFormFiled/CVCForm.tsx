6f081f55cd54580ea16d573cebcc5f65a551307c
ohgus <anakin1108@naver.com>
2025-04-16 16:45:09 +0900

    feat: CVC 입력 영역 구현
    Co-Authored-By: keemsebeen <sebin0580@gmail.com>

--- src/components/features/CardFormFiled/CVCForm.tsx
+++ src/components/features/CardFormFiled/CVCForm.tsx
@@ -1,12 +1,25 @@
 import { CardInputLayout } from '../CardInputLayout';
 import { Flex } from '@/components/common/Flex';
 import { Input } from '@/components/common/Input';
+import { useCardInput } from '@/hooks/useCardInput';
 
 export const CVCForm = () => {
+  const { value: cvcNumber, handleChange, handleBlur } = useCardInput(1);
+
   return (
     <CardInputLayout headerText="CVC 번호를 입력해 주세요." label="CVC">
-      <Flex>
-        <Input maxLength={3} placeholder="CVC 번호" isValid={false} />;
+      <Flex width="100%">
+        {cvcNumber.map((cvc, index) => (
+          <Input
+            key={`cvc-${index}`}
+            value={cvc.value}
+            maxLength={3}
+            placeholder="CVC 번호"
+            isValid={cvc.isValid}
+            onChange={(e) => handleChange(0, e)}
+            onBlur={(e) => handleBlur(0, e)}
+          />
+        ))}
       </Flex>
     </CardInputLayout>
   );
