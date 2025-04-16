1f0c2d7edb3cc8ebe05a5aa2bb289d5f6d0db5c1
H0ngJu <hongju4385@gmail.com>
2025-04-16 16:45:10 +0900

    feat(storybook): storybook 컴포넌트 추가
    Co-authored-by: AHHYUNJU <AHHYUNJU@users.noreply.github.com>

---
+++ src/stories/CardExpirationDate.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import CardExpirationDate from "../CardExpirationDate/CardExpirationDate";
+
+const meta: Meta<typeof CardExpirationDate> = {
+  title: "Example/CardExpirationDate",
+  component: CardExpirationDate,
+};
+
+export default meta;
+type Story = StoryObj<typeof CardExpirationDate>;
+
+export const Primary: Story = {
+  args: {
+    handleChange: () => {},
+    cardExpirationDate: ["", ""],
+    errorMessage: ["", ""],
+  },
+};
+
+export const Error: Story = {
+  args: {
+    handleChange: () => {},
+    cardExpirationDate: ["dd", ""],
+    errorMessage: ["숫자만 입력 가능합니다.", ""],
+  },
+};
