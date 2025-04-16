2d89a7040193818893652b4ffbbdffa90b43ad48
bunju20 <ghkd2271@naver.com>
2025-04-16 14:05:30 +0900

    feat: 카드 컴포넌트 생성 - 카드 엠블럼 변경 로직 추가 필요

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import "./style.css";
 import "./reset.css";
-import Description from "./component/Description";
-import Input from "./component/Input";
+import Card from "./component/Card";
 
 function App() {
+  const cardNumber = {
+    first: 1111,
+    second: 1111,
+    third: 1111,
+    fourth: 1111,
+    MM: 12,
+    YY: 25,
+    CVC: 123,
+  };
+
   return (
     <>
-      <Description headText="안녕하세요" detailText="디테일" />
-      <Input maxLength={4} placeholder="카드번호" isError={false} />
+      <Card cardNumber={cardNumber} cardType="visa" />
     </>
   );
 }
---
+++ src/component/Card.tsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+interface CardNumber {
+  first: number;
+  second: number;
+  third: number;
+  fourth: number;
+  MM: number;
+  YY: number;
+  CVC: number;
+}
+
+interface CardProps {
+  cardNumber: CardNumber;
+  cardType: "visa" | "master";
+}
+
+const CardContainer = styled.div`
+  box-sizing: border-box;
+  width: 212px;
+  height: 132px;
+  background-color: var(--color-black);
+  border-radius: 4px;
+  box-shadow: 3px 3px 5px 0 rgba(0, 0, 0, 0.1);
+  padding: 8px 12px;
+`;
+
+const ChipContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const CardGoldChip = styled.div`
+  width: 36px;
+  height: 22px;
+  background-color: var(--color-yellow);
+  border-radius: 4px;
+  box-shadow: 0px 4px 4px 0 rgba(0, 0, 0, 0.1);
+`;
+
+const CardBrandLogo = styled.img`
+  width: 36px;
+  height: 22px;
+  //임시
+`;
+
+const CardInformation = styled.div`
+  color: var(--color-white);
+  font-size: var(--font-size-subheader);
+  font-weight: var(--font-weight-caption);
+  letter-spacing: 2.56px;
+`;
+
+const CardNumberContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  margin-top: 16px;
+  margin-bottom: 8px;
+`;
+
+// TODO: visa, master 카드 이미지 추가
+const Card = ({ cardNumber, cardType }: CardProps) => {
+  return (
+    <CardContainer>
+      <ChipContainer>
+        <CardGoldChip />
+        <CardBrandLogo src={`./${cardType}.png`} />
+      </ChipContainer>
+      <CardNumberContainer>
+        <CardInformation>{cardNumber.first}</CardInformation>
+        <CardInformation>{cardNumber.second}</CardInformation>
+        <CardInformation>{cardNumber.third}</CardInformation>
+        <CardInformation>{cardNumber.fourth}</CardInformation>
+      </CardNumberContainer>
+      <CardInformation>{`${cardNumber.MM}/${cardNumber.YY}`}</CardInformation>
+    </CardContainer>
+  );
+};
+
+export default Card;
