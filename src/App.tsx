7e341c7845843b843535bf87a99aa547e1c3f4da
keemsebeen <sebin0580@gmail.com>
2025-04-16 20:04:08 +0900

    refactor: FormFiled errorMessage 추가
    Co-Authored-By: ohgus <123801385+ohgus@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,41 @@
 import './App.css';
 import { AppLayout } from './components/common/AppLayout';
 import { Flex } from './components/common/Flex';
-import { Card } from './components/features/Card';
 import { CardNumberForm } from './components/features/CardFormFiled/CardNumberForm';
 import { CVCForm } from './components/features/CardFormFiled/CVCForm';
 import { ExpireDateForm } from './components/features/CardFormFiled/ExpireDateForm';
+import { CardPreview } from './components/features/CardPreview';
 import { useCardInput } from './hooks/useCardInput';
 
 function App() {
-  const { value: cardNumbers, handleChange, handleBlur } = useCardInput(4);
+  const {
+    value: cardNumbers,
+    errorMessage: cardErrorMessage,
+    handleChange,
+    handleBlur,
+  } = useCardInput(4, 4);
   const {
     value: expireDate,
+    errorMessage: expireDateErrorMessage,
     handleChange: handleExpireDateChange,
     handleBlur: handleExpireDateBlur,
-  } = useCardInput(2);
+  } = useCardInput(2, 2);
 
   return (
     <AppLayout>
-      <Flex padding="80px 0 0 0 ">
-        <Card cardNumbers={cardNumbers} expireDate={expireDate} />
+      <Flex padding="60px 0 0 0 ">
+        <CardPreview cardNumbers={cardNumbers} expireDate={expireDate} />
       </Flex>
-      <Flex direction="column" gap="40px" padding="60px 0 0 0">
-        <CardNumberForm cardNumbers={cardNumbers} onChange={handleChange} onBlur={handleBlur} />
+      <Flex direction="column" gap="20px" padding="60px 0 0 0">
+        <CardNumberForm
+          cardNumbers={cardNumbers}
+          errorMessage={cardErrorMessage}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
         <ExpireDateForm
           expireDate={expireDate}
+          errorMessage={expireDateErrorMessage}
           onChange={handleExpireDateChange}
           onBlur={handleExpireDateBlur}
         />
