86dc8e232559bfbfd1edea1cf78d0b7d64c5af8a
eunwoo-levi <eunwoo1341@gmail.com>
2025-04-16 15:37:44 +0900

    feat: preview 상태 로직 및 UI 구현
    Co-authored-by: Claire <74090200+eunsoA@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,52 @@
 import './App.css';
 import Preview from './features/preview/ui/Preview';
 import CardInfoContainer from './features/cardInfo/ui/CardInfoContainer';
+import { useState } from 'react';
 
 function App() {
+  const [cardInfo, setCardInfo] = useState({
+    cardNumber: ['', '', '', ''],
+    cardExpirationDate: { month: '', year: '' },
+    cardCVC: '',
+  });
+
+  const handleCardInfoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    if (name.includes('cardNumber')) {
+      const index = Number(name[name.length - 1]);
+      const updatedCardNumber = [...cardInfo.cardNumber];
+      updatedCardNumber[index] = value;
+
+      setCardInfo({
+        ...cardInfo,
+        cardNumber: updatedCardNumber,
+      });
+    } else if (name.includes('cardExpirationDate')) {
+      const key = name.split('-')[1];
+      const updatedCardExpirationDate = cardInfo.cardExpirationDate;
+      if (key === 'month') {
+        updatedCardExpirationDate.month = value;
+      } else if (key === 'year') {
+        updatedCardExpirationDate.year = value;
+      }
+      setCardInfo({
+        ...cardInfo,
+        cardExpirationDate: updatedCardExpirationDate,
+      });
+    } else {
+      setCardInfo({
+        ...cardInfo,
+        [name]: value,
+      });
+    }
+  };
+
   return (
     <div className='app-container'>
       <main className='card-container'>
-        <Preview />
-        <CardInfoContainer />
+        <Preview cardInfo={cardInfo} />
+        <CardInfoContainer onChange={handleCardInfoChange} />
       </main>
     </div>
   );
--- src/features/preview/ui/Preview.tsx
+++ src/features/preview/ui/Preview.tsx
@@ -1,6 +1,7 @@
+import { CardInfoProps } from '../../../shared/type/types';
 import './preview.css';
 
-export default function Preview() {
+export default function Preview({ cardInfo }: { cardInfo: CardInfoProps }) {
   return (
     <div className='card-background'>
       <div className='card-preview-container'>
@@ -8,6 +9,19 @@ export default function Preview() {
         <div className='card-preview payment-method'>
           <img src='./Mastercard.svg' alt='Mastercard' className='card-preview' />
           {/* <img src='./Visa.svg' alt='Visa' className='payment-logo' /> */}
+        </div>
+      </div>
+      <div className='card-preview-info-container'>
+        <div className='card-preview-info-card-number-container'>
+          <div className='card-preview-info-text'>{cardInfo?.cardNumber[0]}</div>
+          <div className='card-preview-info-text'>{cardInfo?.cardNumber[1]}</div>
+          <div className='card-preview-info-text secret'>{'•'.repeat(cardInfo.cardNumber[2]?.length)}</div>
+          <div className='card-preview-info-text secret'>{'•'.repeat(cardInfo.cardNumber[3]?.length)}</div>
+        </div>
+        <div className='card-preview-info-text'>
+          {cardInfo.cardExpirationDate.month}
+          {cardInfo.cardExpirationDate.year && '/'}
+          {cardInfo.cardExpirationDate.year}
         </div>
       </div>
     </div>
