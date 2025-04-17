eaac7c9f7c7fb98c14848fee0ef109c51047597c
mun-kyeong <horse6953@naver.com>
2025-04-17 11:13:08 +0900

    feat : CarcNumber가 4자리가 안될 경우 에러 처리

--- src/InputField/CardNumberInputField.tsx
+++ src/InputField/CardNumberInputField.tsx
@@ -23,18 +23,51 @@ function CardNumberInputField({
   cardType: CardType;
   setCardType: Dispatch<SetStateAction<CardType>>;
 }) {
-  type ErrorType = 'noneCardType' | null;
+  type ErrorType = 'noneCardType' | 'shortCardSegment';
 
-  const [cardNumberErrors, setCardNumberErrors] = useState<
-    Record<string, { isError: boolean; errorType: ErrorType }>
-  >({
-    cardNumberPart1: { isError: false, errorType: null },
-    cardNumberPart2: { isError: false, errorType: null },
-    cardNumberPart3: { isError: false, errorType: null },
-    cardNumberPart4: { isError: false, errorType: null },
+  const [errorTypes, setErrorTypes] = useState<Record<string, ErrorType[]>>({
+    cardNumberPart1: [],
+    cardNumberPart2: [],
+    cardNumberPart3: [],
+    cardNumberPart4: [],
   });
 
   const [errorMessage, setErrorMessage] = useState('');
+
+  const updateCardError = (
+    inputName: string,
+    errorStatus: { errorType: ErrorType; isError: boolean }
+  ) => {
+    const currentErrorType = errorTypes[inputName];
+
+    if (errorStatus.isError) {
+      const set = new Set(currentErrorType);
+      set.add(errorStatus.errorType);
+      setErrorTypes((prevValue) => ({
+        ...prevValue,
+        [inputName]: Array.from(set),
+      }));
+    } else {
+      const filteredErrorType = currentErrorType.filter(
+        (errorType) => errorType !== errorStatus.errorType
+      );
+      setErrorTypes((prevValue) => ({
+        ...prevValue,
+        [inputName]: filteredErrorType,
+      }));
+    }
+  };
+
+  const checkCardTypeFromPrefix = (value: string) => {
+    if (value.length <= 2) {
+      if (value[0] === '4') setCardType('visa');
+      else if (value >= '51' && value <= '55') setCardType('master');
+      else setCardType(null);
+    } else if (cardType === null) {
+      return true;
+    }
+    return false;
+  };
 
   const onChange = (e: ChangeEvent) => {
     const { value, name } = e.target as HTMLInputElement;
@@ -42,38 +75,37 @@ function CardNumberInputField({
 
     if (numericValue.length <= 4) {
       if (name === 'cardNumberPart1') {
-        setCardNumberErrors((prevValue) => ({
-          ...prevValue,
-          [name]: { isError: false, errorType: null },
-        }));
-        if (value.length <= 2) {
-          if (value[0] === '4') setCardType('visa');
-          else if (value >= '51' && value <= '55') setCardType('master');
-          else setCardType(null);
-        } else if (cardType === null) {
-          setCardNumberErrors((prevValue) => ({
-            ...prevValue,
-            [name]: { isError: true, errorType: 'noneCardType' },
-          }));
-        }
+        const isError = checkCardTypeFromPrefix(value);
+        updateCardError('cardNumberPart1', {
+          errorType: 'noneCardType',
+          isError,
+        });
       }
       setInputValue((prevValue) => ({ ...prevValue, [name]: numericValue }));
     }
   };
 
+  const onBlur = (e: ChangeEvent) => {
+    const { value, name } = e.target as HTMLInputElement;
+    updateCardError(name, {
+      errorType: 'shortCardSegment',
+      isError: value.length > 0 && value.length < 4,
+    });
+  };
+
   const ERROR_TYPE_TO_MESSAGE = {
     noneCardType: '유효하지 않은 카드 번호입니다. 카드 번호를 확인해주세요',
+    shortCardSegment: '카드 번호는 4자리씩 입력해주세요.',
   };
 
   useEffect(() => {
-    const errorStatus = Object.values(cardNumberErrors).find(
-      ({ isError }) => isError
+    const errorStatus = Object.values(errorTypes).find(
+      (errorType) => errorType
     );
-
-    if (errorStatus && errorStatus.errorType)
-      setErrorMessage(ERROR_TYPE_TO_MESSAGE[errorStatus.errorType]);
+    if (errorStatus && errorStatus?.length !== 0)
+      setErrorMessage(ERROR_TYPE_TO_MESSAGE[errorStatus[0]]);
     else setErrorMessage('');
-  }, [cardNumberErrors]);
+  }, [errorTypes]);
 
   return (
     <BaseInputField label="카드 번호" errorMessage={errorMessage}>
@@ -82,32 +114,36 @@ function CardNumberInputField({
         placeholder="1234"
         value={inputValue.cardNumberPart1}
         onChange={onChange}
+        onBlur={onBlur}
         name="cardNumberPart1"
-        isError={cardNumberErrors.cardNumberPart1.isError}
+        isError={Boolean(errorTypes.cardNumberPart1.length)}
       />
       <Input
         inputMode="numeric"
         placeholder="1234"
         value={inputValue.cardNumberPart2}
         onChange={onChange}
+        onBlur={onBlur}
         name="cardNumberPart2"
-        isError={cardNumberErrors.cardNumberPart2.isError}
+        isError={Boolean(errorTypes.cardNumberPart2.length)}
       />
       <Input
         inputMode="numeric"
         placeholder="1234"
         value={inputValue.cardNumberPart3}
         onChange={onChange}
+        onBlur={onBlur}
         name="cardNumberPart3"
-        isError={cardNumberErrors.cardNumberPart3.isError}
+        isError={Boolean(errorTypes.cardNumberPart3.length)}
       />
       <Input
         inputMode="numeric"
         placeholder="1234"
         value={inputValue.cardNumberPart4}
         onChange={onChange}
+        onBlur={onBlur}
         name="cardNumberPart4"
-        isError={cardNumberErrors.cardNumberPart4.isError}
+        isError={Boolean(errorTypes.cardNumberPart4.length)}
       />
     </BaseInputField>
   );
