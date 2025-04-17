9c9b6ba8854b3e16cb02dd7e939b7c71b068903e
dlsxjzld <sinjuk1@naver.com>
2025-04-17 10:51:02 +0900

    refactor: CardCVCField 스토리에 상태 추가
    Co-authored-by: Yugyeong Kim <kimyou1102@users.noreply.github.com>

--- src/components/cardInfoForm/CardCVCField/CardCVCField.stories.tsx
+++ src/components/cardInfoForm/CardCVCField/CardCVCField.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import CardCVCField from './CardCVCField';
+import { useState } from 'react';
 
 const meta = {
   title: 'CardCVCField',
@@ -11,7 +12,31 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  render: () => {
-    return <CardCVCField />;
+  args: {
+    cardCVC: '',
+    isError: false,
+    onChange: () => {},
+  },
+  render: (args) => {
+    const [cardCVC, setCardCVC] = useState('123');
+    const [isCardCVCError, setIsCardCVCError] = useState(false);
+
+    const onChangeCVC = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = e.target;
+
+      const isNotValid =
+        value.length < 3 || value.length > 3 || Number(value) < 0;
+
+      setIsCardCVCError(isNotValid);
+      setCardCVC(value.slice(0, 3));
+    };
+    return (
+      <CardCVCField
+        {...args}
+        cardCVC={cardCVC}
+        isError={isCardCVCError}
+        onChange={onChangeCVC}
+      />
+    );
   },
 };
