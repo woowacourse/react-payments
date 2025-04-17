4bdd7867a9ac50fedaa7dac4baf8f38ecf8c1151
mlnwns <mlnwns@naver.com>
2025-04-17 14:42:47 +0900

    fix: 카드 타입 경로의 잘못된 경로 수정
    Co-authored-by: Minje Sung <minje813@naver.com>

--- src/constants/setting.ts
+++ src/constants/setting.ts
@@ -3,8 +3,8 @@ export const MASTER_CARD_PREFIXES = ["51", "52", "53", "54", "55"];
 export const VISA_CARD_PREFIXES = ["4"];
 
 export const CARD_TYPE_PATH = {
-  VISA: "/assets/Visa.png",
-  MasterCard: "/assets/Mastercard.png",
+  VISA: "./assets/Visa.png",
+  MasterCard: "./assets/Mastercard.png",
   None: "",
 };
 
