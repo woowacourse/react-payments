6a3b988a4d7e5e9c5ce3a18ff70e2fa5d7ad9de1
wo-o29 <woogur29@gmail.com>
2025-04-17 14:52:37 +0900

    refactor: 유효기간 훅 validation 로직 분리​​
    Co-authored-by: spoyodevelop <73924592+spoyodevelop@users.noreply.github.com>

--- src/AddCard/components/AddCardForm/components/ExpireDate/hooks/useControlledExpireDate.ts
+++ src/AddCard/components/AddCardForm/components/ExpireDate/hooks/useControlledExpireDate.ts
@@ -1,46 +1,7 @@
 import { useCallback, useState } from "react";
-import {
-  INITIAL_EXPIRE_DATE_STATE,
-  EXPIRE_DATE_ERROR_MESSAGE,
-} from "../constants";
+import { INITIAL_EXPIRE_DATE_STATE } from "../constants";
 import { ExpireDateState } from "../types";
-
-const isValidMonth = (month: number) => {
-  return month >= 1 && month <= 12;
-};
-
-const validateMonth = (month: string) => {
-  if (isNaN(Number(month))) {
-    return EXPIRE_DATE_ERROR_MESSAGE.INVALID_NUMBER;
-  }
-
-  if (!isValidMonth(Number(month))) {
-    return EXPIRE_DATE_ERROR_MESSAGE.INVALID_MONTH_RANGE;
-  }
-
-  const isInvalidLength = month.length === 0 || month.length === 2;
-  if (!isInvalidLength) {
-    return EXPIRE_DATE_ERROR_MESSAGE.INVALID_MONTH_LENGTH;
-  }
-
-  return "";
-};
-
-const isValidYear = (year: string) => {
-  return year.length === 2;
-};
-
-const validateYear = (year: string) => {
-  if (isNaN(Number(year))) {
-    return EXPIRE_DATE_ERROR_MESSAGE.INVALID_NUMBER;
-  }
-
-  if (!isValidYear(year)) {
-    return EXPIRE_DATE_ERROR_MESSAGE.INVALID_YEAR_LENGTH;
-  }
-
-  return "";
-};
+import { validateMonth, validateYear } from "../validation";
 
 const useControlledExpireDate = () => {
   const [expireDate, setExpireDate] = useState<ExpireDateState>(
