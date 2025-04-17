a5b334711be796ecfa41b28f83dd8c9654e925c8
Jeongeun Lee <leeju1009@gmail.com>
2025-04-17 14:13:09 +0900

    feat: 카드 CVC 검증 로직 추가
    Co-authored-by: dev-dino22 <dev-dino22@users.noreply.github.com>

--- src/components/paymentInputPage/cardInputForm/cardInput/CardCVCInput.tsx
+++ src/components/paymentInputPage/cardInputForm/cardInput/CardCVCInput.tsx
@@ -1,15 +1,30 @@
+import { useState } from 'react';
 import Input from '../../../common/inputForm/input/Input';
 import InputForm from '../../../common/inputForm/InputForm';
+import { validatorUtils } from '../../../../utils/validationUtils';
 
 function CardCVCInput() {
-  function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+  const [feedbackMessage, setFeedbackMessage] = useState<string>('');
+
+  function onChangeHandler(
+    e: React.ChangeEvent<HTMLInputElement>,
+    setIsValid: (state: boolean) => void
+  ) {
     const cvc = e.target.value;
-    // cvc 검증 관련
+    if (!validatorUtils.isNumber(cvc)) {
+      setFeedbackMessage('숫자만 입력 가능합니다.');
+      setIsValid(false);
+      return;
+    }
   }
 
   return (
     <>
-      <InputForm title='CVC 번호를 입력해 주세요.' label='CVC'>
+      <InputForm
+        title='CVC 번호를 입력해 주세요.'
+        label='CVC'
+        feedbackMessage={feedbackMessage}
+      >
         <Input
           type='tel'
           name='cardCVC'
