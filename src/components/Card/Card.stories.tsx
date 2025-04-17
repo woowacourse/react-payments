dc5edcf95718a2cd0b9c193f59ac4db99b24feb0
sanghee01 <sang6299@naver.com>
2025-04-17 10:43:44 +0900

    chore: Card 스토리북 누락된 속성 추가
    Co-authored-by: youdame <youdame@users.noreply.github.com>

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -11,6 +11,6 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: { numbers: ['1345', '1092', '5678', '9299'] },
+  args: { numbers: ['1345', '1092', '5678', '9299'], cardLogo: 'visa', expiration: ['11', '20'] },
   render: (args) => <Card {...args} />
 };
