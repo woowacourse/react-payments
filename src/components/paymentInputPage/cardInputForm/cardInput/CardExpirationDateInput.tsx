b339891070ed0cc9549cec510c549db632387580
Jeongeun Lee <leeju1009@gmail.com>
2025-04-17 14:12:59 +0900

    feat: 카드 유효기간 검증 로직 추가
    Co-authored-by: dev-dino22 <dev-dino22@users.noreply.github.com>

--- src/components/paymentInputPage/cardInputForm/cardInput/CardExpirationDateInput.tsx
+++ src/components/paymentInputPage/cardInputForm/cardInput/CardExpirationDateInput.tsx
@@ -1,22 +1,45 @@
-import { Dispatch, SetStateAction } from 'react';
+import { useState } from 'react';
 import Input from '../../../common/inputForm/input/Input';
 import InputForm from '../../../common/inputForm/InputForm';
+import { validatorUtils } from '../../../../utils/validationUtils';
 
-function CardExpirationDateInput({
-  setExpirationDate,
-}: {
-  setExpirationDate: Dispatch<SetStateAction<string[]>>;
-}) {
+const numbersArray = Array.from({ length: 2 }).fill('') as string[];
+function CardExpirationDateInput({ setExpirationDate }) {
+  const [feedbackMessage, setFeedbackMessage] = useState<string>('');
+
   function onChangeHandler(
     e: React.ChangeEvent<HTMLInputElement>,
+    setIsValid: (state: boolean) => void,
     index: number
   ) {
     const expirationDate = e.target.value;
-    if (index === 0) {
-      //month관련 예외 처리
-    } else {
-      //year관련 예외 처리
+    if (!validatorUtils.isNumber(expirationDate)) {
+      setFeedbackMessage('숫자만 입력 가능합니다.');
+      setIsValid(false);
+      return;
     }
+    numbersArray[index] = expirationDate;
+    setExpirationDate([...numbersArray]);
+    if (
+      (index === 0 &&
+        !validatorUtils.isValidNumberRange(Number(expirationDate), 1, 12)) ||
+      !validatorUtils.isValidExpirationDate(numbersArray[0], numbersArray[1])
+    ) {
+      setFeedbackMessage('유효하지 않은 카드입니다. 유효 기간을 확인해주세요.');
+      setIsValid(false);
+      return;
+    }
+    if (
+      index === 1 &&
+      !validatorUtils.isValidExpirationDate(numbersArray[0], numbersArray[1])
+    ) {
+      setFeedbackMessage('유효하지 않은 카드입니다. 유효 기간을 확인해주세요.');
+      setIsValid(false);
+      return;
+    }
+
+    setFeedbackMessage('');
+    setIsValid(true);
   }
 
   const inputs = Array.from({ length: 2 }).map((_, index) => {
@@ -25,7 +48,7 @@ function CardExpirationDateInput({
         type='tel'
         name='cardExpirationDate'
         placeholder={index === 0 ? 'MM' : 'YY'}
-        onChange={(e) => onChangeHandler(e, index)}
+        onChange={(e, setIsValid) => onChangeHandler(e, setIsValid, index)}
         maxLength={2}
       />
     );
@@ -34,6 +57,7 @@ function CardExpirationDateInput({
   return (
     <>
       <InputForm
+        feedbackMessage={feedbackMessage}
         title='카드 유효기간을 입력해 주세요.'
         description='월/년도(MMYY)를 순서대로 입력해 주세요.'
         label='유효기간'
@@ -43,5 +67,4 @@ function CardExpirationDateInput({
     </>
   );
 }
-
 export default CardExpirationDateInput;
