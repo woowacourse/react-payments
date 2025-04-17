23755910eb09160412b45d7f1ed1b4446f212262
eunwoo-levi <eunwoo1341@gmail.com>
2025-04-17 14:39:36 +0900

    feat:expiration date 유효성 추가
    Co-authored-by: Claire <74090200+eunsoA@users.noreply.github.com>

--- src/features/cardInfo/validation/cardInfoValidator.ts
+++ src/features/cardInfo/validation/cardInfoValidator.ts
@@ -23,7 +23,7 @@ const isFourDigit = (cardNumber: number) => {
   return String(cardNumber).length === 4;
 };
 
-const isValidCardNumber = (input: string): boolean => {
+export const isValidCardNumber = (input: string): boolean => {
   if (input[0] === '4') return true;
   if (input[0] === '5') {
     const secondDigit = Number(input[1]);
@@ -36,12 +36,21 @@ export const cardExpirationDateValidator = (date: { month: string; year: string 
   if (!isNumber(date.month)) {
     return [0, '유효 월은 숫자만 입력 가능합니다.'];
   }
+
   if (!isValidExpirationMonth(date.month)) {
     return [0, '유효 월은 1월과 12월 사이만 입력 가능합니다.'];
   }
 
+  if (!isTwoDigit(date.month)) {
+    return [0, '유효 월은 2자리 숫자여야 합니다.'];
+  }
+
   if (!isNumber(date.year)) {
     return [1, '유효 연도는 숫자만 입력 가능합니다.'];
+  }
+
+  if (!isTwoDigit(date.year)) {
+    return [1, '유효 연도는 2자리 숫자여야 합니다.'];
   }
   return [-1, ''];
 };
@@ -49,6 +58,10 @@ export const cardExpirationDateValidator = (date: { month: string; year: string 
 const isValidExpirationMonth = (month: string) => {
   const num = Number(month);
   return num >= 1 && num <= 12;
+};
+
+const isTwoDigit = (input: string) => {
+  return input.length === 2;
 };
 
 export const cardCVCValidator = (input: string) => {
