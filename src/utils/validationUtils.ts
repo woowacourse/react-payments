b3c73c83c37c3dc2355ac5e32c37a55f003f890e
Jeongeun Lee <leeju1009@gmail.com>
2025-04-17 14:10:52 +0900

    refactor: 유효기간 검증 로직 수정
    Co-authored-by: dev-dino22 <dev-dino22@users.noreply.github.com>

--- src/utils/validationUtils.ts
+++ src/utils/validationUtils.ts
@@ -4,17 +4,24 @@ export const validatorUtils = {
   isValidLength: (value: string, length: number) => value.length === length,
   isValidNumberRange: (value: number, min: number, max: number) =>
     value >= min && value <= max,
-  isValidExpirationDate: (month: number, year: number) => {
+  isValidExpirationDate: (month: string, year: string) => {
+    const numberMonth = Number(month);
+    const numberYear = Number(year);
     const today = new Date();
     const currentFullYear = today.getFullYear();
     const currentYear = currentFullYear % 100;
     const currentMonth = today.getMonth() + 1;
 
-    if (currentYear < year) {
+    if (month === '' && numberYear === currentYear) {
       return true;
     }
-    if (currentYear === year) {
-      return month >= currentMonth;
+
+    if (currentYear < numberYear || year === '') {
+      return true;
+    }
+
+    if (currentYear === numberYear) {
+      return numberMonth >= currentMonth;
     }
     return false;
   },
