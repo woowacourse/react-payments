c6d9e1f4dd71cf68c9b1ae1309f68bc9a782511a
ahhyun <wndkgus0225@naver.com>
2025-04-18 00:08:03 +0900

    fix: 누락된 module.css 추가

--- src/components/InputTitle/InputTitle.tsx
+++ src/components/InputTitle/InputTitle.tsx
@@ -1,4 +1,4 @@
-import "./InputTitle.css";
+import styles from "./InputTitle.module.css";
 interface Props {
   inputValue: string;
 }
@@ -7,7 +7,7 @@ const CONSTANT_INPUT_TITLE = " 입력해 주세요." as const;
 
 export default function InputTitle({ inputValue }: Props) {
   return (
-    <div className="input-title">
+    <div className={styles["input-title"]}>
       <h2>
         {inputValue}
         {CONSTANT_INPUT_TITLE}
