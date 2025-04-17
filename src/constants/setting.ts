4dbe8cb2ddf4c61bac3114886d06f0aa7eed5777
Minje Sung <minje813@dgu.ac.kr>
2025-04-17 14:42:52 +0900

    fix: 이미지 경로 수정

--- src/constants/setting.ts
+++ src/constants/setting.ts
@@ -1,11 +1,11 @@
-export const MASTER_CARD_PREFIXES = ["51", "52", "53", "54", "55"];
+export const MASTER_CARD_PREFIXES = ['51', '52', '53', '54', '55'];
 
-export const VISA_CARD_PREFIXES = ["4"];
+export const VISA_CARD_PREFIXES = ['4'];
 
 export const CARD_TYPE_PATH = {
-  VISA: "/assets/Visa.png",
-  MasterCard: "/assets/Mastercard.png",
-  None: "",
+  VISA: './assets/Visa.png',
+  MasterCard: './assets/Mastercard.png',
+  None: '',
 };
 
 export const CARD_INFO_LENGTH = {
