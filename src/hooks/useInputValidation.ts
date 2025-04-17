7c6fe9e0398abacf741bb862abc7b077fb99a910
deokbin <deokbin@naver.com>
2025-04-17 13:31:57 +0900

    feat: 유효성 검증 커스텀 훅 추가
    Co-authored-by: rosielsh <tnghk9611@naver.com>

---
+++ src/hooks/useInputValidation.ts
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { HandleInputParams } from '../components/CardPage/CardPage';
+
+function useInputValidation(
+  initialErrorState: boolean[],
+  validationFn: (e: React.ChangeEvent<HTMLInputElement>) => void
+) {
+  const [isError, setIsError] = useState(initialErrorState);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const validate = ({ e, idx }: HandleInputParams) => {
+    try {
+      validationFn(e);
+
+      setIsError((prev) => {
+        const updated = [...prev];
+        updated[idx] = false;
+
+        if (updated.every((error) => error === false)) {
+          setErrorMessage('');
+        }
+
+        return updated;
+      });
+    } catch (error) {
+      setIsError((prev) => {
+        const updated = [...prev];
+        updated[idx] = true;
+        return updated;
+      });
+
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+      }
+    }
+  };
+
+  return {
+    isError,
+    errorMessage,
+    validate,
+  };
+}
+
+export default useInputValidation;
