49ca4ff848cdd442723160fa37aff772c904b8cc
mlnwns <mlnwns@naver.com>
2025-04-17 13:56:12 +0900

    feat: 카드 입력 컴포넌트 스타일 분리 및 재사용 가능하도록 수정
    Co-authored-by: Minje Sung <minje813@naver.com>

--- src/components/CardCVCForm.tsx
+++ src/components/CardCVCForm.tsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
 import isExactLength from "../utils/isExactLength";
-import styled from "@emotion/styled";
 import NumberInput from "./NumberInput";
 import { ERROR_MESSAGE } from "../constants/guide";
+import {
+  NumberInputForm,
+  Label,
+  NumberInputContainer,
+  ErrorText,
+} from "../styles/CardForm.styles";
 
 interface CardCVCFormProps {
   cardInfo: {
@@ -52,24 +57,3 @@ function CardCVCForm({
 }
 
 export default CardCVCForm;
-
-const Label = styled.p`
-  font-size: 12px;
-  font-weight: 500;
-  margin-bottom: 8px;
-`;
-
-const NumberInputContainer = styled.div`
-  display: flex;
-  gap: 10px;
-  margin-bottom: 8px;
-`;
-
-const ErrorText = styled.p`
-  color: #ff3d3d;
-  font-size: 9.5px;
-`;
-
-const NumberInputForm = styled.div`
-  height: 70px;
-`;
--- src/components/CardNumberForm.tsx
+++ src/components/CardNumberForm.tsx
@@ -1,8 +1,13 @@
-import styled from "@emotion/styled";
 import NumberInput from "./NumberInput";
 import { useState, useEffect } from "react";
 import isExactLength from "../utils/isExactLength";
 import { ERROR_MESSAGE } from "../constants/guide";
+import {
+  NumberInputForm,
+  Label,
+  NumberInputContainer,
+  ErrorText,
+} from "../styles/CardForm.styles";
 
 interface CardNumberFormProps {
   cardInfo: {
@@ -88,24 +93,3 @@ function CardNumberForm({
 }
 
 export default CardNumberForm;
-
-const Label = styled.p`
-  font-size: 12px;
-  font-weight: 500;
-  margin-bottom: 8px;
-`;
-
-const NumberInputContainer = styled.div`
-  display: flex;
-  gap: 10px;
-  margin-bottom: 8px;
-`;
-
-const ErrorText = styled.p`
-  color: #ff3d3d;
-  font-size: 9.5px;
-`;
-
-const NumberInputForm = styled.div`
-  height: 70px;
-`;
