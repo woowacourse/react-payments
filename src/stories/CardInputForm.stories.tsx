464572b08ff6cb15699c6a29eb59719e33629eac
Jeongeun Lee <leeju1009@gmail.com>
2025-04-17 17:07:39 +0900

    test: storybook 비주얼 테스트 추가
    Co-authored-by: dev-dino22 <dev-dino22@users.noreply.github.com>

---
+++ src/stories/CardInputForm.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+import CardInputForm from '../components/paymentInputPage/cardInputForm/CardInputForm';
+
+const meta = {
+  title: 'CardInputForm',
+  component: CardInputForm,
+  args: {
+    setCardNumbers: () => {},
+    setExpirationDate: () => {},
+  },
+} satisfies Meta<typeof CardInputForm>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => {
+    const [cardNumbers, setCardNumbers] = useState<string[]>([]);
+    const [expirationDate, setExpirationDate] = useState<string[]>([]);
+    return (
+      <CardInputForm
+        setCardNumbers={setCardNumbers}
+        setExpirationDate={setExpirationDate}
+      />
+    );
+  },
+};
