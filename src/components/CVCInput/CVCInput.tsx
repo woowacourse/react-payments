c4abe9e3d10a547f6e3ee6c715e217e63c5e3525
thgml05 <aisohii@naver.com>
2025-04-17 15:00:23 +0900

    refactor : CVCInput storybook 상수 오류 수정

--- src/components/CVCInput/CVCInput.tsx
+++ src/components/CVCInput/CVCInput.tsx
@@ -1,8 +1,8 @@
-import { useRef, useState } from "react";
-import InputContainer from "../InputContainer/InputContainer";
-import { validateCVC } from "../../domain/validate";
-import { INPUT_CONTAINER } from "../../constants/title";
-import { CARD_VALIDATION_INFO } from "../../constants/CardValidationInfo";
+import { useRef, useState } from 'react';
+import InputContainer from '../InputContainer/InputContainer';
+import { validateCVC } from '../../domain/validate';
+import { INPUT_CONTAINER } from '../../constants/title';
+import { CARD_VALIDATION_INFO } from '../../constants/CardValidationInfo';
 
 type CVCInputProps = {
   CVC: string;
@@ -10,14 +10,14 @@ type CVCInputProps = {
 };
 
 const CVCInput = ({ CVC, setCVC }: CVCInputProps) => {
-  const [helperText, setHelperText] = useState("");
+  const [helperText, setHelperText] = useState('');
   const inputRef = useRef<HTMLElement | null>(null);
 
   const handleCVC = (e: React.ChangeEvent<HTMLInputElement>) => {
     try {
       setCVC(e.target.value);
       validateCVC(e.target.value, 3);
-      setHelperText("");
+      setHelperText('');
     } catch (error: unknown) {
       if (error instanceof Error) {
         setHelperText(error.message);
@@ -38,7 +38,7 @@ const CVCInput = ({ CVC, setCVC }: CVCInputProps) => {
           ref={(element) => {
             inputRef.current = element;
           }}
-          className={`input ${helperText !== "" && "errorInput"}`}
+          className={`input ${helperText !== '' && 'errorInput'}`}
           maxLength={CARD_VALIDATION_INFO.CVC_MAX_LENGTH}
         />
       </div>
--- src/stories/CVCInput.stories.tsx
+++ src/stories/CVCInput.stories.tsx
@@ -1,15 +1,15 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import CVCInput from "../components/CVCInput/CVCInput";
-import { within, userEvent } from "@storybook/testing-library";
-import { expect } from "@storybook/jest";
-import { useState } from "react";
-import ERROR from "../constants/errorMessage";
-import { CARD_VALIDATION_INFO } from "../constants/CardValidationInfo";
+import type { Meta, StoryObj } from '@storybook/react';
+import CVCInput from '../components/CVCInput/CVCInput';
+import { within, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { useState } from 'react';
+import ERROR from '../constants/errorMessage';
+import { CARD_VALIDATION_INFO } from '../constants/CardValidationInfo';
 
 const meta: Meta<typeof CVCInput> = {
-  title: "Components/CVCInput",
+  title: 'Components/CVCInput',
   component: CVCInput,
-  tags: ["autodocs"],
+  tags: ['autodocs'],
 };
 
 export default meta;
@@ -17,7 +17,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 const Template = () => {
-  const [CVC, setCVC] = useState("");
+  const [CVC, setCVC] = useState('');
 
   return <CVCInput CVC={CVC} setCVC={setCVC} />;
 };
@@ -26,13 +26,13 @@ export const Invalid_NonNumeric: Story = {
   render: Template,
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    const CVCInput = canvas.getByPlaceholderText("123");
+    const CVCInput = canvas.getByPlaceholderText('123');
 
     await userEvent.clear(CVCInput);
-    await userEvent.type(CVCInput, "ab");
+    await userEvent.type(CVCInput, 'ab');
 
     await expect(
-      canvas.findByText(ERROR.REQUIRE.NUMBER),
+      canvas.findByText(ERROR.REQUIRE.NUMBER)
     ).resolves.toBeInTheDocument();
   },
 };
@@ -41,15 +41,15 @@ export const Invalid_NumberLength: Story = {
   render: Template,
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    const CVCInput = canvas.getByPlaceholderText("123");
+    const CVCInput = canvas.getByPlaceholderText('123');
 
     await userEvent.clear(CVCInput);
-    await userEvent.type(CVCInput, "12");
+    await userEvent.type(CVCInput, '12');
 
     await expect(
       canvas.findByText(
-        `${CARD_VALIDATION_INFO.CVC_MAX_LENGTH}${ERROR.REQUIRE}`,
-      ),
+        `${CARD_VALIDATION_INFO.CVC_MAX_LENGTH}${ERROR.REQUIRE.SPECIFIC_LENGTH}`
+      )
     ).resolves.toBeInTheDocument();
   },
 };
