5a7fb0464afce1bfc1af9771cca65cf92328faae
spoyodevelop <kjy9505@gmail.com>
2025-04-19 04:54:09 +0900

    feat: useAddCard 훅에 상태 및 반환 타입 추가

--- src/AddCard/hooks/useAddCard.ts
+++ src/AddCard/hooks/useAddCard.ts
@@ -1,8 +1,34 @@
 import useControlledCardNumber from "../components/AddCardForm/components/CardNumber/hooks/useControlledCardNumber";
 import useControlledCVC from "../components/AddCardForm/components/CVC/hooks/useControlledCVC";
 import useControlledExpireDate from "../components/AddCardForm/components/ExpireDate/hooks/useControlledExpireDate";
+import type {
+  CardNumberState,
+  CardNumberInputKey,
+} from "../components/AddCardForm/components/CardNumber/types";
+import type { ExpireDateState } from "../components/AddCardForm/components/ExpireDate/types";
 
-const useAddCard = () => {
+interface AddCardState {
+  cardNumberState: CardNumberState;
+  handleCardNumberChange: (key: CardNumberInputKey, value: string) => void;
+  expireDate: ExpireDateState;
+  handleExpireMonthChange: (value: string) => void;
+  handleExpireYearChange: (value: string) => void;
+  handleExpireMonthBlur: (value: string) => void;
+  CVCState: { value: string; errorMessage: string };
+  handleCVCChange: (value: string) => void;
+}
+
+interface PreviewState {
+  cardNumberState: CardNumberState;
+  expireDate: ExpireDateState;
+}
+
+interface UseAddCardReturn {
+  addCardState: AddCardState;
+  previewState: PreviewState;
+}
+
+const useAddCard = (): UseAddCardReturn => {
   const { cardNumberState, handleCardNumberChange } = useControlledCardNumber();
   const {
     expireDate,
