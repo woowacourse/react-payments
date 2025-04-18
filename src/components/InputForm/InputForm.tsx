58bf846b69d51fce7c5a8e29d63639cab25d6c3b
sooyeoniya <tndus1306403@naver.com>
2025-04-18 20:07:18 +0900

    refactor: 객체 타입 안정성을 위하여 반환 타입 추가

--- src/components/InputForm/InputForm.tsx
+++ src/components/InputForm/InputForm.tsx
@@ -10,19 +10,19 @@ export interface InputFormProps {
   type: InputType;
 }
 
-const titleVariants = {
+const titleVariants: Record<InputType, string> = {
   [INPUT_TYPE.cardNumbers]: "결제할 카드 번호를 입력해 주세요",
   [INPUT_TYPE.expirationPeriod]: "카드 유효기간을 입력해 주세요",
   [INPUT_TYPE.cvcNumber]: "CVC 번호를 입력해 주세요",
 };
 
-const descriptionVariants = {
+const descriptionVariants: Record<InputType, string | null> = {
   [INPUT_TYPE.cardNumbers]: "본인 명의의 카드만 결제 가능합니다.",
   [INPUT_TYPE.expirationPeriod]: "월/년도(MMYY)를 순서대로 입력해 주세요.",
   [INPUT_TYPE.cvcNumber]: null,
 };
 
-const subTitleVariants = {
+const subTitleVariants: Record<InputType, string> = {
   [INPUT_TYPE.cardNumbers]: "카드 번호",
   [INPUT_TYPE.expirationPeriod]: "유효기간",
   [INPUT_TYPE.cvcNumber]: "CVC",
