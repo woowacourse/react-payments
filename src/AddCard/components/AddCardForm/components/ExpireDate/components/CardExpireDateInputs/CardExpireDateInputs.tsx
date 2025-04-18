8576a7f63d135508eb0378be869b75b53035de1e
wo-o29 <woogur29@gmail.com>
2025-04-19 00:08:35 +0900

    ​​refactor: expireDate errorMessage가 있을 때만 렌더링하도록 수정
    Co-authored-by: spoyodevelop <73924592+spoyodevelop@users.noreply.github.com>

--- src/AddCard/components/AddCardForm/components/ExpireDate/components/CardExpireDateInputs/CardExpireDateInputs.tsx
+++ src/AddCard/components/AddCardForm/components/ExpireDate/components/CardExpireDateInputs/CardExpireDateInputs.tsx
@@ -3,6 +3,7 @@ import Input from "@components/Input/Input";
 import Label from "@components/Label/Label";
 import { EXPIRE_DATE_KEYS } from "../../constants";
 import type { ExpireDateState } from "../../types";
+import { Fragment } from "react/jsx-runtime";
 
 export interface CardExpireDateInputsProps {
   expireDate: ExpireDateState;
@@ -26,32 +27,36 @@ function CardExpireDateInputs({
     <div className={styles.container}>
       <div className={styles.expireDateInputContainer}>
         {EXPIRE_DATE_KEYS.map((expireKey, idx) => (
-          <p className={styles.expireDateInputBox} key={expireKey}>
-            <Label htmlFor={`expire-${expireKey}-input`} isHidden={idx !== 0}>
-              유효 기간
-            </Label>
-            <Input
-              id={`expire-${expireKey}-input`}
-              type="text"
-              inputMode="numeric"
-              maxLength={2}
-              placeholder={expireKey}
-              isError={Boolean(expireDate[expireKey].errorMessage)}
-              value={expireDate[expireKey].value}
-              onChange={(e) => changeEvent[expireKey](e.target.value)}
-              onBlur={
-                expireKey === "MM"
-                  ? (e) => handleExpireMonthBlur(e.target.value)
-                  : undefined
-              }
-            />
-            <span
-              id={`${expireKey}-error-message`}
-              className={styles.errorMessage}
-            >
-              {expireDate[expireKey].errorMessage}
-            </span>
-          </p>
+          <div className={styles.expireDateBox}>
+            <p className={styles.expireDateInputBox}>
+              <Label htmlFor={`expire-${expireKey}-input`} isHidden={idx !== 0}>
+                유효 기간
+              </Label>
+              <Input
+                id={`expire-${expireKey}-input`}
+                type="text"
+                inputMode="numeric"
+                maxLength={2}
+                placeholder={expireKey}
+                isError={Boolean(expireDate[expireKey].errorMessage)}
+                value={expireDate[expireKey].value}
+                onChange={(e) => changeEvent[expireKey](e.target.value)}
+                onBlur={
+                  expireKey === "MM"
+                    ? (e) => handleExpireMonthBlur(e.target.value)
+                    : undefined
+                }
+              />
+            </p>
+            {expireDate[expireKey].errorMessage && (
+              <span
+                id={`${expireKey}-error-message`}
+                className={styles.errorMessage}
+              >
+                {expireDate[expireKey].errorMessage}
+              </span>
+            )}
+          </div>
         ))}
       </div>
     </div>
