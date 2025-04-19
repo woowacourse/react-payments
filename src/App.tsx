a6fb5f2b2f0025e40f2cbf70e0dfc4243bbd8b50
bunju20 <ghkd2271@naver.com>
2025-04-19 23:57:45 +0900

    refactor(justifyBrandLogo): string 형변환 로직 추가:

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import './style.css';
 import './reset.css';
 import AddCard from './pages/AddCard';
 function App() {
-  return (
-    <>
-      <AddCard />
-    </>
-  );
+  return <AddCard />;
 }
 
 export default App;
--- src/pages/util/justifyBrandLogo.ts
+++ src/pages/util/justifyBrandLogo.ts
@@ -1,13 +1,11 @@
 export const justifyBrandLogo = (cardNumber: number) => {
-  if (cardNumber.toString().startsWith("0")) return "default";
-  if (cardNumber.toString().startsWith("4")) return "visa";
+  const cardNumberStr = cardNumber.toString();
+  if (cardNumberStr.startsWith('0')) return 'default';
+  if (cardNumberStr.startsWith('4')) return 'visa';
 
-  if (
-    cardNumber.toString().startsWith("51") ||
-    cardNumber.toString().startsWith("52")
-  )
-    return "mastercard";
-  if (cardNumber >= 2221 && cardNumber <= 2720) return "mastercard";
+  if (cardNumberStr.startsWith('51') || cardNumberStr.startsWith('52'))
+    return 'mastercard';
+  if (cardNumber >= 2221 && cardNumber <= 2720) return 'mastercard';
 
-  return "default";
+  return 'default';
 };
