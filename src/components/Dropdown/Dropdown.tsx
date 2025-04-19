8de4332658cad2f1d1d216c5236c9561481db728
wo-o29 <woogur29@gmail.com>
2025-04-19 15:30:13 +0900

    feat: Dropdown 컴포넌트 추가

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,46 @@
+import styles from "./Dropdown.module.css";
+import { useState } from "react";
+import useOutsideClick from "@/hooks/useOutsideClick";
+import ArrowIcon from "../ArrowIcon/ArrowIcon";
+
+interface DropdownProps {
+  dropdownList: string[];
+  buttonText: string;
+}
+
+function Dropdown({ dropdownList, buttonText }: DropdownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useOutsideClick<HTMLDivElement>(() => setIsOpen(false));
+
+  const handleDropdownToggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <div ref={dropdownRef} className={styles.container}>
+      <button
+        type="button"
+        onClick={handleDropdownToggle}
+        className={`${styles.button} ${isOpen ? styles.activeButton : ""}`}
+      >
+        {buttonText}
+        {isOpen ? (
+          <ArrowIcon direction="up" color="#000" />
+        ) : (
+          <ArrowIcon direction="down" />
+        )}
+      </button>
+      {isOpen && (
+        <ul className={styles.dropdownList}>
+          {dropdownList.map((text) => (
+            <li key={text} className={styles.dropdownListItem}>
+              {text}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default Dropdown;
