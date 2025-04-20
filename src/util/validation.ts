dc1aca9d4a4de1c3988f3aa037a0d8c89d340cc6
ha-kuku <hakukudh@gmail.com>
2025-04-20 19:46:15 +0900

    refactor: validation 조건 수정

--- src/util/validation.ts
+++ src/util/validation.ts
@@ -1,26 +1,29 @@
 const isValidLength = (number: string, maxLength: number) => {
-    if (number.length < maxLength) {
-        return false;
-    } return true;
-}
+  if (number.length < maxLength) {
+    return false;
+  }
+  return true;
+};
 
 const isValidNumber = (number: string) => {
-    const regex = /^[0-9]*$/;
-    if (regex.test(number)) {
-        return true;
-    } return false;
-}
+  if (Number.isInteger(number)) {
+    return true;
+  }
+  return false;
+};
 
 const isValidMonthRange = (number: string) => {
-    if (Number(number)<=12 && Number(number)>=1) {
-        return true;
-    } return false;
-}
+  if (1 <= Number(number) && Number(number) <= 12) {
+    return true;
+  }
+  return false;
+};
 
 const isValidYearRange = (number: string) => {
-    if (Number(number)<=99 && Number(number)>=25) {
-        return true;
-    } return false;
-}
+  if (Number(number) <= 99 && Number(number) >= 25) {
+    return true;
+  }
+  return false;
+};
 
-export { isValidLength, isValidNumber, isValidMonthRange, isValidYearRange}
+export { isValidLength, isValidNumber, isValidMonthRange, isValidYearRange };
