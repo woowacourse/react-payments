0e93bf7d3cf4492086ff27b20ac643800c5fc0d3
H0ngJu <hongju4385@gmail.com>
2025-04-20 22:59:58 +0900

    refactor: cvc 에러 상태 및 상수 네이밍 수정

--- src/hooks/useCvcNumber.tsx
+++ src/hooks/useCvcNumber.tsx
@@ -1,53 +1,42 @@
 import { useState } from "react";
 import { isNumber, isUnderMaxLength } from "../validation/validate";
-import { setErrorMessage } from "../utils/setErrorMessage";
 
-const CVC_NUMBER = {
+const CVC_NUMBER_LIMIT = {
   MAX_CVC_LENGTH: 3,
 } as const;
 
-const CVC_NUMBER_ERROR = {
-  INVALID_LENGTH_ERROR: `CVC 번호는 ${CVC_NUMBER.MAX_CVC_LENGTH}자리 숫자여야 합니다.`,
+const CVC_NUMBER_ERROR_MESSAGE = {
+  INVALID_LENGTH_ERROR: `CVC 번호는 ${CVC_NUMBER_LIMIT.MAX_CVC_LENGTH}자리 숫자여야 합니다.`,
   NOT_NUMBERIC_ERROR: "숫자만 입력 가능합니다.",
 };
 
-export default function useCvcNumber() {
+export default function useCvcNumberInput() {
   const [cvcNumbers, setCardCvcNumbers] = useState("");
-  const [cvcNumbersError, setError] = useState([""]);
+  const [cvcNumbersError, setError] = useState("");
 
   const validateCvcNumbers = (value: string) => {
-    if (!isUnderMaxLength(value.length, CVC_NUMBER.MAX_CVC_LENGTH)) {
-      setErrorMessage(
-        cvcNumbersError,
-        CVC_NUMBER_ERROR.INVALID_LENGTH_ERROR,
-        0,
-        setError
-      );
+    if (!isUnderMaxLength(value.length, CVC_NUMBER_LIMIT.MAX_CVC_LENGTH)) {
+      setError(CVC_NUMBER_ERROR_MESSAGE.INVALID_LENGTH_ERROR);
       return false;
     }
 
     if (!isNumber(value)) {
-      setErrorMessage(
-        cvcNumbersError,
-        CVC_NUMBER_ERROR.NOT_NUMBERIC_ERROR,
-        0,
-        setError
-      );
+      setError(CVC_NUMBER_ERROR_MESSAGE.NOT_NUMBERIC_ERROR);
       return false;
     }
 
-    setErrorMessage(cvcNumbersError, "", 0, setError);
+    setError("");
     return true;
   };
 
   const onCvcNumberChange = (value: string) => {
     if (!validateCvcNumbers(value)) return;
-    setCardCvcNumbers(value.slice(0, CVC_NUMBER.MAX_CVC_LENGTH));
+    setCardCvcNumbers(value.slice(0, CVC_NUMBER_LIMIT.MAX_CVC_LENGTH));
   };
 
   return {
     cvcNumbers,
-    cvcNumbersError: cvcNumbersError[0],
+    cvcNumbersError,
     onCvcNumberChange,
   };
 }
