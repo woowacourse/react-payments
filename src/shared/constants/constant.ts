ac68723de3c69f30d91f850c35803eee6ba50eb3
yeongipark <yeongi0111@naver.com>
2025-04-20 15:19:48 +0900

    chore: 상수 as const 대신 기존 타입 이용하여 표현

--- src/shared/constants/constant.ts
+++ src/shared/constants/constant.ts
@@ -1,17 +1,25 @@
+import {
+  CardNumberPosition,
+  ExpirationPeriod,
+} from "./../../types/index.types";
 export const NO_ERROR = "";
 
 export const INITIALIZE_VALUE = "";
 
-export const POSITION = {
+export const POSITION: Record<
+  "FIRST" | "SECOND" | "THIRD" | "FOURTH",
+  CardNumberPosition
+> = {
   FIRST: "first",
   SECOND: "second",
   THIRD: "third",
   FOURTH: "fourth",
-} as const;
+};
 
-type PositionValues = (typeof POSITION)[keyof typeof POSITION];
-
-export const EXPIRATION_PERIOD = {
+export const EXPIRATION_PERIOD: Record<
+  "MONTH" | "YEAR",
+  keyof ExpirationPeriod
+> = {
   MONTH: "month",
   YEAR: "year",
 } as const;
