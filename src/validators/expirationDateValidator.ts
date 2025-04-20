bde5c3083a0256ece09c274210779034fda9fed6
deokbin <deokbin@naver.com>
2025-04-20 17:07:19 +0900

    refactor: 월 및 연도 반환 util 함수 수정

--- src/validators/expirationDateValidator.ts
+++ src/validators/expirationDateValidator.ts
@@ -4,26 +4,28 @@ export const checkValidMonth = (value: string) => {
   if (Number(value) < 1 || Number(value) > 12) throw new Error(ERROR_MESSAGES.INVALID_MONTH_RANGE);
 };
 
-export const getCurrentYear = () => {
-  return new Date().getFullYear();
+export const getCurrentYear = (now: Date) => {
+  return now.getFullYear();
 };
 
-export const getCurrentMonth = () => {
-  return new Date().getMonth();
+export const getCurrentMonth = (now: Date) => {
+  return now.getMonth();
 };
 
 export const checkValidYear = (value: string) => {
-  const currentYear = getCurrentYear().toString().slice(2);
+  const now = new Date();
+  const currentYear = getCurrentYear(now) % 1000;
   if (Number(value) < Number(currentYear)) {
     throw new Error(ERROR_MESSAGES.INVALID_YEAR);
   }
 };
 
 export const checkTotalExpirationDate = (month: string, year: string) => {
-  const currentYear = getCurrentYear().toString().slice(2);
-  const currentMonth = getCurrentMonth() + 1;
+  const now = new Date();
+  const currentYear = getCurrentYear(now) % 1000;
+  const currentMonth = getCurrentMonth(now) + 1;
 
-  if (currentYear === year && Number(month) < currentMonth) {
+  if (currentYear === Number(year) && Number(month) < currentMonth) {
     throw new Error(ERROR_MESSAGES.INVALID_EXPIRATION);
   }
 };
