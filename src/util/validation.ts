dfb1a50128ffd528d5ce3503b34bae9d07bd6698
yeongipark <yeongi0111@naver.com>
2025-04-20 15:28:42 +0900

    chore: validation 상수화

--- src/util/validation.ts
+++ src/util/validation.ts
@@ -1,3 +1,13 @@
+const MONTH_RANGE = {
+  MIN: 1,
+  MAX: 12,
+};
+
+const YEAR_RANGE = {
+  MIN: 25,
+  MAX: 99,
+};
+
 const isValidLength = (number: string, maxLength: number) => {
   return number.length >= maxLength;
 };
@@ -8,11 +18,11 @@ const isValidNumber = (number: string) => {
 };
 
 const isValidMonthRange = (number: string) => {
-  return Number(number) >= 1 && Number(number) <= 12;
+  return Number(number) >= MONTH_RANGE.MIN && Number(number) <= MONTH_RANGE.MAX;
 };
 
 const isValidYearRange = (number: string) => {
-  return Number(number) >= 25 && Number(number) <= 99;
+  return Number(number) >= YEAR_RANGE.MIN && Number(number) <= YEAR_RANGE.MAX;
 };
 
 export { isValidLength, isValidNumber, isValidMonthRange, isValidYearRange };
