9636300e4fa8cb0d0274c5cb209647f8a6dbe622
yeji0214 <1225yj50@naver.com>
2025-04-20 18:58:56 +0900

    refactor: InputContainer props 타입 네이밍 변경

--- src/components/InputContainer/InputContainer.tsx
+++ src/components/InputContainer/InputContainer.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "./InputContainer.module.css";
 
-type propsType = {
+type InputContainerProps = {
   children: React.ReactNode;
   title: string;
   subTitle?: string;
 };
 
-const InputContainer = ({ children, title, subTitle }: propsType) => {
+const InputContainer = ({ children, title, subTitle }: InputContainerProps) => {
   return (
     <div className={styles.inputContainer}>
       <h2 className={styles.inputTitle}>{title}</h2>
