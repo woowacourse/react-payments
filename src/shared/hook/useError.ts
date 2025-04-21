d02a40e2a46eb9eb06f3d1e19d3058953d0a5bc0
yeongipark <yeongi0111@naver.com>
2025-04-21 17:18:03 +0900

    refactor: useError 훅 만들어서 에러 처리 로직 추상화

---
+++ src/shared/hook/useError.ts
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { NO_ERROR } from "../constants/constant";
+
+type checkValidationType<T> = {
+  length: number;
+  value: string;
+  type: keyof T;
+};
+
+type getValidationFnsType = (
+  length: number,
+  value: string
+) => { condition: () => boolean; errorMsg: string }[];
+
+export default function useError<T extends {}>(
+  initError: T,
+  getValidationFns: getValidationFnsType
+) {
+  const [error, setError] = useState<T>(initError);
+
+  function checkValidation({ length, value, type }: checkValidationType<T>) {
+    const validationFns = getValidationFns(length, value);
+    const validation = validationFns.find((v) => v.condition());
+    setError((prev) => {
+      return {
+        ...prev,
+        [type]: validation?.errorMsg || NO_ERROR,
+      };
+    });
+  }
+
+  function getErrorMessage() {
+    for (const key in error) {
+      const typedKey = key as keyof typeof error;
+      if (error[typedKey] !== NO_ERROR) {
+        return error[typedKey];
+      }
+    }
+  }
+
+  return { error, checkValidation, getErrorMessage };
+}
