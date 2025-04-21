d5297ad03c4e4e3d4f48d7d9e33c5b71a817961a
deokbin <deokbin@naver.com>
2025-04-21 11:53:04 +0900

    feat: 유효 기간 관련 유효성 검사 묶는 함수 추가

--- src/validators/expirationDateValidator.ts
+++ src/validators/expirationDateValidator.ts
@@ -1,31 +1,60 @@
+import { HandleInputParams } from '../components/CardPage/CardPage';
 import { ERROR_MESSAGES } from './errorMessages';
+import { checkNumber, checkValidLength } from './inputValidator';
 
 export const checkValidMonth = (value: string) => {
   if (Number(value) < 1 || Number(value) > 12) throw new Error(ERROR_MESSAGES.INVALID_MONTH_RANGE);
 };
 
-export const getCurrentYear = (now: Date) => {
-  return now.getFullYear();
+export const getCurrentYear = (date = new Date()) => {
+  return date.getFullYear();
 };
 
-export const getCurrentMonth = (now: Date) => {
-  return now.getMonth();
+export const getCurrentMonth = (date = new Date()) => {
+  return date.getMonth();
 };
 
 export const checkValidYear = (value: string) => {
-  const now = new Date();
-  const currentYear = getCurrentYear(now) % 1000;
+  const currentYear = getCurrentYear() % 1000;
   if (Number(value) < Number(currentYear)) {
     throw new Error(ERROR_MESSAGES.INVALID_YEAR);
   }
 };
 
 export const checkTotalExpirationDate = (month: string, year: string) => {
-  const now = new Date();
-  const currentYear = getCurrentYear(now) % 1000;
-  const currentMonth = getCurrentMonth(now) + 1;
+  const currentYear = getCurrentYear() % 1000;
+  const currentMonth = getCurrentMonth() + 1;
 
   if (currentYear === Number(year) && Number(month) < currentMonth) {
     throw new Error(ERROR_MESSAGES.INVALID_EXPIRATION);
+  }
+};
+
+export const expDateValidation = (
+  values: string[],
+  { e, idx }: HandleInputParams,
+  validLength: number
+) => {
+  const expDateValue = e.target.value;
+
+  checkNumber(expDateValue);
+
+  if (idx === 0 && expDateValue.length === 1) {
+    checkValidLength(expDateValue.padStart(2, '0'), validLength);
+  } else {
+    checkValidLength(expDateValue, validLength);
+  }
+
+  if (idx === 0) {
+    checkValidMonth(expDateValue);
+  } else if (idx === 1) {
+    checkValidYear(expDateValue);
+  }
+
+  const updatedValues = [...values];
+  updatedValues[idx] = expDateValue;
+
+  if (updatedValues[0] && updatedValues[1]) {
+    checkTotalExpirationDate(updatedValues[0], updatedValues[1]);
   }
 };
