7f94b1f2317b7aedcbe5caad3418afc8994b5efb
aydenote <dslgpgh@gmail.com>
2025-04-21 18:33:56 +0900

    refactor: maxLength 값 전달 방식 수정

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 	return (
 		<BrowserRouter basename="/react-payments/">
 			<Routes>
-				<Route path="/123" element={<CardInfo />} />
+				<Route path="/" element={<CardInfo />} />
 				<Route path="/addSuccess" element={<CardCompletePage />} />
 			</Routes>
 		</BrowserRouter>
