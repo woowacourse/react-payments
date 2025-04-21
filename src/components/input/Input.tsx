1827a733d22470c948cb4467cd5e17b5504a6a41
aydenote <dslgpgh@gmail.com>
2025-04-21 20:21:24 +0900

    refactor: onChange prop을 옵셔널 변경

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -4,14 +4,23 @@ import styled from "styled-components";
 type Props = {
 	placeholder?: string;
 	isError?: boolean;
-	value: string;
+	value?: string;
 	maxLength?: number;
-	onChange: (value: string) => void;
+	onChange?: (value: string) => void;
 	onBlur?: (value: string) => void;
 };
 
 const Input = ({ placeholder, isError = false, value, maxLength, onChange, onBlur }: Props) => {
-	return <TextInput maxLength={maxLength} value={value} placeholder={placeholder} onChange={(e) => onChange(e.target.value)} onBlur={onBlur && ((e) => onBlur(e.target.value))} $isError={isError} />;
+	return (
+		<TextInput
+			maxLength={maxLength}
+			value={value}
+			placeholder={placeholder}
+			onChange={onChange && ((e) => onChange(e.target.value))}
+			onBlur={onBlur && ((e) => onBlur(e.target.value))}
+			$isError={isError}
+		/>
+	);
 };
 
 const TextInput = styled.input<{ $isError: boolean }>(
