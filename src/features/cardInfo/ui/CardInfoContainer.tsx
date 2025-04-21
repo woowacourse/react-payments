2d7d06393a6deba637b348fd216eabe9664ac160
Oliy <eunsoa0310@gmail.com>
2025-04-21 15:12:31 +0900

    refactor: cardInfoSectionData를 cardInfoSectionSpec으로 이름 변경

--- src/features/cardInfo/ui/CardInfoContainer.tsx
+++ src/features/cardInfo/ui/CardInfoContainer.tsx
@@ -1,6 +1,6 @@
 import './cardInfoContainer.css';
 import CardInfoSection from './CardInfoSection';
-import { cardInfoSectionData } from '../data/cardInfoSectionData';
+import { cardInfoSectionSpec } from '../data/cardInfoSectionSpec';
 import { ErrorProps } from '../../../shared/type/types';
 
 export default function CardInfoContainer({
@@ -12,7 +12,7 @@ export default function CardInfoContainer({
 }) {
   return (
     <div className="card-info-container">
-      {cardInfoSectionData.map((data, index) => (
+      {cardInfoSectionSpec.map((data, index) => (
         <CardInfoSection key={`data.title-${index}`} {...data} onChange={onChange} error={error} />
       ))}
     </div>
