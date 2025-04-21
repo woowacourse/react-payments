363d373f0ff5278a9a539c7f1092d016c717abf5
aydenote <dslgpgh@gmail.com>
2025-04-21 17:16:35 +0900

    refactor: 변수명 수정

--- src/components/form/ExpirationDate.tsx
+++ src/components/form/ExpirationDate.tsx
@@ -9,6 +9,7 @@ import useAutoFocus from "../../hooks/useAutoFocus";
 import { EXPIRATION_MAX_LENGTH } from "../../utils/validation";
 import styled from "styled-components";
 
+<<<<<<< HEAD
 interface ExpirationDateProps {
 	expirationDate: ExpirationDateType;
 	onChange: (order: keyof ExpirationDateType, value: string) => void;
@@ -26,6 +27,73 @@ const ExpirationDate = React.memo(({ expirationDate, onChange, onBlur, error }: 
 		year: "YY",
 	};
 
+=======
+const INPUT_MAX_LENGTH = 2;
+
+type Props = {
+	expirationDate: date;
+	setExpirationDate: Dispatch<SetStateAction<date>>;
+};
+
+const ExpirationDate = ({ expirationDate, setExpirationDate }: Props) => {
+	const [error, setError] = useState({
+		month: "",
+		year: "",
+	});
+
+	const CURRENT_YEAR = new Date().getFullYear() % 100;
+	const orderLabels = ["month", "year"] as const;
+	const placeholderMap = {
+		month: "MM",
+		year: "YY",
+	};
+
+	const onChange = (order: keyof date, value: string) => {
+		setExpirationDate({ ...expirationDate, [order]: value });
+
+		if (!isNumberWithinRange(value, INPUT_MAX_LENGTH)) {
+			setError({ ...error, [order]: MESSAGE.INVALID_NUMBER });
+			return;
+		}
+
+		if (order === "month") {
+			const month = Number(value);
+			if (month < 0 || month > 12) {
+				setError({ ...error, month: MESSAGE.MONTH_RANGE });
+				return;
+			}
+		}
+
+		if (order === "year") {
+			const year = Number(value);
+			if (year < CURRENT_YEAR && year >= 0) {
+				setError({ ...error, year: MESSAGE.YEAR_RANGE(CURRENT_YEAR) });
+				return;
+			}
+		}
+
+		setError({ ...error, [order]: "" });
+	};
+
+	const onBlur = (order: keyof date, value: string) => {
+		if (value.length < INPUT_MAX_LENGTH) setError({ ...error, [order]: MESSAGE.MONTH_FORMAT });
+	};
+
+	const inputs = orderLabels.map((label: keyof date) => {
+		return (
+			<Input
+				key={label}
+				isError={!!error[label]}
+				placeholder={placeholderMap[label]}
+				value={expirationDate[label]}
+				maxLength={INPUT_MAX_LENGTH}
+				onChange={(numbers) => onChange(label, numbers)}
+				onBlur={(numbers) => onBlur(label, numbers)}
+			/>
+		);
+	});
+
+>>>>>>> refactor: 변수명 수정
 	return (
 		<CardNumberWrap>
 			<Title>카드 유효기간을 입력해 주세요</Title>
