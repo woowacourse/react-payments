40cc155e8b73d903d33c1431401547ea81669266
minji2219 <aabb0219@naver.com>
2025-04-21 22:30:29 +0900

    refactor: ComponentProps를 이용하여 타입 확장

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,30 +1,35 @@
-import { COLORS } from "../../styles/colors";
-import styled from "styled-components";
+import {InputHTMLAttributes} from 'react';
+import {COLORS} from '../../styles/colors';
+import styled from 'styled-components';
 
 type Props = {
-	placeholder?: string;
-	isError?: boolean;
-	value: string;
-	maxLength?: number;
-	handleInput: (value: string) => void;
-	handleFocusout?: (value: string) => void;
+  isError?: boolean;
+  handleInput: (value: string) => void;
+  handleFocusout?: (value: string) => void;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+const Input = ({
+  placeholder,
+  isError = false,
+  value,
+  maxLength,
+  handleInput,
+  handleFocusout,
+}: Props) => {
+  return (
+    <TextInput
+      maxLength={maxLength && maxLength}
+      value={value}
+      placeholder={placeholder}
+      onChange={(e) => handleInput(e.target.value)}
+      onBlur={handleFocusout && ((e) => handleFocusout(e.target.value))}
+      $isError={isError}
+    />
+  );
 };
 
-const Input = ({ placeholder, isError = false, value, maxLength, handleInput, handleFocusout }: Props) => {
-	return (
-		<TextInput
-			maxLength={maxLength && maxLength}
-			value={value}
-			placeholder={placeholder}
-			onChange={(e) => handleInput(e.target.value)}
-			onBlur={handleFocusout && ((e) => handleFocusout(e.target.value))}
-			$isError={isError}
-		/>
-	);
-};
-
-const TextInput = styled.input<{ $isError: boolean }>(
-	(props) => `
+const TextInput = styled.input<{$isError: boolean}>(
+  (props) => `
 	width:100%;
 	padding: 8px;
 	border-radius: 2px;
@@ -32,7 +37,7 @@ const TextInput = styled.input<{ $isError: boolean }>(
 	outline: 1px solid ${props.$isError ? COLORS.ERROR : COLORS.LIGHT_GRAY};
   
 	&:focus {
-		outline: 1px solid ${props.$isError ? COLORS.ERROR : "#000"};
+		outline: 1px solid ${props.$isError ? COLORS.ERROR : '#000'};
 	}
 `
 );
