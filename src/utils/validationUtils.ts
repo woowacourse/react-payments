c659c7f798d48cf9a96418bfbcdb8df799f1c982
Jeongeun Lee <leeju1009@gmail.com>
2025-04-21 21:11:26 +0900

    refactor: 유효기간과 관련된 예외 처리 유틸 로직을 수정
    - isValidNumberRange가 월, 연도 모두에 사용될 수 있도록 범용성 향상
    
    - isValidExpirationDate가 서로 다른 값 (month, year)의 빈 값을 고려하지 않도록 수정

--- src/utils/validationUtils.ts
+++ src/utils/validationUtils.ts
@@ -2,27 +2,26 @@ export const validatorUtils = {
   isNotEmpty: (value: string) => value.trim() !== '',
   isNumber: (value: string) => !Number.isNaN(Number(value)),
   isValidLength: (value: string, length: number) => value.length === length,
-  isValidNumberRange: (value: number, min: number, max: number) =>
-    value >= min && value <= max,
+  isValidNumberRange: ({
+    value,
+    min = Number.NEGATIVE_INFINITY,
+    max = Number.POSITIVE_INFINITY,
+  }: {
+    value: number;
+    min?: number;
+    max?: number;
+  }) => value >= min && value <= max,
   isValidExpirationDate: ({ month, year }: { month: string; year: string }) => {
-    const numberMonth = Number(month);
-    const numberYear = Number(year);
     const today = new Date();
-    const currentFullYear = today.getFullYear();
-    const currentYear = currentFullYear % 100;
+    const currentYear = today.getFullYear();
     const currentMonth = today.getMonth() + 1;
 
-    if (month === '' && numberYear === currentYear) {
-      return true;
-    }
+    const expirationYear = 2000 + Number(year);
+    const expirationMonth = Number(month);
 
-    if (currentYear < numberYear || year === '') {
-      return true;
-    }
-
-    if (currentYear === numberYear) {
-      return numberMonth >= currentMonth;
-    }
-    return false;
+    return (
+      expirationYear > currentYear ||
+      (expirationYear === currentYear && expirationMonth >= currentMonth)
+    );
   },
 };
