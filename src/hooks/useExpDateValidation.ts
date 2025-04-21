ef17867951fd9b484676c66581a2dae5ba3c9f75
deokbin <deokbin@naver.com>
2025-04-21 14:14:49 +0900

    refactor: 에러 배열 상태관리 customHook 적용

--- src/hooks/useExpDateValidation.ts
+++ src/hooks/useExpDateValidation.ts
@@ -1,37 +1,26 @@
 import { useState } from 'react';
 import { HandleInputParams } from '../components/CardPage/CardPage';
-import { ERROR_MESSAGES } from '../validators/errorMessages';
+import useErrorArrayState from './useErrorStateUpdate';
 
 function useExpDateValidation(
   initialErrorState: boolean[],
   values: string[],
   validationFn: (values: string[], params: HandleInputParams, validLength: number) => void
 ) {
-  const [isError, setIsError] = useState(initialErrorState);
+  const { isError, updateErrorState } = useErrorArrayState(initialErrorState.length, () =>
+    setErrorMessage('')
+  );
+
   const [errorMessage, setErrorMessage] = useState('');
 
   const validateExpirationDate = (params: HandleInputParams) => {
-    const newErrorState = [...isError];
-    newErrorState[params.idx] = false;
-
     try {
       validationFn(values, params, 2);
-
-      if (newErrorState.every((state) => !state)) {
-        setErrorMessage('');
-      }
-      setIsError(newErrorState);
+      updateErrorState(params.idx, false);
     } catch (error) {
+      updateErrorState(params.idx, true);
       if (error instanceof Error) {
-        if (error.message === ERROR_MESSAGES.INVALID_EXPIRATION) {
-          newErrorState[0] = true;
-          setIsError(newErrorState);
-          setErrorMessage(error.message);
-        } else {
-          newErrorState[params.idx] = true;
-          setIsError(newErrorState);
-          setErrorMessage(error.message);
-        }
+        setErrorMessage(error.message);
       }
     }
   };
--- src/hooks/useInputValidation.ts
+++ src/hooks/useInputValidation.ts
@@ -1,34 +1,22 @@
 import { useState } from 'react';
 import { HandleInputParams } from '../components/CardPage/CardPage';
+import useErrorArrayState from './useErrorStateUpdate';
 
 function useInputValidation(
   initialErrorState: boolean[],
   validationFn: (e: React.ChangeEvent<HTMLInputElement>) => void
 ) {
-  const [isError, setIsError] = useState(initialErrorState);
+  const { isError, updateErrorState } = useErrorArrayState(initialErrorState.length, () =>
+    setErrorMessage('')
+  );
   const [errorMessage, setErrorMessage] = useState('');
 
   const validate = ({ e, idx }: HandleInputParams) => {
     try {
       validationFn(e);
-
-      setIsError((prev) => {
-        const updated = [...prev];
-        updated[idx] = false;
-
-        if (updated.every((error) => error === false)) {
-          setErrorMessage('');
-        }
-
-        return updated;
-      });
+      updateErrorState(idx, false);
     } catch (error) {
-      setIsError((prev) => {
-        const updated = [...prev];
-        updated[idx] = true;
-        return updated;
-      });
-
+      updateErrorState(idx, true);
       if (error instanceof Error) {
         setErrorMessage(error.message);
       }
