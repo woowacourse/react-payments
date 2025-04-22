2eaf551f439d17a4b35abf24c0bdac932d865f12
H0ngJu <hongju4385@gmail.com>
2025-04-22 22:50:07 +0900

    feat(cardPassword): 비밀번호 필드 추가

---
+++ src/CardPassword/CardPassword.tsx
@@ -0,0 +1,38 @@
+import styles from "./CardPassword.module.css";
+import Input from "../components/Input/Input";
+import Text from "../components/Text/Text";
+import { useState } from "react";
+
+interface CardPasswordProps {
+  handleChange: (value: string) => void;
+}
+
+const CARD_PASSWORD_LABEL = {
+  TITLE: "비밀번호를 입력해주세요",
+  DESCRIPTION: "앞의 2자리를 입력해주세요",
+  SUBTITLE: "비밀번호 앞 2자리",
+} as const;
+
+export default function CardPassword({ handleChange }: CardPasswordProps) {
+  const [password, setPassword] = useState("");
+
+  const onPasswordChange = (value: string) => {
+    setPassword(value);
+  };
+
+  return (
+    <section className={styles["card-password"]}>
+      <Text textType="title">{CARD_PASSWORD_LABEL.TITLE}</Text>
+      <Text textType="description">{CARD_PASSWORD_LABEL.DESCRIPTION}</Text>
+      <Text textType="subtitle">{CARD_PASSWORD_LABEL.SUBTITLE}</Text>
+      <Input
+        onChange={onPasswordChange}
+        value={password}
+        textType="password"
+        placeholder=""
+        // errorMessage={errorMessage}
+      />
+      <Text textType="error">{""}</Text>
+    </section>
+  );
+}
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,16 +1,20 @@
 import styles from "./Input.module.css";
 
 type InputProps = {
+  textType?: TextType;
   onChange: (value: string) => void;
   value: string;
   placeholder?: string;
   errorMessage?: string;
 };
 
+export type TextType = "text" | "password";
+
 const DEFAULT_PLACEHOLDER = "1234";
 const DEFAULT_ERROR_MESSAGE = "";
 
 export default function Input({
+  textType = "text",
   value,
   onChange,
   placeholder = DEFAULT_PLACEHOLDER,
@@ -22,9 +26,10 @@ export default function Input({
 
   return (
     <input
-      className={`${styles["input-number"]} ${
+      className={`${styles[`input-text`]} ${
         errorMessage ? styles["error"] : DEFAULT_ERROR_MESSAGE
       }`}
+      type={textType === "password" ? "password" : ""}
       placeholder={placeholder}
       value={value}
       onChange={handleInputChange}
