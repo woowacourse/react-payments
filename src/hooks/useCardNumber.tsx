56f84e7f9b270b4e405da9cbb15d89dd2fd98d96
dlsxjzld <sinjuk1@naver.com>
2025-04-22 22:23:31 +0900

    refactor: 카드 번호 관련 상수들을 별도의 파일로 분리

--- src/hooks/useCardNumber.tsx
+++ src/hooks/useCardNumber.tsx
@@ -1,16 +1,9 @@
 import { useState } from 'react';
-
-const PARSE_RULE = {
-  length: 4,
-} as const;
-
-const CARD_NUMBER_RULE = {
-  min: 0,
-} as const;
-
-const ERROR_MESSAGE = {
-  CARD_NUMBER_LENGTH: '카드 번호는 16자리입니다.',
-} as const;
+import {
+  PARSE_RULE,
+  CARD_NUMBER_RULE,
+  ERROR_MESSAGE,
+} from '../constants/cardNumber';
 
 function useCardNumber() {
   const [cardNumber, setCardNumber] = useState(['', '', '', '']);
