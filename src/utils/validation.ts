a48ae882465aed1dc1857380f5196b01d91ee93c
sooyeoniya <tndus1306403@naver.com>
2025-04-23 01:54:05 +0900

    feat: isEmpty 유효성 로직 구현
    - select 태그의 value가 비어있는 경우를 확인하기 위한 유효성 검증 로직

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,3 +1,7 @@
 export const isNotNumber = (value: string): boolean => {
   return Number.isNaN(Number(value));
 };
+
+export const isEmpty = (value: string): boolean => {
+  return value === "";
+};
