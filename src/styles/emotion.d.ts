02338e62ea29105754e1074d786d4b7333cf01c3
장정안 <wjddks96@naver.com>
2025-04-23 15:16:17 +0900

    feat: emotion.d.t.s 내 fontType 설정

--- src/styles/emotion.d.ts
+++ src/styles/emotion.d.ts
@@ -1,8 +1,9 @@
 import '@emotion/react';
-import { ColorType } from './theme';
+import { ColorType, FontType } from './theme';
 
 declare module '@emotion/react' {
   export interface Theme {
     color: ColorType;
+    font: FontType;
   }
 }
