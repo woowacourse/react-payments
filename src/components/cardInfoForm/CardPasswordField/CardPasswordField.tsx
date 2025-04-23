3806c36046aa2940c3f2b31deb4711f3d65e4a4b
kimyou1102 <yoosk5485@naver.com>
2025-04-23 21:24:32 +0900

    feat: CardPasswordField 컴포넌트 생성

---
+++ src/components/cardInfoForm/CardPasswordField/CardPasswordField.tsx
@@ -0,0 +1,41 @@
+import styled from '@emotion/styled';
+import Input from '../../common/Input/Input';
+
+interface CardPasswordFieldProps {
+  cardPassword: string;
+  isError: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function CardPasswordField({
+  isError,
+  cardPassword,
+  onChange,
+}: CardPasswordFieldProps) {
+  return (
+    <div>
+      <Label htmlFor="cardPassword">비밀번호 앞 2자리</Label>
+      <InputWrapper>
+        <Input
+          isError={isError}
+          type="password"
+          name="cardPassword"
+          id="cardPassword"
+          value={cardPassword}
+          onChange={onChange}
+        />
+      </InputWrapper>
+    </div>
+  );
+}
+
+export default CardPasswordField;
+
+const InputWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-top: 8px;
+`;
+const Label = styled.label`
+  font-size: 12px;
+`;
