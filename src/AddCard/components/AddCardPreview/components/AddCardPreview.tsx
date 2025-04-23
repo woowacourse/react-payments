d88870f24a7216fbf10e4c233341843769a34ceb
spoyodevelop <kjy9505@gmail.com>
2025-04-23 11:01:09 +0900

    feat: 배경색에 따라 텍스트 컬러 조정 적용

--- src/AddCard/components/AddCardPreview/components/AddCardPreview.tsx
+++ src/AddCard/components/AddCardPreview/components/AddCardPreview.tsx
@@ -2,7 +2,8 @@ import styles from "./AddCardPreview.module.css";
 import type { CardNumberState } from "../../AddCardForm/components/CardNumber/types";
 import type { ExpireDateState } from "../../AddCardForm/components/ExpireDate/types";
 import Dot from "@components/Dot/Dot";
-import { getCardBrand } from "../utils";
+import getCardBrand from "../utils/getCardBrand";
+import getTextColorForBackground from "../utils/getTextColorForBackground";
 import { CARD_BRAND_IMAGES } from "../constants";
 import { Brand } from "../../AddCardForm/components/CardBrand/types";
 import { CARD_BRAND_COLOR } from "../../AddCardForm/components/CardBrand/constants";
@@ -19,8 +20,11 @@ function AddCardPreview({
   selectedBrand,
 }: AddCardPreviewProps) {
   const cardBrand = getCardBrand(cardNumberState.first.value);
-  const cardColor = selectedBrand ? CARD_BRAND_COLOR[selectedBrand] : "#A0A0A0";
 
+  const cardColor = selectedBrand ? CARD_BRAND_COLOR[selectedBrand] : "#A0A0A0";
+  const textColor = selectedBrand
+    ? getTextColorForBackground(CARD_BRAND_COLOR[selectedBrand])
+    : "#FFFFFF";
   return (
     <div className={styles.previewCard} style={{ backgroundColor: cardColor }}>
       <div className={styles.previewHeader}>
@@ -31,20 +35,20 @@ function AddCardPreview({
           </p>
         )}
       </div>
-      <div className={styles.cardNumber}>
+      <div className={styles.cardNumber} style={{ color: textColor }}>
         <span>{cardNumberState.first.value}</span>
         <span>{cardNumberState.second.value}</span>
         <span className={styles.dots}>
           {Array.from({ length: cardNumberState.third.value.length }).map(
             (_, index) => (
-              <Dot key={index} />
+              <Dot key={index} style={{ backgroundColor: textColor }} />
             )
           )}
         </span>
         <span className={styles.dots}>
           {Array.from({ length: cardNumberState.fourth.value.length }).map(
             (_, index) => (
-              <Dot key={index} />
+              <Dot key={index} style={{ backgroundColor: textColor }} />
             )
           )}
         </span>
