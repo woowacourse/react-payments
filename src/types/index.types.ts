b90c53de7dca3a88d37b1abb5ca3eb89d4d4e4f3
yeongipark <yeongi0111@naver.com>
2025-04-23 10:56:23 +0900

    chore: 사용하지 않는 타입 삭제

--- src/types/index.types.ts
+++ src/types/index.types.ts
@@ -1,8 +1,3 @@
 export type CardNumberPosition = "first" | "second" | "third" | "fourth";
 
 export type ExpirationPeriod = "month" | "year";
-
-export type CardCVCNumberSectionProps = {
-  CVCNumber: string;
-  changeCVCNumber: (CVCNumber: string) => void;
-};
