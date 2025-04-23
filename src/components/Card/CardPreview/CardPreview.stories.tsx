57dced79d6e0ec348e033aef665395220b447981
sooyeoniya <tndus1306403@naver.com>
2025-04-23 16:46:56 +0900

    feat: cardCompany 관련 Preview Story 추가

--- src/components/Card/CardPreview/CardPreview.stories.tsx
+++ src/components/Card/CardPreview/CardPreview.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import CardPreview from "./CardPreview";
 import { withCustomCardProvider } from "../../../../.storybook/utils/withCustomCardProvider";
+import { CARD_COMPANY } from "../../../constants/constants";
 
 const meta = {
   title: "CardPreview",
@@ -12,8 +13,25 @@ export default meta;
 
 type Story = StoryObj<typeof CardPreview>;
 
-export const Empty: Story = {
+export const NoneCompanyCard: Story = {
   decorators: [withCustomCardProvider({})],
+};
+
+export const PartiallyMaskedCard: Story = {
+  decorators: [
+    withCustomCardProvider({
+      cardNumbers: {
+        first: "1234",
+        second: "5678",
+        third: "91",
+        fourth: "345",
+      },
+      expirationPeriod: {
+        month: "05",
+        year: "27",
+      },
+    }),
+  ],
 };
 
 export const VisaCard: Story = {
@@ -50,19 +68,66 @@ export const MasterCard: Story = {
   ],
 };
 
-export const PartiallyMaskedCard: Story = {
+export const BCCard: Story = {
   decorators: [
     withCustomCardProvider({
-      cardNumbers: {
-        first: "1234",
-        second: "5678",
-        third: "91",
-        fourth: "345",
-      },
-      expirationPeriod: {
-        month: "05",
-        year: "27",
-      },
+      cardCompany: CARD_COMPANY.bc,
+    }),
+  ],
+};
+
+export const ShinhanCard: Story = {
+  decorators: [
+    withCustomCardProvider({
+      cardCompany: CARD_COMPANY.shinhan,
+    }),
+  ],
+};
+
+export const KakaobankCard: Story = {
+  decorators: [
+    withCustomCardProvider({
+      cardCompany: CARD_COMPANY.kakaobank,
+    }),
+  ],
+};
+
+export const HyundaiCard: Story = {
+  decorators: [
+    withCustomCardProvider({
+      cardCompany: CARD_COMPANY.hyundai,
+    }),
+  ],
+};
+
+export const WooriCard: Story = {
+  decorators: [
+    withCustomCardProvider({
+      cardCompany: CARD_COMPANY.woori,
+    }),
+  ],
+};
+
+export const LotteCard: Story = {
+  decorators: [
+    withCustomCardProvider({
+      cardCompany: CARD_COMPANY.lotte,
+    }),
+  ],
+};
+
+export const HanaCard: Story = {
+  decorators: [
+    withCustomCardProvider({
+      cardCompany: CARD_COMPANY.hana,
+    }),
+  ],
+};
+
+export const KBCard: Story = {
+  decorators: [
+    withCustomCardProvider({
+      cardCompany: CARD_COMPANY.kb,
     }),
   ],
 };
