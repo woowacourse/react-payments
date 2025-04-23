5ee4d75e7ec7f12853641787b1c8980bb5c76b9f
spoyodevelop <kjy9505@gmail.com>
2025-04-23 18:31:38 +0900

    feat: React Router를 사용하여 라우팅 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import AddCard from "./AddCard/components/AddCard";
+import { Route, Routes } from "react-router";
+import AddCardConfirmModal from "./AddCard/components/AddCardConfirmModal/AddCardConfirmModal";
 
 function App() {
-  return <AddCard />;
+  return (
+    <Routes>
+      <Route path="/" element={<AddCard />} />
+      <Route path="/AddCardConfirm" element={<AddCardConfirmModal />} />
+    </Routes>
+  );
 }
 
 export default App;
