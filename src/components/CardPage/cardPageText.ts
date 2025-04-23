45e930d1a4304693335cbac063512c934b26ba7e
deokbin <deokbin@naver.com>
2025-04-24 00:24:50 +0900

    chore: 카그다 선택 및 비밀번호 관련 안내 문구 추가

--- src/components/CardPage/cardPageText.ts
+++ src/components/CardPage/cardPageText.ts
@@ -1,4 +1,7 @@
 export const CARD_PAGE_TEXT = {
+  CARD_COMPANY_TITLE: '카드사를 선택해 주세요',
+  CARD_COMPANY_SUBTITLE: '현재 국내 카드사만 가능합니다.',
+
   CARD_NUMBER_TITLE: '결제할 카드 번호를 입력해 주세요',
   CARD_NUMBER_SUBTITLE: '본인 명의의 카드만 결제 가능합니다.',
 
@@ -6,4 +9,7 @@ export const CARD_PAGE_TEXT = {
   EXPIRATION_SUBTITLE: '월/년도(MMYY)를 순서대로 입력해 주세요.',
 
   CVC_TITLE: 'CVC 번호를 입력해 주세요',
+
+  PASSWORD_TITLE: '비밀번호를 입력해 주세요',
+  PASSWORD_SUBTITLE: '앞의 2자리를 입력해 주세요',
 };
