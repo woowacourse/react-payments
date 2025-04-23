d8da8f1e57acf19b50b0d43f86f1d4aff784ca68
kimyou1102 <yoosk5485@naver.com>
2025-04-23 16:24:40 +0900

    feat: CardCompanySelect 컴포넌트 생성

---
+++ src/components/cardInfoForm/CardCompanySelect/CardCompanySelect.tsx
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+import { CARD_COMPANY, CARD_COMPANY_NAME } from '../../constants/cardCompany';
+
+interface CardCompanySelectProps {
+  cardCompany: keyof typeof CARD_COMPANY_NAME | undefined;
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+function CardCompanySelect({ cardCompany, onChange }: CardCompanySelectProps) {
+  return (
+    <>
+      <Label htmlFor="CardCompany">카드사</Label>
+      <Select id="CardCompany" onChange={onChange}>
+        <option value={cardCompany} disabled hidden selected>
+          카드사를 선택해주세요
+        </option>
+        {CARD_COMPANY.map(({ name }) => (
+          <option value={name}>{name}</option>
+        ))}
+      </Select>
+    </>
+  );
+}
+
+export default CardCompanySelect;
+
+const Select = styled.select`
+  width: 100%;
+  height: 32px;
+  border: 1px solid #acacac;
+  border-radius: 3px;
+  padding: 0px 8px;
+  font-size: 11px;
+  margin-top: 16px;
+`;
+
+const Label = styled.label`
+  visibility: hidden;
+  font-size: 0;
+`;
---
+++ src/components/constants/cardCompany.ts
@@ -0,0 +1,21 @@
+export const CARD_COMPANY_NAME = {
+  BC카드: 'BC카드',
+  신한카드: '신한카드',
+  카카오뱅크: '카카오뱅크',
+  현대카드: '현대카드',
+  우리카드: '우리카드',
+  롯데카드: '롯데카드',
+  하나카드: '하나카드',
+  국민카드: '국민카드',
+} as const;
+
+export const CARD_COMPANY = [
+  { name: 'BC카드', color: '#F04651' },
+  { name: '신한카드', color: '#0046FF' },
+  { name: '카카오뱅크', color: '#FFE600' },
+  { name: '현대카드', color: '#000000' },
+  { name: '우리카드', color: '#007BC8' },
+  { name: '롯데카드', color: '#ED1C24' },
+  { name: '하나카드', color: '#009490' },
+  { name: '국민카드', color: '#6A6056' },
+];
