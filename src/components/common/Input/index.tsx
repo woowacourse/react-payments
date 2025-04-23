1c6960c1ebc96aea29fafc11429fc348e176469e
ohgus <anakin1108@naver.com>
2025-04-23 18:02:59 +0900

    refactor: Input 컴포넌트 value 기본 값 제거

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -2,7 +2,7 @@ import { StyledInputContainer } from './Input.styled';
 import { InputProps } from './Input.types';
 
 export const Input = ({
-  value = '',
+  value,
   minLength = 1,
   maxLength = 4,
   isValid = false,
