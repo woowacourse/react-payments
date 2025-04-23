df6db5f54fd29e970239119dcac736cdffa0886a
thgml05 <aisohii@naver.com>
2025-04-23 13:34:03 +0900

    feat : 확인 버튼 클릭 시 페이지 이동 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,18 @@
-import { useState } from 'react';
-import CardNumbersInput from './components/CardNumbersInput/CardNumbersInput';
-import CardExpiryInput from './components/CardExpiryInput/CardExpiryInput';
-import CVCInput from './components/CVCInput/CVCInput';
-import CardPreview from './components/CardPreview/CardPreview';
-import CardBrandInput from './components/CardBrandInput/CardBrandInput';
-import { CARD_VALIDATION_INFO } from './constants/cardValidationInfo';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import CardPasswordInput from './components/CardPasswordInput/CardPasswordInput';
-import Button from './components/Button/Button';
+import HomePage from './pages/HomePage';
 
-function App() {
-  const [cardNumbers, setCardNumbers] = useState(
-    Array(CARD_VALIDATION_INFO.TOTAL_CARD_INPUTS).fill('')
-  );
-  const [month, setMonth] = useState('');
-  const [year, setYear] = useState('');
-  const [CVC, setCVC] = useState('');
-  const [brand, setBrand] = useState('');
-  const [password, setPassword] = useState('');
+const router = createBrowserRouter([
+  {
+    path: '/react-payments',
+    element: <HomePage />,
+  },
+  {
+    path: '/react-payments/complete',
+    element: <div>결제 완료</div>,
+  },
+]);
 
-  return (
-    <div className="app">
-      <CardPreview
-        cardNumbers={cardNumbers}
-        month={month}
-        year={year}
-        brand={brand}
-      />
-      <form>
-        <CardNumbersInput
-          cardNumbers={cardNumbers}
-          setCardNumbers={setCardNumbers}
-        />
-        <CardBrandInput brand={brand} setBrand={setBrand} />
-        <CardExpiryInput
-          month={month}
-          setMonth={setMonth}
-          year={year}
-          setYear={setYear}
-        />
-        <CVCInput CVC={CVC} setCVC={setCVC} />
-        <CardPasswordInput password={password} setPassword={setPassword} />
-        <Button text="확인" />
-      </form>
-    </div>
-  );
+export default function Router() {
+  return <RouterProvider router={router} />;
 }
-
-export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import CardNumbersInput from '../components/CardNumbersInput/CardNumbersInput';
+import CardExpiryInput from '../components/CardExpiryInput/CardExpiryInput';
+import CVCInput from '../components/CVCInput/CVCInput';
+import CardPreview from '../components/CardPreview/CardPreview';
+import CardBrandInput from '../components/CardBrandInput/CardBrandInput';
+import { CARD_VALIDATION_INFO } from '../constants/cardValidationInfo';
+import '../App.css';
+import CardPasswordInput from '../components/CardPasswordInput/CardPasswordInput';
+import Button from '../components/Button/Button';
+import { useNavigate } from 'react-router-dom';
+
+const HomePage = () => {
+  const [cardNumbers, setCardNumbers] = useState(
+    Array(CARD_VALIDATION_INFO.TOTAL_CARD_INPUTS).fill('')
+  );
+  const [month, setMonth] = useState('');
+  const [year, setYear] = useState('');
+  const [CVC, setCVC] = useState('');
+  const [brand, setBrand] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    navigate('/react-payments/complete');
+  };
+
+  return (
+    <div className="app">
+      <CardPreview
+        cardNumbers={cardNumbers}
+        month={month}
+        year={year}
+        brand={brand}
+      />
+      <form onSubmit={handleSubmit}>
+        <CardNumbersInput
+          cardNumbers={cardNumbers}
+          setCardNumbers={setCardNumbers}
+        />
+        <CardBrandInput brand={brand} setBrand={setBrand} />
+        <CardExpiryInput
+          month={month}
+          setMonth={setMonth}
+          year={year}
+          setYear={setYear}
+        />
+        <CVCInput CVC={CVC} setCVC={setCVC} />
+        <CardPasswordInput password={password} setPassword={setPassword} />
+        <Button text="확인" />
+      </form>
+    </div>
+  );
+};
+
+export default HomePage;
