fffab44dc6c5c0bc134a3a120fe7cc3c21b85d2a
yeongipark <yeongi0111@naver.com>
2025-04-24 10:48:57 +0900

    feat: select 기본 값 disabled 설정

--- src/shared/select/Select.tsx
+++ src/shared/select/Select.tsx
@@ -2,7 +2,7 @@ import { ComponentProps } from "react";
 import { StyledSelect, StyledOption } from "./Select.css";
 
 type SelectProps = ComponentProps<"select"> & {
-  options: Array<{ value: string; label: string }>;
+  options: Array<{ value: string; label: string; default?: boolean }>;
   onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
   value: string;
   isError: boolean;
@@ -13,7 +13,11 @@ export default function Select(props: SelectProps) {
   return (
     <StyledSelect {...props}>
       {props.options.map((option) => (
-        <StyledOption key={option.value} value={option.value}>
+        <StyledOption
+          key={option.value}
+          value={option.value}
+          disabled={option.default}
+        >
           {option.label}
         </StyledOption>
       ))}
