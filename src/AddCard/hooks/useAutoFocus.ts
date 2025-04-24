1c593506acfc4f124ec71ec2d45b7095134c6725
spoyodevelop <kjy9505@gmail.com>
2025-04-24 16:38:38 +0900

    feat: 자동 포커스 훅 추가

---
+++ src/AddCard/hooks/useAutoFocus.ts
@@ -0,0 +1,43 @@
+import { useRef, RefObject } from "react";
+
+type InputRefs<T extends string> = Record<
+  T,
+  RefObject<HTMLInputElement | null>
+>;
+
+interface UseAutoFocusReturn<T extends string> {
+  inputRefs: InputRefs<T>;
+  handleAutoFocus: (
+    currentKey: T,
+    value: string,
+    keys: T[],
+    maxLength: number
+  ) => void;
+}
+
+export function useAutoFocus<T extends string>(
+  keys: T[]
+): UseAutoFocusReturn<T> {
+  const inputRefs = keys.reduce((acc, key) => {
+    acc[key] = useRef<HTMLInputElement | null>(null);
+    return acc;
+  }, {} as InputRefs<T>);
+
+  const handleAutoFocus = (
+    currentKey: T,
+    value: string,
+    keys: T[],
+    maxLength: number
+  ) => {
+    if (value.length === maxLength) {
+      const currentIndex = keys.indexOf(currentKey);
+      const nextKey = keys[currentIndex + 1];
+
+      if (nextKey && inputRefs[nextKey]?.current) {
+        inputRefs[nextKey].current?.focus();
+      }
+    }
+  };
+
+  return { inputRefs, handleAutoFocus };
+}
