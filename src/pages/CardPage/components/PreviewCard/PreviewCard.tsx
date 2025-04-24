8d95cbcdc416b6158ddf442e01bf17ed7d35bb7a
SuHwa Lee <tnghk9611@naver.com>
2025-04-24 22:01:25 +0900

    feat: 카드 색 변경하는 로직 구현

--- src/pages/CardPage/components/PreviewCard/PreviewCard.tsx
+++ src/pages/CardPage/components/PreviewCard/PreviewCard.tsx
@@ -4,13 +4,27 @@ import { checkCardType } from './checkCardType';
 type PreviewCardProps = {
   cardNumber: string[];
   expirationDate: string[];
+  cardCompany: string;
 };
 
-const PreviewCard = ({ cardNumber, expirationDate }: PreviewCardProps) => {
+const PreviewCard = ({ cardNumber, expirationDate, cardCompany }: PreviewCardProps) => {
   const cardType = checkCardType(cardNumber[0]);
+  const cardTypeColors: { [key: string]: string } = {
+    '': '#000000',
+    BC카드: '#F04651',
+    신한카드: '#0046FF',
+    카카오뱅크: '#FFE600',
+    현대카드: '#000000',
+    우리카드: '#007BC8',
+    롯데카드: '#ED1C24',
+    하나카드: '#009490',
+    국민카드: '#6A6056',
+  };
+
+  const cardColor = cardTypeColors[cardCompany] || cardTypeColors[''];
 
   return (
-    <StyledPreviewCard>
+    <StyledPreviewCard backgroundColor={cardColor}>
       <StyledCardHeader>
         <StyledICChip />
         {cardType && <StyledCardType src={cardType} />}
@@ -32,16 +46,17 @@ const PreviewCard = ({ cardNumber, expirationDate }: PreviewCardProps) => {
 
 export default PreviewCard;
 
-const StyledPreviewCard = styled.div`
+const StyledPreviewCard = styled.div<{ backgroundColor: string }>`
   width: 212px;
   height: 120px;
   border-radius: 4px;
   box-shadow: 3px 3px 5px 0px rgb(0, 0, 0, 0.25);
-  background-color: #333333;
+  background-color: ${(props) => props.backgroundColor};
   display: flex;
   flex-direction: column;
   padding: 0.8rem;
   margin: 30px auto;
+  transition: background-color 0.3s ease;
 `;
 
 const StyledCardHeader = styled.div`
