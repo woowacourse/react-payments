706892f9144bb55f33cc65a4136cb8d8319cd26b
Jeongeun Lee <leeju1009@gmail.com>
2025-04-24 21:07:31 +0900

    feat: 카드 비밀번호 컴포넌트 생성

---
+++ src/components/paymentInputPage/cardInputForm/cardInput/CardPasswordInput.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import Input from '../../../common/inputForm/input/Input';
+import InputForm from '../../../common/inputForm/InputForm';
+import { validatorUtils } from '../../../../utils/validationUtils';
+
+export interface CardPasswordInputProps {
+  password: string;
+  setPassword: React.Dispatch<React.SetStateAction<string>>;
+  isValid: boolean;
+  setIsValid: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function CardPasswordInput({
+  password,
+  setPassword,
+  isValid,
+  setIsValid,
+}: CardPasswordInputProps) {
+  const [feedbackMessage, setFeedbackMessage] = useState('');
+
+  function handlePasswordChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const target = e.target;
+    const { value } = target;
+
+    setPassword(value);
+    checkIsValidType(value);
+  }
+
+  function checkIsValidType(passwordInput: string) {
+    if (!validatorUtils.isNumber(passwordInput)) {
+      setFeedbackMessage('숫자만 입력 가능합니다.');
+      setIsValid(false);
+    }
+  }
+
+  return (
+    <>
+      <InputForm
+        feedbackMessage={feedbackMessage}
+        title='비밀번호를 입력해 주세요.'
+        description='앞의 2자리를 입력해주세요.'
+        label='비밀번호 앞 2자리'
+      >
+        <Input
+          type='tel'
+          name='password'
+          value={password}
+          handleInputChange={handlePasswordChange}
+          maxLength={2}
+          isValidInput={isValid}
+        />
+      </InputForm>
+    </>
+  );
+}
+export default CardPasswordInput;
