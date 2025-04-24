18547d2813cab69788f53ebad5934728999b73bf
deokbin <deokbin@naver.com>
2025-04-24 22:44:27 +0900

    feat: 카드 등록 성공 페이지 추가

---
+++ src/pages/RegisterSuccessPage/RegisterSuccess.tsx
@@ -0,0 +1,57 @@
+import styled from '@emotion/styled';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import Text from '../../components/Text/Text';
+import Button from '../../components/Button/Button';
+
+const StyledPage = styled.div`
+  width: 40%;
+  min-width: 400px;
+  min-height: 100vh;
+  height: auto;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 30px;
+  overflow-y: auto;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledImage = styled.img`
+  width: 76px;
+  height: 76px;
+`;
+
+const StyledTextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
+
+const RegisterSuccess = () => {
+  const [searchParams] = useSearchParams();
+  const cardFirstNumbers = searchParams.get('cardNumber');
+  const cardCompany = searchParams.get('cardCompany');
+
+  const navigate = useNavigate();
+
+  const navigateToCardPage = () => {
+    navigate(`/`);
+  };
+
+  return (
+    <StyledPage>
+      <StyledImage src="./success.png"></StyledImage>
+      <StyledTextWrapper>
+        <Text type="title" text={`${cardFirstNumbers}로 시작하는`}></Text>
+        <Text type="title" text={`${cardCompany}가 등록되었어요.`}></Text>
+      </StyledTextWrapper>
+      <Button text="확인" onClick={navigateToCardPage}></Button>
+    </StyledPage>
+  );
+};
+
+export default RegisterSuccess;
