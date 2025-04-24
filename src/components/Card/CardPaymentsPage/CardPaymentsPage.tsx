7be56e1a55c7810b7af64eb5846324bc47ecbb6f
sooyeoniya <tndus1306403@naver.com>
2025-04-24 17:28:39 +0900

    refactor: Provider 위치 변경
    - CardPaymentsPage에 있는 것이 모듈 단위로 상태가 제공된다는 의미에서 좀 더 나은 위치라고 판단

--- src/components/Card/CardPaymentsPage/CardPaymentsPage.tsx
+++ src/components/Card/CardPaymentsPage/CardPaymentsPage.tsx
@@ -1,4 +1,6 @@
 import { CARD_FORM_TYPE } from "../../../constants/constants";
+import { CardProvider } from "../../../contexts/CardContext";
+import { CardValidationProvider } from "../../../contexts/CardValidationContext";
 import CardFormSection from "../CardFormSection/CardFormSection";
 import CardPreview from "../CardPreview/CardPreview";
 import {
@@ -9,17 +11,21 @@ import {
 
 export default function CardPaymentsPage() {
   return (
-    <CardPaymentsPageCSS>
-      <CardFormCSS>
-        <CardPreview />
-        <CardFormScrollCSS>
-          <CardFormSection type={CARD_FORM_TYPE.password} />
-          <CardFormSection type={CARD_FORM_TYPE.cvcNumber} />
-          <CardFormSection type={CARD_FORM_TYPE.expirationPeriod} />
-          <CardFormSection type={CARD_FORM_TYPE.cardCompany} />
-          <CardFormSection type={CARD_FORM_TYPE.cardNumbers} />
-        </CardFormScrollCSS>
-      </CardFormCSS>
-    </CardPaymentsPageCSS>
+    <CardProvider>
+      <CardValidationProvider>
+        <CardPaymentsPageCSS>
+          <CardFormCSS>
+            <CardPreview />
+            <CardFormScrollCSS>
+              <CardFormSection type={CARD_FORM_TYPE.password} />
+              <CardFormSection type={CARD_FORM_TYPE.cvcNumber} />
+              <CardFormSection type={CARD_FORM_TYPE.expirationPeriod} />
+              <CardFormSection type={CARD_FORM_TYPE.cardCompany} />
+              <CardFormSection type={CARD_FORM_TYPE.cardNumbers} />
+            </CardFormScrollCSS>
+          </CardFormCSS>
+        </CardPaymentsPageCSS>
+      </CardValidationProvider>
+    </CardProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import CardPaymentsPage from "./components/Card/CardPaymentsPage/CardPaymentsPage";
-import { CardProvider } from "./contexts/CardContext";
-import { CardValidationProvider } from "./contexts/CardValidationContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <CardProvider>
-      <CardValidationProvider>
-        <CardPaymentsPage />
-      </CardValidationProvider>
-    </CardProvider>
+    <CardPaymentsPage />
   </React.StrictMode>
 );
