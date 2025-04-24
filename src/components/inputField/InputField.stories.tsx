44347284a687eb6bd0b3968f9d49226f00770c0d
aydenote <dslgpgh@gmail.com>
2025-04-24 12:47:26 +0900

    fix: inputField에 input 컴포넌트 children 전달로 변경

--- src/components/inputField/InputField.stories.tsx
+++ src/components/inputField/InputField.stories.tsx
@@ -16,14 +16,20 @@ type Story = StoryObj<typeof InputField>;
 export const Default: Story = {
 	args: {
 		label: "유효기간",
-		inputs: [<Input placeholder="MM" value="" onChange={() => {}} />, <Input placeholder="YY" value="" onChange={() => {}} />],
+	},
+	render: (args) => {
+		const inputs = [<Input placeholder="MM" value="" onChange={() => {}} />, <Input placeholder="YY" value="" onChange={() => {}} />];
+		return <InputField label={args.label}>{inputs}</InputField>;
 	},
 };
 
 export const Error: Story = {
 	args: {
 		label: "유효기간",
-		inputs: [<Input placeholder="MM" value="" onChange={() => {}} isError={true} />, <Input placeholder="YY" value="" onChange={() => {}} />],
 		errorMessage: <Description color={COLORS.ERROR}>숫자만 입력 가능합니다.</Description>,
+	},
+	render: (args) => {
+		const inputs = [<Input placeholder="MM" value="" onChange={() => {}} isError={true} />, <Input placeholder="YY" value="" onChange={() => {}} />];
+		return <InputField label={args.label}>{inputs}</InputField>;
 	},
 };
