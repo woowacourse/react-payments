2291767bda7bc8dbe6d82290553fbc4af27367f1
Ko Daeun <drechsu@gmail.com>
2025-04-24 15:18:42 +0900

    feat : 전 section 입력완료시 다음 section이 나타나는 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -43,33 +43,86 @@ function App() {
       cardCompany: false,
     });
 
-  const canSubmit =
-    canSubmitValidLength({
-      state: cardNumbers,
-      isError: isCardNumbersError,
-      validLength: {
-        firstNumber: 4,
-        secondNumber: 4,
-        thirdNumber: 4,
-        fourthNumber: 4,
-      },
-    }) &&
-    canSubmitValidLength({
-      state: cardExpirationDate,
-      isError: isCardExpirationDateError,
-      validLength: {
-        month: 2,
-        year: 2,
-      },
-    }) &&
-    cardCompany !== '' &&
-    canSubmitValidLength({
-      state: cardCVCNumber,
-      isError: isCardCVCNumberError,
-      validLength: 3,
-    });
+  const [inputCompletion, setInputCompletion] = useState([
+    false,
+    false,
+    false,
+    false,
+  ]);
+  const inputSections = [
+    <CardNumbersInputSection
+      cardNumbers={cardNumbers}
+      setCardNumbers={setCardNumbers}
+      handleCardNumbersBlur={handleCardNumbersBlur}
+      isError={isCardNumbersError}
+      errorMessage={cardNumbersErrorMessage}
+    />,
+    <CardCompanySelectSection
+      cardCompany={cardCompany}
+      setCardCompany={setCardCompany}
+      isError={cardCompanyError.isError.cardCompany}
+      errorMessage={cardCompanyError.errorMessage}
+      clearError={() => clearCardCompanyError('cardCompany')}
+    />,
+    <CardExpirationDateInputSection
+      cardExpirationDate={cardExpirationDate}
+      setCardExpirationDate={setCardExpirationDate}
+      handleCardExpirationDateBlur={handleCardExpirationDateBlur}
+      isError={isCardExpirationDateError}
+      errorMessage={cardExpirationDateErrorMessage}
+    />,
+    <CardCVCNumberInputSection
+      cardCVCNumber={cardCVCNumber}
+      setCardCVCNumber={setCardCVCNumber}
+      handleCardCVCBlur={handleCardCVCBlur}
+      isError={isCardCVCNumberError}
+      errorMessage={cardCVCNumberErrorMessage}
+    />,
+  ];
+  useEffect(() => {
+    setInputCompletion((prev) => {
+      const newCompletion = [...prev];
+      if (inputCompletion[0] === false) {
+        newCompletion[0] = canSubmitValidLength({
+          state: cardNumbers,
+          isError: isCardNumbersError,
+          validLength: {
+            firstNumber: 4,
+            secondNumber: 4,
+            thirdNumber: 4,
+            fourthNumber: 4,
+          },
+        });
+      }
+      if (inputCompletion[1] === false) {
+        newCompletion[1] = cardCompany !== '';
+      }
+      if (inputCompletion[2] === false) {
+        newCompletion[2] = canSubmitValidLength({
+          state: cardExpirationDate,
+          isError: isCardExpirationDateError,
+          validLength: {
+            month: 2,
+            year: 2,
+          },
+        });
+      }
+      if (inputCompletion[3] === false) {
+        console.log('들어옴!');
+        newCompletion[3] = canSubmitValidLength({
+          state: cardCVCNumber,
+          isError: isCardCVCNumberError,
+          validLength: 3,
+        });
+        console.log('cardCVCNumber', cardCVCNumber);
+      }
 
-  console.log('canSubmit', canSubmit);
+      return newCompletion;
+    });
+  }, [cardCompany, cardNumbers, cardCVCNumber, cardExpirationDate]);
+
+  console.log(inputCompletion);
+  console.log(inputSections);
 
   return (
     <>
@@ -81,13 +134,9 @@ function App() {
         />
 
         <div className={styles.cardForm}>
-          <CardCompanySelectSection
-            cardCompany={cardCompany}
-            setCardCompany={setCardCompany}
-            isError={cardCompanyError.isError.cardCompany}
-            errorMessage={cardCompanyError.errorMessage}
-            clearError={() => clearCardCompanyError('cardCompany')}
-          />
+          {inputCompletion.map((isCompleted, index) => {
+            if (isCompleted) return inputSections[index + 1];
+          })}
           <CardNumbersInputSection
             cardNumbers={cardNumbers}
             setCardNumbers={setCardNumbers}
@@ -95,22 +144,8 @@ function App() {
             isError={isCardNumbersError}
             errorMessage={cardNumbersErrorMessage}
           />
-          <CardExpirationDateInputSection
-            cardExpirationDate={cardExpirationDate}
-            setCardExpirationDate={setCardExpirationDate}
-            handleCardExpirationDateBlur={handleCardExpirationDateBlur}
-            isError={isCardExpirationDateError}
-            errorMessage={cardExpirationDateErrorMessage}
-          />
-          <CardCVCNumberInputSection
-            cardCVCNumber={cardCVCNumber}
-            setCardCVCNumber={setCardCVCNumber}
-            handleCardCVCBlur={handleCardCVCBlur}
-            isError={isCardCVCNumberError}
-            errorMessage={cardCVCNumberErrorMessage}
-          />
         </div>
-        {canSubmit && (
+        {inputCompletion.every((value) => value) && (
           <ConfirmButton
             type="submit"
             text="확인"
