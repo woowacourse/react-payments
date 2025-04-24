01ecb8358a099d4ffe627561a20d18b76367d1db
spoyodevelop <kjy9505@gmail.com>
2025-04-24 16:58:06 +0900

    refactor: 매우 정갈해진 커스텀 훅 작성

--- src/AddCard/hooks/useCardRegistrationFlow.ts
+++ src/AddCard/hooks/useCardRegistrationFlow.ts
@@ -1,11 +1,10 @@
-import { useMemo, useRef } from "react";
 import useControlledCardNumber from "../components/AddCardForm/components/CardNumber/hooks/useControlledCardNumber";
 import useControlledExpireDate from "../components/AddCardForm/components/ExpireDate/hooks/useControlledExpireDate";
 import useControlledCVC from "../components/AddCardForm/components/CVC/hooks/useControlledCVC";
 import useControlledSelectedCardBrand from "../components/AddCardForm/components/CardBrand/hooks/useControlledSelectCardBrand";
 import useControlledPassword from "../components/AddCardForm/components/Password/hooks/useControlledPassword";
-import { STEP_ORDER } from "../constants";
-import { validators } from "../validation";
+import useStepValidation from "./useStepValidation";
+import useFormState from "./useFormState";
 
 const useCardRegistrationFlow = () => {
   const card = useControlledCardNumber();
@@ -14,48 +13,22 @@ const useCardRegistrationFlow = () => {
   const cvc = useControlledCVC();
   const password = useControlledPassword();
 
-  const maxReachedStep = useRef(0);
-
   const slices = { card, brand, expire, cvc, password };
 
-  const { currentStep, allValid } = useMemo(() => {
-    let validAll = true;
-    let currentValidStep = 0;
-
-    for (let i = 0; i < validators.length; i++) {
-      if (validators[i](slices)) {
-        currentValidStep = i + 1;
-      } else {
-        validAll = i >= validators.length - 1;
-        break;
-      }
-    }
-
-    if (currentValidStep > maxReachedStep.current) {
-      maxReachedStep.current = currentValidStep;
-    }
-
-    const stepIndex = Math.min(maxReachedStep.current, STEP_ORDER.length - 1);
-
-    return {
-      currentStep: STEP_ORDER[stepIndex],
-      allValid: validAll && currentValidStep >= validators.length,
-    };
-  }, [
+  const dependencies = [
     card.cardNumberState,
     brand.selectedBrand,
     expire.expireDate,
     cvc.CVCState,
     password.passwordState,
-  ]);
+  ];
+
+  const { currentStep, allValid } = useStepValidation(slices, dependencies);
+  const { state, previewState } = useFormState(slices);
 
   return {
-    state: { ...card, ...brand, ...expire, ...cvc, ...password },
-    previewState: {
-      cardNumberState: card.cardNumberState,
-      selectedBrand: brand.selectedBrand,
-      expireDate: expire.expireDate,
-    },
+    state,
+    previewState,
     currentStep,
     allValid,
   };
