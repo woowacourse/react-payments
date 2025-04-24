2cc9de28b2b6f1b38958615fb63aea1296fab380
spoyodevelop <kjy9505@gmail.com>
2025-04-24 11:05:39 +0900

    fix: BrowserRouter에 basename 속성 추가

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-payments">
       <App />
     </BrowserRouter>
   </React.StrictMode>
