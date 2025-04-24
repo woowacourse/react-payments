3188cd786d24bb988a2aaa311d96769ad1991195
aydenote <dslgpgh@gmail.com>
2025-04-24 12:37:06 +0900

    feat: 버튼 컴포넌트 스토리북 생성

---
+++ src/components/button/Button.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from "./Button";
+
+const meta = {
+	title: "components/button",
+	component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+	args: {},
+};
