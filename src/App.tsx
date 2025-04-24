cd192c4db3558d29e19f2490603119f5ec5fedb4
aydenote <dslgpgh@gmail.com>
2025-04-24 17:11:13 +0900

    feat: 렌더링된 컴포넌트 제거 되지 않는 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Card from "./components/card/Card";
 import CardNumber from "./components/form/CardNumber";
 import ExpirationDate from "./components/form/ExpirationDate";
@@ -22,6 +23,7 @@ export type date = {
 
 function App() {
 	const cardForm = useCardForm();
+	const [maxStep, setMaxStep] = useState(0);
 	const RENDERING_STEP = {
 		cardNumber: 0,
 		company: 1,
@@ -31,23 +33,31 @@ function App() {
 	};
 	const step = Object.values(cardForm.isComplete).filter(Boolean).length;
 
+	const onClick = () => {};
+
+	useEffect(() => {
+		setMaxStep((prev) => Math.max(prev, step));
+	}, [step]);
+
 	return (
 		<MainContainer>
-			<Form>
+			<form>
 				<Card cardNumbers={cardForm.cardNumber} cardCompany={cardForm.cardCompany} expirationDate={cardForm.expirationDate} />
 
-				{step >= RENDERING_STEP.password && <CardPassword cardPassword={cardForm.cardPassword} setCardPassword={cardForm.setCardPassword} />}
-				{step >= RENDERING_STEP.cvc && <CardCvc cvcNumber={cardForm.cvcNumber} setCvcNumber={cardForm.setCvcNumber} />}
-				{step >= RENDERING_STEP.expirationDate && <ExpirationDate expirationDate={cardForm.expirationDate} setExpirationDate={cardForm.setExpirationDate} />}
-				{step >= RENDERING_STEP.company && <CardCompany cardCompany={cardForm.cardCompany} setCardCompany={cardForm.setCardCompany} />}
-				{step >= RENDERING_STEP.cardNumber && <CardNumber cardNumber={cardForm.cardNumber} setCardNumber={cardForm.setCardNumber} />}
+				{maxStep >= RENDERING_STEP.password && <CardPassword cardPassword={cardForm.cardPassword} setCardPassword={cardForm.setCardPassword} />}
+				{maxStep >= RENDERING_STEP.cvc && <CardCvc cvcNumber={cardForm.cvcNumber} setCvcNumber={cardForm.setCvcNumber} />}
+				{maxStep >= RENDERING_STEP.expirationDate && <ExpirationDate expirationDate={cardForm.expirationDate} setExpirationDate={cardForm.setExpirationDate} />}
+				{maxStep >= RENDERING_STEP.company && <CardCompany cardCompany={cardForm.cardCompany} setCardCompany={cardForm.setCardCompany} />}
+				{maxStep >= RENDERING_STEP.cardNumber && <CardNumber cardNumber={cardForm.cardNumber} setCardNumber={cardForm.setCardNumber} />}
 
 				{step === 5 && (
 					<ButtonWrap>
-						<Button type="button">확인</Button>
+						<Button type="button" onClick={onClick}>
+							확인
+						</Button>
 					</ButtonWrap>
 				)}
-			</Form>
+			</form>
 		</MainContainer>
 	);
 }
@@ -58,10 +68,6 @@ const MainContainer = styled.div`
 	width: 376px;
 	padding: 77px 30px 20px;
 	margin: auto;
-`;
-
-const Form = styled.form`
-	position: relative;
 `;
 
 const ButtonWrap = styled.div`
