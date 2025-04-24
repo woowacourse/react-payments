e86fe535fe70c6c78f7c97f9f170cd915bae509e
SuHwa Lee <tnghk9611@naver.com>
2025-04-24 23:52:16 +0900

    feat: 현재 스텝을 관리하는 커스텀 훅 구현

---
+++ src/hooks/useStep.ts
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+export enum InputStep {
+  CARD_NUMBER = 0,
+  CARD_COMPANY = 1,
+  EXPIRATION_DATE = 2,
+  CVC = 3,
+  PASSWORD = 4,
+  COMPLETED = 5,
+}
+
+function useStep(initialStep: InputStep = InputStep.CARD_NUMBER) {
+  const [currentStep, setCurrentStep] = useState<InputStep>(initialStep);
+
+  const goToNextStep = (step: number) => {
+    if (currentStep > step) return;
+    setCurrentStep((prev) => (prev < InputStep.COMPLETED ? prev + 1 : prev));
+  };
+
+  const isAtLeastAtStep = (step: InputStep) => {
+    return currentStep >= step;
+  };
+
+  return {
+    currentStep,
+    goToNextStep,
+    isAtLeastAtStep,
+    InputStep,
+  };
+}
+
+export default useStep;
