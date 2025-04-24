65364580584d90c653fb4ab68a38915f95025e54
SuHwa Lee <tnghk9611@naver.com>
2025-04-24 23:50:17 +0900

    refactor: 카드 타입 선언부 위로 작성

--- src/pages/CardPage/components/PreviewCard/PreviewCard.tsx
+++ src/pages/CardPage/components/PreviewCard/PreviewCard.tsx
@@ -7,20 +7,20 @@ type PreviewCardProps = {
   cardCompany: string;
 };
 
+const cardTypeColors: { [key: string]: string } = {
+  '': '#000000',
+  BC카드: '#F04651',
+  신한카드: '#0046FF',
+  카카오뱅크: '#FFE600',
+  현대카드: '#000000',
+  우리카드: '#007BC8',
+  롯데카드: '#ED1C24',
+  하나카드: '#009490',
+  국민카드: '#6A6056',
+};
+
 const PreviewCard = ({ cardNumber, expirationDate, cardCompany }: PreviewCardProps) => {
   const cardType = checkCardType(cardNumber[0]);
-  const cardTypeColors: { [key: string]: string } = {
-    '': '#000000',
-    BC카드: '#F04651',
-    신한카드: '#0046FF',
-    카카오뱅크: '#FFE600',
-    현대카드: '#000000',
-    우리카드: '#007BC8',
-    롯데카드: '#ED1C24',
-    하나카드: '#009490',
-    국민카드: '#6A6056',
-  };
-
   const cardColor = cardTypeColors[cardCompany] || cardTypeColors[''];
 
   return (
