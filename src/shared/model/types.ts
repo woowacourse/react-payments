a2eb3ea403623506448b2fac2dad60d38a382d91
eunwoo-levi <eunwoo1341@gmail.com>
2025-04-25 16:05:29 +0900

    feat: CardInfoProps shared type 정의

--- src/shared/model/types.ts
+++ src/shared/model/types.ts
@@ -2,6 +2,22 @@ export interface CustomInputProps extends React.InputHTMLAttributes<HTMLInputEle
   error?: boolean;
 }
 
+export interface CardInfoProps {
+  cardNumber: string[];
+  cardExpirationDate: { month: string; year: string };
+  cardCVC: string;
+  cardIssuer: string;
+  cardPassword: string;
+  handleCardInfoChange?: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
+  error?: {
+    cardNumberError: { errorIndex: number; errorMessage: string };
+    cardExpirationDateError: { errorIndex: number; errorMessage: string };
+    cardCVCError: { errorIndex: number; errorMessage: string };
+    cardIssuerError: { errorIndex: number; errorMessage: string };
+    cardPasswordError: { errorIndex: number; errorMessage: string };
+  };
+}
+
 export interface ErrorProps {
   cardNumberError: { errorIndex: number; errorMessage: string };
   cardExpirationDateError: { errorIndex: number; errorMessage: string };
