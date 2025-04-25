39f07211dba0ad0112a5d55237d0557737ba4dfd
eunwoo-levi <eunwoo1341@gmail.com>
2025-04-25 15:53:31 +0900

    chore: createBrowserRouter 사용하여 router 설정

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,24 +1,16 @@
-// App.tsx
-import { createBrowserRouter, RouterProvider } from 'react-router';
-import useCardInfo from '../../features/cardInfo/hooks/useCardInfo';
+import { createBrowserRouter } from 'react-router';
 import CardPage from '../../pages/CardPage/CardPage';
 import CompletePage from '../../pages/CompletePage/CompletePage';
 
-function App() {
-  const cardInfo = useCardInfo();
-
-  const router = createBrowserRouter([
+export const router = () => {
+  return createBrowserRouter([
     {
       path: '/',
-      element: <CardPage {...cardInfo} />,
+      element: <CardPage />,
     },
     {
       path: '/complete',
-      element: <CompletePage cardNumber={cardInfo.cardNumber[0]} cardIssuer={cardInfo.cardIssuer} />,
+      element: <CompletePage />,
     },
   ]);
-
-  return <RouterProvider router={router} />;
-}
-
-export default App;
+};
