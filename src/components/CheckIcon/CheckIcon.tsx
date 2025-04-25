a6cd625f37e0a96a500bca9d7582484ec6415df7
wo-o29 <woogur29@gmail.com>
2025-04-25 18:25:58 +0900

    refactor: svg 속성 카멜 케이스로 수정

--- src/components/CheckIcon/CheckIcon.tsx
+++ src/components/CheckIcon/CheckIcon.tsx
@@ -15,9 +15,9 @@ function CheckIcon({ fill = "#333333" }: CheckIconProps) {
       <path
         d="M23 34.0377L35.0471 48L55 28"
         stroke="white"
-        stroke-width="7.49999"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="7.49999"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
