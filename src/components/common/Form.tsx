19e6e574335028c11f400ab64897778144fe275a
youjeong Jeong <wjddbwjd1205@naver.com>
2025-04-25 15:25:33 +0900

    feat: 재사용 가능한 기본 폼 컴포넌트 생성

---
+++ src/components/common/Form.tsx
@@ -0,0 +1,10 @@
+interface FormProps {
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  children: React.ReactNode;
+}
+
+export const Form = ({ onSubmit, children }: FormProps) => (
+  <form onSubmit={onSubmit} noValidate>
+    {children}
+  </form>
+);
