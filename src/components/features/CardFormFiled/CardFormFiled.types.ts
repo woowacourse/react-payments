3d5b8476f0360f257d21aad18df4a412200a8c3c
keemsebeen <sebin0580@gmail.com>
2025-04-25 16:00:39 +0900

    refactor: CardFormFiled, FormData type 정의

--- src/components/features/CardFormFiled/CardFormFiled.types.ts
+++ src/components/features/CardFormFiled/CardFormFiled.types.ts
@@ -4,4 +4,41 @@ export type CardFormFiledProps = {
   onBlur: (index: number, e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export type CardFormFiledType = 'cardNumber' | 'expireDate' | 'CVC';
+export type CardInputType = {
+  value: string;
+  isValid: boolean;
+};
+
+export const CARD_FILED_CONFIG = {
+  cardNumber: {
+    arrLength: 4,
+    valueLength: 4,
+  },
+  expireDate: {
+    arrLength: 2,
+    valueLength: 2,
+  },
+  cvc: {
+    arrLength: 1,
+    valueLength: 3,
+  },
+  password: {
+    valueLength: 2,
+  },
+} as const;
+
+export type CardFormFiled = typeof CARD_FILED_CONFIG;
+
+export type CardFormProps = {
+  /**
+   * Function to be called when moving to the next step.
+   */
+  onNext: (value?: string) => void;
+};
+
+export type FormData<T> = {
+  /**
+   * The state of the form field.
+   */
+  context: { state: T; setState: (newState: T) => void };
+};
