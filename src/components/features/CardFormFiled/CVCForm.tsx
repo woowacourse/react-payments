a0879b4d8c936a29420416b9b99de317b47bd5dc
keemsebeen <sebin0580@gmail.com>
2025-04-25 16:02:26 +0900

    refactor: CVCForm, useSingleCardInput 훅 연결

--- src/components/features/CardFormFiled/CVCForm.tsx
+++ src/components/features/CardFormFiled/CVCForm.tsx
@@ -1,30 +1,40 @@
 import { css } from '@emotion/react';
 
+import { CARD_FILED_CONFIG, CardFormProps, FormData } from './CardFormFiled.types';
+
 import { CardInputLayout } from '../../common/CardInputLayout';
 import { Flex } from '@/components/common/Flex';
 import { Input } from '@/components/common/Input';
 import { Text } from '@/components/common/Text';
-import { useCardInput } from '@/hooks/useCardInput';
+import { CardForm } from '@/hooks/useCardFormState';
+import { useSingleCardInput } from '@/hooks/useSingleCardInput';
 
-export const CVCForm = () => {
-  const { value: cvcNumber, errorMessage, handleChange, handleBlur } = useCardInput('CVC', 1, 3);
+type Props<T> = CardFormProps & FormData<T>;
+export const CVCForm = ({ context, onNext }: Props<CardForm['cvc']>) => {
+  const { state: cvcNumberFormData, setState: setCVCNumberFormData } = context;
 
-  const isInvalidCVC = cvcNumber.some((cvcNumber) => !cvcNumber.isValid);
+  const {
+    state: CVCNumber,
+    error,
+    handleChange,
+  } = useSingleCardInput({
+    state: cvcNumberFormData,
+    setState: setCVCNumberFormData,
+    onValid: onNext,
+    valueLength: CARD_FILED_CONFIG.cvc.valueLength,
+  });
 
   return (
     <CardInputLayout headerText="CVC 번호를 입력해 주세요." label="CVC">
       <Flex direction="column" alignItems="flex-start" width="100%" gap="4px">
-        {cvcNumber.map((cvc, index) => (
-          <Input
-            key={`cvc-${index}`}
-            value={cvc.value}
-            maxLength={3}
-            placeholder="CVC 번호(카드 뒷면의 서명란에 인쇄된 숫자 끝 3자리)"
-            isValid={cvc.isValid}
-            onChange={(e) => handleChange(index, e)}
-            onBlur={(e) => handleBlur(index, e)}
-          />
-        ))}
+        <Input
+          autoFocus={CVCNumber.value.length === 0}
+          value={CVCNumber.value}
+          maxLength={3}
+          placeholder="CVC 번호(카드 뒷면의 서명란에 인쇄된 숫자 끝 3자리)"
+          isValid={CVCNumber.isValid}
+          onChange={(e) => handleChange(e)}
+        />
         <Text
           variant="Caption"
           color="red"
@@ -32,7 +42,7 @@ export const CVCForm = () => {
             height: 20px;
           `}
         >
-          {isInvalidCVC ? errorMessage : ''}
+          {!CVCNumber.isValid ? error : ''}
         </Text>
       </Flex>
     </CardInputLayout>
