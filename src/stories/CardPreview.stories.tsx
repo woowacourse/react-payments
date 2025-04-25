812b033735a297e1dd5c2e70d0badc752244cb39
keemsebeen <sebin0580@gmail.com>
2025-04-25 15:54:03 +0900

    refactor: CardPreview cardBrand에 따른 args 추가

--- src/stories/CardPreview.stories.tsx
+++ src/stories/CardPreview.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { CardInputType } from '@/components/features/CardFormFiled/CardFormFiled.types';
 import { CardPreview } from '@/components/features/CardPreview';
-import { CardInputType } from '@/hooks/useCardInput';
 
 const meta = {
   title: 'features/CardPreview',
@@ -31,6 +31,7 @@ export const Default: Story = {
       { value: '12', isValid: true },
       { value: '34', isValid: true },
     ],
+    cardBrand: 'BC카드',
   },
   argTypes: {
     cardNumbers: {
@@ -38,6 +39,10 @@ export const Default: Story = {
     },
     expireDate: {
       control: false,
+    },
+    cardBrand: {
+      control: { type: 'select' },
+      options: ['BC카드', '신한카드', '삼성카드', '현대카드', '롯데카드', '우리카드'],
     },
   },
 };
