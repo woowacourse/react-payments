b1e4e61343e024af7e24fd089cd78452ecef7b35
keemsebeen <sebin0580@gmail.com>
2025-04-25 15:44:34 +0900

    feat: Select 컴포넌트 구현

---
+++ src/components/common/Select/Select.styled.tsx
@@ -0,0 +1,64 @@
+import styled from '@emotion/styled';
+
+import { colors } from '@/styles/global';
+
+export const StyledSelectContainer = styled.div`
+  position: relative;
+  display: inline-block;
+  width: 100%;
+  border: 1px solid ${colors.GY2};
+  border-radius: 4px;
+  padding: 8px 12px;
+  cursor: pointer;
+`;
+
+export const StyledTriggerButton = styled.button`
+  background-color: transparent;
+  border: none;
+  padding: 0;
+`;
+
+export const StyledOptionsContainer = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  background-color: white;
+  border: 1px solid ${colors.GY2};
+  border-radius: 4px;
+  z-index: 10;
+  width: 100%;
+  max-height: 200px;
+  overflow-y: auto;
+  padding: 8px;
+  margin-top: 10px;
+  box-sizing: border-box;
+`;
+
+export const StyledSelectIcon = styled.img<{ isOpen: boolean }>`
+  position: absolute;
+  top: 8px;
+  right: 12px;
+  height: 24px;
+  transition: transform 0.3s ease;
+
+  transform: ${({ isOpen }) => isOpen && `rotate(180deg); `};
+`;
+
+export const StyledOption = styled.div`
+  background-color: white;
+
+  z-index: 1;
+  width: 100%;
+  max-height: 300px;
+  overflow-y: auto;
+  padding: 8px;
+  box-sizing: border-box;
+
+  :last-child {
+    border-bottom: none;
+  }
+  :hover {
+    border-radius: 4px;
+    background-color: rgba(205, 205, 205, 0.63);
+  }
+`;
---
+++ src/components/common/Select/index.tsx
@@ -0,0 +1,57 @@
+import { Children, ComponentProps, ComponentPropsWithoutRef, useState } from 'react';
+
+import {
+  StyledSelectContainer,
+  StyledOption,
+  StyledSelectIcon,
+  StyledTriggerButton,
+  StyledOptionsContainer,
+} from './Select.styled';
+
+import { Flex } from '../Flex';
+import { Text } from '../Text';
+
+export type Props = {
+  /**
+   * The currently selected category to be displayed.
+   */
+  selectedOptions: string;
+} & ComponentPropsWithoutRef<'div'>;
+
+export const Select = ({ selectedOptions, children, ...props }: Props) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <StyledSelectContainer onClick={() => setIsOpen(!isOpen)} {...props}>
+      <StyledTriggerButton>
+        <Flex justifyContent="space-between">
+          <Text variant="Body">{selectedOptions ?? '전체'}</Text>
+          <StyledSelectIcon isOpen={isOpen} src="/images/arrow.png" alt="arrow" />
+        </Flex>
+      </StyledTriggerButton>
+
+      {isOpen && <StyledOptionsContainer>{children}</StyledOptionsContainer>}
+    </StyledSelectContainer>
+  );
+};
+
+export type OptionProps = {
+  /**
+   * The currently selected category to be displayed.
+   */
+  option: string;
+  /**
+   * Callback function when a category is selected.
+   */
+  onSelectOption: (category: string) => void;
+} & ComponentProps<'div'>;
+
+const Option = ({ option, onSelectOption, ...props }: OptionProps) => {
+  return (
+    <StyledOption onClick={() => onSelectOption(option)} {...props}>
+      {option}
+    </StyledOption>
+  );
+};
+
+Select.Option = Option;
