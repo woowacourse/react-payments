18471f925cbf5ccd358baf61c0da4c536394e420
hoyyChoi <hoyeon8165@gmail.com>
2025-04-25 20:58:29 +0900

    feat: 카드사 색상 정리

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,3 +1,5 @@
+import { CompanyType } from "../types";
+
 export const MIN_MONTH = 1;
 export const MAX_MONTH = 12;
 
@@ -9,4 +11,15 @@ export const ERROR_MESSAGE = {
   IS_NOT_NUMBER: "숫자값만 입력해주세요.",
   MM_VALID: "유효한 월(1~12)만 입력해주세요.",
   YY_VALID: "유효한 년도(25~)만 입력해주세요.",
+};
+
+export const CARD_COMPANY_COLORS: Record<Exclude<CompanyType, "">, string> = {
+  BC카드: "#F04651",
+  신한카드: "#0046FF",
+  카카오뱅크: "#FFE600",
+  현대카드: "#000000",
+  우리카드: "#007BC8",
+  롯데카드: "#ED1C24",
+  하나카드: "#009490",
+  국민카드: "#6A6056",
 };
