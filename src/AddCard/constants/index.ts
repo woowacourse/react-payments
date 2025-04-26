1a02d8ec6e88651b34db9016b47d4ca6f131e9b8
spoyodevelop <kjy9505@gmail.com>
2025-04-26 23:25:30 +0900

    refactor: 인지 부하를 주는 뽑아둔 인덱스를 삭제.

--- src/AddCard/constants/index.ts
+++ src/AddCard/constants/index.ts
@@ -1,4 +1,5 @@
 import type { FlowStep } from "../types/hook";
+
 export const STEP_ORDER: FlowStep[] = [
   "CARD_NUMBER",
   "CARD_BRAND",
@@ -7,14 +8,5 @@ export const STEP_ORDER: FlowStep[] = [
   "PASSWORD",
   "COMPLETE",
 ];
-
-export const STEP_ORDER_INDEX: { [key in FlowStep]: number } = {
-  CARD_NUMBER: 1,
-  CARD_BRAND: 2,
-  EXPIRE_DATE: 3,
-  CVC: 4,
-  PASSWORD: 5,
-  COMPLETE: 6,
-};
 
 export const STEP_LABELS = ["카드번호", "카드사", "만료일", "CVC", "비밀번호"];
