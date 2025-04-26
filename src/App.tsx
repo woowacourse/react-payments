0371575ac7d578c8983adb21c62634457163765a
spoyodevelop <kjy9505@gmail.com>
2025-04-26 23:25:06 +0900

    refactor: url을 상수화

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import AddCard from "./AddCard/components/AddCard";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import AddCardCompleteModal from "./AddCard/components/AddCardCompleteModal/AddCardCompleteModal";
+import { locations } from "./AddCard/constants/locations";
 
 function App() {
   return (
-    <BrowserRouter basename="/react-payments">
+    <BrowserRouter basename={locations.BASE_URL}>
       <Routes>
         <Route index element={<AddCard />} />
-        <Route path="AddCardComplete" element={<AddCardCompleteModal />} />
+
+        <Route
+          path={locations.ADD_CARD_COMPLETE.pathname}
+          element={<AddCardCompleteModal />}
+        />
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
