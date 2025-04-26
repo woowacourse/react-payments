e8d1b9d6e31a611fa008102bdcf7c740b99c0a1b
spoyodevelop <kjy9505@gmail.com>
2025-04-26 14:56:41 +0900

    refactor: BrowserRouter를 사용하여 라우팅 구조 개선

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import AddCard from "./AddCard/components/AddCard";
 import { Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AddCardCompleteModal from "./AddCard/components/AddCardCompleteModal/AddCardCompleteModal";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AddCard />} />
-      <Route path="/AddCardComplete" element={<AddCardCompleteModal />} />
-    </Routes>
+    <BrowserRouter basename="/react-payments">
+      <Routes>
+        <Route index element={<AddCard />} />
+        <Route path="AddCardComplete" element={<AddCardCompleteModal />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import "./styles/reset.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* 서버에 따로 통신을 안한다고 가정한다면.. */}
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
