3d873875b9a7649bb8f1baa43f2e1009e7847de9
jin123457 <jinsol9909@gmail.com>
2025-04-26 16:09:30 +0900

    chore: dropdown 아이콘 추가

--- src/components/Icon/ArrowIcon.tsx
+++ src/components/Icon/ArrowIcon.tsx
@@ -11,9 +11,9 @@ export default function ArrowIcon({ color, rotate }: { color: string; rotate: st
       <path
         d="M9.1799 4.54979L5.35997 0.729858L1.54004 4.54979"
         stroke={color}
-        stroke-width="1.32867"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.32867"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
