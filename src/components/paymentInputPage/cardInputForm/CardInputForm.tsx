a28df337fcd5835b3750f7da2d689b461f6540b9
Jeongeun Lee <leeju1009@gmail.com>
2025-04-26 17:22:29 +0900

    feat: 모든 폼 데이터가 유효할때 확인 버튼이 생성되는 기능 추가

--- src/components/paymentInputPage/cardInputForm/CardInputForm.tsx
+++ src/components/paymentInputPage/cardInputForm/CardInputForm.tsx
@@ -4,24 +4,48 @@ import CardNumberInput from './cardInput/CardNumberInput';
 import styles from './cardInputForm.module.css';
 import { ExpirationDateType } from '../PaymentInputPage';
 import CardIssuerSelector from './cardInput/CardIssuerSelector';
+import { useState } from 'react';
+import CardSubmitButton from './CardSubmitButton';
 
 interface CardInputFormProps {
   cardIssuer: string;
   cardNumbers: string[];
   expirationDate: ExpirationDateType;
+  cardCVC: string;
   setCardIssuer: React.Dispatch<React.SetStateAction<string>>;
   setCardNumbers: React.Dispatch<React.SetStateAction<string[]>>;
   setExpirationDate: React.Dispatch<React.SetStateAction<ExpirationDateType>>;
+  setCardCVC: React.Dispatch<React.SetStateAction<string>>;
 }
 
 function CardInputForm({
   cardIssuer,
   cardNumbers,
   expirationDate,
+  cardCVC,
   setCardIssuer,
   setCardNumbers,
   setExpirationDate,
+  setCardCVC,
 }: CardInputFormProps) {
+  const [isCardNumberValid, setCardNumberIsValid] = useState<boolean[]>([
+    true,
+    true,
+    true,
+    true,
+  ]);
+  const [isExpirationDateValid, setIsExpirationDateValid] = useState({
+    month: true,
+    year: true,
+  });
+  const [isCVCValid, setIsCVCValid] = useState<boolean>(true);
+
+  const isTotalNumbersValid = isCardNumberValid.every((valid) => valid);
+  const isTotalExpirationDateValid =
+    isExpirationDateValid.month && isExpirationDateValid.year;
+  const isFormValid =
+    isTotalNumbersValid && isTotalExpirationDateValid && isCVCValid;
+
   return (
     <div className={styles.cardInputForm}>
       <CardIssuerSelector
@@ -31,12 +55,22 @@ function CardInputForm({
       <CardNumberInput
         cardNumbers={cardNumbers}
         setCardNumbers={setCardNumbers}
+        isValid={isCardNumberValid}
+        setIsValid={setCardNumberIsValid}
       />
       <CardExpirationDateInput
         expirationDate={expirationDate}
         setExpirationDate={setExpirationDate}
+        isValid={isExpirationDateValid}
+        setIsValid={setIsExpirationDateValid}
       />
-      <CardCVCInput />
+      <CardCVCInput
+        cardCVC={cardCVC}
+        setCardCVC={setCardCVC}
+        isValid={isCVCValid}
+        setIsValid={setIsCVCValid}
+      />
+      {isFormValid && <CardSubmitButton isFormValid={isFormValid} />}
     </div>
   );
 }
