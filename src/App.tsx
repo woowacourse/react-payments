4d9bbe5601df75bb1833c7183dfa001cbd4b2f13
eunoia-jaxson <eunoia@handong.ac.kr>
2025-04-27 01:35:44 +0900

    feat: App 컴포넌트, 홈페이지 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,27 @@
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './styles/theme';
-import { CardFormProvider, useCardForm } from './contexts/CardFormContext';
-import Preview from './components/Preview';
-import BrandSelect from './components/BrandSelect';
-import NumberInputs from './components/NumberInputs';
-import ExpirationPeriodInputs from './components/ExpirationPeriodInputs';
-import CVCNumberInput from './components/CVCNumberInput';
-import PasswordInput from './components/PasswordInput';
+import { CardFormProvider } from './contexts/CardFormContext';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CompletePage from './pages/CompletePage';
 import styled from '@emotion/styled';
 
-const ConfirmButton = () => {
-  const { isFormValid } = useCardForm();
-  return <>{isFormValid && <SubmitButton type="submit">확인</SubmitButton>}</>;
-};
-
 const App = () => {
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    alert('카드 정보가 제출되었습니다.');
-  };
-
   return (
     <ThemeProvider theme={theme}>
       <CardFormProvider>
         <Main>
-          <Preview />
-
-          <form onSubmit={onSubmit}>
-            <Column>
-              <PasswordInput />
-              <CVCNumberInput />
-              <ExpirationPeriodInputs />
-              <BrandSelect />
-              <NumberInputs />
-            </Column>
-            <ConfirmButton />
-          </form>
+          <Router>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/complete" element={<CompletePage />} />
+            </Routes>
+          </Router>
         </Main>
       </CardFormProvider>
     </ThemeProvider>
   );
 };
-
 export default App;
 
 const Main = styled.main`
@@ -54,26 +34,4 @@ const Main = styled.main`
   background-color: ${({ theme }) => theme.colors.background};
   gap: 24px;
   overflow: auto;
-`;
-
-const Column = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-  width: 100%;
-  padding: 0 28px;
-`;
-
-const SubmitButton = styled.button`
-  width: 100%;
-  background-color: ${({ theme }) => theme.colors.cardBrandColors.default};
-  color: ${({ theme }) => theme.colors.cardText};
-  font-size: 16px;
-  font-weight: 700;
-  position: sticky;
-  bottom: 0;
-  height: 52px;
-  cursor: pointer;
-  text-align: center;
-  border: none;
 `;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,51 @@
+import Preview from '../components/Preview';
+import BrandSelect from '../components/BrandSelect';
+import NumberInputs from '../components/NumberInputs';
+import ExpirationPeriodInputs from '../components/ExpirationPeriodInputs';
+import CVCNumberInput from '../components/CVCNumberInput';
+import PasswordInput from '../components/PasswordInput';
+import styled from '@emotion/styled';
+import { useNavigate } from 'react-router-dom';
+import SubmitButton from '../components/common/SubmitButton';
+import { useCardForm } from '../contexts/CardFormContext';
+
+const HomePage = () => {
+  const navigate = useNavigate();
+  const { numberFields, brand } = useCardForm();
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const params = new URLSearchParams({
+      number: numberFields[0].value,
+      brand,
+    });
+    navigate(`/complete?${params.toString()}`);
+  };
+
+  return (
+    <>
+      <Preview />
+
+      <form onSubmit={onSubmit}>
+        <Column>
+          <PasswordInput />
+          <CVCNumberInput />
+          <ExpirationPeriodInputs />
+          <BrandSelect />
+          <NumberInputs />
+        </Column>
+        <SubmitButton />
+      </form>
+    </>
+  );
+};
+
+export default HomePage;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 100%;
+  padding: 0 28px;
+`;
