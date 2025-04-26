8444e92b6856760f355d7ece99211805e464440a
jin123457 <jinsol9909@gmail.com>
2025-04-27 06:23:52 +0900

    docs: NotFound 스토리북 생성

---
+++ src/pages/NotFound/NotFound.stories.tsx
@@ -0,0 +1,29 @@
+import NotFound from './NotFound';
+import type { Meta, StoryObj } from '@storybook/react';
+import { BrowserRouter } from 'react-router';
+
+const meta = {
+  title: 'Pages/NotFound',
+  component: NotFound,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: '잘못된 경로로 접속을 했을때, 없는 페이지에 접근했을 때 표현되는 404 에러 페이지입니다.',
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+} satisfies Meta<typeof NotFound>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
