e2a65e880b40a21e32b443d57881db7ed0014dff
sooyeoniya <tndus1306403@naver.com>
2025-04-26 14:38:16 +0900

    fix: router 관련 storybook 오류 수정
    - router 관련 로직은 MemoryRouter로 감싸주어야 함
    - 기본 pathname 설정 및 useLocation으로 넘어온 state 초깃값 정의

--- src/pages/CardForm/CardForm.stories.tsx
+++ src/pages/CardForm/CardForm.stories.tsx
@@ -1,11 +1,26 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import CardForm from "./CardForm";
 import { withCustomCardProvider } from "../../../.storybook/utils/withCustomCardProvider";
+import { withCustomCardValidationProvider } from "../../../.storybook/utils/withCustomCardValidationProvider";
+import { MemoryRouter } from "react-router-dom";
 
 const meta = {
   title: "CardForm",
   component: CardForm,
   tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <MemoryRouter
+        initialEntries={[
+          {
+            pathname: "/",
+          },
+        ]}
+      >
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
 } satisfies Meta<typeof CardForm>;
 
 export default meta;
@@ -13,5 +28,8 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  decorators: [withCustomCardProvider({})],
+  decorators: [
+    withCustomCardProvider({}),
+    withCustomCardValidationProvider({}),
+  ],
 };
--- src/pages/RegisterComplete/RegisterComplete.stories.tsx
+++ src/pages/RegisterComplete/RegisterComplete.stories.tsx
@@ -1,10 +1,25 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import RegisterComplete from "./RegisterComplete";
+import { MemoryRouter } from "react-router-dom";
 
 const meta = {
   title: "RegisterComplete",
   component: RegisterComplete,
   tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <MemoryRouter
+        initialEntries={[
+          {
+            pathname: "/register-complete",
+            state: { cardFirstSegment: "1234", cardName: "BC카드" },
+          },
+        ]}
+      >
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
 } satisfies Meta<typeof RegisterComplete>;
 
 export default meta;
