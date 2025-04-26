0ed232c603639071b5f6a32591be7f741f0f4b2b
dlsxjzld <sinjuk1@naver.com>
2025-04-26 22:20:14 +0900

    refactor: InputProps 인터페이스를 React.InputHTMLAttributes에서 React.ComponentProps로 변경

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends React.ComponentProps<'input'> {
   isError: boolean;
   type?: string;
   placeholder?: string;
