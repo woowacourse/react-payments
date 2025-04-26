fe7433087be9c541b6a511ea0896f4a1847c8355
hoyyChoi <hoyeon8165@gmail.com>
2025-04-26 17:10:25 +0900

    feat: 동적 ui 생성 테스트

--- src/components/FormContainer/FormContainer.tsx
+++ src/components/FormContainer/FormContainer.tsx
@@ -2,44 +2,51 @@ import FormSection from "../FormSection/FormSection";
 import { css } from "@emotion/react";
 import { FormContainerProps } from "../../types/componentPropsType";
 import formUIControllerData from "../../constants/formUIControllerData";
+import { useState } from "react";
 
 const FormContainer = ({ cardInformationState, setCardInformationState, validation }: FormContainerProps) => {
+  const [step, setStep] = useState(0);
+
   return (
     <div css={FormContainerStyle}>
-      {formUIControllerData.map((formSectionData) => {
-        if (formSectionData.type === "input") {
-          const eachValidation = validation[formSectionData.key];
+      <button onClick={() => setStep(step + 1)}></button>
+      {formUIControllerData
+        .slice(0, step + 1)
+        .reverse()
+        .map((formSectionData) => {
+          if (formSectionData.type === "input") {
+            const eachValidation = validation[formSectionData.key];
+            return (
+              <FormSection
+                key={formSectionData.key}
+                type="input"
+                title={formSectionData.title}
+                description={formSectionData.description}
+                fieldData={{
+                  ...formSectionData.fieldData,
+                  cardInformation: cardInformationState,
+                  setCardInformation: setCardInformationState,
+                  eachValidation: eachValidation,
+                  informationType: formSectionData.key,
+                }}
+              />
+            );
+          }
+
           return (
             <FormSection
               key={formSectionData.key}
-              type="input"
+              type="select"
               title={formSectionData.title}
               description={formSectionData.description}
               fieldData={{
                 ...formSectionData.fieldData,
-                cardInformation: cardInformationState,
-                setCardInformation: setCardInformationState,
-                eachValidation: eachValidation,
                 informationType: formSectionData.key,
+                setCardInformation: setCardInformationState,
               }}
             />
           );
-        }
-
-        return (
-          <FormSection
-            key={formSectionData.key}
-            type="select"
-            title={formSectionData.title}
-            description={formSectionData.description}
-            fieldData={{
-              ...formSectionData.fieldData,
-              informationType: formSectionData.key,
-              setCardInformation: setCardInformationState,
-            }}
-          />
-        );
-      })}
+        })}
     </div>
   );
 };
@@ -47,6 +54,7 @@ const FormContainer = ({ cardInformationState, setCardInformationState, validati
 export default FormContainer;
 
 const FormContainerStyle = css`
+  width: 100%;
   display: flex;
   flex-direction: column;
   gap: 16px;
--- src/constants/formUIControllerData.ts
+++ src/constants/formUIControllerData.ts
@@ -13,6 +13,16 @@ const formUIControllerData: formSectionData[] = [
     },
   },
   {
+    key: "company",
+    title: "카드사를 선택해 주세요",
+    description: "현재 국내 카드사만 가능합니다.",
+    type: "select",
+    fieldData: {
+      options: ["BC카드", "신한카드", "카카오뱅크", "현대카드", "우리카드", "롯데카드", "하나카드", "국민카드"],
+      placeholder: "카드사를 선택해주세요",
+    },
+  },
+  {
     key: "expirationDate",
     title: "카드 유효기간을 입력해 주세요",
     description: "월/년도(MMYY)를 순서대로 입력해 주세요.",
@@ -43,16 +53,6 @@ const formUIControllerData: formSectionData[] = [
       label: "비밀번호 앞 2자리",
       inputNumber: 1,
       inputProps: { placeholder: [""], maxLength: 2, masking: true },
-    },
-  },
-  {
-    key: "company",
-    title: "카드사를 선택해 주세요",
-    description: "현재 국내 카드사만 가능합니다.",
-    type: "select",
-    fieldData: {
-      options: ["BC카드", "신한카드", "카카오뱅크", "현대카드", "우리카드", "롯데카드", "하나카드", "국민카드"],
-      placeholder: "카드사를 선택해주세요",
     },
   },
 ];
