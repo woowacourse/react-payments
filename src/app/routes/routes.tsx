02466709dcd3eb1a58282466826d893dd5953d92
eunwoo-levi <eunwoo1341@gmail.com>
2025-04-26 22:32:31 +0900

    feat: createRoutesFromElements 사용하여 효율적으로 Page 관리

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,14 +1,12 @@
-import { createBrowserRouter } from 'react-router';
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router';
 import CardPage from '../../pages/CardPage/CardPage';
 import RegisterPage from '../../pages/RegisterPage/RegisterPage';
 
-export const router = createBrowserRouter([
-  {
-    path: '/card',
-    element: <CardPage />,
-  },
-  {
-    path: '/register',
-    element: <RegisterPage />,
-  },
-]);
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<CardPage />} />,
+      <Route path='/register' element={<RegisterPage />} />
+    </>
+  )
+);
