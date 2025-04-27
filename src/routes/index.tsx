590e4f36b3c383c76bb328a9b306aa01aed9881a
ohgus <anakin1108@naver.com>
2025-04-27 15:46:34 +0900

    chore: react-router 초기 세팅

---
+++ src/routes/index.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router';
+
+import { Home } from './pages/Home';
+import { Register } from './pages/Register';
+import { Result } from './pages/Result';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/result',
+    element: <Result />,
+  },
+]);
+
+export const Router = () => {
+  return <RouterProvider router={router} />;
+};
