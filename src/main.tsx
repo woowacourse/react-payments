74b2a16dbc3aa9d95bc0e7dd0e0fa4047ba36384
spoyodevelop <kjy9505@gmail.com>
2025-04-27 20:18:35 +0900

    refactor: BrowserRouter를 main 에다 옮김

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import "./styles/reset.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { locations } from "./AddCard/constants/locations.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={locations.BASE_URL}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
