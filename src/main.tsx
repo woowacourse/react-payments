f135fbc7d136a51161b78639cd87a208c2ad5f44
spoyodevelop <kjy9505@gmail.com>
2025-04-27 22:14:45 +0900

    refactor: App 컴포넌트를 main.tsx에서 제거하고 createBrowserRouter로 대체

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,35 @@
 import "./styles/reset.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { locations } from "./AddCard/constants/locations.ts";
+import AddCardCompleteModal from "./AddCard/components/AddCardCompleteModal/AddCardCompleteModal.tsx";
+
+import AddCard from "./AddCard/components/AddCard.tsx";
+import FourOFourFallback from "./Fallback/FourOFourFallback.tsx";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AddCard />,
+    },
+    {
+      path: locations.ADD_CARD_COMPLETE,
+      element: <AddCardCompleteModal />,
+    },
+    {
+      path: "*",
+      element: <FourOFourFallback />,
+    },
+  ],
+  {
+    basename: locations.BASE_URL,
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={locations.BASE_URL}>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
