3c0dabb32e01863ac5b6f8dfd7eef3db19fb4105
Jeongeun Lee <leeju1009@gmail.com>
2025-04-27 19:31:20 +0900

    refactor: 카드 정보 입력 페이지로 돌아갈때 폼이 초기화되도록 수정

--- src/components/registerCompletePage.tsx/RegisterComplete.tsx
+++ src/components/registerCompletePage.tsx/RegisterComplete.tsx
@@ -1,16 +1,34 @@
 import { useNavigate } from 'react-router-dom';
 import Button from '../common/button/Button';
-import { ROUTER } from '../../global/constants';
+import { INITIAL_CARD, ROUTER } from '../../global/constants';
 import styles from './RegisterComplete.module.css';
 import useCard from '../../hooks/useCard';
 
 function RegisterComplete() {
-  const { cardNumbers, cardIssuer } = useCard();
+  const {
+    cardNumbers,
+    cardIssuer,
+    setCardNumbers,
+    setCardIssuer,
+    setExpiryDate,
+    setCardCVC,
+    setPassword,
+  } = useCard();
   const navigate = useNavigate();
 
   function navigateMainPage(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
+
+    initForm();
     navigate(ROUTER.main);
+  }
+
+  function initForm() {
+    setCardNumbers(INITIAL_CARD.cardNumbers);
+    setCardIssuer(INITIAL_CARD.cardIssuer);
+    setExpiryDate(INITIAL_CARD.expiryDate);
+    setCardCVC(INITIAL_CARD.cardCVC);
+    setPassword(INITIAL_CARD.password);
   }
 
   return (
