d9359ba56939d220753d9d4f951fffa62c4b96c3
sooyeoniya <tndus1306403@naver.com>
2025-04-27 14:09:17 +0900

    refactor: 선택된 카드 찾는 로직 수정
    - 기존에 있는 객체 배열 활용 -> 중복 코드 제거

--- src/hooks/useCardSubmit.ts
+++ src/hooks/useCardSubmit.ts
@@ -2,18 +2,7 @@ import { FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { useCard } from "./useCard";
 import { useCardValidation } from "./useCardValidation";
-
-const CARD_COMPANY_INFO = {
-  "": "",
-  bc: "BC카드",
-  shinhan: "신한카드",
-  kakaobank: "카카오뱅크",
-  hyundai: "현대카드",
-  woori: "우리카드",
-  lotte: "롯데카드",
-  hana: "하나카드",
-  kb: "국민카드",
-};
+import { CARD_COMPANY_INFO } from "../constants/constants";
 
 export function useCardSubmit() {
   const { cardCompany, cardNumbers, areAllFieldsFilled } = useCard();
@@ -24,10 +13,14 @@ export function useCardSubmit() {
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+
+    const selectedCard = CARD_COMPANY_INFO.find(
+      (card) => card.value === cardCompany
+    );
     navigate("/register-complete", {
       state: {
         cardFirstSegment: cardNumbers.first,
-        cardName: CARD_COMPANY_INFO[cardCompany],
+        cardName: selectedCard ? selectedCard.text : "카드",
       },
     });
   };
