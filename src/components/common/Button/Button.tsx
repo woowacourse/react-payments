cae2dc73ab657889832e5621ce29435f3fe5537a
sanghee01 <sang6299@naver.com>
2025-04-27 16:48:12 +0900

    refactor: Button 컴포넌트 범용적으로 만듦

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,13 +1,15 @@
+import { ButtonHTMLAttributes } from 'react';
 import styles from './Button.module.css';
 
 type Props = {
   text: string;
   height: string;
-};
+  borderRadius?: string;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function Button({ text, height }: Props) {
+export default function Button({ text, height, borderRadius, ...rest }: Props) {
   return (
-    <button className={styles.button} style={{ height }}>
+    <button className={styles.button} style={{ height, borderRadius }} {...rest}>
       {text}
     </button>
   );
