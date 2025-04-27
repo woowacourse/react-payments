0be70f31e5863f8a307e2fee6974cd39929c70f4
minji2219 <aabb0219@naver.com>
2025-04-27 16:09:22 +0900

    chore: 라우터 수정

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,16 +3,21 @@ import Main from '../page/main';
 import Confirm from '../page/confirm';
 import PATH from './path';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: PATH.MAIN,
+      element: <Main />,
+    },
+    {
+      path: PATH.CONFIRM,
+      element: <Confirm />,
+    },
+  ],
   {
-    path: PATH.MAIN,
-    element: <Main />,
-  },
-  {
-    path: PATH.CONFIRM,
-    element: <Confirm />,
-  },
-]);
+    basename: import.meta.env.BASE_URL,
+  }
+);
 
 export default function Router() {
   return <RouterProvider router={router} />;
