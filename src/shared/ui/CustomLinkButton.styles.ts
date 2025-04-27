2f460f65e2c70917c2cdf133b7d097f22c17d647
eunwoo-levi <eunwoo1341@gmail.com>
2025-04-28 01:20:25 +0900

    style: css 오타 수정

--- src/shared/ui/CustomLinkButton.styles.ts
+++ src/shared/ui/CustomLinkButton.styles.ts
@@ -4,7 +4,7 @@ import styled from '@emotion/styled';
 export const CustomLinkButton = styled.button<{ css?: SerializedStyles }>`
   width: 100%;
   height: 52px;
-  size: 16px;
+  font-size: 16px;
   font-weight: 700;
   text-align: center;
   background-color: #333333;
