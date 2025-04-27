10c9d1b9888239d37c1d0809f53e7fbe61fb52f9
Jeongeun Lee <leeju1009@gmail.com>
2025-04-27 18:30:49 +0900

    fix: 등록 완료 이미지가 보이지 않는 문제 수정

--- src/components/registerCompletePage.tsx/RegisterComplete.tsx
+++ src/components/registerCompletePage.tsx/RegisterComplete.tsx
@@ -16,7 +16,7 @@ function RegisterComplete() {
   return (
     <section className={styles.section}>
       <div className={styles.container}>
-        <img src='./CompleteIcon.png' alt='등록 완료 아이콘' />
+        <img src='/CompleteIcon.png' alt='등록 완료 아이콘' />
         <p className='tx-lg'>{cardNumbers[0]}로 시작하는</p>
         <p className='tx-lg'>{cardIssuer}가 등록되었어요.</p>
         <Button
