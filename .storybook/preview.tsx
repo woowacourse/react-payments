d14d84273a21a1f24972c59fd4af2ede52ae6097
guesung <gueit214@naver.com>
2025-04-27 11:58:46 +0900

    chore: GlobalStyle 설정

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,10 @@
 import type { Preview } from '@storybook/react';
 import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { Global, ThemeProvider } from '@emotion/react';
+
 import { theme } from '../src/styles/theme';
-import { ThemeProvider } from '@emotion/react';
+import global from '../src/styles/global';
+import React from 'react';
 
 const preview: Preview = {
   parameters: {
@@ -16,8 +19,11 @@ const preview: Preview = {
 
 export default preview;
 
+const GlobalStyles = () => <Global styles={global} />;
+
 export const decorators = [
   withThemeFromJSXProvider({
+    GlobalStyles, // Adds your GlobalStyles component to all stories
     themes: {
       light: theme,
       dark: theme,
