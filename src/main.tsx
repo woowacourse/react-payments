77737223b3bf41694549bb1695f4bcf2acb00ae1
hoyyChoi <hoyeon8165@gmail.com>
2025-04-27 13:15:36 +0900

    style: Index.css 위치 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles/index.css";
 import Router from "./routes/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
