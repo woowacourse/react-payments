3f366ca6d1a8f81f95c8dfc08b183aeebbae2aaa
guesung <gueit214@naver.com>
2025-04-27 11:11:19 +0900

    fix: fix the tsc

--- src/components/CardExpirationDateInputField/utils.ts
+++
@@ -1,9 +0,0 @@
-export const checkValidMonth = (value: string) => {
-  if (Number(value) >= 1 && Number(value) <= 12 && value.length === 2) return true;
-  return false;
-};
-
-export const checkValidYear = (value: string) => {
-  if (value.length === 2) return true;
-  return false;
-};
