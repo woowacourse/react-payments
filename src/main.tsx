773535f1874941b4f7a95c1fa8b86d2a6f80f205
guesung <gueit214@naver.com>
2025-04-27 12:28:24 +0900

    fix: HashRouter로 교체
    https://stackoverflow.com/questions/71984401/react-router-not-working-with-github-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
