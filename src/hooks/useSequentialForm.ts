f5e6c717b881997b58cd75ccff1886da410738f1
sooyeoniya <tndus1306403@naver.com>
2025-04-27 17:39:37 +0900

    refactor: useSequentialForm 중복 로직 통합

--- src/hooks/useSequentialForm.ts
+++ src/hooks/useSequentialForm.ts
@@ -12,15 +12,7 @@ const formSequence = [
   CARD_FORM_TYPE.password,
 ];
 
-const initialVisibleState = {
-  [CARD_FORM_TYPE.cardNumbers]: true,
-  [CARD_FORM_TYPE.cardCompany]: false,
-  [CARD_FORM_TYPE.expirationPeriod]: false,
-  [CARD_FORM_TYPE.cvcNumber]: false,
-  [CARD_FORM_TYPE.password]: false,
-};
-
-const initialFilledState = {
+const initialState = {
   [CARD_FORM_TYPE.cardNumbers]: false,
   [CARD_FORM_TYPE.cardCompany]: false,
   [CARD_FORM_TYPE.expirationPeriod]: false,
@@ -29,72 +21,35 @@ const initialFilledState = {
 };
 
 export default function useSequentialForm() {
-  const [visibleFields, setVisibleFields] =
-    useState<Record<CardFormType, boolean>>(initialVisibleState);
+  const [visibleFields, setVisibleFields] = useState<
+    Record<CardFormType, boolean>
+  >({ ...initialState, [CARD_FORM_TYPE.cardNumbers]: true });
   const [currentField, setCurrentField] = useState<CardFormType>(
     CARD_FORM_TYPE.cardNumbers
   );
-  const filledState = useRef(initialFilledState);
+  const filledState = useRef(initialState);
   const { hasErrorByType } = useCardValidation();
   const { isFieldFilled } = useCard();
 
   const updateFieldCompletionState = (isFieldComplete: boolean): boolean => {
-    switch (currentField) {
-      case CARD_FORM_TYPE.cardNumbers:
-        if (
-          filledState.current[CARD_FORM_TYPE.cardNumbers] !== isFieldComplete
-        ) {
-          filledState.current[CARD_FORM_TYPE.cardNumbers] = isFieldComplete;
-          return true;
-        }
-        return false;
-      case CARD_FORM_TYPE.cardCompany:
-        if (
-          filledState.current[CARD_FORM_TYPE.cardCompany] !== isFieldComplete
-        ) {
-          filledState.current[CARD_FORM_TYPE.cardCompany] = isFieldComplete;
-          return true;
-        }
-        return false;
-      case CARD_FORM_TYPE.expirationPeriod:
-        if (
-          filledState.current[CARD_FORM_TYPE.expirationPeriod] !==
-          isFieldComplete
-        ) {
-          filledState.current[CARD_FORM_TYPE.expirationPeriod] =
-            isFieldComplete;
-          return true;
-        }
-        return false;
-      case CARD_FORM_TYPE.cvcNumber:
-        if (filledState.current[CARD_FORM_TYPE.cvcNumber] !== isFieldComplete) {
-          filledState.current[CARD_FORM_TYPE.cvcNumber] = isFieldComplete;
-          return true;
-        }
-        return false;
-      case CARD_FORM_TYPE.password:
-        if (filledState.current[CARD_FORM_TYPE.password] !== isFieldComplete) {
-          filledState.current[CARD_FORM_TYPE.password] = isFieldComplete;
-          return true;
-        }
-        return false;
-      default:
-        return false;
+    if (filledState.current[currentField] !== isFieldComplete) {
+      filledState.current[currentField] = isFieldComplete;
+      return true;
     }
+    return false;
   };
 
   const showNextField = () => {
     const currentIndex = formSequence.indexOf(currentField);
-    if (currentIndex < formSequence.length - 1) {
-      const nextField = formSequence[currentIndex + 1];
+    const nextField = formSequence[currentIndex + 1];
+    if (!nextField) return;
 
-      setVisibleFields((prev) => ({
-        ...prev,
-        [nextField]: true,
-      }));
+    setVisibleFields((prev) => ({
+      ...prev,
+      [nextField]: true,
+    }));
 
-      setCurrentField(nextField);
-    }
+    setCurrentField(nextField);
   };
 
   useEffect(() => {
