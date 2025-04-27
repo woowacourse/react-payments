589b4bdd1768744cd15dd8abf397a333c7a5bc67
Jeongeun Lee <leeju1009@gmail.com>
2025-04-28 03:18:57 +0900

    fix: 배포 페이지 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-payments/',
+  base: '/react-payments',
   plugins: [react()],
 });
