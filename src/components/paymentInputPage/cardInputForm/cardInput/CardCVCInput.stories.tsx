e3d316313ff187f06356bf98cb83f64ae624cbd0
Jeongeun Lee <leeju1009@gmail.com>
2025-04-27 18:16:39 +0900

    test: 로직이 정상임에도 카드 CVC 컴포넌트 테스트가 통과되지 않는 문제 수정

--- src/components/paymentInputPage/cardInputForm/cardInput/CardCVCInput.stories.tsx
+++ src/components/paymentInputPage/cardInputForm/cardInput/CardCVCInput.stories.tsx
@@ -21,6 +21,10 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {};
 
 export const Error: Story = {
+  args: {
+    isValid: false,
+  },
+
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
     const firstInput = canvas.getByPlaceholderText('123');
