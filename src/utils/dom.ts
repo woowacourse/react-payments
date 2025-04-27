4b631df76ef78a020caad1fb8908e90162126e19
guesung <gueit214@naver.com>
2025-04-27 22:57:29 +0900

    fix: 에러 던지지 않도록 수정

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -4,7 +4,5 @@ export const $ = <TElement extends Element = HTMLElement, USelector extends stri
 ): USelector extends `${string}?` ? TElement | null : TElement => {
   const target = context.querySelector<TElement>(selector.endsWith('?') ? selector.slice(0, -1) : selector);
 
-  if (!target && !selector.endsWith('?')) throw new Error('존재하지 않는 요소입니다.');
-
   return target!;
 };
