81f0f7c000e98c103ee35dfbf848d8a628da9e6a
kimyou1102 <yoosk5485@naver.com>
2025-04-28 03:49:00 +0900

    refactor: 사용하지 않는 커스텀훅으로 인해 args로만 상태를 고정하여 UI를 확인하는 식으로 스토리 수정

--- src/components/cardInfoForm/CardCVCField/CardCVCField.stories.tsx
+++ src/components/cardInfoForm/CardCVCField/CardCVCField.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import CardCVCField from './CardCVCField';
-import useCardCVC from '../../../hooks/useCardCVC';
 
 const meta = {
   title: 'CardCVCField',
@@ -12,24 +11,6 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {
-    cardCVC: '',
-    isError: false,
-    onChange: () => {},
-  },
-  render: () => {
-    const { cardCVC, isCardCVCError, onChangeCVC } = useCardCVC();
-    return (
-      <CardCVCField
-        cardCVC={cardCVC}
-        isError={isCardCVCError}
-        onChange={onChangeCVC}
-      />
-    );
-  },
-};
-
-export const Pass: Story = {
   args: {
     cardCVC: '123',
     isError: false,
