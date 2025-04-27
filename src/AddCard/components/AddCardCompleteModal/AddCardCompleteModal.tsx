c608f63d3b527e62a75bde06aac6ea04a7494215
spoyodevelop <kjy9505@gmail.com>
2025-04-27 20:19:05 +0900

    refactor: 비 일관적인 코드 제거 및 handler 통합

--- src/AddCard/components/AddCardCompleteModal/AddCardCompleteModal.tsx
+++ src/AddCard/components/AddCardCompleteModal/AddCardCompleteModal.tsx
@@ -6,32 +6,28 @@ import styles from "./AddCardCompleteModal.module.css";
 import RoundCheckIcon from "@/components/RoundCheckedIcon/RoundCheckedIcon";
 import type { AddCardCompleteLocationState } from "@/AddCard/types/location";
 import Fallback from "@/components/Fallback/Fallback";
+import { locations } from "@/AddCard/constants/locations";
 
 export default function AddCardCompleteModal() {
   const location = useLocation();
   const navigate = useNavigate();
+
+  const handleAddCardConfirmButton = () => {
+    navigate(locations.ADD_CARD);
+  };
 
   if (!location.state) {
     return (
       <Fallback
         message="비정상적 접근입니다."
         buttonText="홈으로 돌아가기"
-        onButtonClick={() => navigate("/")}
+        onButtonClick={handleAddCardConfirmButton}
       />
     );
   }
 
   const { firstCardNumber, selectedBrand } =
     location.state as AddCardCompleteLocationState;
-
-  let displayBrand = selectedBrand;
-  if (selectedBrand === "카카오뱅크") {
-    displayBrand += " 카드";
-  }
-
-  const handleAddCardConfirmButton = () => {
-    navigate("/");
-  };
 
   return (
     <div
@@ -55,7 +51,8 @@ export default function AddCardCompleteModal() {
             id="card-complete-brand-description"
             className={styles.detailsSpan}
           >
-            {displayBrand}가 등록되었어요!
+            {selectedBrand === "카카오뱅크" ? "카카오뱅크 카드" : selectedBrand}
+            가 등록되었어요!
           </span>
         </p>
 
