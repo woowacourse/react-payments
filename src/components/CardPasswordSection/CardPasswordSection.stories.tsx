edeada5897a2cbf7d8d2ecc2b2089a370bbf0ae0
sanghee01 <sang6299@naver.com>
2025-04-28 09:48:36 +0900

    feat: CardPasswordSection 스토리북 코드 작성

---
+++ src/components/CardPasswordSection/CardPasswordSection.stories.tsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import type { Meta, StoryObj, StoryFn } from '@storybook/react';
+import CardPasswordSection from './CardPasswordSection';
+
+const meta: Meta<typeof CardPasswordSection> = {
+  title: 'CardPasswordSection',
+  component: CardPasswordSection
+};
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    cardPassword: '',
+    cardPasswordError: ''
+  },
+  render: ({ cardPassword: initialPassword }) => {
+    const [cardPassword, setCardPassword] = useState(initialPassword);
+    const [cardPasswordError, setCardPasswordError] = useState('');
+
+    const handleCardPasswordChange = (key: string, value: string) => {
+      setCardPassword(value);
+      setCardPasswordError('');
+    };
+
+    return (
+      <CardPasswordSection
+        cardPassword={cardPassword}
+        handleCardPasswordChange={handleCardPasswordChange}
+        cardPasswordError={cardPasswordError}
+      />
+    );
+  }
+};
+
+export const Valid: Story = {
+  args: {
+    cardPassword: '12',
+    cardPasswordError: ''
+  },
+  render: ({ cardPassword: initialPassword }) => {
+    const [cardPassword, setCardPassword] = useState(initialPassword);
+    const [cardPasswordError, setCardPasswordError] = useState('');
+
+    const handleCardPasswordChange = (key: string, value: string) => {
+      setCardPassword(value);
+      setCardPasswordError('');
+    };
+
+    return (
+      <CardPasswordSection
+        cardPassword={cardPassword}
+        handleCardPasswordChange={handleCardPasswordChange}
+        cardPasswordError={cardPasswordError}
+      />
+    );
+  }
+};
+
+export const InValid: Story = {
+  args: {
+    cardPassword: 'ab',
+    cardPasswordError: '숫자만 입력해 주세요.'
+  },
+  render: ({ cardPassword: initialPassword, cardPasswordError: initialError }) => {
+    const [cardPassword, setCardPassword] = useState(initialPassword);
+    const [cardPasswordError, setCardPasswordError] = useState(initialError);
+
+    const handleCardPasswordChange = (key: string, value: string) => {
+      setCardPassword(value);
+      setCardPasswordError('');
+    };
+
+    return (
+      <CardPasswordSection
+        cardPassword={cardPassword}
+        handleCardPasswordChange={handleCardPasswordChange}
+        cardPasswordError={cardPasswordError}
+      />
+    );
+  }
+};
