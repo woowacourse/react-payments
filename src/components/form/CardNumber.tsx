dac9d7d36b85d77c50ce679d3d4c29a019152689
aydenote <dslgpgh@gmail.com>
2025-04-28 15:15:39 +0900

    feat: 카드 숫자 포커스 이동 구현

--- src/components/form/CardNumber.tsx
+++ src/components/form/CardNumber.tsx
@@ -7,6 +7,7 @@ import findErrorOrder from "../../utils/findErrorOrder";
 import { CardNumberType } from "../../hooks/useCardNumber";
 import { CARD_MAX_LENGTH } from "../../utils/validation";
 import styled from "styled-components";
+import useAutoFocus from "../../hooks/useAutoFocus";
 
 interface CardNumberProps {
 	cardNumber: CardNumberType;
@@ -15,23 +16,30 @@ interface CardNumberProps {
 	error: CardNumberType;
 }
 
+const INPUT_COUNT = 4;
+
 const CardNumber = React.memo(({ cardNumber, onChange, onBlur, error }: CardNumberProps) => {
 	const orderLabels = ["first", "second", "third", "fourth"] as const;
+	const { inputRef, moveFocus } = useAutoFocus(INPUT_COUNT, CARD_MAX_LENGTH);
 
 	return (
 		<CardNumberWrap>
 			<Title>결제할 카드 번호를 입력해 주세요</Title>
 			<Description>본인 명의의 카드만 결제 가능합니다.</Description>
 			<InputField label="카드 번호" errorMessage={findErrorOrder(error as unknown as Record<string, string>)}>
-				{orderLabels.map((label: keyof CardNumberType) => (
+				{orderLabels.map((label: keyof CardNumberType, index: number) => (
 					<Input
 						key={label}
+						ref={inputRef[index]}
 						isError={!!error[label]}
 						placeholder="1234"
 						value={cardNumber[label]}
 						maxLength={CARD_MAX_LENGTH}
-						onChange={(numbers) => onChange(label, numbers)}
-						onBlur={(numbers) => onBlur(label, numbers)}
+						onChange={(e) => {
+							onChange(label, e.target.value);
+							moveFocus(index, e.target.value);
+						}}
+						onBlur={(e) => onBlur(label, e.target.value)}
 					/>
 				))}
 			</InputField>
