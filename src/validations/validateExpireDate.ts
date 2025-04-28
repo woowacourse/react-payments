c0c753ede1a85e69b2aebae2aea9654a76af9d18
ohgus <anakin1108@naver.com>
2025-04-28 17:16:25 +0900

    refactor: 년도 숫자 타입으로 변경

--- src/validations/validateExpireDate.ts
+++ src/validations/validateExpireDate.ts
@@ -4,7 +4,7 @@ import { ExpireDateInputKey } from '@/hooks/useExpireDateInput';
 
 export const validateExpireDate = (value: string, key: ExpireDateInputKey): ValidateReturnType => {
   const expireDateRegex = new RegExp(`^\\d{2}$`);
-  const currentYear = new Date().getFullYear().toString().slice(-2);
+  const currentYear = new Date().getFullYear() % 100;
 
   if (!expireDateRegex.test(value)) {
     return { isValid: false, errorMessage: '연도와 월은 두 자리 숫자를 입력하세요. 예시: 01, 12' };
@@ -17,7 +17,7 @@ export const validateExpireDate = (value: string, key: ExpireDateInputKey): Vali
     };
   }
 
-  if (key === 'year' && parseInt(value) < parseInt(currentYear)) {
+  if (key === 'year' && parseInt(value) < currentYear) {
     return {
       isValid: false,
       errorMessage: `만료 년도는 현재 년도보다 낮을 수 없습니다. 현재 연도: ${currentYear}`,
