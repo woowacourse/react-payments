affcddf4d4638ca0d46e2c908d35efa1dcfd8ca7
SuHwa Lee <tnghk9611@naver.com>
2025-04-28 16:40:53 +0900

    refactor: 베이스 경로 재설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import GlobalStyles from './styles/GlobalStyles';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import CardPage from './pages/CardPage/CardPage';
 import CardCompletePage from './pages/CardCompletePage/CardCompletePage';
-import { DEVELOPMENT_BASE_URL, PRODUCTION_BASE_URL, ROUTER } from './constants/router';
+import { PRODUCTION_BASE_URL, ROUTER } from './constants/router';
 
 function App() {
-  const basename =
-    import.meta.env.MODE === 'production' ? PRODUCTION_BASE_URL : DEVELOPMENT_BASE_URL;
-
   return (
-    <BrowserRouter basename={basename}>
+    <BrowserRouter basename={PRODUCTION_BASE_URL}>
       <GlobalStyles />
       <Routes>
         <Route path={ROUTER.DEFAULT} element={<CardPage />} />
