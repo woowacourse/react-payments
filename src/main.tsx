70ff84e4ba5bcdcbd0bd933538cdfb250bb6ee0a
dlsxjzld <sinjuk1@naver.com>
2025-04-28 16:52:25 +0900

    feat: 404 페이지 추가 및 BrowserRouter의 basename 설정

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
