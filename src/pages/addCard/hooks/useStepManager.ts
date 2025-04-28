eaeb85a8c2b8e643a4d53382d7bf10644028bb14
mlnwns <mlnwns@naver.com>
2025-04-28 21:00:03 +0900

    refactor: isFormsCompleted 상태 관리 로직 간소화

--- src/pages/addCard/hooks/useStepManager.ts
+++ src/pages/addCard/hooks/useStepManager.ts
@@ -4,19 +4,17 @@ import { CardInfo } from "../../../hooks/useCardInfo";
 
 export function useStepManager(cardInfo: CardInfo) {
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
-  const [isFormsCompleted, setIsFormsCompleted] = useState(false);
 
   useEffect(() => {
     const isCurrentStepValid = STEPS[currentStepIndex].validate(cardInfo);
     const isLastStep = currentStepIndex === STEPS.length - 1;
-
-    const isAllStepsValid = STEPS.every((step) => step.validate(cardInfo));
-    setIsFormsCompleted(isAllStepsValid);
 
     if (isCurrentStepValid && !isLastStep) {
       setCurrentStepIndex(currentStepIndex + 1);
     }
   }, [cardInfo, currentStepIndex]);
 
-  return { currentStepIndex, setCurrentStepIndex, isFormsCompleted };
+  const isFormsCompleted = STEPS.every((step) => step.validate(cardInfo));
+
+  return { currentStepIndex, isFormsCompleted };
 }
