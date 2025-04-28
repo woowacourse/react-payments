8ea1dac1bfe5569c53bff09ed8763efd3b59329a
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-04-28 14:56:26 +0900

    ♻️ refactor: 카드사 목록 상수화

--- src/components/CardBrandInput/CardBrandInput.tsx
+++ src/components/CardBrandInput/CardBrandInput.tsx
@@ -1,3 +1,4 @@
+import { CARD_COMPANIES } from "../../constants/constants";
 import { SelectCSS } from "./CardBrandInput.styled";
 
 interface CardBrandInputProps {
@@ -15,14 +16,11 @@ function CardBrandInput({ value, handleCardBrandChange }: CardBrandInputProps) {
       <option value="" hidden>
         카드사를 선택하세요
       </option>
-      <option value="BC카드">BC카드</option>
-      <option value="신한카드">신한카드</option>
-      <option value="카카오뱅크">카카오뱅크</option>
-      <option value="현대카드">현대카드</option>
-      <option value="우리카드">우리카드</option>
-      <option value="롯데카드">롯데카드</option>
-      <option value="하나카드">하나카드</option>
-      <option value="국민카드">국민카드</option>
+      {CARD_COMPANIES.map((company) => (
+        <option key={company} value={company}>
+          {company}
+        </option>
+      ))}
     </SelectCSS>
   );
 }
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -14,6 +14,17 @@ export const MAGIC_NUMBER = {
   },
 };
 
+export const CARD_COMPANIES = [
+  "BC카드",
+  "신한카드",
+  "카카오뱅크",
+  "현대카드",
+  "우리카드",
+  "롯데카드",
+  "하나카드",
+  "국민카드",
+] as const;
+
 export const INPUT_TYPE = {
   cardNumbers: "cardNumbers",
   expirationPeriod: "expirationPeriod",
