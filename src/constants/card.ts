84bd51e0dea12ffdc4cedbf118017d0d427e4f6e
guesung <gueit214@naver.com>
2025-04-28 17:26:56 +0900

    refactor: src/constants 폴더로 통일

--- src/constants/card.ts
+++ src/constants/card.ts
@@ -2,23 +2,6 @@ export const VISA_CARD_CONDITIONS = ['4'];
 export const MASTER_CARD_CONDITIONS = ['51', '52', '53', '54', '55'];
 export const MASKING = '·';
 
-export const ERROR_MESSAGE = {
-  cardPassword: {
-    length: '비밀번호는 2자리 이상이어야 합니다.',
-  },
-  cardNumber: {
-    number: '카드 번호는 오로지 숫자만 입력 가능합니다.',
-    length: '카드 번호는 4자리씩 입력해주세요.',
-  },
-  cardExpirationDate: {
-    month: 'MM형식의 유효한 월을 입력해주세요.',
-    year: 'YY형식의 유효한 년도를 입력해주세요.',
-  },
-  cardCVCNumber: {
-    length: 'CVC 번호는 3자리만 입력해주세요.',
-  },
-} as const;
-
 export const CARD_COMPANIES = [
   { id: 'bc', name: 'BC카드', color: '#E74C3C' },
   { id: 'shinhan', name: '신한카드', color: '#3498DB' },
