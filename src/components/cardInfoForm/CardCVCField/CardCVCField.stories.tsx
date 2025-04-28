12d1cacddcd853d98f9535982e981deb86e36e3a
dlsxjzld <sinjuk1@naver.com>
2025-04-28 15:03:21 +0900

    refactor: 카드 CVC  스토리의 내부 로직을 커스텀 훅으로 리팩터링

--- src/components/cardInfoForm/CardCVCField/CardCVCField.stories.tsx
+++ src/components/cardInfoForm/CardCVCField/CardCVCField.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import CardCVCField from './CardCVCField';
-import { useState } from 'react';
+import useCardCVC from '../../../hooks/useCardCVC';
 
 const meta = {
   title: 'CardCVCField',
@@ -18,27 +18,17 @@ export const Default: Story = {
     onChange: () => {},
   },
   render: (args) => {
-    const [cardCVC, setCardCVC] = useState('123');
-    const [isCardCVCError, setIsCardCVCError] = useState(false);
+    const {
+      cardCVC,
+      onChangeCVC,
+      errorMessage: cardCVCErrorMessage,
+    } = useCardCVC();
 
-    const onChangeCVC = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const { value } = e.target;
-
-      if (value.length > 3) {
-        return;
-      }
-
-      const isNotValid =
-        value.length < 3 || value.length > 3 || Number(value) < 0;
-
-      setIsCardCVCError(isNotValid);
-      setCardCVC(value);
-    };
     return (
       <CardCVCField
         {...args}
         cardCVC={cardCVC}
-        isError={isCardCVCError}
+        isError={Boolean(cardCVCErrorMessage)}
         onChange={onChangeCVC}
       />
     );
@@ -52,30 +42,6 @@ export const Error: Story = {
     onChange: () => {},
   },
   render: (args) => {
-    const [cardCVC, setCardCVC] = useState('12');
-    const [isCardCVCError, setIsCardCVCError] = useState(true);
-
-    const onChangeCVC = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const { value } = e.target;
-
-      if (value.length > 3) {
-        return;
-      }
-
-      const isNotValid =
-        value.length < 3 || value.length > 3 || Number(value) < 0;
-
-      setIsCardCVCError(isNotValid);
-      setCardCVC(value);
-    };
-
-    return (
-      <CardCVCField
-        {...args}
-        cardCVC={cardCVC}
-        isError={isCardCVCError}
-        onChange={onChangeCVC}
-      />
-    );
+    return <CardCVCField {...args} />;
   },
 };
