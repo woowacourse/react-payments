d1b73742026f7d7e821d9caee56cdc3ada25f3f8
youdame <112458620+youdame@users.noreply.github.com>
2025-04-28 16:05:29 +0900

    feat: base_url 설정

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import '../src/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
