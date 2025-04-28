fb13d11890d2ec8bd8c52496d9e370cdc93b1a05
eunwoo-levi <eunwoo1341@gmail.com>
2025-04-28 16:20:54 +0900

    refactor: ref 매개변수 로직에 맞게 수정

--- src/shared/ui/CustomInput.tsx
+++ src/shared/ui/CustomInput.tsx
@@ -3,14 +3,14 @@ import { CustomInputProps } from '../model/types';
 import * as S from './customInput.styles';
 
 interface CustomInputAllProps extends CustomInputProps {
-  refProp?: React.Ref<HTMLInputElement>;
+  ref?: React.Ref<HTMLInputElement>;
 }
 
-export default function CustomInput({ onBlur, handleRef, error, onChange, refProp, ...props }: CustomInputAllProps) {
+export default function CustomInput({ onBlur, handleRef, error, onChange, ref, ...props }: CustomInputAllProps) {
   return (
     <S.CustomInput
       error={error}
-      ref={refProp}
+      ref={ref}
       onChange={(e) => {
         handleRef?.(e);
         onChange?.(e);
