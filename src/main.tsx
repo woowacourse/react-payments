0a4c6ad71d352e3b7d8a323c84e6ed86c1169271
Minje Sung <minje813@dgu.ac.kr>
2025-04-28 16:34:58 +0900

    fix: BrowserRouter -> HashRouter로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
