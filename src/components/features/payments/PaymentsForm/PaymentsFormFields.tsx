8514ebcaabae7826cc693138257b803198caf85c
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-04-28 14:47:45 +0900

    refactor: Payments 컴포넌트 분리

---
+++ src/components/features/payments/PaymentsForm/PaymentsFormFields.tsx
@@ -0,0 +1,168 @@
+import { RefObject } from 'react';
+import styled from 'styled-components';
+import { DropdownOptionType } from '../../../../types/dropdown';
+import ConditionalRender from '../../../common/ConditionalRender/ConditionalRender';
+import Dropdown from '../../../common/Dropdown/Dropdown';
+import CVCInputField from '../InputField/CVCInputField';
+import CardNumberInputField from '../InputField/CardNumberInputField';
+import CardPasswordInputField from '../InputField/CardPasswordInputField';
+import ExpirationDateInputField from '../InputField/ExpirationDateInputField';
+import InputSection from '../InputSection/InputSection';
+import { CARD_BANK } from '../config/card';
+import { ErrorType } from '../config/error';
+import {
+  CardNumberInputType,
+  ExpirationDateInputType,
+} from '../config/inputField';
+import { INPUT_STEP } from '../config/step';
+import PaymentsSubmitButton from './PaymentsSubmitButton';
+
+type PaymentsFormFieldsProps = {
+  paymentsFormValues: {
+    cardNumber: Record<CardNumberInputType, string>;
+    cardBank: DropdownOptionType | null;
+    expirationDate: Record<ExpirationDateInputType, string>;
+    CVC: string;
+    cardPassword: string;
+  };
+  paymentsFormInputRefs: {
+    cardNumber: Record<CardNumberInputType, RefObject<HTMLInputElement | null>>;
+    expirationDate: Record<
+      ExpirationDateInputType,
+      RefObject<HTMLInputElement | null>
+    >;
+    CVC: RefObject<HTMLInputElement | null>;
+    cardPassword: RefObject<HTMLInputElement | null>;
+  };
+  paymentsFormErrorTypes: {
+    cardNumber: Record<CardNumberInputType, ErrorType[]>;
+    CVC: ErrorType[];
+    cardPassword: ErrorType[];
+  };
+  paymentsFormHandlers: {
+    cardNumber: ({
+      name,
+      value,
+    }: {
+      name: CardNumberInputType;
+      value: string;
+    }) => void;
+    cardBank: React.Dispatch<React.SetStateAction<DropdownOptionType | null>>;
+    expirationDate: ({
+      name,
+      value,
+    }: {
+      name: ExpirationDateInputType;
+      value: string;
+    }) => void;
+    CVC: (value: string) => void;
+    cardPassword: (value: string) => void;
+  };
+  paymentFormBlurHandlers: {
+    cardNumber: (e: React.ChangeEvent) => void;
+    expirationDate: (e: React.ChangeEvent) => void;
+    CVC: (e: React.ChangeEvent) => void;
+    cardPassword: (e: React.ChangeEvent) => void;
+  };
+  inputStep: number;
+  allInputComplete: boolean;
+  handleSubmit: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+function PaymentsFormFields({
+  paymentsFormValues,
+  paymentsFormInputRefs,
+  paymentsFormErrorTypes,
+  paymentsFormHandlers,
+  paymentFormBlurHandlers,
+  inputStep,
+  allInputComplete,
+  handleSubmit,
+}: PaymentsFormFieldsProps) {
+  return (
+    <PaymentsInputForm>
+      <ConditionalRender condition={inputStep >= INPUT_STEP.cardPassword}>
+        <InputSection
+          title="비밀번호를 입력해 주세요"
+          caption="앞의 2자리를 입력해주세요"
+        >
+          <CardPasswordInputField
+            inputValue={paymentsFormValues.cardPassword}
+            inputRef={paymentsFormInputRefs.cardPassword}
+            errorTypes={paymentsFormErrorTypes.cardPassword}
+            handleInputValue={paymentsFormHandlers.cardPassword}
+            onBlur={paymentFormBlurHandlers.cardPassword}
+          />
+        </InputSection>
+      </ConditionalRender>
+
+      <ConditionalRender condition={inputStep >= INPUT_STEP.CVC}>
+        <InputSection title="CVC 번호를 입력해 주세요">
+          <CVCInputField
+            inputValue={paymentsFormValues.CVC}
+            inputRef={paymentsFormInputRefs.CVC}
+            errorTypes={paymentsFormErrorTypes.CVC}
+            handleInputValue={paymentsFormHandlers.CVC}
+            onBlur={paymentFormBlurHandlers.CVC}
+          />
+        </InputSection>
+      </ConditionalRender>
+
+      <ConditionalRender condition={inputStep >= INPUT_STEP.expirationDate}>
+        <InputSection
+          title="카드 유효기간을 입력해 주세요"
+          caption="월/년도(MMYY)를 순서대로 입력해 주세요."
+        >
+          <ExpirationDateInputField
+            inputValues={paymentsFormValues.expirationDate}
+            inputRefs={paymentsFormInputRefs.expirationDate}
+            handleInputValue={paymentsFormHandlers.expirationDate}
+            onBlur={paymentFormBlurHandlers.expirationDate}
+          />
+        </InputSection>
+      </ConditionalRender>
+
+      <ConditionalRender condition={inputStep >= INPUT_STEP.cardBank}>
+        <InputSection
+          title="카드사를 선택해 주세요"
+          caption="현재 국내 카드사만 가능합니다."
+        >
+          <Dropdown
+            options={Object.entries(CARD_BANK).map(([key, value]) => ({
+              label: value.label,
+              value: key,
+            }))}
+            selectedValue={paymentsFormValues.cardBank}
+            setSelectedValue={paymentsFormHandlers.cardBank}
+            placeholder="카드사를 선택해주세요"
+          />
+        </InputSection>
+      </ConditionalRender>
+
+      <InputSection
+        title="결제할 카드 번호를 입력해 주세요"
+        caption="본인 명의의 카드만 결제 가능합니다."
+      >
+        <CardNumberInputField
+          inputValues={paymentsFormValues.cardNumber}
+          inputRefs={paymentsFormInputRefs.cardNumber}
+          errorTypes={paymentsFormErrorTypes.cardNumber}
+          handleInputValue={paymentsFormHandlers.cardNumber}
+          onBlur={paymentFormBlurHandlers.cardNumber}
+        />
+      </InputSection>
+
+      <ConditionalRender condition={allInputComplete}>
+        <PaymentsSubmitButton handleSubmit={handleSubmit} />
+      </ConditionalRender>
+    </PaymentsInputForm>
+  );
+}
+
+const PaymentsInputForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export default PaymentsFormFields;
--- src/pages/payments/Payments.tsx
+++ src/pages/payments/Payments.tsx
@@ -1,17 +1,8 @@
 import styled from 'styled-components';
-import ConditionalRender from '../../components/common/ConditionalRender/ConditionalRender';
-import Dropdown from '../../components/common/Dropdown/Dropdown';
-import FooterButton from '../../components/common/FooterButton/FooterButton';
 import MobileLayoutContainer from '../../components/common/MobileLayoutContainer/MobileLayoutContainer';
 import CardPreview from '../../components/features/payments/CardPreview/CardPreview';
-import { CARD_BANK } from '../../components/features/payments/config/card';
-import { INPUT_STEP } from '../../components/features/payments/config/step';
 import usePaymentsForm from '../../components/features/payments/hooks/usePaymentsForm';
-import CardNumberInputField from '../../components/features/payments/InputField/CardNumberInputField';
-import CardPasswordInputField from '../../components/features/payments/InputField/CardPasswordInputField';
-import CVCInputField from '../../components/features/payments/InputField/CVCInputField';
-import ExpirationDateInputField from '../../components/features/payments/InputField/ExpirationDateInputField';
-import InputSection from '../../components/features/payments/InputSection/InputSection';
+import PaymentsFormFields from '../../components/features/payments/PaymentsForm/PaymentsFormFields';
 
 function Payments() {
   const {
@@ -35,86 +26,16 @@ function Payments() {
           expirationDateInputValue={paymentsFormValues.expirationDate}
           cardType={cardType}
         />
-        <PaymentsInputForm>
-          <ConditionalRender condition={inputStep >= INPUT_STEP.cardPassword}>
-            <InputSection
-              title="비밀번호를 입력해 주세요"
-              caption="앞의 2자리를 입력해주세요"
-            >
-              <CardPasswordInputField
-                inputValue={paymentsFormValues.cardPassword}
-                inputRef={paymentsFormInputRefs.cardPassword}
-                errorTypes={paymentsFormErrorTypes.cardPassword}
-                handleInputValue={paymentsFormHandlers.cardPassword}
-                onBlur={paymentFormBlurHandlers.cardPassword}
-              />
-            </InputSection>
-          </ConditionalRender>
-
-          <ConditionalRender condition={inputStep >= INPUT_STEP.CVC}>
-            <InputSection title="CVC 번호를 입력해 주세요">
-              <CVCInputField
-                inputValue={paymentsFormValues.CVC}
-                inputRef={paymentsFormInputRefs.CVC}
-                errorTypes={paymentsFormErrorTypes.CVC}
-                handleInputValue={paymentsFormHandlers.CVC}
-                onBlur={paymentFormBlurHandlers.CVC}
-              />
-            </InputSection>
-          </ConditionalRender>
-
-          <ConditionalRender condition={inputStep >= INPUT_STEP.expirationDate}>
-            <InputSection
-              title="카드 유효기간을 입력해 주세요"
-              caption="월/년도(MMYY)를 순서대로 입력해 주세요."
-            >
-              <ExpirationDateInputField
-                inputValues={paymentsFormValues.expirationDate}
-                inputRefs={paymentsFormInputRefs.expirationDate}
-                handleInputValue={paymentsFormHandlers.expirationDate}
-                onBlur={paymentFormBlurHandlers.expirationDate}
-              />
-            </InputSection>
-          </ConditionalRender>
-
-          <ConditionalRender condition={inputStep >= INPUT_STEP.cardBank}>
-            <InputSection
-              title="카드사를 선택해 주세요"
-              caption="현재 국내 카드사만 가능합니다."
-            >
-              <Dropdown
-                options={Object.entries(CARD_BANK).map(([key, value]) => ({
-                  label: value.label,
-                  value: key,
-                }))}
-                selectedValue={paymentsFormValues.cardBank}
-                setSelectedValue={paymentsFormHandlers.cardBank}
-                placeholder="카드사를 선택해주세요"
-              />
-            </InputSection>
-          </ConditionalRender>
-
-          <InputSection
-            title="결제할 카드 번호를 입력해 주세요"
-            caption="본인 명의의 카드만 결제 가능합니다."
-          >
-            <CardNumberInputField
-              inputValues={paymentsFormValues.cardNumber}
-              inputRefs={paymentsFormInputRefs.cardNumber}
-              errorTypes={paymentsFormErrorTypes.cardNumber}
-              handleInputValue={paymentsFormHandlers.cardNumber}
-              onBlur={paymentFormBlurHandlers.cardNumber}
-            />
-          </InputSection>
-
-          <ConditionalRender condition={allInputComplete}>
-            <SubmitButtonWrapper>
-              <FooterButton size="large" onClick={handleSubmit}>
-                확인
-              </FooterButton>
-            </SubmitButtonWrapper>
-          </ConditionalRender>
-        </PaymentsInputForm>
+        <PaymentsFormFields
+          paymentsFormValues={paymentsFormValues}
+          paymentsFormInputRefs={paymentsFormInputRefs}
+          paymentsFormErrorTypes={paymentsFormErrorTypes}
+          paymentsFormHandlers={paymentsFormHandlers}
+          paymentFormBlurHandlers={paymentFormBlurHandlers}
+          inputStep={inputStep}
+          allInputComplete={allInputComplete}
+          handleSubmit={handleSubmit}
+        />
       </PaymentsContainer>
     </MobileLayoutContainer>
   );
@@ -128,17 +49,6 @@ const PaymentsContainer = styled.div`
   box-sizing: border-box;
   padding: 44px 28px;
   min-height: 100vh;
-`;
-
-const PaymentsInputForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-`;
-
-const SubmitButtonWrapper = styled.div`
-  position: sticky;
-  bottom: 20px;
 `;
 
 export default Payments;
