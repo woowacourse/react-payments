54becd6ddddf50b80b9c0b4cd571999157f5c45e
dlsxjzld <sinjuk1@naver.com>
2025-04-28 11:55:52 +0900

    feat: App 컴포넌트를 BrowserRouter로 감싸 라우팅 기능 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './reset.css';
+import { BrowserRouter } from 'react-router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
