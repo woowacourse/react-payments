ff3b7eeff039268dfe1fc22a164ff9c4f5361588
sanghee01 <sang6299@naver.com>
2025-04-28 10:28:16 +0900

    fix: BrowserRouter에 basename 속성을 추가
    GitHub Pages에서 "No routes matched location "/react-payments/" 에러가 발생해서 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-payments">
       <App />
     </BrowserRouter>
   </React.StrictMode>
