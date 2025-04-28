dae43888c5c1268afe6b431caeca0903077394f5
aydenote <dslgpgh@gmail.com>
2025-04-28 15:54:38 +0900

    chore: 페이지 라우트 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import Router from "./Router";
+import { BrowserRouter, Route, Routes } from "react-router";
+import CardInfo from "./components/page/CardInfo";
+import CardCompletePage from "./components/page/CardAddSuccess";
 
 function App() {
-	return <Router />;
+	<BrowserRouter basename={"/react-payments"}>
+		<Routes>
+			<Route path="/" element={<CardInfo />} />
+			<Route path="/add-card" element={<CardCompletePage />} />
+		</Routes>
+	</BrowserRouter>;
 }
 
 export default App;
