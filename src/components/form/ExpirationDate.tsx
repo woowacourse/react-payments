c6f5c1782691e3f0cd8059b4655b395df27baba6
aydenote <dslgpgh@gmail.com>
2025-04-28 15:16:04 +0900

    feat: 카드 유효 기간 포커스 이동 구현

--- src/components/form/ExpirationDate.tsx
+++ src/components/form/ExpirationDate.tsx
@@ -5,6 +5,7 @@ import InputField from "../inputField/InputField";
 import Title from "../title/Title";
 import Input from "../input/Input";
 import findErrorOrder from "../../utils/findErrorOrder";
+import useAutoFocus from "../../hooks/useAutoFocus";
 import { EXPIRATION_MAX_LENGTH } from "../../utils/validation";
 import styled from "styled-components";
 
@@ -15,8 +16,11 @@ interface ExpirationDateProps {
 	error: ExpirationDateType;
 }
 
+const INPUT_COUNT = 2;
+
 const ExpirationDate = React.memo(({ expirationDate, onChange, onBlur, error }: ExpirationDateProps) => {
 	const orderLabels = ["month", "year"] as const;
+	const { inputRef, moveFocus } = useAutoFocus(INPUT_COUNT, EXPIRATION_MAX_LENGTH);
 	const placeholderMap = {
 		month: "MM",
 		year: "YY",
@@ -27,16 +31,20 @@ const ExpirationDate = React.memo(({ expirationDate, onChange, onBlur, error }: 
 			<Title>카드 유효기간을 입력해 주세요</Title>
 			<Description>월/년도(MMYY)를 순서대로 입력해 주세요.</Description>
 			<InputField label="유효기간" errorMessage={findErrorOrder(error as unknown as Record<string, string>)}>
-				{orderLabels.map((label: keyof ExpirationDateType) => {
+				{orderLabels.map((label: keyof ExpirationDateType, index: number) => {
 					return (
 						<Input
 							key={label}
+							ref={inputRef[index]}
 							isError={!!error[label]}
 							placeholder={placeholderMap[label]}
 							value={expirationDate[label]}
 							maxLength={EXPIRATION_MAX_LENGTH}
-							onChange={(numbers) => onChange(label, numbers)}
-							onBlur={(numbers) => onBlur(label, numbers)}
+							onChange={(e) => {
+								onChange(label, e.target.value);
+								moveFocus(index, e.target.value);
+							}}
+							onBlur={(e) => onBlur(label, e.target.value)}
 						/>
 					);
 				})}
