ea5742b9643d6dc4a7f828eb18589f26d52ff2c0
aydenote <dslgpgh@gmail.com>
2025-04-28 15:15:56 +0900

    feat: 카드 패스워드 포커스 이동 구현

--- src/components/form/CardPassword.tsx
+++ src/components/form/CardPassword.tsx
@@ -5,6 +5,7 @@ import Input from "../input/Input";
 import Description from "../description/Description";
 import { PASSWORD_MAX_LENGTH } from "../../utils/validation";
 import styled from "styled-components";
+import useAutoFocus from "../../hooks/useAutoFocus";
 
 interface CardPasswordProps {
 	cardPassword: string;
@@ -13,13 +14,25 @@ interface CardPasswordProps {
 	error: string;
 }
 
+const INPUT_COUNT = 1;
+
 const CardPassword = React.memo(({ cardPassword, onChange, onBlur, error }: CardPasswordProps) => {
+	const { inputRef } = useAutoFocus(INPUT_COUNT, PASSWORD_MAX_LENGTH);
+
 	return (
 		<CardNumberWrap>
 			<Title>비밀번호를 입력해 주세요</Title>
 			<Description>앞의 2자리를 입력해주세요</Description>
 			<InputField label="비밀번호 앞 2자리" errorMessage={error}>
-				<Input type="password" maxLength={PASSWORD_MAX_LENGTH} isError={!!error} value={cardPassword} onChange={(value) => onChange(value)} onBlur={(value) => onBlur(value)} />
+				<Input
+					ref={inputRef[0]}
+					type="password"
+					maxLength={PASSWORD_MAX_LENGTH}
+					isError={!!error}
+					value={cardPassword}
+					onChange={(e) => onChange(e.target.value)}
+					onBlur={(e) => onBlur(e.target.value)}
+				/>
 			</InputField>
 		</CardNumberWrap>
 	);
