3c4e239cf7c6178a61abadddcb0b627d1e215385
dlsxjzld <sinjuk1@naver.com>
2025-04-28 15:11:36 +0900

    feat: 카드사 선택 스토리 추가

---
+++ src/components/cardInfoForm/CardCompanyField/CardCompanyField.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import CardCompany from './CardCompanyField';
+import useCardCompany from '../../../hooks/useCardCompany';
+import CardCompanyField from './CardCompanyField';
+
+const meta = {
+  title: 'CardCompany',
+  component: CardCompany,
+} satisfies Meta<typeof CardCompany>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    selectedCard: null,
+    CARD_COMPANY_NAMES: [
+      'BC카드',
+      '신한카드',
+      '카카오뱅크',
+      '현대카드',
+      '우리카드',
+      '롯데카드',
+      '하나카드',
+      '국민카드',
+    ],
+    CARD_COMPANY_PLACEHOLDER: '카드사를 선택해주세요',
+    onClickCardCompany: () => {},
+  },
+  render: (args) => {
+    const {
+      selectedCard,
+      CARD_COMPANY_NAMES,
+      CARD_COMPANY_PLACEHOLDER,
+      onClickCardCompany,
+    } = useCardCompany();
+
+    return (
+      <CardCompanyField
+        {...args}
+        selectedCard={selectedCard}
+        CARD_COMPANY_NAMES={CARD_COMPANY_NAMES}
+        CARD_COMPANY_PLACEHOLDER={CARD_COMPANY_PLACEHOLDER}
+        onClickCardCompany={onClickCardCompany}
+      />
+    );
+  },
+};
