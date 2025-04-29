37bcc171fd4115e6a088a1664845bc5ba82e9e3e
youjeong Jeong <wjddbwjd1205@naver.com>
2025-04-30 00:29:43 +0900

    refactor: 경로 문자열(PATH) 상수화

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,18 +2,24 @@ import { createBrowserRouter, Navigate } from 'react-router-dom';
 import CardRegisterPage from '../pages/card-register/CardRegisterPage';
 import CardRegisterCompletePage from '../pages/card-register-complete/CardRegisterCompletePage';
 
+export const PATH = {
+  ROOT: '/',
+  CARD_REGISTER: '/card-register',
+  CARD_REGISTER_COMPLETE: '/card-register-complete',
+};
+
 export const router = createBrowserRouter(
   [
     {
-      path: '/',
-      element: <Navigate to='/card-register' replace />,
+      path: PATH.ROOT,
+      element: <Navigate to={PATH.CARD_REGISTER} replace />,
     },
     {
-      path: '/card-register',
+      path: PATH.CARD_REGISTER,
       element: <CardRegisterPage />,
     },
     {
-      path: '/card-register-complete',
+      path: PATH.CARD_REGISTER_COMPLETE,
       element: <CardRegisterCompletePage />,
     },
   ],
