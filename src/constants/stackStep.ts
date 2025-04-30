558dc92d6c066a0d45df957c388cbdd15b870eb1
keemsebeen <sebin0580@gmail.com>
2025-05-01 01:56:36 +0900

    chore: 사용하지 않는 코드 제거

--- src/constants/stackStep.ts
+++ src/constants/stackStep.ts
@@ -7,24 +7,3 @@ export const STEPS = {
 } as const;
 
 export type StepType = (typeof STEPS)[keyof typeof STEPS];
-
-export const FLOW_CONFIG = {
-  [STEPS.CARD_NUMBER]: {
-    next: STEPS.BRAND,
-    prev: null,
-  },
-  [STEPS.BRAND]: {
-    next: STEPS.EXPIRE_DATE,
-    prev: STEPS.CARD_NUMBER,
-  },
-  [STEPS.EXPIRE_DATE]: {
-    next: STEPS.CVC,
-    prev: STEPS.BRAND,
-  },
-  [STEPS.CVC]: {
-    next: STEPS.PASS_WORD,
-    prev: STEPS.EXPIRE_DATE,
-  },
-} as const;
-
-export type FlowType = keyof typeof FLOW_CONFIG;
