e15c60a9dd548e591a3d9910e080bc407af35396
H0ngJu <hongju4385@gmail.com>
2025-04-30 13:18:12 +0900

    refactor(select): select 컴포넌트의 props에 title 추가

--- src/components/Select/select.tsx
+++ src/components/Select/select.tsx
@@ -1,11 +1,12 @@
 import styles from "./select.module.css";
 
 interface SelectProps {
+  title: string;
   options: string[];
   handleChange: (value: string) => void;
 }
 
-export default function Select({ handleChange, options }: SelectProps) {
+export default function Select({ title, handleChange, options }: SelectProps) {
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     handleChange(e.target.value);
   };
@@ -17,7 +18,7 @@ export default function Select({ handleChange, options }: SelectProps) {
       autoFocus
     >
       <option value="default" disabled hidden>
-        카드사를 선택해주세요
+        {title}
       </option>
       {options.map((option) => (
         <option key={option} value={option}>
