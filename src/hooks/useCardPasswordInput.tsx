9a0da7a9a123250e5549626fab4b4a582597bebc
H0ngJu <hongju4385@gmail.com>
2025-04-30 11:03:44 +0900

    refactor(useCardPassword): 에러 메시지 상수화

--- src/hooks/useCardPasswordInput.tsx
+++ src/hooks/useCardPasswordInput.tsx
@@ -5,18 +5,23 @@ const PASSWORD_LIMIT = {
   MAX_LENGTH: 2,
 } as const;
 
+const PASSWORD_ERROR_MESSAGE = {
+  INVALID_LENGTH_ERROR: `비밀번호 앞 ${PASSWORD_LIMIT.MAX_LENGTH}자리만 입력해주세요.`,
+  NOT_NUMBERIC_ERROR: "숫자만 입력 가능합니다.",
+};
+
 export default function useCardPasswordInput() {
   const [password, setPassword] = useState("");
   const [passwordError, setPasswordError] = useState("");
 
   const validatePassword = (value: string) => {
     if (!isUnderMaxLength(value.length, PASSWORD_LIMIT.MAX_LENGTH)) {
-      setPasswordError("비밀번호 앞 2자리만 입력해주세요.");
+      setPasswordError(PASSWORD_ERROR_MESSAGE.INVALID_LENGTH_ERROR);
       return false;
     }
 
     if (!isNumber(value)) {
-      setPasswordError("숫자만 입력 가능합니다.");
+      setPasswordError(PASSWORD_ERROR_MESSAGE.NOT_NUMBERIC_ERROR);
       return false;
     }
 
