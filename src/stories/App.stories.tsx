a8f0353cfde5915436af1720cdd1cfcb648187d2
H0ngJu <hongju4385@gmail.com>
2025-04-30 11:03:28 +0900

    storybook: 스토리북 App 오류 해결

--- src/stories/App.stories.tsx
+++ src/stories/App.stories.tsx
@@ -5,6 +5,13 @@ import App from "../App";
 const meta = {
   title: "App",
   component: App,
+} satisfies Meta<typeof App>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
   decorators: [
     (Story) => (
       <BrowserRouter>
@@ -12,10 +19,4 @@ const meta = {
       </BrowserRouter>
     ),
   ],
-} satisfies Meta<typeof App>;
-
-export default meta;
-
-type Story = StoryObj<typeof meta>;
-
-export const Default: Story = {};
+};
