b9b761923f78e571b8af88d5e5f8983ab1a52449
jin123457 <jinsol9909@gmail.com>
2025-04-30 22:50:17 +0900

    refactor: card step을 조건 분기문에서 객체 매핑 방식으로 수정

--- src/pages/AddCard/components/AddCardForm/AddCardForm.tsx
+++ src/pages/AddCard/components/AddCardForm/AddCardForm.tsx
@@ -6,7 +6,7 @@ import CardCVCNumber from '../../../../domain/card/CardCVCNumber/CardCVCNumber';
 import CardPasswordNumber from '../../../../domain/card/CardPasswordNumber/CardPasswordNumber';
 import Button from '../../../../components/Button/Button';
 import { AddCardFormProps } from '../../types';
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, ReactNode, useEffect, useState } from 'react';
 import { CARD_STEP, CARD_STEPS } from '../../constants';
 import { useNavigate } from 'react-router';
 
@@ -41,7 +41,10 @@ export default function AddCardForm({
     handleCardPasswordInputChange,
   } = addFormState;
 
-  const [steps, setSteps] = useState<string[]>(_testModeSteps || [CARD_STEP[0]]);
+  type CardStepKey = keyof typeof CARD_STEPS;
+  const [steps, setSteps] = useState<(typeof CARD_STEPS)[CardStepKey][]>(
+    (_testModeSteps as (typeof CARD_STEPS)[CardStepKey][]) || [CARD_STEP[0]],
+  );
 
   useEffect(() => {
     if (_testModeSteps) return;
@@ -68,45 +71,41 @@ export default function AddCardForm({
     isCardPasswordNextStep,
   ]);
 
-  const renderContents = steps.map((step) => {
-    return (
-      <Fragment key={step}>
-        {step === CARD_STEPS.CARD_NUMBERS && (
-          <CardNumber
-            cardNumberRefs={cardNumberRefs}
-            cardNumber={cardNumber}
-            cardNumberErrorMessage={cardNumberErrorMessage}
-            handleCardNumberInputChange={handleCardNumberInputChange}
-          />
-        )}
-        {step === CARD_STEPS.CARD_BRAND && (
-          <CardBrand cardBrandTypeState={cardBrandTypeState} handleDropdownChange={handleDropdownChange} />
-        )}
-        {step === CARD_STEPS.CARD_EXPIRATION_DATE && (
-          <CardExpirationDate
-            cardExpirationDateRefs={cardExpirationDateRefs}
-            cardExpirationDate={cardExpirationDate}
-            cardExpirationDateErrorMessage={cardExpirationDateErrorMessage}
-            handleCardExpirationDateInputChange={handleCardExpirationDateInputChange}
-          />
-        )}
-        {step === CARD_STEPS.CARD_CVC_NUMBER && (
-          <CardCVCNumber
-            cardCVCNumber={cardCVCNumber}
-            cardCVCNumberErrorMessage={cardCVCNumberErrorMessage}
-            handleCardCVCNumberInputChange={handleCardCVCNumberInputChange}
-          />
-        )}
-        {step === CARD_STEPS.CARD_PASSWORD && (
-          <CardPasswordNumber
-            cardPassword={cardPassword}
-            cardPasswordErrorMessage={cardPasswordErrorMessage}
-            handleCardPasswordInputChange={handleCardPasswordInputChange}
-          />
-        )}
-      </Fragment>
-    );
-  });
+  const stepComponents: Record<(typeof CARD_STEPS)[CardStepKey], ReactNode> = {
+    [CARD_STEPS.CARD_NUMBERS]: (
+      <CardNumber
+        cardNumberRefs={cardNumberRefs}
+        cardNumber={cardNumber}
+        cardNumberErrorMessage={cardNumberErrorMessage}
+        handleCardNumberInputChange={handleCardNumberInputChange}
+      />
+    ),
+    [CARD_STEPS.CARD_BRAND]: (
+      <CardBrand cardBrandTypeState={cardBrandTypeState} handleDropdownChange={handleDropdownChange} />
+    ),
+    [CARD_STEPS.CARD_EXPIRATION_DATE]: (
+      <CardExpirationDate
+        cardExpirationDateRefs={cardExpirationDateRefs}
+        cardExpirationDate={cardExpirationDate}
+        cardExpirationDateErrorMessage={cardExpirationDateErrorMessage}
+        handleCardExpirationDateInputChange={handleCardExpirationDateInputChange}
+      />
+    ),
+    [CARD_STEPS.CARD_CVC_NUMBER]: (
+      <CardCVCNumber
+        cardCVCNumber={cardCVCNumber}
+        cardCVCNumberErrorMessage={cardCVCNumberErrorMessage}
+        handleCardCVCNumberInputChange={handleCardCVCNumberInputChange}
+      />
+    ),
+    [CARD_STEPS.CARD_PASSWORD]: (
+      <CardPasswordNumber
+        cardPassword={cardPassword}
+        cardPasswordErrorMessage={cardPasswordErrorMessage}
+        handleCardPasswordInputChange={handleCardPasswordInputChange}
+      />
+    ),
+  };
 
   const isCardNumberError = Object.values(cardNumberErrorMessage).every((message) => message === '');
   const isCardExpirationDateError = Object.values(cardExpirationDateErrorMessage).every((message) => message === '');
@@ -136,7 +135,9 @@ export default function AddCardForm({
         });
       }}
     >
-      {renderContents}
+      {steps.map((step) => {
+        return <Fragment key={step}>{stepComponents[step]}</Fragment>;
+      })}
       {button}
     </S.CardAddFrom>
   );
