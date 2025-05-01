8a87c0a32df96333dc3045fceeca5e84791b6781
guesung <gueit214@naver.com>
2025-05-01 12:36:17 +0900

    refactor: 라우터 컴포넌트를 App으로 이동

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,6 @@
 import { CompletePage, RegisterPage } from '@/pages';
 import { Global, ThemeProvider } from '@emotion/react';
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import global from '../styles/global';
 import { theme } from '../styles/theme';
 
@@ -8,10 +8,12 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={global} />
-      <Routes>
-        <Route path="/" element={<RegisterPage />} />
-        <Route path="/complete" element={<CompletePage />} />
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<RegisterPage />} />
+          <Route path="/complete" element={<CompletePage />} />
+        </Routes>
+      </HashRouter>
     </ThemeProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
 import App from './App/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
 );
