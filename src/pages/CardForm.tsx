9b3ee30b8507fbb06647b4073d3a5caeeadcb60f
keemsebeen <sebin0580@gmail.com>
2025-05-01 20:05:54 +0900

    refactor: CardForm에서 사용하는 컴포넌트 props 제거, CardFormProvider 적용

--- src/pages/CardForm.tsx
+++ src/pages/CardForm.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from 'react-router-dom';
 
-import { useCardFormState } from '../hooks/useCardFormState';
 import { Flex } from '@/components/common/Flex';
 import { BrandForm } from '@/components/features/CardFormFiled/BrandForm';
 import { CardNumberForm } from '@/components/features/CardFormFiled/CardNumberForm';
@@ -10,12 +9,14 @@ import { PassWordForm } from '@/components/features/CardFormFiled/PassWordForm';
 import { CardFormLayout } from '@/components/features/CardFormLayout';
 import { CardPreview } from '@/components/features/CardPreview';
 import { STEPS, StepType } from '@/constants/stackStep';
+import { CardFormProvider } from '@/context/cardFormContext';
+import { useCardForm } from '@/hooks/useCardForm';
 import { useStack } from '@/hooks/useStack';
 
 export const CardForm = () => {
   const navigate = useNavigate();
   const { Stack, setStep } = useStack<StepType>(STEPS.CARD_NUMBER);
-  const [formData, dispatch] = useCardFormState();
+  const { formData } = useCardForm();
 
   const isFilledAndValid = (item: { value: string; isValid: boolean }) =>
     item.value !== '' && item.isValid;
@@ -39,67 +40,30 @@ export const CardForm = () => {
   };
 
   return (
-    <>
+    <CardFormProvider>
       <Flex>
-        <CardPreview
-          cardNumbers={formData.cardNumber}
-          cardBrand={formData.brand}
-          expireDate={formData.expireDate}
-        />
+        <CardPreview />
       </Flex>
 
       <CardFormLayout disabled={!allValid} onSubmit={handleCardFormSubmit}>
         <Stack>
           <Stack.Step name="카드번호">
-            <CardNumberForm
-              context={{
-                state: formData.cardNumber,
-                setState: (cardNumber) =>
-                  dispatch({ type: 'CARD_NUMBER', payload: { ...formData, cardNumber } }),
-              }}
-              onNext={() => setStep('카드사')}
-            />
+            <CardNumberForm onNext={() => setStep('카드사')} />
           </Stack.Step>
           <Stack.Step name="카드사">
-            <BrandForm
-              context={{
-                state: formData.brand,
-                setState: (brand) => dispatch({ type: 'BRAND', payload: { ...formData, brand } }),
-              }}
-              onNext={() => setStep('유효기간')}
-            />
+            <BrandForm onNext={() => setStep('유효기간')} />
           </Stack.Step>
           <Stack.Step name="유효기간">
-            <ExpireDateForm
-              context={{
-                state: formData.expireDate,
-                setState: (expireDate) =>
-                  dispatch({ type: 'EXPIRE_DATE', payload: { ...formData, expireDate } }),
-              }}
-              onNext={() => setStep('CVC')}
-            />
+            <ExpireDateForm onNext={() => setStep('CVC')} />
           </Stack.Step>
           <Stack.Step name="CVC">
-            <CVCForm
-              context={{
-                state: formData.cvc,
-                setState: (cvc) => dispatch({ type: 'CVC', payload: { ...formData, cvc } }),
-              }}
-              onNext={() => setStep('비밀번호')}
-            />
+            <CVCForm onNext={() => setStep('비밀번호')} />
           </Stack.Step>
           <Stack.Step name="비밀번호">
-            <PassWordForm
-              context={{
-                state: formData.password,
-                setState: (password) =>
-                  dispatch({ type: 'PASSWORD', payload: { ...formData, password } }),
-              }}
-              onNext={() => setStep('비밀번호')}
-            />
+            <PassWordForm onNext={() => setStep('비밀번호')} />
           </Stack.Step>
         </Stack>
       </CardFormLayout>
-    </>
+    </CardFormProvider>
   );
 };
