2be31b5dda6324f2b86671f1b07c21dabd6ceed8
keemsebeen <sebin0580@gmail.com>
2025-05-01 20:06:38 +0900

    chore: react-router 버전 업

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router';
 
 import { router } from './routes/route.tsx';
 
--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,4 +1,4 @@
-import { createHashRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router';
 
 import App from '@/App';
 
