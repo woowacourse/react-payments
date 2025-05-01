fd073a14947e7de2b13fa695609c257d77e8c0c7
hoyyChoi <hoyeon8165@gmail.com>
2025-05-01 21:36:56 +0900

    refactor: useAllComplete 훅 every 메소드 bool 네이밍 명시적으로 변경

--- src/hooks/common/useAllComplete.ts
+++ src/hooks/common/useAllComplete.ts
@@ -4,8 +4,8 @@ import { isErrorCompletesType } from "../../types/useValidationType";
 
 const useAllComplete = (isStateCompletes: isStateCompletesType, isErrorCompletes: isErrorCompletesType) => {
   return useMemo(() => {
-    const isAllStateCompletes = Object.values(isStateCompletes).every((stateComplete) => stateComplete === true);
-    const isAllErrorCompletes = Object.values(isErrorCompletes).every((errorComplete) => errorComplete === true);
+    const isAllStateCompletes = Object.values(isStateCompletes).every((isComplete) => isComplete === true);
+    const isAllErrorCompletes = Object.values(isErrorCompletes).every((isErrorComplete) => isErrorComplete === true);
 
     if (isAllStateCompletes && isAllErrorCompletes) return true;
     return false;
