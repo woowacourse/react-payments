f339b71dd44ba5b7bd41746813ee77b02dff27aa
minji2219 <aabb0219@naver.com>
2025-05-03 01:28:38 +0900

    refactor: 불필요한 style props 삭제

--- src/components/form/CardCompanySection.tsx
+++ src/components/form/CardCompanySection.tsx
@@ -15,7 +15,7 @@ const CardCompanySection = ({onChange, name}: Props) => {
     <CardNumberWrap>
       <Title>카드사를 선택해 주세요</Title>
       <Description>현재 국내 카드사만 가능합니다.</Description>
-      <Select name={name} onChange={onChange} $isError={false}>
+      <Select name={name} onChange={onChange}>
         <option value="" hidden>
           카드사를 선택해주세요
         </option>
@@ -35,17 +35,15 @@ const CardNumberWrap = styled.div`
   height: 130px;
 `;
 
-const Select = styled.select<{$isError: boolean}>(
-  (props) => `
-  width:100%;
-  margin-top:16px;
+const Select = styled.select`
+  width: 100%;
+  margin-top: 16px;
   padding: 8px;
   border-radius: 2px;
-  border:none;
-  outline: 1px solid ${props.$isError ? COLORS.ERROR : COLORS.LIGHT_GRAY};
-  
+  border: none;
+  outline: 1px solid ${COLORS.LIGHT_GRAY};
+
   &:focus {
-    outline: 1px solid ${props.$isError ? COLORS.ERROR : '#000'};
+    outline: 1px solid '#000';
   }
-`
-);
+`;
