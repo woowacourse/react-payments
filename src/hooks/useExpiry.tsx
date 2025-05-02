525a0d5ceee27ce0d7e3683bb8b59e4b957d6431
thgml05 <aisohii@naver.com>
2025-05-02 15:32:45 +0900

    refactor : useExpiry 훅 상태 및 로직 수정

--- src/hooks/useExpiry.tsx
+++ src/hooks/useExpiry.tsx
@@ -1,100 +1,111 @@
-import React from 'react';
-import { useState, useRef } from 'react';
-import { validateMonth, validateYear } from '../domain/validate';
-import { CARD_VALIDATION_INFO } from '../constants/cardValidationInfo';
+import { useState } from 'react';
 
-export const useExpiry = (): {
-  month: string;
-  setMonth: React.Dispatch<React.SetStateAction<string>>;
-  year: string;
-  setYear: React.Dispatch<React.SetStateAction<string>>;
-  updateDate: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  expiryRef: React.RefObject<HTMLInputElement[]>;
-  isErrors: boolean[];
-  errorMessage: string;
-  isComplete: boolean;
-  setIsComplete: React.Dispatch<React.SetStateAction<boolean>>;
-  isDisplay: boolean;
-  setIsDisplay: React.Dispatch<React.SetStateAction<boolean>>;
-} => {
-  const [month, setMonth] = useState('');
-  const [year, setYear] = useState('');
-  const [isErrors, setIsErrors] = useState([false, false]);
-  const [errorMessage, setErrorMessage] = useState('');
-  const expiryRef = useRef<HTMLInputElement[]>([]);
-  const [isComplete, setIsComplete] = useState(false);
-  const [isDisplay, setIsDisplay] = useState<boolean>(false);
+const EXPIRYDATE_RULE = {
+  INVALID_YEAR_LENGTH_ERROR: '연도는 2자리로 입력해 주세요.',
+  INVALID_MONTH_LENGTH_ERROR: '월은 2자리로 입력해 주세요.',
+  YEAR_IS_NOT_A_NUMBER: '연도는 숫자로 입력해 주세요.',
+  MONTH_IS_NOT_A_NUMBER: '월은 숫자로 입력해 주세요.',
+  INVALID_YEAR: '유효하지 않은 연도입니다.',
+  INVALID_MONTH: '유효하지 않은 월입니다.',
+  DATE_MAX_LENGTH: 2,
+  DATE_MONTH_MIN: 1,
+  DATE_MONTH_MAX: 12,
+  DATE_YEAR_MIN: 25,
+} as const;
 
-  const updateDate = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    if (name === 'month') {
-      setMonth(value);
-    } else if (name === 'year') {
-      setYear(value);
-    }
-    validate(name, value);
-    const isMonthValid =
-      (name === 'month' ? value : month).length ===
-      CARD_VALIDATION_INFO.EXPIRE_DATE_MAX_LENGTH;
-    const isYearValid =
-      (name === 'year' ? value : year).length ===
-      CARD_VALIDATION_INFO.EXPIRE_DATE_MAX_LENGTH;
-
-    const isExpiryComplete = isMonthValid && isYearValid;
-
-    setIsComplete(isExpiryComplete);
-    if (isExpiryComplete) setIsDisplay(true);
-
-    if (e.target.value.length === CARD_VALIDATION_INFO.EXPIRE_DATE_MAX_LENGTH) {
-      expiryRef.current[1]?.focus();
-    }
-  };
-
-  const validate = (name: string, value: string) => {
-    try {
-      if (name === 'month') {
-        if (value.length === 0) {
-          setIsErrors([false, isErrors[1]]);
-          setErrorMessage('');
-          return;
-        }
-        validateMonth(value, CARD_VALIDATION_INFO.EXPIRE_DATE_MAX_LENGTH);
-        setIsErrors([false, isErrors[1]]);
-        setErrorMessage('');
-      } else if (name === 'year') {
-        if (value.length === 0) {
-          setIsErrors([isErrors[0], false]);
-          setErrorMessage('');
-          return;
-        }
-        validateYear(value, CARD_VALIDATION_INFO.EXPIRE_DATE_MAX_LENGTH);
-        setIsErrors([isErrors[0], false]);
-        setErrorMessage('');
-      }
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        if (name === 'month') {
-          setIsErrors([true, isErrors[1]]);
-        } else if (name === 'year') {
-          setIsErrors([isErrors[0], true]);
-        }
-        setErrorMessage(error.message);
-      }
-    }
-  };
-
-  return {
-    month,
-    setMonth,
-    year,
-    setYear,
-    updateDate,
-    expiryRef,
-    isErrors,
-    errorMessage,
-    isComplete,
-    setIsComplete,
-    isDisplay,
-    setIsDisplay,
-  };
+type ValitationResult = {
+  date: dateType;
+  error: errorType[];
+  validate: (value: string, dateName: string) => void;
 };
+
+type dateType = {
+  month: string;
+  year: string;
+};
+
+type errorType = {
+  isValidate: boolean;
+  errorMessage: string;
+};
+
+const initialDate = [
+  {
+    isValidate: false,
+    errorMessage: '',
+  },
+  {
+    isValidate: false,
+    errorMessage: '',
+  },
+];
+
+export default function useExpiryDate(): ValitationResult {
+  const [date, setDate] = useState<dateType>({ month: '', year: '' });
+  const [error, setError] = useState<errorType[]>(initialDate);
+
+  const updateDate = (index: number, isError: boolean, message: string) => {
+    setError((prev) => {
+      prev[index] = {
+        isValidate: isError,
+        errorMessage: message,
+      };
+      return prev;
+    });
+  };
+
+  const validate = (value: string, dateName: string) => {
+    if (dateName === 'month') {
+      if (value.length > EXPIRYDATE_RULE.DATE_MAX_LENGTH) return;
+      setDate((prev) => ({ ...prev, month: value }));
+
+      if (value === '') {
+        updateDate(0, false, '');
+        return;
+      }
+
+      if (!/^\d*$/.test(value)) {
+        updateDate(0, true, EXPIRYDATE_RULE.MONTH_IS_NOT_A_NUMBER);
+        return;
+      }
+      if (value.length < EXPIRYDATE_RULE.DATE_MAX_LENGTH) {
+        updateDate(0, true, EXPIRYDATE_RULE.INVALID_MONTH_LENGTH_ERROR);
+        return;
+      }
+      if (
+        Number(value) < EXPIRYDATE_RULE.DATE_MONTH_MIN ||
+        Number(value) > EXPIRYDATE_RULE.DATE_MONTH_MAX
+      ) {
+        updateDate(0, true, EXPIRYDATE_RULE.INVALID_MONTH);
+        return;
+      }
+      updateDate(0, false, '');
+    }
+
+    if (dateName === 'year') {
+      if (value.length > EXPIRYDATE_RULE.DATE_MAX_LENGTH) return;
+      setDate((prev) => ({ ...prev, year: value }));
+
+      if (value === '') {
+        updateDate(1, false, '');
+        return;
+      }
+
+      if (!/^\d*$/.test(value)) {
+        updateDate(1, true, EXPIRYDATE_RULE.YEAR_IS_NOT_A_NUMBER);
+        return;
+      }
+      if (value.length < EXPIRYDATE_RULE.DATE_MAX_LENGTH) {
+        updateDate(1, true, EXPIRYDATE_RULE.INVALID_YEAR_LENGTH_ERROR);
+        return;
+      }
+      if (Number(value) < EXPIRYDATE_RULE.DATE_YEAR_MIN) {
+        updateDate(1, true, EXPIRYDATE_RULE.INVALID_YEAR);
+        return;
+      }
+      updateDate(1, false, '');
+    }
+  };
+
+  return { date, error, validate };
+}
