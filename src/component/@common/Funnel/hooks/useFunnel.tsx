a0588c5930a964799d6ad420816521d659c29d0e
장정안 <wjddks96@naver.com>
2025-05-02 16:14:18 +0900

    refactor: useFunnel 훅 폴더 구조 위치 변경

---
+++ src/component/@common/Funnel/hooks/useFunnel.tsx
@@ -0,0 +1,27 @@
+import { useMemo, useState } from 'react';
+import { Funnel, FunnelProps, Step } from '../Funnel';
+import { NonEmptyArray } from '../../../../../types/types';
+
+type UseFunnelProps<T extends NonEmptyArray<string>> = Omit<
+  FunnelProps<T>,
+  'children'
+>;
+
+export const useFunnel = <Steps extends NonEmptyArray<string>>(
+  props: UseFunnelProps<Steps>
+) => {
+  const { steps, step: defaultStep } = props;
+  const [step, setStep] = useState<Steps[number]>(defaultStep);
+
+  const FunnelComponent = useMemo(() => {
+    const FunnelWithSteps = (
+      props: Omit<FunnelProps<Steps>, 'step' | 'steps'>
+    ) => <Funnel<Steps> step={step} steps={steps} {...props} />;
+
+    FunnelWithSteps.Step = Step;
+
+    return FunnelWithSteps;
+  }, [step, steps]);
+
+  return { Funnel: FunnelComponent, step, setStep };
+};
