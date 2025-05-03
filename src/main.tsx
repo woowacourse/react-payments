e4b80f053c6e8b2949e9b030b1e3fbe9ff7636f3
jin123457 <jinsol9909@gmail.com>
2025-05-04 08:56:52 +0900

    refactor: BrowserRouter에서 HashRouter로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router';
+import { HashRouter } from 'react-router';
 import GlobalStyle from './GlobalStyle.styles.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-payments">
+    <HashRouter>
       <GlobalStyle />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
