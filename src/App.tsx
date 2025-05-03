532139186b765c886a3d48708e301e54990e8a2e
thgml05 <aisohii@naver.com>
2025-05-03 17:19:16 +0900

    refactor : CardFormProvider 설정 CardRegisterPage 컴포넌트에 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { CardRegisterSuccessPage } from './pages/CardRegisterSuccessPage/CardRegisterSuccessPage';
 import CardRegisterPage from './pages/CardRegisterPage/CardRegisterPage';
+import { CardFormProvider } from './context/CardFormContext';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/react-payments" element={<CardRegisterPage />} />
         <Route
-          path="/react-payments/complete"
+          path="/payments"
+          element={
+            <CardFormProvider>
+              <CardRegisterPage />
+            </CardFormProvider>
+          }
+        />
+
+        <Route
+          path="/payments/complete"
           element={<CardRegisterSuccessPage />}
         />
       </Routes>
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { CardFormProvider } from './context/CardFormContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CardFormProvider>
-      <App />
-    </CardFormProvider>
+    <App />
   </React.StrictMode>
 );
