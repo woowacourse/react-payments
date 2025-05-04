3b5522e14290aff48463a4b88af56e74ccf9ddcb
hoyyChoi <hoyeon8165@gmail.com>
2025-05-05 02:28:01 +0900

    fix: useStep 훅 step인자 제거

--- src/hooks/common/useStep.ts
+++ src/hooks/common/useStep.ts
@@ -5,20 +5,17 @@ import { isErrorCompletesType, isStateCompletesType } from "../../types/CardInfo
 const useStep = (isStateCompletes: isStateCompletesType, isErrorCompletes: isErrorCompletesType) => {
   const [step, setStep] = useState(0);
 
-  const checkCurrentStep = useCallback(
-    (step: number) => {
-      if (STEP_ORDER.length - 1 <= step) return false;
-      const currentKey = STEP_ORDER[step];
-      const isStateComplete = isStateCompletes[currentKey];
-      const isErrorComplete = currentKey === "company" || isErrorCompletes[currentKey];
+  const checkCurrentStep = useCallback(() => {
+    if (STEP_ORDER.length - 1 <= step) return false;
+    const currentKey = STEP_ORDER[step];
+    const isStateComplete = isStateCompletes[currentKey];
+    const isErrorComplete = currentKey === "company" || isErrorCompletes[currentKey];
 
-      return isStateComplete && isErrorComplete;
-    },
-    [isStateCompletes, isErrorCompletes],
-  );
+    return isStateComplete && isErrorComplete;
+  }, [isStateCompletes, isErrorCompletes]);
 
   useEffect(() => {
-    if (checkCurrentStep(step)) setStep(step + 1);
+    if (checkCurrentStep()) setStep(step + 1);
   }, [checkCurrentStep]);
 
   return step;
