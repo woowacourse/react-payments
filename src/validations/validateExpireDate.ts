fa93264b35bf1665db8d13835b3fb784a3e9547a
ohgus <anakin1108@naver.com>
2025-05-06 18:42:32 +0900

    refactor: validateExpireDate 조건문을 변수화해 가독성 향상

--- src/validations/validateExpireDate.ts
+++ src/validations/validateExpireDate.ts
@@ -10,18 +10,26 @@ export const validateExpireDate = (value: string, key: ExpireDateInputKey): Vali
     return { isValid: false, errorMessage: '연도와 월은 두 자리 숫자를 입력하세요. 예시: 01, 12' };
   }
 
-  if (key === 'month' && (parseInt(value) < 1 || parseInt(value) > 12)) {
-    return {
-      isValid: false,
-      errorMessage: '월을 잘못 입력했습니다. 1~12 사이의 숫자를 입력해주세요.',
-    };
+  if (key === 'month') {
+    const isInvalidMonth = parseInt(value) < 1 || parseInt(value) > 12;
+
+    if (isInvalidMonth) {
+      return {
+        isValid: false,
+        errorMessage: '월을 잘못 입력했습니다. 1~12 사이의 숫자를 입력해주세요.',
+      };
+    }
   }
 
-  if (key === 'year' && parseInt(value) < currentYear) {
-    return {
-      isValid: false,
-      errorMessage: `만료 년도는 현재 년도보다 낮을 수 없습니다. 현재 연도: ${currentYear}`,
-    };
+  if (key === 'year') {
+    const isPastYear = parseInt(value) < currentYear;
+
+    if (isPastYear) {
+      return {
+        isValid: false,
+        errorMessage: `만료 년도는 현재 년도보다 낮을 수 없습니다. 현재 연도: ${currentYear}`,
+      };
+    }
   }
 
   return { isValid: true, errorMessage: null };
