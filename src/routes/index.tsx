d3e8ffe7558e3bbdb21a57bbda0c63abd3166498
ohgus <anakin1108@naver.com>
2025-05-07 20:39:45 +0900

    refactor: ROUTE_PATH 정의

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,18 +4,24 @@ import { Home } from './pages/Home';
 import { Register } from './pages/Register';
 import { Result } from './pages/Result';
 
+const ROUTE_PATH = {
+  HOME: '/',
+  REGISTER: '/register',
+  RESULT: '/result',
+};
+
 const router = createBrowserRouter(
   [
     {
-      path: '/',
+      path: ROUTE_PATH.HOME,
       element: <Home />,
     },
     {
-      path: '/register',
+      path: ROUTE_PATH.REGISTER,
       element: <Register />,
     },
     {
-      path: '/result',
+      path: ROUTE_PATH.RESULT,
       element: <Result />,
     },
   ],
