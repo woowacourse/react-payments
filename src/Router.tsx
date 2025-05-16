b51e53e014edd5fd71e333322cd072a38036814c
ShinjungOh <tlswjd9260@gmail.com>
2025-05-16 12:58:27 +0900

    refactor: route 상수화

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
-import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CardRegistrationCompletePage from "./pages/CardRegistrationCompletePage";
+import {Route, Routes} from "react-router-dom";
+import {PATHS} from "./constants/routes";
 
 const Router = () => (
   <Routes>
-    <Route path="/*" element={<HomePage/>}/>
-    <Route path="/complete" element={<CardRegistrationCompletePage/>}/>
+    <Route path={PATHS.HOME} element={<HomePage/>}/>
+    <Route path={PATHS.COMPLETE} element={<CardRegistrationCompletePage/>}/>
   </Routes>
 )
 
