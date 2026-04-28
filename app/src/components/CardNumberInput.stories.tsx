ee552a75fec1e4ed16443dd2ac7e093c4cb5e719
Antoliny0919 <antoliny0919@gmail.com>
2026-04-28 20:33:25 +0900

    fix: CardNumberInput 스토리북 라벨 수정
    Co-authored-by: hjkim0905 <eax9952@gmail.com>

--- app/src/components/CardNumberInput.stories.tsx
+++ app/src/components/CardNumberInput.stories.tsx
@@ -1,12 +1,12 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { fn } from "storybook/test";
-import { CardNumberInput } from "./CardNumberInput";
+import { CardNumberInputContainer } from "./CardNumberInput";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "CardNumberInput",
-  component: CardNumberInput,
+  component: CardNumberInputContainer,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
@@ -19,7 +19,7 @@ const meta = {
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#story-args
   args: { onClick: fn() },
-} satisfies Meta<typeof CardNumberInput>;
+} satisfies Meta<typeof CardNumberInputContainer>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -27,6 +27,6 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const base: Story = {
   args: {
-    label: "CardNumberInput",
+    label: "",
   },
 };
