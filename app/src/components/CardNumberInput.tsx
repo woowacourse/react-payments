f5ee9ea09cac68aa684b6e9e1bd7e20cc9551f07
Antoliny0919 <antoliny0919@gmail.com>
2026-04-28 20:32:56 +0900

    style: 카드 번호 입력 스타일 추가
    Co-authored-by: hjkim0905 <eax9952@gmail.com>

--- app/src/components/CardNumberInput.tsx
+++ app/src/components/CardNumberInput.tsx
@@ -1,12 +1,50 @@
-export function CardNumberInput() {
+import styled from "@emotion/styled";
+
+export function CardNumberInputContainer() {
   return (
-    <>
-      <fieldset>
-        <input type="number" minLength={1} maxLength={4}></input>
-        <input type="number" minLength={1} maxLength={4}></input>
-        <input type="number" minLength={1} maxLength={4}></input>
-        <input type="number" minLength={1} maxLength={4}></input>
-      </fieldset>
-    </>
+    <fieldset>
+      <legend>카드 번호</legend>
+      <CardNumberInput
+        type="number"
+        minLength={1}
+        maxLength={4}
+        placeholder="1234"
+      />
+      <CardNumberInput
+        type="number"
+        minLength={1}
+        maxLength={4}
+        placeholder="1234"
+      />
+      <CardNumberInput
+        type="number"
+        minLength={1}
+        maxLength={4}
+        placeholder="1234"
+      />
+      <CardNumberInput
+        type="number"
+        minLength={1}
+        maxLength={4}
+        placeholder="1234"
+      />
+    </fieldset>
   );
 }
+
+const CardNumberInput = styled.input`
+  border: solid 1px #acacac;
+  border-radius: 2px;
+  padding: 0.5rem;
+  font-size: 11px;
+  -moz-appearance: textfield;
+  &::placeholder {
+    color: #acacac;
+  }
+
+  &::-webkit-inner-spin-button,
+  &::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+`;
