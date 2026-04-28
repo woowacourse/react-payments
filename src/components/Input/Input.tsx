2e684c399c38c87c5626eac3069b925bbc810d9c
kojesung <js95112345@gmail.com>
2026-04-28 17:24:33 +0900

    feat: Input 컴포넌트 value 변경 조건 추가
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,16 +1,43 @@
 import styled from '@emotion/styled';
+import { isCardNumber } from '../../utils/isCardNumber';
+import { isCVCNumber } from '../../utils/isCVCNumber';
+import { isMonthNumber } from '../../utils/isMonthNumber';
+import { isYearNumber } from '../../utils/isYearNumber';
+
+type InputType = { type: 'card-number' } | { type: 'exp'; expType: 'month' | 'year' } | { type: 'cvc' };
 
 interface InputProps {
-    value?: number;
+    value?: string;
     setValue?: (value: string) => void;
-    type: 'card-number' | 'exp' | 'cvc';
+    type: InputType;
+}
+
+interface InputStyleProps {
+    inputType: 'card-number' | 'exp' | 'cvc';
 }
 
 export default function Input({ value, setValue, type }: InputProps) {
-    return <InputStyle value={value} onChange={(e) => setValue(e.target.value)} type={type} />;
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
+        const tmpValue = e.target.value;
+        if (type.type === 'card-number') {
+            if (isCardNumber(tmpValue)) return;
+            setValue(e.target.value);
+        } else if (type.type === 'cvc') {
+            if (isCVCNumber(tmpValue)) return;
+            setValue(e.target.value);
+        } else if (type.type === 'exp' && type.expType === 'month') {
+            if (isMonthNumber(tmpValue)) return;
+            setValue(e.target.value);
+        } else if (type.type === 'exp' && type.expType === 'year') {
+            if (isYearNumber(tmpValue)) return;
+            setValue(e.target.value);
+        }
+    };
+
+    return <InputStyle value={value} onChange={(e) => handleInputChange(e)} inputType={type.type} />;
 }
 
-const InputStyle = styled.input<InputProps>`
+const InputStyle = styled.input<InputStyleProps>`
     border: 1px solid #acacac;
     &::placeholder {
         color: #acacac;
