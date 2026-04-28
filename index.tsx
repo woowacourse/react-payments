74a785b4fa40037f16d12215a2a6e29350443b4e
inaemin <inaemin@gmail.com>
2026-04-28 17:58:17 +0900

    feat: react-ts 초기 셋팅
    Co-authored-by: 송아영 <azerosong@gmail.com>

---
+++ index.tsx
@@ -0,0 +1,5 @@
+import { InputHTMLAttributes, ButtonHTMLAttributes, FieldsetHTMLAttributes } from "react";
+
+interface Props extends FieldsetHTMLAttributes<HTMLFieldSetElement> {}
+
+<input  inputMode="numeric"/>
