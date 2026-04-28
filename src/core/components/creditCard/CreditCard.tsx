a056f94870b5db3000ab6f3bafa402df6c7ce254
stardusty-lab <startdusty.lab@gmail.com>
2026-04-28 20:02:35 +0900

    feature: CreditCard 컴포넌트 구현
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/core/components/creditCard/CreditCard.tsx
+++ src/core/components/creditCard/CreditCard.tsx
@@ -1,3 +1,5 @@
+import styles from './CreditCard.module.css';
+
 import DefaultCardImage from '@/core/assets/defaultCardImage.png';
 import MastercardSvg from '@/core/assets/Mastercard.svg?react';
 import VisaSvg from '@/core/assets/Visa.svg?react';
@@ -21,15 +23,14 @@ export const CreditCard = ({
   expirationDate,
 }: DefaultCreditCardProps) => {
   return (
-    <div className="credit-card">
-      {bank === 'default' ? <img className="credit-card-image" src={DefaultCardImage} alt="Default Card" /> : null}
-      <div className="credit-card-brand">{cardBrand === 'mastercard' ? <MastercardSvg /> : <VisaSvg />}</div>
-      <div>
+    <div className={styles.creditCard}>
+      <div className={styles.creditCardBrand}>{cardBrand === 'mastercard' ? <MastercardSvg /> : <VisaSvg />}</div>
+      <div className={styles.creditCardNumber}>
         {cardNumberList?.map((number, index) => (
           <span key={index}>{number}</span>
         ))}
       </div>
-      <div>
+      <div className={styles.creditCardExpirationDate}>
         {expirationDate?.map((number, index) => (
           <span key={index}>{number}</span>
         ))}
---
+++ src/core/components/creditCard/index.ts
@@ -0,0 +1,1 @@
+export { CreditCard } from './CreditCard';
