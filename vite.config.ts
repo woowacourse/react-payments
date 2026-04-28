b6b6b4c556c5ec084c4b5a65cd1191006d8adf8a
박건규 <geongyu09@gmail.com>
2026-04-28 15:31:19 +0900

    chore: vite host 옵션 활성화
    Co-authored-by: Boyeon <boboyoii@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  server: {
+    host: true, // 또는 '0.0.0.0'
+    port: 5173,
+  },
+});
