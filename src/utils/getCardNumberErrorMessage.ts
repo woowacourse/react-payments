1a08f1733a4b8251bade174dc2fe8b90622b1610
kojesung <js95112345@gmail.com>
2026-04-29 16:56:20 +0900

    refactor: cardNumber errorMessage용, 입력 방지용 검증 함수 분리
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

---
+++ src/utils/getCardNumberErrorMessage.ts
@@ -0,0 +1,14 @@
+// 숫자인지 검증은 isCardNumber에서 담당
+// getCardNumberErrorMessage는 4개씩 총 16개의 숫자인지만 검증
+export const getCardNumberErrorMessage = (values: string[]) => {
+    const ERROR_MESSAGE = '카드번호는 4개씩 총 16개의 숫자로 이루어져야 합니다.';
+
+    values.forEach((value) => {
+        if (value === '') return null;
+    });
+
+    values.forEach((value) => {
+        if (!(value.length === 4)) return ERROR_MESSAGE;
+    });
+    return null;
+};
