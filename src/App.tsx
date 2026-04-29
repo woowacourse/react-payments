aae602e56023f3d9ee8c16bb09b2ac7c935ef32c
kojesung <js95112345@gmail.com>
2026-04-29 18:45:08 +0900

    feat: 카드 정보 입력 섹션 렌더링 및 getEXPNumberErrorMessage 빈 문자열 검사 로직 수정
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,107 @@
+import styled from '@emotion/styled';
 import { useState } from 'react';
 import CardInputWrapper from './components/CardInputWrapper';
 import { getCardNumberErrorMessage } from './utils/getCardNumberErrorMessage';
 import CardInfoInput from './components/Input/CardInfoInput';
 import { isCardNumber } from './utils/isCardNumber';
+import CardInfoSection from './components/CardInfoSection';
+import { getCVCumberErrorMessage } from './utils/getCVCNumberErrorMessage';
+import { getEXPNumberErrorMessage } from './utils/getEXPNumberErrorMessage';
 
 function App() {
     const [firstCardNumber, setFirstCardNumber] = useState('');
     const [secondCardNumber, setSecondCardNumber] = useState('');
     const [thirdCardNumber, setThirdCardNumber] = useState('');
     const [fourthCardNumber, setFourthCardNumber] = useState('');
+
+    const [expMonth, setEXPMonth] = useState('');
+    const [expYear, setEXPYear] = useState('');
+
+    const [cvc, setCVC] = useState('');
     // TODO 커링, useReducer 등의 상태 관리 방식 변경 고려
 
     const cardNumberValues = [firstCardNumber, secondCardNumber, thirdCardNumber, fourthCardNumber];
+    const expValues = [expMonth, expYear];
     return (
-        <>
-            <CardInputWrapper validator={getCardNumberErrorMessage} value={cardNumberValues}>
-                <CardInfoInput
-                    value={firstCardNumber}
-                    setValue={setFirstCardNumber}
-                    type={{ type: 'card-number' }}
-                    placeHolder="1234"
-                    validator={isCardNumber}
-                />
-                <CardInfoInput
-                    value={secondCardNumber}
-                    setValue={setSecondCardNumber}
-                    type={{ type: 'card-number' }}
-                    placeHolder="1234"
-                    validator={isCardNumber}
-                />
-                <CardInfoInput
-                    value={thirdCardNumber}
-                    setValue={setThirdCardNumber}
-                    type={{ type: 'card-number' }}
-                    placeHolder="1234"
-                    validator={isCardNumber}
-                />
-                <CardInfoInput
-                    value={fourthCardNumber}
-                    setValue={setFourthCardNumber}
-                    type={{ type: 'card-number' }}
-                    placeHolder="1234"
-                    validator={isCardNumber}
-                />
-            </CardInputWrapper>
-        </>
+        <InputSectionContainer>
+            <CardInfoSection
+                title="결제할 카드 번호를 입력해 주세요"
+                caption="본인 명의의 카드만 결제 가능합니다."
+                inputLabel="카드 번호"
+            >
+                <CardInputWrapper validator={getCardNumberErrorMessage} value={cardNumberValues}>
+                    <CardInfoInput
+                        value={firstCardNumber}
+                        setValue={setFirstCardNumber}
+                        type={{ type: 'card-number' }}
+                        placeHolder="1234"
+                        validator={isCardNumber}
+                    />
+                    <CardInfoInput
+                        value={secondCardNumber}
+                        setValue={setSecondCardNumber}
+                        type={{ type: 'card-number' }}
+                        placeHolder="1234"
+                        validator={isCardNumber}
+                    />
+                    <CardInfoInput
+                        value={thirdCardNumber}
+                        setValue={setThirdCardNumber}
+                        type={{ type: 'card-number' }}
+                        placeHolder="1234"
+                        validator={isCardNumber}
+                    />
+                    <CardInfoInput
+                        value={fourthCardNumber}
+                        setValue={setFourthCardNumber}
+                        type={{ type: 'card-number' }}
+                        placeHolder="1234"
+                        validator={isCardNumber}
+                    />
+                </CardInputWrapper>
+            </CardInfoSection>
+
+            <CardInfoSection
+                title="카드 유효기간을 입력해 주세요"
+                caption="월/년도(MMYY)를 순서대로 입력해 주세요."
+                inputLabel="유효기간"
+            >
+                <CardInputWrapper validator={getEXPNumberErrorMessage} value={expValues}>
+                    <CardInfoInput
+                        value={expMonth}
+                        setValue={setEXPMonth}
+                        type={{ type: 'exp', expType: 'month' }}
+                        placeHolder="MM"
+                        validator={isCardNumber}
+                    />
+                    <CardInfoInput
+                        value={expYear}
+                        setValue={setEXPYear}
+                        type={{ type: 'exp', expType: 'year' }}
+                        placeHolder="YY"
+                        validator={isCardNumber}
+                    />
+                </CardInputWrapper>
+            </CardInfoSection>
+            <CardInfoSection title="CVC 번호를 입력해 주세요" inputLabel="CVC">
+                <CardInputWrapper validator={getCVCumberErrorMessage} value={[cvc]}>
+                    <CardInfoInput
+                        value={cvc}
+                        setValue={setCVC}
+                        type={{ type: 'cvc' }}
+                        placeHolder="123"
+                        validator={isCardNumber}
+                    />
+                </CardInputWrapper>
+            </CardInfoSection>
+        </InputSectionContainer>
     );
 }
 
 export default App;
+
+const InputSectionContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+`;
