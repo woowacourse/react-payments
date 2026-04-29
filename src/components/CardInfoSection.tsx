82345958198ccba253053ddb6859bb52ca800ecd
kojesung <js95112345@gmail.com>
2026-04-29 18:44:39 +0900

    feat: CardInfoSection 컴포넌트 구현
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

--- src/components/CardInfoSection.tsx
+++ src/components/CardInfoSection.tsx
@@ -1,56 +1,51 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
-interface Props {
-  title: string;
-  caption?: string;
-  inputLabel: string;
-  children: React.ReactNode;
+interface CardSectionProps {
+    title: string;
+    caption?: string;
+    inputLabel: string;
+    children: React.ReactNode;
 }
 
-export default function CardInfoSection({
-  title,
-  caption,
-  inputLabel,
-  children,
-}: Props) {
-  return (
-    <CardSectionContainer>
-      <Title>{title}</Title>
-      {caption && <Caption>{caption}</Caption>}
-      <InputLabel>{inputLabel}</InputLabel>
-      {children}
-    </CardSectionContainer>
-  );
+export default function CardInfoSection({ title, caption, inputLabel, children }: CardSectionProps) {
+    return (
+        <CardSectionStyle>
+            <TitleStyle>{title}</TitleStyle>
+            {caption && <CaptionStyle>{caption}</CaptionStyle>}
+            <InputLabelStyle>{inputLabel}</InputLabelStyle>
+            {children}
+        </CardSectionStyle>
+    );
 }
 
-const CardSectionContainer = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: start;
+const CardSectionStyle = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
 `;
 
-const Title = styled.h3`
-  font-weight: 700;
-  font-size: 18px;
-  line-height: 100%;
-  letter-spacing: 0%;
-  margin: 0 0 4px 0;
+const TitleStyle = styled.h3`
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 100%;
+    letter-spacing: 0%;
+    margin: 0 0 4px 0;
 `;
 
-const Caption = styled.caption`
-  font-weight: 400;
-  font-size: 9.5px;
-  line-height: 100%;
-  letter-spacing: 0%;
-  color: #8b95a1;
+const CaptionStyle = styled.caption`
+    font-weight: 400;
+    font-size: 9.5px;
+    line-height: 100%;
+    letter-spacing: 0%;
+    color: #8b95a1;
 `;
 
-const InputLabel = styled.p`
-  font-weight: 500;
-  font-size: 12px;
-  line-height: 15px;
-  letter-spacing: 0%;
-  color: #0a0d13;
-  margin-top: 16px;
-  margin-bottom: 8px;
+const InputLabelStyle = styled.p`
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 15px;
+    letter-spacing: 0%;
+    color: #0a0d13;
+    margin-top: 16px;
+    margin-bottom: 8px;
 `;
