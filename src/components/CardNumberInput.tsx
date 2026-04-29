3b124ebe3daa28168a2939799d2a102bbb19cde5
bel1c10ud <mail@bellclou.de>
2026-04-29 22:12:21 +0900

    feat: 카드 번호 입력 컴포넌트 구현
    Co-authored-by: Hyewon Jang <janghw232625@gmail.com>

---
+++ src/components/CardNumberInput.tsx
@@ -0,0 +1,44 @@
+import { type ComponentProps } from 'react';
+import ValidationInput from './ValidationInput';
+import { validateNumberString, validateStringLength, validateStringMaxLength } from '../utils';
+
+interface CardNumberInputProps {
+  value: [string, string, string, string];
+  onChange: ComponentProps<'input'>['onChange'];
+}
+
+function CardNumberInput(props: CardNumberInputProps) {
+  return (
+    <div>
+      {props.value.map((el, index) => (
+        <ValidationInput
+          key={index}
+          data-index={index}
+          type="text"
+          placeholder="1234"
+          value={el}
+          onChange={props.onChange}
+          validations={[
+            {
+              type: 'limit',
+              validator: validateNumberString,
+              message: '숫자만 입력 가능합니다',
+            },
+            {
+              type: 'limit',
+              validator: (input: string) => validateStringMaxLength(input, 4),
+              message: '4자리까지만 입력 가능합니다',
+            },
+            {
+              type: 'check',
+              validator: (input: string) => validateStringLength(input, 4),
+              message: '4자리를 입력하세요',
+            },
+          ]}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default CardNumberInput;
