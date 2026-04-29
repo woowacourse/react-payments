f7abc7184daf5a32d8d6976958e9dab29f310359
iftype <conditionaltype@gmail.com>
2026-04-29 16:45:50 +0900

    feature: FormGroup Component 구현
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

---
+++ src/core/components/formGroup/FormGroup.tsx
@@ -0,0 +1,28 @@
+import cn from 'classnames';
+import styles from './FormGroup.module.css';
+
+import type { ReactNode } from 'react';
+
+interface FormGroupProps {
+  title?: string;
+  subTitle?: string;
+  label?: string;
+  chlidren: ReactNode;
+  errorMessage?: string;
+}
+
+export const FormGroup = ({ title, subTitle, label, chlidren, errorMessage }: FormGroupProps) => {
+  return (
+    <fieldset className={cn(styles.formGroup)}>
+      <div className={cn(styles.titleContainer)}>
+        <div className={cn(styles.title)}>{title}</div>
+        <div className={cn(styles.subTitle)}>{subTitle}</div>
+      </div>
+      <div className={cn(styles.formContainer)}>
+        <label className={cn(styles.label)}>{label}</label>
+        <div className={cn(styles.chlidren)}>{chlidren}</div>
+        <div className={cn(styles.errorMessage)}>{errorMessage}</div>
+      </div>
+    </fieldset>
+  );
+};
