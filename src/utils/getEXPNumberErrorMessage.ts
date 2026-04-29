f512f92c30c4bfc4ea784f030897337e9797bb1b
kojesung <js95112345@gmail.com>
2026-04-29 18:45:08 +0900

    feat: 카드 정보 입력 섹션 렌더링 및 getEXPNumberErrorMessage 빈 문자열 검사 로직 수정
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

--- src/utils/getEXPNumberErrorMessage.ts
+++ src/utils/getEXPNumberErrorMessage.ts
@@ -5,7 +5,7 @@ const EXP_YEAR_LENGTH_ERROR_MESSAGE = '유효기간 연도는 2개의 숫자로 
 export const getEXPNumberErrorMessage = (values: string[]) => {
     if (values.length !== 2) throw new Error('EXP 에러 메세지를 받기 위해선 length가 2인 배열이 필요합니다.');
 
-    values.forEach((value) => value === '' && null);
+    if (values.some((value) => value === '')) return null;
 
     const monthString = values[0];
     const monthNumber = Number(values[0]);
