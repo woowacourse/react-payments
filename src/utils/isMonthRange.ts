3f90d54fee3116d059f132dff7789eb9bd183731
kojesung <js95112345@gmail.com>
2026-04-29 17:09:24 +0900

    refactor: EXPNumber errorMessage용, 입력 방지용 검증 함수 분리
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

--- src/utils/isMonthRange.ts
+++
@@ -1,5 +0,0 @@
-export const isMonthNumberRange = (monthNumber: string) => {
-    console.log(monthNumber);
-    if (Number(monthNumber) > 12 || Number(monthNumber) < 1) return false;
-    return true;
-};
