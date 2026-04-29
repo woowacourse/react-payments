b675281d57ac5897f579b4f3305cd224ed6751e8
kojesung <js95112345@gmail.com>
2026-04-29 16:56:20 +0900

    refactor: cardNumber errorMessage용, 입력 방지용 검증 함수 분리
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

--- src/utils/getCardNumberErrorMessage.ts
+++ src/utils/getCardNumberErrorMessage.ts
@@ -1,22 +1,14 @@
 // 숫자인지 검증은 isCardNumber에서 담당
 // getCardNumberErrorMessage는 4개씩 총 16개의 숫자인지만 검증
-const ERROR_MESSAGE = "카드번호는 4개씩 총 16개의 숫자로 이루어져야 합니다.";
-export const getCardNumberErrorMessage = (values: {
-  first: string;
-  second: string;
-  third: string;
-  fourth: string;
-}): {
-  message: string;
-  key: "first" | "second" | "third" | "fourth";
-} | null => {
-  if (values.first.length !== 4)
-    return { message: ERROR_MESSAGE, key: "first" };
-  if (values.second.length !== 4)
-    return { message: ERROR_MESSAGE, key: "second" };
-  if (values.third.length !== 4)
-    return { message: ERROR_MESSAGE, key: "third" };
-  if (values.fourth.length !== 4)
-    return { message: ERROR_MESSAGE, key: "fourth" };
-  return null;
+export const getCardNumberErrorMessage = (values: string[]) => {
+    const ERROR_MESSAGE = '카드번호는 4개씩 총 16개의 숫자로 이루어져야 합니다.';
+
+    values.forEach((value) => {
+        if (value === '') return null;
+    });
+
+    values.forEach((value) => {
+        if (!(value.length === 4)) return ERROR_MESSAGE;
+    });
+    return null;
 };
