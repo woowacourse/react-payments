01d50209fa3a2c2ebce8229008668b19d8961674
박건규 <geongyu09@gmail.com>
2026-04-29 16:21:52 +0900

    feat(Card.tsx): Card UI 컴포넌트 구현
    Co-authored-by: Boyeon <boboyoii@users.noreply.github.com>

---
+++ src/components/Card.tsx
@@ -0,0 +1,94 @@
+import styled from "@emotion/styled";
+import masterCard from "../assets/Mastercard.png";
+import { splitCardNumber } from "../utils/card";
+
+interface CardProps {
+  cardNumber: number;
+  validityPeriod: {
+    month: number;
+    year: number;
+  };
+}
+
+const Card = ({ cardNumber, validityPeriod }: CardProps) => {
+  const cardNumberUnitArr = splitCardNumber(cardNumber);
+  const { month, year } = validityPeriod;
+  const CardValidityPeriodUnitString = `${month < 10 ? "0" + month : month}/${year}`;
+
+  return (
+    <Wrapper>
+      <ChipWrapper>
+        <Chip />
+        <MasterCardImg src={masterCard} />
+      </ChipWrapper>
+      <CardNumberWrapper>
+        {cardNumberUnitArr.map((cardNumberUnit) => (
+          <CardNumberUnit>{cardNumberUnit}</CardNumberUnit>
+        ))}
+      </CardNumberWrapper>
+      <CardValidityPeriodWrapper>
+        <CardValidityPeriodUnit>
+          {CardValidityPeriodUnitString}
+        </CardValidityPeriodUnit>
+      </CardValidityPeriodWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 13rem;
+  height: 8rem;
+  background-color: #333;
+  border-radius: 0.25rem;
+  box-shadow: 3px 3px 5px 0px #00000040;
+`;
+
+const ChipWrapper = styled.div`
+  display: flex;
+  padding: 0.5rem 0.75rem;
+  justify-content: space-between;
+`;
+
+const Chip = styled.div`
+  width: 2.25rem;
+  height: 1.5rem;
+  background-color: #ddcd78;
+  border-radius: 0.2rem;
+`;
+
+const MasterCardImg = styled.img`
+  width: 2.25rem;
+  height: 1.5rem;
+`;
+
+const CardNumberWrapper = styled.div`
+  display: flex;
+  padding-inline: 1rem;
+  justify-content: space-between;
+  margin-top: 0.875rem;
+`;
+
+const CardNumberUnit = styled.span`
+  font-weight: 500;
+  font-style: Medium;
+  font-size: 0.875rem;
+  color: #fff;
+  letter-spacing: 16%;
+`;
+
+const CardValidityPeriodWrapper = styled.div`
+  display: flex;
+  padding-inline: 1rem;
+  justify-content: space-between;
+  margin-top: 0.5rem;
+`;
+
+const CardValidityPeriodUnit = styled.span`
+  font-weight: 500;
+  font-style: Medium;
+  font-size: 0.875rem;
+  color: #fff;
+  letter-spacing: 16%;
+`;
+
+export default Card;
