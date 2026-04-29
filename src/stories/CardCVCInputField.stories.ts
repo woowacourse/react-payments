dae59fd2a4c7949f7075b8641e145b085954ebd7
박건규 <geongyu09@gmail.com>
2026-04-29 17:25:52 +0900

    test(CardCVCInputField.stories.ts): CardCVCInputField 컴포넌트 스토리 작성
    Co-authored-by: Boyeon <boboyoii@users.noreply.github.com>

---
+++ src/stories/CardCVCInputField.stories.ts
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import CardCVCInputField from "../components/CardCVCInputField";
+
+const meta = {
+  title: "CardCVCInputField",
+  component: CardCVCInputField,
+} satisfies Meta<typeof CardCVCInputField>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
