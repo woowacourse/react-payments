0f3c5bddffe0b821214a1328390dd161f8a0880a
박건규 <geongyu09@gmail.com>
2026-04-29 17:19:53 +0900

    refactor(CardValidityPeriodInputField.tsx): CardValidityPeriodInputField 컴포넌트를 InputField 공통 컴포넌트로 리팩토링
    Co-authored-by: Boyeon <boboyoii@users.noreply.github.com>

--- src/components/CardValidityPeriodInputField.tsx
+++ src/components/CardValidityPeriodInputField.tsx
@@ -1,45 +1,17 @@
-import styled from "@emotion/styled";
-import Input from "./Input.tsx";
+import InputField from "./InputField.tsx";
 
 const CardValidityPeriodInputField = () => {
   return (
-    <Container>
-      <Title>카드 유효기간을 입력해 주세요</Title>
-      <Caption>월/년도(MMYY)를 순서대로 입력해 주세요.</Caption>
-      <Label>유효기간</Label>
-      <InputWrapper>
-        <Input placeholder="MM" fullWidth />
-        <Input placeholder="YY" fullWidth />
-      </InputWrapper>
-    </Container>
+    <InputField
+      title="카드 유효기간을 입력해 주세요"
+      caption="월/년도(MMYY)를 순서대로 입력해 주세요."
+      label="유효기간"
+      inputPropsList={[
+        { placeholder: "MM", fullWidth: true },
+        { placeholder: "YY", fullWidth: true },
+      ]}
+    />
   );
 };
-
-const Container = styled.section``;
-
-const Title = styled.h2`
-  font-weight: 700;
-  font-size: 1.2rem;
-`;
-
-const Caption = styled.p`
-  font-weight: 400;
-  font-size: 0.6rem;
-  color: #8b95a1;
-  margin-top: 0.25rem;
-`;
-
-const InputWrapper = styled.div`
-  display: flex;
-  gap: 0.625rem;
-  margin-top: 0.5rem;
-`;
-
-const Label = styled.label`
-  font-weight: 500;
-  font-size: 0.75rem;
-  color: #0a0d13;
-  margin-top: 1rem;
-`;
 
 export default CardValidityPeriodInputField;
