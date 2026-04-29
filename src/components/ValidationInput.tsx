b559d710b14201d5a9635f0a605b6951168fd29e
bel1c10ud <mail@bellclou.de>
2026-04-29 18:32:58 +0900

    refactor: ValidationInput 컴포넌트의 검증 로직을 제한과 검사로 분리
    Co-authored-by: Hyewon Jang <janghw232625@gmail.com>

--- src/components/ValidationInput.tsx
+++ src/components/ValidationInput.tsx
@@ -1,14 +1,17 @@
 import { useState, type ChangeEvent, type ComponentProps } from 'react';
 
 interface ValidationInputProps extends ComponentProps<'input'> {
-  validations: { pattern: RegExp; message: string }[];
+  validations: { type: 'limit' | 'check'; pattern: RegExp; message: string }[];
 }
 
-export default function ValidationInput({ validations, onChange, ...props }: ValidationInputProps) {
+export default function ValidationInput({ validations, onChange, onBlur, ...props }: ValidationInputProps) {
   const [inputError, setInputError] = useState<null | Error>(null);
 
   const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const failedValidation = validations.find((validation) => !validation.pattern.test(event.target.value));
+    const failedValidation = validations.find(
+      (validation) => validation.type === 'limit' && !validation.pattern.test(event.target.value),
+    );
+
     if (failedValidation) {
       setInputError(new Error(failedValidation.message));
       return;
@@ -18,9 +21,25 @@ export default function ValidationInput({ validations, onChange, ...props }: Val
     onChange?.(event);
   };
 
+  const handleOnBlur = (event: React.FocusEvent<HTMLInputElement, Element>) => {
+    onBlur?.(event);
+
+    const failedValidation = validations.find(
+      (validation) =>
+        validation.type === 'check' && typeof props.value === 'string' && !validation.pattern.test(props.value),
+    );
+
+    if (failedValidation) {
+      setInputError(new Error(failedValidation.message));
+      return;
+    }
+
+    setInputError(null);
+  };
+
   return (
     <div>
-      <input onChange={handleOnChange} {...props} />
+      <input onChange={handleOnChange} onBlur={handleOnBlur} {...props} />
       {inputError && <p>{inputError.message}</p>}
     </div>
   );
