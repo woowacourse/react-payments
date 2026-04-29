42d1d6d1274cb787139ccea1b6a63afe0178ece5
kojesung <js95112345@gmail.com>
2026-04-29 16:56:20 +0900

    refactor: cardNumber errorMessage용, 입력 방지용 검증 함수 분리
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

--- src/utils/isCardNumber.ts
+++ src/utils/isCardNumber.ts
@@ -1,4 +1,5 @@
+// 전달받은 값이 숫자가 아니면 false 반환
 export const isCardNumber = (cardNumber: string) => {
     if (Number.isNaN(Number(cardNumber))) return false;
-    return cardNumber.length < 5;
+    return true;
 };
