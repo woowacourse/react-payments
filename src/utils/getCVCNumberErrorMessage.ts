69339f67f1d76fc06852b670da27f83ab0aa9d8e
kojesung <js95112345@gmail.com>
2026-04-29 17:16:44 +0900

    refactor: CVCNumber errorMessage용, 입력 방지용 검증 함수 분리
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

---
+++ src/utils/getCVCNumberErrorMessage.ts
@@ -0,0 +1,8 @@
+const CVC_LENGTH_ERROR_MESSAGE = 'CVC는 3개의 숫자로 이루어져야 합니다.';
+
+export const getCVCumberErrorMessage = (values: string[]) => {
+    const CVC = values[0];
+    if (CVC === '') return null;
+    if (CVC.length !== 3) return CVC_LENGTH_ERROR_MESSAGE;
+    return null;
+};
--- src/utils/isCVCNumber.ts
+++
@@ -1,3 +0,0 @@
-export const isCVCNumber = (CVCNumber: string) => {
-    return CVCNumber.length < 4;
-};
