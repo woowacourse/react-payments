7c9f5816209de946f739876c50573dbc21f97866
kojesung <js95112345@gmail.com>
2026-04-29 17:09:24 +0900

    refactor: EXPNumber errorMessage용, 입력 방지용 검증 함수 분리
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

--- src/utils/isCardNumber.ts
+++ src/utils/isCardNumber.ts
@@ -3,3 +3,6 @@ export const isCardNumber = (cardNumber: string) => {
     if (Number.isNaN(Number(cardNumber))) return false;
     return true;
 };
+
+// TODO 리팩토링하다 보니까 길이 검증 빠지게 돼서 그냥 `Number.isNaN(Number(cardNumber))` 그대로 박아두면 될 것 같은데 어떻게 할지 고민
+// isMonthNumberLength, isYearNumber도 똑같음
