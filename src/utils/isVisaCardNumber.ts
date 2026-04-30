5c20b8c96a94400728530a9b203ad796f63ab92c
kojesung <js95112345@gmail.com>
2026-04-30 19:49:27 +0900

    refactor: isVisaCardNumber 유틸 반환 타입 boolean으로 고정
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

--- src/utils/isVisaCardNumber.ts
+++ src/utils/isVisaCardNumber.ts
@@ -1,7 +1,6 @@
 export const isVisaCardNumber = (cardNumbers: string[]) => {
     if (cardNumbers[0].length < 1) {
-        return;
+        return false;
     }
     if (cardNumbers[0][0] === '4') return true;
 };
-//TODO 반환타입 boolean으로 (false) 명확하게
