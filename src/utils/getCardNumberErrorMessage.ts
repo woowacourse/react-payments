290663e509db67a024ac18189ca5069130e9d100
kojesung <js95112345@gmail.com>
2026-04-30 20:22:10 +0900

    refactor: errorMessage 반환 유틸에서 불필요한 조건문 삭제
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

--- src/utils/getCardNumberErrorMessage.ts
+++ src/utils/getCardNumberErrorMessage.ts
@@ -2,7 +2,6 @@
 // getCardNumberErrorMessage는 4개씩 총 16개의 숫자인지만 검증
 const ERROR_MESSAGE = '카드번호는 4개씩 총 16개의 숫자로 이루어져야 합니다.';
 export const getCardNumberErrorMessage = (values: string[]) => {
-    if (values.some((value) => value === '')) return null;
     if (values.some((value) => value.length !== 4)) return ERROR_MESSAGE;
     return null;
 };
