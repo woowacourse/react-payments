c51221e3d5615dd7806aa5d41ba76718f9125793
JuHyeong424 <wkdwngud963@gmail.com>
2026-04-30 15:12:13 +0900

    feat: 카드 정보 헤더 컴포넌트 구현

---
+++ src/components/cardInfo/CardInfoHeader.tsx
@@ -0,0 +1,38 @@
+import styled from "@emotion/styled";
+
+interface CardInfoHeaderProps {
+  guide: string;
+  subGuide: string;
+}
+
+export default function CardInfoHeader({
+  guide,
+  subGuide,
+}: CardInfoHeaderProps) {
+  return (
+    <CardInfoHeaderWrapper>
+      <h1>{guide}</h1>
+      <p>{subGuide}</p>
+    </CardInfoHeaderWrapper>
+  );
+}
+
+const CardInfoHeaderWrapper = styled.div`
+  gap: 4px;
+  h1 {
+    font-size: 18px;
+    font-weight: 700;
+    letter-spacing: 0%;
+    line-height: 100%;
+    vertical-align: middle;
+    color: rgba(0, 0, 0, 1);
+  }
+  p {
+    font-size: 9.5px;
+    font-weight: 400;
+    letter-spacing: 0%;
+    line-height: 100%;
+    vertical-align: middle;
+    color: rgba(139, 149, 161, 1);
+  }
+`;
