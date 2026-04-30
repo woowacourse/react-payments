44b9bcb510692cf02b3e3e1b23d226c9def3a663
kojesung <js95112345@gmail.com>
2026-04-30 14:23:33 +0900

    feat: App.tsx에 CardPreview 렌더링
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { getCVCumberErrorMessage } from './utils/getCVCNumberErrorMessage';
 import { getEXPNumberErrorMessage } from './utils/getEXPNumberErrorMessage';
 import { isLengthMatch } from './utils/isLengthMatch';
 import { isMonthMatch } from './utils/isMonthMatch';
+import CardPreview from './components/Card/CardPreview';
 
 function App() {
     const [firstCardNumber, setFirstCardNumber] = useState('');
@@ -24,85 +25,88 @@ function App() {
     const cardNumberValues = [firstCardNumber, secondCardNumber, thirdCardNumber, fourthCardNumber];
     const expValues = [expMonth, expYear];
     return (
-        <InputSectionContainer>
-            <CardInfoSection
-                title="결제할 카드 번호를 입력해 주세요"
-                caption="본인 명의의 카드만 결제 가능합니다."
-                inputLabel="카드 번호"
-            >
-                <CardInputWrapper validator={getCardNumberErrorMessage} value={cardNumberValues}>
-                    <CardInfoInput
-                        value={firstCardNumber}
-                        setValue={setFirstCardNumber}
-                        type="card-number"
-                        placeHolder="1234"
-                        validator={(value: string) => isLengthMatch(4, value)}
-                        maxLength={4}
-                    />
-                    <CardInfoInput
-                        value={secondCardNumber}
-                        setValue={setSecondCardNumber}
-                        type="card-number"
-                        placeHolder="1234"
-                        validator={(value: string) => isLengthMatch(4, value)}
-                        maxLength={4}
-                    />
-                    <CardInfoInput
-                        value={thirdCardNumber}
-                        setValue={setThirdCardNumber}
-                        type="card-number"
-                        placeHolder="1234"
-                        validator={(value: string) => isLengthMatch(4, value)}
-                        maxLength={4}
-                    />
-                    <CardInfoInput
-                        value={fourthCardNumber}
-                        setValue={setFourthCardNumber}
-                        type="card-number"
-                        placeHolder="1234"
-                        validator={(value: string) => isLengthMatch(4, value)}
-                        maxLength={4}
-                    />
-                </CardInputWrapper>
-            </CardInfoSection>
+        <MainContainer>
+            <CardPreview cardNumbers={cardNumberValues} EXP={expValues} />
+            <InputSectionContainer>
+                <CardInfoSection
+                    title="결제할 카드 번호를 입력해 주세요"
+                    caption="본인 명의의 카드만 결제 가능합니다."
+                    inputLabel="카드 번호"
+                >
+                    <CardInputWrapper validator={getCardNumberErrorMessage} value={cardNumberValues}>
+                        <CardInfoInput
+                            value={firstCardNumber}
+                            setValue={setFirstCardNumber}
+                            type="card-number"
+                            placeHolder="1234"
+                            validator={(value: string) => isLengthMatch(4, value)}
+                            maxLength={4}
+                        />
+                        <CardInfoInput
+                            value={secondCardNumber}
+                            setValue={setSecondCardNumber}
+                            type="card-number"
+                            placeHolder="1234"
+                            validator={(value: string) => isLengthMatch(4, value)}
+                            maxLength={4}
+                        />
+                        <CardInfoInput
+                            value={thirdCardNumber}
+                            setValue={setThirdCardNumber}
+                            type="card-number"
+                            placeHolder="1234"
+                            validator={(value: string) => isLengthMatch(4, value)}
+                            maxLength={4}
+                        />
+                        <CardInfoInput
+                            value={fourthCardNumber}
+                            setValue={setFourthCardNumber}
+                            type="card-number"
+                            placeHolder="1234"
+                            validator={(value: string) => isLengthMatch(4, value)}
+                            maxLength={4}
+                        />
+                    </CardInputWrapper>
+                </CardInfoSection>
 
-            <CardInfoSection
-                title="카드 유효기간을 입력해 주세요"
-                caption="월/년도(MMYY)를 순서대로 입력해 주세요."
-                inputLabel="유효기간"
-            >
-                <CardInputWrapper validator={getEXPNumberErrorMessage} value={expValues}>
-                    <CardInfoInput
-                        value={expMonth}
-                        setValue={setEXPMonth}
-                        type="exp"
-                        placeHolder="MM"
-                        validator={(value: string) => isMonthMatch(value)}
-                        maxLength={2}
-                    />
-                    <CardInfoInput
-                        value={expYear}
-                        setValue={setEXPYear}
-                        type="exp"
-                        placeHolder="YY"
-                        validator={(value: string) => isLengthMatch(2, value)}
-                        maxLength={2}
-                    />
-                </CardInputWrapper>
-            </CardInfoSection>
-            <CardInfoSection title="CVC 번호를 입력해 주세요" inputLabel="CVC">
-                <CardInputWrapper validator={getCVCumberErrorMessage} value={[cvc]}>
-                    <CardInfoInput
-                        value={cvc}
-                        setValue={setCVC}
-                        type="cvc"
-                        placeHolder="123"
-                        validator={(value: string) => isLengthMatch(3, value)}
-                        maxLength={3}
-                    />
-                </CardInputWrapper>
-            </CardInfoSection>
-        </InputSectionContainer>
+                <CardInfoSection
+                    title="카드 유효기간을 입력해 주세요"
+                    caption="월/년도(MMYY)를 순서대로 입력해 주세요."
+                    inputLabel="유효기간"
+                >
+                    <CardInputWrapper validator={getEXPNumberErrorMessage} value={expValues}>
+                        <CardInfoInput
+                            value={expMonth}
+                            setValue={setEXPMonth}
+                            type="exp"
+                            placeHolder="MM"
+                            validator={(value: string) => isMonthMatch(value)}
+                            maxLength={2}
+                        />
+                        <CardInfoInput
+                            value={expYear}
+                            setValue={setEXPYear}
+                            type="exp"
+                            placeHolder="YY"
+                            validator={(value: string) => isLengthMatch(2, value)}
+                            maxLength={2}
+                        />
+                    </CardInputWrapper>
+                </CardInfoSection>
+                <CardInfoSection title="CVC 번호를 입력해 주세요" inputLabel="CVC">
+                    <CardInputWrapper validator={getCVCumberErrorMessage} value={[cvc]}>
+                        <CardInfoInput
+                            value={cvc}
+                            setValue={setCVC}
+                            type="cvc"
+                            placeHolder="123"
+                            validator={(value: string) => isLengthMatch(3, value)}
+                            maxLength={3}
+                        />
+                    </CardInputWrapper>
+                </CardInfoSection>
+            </InputSectionContainer>
+        </MainContainer>
     );
 }
 
@@ -112,4 +116,16 @@ const InputSectionContainer = styled.div`
     display: flex;
     flex-direction: column;
     gap: 16px;
+`;
+
+const MainContainer = styled.main`
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 45px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 `;
