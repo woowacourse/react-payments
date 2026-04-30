a712e59d6677370716afd2d3ca34373e61d6aa09
kojesung <js95112345@gmail.com>
2026-04-30 14:25:58 +0900

    feat: 컴포넌트 story 추가
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

--- src/components/Card/CardPreview.stories.tsx
+++ src/components/Card/CardPreview.stories.tsx
@@ -9,17 +9,31 @@ const meta: Meta<typeof CardPreview> = {
 export default meta;
 type Story = StoryObj<typeof CardPreview>;
 
-export const Default: Story = {
-    args: {
-        cardNumbers: ['1234', '5678', '9012', '3456'],
-        EXP: ['12', '26'],
-    },
-};
-
 export const Empty: Story = {
     args: {
         cardNumbers: ['', '', '', ''],
         EXP: ['', ''],
+    },
+};
+
+export const Full: Story = {
+    args: {
+        cardNumbers: ['1234', '1234', '1234', '1234'],
+        EXP: ['12', '55'],
+    },
+};
+
+export const VisaFull: Story = {
+    args: {
+        cardNumbers: ['4234', '1234', '1234', '1234'],
+        EXP: ['12', '55'],
+    },
+};
+
+export const MasterCardFull: Story = {
+    args: {
+        cardNumbers: ['5134', '1234', '1234', '1234'],
+        EXP: ['12', '55'],
     },
 };
 
