f7d4a0c29ef548239a2f9b05eec0afe28123da2c
박건규 <geongyu09@gmail.com>
2026-04-30 21:01:43 +0900

    refactor(utils/cars.ts): 카드 유효기간을 포맷하는 유틸 함수 구현
    Co-authored-by: Boyeon <boboyoii@users.noreply.github.com>

--- src/utils/card.ts
+++ src/utils/card.ts
@@ -1,10 +1,21 @@
-import type { CardNumberUnits } from "../components/CardNumberInputField";
+import type { CardBrand } from "@components/Card";
+import type { ValidityPeriod } from "@components/CardValidityPeriodInputField";
+import type { CardNumberUnits } from "@components/CardNumberInputField";
 
-export const detectCardBrand = (cardNumber: CardNumberUnits) => {
+export const detectCardBrand = (
+  cardNumber: CardNumberUnits,
+): CardBrand | null => {
   if (cardNumber[0].startsWith("4")) return "Visa";
 
   if (["51", "52", "53", "54", "55"].includes(cardNumber[0].slice(0, 2)))
     return "MasterCard";
 
   return null;
+};
+
+export const getFormattedValidityPeriodUnit = (
+  validityPeriod: ValidityPeriod,
+) => {
+  const { month, year } = validityPeriod;
+  return `${month ? month + "/" : ""}${year ? year : ""}`;
 };
