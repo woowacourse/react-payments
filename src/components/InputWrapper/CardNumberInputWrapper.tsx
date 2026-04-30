92ad53ebb2ad4550843f8c9a428555e101ea5f78
kojesung <js95112345@gmail.com>
2026-04-30 17:47:04 +0900

    docs: TODO 리스트업
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

--- src/components/InputWrapper/CardNumberInputWrapper.tsx
+++ src/components/InputWrapper/CardNumberInputWrapper.tsx
@@ -16,6 +16,7 @@ export default function CardNumberInputWrapper({ validator, setCardNumber, value
         setInputErrors((prev) => prev.with(index, message));
     };
 
+    // TODO 이거 중복되는 부분 없애기
     const inputError =
         inputErrors.find((err) => err !== null) ?? null
             ? inputErrors.find((err) => err !== null) ?? null
