9cac264dcd359c17054a0fe707feb1d5aee928d9
kojesung <js95112345@gmail.com>
2026-04-30 14:25:58 +0900

    feat: 컴포넌트 story 추가
    Co-authored-by: yuncic <yuncic@users.noreply.github.com>

--- src/components/Input/CardInfoInput.stories.tsx
+++ src/components/Input/CardInfoInput.stories.tsx
@@ -1,24 +1,60 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { useState } from 'react';
 import CardInfoInput from './CardInfoInput';
+import { useState } from 'react';
+import { isLengthMatch } from '../../utils/isLengthMatch';
+import { isMonthMatch } from '../../utils/isMonthMatch';
 
 const meta: Meta<typeof CardInfoInput> = {
-    title: 'Components/Input',
+    title: 'Components/CardInfoInput',
     component: CardInfoInput,
 };
 
 export default meta;
 type Story = StoryObj<typeof CardInfoInput>;
 
-export const Default: Story = {
+export const CardNumber: Story = {
     render: () => {
         const [value, setValue] = useState('');
         return (
             <CardInfoInput
                 value={value}
-                type={{ type: 'exp', expType: 'month' }}
                 setValue={setValue}
-                validator={() => true}
+                type="card-number"
+                validator={(v) => isLengthMatch(4, v)}
+                maxLength={4}
+                placeHolder="0000"
+            />
+        );
+    },
+};
+
+export const EXP: Story = {
+    render: () => {
+        const [value, setValue] = useState('');
+        return (
+            <CardInfoInput
+                value={value}
+                setValue={setValue}
+                type="exp"
+                validator={(v) => isMonthMatch(v)}
+                maxLength={2}
+                placeHolder="MM"
+            />
+        );
+    },
+};
+
+export const CVC: Story = {
+    render: () => {
+        const [value, setValue] = useState('');
+        return (
+            <CardInfoInput
+                value={value}
+                setValue={setValue}
+                type="cvc"
+                validator={(v) => isLengthMatch(3, v)}
+                maxLength={3}
+                placeHolder="000"
             />
         );
     },
