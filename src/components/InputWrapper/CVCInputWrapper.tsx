ef84b6520017eb877ea8a319780ade31b171f50d
김상윤 <ksy151020@gmail.com>
2026-05-01 16:17:25 +0900

    refactor: CVC value 값 타입 변경에 따른 전달 값 타입 수정 ([] -> string)

--- src/components/InputWrapper/CVCInputWrapper.tsx
+++ src/components/InputWrapper/CVCInputWrapper.tsx
@@ -1,34 +1,38 @@
-import { useState } from 'react';
-import { isLengthMatch } from '../../utils/isLengthMatch';
-import CardInfoInput from '../Input/CardInfoInput';
-import CardInputWrapper from './CardInputWrapper';
+import { useState } from "react";
+import { isLengthMatch } from "../../utils/isLengthMatch";
+import CardInfoInput from "../Input/CardInfoInput";
+import CardInputWrapper from "./CardInputWrapper";
 
 interface CVCInputWrapperProps {
-    validator: (value: string) => string | null;
-    setCVCNumber: (value: string) => void;
-    value: string;
+  validator: (value: string) => string | null;
+  setCVCNumber: (value: string) => void;
+  value: string;
 }
 
-export default function CVCInputWrapper({ validator, setCVCNumber, value }: CVCInputWrapperProps) {
-    const [inputError, setInputError] = useState<string | null>(null);
+export default function CVCInputWrapper({
+  validator,
+  setCVCNumber,
+  value,
+}: CVCInputWrapperProps) {
+  const [inputError, setInputError] = useState<string | null>(null);
 
-    const [hasTouched, setHasTouched] = useState(false);
-    const errorAfterCompleted = hasTouched ? validator(value) : null;
-    const errorMessage = inputError ?? errorAfterCompleted;
+  const [hasTouched, setHasTouched] = useState(false);
+  const errorAfterCompleted = hasTouched ? validator(value) : null;
+  const errorMessage = inputError ?? errorAfterCompleted;
 
-    return (
-        <CardInputWrapper errorMessage={errorMessage}>
-            <CardInfoInput
-                value={value[0]}
-                setValue={setCVCNumber}
-                type="cvc"
-                placeHolder="123"
-                validator={(value: string) => isLengthMatch(3, value)}
-                maxLength={3}
-                onError={setInputError}
-                onBlur={() => setHasTouched(true)}
-                onFocus={() => setHasTouched(false)}
-            />
-        </CardInputWrapper>
-    );
+  return (
+    <CardInputWrapper errorMessage={errorMessage}>
+      <CardInfoInput
+        value={value}
+        setValue={setCVCNumber}
+        type="cvc"
+        placeHolder="123"
+        validator={(value: string) => isLengthMatch(3, value)}
+        maxLength={3}
+        onError={setInputError}
+        onBlur={() => setHasTouched(true)}
+        onFocus={() => setHasTouched(false)}
+      />
+    </CardInputWrapper>
+  );
 }
