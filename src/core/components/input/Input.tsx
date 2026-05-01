be6f4c33e5bb3ab7c80795a6d3884179d791b55e
stardusty-lab <startdusty.lab@gmail.com>
2026-05-01 09:53:22 +0900

    feature: Input 컴포넌트 props 에 type 추가 - text, tel
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/core/components/input/Input.tsx
+++ src/core/components/input/Input.tsx
@@ -2,6 +2,7 @@ import cn from 'classnames';
 import styles from './Input.module.css';
 
 interface InputProps {
+  type?: 'text' | 'tel';
   placeholder?: string;
   maxLength?: number;
   isError?: boolean;
@@ -9,9 +10,10 @@ interface InputProps {
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Input = ({ placeholder, maxLength, isError, value, onChange }: InputProps) => {
+export const Input = ({ type = 'text', placeholder, maxLength, isError, value, onChange }: InputProps) => {
   return (
     <input
+      type={type}
       className={cn(styles.input, isError && styles.isError)}
       placeholder={placeholder}
       maxLength={maxLength}
