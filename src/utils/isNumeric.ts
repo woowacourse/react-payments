b4ce822b60d93eabe257b9d88386e644a1a07fd4
kojesung <js95112345@gmail.com>
2026-05-03 19:39:05 +0900

    fix: isNumeric에 공백 검증 추가

--- src/utils/isNumeric.ts
+++ src/utils/isNumeric.ts
@@ -1,2 +1,4 @@
-export const isNumeric = (value: string): string | null =>
-    Number.isNaN(Number(value)) ? '숫자만 입력할 수 있습니다.' : null;
+export const isNumeric = (value: string): string | null => {
+    if (value !== '' && value.trim() === '') return '숫자만 입력할 수 있습니다.';
+    return Number.isNaN(Number(value)) ? '숫자만 입력할 수 있습니다.' : null;
+};
