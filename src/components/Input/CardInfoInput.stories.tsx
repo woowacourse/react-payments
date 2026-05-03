c31cbe1cb92ba7b056a009ecf2545e16dbf95239
김상윤 <ksy151020@gmail.com>
2026-05-03 16:18:22 +0900

    refactor: CardInfoInput 스토리에서 placeholder 속성의 오타 수정 및 제거한 type속성 제거

--- src/components/Input/CardInfoInput.stories.tsx
+++ src/components/Input/CardInfoInput.stories.tsx
@@ -1,74 +1,71 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import CardInfoInput from './CardInfoInput';
-import { useState } from 'react';
+import type { Meta, StoryObj } from "@storybook/react";
+import CardInfoInput from "./CardInfoInput";
+import { useState } from "react";
 
 const meta: Meta<typeof CardInfoInput> = {
-    title: 'Components/CardInfoInput',
-    component: CardInfoInput,
+  title: "Components/CardInfoInput",
+  component: CardInfoInput,
 };
 
 export default meta;
 type Story = StoryObj<typeof CardInfoInput>;
 
 export const CardNumber: Story = {
-    render: () => {
-        const [value, setValue] = useState('');
-        const [error, setError] = useState<string | null>(null);
-        return (
-            <>
-                <CardInfoInput
-                    value={value}
-                    setValue={setValue}
-                    type="card-number"
-                    validator={(v) => v.length === 4}
-                    maxLength={4}
-                    placeHolder="0000"
-                    onError={setError}
-                />
-                {error && <p style={{ color: 'red', fontSize: '12px' }}>{error}</p>}
-            </>
-        );
-    },
+  render: () => {
+    const [value, setValue] = useState("");
+    const [error, setError] = useState<string | null>(null);
+    return (
+      <>
+        <CardInfoInput
+          value={value}
+          setValue={setValue}
+          validator={(v) => v.length === 4}
+          maxLength={4}
+          placeholder="0000"
+          onError={setError}
+        />
+        {error && <p style={{ color: "red", fontSize: "12px" }}>{error}</p>}
+      </>
+    );
+  },
 };
 
 export const EXP: Story = {
-    render: () => {
-        const [value, setValue] = useState('');
-        const [error, setError] = useState<string | null>(null);
-        return (
-            <>
-                <CardInfoInput
-                    value={value}
-                    setValue={setValue}
-                    type="exp"
-                    validator={(v) => v.length === 2}
-                    maxLength={2}
-                    placeHolder="MM"
-                    onError={setError}
-                />
-                {error && <p style={{ color: 'red', fontSize: '12px' }}>{error}</p>}
-            </>
-        );
-    },
+  render: () => {
+    const [value, setValue] = useState("");
+    const [error, setError] = useState<string | null>(null);
+    return (
+      <>
+        <CardInfoInput
+          value={value}
+          setValue={setValue}
+          validator={(v) => v.length === 2}
+          maxLength={2}
+          placeholder="MM"
+          onError={setError}
+        />
+        {error && <p style={{ color: "red", fontSize: "12px" }}>{error}</p>}
+      </>
+    );
+  },
 };
 
 export const CVC: Story = {
-    render: () => {
-        const [value, setValue] = useState('');
-        const [error, setError] = useState<string | null>(null);
-        return (
-            <>
-                <CardInfoInput
-                    value={value}
-                    setValue={setValue}
-                    type="cvc"
-                    validator={(v) => v.length === 3}
-                    maxLength={3}
-                    placeHolder="123"
-                    onError={setError}
-                />
-                {error && <p style={{ color: 'red', fontSize: '12px' }}>{error}</p>}
-            </>
-        );
-    },
+  render: () => {
+    const [value, setValue] = useState("");
+    const [error, setError] = useState<string | null>(null);
+    return (
+      <>
+        <CardInfoInput
+          value={value}
+          setValue={setValue}
+          validator={(v) => v.length === 3}
+          maxLength={3}
+          placeholder="123"
+          onError={setError}
+        />
+        {error && <p style={{ color: "red", fontSize: "12px" }}>{error}</p>}
+      </>
+    );
+  },
 };
