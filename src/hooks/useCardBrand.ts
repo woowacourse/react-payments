82e7d03c3aa0e709ea206ccb11396126867c4102
bel1c10ud <mail@bellclou.de>
2026-05-03 23:36:28 +0900

    fix: 불필요한 정규식 플래그 제거

--- src/hooks/useCardBrand.ts
+++ src/hooks/useCardBrand.ts
@@ -3,7 +3,7 @@ import { useMemo } from "react";
 export function useCardBrand(CardNumberSegments: [string, string, string, string]) {
   const cardBrand = useMemo(() => {
     if (CardNumberSegments[0].startsWith('4')) return 'VISA';
-    if (/^(51|52|53|54|55)/g.test(CardNumberSegments[0])) return 'MasterCard';
+    if (/^(51|52|53|54|55)/.test(CardNumberSegments[0])) return 'MasterCard';
     return null;
   }, [CardNumberSegments]);
 
