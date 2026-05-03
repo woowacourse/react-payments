a76edcf4551c1045ec6bfd40f3858256bf2e6537
김상윤 <ksy151020@gmail.com>
2026-05-03 15:12:04 +0900

    refactor: 카드 브랜드 로고 분기 로직 한 함수에서 관리하도록 개선

--- src/utils/getCardBrand.ts
+++ src/utils/getCardBrand.ts
@@ -1,16 +1,13 @@
-export const getCardBrand = (cardNumbers: {
-  first: string;
-  second: string;
-  third: string;
-  fourth: string;
-}): "visa" | "master" | null => {
-  if (cardNumbers.first.length < 1) {
+export const getCardBrand = (
+  cardNumbers: string[],
+): "visa" | "master" | null => {
+  if (cardNumbers[0].length < 1) {
     return null;
   }
-  if (cardNumbers.first[0] === "4") return "visa";
+  if (cardNumbers[0][0] === "4") return "visa";
 
-  if (cardNumbers.first.length < 2) return null;
-  if (cardNumbers.first[0] !== "5") return null;
-  const secondNumber = Number(cardNumbers.first[1]);
+  if (cardNumbers[0].length < 2) return null;
+  if (cardNumbers[0][0] !== "5") return null;
+  const secondNumber = Number(cardNumbers[0][1]);
   return 1 <= secondNumber && secondNumber <= 5 ? "master" : null;
 };
