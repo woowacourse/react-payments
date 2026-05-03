2acf0d94ce1c312e547a0448d02a57399f9fab33
iftype <conditionaltype@gmail.com>
2026-05-03 17:56:38 +0900

    refactor: 공용 input의 props 를 확장

--- src/core/components/input/Input.tsx
+++ src/core/components/input/Input.tsx
@@ -1,26 +1,12 @@
 import cn from 'classnames';
 import styles from './Input.module.css';
 
-interface InputProps {
-  type?: 'text' | 'tel';
-  placeholder?: string;
-  maxLength?: number;
+import type { InputHTMLAttributes } from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   isError?: boolean;
-  value?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onBlur?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Input = ({ type = 'text', placeholder, maxLength, isError, value, onChange, onBlur }: InputProps) => {
-  return (
-    <input
-      type={type}
-      className={cn(styles.input, isError && styles.isError)}
-      placeholder={placeholder}
-      maxLength={maxLength}
-      value={value}
-      onChange={onChange}
-      onBlur={onBlur}
-    />
-  );
+export const Input = ({ isError, ...rest }: InputProps) => {
+  return <input className={cn(styles.input, isError && styles.isError)} {...rest} />;
 };
--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@/*': path.resolve(__dirname, './src/*'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
