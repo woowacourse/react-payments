b3024b9a0c18db9ca6aa4d334b3fd2c62115f675
김상윤 <ksy151020@gmail.com>
2026-05-03 16:07:54 +0900

    efactor: validator 반환값에 에러 발생 index 포함하도록 개선

--- src/components/InputWrapper/EXPInputWrapper.tsx
+++ src/components/InputWrapper/EXPInputWrapper.tsx
@@ -6,7 +6,7 @@ import CardInfoInput from "../Input/CardInfoInput";
 import CardInputWrapper from "./CardInputWrapper";
 
 interface Props {
-  validator: (value: string[]) => string | null;
+  validator: (value: string[]) => { message: string; index: number } | null;
   setEXPNumber: (index: number) => (value: string) => void;
   value: string[];
 }
@@ -24,14 +24,11 @@ export default function EXPInputWrapper({
   const setError = (index: number) => (message: string | null) => {
     setInputErrors((prev) => prev.with(index, message));
   };
-  const [hasTouched, setHasTouched] = useState<boolean>(false);
-  const errorAfterCompleted = hasTouched ? validator(value) : null;
-
-  const inputError =
-    inputErrors.find((err) => err !== null) ?? errorAfterCompleted;
 
   return (
-    <CardInputWrapper errorMessage={inputError}>
+    <CardInputWrapper
+      errorMessage={inputErrors.find((err) => err !== null) ?? null}
+    >
       <CardInfoInput
         value={value[0]}
         setValue={setEXPNumber(0)}
@@ -39,8 +36,10 @@ export default function EXPInputWrapper({
         validator={(value: string) => isMonthMatch(value)}
         maxLength={2}
         onError={setError(0)}
-        onBlur={() => setHasTouched(true)}
-        onFocus={() => setHasTouched(false)}
+        onBlur={() => {
+          const result = validator(value);
+          if (result && result.index === 0) setError(0)(result.message);
+        }}
         style={{ width: "152px" }}
       />
       <CardInfoInput
@@ -50,8 +49,10 @@ export default function EXPInputWrapper({
         validator={(value: string) => isLengthMatch(2, value)}
         maxLength={2}
         onError={setError(1)}
-        onBlur={() => setHasTouched(true)}
-        onFocus={() => setHasTouched(false)}
+        onBlur={() => {
+          const result = validator(value);
+          if (result && result.index === 1) setError(1)(result.message);
+        }}
         style={{ width: "152px" }}
       />
     </CardInputWrapper>
--- src/utils/getCardNumberErrorMessage.ts
+++ src/utils/getCardNumberErrorMessage.ts
@@ -1,7 +1,10 @@
 // 숫자인지 검증은 isCardNumber에서 담당
 // getCardNumberErrorMessage는 4개씩 총 16개의 숫자인지만 검증
-const ERROR_MESSAGE = '카드번호는 4개씩 총 16개의 숫자로 이루어져야 합니다.';
-export const getCardNumberErrorMessage = (values: string[]) => {
-    if (values.some((value) => value.length !== 4)) return ERROR_MESSAGE;
-    return null;
+const ERROR_MESSAGE = "카드번호는 4개씩 총 16개의 숫자로 이루어져야 합니다.";
+export const getCardNumberErrorMessage = (
+  values: string[],
+): { message: string; index: number } | null => {
+  const errorIndex = values.findIndex((value) => value.length !== 4);
+  if (errorIndex !== -1) return { message: ERROR_MESSAGE, index: errorIndex };
+  return null;
 };
