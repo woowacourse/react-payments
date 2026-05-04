d2630045673b5eabc7fd881df32f4aaf05e3acc2
iftype <conditionaltype@gmail.com>
2026-05-04 23:06:03 +0900

    chore: CreditCard CardPreview로 이름변경

--- src/features/cardPreview/CardPreview.stories.tsx
+++ src/features/cardPreview/CardPreview.stories.tsx
@@ -1,12 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
-import { CreditCard } from './CreditCard';
+import { CardPreview } from './CardPreview';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Example/CreditCard',
-  component: CreditCard,
-} satisfies Meta<typeof CreditCard>;
+  component: CardPreview,
+} satisfies Meta<typeof CardPreview>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
--- src/features/cardPreview/CardPreview.tsx
+++ src/features/cardPreview/CardPreview.tsx
@@ -3,7 +3,7 @@ import styles from './CreditCard.module.css';
 import MastercardSvg from '@/core/assets/Mastercard.svg?react';
 import VisaSvg from '@/core/assets/Visa.svg?react';
 
-import type { CardNumbers, ExpirationDate } from '../types';
+import type { CardNumbers, ExpirationDate } from '../../pages/payments/types';
 
 export interface DefaultCreditCardProps {
   cardBrand: 'default' | 'visa' | 'mastercard';
@@ -19,9 +19,9 @@ const BrandMap = {
 
 const STAR = '●';
 
-export const CreditCard = ({ cardBrand = 'default', cardNumbers, expirationDate }: DefaultCreditCardProps) => {
+export const CardPreview = ({ cardBrand = 'default', cardNumbers, expirationDate }: DefaultCreditCardProps) => {
   return (
-    <div className={styles.creditCard}>
+    <div className={styles.cardPreview}>
       <div className={styles.card}>
         <div className={styles.brand}>{BrandMap[cardBrand]}</div>
         <div className={styles.number}>
