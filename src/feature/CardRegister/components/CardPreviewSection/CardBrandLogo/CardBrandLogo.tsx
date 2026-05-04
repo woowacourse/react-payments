0a74f46b85989080ba7a1a924a05f759407c4a8f
jebiyeon02 <jhje5595@naver.com>
2026-05-05 03:25:48 +0900

    feat: Logo 이미지 alt 설정

--- src/feature/CardRegister/components/CardPreviewSection/CardBrandLogo/CardBrandLogo.tsx
+++ src/feature/CardRegister/components/CardPreviewSection/CardBrandLogo/CardBrandLogo.tsx
@@ -7,7 +7,12 @@ type BrandName = "visa" | "masterCard" | null;
 const CardBrandLogo = ({ brandName }: { brandName: BrandName }) => {
   if (brandName === null) return null;
 
-  return <BrandLogoImg src={brandName === "visa" ? visaImg : masterCardImg} />;
+  return (
+    <BrandLogoImg
+      src={brandName === "visa" ? visaImg : masterCardImg}
+      alt={`${brandName} 로고`}
+    />
+  );
 };
 
 const BrandLogoImg = styled.img`
