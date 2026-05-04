8151135de55b338e2d862e3e37724a188ba8797e
binggwa <qudrhks317@gmail.com>
2026-05-04 14:50:38 +0900

    chore: 배포를 위한 환경설정 및 PR템플릿 작성

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -12,6 +12,7 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
+  base: './',
   test: {
     projects: [{
       extends: true,
