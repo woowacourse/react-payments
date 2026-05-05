db2dc0a9e0731384662d992a4aae40b01f522c79
janghw0126 <janghw232625@gmail.com>
2026-05-06 01:15:54 +0900

    refactor: 자식 컴포넌트의 내부 상태 제거 후 변경값을 부모로 직접 전달하도록 변경

--- src/components/CardExpiryDateInput.tsx
+++ src/components/CardExpiryDateInput.tsx
@@ -1,35 +1,28 @@
-import { useState, useEffect, type ChangeEvent } from 'react';
-import type { CardFormState } from '../types';
-import ValidationInput from './Common/ValidationInput';
-import Flex from './Common/Flex';
+import { type ChangeEvent } from "react";
+import type { CardFormState } from "../types";
+import ValidationInput from "./Common/ValidationInput";
+import Flex from "./Common/Flex";
 import {
   validateMonth,
   validateNumberString,
   validateStringLength,
   validateStringMaxLength,
   validateYear,
-} from '../utils';
-import Label from './Common/Label';
+} from "../utils";
+import Label from "./Common/Label";
 
 interface CardExpiryDateInputProps {
-  value: Pick<CardFormState, 'expiryMonth' | 'expiryYear'>;
+  value: Pick<CardFormState, "expiryMonth" | "expiryYear">;
   onChange: (value: [string, string]) => void;
 }
 
 export default function CardExpiryDateInput(props: CardExpiryDateInputProps) {
-  const [expiryMonth, setExpiryMonth] = useState(props.value.expiryMonth);
-  const [expiryYear, setExpiryYear] = useState(props.value.expiryYear);
-
-  useEffect(() => {
-    props.onChange([expiryMonth, expiryYear]);
-  }, [expiryMonth, expiryYear]);
-
   const handleChangeMonth = (event: ChangeEvent<HTMLInputElement>) => {
-    setExpiryMonth(event.target.value);
+    props.onChange([event.target.value, props.value.expiryYear]);
   };
 
   const handleChangeYear = (event: ChangeEvent<HTMLInputElement>) => {
-    setExpiryYear(event.target.value);
+    props.onChange([props.value.expiryMonth, event.target.value]);
   };
 
   return (
@@ -41,29 +34,29 @@ export default function CardExpiryDateInput(props: CardExpiryDateInputProps) {
           inputMode="numeric"
           autoComplete="cc-exp-month"
           placeholder="MM"
-          value={expiryMonth}
+          value={props.value.expiryMonth}
           onChange={handleChangeMonth}
           isShowError={true}
           validations={[
             {
-              type: 'validateOnChange',
+              type: "validateOnChange",
               validator: validateNumberString,
-              message: '숫자만 입력 가능합니다.',
+              message: "숫자만 입력 가능합니다.",
             },
             {
-              type: 'validateOnChange',
+              type: "validateOnChange",
               validator: (input: string) => validateStringMaxLength(input, 2),
-              message: '2자리까지 입력 가능합니다.',
+              message: "2자리까지 입력 가능합니다.",
             },
             {
-              type: 'validateOnBlur',
+              type: "validateOnBlur",
               validator: (input: string) => validateStringLength(input, 2),
-              message: '2자리를 입력해주세요.',
+              message: "2자리를 입력해주세요.",
             },
             {
-              type: 'validateOnBlur',
+              type: "validateOnBlur",
               validator: validateMonth,
-              message: '유효한 월을 입력해주세요. (01 ~ 12)',
+              message: "유효한 월을 입력해주세요. (01 ~ 12)",
             },
           ]}
         />
@@ -72,29 +65,29 @@ export default function CardExpiryDateInput(props: CardExpiryDateInputProps) {
           inputMode="numeric"
           autoComplete="cc-exp-year"
           placeholder="YY"
-          value={expiryYear}
+          value={props.value.expiryYear}
           onChange={handleChangeYear}
           isShowError={true}
           validations={[
             {
-              type: 'validateOnChange',
+              type: "validateOnChange",
               validator: validateNumberString,
-              message: '숫자만 입력 가능합니다.',
+              message: "숫자만 입력 가능합니다.",
             },
             {
-              type: 'validateOnChange',
+              type: "validateOnChange",
               validator: (input: string) => validateStringMaxLength(input, 2),
-              message: '2자리까지 입력 가능합니다.',
+              message: "2자리까지 입력 가능합니다.",
             },
             {
-              type: 'validateOnBlur',
+              type: "validateOnBlur",
               validator: (input: string) => validateStringLength(input, 2),
-              message: '2자리를 입력해주세요.',
+              message: "2자리를 입력해주세요.",
             },
             {
-              type: 'validateOnBlur',
+              type: "validateOnBlur",
               validator: validateYear,
-              message: '유효한 년도을 입력해주세요. (00 ~ 99)',
+              message: "유효한 년도을 입력해주세요. (00 ~ 99)",
             },
           ]}
         />
--- src/components/CardNumberSegmentsInput.tsx
+++ src/components/CardNumberSegmentsInput.tsx
@@ -1,9 +1,13 @@
-import { useState, useEffect, type ChangeEvent } from 'react';
-import ValidationInput from './Common/ValidationInput';
-import { validateNumberString, validateStringLength, validateStringMaxLength } from '../utils';
-import type { CardNumberSegments } from '../types';
-import Flex from './Common/Flex';
-import Label from './Common/Label';
+import { type ChangeEvent } from "react";
+import ValidationInput from "./Common/ValidationInput";
+import {
+  validateNumberString,
+  validateStringLength,
+  validateStringMaxLength,
+} from "../utils";
+import type { CardNumberSegments } from "../types";
+import Flex from "./Common/Flex";
+import Label from "./Common/Label";
 
 interface CardNumberSegmentsInputProps {
   value: CardNumberSegments;
@@ -11,24 +15,18 @@ interface CardNumberSegmentsInputProps {
 }
 
 function CardNumberSegmentsInput(props: CardNumberSegmentsInputProps) {
-  const [segments, setSegments] = useState<CardNumberSegments>(props.value);
-
-  useEffect(() => {
-    props.onChange(segments);
-  }, [segments]);
-
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const inputIndex = Number(event.target.dataset.index);
-    const newSegments = [...segments] as CardNumberSegments;
+    const newSegments = [...props.value] as CardNumberSegments;
     newSegments.splice(inputIndex, 1, event.target.value);
-    setSegments(newSegments);
+    props.onChange(newSegments);
   };
 
   return (
     <Flex direction="column" gap={10}>
       <Label>카드 번호</Label>
       <Flex gap={10}>
-        {segments.map((el, index) => (
+        {props.value.map((el, index) => (
           <ValidationInput
             key={index}
             data-index={index}
@@ -40,19 +38,19 @@ function CardNumberSegmentsInput(props: CardNumberSegmentsInputProps) {
             isShowError={true}
             validations={[
               {
-                type: 'validateOnChange',
+                type: "validateOnChange",
                 validator: validateNumberString,
-                message: '숫자만 입력 가능합니다.',
+                message: "숫자만 입력 가능합니다.",
               },
               {
-                type: 'validateOnChange',
+                type: "validateOnChange",
                 validator: (input: string) => validateStringMaxLength(input, 4),
-                message: '4자리까지만 입력 가능합니다.',
+                message: "4자리까지만 입력 가능합니다.",
               },
               {
-                type: 'validateOnBlur',
+                type: "validateOnBlur",
                 validator: (input: string) => validateStringLength(input, 4),
-                message: '4자리를 입력해주세요.',
+                message: "4자리를 입력해주세요.",
               },
             ]}
           />
