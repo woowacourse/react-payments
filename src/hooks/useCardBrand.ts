958631b467644bc3c4f11484e1c5f1c55edf7615
janghw0126 <janghw232625@gmail.com>
2026-05-06 01:27:19 +0900

    fix: MasterCard 정규식 앵커 오류 수정 및 판별 로직 통일

--- src/hooks/useCardBrand.ts
+++ src/hooks/useCardBrand.ts
@@ -1,7 +1,6 @@
 export function useCardBrand(
-  CardNumberSegments: [string, string, string, string],
+  cardNumberSegments: [string, string, string, string],
 ) {
-  if (CardNumberSegments[0].startsWith("4")) return "VISA";
-  if (/^(51)|(52)|(53)|(54)|(55)/.test(CardNumberSegments[0]))
-    return "MasterCard";
+  if (/^4/.test(cardNumberSegments[0])) return "VISA";
+  if (/^5[1-5]/.test(cardNumberSegments[0])) return "MasterCard";
 }
