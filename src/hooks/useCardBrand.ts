47829e6e0d2a49cb8307307b2b7d023ff7433f6a
janghw0126 <janghw232625@gmail.com>
2026-05-06 01:23:14 +0900

    refactor: useMemo 제거

--- src/hooks/useCardBrand.ts
+++ src/hooks/useCardBrand.ts
@@ -1,11 +1,7 @@
-import { useMemo } from "react";
-
-export function useCardBrand(CardNumberSegments: [string, string, string, string]) {
-  const cardBrand = useMemo(() => {
-    if (CardNumberSegments[0].startsWith('4')) return 'VISA';
-    if (/^(51)|(52)|(53)|(54)|(55)/g.test(CardNumberSegments[0])) return 'MasterCard';
-    return null;
-  }, [CardNumberSegments]);
-
-  return cardBrand;
+export function useCardBrand(
+  CardNumberSegments: [string, string, string, string],
+) {
+  if (CardNumberSegments[0].startsWith("4")) return "VISA";
+  if (/^(51)|(52)|(53)|(54)|(55)/.test(CardNumberSegments[0]))
+    return "MasterCard";
 }
