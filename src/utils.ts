e3dc5e5f7a3fe6dc34f63866d2644277abd1ff0e
gustn99 <azerosong@gmail.com>
2026-05-05 22:37:01 +0900

    feat: cardNumbers[0]이 빈 문자열인 경우 명시적인 방어 로직 추가

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,19 @@
 import type { CardBrand, CardInfo } from './types';
 
 export const categorizeCardBrand = (cardNumbers: CardInfo['cardNumbers']): CardBrand => {
+  if (cardNumbers[0].length === 0) {
+    return 'local';
+  }
+
   if (cardNumbers[0].startsWith('4')) {
     return 'visa';
   }
+
   const firstTwoNumber = Number.parseInt(cardNumbers[0].slice(0, 2));
   if (firstTwoNumber >= 51 && firstTwoNumber <= 55) {
     return 'mastercard';
   }
+
   return 'local';
 };
 
