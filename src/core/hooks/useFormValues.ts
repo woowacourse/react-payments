18555e8f6ced53bc0275761283d9b79c19ed0d66
stardusty-lab <startdusty.lab@gmail.com>
2026-05-06 17:27:59 +0900

    feature(useFormValues): blur 상태 관리 추가
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/core/hooks/useFormValues.ts
+++ src/core/hooks/useFormValues.ts
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { type ChangeEvent } from 'react';
+import type { ChangeEvent, FocusEvent } from 'react';
 
 import type { ResultValid } from './validateFormValueRules';
 
@@ -8,11 +8,29 @@ interface Options<TFormValues> {
   validate: (formValues: TFormValues) => { [formKey in keyof TFormValues]: ResultValid[] };
 }
 
-export const useFormValues = <TFormValues>({ initialValues, validate }: Options<TFormValues>) => {
+export const useFormValues = <TFormValues extends Record<string, string>>({
+  initialValues,
+  validate,
+}: Options<TFormValues>) => {
   const [formValues, setFormValues] = useState<TFormValues>(initialValues);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFormValues((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const [blur, setBlur] = useState(() =>
+    Object.keys(initialValues).reduce(
+      (acc, key) => {
+        return { ...acc, [key]: false };
+      },
+      {} as {
+        [key in keyof TFormValues]: boolean;
+      },
+    ),
+  );
+
+  const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
+    setBlur((prev) => ({ ...prev, [e.target.id]: true }));
   };
 
   const errors = validate(formValues);
@@ -20,6 +38,8 @@ export const useFormValues = <TFormValues>({ initialValues, validate }: Options<
   return {
     values: formValues,
     onChange: handleChange,
+    blur,
+    onBlur: handleBlur,
     errors,
   };
 };
