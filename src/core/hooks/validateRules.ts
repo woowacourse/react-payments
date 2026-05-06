1c34c8caf14c84f0fee501de682954c5e1ffe909
stardusty-lab <startdusty.lab@gmail.com>
2026-05-06 15:51:43 +0900

    feature(validateRules): 공통 validateRules 함수
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

---
+++ src/core/hooks/validateRules.ts
@@ -0,0 +1,22 @@
+import { isRequired, isNumericString, isValidMonth, min } from '../utils/validator';
+
+interface Rule {
+  type: string;
+}
+
+export const validateRules = <T>(value: T, rules: Rule[]) => {
+  return rules.every((rule) => {
+    switch (rule.type) {
+      case 'isRequired':
+        return isRequired(value);
+      case 'isNumericString':
+        return isNumericString(value);
+      case 'isValidMonth':
+        return isValidMonth(value);
+      case 'min':
+        return min(value, 3);
+      default:
+        return true;
+    }
+  });
+};
