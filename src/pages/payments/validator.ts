21d39063bac4d2957e40f2fabd10096c3801acbf
stardusty-lab <startdusty.lab@gmail.com>
2026-05-06 16:24:30 +0900

    feature: 오류 원인까지 return 하는 validateFormValuesRules 사용하는 validateCvc로 변경
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/pages/payments/validator.ts
+++ src/pages/payments/validator.ts
@@ -2,7 +2,7 @@
 import { isNumericString, isValidMonth } from '../../core/utils/validator';
 import type { ExpirationDate } from './types';
 
-import { validateFormValuesRules } from '@/core/hooks/validateRules';
+import { validateFormValuesRules } from '@/core/hooks/validateFormValueRules';
 
 export const validateCardNumber = (cardNumber: string) => {
   if (!isNumericString(cardNumber)) return false;
@@ -29,5 +29,7 @@ export const validateCvc = ({ cvc }: { cvc: string }) => {
     cvc: [{ type: 'isRequired' }, { type: 'isNumericString' }, { type: 'min' }],
   };
 
-  return validateFormValuesRules({ cvc }, rules);
+  const valids = validateFormValuesRules({ cvc }, rules);
+
+  return valids.cvc.every((valid) => valid === true);
 };
