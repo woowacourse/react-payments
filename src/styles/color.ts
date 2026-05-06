d9838acedd482dd457882d30a1de943b8df2fd43
jebiyeon02 <jhje5595@naver.com>
2026-05-06 22:55:21 +0900

    feat: 카드 색상 지정 및 카드 브랜드 이미지 추가

---
+++ src/styles/color.ts
@@ -0,0 +1,10 @@
+export const cardColors = {
+  bc: "#F04651",
+  shinHan: "#0046FF",
+  kakao: "#FFE600",
+  hyundai: "#000000",
+  woori: "#007BC8",
+  lotte: "#ED1C24",
+  hana: "#009490",
+  kb: "#6A6056",
+};
