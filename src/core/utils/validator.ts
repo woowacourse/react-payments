cfc914c479b5ff66a952839c2f30d8bf714f4ef8
stardusty-lab <startdusty.lab@gmail.com>
2026-05-06 21:46:00 +0900

    fix: validate 원시 함수 실행시 unknown 타입을 받아서 나는 오류 isString 으로 잡기
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/core/utils/validator.ts
+++ src/core/utils/validator.ts
@@ -1,13 +1,20 @@
-export const isRequired = (str: string) => {
+export const isString = (value: unknown): value is string => {
+  return typeof value === 'string';
+};
+
+export const isRequired = (str: unknown) => {
+  if (!isString(str)) return false;
   return !!str;
 };
 
-export const isNumericString = (str: string) => {
+export const isNumericString = (str: unknown) => {
+  if (!isString(str)) return false;
   const regex = /^\d+$/;
   return regex.test(str);
 };
 
-export const isValidMonth = (month: string) => {
+export const isValidMonth = (month: unknown) => {
+  if (!isString(month)) return false;
   // ^0[1-9] : 0으로 시작하고 뒤에 1~9가 오거나 (01~09)
   // | : 또는
   // ^1[0-2] : 1로 시작하고 뒤에 0~2가 오는 경우 (10~12)
@@ -15,14 +22,17 @@ export const isValidMonth = (month: string) => {
   return regex.test(month);
 };
 
-export const length = (str: string, length: number) => {
-  return str.length === length;
+export const length = (value: unknown, length: number) => {
+  if (!isString(value)) return false;
+  return value.length === length;
 };
 
-export const minLength = (str: string, minLength: number) => {
-  return str.length >= minLength;
+export const minLength = (value: unknown, minLength: number) => {
+  if (!isString(value)) return false;
+  return value.length >= minLength;
 };
 
-export const maxLength = (str: string, maxLength: number) => {
-  return str.length <= maxLength;
+export const maxLength = (value: unknown, maxLength: number) => {
+  if (!isString(value)) return false;
+  return value.length <= maxLength;
 };
