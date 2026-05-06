208129e9cf5f65b22deb08c44d073272e9a77c87
stardusty-lab <startdusty.lab@gmail.com>
2026-05-06 16:01:20 +0900

    feature(validateRules): 매개변수로 받는 formValues를 객체로 받는 걸로 수정
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/core/hooks/validateRules.ts
+++ src/core/hooks/validateRules.ts
@@ -1,22 +1,32 @@
 import { isRequired, isNumericString, isValidMonth, min } from '../utils/validator';
 
+interface FormValuesRules {
+  [formKey: string]: Rule[];
+}
+
 interface Rule {
   type: string;
 }
 
-export const validateRules = <T extends string>(value: T, rules: Rule[]) => {
-  return rules.every((rule) => {
-    switch (rule.type) {
-      case 'isRequired':
-        return isRequired(value);
-      case 'isNumericString':
-        return isNumericString(value);
-      case 'isValidMonth':
-        return isValidMonth(value);
-      case 'min':
-        return min(value, 3);
-      default:
-        return true;
-    }
+export const validateRules = <TFormValues extends Record<string, string>>(
+  formValues: TFormValues,
+  formValuesRules: FormValuesRules,
+) => {
+  return Object.entries(formValues).every(([key, value]: [string, string]) => {
+    const rules = formValuesRules[key];
+    return rules.every((rule) => {
+      switch (rule.type) {
+        case 'isRequired':
+          return isRequired(value);
+        case 'isNumericString':
+          return isNumericString(value);
+        case 'isValidMonth':
+          return isValidMonth(value);
+        case 'min':
+          return min(value, 3);
+        default:
+          return true;
+      }
+    });
   });
 };
