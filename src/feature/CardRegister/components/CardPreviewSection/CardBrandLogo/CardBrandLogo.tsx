ffaca86831785dac1740ecb8e52fa6bc4d624973
JetProc <soonjae8297@gmail.com>
2026-05-06 18:36:16 +0900

    a11y: 카드 브랜드 로고 이미지에 alt 속성 추가
    Co-authored-by: jebiyeon02 <jebiyeon02@users.noreply.github.com>
    Co-authored-by: GamjaIsMine02 <GamjaIsMine02@users.noreply.github.com>

--- src/feature/CardRegister/components/CardPreviewSection/CardBrandLogo/CardBrandLogo.tsx
+++ src/feature/CardRegister/components/CardPreviewSection/CardBrandLogo/CardBrandLogo.tsx
@@ -7,7 +7,7 @@ type BrandName = 'visa' | 'masterCard' | null;
 const CardBrandLogo = ({brandName}: {brandName: BrandName}) => {
   if (brandName === null) return null;
 
-  return <BrandLogoImg src={brandName === 'visa' ? visaImg : masterCardImg} />;
+  return <BrandLogoImg src={brandName === 'visa' ? visaImg : masterCardImg} alt={brandName} />;
 };
 
 const BrandLogoImg = styled.img`
