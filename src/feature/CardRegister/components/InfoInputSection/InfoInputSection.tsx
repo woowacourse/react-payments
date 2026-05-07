aaa570db9ae8c0ad3e64ea1eb7848e163c5af445
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-05-08 01:32:57 +0900

    feat: 동적 UI 조건부 렌더링 로직 구현

--- src/feature/CardRegister/components/InfoInputSection/InfoInputSection.tsx
+++ src/feature/CardRegister/components/InfoInputSection/InfoInputSection.tsx
@@ -8,7 +8,14 @@ import type {
 } from '../../../../common/types/CardPreviewInfoType';
 import styled from 'styled-components';
 import FieldSection from './FieldSection';
-import { hasCardFormError } from '../../utils/cardFormValidator';
+import {
+  hasCardFormError,
+  validateCardNumber,
+  validateCvcNumber,
+  validateExpiryMonth,
+  validateExpiryYear,
+  validatePassword,
+} from '../../utils/cardFormValidator';
 import SelectCardBrandField from './SelectCardBrandField';
 import PasswordField from './PasswordField';
 import Button from '../../../../common/components/Button';
@@ -22,6 +29,7 @@ const InfoInputSection = ({
 }) => {
   const [cvcNumber, setCvcNumber] = useState('');
   const [password, setPassword] = useState('');
+  const [currentStep, setCurrentStep] = useState(0);
 
   const { cardNumbers, expiryMonth, expiryYear, cardBrandId } = cardPreviewInfo;
   const {
@@ -65,8 +73,67 @@ const InfoInputSection = ({
     console.log('제출');
   };
 
+  const hasCardNumberError = cardNumbers.some(
+    (cardNumber) => validateCardNumber(cardNumber) !== null,
+  );
+  if (currentStep === 0 && !hasCardNumberError)
+    setCurrentStep((prev) => prev + 1);
+  if (currentStep === 1 && cardBrandId !== null)
+    setCurrentStep((prev) => prev + 1);
+  if (
+    currentStep === 2 &&
+    validateExpiryMonth(expiryMonth) === null &&
+    validateExpiryYear(expiryYear) === null
+  )
+    setCurrentStep((prev) => prev + 1);
+  if (currentStep === 3 && validateCvcNumber(cvcNumber) === null)
+    setCurrentStep((prev) => prev + 1);
+  if (currentStep === 4 && validatePassword(password) === null)
+    setCurrentStep((prev) => prev + 1);
+
   return (
     <Container onSubmit={handleSubmit}>
+      {currentStep >= 4 && (
+        <FieldSection
+          title="비밀번호를 입력해 주세요"
+          description="앞의 2자리를 입력해주세요."
+        >
+          <PasswordField
+            password={password}
+            handlePasswordNumberChange={handlePasswordNumberChange}
+          />
+        </FieldSection>
+      )}
+      {currentStep >= 3 && (
+        <FieldSection title="CVC 번호를 입력해 주세요">
+          <CvcField
+            cvcNumber={cvcNumber}
+            handleCvcNumberChange={handleCvcNumberChange}
+          />
+        </FieldSection>
+      )}
+      {currentStep >= 2 && (
+        <FieldSection
+          title="카드 유효기간을 입력해 주세요"
+          description="월/년도(MMYY)를 순서대로 입력해 주세요."
+        >
+          <ExpiryField
+            expiryMonth={expiryMonth}
+            expiryYear={expiryYear}
+            handleExpiryMonthChange={handleExpiryMonthChange}
+            handleExpiryYearChange={handleExpiryYearChange}
+          />
+        </FieldSection>
+      )}
+      {currentStep >= 1 && (
+        <FieldSection
+          title="카드사를 선택해 주세요"
+          description="현재 국내 카드사만 가능합니다."
+        >
+          <SelectCardBrandField handleCardBrandChange={handleCardBrandChange} />
+        </FieldSection>
+      )}
+
       <FieldSection
         title="결제할 카드 번호를 입력해 주세요"
         description="본인 명의의 카드만 결제 가능합니다."
@@ -76,38 +143,7 @@ const InfoInputSection = ({
           handleCardNumbersChange={handleCardNumbersChange}
         />
       </FieldSection>
-      <FieldSection
-        title="카드 유효기간을 입력해 주세요"
-        description="월/년도(MMYY)를 순서대로 입력해 주세요."
-      >
-        <ExpiryField
-          expiryMonth={expiryMonth}
-          expiryYear={expiryYear}
-          handleExpiryMonthChange={handleExpiryMonthChange}
-          handleExpiryYearChange={handleExpiryYearChange}
-        />
-      </FieldSection>
-      <FieldSection title="CVC 번호를 입력해 주세요">
-        <CvcField
-          cvcNumber={cvcNumber}
-          handleCvcNumberChange={handleCvcNumberChange}
-        />
-      </FieldSection>
-      <FieldSection
-        title="카드사를 선택해 주세요"
-        description="현재 국내 카드사만 가능합니다."
-      >
-        <SelectCardBrandField handleCardBrandChange={handleCardBrandChange} />
-      </FieldSection>
-      <FieldSection
-        title="비밀번호를 입력해 주세요"
-        description="앞의 2자리를 입력해주세요."
-      >
-        <PasswordField
-          password={password}
-          handlePasswordNumberChange={handlePasswordNumberChange}
-        />
-      </FieldSection>
+
       <Button type="submit">제출</Button>
     </Container>
   );
