2fc7fd97c4e09a56c4ea5724f51647190f39afd1
jebiyeon02 <jhje5595@naver.com>
2026-05-07 21:06:15 +0900

    feat: 점진적 필드 렌더링 구현

--- src/feature/CardRegister/components/InfoInputSection/InfoInputSection.tsx
+++ src/feature/CardRegister/components/InfoInputSection/InfoInputSection.tsx
@@ -10,6 +10,8 @@ import type {
 import styled from "styled-components";
 import CardCompanySelectField from "./CardCompanySelectField/CardCompanySelectField";
 import type { CardCompanyType } from "../../../../common/types/CardCompany";
+import PasswordField from "./PasswordField/PasswordField";
+import Button from "../../../../common/components/Button/Button";
 
 const InfoInputSection = ({
   cardInfo,
@@ -21,71 +23,133 @@ const InfoInputSection = ({
   handleCardCompanyClick: (cardCompany: CardCompanyType) => void;
 }) => {
   const [cvcNumber, setCvcNumber] = useState("");
+  const [password, setPassword] = useState("");
   const [formError, setFormError] = useState({
     cardNumber: false,
     expiryDate: false,
     cvc: false,
-  });
-  const { cardNumbers, expiryMonth, expiryYear } = cardInfo;
+    password: false,
+  }); // 이부분 state말고 파생값으로 바꿔야함
+  const [formStep, setFormStep] = useState(1);
+
+  const { cardNumbers, expiryMonth, expiryYear, selectedCardCompany } =
+    cardInfo;
   const { setCardNumbers, setExpiryMonth, setExpiryYear } = cardInfoHandlers;
 
-  // const hasFormError = Object.values(formError).some((value) => value);
+  const fieldCompleteState = {
+    cardNumber: cardNumbers.every((chunk) => chunk.length === 4),
+    cardCompany: selectedCardCompany,
+    expirayDate: expiryMonth.length === 2 && expiryYear.length === 2,
+    cvc: cvcNumber.length === 3,
+    password: password.length === 2,
+  };
+
+  const isFormComplete = Object.values(fieldCompleteState).every(
+    (isCompleteFiled) => isCompleteFiled,
+  );
+
+  if (fieldCompleteState.cardNumber && formStep < 2) {
+    setFormStep((previous) => previous + 1);
+  }
+
+  if (fieldCompleteState.cardCompany && formStep < 3) {
+    setFormStep((previous) => previous + 1);
+  }
+
+  if (fieldCompleteState.expirayDate && formStep < 4) {
+    setFormStep((previous) => previous + 1);
+  }
+
+  if (fieldCompleteState.cvc && formStep < 5) {
+    setFormStep((previous) => previous + 1);
+  }
+
+  if (fieldCompleteState.password && formStep < 6) {
+    setFormStep((previous) => previous + 1);
+  }
 
   const handleFieldErrorChange = (
-    fieldName: "cardNumber" | "expiryDate" | "cvc",
+    fieldName: "cardNumber" | "expiryDate" | "cvc" | "password",
     hasError: boolean,
   ) => {
     setFormError({ ...formError, [fieldName]: hasError });
   };
 
   return (
-    <Container>
-      <InputContainer
-        title="결제할 카드 번호를 입력해 주세요"
-        description="본인 명의의 카드만 결제 가능합니다."
-      >
-        <NumberField
-          cardNumbers={cardNumbers}
-          setCardNumbers={setCardNumbers}
-          onErrorChange={(hasError) =>
-            handleFieldErrorChange("cardNumber", hasError)
-          }
-        />
-      </InputContainer>
+    <Form>
+      {formStep >= 5 && (
+        <InputContainer
+          title="비밀번호를 입력해 주세요"
+          description="앞의 2자리를 입력해주세요"
+        >
+          <PasswordField
+            password={password}
+            setPassword={setPassword}
+            onErrorChange={(hasError) =>
+              handleFieldErrorChange("password", hasError)
+            }
+          />
+        </InputContainer>
+      )}
 
-      <InputContainer
-        title="카드사를 선택해 주세요"
-        description="현재 국내 카드사만 가능합니다."
-      >
-        <CardCompanySelectField onSelect={handleCardCompanyClick} />
-      </InputContainer>
+      {formStep >= 4 && (
+        <InputContainer title="CVC 번호를 입력해 주세요">
+          <CvcField
+            cvcNumber={cvcNumber}
+            setCvcNumber={setCvcNumber}
+            onErrorChange={(hasError) =>
+              handleFieldErrorChange("cvc", hasError)
+            }
+          />
+        </InputContainer>
+      )}
 
-      <InputContainer
-        title="카드 유효기간을 입력해 주세요"
-        description="월/년도(MMYY)를 순서대로 입력해 주세요."
-      >
-        <ExpiryField
-          expiryMonth={expiryMonth}
-          expiryYear={expiryYear}
-          setExpiryMonth={setExpiryMonth}
-          setExpiryYear={setExpiryYear}
-          onErrorChange={(hasError) =>
-            handleFieldErrorChange("expiryDate", hasError)
-          }
-        />
-      </InputContainer>
-      <InputContainer title="CVC 번호를 입력해 주세요">
-        <CvcField
-          cvcNumber={cvcNumber}
-          setCvcNumber={setCvcNumber}
-          onErrorChange={(hasError) => handleFieldErrorChange("cvc", hasError)}
-        />
-      </InputContainer>
-    </Container>
+      {formStep >= 3 && (
+        <InputContainer
+          title="카드 유효기간을 입력해 주세요"
+          description="월/년도(MMYY)를 순서대로 입력해 주세요."
+        >
+          <ExpiryField
+            expiryMonth={expiryMonth}
+            expiryYear={expiryYear}
+            setExpiryMonth={setExpiryMonth}
+            setExpiryYear={setExpiryYear}
+            onErrorChange={(hasError) =>
+              handleFieldErrorChange("expiryDate", hasError)
+            }
+          />
+        </InputContainer>
+      )}
+
+      {formStep >= 2 && (
+        <InputContainer
+          title="카드사를 선택해 주세요"
+          description="현재 국내 카드사만 가능합니다."
+        >
+          <CardCompanySelectField onSelect={handleCardCompanyClick} />
+        </InputContainer>
+      )}
+
+      {formStep >= 1 && (
+        <InputContainer
+          title="결제할 카드 번호를 입력해 주세요"
+          description="본인 명의의 카드만 결제 가능합니다."
+        >
+          <NumberField
+            cardNumbers={cardNumbers}
+            setCardNumbers={setCardNumbers}
+            onErrorChange={(hasError) =>
+              handleFieldErrorChange("cardNumber", hasError)
+            }
+          />
+        </InputContainer>
+      )}
+      {isFormComplete && <Button size="full">확인</Button>}
+    </Form>
   );
 };
 
-const Container = styled.div`
+const Form = styled.form`
   display: flex;
   flex-direction: column;
   gap: 12px;
