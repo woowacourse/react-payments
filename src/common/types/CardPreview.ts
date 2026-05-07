ce16af609f76ca9859c8f61d77089ce48b167d1b
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-05-07 21:07:40 +0900

    feat: 카드 브랜드 ID 타입 추가

--- src/common/types/CardPreview.ts
+++ src/common/types/CardPreview.ts
@@ -1,1 +1,5 @@
+import type { CARD_BRANDS } from '../../feature/CardRegister/constant/CARD_BRANDS';
+
 export type CardTypeName = 'visa' | 'masterCard' | null;
+
+export type CardBrandId = (typeof CARD_BRANDS)[number]['id'];
