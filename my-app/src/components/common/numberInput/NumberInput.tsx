ae9e3a1f627ebc400d4a91d49b4915ba11dec550
binggwa <qudrhks317@gmail.com>
2026-05-07 17:58:55 +0900

    feat: 비밀번호 마스킹 타입을 위한 password type 추가

--- my-app/src/components/common/numberInput/NumberInput.tsx
+++ my-app/src/components/common/numberInput/NumberInput.tsx
@@ -8,16 +8,17 @@ interface Props {
   placeholder: string;
   isError: boolean;
   maxLength: number;
-  ref: Ref<HTMLInputElement>;
+  ref?: Ref<HTMLInputElement>;
   id: string;
+  type?: 'text' | 'password';
 }
 
-const NumberInput = ({value, onChange, onBlur, placeholder, isError, maxLength, ref, id}: Props) => {
+const NumberInput = ({value, onChange, onBlur, placeholder, isError, maxLength, ref, id, type='text'}: Props) => {
 
   return (
     <StyledInput
       id={id}
-      type="text"
+      type={type}
       inputMode="numeric"
       value={value}
       onChange={(e)=>onChange(e.target.value)}
