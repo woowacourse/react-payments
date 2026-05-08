dd18464670052f6082e9e0fbb6b7824c2b242b21
binggwa <qudrhks317@gmail.com>
2026-05-08 19:17:03 +0900

    refactor: useInputShell로 공통로직 리팩토링

--- my-app/src/components/cardNumberSection/CardNumberSection.tsx
+++ my-app/src/components/cardNumberSection/CardNumberSection.tsx
@@ -2,6 +2,7 @@ import { useId } from 'react';
 import CommonSection from '../common/commonSection/CommonSection';
 import NumberInput from '../common/numberInput/NumberInput';
 import { useCardNumber } from './useCardNumber';
+import { getCardBrand, getCardNumberArrayByBrand } from '../../utils/Validation';
 
 interface Props {
   value: string[];
@@ -10,7 +11,9 @@ interface Props {
 
 const CardNumberSection = ({ value, setValue }: Props) => {
   const cardNumberIds = useId();
-  const { format, errors, inputRefs, handleOnChange, handleOnBlur, finalErrorMessage } = useCardNumber({value, setValue});
+  const { errors, inputRefs, handleOnChange, handleOnBlur, finalErrorMessage } = useCardNumber({value, setValue});
+  const currentBrand = getCardBrand(value.join(''));
+  const format = getCardNumberArrayByBrand(currentBrand);
 
   return (
     <CommonSection
@@ -28,7 +31,7 @@ const CardNumberSection = ({ value, setValue }: Props) => {
           value={value[index] || ''}
           onChange={(v) => handleOnChange(v, index)}
           onBlur={(v) => handleOnBlur(v, index)}
-          placeholder={index === 0 ? "1234" : ""}
+          placeholder={"1234"}
           maxLength={maxLength}
           isError={errors[index]}
         />
--- my-app/src/components/cardNumberSection/useCardNumber.ts
+++ my-app/src/components/cardNumberSection/useCardNumber.ts
@@ -1,5 +1,5 @@
-import { useRef, useState } from "react";
 import { getCardBrand, getCardNumberArrayByBrand, getCardNumberError } from "../../utils/Validation";
+import { useInputShell } from "../common/commonHook/useInputShell";
 
 
 interface Props {
@@ -8,56 +8,33 @@ interface Props {
 }
 
 export const useCardNumber = ({value, setValue}: Props) => {
-  const [errors, setErrors] = useState<boolean[]>([false, false, false, false]);
-  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
-
   const currentBrand = getCardBrand(value.join(''));
-  const cardNumberArray = getCardNumberArrayByBrand(currentBrand);
-    
-  const handleOnChange = (inputValue: string, index: number) => {
-    const isNumberOnly = /^[0-9]*$/.test(inputValue);
-    if (!isNumberOnly) return false;
+  const maxLengthList = getCardNumberArrayByBrand(currentBrand);
 
-    const currentMaxLength = cardNumberArray[index];
-    const slicedValue = inputValue.slice(0, currentMaxLength);
+  return useInputShell({
+    value,
+    setValue,
+    maxLengthList,
 
-    let newValue = [...value];
-    newValue[index] = slicedValue;
+    valueUpdater: (currentValue, newValue, index) => {
+      const newArr = [...currentValue];
+      newArr[index] = newValue;
 
-    const newBrand = getCardBrand(newValue.join(''));
-    const newCardFormat = getCardNumberArrayByBrand(newBrand);
+      const newBrand = getCardBrand(newArr.join(''));
+      const newFormat = getCardNumberArrayByBrand(newBrand);
 
-    // 현재 값과 새로 입력된 값으로 인해 브랜드가 달라진다면, 배열 길이를 가변적으로 변경시키고, 바뀌지 않은 칸의 데이터는 남겨둬야 함
-    if (newValue.length !== newCardFormat.length) {
-      newValue = newCardFormat.map((_, index) => newValue[index] || '');
-    }
+      if (newArr.length !== newFormat.length) {
+        return newFormat.map((_, index) => newArr[index] || '');
+      }
 
-    setValue(newValue);
-    
-    // 각 칸의 최대길이에 마주쳤을 때만 포커스 이동
-    if (inputValue.length === newCardFormat[index] && index < newCardFormat.length - 1) {
-      inputRefs.current[index + 1]?.focus();
-    }
-  };
+      return newArr;
+    },
 
-  const handleOnBlur = (inputValue: string, index: number) => {
-    const isError = getCardNumberError(inputValue) !== '';
-    setErrors((prev) => {
-      const newErrors = [...prev];
-      newErrors[index] = isError;
-      return newErrors;
-    });
-  };
+    errorChecker: (val) => val.map((num, index) => getCardNumberError(num, maxLengthList[index]) !== ''),
 
-  const errorIndex = errors.findIndex((isError) => isError);
-  const finalErrorMessage = errorIndex !== -1 ? getCardNumberError(value[errorIndex]) : '';
-
-  return {
-    format: cardNumberArray,
-    errors,
-    inputRefs,
-    handleOnChange,
-    handleOnBlur,
-    finalErrorMessage,
-  };
+    errorMessageGenerator: (val) => {
+      const errorIndex = val.findIndex((num, index) => getCardNumberError(num, maxLengthList[index]) !== '');
+      return errorIndex !== -1 ? getCardNumberError(val[errorIndex], maxLengthList[errorIndex]) : '';
+    },
+  });
 };
