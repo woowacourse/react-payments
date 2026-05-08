1e3c0d0b90e0b021636db9dde6d59f00abe452cd
binggwa <qudrhks317@gmail.com>
2026-05-08 18:36:31 +0900

    feat: 공통로직 분리를 위한 커스텀훅 껍데기 useInputShell 추가

---
+++ my-app/src/components/common/commonHook/useInputShell.ts
@@ -0,0 +1,48 @@
+// src/hooks/useInputShell.ts
+import { useRef, useState } from "react";
+
+interface UseInputShellProps<T> {
+  value: T;
+  setValue: (value: T) => void;
+  maxLengthList: number[];
+  valueUpdater: (currentValue: T, newValue: string, index: number) => T;
+  errorChecker: (currentValue: T) => boolean[];
+  errorMessageGenerator: (currentValue: T) => string;
+}
+
+export const useInputShell = <T>({
+  value,
+  setValue,
+  maxLengthList,
+  valueUpdater,
+  errorChecker,
+  errorMessageGenerator,
+}: UseInputShellProps<T>) => {
+  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+  const [errors, setErrors] = useState<boolean[]>(Array(maxLengthList.length).fill(false));
+
+  const handleOnChange = (inputValue: string, index: number) => {
+    if (!/^[0-9]*$/.test(inputValue)) return;
+
+    const maxLength = maxLengthList[index];
+    const slicedValue = inputValue.slice(0, maxLength);
+
+    setValue(valueUpdater(value, slicedValue, index));
+
+    if (slicedValue.length === maxLength && index < maxLengthList.length - 1) {
+      inputRefs.current[index + 1]?.focus();
+    }
+  };
+
+  const handleOnBlur = () => {
+    setErrors(errorChecker(value));
+  };
+
+  return {
+    inputRefs,
+    errors,
+    handleOnChange,
+    handleOnBlur,
+    finalErrorMessage: errors.some((error) => error) ? errorMessageGenerator(value) : "",
+  };
+};
