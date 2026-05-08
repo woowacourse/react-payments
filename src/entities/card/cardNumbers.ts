4cfee20c18be0124c69578b06fcf9762d53423c8
iftype <conditionaltype@gmail.com>
2026-05-08 21:05:34 +0900

    refactor: 상수 대문자로 변경

--- src/entities/card/cardNumbers.ts
+++ src/entities/card/cardNumbers.ts
@@ -3,8 +3,8 @@ import { isNumericString } from '@/core/utils/validator';
 export const CVC_LENGTH = 3;
 
 const ERROR_MESSAGE = {
-  type: '숫자만 입력 가능합니다.',
-  length: '카드 번호를 전부 채워주세요.',
+  TYPE: '숫자만 입력 가능합니다.',
+  LENGTH: '카드 번호를 전부 채워주세요.',
 };
 
 import type { Brand } from '@/entities/card/brand';
@@ -12,12 +12,12 @@ import { BRAND_CARD_LENGTH } from './brand';
 
 // 카드번호
 export const validateCardNumber = (cardNumber: string, brand: Brand): string | undefined => {
-  if (cardNumber.length !== BRAND_CARD_LENGTH[brand]) return ERROR_MESSAGE.length;
-  if (!isNumericString(cardNumber)) return ERROR_MESSAGE.type;
+  if (cardNumber.length !== BRAND_CARD_LENGTH[brand]) return ERROR_MESSAGE.LENGTH;
+  if (!isNumericString(cardNumber)) return ERROR_MESSAGE.TYPE;
   return;
 };
 
 export const validateCardNumberFormat = (cardNumber: string): string | undefined => {
-  if (cardNumber !== '' && !isNumericString(cardNumber)) return ERROR_MESSAGE.type;
+  if (cardNumber !== '' && !isNumericString(cardNumber)) return ERROR_MESSAGE.TYPE;
   return;
 };
--- src/entities/card/cvc.ts
+++ src/entities/card/cvc.ts
@@ -3,16 +3,16 @@ import { isNumericString } from '@/core/utils/validator';
 export const CVC_LENGTH = 3;
 
 const ERROR_MESSAGE = {
-  type: '숫자만 입력 가능합니다.',
-  range: 'CVC를 전부 채워주세요.',
+  TYPE: '숫자만 입력 가능합니다.',
+  RANGE: 'CVC를 전부 채워주세요.',
 };
 
 export const validateCvcFormat = (cvc: string): string | undefined => {
-  if (cvc !== '' && !isNumericString(cvc)) return ERROR_MESSAGE.type;
+  if (cvc !== '' && !isNumericString(cvc)) return ERROR_MESSAGE.TYPE;
   return;
 };
 
 export const validateCvc = (cvc: string): string | undefined => {
-  if (cvc.length !== CVC_LENGTH) return ERROR_MESSAGE.range;
+  if (cvc.length !== CVC_LENGTH) return ERROR_MESSAGE.RANGE;
   return;
 };
