1aa74335f9c5cb0f9547e5b6b0c569ffe77a3d0c
Antoliny0919 <antoliny0919@gmail.com>
2026-05-08 09:48:54 +0900

    refactor: 카드네트워크 브랜드를 카드네트워크 클래스와 분리

--- app/src/features/card/CardNetwork.ts
+++ app/src/features/card/CardNetwork.ts
@@ -1,10 +1,13 @@
+import { Visa, MasterCard, UnionPay } from "./CardNetworkBrand";
+
+const ALL_CARD_NETWORK_BRAND = [Visa, MasterCard, UnionPay];
+
 export class CardNetwork {
-  private allNetworkBrands = [Visa, MasterCard, UnionPay];
   #name: string | undefined;
 
   constructor(cardNumber: string) {
     this.#name = undefined;
-    for (const networkBrand of this.allNetworkBrands) {
+    for (const networkBrand of ALL_CARD_NETWORK_BRAND) {
       this.#name = networkBrand.check(cardNumber);
       if (this.#name !== undefined) {
         break;
@@ -16,41 +19,3 @@ export class CardNetwork {
     return this.#name;
   }
 }
-
-const Visa = {
-  title: "visa",
-  check(value: string): string | undefined {
-    if (value.startsWith("4")) return this.title;
-  },
-};
-
-const MasterCard = {
-  title: "master",
-  check(value: string): string | undefined {
-    if (value.startsWith("5") && ["1", "2", "3", "4", "5"].includes(value[1]))
-      return this.title;
-  },
-};
-
-const UnionPay = {
-  title: "union",
-  check(value: string): string | undefined {
-    if (value.startsWith("62")) {
-      if (["624", "625", "626"].includes(value.slice(0, 3))) {
-        return this.title;
-      }
-      if (
-        6282 <= Number(value.slice(0, 4)) &&
-        Number(value.slice(0, 4)) <= 6288
-      ) {
-        return this.title;
-      }
-      if (
-        622126 <= Number(value.slice(0, 6)) &&
-        Number(value.slice(0, 6)) <= 622925
-      ) {
-        return this.title;
-      }
-    }
-  },
-};
---
+++ app/src/features/card/CardNetworkBrand.ts
@@ -0,0 +1,37 @@
+export const Visa = {
+  title: "visa",
+  check(value: string): string | undefined {
+    if (value.startsWith("4")) return this.title;
+  },
+};
+
+export const MasterCard = {
+  title: "master",
+  check(value: string): string | undefined {
+    if (value.startsWith("5") && ["1", "2", "3", "4", "5"].includes(value[1]))
+      return this.title;
+  },
+};
+
+export const UnionPay = {
+  title: "union",
+  check(value: string): string | undefined {
+    if (value.startsWith("62")) {
+      if (["624", "625", "626"].includes(value.slice(0, 3))) {
+        return this.title;
+      }
+      if (
+        6282 <= Number(value.slice(0, 4)) &&
+        Number(value.slice(0, 4)) <= 6288
+      ) {
+        return this.title;
+      }
+      if (
+        622126 <= Number(value.slice(0, 6)) &&
+        Number(value.slice(0, 6)) <= 622925
+      ) {
+        return this.title;
+      }
+    }
+  },
+};
