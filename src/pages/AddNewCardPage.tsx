5816858fc95ad84c24632da5eebcb3444255e44a
박건규 <geongyu09@gmail.com>
2026-05-09 23:50:47 +0900

    feat: 사용자의 입력이 완료되면 다음 필드가 상단에 나타나는 기능 추가

--- src/pages/AddNewCardPage.tsx
+++ src/pages/AddNewCardPage.tsx
@@ -1,11 +1,12 @@
-import Card from "@/components/common/Card";
-import CardCVCInputField from "@/components/feature/CardCVCInputField";
-import CardNumberInputField from "@/components/feature/CardNumberInputField/CardNumberInputField";
-import CardValidityPeriodInputField, {
-  type ValidityPeriod,
-} from "@/components/feature/CardValidityPeriodInputField/CardValidityPeriodInputField";
+import type CARD_COMPANY_SELECT_FIELD from "@constants/card";
+import type { CardNumberUnits, ValidityPeriod } from "@/types/card";
+import Card from "@components/common/Card";
+import StepFunnel from "@components/common/StepFunnel/StepFunnel";
+import CardCompanySelectField from "@components/feature/CardCompanySelectField/CardCompanySelectField";
+import CardCVCInputField from "@components/feature/CardCVCInputField";
+import CardNumberInputField from "@components/feature/CardNumberInputField/CardNumberInputField";
+import CardValidityPeriodInputField from "@components/feature/CardValidityPeriodInputField/CardValidityPeriodInputField";
 import styled from "@emotion/styled";
-import type { CardNumberUnits } from "@/types/card";
 import { detectCardBrand } from "@utils/card";
 import { useState } from "react";
 
@@ -16,6 +17,9 @@ const AddNewCardPage = () => {
   const [cardNumber, setCardNumber] = useState(DEFAULT_CARD_NUMBER_UNITS);
   const [validityPeriod, setValidityPeriod] = useState(DEFAULT_VALIDITY_PERIOD);
   const [CVC, setCVC] = useState("");
+  const [selectedCardCompany, setSelectedCardCompany] = useState<
+    (typeof CARD_COMPANY_SELECT_FIELD)[number]["value"] | null
+  >(null);
 
   return (
     <PageWrapper>
@@ -24,27 +28,59 @@ const AddNewCardPage = () => {
           cardNumberUnits={cardNumber}
           validityPeriod={validityPeriod}
           brand={detectCardBrand(cardNumber)}
+          company={selectedCardCompany}
         />
       </CardWrapper>
       <CardInfoForm>
-        <CardNumberInputField
-          cardNumberUnits={cardNumber}
-          onChange={(input) => {
-            setCardNumber(input);
-          }}
-        />
-        <CardValidityPeriodInputField
-          validityPeriod={validityPeriod}
-          onChange={(input) => {
-            setValidityPeriod(input);
-          }}
-        />
-        <CardCVCInputField
-          CVC={CVC}
-          onChange={(input) => {
-            setCVC(input);
-          }}
-        />
+        <StepFunnel>
+          <StepFunnel.Step step={3} comparisonOperator="greaterThanOrEqual">
+            {() => (
+              <CardCVCInputField
+                CVC={CVC}
+                onChange={(input) => {
+                  setCVC(input);
+                }}
+              />
+            )}
+          </StepFunnel.Step>
+          <StepFunnel.Step step={2} comparisonOperator="greaterThanOrEqual">
+            {({ goToStep }) => (
+              <CardValidityPeriodInputField
+                validityPeriod={validityPeriod}
+                onChange={(input) => {
+                  setValidityPeriod(input);
+                  if (input.month.length === 2 && input.year.length === 2) {
+                    goToStep((prev) => (prev < 3 ? 3 : prev));
+                  }
+                }}
+              />
+            )}
+          </StepFunnel.Step>
+          <StepFunnel.Step step={1} comparisonOperator="greaterThanOrEqual">
+            {({ goToStep }) => (
+              <CardCompanySelectField
+                selectedCompany={selectedCardCompany}
+                onChange={(company) => {
+                  setSelectedCardCompany(company);
+                  goToStep((prev) => (prev < 2 ? 2 : prev));
+                }}
+              />
+            )}
+          </StepFunnel.Step>
+          <StepFunnel.Step step={0} comparisonOperator="greaterThanOrEqual">
+            {({ goToStep }) => (
+              <CardNumberInputField
+                cardNumberUnits={cardNumber}
+                onChange={(input) => {
+                  setCardNumber(input);
+                  if (input.join("")?.length === 16) {
+                    goToStep((prev) => (prev < 1 ? 1 : prev));
+                  }
+                }}
+              />
+            )}
+          </StepFunnel.Step>
+        </StepFunnel>
       </CardInfoForm>
     </PageWrapper>
   );
