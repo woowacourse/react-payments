4335bb8c77f41ae71262625f0950f09dd3193a04
less <lejin0906@gmail.com>
2026-05-09 23:08:52 +0900

    style: theme 추가

---
+++ my-app/src/styles/theme.ts
@@ -0,0 +1,19 @@
+import { css } from "@emotion/react";
+
+export const theme = {
+  colors: {
+    blackHyundaiCard: "#000000",
+    blueShinhanCard: "#0046FF",
+    blue2WooriCard: "#007BC8",
+    blue3HanaCard: "#009490",
+    brownKukminCard: "#6A6056",
+    redLotteCard: "#ED1C24",
+    red2BCCard: "#F04651",
+    yellowKakaoBank: "#FFE600",
+    goldIcChip: "#DDCD78",
+    textCard: "#FFFFFF",
+  },
+  shadows: {
+    shadow: "3px 3px 5px 0px rgba(0, 0, 0, 0.25)",
+  },
+};
