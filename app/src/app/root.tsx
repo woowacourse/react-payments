1619d517c759cf39cb0e59365c71d28045d636ff
Antoliny0919 <antoliny0919@gmail.com>
2026-05-09 12:56:11 +0900

    fix: favicon 로드 제거

--- app/src/app/root.tsx
+++ app/src/app/root.tsx
@@ -6,7 +6,6 @@ export default function Root() {
     <html lang="ko">
       <head>
         <meta charSet="UTF-8" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Meta />
         <Links />
