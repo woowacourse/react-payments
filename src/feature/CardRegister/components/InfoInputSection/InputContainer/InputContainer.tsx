19e775b555005955a1ef965f027405392276aafb
JetProc <soonjae8297@gmail.com>
2026-05-09 19:04:31 +0900

    refactor: Description/Label/Title을 common에서 CardRegister/components로 이동

--- src/feature/CardRegister/components/InfoInputSection/InputContainer/InputContainer.tsx
+++ src/feature/CardRegister/components/InfoInputSection/InputContainer/InputContainer.tsx
@@ -1,7 +1,7 @@
 import type {ReactNode} from 'react';
-import Description from '../../../../../common/components/Description/Description';
-import Label from '../../../../../common/components/Label/Label';
-import Title from '../../../../../common/components/Title/Title';
+import Description from '../../Description/Description';
+import Label from '../../Label/Label';
+import Title from '../../Title/Title';
 import styled from 'styled-components';
 
 const InputContainer = ({
