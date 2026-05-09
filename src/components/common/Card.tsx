9efcb532c4f1e63b6db757ddf17741b66971f6ae
박건규 <geongyu09@gmail.com>
2026-05-09 23:48:47 +0900

    feat(Card.tsx): 선택한 카드사에 따라 프리뷰 카드의 색상이 변경되는 기능 추가

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,23 +1,30 @@
-import { COLOR_PALETTE } from "@/styles/colorPalette";
 import type { CardNumberUnits, ValidityPeriod } from "@/types/card";
 import masterCard from "@assets/Mastercard.png";
 import visa from "@assets/Visa.png";
 import SwitchCase from "@components/common/SwitchCase";
+import type CARD_COMPANY_SELECT_FIELD from "@constants/card";
 import styled from "@emotion/styled";
+import { COLOR_PALETTE } from "@styles/colorPalette";
 import { detectCardBrand, getFormattedValidityPeriodUnit } from "@utils/card";
 
 interface CardProps {
   cardNumberUnits: CardNumberUnits;
   validityPeriod: ValidityPeriod;
   brand?: ReturnType<typeof detectCardBrand>;
+  company?: (typeof CARD_COMPANY_SELECT_FIELD)[number]["value"] | null;
 }
 
-const Card = ({ cardNumberUnits, validityPeriod, brand }: CardProps) => {
+const Card = ({
+  cardNumberUnits,
+  validityPeriod,
+  brand,
+  company,
+}: CardProps) => {
   const CardValidityPeriodUnitString =
     getFormattedValidityPeriodUnit(validityPeriod);
 
   return (
-    <Wrapper>
+    <Wrapper company={company}>
       <ChipWrapper>
         <Chip />
         <SwitchCase
@@ -51,10 +58,13 @@ const Card = ({ cardNumberUnits, validityPeriod, brand }: CardProps) => {
   );
 };
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<Pick<CardProps, "company">>`
   width: 13rem;
   height: 8rem;
-  background-color: ${COLOR_PALETTE["BLACK-800"]};
+  background-color: ${({ company }) =>
+    company
+      ? COLOR_PALETTE[company.toUpperCase() as keyof typeof COLOR_PALETTE]
+      : COLOR_PALETTE["BLACK-800"]};
   border-radius: 0.25rem;
   box-shadow: 3px 3px 5px 0px ${COLOR_PALETTE["BLACK-900"]}40;
 `;
