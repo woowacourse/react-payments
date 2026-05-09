962ffffd75b9bcfeb70c40266aeb9e84d6c3cc69
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-05-10 00:20:37 +0900

    fix: BrowserRouter를 HashRouter로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import { createRoot } from 'react-dom/client';
 import CardRegisterPage from './feature/CardRegister/CardRegisterPage';
 import './styles/reset.css';
 import CardRegisterCompletePage from './feature/CardRegisterComplete/CardRegisterCompletePage';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/react-payments">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/register" replace />} />
         <Route path="/register" element={<CardRegisterPage />} />
         <Route path="/complete" element={<CardRegisterCompletePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
