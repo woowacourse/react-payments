2b238d64843e7ec379857f2ff30d209ce1269dc4
Antoliny0919 <antoliny0919@gmail.com>
2026-05-09 12:55:45 +0900

    feat: 카드 생성 완료 페이지 추가

--- app/src/app/routes.tsx
+++ app/src/app/routes.tsx
@@ -1,3 +1,6 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, route } from "@react-router/dev/routes";
 
-export default [index("../pages/CardNewPage.tsx")] satisfies RouteConfig;
+export default [
+  route("card", "../pages/CardNewPage.tsx"),
+  route("card/done", "../pages/CardCreateDonePage.tsx"),
+] satisfies RouteConfig;
---
+++ app/src/pages/CardCreateDonePage.tsx
@@ -0,0 +1,3 @@
+export default function CardCreateDonePage() {
+  return <div>Card Create Done</div>;
+}
