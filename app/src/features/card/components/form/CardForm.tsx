fb0d4bd1dad63c91b0b6bc0f031b76249ec96619
Antoliny0919 <antoliny0919@gmail.com>
2026-05-09 11:19:02 +0900

    feat: 카드 폼 제출 버튼 추가

--- app/src/features/card/components/form/CardForm.tsx
+++ app/src/features/card/components/form/CardForm.tsx
@@ -72,6 +72,12 @@ export function CardForm({
           setCardNumber={setCardNumber}
         />
       </CardSection>
+      <CardSubmitButton
+        type="submit"
+        disabled={progress["complete"] ? false : true}
+      >
+        확인
+      </CardSubmitButton>
     </CardFormContainer>
   );
 }
@@ -82,4 +88,18 @@ const CardFormContainer = styled.form`
   gap: 1rem;
   padding: 24px 30px 20px 30px;
   box-sizing: border-box;
+`;
+
+const CardSubmitButton = styled.button`
+  background-color: #333333;
+  padding: 20px 0;
+  color: #f3f3f3;
+  font-size: 16px;
+  font-weight: 700;
+  cursor: pointer;
+  border: none;
+  &:disabled {
+    background-color: #3333334d;
+    cursor: auto;
+  }
 `;
