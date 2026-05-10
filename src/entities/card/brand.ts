512eea7e103a9ad71ecd66864f863ddb582cb160
iftype <conditionaltype@gmail.com>
2026-05-10 20:49:10 +0900

    refactor: 분리된 브랜드를 의미단위로 통합

--- src/entities/card/brand.ts
+++ src/entities/card/brand.ts
@@ -1,26 +1,71 @@
-export type Brand = 'default' | 'visa' | 'mastercard';
+export const BRAND = {
+  VISA: 'visa',
+  MASTERCARD: 'mastercard',
+  DINERS: 'diners',
+  AMEX: 'amex',
+  UNIONPAY: 'unionpay',
+  DEFAULT: 'default',
+} as const;
 
-const BRAND_NUMBER = {
-  visa: '4',
-  mastercard: ['51', '52', '53', '54', '55'],
-  default: '',
+export type Brand = (typeof BRAND)[keyof typeof BRAND];
+
+type MatchCondition = (str: string) => boolean;
+
+interface BrandConfig {
+  format: number[];
+  length: number;
+  isMatch: MatchCondition[];
+}
+
+const isInRange = (str: string, min: number, max: number): boolean => {
+  return Number(str) >= min && Number(str) <= max;
 };
 
-export const BRAND_CARD_LENGTH = {
-  visa: 16,
-  mastercard: 16,
-  default: 16,
+export const RULES: Record<Brand, BrandConfig> = {
+  visa: {
+    format: [4, 4, 4, 4],
+    length: 16,
+    isMatch: [(str) => str.startsWith('4')],
+  },
+  mastercard: {
+    format: [4, 4, 4, 4],
+    length: 16,
+    isMatch: [(str) => isInRange(str.slice(0, 2), 51, 55)],
+  },
+  diners: {
+    format: [4, 6, 4],
+    length: 14,
+    isMatch: [(str) => str.startsWith('36')],
+  },
+  amex: {
+    format: [4, 6, 5],
+    length: 15,
+    isMatch: [(str) => str.startsWith('34'), (str) => str.startsWith('37')],
+  },
+  unionpay: {
+    format: [4, 4, 4, 4],
+    length: 16,
+    isMatch: [
+      (str) => isInRange(str.slice(0, 6), 622126, 622925),
+      (str) => isInRange(str.slice(0, 3), 624, 626),
+      (str) => isInRange(str.slice(0, 4), 6282, 6288),
+    ],
+  },
+  default: {
+    format: [4, 4, 4, 4],
+    length: 16,
+    isMatch: [],
+  },
 };
 
-export const getBrand = (cardNumberStr: string): Brand => {
-  if (cardNumberStr.startsWith(BRAND_NUMBER.visa.toString())) return 'visa';
-  if (BRAND_NUMBER.mastercard.some((card) => cardNumberStr.startsWith(card))) return 'mastercard';
+export const getBrand = (cardNumber: string): Brand => {
+  const rulesEntries = Object.entries(RULES) as [Brand, BrandConfig][];
 
-  return 'default';
-};
+  for (const [brand, config] of rulesEntries) {
+    if (config.isMatch.some((match) => match(cardNumber))) {
+      return brand;
+    }
+  }
 
-export const CARD_BRAND_FORMAT = {
-  visa: [4, 4, 4, 4],
-  mastercard: [4, 4, 4, 4],
-  default: [4, 4, 4, 4],
+  return BRAND.DEFAULT;
 };
