9d0989e306abb883b6ea6eb91d4887c3cbbe6ac9
stardusty-lab <startdusty.lab@gmail.com>
2026-05-10 19:25:59 +0900

    feature: CARD_OPTIONS 추가
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/pages/payments/constant.ts
+++ src/pages/payments/constant.ts
@@ -2,3 +2,15 @@ export const BRAND_NUMBER = {
   visa: '4',
   mastercard: ['51', '52', '53', '54', '55'],
 };
+
+export const CARD_OPTIONS = [
+  { value: '', text: '카드사를 선택해주세요' },
+  { value: 'bc', text: 'BC카드' },
+  { value: 'shinhan', text: '신한카드' },
+  { value: 'kakao', text: '카카오뱅크' },
+  { value: 'hyundai', text: '현대카드' },
+  { value: 'woori', text: '우리카드' },
+  { value: 'lotte', text: '롯데카드' },
+  { value: 'hana', text: '하나카드' },
+  { value: 'kb', text: '국만카드' },
+];
