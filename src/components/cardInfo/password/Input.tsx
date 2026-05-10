ea552ffbe0ab080aae254d0ddb4df6a446efc5e7
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-10 14:14:38 +0900

    feat: password 컴포넌트 생성

---
+++ src/components/cardInfo/password/Input.tsx
@@ -0,0 +1,64 @@
+import styled from "@emotion/styled";
+import { usePasswordContext } from "../../../context/password/PasswordContext";
+
+export default function Input() {
+  const passwordContext = usePasswordContext();
+
+  return (
+    <Wrapper>
+      <Container>
+        <InfoInput
+          type="password"
+          value={passwordContext.password}
+          placeholder="**"
+          maxLength={2}
+          onChange={(e) => passwordContext.handlePasswordChange(e.target.value)}
+          $hasError={!!passwordContext.passwordError}
+        />
+      </Container>
+
+      {passwordContext.passwordError && (
+        <Error>{passwordContext.passwordError}</Error>
+      )}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 8px;
+  flex-direction: column;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+  width: 100%;
+`;
+
+const InfoInput = styled.input<{ $hasError: boolean }>`
+  flex: 1;
+  min-width: 0;
+  height: 32px;
+  border: 1px solid rgba(172, 172, 172, 1);
+  border-radius: 6px;
+  padding: 4px;
+  ${(props) =>
+    props.$hasError &&
+    `
+    border: 1px solid red;
+  `}
+`;
+
+const Error = styled.p`
+  padding: 0;
+  margin: 0 0;
+  font-size: 9.5px;
+  font-weight: 400;
+  line-height: 100%;
+  letter-spacing: 0%;
+  vertical-align: middle;
+  color: rgba(255, 61, 61, 1);
+`;
