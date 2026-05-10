0daff31e07f3db7ab86683af1a86d98d7fc170b6
TH_97 <hood3392@gmail.com>
2026-05-10 19:34:56 +0900

    refactor: 단계 전환 로직을 useCardStep 훅으로 분리

--- src/components/cardInfo/CardInfo.tsx
+++ src/components/cardInfo/CardInfo.tsx
@@ -1,49 +1,18 @@
 import { ConfirmButton, Wrapper } from './CardInfo.styles';
 import CardNumberField from './cardNumber/CardNumberField';
 import { useCardForm } from '../useCardForm';
-import {
-  isCardBrandComplete,
-  isCardNumberComplete,
-  isCardPasswordComplete,
-  isCvcComplete,
-  isExpireDateComplete,
-} from './validator';
 import CardBrandField from './cardBrand/CardBrandField';
-import { useState } from 'react';
 import ExpireDateField from './expireDate/ExpireDateField';
 import CvcField from './cvc/CvcField';
 import CardPasswordField from './cardPassword/CardPasswordField';
+import { useCardStep } from './useCardStep';
 
 interface Props {
   cardForm: ReturnType<typeof useCardForm>;
 }
 //카드 정보를 담고있는 컴포넌트
 export default function CardInfo({ cardForm }: Props) {
-  const [step, setStep] = useState(0);
-  if (step === 0 && isCardNumberComplete(cardForm.cardNumber.value)) setStep(1);
-  if (step === 1 && isCardBrandComplete(cardForm.cardBrand.value)) setStep(2);
-  if (step === 2 && isExpireDateComplete(cardForm.expireDate.value)) setStep(3);
-  if (step === 3 && isCvcComplete(cardForm.cvc.value)) setStep(4);
-  if (
-    step === 4 &&
-    isCardNumberComplete(cardForm.cardNumber.value) &&
-    isCardBrandComplete(cardForm.cardBrand.value) &&
-    isExpireDateComplete(cardForm.expireDate.value) &&
-    isCvcComplete(cardForm.cvc.value) &&
-    isCardPasswordComplete(cardForm.cardPassword.value)
-  )
-    setStep(5);
-  if (
-    step === 5 &&
-    !(
-      isCardNumberComplete(cardForm.cardNumber.value) &&
-      isCardBrandComplete(cardForm.cardBrand.value) &&
-      isExpireDateComplete(cardForm.expireDate.value) &&
-      isCvcComplete(cardForm.cvc.value) &&
-      isCardPasswordComplete(cardForm.cardPassword.value)
-    )
-  )
-    setStep(4);
+  const step = useCardStep(cardForm);
 
   return (
     <Wrapper>
