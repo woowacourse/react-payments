29b7a335a5a62a2570d0b26c49a20124fa692c31
stardusty-lab <startdusty.lab@gmail.com>
2026-05-10 23:46:17 +0900

    feature: complete 페이지 template 추가
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

---
+++ src/pages/complete/Complete.tsx
@@ -0,0 +1,25 @@
+import { SymbolInfo } from '@/core/components/symbolInfo';
+import { Button } from '@/core/components/button';
+import { CARD_OPTIONS } from '../payments/constant';
+
+interface CompleteProps {
+  cardNumber: string;
+  card: string;
+}
+
+export const Complete = ({ cardNumber, card }: CompleteProps) => {
+  const cardOption = CARD_OPTIONS.find((option) => option.value === card);
+  return (
+    <SymbolInfo
+      full={true}
+      symbol="complete"
+      action={
+        <Button variant="primary" block>
+          확인
+        </Button>
+      }
+    >
+      {cardNumber}로 시작하는 {cardOption?.text}가 등록되었어요.
+    </SymbolInfo>
+  );
+};
