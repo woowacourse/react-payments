ce0c0d4342ab2c5cbf3aa1ccb09f8d17834cca93
Antoliny0919 <antoliny0919@gmail.com>
2026-05-11 16:02:30 +0900

    chore: react-router 빌드로 전환

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -14,6 +14,7 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
+  base: "/react-payments/",
   plugins: [reactRouter()],
   test: {
     projects: [
