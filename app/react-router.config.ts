7bec3794f66220408eb4f404de46a0368983ac38
Antoliny0919 <antoliny0919@gmail.com>
2026-05-11 16:17:09 +0900

    fix: 개발서버가 실행되지 않은 현상 수정

--- app/react-router.config.ts
+++ app/react-router.config.ts
@@ -3,5 +3,5 @@ import type { Config } from "@react-router/dev/config";
 export default {
   ssr: false,
   appDirectory: "src/app",
-  basename: "/react-payments",
+  basename: "/react-payments/",
 } satisfies Config;
