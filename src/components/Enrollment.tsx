22cec92ba1e6c7d30eebb0b778807e0c21b5cc49
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-11 22:52:08 +0900

    feat: 등록 화면 구현

---
+++ src/components/Enrollment.tsx
@@ -0,0 +1,89 @@
+import { useNavigate } from "react-router-dom";
+import { useCardBrandContext } from "../context/cardBrand/CardBrandContext";
+import { useCardNumberContext } from "../context/cardNumber/CardNumberContext";
+import styled from "@emotion/styled";
+import { FaCheckCircle } from "react-icons/fa";
+import { useCvcContext } from "../context/cvc/CvcContext";
+import { useExpireDateContext } from "../context/expireDate/ExpireDateContext";
+import { usePasswordContext } from "../context/password/PasswordContext";
+
+export default function Enrollment() {
+  const { cardNumber, resetCardNumber } = useCardNumberContext();
+  const {
+    selectedItem: { brand },
+    resetCardBrand,
+  } = useCardBrandContext();
+  const { resetCvc } = useCvcContext();
+  const { resetExpireDate } = useExpireDateContext();
+  const { resetPassword } = usePasswordContext();
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    resetCardNumber();
+    resetCardBrand();
+    resetCvc();
+    resetExpireDate();
+    resetPassword();
+    navigate("/react-payments/");
+  };
+
+  return (
+    <Wrapper>
+      <CheckIcon />
+      <Phrase>
+        <span>{cardNumber[0]}로 시작하는</span>
+        <span>{brand}가 등록되었어요.</span>
+      </Phrase>
+      <Button onClick={() => handleClick()}>확인</Button>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  background-color: white;
+  width: 376px;
+  height: 750px;
+  border-radius: 10px;
+  padding: 0 20px 20px 20px;
+  overflow-y: auto;
+  position: relative;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CheckIcon = styled(FaCheckCircle)`
+  width: 76px;
+  height: 76px;
+`;
+
+const Phrase = styled.p`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  font-weight: 700;
+  font-size: 25px;
+  line-height: 100%;
+  color: rgba(53, 60, 73, 1);
+  text-align: center;
+
+  span {
+    margin: 0px;
+  }
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 44px;
+  border: none;
+  border-radius: 5px;
+  background-color: rgba(51, 51, 51, 1);
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 100%;
+  text-align: center;
+  color: rgba(255, 255, 255, 1);
+  cursor: pointer;
+`;
