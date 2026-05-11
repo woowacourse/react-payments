aa860a8dcf828eb5e48f6e93b6ac261123c4eeb2
stardusty-lab <startdusty.lab@gmail.com>
2026-05-11 17:01:32 +0900

    feature: / 로 접속시 /payments/register 로 이동
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router';
+import { Navigate, Routes, Route } from 'react-router';
 
 import { Flow } from '@/pages/payments/register/flow';
 import { Form } from '@/pages/payments/register/form';
@@ -7,6 +7,7 @@ import { Complete } from '@/pages/payments/register/complete';
 export const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/payments/register" replace />} />
       <Route path="/payments/register" element={<Flow />}>
         <Route index element={<Form />} />
         <Route path="complete" element={<Complete />} />
