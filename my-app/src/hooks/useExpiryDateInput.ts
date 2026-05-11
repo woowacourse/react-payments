c8d41fcd0299cce073743779f0c6d03fef1f0823
less <lejin0906@gmail.com>
2026-05-12 03:36:12 +0900

    refactor: useExpiryDateInput도 useMultiFieldInput을 사용하도록 통일

--- my-app/src/hooks/useExpiryDateInput.ts
+++ my-app/src/hooks/useExpiryDateInput.ts
@@ -1,46 +1,21 @@
-import { useRef, useState } from "react";
-
 import { EXPIRY_FIELD_COUNT, EXPIRY_FIELD_MAX_LENGTH } from "../constants/cardField";
 import { validateExpiry } from "../utils/validators";
+import { useMultiFieldInput } from "./useMultiFieldInput";
 
 type UseExpiryDateInputParams = {
   onValueHandler: (values: string[]) => void;
 };
 
 export const useExpiryDateInput = ({ onValueHandler }: UseExpiryDateInputParams) => {
-  const [inputValues, setInputValues] = useState<string[]>([]);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [errorIndex, setErrorIndex] = useState<number>(-1);
-  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
-  const latestValues = useRef<string[]>([]);
-
-  const onChange = (index: number, value: string) => {
-    const newValues = [...inputValues];
-    newValues[index] = value;
-    latestValues.current = newValues;
-
-    setInputValues(newValues);
-    setErrorMessage("");
-    onValueHandler(newValues);
-
-    if (value.length === EXPIRY_FIELD_MAX_LENGTH) {
-      inputRefs.current[index + 1]?.focus();
-    }
-  };
-
-  const handleBlur = () => {
-    const { index, message } = validateExpiry(latestValues.current);
-    setErrorIndex(index);
-    setErrorMessage(message);
-  };
+  const base = useMultiFieldInput({
+    fieldCount: EXPIRY_FIELD_COUNT,
+    getMaxLength: () => EXPIRY_FIELD_MAX_LENGTH,
+    validate: validateExpiry,
+    onValueHandler,
+  });
 
   return {
-    inputValues,
-    inputRefs,
-    errorMessage,
-    errorIndex,
-    fieldCount: EXPIRY_FIELD_COUNT,
+    ...base,
     fieldMaxLength: EXPIRY_FIELD_MAX_LENGTH,
-    handlers: { onChange, handleBlur },
   };
 };
