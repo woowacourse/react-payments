522632bdd19c2a9cbf648422d3682158e29c17b4
inaemin <inaemin@gmail.com>
2026-05-11 16:57:39 +0900

    feat: PASSWORD_LENGTH, CARD_COMPANY_OPTIONS, FIELD_STEP 상수 추가

--- src/constants.ts
+++ src/constants.ts
@@ -15,3 +15,24 @@ export const EXPIRATION_PERIOD_ERROR_MESSAGES: Record<Exclude<ExpirationPeriodEr
 export const CARD_NUMBER_LENGTH_PER_INPUT = 4;
 export const PERIOD_LENGTH_PER_INPUT = 2;
 export const CVC_LENGTH = 3;
+export const PASSWORD_LENGTH = 2;
+
+export const CARD_COMPANY_OPTIONS = [
+  { label: '카드사를 선택해 주세요', value: '' },
+  { label: 'BC카드', value: 'bc' },
+  { label: '신한카드', value: 'shinhan' },
+  { label: '카카오뱅크', value: 'kakao' },
+  { label: '현대카드', value: 'hyundai' },
+  { label: '우리카드', value: 'woori' },
+  { label: '롯데카드', value: 'lotte' },
+  { label: 'NH농협카드', value: 'nh' },
+  { label: '하나카드', value: 'hana' },
+] as const;
+
+export const FIELD_STEP = {
+  cardNumbers: 0,
+  cardCompany: 1,
+  expirationPeriod: 2,
+  cvc: 3,
+  password: 4,
+};
