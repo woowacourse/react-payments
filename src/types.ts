e3fa3a3991d8dfcbafb1cb644014e0072ba09b2f
inaemin <inaemin@gmail.com>
2026-05-11 16:57:34 +0900

    feat: CardBrand amex/diners/unionpay 추가, CardCompany/password 타입 추가

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,17 @@
+import { CARD_COMPANY_OPTIONS } from './constants';
+
 export interface CardInfo {
   cardNumbers: [string, string, string, string];
   expirationPeriod: [string, string];
   cvc: string;
   cardBrand: CardBrand;
+  cardCompany: CardCompany;
+  password: string;
 }
 
-export type CardBrand = 'local' | 'visa' | 'mastercard';
+export type CardBrand = 'local' | 'visa' | 'mastercard' | 'amex' | 'diners' | 'unionpay';
+
+export type CardCompany = (typeof CARD_COMPANY_OPTIONS)[number]['value'];
 
 export type ErrorStatus = null | 'required' | 'invalidLength' | 'numberOnly';
 
