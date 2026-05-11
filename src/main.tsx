94f5890226e579a62fcd2d394aaef426a916c3fb
jebiyeon02 <jhje5595@naver.com>
2026-05-11 15:46:31 +0900

    build: ghpages 배포를 위해 hash router로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./styles/reset.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/react-payments">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
