d03ff4b51bd9e004f0f33d511107c6ff9fa0cf83
JetProc <soonjae8297@gmail.com>
2026-05-11 14:27:19 +0900

    refactor: reset.css를 common 폴더 내부로 이동

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
-import './styles/reset.css';
+import './common/styles/reset.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
