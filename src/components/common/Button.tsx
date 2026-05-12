9cef91e8732f613cb002cad837c8f51002126e65
박건규 <geongyu09@gmail.com>
2026-05-12 13:29:32 +0900

    feat(Button.tsx): disable시 opacity가 아닌 gray 색상값을 주도록 수정

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -10,14 +10,14 @@ interface ButtonProps {
 const Button = styled.button<ButtonProps>`
   width: ${(props) => props.fullWidth && "100%"};
   padding: 1.2rem 2.4rem;
-  background-color: ${COLOR_PALETTE["BLACK-800"]};
+  background-color: ${({ disabled }) =>
+    disabled ? COLOR_PALETTE.GRAY : COLOR_PALETTE["BLACK-800"]};
   color: ${COLOR_PALETTE.WHITE};
   border-radius: ${({ rounded }) => (rounded ? "5px" : "0")};
   border: none;
   font-weight: 700;
   font-style: Bold;
   font-size: 1rem;
-  opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
   cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
 `;
 
