a59bb8176d34feaeb8d28b10288841cfe559e925
stardusty-lab <startdusty.lab@gmail.com>
2026-05-12 10:55:57 +0900

    feature: CreditCard 에 card 사에 따라 컬러 처리
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/core/components/creditCard/CreditCard.tsx
+++ src/core/components/creditCard/CreditCard.tsx
@@ -9,6 +9,7 @@ type CardNumberType = string[] | undefined;
 type ExpirationDate = string[] | undefined;
 
 export interface DefaultCreditCardProps {
+  card?: keyof typeof cardColors;
   bank: Bank;
   cardBrand: BrandType;
   cardNumberList?: CardNumberType;
@@ -23,15 +24,29 @@ const BrandMap = {
   default: undefined,
 };
 
+const cardColors = {
+  bc: '#F04651',
+  shinhan: '#0046FF',
+  kakao: '#FFE600',
+  hyundai: '#000000',
+  woori: '#007BC8',
+  lotte: '#ED1C24',
+  hana: '#009490',
+  kb: '#6A6056',
+};
+
 export const CreditCard = ({
   // bank = 'default',
+  card,
   cardBrand = 'mastercard',
   cardNumberList,
   expirationDate,
 }: DefaultCreditCardProps) => {
+  const cardColor = cardColors[card as keyof typeof cardColors];
+
   return (
     <div className={styles.creditCard}>
-      <div className={styles.card}>
+      <div className={styles.card} style={{ background: cardColor }}>
         <div className={styles.brand}>{BrandMap[cardBrand]}</div>
         <div className={styles.number}>
           {cardNumberList?.map((number, index) => (
