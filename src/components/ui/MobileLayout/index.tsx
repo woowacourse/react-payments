5b7f07e54129b3dfb0b2b74ca5ea54989dbfcaa9
inaemin <inaemin@gmail.com>
2026-05-12 16:14:03 +0900

    refactor: MobileLayout padding 제거, overflow hidden으로 변경

--- src/components/ui/MobileLayout/index.tsx
+++ src/components/ui/MobileLayout/index.tsx
@@ -2,7 +2,11 @@ import { css } from '@emotion/react';
 import { Outlet } from 'react-router';
 
 export default function MobileLayout() {
-  return <div css={mobileLayout}><Outlet /></div>;
+  return (
+    <div css={mobileLayout}>
+      <Outlet />
+    </div>
+  );
 }
 
 const mobileLayout = css`
@@ -11,8 +15,6 @@ const mobileLayout = css`
   width: 376px;
   height: 700px;
   box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.2);
-  padding: 20px 30px;
-  overflow: auto;
+  overflow: hidden;
   border-radius: 20px;
-  position: relative;
 `;
