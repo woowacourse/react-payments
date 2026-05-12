ca66ad7da69a600411fc492d4ba7f1599f286872
stardusty-lab <startdusty.lab@gmail.com>
2026-05-12 13:06:30 +0900

    feature: Field 에 style 넘기기 위해 props 수정
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/core/components/field/Field.tsx
+++ src/core/components/field/Field.tsx
@@ -2,11 +2,13 @@ import cn from 'classnames';
 import styles from './Field.module.css';
 import type { FieldProps } from './types';
 
-export const Field = ({ label, children, errorMessage }: FieldProps) => {
+export const Field = ({ label, children, errorMessage, ...rest }: FieldProps) => {
   return (
     <div className={cn(styles.field)}>
       <label className={cn(styles.label)}>{label}</label>
-      <div className={cn(styles.children)}>{children}</div>
+      <div className={cn(styles.children)} {...rest}>
+        {children}
+      </div>
       <div className={cn(styles.errorMessage, errorMessage === '' && styles.hidden)}>{errorMessage}</div>
     </div>
   );
