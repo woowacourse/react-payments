03a67ab009f044586890760839c46116acc53203
kojesung <js95112345@gmail.com>
2026-05-12 12:00:06 +0900

    refactor: getCardNumberMaxLengths 함수에서 검증 함수 호출하는 방식으로 수정

--- src/utils/getCardNumberMaxLengths.ts
+++ src/utils/getCardNumberMaxLengths.ts
@@ -1,5 +1,8 @@
+import { isDinersCardNumber } from './isDinersCardNumber';
+import { isAMEXCardNumber } from './isAMEXCardNumber';
+
 export const getCardNumberMaxLengths = (firstGroup: string): number[] => {
-    if (firstGroup.startsWith('36')) return [4, 6, 4];
-    if (firstGroup.startsWith('34') || firstGroup.startsWith('37')) return [4, 6, 5];
+    if (isDinersCardNumber(firstGroup)) return [4, 6, 4];
+    if (isAMEXCardNumber(firstGroup)) return [4, 6, 5];
     return [4, 4, 4, 4];
 };
