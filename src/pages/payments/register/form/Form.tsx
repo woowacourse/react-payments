77345d2fbbf1fb302347b972119cfd0ee7297727
stardusty-lab <startdusty.lab@gmail.com>
2026-05-12 14:14:16 +0900

    feature: 카드 등록 입력 요소 ui 순서 반전
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/pages/payments/register/form/Form.tsx
+++ src/pages/payments/register/form/Form.tsx
@@ -94,51 +94,37 @@ export const Form = () => {
             expirationDate={[expirationDate.values.month, expirationDate.values.year]}
           />
 
-          <FormGroup
-            title="결제할 카드 번호를 입력해 주세요"
-            subTitle="본인 명의의 카드만 결제 가능합니다."
-            hide={!(step >= 0)}
-          >
-            <Field
-              label="카드 번호"
-              errorMessage={cardNumbers.renderErrorMessage()}
-              style={{ justifyContent: 'flex-start' }}
-            >
-              {Object.values(cardNumbers.values).map((value, index, array) => (
-                <Input
-                  ref={cardNumbers.ref}
-                  type="tel"
-                  id={String(index)}
-                  key={index}
-                  value={value as string}
-                  maxLength={array.length - 1 !== index ? 4 : (branchNumberCard?.length || 16) % 4 || 4}
-                  style={{
-                    flex: '0 0 auto',
-                    flexWrap: 'wrap',
-                    width: `calc(${((array.length - 1 !== index ? 4 : (branchNumberCard?.length || 16) % 4 || 4) * 25) / 4}% - 10px)`,
-                  }}
-                  placeholder="1234"
-                  isError={cardNumbers.renderErrorInput(String(index))}
-                  onChange={cardNumbers.onChange}
-                  onBlur={cardNumbers.onBlur}
-                />
-              ))}
-            </Field>
-          </FormGroup>
-
-          <FormGroup title="카드사를 선택해 주세요" subTitle="현재 국내 카드사만 가능합니다." hide={!(step >= 1)}>
-            <Field errorMessage={card.renderErrorMessage()}>
-              <Select
-                {...{ ref: card.ref }}
-                id="card"
-                value={card.values.card}
-                onChange={card.onChange}
-                options={CARD_OPTIONS}
-                onBlur={card.onBlur}
+          <FormGroup title="비밀번호를 입력해 주세요" subTitle="앞의 2자리를 입력해주세요" hide={!(step >= 4)}>
+            <Field label="비밀번호 앞 2자리" errorMessage={password.renderErrorMessage()}>
+              <Input
+                {...{ ref: password.ref }}
+                type="password"
+                id="password"
+                value={password.values.password}
+                maxLength={2}
+                isError={
+                  !!password.invalidAttemp.password || (password.blur.password && !password.errors.password.length)
+                }
+                onChange={password.onChange}
+                onBlur={password.onBlur}
               />
             </Field>
           </FormGroup>
-
+          <FormGroup title="CVC 번호를 입력해 주세요" hide={!(step >= 3)}>
+            <Field label="CVC" errorMessage={cvc.renderErrorMessage()}>
+              <Input
+                {...{ ref: cvc.ref }}
+                type="tel"
+                id="cvc"
+                value={cvc.values.cvc}
+                maxLength={3}
+                placeholder="123"
+                isError={!!cvc.invalidAttemp.cvc || (cvc.blur.cvc && !cvc.errors.cvc.length)}
+                onChange={cvc.onChange}
+                onBlur={cvc.onBlur}
+              />
+            </Field>
+          </FormGroup>
           <FormGroup
             title="카드 유효기간을 입력해 주세요"
             subTitle="월/년도(MMYY)를 순서대로 입력해 주세요"
@@ -175,37 +161,47 @@ export const Form = () => {
               />
             </Field>
           </FormGroup>
-
-          <FormGroup title="CVC 번호를 입력해 주세요" hide={!(step >= 3)}>
-            <Field label="CVC" errorMessage={cvc.renderErrorMessage()}>
-              <Input
-                {...{ ref: cvc.ref }}
-                type="tel"
-                id="cvc"
-                value={cvc.values.cvc}
-                maxLength={3}
-                placeholder="123"
-                isError={!!cvc.invalidAttemp.cvc || (cvc.blur.cvc && !cvc.errors.cvc.length)}
-                onChange={cvc.onChange}
-                onBlur={cvc.onBlur}
+          <FormGroup title="카드사를 선택해 주세요" subTitle="현재 국내 카드사만 가능합니다." hide={!(step >= 1)}>
+            <Field errorMessage={card.renderErrorMessage()}>
+              <Select
+                {...{ ref: card.ref }}
+                id="card"
+                value={card.values.card}
+                onChange={card.onChange}
+                options={CARD_OPTIONS}
+                onBlur={card.onBlur}
               />
             </Field>
           </FormGroup>
-
-          <FormGroup title="비밀번호를 입력해 주세요" subTitle="앞의 2자리를 입력해주세요" hide={!(step >= 4)}>
-            <Field label="비밀번호 앞 2자리" errorMessage={password.renderErrorMessage()}>
-              <Input
-                {...{ ref: password.ref }}
-                type="password"
-                id="password"
-                value={password.values.password}
-                maxLength={2}
-                isError={
-                  !!password.invalidAttemp.password || (password.blur.password && !password.errors.password.length)
-                }
-                onChange={password.onChange}
-                onBlur={password.onBlur}
-              />
+          <FormGroup
+            title="결제할 카드 번호를 입력해 주세요"
+            subTitle="본인 명의의 카드만 결제 가능합니다."
+            hide={!(step >= 0)}
+          >
+            <Field
+              label="카드 번호"
+              errorMessage={cardNumbers.renderErrorMessage()}
+              style={{ justifyContent: 'flex-start' }}
+            >
+              {Object.values(cardNumbers.values).map((value, index, array) => (
+                <Input
+                  ref={cardNumbers.ref}
+                  type="tel"
+                  id={String(index)}
+                  key={index}
+                  value={value as string}
+                  maxLength={array.length - 1 !== index ? 4 : (branchNumberCard?.length || 16) % 4 || 4}
+                  style={{
+                    flex: '0 0 auto',
+                    flexWrap: 'wrap',
+                    width: `calc(${((array.length - 1 !== index ? 4 : (branchNumberCard?.length || 16) % 4 || 4) * 25) / 4}% - 10px)`,
+                  }}
+                  placeholder="1234"
+                  isError={cardNumbers.renderErrorInput(String(index))}
+                  onChange={cardNumbers.onChange}
+                  onBlur={cardNumbers.onBlur}
+                />
+              ))}
             </Field>
           </FormGroup>
         </FullScreen.Content>
