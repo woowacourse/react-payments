adc6a4f6c866ba2e12d425b62298f5d5df997b7d
김예안 <kjan0929@naver.com>
2026-05-12 12:39:51 +0900

    feat(form): 카드 등록 후 등록 완료 페이지로 이동 구현

--- src/components/Form/PaymentForm.tsx
+++ src/components/Form/PaymentForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, SubmitEvent, useState } from 'react';
 import styled from '@emotion/styled';
 import CardPreview from '../CardPreview/CardPreview';
 import InputFieldLayout from '../Layout/InputFieldLayout';
@@ -19,6 +19,8 @@ import { convertValueFormat } from '../../utils/convert';
 import CardSelect from '../Select/CardSelect';
 import { detectCardBrand, getCardIssuerBackgroundColor } from '../../utils/cards';
 import { getCardNumbersMaxLength } from '../../utils/fields';
+import Button from '../Common/Button/Button';
+import { useNavigate } from 'react-router-dom';
 
 export type Step = 1 | 2 | 3 | 4 | 5;
 export type CardNumbersType = [string, string, string, string];
@@ -26,13 +28,25 @@ export type ExpirationDateType = { month: string; year: string };
 export type CardIssuerType = (typeof CARD_ISSUER_CONFIG)[keyof typeof CARD_ISSUER_CONFIG]['name'];
 
 export default function PaymentForm() {
-  const [step, setStep] = useState<Step>(1);
+  const navigate = useNavigate();
 
+  const [step, setStep] = useState<Step>(1);
   const [password, setPassword] = useState<string>('');
   const [cvc, setCVC] = useState<string>('');
   const [expirationDate, setExpirationDate] = useState<ExpirationDateType>({ month: '', year: '' });
   const [cardIssuer, setCardIssuer] = useState<CardIssuerType | null>(null);
   const [cardNumbers, setCardNumbers] = useState<CardNumbersType>(['', '', '', '']);
+
+  const isValid =
+    cardNumbers.every(
+      (value, index) =>
+        !cardNumbersValidator(value, getCardNumbersMaxLength(detectCardBrand(cardNumbers), index))
+          .error
+    ) &&
+    cardIssuer &&
+    Object.values(expirationDate).every((value, i) => !expirationDateValidator(value, i).error) &&
+    !cvcValidator(cvc).error &&
+    !passwordValidator(password).error;
 
   const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
@@ -81,6 +95,18 @@ export default function PaymentForm() {
       setStep(2);
   };
 
+  const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    navigate('/registration/completion', {
+      state: {
+        prefix: cardNumbers[0],
+        cardIssuer,
+      },
+      replace: true,
+    });
+  };
+
   return (
     <Container>
       <CardPreview
@@ -92,7 +118,7 @@ export default function PaymentForm() {
         backgroundColor={getCardIssuerBackgroundColor(cardIssuer)}
       />
 
-      <FormWrapper>
+      <FormWrapper onSubmit={handleSubmit}>
         {step >= 5 && (
           <InputFieldLayout
             sectionTitle={INPUT_FIELD_CONFIG['PASSWORD'].sectionTitle}
@@ -175,6 +201,8 @@ export default function PaymentForm() {
             onChanges={[0, 1, 2, 3].map(handleCardNumbersChange)}
           />
         </InputFieldLayout>
+
+        {isValid && <Button>확인</Button>}
       </FormWrapper>
     </Container>
   );
