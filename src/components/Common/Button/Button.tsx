6d879e4c259f58eb6ec12c538391f71cf6e29017
김예안 <kjan0929@naver.com>
2026-05-12 12:37:53 +0900

    feat(button): 버튼 공통 컴포넌트 구현

---
+++ src/components/Common/Button/Button.tsx
@@ -0,0 +1,20 @@
+import { ComponentProps, ReactNode } from 'react';
+import styled from '@emotion/styled';
+
+interface Props extends Omit<ComponentProps<'button'>, 'children'> {
+  children: ReactNode;
+}
+
+export default function Button({ children, ...props }: Props) {
+  return <ButtonStyled {...props}>{children}</ButtonStyled>;
+}
+
+const ButtonStyled = styled.button`
+  width: 100%;
+  height: 44px;
+  font-size: 15px;
+  font-weight: 700;
+  border-radius: 5px;
+  color: #fff;
+  background-color: #333;
+`;
