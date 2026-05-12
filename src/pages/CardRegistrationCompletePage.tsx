78c4a47633e04415a68762e05eb5f65c59bd3376
박건규 <geongyu09@gmail.com>
2026-05-12 13:18:03 +0900

    feat(CardRegistrationCompletePage.tsx): 카드 등록 완료 페이지 구현

---
+++ src/pages/CardRegistrationCompletePage.tsx
@@ -0,0 +1,47 @@
+import styled from "@emotion/styled";
+import { useSearchParams } from "react-router";
+
+import Check from "@assets/Check.png";
+import CARD from "@constants/card";
+
+const CARD_COMPANY_LABEL_MAP = Object.fromEntries(
+  CARD.COMPANY_SELECT_FIELD.map(({ value, label }) => [value, label]),
+);
+
+const CardRegistrationCompletePage = () => {
+  const [searchParams] = useSearchParams();
+  const cardNumber = searchParams.get("card-number") ?? "";
+  const cardCompany = searchParams.get("card-company") ?? "";
+
+  const cardNumberPrefix = cardNumber.slice(0, 4);
+  const cardCompanyLabel = CARD_COMPANY_LABEL_MAP[cardCompany] ?? "";
+
+  return (
+    <Wrapper>
+      <CheckIcon src={Check} alt="Check" />
+      <Description>{cardNumberPrefix}로 시작하는</Description>
+      <Description>{cardCompanyLabel}가 등록되었어요.</Description>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const CheckIcon = styled.img`
+  width: 4.75rem;
+  height: 4.75rem;
+`;
+
+const Description = styled.p`
+  font-size: 1.2rem;
+  font-weight: 700;
+  margin: 1rem 0 0 0;
+`;
+
+export default CardRegistrationCompletePage;
