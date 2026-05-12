c3522a5bf5ead24ad7f1027bc590f583eb8471ef
kojesung <js95112345@gmail.com>
2026-05-12 12:00:38 +0900

    refactor: 기존 masterCard, visaCard 검증 로직 인덱스 확인 -> startWith 메서드 활용 방식으로 수정

--- src/utils/isMasterCardNumber.ts
+++ src/utils/isMasterCardNumber.ts
@@ -1,6 +1,5 @@
-export const isMasterCardNumber = (cardNumbers: string[]) => {
-    if (cardNumbers[0].length < 2) return false;
-    if (cardNumbers[0][0] !== '5') return false;
-    const secondNumber = Number(cardNumbers[0][1]);
-    return 1 <= secondNumber && secondNumber <= 5;
+export const isMasterCardNumber = (firstGroup: string) => {
+    if (firstGroup.length < 2) return false;
+    const secondDigit = Number(firstGroup[1]);
+    return firstGroup.startsWith('5') && secondDigit >= 1 && secondDigit <= 5;
 };
--- src/utils/isVisaCardNumber.ts
+++ src/utils/isVisaCardNumber.ts
@@ -1,7 +1,3 @@
-export const isVisaCardNumber = (cardNumbers: string[]) => {
-    if (cardNumbers[0].length < 1) {
-        return false;
-    }
-    if (cardNumbers[0][0] === '4') return true;
-    return false;
+export const isVisaCardNumber = (firstGroup: string) => {
+    return firstGroup.startsWith('4');
 };
