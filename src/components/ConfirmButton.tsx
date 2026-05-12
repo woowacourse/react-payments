522998edbced7f27fc62cbdeafae447147ab99ad
kojesung <js95112345@gmail.com>
2026-05-12 13:40:41 +0900

    feat: 라우팅용 확인 버튼 컴포넌트 구현

---
+++ src/components/ConfirmButton.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
+interface ConfirmButtonProps {
+    to: string;
+    purpose: 'submit' | 'confirm';
+}
+
+export default function ConfirmButton({ to, purpose }: ConfirmButtonProps) {
+    return (
+        <ConfirmButtonStyled purpose={purpose} to={to}>
+            확인
+        </ConfirmButtonStyled>
+    );
+}
+
+const BUTTON_SIZE = {
+    submit: { width: '100%', height: '44px', borderRadius: '0px' },
+    confirm: { width: '320px', height: '44px', borderRadius: '5px' },
+};
+
+const ConfirmButtonStyled = styled(Link)<{ isRounded?: boolean; purpose: 'submit' | 'confirm' }>`
+    width: ${({ purpose }) => BUTTON_SIZE[purpose].width};
+    height: ${({ purpose }) => BUTTON_SIZE[purpose].height};
+    border-radius: ${({ purpose }) => BUTTON_SIZE[purpose].borderRadius};
+    background-color: #333333;
+    color: #f3f3f3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+`;
