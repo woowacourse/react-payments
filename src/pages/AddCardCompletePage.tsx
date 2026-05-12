55610f565d0647fbb0b8779c137e12c678e14ce2
inaemin <inaemin@gmail.com>
2026-05-12 16:17:21 +0900

    feat: AddCardCompletePage 확인 버튼 클릭 시 홈으로 이동

--- src/pages/AddCardCompletePage.tsx
+++ src/pages/AddCardCompletePage.tsx
@@ -1,7 +1,7 @@
 import Button from '../components/ui/Button';
 import CheckIcon from '../components/ui/CheckIcon';
 import { css } from '@emotion/react';
-import { useLocation, Navigate } from 'react-router';
+import { useLocation, Navigate, useNavigate } from 'react-router';
 
 interface LocationState {
   firstFourDigits: string;
@@ -9,11 +9,14 @@ interface LocationState {
 }
 
 export default function AddCardCompletePage() {
+  const navigate = useNavigate();
   const { state } = useLocation();
 
   if (!state) return <Navigate to="/" replace />;
 
   const { firstFourDigits, cardCompany } = state as LocationState;
+
+  const handleConfirm = () => navigate('/');
 
   return (
     <main css={mainStyle}>
@@ -24,7 +27,7 @@ export default function AddCardCompletePage() {
           <br />
           {cardCompany}가 등록되었어요.
         </p>
-        <Button>확인</Button>
+        <Button onClick={handleConfirm}>확인</Button>
       </div>
     </main>
   );
@@ -43,7 +46,7 @@ const contentStyle = css`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 25px;
+  gap: 12px;
 `;
 
 const completeMessageStyle = css`
