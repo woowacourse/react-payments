efa009c9e487c9dd65766467569e0525cbe5b6c3
박건규 <geongyu09@gmail.com>
2026-05-12 11:10:22 +0900

    feat(App.tsx): 라우팅 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router";
 import AddNewCardPage from "./pages/AddNewCardPage";
+import CardRegistrationCompletePage from "./pages/CardRegistrationCompletePage";
 
 const App = () => {
-  return <AddNewCardPage />;
+  return (
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+        <Route path="/" element={<AddNewCardPage />} />
+        <Route path="/complete" element={<CardRegistrationCompletePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
