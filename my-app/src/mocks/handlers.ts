c10aac399a56a1b3900e1c39d396ddf1b1f34d8f
binggwa <qudrhks317@gmail.com>
2026-05-14 17:19:40 +0900

    feat: msw 핸들러 기본 틀 작성 및 main에서 msw 연결

--- my-app/src/mocks/handlers.ts
+++ my-app/src/mocks/handlers.ts
@@ -1,11 +1,61 @@
-import { http, HttpResponse } from 'msw'
+import { http, HttpResponse } from 'msw';
 
 export const handlers = [
-    http.get('주소', () => {
-        return HttpResponse.json({
-            cardNumber: "1111111111111111",
-            expirationDate: {month: 12, year: 12},
-            cvc: "123",
-        })
-    })
-]
+  http.post('/cards', async ({ request }) => {
+    const requestDetail = await request.json();
+    const { number, expirationDate, cvc } = requestDetail;
+
+    // 카드 번호 에러 처리, 카드 브랜드 에러 처리
+    if (isCardNumberIncorrect(number) || isCardBrandIncorrect(number)) {
+      return HttpResponse.json(
+        {
+          code: 'INVALID_CARD_NUMBER',
+          message: '유효하지 않은 카드 번호입니다.',
+        },
+        { status: 400 },
+      );
+    }
+
+    // 유효기간 에러 처리
+    if (isExpirationDateIncorrect(expirationDate)) {
+      return HttpResponse.json(
+        {
+          code: 'INVALID_EXPIRATION_DATE',
+          message: '유효하지 않은 만료일입니다.',
+        },
+        { status: 400 },
+      );
+    }
+
+    // CVC 에러 처리
+    if (isCvcIncorrect(cvc)) {
+      return HttpResponse.json(
+        {
+          code: 'INVALID_CVC',
+          message: '유효하지 않은 CVC입니다.',
+        },
+        { status: 400 },
+      );
+    }
+
+    return HttpResponse.json({ id: crypto.randomUUID() }, { status: 201 });
+  }),
+
+  http.get('/cards', () => {
+    return HttpResponse.json(
+      [
+        {
+          id: '550e8400-e29b-41d4-a716-446655440000',
+          issuerCode: '31',
+          number: '411111******1111',
+          expirationDate: { month: 12, year: 12 },
+        },
+      ],
+      { status: 200 },
+    );
+  }),
+
+  http.delete('/cards/:id', () => {
+    return new HttpResponse(null, { status: 204 });
+  }),
+];
---
+++ my-app/src/mocks/initMsw.ts
@@ -0,0 +1,14 @@
+const initMsw = async () => {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./browser');
+  return worker.start({
+    serviceWorker: {
+      url: `${import.meta.env.BASE_URL}/mockServiceWorker.js`,
+    },
+  });
+};
+
+export default initMsw;
