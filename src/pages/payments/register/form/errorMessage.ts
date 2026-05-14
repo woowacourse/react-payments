0560dba0b49d0dd978b0e7e51e989fd5254abe74
stardusty-lab <startdusty.lab@gmail.com>
2026-05-15 01:58:54 +0900

    feature: renderMessage 를 위한 에러 원인 코드 별 에러 메시지
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

---
+++ src/pages/payments/register/form/errorMessage.ts
@@ -0,0 +1,18 @@
+export const errorMessages = {
+  cardNumbers: {
+    invalidAttemp: '유효현 카드번호(숫자)를 입력해주세요',
+    incomplete: '카드 번호를 전부 채워주세요',
+  },
+  card: {
+    invalidAttemp: '유효한 카드사를 선택해주세요',
+  },
+  cvc: {
+    invalidAttemp: '유효한 CVC(숫자)를 입력해주세요',
+  },
+  expirationDate: {
+    invalidAttemp: '유효햔 유효기간(숫자)을 입력해주세요',
+  },
+  password: {
+    invalidAttemp: '유효한 비밀번호를 입력해주세요',
+  },
+};
