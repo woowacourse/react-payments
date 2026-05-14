de5a4464cbe13e3030f0995180bb660a37aeb292
stardusty-lab <startdusty.lab@gmail.com>
2026-05-14 11:19:28 +0900

    refactor: Router 의 basename을 환경 변수 상수변수 사응으로 변경
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -2,10 +2,12 @@ import type { ReactNode } from 'react';
 
 import { BrowserRouter as Router } from 'react-router';
 
+import { ENV } from '@/configs/env';
+
 type Props = {
   children: ReactNode;
 };
 
 export const AppProviders = ({ children }: Props) => {
-  return <Router basename="/react-payments">{children}</Router>;
+  return <Router basename={ENV.BASE_URL}>{children}</Router>;
 };
