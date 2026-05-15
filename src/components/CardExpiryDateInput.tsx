13d7cd59194309d6908cda610d22c06aab521ef6
janghw0126 <janghw232625@gmail.com>
2026-05-16 01:50:09 +0900

    feat: CardExpiryDateInput 컴포넌트를 forwardRef로 변경하고 코드 정리

--- src/components/CardExpiryDateInput.tsx
+++ src/components/CardExpiryDateInput.tsx
@@ -1,4 +1,4 @@
-import { type ChangeEvent } from "react";
+import { forwardRef, useRef, type ChangeEvent } from "react";
 import type { CardFormState } from "../types";
 import ValidationInput from "./Common/ValidationInput";
 import Flex from "./Common/Flex";
@@ -10,40 +10,51 @@ interface CardExpiryDateInputProps {
   onChange: (value: [string, string]) => void;
 }
 
-export default function CardExpiryDateInput(props: CardExpiryDateInputProps) {
-  const handleChangeMonth = (event: ChangeEvent<HTMLInputElement>) => {
-    props.onChange([event.target.value, props.value.expiryYear]);
-  };
+const CardExpiryDateInput = forwardRef<HTMLInputElement, CardExpiryDateInputProps>(
+  function CardExpiryDateInput(props, ref) {
+    const yearRef = useRef<HTMLInputElement>(null);
 
-  const handleChangeYear = (event: ChangeEvent<HTMLInputElement>) => {
-    props.onChange([props.value.expiryMonth, event.target.value]);
-  };
+    const handleChangeMonth = (event: ChangeEvent<HTMLInputElement>) => {
+      props.onChange([event.target.value, props.value.expiryYear]);
+      if (event.target.value.length === 2) {
+        setTimeout(() => yearRef.current?.focus(), 0);
+      }
+    };
 
-  return (
-    <Flex direction="column" gap={10}>
-      <Label>유효기간</Label>
-      <Flex gap={10}>
-        <ValidationInput
-          type="text"
-          inputMode="numeric"
-          autoComplete="cc-exp-month"
-          placeholder="MM"
-          value={props.value.expiryMonth}
-          onChange={handleChangeMonth}
-          isShowError={true}
-          validations={expiryDateValidations("month")}
-        />
-        <ValidationInput
-          type="text"
-          inputMode="numeric"
-          autoComplete="cc-exp-year"
-          placeholder="YY"
-          value={props.value.expiryYear}
-          onChange={handleChangeYear}
-          isShowError={true}
-          validations={expiryDateValidations("year")}
-        />
+    const handleChangeYear = (event: ChangeEvent<HTMLInputElement>) => {
+      props.onChange([props.value.expiryMonth, event.target.value]);
+    };
+
+    return (
+      <Flex direction="column" gap={10}>
+        <Label>유효기간</Label>
+        <Flex gap={10}>
+          <ValidationInput
+            ref={ref}
+            type="text"
+            inputMode="numeric"
+            autoComplete="cc-exp-month"
+            placeholder="MM"
+            value={props.value.expiryMonth}
+            onChange={handleChangeMonth}
+            isShowError={true}
+            validations={expiryDateValidations("month")}
+          />
+          <ValidationInput
+            ref={yearRef}
+            type="text"
+            inputMode="numeric"
+            autoComplete="cc-exp-year"
+            placeholder="YY"
+            value={props.value.expiryYear}
+            onChange={handleChangeYear}
+            isShowError={true}
+            validations={expiryDateValidations("year")}
+          />
+        </Flex>
       </Flex>
-    </Flex>
-  );
-}
+    );
+  }
+);
+
+export default CardExpiryDateInput;
