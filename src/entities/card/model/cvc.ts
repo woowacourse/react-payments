9ce03ab90cf07aad5e098d43a0bfbede4c3249e2
iftype <conditionaltype@gmail.com>
2026-05-15 18:29:03 +0900

    refactor: cvc의 타입설정

--- src/entities/card/model/cvc.ts
+++ src/entities/card/model/cvc.ts
@@ -4,3 +4,11 @@ export const ERROR_MESSAGE = {
   TYPE: '숫자만 입력 가능합니다.',
   RANGE: 'CVC를 전부 채워주세요.',
 };
+
+export const getErrorCvc = (cvc: string): string | undefined => {
+  if (cvc.length !== CVC_LENGTH) return ERROR_MESSAGE.RANGE;
+  return;
+};
+export const validateCvc = (cvc: string): boolean => {
+  return getErrorCvc(cvc) !== undefined;
+};
--- src/features/registerCard/ui/fields/CvcField.tsx
+++ src/features/registerCard/ui/fields/CvcField.tsx
@@ -2,18 +2,15 @@ import { Field } from '@/core/components/field/Field';
 import { Input } from '@/core/components/input/Input';
 import { useState } from 'react';
 import { CVC_LENGTH } from '@/entities/card';
-import { getErrorCvc, validateCvc } from '@/entities/card/lib/validatorCvc';
+import { getErrorCvc, validateCvc } from '@/entities/card/model/cvc';
+import { isNumericString } from '@/core/utils/validator';
+import type { FieldControl } from './types';
 
-interface CvcFieldProps {
-  cvc: string;
-  setStepRef: (node: HTMLInputElement | null) => void;
-  handleChange: (value: string) => void;
-}
-
-export const CvcField = ({ cvc, handleChange, setStepRef }: CvcFieldProps) => {
+export const CvcField = ({ value, handleChange }: FieldControl) => {
   const [touched, setTouched] = useState<boolean>(false);
 
   const handleChangeCvc = (inputValue: string): void => {
+    if (inputValue !== '' && !isNumericString(inputValue)) return;
     handleChange(inputValue);
     setTouched(false);
   };
@@ -21,18 +18,19 @@ export const CvcField = ({ cvc, handleChange, setStepRef }: CvcFieldProps) => {
   const handleBlur = () => {
     setTouched(true);
   };
-  const error = validateCvc(cvc);
+
+  const error = validateCvc(value);
+  const errorMessgae = touched ? getErrorCvc(value) : undefined;
 
   return (
-    <Field title="CVC 번호를 입력해 주세요" label="CVC" errorMessage={getErrorCvc(cvc)}>
+    <Field title="CVC 번호를 입력해 주세요" label="CVC" errorMessage={errorMessgae}>
       <Input
-        ref={(node) => setStepRef(node)}
         type="text"
         inputMode="numeric"
-        value={cvc}
+        value={value}
         maxLength={CVC_LENGTH}
         placeholder="123"
-        isError={touched && !error}
+        isError={touched && error}
         onChange={(e) => handleChangeCvc(e.target.value)}
         onBlur={() => handleBlur()}
       />
