1cdd4a2cc265c9c5eecf8960036de7cfa568ba26
stardusty-lab <startdusty.lab@gmail.com>
2026-05-15 21:09:09 +0900

    chore: test를 위한 Provider 에 route props 받아서 처리
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- tests/utils/render.tsx
+++ tests/utils/render.tsx
@@ -1,6 +1,11 @@
 import type { ReactNode } from 'react';
 import { MemoryRouter } from 'react-router';
 
-export const TestProvider = ({ children }: { children: ReactNode }) => {
-  return <MemoryRouter>{children}</MemoryRouter>;
+interface TestProviderProps {
+  children: ReactNode;
+  route?: string;
+}
+
+export const TestProvider = ({ children, route = '/' }: TestProviderProps) => {
+  return <MemoryRouter initialEntries={[route]}>{children}</MemoryRouter>;
 };
