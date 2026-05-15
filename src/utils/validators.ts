09778fa60a5f82d5475daf8cf98372097e9f7a96
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-15 16:56:51 +0900

    refactor: 다음 단계 검증 로직에 에러 요소 추가

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -10,7 +10,10 @@ export const isNumeric = (value: string) => {
 
 export const canShowCardBrand = (cardNumberContext: CardNumberContextValue) => {
   return cardNumberContext.inputConfig.every((value, index) => {
-    return cardNumberContext.cardNumber[index].length === value.maxLength;
+    return (
+      cardNumberContext.cardNumber[index].length === value.maxLength &&
+      cardNumberContext.cardNumberError[index].length === 0
+    );
   });
 };
 
@@ -25,14 +28,19 @@ export const canShowExpireDate = (cardBrandContext: CardBrandContextValue) => {
 export const canShowCvc = (expireDateContext: ExpireDateContextValue) => {
   return (
     expireDateContext.expireDate.month.length === 2 &&
-    expireDateContext.expireDate.year.length === 2
+    expireDateContext.expireDate.year.length === 2 &&
+    expireDateContext.expireDateError.month.length === 0 &&
+    expireDateContext.expireDateError.year.length === 0
   );
 };
 
 export const canShowPassword = (cvcContext: CvcContextValue) => {
-  return cvcContext.cvc.length === 3;
+  return cvcContext.cvc.length === 3 && cvcContext.cvcError.length === 0;
 };
 
 export const canShowSendButton = (passwordContext: PasswordContextValue) => {
-  return passwordContext.password.length === 2;
+  return (
+    passwordContext.password.length === 2 &&
+    passwordContext.passwordError.length === 0
+  );
 };
