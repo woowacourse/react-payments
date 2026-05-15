d536fdc8cf83fed7b98a2a9ba870677e1f4b2f7b
iftype <conditionaltype@gmail.com>
2026-05-15 19:22:02 +0900

    refactor: 사용하지 않는 훅 정리

--- src/entities/card/lib/validatorCvc.ts
+++
@@ -1,9 +0,0 @@
-import { CVC_LENGTH, ERROR_MESSAGE } from '../model/cvc';
-
-export const getErrorCvc = (cvc: string): string | undefined => {
-  if (cvc.length !== CVC_LENGTH) return ERROR_MESSAGE.RANGE;
-  return;
-};
-export const validateCvc = (cvc: string): boolean => {
-  return getErrorCvc(cvc) !== undefined;
-};
