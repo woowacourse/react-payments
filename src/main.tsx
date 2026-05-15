4e8cc1e27a4a7078a49d7d2ffb8af78702485cda
jebiyeon02 <jhje5595@naver.com>
2026-05-15 16:41:44 +0900

    build: msw 초기 설정

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,16 @@ import "./styles/reset.css";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>,
+async function enableMocking() {
+  const { worker } = await import("./mocks/browser"); //Dynamic import하는 것이 눈에 띄였다.
+  return worker.start();
+}
+enableMocking().then(() =>
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </StrictMode>,
+  ),
 );
--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  base: "/react-payments/",
+  base: process.env.NODE_ENV === "production" ? "/react-payments/" : "/",
   plugins: [react()],
   test: {
     projects: [
