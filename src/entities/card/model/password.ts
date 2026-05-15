85417afb9aac453ab3bd6a4bfcd03650f75412ca
iftype <conditionaltype@gmail.com>
2026-05-15 18:39:13 +0900

    refactor: 패스워드 커스텀 훅 삭제

---
+++ src/entities/card/model/password.ts
@@ -0,0 +1,14 @@
+export const PASSWORD_LENGTH = 2;
+
+const ERROR_MESSAGE = {
+  RANGE: 'PASSWORD를 전부 채워주세요.',
+};
+
+export const getErrorPassword = (password: string): string | undefined => {
+  if (password.length !== PASSWORD_LENGTH) return ERROR_MESSAGE.RANGE;
+  return;
+};
+
+export const validatePassword = (password: string): boolean => {
+  return getErrorPassword(password) !== undefined;
+};
---
+++ src/features/registerCard/ui/fields/PasswordField.tsx
@@ -0,0 +1,46 @@
+import { Field } from '@/core/components/field/Field';
+import { Input } from '@/core/components/input/Input';
+import type { FieldControl } from './types';
+import { useState } from 'react';
+import { isNumericString } from '@/core/utils/validator';
+import {
+  getErrorPassword,
+  PASSWORD_LENGTH,
+  validatePassword,
+} from '@/entities/card/model/password';
+
+export const PasswordField = ({ value, handleChange }: FieldControl) => {
+  const [touched, setTouched] = useState<boolean>(false);
+
+  const handleChangePW = (inputValue: string): void => {
+    if (inputValue !== '' && !isNumericString(inputValue)) return;
+    handleChange(inputValue);
+    setTouched(false);
+  };
+
+  const handleBlur = () => {
+    setTouched(true);
+  };
+
+  const error = validatePassword(value);
+  const errorMessage = touched ? getErrorPassword(value) : undefined;
+  return (
+    <Field
+      title="비밀번호를 입력해 주세요"
+      subTitle="앞의 2자리를 입력해주세요"
+      label="비밀번호 앞 2자리"
+      errorMessage={errorMessage}
+    >
+      <Input
+        type="password"
+        inputMode="numeric"
+        value={value}
+        maxLength={PASSWORD_LENGTH}
+        placeholder="**"
+        isError={touched && error}
+        onChange={(e) => handleChangePW(e.target.value)}
+        onBlur={() => handleBlur()}
+      />
+    </Field>
+  );
+};
