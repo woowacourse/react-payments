81f35972b826738bb0448a65d5344a6256277966
jebiyeon02 <jhje5595@naver.com>
2026-05-17 03:13:06 +0900

    feat(card-list): 카드 목록 로딩화면, 카드 목록 skeleton, button skeleton 추가

--- src/feature/CardListPage/components/Loading/Loading.tsx
+++ src/feature/CardListPage/components/Loading/Loading.tsx
@@ -1,5 +1,30 @@
+import styled from "styled-components";
+import CardItemSkeleton from "./CardItemSkeleton/CardItemSkeleton";
+
 const Loading = () => {
-  return <div>로딩</div>;
+  return (
+    <LoadingLayout>
+      <CardItemSkeleton />
+      <CardItemSkeleton />
+      <CardItemSkeleton />
+      <ButtonSkeleton />
+    </LoadingLayout>
+  );
 };
 
 export default Loading;
+
+const LoadingLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 100%;
+`;
+
+const ButtonSkeleton = styled.div`
+  width: 100%;
+  height: 44px;
+  border: 1px dashed #f0f0f0;
+  border-radius: 6px;
+  background-color: #f7f7f7;
+`;
