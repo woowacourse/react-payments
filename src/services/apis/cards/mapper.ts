b1e983c2c23dfb0b405b173432cd6f57d940992a
stardusty-lab <startdusty.lab@gmail.com>
2026-05-16 23:06:26 +0900

    refactor: 정의한 서버응답 타입, 프론트 모델 타입을 mapper 함수 정의에 사용
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/services/apis/cards/mapper.ts
+++ src/services/apis/cards/mapper.ts
@@ -1,10 +1,14 @@
-export const mapCardsResponseDTOToModel = (cards) => {
+import type { GetCardsResponseDTO } from '@/services/apis/cards/dto';
+import type { Card } from '@/pages/payments/cards/list/model';
+
+export const mapCardsResponseDTOToModel = (cards: GetCardsResponseDTO): Card[] => {
   return cards.map((card) => {
+    const [month, year] = card.expirationDate.split('/');
     return {
       id: card.id,
       card: card.issuerCode,
       cardNumbers: card.number,
-      expirationDate: card.expirationDate,
+      expirationDate: { month, year },
     };
   });
 };
