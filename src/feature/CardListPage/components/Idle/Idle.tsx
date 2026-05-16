a7a45cd3b0e6a029a486ff3903347ae750d94781
jebiyeon02 <jhje5595@naver.com>
2026-05-17 01:44:40 +0900

    feat(card-list): 보유 카드가 없을때 화면 ui 구현

--- src/feature/CardListPage/components/Idle/Idle.tsx
+++ src/feature/CardListPage/components/Idle/Idle.tsx
@@ -1,5 +1,38 @@
+import styled from "styled-components";
+import NextActionGuide from "../shared/NextActionGuide/NextActionGuide";
+import BaseButton from "../../../../common/components/Button/BaseButton";
+
 const Idle = () => {
-  return <div>카드없음</div>;
+  return (
+    <IdleLayout>
+      <NextActionGuide
+        visualElement={<IdleCard />}
+        title="등록된 카드가 없습니다"
+        description="아래 버튼을 눌러 첫 카드를 등록해보세요"
+        actionButton={
+          <BaseButton onClick={() => console.log("카드 추가")} style="rounded">
+            카드 추가하기
+          </BaseButton>
+        }
+      />
+    </IdleLayout>
+  );
 };
 
 export default Idle;
+
+const IdleLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const IdleCard = styled.div`
+  width: 160px;
+  height: 100px;
+  border: 1px dashed #d9d9d9;
+  border-radius: 8px;
+  background-color: #f5f5f5;
+`;
