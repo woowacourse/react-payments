c25ca41c18626af19d5377267aa89df4564e51f3
stardusty-lab <startdusty.lab@gmail.com>
2026-05-16 22:49:43 +0900

    feature: 서버의 카드 목록 데이터를 프론트용 데이터로 mapper를 활용해 변환
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/pages/payments/cards/list/hooks/useCards.ts
+++ src/pages/payments/cards/list/hooks/useCards.ts
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 
 import { getCards } from '@/services/apis/cards/cards';
+import { mapCardsResponseDTOToModel } from '@/services/apis/cards/mapper';
 
 export const useCards = () => {
   const [cards, setCards] = useState<any>([]);
 
   useEffect(() => {
     getCards().then((cards) => {
-      setCards(cards);
+      setCards(mapCardsResponseDTOToModel(cards));
     });
   }, []);
 
