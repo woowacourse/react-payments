c1b2fde1efe8dc53d69e50c2786164716585c01a
jebiyeon02 <jhje5595@naver.com>
2026-05-16 17:51:03 +0900

    feat(card): 카드 목록 Item, 대응되는 스켈레톤 UI 추가

---
+++ src/feature/CardListPage/components/Loading/CardItemSkeleton/CardItemSkeleton.tsx
@@ -0,0 +1,5 @@
+const CardItemSkeleton = () => {
+  return <div></div>;
+};
+
+export default CardItemSkeleton;
---
+++ src/feature/CardListPage/components/Success/CardItem/CardItem.tsx
@@ -0,0 +1,5 @@
+const CardItem = () => {
+  return <div>카드 아이템</div>;
+};
+
+export default CardItem;
