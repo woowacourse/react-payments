b7e17830362b15dd44a19cb99517635e3cfcf670
jebiyeon02 <jhje5595@naver.com>
2026-05-16 20:12:29 +0900

    feat(card-company): 카드사 정보 상수화 및 타입 추가

---
+++ src/feature/CardListPage/constants.ts
@@ -0,0 +1,42 @@
+export const CARD_COMPANY = {
+  31: {
+    KOR: "BC카드",
+    ENG: "BC",
+    COLOR: "#DB4D4D",
+  },
+  41: {
+    KOR: "신한카드",
+    ENG: "SHINHAN",
+    COLOR: "#0D66C7",
+  },
+  15: {
+    KOR: "카카오뱅크",
+    ENG: "KAKAOBANK",
+    COLOR: "#FCD433",
+  },
+  61: {
+    KOR: "현대카드",
+    ENG: "HYUNDAI",
+    COLOR: "#000000",
+  },
+  W1: {
+    KOR: "우리카드",
+    ENG: "WOORI",
+    COLOR: "skyblue",
+  },
+  71: {
+    KOR: "롯데카드",
+    ENG: "LOTTE",
+    COLOR: "orange",
+  },
+  21: {
+    KOR: "하나카드",
+    ENG: "HANA",
+    COLOR: "teal",
+  },
+  11: {
+    KOR: "국민카드",
+    ENG: "KOOKMIN",
+    COLOR: "gray",
+  },
+};
---
+++ src/feature/CardListPage/types/CardCompay.ts
@@ -0,0 +1,15 @@
+export type IssuerCodeType =
+  | "31"
+  | "41"
+  | "15"
+  | "61"
+  | "W1"
+  | "71"
+  | "21"
+  | "11";
+
+export type CardCompanyType = {
+  KOR: string;
+  ENG: string;
+  COLOR: string;
+};
