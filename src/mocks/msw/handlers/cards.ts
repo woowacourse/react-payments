1b47ca24ac0e900733dd0da9db43d1edbd2cd601
stardusty-lab <startdusty.lab@gmail.com>
2026-05-16 16:01:14 +0900

    refactor: post /cards 프론트 요청 값 오류 검사 함수 로 빼기
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/mocks/msw/handlers/cards.ts
+++ src/mocks/msw/handlers/cards.ts
@@ -2,19 +2,45 @@ import { http, HttpResponse } from 'msw';
 
 import { BRAND_NUMBER } from '@/pages/payments/register/form/constant';
 
+const validateBin = (numbers: unknown) => {
+  if (typeof numbers !== 'string') return false;
+  const VALID_BIN = Object.values(BRAND_NUMBER)
+    .map((brand) => brand.startNumber)
+    .flat();
+  const isValidBin = VALID_BIN.find((bin) => {
+    return numbers?.startsWith(bin);
+  });
+  return isValidBin;
+};
+
+const validateCvc = (cvc: unknown) => {
+  const isValidCvc = cvc !== '000';
+  return isValidCvc;
+};
+
+const validateExpirationDate = (value: unknown): boolean => {
+  if (typeof value !== 'string') return false;
+
+  // MM/YY 형식 검사
+  const match = value.match(/^(\d{2})\/(\d{2})$/);
+
+  if (!match) {
+    return false;
+  }
+
+  const [, month, year] = match;
+
+  // MM 범위 검사 (01 ~ 12)
+  const monthNumber = Number(month);
+
+  return monthNumber >= 1 && monthNumber <= 12;
+};
+
 export const handlers = [
   http.post('/cards', async ({ request }) => {
     const data = await request.clone().json();
-
     // 400 - INVALID_CARD_NUMBER
-    const VALID_BIN = Object.values(BRAND_NUMBER)
-      .map((brand) => brand.startNumber)
-      .flat();
-    const isValidBin = VALID_BIN.find((bin) => {
-      if (typeof data?.numbers !== 'string') return false;
-      return data?.numbers?.startsWith(bin);
-    });
-
+    const isValidBin = validateBin(data?.numbers);
     if (!isValidBin) {
       return HttpResponse.json(
         { code: 'INVALID_CARD_NUMBER', message: '유효하지 않은 카드 번호입니다.' },
@@ -22,32 +48,15 @@ export const handlers = [
       );
     }
 
-    const isValidCvc = data?.cvc !== '000';
+    // 400 - INVALID_CARD_NUMBER
+    const isValidCvc = validateCvc(data?.cvc);
     if (!isValidCvc) {
-      // 400 - INVALID_CARD_NUMBER
       return HttpResponse.json({ code: 'INVALID_CVC', message: '유효하지 않은 CVC입니다.' }, { status: 400 });
     }
 
-    const validateExpirationDate = (value: unknown): boolean => {
-      if (typeof value !== 'string') return false;
-
-      // MM/YY 형식 검사
-      const match = value.match(/^(\d{2})\/(\d{2})$/);
-
-      if (!match) {
-        return false;
-      }
-
-      const [, month, year] = match;
-
-      // MM 범위 검사 (01 ~ 12)
-      const monthNumber = Number(month);
-
-      return monthNumber >= 1 && monthNumber <= 12;
-    };
+    // 400 - INVALID_EXPIRATION_DATE
     const isValidExpirationDate = validateExpirationDate(data?.expirationDate);
     if (!isValidExpirationDate) {
-      // 400 - INVALID_EXPIRATION_DATE
       return HttpResponse.json(
         { code: 'INVALID_EXPIRATION_DATE', message: '유효하지 않은 만료일입니다.' },
         { status: 400 },
