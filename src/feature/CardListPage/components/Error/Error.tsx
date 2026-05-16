4411a15414c4e1e5b11cd0ac7622815ec4662151
jebiyeon02 <jhje5595@naver.com>
2026-05-17 02:17:41 +0900

    feat(card-list): 카드 목록 가져오기 실패 시 화면 ui 구현

--- src/feature/CardListPage/components/Error/Error.tsx
+++ src/feature/CardListPage/components/Error/Error.tsx
@@ -1,5 +1,43 @@
+import styled from "styled-components";
+import NextActionGuide from "../shared/NextActionGuide/NextActionGuide";
+import BaseButton from "../../../../common/components/Button/BaseButton";
+
 const Error = () => {
-  return <div>에러</div>;
+  return (
+    <ErrorLayout>
+      <NextActionGuide
+        visualElement={<ErrorIcon>!</ErrorIcon>}
+        title="카드 목록을 불러올 수 없어요"
+        description="잠시 후 다시 시도해 주세요"
+        actionButton={
+          <BaseButton onClick={() => console.log("다시 시도")} style="rounded">
+            다시 시도
+          </BaseButton>
+        }
+      />
+    </ErrorLayout>
+  );
 };
 
 export default Error;
+
+const ErrorLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const ErrorIcon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 64px;
+  height: 64px;
+  border-radius: 50%;
+  background-color: #333333;
+  font-size: 32px;
+  font-weight: 700;
+  color: white;
+`;
