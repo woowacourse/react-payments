d0c1e67622308a0ae33067fcd60a2bb7a4a07543
stardusty-lab <startdusty.lab@gmail.com>
2026-05-16 15:55:07 +0900

    feature: post /cards msw handler 추가
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

---
+++ src/mocks/msw/handlers/cards.ts
@@ -0,0 +1,60 @@
+import { http, HttpResponse } from 'msw';
+
+import { BRAND_NUMBER } from '@/pages/payments/register/form/constant';
+
+export const handlers = [
+  http.post('/cards', async ({ request }) => {
+    const data = await request.clone().json();
+
+    // 400 - INVALID_CARD_NUMBER
+    const VALID_BIN = Object.values(BRAND_NUMBER)
+      .map((brand) => brand.startNumber)
+      .flat();
+    const isValidBin = VALID_BIN.find((bin) => {
+      if (typeof data?.numbers !== 'string') return false;
+      return data?.numbers?.startsWith(bin);
+    });
+
+    if (!isValidBin) {
+      return HttpResponse.json(
+        { code: 'INVALID_CARD_NUMBER', message: '유효하지 않은 카드 번호입니다.' },
+        { status: 400 },
+      );
+    }
+
+    const isValidCvc = data?.cvc !== '000';
+    if (!isValidCvc) {
+      // 400 - INVALID_CARD_NUMBER
+      return HttpResponse.json({ code: 'INVALID_CVC', message: '유효하지 않은 CVC입니다.' }, { status: 400 });
+    }
+
+    const validateExpirationDate = (value: unknown): boolean => {
+      if (typeof value !== 'string') return false;
+
+      // MM/YY 형식 검사
+      const match = value.match(/^(\d{2})\/(\d{2})$/);
+
+      if (!match) {
+        return false;
+      }
+
+      const [, month, year] = match;
+
+      // MM 범위 검사 (01 ~ 12)
+      const monthNumber = Number(month);
+
+      return monthNumber >= 1 && monthNumber <= 12;
+    };
+    const isValidExpirationDate = validateExpirationDate(data?.expirationDate);
+    if (!isValidExpirationDate) {
+      // 400 - INVALID_EXPIRATION_DATE
+      return HttpResponse.json(
+        { code: 'INVALID_EXPIRATION_DATE', message: '유효하지 않은 만료일입니다.' },
+        { status: 400 },
+      );
+    }
+
+    // 201
+    return HttpResponse.json({ id: crypto.randomUUID() }, { status: 201 });
+  }),
+];
