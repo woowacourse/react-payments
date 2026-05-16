d0b1b639e1fa8d552a69a5f84c8e9b2d3964cacb
stardusty-lab <startdusty.lab@gmail.com>
2026-05-16 21:18:34 +0900

    feauter: useCards 에서 api 호출하여 응답값을 상태값으로 저장
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/pages/payments/cards/list/hooks/useCards.ts
+++ src/pages/payments/cards/list/hooks/useCards.ts
@@ -1,5 +1,15 @@
-import { cards } from '@/mocks/data/cards';
+import { useState, useEffect } from 'react';
+
+import { getCards } from '@/services/apis/cards/cards';
 
 export const useCards = () => {
+  const [cards, setCards] = useState<any>([]);
+
+  useEffect(() => {
+    getCards().then((cards) => {
+      setCards(cards);
+    });
+  }, []);
+
   return { cards };
 };
