b3faab5214c3e24fbc94e80347894ee20b26563e
stardusty-lab <startdusty.lab@gmail.com>
2026-05-16 19:15:37 +0900

    feature: 카드 목록 페이지 template
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/pages/payments/cards/list/Cards.tsx
+++ src/pages/payments/cards/list/Cards.tsx
@@ -1,3 +1,26 @@
+import { Title } from '@/core/components/title';
+import { ContentBox } from '@/core/components/contentBox';
+import { List } from '@/core/components/list';
+import { CreditCard } from '@/core/components/creditCard';
+import { IconButton } from '@/core/components/iconButton';
+import { Button } from '@/core/components/button';
+
 export const Cards = () => {
-  return null;
+  return (
+    <ContentBox>
+      <Title>보유 카드 (3)</Title>
+      <List>
+        <List.Item
+          left={<CreditCard size="small" />}
+          right={<IconButton icon="close" />}
+          title="BC카드"
+          content="5511 **** **** 9012"
+          description="유효기간 12/28"
+        />
+      </List>
+      <Button variant="placeholder" block>
+        + 카드 추가
+      </Button>
+    </ContentBox>
+  );
 };
