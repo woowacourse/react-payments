cf579fe35d2763163321436261f88a27d53168cb
inaemin <inaemin@gmail.com>
2026-05-17 00:21:49 +0900

    refact(useAddCardForm): useCardBrand, useCardFormState, useCardFormValidation 조합으로 단순화
    - 검증 로직을 useCardFormValidation으로 분리 후 조합
    - buildCompletePageState와 반환값 조립만 담당하는 얇은 레이어로 정리
    - derived에서 불필요한 cvcLength 제거

--- src/hooks/useAddCardForm.ts
+++ src/hooks/useAddCardForm.ts
@@ -1,18 +1,10 @@
-import { useState } from 'react';
-import {
-  CARD_COMPANY_OPTIONS,
-  CARD_CVC_MAX_LENGTH,
-  CARD_TOTAL_LENGTH,
-  DEFAULT_CARD_TOTAL_LENGTH,
-  DEFAULT_CVC_LENGTH,
-  PASSWORD_LENGTH,
-  PERIOD_LENGTH_PER_INPUT,
-  RULES,
-} from '../constants';
-import type { CardBrand, CardInfo, ErrorStatus, ExpirationPeriodErrorStatus, ExpirationValidationRule } from '../types';
-import { categorizeCardBrand, validate, validateAll } from '../utils';
+import { CARD_COMPANY_OPTIONS } from '../constants';
+import type { CardInfo, ErrorStatus, ExpirationPeriodErrorStatus } from '../types';
+import useCardBrand from './useCardBrand';
+import useCardFormState from './useCardFormState';
+import useCardFormValidation from './useCardFormValidation';
 
-interface FieldState<V, E extends (ErrorStatus | ExpirationPeriodErrorStatus)[] = [ErrorStatus]> {
+export interface FieldState<V, E extends (ErrorStatus | ExpirationPeriodErrorStatus)[] = [ErrorStatus]> {
   value: V;
   errorStatuses: E;
 }
@@ -22,7 +14,7 @@ export interface FormValue {
   cardCompany: FieldState<CardInfo['cardCompany']>;
   expirationPeriod: FieldState<
     CardInfo['expirationPeriod'],
-    [ExpirationPeriodErrorStatus, ExpirationPeriodErrorStatus]
+    [ExpirationPeriodErrorStatus, ExpirationPeriodErrorStatus, ExpirationPeriodErrorStatus]
   >;
   cvc: FieldState<CardInfo['cvc']>;
   password: FieldState<CardInfo['password']>;
@@ -30,178 +22,35 @@ export interface FormValue {
 
 export type AddCardFormFieldKey = keyof FormValue;
 
-export const createInitialFormValue = (): FormValue => ({
-  cardNumbers: { value: ['', '', '', ''], errorStatuses: [null, null, null, null, null] },
-  cardCompany: { value: '', errorStatuses: [null] },
-  expirationPeriod: { value: ['', ''], errorStatuses: [null, null] },
-  cvc: { value: '', errorStatuses: [null] },
-  password: { value: '', errorStatuses: [null] },
-});
-
 type CompletePageState = {
   firstFourDigits: string;
   cardCompany: string;
 };
 
 export default function useAddCardForm() {
-  const [formValue, setFormValue] = useState<FormValue>(createInitialFormValue);
-
-  const cardBrand = categorizeCardBrand(formValue.cardNumbers.value);
-  const cvcLength = CARD_CVC_MAX_LENGTH[cardBrand] ?? DEFAULT_CVC_LENGTH;
-  const cardNumbersTotalLength = CARD_TOTAL_LENGTH[cardBrand] ?? DEFAULT_CARD_TOTAL_LENGTH;
-
-  const cardNumbersRules = [RULES.numberOnly, RULES.required, RULES.exactLength(cardNumbersTotalLength)];
-  const expirationPeriodRules: [ExpirationValidationRule[], ExpirationValidationRule[]] = [
-    [RULES.numberOnly, RULES.required, RULES.exactLength(PERIOD_LENGTH_PER_INPUT), RULES.validMonth],
-    [RULES.numberOnly, RULES.required, RULES.exactLength(PERIOD_LENGTH_PER_INPUT), RULES.validYear],
-  ];
-  const cvcRules = [RULES.numberOnly, RULES.required, RULES.exactLength(cvcLength)];
-  const cardCompanyRules = [RULES.required];
-  const passwordRules = [RULES.numberOnly, RULES.required, RULES.exactLength(PASSWORD_LENGTH)];
-
-  const updateValue = <K extends keyof FormValue>(key: K, value: FormValue[K]['value']) => {
-    setFormValue((prev) => ({
-      ...prev,
-      [key]: { ...prev[key], value },
-    }));
-  };
-
-  const updateErrors = <K extends keyof FormValue>(key: K, errorStatuses: FormValue[K]['errorStatuses']) => {
-    setFormValue((prev) => ({
-      ...prev,
-      [key]: { ...prev[key], errorStatuses },
-    }));
-  };
-
-  const validateCardNumbersOnComplete = (value: CardInfo['cardNumbers']) => {
-    const totalError = validate([RULES.exactLength(cardNumbersTotalLength)], 'onBlur', value.join(''));
-
-    setFormValue((prev) => ({
-      ...prev,
-      cardNumbers: {
-        ...prev.cardNumbers,
-        errorStatuses: [
-          ...prev.cardNumbers.errorStatuses.slice(0, 4),
-          totalError as FormValue['cardNumbers']['errorStatuses'][4],
-        ] as FormValue['cardNumbers']['errorStatuses'],
-      },
-    }));
-
-    return totalError === null;
-  };
-
-  const getExpirationPeriodErrorStatuses = (
-    value: CardInfo['expirationPeriod'],
-  ): FormValue['expirationPeriod']['errorStatuses'] => {
-    const monthError = validateAll(
-      expirationPeriodRules[0],
-      value[0],
-    ) as FormValue['expirationPeriod']['errorStatuses'][0];
-    const yearError = validateAll(
-      expirationPeriodRules[1],
-      value[1],
-    ) as FormValue['expirationPeriod']['errorStatuses'][1];
-
-    const hasIndividualError = monthError !== null || yearError !== null;
-    const combinedError = hasIndividualError
-      ? null
-      : (validate(
-          [RULES.validMonthAndYear],
-          'onBlur',
-          value.join(''),
-        ) as FormValue['expirationPeriod']['errorStatuses'][1]);
-
-    return [monthError, yearError ?? combinedError];
-  };
-
-  const validateExpirationPeriodOnComplete = (value: CardInfo['expirationPeriod']) => {
-    const combinedError = validate(
-      [RULES.validMonthAndYear],
-      'onBlur',
-      value.join(''),
-    ) as FormValue['expirationPeriod']['errorStatuses'][1];
-
-    updateErrors('expirationPeriod', [null, combinedError]);
-    return combinedError === null;
-  };
-
-  const validateCvcOnComplete = (value: CardInfo['cvc']) => {
-    const errorStatus = validate([RULES.exactLength(cvcLength)], 'onBlur', value) as FormValue['cvc']['errorStatuses'][0];
-    updateErrors('cvc', [errorStatus]);
-    return errorStatus === null;
-  };
-
-  const validateCardCompanyOnComplete = (value: CardInfo['cardCompany']) => {
-    const errorStatus = validateAll(cardCompanyRules, value) as FormValue['cardCompany']['errorStatuses'][0];
-    updateErrors('cardCompany', [errorStatus]);
-    return errorStatus === null;
-  };
-
-  const validateAllFields = () => {
-    let isValid = false;
-
-    setFormValue((prev) => {
-      const cardNumbersErrorStatuses = [
-        ...prev.cardNumbers.value.map((fieldValue) => validateAll([RULES.numberOnly, RULES.required], fieldValue)),
-        validateAll([RULES.exactLength(cardNumbersTotalLength)], prev.cardNumbers.value.join('')),
-      ] as FormValue['cardNumbers']['errorStatuses'];
-
-      const cardCompanyErrorStatuses = [
-        validateAll(cardCompanyRules, prev.cardCompany.value),
-      ] as FormValue['cardCompany']['errorStatuses'];
-
-      const expirationPeriodErrorStatuses = getExpirationPeriodErrorStatuses(prev.expirationPeriod.value);
-
-      const cvcErrorStatuses = [validateAll(cvcRules, prev.cvc.value)] as FormValue['cvc']['errorStatuses'];
-
-      const passwordErrorStatuses = [
-        validateAll(passwordRules, prev.password.value),
-      ] as FormValue['password']['errorStatuses'];
-
-      isValid = [
-        ...cardNumbersErrorStatuses,
-        ...cardCompanyErrorStatuses,
-        ...expirationPeriodErrorStatuses,
-        ...cvcErrorStatuses,
-        ...passwordErrorStatuses,
-      ].every((errorStatus) => errorStatus === null);
-
-      return {
-        cardNumbers: { ...prev.cardNumbers, errorStatuses: cardNumbersErrorStatuses },
-        cardCompany: { ...prev.cardCompany, errorStatuses: cardCompanyErrorStatuses },
-        expirationPeriod: { ...prev.expirationPeriod, errorStatuses: expirationPeriodErrorStatuses },
-        cvc: { ...prev.cvc, errorStatuses: cvcErrorStatuses },
-        password: { ...prev.password, errorStatuses: passwordErrorStatuses },
-      };
-    });
-
-    return isValid;
-  };
+  const { formValue, setFormValue, updateValue, updateErrors, areAllFieldErrorsClear } = useCardFormState();
+  const { cardBrand, cvcLength, cardNumbersTotalLength } = useCardBrand(formValue.cardNumbers.value);
+  const {
+    rules,
+    validateCardNumbersOnComplete,
+    validateExpirationPeriodOnComplete,
+    validateCvcOnComplete,
+    validateCardCompanyOnComplete,
+    validateAllFields,
+  } = useCardFormValidation({ formValue, setFormValue, updateErrors, cardNumbersTotalLength, cvcLength });
 
   const buildCompletePageState = (): CompletePageState => ({
     firstFourDigits: formValue.cardNumbers.value[0],
     cardCompany: CARD_COMPANY_OPTIONS.find((option) => option.value === formValue.cardCompany.value)?.label ?? '카드',
   });
 
-  const areAllFieldErrorsClear = Object.values(formValue).every((field) =>
-    field.errorStatuses.every((status: ErrorStatus | ExpirationPeriodErrorStatus) => status === null),
-  );
-
   return {
     formValue,
     derived: {
-      cardBrand: cardBrand as CardBrand,
-      cvcLength,
-      cardNumbersTotalLength,
+      cardBrand,
       areAllFieldErrorsClear,
-      rules: {
-        cardNumbers: cardNumbersRules,
-        cardCompany: cardCompanyRules,
-        expirationPeriod: expirationPeriodRules,
-        cvc: cvcRules,
-        password: passwordRules,
-      },
     },
+    rules,
     actions: {
       updateValue,
       updateErrors,
