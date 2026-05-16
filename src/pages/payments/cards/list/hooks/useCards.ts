5cae35e5914907c498c59527bbb6d880d72711a3
stardusty-lab <startdusty.lab@gmail.com>
2026-05-16 23:10:32 +0900

    refactor: 카드 목록 상태값 타입 정의
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/pages/payments/cards/list/hooks/useCards.ts
+++ src/pages/payments/cards/list/hooks/useCards.ts
@@ -2,9 +2,10 @@ import { useState, useEffect } from 'react';
 
 import { getCards } from '@/services/apis/cards/cards';
 import { mapCardsResponseDTOToModel } from '@/services/apis/cards/mapper';
+import type { Card } from '@/pages/payments/cards/list/model';
 
 export const useCards = () => {
-  const [cards, setCards] = useState<any>([]);
+  const [cards, setCards] = useState<Card[]>([]);
 
   useEffect(() => {
     getCards().then((cards) => {
