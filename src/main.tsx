aa474c5a993638d8b3b0c02721b43aa1e0501290
boyeon <boyeon6331@gmail.com>
2026-05-17 21:20:31 +0900

    feat: `success` + 목록이 비어 있는경우 해당 UI 표시

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const renderApp = () => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+};
+
+async function main() {
+  try {
+    const { worker } = await import("./mocks/browser");
+
+    await worker.start({
+      serviceWorker: {
+        url: `${import.meta.env.BASE_URL}mockServiceWorker.js`,
+      },
+      onUnhandledRequest: "bypass",
+    });
+  } catch (error) {
+    console.error("MSW 시작에 실패했습니다.", error);
+  }
+
+  renderApp();
+}
+
+main();
--- src/pages/CardListPage.tsx
+++ src/pages/CardListPage.tsx
@@ -1,14 +1,36 @@
+import { getCards, type CardListResponse } from "@/api/cards";
 import CardListEmpty from "@/components/CardList/CardListEmpty/CardListEmpty";
 import PageLayout from "@/components/common/PageLayout";
 import { COLOR_PALETTE } from "@/styles/colorPalette";
 import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
+
+type CardListStatus = "idle" | "loading" | "success" | "error";
 
 const CardListPage = () => {
+  const [cards, setCards] = useState<CardListResponse>([]);
+  const [status, setStatus] = useState<CardListStatus>("idle");
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const cards = await getCards();
+
+        setCards(cards);
+        setStatus("success");
+      } catch {
+        setStatus("error");
+      }
+    };
+
+    fetchCards();
+  }, []);
+
   return (
     <PageLayout>
       <PageWrapper>
         <PageTitle>보유 카드</PageTitle>
-        <CardListEmpty />
+        {status === "success" && cards.length === 0 && <CardListEmpty />}
       </PageWrapper>
     </PageLayout>
   );
