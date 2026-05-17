bd222ceaf55a8a10397e7f112aa05561a7ee3f37
김상윤 <ksy151020@gmail.com>
2026-05-17 22:41:50 +0900

    feat: AddCardButton 구현

---
+++ src/components/CardList/AddCardButton.tsx
@@ -0,0 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import styled from "@emotion/styled";
+export default function AddCardButton() {
+  const navigate = useNavigate();
+  return <Button onClick={() => navigate("/")}>+카드 추가</Button>;
+}
+
+const Button = styled.button`
+  height: 40px;
+  border-radius: 5px;
+  border: 1px;
+  padding: 12px 16px;
+  font-size: 13px;
+  font-family: sans-serif;
+`;
