31d25f79000eebc9c15f3100a6354ac009bbb481
stardusty-lab <startdusty.lab@gmail.com>
2026-05-17 18:33:01 +0900

    feature: 서버 데이터 issuerCode 를 프론트 모델 card(ex) BC) 로 변환하여 mapping 처리
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/services/apis/cards/mapper.ts
+++ src/services/apis/cards/mapper.ts
@@ -1,12 +1,16 @@
 import type { GetCardsResponseDTO } from '@/services/apis/cards/dto';
 import type { Card } from '@/pages/payments/cards/list/model';
 
+import { ISSUER_CODE } from '@/pages/payments/cards/list/constants';
+
 export const mapCardsResponseDTOToModel = (cards: GetCardsResponseDTO): Card[] => {
   return cards.map((card) => {
     const [month, year] = card.expirationDate.split('/');
+    const issuerCodeCard = ISSUER_CODE?.[card.issuerCode as keyof typeof ISSUER_CODE]?.card;
+
     return {
       id: card.id,
-      card: card.issuerCode,
+      card: issuerCodeCard,
       cardNumbers: card.number,
       expirationDate: { month, year },
     };
