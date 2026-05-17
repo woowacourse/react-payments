965e966642823ab5522fb616255859372bed3a92
stardusty-lab <startdusty.lab@gmail.com>
2026-05-17 23:57:43 +0900

    refactor: AppRoutes 에서 ROUTES 상수 변수 사용
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,7 @@
 import { Navigate, Routes, Route } from 'react-router';
 
+import { ROUTES } from '@/constants/routes';
+
 import { Flow } from '@/pages/payments/register/flow';
 import { Form } from '@/pages/payments/register/form';
 import { Complete } from '@/pages/payments/register/complete';
@@ -9,12 +11,12 @@ import { Cards } from '@/pages/payments/cards';
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/payments/register" replace />} />
-      <Route path="/payments/register" element={<Flow />}>
+      <Route path={ROUTES.HOME} element={<Navigate to={ROUTES.PAYMENTS.REGISTER} replace />} />
+      <Route path={ROUTES.PAYMENTS.REGISTER} element={<Flow />}>
         <Route index element={<Form />} />
         <Route path="complete" element={<Complete />} />
       </Route>
-      <Route path="/payments/cards" element={<Cards />}></Route>
+      <Route path={ROUTES.PAYMENTS.CARDS} element={<Cards />}></Route>
     </Routes>
   );
 };
