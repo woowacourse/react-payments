8f4fdfbf420c38371701d3ddf3b6def448238bfb
jebiyeon02 <jhje5595@naver.com>
2026-05-17 18:07:19 +0900

    test: MSW가 db와 유사하게 작동하도록 설정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,25 +1,27 @@
 import { http, HttpResponse } from "msw";
 import { BASE_URL } from "../constants";
+import type { PostCardRequestBody } from "../feature/CardRegister/api/card";
+import type { CardItemInformation } from "../feature/CardListPage/components/Success/CardItem/CardItem";
+
+const cards: CardItemInformation[] = [];
 
 const getCardListHandler = http.get(`${BASE_URL}/cards`, () => {
-  return HttpResponse.json([
-    {
-      id: "550e8400-e29b-41d4-a716-446655440000",
-      issuerCode: "31",
-      number: "551112******9012",
-      expirationDate: "12/28",
-    },
-  ]);
+  return HttpResponse.json(cards);
 });
 
 const postCardHandler = http.post(`${BASE_URL}/cards`, async ({ request }) => {
-  // const body = await request.json();
+  const body = (await request.json()) as PostCardRequestBody;
+  const id = crypto.randomUUID().toString();
+
+  const card: CardItemInformation = {
+    id,
+    ...body,
+  };
+
+  cards.push(card);
   // todo
   // 검증 로직에 따라 다른 에러처리 필요
-  return HttpResponse.json(
-    { id: "550e8400-e29b-41d4-a716-446655440000" },
-    { status: 201 },
-  );
+  return HttpResponse.json({ id: id }, { status: 201 });
 });
 
 const deleteCardHandler = http.delete(
