7fe3df02ca6af3536c39ccc8ea398bfd903ad0c2
jebiyeon02 <jhje5595@naver.com>
2026-05-17 18:09:25 +0900

    feat(issuer-code): 카드회사명을 받아 올바른 issuerCode를 반환

---
+++ src/feature/CardRegister/utils/cardCompany.ts
@@ -0,0 +1,24 @@
+import type { CardCompanyType } from "../../../common/types/CardCompany";
+import { CARD_COMPANY } from "../../CardListPage/constants";
+import type { IssuerCodeType } from "../../CardListPage/types/CardCompay";
+
+export const getIssuerCodeByCompanyName = (
+  companyName: CardCompanyType | null,
+): IssuerCodeType | undefined => {
+  if (!companyName) {
+    console.log("카드사가 존재하지 않습니다.");
+    return;
+  }
+  const foundCompany = Object.entries(CARD_COMPANY).find(
+    ([_, information]) => information.KOR === companyName,
+  );
+
+  if (!foundCompany) {
+    console.log(
+      `${companyName}와(과) 일치하는 카드사 코드를 찾을 수 없습니다.`,
+    );
+    return;
+  }
+
+  return foundCompany[0] as IssuerCodeType;
+};
