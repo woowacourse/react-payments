0ecbc7e2f54e80f91d97149e85c35e9ef4f8b4da
bel1c10ud <mail@bellclou.de>
2026-05-17 23:48:12 +0900

    feat: Text 컴포넌트 align 옵션 및 사이즈 옵션 확장

--- src/components/Common/Text.tsx
+++ src/components/Common/Text.tsx
@@ -1,12 +1,14 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import type { Property } from 'csstype';
 
 const FONT_SIZE = {
   xs: '10px',
   s: '12px',
   m: '14px',
   l: '18px',
-  xl: '24px',
+  xl: '20px',
+  '2xl': '24px',
 } as const;
 
 const FONT_WEIGHT = {
@@ -25,16 +27,42 @@ interface CommonTextProps {
   size?: keyof typeof FONT_SIZE;
   weight?: keyof typeof FONT_WEIGHT;
   color?: keyof typeof FONT_COLOR;
+  align?: Property.TextAlign;
 }
 
 const textStyles = (props: CommonTextProps) => css`
   font-size: ${FONT_SIZE[props.size ?? 'm']};
   font-weight: ${FONT_WEIGHT[props.weight ?? 'medium']};
   color: ${FONT_COLOR[props.color ?? 'black']};
+  ${props.align ? `text-align: ${props.align};` : ''}
 `;
 
 const TextBase = styled.p<CommonTextProps>`
   margin: 0;
+  ${textStyles}
+`;
+
+const H1 = styled.h1<CommonTextProps>`
+  ${textStyles}
+`;
+
+const H2 = styled.h2<CommonTextProps>`
+  ${textStyles}
+`;
+
+const H3 = styled.h3<CommonTextProps>`
+  ${textStyles}
+`;
+
+const H4 = styled.h4<CommonTextProps>`
+  ${textStyles}
+`;
+
+const H5 = styled.h5<CommonTextProps>`
+  ${textStyles}
+`;
+
+const H6 = styled.h6<CommonTextProps>`
   ${textStyles}
 `;
 
@@ -46,6 +74,6 @@ const Label = styled.label<CommonTextProps>`
   ${textStyles}
 `;
 
-const Text = Object.assign(TextBase, { Span, Label });
+const Text = Object.assign(TextBase, { H1, H2, H3, H4, H5, H6, Span, Label });
 
 export default Text;
