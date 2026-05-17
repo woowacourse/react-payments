0f800948e0af8fdfa6b417ef734839ee0ccc0056
박건규 <geongyu09@gmail.com>
2026-05-17 16:43:20 +0900

    feat(useQuery.ts): query api를 처리할 수 있는 커스텀 훅 구현

---
+++ src/hooks/useQuery.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+type QueryState = "loading" | "success" | "error";
+
+interface UseQueryParams<ResponseType> {
+  queryFn: () => Promise<ResponseType>;
+}
+
+const useQuery = <ResponseType>({ queryFn }: UseQueryParams<ResponseType>) => {
+  const [state, setState] = useState<QueryState>("loading");
+  const [data, setData] = useState<ResponseType | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await queryFn();
+        setData(response);
+        setState("success");
+      } catch (err) {
+        setError(err instanceof Error ? err : new Error(String(err)));
+        setState("error");
+      }
+    })();
+  }, [queryFn]);
+
+  return { state, data, error };
+};
+export default useQuery;
