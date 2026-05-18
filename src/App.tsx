36b099b32047635a440ec9b732e6eca962786db9
jebiyeon02 <jhje5595@naver.com>
2026-05-18 13:17:03 +0900

    fix(app): 페이지 컴포넌트 폴더명 변경에 따른 import 구문 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import CardRegisterPage from "./feature/CardRegisterPage/CardRegisterPage";
-import CardRegisterCompletePage from "./feature/CardRegisterComplete/CardRegisterCompletePage";
+
 import styled from "styled-components";
 import CardListPage from "./feature/CardListPage/CardListPage";
+import CardRegisterPage from "./feature/CardRegisterPage/CardRegisterPage";
+import CardRegisterCompletePage from "./feature/CardRegisterCompletePage/CardRegisterCompletePage";
 
 const App = () => {
   return (
