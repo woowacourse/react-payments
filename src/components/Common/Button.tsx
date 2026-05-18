bf2439fc1c99ebc403b2ba092ad7bd1c1abde235
bel1c10ud <mail@bellclou.de>
2026-05-18 11:04:34 +0900

    refactor: Button 컴포넌트 variant 분리

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -1,13 +1,26 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
-const Button = styled.button`
+const variants = {
+  default: css`
+    border-radius: 8px;
+    background: var(--color-card-background);
+    color: var(--color-white);
+  `,
+  ghost: css`
+    background: transparent;
+    color: var(--color-card-background);
+  `,
+};
+
+const Button = styled.button<{ variant: keyof typeof variants }>`
   border: 0;
-  border-radius: 8px;
-  background: var(--color-card-background);
-  color: var(--color-white);
   font-size: 14px;
   font-weight: 700;
   height: 52px;
+  padding: 0 14px;
+
+  ${({ variant }) => variants[variant ?? 'default']}
 
   &:hover {
     cursor: pointer;
