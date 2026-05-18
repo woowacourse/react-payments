5939ed7236776194058fea58203add33af136a57
eunnn <ehddms0517@naver.com>
2026-05-18 14:19:10 +0900

    chore: vitest 테스트 환경 설정

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,15 @@ export default defineConfig({
     projects: [
       {
         extends: true,
+        test: {
+          name: 'unit',
+          environment: 'node',
+          setupFiles: ['./src/mocks/setupTests.ts'],
+          include: ['__tests__/**/*.test.ts', 'src/**/__tests__/**/*.test.ts'],
+        },
+      },
+      {
+        extends: true,
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
