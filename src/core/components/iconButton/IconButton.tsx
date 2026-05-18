76a2201253393e95d341ec09056292c0a7ce0252
stardusty-lab <startdusty.lab@gmail.com>
2026-05-18 11:41:19 +0900

    fix: IconButton 의 기본 as button 으로 설정
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/core/components/iconButton/IconButton.tsx
+++ src/core/components/iconButton/IconButton.tsx
@@ -10,7 +10,7 @@ import styles from './IconButton.module.css';
 
 import type { IconButtonProps } from '.';
 
-export const IconButton = <T extends ElementType>({ as, icon, ...rest }: IconButtonProps<T>) => {
+export const IconButton = <T extends ElementType>({ as = 'button', icon, ...rest }: IconButtonProps<T>) => {
   return (
     <View as={as} className={cn(styles.iconButton)} {...rest}>
       <Icon icon={icon as IconProps['icon']} />
