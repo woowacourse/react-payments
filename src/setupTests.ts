700ca51edda76582cfe0d86779bc85126534ab01
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-19 01:44:20 +0900

    feat: vitest setup 파일 작성

---
+++ src/setupTests.ts
@@ -0,0 +1,7 @@
+import "@testing-library/jest-dom";
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "./mocks/server";
+
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
