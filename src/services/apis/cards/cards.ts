bfa9f8ba1156f89ad4571fdca47fee8fcfafb77b
stardusty-lab <startdusty.lab@gmail.com>
2026-05-18 11:46:11 +0900

    feature: delete api 함수 및 dto 구현
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/services/apis/cards/cards.ts
+++ src/services/apis/cards/cards.ts
@@ -1,3 +1,11 @@
+import type { DeleteCardsRequestDTO } from './dto';
+
 export const getCards = async () => {
   return await fetch('/cards').then((res) => res.json());
+};
+
+export const deleteCards = async ({ id }: DeleteCardsRequestDTO) => {
+  return await fetch(`/cards/${id}`, {
+    method: 'delete',
+  });
 };
--- src/services/apis/cards/dto.ts
+++ src/services/apis/cards/dto.ts
@@ -6,3 +6,7 @@ interface CardRseponseDTO {
 }
 
 export type GetCardsResponseDTO = CardRseponseDTO[];
+
+export interface DeleteCardsRequestDTO {
+  id: string;
+}
