66313e9e9969a9383c6552e11619170aa21274dc
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-18 17:44:45 +0900

    feat: options 변수 export 설정 및 issuerCode 추가

--- src/hooks/useCardBrand.ts
+++ src/hooks/useCardBrand.ts
@@ -1,19 +1,19 @@
 import { useState } from "react";
 
-const options = [
-  { brand: "BC카드", color: "rgba(240, 70, 81, 1)" },
-  { brand: "신한카드", color: "rgba(0, 70, 255, 1)" },
-  { brand: "카카오뱅크", color: "rgba(255, 230, 0, 1)" },
-  { brand: "현대카드", color: "rgba(0,0,0,1)" },
-  { brand: "우리카드", color: "rgba(0,123,200,1)" },
-  { brand: "롯데카드", color: "rgba(237, 28, 36,1)" },
-  { brand: "하나카드", color: "rgba(0,148,144,1)" },
-  { brand: "국민카드", color: "rgba(106,96,86,1)" },
+export const options = [
+  { brand: "BC카드", color: "rgba(240, 70, 81, 1)", issuerCode: "31" },
+  { brand: "신한카드", color: "rgba(0, 70, 255, 1)", issuerCode: "41" },
+  { brand: "카카오뱅크", color: "rgba(255, 230, 0, 1)", issuerCode: "15" },
+  { brand: "현대카드", color: "rgba(0,0,0,1)", issuerCode: "61" },
+  { brand: "우리카드", color: "rgba(0,123,200,1)", issuerCode: "W1" },
+  { brand: "롯데카드", color: "rgba(237, 28, 36,1)", issuerCode: "71" },
+  { brand: "하나카드", color: "rgba(0,148,144,1)", issuerCode: "21" },
+  { brand: "국민카드", color: "rgba(106,96,86,1)", issuerCode: "11" },
 ];
 
 const initialState = {
   isOpen: false,
-  selectedItem: { brand: "", color: "" },
+  selectedItem: { brand: "", color: "", issuerCode: "" },
 };
 
 export function useCardBrand() {
@@ -22,8 +22,16 @@ export function useCardBrand() {
 
   const toggleDropdown = () => setIsOpen(!isOpen);
 
-  const handleItemClick = (option: { brand: string; color: string }) => {
-    setSelectedItem({ brand: option.brand, color: option.color });
+  const handleItemClick = (option: {
+    brand: string;
+    color: string;
+    issuerCode: string;
+  }) => {
+    setSelectedItem({
+      brand: option.brand,
+      color: option.color,
+      issuerCode: option.issuerCode,
+    });
     setIsOpen(false);
   };
 
