b2550b3eb7554f7c3925b1f135180a187df539da
kojesung <js95112345@gmail.com>
2026-05-18 20:11:21 +0900

    feat: CardListPage에서 useMyCard 훅 호출

--- src/pages/CardListPage.tsx
+++ src/pages/CardListPage.tsx
@@ -1,18 +1,19 @@
-import AddCardButton from '../components/CardListResult/AddCardButton';
 import CardListEmpty from '../components/CardListResult/CardListEmpty';
 import CardListError from '../components/CardListResult/CardListError';
 import CardListLoading from '../components/CardListResult/CardListLoading';
 import CardListView from '../components/CardListResult/CardListView';
+import { useMyCard } from '../hooks/useMyCard';
 
 export default function CardListPage() {
+    const { myCards, isLoading, isError, deleteMyCard } = useMyCard();
+
+    if (isLoading) return <CardListLoading />;
+    if (isError) return <CardListError />;
+    if (myCards.length === 0) return <CardListEmpty />;
     return (
         <div>
-            <h3>보유 카드</h3>
-            <CardListView />
-            <AddCardButton />
-            <CardListLoading />
-            <CardListEmpty />
-            <CardListError />
+            <h3>보유 카드({myCards?.length})</h3>
+            <CardListView cardItemInfos={myCards} onDelete={deleteMyCard} />
         </div>
     );
 }
