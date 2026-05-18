57f71e9e6ca609609a1ccb2d09d2ffa85034eb28
binggwa <qudrhks317@gmail.com>
2026-05-18 20:41:55 +0900

    chore: 배포설정 및 PR문구 작성

--- my-app/src/mocks/initMsw.ts
+++ my-app/src/mocks/initMsw.ts
@@ -1,7 +1,7 @@
 const initMsw = async () => {
-  if (process.env.NODE_ENV !== 'development') {
-    return;
-  }
+  // if (process.env.NODE_ENV !== 'development') {
+  //   return;
+  // }
 
   const { worker } = await import('./browser');
   return worker.start({
