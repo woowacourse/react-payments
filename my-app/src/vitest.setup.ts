c23f9fb458334746a165761fa09117f3e4b23d30
binggwa <qudrhks317@gmail.com>
2026-05-18 19:44:39 +0900

    chore: 배포 전 환경설정

--- my-app/src/vitest.setup.ts
+++
@@ -1,6 +0,0 @@
-import { server } from './mocks/node.ts';
-import { beforeAll, afterEach, afterAll } from 'vitest';
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
