8aae302f40e098fee53e22db6b669a8cf7dbca08
gustn99 <azerosong@gmail.com>
2026-05-18 13:58:36 +0900

    refactor: 모조 cards 데이터 내 기본값 1개로 변경

--- src/mocks/datas/cards.ts
+++ src/mocks/datas/cards.ts
@@ -7,10 +7,4 @@ export const cards: SCardList = [
     number: '5511121987769012',
     expirationDate: '12/28',
   },
-  {
-    id: '550e8400-e29b-41d4-a716-446655440001',
-    issuerCode: '31',
-    number: '5511121234569012',
-    expirationDate: '12/28',
-  },
 ];
