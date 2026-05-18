d00b84b006971975a4545f7ab8405df7e742bd14
stardusty-lab <startdusty.lab@gmail.com>
2026-05-18 20:51:02 +0900

    fix: msw 를 위한 mockServiceWorker 경로 수정
    Co-authored-by: iftype conditionaltype@gmail.com
    Co-authored-by: stardusty-lab stardusty.lab@gmail.com

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,15 @@ import { worker } from '@/mocks/msw/browser';
 
 import { AppProviders } from './providers/AppProviders.tsx';
 
+import { ENV } from './configs/env.ts';
+
 import App from './App.tsx';
 
-await worker.start();
+await worker.start({
+  serviceWorker: {
+    url: `${ENV.BASE_URL}mockServiceWorker.js`,
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
