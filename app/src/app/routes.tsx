48afa28fa09807c514edb061efe04f8fc343b2b2
Antoliny0919 <antoliny0919@gmail.com>
2026-05-18 17:59:50 +0900

    feat: 카드리스트 페이지 추가

--- app/src/app/routes.tsx
+++ app/src/app/routes.tsx
@@ -1,6 +1,7 @@
 import { type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
-  route("card", "../pages/card/Create.tsx"),
-  route("card/done", "../pages/card/CreateComplete.tsx"),
+  route("card/", "../pages/card/List.tsx"),
+  route("card/create/", "../pages/card/Create.tsx"),
+  route("card/create/done", "../pages/card/CreateComplete.tsx"),
 ] satisfies RouteConfig;
