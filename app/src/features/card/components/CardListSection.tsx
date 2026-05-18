18d0d6b18a50d87bb616652a2533ba897f5a7334
Antoliny0919 <antoliny0919@gmail.com>
2026-05-18 23:58:37 +0900

    fix: 카드 UI를 위한 Mock 데이터 제거

--- app/src/features/card/components/CardListSection.tsx
+++ app/src/features/card/components/CardListSection.tsx
@@ -6,37 +6,16 @@ import CardList from "./list/CardList";
 export default function CardListSection({ cards }) {
   const navigate = useNavigate();
 
-  const mockCards = [
-    {
-      id: "1234-1234-1234-1234-1234",
-      issuerCode: "31",
-      number: "341112123456901",
-      expirationDate: "12/28",
-    },
-    {
-      id: "1234-1234-1234-1234-1235",
-      issuerCode: "41",
-      number: "36333212345688",
-      expirationDate: "12/28",
-    },
-    {
-      id: "1234-1234-1234-1234-1236",
-      issuerCode: "15",
-      number: "6221261234567129",
-      expirationDate: "12/28",
-    },
-  ];
-
   const gotoCardCreatePage = () => {
     navigate("/card/create/");
   };
 
   return (
     <CardListSectionContainer>
-      <h1>보유 카드 {mockCards.length ? `(${mockCards.length})` : ""}</h1>
+      <h1>보유 카드 {cards.length ? `(${cards.length})` : ""}</h1>
       <Content>
-        {mockCards.length ? (
-          <CardList cards={mockCards} />
+        {cards.length ? (
+          <CardList cards={cards} />
         ) : (
           <EmptyCardList onClick={gotoCardCreatePage} />
         )}
