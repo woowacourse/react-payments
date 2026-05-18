30b9579fa4624d2fc0fe81b104fa5dedfebbecc1
김상윤 <ksy151020@gmail.com>
2026-05-18 20:46:26 +0900

    fix: HashRouter로 교체하여 새로고침 시 404 문제 해결

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 
@@ -19,9 +19,9 @@ async function main() {
 
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <BrowserRouter basename="/react-payments/">
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </StrictMode>,
   );
 }
