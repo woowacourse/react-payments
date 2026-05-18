eb0e317dccbc8c8fc0de2e2abfac80839406ad99
bel1c10ud <mail@bellclou.de>
2026-05-18 11:07:01 +0900

    feat: 카드 삭제 기능 구현

--- src/components/Cards/CardItem.tsx
+++ src/components/Cards/CardItem.tsx
@@ -1,21 +1,47 @@
+import { useCallback, useMemo } from 'react';
 import { CARD_ISSUER } from '../../constants';
-import type { Card } from '../../types';
+import Button from '../Common/Button';
 import Flex from '../Common/Flex';
 import Text from '../Common/Text';
+import { useNavigate } from 'react-router';
+import type { Card } from '../../types';
 
 export default function CardItem(props: { card: Card }) {
-  const issuer = Object.entries(CARD_ISSUER).find(([, info]) => info.issuerCode === props.card.issuerCode);
+  const navigate = useNavigate();
 
-  const cardNumberSegments = props.card.number.split('').reduce(
-    (prev, cur) => {
-      const newArray = [...prev];
-      const lastIndex = newArray.length - 1;
-      if (newArray[lastIndex].length < 4) newArray[lastIndex] = newArray[lastIndex] + cur;
-      else newArray.push(cur);
-      return newArray;
-    },
-    [''],
-  );
+  const issuer = useMemo(() => {
+    return Object.entries(CARD_ISSUER).find(([, info]) => info.issuerCode === props.card.issuerCode);
+  }, [props.card.issuerCode]);
+
+  const cardNumberSegments = useMemo(() => {
+    return props.card.number.split('').reduce(
+      (prev, cur) => {
+        const newArray = [...prev];
+        const lastIndex = newArray.length - 1;
+        if (newArray[lastIndex].length < 4) newArray[lastIndex] = newArray[lastIndex] + cur;
+        else newArray.push(cur);
+        return newArray;
+      },
+      [''],
+    );
+  }, [props.card.number]);
+
+  const handleDelete = useCallback(() => {
+    const id = props.card.id;
+
+    if (!window.confirm(`${cardNumberSegments[0]}로 시작하는 카드를 삭제할게요`)) return;
+
+    fetch(`/cards/${id}`, {
+      method: 'delete',
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error(`HTTP error: ${res.status}`);
+        navigate(0);
+      })
+      .catch(() => {
+        window.alert('카드를 삭제하지 못했어요');
+      });
+  }, [cardNumberSegments, navigate, props.card.id]);
 
   return (
     <Flex
@@ -26,6 +52,7 @@ export default function CardItem(props: { card: Card }) {
         borderRadius: '3px',
         padding: '14px',
       }}
+      data-id={props.card.id}
     >
       <Flex
         style={{
@@ -35,7 +62,7 @@ export default function CardItem(props: { card: Card }) {
           backgroundColor: `var(--color-card-${issuer?.[0]}, var(--color-gray-400))`,
         }}
       ></Flex>
-      <Flex direction="column">
+      <Flex direction="column" flexGrow={1}>
         <Text size="l">{issuer?.[1].label ?? '알 수 없는 카드'}</Text>
         <Flex gap={4}>
           {cardNumberSegments.map((segment, segmentIndex) => (
@@ -48,6 +75,9 @@ export default function CardItem(props: { card: Card }) {
           유효기간 {props.card.expirationDate}
         </Text>
       </Flex>
+      <Button variant="ghost" onClick={handleDelete}>
+        ✕
+      </Button>
     </Flex>
   );
 }
