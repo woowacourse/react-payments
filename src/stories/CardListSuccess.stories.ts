86e21e5ae1d17bdf290bbb92e2e6fca00729d406
boyeon <boyeon6331@gmail.com>
2026-05-18 12:33:53 +0900

    test(CardListISuccess.sotries.ts): 카드 목록 화면 스토리 추가

---
+++ src/stories/CardListSuccess.stories.ts
@@ -0,0 +1,36 @@
+import CardListSuccess from "@/components/CardList/CardListSuccess";
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+const meta = {
+  title: "CardList/CardListSuccess",
+  component: CardListSuccess,
+  args: {
+    cards: [
+      {
+        id: "card-1",
+        issuerCode: "41",
+        number: "551112******9012",
+        expirationDate: "12/28",
+      },
+      {
+        id: "card-2",
+        issuerCode: "61",
+        number: "371234*****1234",
+        expirationDate: "03/29",
+      },
+      {
+        id: "card-3",
+        issuerCode: "W1",
+        number: "361234****1234",
+        expirationDate: "08/30",
+      },
+    ],
+    onAddCard: () => {},
+  },
+} satisfies Meta<typeof CardListSuccess>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
