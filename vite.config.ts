ab6c492d926234659f344dd3ab24de08c1d20a42
janghw0126 <janghw232625@gmail.com>
2026-05-19 03:57:22 +0900

    feat: Vite config에서 BASE_URL 정의 제거

--- vite.config.ts
+++ vite.config.ts
@@ -34,9 +34,6 @@ export default defineConfig({
       },
       {
         extends: true,
-        define: {
-          'import.meta.env.BASE_URL': JSON.stringify('/react-payments/'),
-        },
         test: {
           name: 'unit',
           environment: 'jsdom',
