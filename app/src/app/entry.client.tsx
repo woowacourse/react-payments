c7db5bc775f9b27f3e6b2b64eeddeba882037be7
Antoliny0919 <antoliny0919@gmail.com>
2026-05-19 01:43:13 +0900

    fix: 프로덕트 환경에서도 MSW가 동작하도록 수정

--- app/src/app/entry.client.tsx
+++ app/src/app/entry.client.tsx
@@ -3,12 +3,10 @@ import { hydrateRoot } from "react-dom/client";
 import { HydratedRouter } from "react-router/dom";
 import { worker } from "../features/card/mocks/browser";
 
-if (import.meta.env.DEV) {
-  await worker.start({
-    onUnhandledRequest: "bypass",
-    serviceWorker: { url: "/react-payments/mockServiceWorker.js" },
-  });
-}
+await worker.start({
+  onUnhandledRequest: "bypass",
+  serviceWorker: { url: "/react-payments/mockServiceWorker.js" },
+});
 
 startTransition(() => {
   hydrateRoot(
