590db59bc77649a41b1e9d6eb3b17c056558b426
김상윤 <ksy151020@gmail.com>
2026-05-18 14:25:35 +0900

    test: CardListPage 카드 목록이 있을 때 테스트 추가

--- src/test/CardListPage.test.tsx
+++ src/test/CardListPage.test.tsx
@@ -2,6 +2,9 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import CardListPage from "../pages/CardListPage";
 
+import { server } from "../mocks/server";
+import { http, HttpResponse } from "msw";
+
 test("카드 목록을 불러오는 동안 스켈레톤이 표시된다", () => {
   render(
     <MemoryRouter>
@@ -20,5 +23,28 @@ test("카드가 없으면 빈 목록 안내가 표시된다", async () => {
   );
   expect(
     await screen.findByText("등록된 카드가 없습니다", {}, { timeout: 3000 }),
+  ).toBeInTheDocument();
+});
+
+test("카드 목록이 있을 때 카드 리스트가 화면에 보인다", async () => {
+  server.use(
+    http.get("/cards", () =>
+      HttpResponse.json([
+        {
+          id: "1",
+          issuerCode: "41",
+          number: "411111******1111",
+          expirationDate: "12/28",
+        },
+      ]),
+    ),
+  );
+  render(
+    <MemoryRouter>
+      <CardListPage />
+    </MemoryRouter>,
+  );
+  expect(
+    await screen.findByText("신한카드", {}, { timeout: 3000 }),
   ).toBeInTheDocument();
 });
