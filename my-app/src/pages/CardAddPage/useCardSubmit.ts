2885b0623e4b4f11cdc207b17d8c91361cbd87db
binggwa <qudrhks317@gmail.com>
2026-05-18 16:54:40 +0900

    fix: 카드등록성공페이지 라우팅 시 에러문구 추가로 UX 증진

--- my-app/src/pages/CardAddPage/useCardSubmit.ts
+++ my-app/src/pages/CardAddPage/useCardSubmit.ts
@@ -36,7 +36,7 @@ export const useCardSubmit = (formState: FormState, isFormValid: boolean) => {
         },
       });
     } catch (err) {
-      console.error(err);
+      alert('카드 등록 중 서버 오류가 발생했습니다. 다시 시도해 주세요.');
     }
   };
 
