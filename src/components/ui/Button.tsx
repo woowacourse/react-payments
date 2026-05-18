ff34b2f8eb58bdc14ad2d0dc04dccdefbe9769b4
gustn99 <azerosong@gmail.com>
2026-05-18 17:21:10 +0900

    feat: 로딩 시 button disabled

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -18,6 +18,7 @@ export default function Button({
   return (
     <button
       type={props.type ?? 'button'}
+      disabled={props.disabled || loading}
       css={[buttonStyle, variants[variant], sizes[size], rounded ? roundedStyle : null, loading ? loadingStyle : null]}
       {...props}
     >
