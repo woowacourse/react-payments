3bf1870900d3870669f3cee2550b5440d3abca8c
김상윤 <ksy151020@gmail.com>
2026-05-18 14:17:29 +0900

    test: CardListPage 등록된 카드가 없을때 테스트

--- src/test/CardListPage.test.tsx
+++ src/test/CardListPage.test.tsx
@@ -11,3 +11,14 @@ test("카드 목록을 불러오는 동안 스켈레톤이 표시된다", () => 
 
   expect(screen.getByTestId("card-list-skeleton")).toBeInTheDocument();
 });
+
+test("카드가 없으면 빈 목록 안내가 표시된다", async () => {
+  render(
+    <MemoryRouter>
+      <CardListPage />
+    </MemoryRouter>,
+  );
+  expect(
+    await screen.findByText("등록된 카드가 없습니다", {}, { timeout: 3000 }),
+  ).toBeInTheDocument();
+});
