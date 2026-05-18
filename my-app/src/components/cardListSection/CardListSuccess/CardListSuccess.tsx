0432765bda8175eb7f154c648ac64622e8d03730
binggwa <qudrhks317@gmail.com>
2026-05-18 14:25:35 +0900

    feat: CardListSuccess 컴포넌트 구현

--- my-app/src/components/cardListSection/CardListSuccess/CardListSuccess.tsx
+++ my-app/src/components/cardListSection/CardListSuccess/CardListSuccess.tsx
@@ -1,3 +1,8 @@
+import type { Card } from "../../../pages/CardListPage/useCardList";
+import { getIssuerCodeInfo } from "../../../utils/Validation";
+import DeleteButton from "../../../assets/DeleteButton.svg"
+import { CardAddButton, CardCompany, CardDeleteButton, CardInfoContainer, CardItem, CardListContainer, CardListPageLayout, CardNumber, CardPreviewBox, ExpirationDate, Title } from "./CardListSuccess.styles";
+
 interface Props {
     cards: Card[];
     onClick: () => void;
@@ -8,6 +13,7 @@ const CardListSuccess = ({ cards, onClick, onDelete }: Props) => {
     return (
         <CardListPageLayout>
             <Title>보유 카드 ({cards.length})</Title>
+
             <CardListContainer>
                 {cards.map((card) => {
                     const issuerCodeInfo = getIssuerCodeInfo(card.issuerCode);
@@ -17,17 +23,20 @@ const CardListSuccess = ({ cards, onClick, onDelete }: Props) => {
                             <CardPreviewBox backgroundColor={issuerCodeInfo.color} />
                             <CardInfoContainer>
                                 <CardCompany>{issuerCodeInfo.name}</CardCompany>
-                                <CardNumber></CardNumber>
+                                <CardNumber>{card.number}</CardNumber>
                                 <ExpirationDate>유효기간 {card.expirationDate}</ExpirationDate>
                             </CardInfoContainer>
-                            <CardDeleteButton onClick={() => onDelete?.(card.id)} />
+                            <CardDeleteButton onClick={() => onDelete?.(card.id)} aria-label='카드 삭제'> 
+                              <img src={DeleteButton} alt='삭제 버튼' />
+                            </CardDeleteButton>
                         </CardItem>
                     );
                 })}
+
+                <CardAddButton onClick={onClick} aria-label="카드 추가 버튼">+ 카드 추가</CardAddButton>
             </CardListContainer>
         </CardListPageLayout>
     );
-}
-
+};
 
 export const CardListSuccess;
