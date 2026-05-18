199ade873b6469eb0b65947435f6d8ee5f1ba861
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-18 14:46:42 +0900

    feat: msw browser가 app에 연결되도록 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function enableMocking() {
+  const { worker } = await import("./mocks/browser");
+  return worker.start({
+    serviceWorker: {
+      url: "/react-payments/mockServiceWorker.js",
+    },
+  });
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+});
