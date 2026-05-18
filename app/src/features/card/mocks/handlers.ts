e599dd1c3c2e269c3806eb9f400d050f4dac76bf
Antoliny0919 <antoliny0919@gmail.com>
2026-05-19 00:30:59 +0900

    fix: 잘못된 URL 수정

--- app/src/features/card/mocks/handlers.ts
+++ app/src/features/card/mocks/handlers.ts
@@ -3,7 +3,7 @@ import { CardSerializer } from "../Serializer";
 import db from "./db";
 
 export const handlers = [
-  http.get("https://api.autholibank.com/cards", () => {
+  http.get("https://api.antolibank.com/cards", () => {
     const cards = db.card.getAll();
     return HttpResponse.json({
       ...cards,
