1975c98090b7ee62b621081b989981ec2a32bb29
박건규 <geongyu09@gmail.com>
2026-05-18 19:02:52 +0900

    feat: 카드 리스트 페이지에서 요청 상태에 따라서 ui를 보여주도록 기능 추가

--- src/components/feature/AddCardNavigateButton/AddCardNavigateButton.tsx
+++ src/components/feature/AddCardNavigateButton/AddCardNavigateButton.tsx
@@ -1,12 +1,43 @@
-import useNavigateAddNewCardPage from "@/hooks/feature/navigation/useNavigateAddNewCardPage";
+import Button from "@components/common/Button";
 import DashedButton from "@components/common/DashedButton";
+import SwitchCase from "@components/common/SwitchCase";
+import useNavigateAddNewCardPage from "@hooks/feature/navigation/useNavigateAddNewCardPage";
 
-const AddCardNavigateButton = () => {
+interface AddCardNavigateButtonProps {
+  buttonType?: "default" | "dashed";
+}
+
+const AddCardNavigateButton = ({
+  buttonType = "default",
+}: AddCardNavigateButtonProps) => {
   const navigateToAddNewCardPage = useNavigateAddNewCardPage();
   return (
-    <DashedButton fullWidth onClick={navigateToAddNewCardPage}>
-      + 카드 추가
-    </DashedButton>
+    <SwitchCase
+      value={buttonType}
+      caseBy={[
+        {
+          case: "dashed",
+          component: (
+            <DashedButton fullWidth onClick={navigateToAddNewCardPage}>
+              + 카드 추가
+            </DashedButton>
+          ),
+        },
+        {
+          case: "default",
+          component: (
+            <Button fullWidth onClick={navigateToAddNewCardPage}>
+              + 카드 추가
+            </Button>
+          ),
+        },
+      ]}
+      defaultCase={
+        <Button fullWidth onClick={navigateToAddNewCardPage}>
+          + 카드 추가
+        </Button>
+      }
+    />
   );
 };
 
--- src/components/feature/MyCardListSection/MyCardListSection.tsx
+++ src/components/feature/MyCardListSection/MyCardListSection.tsx
@@ -1,32 +1,43 @@
-import useCards from "@hooks/feature/query/useCards";
 import CardItem from "@components/common/CardItem";
 import styled from "@emotion/styled";
+import useCards from "@hooks/feature/query/useCards";
+import { COLOR_PALETTE } from "@styles/colorPalette";
 
-import MyCardListSectionLoader from "./MyCardListSectionLoader";
 import AddCardNavigateButton from "../AddCardNavigateButton/AddCardNavigateButton";
+import MyCardListSectionErrorFallback from "./MyCardListSectionErrorFallback";
+import MyCardListSectionLoader from "./MyCardListSectionLoader";
 
 const MyCardListSection = () => {
-  const { data, error, state } = useCards();
+  const { data, state } = useCards();
 
-  if (state === "loading") {
+  if (state === "loading" || state === "idle")
     return <MyCardListSectionLoader />;
-  }
 
-  if (state === "error" || !data) {
-    return (
-      <div>카드 정보를 불러오는 중 오류가 발생했습니다: {error?.message}</div>
-    );
-  }
+  if (state === "error" || !data) return <MyCardListSectionErrorFallback />;
 
   return (
     <Wrapper>
-      <Header>보유 카드</Header>
-      <CardContainer>
-        {data.map((props) => (
-          <CardItem {...props} key={props.id} />
-        ))}
-      </CardContainer>
-      <AddCardNavigateButton />
+      <Header>보유 카드 ({data.length})</Header>
+      {data.length > 0 && (
+        <>
+          <CardContainer>
+            {data.map((props) => (
+              <CardItem {...props} key={props.id} />
+            ))}
+          </CardContainer>
+          <AddCardNavigateButton buttonType="dashed" />
+        </>
+      )}
+      {data.length === 0 && (
+        <EmptyStateContainer>
+          <EmptyCard />
+          <EmptyStateHeading>등록된 카드가 없습니다.</EmptyStateHeading>
+          <EmptyStateText>
+            아래 버튼을 눌러 첫 카드를 등록해보세요
+          </EmptyStateText>
+          <AddCardNavigateButton />
+        </EmptyStateContainer>
+      )}
     </Wrapper>
   );
 };
@@ -48,6 +59,40 @@ const CardContainer = styled.ul`
   gap: 1rem;
   list-style: none;
   padding: 0;
+`;
+
+const EmptyStateContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  padding: 2rem;
+  flex: 1;
+`;
+
+const EmptyCard = styled.div`
+  width: 160px;
+  height: 100px;
+  background-color: ${COLOR_PALETTE["BLACK-500"]};
+  border-radius: 5px;
+  border-width: 1px;
+  border-style: dashed;
+  dashes: 6, 4;
+  angle: 0 deg;
+  opacity: 1;
+`;
+
+const EmptyStateHeading = styled.p`
+  font-weight: 700;
+  font-style: Bold;
+  font-size: 20px;
+  margin: 0;
+`;
+
+const EmptyStateText = styled.p`
+  font-weight: 400;
+  font-size: 12px;
+  margin: 0;
 `;
 
 export default MyCardListSection;
