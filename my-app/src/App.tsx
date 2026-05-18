90d2e76bc2acf81e8251c356384569752c477e47
binggwa <qudrhks317@gmail.com>
2026-05-18 15:51:22 +0900

    feat: CardListPage로 기본 라우팅 변경 및 CardAddPage 라우팅 주소 변경

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AppContainer } from './App.styles';
 import CardAddPage from './pages/CardAddPage/CardAddPage';
 import CardAddSuccessPage from './pages/CardAddSuccessPage/CardAddSuccessPage';
+import CardListPage from './pages/CardListPage/CardListPage';
 
 const App = () => {
   return (
     <BrowserRouter basename="/react-payments">
       <AppContainer>
         <Routes>
-          <Route path="/" element={<CardAddPage />} />
+          <Route path="/" element={<CardListPage />} />
+          <Route path="/card-add" element={<CardAddPage />} />
           <Route path="/card-add-success" element={<CardAddSuccessPage />} />
         </Routes>
       </AppContainer>
