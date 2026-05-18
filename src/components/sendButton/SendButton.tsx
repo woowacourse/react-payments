6d0e26dffdbefb8f435066ba0ba0c977bd39734f
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-18 17:45:16 +0900

    feat: 등록 버튼 클릭시, 해당 카드 정보 서버로 post

--- src/components/sendButton/SendButton.tsx
+++ src/components/sendButton/SendButton.tsx
@@ -2,17 +2,71 @@ import styled from "@emotion/styled";
 import { useNavigate } from "react-router-dom";
 import { useCardNumberContext } from "../../context/cardNumber/CardNumberContext";
 import { useCardBrandContext } from "../../context/cardBrand/CardBrandContext";
+import { useExpireDateContext } from "../../context/expireDate/ExpireDateContext";
+import { useCvcContext } from "../../context/cvc/CvcContext";
 
 export default function SendButton() {
   const { cardNumber } = useCardNumberContext();
   const {
-    selectedItem: { brand },
+    selectedItem: { issuerCode },
   } = useCardBrandContext();
+  const { expireDate } = useExpireDateContext();
+  const { cvc } = useCvcContext();
+
   const navigate = useNavigate();
-  const sendResult = () => {
-    navigate("/react-payments/enrollment", {
-      state: { cardNumber, brand },
-    });
+  const sendResult = async () => {
+    try {
+      const requestCardNumber = cardNumber.join("");
+      const requestExpirationDate = `${expireDate.month}/${expireDate.year}`;
+      const requestIssuerCode = issuerCode;
+      const requestCvc = cvc;
+
+      const response = await fetch("/cards", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          number: requestCardNumber,
+          expirationDate: requestExpirationDate,
+          cvc: requestCvc,
+          issuerCode: requestIssuerCode,
+        }),
+      });
+
+      if (!response.ok) {
+        if (response.status === 400) {
+          const errorData = await response.json();
+          throw errorData;
+        } else {
+          throw new Error(
+            `카드 정보를 저장할 수 없습니다. 다시 시도해주세요.: ${response.status}`,
+          );
+        }
+      }
+
+      navigate("/react-payments/enrollment", {
+        state: {
+          cardNumber: requestCardNumber,
+          issuerCode: requestIssuerCode,
+        },
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("에러 발생:", error);
+        alert(error.message || "카드 정보를 저장할 수 없습니다.");
+        return;
+      }
+
+      const customError = error as { code?: string; message?: string };
+      if (customError.code) {
+        console.error(`${customError.code}: ${customError.message}`);
+        alert(customError.message);
+      } else {
+        console.error("알 수 없는 에러 발생:", error);
+        alert("카드 정보를 저장할 수 없습니다.");
+      }
+    }
   };
 
   return (
