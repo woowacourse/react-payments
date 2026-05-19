0ee79636d93e649e5dd294c6f4de7e0940e2db7a
kojesung <js95112345@gmail.com>
2026-05-19 09:26:00 +0900

    init: RTL 초기 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,14 @@ export default defineConfig({
     test: {
         projects: [
             {
+                test: {
+                    name: 'unit',
+                    environment: 'jsdom',
+                    setupFiles: ['./src/test/setup.ts'],
+                    include: ['src/**/*.test.{ts,tsx}'],
+                },
+            },
+            {
                 extends: true,
                 plugins: [
                     // The plugin will run tests for the stories defined in your Storybook config
