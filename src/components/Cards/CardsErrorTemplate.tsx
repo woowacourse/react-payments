46f768e8bbd3f9402f16643e26f63615cd184502
bel1c10ud <mail@bellclou.de>
2026-05-21 18:29:25 +0900

    fix: 카드 목록 에러 템플릿에서 다시 시도가 동작하지 않던 문제 해결

--- src/components/Cards/CardsErrorTemplate.tsx
+++ src/components/Cards/CardsErrorTemplate.tsx
@@ -11,15 +11,28 @@ const Image = styled.img`
   height: 64px;
 `;
 
-export default function CardsErrorTemplate() {
+export default function CardsErrorTemplate(props: { refetcher: () => void }) {
   return (
     <View>
       <Flex direction="column" gap={16}>
         <Text.H1 size="l" weight="bold">
           보유 카드
         </Text.H1>
-        <Flex alignItems="flex-end" customStyle={css`width: 100%; height: 330px;`}>
-          <Flex direction="column" alignItems="center" gap={8} customStyle={css`width: 100%;`}>
+        <Flex
+          alignItems="flex-end"
+          customStyle={css`
+            width: 100%;
+            height: 330px;
+          `}
+        >
+          <Flex
+            direction="column"
+            alignItems="center"
+            gap={8}
+            customStyle={css`
+              width: 100%;
+            `}
+          >
             <Image alt={`error icon`} src={`${import.meta.env.BASE_URL}error.svg`} />
             <Text size="xl" weight="bold">
               카드 목록을 불러올 수 없어요
@@ -28,7 +41,14 @@ export default function CardsErrorTemplate() {
               잠시 후 다시 시도해 주세요.
             </Text>
             <Link to="/" style={{ width: '100%' }}>
-              <Button customStyle={css`width: 100%;`}>다시 시도</Button>
+              <Button
+                customStyle={css`
+                  width: 100%;
+                `}
+                onClick={props.refetcher}
+              >
+                다시 시도
+              </Button>
             </Link>
           </Flex>
         </Flex>
--- src/pages/CardsPage.tsx
+++ src/pages/CardsPage.tsx
@@ -8,7 +8,7 @@ import type { CardsResponse } from '../types/api';
 function CardsPage() {
   const { status, data, error, refetch } = useQuery<CardsResponse>({ url: `${import.meta.env.BASE_URL}cards` });
 
-  if (status === 'error' || error) return <CardsErrorTemplate />;
+  if (status === 'error' || error) return <CardsErrorTemplate refetcher={refetch} />;
   if (status === 'success' && data?.length) return <CardsTemplate data={data} refetcher={refetch} />;
   if (status === 'success' && data?.length === 0) return <CardsEmptyTemplate />;
   return <CardsSkeletonTemplate />;
