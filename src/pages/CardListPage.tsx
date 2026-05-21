7be790402e3113264988583c28b93e1e9bbc1589
김상윤 <ksy151020@gmail.com>
2026-05-21 22:51:16 +0900

    refactor: 테스트, 스켈레톤 파일을 관련 feature 폴더로 이동

--- src/pages/CardListPage.tsx
+++ src/pages/CardListPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import CardListSkeleton from "../components/skeleton/CardListSkeleton";
+import CardListSkeleton from "./CardListSkeleton";
 import EmptyCardList from "./EmptyCardList";
 import CardList from "../components/CardList/CardList";
 import styled from "@emotion/styled";
