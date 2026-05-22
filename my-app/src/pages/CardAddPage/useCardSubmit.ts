08a8e66fec569baeb8cedbc55013548e1f1aa0a1
binggwa <qudrhks317@gmail.com>
2026-05-22 14:04:17 +0900

    refactor: useCardSubmit의 불필요한 try/catch 제거를 통해 useAsync로의 에러 처리 책임 통일

--- my-app/src/pages/CardAddPage/useCardSubmit.ts
+++ my-app/src/pages/CardAddPage/useCardSubmit.ts
@@ -15,22 +15,18 @@ export const useCardSubmit = (formState: FormState, isFormValid: boolean) => {
     // 서버에 보낼 데이터 포매팅
     const sendingData = sendingDataFormatter(formState);
 
-    try {
-      const response = await run(() => cardRepository.postCard(sendingData));
-      if (!response) return;
+    const response = await run(() => cardRepository.postCard(sendingData));
+    if (!response) return;
 
-      // 라우팅 페이지 연결
-      navigate('/card-add-success', {
-        replace: true,
-        state: {
-          id: response.id,
-          cardNumberPrefixFourth: formState.cardNumber[0],
-          cardCompany: formState.cardCompany,
-        },
-      });
-    } catch (err) {
-      alert('카드 등록 중 서버 오류가 발생했습니다. 다시 시도해 주세요.');
-    }
+    // 라우팅 페이지 연결
+    navigate('/card-add-success', {
+      replace: true,
+      state: {
+        id: response.id,
+        cardNumberPrefixFourth: formState.cardNumber[0],
+        cardCompany: formState.cardCompany,
+      },
+    });
   };
 
   const serverError = error as unknown as {
