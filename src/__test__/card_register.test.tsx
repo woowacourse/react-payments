bdce5e95002675fb219e5c5c32cbd5d3389bc9f3
bel1c10ud <mail@bellclou.de>
2026-05-22 14:20:58 +0900

    fix: 테스트 코드 타입 에러 해결

--- src/__test__/card_register.test.tsx
+++ src/__test__/card_register.test.tsx
@@ -1,3 +1,4 @@
+import { beforeAll, expect, afterEach, afterAll, test } from 'vitest';
 import { render, screen, waitFor, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter, Route, Routes } from 'react-router';
