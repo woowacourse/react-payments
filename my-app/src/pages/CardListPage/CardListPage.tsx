5a5e28282ac23eeb0e8577aa3a24b7b1fcb76833
binggwa <qudrhks317@gmail.com>
2026-05-22 17:49:57 +0900

    fix: 카드 삭제 로직에 try/catch 제거 후 useAsync 도입, useEffect로 삭제관련 상태 추적 후 카드목록 다시불러오도록 수정

--- my-app/src/pages/CardListPage/CardListPage.tsx
+++ my-app/src/pages/CardListPage/CardListPage.tsx
@@ -5,21 +5,21 @@ import CardListSuccess from '../../components/cardListSection/CardListSuccess/Ca
 import { useCardList } from './useCardList';
 
 const CardListPage = () => {
-  const { status, cards, fetchCards, handleCardAdd, handleCardDelete } =
+  const { fetchStatus, cards, fetchCards, handleCardAdd, handleCardDelete } =
     useCardList();
 
   // 로딩 중, 기본 상태
-  if (status === 'loading' || status === 'idle') {
+  if (fetchStatus === 'loading' || fetchStatus === 'idle') {
     return <CardListLoading />;
   }
 
   // 에러 상태 ui
-  if (status === 'error') {
+  if (fetchStatus === 'error') {
     return <CardListError onClick={fetchCards} />;
   }
 
   // 성공 상태 목록 빔
-  if (status === 'success' && cards.length === 0) {
+  if (fetchStatus === 'success' && cards.length === 0) {
     return <CardListEmpty onClick={handleCardAdd} />;
   }
 
--- my-app/src/pages/CardListPage/useCardList.ts
+++ my-app/src/pages/CardListPage/useCardList.ts
@@ -7,36 +7,52 @@ import { confirmDialog } from '../../utils/confirmDialog';
 
 export const useCardList = () => {
   const navigate = useNavigate();
-  const { run, status, data } = useAsync<Card[]>();
+  const {
+    run: runFetch,
+    status: fetchStatus,
+    data: cards,
+  } = useAsync<Card[]>();
+  const {
+    run: runDelete,
+    status: deleteStatus,
+    error: deleteError,
+  } = useAsync<void>();
 
   const fetchCards = async () => {
-    await run(() => cardRepositoryInstance.getCards());
+    await runFetch(() => cardRepositoryInstance.getCards());
   };
 
   useEffect(() => {
     fetchCards();
-  }, [run]);
+  }, [runFetch]);
+
+  useEffect(() => {
+    if (deleteStatus === 'error') {
+      alert('카드 삭제에 실패했습니다!');
+    }
+
+    if (deleteStatus === 'success') {
+      alert('카드가 성공적으로 삭제되었습니다.');
+      fetchCards();
+    }
+  }, [deleteStatus, deleteError]);
 
   const handleCardAdd = () => {
     navigate('/card-add');
   };
 
   const handleCardDelete = async (id: string) => {
+    if (deleteStatus === 'loading') return;
+
     const isConfirmed = await confirmDialog('카드를 삭제하시겠습니까?');
     if (!isConfirmed) return;
 
-    try {
-      await cardRepositoryInstance.deleteCard(id);
-      alert('카드가 삭제되었습니다.');
-      fetchCards();
-    } catch (error) {
-      alert('카드 삭제 중 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.');
-    }
+    await runDelete(() => cardRepositoryInstance.deleteCard(id));
   };
 
   return {
-    status,
-    cards: data || [],
+    fetchStatus,
+    cards: cards || [],
     fetchCards,
     handleCardAdd,
     handleCardDelete,
