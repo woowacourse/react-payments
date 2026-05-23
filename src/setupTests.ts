4b8a151ceb595e157206671de51cd96c563786cc
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-23 15:07:22 +0900

    refactor: 테스트 종료 시 CARD 목 데이터 의존성 초기화

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,11 @@
 import "@testing-library/jest-dom";
 import { beforeAll, afterEach, afterAll } from "vitest";
 import { server } from "./mocks/server";
+import { CARD } from "./mocks/cards";
 
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
-afterEach(() => server.resetHandlers());
+afterEach(() => {
+  server.resetHandlers();
+  CARD.splice(0, CARD.length);
+});
 afterAll(() => server.close());
