d64fca5d805e83e4ff71ebf0a169370e169ea02e
박건규 <geongyu09@gmail.com>
2026-05-23 09:51:41 +0900

    fix: produce 환경에서 서버 주소를 찾을 수 있도록 url 등록

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,12 @@ import App from "./App.tsx";
 
 async function enableMocking() {
   const { worker } = await import("./mock/browser");
-  return worker.start({ onUnhandledRequest: "bypass" });
+  return worker.start({
+    onUnhandledRequest: "bypass",
+    serviceWorker: {
+      url: `${import.meta.env.BASE_URL}mockServiceWorker.js`,
+    },
+  });
 }
 
 enableMocking()
