4a6d287cbe5ce5099187f37a72a9cf1b307d45f7
lee-eojin <lejin0906@snu.ac.kr>
2026-05-24 22:11:41 +0900

    refactor: cardStore의 let 배열을 const Map으로 변경

--- my-app/src/mocks/cardStore.ts
+++ my-app/src/mocks/cardStore.ts
@@ -7,24 +7,25 @@ type StoredCard = {
   expirationDate: string;
 };
 
-let cards: StoredCard[] = [];
+const cards = new Map<string, StoredCard>();
 
 const maskNumber = (number: string) => `${number.slice(0, 6)}******${number.slice(-4)}`;
 
 export const store = {
-  list: (): CardListItem[] => cards.map((card) => ({ ...card, number: maskNumber(card.number) })),
+  list: (): CardListItem[] =>
+    [...cards.values()].map((card) => ({ ...card, number: maskNumber(card.number) })),
 
   add: (card: Omit<StoredCard, "id">): string => {
     const id = crypto.randomUUID();
-    cards.push({ id, ...card });
+    cards.set(id, { id, ...card });
     return id;
   },
 
   remove: (id: string) => {
-    cards = cards.filter((card) => card.id !== id);
+    cards.delete(id);
   },
 
   reset: () => {
-    cards = [];
+    cards.clear();
   },
 };
