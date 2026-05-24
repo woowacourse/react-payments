923726b14090c601ef14b3ffb2abfe91aa525b18
박건규 <geongyu09@gmail.com>
2026-05-24 15:52:07 +0900

    feat(useNavigateCompletePage): 네비게이션시 전달하는 상태를 가져오는 훅 추가

--- src/hooks/feature/navigation/useNavigateCompletePage.ts
+++ src/hooks/feature/navigation/useNavigateCompletePage.ts
@@ -1,17 +1,23 @@
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 
-interface NavigateCompletePageParams {
+interface CompletePageState {
   cardNumber: string;
   cardCompany: string;
 }
 
+export const useCompletePageState = () => {
+  const { state } = useLocation() as { state: CompletePageState | null };
+  return {
+    cardNumber: state?.cardNumber ?? "",
+    cardCompany: state?.cardCompany ?? "",
+  };
+};
+
 const useNavigateCompletePage = () => {
   const navigate = useNavigate();
 
-  const navigateToCompletePage = (state: NavigateCompletePageParams) => {
-    navigate("/complete", {
-      state,
-    });
+  const navigateToCompletePage = (state: CompletePageState) => {
+    navigate("/complete", { state });
   };
 
   return navigateToCompletePage;
